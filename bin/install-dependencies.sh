05cfa498b61b5db45ec8696539e07f68af998011
Emily Chen <emily@truss.works>
2017-12-20 11:49:37 -0800

    Fixed installation of yarn

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -16,4 +16,6 @@ sudo apt-get update
 sudo curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get -qq install -y nodejs > /dev/null
-npm install -g yarn
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
