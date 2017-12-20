86f8072ccae6b435b30aa1a1d4ffa22c99903b7d
Emily Chen <emily@truss.works>
2017-12-20 14:33:33 -0800

    Reimplemented fix that accidentally got reverted

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -14,8 +14,8 @@ sudo apt-get -qq install -y apt-transport-https > /dev/null
 # Throwing away stdout logs because they were too plentiful and
 # Circle couldn't display them in their web interface.
 # Errors should still post to the console.
-curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-bash nodesource_setup.sh
+sudo curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
 sudo apt-get -qq install -y nodejs > /dev/null
 sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
