e58b9a2ca4d507e146f15cd684cf8d569f8f0324
Breanne Boland <breanneboland@gmail.com>
2017-12-26 15:49:20 -0800

    moved adapter setup to separate file

---
+++ client/src/setupTests.js
@@ -0,0 +1,5 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Set up enzyme's React adapter for use in other test files
+Enzyme.configure({ adapter: new Adapter() });
