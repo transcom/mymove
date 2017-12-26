5c5fe7483613b30c990eda98688c61e263297f67
Breanne Boland <breanneboland@gmail.com>
2017-12-26 15:53:06 -0800

    test update

--- client/src/scenes/Feedback/FeedbackForm.test.js
+++ client/src/scenes/Feedback/FeedbackForm.test.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import FeedbackForm from './FeedbackForm';
 
-// Enzyme.configure({ adapter: new Adapter() });
-
 describe('<FeedbackForm />', () => {
   it('displays the feedback form, testing with Enzyme/Jest', () => {
     const feedbackFormWrapper = shallow(<FeedbackForm />);
