7afdf543934b25d891783e7a57fff64cf8cfa660
Breanne Boland <breanneboland@gmail.com>
2017-12-26 15:49:20 -0800

    moved adapter setup to separate file

--- client/src/scenes/Feedback/FeedbackForm.test.js
+++ client/src/scenes/Feedback/FeedbackForm.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
 import FeedbackForm from './FeedbackForm';
 
-Enzyme.configure({ adapter: new Adapter() });
+// Enzyme.configure({ adapter: new Adapter() });
 
 describe('<FeedbackForm />', () => {
   it('displays the feedback form, testing with Enzyme/Jest', () => {
