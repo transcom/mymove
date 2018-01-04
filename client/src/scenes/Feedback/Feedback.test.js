93f02cbd08c97edce83f605753797afbc07bf35b
Breanne Boland <breanneboland@gmail.com>
2018-01-04 14:04:55 -0500

    rebasing again

--- client/src/scenes/Feedback/Feedback.test.js
+++ client/src/scenes/Feedback/Feedback.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
 import Feedback from './Feedback';
 
 it('renders without crashing', () => {
@@ -9,8 +8,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<Feedback />, div);
 });
 
-// https://stackoverflow.com/questions/45236277/how-to-test-react-component-class-methods
+// instance() is needed to instantiate the shallow render. The handleChange method needs to be passed an object, rather than just the key's value, to work. More here: https://stackoverflow.com/questions/45236277/how-to-test-react-component-class-methods
 test('it should change state when provided a key and value', () => {
   const wrapper = shallow(<Feedback />);
-  expect(wrapper.instance().handleChange({ target: 'test text' }));
+  wrapper.instance().handleChange({ target: { value: 'test text' } });
+  expect(wrapper.instance().state.value).toEqual('test text');
 });
