66fa83260e2bf3b3f4a1c5881d18eba7bc6be146
Erin Stanfill <erin@truss.works>
2018-01-05 10:45:35 -0500

    SubmittedFeedback loads issues from hardcoded api endpoint

--- client/src/scenes/SubmittedFeedback/index.jsx
+++ client/src/scenes/SubmittedFeedback/index.jsx
@@ -2,14 +2,17 @@
 import React, { Component } from 'react';
 
 import IssueCards from './IssueCards';
-const issues = [
-  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.',
-  'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.',
-  'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. ',
-];
 
 class SubmittedFeedback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { issues: null };
+  }
+  componentDidMount() {
+    this.loadIssues();
+  }
   render() {
+    const { issues } = this.state;
     return (
       <div className="usa-grid">
         <h1>Submitted Feedback</h1>
@@ -17,5 +20,14 @@ class SubmittedFeedback extends Component {
       </div>
     );
   }
+  loadIssues = () => {
+    const config = {
+      mode: 'no-cors',
+    };
+    fetch('http://localhost:8080/api/v1/issues', config)
+      .then(response => response.json())
+      .then(data => this.setState({ issues: data }))
+      .catch(response => console.error(response));
+  };
 }
 export default SubmittedFeedback;
