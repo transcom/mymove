7e542c14351279b2b262a5310e9a6dc842cf3273
Erin Stanfill <erin@truss.works>
2018-01-05 09:23:53 -0500

    converted SubmittedFeedback to class in prep for state`

--- client/src/scenes/SubmittedFeedback/index.jsx
+++ client/src/scenes/SubmittedFeedback/index.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { Component } from 'react';
 
 import IssueCards from './IssueCards';
 const issues = [
@@ -7,10 +7,15 @@ const issues = [
   'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.',
   'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. ',
 ];
-const SubmittedFeedback = () => (
-  <div className="usa-grid">
-    <h1>Submitted Feedback</h1>
-    <IssueCards issues={issues} />
-  </div>
-);
+
+class SubmittedFeedback extends Component {
+  render() {
+    return (
+      <div className="usa-grid">
+        <h1>Submitted Feedback</h1>
+        <IssueCards issues={issues} />
+      </div>
+    );
+  }
+}
 export default SubmittedFeedback;
