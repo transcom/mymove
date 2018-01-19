dfd10f39ba6fa5733bb6f30aa4d6b7bc8b0c4995
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-01-19 14:22:15 -0800

    include new .env file with NODE_PATH var

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'uswds';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'index.css';
+import './index.css';
 import '../node_modules/uswds/dist/css/uswds.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
