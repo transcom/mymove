0b7ee56c7ddd13d93a264625110893064d854f1c
Erin Stanfill <erin@truss.works>
2018-01-22 16:17:32 -0500

    WIP on creation of issues with swagger-client

--- client/src/shared/api.js
+++ client/src/shared/api.js
@@ -4,3 +4,8 @@ export const IssuesIndex = () =>
   Swagger('api/v1/swagger.yaml').then(client => {
     return client.apis.default.indexIssues();
   });
+
+export const CreateIndex = issueBody =>
+  Swagger('api/v1/swagger.yaml').then(client => {
+    return client.apis.default.createIssue({ body: issueBody });
+  });
