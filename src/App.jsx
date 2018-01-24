5fe8b13e56b6f01b5ff5094881adce0c037908ba
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-01-24 11:33:43 -0800

    add combineReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import { appReducer } from './appReducer';
 import AppWrapper from 'shared/App/AppWrapper';
-
 import './App.css';
 
-function issues(state = [], action) {
-  return state;
-}
-
-const store = createStore(issues, {}, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'uswds';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
