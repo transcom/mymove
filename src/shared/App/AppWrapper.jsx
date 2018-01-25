782988354eb9542049fd3cb3b6765e2f976e02a5
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-01-24 16:48:02 -0800

    use react-redux-router, move source to its own file, and add history

--- src/shared/App/AppWrapper.jsx
+++ src/shared/App/AppWrapper.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import Feedback from 'scenes/Feedback/Feedback';
 import SubmittedFeedback from 'scenes/SubmittedFeedback';
 import Header from 'shared/Header/Header';
+import history from 'shared/store';
 import Footer from 'shared/Footer/Footer';
 
 const AppWrapper = () => (
-  <Router>
+  <ConnectedRouter history={history}>
     <div className="App site">
       <Header />
       <main className="site__content">
@@ -15,7 +17,7 @@ const AppWrapper = () => (
       </main>
       <Footer />
     </div>
-  </Router>
+  </ConnectedRouter>
 );
 
 export default AppWrapper;
