de16d6a01a19e5455ae54ba250c67fd2525f377a
MacRae Linton <macrae.linton@gmail.com>
2018-01-25 19:15:37 -0800

    Don't rely on $GOPATH/bin being in a developer's path

--- bin/gen_server.sh
+++ bin/gen_server.sh
@@ -4,4 +4,4 @@ gendir=./pkg/gen
 
 rm -rf $gendir
 mkdir -p $gendir
-swagger generate server -f swagger.yaml -t $gendir --model-package messages --exclude-main -A mymove
+"$GOPATH/bin/swagger" generate server -f swagger.yaml -t $gendir --model-package messages --exclude-main -A mymove
