718bb7220b822b51150699f204f3f2a4173f1c37
Alexi Kostibas <alexi@truss.works>
2018-02-07 18:42:14 -0800

    some comments

--- pkg/awardqueue/awardqueue.go
+++ pkg/awardqueue/awardqueue.go
@@ -10,6 +10,7 @@ import (
 
 var dbConnection *pop.Connection
 
+// This function was made just to get my Golang legs on and play with data
 func makeSomeStuffUp() {
 	tdl1 := models.TrafficDistributionList{
 		SourceRateArea:    "here",
@@ -19,6 +20,7 @@ func makeSomeStuffUp() {
 	dbConnection.ValidateAndSave(&tdl1)
 }
 
+// This function was made just to get my Golang legs on and play with data
 func findAllShipments() error {
 	shipments := []models.Shipment{}
 	err := dbConnection.All(&shipments)
@@ -27,20 +29,37 @@ func findAllShipments() error {
 		return err
 	}
 	fmt.Printf("Shipments:\n%v\n", shipments)
+
 	return nil
 }
 
-func findAllTrafficDistributionLists() {
+// This function was made just to get my Golang legs on and play with data
+func findAllTrafficDistributionLists() error {
 	tdls := []models.TrafficDistributionList{}
 	err := dbConnection.All(&tdls)
 	if err != nil {
 		fmt.Printf("Oh snap! %s", err)
+		return err
 	}
 
 	for i, tdl := range tdls {
 		fmt.Printf("TDL %d:\n%v\n", i, tdl)
 	}
+
+	return nil
 }
+
+/*Run will execute the Award Queue algorithm described below.
+
+- Query for TDLs, so we know what markets exist
+- Query for shipments that do not have a matching awarded_shipment
+- Group shipments by the TDL they belong to
+- For each TDL:
+  - Sort Shipments by BVS
+  - Query TSPs in this TDL, joined on & sorted by BVS
+  - In order of descending BVS, create AwardedShipments matching
+    shipments to TSPs.
+*/
 func Run(db *pop.Connection) {
 	dbConnection = db
 
@@ -50,4 +69,5 @@ func Run(db *pop.Connection) {
 
 	findAllShipments()
 	findAllTrafficDistributionLists()
+
 }
