4914c7661080eef50fe50cf864bba7de2eb542bb
Aileen Chen <aileen@dds.mil>
2018-02-09 23:23:53 -0500

    Add landing page with Sign In button

---
+++ src/scenes/Landing/LoginButton.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class LoginButton extends Component {
+  render() {
+    return <button class="usa-button">Sign In</button>;
+  }
+}
+
+export default LoginButton;
