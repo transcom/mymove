b058f6682ef705702630f6161949d3ff28c3ea5f
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-02-13 15:09:54 -0800

    script to hit staging env to make sure it loads

---
+++ bin/e2e/e2e.js
@@ -0,0 +1,31 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    username = "movemil",
+    accessKey = process.env.SAUCE_ACCESS_KEY,
+    driver;
+
+driver = new webdriver.Builder().
+  withCapabilities({
+    'browserName': 'internet explorer',
+    'platform': 'Windows 8.1',
+    'version': '11.0',
+    'username': username,
+    'accessKey': accessKey
+  }).
+  usingServer("http://" + username + ":" + accessKey +
+              "@ondemand.saucelabs.com:80/wd/hub").
+  build();
+
+driver.get("https://app.staging.dp3.us/");
+
+driver.sleep(2000).then(function() {
+  driver.getTitle().then(function(title) {
+    if(title === 'Transcom PPP: Submit Feedback') {
+      console.log('Test passed');
+    } else {
+      console.log('Test failed');
+    }
+  });
+});
+
+driver.quit();
