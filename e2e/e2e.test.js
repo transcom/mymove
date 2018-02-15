a4d4f0606f775e4656c32e22713bc2f3c0bab492
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-02-15 12:20:42 -0800

    add simple e2e test for shipments and DD1299 pages

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -25,7 +25,7 @@ driver = new webdriver.Builder()
 // jest.set_timeout() doesn't work in this circumstance, so using jasmine timeout.
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
 
-describe('index page loads', () => {
+describe('issue pages', () => {
   beforeEach(() => driver.navigate().to('https://app.staging.dp3.us/'));
 
   it('loads Submit Feedback page', () => {
@@ -33,7 +33,7 @@ describe('index page loads', () => {
     driver.wait(until.titleIs('Transcom PPP: Submit Feedback'), 2000);
   });
 
-  it('allows issue submission', () => {
+  it('allows issue submission and retrieval', () => {
     // Given: A test issue and a feedback form on index page
     test_issue = 'Too few dogs. Time: ' + Date.now();
     driver.wait(until.elementLocated(By.css('[data-test="feedback-form"]')));
@@ -47,6 +47,41 @@ describe('index page loads', () => {
     issue_cards = driver.findElement(By.className('issue-cards'));
     // Expect: Submitted issue exists on page
     driver.wait(until.elementTextContains(issue_cards, test_issue), 1000);
+  });
+});
+
+describe('shipments pages', () => {
+  it('loads all shipments page', () => {
+    // When: Page is loaded, should display expected title
+    driver.navigate().to('https://app.staging.dp3.us/shipments/all');
+    driver.wait(until.titleIs('Transcom PPP: All Shipments'), 2000);
+  });
+
+  it('loads available shipments page', () => {
+    // When: Page is loaded, should display expected title
+    driver.navigate().to('https://app.staging.dp3.us/shipments/available');
+    driver.wait(until.titleIs('Transcom PPP: Available Shipments'), 2000);
+  });
+
+  it('loads awarded shipments page', () => {
+    // When: Page is loaded, should display expected title
+    driver.navigate().to('https://app.staging.dp3.us/shipments/awarded');
+    driver.wait(until.titleIs('Transcom PPP: Awarded Shipments'), 2000);
+  });
+
+  it('displays alert on incorrect url', () => {
+    driver.navigate().to('https://app.staging.dp3.us/shipments/dogs');
+    // Expect: Alert error exists on page
+    driver.wait(until.elementLocated(By.className('usa-alert-error')), 2000);
+  });
+});
+
+describe('DD1299 page', () => {
+  beforeEach(() => driver.navigate().to('https://app.staging.dp3.us/DD1299'));
+
+  it('loads Submit Feedback page', () => {
+    // When: Page is loaded, should display expected title
+    driver.wait(until.titleIs('Transcom PPP: 1299'), 2000);
   });
 });
 
