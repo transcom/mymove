64f8ad65754bcea5a540b07615de1f7723df9200
Aileen Chen <aileen@dds.mil>
2018-02-15 10:57:35 -0500

    Implement auto-discovery and auth request with Goth

---
+++ pkg/auth/auth.go
@@ -0,0 +1,92 @@
+package auth
+
+import (
+	"encoding/base64"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/openidConnect"
+)
+
+// TODO: replace with secret store
+const jwtPrivateKeyEnvVariable = "AUTH_CLIENT_SECRET_KEY"
+const loginGovClientID = "urn:gov:dod:openidconnect:sp:mymovemil"
+const gothProviderType = "openid-connect"
+
+// RegisterProvider registers Login.gov with Goth, which uses
+// auto-discovery to get the OpenID configuration
+func RegisterProvider() {
+	jwtSecret, isSet := os.LookupEnv(jwtPrivateKeyEnvVariable)
+	if !isSet {
+		zap.L().Info("Auth secret key environment variable not set")
+	}
+
+	provider, err := openidConnect.New(
+		loginGovClientID,
+		jwtSecret,
+		"http://localhost:3000/auth/login-gov/callback", "http://localhost:8000/.well-known/openid-configuration")
+
+	if err != nil {
+		zap.L().Error("Register Login.gov provider with Goth", zap.Error(err))
+	}
+
+	goth.UseProviders(provider)
+}
+
+// AuthorizationRedirectHandler constructs the Login.gov authentication URL and redirects to it
+func AuthorizationRedirectHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url, err := getAuthorizationURL(w, r)
+		if err != nil {
+			zap.L().Error("Construct Login.gov authorization URL", zap.Error(err))
+			url = "http://localhost:3000/landing"
+		}
+
+		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	}
+}
+
+func getAuthorizationURL(w http.ResponseWriter, r *http.Request) (string, error) {
+	provider, err := goth.GetProvider(gothProviderType)
+	if err != nil {
+		return "", err
+	}
+	state := generateNonce()
+	sess, err := provider.BeginAuth(state)
+	if err != nil {
+		return "", err
+	}
+
+	baseURL, err := sess.GetAuthURL()
+	if err != nil {
+		return "", err
+	}
+
+	authURL, err := url.Parse(baseURL)
+	if err != nil {
+		return "", err
+	}
+
+	params := authURL.Query()
+	params.Add("acr_values", "http://idmanagement.gov/ns/assurance/loa/1")
+	params.Add("nonce", state)
+	params.Set("scope", "openid email")
+
+	authURL.RawQuery = params.Encode()
+	return authURL.String(), err
+}
+
+func generateNonce() string {
+	nonceBytes := make([]byte, 64)
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 64; i++ {
+		nonceBytes[i] = byte(random.Int63() % 256)
+	}
+	return base64.URLEncoding.EncodeToString(nonceBytes)
+}
