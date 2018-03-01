dbc2a94992eacc1a62465482bed036a19784dcd5
Breanne Boland <breanne@truss.works>
2018-03-01 15:55:06 -0800

    updated model too

--- pkg/models/blackout_dates.go
+++ pkg/models/blackout_dates.go
@@ -19,6 +19,11 @@ type BlackoutDate struct {
 	EndBlackoutDate                 time.Time `json:"end_blackout_date" db:"end_blackout_date"`
 	TrafficDistributionListID       uuid.UUID `json:"traffic_distribution_list_id" db:"traffic_distribution_list_id"`
 	CodeOfService                   string    `json:"code_of_service" db:"code_of_service"`
+	Channel                         string    `json:"channel" db:"channel"`
+	Gbloc                           string    `json:"gbloc" db:"gbloc"`
+	Market                          string    `json:"market" db:"market"`
+	Zip3                            int       `json:"zip3" db:"zip3"`
+	VolumeMove                      bool      `json:"volume_move" db:"volume_move"`
 }
 
 // String is not required by pop and may be deleted
