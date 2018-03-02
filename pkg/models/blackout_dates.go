0067577fa219e29306b9d11f662016891c4763c5
Breanne Boland <breanne@truss.works>
2018-03-01 16:58:00 -0800

    fixed a test broken by a pointer

--- pkg/models/blackout_dates.go
+++ pkg/models/blackout_dates.go
@@ -18,7 +18,7 @@ type BlackoutDate struct {
 	StartBlackoutDate               time.Time  `json:"start_blackout_date" db:"start_blackout_date"`
 	EndBlackoutDate                 time.Time  `json:"end_blackout_date" db:"end_blackout_date"`
 	TrafficDistributionListID       *uuid.UUID `json:"traffic_distribution_list_id" db:"traffic_distribution_list_id"`
-	CodeOfService                   *string    `json:"code_of_service" db:"code_of_service"`
+	CodeOfService                   string     `json:"code_of_service" db:"code_of_service"`
 	Channel                         *string    `json:"channel" db:"channel"`
 	Gbloc                           *string    `json:"gbloc" db:"gbloc"`
 	Market                          *string    `json:"market" db:"market"`
@@ -47,7 +47,8 @@ func (b *BlackoutDate) Validate(tx *pop.Connection) (*validate.Errors, error) {
 	return validate.Validate(
 		&validators.TimeIsPresent{Field: b.StartBlackoutDate, Name: "StartBlackoutDate"},
 		&validators.TimeIsPresent{Field: b.EndBlackoutDate, Name: "EndBlackoutDate"},
-		&validators.StringIsPresent{Field: b.CodeOfService, Name: "CodeOfService"},
+		// &validators.StringIsPresent{Field: b.CodeOfService, Name: "CodeOfService"},
+		// TODO: write our own validator that can validate pointers; Pop lacks that
 	), nil
 }
 
