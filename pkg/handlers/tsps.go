65292702fa76ff2e45b174b30421be43d63cdb5f
Nicholas Twyman <nick@truss.works>
2018-03-07 14:56:35 -0800

    Fixed go-lint errors on comments

--- pkg/handlers/tsps.go
+++ pkg/handlers/tsps.go
@@ -8,13 +8,14 @@ import (
 	"github.com/transcom/mymove/pkg/gen/restapi/apioperations"
 )
 
-// NewCreateIssueHandler returns a new CreateIssueHandler
+// NewTSPIndexHandler simply returns a NotImplemented handler
 func NewTSPIndexHandler(db *pop.Connection, logger *zap.Logger) apioperations.IndexTSPsHandler {
 	return apioperations.IndexTSPsHandlerFunc(func(params apioperations.IndexTSPsParams) middleware.Responder {
 		return middleware.NotImplemented("operation .IndexTSPs has not yet been implemented")
 	})
 }
 
+// NewTSPShipmentsHandler simply returns a NotImplemented handler
 func NewTSPShipmentsHandler(db *pop.Connection, logger *zap.Logger) apioperations.TspShipmentsHandler {
 	return apioperations.TspShipmentsHandlerFunc(func(params apioperations.TspShipmentsParams) middleware.Responder {
 		return middleware.NotImplemented("operation .TspShipments has not yet been implemented")
