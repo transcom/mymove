2f968b360aaadacaaf91934e2a519dff8b2b823d
Andrea Mitchell <8170735+amitch23@users.noreply.github.com>
2018-03-07 16:51:08 -0800

    make selected move type a string to anticipate enum values

--- pkg/models/move.go
+++ pkg/models/move.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/markbates/pop"
 	"github.com/markbates/validate"
+	"github.com/markbates/validate/validators"
 	"github.com/satori/go.uuid"
 	"time"
 )
@@ -14,7 +15,7 @@ type Move struct {
 	CreatedAt        time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt        time.Time `json:"updated_at" db:"updated_at"`
 	UserID           uuid.UUID `json:"user_id" db:"user_id"`
-	SelectedMoveType *int64    `json:"selected_move_type" db:"selected_move_type"`
+	SelectedMoveType *string   `json:"selected_move_type" db:"selected_move_type"`
 }
 
 // String is not required by pop and may be deleted
@@ -35,7 +36,9 @@ func (m Moves) String() string {
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
 func (m *Move) Validate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
+	return validate.Validate(
+		&validators.UUIDIsPresent{Field: m.UserID, Name: "UserID"},
+	), nil
 }
 
 // ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
