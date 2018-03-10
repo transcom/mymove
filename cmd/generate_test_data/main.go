a24a3eb4f1ae3ef2459365d0ccbbfe252a6ee529
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-03-09 17:40:57 -0800

    splitting test scenarios into their own modules

--- cmd/generate_test_data/main.go
+++ cmd/generate_test_data/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"log"
-	"time"
 
-	"github.com/go-openapi/swag"
 	"github.com/markbates/pop"
 	"github.com/namsral/flag"
 	"github.com/transcom/mymove/pkg/testdatagen"
@@ -19,8 +17,8 @@ func main() {
 	env := flag.String("env", "development", "The environment to run in, configures the database, presently.")
 	rounds := flag.String("rounds", "none", "Choose none (no awards), full (1 full round of awards), or half (partial round of awards)")
 	numTSP := flag.Int("numTSP", 15, "The number of TSPs you'd like to create")
-	fullRound := flag.Bool("fullRound", false, "If you want to see how a full round of shipments are awarded.")
-	halfRound := flag.Bool("halfRound", false, "If you want to see how half a round of shipments are awarded.")
+	scenarioOne := flag.Bool("scenarioOne", false, "To run test data generation scenario 1.")
+	scenarioTwo := flag.Bool("scenarioTwo", false, "To run test data generation scenario 2.")
 	flag.Parse()
 
 	//DB connection
@@ -30,63 +28,10 @@ func main() {
 		log.Panic(err)
 	}
 
-	if *fullRound == true {
-		shipmentsToMake := 17
-
-		// Make a TDL to contain our tests
-		tdl, _ := testdatagen.MakeTDL(db, "california", "90210", "2")
-
-		// Make shipments in this TDL
-		for i := 0; i < shipmentsToMake; i++ {
-			testdatagen.MakeShipment(db, time.Now(), time.Now(), tdl)
-		}
-
-		// Make TSPs in the same TDL to handle these shipments
-		tsp1, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 1", "TSP1")
-		tsp2, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 1", "TSP2")
-		tsp3, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 2", "TSP3")
-		tsp4, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 3", "TSP4")
-		tsp5, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 4", "TSP5")
-
-		// TSPs should be orderd by award_count first, then BVS.
-		testdatagen.MakeTSPPerformance(db, tsp1, tdl, swag.Int(1), mps+5, 0)
-		testdatagen.MakeTSPPerformance(db, tsp2, tdl, swag.Int(1), mps+4, 0)
-		testdatagen.MakeTSPPerformance(db, tsp3, tdl, swag.Int(2), mps+3, 0)
-		testdatagen.MakeTSPPerformance(db, tsp4, tdl, swag.Int(3), mps+2, 0)
-		testdatagen.MakeTSPPerformance(db, tsp5, tdl, swag.Int(4), mps+1, 0)
-	} else if *halfRound == true {
-		shipmentsToMake := 9
-
-		// Make a TDL to contain our tests
-		tdl, _ := testdatagen.MakeTDL(db, "california", "90210", "2")
-		tdl2, _ := testdatagen.MakeTDL(db, "New York", "10024", "2")
-
-		// Make shipments in this TDL
-		for i := 0; i < shipmentsToMake; i++ {
-			testdatagen.MakeShipment(db, time.Now(), time.Now(), tdl)
-		}
-
-		// Make TSPs in the same TDL to handle these shipments
-		tsp1, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 1", "TSP1")
-		tsp2, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 1", "TSP2")
-		tsp3, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 2", "TSP3")
-		tsp4, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 3", "TSP4")
-		tsp5, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 4", "TSP5")
-		tsp6, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 1", "TSP6")
-		tsp7, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 2", "TSP7")
-		tsp8, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 3", "TSP8")
-		tsp9, _ := testdatagen.MakeTSP(db, "Test TSP Quality Band 4", "TSP9")
-
-		// TSPs should be orderd by award_count first, then BVS.
-		testdatagen.MakeTSPPerformance(db, tsp1, tdl, swag.Int(1), mps+5, 0)
-		testdatagen.MakeTSPPerformance(db, tsp2, tdl, swag.Int(1), mps+4, 0)
-		testdatagen.MakeTSPPerformance(db, tsp3, tdl, swag.Int(2), mps+3, 0)
-		testdatagen.MakeTSPPerformance(db, tsp4, tdl, swag.Int(3), mps+2, 0)
-		testdatagen.MakeTSPPerformance(db, tsp5, tdl, swag.Int(4), mps+1, 0)
-		testdatagen.MakeTSPPerformance(db, tsp6, tdl2, swag.Int(1), mps+5, 0)
-		testdatagen.MakeTSPPerformance(db, tsp7, tdl2, swag.Int(2), mps+4, 0)
-		testdatagen.MakeTSPPerformance(db, tsp8, tdl2, swag.Int(3), mps+2, 0)
-		testdatagen.MakeTSPPerformance(db, tsp9, tdl2, swag.Int(4), mps+1, 0)
+	if *scenarioOne == true {
+		testdatagen.RunScenarioOne(db)
+	} else if *scenarioTwo == true {
+		testdatagen.RunScenarioTwo(db)
 	} else {
 		// Can this be less repetitive without being overly clever?
 		testdatagen.MakeTDLData(db)
---
+++ pkg/testdatagen/scenario_2.go
@@ -0,0 +1,45 @@
+package testdatagen
+
+import (
+	"time"
+
+	"github.com/go-openapi/swag"
+	"github.com/markbates/pop"
+)
+
+// RunScenarioOne creates 17 shipments and 5 TSPs in 1 TDL. This allows testing against
+// award queue to ensure it behaves as expected. This doesn't track blackout dates.
+func RunScenarioTwo(db *pop.Connection) {
+	shipmentsToMake := 9
+
+	// Make a TDL to contain our tests
+	tdl, _ := MakeTDL(db, "california", "90210", "2")
+	tdl2, _ := MakeTDL(db, "New York", "10024", "2")
+
+	// Make shipments in this TDL
+	for i := 0; i < shipmentsToMake; i++ {
+		MakeShipment(db, time.Now(), time.Now(), tdl)
+	}
+
+	// Make TSPs in the same TDL to handle these shipments
+	tsp1, _ := MakeTSP(db, "Test TSP Quality Band 1", "TSP1")
+	tsp2, _ := MakeTSP(db, "Test TSP Quality Band 1", "TSP2")
+	tsp3, _ := MakeTSP(db, "Test TSP Quality Band 2", "TSP3")
+	tsp4, _ := MakeTSP(db, "Test TSP Quality Band 3", "TSP4")
+	tsp5, _ := MakeTSP(db, "Test TSP Quality Band 4", "TSP5")
+	tsp6, _ := MakeTSP(db, "Test TSP Quality Band 1", "TSP6")
+	tsp7, _ := MakeTSP(db, "Test TSP Quality Band 2", "TSP7")
+	tsp8, _ := MakeTSP(db, "Test TSP Quality Band 3", "TSP8")
+	tsp9, _ := MakeTSP(db, "Test TSP Quality Band 4", "TSP9")
+
+	// TSPs should be orderd by award_count first, then BVS.
+	MakeTSPPerformance(db, tsp1, tdl, swag.Int(1), 5, 0)
+	MakeTSPPerformance(db, tsp2, tdl, swag.Int(1), 4, 0)
+	MakeTSPPerformance(db, tsp3, tdl, swag.Int(2), 3, 0)
+	MakeTSPPerformance(db, tsp4, tdl, swag.Int(3), 2, 0)
+	MakeTSPPerformance(db, tsp5, tdl, swag.Int(4), 1, 0)
+	MakeTSPPerformance(db, tsp6, tdl2, swag.Int(1), 5, 0)
+	MakeTSPPerformance(db, tsp7, tdl2, swag.Int(2), 4, 0)
+	MakeTSPPerformance(db, tsp8, tdl2, swag.Int(3), 2, 0)
+	MakeTSPPerformance(db, tsp9, tdl2, swag.Int(4), 1, 0)
+}
