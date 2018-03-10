270989989a024ec19bface9864d1577a13dbbe64
MacRae Linton <55759+macrael@users.noreply.github.com>
2018-03-09 17:12:52 -0800

    gitfmt

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -1,9 +1,9 @@
 package context
 
 import (
-    "context"
+	"context"
 
-    "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 )
 
 type authCtxKey string
@@ -11,19 +11,22 @@ type authCtxKey string
 var userIDKey = authCtxKey("user_id")
 var idTokenKey = authCtxKey("id_token")
 
-func PopulateAuthContext(ctx context.Context, user_id uuid.UUID, id_token string) context.Context {
-    ctx = context.WithValue(ctx, userIDKey, user_id)
-    ctx = context.WithValue(ctx, idTokenKey, id_token)
+// PopulateAuthContext sets the values that the auth package wants in the context
+func PopulateAuthContext(ctx context.Context, userID uuid.UUID, idToken string) context.Context {
+	ctx = context.WithValue(ctx, userIDKey, userID)
+	ctx = context.WithValue(ctx, idTokenKey, idToken)
 
-    return ctx
+	return ctx
 }
 
+// GetUserID retrieves the UserID from the context
 func GetUserID(ctx context.Context) (uuid.UUID, bool) {
-    id, ok := ctx.Value(userIDKey).(uuid.UUID)
-    return id, ok
+	id, ok := ctx.Value(userIDKey).(uuid.UUID)
+	return id, ok
 }
 
+// GetIDToken retrieves the IDToken from the context
 func GetIDToken(ctx context.Context) (string, bool) {
-    token, ok := ctx.Value(idTokenKey).(string)
-    return token, ok
+	token, ok := ctx.Value(idTokenKey).(string)
+	return token, ok
 }
