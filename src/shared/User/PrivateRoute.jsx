dfab17fb9c8f4857ff7b53bed398daba8a774064
Erin Stanfill <erin@truss.works>
2018-03-13 11:08:01 -0400

    PrivateRoute works with render and children to better match Route API
    [#155870254]

--- src/shared/User/PrivateRoute.jsx
+++ src/shared/User/PrivateRoute.jsx
@@ -5,10 +5,11 @@ import { connect } from 'react-redux';
 import LoginButton from './LoginButton';
 
 const NotAuthenticated = ({ location }) => (
-  <div>
+  <div className="usa-grid">
     <h3>
       Please login to access <code>{location.pathname}</code>
     </h3>
+    <LoginButton />
   </div>
 );
 
@@ -16,24 +17,9 @@ const NotAuthenticated = ({ location }) => (
 // note that it does not work if the route is not inside a Switch
 class PrivateRouteContainer extends React.Component {
   render() {
-    const { isLoggedIn, component: Component, ...props } = this.props;
-    return (
-      <Route
-        {...props}
-        render={props => {
-          if (isLoggedIn) {
-            return <Component {...props} />;
-          } else {
-            return (
-              <div className="usa-grid">
-                <NotAuthenticated location={props.location} />
-                <LoginButton />
-              </div>
-            );
-          }
-        }}
-      />
-    );
+    const { isLoggedIn, path, ...props } = this.props;
+    if (isLoggedIn) return <Route {...props} />;
+    else return <Route path={path} component={NotAuthenticated} />;
   }
 }
 const mapStateToProps = state => ({
