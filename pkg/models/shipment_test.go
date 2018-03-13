a805ff355dd240ed41b95a5b318131b1af688774
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-03-12 17:00:08 -0700

    add pas test

--- pkg/models/shipment_test.go
+++ pkg/models/shipment_test.go
@@ -2,6 +2,8 @@ package models_test
 
 import (
 	. "github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+	"time"
 )
 
 func (suite *ModelSuite) Test_ShipmentValidations() {
@@ -13,4 +15,21 @@ func (suite *ModelSuite) Test_ShipmentValidations() {
 	}
 
 	suite.verifyValidationErrors(shipment, expErrors)
+}
+
+// Test_FetchPossiblyAwardedShipments tests that a shipment is returned when we fetch possibly awarded shipments
+func (suite *ModelSuite) Test_FetchPossiblyAwardedShipments() {
+	t := suite.T()
+	now := time.Now()
+	tdl, _ := testdatagen.MakeTDL(suite.db, "california", "90210", "2")
+	shipment, _ := testdatagen.MakeShipment(suite.db, now, now.AddDate(0, 0, 1), tdl)
+
+	shipments, err := FetchPossiblyAwardedShipments(suite.db)
+
+	if err != nil {
+		t.Errorf("Failed to find Shipments: %v", err)
+	} else if shipments[0].ID != shipment.ID {
+		t.Errorf("Wrong shipment returned: expected %s, got %s",
+			shipment.ID, shipments[0].ID)
+	}
 }
