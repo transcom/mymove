7f8f715b3b061c2ca0a699c42b2e0777d5f67ebc
Patrick Stanger <patrick@truss.works>
2018-03-14 09:51:35 -0700

    Hooks up index moves handler to route

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -114,6 +114,7 @@ func main() {
 	internalAPI.ShipmentsIndexShipmentsHandler = handlers.IndexShipmentsHandler(handlerContext)
 
 	internalAPI.MovesCreateMoveHandler = handlers.CreateMoveHandler(handlerContext)
+	internalAPI.MovesIndexMovesHandler = handlers.NewIndexMovesHandler(dbConnection, logger)
 
 	// Serves files out of build folder
 	clientHandler := http.FileServer(http.Dir(*build))
