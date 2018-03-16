c3723bbe7fa04e703c4c86c44cde89cb261e14e7
Erin Stanfill <erin@truss.works>
2018-03-16 11:19:56 -0400

    wip

--- src/appReducer.js
+++ src/appReducer.js
@@ -1,18 +1,22 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { routerReducer } from 'react-router-redux';
+
+import userReducer from 'shared/User/ducks';
+import swaggerReducer from 'shared/Swagger/ducks';
+
 import { feedbackReducer } from 'scenes/Feedback/ducks';
 import { moveReducer } from 'scenes/Moves/ducks';
 import { ppmReducer } from 'scenes/Moves/Ppm/ducks';
-import { default as userReducer } from 'shared/User/ducks';
 import issuesReducer from 'scenes/SubmittedFeedback/ducks';
 import { shipmentsReducer } from 'scenes/Shipments/ducks';
 import dd1299Reducer from 'scenes/DD1299/ducks';
 import { signedCertificationReducer } from 'scenes/Legalese/ducks';
 import { documentReducer } from 'shared/Uploader/ducks';
-import { reducer as formReducer } from 'redux-form';
-import { routerReducer } from 'react-router-redux';
 
 export const appReducer = combineReducers({
   user: userReducer,
+  // swagger: swaggerReducer,
   submittedIssues: issuesReducer,
   submittedMoves: moveReducer,
   ppm: ppmReducer,
---
+++ src/shared/ReduxHelpers/index.js
@@ -0,0 +1,80 @@
+function actionName(resource, actionType) {
+  return `${resource}_${actionType.toUpperCase()}`;
+}
+
+function generateActionTypes(resourceName, actionTypes) {
+  resourceName = (resourceName || '').trim();
+  if (!resourceName) throw new Error('No resource name provided');
+  let actions = {};
+  actionTypes.forEach(actionType => {
+    actions[actionType] = actionName(resourceName, actionType);
+  });
+  return actions;
+}
+
+export function generateAsyncActionTypes(resourceName) {
+  return generateActionTypes(resourceName, ['start', 'success', 'failure']);
+}
+
+export function generateAsyncActions(resourceName) {
+  const actionTypes = generateAsyncActionTypes(resourceName);
+  const actions = {
+    start: () => ({
+      type: actionTypes.start,
+    }),
+    success: payload => ({
+      type: actionTypes.success,
+      payload,
+    }),
+    error: error => ({
+      type: actionTypes.failure,
+      error,
+    }),
+  };
+  return actions;
+}
+
+export function generateAsyncActionCreator(resourceName, asyncAction) {
+  const actions = generateAsyncActions(resourceName);
+  return function actionCreator(...args) {
+    return function(dispatch) {
+      dispatch(actions.start());
+      asyncAction(...args)
+        .then(item => dispatch(actions.success(item)))
+        .catch(error => dispatch(actions.error(error)));
+    };
+  };
+}
+
+export function generateAsyncReducer(resourceName, onSuccess) {
+  const actions = generateAsyncActionTypes(resourceName);
+  const initialState = { isLoading: false, hasErrored: false };
+  return function(state = initialState, action) {
+    switch (action.type) {
+      case actions.start:
+        return Object.assign({}, state, {
+          isLoading: true,
+          hasErrored: false,
+        });
+      case action.success: {
+        const result = Object.assign({}, state, {
+          isLoading: false,
+          hasErrored: false,
+        });
+        result[resourceName] = onSuccess(action.payload);
+        return result;
+      }
+      case action.failure: {
+        const result = Object.assign({}, state, {
+          isLoading: false,
+          hasErrored: true,
+          error: action.error,
+        });
+        result[resourceName] = null;
+        return result;
+      }
+      default:
+        return state;
+    }
+  };
+}
