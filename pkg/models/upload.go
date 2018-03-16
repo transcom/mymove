e7113c2af00d3cf0fd07240e4859d2e397187ac9
Jim Benton <jim@truss.works>
2018-03-16 17:15:13 -0500

    Add migrations and structs for documents and uploads.

---
+++ pkg/models/upload.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/markbates/pop"
+	"github.com/markbates/validate"
+	"github.com/markbates/validate/validators"
+	"github.com/pkg/errors"
+	"github.com/satori/go.uuid"
+)
+
+// An Upload represents an uploaded file, such as an image or PDF.
+type Upload struct {
+	ID          uuid.UUID `db:"id"`
+	DocumentID  uuid.UUID `db:"document_id"`
+	UploaderID  uuid.UUID `db:"uploader_id"`
+	Filename    string    `db:"filename"`
+	Bytes       int64     `db:"bytes"`
+	ContentType string    `db:"content_type"`
+	Checksum    string    `db:"checksum"`
+	S3ID        uuid.UUID `db:"s3_id"`
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (u Upload) String() string {
+	ju, _ := json.Marshal(u)
+	return string(ju)
+}
+
+// Uploads is not required by pop and may be deleted
+type Uploads []Upload
+
+// String is not required by pop and may be deleted
+func (u Uploads) String() string {
+	ju, _ := json.Marshal(u)
+	return string(ju)
+}
+
+func (u *Upload) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.UUIDIsPresent{Field: u.DocumentID, Name: "DocumentID"},
+		&validators.UUIDIsPresent{Field: u.UploaderID, Name: "UploaderID"},
+		&validators.StringIsPresent{Field: u.Filename, Name: "Filename"},
+		// TODO create an Int64IsPresent validator
+		&validators.IntIsPresent{Field: int(u.Bytes), Name: "Bytes"},
+		&validators.StringIsPresent{Field: u.ContentType, Name: "ContentType"},
+		&validators.StringIsPresent{Field: u.Checksum, Name: "Checksum"},
+	), nil
+}
+
+// BeforeCreate sets an Upload's S3ID before it is saved the first time
+func (u *Upload) BeforeCreate(tx *pop.Connection) error {
+	id, err := uuid.NewV4()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	u.S3ID = id
+	return nil
+}
---
+++ pkg/testdatagen/make_move.go
@@ -0,0 +1,34 @@
+package testdatagen
+
+import (
+	"log"
+
+	"github.com/markbates/pop"
+
+	"github.com/transcom/mymove/pkg/gen/internalmessages"
+	"github.com/transcom/mymove/pkg/models"
+)
+
+func MakeMove(db *pop.Connection) (models.Move, error) {
+	var move models.Move
+
+	user, err := MakeUser(db)
+	if err != nil {
+		return move, err
+	}
+
+	move = models.Move{
+		UserID:           user.ID,
+		SelectedMoveType: internalmessages.SelectedMoveTypeCOMBO,
+	}
+
+	verrs, err := db.ValidateAndSave(&move)
+	if err != nil {
+		log.Panic(err)
+	}
+	if verrs.Count() != 0 {
+		log.Panic(verrs.Error())
+	}
+
+	return move, err
+}
