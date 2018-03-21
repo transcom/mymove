830efa6135d493860a034749f8d6e23966755656
Andrea Mitchell <8170735+amitch23@users.noreply.github.com>
2018-03-21 11:43:39 -0700

    fix css

--- src/scenes/Moves/PpmSize/index.jsx
+++ src/scenes/Moves/PpmSize/index.jsx
@@ -12,14 +12,14 @@ import { createPpm } from './ducks';
 
 export class BigButton extends Component {
   render() {
-    let className = 'size-button';
+    let className = 'ppm-size-button';
     if (this.props.selected) {
       className += ' selected';
     }
     return (
       <div className={className} onClick={this.props.onButtonClick}>
-        <div>{this.props.firstLine}</div>
-        <div>{this.props.secondLine}</div>
+        <p>{this.props.firstLine}</p>
+        <p>{this.props.secondLine}</p>
         <img className="icon" src={this.props.icon} />
       </div>
     );
@@ -35,43 +35,36 @@ BigButton.propTypes = {
 };
 
 export class BigButtonGroup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedOption: null,
-    };
-  }
   render() {
     var createButton = (value, firstLine, secondLine, icon) => {
       var onButtonClick = () => {
-        this.setState({ selectedOption: value });
         this.props.onMoveTypeSelected(value);
       };
       return (
         <BigButton
+          value={value}
           firstLine={firstLine}
           secondLine={secondLine}
           icon={icon}
-          selected={this.state.selectedOption === value}
+          selected={this.props.selectedOption === value}
           onButtonClick={onButtonClick}
         />
       );
     };
-    console.log(this.state.selectedOption);
     var small = createButton(
-      'small',
+      'S',
       'A few items in your car?',
       '(approx 100 - 800 lbs)',
       carGray,
     );
     var medium = createButton(
-      'medium',
+      'M',
       'A trailer full of household goods?',
       '(approx 400 - 1,200 lbs)',
       trailerGray,
     );
     var large = createButton(
-      'large',
+      'L',
       'A moving truck that you rent yourself?',
       '(approx 1,000 - 5,000 lbs)',
       truckGray,
@@ -79,7 +72,7 @@ export class BigButtonGroup extends Component {
 
     return (
       <div>
-        <div className="usa-width-one-third container">{small}</div>
+        <div className="usa-width-one-third">{small}</div>
         <div className="usa-width-one-third">{medium}</div>
         <div className="usa-width-one-third">{large}</div>
       </div>
@@ -87,7 +80,7 @@ export class BigButtonGroup extends Component {
   }
 }
 BigButtonGroup.propTypes = {
-  selectedValue: PropTypes.string,
+  selectedOption: PropTypes.string,
   onMoveTypeSelected: PropTypes.func,
 };
 
@@ -109,7 +102,10 @@ export class PpmSize extends Component {
     return (
       <div className="usa-grid-full ppm-size-content">
         <h3>How much of your stuff do you intend to move yourself?</h3>
-        <BigButtonGroup onMoveTypeSelected={this.onMoveTypeSelected} />
+        <BigButtonGroup
+          selectedOption={this.state.selectedOption}
+          onMoveTypeSelected={this.onMoveTypeSelected}
+        />
       </div>
     );
   }
