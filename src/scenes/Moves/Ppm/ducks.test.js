660f65d1437113a06d6652572db132244dd9a492
Andrea Mitchell <8170735+amitch23@users.noreply.github.com>
2018-03-22 13:55:38 -0700

    fix ducks test for create or update ppm

--- src/scenes/Moves/Ppm/ducks.test.js
+++ src/scenes/Moves/Ppm/ducks.test.js
@@ -1,33 +1,38 @@
-import { CREATE_PPM_SUCCESS, CREATE_PPM_FAILURE, ppmReducer } from './ducks';
+import {
+  CREATE_OR_UPDATE_PPM_SUCCESS,
+  CREATE_OR_UPDATE_PPM_FAILURE,
+  ppmReducer,
+} from './ducks';
 
-describe('Feedback Reducer', () => {
-  it('Should handle CREATE_PPM_SUCCESS', () => {
+describe('Ppm Reducer', () => {
+  it('Should handle CREATE_OR_UPDATE_PPM_SUCCESS', () => {
     const initialState = { pendingValue: '' };
 
     const newState = ppmReducer(initialState, {
-      type: CREATE_PPM_SUCCESS,
+      type: CREATE_OR_UPDATE_PPM_SUCCESS,
       item: 'Successful ppm!',
     });
 
     expect(newState).toEqual({
       pendingValue: '',
+      pendingPpmSize: null,
       currentPpm: 'Successful ppm!',
       hasSubmitError: false,
       hasSubmitSuccess: true,
     });
   });
 
-  it('Should handle CREATE_PPM_FAILURE', () => {
+  it('Should handle CREATE_OR_UPDATE_PPM_FAILURE', () => {
     const initialState = { pendingValue: '' };
 
     const newState = ppmReducer(initialState, {
-      type: CREATE_PPM_FAILURE,
+      type: CREATE_OR_UPDATE_PPM_FAILURE,
       error: 'No bueno.',
     });
 
     expect(newState).toEqual({
       pendingValue: '',
-      currentPpm: {},
+      currentPpm: null,
       hasSubmitError: true,
       hasSubmitSuccess: false,
     });
