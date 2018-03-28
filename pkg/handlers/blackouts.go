431e0b27ab439eed62eb837d8a098aea6e5e2f74
Nicholas Twyman <nick@truss.works>
2018-03-28 16:18:31 -0700

    Added endpoints to delete and update blackouts/shipments

--- pkg/handlers/blackouts.go
+++ pkg/handlers/blackouts.go
@@ -12,3 +12,27 @@ type BlackoutIndexHandler HandlerContext
 func (h BlackoutIndexHandler) Handle(params apioperations.IndexBlackoutsParams) middleware.Responder {
 	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
 }
+
+// DeleteBlackoutHandler returns a list of all the Blackouts
+type DeleteBlackoutHandler HandlerContext
+
+// Handle simply returns a NotImplementedError
+func (h DeleteBlackoutHandler) Handle(params apioperations.DeleteBlackoutParams) middleware.Responder {
+	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+}
+
+// GetBlackoutHandler returns a list of all the Blackouts
+type GetBlackoutHandler HandlerContext
+
+// Handle simply returns a NotImplementedError
+func (h GetBlackoutHandler) Handle(params apioperations.GetBlackoutParams) middleware.Responder {
+	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+}
+
+// UpdateBlackoutHandler returns a list of all the Blackouts
+type UpdateBlackoutHandler HandlerContext
+
+// Handle simply returns a NotImplementedError
+func (h UpdateBlackoutHandler) Handle(params apioperations.UpdateBlackoutParams) middleware.Responder {
+	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+}
