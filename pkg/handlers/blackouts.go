b26e64212179689c20e1bef9d8995d3356d8112d
Nicholas Twyman <nick@truss.works>
2018-03-29 10:54:41 -0700

    Added CreateBlackout endpoint

--- pkg/handlers/blackouts.go
+++ pkg/handlers/blackouts.go
@@ -5,12 +5,20 @@ import (
 	"github.com/transcom/mymove/pkg/gen/restapi/apioperations"
 )
 
+// CreateBlackoutHandler returns a list of all the Blackouts
+type CreateBlackoutHandler HandlerContext
+
+// Handle simply returns a NotImplementedError
+func (h CreateBlackoutHandler) Handle(params apioperations.CreateBlackoutParams) middleware.Responder {
+	return middleware.NotImplemented("operation .createBlackout has not yet been implemented")
+}
+
 // BlackoutIndexHandler returns a list of all the Blackouts
 type BlackoutIndexHandler HandlerContext
 
 // Handle simply returns a NotImplementedError
 func (h BlackoutIndexHandler) Handle(params apioperations.IndexBlackoutsParams) middleware.Responder {
-	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+	return middleware.NotImplemented("operation .indexBlackouts has not yet been implemented")
 }
 
 // DeleteBlackoutHandler returns a list of all the Blackouts
@@ -18,7 +26,7 @@ type DeleteBlackoutHandler HandlerContext
 
 // Handle simply returns a NotImplementedError
 func (h DeleteBlackoutHandler) Handle(params apioperations.DeleteBlackoutParams) middleware.Responder {
-	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+	return middleware.NotImplemented("operation .deleteBlackout has not yet been implemented")
 }
 
 // GetBlackoutHandler returns a list of all the Blackouts
@@ -26,7 +34,7 @@ type GetBlackoutHandler HandlerContext
 
 // Handle simply returns a NotImplementedError
 func (h GetBlackoutHandler) Handle(params apioperations.GetBlackoutParams) middleware.Responder {
-	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+	return middleware.NotImplemented("operation .getBlackout has not yet been implemented")
 }
 
 // UpdateBlackoutHandler returns a list of all the Blackouts
@@ -34,5 +42,5 @@ type UpdateBlackoutHandler HandlerContext
 
 // Handle simply returns a NotImplementedError
 func (h UpdateBlackoutHandler) Handle(params apioperations.UpdateBlackoutParams) middleware.Responder {
-	return middleware.NotImplemented("operation .indexTSPs has not yet been implemented")
+	return middleware.NotImplemented("operation .updateBlackout has not yet been implemented")
 }
