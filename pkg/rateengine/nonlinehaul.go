727e25d90c5677e70875b2d1ac279de91afd9238
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-04-04 16:24:06 -0700

    full nonlinehaulcharge mockout

--- pkg/rateengine/nonlinehaul.go
+++ pkg/rateengine/nonlinehaul.go
@@ -1,8 +1,54 @@
 package rateengine
 
-func (re *RateEngine) originServiceFee(weight int, zip string) (float64, error) {
-	serviceArea := 3
-	rate := 0.45
+import (
+	"errors"
+	"fmt"
+)
 
-	return float64(serviceArea) * rate, nil
+func (re *RateEngine) serviceFee(weight int, zip string) (float64, error) {
+	fmt.Print(zip)
+	// TODO: Fetch service area from zip
+	serviceArea := 3
+	fmt.Print(serviceArea)
+	// TODO: Fetch 135A or 135B Rate
+	rate := 3.88
+	return float64(weight/100) * rate, nil
+}
+
+func (re *RateEngine) fullPack(weight int, zip string) (float64, error) {
+	fmt.Print(zip)
+	// TODO: Fetch service area from zip
+	serviceArea := 3
+	fmt.Print(serviceArea)
+	// TODO: Fetch service schedule from service area
+	serviceSchedule := 1
+	fmt.Print(serviceSchedule)
+	// TODO: Fetch fullpack rate
+	rate := 55.00
+	return float64(weight/100) * rate, nil
+}
+
+func (re *RateEngine) fullUnpack(weight int, zip string) (float64, error) {
+	fmt.Print(zip)
+	// TODO: Fetch service area from zip
+	serviceArea := 3
+	fmt.Print(serviceArea)
+	// TODO: Fetch service schedule from service area
+	serviceSchedule := 1
+	fmt.Print(serviceSchedule)
+	// TODO: Fetch full unpack rate
+	rate := 5.00
+	return float64(weight/100) * rate, nil
+}
+
+func (re *RateEngine) nonLinehaulChargeTotal(originZip string, destinationZip string, inverseDiscount float64) (float64, error) {
+	weight := 4000
+	originServiceFee, err := re.serviceFee(weight, originZip)
+	destinationServiceFee, err := re.serviceFee(weight, destinationZip)
+	pack, err := re.fullPack(weight, originZip)
+	unpack, err := re.fullUnpack(weight, destinationZip)
+	if err != nil {
+		err = errors.New("Oops nonlinehaulChargeTotal")
+	}
+	return (originServiceFee + destinationServiceFee + pack + unpack) * inverseDiscount, err
 }
--- pkg/rateengine/nonlinehaul_test.go
+++ pkg/rateengine/nonlinehaul_test.go
@@ -1,16 +1,61 @@
 package rateengine
 
-func (suite *RateEngineSuite) Test_originServiceFee() {
+func (suite *RateEngineSuite) Test_CheckServiceFee() {
 	t := suite.T()
 	engine := NewRateEngine(suite.db, suite.logger)
 
-	fee, err := engine.originServiceFee(2500, "18209")
+	fee, err := engine.serviceFee(2500, "18209")
 	if err != nil {
-		t.Fatalf("failed to calculate origin service fee: %s", err)
+		t.Fatalf("failed to calculate service fee: %s", err)
 	}
 
-	expected := 1.35
+	expected := float64(97)
 	if fee != expected {
-		t.Errorf("wrong origin service fee: expected %f, got %f", expected, fee)
+		t.Errorf("wrong service fee: expected %f, got %f", expected, fee)
+	}
+}
+
+func (suite *RateEngineSuite) Test_CheckFullPack() {
+	t := suite.T()
+	engine := NewRateEngine(suite.db, suite.logger)
+
+	fee, err := engine.fullPack(2500, "18209")
+	if err != nil {
+		t.Fatalf("failed to calculate full pack fee: %s", err)
+	}
+
+	expected := float64(1375)
+	if fee != expected {
+		t.Errorf("wrong full pack fee: expected %f, got %f", expected, fee)
+	}
+}
+
+func (suite *RateEngineSuite) Test_CheckFullUnpack() {
+	t := suite.T()
+	engine := NewRateEngine(suite.db, suite.logger)
+
+	fee, err := engine.fullUnpack(2500, "18209")
+	if err != nil {
+		t.Fatalf("failed to calculate full pack fee: %s", err)
+	}
+
+	expected := float64(125)
+	if fee != expected {
+		t.Errorf("wrong full pack fee: expected %f, got %f", expected, fee)
+	}
+}
+
+func (suite *RateEngineSuite) Test_CheckNonLinehaulChargeTotal() {
+	t := suite.T()
+	engine := NewRateEngine(suite.db, suite.logger)
+
+	fee, err := engine.nonLinehaulChargeTotal("10024", "18209", 0.5)
+	if err != nil {
+		t.Fatalf("failed to calculate full pack fee: %s", err)
+	}
+	// (155.2 + 155.2 + 2200 + 200) * .5
+	expected := float64(1355.2)
+	if fee != expected {
+		t.Errorf("wrong non-linehaul charge total: expected %f, got %f", expected, fee)
 	}
 }
