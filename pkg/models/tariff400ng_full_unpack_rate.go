16319df5b39a95f3912b81ec8bdbd529b9f09228
Jim Benton <jim@truss.works>
2018-04-05 11:51:26 -0500

    Remove generated cruft.

--- pkg/models/tariff400ng_full_unpack_rate.go
+++ pkg/models/tariff400ng_full_unpack_rate.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 
 	"github.com/gobuffalo/pop"
@@ -12,29 +11,17 @@ import (
 
 // Tariff400ngFullUnpackRate describes the rates paid to unpack various weights of goods
 type Tariff400ngFullUnpackRate struct {
-	ID                 uuid.UUID `json:"id" db:"id"`
-	CreatedAt          time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt          time.Time `json:"updated_at" db:"updated_at"`
-	Schedule           int       `json:"schedule" db:"schedule"`
-	RateMillicents     int       `json:"rate_millicents" db:"rate_millicents"`
-	EffectiveDateLower time.Time `json:"effective_date_lower" db:"effective_date_lower"`
-	EffectiveDateUpper time.Time `json:"effective_date_upper" db:"effective_date_upper"`
-}
-
-// String is not required by pop and may be deleted
-func (t Tariff400ngFullUnpackRate) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
+	ID                 uuid.UUID `db:"id"`
+	CreatedAt          time.Time `db:"created_at"`
+	UpdatedAt          time.Time `db:"updated_at"`
+	Schedule           int       `db:"schedule"`
+	RateMillicents     int       `db:"rate_millicents"`
+	EffectiveDateLower time.Time `db:"effective_date_lower"`
+	EffectiveDateUpper time.Time `db:"effective_date_upper"`
 }
 
 // Tariff400ngFullUnpackRates is not required by pop and may be deleted
 type Tariff400ngFullUnpackRates []Tariff400ngFullUnpackRate
-
-// String is not required by pop and may be deleted
-func (t Tariff400ngFullUnpackRates) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
 
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
@@ -45,16 +32,4 @@ func (t *Tariff400ngFullUnpackRate) Validate(tx *pop.Connection) (*validate.Erro
 			FirstTime: t.EffectiveDateUpper, FirstName: "EffectiveDateUpper",
 			SecondTime: t.EffectiveDateLower, SecondName: "EffectiveDateLower"},
 	), nil
-}
-
-// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
-// This method is not required and may be deleted.
-func (t *Tariff400ngFullUnpackRate) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
-// This method is not required and may be deleted.
-func (t *Tariff400ngFullUnpackRate) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
 }
--- pkg/models/tariff400ng_linehaul_rate.go
+++ pkg/models/tariff400ng_linehaul_rate.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 
 	"github.com/gobuffalo/pop"
@@ -13,33 +12,21 @@ import (
 // Tariff400ngLinehaulRate describes the rate paids paid to transport various weights of goods
 // various distances.
 type Tariff400ngLinehaulRate struct {
-	ID                 uuid.UUID `json:"id" db:"id"`
-	CreatedAt          time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt          time.Time `json:"updated_at" db:"updated_at"`
-	DistanceMilesLower int       `json:"distance_miles_lower" db:"distance_miles_lower"`
-	DistanceMilesUpper int       `json:"distance_miles_upper" db:"distance_miles_upper"`
-	Type               string    `json:"type" db:"type"`
-	WeightLbsLower     int       `json:"weight_lbs_lower" db:"weight_lbs_lower"`
-	WeightLbsUpper     int       `json:"weight_lbs_upper" db:"weight_lbs_upper"`
-	RateCents          int       `json:"rate_cents" db:"rate_cents"`
-	EffectiveDateLower time.Time `json:"effective_date_lower" db:"effective_date_lower"`
-	EffectiveDateUpper time.Time `json:"effective_date_upper" db:"effective_date_upper"`
-}
-
-// String is not required by pop and may be deleted
-func (t Tariff400ngLinehaulRate) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
+	ID                 uuid.UUID `db:"id"`
+	CreatedAt          time.Time `db:"created_at"`
+	UpdatedAt          time.Time `db:"updated_at"`
+	DistanceMilesLower int       `db:"distance_miles_lower"`
+	DistanceMilesUpper int       `db:"distance_miles_upper"`
+	Type               string    `db:"type"`
+	WeightLbsLower     int       `db:"weight_lbs_lower"`
+	WeightLbsUpper     int       `db:"weight_lbs_upper"`
+	RateCents          int       `db:"rate_cents"`
+	EffectiveDateLower time.Time `db:"effective_date_lower"`
+	EffectiveDateUpper time.Time `db:"effective_date_upper"`
 }
 
 // Tariff400ngLinehaulRates is not required by pop and may be deleted
 type Tariff400ngLinehaulRates []Tariff400ngLinehaulRate
-
-// String is not required by pop and may be deleted
-func (t Tariff400ngLinehaulRates) String() string {
-	jt, _ := json.Marshal(t)
-	return string(jt)
-}
 
 // Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
 // This method is not required and may be deleted.
@@ -54,16 +41,4 @@ func (t *Tariff400ngLinehaulRate) Validate(tx *pop.Connection) (*validate.Errors
 			FirstTime: t.EffectiveDateUpper, FirstName: "EffectiveDateUpper",
 			SecondTime: t.EffectiveDateLower, SecondName: "EffectiveDateLower"},
 	), nil
-}
-
-// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
-// This method is not required and may be deleted.
-func (t *Tariff400ngLinehaulRate) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
-}
-
-// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
-// This method is not required and may be deleted.
-func (t *Tariff400ngLinehaulRate) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
-	return validate.NewErrors(), nil
 }
