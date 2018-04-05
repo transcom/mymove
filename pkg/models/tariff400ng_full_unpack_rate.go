2bc5dc81c3eb15335fa7a28967b596760e47ddc0
Alexi Kostibas <alexi@truss.works>
2018-04-04 17:21:56 -0700

    adding models for pack and unpack

---
+++ pkg/models/tariff400ng_full_unpack_rate.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+)
+
+// Tariff400ngFullUnpackRate describes the rate paid for a certain weight range for a certain
+// schedule
+type Tariff400ngFullUnpackRate struct {
+	ID                 uuid.UUID `json:"id" db:"id"`
+	CreatedAt          time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at" db:"updated_at"`
+	Schedule           int       `json:"schedule" db:"schedule"`
+	RateMillicents     int       `json:"rate_millicents" db:"rate_millicents"`
+	EffectiveDateLower time.Time `json:"effective_date_lower" db:"effective_date_lower"`
+	EffectiveDateUpper time.Time `json:"effective_date_upper" db:"effective_date_upper"`
+}
+
+// String is not required by pop and may be deleted
+func (t Tariff400ngFullUnpackRate) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Tariff400ngFullUnpackRates is not required by pop and may be deleted
+type Tariff400ngFullUnpackRates []Tariff400ngFullUnpackRate
+
+// String is not required by pop and may be deleted
+func (t Tariff400ngFullUnpackRates) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (t *Tariff400ngFullUnpackRate) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.IntIsGreaterThan{Field: t.RateMillicents, Name: "RateMillicents", Compared: -1},
+		&validators.TimeAfterTime{
+			FirstTime: t.EffectiveDateUpper, FirstName: "EffectiveDateUpper",
+			SecondTime: t.EffectiveDateLower, SecondName: "EffectiveDateLower"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (t *Tariff400ngFullUnpackRate) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (t *Tariff400ngFullUnpackRate) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
