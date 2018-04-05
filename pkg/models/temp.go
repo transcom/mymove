b58e8b061723f3c192390691d0f3e8c87867109f
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-04-05 14:42:14 -0700

    add comments to pass tests

--- pkg/models/temp.go
+++ pkg/models/temp.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gobuffalo/pop"
 )
 
+// ServiceAreaForZip3 is the service area for a specific Zip3
 func ServiceAreaForZip3(db *pop.Connection, zip3 string) (int, error) {
 	return 3, nil
 }
 
+// Rate135A is the service charge for origin per cwt
 func Rate135A(db *pop.Connection, serviceArea int) (float64, error) {
 	return 3.88, nil
 }
