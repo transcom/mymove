5ff711066a2d31da1d386df0e0ec22eaec45ca55
Breanne Boland <breanne@truss.works>
2018-04-12 17:13:33 -0700

    more tweaking the query and table

--- dataload/create_rates.sql
+++ dataload/create_rates.sql
@@ -10,7 +10,7 @@ CREATE TABLE temp_tsp_discount_rates (
 	sit_rate numeric(6,2)
 );
 
-\copy temp_tsp_discount_rates FROM '/Users/jimb/go/src/github.com/transcom/mymove/tsp_rates/2018 Code 2 Peak Rates.txt' WITH CSV HEADER;
+\copy temp_tsp_discount_rates FROM '/Users/breanneboland/Desktop/datadump/2018 Code 2 Peak Rates.txt' WITH CSV HEADER;
 
 DROP TABLE IF EXISTS temp_tdl_scores;
 
@@ -27,16 +27,35 @@ CREATE TABLE temp_tdl_scores (
 	bvs numeric(8,4)
 );
 
-\copy temp_tdl_scores FROM '/Users/jimb/go/src/github.com/transcom/mymove/tsp_rates/(Pre-Decisional FOUO) TDL Scores 15May18-30September18 - Code 2.csv' WITH CSV HEADER;
+\copy temp_tdl_scores FROM '/Users/breanneboland/Desktop/datadump/(Pre-Decisional FOUO) TDL Scores 15May18-30September18 - Code 2.csv' WITH CSV HEADER;
 
+DROP TABLE IF EXISTS tdl_scores_and_discounts;
 
-/* SELECT * FROM temp_tdl_scores AS s */
-/* 	LEFT JOIN temp_tsp_discount_rates AS dr */
-/* 	ON s.origin = dr.origin */
-/* 		AND s.destination = dr.destination */
-/* 		AND s.cos = dr.cos */
-/* 		AND s.scac = dr.scac; */
+-- CREATE TABLE tdl_scores_and_discounts (
+-- 	rate_cycle text,
+-- 	market text,
+-- 	origin text,
+-- 	destination text,
+-- 	cos text,
+-- 	quartile int,
+-- 	rank int,
+-- 	scac text,
+-- 	lh_rate numeric(6,2),
+-- 	sit_rate numeric(6,2)	,
+-- 	svy_score numeric(8,4),
+-- 	rate_score numeric(8,4),
+-- 	bvs numeric(8,4),
+-- 	rate_cycle_start date DEFAULT 2018-05-15,
+-- 	rate_cycle_end date DEFAULT 2018-09-30
+-- );
 
-/* effective_date_lower date, */
-/* effective_date_upper date */
-/* UPDATE temp_tdl_scores (effective_date_lower, effective_date_upper) VALUES ('2018-05-15', '2018-09-30'); */
+CREATE TABLE tdl_scores_and_discounts AS
+	SELECT dr.rate_cycle, s.market, s.origin, s.destination, s.cos, s.quartile, s.rank, s.scac, s.bvs, dr.lh_rate, dr.sit_rate FROM temp_tdl_scores AS s
+	LEFT JOIN temp_tsp_discount_rates as dr
+ 	ON s.origin = dr.origin
+ 	AND s.destination = dr.destination
+	AND s.cos = dr.cos
+	AND s.scac = dr.scac;
+
+ALTER TABLE tdl_scores_and_discounts ADD COLUMN effective_date_lower date DEFAULT '2018-05-15';
+ALTER TABLE tdl_scores_and_discounts ADD COLUMN effective_date_upper date DEFAULT '2018-09-30';
