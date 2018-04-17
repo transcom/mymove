ab1b77a15187669c4e0fe492cc5752da532262e2
Breanne Boland <breanne@truss.works>
2018-04-16 17:09:17 -0700

    got data into tsp table with all needed nulls denulled

--- migrations/20180416160830_add_scacs_and_uuids_to_tsp_table.up.sql
+++ migrations/20180416160830_add_scacs_and_uuids_to_tsp_table.up.sql
@@ -1,8 +1,9 @@
+-- Remove (for now) constraint requiring TSP name field to be not null
+ALTER TABLE ONLY transportation_service_providers ALTER COLUMN name DROP not null;
+
 -- Import SCACs and UUIDs into existing transportation_service_providers table
-SELECT
-  id,
-  scac
-  INTO transportation_service_providers
-  FROM transportation_service_provider_data;
+INSERT INTO transportation_service_providers (id, standard_carrier_alpha_code, created_at, updated_at)
+SELECT id, scac, CURRENT_TIMESTAMP as created_at, CURRENT_TIMESTAMP as updated_at
+FROM transportation_service_provider_data;
 
 DROP TABLE transportation_service_provider_data;
