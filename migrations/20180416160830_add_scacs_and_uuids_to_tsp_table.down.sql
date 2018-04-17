90a9fd106a76cbcdbb4a85a7da91366403844827
Breanne Boland <breanne@truss.works>
2018-04-17 10:38:43 -0700

    fixed down.sql file so it'll work properly

--- migrations/20180416160830_add_scacs_and_uuids_to_tsp_table.down.sql
+++ migrations/20180416160830_add_scacs_and_uuids_to_tsp_table.down.sql
@@ -1,9 +1,1 @@
-DROP TABLE transportation_service_providers;
-
-CREATE TABLE transportation_service_providers (
-    id uuid NOT NULL,
-    standard_carrier_alpha_code text NOT NULL,
-    name text NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
+TRUNCATE TABLE transportation_service_providers CASCADE;
