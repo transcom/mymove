48128cbad2c1a7facbc45b8df5690741e1d15a69
Andrea Mitchell <8170735+amitch23@users.noreply.github.com>
2018-04-20 08:43:42 -1000

    add assets from design

--- src/scenes/Orders/TransitionToMove.jsx
+++ src/scenes/Orders/TransitionToMove.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import ordersComplete from 'shared/images/orders_check.png';
-import moveIcon from 'shared/images/move_icon.png';
+import ordersComplete from 'shared/images/orders-complete-gray-icon.png';
+import moveIcon from 'shared/images/move-icon.png';
 
 export class TransitionToMove extends Component {
   componentDidMount() {
@@ -11,20 +11,13 @@ export class TransitionToMove extends Component {
       <div className="usa-grid">
         <div className="lg center">
           <p> Great, we're done with your orders.</p>
-          <img className="sm Todo" src={ordersComplete} alt="profile-check" />
+          <img className="sm" src={ordersComplete} alt="profile-check" />
         </div>
 
         <div className="lg center">
           <p>Now, we're ready to schedule your move!</p>
-          <img className="sm Todo" src={moveIcon} alt="onto-move-orders" />
+          <img className="sm" src={moveIcon} alt="onto-move-orders" />
         </div>
-
-        <ul className="Todo">
-          <li>
-            Replace screenshots of icons with svgs once we get assets from
-            design.
-          </li>
-        </ul>
       </div>
     );
   }
