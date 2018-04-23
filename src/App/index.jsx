eff642737999a14353d5841e5cea0a9d613d924d
Erin Stanfill <erin@truss.works>
2018-04-23 19:19:29 -0400

    renamed shared/AppWrapper to scenes/MyMove, created scenes/Office and setup App to use dynamic imports to load appropriate app
    [#156926353]

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 
-import AppWrapper from 'shared/AppWrapper';
 import store from 'shared/store';
 import './index.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <AppWrapper />
-      </Provider>
-    );
-  }
-}
+import Loadable from 'react-loadable';
+
+const Office = Loadable({
+  loader: () => import('scenes/Office'),
+  loading: () => <div>Loading...</div>,
+});
+
+const MyMove = Loadable({
+  loader: () => import('scenes/MyMove'),
+  loading: () => <div>Loading...</div>,
+});
+
+const officeSiteNames = [
+  'bolocal',
+  'office.staging.dp3.us',
+  'office.prod.dp3.us',
+];
+const hostname = window && window.location && window.location.hostname;
+console.log(hostname);
+const isOfficeSite = officeSiteNames.includes(hostname);
+const App = () => (
+  <Provider store={store}>{!isOfficeSite ? <MyMove /> : <Office />}</Provider>
+);
 
 export default App;
