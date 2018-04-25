fa7488517ed9989794ac4f410925f95a3783f7d7
Patrick Stanger <patrick@truss.works>
2018-04-25 12:46:55 -0700

    Manually runs dependency update | Wed Apr 25 2018

--- pkg/models/backup_contact.go
+++ pkg/models/backup_contact.go
@@ -66,7 +66,7 @@ func (b *BackupContact) ValidateUpdate(tx *pop.Connection) (*validate.Errors, er
 // FetchBackupContact returns a specific backup contact model
 func FetchBackupContact(db *pop.Connection, authUser User, id uuid.UUID) (BackupContact, error) {
 	var contact BackupContact
-	err := db.Eager().Find(&contact, id)
+	err := db.Q().Eager().Find(&contact, id)
 	if err != nil {
 		if errors.Cause(err).Error() == RecordNotFoundErrorString {
 			return BackupContact{}, ErrFetchNotFound
