ae2d121e6fd1bdb32eec80ef1389012d19c99671
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-04-30 15:23:20 -0700

    icons and alignments

--- src/shared/Header/Office/index.jsx
+++ src/shared/Header/Office/index.jsx
@@ -1,63 +1,47 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
+import magnifyingGlass from '../../../../node_modules/uswds/src/img/search.svg';
 import LoginButton from 'shared/User/LoginButton';
+
+import './index.css';
 
 function QueueHeader() {
   return (
-    <header className="usa-header usa-header-basic" role="banner">
-      <div className="usa-nav-container">
-        <div className="usa-navbar">
-          <button className="usa-menu-btn">Menu</button>
-          <div className="usa-logo" id="basic-logo">
-            <em className="usa-logo-text">
-              <NavLink to="/" title="Home" aria-label="Admin Move.Mil">
-                admin.move.mil
-              </NavLink>
-            </em>
-          </div>
+    <header role="banner">
+      <div className="officeHeaderOne">
+        <h3>admin.move.mil</h3>
+      </div>
+      <div className="officeHeaderTwo">
+        <button>Queues</button>
+      </div>
+      <div className="officeHeaderThree">
+        <div className="forms-shared">
+          <form>
+            <input type="text" placeholder="Search" name="search" />
+          </form>
         </div>
-        <nav className="usa-nav">
-          <ul className="usa-nav-primary usa-accordion">
-            <li>
-              <NavLink to="/" className="usa-nav-link">
-                <div className="button_wrapper">
-                  <button className="usa-button">Queues</button>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <form className="usa-search usa-search-small">
-                <div role="search">
-                  <label className="usa-sr-only" htmlFor="search-field-small">
-                    Search small
-                  </label>
-                  <input id="search-field-small" type="search" name="search" />
-                  <button type="submit">
-                    <span className="usa-sr-only">Search</span>
-                  </button>
-                </div>
-              </form>
-            </li>
-            <li>
-              <form className="usa-form">
-                <select name="options" id="options">
-                  <option value>All</option>
-                  <option value="locator">Move Locator</option>
-                  <option value="edipi">DOD ID</option>
-                  <option value="customerName">Name (Last, First)</option>
-                  <option value="gbl">GBL</option>
-                  <option value="order">Orders</option>
-                  <option value="status">Status</option>
-                </select>
-              </form>
-            </li>
-            <li>Welcome Janelle!</li>
-            <li>
-              <LoginButton />
-            </li>
-          </ul>
-        </nav>
+        <form className="usa-form forms-shared">
+          <select name="options" id="options">
+            <option value>All</option>
+            <option value="locator">Move Locator</option>
+            <option value="edipi">DOD ID</option>
+            <option value="customerName">Name (Last, First)</option>
+            <option value="gbl">GBL</option>
+            <option value="order">Orders</option>
+            <option value="status">Status</option>
+          </select>
+        </form>
+        <button>
+          <img src={magnifyingGlass} alt="Search" />
+        </button>
+      </div>
+      <div className="officeHeaderFour">
+        <ul className="usa-nav-primary">
+          <li>Welcome, Janelle</li>
+          <li>
+            <LoginButton />
+          </li>
+        </ul>
       </div>
     </header>
   );
