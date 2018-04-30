59b5be251b795601b297edbeacfb1154ee1fbe8d
Erin Stanfill <erin@truss.works>
2018-04-30 17:46:58 -0400

    updated react upload page to work with modified endpoint

--- src/shared/Uploader/api.js
+++ src/shared/Uploader/api.js
@@ -5,25 +5,27 @@ export async function GetSpec() {
   return client.spec;
 }
 
-export async function CreateUpload(fileUpload, moveID, documentID) {
+export async function CreateUpload(fileUpload, documentId) {
+  console.log(arguments);
   const client = await getClient();
   const response = await client.apis.uploads.createUpload({
     file: fileUpload,
-    moveId: moveID,
-    documentId: documentID,
+    documentId,
   });
   checkResponse(response, 'failed to upload file due to server error');
   return response.body;
 }
 
-export async function CreateDocument(fileUpload, moveID) {
+export async function CreateDocument(fileUpload, serviceMemberId) {
   const client = await getClient();
   const response = await client.apis.documents.createDocument({
-    documentPayload: { name: 'document' },
-    moveId: moveID,
+    documentPayload: {
+      name: 'document',
+      service_member_id: serviceMemberId,
+    },
   });
   checkResponse(response, 'failed to create document due to server error');
-  return CreateUpload(fileUpload, moveID, response.body.id);
+  return CreateUpload(fileUpload, response.body.id);
 }
 
 export default CreateDocument;
