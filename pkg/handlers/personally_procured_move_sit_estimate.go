69374c146a3ba1bb463373624349c6c50ccb4704
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-05-03 14:55:45 -0700

    correct zap logging

--- pkg/handlers/personally_procured_move_sit_estimate.go
+++ pkg/handlers/personally_procured_move_sit_estimate.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
+	"go.uber.org/zap"
 
 	ppmop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/ppm"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
@@ -21,26 +22,27 @@ func (h ShowPPMSitEstimateHandler) Handle(params ppmop.ShowPPMSitEstimateParams)
 	engine := rateengine.NewRateEngine(h.db, h.logger, h.planner)
 	sitZip3 := engine.Zip5ToZip3(params.DestinationZip)
 	cwtWeight := unit.Pound(params.WeightEstimate).ToCWT()
-	sitTotal, err := engine.SitCharge(cwtWeight, int(params.DaysInStorage), sitZip3, time.Time(params.PlannedMoveDate), true)
+	plannedMoveDateTime := time.Time(params.PlannedMoveDate)
+	sitTotal, err := engine.SitCharge(cwtWeight, int(params.DaysInStorage), sitZip3, plannedMoveDateTime, true)
 	if err != nil {
 		return responseForError(h.logger, err)
 	}
 
 	// Most PPMs use COS D, but when there is no COS D rate, the calculation is based on Code 2
-	_, sitDiscount, err := models.FetchDiscountRates(h.db, params.OriginZip, params.DestinationZip, "D", time.Time(params.PlannedMoveDate))
+	_, sitDiscount, err := models.FetchDiscountRates(h.db, params.OriginZip, params.DestinationZip, "D", plannedMoveDateTime)
 	if err != nil {
 		if err != models.ErrFetchNotFound {
 			return responseForError(h.logger, err)
 		}
-		h.logger.Info("Couldn't find SIT Discount for COS D, trying with COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.String("move_date", params.PlannedMoveDate), zap.Error(err))
+		h.logger.Info("Couldn't find SIT Discount for COS D, trying with COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.Time("move_date", plannedMoveDateTime), zap.Error(err))
 		_, sitDiscount, err = models.FetchDiscountRates(h.db, params.OriginZip, params.DestinationZip, "2", time.Time(params.PlannedMoveDate))
 		if err != nil {
-			h.logger.Info("Couldn't find SIT Discount for COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.String("move_date", params.PlannedMoveDate), zap.Error(err))
+			h.logger.Info("Couldn't find SIT Discount for COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.Time("move_date", plannedMoveDateTime), zap.Error(err))
 			return responseForError(h.logger, err)
 		}
-		h.logger.Info("Found SIT Discount for TDL with COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.String("move_date", params.PlannedMoveDate))
+		h.logger.Info("Found SIT Discount for TDL with COS 2.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.Time("move_date", plannedMoveDateTime))
 	} else {
-		h.logger.Info("Found SIT Discount for TDL with COS D.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.String("move_date", params.PlannedMoveDate))
+		h.logger.Info("Found SIT Discount for TDL with COS D.", zap.String("origin_zip", params.OriginZip), zap.String("destination_zip", params.DestinationZip), zap.Time("move_date", plannedMoveDateTime))
 	}
 
 	inverseDiscount := (100.00 - sitDiscount) / 100
