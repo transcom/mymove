400e857883665402fe084769a00f6aa95475d77e
Patrick Stanger <patrick@truss.works>
2018-05-04 08:44:43 -1000

    Adds package that mocks out Canvas API for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-canvas-mock';
 
 // Set up enzyme's React adapter for use in other test files
 Enzyme.configure({ adapter: new Adapter() });
