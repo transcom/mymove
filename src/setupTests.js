2c8209a528795abedc1aaca9b6bfd634b3eef789
Erin Stanfill <erin@truss.works>
2018-05-04 12:28:12 -0400

    stubbing performance.now so Filepond doesn't explode tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@ import Adapter from 'enzyme-adapter-react-16';
 
 // Set up enzyme's React adapter for use in other test files
 Enzyme.configure({ adapter: new Adapter() });
+global.performance = {
+  now: function() {
+    return Date.now();
+  },
+};
