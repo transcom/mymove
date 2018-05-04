8924a0b69647fed3142a34955aa86cf16ef740d4
Erin Stanfill <erin@truss.works>
2018-05-04 15:40:54 -0400

    corrected className

--- src/shared/JsonSchemaForm/SingleDatePicker.jsx
+++ src/shared/JsonSchemaForm/SingleDatePicker.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 
-export default function Example(props) {
+export default function SingleDatePicker(props) {
   const {
     input: { value = null, onChange },
   } = props;
