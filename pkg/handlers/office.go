0dd7abbd6c1463ea460c5d5275c5f81c7b6a20de
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-05-09 17:00:58 -0700

    start office handlers

--- pkg/handlers/office.go
+++ pkg/handlers/office.go
@@ -10,7 +10,7 @@ import (
 	"github.com/transcom/mymove/pkg/models"
 )
 
-// ShowAccountingHandler creates a new service member via GET /move/{moveId}/accounting
+// ShowAccountingHandler creates a new service member via GET /moves/{moveId}/accounting
 type ShowAccountingHandler HandlerContext
 
 // Handle ... creates a new ServiceMember from a request payload
@@ -29,9 +29,11 @@ func (h ShowAccountingHandler) Handle(params officeop.ShowAccountingParams) midd
 	if err != nil {
 		return responseForError(h.logger, err)
 	}
+
+	return officeop.NewShowAccountingInfoOK().WithPayload(&ppmEstimate)
 }
 
-// PatchAccountingHandler patches a move's accounting information via PATCH /move/{moveId}/accounting
+// PatchAccountingHandler patches a move's accounting information via PATCH /moves/{moveId}/accounting
 type PatchAccountingHandler HandlerContext
 
 // Handle ... patches a new ServiceMember from a request payload
@@ -45,4 +47,33 @@ func (h PatchAccountingHandler) Handle(params officeop.PatchAccountingParams) mi
 	if err != nil {
 		return responseForError(h.logger, err)
 	}
+
+	accountingInfo, err := FetchAccountingInfo(h.db, moveID)
+	if err != nil {
+		return responseForError(h.logger, err)
+	}
+	payload := params.PatchAccountingParams
+	newTAC := payload.Tac
+	newDeptIndicator := payload.DeptIndicator
+
+	if newTAC != nil {
+		// TODO: Set TAC here
+	}
+
+	if newDeptIndicator != nil {
+		// TODO: Set DeptIndicator here
+	}
+
+	// TODO: Validate and update whatever obj holds these values
+	// verrs, err := h.db.ValidateAndUpdate(move)
+	// if err != nil || verrs.HasAny() {
+	// 	return responseForVErrors(h.logger, verrs, err)
+	// }
+
+	accountingInfo = internalmessages.AccountingInfo{
+		Tac:           newTAC,
+		DeptIndicator: newDeptIndicator,
+	}
+
+	return officeop.NewShowAccountingOK().WithPayload(&accountingInfo)
 }
