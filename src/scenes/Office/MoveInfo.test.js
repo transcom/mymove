431232d82d7b73c63ccfe0a3ff7f58a61bb29af0
Alexi Kostibas <alexi@truss.works>
2018-05-12 13:10:30 -0700

    adding react-mock-router to test components with <Switch> statements in them

--- src/scenes/Office/MoveInfo.test.js
+++ src/scenes/Office/MoveInfo.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MoveInfo from './MoveInfo';
 import store from 'shared/store';
+import MockRouter from 'react-mock-router';
 
 const dummyFunc = () => {};
 const moveIsLoading = false;
@@ -14,17 +15,21 @@ const match = {
   path: '/moveIt/moveIt',
 };
 
+const push = jest.fn();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <MoveInfo
-        moveIsLoading={moveIsLoading}
-        moveHasLoadError={moveHasLoadError}
-        moveHasLoadSuccess={moveHasLoadSuccess}
-        match={match}
-        loadMove={dummyFunc}
-      />
+      <MockRouter push={push}>
+        <MoveInfo
+          moveIsLoading={moveIsLoading}
+          moveHasLoadError={moveHasLoadError}
+          moveHasLoadSuccess={moveHasLoadSuccess}
+          match={match}
+          loadMove={dummyFunc}
+        />
+      </MockRouter>
     </Provider>,
     div,
   );
