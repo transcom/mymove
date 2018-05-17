aeb5edb3fb3b29dab266aee2de32c59bea6756b4
Patrick Stanger <patrick@truss.works>
2018-05-16 23:01:03 -0700

    Updates text and styling of order upload, adds framework for amended orders upload

--- src/scenes/Orders/UploadOrders.jsx
+++ src/scenes/Orders/UploadOrders.jsx
@@ -8,8 +8,10 @@ import moment from 'moment';
 
 import { loadServiceMember } from 'scenes/ServiceMembers/ducks';
 import { showCurrentOrders, deleteUpload, addUploads } from './ducks';
+import YesNoBoolean from 'shared/Inputs/YesNoBoolean';
 import Uploader from 'shared/Uploader';
 import WizardPage from 'shared/WizardPage';
+import { no_op } from 'shared/utils.js';
 
 import './UploadOrders.css';
 
@@ -19,11 +21,13 @@ export class UploadOrders extends Component {
 
     this.state = {
       newUploads: [],
+      showAmendedOrders: false,
     };
 
     this.onChange = this.onChange.bind(this);
     this.deleteFile = this.deleteFile.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setShowAmendedOrders = this.setShowAmendedOrders.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -41,6 +45,10 @@ export class UploadOrders extends Component {
 
   handleSubmit() {
     this.props.addUploads(this.state.newUploads);
+  }
+
+  setShowAmendedOrders(show) {
+    this.setState({ showAmendedOrders: show });
   }
 
   onChange(files) {
@@ -63,6 +71,7 @@ export class UploadOrders extends Component {
       currentOrders,
       uploads,
     } = this.props;
+    const { showAmendedOrders } = this.state;
     const isValid = Boolean(uploads.length || this.state.newUploads.length);
     const isDirty = Boolean(this.state.newUploads.length);
     return (
@@ -75,9 +84,17 @@ export class UploadOrders extends Component {
         hasSucceeded={hasSubmitSuccess}
         error={error}
       >
+        <div>
+          <h1 className="sm-heading">Upload Your Orders</h1>
+          <p>
+            In order to schedule your move, we need to have a complete copy of
+            your orders.<br />
+            You can upload a PDF, or you can take a picture of each page and
+            upload the images.
+          </p>
+        </div>
         {!!uploads.length && (
           <div>
-            <h1 className="sm-heading">Previous Uploads of Your Orders</h1>
             <table>
               <thead>
                 <tr>
@@ -108,13 +125,32 @@ export class UploadOrders extends Component {
             </table>
           </div>
         )}
-        <h1 className="sm-heading">Upload Photos or PDFs of Your Orders</h1>
         {currentOrders && (
-          <Uploader
-            document={currentOrders.uploaded_orders}
-            onChange={this.onChange}
-          />
+          <div className="uploader-box">
+            <Uploader
+              document={currentOrders.uploaded_orders}
+              onChange={this.onChange}
+            />
+            <div className="hint">(Each page must be clear and legible)</div>
+          </div>
         )}
+
+        <div className="Todo amended-orders">
+          <p>
+            Do you have amended orders? If so, you need to upload those as well.
+          </p>
+          <YesNoBoolean
+            value={showAmendedOrders}
+            onChange={this.setShowAmendedOrders}
+          />
+          {this.state.showAmendedOrders && (
+            <div className="uploader-box">
+              <h4>Upload amended orders</h4>
+              <Uploader document={{}} onChange={no_op} />
+              <div className="hint">(Each page must be clear and legible)</div>
+            </div>
+          )}
+        </div>
       </WizardPage>
     );
   }
