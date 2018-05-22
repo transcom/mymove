f6782f063271f5031f4a52c44c54ad7494e08dbe
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-05-22 16:45:41 -0700

    no advance pane complete

--- src/scenes/Office/PaymentsPanel.jsx
+++ src/scenes/Office/PaymentsPanel.jsx
@@ -1,54 +1,57 @@
 import { get } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 
 // import { updatePaymentInfo } from './ducks';
 import { no_op } from 'shared/utils';
 
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle';
 
 const PaymentsTable = props => {
   const ppm = props.ppm;
   return (
     <div className="usa-grid">
-      <table>
-        <tbody>
-          <tr>
-            <th />
-            <th>Amount</th>
-            <th>Disbursement</th>
-            <th>Requested on</th>
-            <th>Approved</th>
-          </tr>
-          {ppm.has_requested_advance ? (
-            [
-              <tr>
-                <th>Payments against PPM Incentive</th>
-              </tr>,
-              <tr>
-                <td />
-                <td>{ppm.requested_amount}</td>
-                <td>{ppm.method_of_receipt}</td>
-                <td>Dogs</td>
-                <td>Dogs</td>
-              </tr>,
-            ]
-          ) : (
-            <tr>
-              <th>No payments requested</th>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <div className="payment-table">
+        <div className="payment-table payment-table-header">Payments</div>
+        {ppm.has_requested_advance ? (
+          [
+            <div className="payment-table-content">Amount</div>,
+            <div className="payment-table-cell payment-table-cell-header">
+              Disbursement
+            </div>,
+            <div className="payment-table-cell payment-table-cell-header">
+              Requested on
+            </div>,
+            <div className="payment-table-cell payment-table-cell-header">
+              Approved
+            </div>,
+            <div className="payment-table-cell payment-table-cell-header">
+              Payments against PPM incentive
+            </div>,
+            <div className="payment-table-cell">ppm.requested_amount</div>,
+            <div className="payment-table-cell">ppm.method_of_receipt</div>,
+          ]
+        ) : (
+          <div className="payment-table payment-table-content">
+            <Link to="blank">
+              <FontAwesomeIcon
+                className="icon"
+                icon={faPlusCircle}
+                flip="horizontal"
+              />{' '}
+              Add a payment
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
 function mapStateToProps(state) {
-  // let serviceMember = get(state, 'office.officeServiceMember', {});
-  // let backupContact = get(state, 'office.officeBackupContacts.0', {}); // there can be only one
-
   return {
     initialValues: {},
     ppm: get(state, 'office.officePPMs[0]', {}),
