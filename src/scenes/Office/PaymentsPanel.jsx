4f988c65dc03badea13be1ab1f341c4df1530d2c
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-05-22 18:39:56 -0700

    more like a table

--- src/scenes/Office/PaymentsPanel.jsx
+++ src/scenes/Office/PaymentsPanel.jsx
@@ -1,52 +1,61 @@
 import { get } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 
 // import { updatePaymentInfo } from './ducks';
 import { no_op } from 'shared/utils';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import faPencil from '@fortawesome/fontawesome-free-solid/faPencilAlt';
+import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
+
+// import faPlusCircle from '@fortawesome/fontawesome-free-solid/faPlusCircle';
 
 const PaymentsTable = props => {
   const ppm = props.ppm;
   return (
     <div className="usa-grid">
-      <div className="payment-table">
-        <div className="payment-table payment-table-header">Payments</div>
-        {ppm.has_requested_advance ? (
-          [
-            <div className="payment-table-content">Amount</div>,
-            <div className="payment-table-cell payment-table-cell-header">
-              Disbursement
-            </div>,
-            <div className="payment-table-cell payment-table-cell-header">
-              Requested on
-            </div>,
-            <div className="payment-table-cell payment-table-cell-header">
-              Approved
-            </div>,
-            <div className="payment-table-cell payment-table-cell-header">
-              Payments against PPM incentive
-            </div>,
-            <div className="payment-table-cell">ppm.requested_amount</div>,
-            <div className="payment-table-cell">ppm.method_of_receipt</div>,
-          ]
-        ) : (
-          <div className="payment-table payment-table-content">
-            <Link to="blank">
-              <FontAwesomeIcon
-                className="icon"
-                icon={faPlusCircle}
-                flip="horizontal"
-              />{' '}
-              Add a payment
-            </Link>
-          </div>
-        )}
-      </div>
+      <table className="payment-table">
+        <tbody>
+          <tr>
+            <th className="payment-table-title">Payments</th>
+          </tr>
+          <tr>
+            <th className="payment-table-column-title" />
+            <th className="payment-table-column-title">Amount</th>
+            <th className="payment-table-column-title">Disbursement</th>
+            <th className="payment-table-column-title">Requested on</th>
+            <th className="payment-table-column-title">Approved</th>
+          </tr>
+          {ppm ? (
+            [
+              <tr>
+                <th className="payment-table-subheader">
+                  Payments against PPM Incentive
+                </th>
+              </tr>,
+              <tr>
+                <td className="payment-table-column-content" />
+                <td className="payment-table-column-content">I</td>
+                <td className="payment-table-column-content">Like</td>
+                <td className="payment-table-column-content">Dogs</td>
+                <td className="payment-table-column-content">
+                  <FontAwesomeIcon className="icon" icon={faCheck} />
+                  <FontAwesomeIcon className="icon" icon={faTimes} />
+                  <FontAwesomeIcon className="icon" icon={faPencil} />
+                </td>
+              </tr>,
+            ]
+          ) : (
+            <tr>
+              <th className="payment-table-subheader">No payments requested</th>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
