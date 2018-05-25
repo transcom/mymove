65811c8a21a70b4fc83262d05872f037b251bd33
Erin Stanfill <erin@truss.works>
2018-05-25 12:03:45 -0400

    make cancel buttons secondary

--- src/scenes/Review/EditBackupContact.jsx
+++ src/scenes/Review/EditBackupContact.jsx
@@ -32,7 +32,12 @@ let EditBackupContactForm = props => {
       <button type="submit" disabled={submitting || !valid}>
         Save
       </button>
-      <button type="button" disabled={submitting} onClick={onCancel}>
+      <button
+        type="button"
+        className="usa-button-secondary"
+        disabled={submitting}
+        onClick={onCancel}
+      >
         Cancel
       </button>
     </form>
