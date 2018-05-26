c50db424ea3e98b43d52aa431e038ef1e1d8c803
Erin Stanfill <erin@truss.works>
2018-05-25 22:13:47 -0400

    edit profile locks down some fields when move is approved

--- src/scenes/Review/EditProfile.jsx
+++ src/scenes/Review/EditProfile.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
@@ -20,7 +20,16 @@ import profileImage from './images/profile.png';
 const editProfileFormName = 'edit_profile';
 
 let EditProfileForm = props => {
-  const { schema, handleSubmit, submitting, valid } = props;
+  const {
+    schema,
+    handleSubmit,
+    submitting,
+    valid,
+    moveIsApproved,
+    initialValues,
+    schemaAffiliation,
+    schemaRank,
+  } = props;
   return (
     <form onSubmit={handleSubmit}>
       <img src={profileImage} alt="" /> Profile
@@ -31,10 +40,30 @@ let EditProfileForm = props => {
       <SwaggerField fieldName="last_name" swagger={schema} required />
       <SwaggerField fieldName="suffix" swagger={schema} />
       <hr className="spacer" />
-      <SwaggerField fieldName="affiliation" swagger={schema} required />
-      <SwaggerField fieldName="rank" swagger={schema} required />
-      <SwaggerField fieldName="edipi" swagger={schema} required />
-      <Field name="current_station" component={DutyStationSearchBox} />
+      {!moveIsApproved && (
+        <Fragment>
+          <SwaggerField fieldName="affiliation" swagger={schema} required />
+          <SwaggerField fieldName="rank" swagger={schema} required />
+          <SwaggerField fieldName="edipi" swagger={schema} required />
+          <Field name="current_station" component={DutyStationSearchBox} />
+        </Fragment>
+      )}
+      {moveIsApproved && (
+        <Fragment>
+          <div>To change the fields below, contact your local PPPO office.</div>
+          <label>Branch</label>
+          <strong>
+            {schemaAffiliation['x-display-value'][initialValues.affiliation]}
+          </strong>
+          <label>Rank</label>
+          <strong>{schemaRank['x-display-value'][initialValues.rank]}</strong>
+          <label>DoD ID #</label>
+          <strong>{initialValues.edipi}</strong>
+
+          <label>Current Duty Station</label>
+          <strong>{get(initialValues, 'current_station.name')}</strong>
+        </Fragment>
+      )}
       <SaveCancelButtons valid={valid} submitting={submitting} />
     </form>
   );
@@ -60,7 +89,14 @@ class EditProfile extends Component {
   };
 
   render() {
-    const { error, schema, serviceMember } = this.props;
+    const {
+      error,
+      schema,
+      serviceMember,
+      moveIsApproved,
+      schemaAffiliation,
+      schemaRank,
+    } = this.props;
 
     return (
       <div className="usa-grid">
@@ -77,6 +113,9 @@ class EditProfile extends Component {
             onSubmit={this.updateProfile}
             onCancel={this.returnToReview}
             schema={schema}
+            moveIsApproved={moveIsApproved}
+            schemaRank={schemaRank}
+            schemaAffiliation={schemaAffiliation}
           />
         </div>
       </div>
@@ -95,6 +134,14 @@ function mapStateToProps(state) {
       'swagger.spec.definitions.CreateServiceMemberPayload',
       {},
     ),
+    moveIsApproved:
+      get(
+        state,
+        'loggedInUser.loggedInUser.service_member.orders.0.moves.0.status',
+        'SUBMITTED',
+      ) === 'APPROVED',
+    schemaRank: get(state, 'swagger.spec.definitions.ServiceMemberRank', {}),
+    schemaAffiliation: get(state, 'swagger.spec.definitions.Affiliation', {}),
   };
 }
 
