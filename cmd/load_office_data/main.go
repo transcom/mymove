83f835a9958e87db9e12f7700b473d9ea32a9778
nickt truss <nick@truss.works>
2018-05-28 12:22:14 -0700

    Tidied up hours and services

--- cmd/load_office_data/main.go
+++ cmd/load_office_data/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
 	"github.com/namsral/flag"
+	"github.com/transcom/mymove/pkg/models"
 	"io/ioutil"
 	"log"
 	"strings"
@@ -16,7 +17,7 @@ type OfficeLocation struct {
 	Address2    string  `json:"extended_address"`
 	Locality    string  `json:"locality"`
 	State       string  `json:"region"`
-	StateCode   string  `json:"TX"`
+	StateCode   string  `json:"region_code"`
 	PostalCode  string  `json:"postal_code"`
 	Country     string  `json:"country_name"`
 	CountryCode string  `json:"country_code"`
@@ -29,9 +30,14 @@ type JSONOffice struct {
 	Name               string         `json:"name"`
 	ShippingOfficeName *string        `json:"shipping_office_name"`
 	Location           OfficeLocation `json:"location"`
-	parent             *JSONOffice
+	Hours              string         `json:"hours"`
+	Services           []string       `json:"services"`
 }
 
+/* This is code used to parse the XML data from the DPS database.
+We are not (yet) using that as it seems dirty, e.g. there are two entries for the Fort Stewart PPPO each with different
+PPSO's listed.
+This code will need to come back i once we have some clarity around this and need to HOOK up PPSOs
 // XMLOffice contains the fields for a PPPO or PPPSO in the xlsx file
 type XMLOffice struct {
 	Name           string
@@ -55,12 +61,36 @@ func xmlOfficeFromStringSlice(slice []string) *XMLOffice {
 		Country:  strings.TrimSpace(slice[6]),
 	}
 }
+*/
+func validated(verrs *validate.Errors, err error) (retError error) {
+	if verrs.HasAny() {
+		log.Printf("Validation errors:- %v", verrs)
+		retError = verrs
+	}
+	if err != nil {
+		log.Printf("Creation error:- %v", err)
+		retError = err
+	}
+	return
+}
+
+func splitOnNewLineAndCommaThenJoin(s string) string {
+	var all []string
+	lines := strings.Split(s, "\n")
+	for _, line := range lines {
+		partsOfLine := strings.Split(line, ",")
+		for _, part := range partsOfLine {
+			all = append(all, strings.TrimSpace(part))
+		}
+	}
+	return strings.Join(all, "; ")
+}
 
 func main() {
 	config := flag.String("config-dir", "config", "The location of server config files")
-	// env := flag.String("env", "development", "The environment to run in, configures the database, presently.")
+	env := flag.String("env", "development", "The environment to run in, configures the database, presently.")
 	jsonFile := flag.String("jsonFile", "", "Json File name to load - should be a *.json file containing PPO data")
-	xmlFile := flag.String("xmlFile", "", "XML File name to load - should be a *.xlsx file containing PPPO-PPSO relationships")
+	// xmlFile := flag.String("xmlFile", "", "XML File name to load - should be a *.xlsx file containing PPPO-PPSO relationships")
 	flag.Parse()
 
 	//DB connection
@@ -68,13 +98,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	/*	db, err := pop.Connect(*env)
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	if *jsonFile == "" || *xmlFile == "" {
-		log.Fatal("usage: load-office-data -jsonFile <filename> -xmlFile <other_filename>")
+	db, err := pop.Connect(*env)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *jsonFile == "" { //|| *xmlFile == "" {
+		log.Fatal("usage: load-office-data -jsonFile <filename> ") //  -xmlFile <other_filename>")
 	}
 
 	var offices []JSONOffice
@@ -87,42 +116,86 @@ func main() {
 		log.Fatalf("Error %v parsing json", err)
 	}
 
-	xf, err := excelize.OpenFile(*xmlFile)
+	err = db.Transaction(func(connection *pop.Connection) error {
+		for _, office := range offices {
+			if office.Location.CountryCode != "US" {
+				continue
+			}
+			// Address
+			address := models.Address{
+				StreetAddress1: office.Location.Address1,
+				StreetAddress2: &office.Location.Address2,
+				City:           office.Location.Locality,
+				State:          office.Location.StateCode,
+				PostalCode:     office.Location.PostalCode,
+				Country:        &office.Location.Country,
+			}
+			err = validated(db.ValidateAndCreate(&address))
+			if err != nil {
+				log.Printf("%v - address.", office.Name)
+				return err
+			}
+
+			hours := splitOnNewLineAndCommaThenJoin(office.Hours)
+			services := splitOnNewLineAndCommaThenJoin(strings.Join(office.Services, ", "))
+			transportationOffice := models.TransportationOffice{
+				Name:      office.Name,
+				AddressID: address.ID,
+				Latitude:  office.Location.Latitude,
+				Longitude: office.Location.Longitude,
+				Hours:     &hours,
+				Services:  &services,
+			}
+			err = validated(db.ValidateAndCreate(&transportationOffice))
+			if err != nil {
+				log.Printf("%v - office.", office.Name)
+				return err
+			}
+
+		}
+		return nil
+	})
 	if err != nil {
-		log.Fatalf("Error %v opening XLSX %s.", err, *xmlFile)
+		log.Printf("Tranaction failed:- %v", err)
 	}
 
-	ppoByZip := make(map[string][]*XMLOffice)
-	for idx, row := range xf.GetRows(xf.GetSheetName(1)) {
-		if idx > 0 {
-			pppo := xmlOfficeFromStringSlice(row)
-			list, ok := ppoByZip[pppo.Zip]
-			if ok {
-				ppoByZip[pppo.Zip] = append(list, pppo)
-			} else {
-				ppoByZip[pppo.Zip] = []*XMLOffice{pppo}
-			}
-			pppo.ShippingOffice = xmlOfficeFromStringSlice(row[7:])
+	/*	xf, err := excelize.OpenFile(*xmlFile)
+		if err != nil {
+			log.Fatalf("Error %v opening XLSX %s.", err, *xmlFile)
+		}
 
-		}
-	}
+		ppoByZip := make(map[string][]*XMLOffice)
+		for idx, row := range xf.GetRows(xf.GetSheetName(1)) {
+			if idx > 0 {
+				pppo := xmlOfficeFromStringSlice(row)
+				list, ok := ppoByZip[pppo.Zip]
+				if ok {
+					ppoByZip[pppo.Zip] = append(list, pppo)
+				} else {
+					ppoByZip[pppo.Zip] = []*XMLOffice{pppo}
+				}
+				pppo.ShippingOffice = xmlOfficeFromStringSlice(row[7:])
 
-	officeMap := make(map[string]JSONOffice)
-	for _, office := range offices {
-		if office.Location.CountryCode != "US" {
-			continue
-		}
-		officeMap[office.Name] = office
-		pppos, ok := ppoByZip[office.Location.PostalCode]
-		if !ok {
-			log.Printf("Couldn't find pppos for %s", office.Name)
-		}
-		if len(pppos) > 1 {
-			log.Printf("Ambiguous ppos for %s", office.Name)
-			for _, p := range pppos {
-				log.Printf("\t%s", p.Name)
 			}
 		}
-	}
+
+		officeMap := make(map[string]JSONOffice)
+		for _, office := range offices {
+			if office.Location.CountryCode != "US" {
+				continue
+			}
+			officeMap[office.Name] = office
+			pppos, ok := ppoByZip[office.Location.PostalCode]
+			if !ok {
+				log.Printf("Couldn't find pppos for %s", office.Name)
+			}
+			if len(pppos) > 1 {
+				log.Printf("Ambiguous ppos for %s", office.Name)
+				for _, p := range pppos {
+					log.Printf("\t%s", p.Name)
+				}
+			}
+		}
+	*/
 	log.Printf("Loaded %d offices", len(offices))
 }
