bab6cb4c9cfc61ccb97d0cd7ce0380da1d66138c
nickt truss <nick@truss.works>
2018-05-29 10:05:38 -0700

    WIP: Need load office data landed

---
+++ migrations/secure-dev/20180529024018_add_initial_office_users.up.sql
@@ -0,0 +1,16 @@
+--
+-- Data for Name: office_users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.office_users VALUES ('302516f9-9e3b-491c-8a13-8cbb0e39c386', NULL, 'User', 'Test1', NULL, 'test1@example.com', '(940) 676-5499', '6dc9c87d-37af-4157-a1ce-af45bb954eba', '2018-05-29 00:54:01.627967', '2018-05-29 00:54:01.627967');
+INSERT INTO public.office_users VALUES ('58fea332-9383-4583-a8be-a27158691491', NULL, 'User', 'Test2', NULL, 'test1@example.com', '(719) 556-4196', 'cc107598-3d72-4679-a4aa-c28d1fd2a016', '2018-05-29 00:57:24.687708', '2018-05-29 00:57:24.687708');
+INSERT INTO public.office_users VALUES ('37718125-34c6-4353-9845-1776d7395014', NULL, 'User', 'Test3', NULL, 'test3@example.com', '(210) 652-1848', '55c4e988-9534-4bfd-863f-831c5c3d9421', '2018-05-29 01:01:18.630098', '2018-05-29 01:01:18.630098');
+INSERT INTO public.office_users VALUES ('8a5821d3-4ec1-4bf8-b67d-8e4f5609c64c', NULL, 'User', 'Test4', NULL, 'test4@example.com', '(940) 676-5499', '6dc9c87d-37af-4157-a1ce-af45bb954eba', '2018-05-29 01:45:07.15128', '2018-05-29 01:45:07.15128');
+INSERT INTO public.office_users VALUES ('0ffa93e3-836a-4df8-85e5-7b83adba56f2', NULL, 'User', 'Test5', NULL, 'test5@example.com', '(334) 953-4774', '9215499f-4ac2-488f-b91d-6f14ec9d9160', '2018-05-29 01:48:44.254011', '2018-05-29 01:48:44.254011');
+INSERT INTO public.office_users VALUES ('65654075-68d6-41b6-b832-36d4224a5be4', NULL, 'User', 'Test6', NULL, 'test6@example.com', '(325) 654-3707', '2c6f2ac9-210c-4053-9770-1ed6225db248', '2018-05-29 01:52:28.415016', '2018-05-29 01:52:28.415016');
+INSERT INTO public.office_users VALUES ('0050534f-8222-4f95-af35-c22aac342682', NULL, 'User', 'Test7', NULL, 'test7@example.com', '(609) 754-6911', '10052404-6cf0-47da-b5bc-42b03e02f904', '2018-05-29 02:21:11.322433', '2018-05-29 02:21:11.322433');
+INSERT INTO public.office_users VALUES ('b3f2f3a9-d45f-4f9e-8b0c-4cc02926e998', NULL, 'User', 'Test8', NULL, 'test8@example.com ', '(210) 221-1605', 'e49796ab-bbae-4759-8b18-e0bf64eae315', '2018-05-29 02:27:51.843329', '2018-05-29 02:27:51.843329');
+INSERT INTO public.office_users VALUES ('dee62f0a-8e0c-41ee-9842-0223133f8a01', NULL, 'User', 'Test9', NULL, 'test9@example.com', '(210) 671-2821', '3456949e-8615-4949-8899-4faa34f3880b', '2018-05-29 02:30:28.665107', '2018-05-29 02:30:28.665107');
+INSERT INTO public.office_users VALUES ('c06abe17-23af-40ca-bc94-514068e1a42c', NULL, 'User', 'Test10', NULL, 'test10@example.com', '(325) 654-3707', '2c6f2ac9-210c-4053-9770-1ed6225db248', '2018-05-29 02:34:09.325659', '2018-05-29 02:34:09.325659');
+
+
