928d7307745fc7f4f7511fd588e45239434660d1
Breanne Boland <breanne@truss.works>
2018-05-29 13:31:19 -0700

    removed search box, icon, and related css

--- src/shared/Header/Office/index.jsx
+++ src/shared/Header/Office/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import magnifyingGlass from '../../../../node_modules/uswds/src/img/search.svg';
 import LoginButton from 'shared/User/LoginButton';
 import Email from 'shared/User/Email';
 
@@ -25,27 +24,6 @@ function QueueHeader() {
         </NavLink>
       </div>
       <div className="officeHeaderThree">
-        <div className="forms-shared">
-          <form>
-            <input type="text" placeholder="Search" name="search" />
-          </form>
-        </div>
-        <form className="usa-form forms-shared">
-          <select name="options" id="options">
-            <option value>All</option>
-            <option value="locator">Move Locator</option>
-            <option value="edipi">DOD ID</option>
-            <option value="customerName">Name (Last, First)</option>
-            <option value="gbl">GBL</option>
-            <option value="order">Orders</option>
-            <option value="status">Status</option>
-          </select>
-        </form>
-        <button>
-          <img src={magnifyingGlass} alt="Search" />
-        </button>
-      </div>
-      <div className="officeHeaderFour">
         <ul className="usa-nav-primary">
           <li>
             <Email />
