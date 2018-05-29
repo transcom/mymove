660de45fe993d810ed409359540a8df166c95bfa
Andrea Mitchell <8170735+amitch23@users.noreply.github.com>
2018-05-28 23:26:42 -0700

    links open to new windows

--- src/shared/Statements/AccessibilityStatement.jsx
+++ src/shared/Statements/AccessibilityStatement.jsx
@@ -16,14 +16,22 @@ function PrivacyPolicy() {
           The U.S. Department of Defense is committed to making its electronic
           and information technologies accessible to individuals with
           disabilities in accordance with{' '}
-          <a href="https://www.access-board.gov/the-board/laws/rehabilitation-act-of-1973#508%20">
+          <a
+            href="https://www.access-board.gov/the-board/laws/rehabilitation-act-of-1973#508%20"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Section 508 of the Rehabilitation Act (29 U.S.C. 794d)
           </a>, as amended in 1998.
         </p>
         <p>
           For persons with disabilities experiencing difficulties accessing
           content on a particular website, please use the{' '}
-          <a href="http://dodcio.defense.gov/DoDSection508/Section508Form.aspx">
+          <a
+            href="http://dodcio.defense.gov/DoDSection508/Section508Form.aspx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             DoD Section 508 Form
           </a>. In this form, please indicate the nature of your accessibility
           issue/problem and your contact information so we can address your
@@ -31,7 +39,11 @@ function PrivacyPolicy() {
         </p>
         <p>
           For more information about Section 508, please visit the{' '}
-          <a href="http://dodcio.defense.gov/DoDSection508.aspx">
+          <a
+            href="http://dodcio.defense.gov/DoDSection508.aspx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             {' '}
             DoD Section 508 website.
           </a>
