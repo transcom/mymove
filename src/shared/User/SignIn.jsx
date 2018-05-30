3eca533d012ea2dc8767c66e042f1eceee1dac75
Erin Stanfill <erin@truss.works>
2018-05-30 11:00:07 -0400

    updated signin with latest from design

--- src/shared/User/SignIn.jsx
+++ src/shared/User/SignIn.jsx
@@ -4,36 +4,24 @@ const SignIn = () => (
   <div className="usa-grid">
     <div className="usa-width-one-sixth">&nbsp; </div>
     <div className="usa-width-two-thirds">
+      <h2 className="align-center">Welcome to my.move.mil!</h2>
+      <br />
+      <p>
+        This is a new system from USTRANSCOM to support the relocation of
+        families during PCS.
+      </p>
+      <p>
+        Right now, use of this system is by invitation only. If you haven't
+        received an invitation, please go to DPS to schedule your move.
+      </p>
+      <p>
+        Over the coming months, we'll be rolling this new tool out to more and
+        more people. Stay tuned.
+      </p>
       <div className="align-center">
-        <p>
-          <img src={lockImage} width="120" alt="" />
-        </p>
-        <br />
-        <p>
-          Your account security is important. Please sign in before proceeding.
-        </p>
-        <br />
         <a href="/auth/login-gov" className="usa-button  usa-button-big ">
           Sign in
         </a>
-        <br /> <br />
-      </div>
-      <div className="storage-estimate">
-        <p>
-          Welcome to my.move.mil! This is a new system put together by
-          USTRANSCOM to support the relocation of families during the PCS
-          process.{' '}
-        </p>
-        <p>
-          Right now, use of this system is by invitation only. If you haven't
-          received an invitation, you need to go to{' '}
-          <a href="https://eta.sddc.army.mil/ETASSOPortal/default.aspx">DPS</a>{' '}
-          to schedule your move.
-        </p>
-        <p>
-          Over the coming months we'll be rolling this new tool out to more and
-          more people. Stay tuned.
-        </p>
       </div>
     </div>
   </div>
