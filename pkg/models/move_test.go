c8a94c42349d799f69e4e611e36bf571ca714551
Breanne Boland <breanne@truss.works>
2018-06-19 11:22:17 -0700

    fine-tuned test

--- pkg/models/move_test.go
+++ pkg/models/move_test.go
@@ -61,7 +61,7 @@ func (suite *ModelSuite) TestMoveStateMachine() {
 	move, verrs, err := order1.CreateNewMove(suite.db, &selectedType)
 	suite.Nil(err)
 	suite.False(verrs.HasAny(), "failed to validate move")
-	reason := "Just joking"
+	reason := ""
 
 	// Can't cancel a move with DRAFT status
 	err = move.Cancel(reason)
@@ -76,7 +76,7 @@ func (suite *ModelSuite) TestMoveStateMachine() {
 	err = move.Cancel(reason)
 	suite.Nil(err)
 	suite.Equal(MoveStatusCANCELED, move.Status, "expected Canceled")
-	suite.Equal(&reason, move.CancelReason, "expected 'Just joking'")
+	suite.Nil(move.CancelReason)
 
 }
 
