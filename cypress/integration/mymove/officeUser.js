330277fd14c36f64cd33f9ee39606f520659cb5c
Chris Gilmer <chris@truss.works>
2018-06-28 16:42:06 -0700

    Go into PPM queue

--- cypress/integration/mymove/officeUser.js
+++ cypress/integration/mymove/officeUser.js
@@ -9,13 +9,21 @@ describe('office user finds the move', function() {
 });
 
 function officeUserViewsMoves() {
+  // Open new moves queue
   cy.location().should(loc => {
     expect(loc.pathname).to.match(/^\/queues\/new/);
   });
+
+  // Find move and open it
   cy
     .get('div')
     .contains('H9VSGX')
     .dblclick();
+
+  // Verify basics and edit
+  // tbd
+
+  // Approve the move
   cy
     .get('button')
     .contains('Approve PPM')
@@ -32,4 +40,25 @@ function officeUserViewsMoves() {
     .get('button')
     .contains('Approve PPM')
     .click();
+
+  // Go back to new moves queue
+  cy
+    .get('span')
+    .contains('New Moves Queue')
+    .click();
+
+  // Open PPMs
+  cy
+    .get('span')
+    .contains('PPMs')
+    .click();
+
+  // Find move and open it
+  cy
+    .get('div')
+    .contains('H9VSGX')
+    .dblclick();
+
+  // Verify PPM
+  // tbd
 }
