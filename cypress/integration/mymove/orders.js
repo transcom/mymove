8bb522949a770aba1110e8e4adb9813d95f9889d
Erin Stanfill <erin@truss.works>
2018-06-28 19:20:44 -0400

    initial data entry for orders

--- cypress/integration/mymove/orders.js
+++ cypress/integration/mymove/orders.js
@@ -13,7 +13,7 @@ describe('orders entry', function() {
 });
 
 function createServiceMember() {
-  cy
+  return cy
     .location()
     .should(loc => {
       expect(loc.pathname).to.match(/^\/service-member\/[^/]+\/create/);
@@ -27,17 +27,56 @@ function createServiceMember() {
     })
     .then(serviceMemberId =>
       cy.fixture('serviceMember.json').then(serviceMember =>
-        cy.window().then(window => {
-          serviceMember.user_id = window._state.user.userId;
-          cy.request(
-            'PATCH',
-            `/internal/service_members/${serviceMemberId}`,
-            serviceMember,
-          );
-        }),
+        cy
+          .window()
+          .then(window => {
+            serviceMember.user_id = window._state.user.userId;
+            return cy.request(
+              'PATCH',
+              `/internal/service_members/${serviceMemberId}`,
+              serviceMember,
+            );
+          })
+          .then(() =>
+            cy
+              .fixture('backupContact.json')
+              .then(backupContact =>
+                cy.request(
+                  'POST',
+                  `/internal/service_members/${serviceMemberId}/backup_contacts`,
+                  backupContact,
+                ),
+              ),
+          ),
       ),
     );
 }
 function orders(restartAfterEachPage) {
-  createServiceMember();
+  createServiceMember().then(() => cy.visit('/'));
+  cy.contains('New move from Ft Carson');
+  cy.contains('No detail');
+  cy.contains('No documents');
+  cy.contains('Continue Move Setup').click();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.eq('/orders/');
+  });
+
+  cy.get('select[name="orders_type"]').select('Permanent Change Of Station');
+  cy
+    .get('input[placeholder="Date"]')
+    .first()
+    .click();
+  // .type('20181020')
+  // .blur();
+  //   cy
+  //     .get('input[placeholder="Date"]')
+  //     .last()
+  //     .type('20181120')
+  //     .blur();
+
+  //   cy
+  //     .get('.duty-input-box__input input')
+  //     .first()
+  //     .type('Fort Worth{downarrow}{enter}', { force: true, delay: 150 });
 }
