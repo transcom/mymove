fae8d033ed8311d741685cdea4ad07712a1b66ee
Chris Gilmer <chris@truss.works>
2018-07-09 13:53:18 -0700

    Add tsp user type to DB

---
+++ pkg/models/tsp_users_test.go
@@ -0,0 +1,69 @@
+package models_test
+
+import (
+	"github.com/gobuffalo/uuid"
+	. "github.com/transcom/mymove/pkg/models"
+)
+
+func (suite *ModelSuite) Test_TspUserInstantiation() {
+	user := &TspUser{}
+	expErrors := map[string][]string{
+		"first_name": {"FirstName can not be blank."},
+		"last_name":  {"LastName can not be blank."},
+		"telephone":  {"Telephone can not be blank."},
+		"email":      {"Email can not be blank."},
+		"tsp_id":     {"TspID can not be blank."},
+	}
+	suite.verifyValidationErrors(user, expErrors)
+}
+
+func (suite *ModelSuite) Test_BasicTspUser() {
+	fakeUUID, _ := uuid.FromString("39b28c92-0506-4bef-8b57-e39519f42dc1")
+	userEmail := "sally@government.gov"
+	sally := User{
+		LoginGovUUID:  fakeUUID,
+		LoginGovEmail: userEmail,
+	}
+	suite.mustSave(&sally)
+	tsp := CreateTestShippingTsp(suite)
+
+	user := TspUser{
+		LastName:            "Tester",
+		FirstName:           "Sally",
+		Email:               "sally.work@government.gov",
+		Telephone:           "(907) 555-1212",
+		UserID:              &sally.ID,
+		User:                sally,
+		TransportationTspID: tsp.ID,
+	}
+	suite.mustSave(&user)
+
+	var loadUser TspUser
+	err := suite.db.Eager().Find(&loadUser, user.ID)
+	suite.Nil(err, "loading user")
+	suite.Equal(user.ID, loadUser.ID)
+	suite.Equal(tsp.ID, loadUser.TransportationTsp.ID)
+}
+
+func (suite *ModelSuite) TestFetchTspUserByEmail() {
+
+	user, err := FetchTspUserByEmail(suite.db, "not_here@example.com")
+	suite.Equal(err, ErrFetchNotFound)
+	suite.Nil(user)
+
+	const email = "sally.work@government.gov"
+	tsp := CreateTestShippingTsp(suite)
+	newUser := TspUser{
+		LastName:            "Tester",
+		FirstName:           "Sally",
+		Email:               email,
+		Telephone:           "(907) 555-1212",
+		TransportationTspID: tsp.ID,
+	}
+	suite.mustSave(&newUser)
+
+	user, err = FetchTspUserByEmail(suite.db, email)
+	suite.Nil(err)
+	suite.NotNil(user)
+	suite.Equal(newUser.ID, user.ID)
+}
