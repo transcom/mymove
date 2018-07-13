a47ef774f76b2115336d3a5cf08e21c574927753
Aileen Chen <aileen@dds.mil>
2018-07-13 14:14:38 -0400

    Remove prints

--- src/scenes/Gex/api.js
+++ src/scenes/Gex/api.js
@@ -1,8 +1,6 @@
 import { getClient, checkResponse } from 'shared/api';
 
 export async function SendGexRequest(payload) {
-  console.log('sending');
-  console.log(payload);
   const client = await getClient();
   const response = await client.apis.gex.sendGexRequest({
     sendGexRequestPayload: payload,
