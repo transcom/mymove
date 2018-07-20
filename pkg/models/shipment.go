803bd0dde03a2b214a9d05c968cbbad401873ede
Chris Gilmer <chris@truss.works>
2018-07-20 16:16:19 -0700

    Attempt to hydrate addresses in shipment model

--- pkg/models/shipment.go
+++ pkg/models/shipment.go
@@ -32,16 +32,16 @@ type Shipment struct {
 	EstimatedPackDays            *int64      `json:"estimated_pack_days" db:"estimated_pack_days"`
 	EstimatedTransitDays         *int64      `json:"estimated_transit_days" db:"estimated_transit_days"`
 	PickupAddressID              *uuid.UUID  `json:"pickup_address_id" db:"pickup_address_id"`
-	PickupAddress                *Address    `belongs_to:"address"`
+	PickupAddress                *Address    `json:"pickup_address" belongs_to:"address"`
 	HasSecondaryPickupAddress    bool        `json:"has_secondary_pickup_address" db:"has_secondary_pickup_address"`
 	SecondaryPickupAddressID     *uuid.UUID  `json:"secondary_pickup_address_id" db:"secondary_pickup_address_id"`
-	SecondaryPickupAddress       *Address    `belongs_to:"address"`
+	SecondaryPickupAddress       *Address    `json:"secondary_pickup_address" belongs_to:"address"`
 	HasDeliveryAddress           bool        `json:"has_delivery_address" db:"has_delivery_address"`
 	DeliveryAddressID            *uuid.UUID  `json:"delivery_address_id" db:"delivery_address_id"`
-	DeliveryAddress              *Address    `belongs_to:"address"`
+	DeliveryAddress              *Address    `json:"delivery_address" belongs_to:"address"`
 	HasPartialSITDeliveryAddress bool        `json:"has_partial_sit_delivery_address" db:"has_partial_sit_delivery_address"`
 	PartialSITDeliveryAddressID  *uuid.UUID  `json:"partial_sit_delivery_address_id" db:"partial_sit_delivery_address_id"`
-	PartialSITDeliveryAddress    *Address    `belongs_to:"address"`
+	PartialSITDeliveryAddress    *Address    `json:"partial_sit_delivery_address" belongs_to:"address"`
 	WeightEstimate               *unit.Pound `json:"weight_estimate" db:"weight_estimate"`
 	ProgearWeightEstimate        *unit.Pound `json:"progear_weight_estimate" db:"progear_weight_estimate"`
 	SpouseProgearWeightEstimate  *unit.Pound `json:"spouse_progear_weight_estimate" db:"spouse_progear_weight_estimate"`
@@ -121,7 +121,11 @@ func FetchShipmentsByTSP(dbConnection *pop.Connection, TspID uuid.UUID) ([]Shipm
 	var sql string
 
 	sql = `SELECT
-			shipments.*
+			shipments.*,
+			(SELECT json_strip_nulls(row_to_json(addresses)) FROM addresses WHERE addresses.id = shipments.pickup_address_id) AS pickup_address,
+			(SELECT json_strip_nulls(row_to_json(addresses)) FROM addresses WHERE addresses.id = shipments.secondary_pickup_address_id) AS secondary_pickup_address,
+			(SELECT json_strip_nulls(row_to_json(addresses)) FROM addresses WHERE addresses.id = shipments.delivery_address_id) AS delivery_address,
+			(SELECT json_strip_nulls(row_to_json(addresses)) FROM addresses WHERE addresses.id = shipments.partial_sit_delivery_address_id) AS partial_sit_delivery_address
 		FROM shipments
 		LEFT JOIN shipment_offers ON
 			shipments.id=shipment_offers.shipment_id
