7455f4b7a828ad3f2000a90ff936d59763d98060
Chris Gilmer <chris@truss.works>
2018-07-26 10:40:46 -0700

    Make shipment struct order the same for readability

--- pkg/models/shipment.go
+++ pkg/models/shipment.go
@@ -20,13 +20,15 @@ import (
 // BookDate: when the shipment was most recently offered to a TSP
 type Shipment struct {
 	ID                        uuid.UUID  `json:"id" db:"id"`
-	PickupDate                *time.Time `json:"pickup_date" db:"pickup_date"`
-	RequestedPickupDate       *time.Time `json:"requested_pickup_date" db:"requested_pickup_date"`
-	DeliveryDate              *time.Time `json:"delivery_date" db:"delivery_date"`
-	BookDate                  *time.Time `json:"book_date" db:"book_date"`
 	TrafficDistributionListID *uuid.UUID `json:"traffic_distribution_list_id" db:"traffic_distribution_list_id"`
+	PickupDate                *time.Time `json:"pickup_date" db:"pickup_date"`
+	DeliveryDate              *time.Time `json:"delivery_date" db:"delivery_date"`
+	CreatedAt                 time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt                 time.Time  `json:"updated_at" db:"updated_at"`
 	SourceGBLOC               *string    `json:"source_gbloc" db:"source_gbloc"`
 	Market                    *string    `json:"market" db:"market"`
+	BookDate                  *time.Time `json:"book_date" db:"book_date"`
+	RequestedPickupDate       *time.Time `json:"requested_pickup_date" db:"requested_pickup_date"`
 	MoveID                    uuid.UUID  `json:"move_id" db:"move_id"`
 	Status                    string     `json:"status" db:"status"`
 
@@ -46,8 +48,6 @@ type Shipment struct {
 	WeightEstimate               *unit.Pound `json:"weight_estimate" db:"weight_estimate"`
 	ProgearWeightEstimate        *unit.Pound `json:"progear_weight_estimate" db:"progear_weight_estimate"`
 	SpouseProgearWeightEstimate  *unit.Pound `json:"spouse_progear_weight_estimate" db:"spouse_progear_weight_estimate"`
-	CreatedAt                    time.Time   `json:"created_at" db:"created_at"`
-	UpdatedAt                    time.Time   `json:"updated_at" db:"updated_at"`
 }
 
 // ShipmentWithOffer represents a single offered shipment within a Service Member's move.
