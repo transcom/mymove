9e0ddbc33decb594b2f7fea67010d9cc3b9a4254
Chris Gilmer <chris@truss.works>
2018-07-30 10:56:55 -0700

    First commit for TSP queues

--- src/scenes/TransportationServiceProvider/index.jsx
+++ src/scenes/TransportationServiceProvider/index.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { Redirect, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { history } from 'shared/store';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import TspHeader from 'shared/Header/Tsp';
+import QueueList from './QueueList';
+import QueueTable from './QueueTable';
 import { loadLoggedInUser } from 'shared/User/ducks';
 import { loadSchema } from 'shared/Swagger/ducks';
 import { no_op } from 'shared/utils';
 import LogoutOnInactivity from 'shared/User/LogoutOnInactivity';
+import PrivateRoute from 'shared/User/PrivateRoute';
 
 import './tsp.css';
+
+class Queues extends Component {
+  render() {
+    return (
+      <div className="usa-grid grid-wide queue-columns">
+        <div className="queue-menu-column">
+          <QueueList />
+        </div>
+        <div className="queue-list-column">
+          <div className="queue-table-scrollable">
+            <QueueTable queueType={this.props.match.params.queueType} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class TspWrapper extends Component {
   componentDidMount() {
@@ -26,6 +47,10 @@ class TspWrapper extends Component {
           <main className="site__content">
             <div>
               <LogoutOnInactivity />
+              <Switch>
+                <Redirect from="/" to="/queues/all" exact />
+                <PrivateRoute path="/queues/:queueType" component={Queues} />
+              </Switch>
             </div>
           </main>
         </div>
