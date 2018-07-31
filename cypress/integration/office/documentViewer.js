0bc6ed3c931b4b77ed5703674e6ec3af5e929e3c
andrea <andrea@truss.works>
2018-07-31 15:05:39 -0700

    add e2e tests to documentviewer

--- cypress/integration/office/documentViewer.js
+++ cypress/integration/office/documentViewer.js
@@ -33,7 +33,6 @@ describe('The document viewer', function() {
       .get('button.submit')
       .should('not.be.disabled')
       .click();
-    // TODO: add tests for uploaded document viewer
   });
   it('shows the newly uploaded document in the document list tab', () => {
     cy.visit('/moves/c9df71f2-334f-4f0e-b2e7-050ddb22efa1/documents');
@@ -44,6 +43,119 @@ describe('The document viewer', function() {
       .find('a')
       .should('have.attr', 'href')
       .and('match', /^\/moves\/[^/]+\/documents\/[^/]+/);
+  });
+  it('can upload an expense document', () => {
+    cy.visit('/moves/c9df71f2-334f-4f0e-b2e7-050ddb22efa1/documents/new');
+    cy.contains('Upload a new document');
+    cy.get('button.submit').should('be.disabled');
+    cy.get('select[name="move_document_type"]').select('Expense');
+    cy.get('input[name="title"]').type('expense document');
+    cy.get('select[name="moving_expense_type"]').select('Contracted Expense');
+    cy.get('input[name="reimbursement.requested_amount"]').type('12');
+    cy
+      .get('select[name="reimbursement.method_of_receipt"]')
+      .select('Direct deposit to MilPay');
+
+    cy.get('button.submit').should('be.disabled');
+
+    cy.upload_file('.filepond--root', 'top-secret.png');
+    cy
+      .get('button.submit')
+      .should('not.be.disabled')
+      .click();
+  });
+  it('can select and update newly-uploaded expense document', () => {
+    cy.visit('/moves/c9df71f2-334f-4f0e-b2e7-050ddb22efa1/documents');
+    cy.contains('expense document');
+    cy
+      .get('.pad-ns')
+      .find('a')
+      .should('have.attr', 'href')
+      .and('match', /^\/moves\/[^/]+\/documents\/[^/]+/);
+
+    cy.contains('expense document').click();
+    cy.contains('Details').click();
+    cy.contains('Direct deposit to MilPay');
+    cy.contains('Edit').click();
+
+    cy
+      .get('select[name="moveDocument.reimbursement.method_of_receipt"]')
+      .select('GTCC');
+    cy.get('select[name="moveDocument.status"]').select('OK');
+
+    cy
+      .get('button')
+      .contains('Save')
+      .should('not.be.disabled')
+      .click();
+
+    cy.contains('GTCC');
+  });
+  it('can update expense document to other doc type', () => {
+    cy.visit('/moves/c9df71f2-334f-4f0e-b2e7-050ddb22efa1/documents');
+    cy.contains('expense document');
+    cy
+      .get('.pad-ns')
+      .find('a')
+      .should('have.attr', 'href')
+      .and('match', /^\/moves\/[^/]+\/documents\/[^/]+/);
+
+    cy.contains('expense document').click();
+    cy.contains('Details').click();
+    cy.contains('GTCC');
+    cy.contains('Edit').click();
+
+    cy
+      .get('select[name="moveDocument.move_document_type"]')
+      .select('Other document type');
+
+    cy
+      .get('button')
+      .contains('Save')
+      .should('not.be.disabled')
+      .click();
+
+    cy.contains('Other document type');
+
+    cy
+      .get('.field-title')
+      .contains('Expense Type')
+      .should('not.exist');
+  });
+  it('can update other document type back to expense type', () => {
+    cy.visit('/moves/c9df71f2-334f-4f0e-b2e7-050ddb22efa1/documents');
+    cy.contains('expense document');
+    cy
+      .get('.pad-ns')
+      .find('a')
+      .should('have.attr', 'href')
+      .and('match', /^\/moves\/[^/]+\/documents\/[^/]+/);
+
+    cy.contains('expense document').click();
+    cy.contains('Details').click();
+    cy.contains('OK');
+    cy.contains('Edit').click();
+
+    cy.get('select[name="moveDocument.move_document_type"]').select('Expense');
+    cy
+      .get('select[name="moveDocument.moving_expense_type"]')
+      .select('Contracted Expense');
+    cy
+      .get('input[name="moveDocument.reimbursement.requested_amount"]')
+      .type('12');
+    cy
+      .get('select[name="moveDocument.reimbursement.method_of_receipt"]')
+      .select('GTCC');
+    cy.get('select[name="moveDocument.status"]').select('OK');
+
+    cy
+      .get('button')
+      .contains('Save')
+      .should('not.be.disabled')
+      .click();
+
+    cy.contains('Expense');
+    cy.contains('GTCC');
   });
 });
 
