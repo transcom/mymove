f80f13ae634d7bfe6eef65c20925e26669b31ff0
Chris Gilmer <chris@truss.works>
2018-07-31 17:19:40 -0700

    Fix problem with null cancel reason

--- pkg/handlers/moves.go
+++ pkg/handlers/moves.go
@@ -198,15 +198,21 @@ func publicPayloadForMoveModel(move *models.Move) *apimessages.Move {
 	if move == nil {
 		return nil
 	}
+
 	var SelectedMoveType = apimessages.SelectedMoveTypeHHG
 	if move.SelectedMoveType != nil {
 		SelectedMoveType = apimessages.SelectedMoveType(*move.SelectedMoveType)
+	}
+
+	cancelReason := ""
+	if move.CancelReason != nil {
+		cancelReason = *move.CancelReason
 	}
 	return &apimessages.Move{
 		SelectedMoveType: &SelectedMoveType,
 		OrdersID:         fmtUUID(move.OrdersID),
 		Status:           apimessages.MoveStatus(move.Status),
 		Locator:          swag.String(move.Locator),
-		CancelReason:     swag.String(*move.CancelReason),
+		CancelReason:     swag.String(cancelReason),
 	}
 }
