d25dbd6510f4939ae9c90892fd2faa6eddb4c039
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-08-06 12:37:50 -0700

    add new counselors--typo will exist in perpetuity

---
+++ local_migrations/20180806181625_add_conselors.sql
@@ -0,0 +1,11 @@
+-- Local test migration.
+-- This will be run on development environments. It should mirror what you
+-- intend to apply on production, but do not include any sensitive data.
+
+INSERT INTO public.office_users VALUES ('3c923c57-7d63-4727-8080-5c96977a7f22', NULL, 'Test', 'Test41', NULL, 'test41@example.com', '(800) 555-5869', '10052404-6cf0-47da-b5bc-42b03e02f904', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('30d2a774-39aa-4f78-b560-9153bb0ec2a2', NULL, 'Test', 'Test42', NULL, 'test42@example.com', '(800) 555-5869', 'e05e4d21-7655-41f7-93f1-c2aac39be98c', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('3c9f9230-531d-4079-b3e0-3a5273196412', NULL, 'Test', 'Test43', NULL, 'test43@example.com', '(800) 555-5869', '6dc9c87d-37af-4157-a1ce-af45bb954eba', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('36d31ca9-5a10-46c3-8b88-b252231443f2', NULL, 'Test', 'Test44', NULL, 'test44@example.com', '(800) 555-5869', '1ef624a7-d612-4457-b310-d9e4c3fd6c85', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('3d563e8a-4197-44af-8e29-5a896e982532', NULL, 'Test', 'Test45', NULL, 'test45@example.com', '(800) 555-5869', '4b8584e4-0db6-4554-9a8f-3062456884e8', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('30662f87-be3c-4d1c-adba-bcda278ab292', NULL, 'Test', 'Test46', NULL, 'test46@example.com', '(800) 555-5869', '525af4ce-3656-4ec6-a23d-2df6b4f39b64', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
+INSERT INTO public.office_users VALUES ('32a933e4-453e-4107-b60c-b7f70ebb5902', NULL, 'Test', 'Test47', NULL, 'test47@example.com', '(800) 555-5869', 'f760ee76-d386-47d8-9df1-aff8c347fa96', '2018-08-06 18:16:25.630098', '2018-08-06 18:16:25.630098');
