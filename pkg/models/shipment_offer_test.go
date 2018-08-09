63860efa9b5709e0cda14f631144838c49c57d96
Chris Gilmer <chris@truss.works>
2018-08-09 11:48:43 -0700

    Use new MakeShipment in awardqueue and shipment_offer tests

--- pkg/models/shipment_offer_test.go
+++ pkg/models/shipment_offer_test.go
@@ -21,7 +21,8 @@ func (suite *ModelSuite) Test_ShipmentOfferValidations() {
 // Test_CreateShipmentOffer tests that a shipment is created when expected
 func (suite *ModelSuite) Test_CreateShipmentOffer() {
 	t := suite.T()
-	now := time.Now()
+	pickupDate := time.Now()
+	deliveryDate := time.Now().AddDate(0, 0, 1)
 	tdl, err := testdatagen.MakeTDL(suite.db,
 		testdatagen.DefaultSrcRateArea,
 		testdatagen.DefaultDstRegion,
@@ -31,9 +32,20 @@ func (suite *ModelSuite) Test_CreateShipmentOffer() {
 	tsp, err := testdatagen.MakeTSP(suite.db, testdatagen.RandomSCAC())
 	suite.Nil(err, "error making TSP")
 
+	sourceGBLOC := "OHAI"
 	market := "dHHG"
-	shipment, err := testdatagen.MakeShipment(suite.db, now, now, now.AddDate(0, 0, 1), tdl, "OHAI", &market, nil, nil)
-	suite.Nil(err, "error making Shipment")
+
+	shipment := testdatagen.MakeShipment(db, Assertions{
+		Shipment: models.Shipment{
+			RequestedPickupDate:     &pickupDate,
+			PickupDate:              &pickupDate,
+			DeliveryDate:            &deliveryDate,
+			TrafficDistributionList: &tdl,
+			SourceGBLOC:             &sourceGBLOC,
+			Market:                  &market,
+		},
+	})
+
 	shipmentOffer, err := CreateShipmentOffer(suite.db, shipment.ID, tsp.ID, false)
 	suite.Nil(err, "error making ShipmentOffer")
 
