070191bb9594ead9d0cdaa34d80c5934c4ab82a6
Kim Allen <kim@truss.works>
2018-08-13 13:22:06 -0700

    Start a shipment e2e for SM- add pseudo-code

---
+++ cypress/integration/mymove/shipment.js
@@ -0,0 +1,14 @@
+/* global cy */
+
+// Sign in user
+//  choose hhg move type
+// Fill in the form (including optional addresses)
+// click next (which submits data)
+// verify next page is Review page
+// Update hhg. . .
+describe.skip('completing the hhg flow', function() {
+  beforeEach(() => {
+    //profile@comple.te
+    cy.signInAsUser('13F3949D-0D53-4BE4-B1B1-AE4314793F34');
+  });
+});
