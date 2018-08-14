2711922d221078e7c2789ed393080f6e2023aff7
Kim Allen <kim@truss.works>
2018-08-14 11:51:53 -0700

    Fill all values in e2e test except calendar date in hhg form

--- cypress/integration/mymove/shipment.js
+++ cypress/integration/mymove/shipment.js
@@ -20,5 +20,51 @@ describe('completing the hhg flow', function() {
     cy.location().should(loc => {
       expect(loc.pathname).to.match(/^\/moves\/[^/]+\/hhg-start/);
     });
+    // cy.get(calendar date)
+    // Pickup address
+    cy
+      .get('input[name="pickup_address.street_address_1"]')
+      .clear({ force: true })
+      .type('123 Elm Street');
+    cy
+      .get('input[name="pickup_address.city"]')
+      .clear()
+      .type('Alameda');
+    cy.get('select[name="pickup_address.state"]').select('CA');
+    cy.get('input[name="pickup_address.postal_code"]').type('94607');
+    cy.get('input[type="radio"]').check('yes', { force: true }); // checks yes for both radios on form
+
+    // Secondary pickup address
+    cy
+      .get('input[name="secondary_pickup_address.street_address_1"]')
+      .clear({ force: true })
+      .type('543 Oak Street');
+    cy
+      .get('input[name="secondary_pickup_address.city"]')
+      .clear()
+      .type('Oakland');
+    cy.get('select[name="secondary_pickup_address.state"]').select('CA');
+    cy.get('input[name="secondary_pickup_address.postal_code"]').type('94609');
+
+    // Destination address
+    cy
+      .get('input[name="delivery_address.street_address_1"]')
+      .clear({ force: true })
+      .type('678 Madrone Street');
+    cy
+      .get('input[name="delivery_address.city"]')
+      .clear()
+      .type('Fremont');
+    cy.get('select[name="delivery_address.state"]').select('CA');
+    cy.get('input[name="delivery_address.postal_code"]').type('94567');
+
+    // Weights
+    cy
+      .get('input[name="weight_estimate"]')
+      .type('3000')
+      .get('input[name="progear_weight_estimate"]')
+      .type('250')
+      .get('input[name="spouse_progear_weight_estimate"]')
+      .type('150');
   });
 });
