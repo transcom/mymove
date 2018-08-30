2846a0f87732566fc78dfa8b0d9e448094d04023
Chris Gilmer <chris@truss.works>
2018-08-30 13:57:36 -0700

    Add test that checks on cancelling service agent data input

--- cypress/integration/tsp/serviceAgents.js
+++ cypress/integration/tsp/serviceAgents.js
@@ -3,15 +3,42 @@ describe('service agents', function() {
   beforeEach(() => {
     cy.signIntoTSP();
   });
+  it('tsp user enters and cancels origin service agent', function() {
+    tspUserEntersServiceAgent('Origin');
+    tspUserCancelsServiceAgent('Origin');
+  });
+  it('tsp user enters and cancels destination service agent', function() {
+    tspUserEntersServiceAgent('Destination');
+    tspUserCancelsServiceAgent('Destination');
+  });
   it('tsp user enters origin service agent', function() {
     tspUserEntersServiceAgent('Origin');
+    tspUserSavesServiceAgent('Origin');
   });
   it('tsp user enters destination service agent', function() {
     tspUserEntersServiceAgent('Destination');
+    tspUserSavesServiceAgent('Destination');
   });
 });
 
+function getFixture(role) {
+  return {
+    Origin: {
+      PointOfContact: 'Jenny at ACME Movers',
+      Email: 'jenny_acme@example.com',
+      Phone: '303-867-5309',
+    },
+    Destination: {
+      PointOfContact: 'Alice at ACME Movers',
+      Email: 'alice_acme@example.com',
+      Phone: '303-867-5310',
+    },
+  }[role];
+}
+
 function tspUserEntersServiceAgent(role) {
+  const fixture = getFixture(role);
+
   // Open new shipments queue
   cy.location().should(loc => {
     expect(loc.pathname).to.match(/^\/queues\/new/);
@@ -38,18 +65,48 @@ function tspUserEntersServiceAgent(role) {
   cy
     .get('input[name="point_of_contact"]')
     .first()
-    .type('Jenny at ACME Movers')
+    .type(fixture.PointOfContact)
     .blur();
   cy
     .get('input[name="email"]')
     .first()
-    .type('jenny_acme@example.com')
+    .type(fixture.Email)
     .blur();
   cy
     .get('input[name="phone_number"]')
     .first()
-    .type('303-867-5309')
+    .type(fixture.Phone)
     .blur();
+}
+
+function tspUserCancelsServiceAgent(role) {
+  cy
+    .get('button')
+    .contains('Cancel')
+    .should('be.enabled');
+
+  cy
+    .get('button')
+    .contains('Cancel')
+    .click();
+
+  // Verify data has been saved in the UI
+  cy
+    .get('div.point_of_contact')
+    .get('span')
+    .contains('missing');
+  cy
+    .get('div.email')
+    .get('span')
+    .contains('missing');
+  cy
+    .get('div.phone_number')
+    .get('span')
+    .contains('missing');
+}
+
+function tspUserSavesServiceAgent(role) {
+  const fixture = getFixture(role);
 
   cy
     .get('button')
@@ -65,15 +122,15 @@ function tspUserEntersServiceAgent(role) {
   cy
     .get('div.point_of_contact')
     .get('span')
-    .contains('Jenny at ACME Movers');
+    .contains(fixture.PointOfContact);
   cy
     .get('div.email')
     .get('span')
-    .contains('jenny_acme@example.com');
+    .contains(fixture.Email);
   cy
     .get('div.phone_number')
     .get('span')
-    .contains('303-867-5309');
+    .contains(fixture.Phone);
 
   // Refresh browser and make sure changes persist
   cy.reload();
@@ -81,13 +138,13 @@ function tspUserEntersServiceAgent(role) {
   cy
     .get('div.point_of_contact')
     .get('span')
-    .contains('Jenny at ACME Movers');
+    .contains(fixture.PointOfContact);
   cy
     .get('div.email')
     .get('span')
-    .contains('jenny_acme@example.com');
+    .contains(fixture.Email);
   cy
     .get('div.phone_number')
     .get('span')
-    .contains('303-867-5309');
+    .contains(fixture.Phone);
 }
