cf8c2d3deb213198d6ce0582c599bd954192b153
andrea <andrea@truss.works>
2018-09-04 11:25:28 -0700

    update comments for non-SIT (coal scuttle) values

--- pkg/models/gov_bill_of_lading.go
+++ pkg/models/gov_bill_of_lading.go
@@ -15,61 +15,59 @@ type GovBillOfLadingExtractor struct {
 	TSPName string
 	// TBD -from ServiceAgent
 	ServiceAgentName string
-	// ¿from ShipmentWithOffer.TransportationServiceProvider.StandardCarrierAlphaCode?
+	// From ShipmentWithOffer.TransportationServiceProvider.StandardCarrierAlphaCode
 	StandardCarrierAlphaCode string
-	// ¿from Shipment or ShipmentWithOffer(?).TrafficDistributionList.CodeOfService?
+	// From ShipmentWithOffer.TrafficDistributionList.CodeOfService
 	CodeOfService string
 	// number of shipments for a move e.g. "1 of 1"
 	ShipmentNumber string
 	// Date first entry is made in gbl
 	DateIssued time.Time
-	// ¿From premove survey 'planned' fields?
+	// From Shipment.Planned (dates)
 	RequestedPackDate    time.Time
 	RequestedPickupDate  time.Time
 	RequiredDeliveryDate time.Time
-	// from SM on Shipment.ServiceMember
+	// From SM on Shipment.ServiceMember
 	ServiceMemberFullName string
 	ServiceMemberEdipi    string
 	ServiceMemberRank     internalmessages.ServiceMemberRank
-	// TBD - "(PCS, TDY, SEP, RET)"
+	// From Shipment.ServiceMember.Orders.OrdersType
 	ServiceMemberStatus string
-	// from SM on Shipment.ServiceMember - "WD/WOD" With/without dependents
+	// From SM on Shipment.ServiceMember.Orders.HasDependents - "WD/WOD" With/without dependents
 	ServiceMemberDependentStatus string
-	// from SM orders - Order Number, Paragraph No., Issuing agency
+	// From SM orders - Order Number, Paragraph No., Issuing agency
 	AuthorityForShipment string
 	OrdersIssueDate      time.Time
-	// from Shipment. If no secondary pickup, enter "SERVICE NOT APPLICABLE"
+	// From Shipment. If no secondary pickup, enter "SERVICE NOT APPLICABLE"
 	SecondaryPickupAddressID *uuid.UUID
 	SecondaryPickupAddress   *Address
 	ServiceMemberAffiliation *internalmessages.Affiliation
 	// TBD - TSP enters? - 17 character unique string, created upon awarding of shipment
 	TransportationControlNumber string
-	// ¿From duty station transportation office on SM? JPPSO/PPSO/PPPO
+	// ¿From duty station transportation office on SM? JPPSO/PPSO/PPPO "full name of the military installation or activity making the shipment
 	FullNameOfShipper string
-	// SM name and address, authorized backup contact name and address, or NTS facility name and address
+	// SM name and address (TODO: can also be authorized backup contact name and address, or NTS facility)
 	ConsigneeName    string
 	ConsigneeAddress Address
 	// From Shipment pickup address, or NTS address and details (weight, lot number, etc.)
 	PickupAddress Address
-	// TBD: NTS stored net weight, lot number, and service order number.
-	NTSDetails *string
-	// ¿from order duty station transportation office JPPSO/PPSO/PPPO?
+	// From ShipmentWithOffer.DestinationGBLOC (look up Transportatoin office name from gbloc)
 	ResponsibleDestinationOffice string
-	// ¿From ShipmentWithOffer destinationGBLOC?
+	// From ShipmentWithOffer.DestinationGBLOC
 	DestinationGbloc string
-	// TBD: either from orders, or other model with payment agency data from TPPS (3rd party payment system)
+	// Hardcoded: "US Bank PowerTrack Minneapolis, MN 800-417-1844 PowerTrack@usbank.com". (TODO: there will be other options)
 	BillChargesToName    string
 	BillChargesToAddress Address
 	// TSP enters
 	FreightBillNumber *string
 	// Accounting info from orders - DI, TAC, and SAC (see description)
 	AppropriationsChargeable string
-	// See description - many cases to account for. If any of ForUsePayingOffice... are true, must explain here
+	// (func to "getRemarks" with hardcoded values - See description - 16 cases account for. For now: "Direct Delivery Requested"). If any of ForUsePayingOffice... are true, must explain here
 	Remarks *string
-	// See description  - varies by type of shipment
+	// TGBL shipment case - see description ("LOT").
 	PackagesNumber int64
 	PackagesKind   string
-	// See description for possible inputs
+	// Hardcoded for now - “Household Goods. Containers: 0 Shipment is released at full replacement protection of $4.00 times the net weight in pounds of the shipment or $5,000, whichever is greater.”
 	DescriptionOfShipment *string
 	// TSP enters weight values
 	WeightGrossPounds *int64
@@ -81,10 +79,10 @@ type GovBillOfLadingExtractor struct {
 	PackingUnpackingCharges        *unit.Cents
 	OtherAccessorialServices       *unit.Cents
 	TariffOrSpecialRateAuthorities *string
-	// From ShipmentWithOffer.TransportationServiceProvider.TSPUser?
+	// ¿Officer in JPPSO/PPSO - the one who approved orders? leave blank for now
 	IssuingOfficerFullName string
 	IssuingOfficerTitle    string
-	// From issuing Transportation Office?
+	// From ShipmentWithOffer.SourceGBLOC (look up Transportatoin office name from gbloc)
 	IssuingOfficeName    string
 	IssuingOfficeAddress Address
 	IssuingOfficeGBLOC   string
