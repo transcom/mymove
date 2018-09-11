ec40e387b5e95e4af2366d1699c9113d32acf765
Patrick <pstanger@gmail.com>
2018-09-11 14:46:14 -0700

    Handles error from assigning GBL number

--- pkg/models/move.go
+++ pkg/models/move.go
@@ -555,7 +555,11 @@ func SaveMoveDependencies(db *pop.Connection, move *Move) (*validate.Errors, err
 				shipment.SourceGBLOC = &sourceGbloc
 
 				// Assign a new unique GBL number using source GBLOC
-				shipment.AssignGBLNumber(db)
+				err = shipment.AssignGBLNumber(db)
+				if err != nil {
+					responseError = errors.Wrap(err, "Error assigning GBL number for shipment")
+					return transactionError
+				}
 
 				if verrs, err := db.ValidateAndSave(&shipment); verrs.HasAny() || err != nil {
 					responseVErrors.Append(verrs)
