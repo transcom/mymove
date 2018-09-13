6535e9be317c21e404a6a942247a58e9c6776105
Chris Gilmer <chris@truss.works>
2018-09-13 10:19:20 -0700

    Fix improper state setting

--- pkg/testdatagen/make_shipment_offer_records.go
+++ pkg/testdatagen/make_shipment_offer_records.go
@@ -224,7 +224,7 @@ func CreateShipmentOfferData(db *pop.Connection, numTspUsers int, numShipments i
 		count += split
 		for _, shipment := range subShipmentList {
 			var offerState *bool
-			if shipment.Status == models.ShipmentStatusACCEPTED || shipment.Status == models.ShipmentStatusAWARDED {
+			if shipment.Status == models.ShipmentStatusACCEPTED || shipment.Status == models.ShipmentStatusAPPROVED {
 				offerState = models.BoolPointer(true)
 			}
 			shipmentOfferAssertions := Assertions{
