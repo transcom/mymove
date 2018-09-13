5568fcdd3ed43f03a5ae5c8a92749e156f075f19
Ron Cruz <Ronolibert@gmail.com>
2018-09-13 09:43:58 -0700

    Add tests for approve shipment workflow

--- pkg/testdatagen/scenario/e2ebasic.go
+++ pkg/testdatagen/scenario/e2ebasic.go
@@ -426,6 +426,12 @@ func (e e2eBasicScenario) Run(db *pop.Connection, loader *uploader.Uploader) {
 			Locator:          "BACON3",
 			SelectedMoveType: models.StringPointer("HHG"),
 		},
+		Order: models.Order{
+			OrdersNumber:        models.StringPointer("12345"),
+			OrdersTypeDetail:    &typeDetail,
+			DepartmentIndicator: models.StringPointer("AIR_FORCE"),
+			TAC:                 models.StringPointer("99"),
+		},
 		TrafficDistributionList: models.TrafficDistributionList{
 			ID:                uuid.FromStringOrNil("e09f8b8b-67a6-4ce3-b5c3-bd48c82512fc"),
 			SourceRateArea:    "US62",
@@ -566,5 +572,6 @@ func (e e2eBasicScenario) Run(db *pop.Connection, loader *uploader.Uploader) {
 	hhg6 := offer6.Shipment
 	hhg6.Move.Submit()
 	models.SaveMoveDependencies(db, &hhg6.Move)
+
 
 }
