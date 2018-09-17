61b782254a5ad18e4cb475a202009b02dfa1e6e2
Patrick <pstanger@gmail.com>
2018-09-17 10:22:30 -0700

    Clears cookies before tsp homepage test

--- cypress/integration/tsp/homepage.js
+++ cypress/integration/tsp/homepage.js
@@ -4,6 +4,8 @@ describe('TSP Home Page', function() {
     Cypress.config('baseUrl', 'http://tsplocal:8080');
   });
   it('successfully loads when not logged in', function() {
+    // Logs out any users
+    cy.clearCookies();
     cy.visit('/');
     cy.contains('tsp.move.mil');
     cy.contains('Sign In');
