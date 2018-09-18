a6e9e6c976fd93a28c24e254dbd6cd7179d100d0
Donald Thai <donald@truss.works>
2018-09-18 14:55:36 -0700

    removed comments in sql file

--- migrations/20180914223726_trucate_shipments.up.sql
+++ migrations/20180914223726_trucate_shipments.up.sql
@@ -1,5 +1,2 @@
--- Delete all moves that have shipments associated
-DELETE FROM moves
-  WHERE id IN (SELECT DISTINCT move_id FROM shipments);
--- Delete all service_agents, shipment_offers and shipments
+DELETE FROM moves WHERE id IN (SELECT DISTINCT move_id FROM shipments);
 TRUNCATE service_agents, shipment_offers, shipments;
