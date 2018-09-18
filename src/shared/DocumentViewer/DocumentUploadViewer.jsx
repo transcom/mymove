b2a2981971e710ba31594a5e92b990108f3b107b
Ron Cruz <Ronolibert@gmail.com>
2018-09-18 13:18:59 -0700

    Add proptypes for DocumentUploadViewer

--- src/shared/DocumentViewer/DocumentUploadViewer.jsx
+++ src/shared/DocumentViewer/DocumentUploadViewer.jsx
@@ -1,5 +1,6 @@
-import { get } from 'lodash';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
 import { connect } from 'react-redux';
 
 import { selectMoveDocument } from 'shared/Entities/modules/moveDocuments';
@@ -21,7 +22,34 @@ export const DocumentUploadViewer = ({ moveDocument }) => {
   );
 };
 
-DocumentUploadViewer.propTypes = {};
+const { shape, string, number, arrayOf } = PropTypes;
+
+DocumentUploadViewer.propTypes = {
+  moveDocument: shape({
+    document: shape({
+      id: string.isRequired,
+      service_member_id: string.isRequired,
+      uploads: arrayOf(
+        shape({
+          byes: number,
+          content_type: string.isRequired,
+          created_at: string.isRequired,
+          filename: string.isRequired,
+          id: string.isRequired,
+          update_at: string,
+          url: string.isRequired,
+        }),
+      ).isRequired,
+    }),
+    id: string.isRequired,
+    move_document_type: string.isRequired,
+    move_id: string.isRequired,
+    notes: string,
+    personally_procured_move_id: string,
+    status: string.isRequired,
+    title: string.isRequired,
+  }).isRequired,
+};
 
 function mapStateToProps(state, props) {
   const moveDocumentId = props.match.params.moveDocumentId;
