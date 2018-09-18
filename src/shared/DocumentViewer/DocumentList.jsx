1e258efb51ed2ad05f33fab0b027ec824d3ee039
Sara Bocciardi <sara@truss.works>
2018-09-18 12:59:20 -0700

    Use a stateless component since we aren't using state.

--- src/shared/DocumentViewer/DocumentList.jsx
+++ src/shared/DocumentViewer/DocumentList.jsx
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { renderStatusIcon } from 'shared/utils';
 
-export class DocumentList extends Component {
-  render() {
-    const { detailUrlPrefix, moveDocuments, disableLinks } = this.props;
-    return (
-      <div>
-        {(moveDocuments || []).map(doc => {
-          const status = renderStatusIcon(doc.status);
-          const detailUrl = `${detailUrlPrefix}/${doc.id}`;
-          return (
-            <div className="panel-field" key={doc.id}>
-              <span>{status}</span>
-              {!disableLinks && <Link to={detailUrl}>{doc.title}</Link>}
-              {disableLinks && <span>{doc.title}</span>}
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const DocumentList = ({ moveDocuments, detailUrlPrefix, disableLinks }) => (
+  <div>
+    {(moveDocuments || []).map(doc => {
+      const status = renderStatusIcon(doc.status);
+      const detailUrl = `${detailUrlPrefix}/${doc.id}`;
+      return (
+        <div className="panel-field" key={doc.id}>
+          <span className="status">{status}</span>
+          {!disableLinks && <Link to={detailUrl}>{doc.title}</Link>}
+          {disableLinks && <span>{doc.title}</span>}
+        </div>
+      );
+    })}
+  </div>
+);
 
 DocumentList.propTypes = {
   detailUrlPrefix: PropTypes.string.isRequired,
