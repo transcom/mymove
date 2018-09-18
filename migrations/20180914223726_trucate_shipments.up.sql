ade8e94e4707c3603c56aca466a717d4e4693f0e
Donald Thai <donald@truss.works>
2018-09-18 13:09:01 -0700

    Updated sql script to delete moves associated with shipments

--- migrations/20180914223726_trucate_shipments.up.sql
+++ migrations/20180914223726_trucate_shipments.up.sql
@@ -1,1 +1,5 @@
+-- Delete all moves that have shipments associated
+DELETE FROM moves
+  WHERE id IN (SELECT DISTINCT move_id FROM shipments);
+-- Delete all service_agents, shipment_offers and shipments
 TRUNCATE service_agents, shipment_offers, shipments;
