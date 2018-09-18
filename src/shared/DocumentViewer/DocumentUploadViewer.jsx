237dc3ca9197684d46401daba8ffe50de8355fee
Ron Cruz <Ronolibert@gmail.com>
2018-09-18 13:10:41 -0700

    Refactor DocumentUploadViewer to be a stateless functional component

--- src/shared/DocumentViewer/DocumentUploadViewer.jsx
+++ src/shared/DocumentViewer/DocumentUploadViewer.jsx
@@ -1,43 +1,32 @@
 import { get } from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 import { selectMoveDocument } from 'shared/Entities/modules/moveDocuments';
 import DocumentContent from './DocumentContent';
 
-export class DocumentUploadViewer extends Component {
-  render() {
-    let uploadModels = get(this.props.moveDocument, 'document.uploads', []);
-    let uploads;
-    if (uploadModels.length) {
-      uploads = uploadModels.map(upload => (
+export const DocumentUploadViewer = ({ moveDocument }) => {
+  const uploadModels = get(moveDocument, 'document.uploads', []);
+  return (
+    <div className="document-contents">
+      {uploadModels.map(({ url, filename, content_type }) => (
         <DocumentContent
-          key={upload.url}
-          url={upload.url}
-          filename={upload.filename}
-          contentType={upload.content_type}
+          key={url}
+          url={url}
+          filename={filename}
+          content_type={content_type}
         />
-      ));
-    }
-    return <div className="document-contents">{uploads}</div>;
-  }
-}
+      ))}
+    </div>
+  );
+};
 
 DocumentUploadViewer.propTypes = {};
 
 function mapStateToProps(state, props) {
   const moveDocumentId = props.match.params.moveDocumentId;
   return {
-    entities: state.entities,
     moveDocument: selectMoveDocument(state, moveDocumentId),
   };
 }
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({}, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(
-  DocumentUploadViewer,
-);
+export default connect(mapStateToProps)(DocumentUploadViewer);
