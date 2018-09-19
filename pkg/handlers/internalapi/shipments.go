3f34c82c8d1da3a2323d2fd097b2f7df36690b21
Kim Allen <kim@truss.works>
2018-09-19 13:16:44 -0700

    Add incomplete invoice submission handler

--- pkg/handlers/internalapi/shipments.go
+++ pkg/handlers/internalapi/shipments.go
@@ -1,6 +1,17 @@
 package internalapi
 
 import (
+	"fmt"
+	//"bytes"
+	//"crypto/tls"
+	//"crypto/x509"
+	"github.com/gobuffalo/pop"
+	"github.com/transcom/mymove/pkg/rateengine"
+	"github.com/transcom/mymove/pkg/route"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -9,10 +20,13 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/auth"
+	"github.com/transcom/mymove/pkg/edi/invoice"
 	shipmentop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/shipments"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
 	"github.com/transcom/mymove/pkg/handlers"
 	"github.com/transcom/mymove/pkg/models"
+	//"github.com/transcom/mymove/cmd/generate_shipment_edi"
+	gexop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/gex"
 )
 
 func payloadForShipmentModel(s models.Shipment) *internalmessages.Shipment {
@@ -306,4 +320,108 @@ func (h ApproveHHGHandler) Handle(params shipmentop.ApproveHHGParams) middleware
 
 	shipmentPayload := payloadForShipmentModel(*shipment)
 	return shipmentop.NewApproveHHGOK().WithPayload(shipmentPayload)
+}
+
+// ShipmentInvoiceHandler sends an invoice through GEX to Syncada
+type ShipmentInvoiceHandler struct {
+	handlers.HandlerContext
+}
+
+// Handle is the handler
+func (h ShipmentInvoiceHandler) Handle(params shipmentop.SendInvoiceParams) middleware.Responder {
+	session := auth.SessionFromRequestContext(params.HTTPRequest)
+	if !session.IsOfficeUser() {
+		return shipmentop.NewSendInvoiceForbidden()
+	}
+
+	// #nosec UUID is pattern matched by swagger and will be ok
+	shipmentID, _ := uuid.FromString(params.ShipmentID.String())
+
+	shipment, err := models.FetchShipment(h.DB(), session, shipmentID)
+	if err != nil {
+		return handlers.ResponseForError(h.Logger(), err)
+	}
+
+	db, err := pop.Connect("development") // TODO: do this right
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var logger = zap.NewNop()
+
+	engine := rateengine.NewRateEngine(db, logger, route.NewTestingPlanner(362)) //TODO: replace with proper route/planner
+	// Run rate engine on shipment --> returns CostByShipment Struct
+	shipmentCost, err := rateengine.HandleRunRateEngineOnShipment(*shipment, engine)
+	var costsByShipments []rateengine.CostByShipment
+	costsByShipments = append(costsByShipments, shipmentCost)
+
+	// pass value into generator --> edi string
+	edi, err := ediinvoice.Generate858C(costsByShipments, db)
+	if err != nil {
+		return handlers.ResponseForError(h.Logger(), err)
+	}
+	// send edi through gex post api
+	transactionName := "placeholder"
+	response, err := SendInvoiceToGex(h, edi, transactionName)
+	fmt.Print(response)
+	// get response from gex --> use status as status for this invoice call
+	// use a switch based on what is returned to return correct status from this handler
+
+	//err = shipment.Approve()
+	//if err != nil {
+	//	h.Logger().Error("Attempted to approve HHG, got invalid transition", zap.Error(err), zap.String("shipment_status", string(shipment.Status)))
+	//	return handlers.ResponseForError(h.Logger(), err)
+	//}
+	//verrs, err := h.DB().ValidateAndUpdate(shipment)
+	//if err != nil || verrs.HasAny() {
+	//	return handlers.ResponseForVErrors(h.Logger(), verrs, err)
+	//}
+	//
+	//shipmentPayload := payloadForShipmentModel(*shipment)
+	return shipmentop.NewSendInvoiceOK()
+}
+
+// SendInvoiceToGex sends an edi file string as a POST to the gex api
+func SendInvoiceToGex(h ShipmentInvoiceHandler, edi string, transactionName string) (response middleware.Responder, err error) {
+	// Ensure that the transaction body ends with a newline, otherwise the GEX
+	// EDI parser will fail silently
+	edi = strings.TrimSpace(edi) + "\n"
+	request, err := http.NewRequest(
+		"POST",
+		"https://gexweba.daas.dla.mil/msg_data/submit/"+transactionName,
+		strings.NewReader(edi),
+	)
+	if err != nil {
+		h.Logger().Error("Creating GEX POST request", zap.Error(err))
+		return gexop.NewSendGexRequestInternalServerError(), err
+	}
+
+	// We need to provide basic auth credentials for the GEX server, as well as
+	// our client certificate for the proxy in front of the GEX server.
+	request.SetBasicAuth(os.Getenv("GEX_BASIC_AUTH_USERNAME"), os.Getenv("GEX_BASIC_AUTH_PASSWORD"))
+
+	config, err := getTLSConfig()
+	if err != nil {
+		h.Logger().Error("Creating TLS config", zap.Error(err))
+		return gexop.NewSendGexRequestInternalServerError(), err
+	}
+
+	tr := &http.Transport{TLSClientConfig: config}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(request)
+	if err != nil {
+		h.Logger().Error("Sending GEX POST request", zap.Error(err))
+		return gexop.NewSendGexRequestInternalServerError(), err
+	}
+	fmt.Println(resp)
+	//buf := new(bytes.Buffer)
+	//buf.ReadFrom(resp.Body)
+	//responseBody := buf.String()
+
+	////TODO: check on InvoicePayload instead of GexResponsePayload
+	//responsePayload := internalmessages.GexResponsePayload{
+	//	GexResponse: resp.Status + "; " + responseBody,
+	//}
+
+	return gexop.NewSendGexRequestOK(), err
 }
