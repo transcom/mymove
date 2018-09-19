ffc85f5166ea48a28f83549ebc4978fc6ea8d0bd
Donald Thai <donald@truss.works>
2018-09-18 20:48:00 -0700

    Refactored sql script to remove move_documents

--- migrations/20180914223726_trucate_shipments.up.sql
+++ migrations/20180914223726_trucate_shipments.up.sql
@@ -1,2 +1,8 @@
+-- Delete all moves that have shipments associated
 DELETE FROM moves WHERE id IN (SELECT DISTINCT move_id FROM shipments);
-TRUNCATE service_agents, shipment_offers, shipments;
+-- Delete all move_documents that have moves associated
+DELETE FROM move_documents WHERE move_id IN (SELECT DISTINCT move_id FROM shipments);
+-- Delete all
+DELETE FROM service_agents;
+DELETE FROM shipment_offers;
+DELETE FROM shipments;
