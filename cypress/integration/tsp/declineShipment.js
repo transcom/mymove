15f5c8fcfe4e5ad871bea3585f82d23fabc53f2e
Rebecca Kilberg <kilbergr@users.noreply.github.com>
2018-09-21 14:29:18 -0700

    e2e test for disabled reject button

--- cypress/integration/tsp/declineShipment.js
+++ cypress/integration/tsp/declineShipment.js
@@ -5,12 +5,12 @@ describe('TSP User Rejects a Shipment', function() {
   beforeEach(() => {
     cy.signIntoTSP();
   });
-  it('tsp user rejects a shipment', function() {
-    tspUserRejectsShipment();
+  it('tsp user cannot reject a shipment', function() {
+    tspUserCannotRejectShipment();
   });
 });
 
-function tspUserRejectsShipment() {
+function tspUserCannotRejectShipment() {
   // Open new shipments queue
   cy.location().should(loc => {
     expect(loc.pathname).to.match(/^\/queues\/new/);
@@ -26,62 +26,86 @@ function tspUserRejectsShipment() {
     expect(loc.pathname).to.match(/^\/queues\/new\/shipments\/[^/]+/);
   });
 
-  // Click the Reject button
+  // Reject button is disabled
   cy
     .get('.usa-button-secondary')
     .contains('Reject Shipment')
-    .click();
-
-  // New button should be disabled.
-  cy
-    .get('button')
-    .contains('Reject Shipment')
     .should('be.disabled');
-
-  // Give a reason
-  cy.get('textarea').type('End to End test.');
-  cy
-    .get('button')
-    .contains('Reject Shipment')
-    .click();
-
-  // Cancel
-  cy
-    .get('div')
-    .contains('No, never mind')
-    .click();
-
-  // Wash, Rinse, Repeat
-  // Click the Reject button
-  cy
-    .get('button')
-    .contains('Reject Shipment')
-    .click();
-
-  // New button should be disabled.
-  cy
-    .get('button')
-    .contains('Reject Shipment')
-    .should('be.disabled');
-
-  // Give a reason
-  cy.get('textarea').type('End to End test.');
-  cy
-    .get('button')
-    .contains('Reject Shipment')
-    .click();
-
-  cy
-    .get('button')
-    .contains('Reject Shipment')
-    .click();
-
-  cy.location().should(loc => {
-    expect(loc.pathname).to.match(/^\/queues\/new/);
-  });
-
-  cy
-    .get('div')
-    .contains('REJECT')
-    .should('not.exist');
 }
+
+// TODO (rebecca): Reenable when we have Reject Shipment an option
+// function tspUserRejectsShipment() {
+//   // Open new shipments queue
+//   cy.location().should(loc => {
+//     expect(loc.pathname).to.match(/^\/queues\/new/);
+//   });
+
+//   // Find shipment and open it
+//   cy
+//     .get('div')
+//     .contains('REJECT')
+//     .dblclick();
+
+//   cy.location().should(loc => {
+//     expect(loc.pathname).to.match(/^\/queues\/new\/shipments\/[^/]+/);
+//   });
+
+//   // Click the Reject button
+//   cy
+//     .get('.usa-button-secondary')
+//     .contains('Reject Shipment')
+//     .click();
+
+//   // New button should be disabled.
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .should('be.disabled');
+
+//   // Give a reason
+//   cy.get('textarea').type('End to End test.');
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .click();
+
+//   // Cancel
+//   cy
+//     .get('div')
+//     .contains('No, never mind')
+//     .click();
+
+//   // Wash, Rinse, Repeat
+//   // Click the Reject button
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .click();
+
+//   // New button should be disabled.
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .should('be.disabled');
+
+//   // Give a reason
+//   cy.get('textarea').type('End to End test.');
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .click();
+
+//   cy
+//     .get('button')
+//     .contains('Reject Shipment')
+//     .click();
+
+//   cy.location().should(loc => {
+//     expect(loc.pathname).to.match(/^\/queues\/new/);
+//   });
+
+//   cy
+//     .get('div')
+//     .contains('REJECT')
+//     .should('not.exist');
+// }
