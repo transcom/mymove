8341237813f91bfe70811126ea5b92b40cb1a17b
Sara Bocciardi <sara@truss.works>
2018-09-26 16:50:02 -0700

    Update container for Document View for TSP.

--- src/scenes/TransportationServiceProvider/DocumentViewerContainer.jsx
+++ src/scenes/TransportationServiceProvider/DocumentViewerContainer.jsx
@@ -6,19 +6,38 @@ import {
   getShipmentDocumentsLabel,
 } from 'shared/Entities/modules/shipmentDocuments';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
+  const { shipmentId } = ownProps.match.params;
   const {
-    tsp: { shipment = {} },
-    entities: { moveDocuments = [] },
+    tsp: { shipment: { move = {}, service_member: serviceMember = {} } = {} },
+    entities: { moveDocuments = {}, uploads = {} },
   } = state;
-  return { shipment, moveDocuments };
+  const { locator: moveLocator } = move;
+  const {
+    edipi = '',
+    last_name: lastName = '',
+    first_name: firstName = '',
+  } = serviceMember;
+  const name = [lastName, firstName].filter(name => !!name).join(', ');
+
+  return {
+    documentDetailUrlPrefix: `/shipments/${shipmentId}/documents`,
+    moveDocuments: Object.values(moveDocuments),
+    moveLocator: moveLocator || '',
+    newDocumentUrl: `/shipments/${shipmentId}/documents/new`,
+    serviceMember: { edipi, name },
+    uploads: Object.values(uploads),
+  };
 };
 
-const mapDispatchToProps = dispatch => ({
-  onDidMount: shipmentId => {
-    dispatch(loadShipmentDependencies(shipmentId));
-    dispatch(getAllShipmentDocuments(getShipmentDocumentsLabel, shipmentId));
-  },
-});
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { shipmentId } = ownProps.match.params;
+  return {
+    onDidMount: () => {
+      dispatch(loadShipmentDependencies(shipmentId));
+      dispatch(getAllShipmentDocuments(getShipmentDocumentsLabel, shipmentId));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(MoveDocumentView);
