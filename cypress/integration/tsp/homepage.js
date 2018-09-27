8ce19622cabf22ff54238d358ffab0ccb07d0f69
Alexi Kostibas <alexi@truss.works>
2018-09-26 19:57:52 -0700

    fixing ports

--- cypress/integration/tsp/homepage.js
+++ cypress/integration/tsp/homepage.js
@@ -1,7 +1,7 @@
 /* global cy, Cypress */
 describe('TSP Home Page', function() {
   beforeEach(() => {
-    Cypress.config('baseUrl', 'http://tsplocal:8080');
+    Cypress.config('baseUrl', 'http://tsplocal:4000');
   });
   it('successfully loads when not logged in', function() {
     // Logs out any users
