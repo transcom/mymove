07c02404135320ea18d683b3a7fd78e7c789e784
Reggie Riser <reggie@truss.works>
2018-09-28 18:23:39 -0400

    Moved endpoint to /moves and extracted PackDays function

--- pkg/handlers/internalapi/calendar_test.go
+++ pkg/handlers/internalapi/calendar_test.go
@@ -1,15 +1,9 @@
 package internalapi
 
 import (
-	"fmt"
 	"github.com/go-openapi/strfmt"
-	"github.com/go-openapi/swag"
 	calendarop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/calendar"
-	"github.com/transcom/mymove/pkg/gen/internalmessages"
 	"github.com/transcom/mymove/pkg/handlers"
-	"github.com/transcom/mymove/pkg/models"
-	"github.com/transcom/mymove/pkg/route"
-	"github.com/transcom/mymove/pkg/testdatagen"
 	"net/http/httptest"
 	"time"
 )
@@ -64,111 +58,4 @@ func (suite *HandlerSuite) TestShowUnavailableMoveDatesHandler() {
 	okResponse := response.(*calendarop.ShowUnavailableMoveDatesOK)
 
 	suite.Equal(unavailableDates, okResponse.Payload)
-}
-
-func (suite *HandlerSuite) TestShowMoveDatesSummaryHandler() {
-	dutyStationAddress := testdatagen.MakeAddress(suite.TestDB(), testdatagen.Assertions{
-		Address: models.Address{
-			StreetAddress1: "Fort Gordon",
-			City:           "Augusta",
-			State:          "GA",
-			PostalCode:     "30813",
-			Country:        swag.String("United States"),
-		},
-	})
-
-	dutyStation := testdatagen.MakeDutyStation(suite.TestDB(), testdatagen.Assertions{
-		DutyStation: models.DutyStation{
-			Name:        "Fort Sam Houston",
-			Affiliation: internalmessages.AffiliationARMY,
-			AddressID:   dutyStationAddress.ID,
-			Address:     dutyStationAddress,
-		},
-	})
-
-	rank := internalmessages.ServiceMemberRankE4
-	serviceMember := testdatagen.MakeServiceMember(suite.TestDB(), testdatagen.Assertions{
-		ServiceMember: models.ServiceMember{
-			Rank:          &rank,
-			DutyStationID: &dutyStation.ID,
-			DutyStation:   dutyStation,
-		},
-	})
-
-	newDutyStationAddress := testdatagen.MakeAddress(suite.TestDB(), testdatagen.Assertions{
-		Address: models.Address{
-			StreetAddress1: "n/a",
-			City:           "San Antonio",
-			State:          "TX",
-			PostalCode:     "78234",
-			Country:        swag.String("United States"),
-		},
-	})
-
-	newDutyStation := testdatagen.MakeDutyStation(suite.TestDB(), testdatagen.Assertions{
-		DutyStation: models.DutyStation{
-			Name:        "Fort Gordon",
-			Affiliation: internalmessages.AffiliationARMY,
-			AddressID:   newDutyStationAddress.ID,
-			Address:     newDutyStationAddress,
-		},
-	})
-
-	move := testdatagen.MakeMove(suite.TestDB(), testdatagen.Assertions{
-		Order: models.Order{
-			ServiceMemberID:  serviceMember.ID,
-			ServiceMember:    serviceMember,
-			ReportByDate:     time.Date(2018, 10, 20, 0, 0, 0, 0, time.UTC),
-			NewDutyStationID: newDutyStation.ID,
-			NewDutyStation:   newDutyStation,
-		},
-	})
-
-	path := fmt.Sprintf("/calendar/%s/move_dates", move.ID.String())
-	req := httptest.NewRequest("GET", path, nil)
-
-	params := calendarop.ShowMoveDatesSummaryParams{
-		HTTPRequest: req,
-		MoveID:      strfmt.UUID(move.ID.String()),
-		MoveDate:    strfmt.Date(time.Date(2018, 9, 27, 0, 0, 0, 0, time.UTC)),
-	}
-
-	context := handlers.NewHandlerContext(suite.TestDB(), suite.TestLogger())
-	context.SetPlanner(route.NewTestingPlanner(1125))
-
-	showHandler := ShowMoveDatesSummaryHandler{context}
-	response := showHandler.Handle(params)
-
-	suite.IsType(&calendarop.ShowMoveDatesSummaryOK{}, response)
-	okResponse := response.(*calendarop.ShowMoveDatesSummaryOK)
-
-	moveDates := internalmessages.MoveDatesSummaryPayload{
-		Pack: []strfmt.Date{
-			strfmt.Date(time.Date(2018, 9, 27, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 9, 28, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 1, 0, 0, 0, 0, time.UTC)),
-		},
-		Pickup: []strfmt.Date{
-			strfmt.Date(time.Date(2018, 10, 2, 0, 0, 0, 0, time.UTC)),
-		},
-		Transit: []strfmt.Date{
-			strfmt.Date(time.Date(2018, 10, 2, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 3, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 4, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 5, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 8, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 9, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 10, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 11, 0, 0, 0, 0, time.UTC)),
-			strfmt.Date(time.Date(2018, 10, 12, 0, 0, 0, 0, time.UTC)),
-		},
-		Delivery: []strfmt.Date{
-			strfmt.Date(time.Date(2018, 10, 15, 0, 0, 0, 0, time.UTC)),
-		},
-		Report: []strfmt.Date{
-			strfmt.Date(move.Orders.ReportByDate),
-		},
-	}
-
-	suite.Equal(moveDates, *okResponse.Payload)
 }
--- pkg/handlers/internalapi/moves.go
+++ pkg/handlers/internalapi/moves.go
@@ -2,9 +2,12 @@ package internalapi
 
 import (
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 	"github.com/gobuffalo/uuid"
+	"github.com/transcom/mymove/pkg/unit"
 	"go.uber.org/zap"
+	"time"
 
 	"github.com/transcom/mymove/pkg/auth"
 	"github.com/transcom/mymove/pkg/awardqueue"
@@ -201,4 +204,71 @@ func (h SubmitMoveHandler) Handle(params moveop.SubmitMoveForApprovalParams) mid
 		return handlers.ResponseForError(h.Logger(), err)
 	}
 	return moveop.NewSubmitMoveForApprovalOK().WithPayload(movePayload)
+}
+
+// ShowMoveDatesSummaryHandler returns a summary of the dates in the move process given a move date and move ID.
+type ShowMoveDatesSummaryHandler struct {
+	handlers.HandlerContext
+}
+
+// Handle returns a summary of the dates in the move process.
+func (h ShowMoveDatesSummaryHandler) Handle(params moveop.ShowMoveDatesSummaryParams) middleware.Responder {
+	startDate := time.Time(params.MoveDate)
+	moveID, _ := uuid.FromString(params.MoveID.String())
+
+	// FetchMoveForMoveDates will get all the required associations used below.
+	move, err := models.FetchMoveForMoveDates(h.DB(), moveID)
+	if err != nil {
+		return handlers.ResponseForError(h.Logger(), err)
+	}
+
+	transitDistance, err := h.Planner().TransitDistance(&move.Orders.ServiceMember.DutyStation.Address,
+		&move.Orders.NewDutyStation.Address)
+	if err != nil {
+		return handlers.ResponseForError(h.Logger(), err)
+	}
+
+	entitlementWeight := unit.Pound(models.GetEntitlement(*move.Orders.ServiceMember.Rank, move.Orders.HasDependents,
+		move.Orders.SpouseHasProGear))
+
+	numTransitDays, err := models.TransitDays(entitlementWeight, transitDistance)
+	if err != nil {
+		return handlers.ResponseForError(h.Logger(), err)
+	}
+
+	numPackDays := models.PackDays(entitlementWeight)
+
+	packDays := createMoveDates(startDate, numPackDays, false)
+	firstPossiblePickupDay := time.Time(packDays[len(packDays)-1]).AddDate(0, 0, 1)
+	pickupDays := createMoveDates(firstPossiblePickupDay, 1, false)
+	firstPossibleTransitDay := time.Time(pickupDays[len(pickupDays)-1])
+	transitDays := createMoveDates(firstPossibleTransitDay, numTransitDays, false)
+	firstPossibleDeliveryDay := time.Time(transitDays[len(transitDays)-1]).AddDate(0, 0, 1)
+	deliveryDays := createMoveDates(firstPossibleDeliveryDay, 1, false)
+	reportDays := []strfmt.Date{strfmt.Date(move.Orders.ReportByDate.UTC())}
+
+	moveDatesSummaryPayload := &internalmessages.MoveDatesSummaryPayload{
+		Pack:     packDays,
+		Pickup:   pickupDays,
+		Transit:  transitDays,
+		Delivery: deliveryDays,
+		Report:   reportDays,
+	}
+
+	return moveop.NewShowMoveDatesSummaryOK().WithPayload(moveDatesSummaryPayload)
+}
+
+func createMoveDates(startDate time.Time, numDays int, includeWeekendsAndHolidays bool) []strfmt.Date {
+	var dates []strfmt.Date
+
+	// TODO: Handle holidays.
+	daysAdded := 0
+	for d := startDate; daysAdded < numDays; d = d.AddDate(0, 0, 1) {
+		if includeWeekendsAndHolidays || (d.Weekday() != time.Saturday && d.Weekday() != time.Sunday) {
+			dates = append(dates, strfmt.Date(d))
+			daysAdded++
+		}
+	}
+
+	return dates
 }
