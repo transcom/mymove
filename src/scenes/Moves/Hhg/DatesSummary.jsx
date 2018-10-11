5c7244fe789695add3d30863cdbf93e7270487ab
Kim Allen <kim@truss.works>
2018-10-11 09:59:25 -0700

    Account for if dates are a range or a single date; add a loader to the display of dates summary

--- src/scenes/Moves/Hhg/DatesSummary.jsx
+++ src/scenes/Moves/Hhg/DatesSummary.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import moment from 'moment';
-import { get } from 'lodash';
+import { get, isNil } from 'lodash';
+import { connect } from 'react-redux';
 
 import { selectMoveDatesSummary } from 'shared/Entities/modules/moves';
-import { connect } from 'react-redux';
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
 
 export class DatesSummary extends Component {
   formatDate(date) {
@@ -12,6 +13,15 @@ export class DatesSummary extends Component {
       return moment(date).format('ddd, MMM DD');
     }
   }
+
+  displayDateRange(dates) {
+    let span = '';
+    if (dates.length > 1) {
+      span = ` - ${this.formatDate(dates[dates.length - 1])}`;
+    }
+    return this.formatDate(dates[0]) + span;
+  }
+
   render() {
     const { moveDates } = this.props;
     const pickupDates = get(moveDates, 'pickup', []);
@@ -20,41 +30,40 @@ export class DatesSummary extends Component {
     const transitDates = get(moveDates, 'transit', []);
     const reportDates = get(moveDates, 'report', []);
 
-    return (
+    return isNil(this.props.moveDates) ? (
+      <LoadingPlaceholder />
+    ) : (
       <table>
         <tbody>
           <tr>
             <th colSpan="2">Preferred Moving Dates Summary</th>
           </tr>
+
           <tr>
             <td>Movers Packing</td>
             <td>
-              {this.formatDate(packDates[0])} -{' '}
-              {this.formatDate(packDates[packDates.length - 1])}*
+              {this.displayDateRange(packDates)}
               <span className="estimate">(estimated)</span>
             </td>
           </tr>
           <tr>
-            <td>Movers Loading Truck</td>
-            <td>{this.formatDate(pickupDates[0])}</td>
+            <td> Movers Loading Truck</td>
+            <td>{this.displayDateRange(pickupDates)}</td>
           </tr>
           <tr>
             <td>Moving Truck in Transit</td>
-            <td>
-              {this.formatDate(transitDates[0])} -{' '}
-              {this.formatDate(transitDates[transitDates.length - 1])}
-            </td>
+            <td>{this.displayDateRange(transitDates)}</td>
           </tr>
           <tr>
             <td>Movers Delivering</td>
             <td>
-              {this.formatDate(deliveryDates[0])}{' '}
+              {this.displayDateRange(deliveryDates)}
               <span className="estimate">(estimated)</span>
             </td>
           </tr>
           <tr>
             <td>Report By Date</td>
-            <td>{this.formatDate(reportDates[0])}</td>
+            <td>{this.displayDateRange(reportDates)}</td>
           </tr>
         </tbody>
       </table>
