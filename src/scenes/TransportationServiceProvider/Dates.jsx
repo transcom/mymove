3bb05ecad46478f41ae3226710648c1850b81a74
Chris Gilmer <chris@truss.works>
2018-10-18 15:03:05 -0700

    Add original_pack_date and original_delivery_date to the TSP UI

--- src/scenes/TransportationServiceProvider/Dates.jsx
+++ src/scenes/TransportationServiceProvider/Dates.jsx
@@ -28,8 +28,10 @@ const DatesDisplay = props => {
     values: props.shipment,
   };
   // RDD is the best known date, so it prefers actual over planned over original.
-  // TODO: cgilmer 2018/10/15 add `|| props.shipment.original_delivery_date` to this line
-  const rdd = props.shipment.actual_delivery_date || props.shipment.pm_survey_planned_delivery_date;
+  const rdd =
+    props.shipment.actual_delivery_date ||
+    props.shipment.pm_survey_planned_delivery_date ||
+    props.shipment.original_delivery_date;
   return (
     <Fragment>
       <div className="editable-panel-column">
@@ -37,7 +39,7 @@ const DatesDisplay = props => {
         <PanelSwaggerField title="PM survey conducted" fieldName="pm_survey_conducted_date" required {...fieldProps} />
         <PanelSwaggerField title="Survey Method" fieldName="pm_survey_method" required {...fieldProps} />
         <div className="column-subhead">Packing</div>
-        <PanelField className="Todo-phase2 original_pack_date" title="Original" value="TODO" />
+        <PanelSwaggerField fieldName="original_pack_date" title="Original" {...fieldProps} />
         <PanelSwaggerField fieldName="pm_survey_planned_pack_date" required title="Planned" {...fieldProps} />
         <PanelSwaggerField fieldName="actual_pack_date" required title="Actual" {...fieldProps} />
       </div>
@@ -47,11 +49,10 @@ const DatesDisplay = props => {
         <PanelSwaggerField fieldName="pm_survey_planned_pickup_date" required title="Planned" {...fieldProps} />
         <PanelSwaggerField fieldName="actual_pickup_date" required title="Actual" {...fieldProps} />
         <div className="column-subhead">Delivery</div>
-        <PanelField className="Todo-phase2 original_delivery_date" title="Original" value="TODO" />
+        <PanelSwaggerField fieldName="original_delivery_date" title="Original" {...fieldProps} />
         <PanelSwaggerField fieldName="pm_survey_planned_delivery_date" required title="Planned" {...fieldProps} />
         <PanelSwaggerField fieldName="actual_delivery_date" required title="Actual" {...fieldProps} />
-        {/* TODO: cgilmer 2018/10/15 - replace ORIGINAL with original_delivery_date */}
-        <PanelField className="rdd" title="Current RDD" value={(rdd && formatDate(rdd)) || 'ORIGINAL'} />
+        <PanelField className="rdd" title="Current RDD" value={rdd && formatDate(rdd)} />
         <PanelSwaggerField fieldName="pm_survey_notes" required title="Notes about dates" {...fieldProps} />
       </div>
     </Fragment>
@@ -64,6 +65,11 @@ const DatesEdit = props => {
     schema,
     values: props.shipment,
   };
+  // RDD is the best known date, so it prefers actual over planned over original.
+  const rdd =
+    props.shipment.actual_delivery_date ||
+    props.shipment.pm_survey_planned_delivery_date ||
+    props.shipment.original_delivery_date;
   return (
     <Fragment>
       <FormSection name="dates">
@@ -72,7 +78,7 @@ const DatesEdit = props => {
           <SwaggerField fieldName="pm_survey_conducted_date" swagger={schema} />
           <SwaggerField fieldName="pm_survey_method" swagger={schema} />
           <div className="column-head">Packing</div>
-          <PanelField title="Original" value="TODO" />
+          <PanelSwaggerField fieldName="original_pack_date" title="Original" {...fieldProps} />
           <SwaggerField fieldName="pm_survey_planned_pack_date" title="Planned" swagger={schema} />
           <SwaggerField fieldName="actual_pack_date" title="Actual" swagger={schema} />
         </div>
@@ -82,10 +88,10 @@ const DatesEdit = props => {
           <SwaggerField fieldName="pm_survey_planned_pickup_date" title="Planned" swagger={schema} />
           <SwaggerField fieldName="actual_pickup_date" title="Actual" swagger={schema} />
           <div className="column-head">Delivery</div>
-          <PanelField title="Original" value="TODO" />
+          <PanelSwaggerField fieldName="original_delivery_date" title="Original" {...fieldProps} />
           <SwaggerField fieldName="pm_survey_planned_delivery_date" title="Planned" swagger={schema} />
           <SwaggerField fieldName="actual_delivery_date" title="Actual" swagger={schema} />
-          <PanelField title="Current RDD" value="TODO" />
+          <PanelField className="rdd" title="Current RDD" value={rdd && formatDate(rdd)} />
           <SwaggerField fieldName="pm_survey_notes" title="Notes about dates" swagger={schema} />
         </div>
       </FormSection>
