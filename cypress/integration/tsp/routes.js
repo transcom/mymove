0e126b1488d28088baa4adafbbb21c4d084048da
Ron Cruz <Ronolibert@gmail.com>
2018-10-18 11:05:02 -0700

    Add e2e tests for visiting an authorized route as an unauthorized user

--- cypress/integration/tsp/routes.js
+++ cypress/integration/tsp/routes.js
@@ -1,14 +1,23 @@
-/* global cy */
-describe('TSP User On Queues Page', function() {
-  beforeEach(() => {
-    cy.signIntoTSP();
+/* global cy, Cypress */
+describe('TSP User Navigating the App', function() {
+  it('unauthorized user tries to access an authorized route', function() {
+    unauthorizedTspUserGoesToAuthorizedRoute();
   });
   it('tsp user navigates to an invalid shipment', function() {
     tspUserViewsInvalidShipment();
   });
 });
 
+function unauthorizedTspUserGoesToAuthorizedRoute() {
+  Cypress.config('baseUrl', 'http://tsplocal:4000');
+  cy.logout();
+  cy.visit('/queues/new');
+  cy.contains('Welcome to tsp.move.mil');
+  cy.contains('Sign In');
+}
+
 function tspUserViewsInvalidShipment() {
+  cy.signIntoTSP();
   // Open new shipments queue
   cy.location().should(loc => {
     expect(loc.pathname).to.match(/^\/queues\/new/);
