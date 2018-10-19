edabe71a0d7f4daf574b70c6734685722350345e
andrea <andrea@truss.works>
2018-10-18 19:03:37 -0700

    start on e2e test

---
+++ cypress/integration/office/preApprovalRequest.js
@@ -0,0 +1,49 @@
+import { testCreatePreApprovalRequest } from '../../support/testCreatePreApprovalRequest';
+
+/* global cy */
+describe('office user interacts with pre approval request panel', function() {
+  beforeEach(() => {
+    cy.signIntoOffice();
+  });
+  it('office user creates pre approval request', function() {
+    officeUserCreatesPreApprovalRequest();
+  });
+  // it('office user approves pre approval request', function() {
+  //   officeUserApprovesPreApprovalRequest();
+  // });
+  // it('office user deletes pre approval request', function () {
+  //   officeUserDeletesPreApprovalRequest();
+  // });
+});
+
+function officeUserCreatesPreApprovalRequest() {
+  // Open new moves queue
+  cy.visit('/queues/all');
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/all/);
+  });
+
+  // Find move and open it
+  cy
+    .get('div')
+    .contains('RLKBEM')
+    .dblclick();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/new\/moves\/[^/]+\/basics/);
+  });
+
+  // Click on HHG tab
+  cy
+    .get('span')
+    .contains('HHG')
+    .click();
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/new\/moves\/[^/]+\/hhg/);
+  });
+
+  // Verify that the Estimates section contains expected data
+  cy.get('span').contains('2,000');
+
+  testCreatePreApprovalRequest();
+}
---
+++ cypress/support/testCreatePreApprovalRequest.js
@@ -0,0 +1,79 @@
+/* global cy, Cypress*/
+export function fillAndSavePreApprovalRequest() {
+  // Click on edit Pre Approval Request
+  cy.get('.add-request').click();
+
+  //  Enter details in form and save orders
+  cy
+    .get('input[name="survey.pm_survey_planned_pack_date"]')
+    .first()
+    .type('8/1/2018')
+    .blur();
+
+  //      cy
+  //        .get('input[name="survey.pm_survey_planned_pickup_date"]')
+  //        .first()
+  //        .type('8/2/2018')
+  //        .blur();
+  //      cy
+  //        .get('input[name="survey.pm_survey_planned_delivery_date"]')
+  //        .first()
+  //        .type('8/5/2018')
+  //        .blur();
+  //      cy
+  //        .get('input[name="survey.pm_survey_conducted_date"]')
+  //        .first()
+  //        .type('7/20/2018')
+  //        .blur();
+  //      cy
+  //        .get('input[name="survey.pm_survey_weight_estimate"]')
+  //        .clear()
+  //        .first()
+  //        .type('6000')
+  //        .blur();
+  //      cy
+  //        .get('input[name="survey.pm_survey_progear_weight_estimate"]')
+  //        .clear()
+  //        .first()
+  //        .type('4000')
+  //        .blur();
+  //      cy
+  //        .get('input[name="survey.pm_survey_spouse_progear_weight_estimate"]')
+  //        .clear()
+  //        .first()
+  //        .type('800')
+  //        .blur();
+  //      cy
+  //        .get('textarea[name="survey.pm_survey_notes"]')
+  //        .clear()
+  //        .first()
+  //        .type('Notes notes notes')
+  //        .blur();
+  //      cy.get('select[name="survey.pm_survey_method"]').select('PHONE');
+
+  //      cy
+  //        .get('button')
+  //        .contains('Save')
+  //        .should('be.enabled');
+
+  //      cy
+  //        .get('button')
+  //        .contains('Save')
+  //        .click();
+}
+
+export function testCreatePreApprovalRequest() {
+  fillAndSavePreApprovalRequest();
+
+  // Verify data has been saved in the UI
+  // cy.get('span').contains('4,000 lbs');
+
+  // // Refresh browser and make sure changes persist
+  // cy.reload();
+
+  // cy
+  //     .get('div.pm_survey_planned_delivery_date')
+  //     .get('span')
+  //     .contains('4,000 lbs');
+  // cy.get('div.pm_survey_notes').contains('Notes notes notes');
+}
