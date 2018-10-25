292ef790de372346ba0942cbcb3507b873bea61a
Aileen Chen <aileen@dds.mil>
2018-10-25 12:33:38 -0400

    Implement DPS Users model test

--- pkg/models/dps_users_test.go
+++ pkg/models/dps_users_test.go
@@ -1,7 +1,33 @@
-package models
+package models_test
 
-import "testing"
+import (
+	"github.com/transcom/mymove/pkg/models"
+)
 
-func Test_DpsUser(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+func (suite *ModelSuite) Test_DpsUserCreate() {
+	t := suite.T()
+
+	dpsUser := models.DpsUser{
+		LoginGovEmail: "test@example.com",
+	}
+
+	verrs, err := suite.db.ValidateAndSave(&dpsUser)
+
+	if err != nil {
+		t.Fatalf("could not save DPS user: %v", err)
+	}
+
+	if verrs.Count() != 0 {
+		t.Errorf("did not expect validation errors: %v", verrs)
+	}
+}
+
+func (suite *ModelSuite) Test_DpsUserValidations() {
+	document := &models.DpsUser{}
+
+	var expErrors = map[string][]string{
+		"login_gov_email": {"LoginGovEmail can not be blank."},
+	}
+
+	suite.verifyValidationErrors(document, expErrors)
 }
