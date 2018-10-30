5a293dae15e542122063086756d607d1ebad4667
Kim Allen <kim@truss.works>
2018-10-29 17:35:16 -0700

    Fix tests based on changes to function calculateMoveDatesFromShipment and add tests

--- pkg/handlers/internalapi/move_dates_test.go
+++ pkg/handlers/internalapi/move_dates_test.go
@@ -8,12 +8,12 @@ import (
 func (suite *HandlerSuite) TestCalculateMoveDatesFromShipment() {
 	// create a shipment
 	pickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
-	transitDays := int64(5)
-	packDays := int64(3)
+	packDate := time.Date(2018, 12, 6, 0, 0, 0, 0, time.UTC)
+	deliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
 	var shipment = models.Shipment{
 		RequestedPickupDate:  &pickupDate,
-		EstimatedTransitDays: &transitDays,
-		EstimatedPackDays:    &packDays,
+		OriginalPackDate:     &packDate,
+		OriginalDeliveryDate: &deliveryDate,
 	}
 
 	summary, err := calculateMoveDatesFromShipment(&shipment)
@@ -24,7 +24,6 @@ func (suite *HandlerSuite) TestCalculateMoveDatesFromShipment() {
 	expectedPickupDays := []time.Time{pickupDate}
 	suite.Equal(expectedPickupDays, summary.PickupDays)
 
-	deliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
 	expectedDeliveryDays := []time.Time{deliveryDate}
 	suite.Equal(expectedDeliveryDays, summary.DeliveryDays)
 
@@ -60,29 +59,208 @@ func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentMissingPickupDate()
 	suite.Error(err)
 }
 
-func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentMissingTransitDays() {
+func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentMissingOriginalPackDate() {
 	// create a shipment
 	pickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
-	packDays := int64(3)
-	var shipment = models.Shipment{
-		RequestedPickupDate: &pickupDate,
-		EstimatedPackDays:   &packDays,
-	}
-	_, err := calculateMoveDatesFromShipment(&shipment)
-
-	suite.Error(err)
-}
-
-func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentMissingPackDays() {
-	// create a shipment
-	pickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
-	transitDays := int64(5)
+	deliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
 	var shipment = models.Shipment{
 		RequestedPickupDate:  &pickupDate,
-		EstimatedTransitDays: &transitDays,
+		OriginalDeliveryDate: &deliveryDate,
+	}
+	_, err := calculateMoveDatesFromShipment(&shipment)
+
+	suite.Error(err)
+}
+
+func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentOriginalDeliveryDate() {
+	// create a shipment
+	pickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
+	packDate := time.Date(2018, 12, 6, 0, 0, 0, 0, time.UTC)
+	var shipment = models.Shipment{
+		RequestedPickupDate: &pickupDate,
+		OriginalPackDate:    &packDate,
 	}
 
 	_, err := calculateMoveDatesFromShipment(&shipment)
 
 	suite.Error(err)
 }
+
+func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentUsePMSurveyDates() {
+	// create a shipment
+	requestedPickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
+	pmSurveyPickupDate := time.Date(2018, 12, 12, 0, 0, 0, 0, time.UTC)
+	originalPackDate := time.Date(2018, 12, 6, 0, 0, 0, 0, time.UTC)
+	pmSurveyPackDate := time.Date(2018, 12, 7, 0, 0, 0, 0, time.UTC)
+	originalDeliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
+	pmSurveyDeliveryDate := time.Date(2018, 12, 18, 0, 0, 0, 0, time.UTC)
+	var shipment = models.Shipment{
+		RequestedPickupDate:         &requestedPickupDate,
+		OriginalPackDate:            &originalPackDate,
+		OriginalDeliveryDate:        &originalDeliveryDate,
+		PmSurveyPlannedPickupDate:   &pmSurveyPickupDate,
+		PmSurveyPlannedPackDate:     &pmSurveyPackDate,
+		PmSurveyPlannedDeliveryDate: &pmSurveyDeliveryDate,
+	}
+
+	summary, err := calculateMoveDatesFromShipment(&shipment)
+
+	// check that there is no error
+	suite.Nil(err)
+	// compare expected output with actual output
+	expectedPickupDays := []time.Time{pmSurveyPickupDate}
+	suite.Equal(expectedPickupDays, summary.PickupDays)
+
+	expectedDeliveryDays := []time.Time{pmSurveyDeliveryDate}
+	suite.Equal(expectedDeliveryDays, summary.DeliveryDays)
+
+	expectedPackDays := []time.Time{
+		time.Date(2018, 12, 7, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 10, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedPackDays, summary.PackDays)
+
+	expectedTransitDays := []time.Time{
+		time.Date(2018, 12, 13, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 14, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 15, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 16, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedTransitDays, summary.TransitDays)
+}
+
+func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentUseActualDates() {
+	// create a shipment
+	requestedPickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
+	actualPickupDate := time.Date(2018, 12, 12, 0, 0, 0, 0, time.UTC)
+	originalPackDate := time.Date(2018, 12, 6, 0, 0, 0, 0, time.UTC)
+	actualPackDate := time.Date(2018, 12, 7, 0, 0, 0, 0, time.UTC)
+	originalDeliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
+	actualDeliveryDate := time.Date(2018, 12, 18, 0, 0, 0, 0, time.UTC)
+	var shipment = models.Shipment{
+		RequestedPickupDate:  &requestedPickupDate,
+		OriginalPackDate:     &originalPackDate,
+		OriginalDeliveryDate: &originalDeliveryDate,
+		ActualPickupDate:     &actualPickupDate,
+		ActualPackDate:       &actualPackDate,
+		ActualDeliveryDate:   &actualDeliveryDate,
+	}
+
+	summary, err := calculateMoveDatesFromShipment(&shipment)
+
+	// check that there is no error
+	suite.Nil(err)
+	// compare expected output with actual output
+	expectedPickupDays := []time.Time{actualPickupDate}
+	suite.Equal(expectedPickupDays, summary.PickupDays)
+
+	expectedDeliveryDays := []time.Time{actualDeliveryDate}
+	suite.Equal(expectedDeliveryDays, summary.DeliveryDays)
+
+	expectedPackDays := []time.Time{
+		time.Date(2018, 12, 7, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 10, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedPackDays, summary.PackDays)
+
+	expectedTransitDays := []time.Time{
+		time.Date(2018, 12, 13, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 14, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 15, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 16, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedTransitDays, summary.TransitDays)
+}
+
+func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentUseMostCurrentDates() {
+	// create a shipment
+	requestedPickupDate := time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC)
+	pmSurveyPickupDate := time.Date(2018, 12, 13, 0, 0, 0, 0, time.UTC)
+	originalPackDate := time.Date(2018, 12, 6, 0, 0, 0, 0, time.UTC)
+	actualPackDate := time.Date(2018, 12, 10, 0, 0, 0, 0, time.UTC)
+	originalDeliveryDate := time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC)
+
+	var shipment = models.Shipment{
+		RequestedPickupDate:  &requestedPickupDate,
+		OriginalPackDate:     &originalPackDate,
+		OriginalDeliveryDate: &originalDeliveryDate,
+
+		ActualPackDate:            &actualPackDate,
+		PmSurveyPlannedPickupDate: &pmSurveyPickupDate,
+	}
+
+	summary, err := calculateMoveDatesFromShipment(&shipment)
+
+	// check that there is no error
+	suite.Nil(err)
+	// compare expected output with actual output
+	expectedPickupDays := []time.Time{pmSurveyPickupDate}
+	suite.Equal(expectedPickupDays, summary.PickupDays)
+
+	expectedDeliveryDays := []time.Time{originalDeliveryDate}
+	suite.Equal(expectedDeliveryDays, summary.DeliveryDays)
+
+	expectedPackDays := []time.Time{
+		time.Date(2018, 12, 10, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 11, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 12, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedPackDays, summary.PackDays)
+
+	expectedTransitDays := []time.Time{
+		time.Date(2018, 12, 14, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 15, 0, 0, 0, 0, time.UTC),
+		time.Date(2018, 12, 16, 0, 0, 0, 0, time.UTC),
+	}
+	suite.Equal(expectedTransitDays, summary.TransitDays)
+}
+
+//func (suite *HandlerSuite) TestCalculateMoveDatesFromShipmentMostCurrentDateBeforePreviousDate() {
+//	// create a shipment
+//	requestedPickupDate := time.Date(2018, 12, 4, 0, 0, 0, 0, time.UTC)
+//	actualPickupDate := time.Date(2018, 12, 12, 0, 0, 0, 0, time.UTC)
+//	originalPackDate := time.Date(2018, 11, 29, 0, 0, 0, 0, time.UTC)
+//	actualPackDate := time.Date(2018, 12, 13, 0, 0, 0, 0, time.UTC)
+//	originalDeliveryDate := time.Date(2018, 12, 10, 0, 0, 0, 0, time.UTC)
+//	pmSurveyDeliveryDate := time.Date(2018, 12, 12, 0, 0, 0, 0, time.UTC)
+//	var shipment = models.Shipment{
+//		RequestedPickupDate:  &requestedPickupDate,
+//		OriginalPackDate: &originalPackDate,
+//		ActualPickupDate:  &actualPickupDate,
+//		ActualPackDate: &actualPackDate,
+//		OriginalDeliveryDate:    &originalDeliveryDate,
+//
+//		PmSurveyPlannedDeliveryDate:    &pmSurveyDeliveryDate,
+//	}
+//
+//	summary, err := calculateMoveDatesFromShipment(&shipment)
+//
+//	// check that there is no error
+//	suite.Nil(err)
+//	// compare expected output with actual output
+//	expectedPickupDays := []time.Time{actualPickupDate}
+//	suite.Equal(expectedPickupDays, summary.PickupDays)
+//
+//	expectedDeliveryDays := []time.Time{pmSurveyDeliveryDate}
+//	suite.Equal(expectedDeliveryDays, summary.DeliveryDays)
+//
+//	expectedPackDays := []time.Time{
+//		time.Date(2018, 11, 29, 0, 0, 0, 0, time.UTC),
+//		time.Date(2018, 11, 30, 0, 0, 0, 0, time.UTC),
+//		time.Date(2018, 12, 3, 0, 0, 0, 0, time.UTC),
+//	}
+//	suite.Equal(expectedPackDays, summary.PackDays)
+//
+//	expectedTransitDays := []time.Time{
+//		time.Date(2018, 12, 13, 0, 0, 0, 0, time.UTC),
+//		//time.Date(2018, 12, 14, 0, 0, 0, 0, time.UTC),
+//		//time.Date(2018, 12, 15, 0, 0, 0, 0, time.UTC),
+//		//time.Date(2018, 12, 16, 0, 0, 0, 0, time.UTC),
+//		//time.Date(2018, 12, 17, 0, 0, 0, 0, time.UTC),
+//	}
+//	suite.Equal(expectedTransitDays, summary.TransitDays)
+//}
