7a179dd35e270136b18e17e6ed2fa1b61690778c
Chris Gilmer <chris@truss.works>
2018-10-30 15:10:57 -0700

    Add tests for the header of the shipment info page

---
+++ cypress/integration/tsp/checkHeader.js
@@ -0,0 +1,47 @@
+/* global cy */
+describe('TSP User Checks Shipment Info Header', function() {
+  beforeEach(() => {
+    cy.signIntoTSP();
+  });
+  it('tsp user sees header info', function() {
+    tspUserViewsHeaderInfo();
+  });
+});
+
+function tspUserViewsHeaderInfo() {
+  // Open new shipments queue
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/new/);
+  });
+
+  // Find a shipment and open it
+  cy
+    .get('div')
+    .contains('BACON1')
+    .dblclick();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/shipments\/[^/]+/);
+  });
+
+  // Check the move type and code
+  cy.contains('MOVE INFO - HHG CODE D');
+
+  // Check the name is correct
+  cy.get('h1').contains('Submitted, HHG');
+
+  // Check the info bar
+  cy
+    .get('ul')
+    .contains('li', 'GBL# LKBM7000002')
+    .parentsUntil('div')
+    .contains('li', 'Locator# BACON1')
+    .parentsUntil('div')
+    .contains('li', 'LKBM to LKBM')
+    .parentsUntil('div')
+    .contains('li', 'DoD ID# 4444567890')
+    .parentsUntil('div')
+    .contains('li', 'Status: Awarded')
+    .parentsUntil('div')
+    .contains('li', '555-555-5555');
+}
