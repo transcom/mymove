6b478fd947ef053b50ebb32d2df84539049a1dc9
andrea <andrea@truss.works>
2018-11-05 20:32:21 -0800

    fix tests

--- pkg/models/entitlements_test.go
+++ pkg/models/entitlements_test.go
@@ -8,11 +8,15 @@ func (suite *ModelSuite) TestGetEntitlementWithValidValues() {
 	E1 := models.ServiceMemberRankE1
 
 	// When: E1 has dependents and spouse gear
-	suite.Assertions.Equal(10500, models.GetEntitlement(E1, true, true))
+	E1FullLoad, _ := models.GetEntitlement(E1, true, true)
+	suite.Assertions.Equal(10500, E1FullLoad)
 	// When: E1 doesn't have dependents or spouse gear
-	suite.Assertions.Equal(7000, models.GetEntitlement(E1, false, false))
+	E1Solo, _ := models.GetEntitlement(E1, false, false)
+	suite.Assertions.Equal(7000, E1Solo)
 	// When: E1 doesn't have dependents but has spouse gear - impossible state
-	suite.Assertions.Equal(7000, models.GetEntitlement(E1, false, true))
+	E1FakeSpouse, _ := models.GetEntitlement(E1, false, true)
+	suite.Assertions.Equal(7000, E1FakeSpouse)
 	// When: E1 has dependents but no spouse gear
-	suite.Assertions.Equal(10000, models.GetEntitlement(E1, true, false))
+	E1DivorcedWithKids, _ := models.GetEntitlement(E1, true, false)
+	suite.Assertions.Equal(10000, E1DivorcedWithKids)
 }
