e67d3a24230c4c739d5d1cdaa3bf272f867cd480
Ron Cruz <Ronolibert@gmail.com>
2018-11-08 14:10:56 -0800

    Add e2e test for going to hhg-ppm route via anchor tag

--- cypress/integration/mymove/hhgPPM.js
+++ cypress/integration/mymove/hhgPPM.js
@@ -1,27 +1,40 @@
 /* global cy */
 
-const serviceMemberSignsIn = uuid => {
-  cy.signInAsUser(uuid);
-
-  cy.location().should(loc => {
-    expect(loc.pathname).to.match(/\//);
-  });
-};
-
-const serviceMemberAddsPPMToHHG = () => {
-  cy.get('.step a').contains('Add PPM Shipment');
-
-  // should('have.attr', 'href', /^\/moves\/[^/]+\/hhg-ppm-start/)
-};
-
-describe('completing the ppm flow', function() {
-  //tear down currently means doing this:
-  //update moves set status='DRAFT';
-  //delete from personally_procured_moves
-
-  it('progresses thru forms', function() {
+describe('service member adds a ppm to an hhg', function() {
+  it('service member clicks on Add PPM Shipment', function() {
     serviceMemberSignsIn('7980f0cf-63e3-4722-b5aa-ba46f8f7ac64');
-
     serviceMemberAddsPPMToHHG();
+    serviceMemeberCancelsAddPPMToHHG();
+    serviceMemberCannotAddPPMToHHG();
   });
 });
+
+function serviceMemberSignsIn(uuid) {
+  cy.signInAsUser(uuid);
+}
+
+function serviceMemberAddsPPMToHHG() {
+  cy
+    .get('.sidebar > div > a')
+    .contains('Add PPM Shipment')
+    .click();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/moves\/[^/]+\/hhg-ppm-start/);
+  });
+}
+
+function serviceMemeberCancelsAddPPMToHHG() {
+  cy
+    .get('.usa-button-secondary')
+    .contains('Cancel')
+    .click();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\//);
+  });
+}
+
+function serviceMemberCannotAddPPMToHHG() {
+  cy.get('.sidebar > div > a').should('not.exist');
+}
