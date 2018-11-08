40608cbcf9cab8c0979377d942cdf0a9ffe407e7
Chris Coles <chriscoles@truss.works>
2018-11-08 19:18:25 -0400

    Pulls in masters updates - fixes broken tests.

--- src/scenes/Landing/MoveSummary.test.jsx
+++ src/scenes/Landing/MoveSummary.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import {
-  MoveSummaryWithoutContext,
+  MoveSummary,
   CanceledMoveSummary,
   ApprovedMoveSummary,
   DraftMoveSummary,
@@ -27,7 +27,7 @@ describe('MoveSummary', () => {
     hhgObj,
     editMoveFn,
     resumeMoveFn,
-  ) => {  
+  ) => {
     const componentWrapper = shallow(
       <MoveSummary
         entitlement={entitlementObj}
