1a67916ea05002101a2dfa5ae7dae8c9123c07ac
Aileen Chen <aileen@dds.mil>
2018-11-13 12:19:08 -0500

    Use explicit UUIDs

--- local_migrations/20181024190235_add_dps_users.sql
+++ local_migrations/20181024190235_add_dps_users.sql
@@ -2,14 +2,12 @@
 -- This will be run on development environments. It should mirror what you
 -- intend to apply on production, but do not include any sensitive data.
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
-INSERT INTO public.dps_users VALUES (uuid_generate_v4(), 'test@example.com', now(), now());
+INSERT INTO public.dps_users VALUES ('f5bb0679-6d2e-4c90-ab6b-075e6e099c45', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('1eb593a0-fa80-40d4-bee2-837ebf9eeaa4', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('0bf29e37-45aa-4fa1-b350-8e71d146e7dd', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('601e2086-ec2c-48c8-a83b-f5ed9a6a6217', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('cc51aeea-5472-4759-ac37-e404828e308d', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('4d2a953b-d4fa-497c-916c-7947a9787257', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('fbc452c1-7f04-4ba6-b5b8-b8107308dd19', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('58af2146-44ee-4bc4-a48d-2d7b320e8fe8', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
+INSERT INTO public.dps_users VALUES ('0d92bf5c-ab58-4c58-a66e-f355a191767c', 'test@example.com', '2018-11-13 17:15:47.377193', '2018-11-13 17:15:47.377193');
