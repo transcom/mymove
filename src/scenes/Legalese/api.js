6d3d551b8048b813b0f1689c14f5409c414231a4
Ron Cruz <Ronolibert@gmail.com>
2018-11-15 08:08:35 -0800

    Remove unused get latest certification code

--- src/scenes/Legalese/api.js
+++ src/scenes/Legalese/api.js
@@ -2,12 +2,10 @@ import { getClient, checkResponse } from 'shared/Swagger/api';
 import * as legalese from './legaleseText';
 import { formatPayload } from 'shared/utils';
 
-// NOTE: GetCertificationText uses static text on client side for legalese. If a user has already certified a move before (ex. HHG_PPM setup), then GetCertifications is used instead which uses text of previous certification instead of static text
-// TODO: Use an API call to get the appropriate legalese text
 // This function will be an API call one day. For now loads a sample.
 export async function GetCertificationText(hasSIT, hasAdvance, moveType) {
   let txt;
-  if (moveType === 'PPM') {
+  if (moveType === 'PPM' || moveType === 'HHG_PPM') {
     txt = [legalese.ppmStandardLiability];
   } else if (moveType === 'HHG') {
     txt = [legalese.hhgStandardLiability];
@@ -15,18 +13,8 @@ export async function GetCertificationText(hasSIT, hasAdvance, moveType) {
 
   if (hasSIT) txt.push(legalese.storageLiability);
   if (hasAdvance) txt.push(legalese.ppmAdvance);
-  if (moveType === 'PPM') txt.push(legalese.additionalInformation);
+  if (moveType === 'PPM' || moveType === 'HHG_PPM') txt.push(legalese.additionalInformation);
   return txt.join('');
-}
-
-export async function GetCertifications(moveId, limit) {
-  const client = await getClient();
-  const response = await client.apis.certification.indexSignedCertifications({
-    moveId,
-    limit,
-  });
-  checkResponse(response, 'failed to find certs due to server error');
-  return response.body;
 }
 
 export async function CreateCertification(certificationRequest) {
