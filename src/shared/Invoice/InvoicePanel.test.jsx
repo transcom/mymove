2387f2f7a456b7895b476cdad98eabcf6a9e2ffc
Donald Thai <donald@truss.works>
2018-11-15 16:00:18 -0800

    Added test for jest/enzyme invoice panel

--- src/shared/Invoice/InvoicePanel.test.jsx
+++ src/shared/Invoice/InvoicePanel.test.jsx
@@ -2,17 +2,38 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { InvoicePanel } from './InvoicePanel';
+import { isOfficeSite } from 'shared/constants.js';
+import * as CONSTANTS from 'shared/constants.js';
 
 describe('InvoicePanel tests', () => {
-  let wrapper;
+  let wrapper,
+    shipmentState = null;
   const shipmentLineItems = [''];
   beforeEach(() => {
-    wrapper = mount(<InvoicePanel shipmentLineItems={shipmentLineItems} />);
+    CONSTANTS.isOfficeSite = true;
+    shipmentState = 'DELIVERED';
+    wrapper = mount(<InvoicePanel shipmentLineItems={shipmentLineItems} shipmentState={shipmentState} />);
   });
 
   describe('When no items exist', () => {
     it('renders without crashing', () => {
       expect(wrapper.find('.empty-content').length).toEqual(1);
+    });
+  });
+
+  describe('Approve Payment button shows on delivered state and office app', () => {
+    it('renders disabled "Approve Payment" button', () => {
+      expect(isOfficeSite).toBe(true);
+
+      wrapper.setState({ shipmentState: 'DELIVERED' });
+      expect(wrapper.state().shipmentState).toBe('DELIVERED');
+
+      wrapper.update();
+      expect(
+        wrapper
+          .children()
+          .containsMatchingElement(<button className="button button-secondary">Approve Payment</button>),
+      ).toBeTruthy();
     });
   });
 });
