62e329b7212e4168f074a52baaf7134387456068
Ron Cruz <Ronolibert@gmail.com>
2018-11-28 09:08:55 -0800

    Add migration for office users that need to be added

---
+++ local_migrations/20181128160411_add-office-users.sql
@@ -0,0 +1,8 @@
+-- Local test migration.
+-- This will be run on development environments. It should mirror what you
+-- intend to apply on production, but do not include any sensitive data.
+INSERT INTO public.office_users VALUES ('c16e65cc-0587-4bd8-a43d-db3b584c2383', NULL, 'Boy', 'Danny', NULL, 'daniel@example.com', '(555) 555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
+INSERT INTO public.office_users VALUES ('a3df3add-cfe2-4179-bf61-23b05a5affbf', NULL, 'Busters', 'Dave', NULL, 'davenbusters7@test.com', '(555) 555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
+INSERT INTO public.office_users VALUES ('326400d4-56fd-498c-8144-ca6e7b09ced5', NULL, 'Son', 'Danny', NULL, 'dannybegood@example.com', '(555) 555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
+INSERT INTO public.office_users VALUES ('89aaf3bb-c245-4f9e-b8e4-e4cd041be908', NULL, 'Berry', 'Sherri', NULL, 'sherriberry@example.com', '(555) 555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
+INSERT INTO public.office_users VALUES ('afaaf129-6b16-4fd3-aad9-8b831ec297e9', NULL, 'Bear', 'Rosia', NULL, 'rosia@mail.com', '(555) 555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
