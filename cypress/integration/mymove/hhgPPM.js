13576319c7a58bbfb271a1cf7899dd0c805dafd0
Ron Cruz <Ronolibert@gmail.com>
2018-12-04 22:18:22 +0000

    Add test case for verifying all information on edit page for HHG+PPM moves

--- cypress/integration/mymove/hhgPPM.js
+++ cypress/integration/mymove/hhgPPM.js
@@ -13,7 +13,73 @@ describe('service member adds a ppm to an hhg', function() {
     serviceMemberCanSignAgreement();
     serviceMemberViewsUpdatedHomePage();
   });
+  it('service member edits an HHG_PPM Shipment', function() {
+    serviceMemberSignsIn('f83bc69f-10aa-48b7-b9fe-425b393d49b8');
+    serviceMemberClicksEditMove();
+    serviceMemberVerifiesHHGPPMSummary();
+  });
 });
+
+function serviceMemberVerifiesHHGPPMSummary() {
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/moves\/[^/]+\/edit/);
+  });
+
+  cy.get('.review-content').should($div => {
+    const text = $div.text();
+    // Profile section
+    expect(text).to.include('Name: HHG Ready  For PPM');
+    expect(text).to.include('Branch:Army');
+    expect(text).to.include('Rank/Pay Grade: E-1');
+    expect(text).to.include('DoD ID#: 7777567890');
+    expect(text).to.include('Current Duty Station: Yuma AFB');
+
+    // Orders section
+    expect(text).to.include('Orders Type: Permanent Change Of Station');
+    expect(text).to.include('Orders Date:  05/20/2018');
+    expect(text).to.include('Report-by Date: 08/01/2018');
+    expect(text).to.include('New Duty Station:  Yuma AFB');
+    expect(text).to.include('Dependents?:  Yes');
+    expect(text).to.include('Spouse Pro Gear?: Yes');
+    expect(text).to.include('Orders Uploaded: 1');
+
+    // Contact Info
+    expect(text).to.include('Best Contact Phone: 555-555-5555');
+    expect(text).to.include('Alt. Phone:');
+    expect(text).to.include('Personal Email: hhgforppm@award.ed');
+    expect(text).to.include('Preferred Contact Method: Email');
+    expect(text).to.include('Current Mailing Address: 123 Any Street');
+    expect(text).to.include('P.O. Box 12345');
+    expect(text).to.include('Beverly Hills, CA 90210');
+    expect(text).to.include('Backup Mailing Address: 123 Any Street');
+    expect(text).to.include('P.O. Box 12345');
+    expect(text).to.include('Beverly Hills, CA 90210');
+
+    // Backup Contact info
+    expect(text).to.include('Backup Contact: name');
+    expect(text).to.include('Email:  email@example.com');
+    expect(text).to.include('Phone:  555-555-5555');
+  });
+
+  cy.get('body').should($div => expect($div.text()).not.to.include('Government moves all of your stuff (HHG)'));
+  cy.get('.ppm-container').should($div => {
+    const text = $div.text();
+    expect(text).to.include('Shipment - You move your stuff (PPM)');
+    expect(text).to.include('Move Date: 05/20/2018');
+    expect(text).to.include('Pickup ZIP Code:  90210');
+    expect(text).to.include('Delivery ZIP Code:  50309');
+    expect(text).not.to.include('Storage: Not requested');
+    expect(text).to.include('Estimated Weight:  1,50');
+    expect(text).to.include('Estimated PPM Incentive:  $4,255.80 - 4,703.78');
+  });
+}
+
+function serviceMemberClicksEditMove() {
+  cy
+    .get('.usa-button-secondary')
+    .contains('Edit Move')
+    .click();
+}
 
 function serviceMemberSignsIn(uuid) {
   cy.signInAsUser(uuid);
