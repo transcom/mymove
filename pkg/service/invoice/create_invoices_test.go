29bdf1f29d784221759deca384323fcb4b4418fa
Mikena Wood <mikena@truss.works>
2018-12-06 01:24:52 +0000

    shuffle around some testing

--- pkg/service/invoice/create_invoices_test.go
+++ pkg/service/invoice/create_invoices_test.go
@@ -1,18 +1,12 @@
 package invoice
 
 import (
-	"log"
-	"testing"
-
 	"github.com/facebookgo/clock"
-	"github.com/gobuffalo/pop"
-	"github.com/stretchr/testify/suite"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
-	"go.uber.org/zap"
 )
 
-func (suite *CreateInvoicesSuite) TestCreateInvoicesCall() {
+func (suite *InvoiceServiceSuite) TestCreateInvoicesCall() {
 	shipments := models.Shipments{testdatagen.MakeDefaultShipment(suite.db)}
 	createInvoices := CreateInvoices{
 		DB:    suite.db,
@@ -27,28 +21,4 @@ func (suite *CreateInvoicesSuite) TestCreateInvoicesCall() {
 	suite.Equal(1, len(invoices))
 	suite.Equal(models.InvoiceStatusINPROCESS, invoices[0].Status)
 	suite.NotEqual(models.Invoice{}.ID, invoices[0].ID)
-}
-
-type CreateInvoicesSuite struct {
-	suite.Suite
-	db     *pop.Connection
-	logger *zap.Logger
-}
-
-func (suite *CreateInvoicesSuite) SetupTest() {
-	suite.db.TruncateAll()
-}
-func TestInvoiceSuite(t *testing.T) {
-	configLocation := "../../../config"
-	pop.AddLookupPaths(configLocation)
-	db, err := pop.Connect("test")
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// Use a no-op logger during testing
-	logger := zap.NewNop()
-
-	hs := &CreateInvoicesSuite{db: db, logger: logger}
-	suite.Run(t, hs)
 }
--- pkg/service/invoice/update_invoices_submitted_test.go
+++ pkg/service/invoice/update_invoices_submitted_test.go
@@ -1,18 +1,12 @@
 package invoice
 
 import (
-	"log"
-	"testing"
-
 	"github.com/facebookgo/clock"
-	"github.com/gobuffalo/pop"
-	"github.com/stretchr/testify/suite"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
-	"go.uber.org/zap"
 )
 
-func (suite *UpdateInvoicesSuite) TestUpdateInvoicesCall() {
+func (suite *InvoiceServiceSuite) TestUpdateInvoicesCall() {
 	shipmentLineItem := testdatagen.MakeDefaultShipmentLineItem(suite.db)
 	suite.db.Eager("ShipmentLineItems.ID").Reload(&shipmentLineItem.Shipment)
 
@@ -36,28 +30,4 @@ func (suite *UpdateInvoicesSuite) TestUpdateInvoicesCall() {
 	suite.Equal(1, len(invoices))
 	suite.Equal(models.InvoiceStatusSUBMITTED, invoices[0].Status)
 	suite.Equal(invoices[0].ID, *shipmentLineItems[0].InvoiceID)
-}
-
-type UpdateInvoicesSuite struct {
-	suite.Suite
-	db     *pop.Connection
-	logger *zap.Logger
-}
-
-func (suite *UpdateInvoicesSuite) SetupTest() {
-	suite.db.TruncateAll()
-}
-func TestUpdateInvoiceSuite(t *testing.T) {
-	configLocation := "../../../config"
-	pop.AddLookupPaths(configLocation)
-	db, err := pop.Connect("test")
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// Use a no-op logger during testing
-	logger := zap.NewNop()
-
-	hs := &UpdateInvoicesSuite{db: db, logger: logger}
-	suite.Run(t, hs)
 }
