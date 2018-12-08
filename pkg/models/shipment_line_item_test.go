2163273ea285f4e55f7240c998e051c92901e970
Patrick <pstanger@gmail.com>
2018-12-08 00:28:25 +0000

    Test for line item fetcher

--- pkg/models/shipment_line_item_test.go
+++ pkg/models/shipment_line_item_test.go
@@ -1,6 +1,8 @@
 package models_test
 
 import (
+	"github.com/gofrs/uuid"
+
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
 )
@@ -19,4 +21,31 @@ func (suite *ModelSuite) TestFetchLineItem() {
 	suite.NoError(err)
 	suite.Equal(1, len(accs))
 	suite.Equal(lineItem.ID, accs[0].ID)
+}
+
+func (suite *ModelSuite) TestFetchApprovedPreapprovalRequestsByShipment() {
+	shipment, err := testdatagen.MakeShipmentForPricing(suite.db, testdatagen.Assertions{})
+	suite.FatalNoError(err)
+
+	lineItem := testdatagen.MakeCompleteShipmentLineItem(suite.db, testdatagen.Assertions{
+		ShipmentLineItem: models.ShipmentLineItem{
+			Shipment:   shipment,
+			ShipmentID: shipment.ID,
+			Status:     models.ShipmentLineItemStatusAPPROVED,
+		},
+		Tariff400ngItem: models.Tariff400ngItem{
+			RequiresPreApproval: true,
+		},
+	})
+
+	returnedItems, err := models.FetchApprovedPreapprovalRequestsByShipment(suite.db, shipment)
+
+	if suite.NoError(err) && suite.Len(returnedItems, 1) {
+		// We should get back the line item
+		suite.Equal(lineItem.ID, returnedItems[0].ID)
+		// And the Tariff400ngItem association should be populated
+		suite.NotEqual(uuid.Nil, returnedItems[0].Tariff400ngItemID)
+		suite.Equal(lineItem.Tariff400ngItemID, returnedItems[0].Tariff400ngItemID)
+		suite.Equal(returnedItems[0].Tariff400ngItemID, returnedItems[0].Tariff400ngItem.ID)
+	}
 }
