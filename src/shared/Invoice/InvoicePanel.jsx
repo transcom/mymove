6f13c18f188b61023cfc880140501129af0e1ff5
Rupinder Dhariwal <rd@truss.works>
2018-12-11 21:36:54 +0000

    refactor invoice flow logic into a redux store

--- src/shared/Invoice/InvoicePanel.jsx
+++ src/shared/Invoice/InvoicePanel.jsx
@@ -1,16 +1,27 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
+
 import './InvoicePanel.css';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { isOfficeSite, isDevelopment } from 'shared/constants.js';
+
 import BasicPanel from 'shared/BasicPanel';
 import {
   selectUnbilledShipmentLineItems,
   selectTotalFromUnbilledLineItems,
 } from 'shared/Entities/modules/shipmentLineItems';
-import { sendHHGInvoice } from 'scenes/Office/ducks';
+import { sendHHGInvoice, draftInvoice, resetInvoiceFlow } from 'scenes/Office/ducks';
 import InvoiceTable from 'shared/Invoice/InvoiceTable';
+import {
+  default as InvoicePayment,
+  PAYMENT_IN_PROCESSING,
+  PAYMENT_IN_CONFIRMATION,
+  PAYMENT_FAILED,
+  PAYMENT_APPROVED,
+} from './InvoicePayment';
 
 export class InvoicePanel extends PureComponent {
   approvePayment = () => {
@@ -22,12 +33,52 @@ export class InvoicePanel extends PureComponent {
     let title = 'Invoicing';
 
     if (this.props.unbilledShipmentLineItems.length > 0) {
+      let tableTitle = <h5>Unbilled line items</h5>;
+      let paymentStatus = null;
+
+      //figure out payment status
+      if (this.props.isInvoiceInDraft) {
+        paymentStatus = PAYMENT_IN_CONFIRMATION;
+      } else if (this.props.invoiceProcessing) {
+        paymentStatus = PAYMENT_IN_PROCESSING;
+      } else if (this.props.invoiceSentSuccessfully) {
+        paymentStatus = PAYMENT_APPROVED;
+      } else if (this.props.invoiceHasFailed) {
+        paymentStatus = PAYMENT_FAILED;
+      }
+
+      if (
+        isOfficeSite && //user is an office user
+        isDevelopment && //only for development env
+        (!paymentStatus || paymentStatus === PAYMENT_FAILED)
+      ) {
+        //payment status is empty or invoice payment has failed
+        tableTitle = (
+          <div className="invoice-panel-header-cont">
+            <div className="usa-width-one-half">
+              <h5>Unbilled line items</h5>
+            </div>
+            <div className="usa-width-one-half align-right">
+              <button className="button button-secondary" onClick={this.props.draftInvoice}>
+                Approve Payment
+              </button>
+            </div>
+          </div>
+        );
+      }
+
       invoicingContent = (
         <div>
+          <InvoicePayment
+            cancelPayment={this.props.resetInvoiceFlow}
+            approvePayment={this.approvePayment}
+            isDelivered={this.props.isShipmentDelivered}
+            paymentStatus={paymentStatus}
+          />
           <InvoiceTable
             shipmentLineItems={this.props.unbilledShipmentLineItems}
             totalAmount={this.props.lineItemsTotal}
-            title={<h5>Unbilled Line Items</h5>}
+            title={tableTitle}
           />
         </div>
       );
@@ -49,6 +100,7 @@ InvoicePanel.propTypes = {
   lineItemsTotal: PropTypes.number,
   onApprovePayment: PropTypes.func,
   canApprove: PropTypes.bool,
+  isShipmentDelivered: PropTypes.bool,
 };
 
 const mapStateToProps = (state, ownProps) => {
@@ -56,10 +108,15 @@ const mapStateToProps = (state, ownProps) => {
   return {
     unbilledShipmentLineItems: isShipmentDelivered ? selectUnbilledShipmentLineItems(state, ownProps.shipmentId) : [],
     lineItemsTotal: isShipmentDelivered ? selectTotalFromUnbilledLineItems(state, ownProps.shipmentId) : 0,
+    isShipmentDelivered: ownProps.shipmentStatus.toUpperCase() === 'DELIVERED',
+    invoiceProcessing: get(state, 'office.hhgInvoiceIsSending'),
+    invoiceSentSuccessfully: get(state, 'office.hhgInvoiceHasSendSuccess'),
+    invoiceHasFailed: get(state, 'office.hhgInvoiceHasFailure'),
+    isInvoiceInDraft: get(state, 'office.hhgInvoiceInDraft'),
   };
 };
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ sendHHGInvoice }, dispatch);
+  return bindActionCreators({ sendHHGInvoice, draftInvoice, resetInvoiceFlow }, dispatch);
 }
 export default connect(mapStateToProps, mapDispatchToProps)(InvoicePanel);
--- src/shared/Invoice/InvoicePayment.jsx
+++ src/shared/Invoice/InvoicePayment.jsx
@@ -6,121 +6,39 @@ import Alert from 'shared/Alert';
 import { isOfficeSite, isDevelopment } from 'shared/constants.js';
 import './InvoicePanel.css';
 
-const PAYMENT_PENDING = 'IN_PENDING_FLOW';
-const PAYMENT_IN_CONFIRMATION = 'IN_CONFIRMATION_FLOW';
-const PAYMENT_IN_PROCESSING = 'IN_PROCESSING_FLOW';
-const PAYMENT_APPROVED = 'IN_APPROVED_FLOW';
-const PAYMENT_FAILED = 'IN_FAILURE_FLOW';
+export const PAYMENT_IN_CONFIRMATION = 'IN_CONFIRMATION_FLOW';
+export const PAYMENT_IN_PROCESSING = 'IN_PROCESSING_FLOW';
+export const PAYMENT_APPROVED = 'IN_APPROVED_FLOW';
+export const PAYMENT_FAILED = 'IN_FAILURE_FLOW';
 
 class InvoicePayment extends PureComponent {
-  state = {
-    paymentStatus: this.props.paymentStatus,
-  };
-
-  static defaultProps = {
-    paymentStatus: PAYMENT_PENDING,
-  };
-
-  approvePayment = () => {
-    this.setState({ paymentStatus: PAYMENT_IN_CONFIRMATION });
-  };
-
-  cancelPayment = () => {
-    this.setState({ paymentStatus: PAYMENT_PENDING });
-  };
-
-  submitPayment = () => {
-    this.setState({ paymentStatus: PAYMENT_IN_PROCESSING });
-
-    //dispatch action to submit invoice to GEX
-    this.props.approvePayment().then(status => {
-      //this is a temp workaround until invoice table gets refactored
-      //and invoice status starts coming from redux store instead of being in-state
-      if (status.type === 'SEND_HHG_INVOICE_SUCCESS') {
-        this.invoiceSuccess();
-      } else {
-        this.invoiceFail();
-      }
-    });
-  };
-
-  invoiceSuccess = () => {
-    this.setState({ paymentStatus: PAYMENT_APPROVED });
-  };
-
-  invoiceFail = () => {
-    this.setState({ paymentStatus: PAYMENT_FAILED });
-  };
-
   render() {
     let paymentContainer = null;
 
-    //calculate what payment status view to display
-    switch (this.state.paymentStatus) {
-      default:
-        paymentContainer = null;
-        break;
-      case PAYMENT_PENDING:
-        if (isOfficeSite && this.props.isDelivered) {
+    if (isOfficeSite && this.props.isDelivered) {
+      //calculate what payment status view to display
+      switch (this.props.paymentStatus) {
+        case PAYMENT_IN_CONFIRMATION:
           paymentContainer = (
-            <div className="invoice-panel-header-cont">
-              <div className="usa-width-one-half">
-                <h5>Unbilled line items</h5>
-              </div>
-              <div className="usa-width-one-half align-right">
-                <button
-                  className="button button-secondary"
-                  disabled={!this.props.canApprove || !isDevelopment}
-                  onClick={this.approvePayment}
-                >
-                  Approve Payment
-                </button>
-              </div>
-            </div>
+            <Alert type="warning" heading="Approve payment?">
+              <span className="warning--header">Please make sure you've double-checked everything.</span>
+              <button className="button usa-button-secondary" onClick={this.props.cancelPayment}>
+                Cancel
+              </button>
+              <button className="button usa-button-primary" onClick={this.props.approvePayment}>
+                Approve
+              </button>
+            </Alert>
           );
-        }
-        break;
-      case PAYMENT_IN_CONFIRMATION:
-        if (isOfficeSite && this.props.isDelivered) {
+          break;
+        case PAYMENT_IN_PROCESSING:
           paymentContainer = (
-            <div>
-              <Alert type="warning" heading="Approve payment?">
-                <span className="warning--header">Please make sure you've double-checked everything.</span>
-                <button className="button usa-button-secondary" onClick={this.cancelPayment}>
-                  Cancel
-                </button>
-                <button className="button usa-button-primary" onClick={this.submitPayment}>
-                  {' '}
-                  Approve
-                </button>
-              </Alert>
-              <div className="invoice-panel-header-cont">
-                <div className="usa-width-one-half">
-                  <h5>Unbilled line items</h5>
-                </div>
-              </div>
-            </div>
+            <Alert type="loading" heading="Creating invoice">
+              <span className="warning--header">Sending information to USBank/Syncada.</span>
+            </Alert>
           );
-        }
-        break;
-      case PAYMENT_IN_PROCESSING:
-        if (isOfficeSite && this.props.isDelivered) {
-          paymentContainer = (
-            <div>
-              <Alert type="loading" heading="Creating invoice">
-                <span className="warning--header">Sending information to USBank/Syncada.</span>
-              </Alert>
-              <div className="invoice-panel-header-cont">
-                <div className="usa-width-one-half">
-                  <h5>Unbilled line items</h5>
-                </div>
-              </div>
-            </div>
-          );
-        }
-        break;
-      case PAYMENT_APPROVED:
-        if (isOfficeSite && this.props.isDelivered) {
+          break;
+        case PAYMENT_APPROVED:
           paymentContainer = (
             <div>
               <Alert type="success" heading="Success!">
@@ -128,42 +46,26 @@ class InvoicePayment extends PureComponent {
               </Alert>
             </div>
           );
-        }
-        break;
-      case PAYMENT_FAILED:
-        if (isOfficeSite && this.props.isDelivered) {
+          break;
+        case PAYMENT_FAILED:
           paymentContainer = (
-            <div>
-              <Alert type="error" heading="Oops, something went wrong!">
-                <span className="warning--header">Please try again.</span>
-              </Alert>
-              <div className="invoice-panel-header-cont">
-                <div className="usa-width-one-half">
-                  <h5>Unbilled line items</h5>
-                </div>
-                <div className="usa-width-one-half align-right">
-                  <button
-                    className="button button-secondary"
-                    disabled={!this.props.canApprove || !isDevelopment}
-                    onClick={this.approvePayment}
-                  >
-                    Approve Payment
-                  </button>
-                </div>
-              </div>
-            </div>
+            <Alert type="error" heading="Oops, something went wrong!">
+              <span className="warning--header">Please try again.</span>
+            </Alert>
           );
-        }
-        break;
+          break;
+      }
     }
-    return { paymentContainer };
+    return paymentContainer;
   }
 }
 
 InvoicePayment.propTypes = {
   approvePayment: PropTypes.func,
+  cancelPayment: PropTypes.func,
   canApprove: PropTypes.bool,
   isDelivered: PropTypes.bool,
+  paymentStatus: PropTypes.string,
 };
 
 export default InvoicePayment;
