611c7bed59c30c2a5c81dbde4a94ce17c0139bc7
Mikena Wood <mikena@truss.works>
2018-12-12 02:56:28 +0000

    update command line util to only get approved line items

--- cmd/generate_shipment_edi/main.go
+++ cmd/generate_shipment_edi/main.go
@@ -9,19 +9,19 @@ import (
 	"github.com/gobuffalo/pop"
 	"github.com/gofrs/uuid"
 	"github.com/namsral/flag"
+	"github.com/transcom/mymove/pkg/service/invoice"
 	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/edi"
 	"github.com/transcom/mymove/pkg/edi/gex"
 	"github.com/transcom/mymove/pkg/edi/invoice"
-	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/rateengine"
 	"github.com/transcom/mymove/pkg/route"
 )
 
 // Call this from command line with go run cmd/generate_shipment_edi/main.go -moveID <UUID>
 func main() {
-	moveIDString := flag.String("moveID", "", "The ID of the move where shipments are found")
+	shipmentIDString := flag.String("shipmentID", "", "The ID of the shipment to invoice")
 	env := flag.String("env", "development", "The environment to run in, which configures the database.")
 	sendToGex := flag.Bool("gex", false, "Choose to send the file to gex")
 	transactionName := flag.String("transactionName", "test", "The required name sent in the url of the gex api request")
@@ -31,8 +31,8 @@ func main() {
 	hereAppCode := flag.String("here_maps_app_code", "", "HERE maps App API code")
 	flag.Parse()
 
-	if *moveIDString == "" {
-		log.Fatal("Usage: go run cmd/generate_shipment_edi/main.go --moveID <29cb984e-c70d-46f0-926d-cd89e07a6ec3> --gex false")
+	if *shipmentIDString == "" {
+		log.Fatal("Usage: go run cmd/generate_shipment_edi/main.go --shipmentID <29cb984e-c70d-46f0-926d-cd89e07a6ec3> --gex false")
 	}
 
 	db, err := pop.Connect(*env)
@@ -40,26 +40,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	moveID := uuid.Must(uuid.FromString(*moveIDString))
-	var shipments models.Shipments
-
-	err = db.Eager(
-		"PickupAddress",
-		"Move.Orders.NewDutyStation.Address",
-		"Move.Orders.NewDutyStation.TransportationOffice",
-		"ServiceMember.DutyStation.TransportationOffice",
-		"ShipmentOffers.TransportationServiceProvider",
-		"ShipmentOffers.TransportationServiceProviderPerformance",
-		"ShipmentLineItems.Tariff400ngItem",
-	).Where("shipment_offers.accepted=true").
-		Where("move_id = $1", &moveID).
-		Join("shipment_offers", "shipment_offers.shipment_id = shipments.id").
-		All(&shipments)
+	shipmentID := uuid.Must(uuid.FromString(*shipmentIDString))
+	shipment, err := invoice.FetchInvoiceShipment{db}.Call(shipmentID)
 	if err != nil {
 		log.Fatal(err)
-	}
-	if len(shipments) == 0 {
-		log.Fatal("No shipments with accepted shipment offers found")
 	}
 
 	logger, err := zap.NewDevelopment()
@@ -67,16 +51,13 @@ func main() {
 		log.Fatalf("Failed to initialize Zap logging due to %v", err)
 	}
 	planner := route.NewHEREPlanner(logger, *hereGeoEndpoint, *hereRouteEndpoint, *hereAppID, *hereAppCode)
-	var costsByShipments []rateengine.CostByShipment
 
 	engine := rateengine.NewRateEngine(db, logger, planner)
-	for _, shipment := range shipments {
-		costByShipment, err := engine.HandleRunOnShipment(shipment)
-		if err != nil {
-			log.Fatal(err)
-		}
-		costsByShipments = append(costsByShipments, costByShipment)
+	costByShipment, err := engine.HandleRunOnShipment(shipment)
+	if err != nil {
+		log.Fatal(err)
 	}
+	costsByShipments := []rateengine.CostByShipment{costByShipment}
 	invoice858C, err := ediinvoice.Generate858C(costsByShipments, db, false, clock.New())
 	if err != nil {
 		log.Fatal(err)
@@ -94,5 +75,4 @@ func main() {
 		ediWriter := edi.NewWriter(os.Stdout)
 		ediWriter.WriteAll(invoice858C.Segments())
 	}
-
 }
