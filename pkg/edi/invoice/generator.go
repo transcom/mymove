d5c0284108f5cb3e9c46ffc7728ac3d80a9504e2
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2018-12-14 19:18:42 +0000

    Add clause for the neither location value. Add unit test for this and test generation. Lastly, update golden with new line item.

--- pkg/edi/invoice/generator.go
+++ pkg/edi/invoice/generator.go
@@ -365,6 +365,8 @@ func MakeHLSegment(lineItem models.ShipmentLineItem) *edisegment.HL {
 	case models.ShipmentLineItemLocationDESTINATION:
 		hierarchicalLevelID = "303"
 
+	case models.ShipmentLineItemLocationNEITHER:
+		hierarchicalLevelID = "303"
 	}
 	return &edisegment.HL{
 		HierarchicalIDNumber:  hierarchicalLevelID,
