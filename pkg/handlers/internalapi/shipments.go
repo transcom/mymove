a074d2a455308db6cdfd1358afc704fbbab004ef
Rupinder Dhariwal <rd@truss.works>
2018-12-19 16:09:14 +0000

    refactor the invoice creation endpoint to return invoice model when another invoice is already in process or has been processed successfully

--- pkg/handlers/internalapi/shipments.go
+++ pkg/handlers/internalapi/shipments.go
@@ -1,7 +1,6 @@
 package internalapi
 
 import (
-	"errors"
 	"os"
 	"time"
 
@@ -513,9 +512,9 @@ func (h ShipmentInvoiceHandler) Handle(params shipmentop.CreateAndSendHHGInvoice
 		if element.Status == models.InvoiceStatusDRAFT ||
 			element.Status == models.InvoiceStatusINPROCESS ||
 			element.Status == models.InvoiceStatusSUBMITTED {
-			return handlers.ResponseForCustomErrors(h.Logger(), errors.New("invoice is processing or already processed for this shipment"), 409)
+			payload := payloadForInvoiceModel(&element)
+			return shipmentop.NewCreateAndSendHHGInvoiceConflict().WithPayload(payload)
 		}
-
 	}
 
 	approver, err := models.FetchOfficeUserByID(h.DB(), session.OfficeUserID)
