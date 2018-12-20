614ad9d6a6631e72621a04270f801150997acd9a
Donald Thai <donald@truss.works>
2018-12-20 13:54:15 -0800

    Local migration not needed

--- local_migrations/20181220182247_add-office-users-#162792520.sql
+++ local_migrations/20181220182247_add-office-users-#162792520.sql
@@ -1,9 +1,3 @@
 -- Local test migration.
 -- This will be run on development environments. It should mirror what you
 -- intend to apply on production, but do not include any sensitive data.
-
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'McTest', concat('Test-', uuid_generate_v4()), NULL, concat('Test-', uuid_generate_v4(), '@example.com'), '', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'McTest', concat('Test-', uuid_generate_v4()), NULL, concat('Test-', uuid_generate_v4(), '@example.com'), '', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'McTest', concat('Test-', uuid_generate_v4()), NULL, concat('Test-', uuid_generate_v4(), '@example.com'), '', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'McTest', concat('Test-', uuid_generate_v4()), NULL, concat('Test-', uuid_generate_v4(), '@example.com'), '', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'McTest', concat('Test-', uuid_generate_v4()), NULL, concat('Test-', uuid_generate_v4(), '@example.com'), '', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
