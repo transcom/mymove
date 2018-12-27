07d41cda40219450e444a75d0304af4d71634f0c
Rupinder Dhariwal <rd@truss.works>
2018-12-27 19:22:17 +0000

    handle use case where a duplicate invoice request is recieved. The UI will indicate invoice is already under process and show the user who approved the invoice

--- src/shared/Invoice/InvoicePaymentAlert.jsx
+++ src/shared/Invoice/InvoicePaymentAlert.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { get } from 'lodash';
 
 import Alert from 'shared/Alert';
 import './InvoicePanel.css';
@@ -10,11 +11,39 @@ class InvoicePaymentAlert extends PureComponent {
     const status = this.props.createInvoiceStatus;
 
     if (status.error) {
-      paymentAlert = (
-        <Alert type="error" heading="Oops, something went wrong!">
-          <span className="warning--header">Please try again.</span>
-        </Alert>
-      );
+      //handle 409 status: shipment invoice already processed
+      let httpResCode = get(status, 'error.response.status');
+      let invoiceStatus = get(status, 'error.response.response.body.status');
+      let aproverFirstName = get(status, 'error.response.response.body.approver_first_name');
+      let aproverLastName = get(status, 'error.response.response.body.approver_last_name');
+      if (httpResCode === 409 && invoiceStatus === 'SUBMITTED') {
+        paymentAlert = (
+          <div>
+            <Alert type="success" heading="Success!">
+              <span className="warning--header">
+                Counselor {aproverFirstName} {aproverLastName} already approved this invoice.
+              </span>
+            </Alert>
+          </div>
+        );
+      } else if (httpResCode === 409 && (invoiceStatus === 'IN_PROCESS' || invoiceStatus === 'DRAFT')) {
+        paymentAlert = (
+          <div>
+            <Alert type="success" heading="Success!">
+              <span className="warning--header">
+                Counselor {aproverFirstName} {aproverLastName} already submitted this invoice. Please reload your screen
+                to see updated information.
+              </span>
+            </Alert>
+          </div>
+        );
+      } else {
+        paymentAlert = (
+          <Alert type="error" heading="Oops, something went wrong!">
+            <span className="warning--header">Please try again.</span>
+          </Alert>
+        );
+      }
     } else if (status.isLoading) {
       paymentAlert = (
         <Alert type="loading" heading="Creating invoice">
