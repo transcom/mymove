7d5b812fbe1d842764746af195a0cca0a6e3e4d4
Rupinder Dhariwal <rd@truss.works>
2018-12-27 19:57:40 +0000

    add test coverage for different error conditions around invoice payment flow

--- src/shared/Invoice/InvoicePaymentAlert.test.js
+++ src/shared/Invoice/InvoicePaymentAlert.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import InvoicePaymentAlert from './InvoicePaymentAlert';
+import { get } from 'lodash';
 
 describe('Invoice Payment Component tests', () => {
   let wrapper;
@@ -31,6 +32,87 @@ describe('Invoice Payment Component tests', () => {
         />,
       );
       expect(wrapper.find('.warning--header').text()).toEqual('Please try again.');
+    });
+    describe('and the api response is 409 and invoice status is SUBMITTED', () => {
+      it('renders invoice already processed by another user', () => {
+        wrapper = shallow(
+          <InvoicePaymentAlert
+            createInvoiceStatus={{
+              error: {
+                response: {
+                  status: 409,
+                  response: {
+                    body: {
+                      status: 'SUBMITTED',
+                      approver_first_name: 'Leo',
+                      approver_last_name: 'Spaceman',
+                    },
+                  },
+                },
+              },
+              isLoading: false,
+              isSuccess: false,
+            }}
+          />,
+        );
+        expect(wrapper.find('.warning--header').text()).toEqual(
+          'Counselor Leo Spaceman already approved this invoice.',
+        );
+      });
+    });
+    describe('and the api response is 409 and invoice status is IN_PROCESS', () => {
+      it('renders invoice already processed by another user', () => {
+        wrapper = shallow(
+          <InvoicePaymentAlert
+            createInvoiceStatus={{
+              error: {
+                response: {
+                  status: 409,
+                  response: {
+                    body: {
+                      status: 'IN_PROCESS',
+                      approver_first_name: 'Leo',
+                      approver_last_name: 'Spaceman',
+                    },
+                  },
+                },
+              },
+              isLoading: false,
+              isSuccess: false,
+            }}
+          />,
+        );
+        expect(wrapper.find('.warning--header').text()).toEqual(
+          'Counselor Leo Spaceman already submitted this invoice. Please reload your screen to see updated information.',
+        );
+      });
+    });
+    describe('and the api response is 409 and invoice status is DRAFT', () => {
+      it('renders invoice already processed by another user', () => {
+        wrapper = shallow(
+          <InvoicePaymentAlert
+            createInvoiceStatus={{
+              error: {
+                response: {
+                  status: 409,
+                  response: {
+                    body: {
+                      status: 'DRAFT',
+                      approver_first_name: 'Leo',
+                      approver_last_name: 'Spaceman',
+                    },
+                  },
+                },
+              },
+              isLoading: false,
+              isSuccess: false,
+            }}
+          />,
+        );
+        expect(wrapper.find('.warning--header').text()).toEqual(
+          'Counselor Leo Spaceman already submitted this invoice. Please reload your screen to see updated information.',
+        );
+      });
     });
   });
   describe('When invoice status is approved', () => {
