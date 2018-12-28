bcabb38716cfb83c6361567d45948089b722ad30
Reggie Riser <reggie@truss.works>
2018-12-28 17:35:34 +0000

    Reworked the invoice number migration script and made a field non-nullable

--- migrations/20181221151952_create_invoice_number_trackers.up.sql
+++ migrations/20181221151952_create_invoice_number_trackers.up.sql
@@ -1,3 +1,4 @@
+-- Create new invoice_number_trackers table to support invoice number generation.
 CREATE TABLE invoice_number_trackers (
     standard_carrier_alpha_code text NOT NULL,
     year integer NOT NULL,
@@ -5,82 +6,88 @@ CREATE TABLE invoice_number_trackers (
     PRIMARY KEY(standard_carrier_alpha_code, year)
 );
 
--- Fix any invoices already in the system that may have placeholder invoice numbers.
+-- Fix any invoices already in the system since they have placeholder invoice numbers.
 DO
   $do$
     DECLARE
-      invoice                          RECORD;
-      scac                             TEXT;
-      processed_shipment_invoice_count INT;
-      shipment_year                    INT;
-      shipment_two_digit_year          VARCHAR(2);
-      base_invoice_number              VARCHAR(255);
-      new_sequence_number              INT;
+      current_shipment_id     UUID;
+      current_invoice_id      UUID;
+      scac                    TEXT;
+      invoice_count           INT;
+      shipment_year           INT;
+      shipment_two_digit_year VARCHAR(2);
+      base_invoice_number     VARCHAR(255);
+      new_sequence_number     INT;
     BEGIN
 
-      -- Go through all the invoices
-      FOR invoice IN SELECT * FROM invoices ORDER BY created_at
+      -- Get all distinct shipment IDs currently associated with invoices, ordered by earliest invoice creation.
+      FOR current_shipment_id IN SELECT DISTINCT shipment_id, MIN(created_at) as min_created_at
+                                 FROM invoices
+                                 GROUP BY shipment_id
+                                 ORDER BY MIN(created_at)
         LOOP
-          -- Get the SCAC for the invoice
+          scac := NULL;
+
+          -- Get the SCAC for the current_shipment_id
           SELECT tsp.standard_carrier_alpha_code INTO scac
-          FROM invoices i
-                 INNER JOIN shipments s on i.shipment_id = s.id
+          FROM shipments s
                  INNER JOIN shipment_offers so on s.id = so.shipment_id
                  INNER JOIN transportation_service_provider_performances tspp
                             on so.transportation_service_provider_performance_id = tspp.id
                  INNER JOIN transportation_service_providers tsp on tspp.transportation_service_provider_id = tsp.id
-          WHERE i.shipment_id = invoice.shipment_id
-            AND so.accepted = TRUE;
+          WHERE s.id = current_shipment_id
+            AND so.accepted = TRUE
+          ORDER BY so.created_at
+          LIMIT 1;
 
-          -- No need to update this row if already using SCAC.
-          CONTINUE WHEN invoice.invoice_number LIKE scac || '%';
+          -- Get shipment's created at date.
+          SELECT EXTRACT(YEAR FROM created_at), to_char(created_at, 'YY') INTO shipment_year, shipment_two_digit_year
+          FROM shipments s
+          WHERE s.ID = current_shipment_id;
 
-          -- Do we already have correct invoice numbers for this shipment?
-          -- They may have already been processed in this loop.
-          SELECT count(*) INTO processed_shipment_invoice_count
-          FROM invoices i
-                 INNER JOIN shipments s on i.shipment_id = s.id
-          WHERE i.shipment_id = invoice.shipment_id
-            AND invoice_number LIKE scac || '%';
+          -- Get all invoice records for that shipment, ordered by creation date.
+          invoice_count := 0;
+          base_invoice_number := NULL;
+          FOR current_invoice_id IN SELECT id FROM invoices WHERE shipment_id = current_shipment_id ORDER BY created_at
+            LOOP
+              IF invoice_count = 0 THEN
+                -- Set the first invoice number to the baseline invoice number.
 
-          IF processed_shipment_invoice_count = 0 THEN
-            -- Get shipment's created at date.
-            SELECT EXTRACT(YEAR FROM created_at), to_char(created_at, 'YY') INTO shipment_year, shipment_two_digit_year
-            FROM shipments s
-            WHERE s.ID = invoice.shipment_id;
+                -- Insert/update sequence number.
+                INSERT INTO invoice_number_trackers as trackers (standard_carrier_alpha_code, year, sequence_number)
+                VALUES (scac, shipment_year, 1)
+                ON CONFLICT (
+                   standard_carrier_alpha_code,
+                   year)
+                   DO
+                     UPDATE
+                     SET sequence_number = trackers.sequence_number + 1
+                     WHERE trackers.standard_carrier_alpha_code = scac AND trackers.year = shipment_year
+                   RETURNING sequence_number INTO new_sequence_number;
 
-            -- Insert/update sequence number.
-            INSERT INTO invoice_number_trackers as trackers (standard_carrier_alpha_code, year, sequence_number)
-            VALUES (scac, shipment_year, 1)
-            ON CONFLICT (standard_carrier_alpha_code, year)
-               DO
-                 UPDATE
-                 SET sequence_number = trackers.sequence_number + 1
-                 WHERE trackers.standard_carrier_alpha_code = scac AND trackers.year = shipment_year
-               RETURNING sequence_number INTO new_sequence_number;
+                base_invoice_number := scac || shipment_two_digit_year || to_char(new_sequence_number, 'fm0000');
 
-            -- Update baseline invoice number.
-            UPDATE invoices
-            SET invoice_number = scac || shipment_two_digit_year || to_char(new_sequence_number, 'fm0000')
-            WHERE id = invoice.id;
-          ELSE
-            -- Get baseline invoice number.
-            SELECT invoice_number INTO base_invoice_number
-            FROM invoices i
-                   INNER JOIN shipments s on i.shipment_id = s.id
-            WHERE i.shipment_id = invoice.shipment_id
-              AND invoice_number LIKE scac || '%'
-              AND invoice_number NOT LIKE '%-%';
+                -- Update baseline invoice number.
+                UPDATE invoices
+                SET invoice_number = base_invoice_number
+                WHERE id = current_invoice_id;
+              ELSE
+                -- Set subsequent invoice numbers to the baseline number suffixed by "-01", "-02", etc.
 
-            -- Update invoice number for subsequent invoices.
-            UPDATE invoices
-            SET invoice_number = base_invoice_number || '-' || to_char(processed_shipment_invoice_count, 'fm00')
-            WHERE id = invoice.id;
-          END IF;
+                UPDATE invoices
+                SET invoice_number = base_invoice_number || '-' || to_char(invoice_count, 'fm00')
+                WHERE id = current_invoice_id;
+              END IF;
+
+              invoice_count := invoice_count + 1;
+
+            END LOOP;
         END LOOP;
 
     END
     $do$;
 
--- Add a unique constraint for the invoice number.
-ALTER TABLE invoices ADD CONSTRAINT unique_invoice_number UNIQUE (invoice_number);
+-- Now add a unique constraint for the invoice number and set the shipment_id to non-nullable.
+ALTER TABLE invoices
+  ADD CONSTRAINT unique_invoice_number UNIQUE (invoice_number),
+  ALTER COLUMN shipment_id SET NOT NULL;
