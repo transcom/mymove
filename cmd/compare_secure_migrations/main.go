835f9af0878cd6be79617c0b6169399977377ec8
Chris Gilmer <chris@truss.works>
2018-12-31 22:03:36 +0000

    Ignore files larger than 10MB. Up the concurrency and increase the downloaded part size.  Simplify the functon calls.  Pass through the wait group do call Done(). Do comparisons of the hashes after downloading

--- cmd/compare_secure_migrations/main.go
+++ cmd/compare_secure_migrations/main.go
@@ -20,12 +20,6 @@ func initFlags(flag *pflag.FlagSet) {
 	flag.String("aws_s3_region", "", "AWS region used for S3 file storage")
 }
 
-func exitError(msg string, err error) {
-	fmt.Println(msg)
-	fmt.Println(err)
-	os.Exit(1)
-}
-
 func hashObjectMd5(buff *aws.WriteAtBuffer) string {
 
 	// Sum the bytes in the buffer
@@ -34,28 +28,6 @@ func hashObjectMd5(buff *aws.WriteAtBuffer) string {
 
 	// Convert the bytes to a string
 	return hex.EncodeToString(hashInBytes[:16])
-}
-
-func downloadAndHash(downloader *s3manager.Downloader, bucket string, objectName string) (string, error) {
-	var hash string
-
-	// Create an in-memory buffer to write the object
-	buff := &aws.WriteAtBuffer{}
-
-	// Download the object to the buffer
-	_, err := downloader.Download(buff,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key:    aws.String(objectName),
-		})
-	if err != nil {
-		return hash, err
-	}
-
-	// Calculate the hash for the object
-	hash = hashObjectMd5(buff)
-
-	return hash, err
 }
 
 func main() {
@@ -74,7 +46,10 @@ func main() {
 	}))
 
 	s3Service := s3.New(sess)
-	downloader := s3manager.NewDownloader(sess)
+	downloader := s3manager.NewDownloader(sess, func(d *s3manager.Downloader) {
+		d.PartSize = 128 * 1024 * 1024 // 128MB per part
+		d.Concurrency = 100
+	})
 
 	bucketNames := []string{
 		"transcom-ppp-app-experimental-us-west-2",
@@ -85,33 +60,72 @@ func main() {
 	hashCompare := map[string]map[string]string{}
 
 	var wg sync.WaitGroup
+	var errs []error
+
 	for _, bucket := range bucketNames {
 		resp, err := s3Service.ListObjects(&s3.ListObjectsInput{
 			Bucket: aws.String(bucket),
 			Marker: aws.String("secure-migrations"),
 		})
 		if err != nil {
-			exitError("Failed to List Objects", err)
+			errs = append(errs, err)
 		}
 
 		// Download and hash all objects concurrently for this bucket
 		for _, item := range resp.Contents {
 			key := *item.Key
+
+			// Ignore directories and huge migrations
+			// TODO: Make size configurable
+			if *item.Size == 0 || *item.Size >= 10*1024*1024 {
+				fmt.Println("SKIP", bucket, key, "size", *item.Size, "bytes")
+				continue
+			}
+
 			if _, ok := hashCompare[key]; !ok {
 				hashCompare[key] = map[string]string{}
 			}
+
 			wg.Add(1)
-			go func(downloader *s3manager.Downloader, bucket string, objectName string, compare map[string]map[string]string) {
-				hash, err := downloadAndHash(downloader, bucket, objectName)
-				compare[objectName][bucket] = hash
+			go func(downloader *s3manager.Downloader, bucket string, objectName string, compare map[string]map[string]string, wg *sync.WaitGroup) {
+				var hash string
+
+				// Create an in-memory buffer to write the object
+				buff := &aws.WriteAtBuffer{}
+
+				// Download the object to the buffer
+				_, err := downloader.Download(buff,
+					&s3.GetObjectInput{
+						Bucket: aws.String(bucket),
+						Key:    aws.String(objectName),
+					})
+
+				// Save errors for later
 				if err != nil {
-					exitError("Unable to download or hash file", err)
+					errs = append(errs, err)
+					fmt.Println("ERROR", bucket, objectName, err)
 				}
-				fmt.Println(bucket, objectName, hash)
-			}(downloader, bucket, key, hashCompare)
+
+				// Calculate the hash for the object
+				hash = hashObjectMd5(buff)
+				compare[objectName][bucket] = hash
+				wg.Done()
+			}(downloader, bucket, key, hashCompare, &wg)
 		}
 	}
+
 	wg.Wait()
 
-	fmt.Println(hashCompare)
+	for migration, v := range hashCompare {
+		if !(v[bucketNames[0]] == v[bucketNames[1]] && v[bucketNames[1]] == v[bucketNames[2]]) {
+			fmt.Println("Migration:", migration)
+			for _, bucket := range bucketNames {
+				hash := v[bucket]
+				fmt.Println("\t", bucket, "\t", hash)
+			}
+		} else {
+			fmt.Println("Migration:", migration)
+			fmt.Println("\t", "Hash:", v[bucketNames[0]])
+		}
+	}
 }
