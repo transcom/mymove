d3a75374a5e64a80a5da15faef412d891a70ac4e
Rupinder Dhariwal <rd@truss.works>
2019-01-07 20:06:22 +0000

    add test coverage to all pre approval requests

--- cypress/integration/office/preApprovalRequest.js
+++ cypress/integration/office/preApprovalRequest.js
@@ -1,5 +1,6 @@
 import {
   fillAndSavePreApprovalRequest,
+  fillInvalidPreApprovalRequest,
   editPreApprovalRequest,
   approvePreApprovalRequest,
   deletePreApprovalRequest,
@@ -22,7 +23,81 @@ describe('office user interacts with pre approval request panel', function() {
   it('office user deletes pre approval request', function() {
     officeUserDeletesPreApprovalRequest();
   });
+
+  it('office user iterates through every pre approval request', () => {
+    officeUserIterateThroughAllPARS();
+  });
 });
+
+function officeUserIterateThroughAllPARS() {
+  // Open new moves queue
+  cy.visit('/queues/all');
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/all/);
+  });
+
+  // Find move and open it
+  cy
+    .get('div')
+    .contains('RLKBEM')
+    .dblclick();
+
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/new\/moves\/[^/]+\/basics/);
+  });
+
+  // Click on HHG tab
+  cy
+    .get('span')
+    .contains('HHG')
+    .click();
+  cy.location().should(loc => {
+    expect(loc.pathname).to.match(/^\/queues\/new\/moves\/[^/]+\/hhg/);
+  });
+
+  let PARS = [
+    '4A',
+    '4B',
+    '28A',
+    '28B',
+    '28C',
+    '35A',
+    '105B',
+    '105D',
+    '105E',
+    '120A',
+    '120B',
+    '120C',
+    '120D',
+    '120E',
+    '120F',
+    '125A',
+    '125B',
+    '125C',
+    '125D',
+    '130A',
+    '130B',
+    '130C',
+    '130D',
+    '130E',
+    '130F',
+    '130G',
+    '130H',
+    '130I',
+    '130J',
+    '175A',
+    '225A',
+    '225B',
+    '226A',
+  ];
+  PARS.forEach(PAR => {
+    //add a pre approval request first
+    fillAndSavePreApprovalRequest(PAR);
+
+    // Verify data has been saved in the UI
+    cy.get('td').contains(PAR);
+  });
+}
 
 function officeUserCreatesPreApprovalRequest() {
   // Open new moves queue
@@ -53,7 +128,8 @@ function officeUserCreatesPreApprovalRequest() {
   // Verify that the Estimates section contains expected data
   cy.get('span').contains('2,000');
 
-  fillAndSavePreApprovalRequest();
+  fillInvalidPreApprovalRequest();
+  fillAndSavePreApprovalRequest('Article: Motorcycle');
   // Verify data has been saved in the UI
   cy.get('td').contains('Bulky Article: Motorcycle/Rec vehicle');
 }
--- cypress/support/testCreatePreApprovalRequest.js
+++ cypress/support/testCreatePreApprovalRequest.js
@@ -1,20 +1,13 @@
 /* global cy, Cypress*/
-export function fillAndSavePreApprovalRequest() {
+export function fillAndSavePreApprovalRequest(item) {
   // Click on add Pre Approval Request
   cy
     .get('.add-request')
     .contains('Add a request')
     .click();
 
-  // Verify tariff items that don't require approval are not loaded into drop down
-  cy
-    .get('.tariff400-select #react-select-2-input')
-    .first()
-    .type('Linehaul Transportation{downarrow}{enter}', { force: true, delay: 75 });
-  cy.get('.tariff400__single-value').should('not.exist');
-
   //  Enter details in form and create pre approval request
-  cy.selectTariff400ngItem('Article: Motorcycle');
+  cy.selectTariff400ngItem(item);
 
   cy.get('select[name="location"]').select('ORIGIN');
 
@@ -37,6 +30,21 @@ export function fillAndSavePreApprovalRequest() {
     .get('button')
     .contains('Save & Close')
     .click();
+}
+
+export function fillInvalidPreApprovalRequest() {
+  // Click on add Pre Approval Request
+  cy
+    .get('.add-request')
+    .contains('Add a request')
+    .click();
+
+  // Verify tariff items that don't require approval are not loaded into drop down
+  cy
+    .get('.tariff400-select #react-select-2-input')
+    .first()
+    .type('Linehaul Transportation{downarrow}{enter}', { force: true, delay: 75 });
+  cy.get('.tariff400__single-value').should('not.exist');
 }
 
 export function editPreApprovalRequest() {
