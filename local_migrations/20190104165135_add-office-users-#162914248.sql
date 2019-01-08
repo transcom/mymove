96483f44681f638d95bd11a154ac6a378d7d45ab
lindsay t <lindsay@truss.works>
2019-01-08 04:58:24 +0000

    dont insert unnecessary new office users to dev, update office user phone to truss number

--- local_migrations/20190104165135_add-office-users-#162914248.sql
+++ local_migrations/20190104165135_add-office-users-#162914248.sql
@@ -1,5 +1,4 @@
 -- Local test migration.
 -- This will be run on development environments. It should mirror what you
 -- intend to apply on production, but do not include any sensitive data.
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'Person1', 'Ima', NULL, 'ot1@test.test', '415-555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
-INSERT INTO public.office_users VALUES (uuid_generate_v4(), NULL, 'Person2', 'Ima', NULL, 'ot2@test.test', '415-555-5555', '0931a9dc-c1fd-444a-b138-6e1986b1714c', now(), now());
+-- * Leaving empty- new users not needed on development *
