f1297c1de60d04ca8f82a3297a96ea65a47452dc
Matt Krump <matt@truss.works>
2019-01-10 22:17:21 +0000

    Updated for comments in review

--- src/scenes/StyleGuide/index.js
+++ src/scenes/StyleGuide/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const StyleGuide = () => (
   <div>
-    <h1 style={{ 'border-bottom': '1px solid lightgrey' }}>Wizard Styles</h1>
+    <h1>Wizard Styles</h1>
+    <hr />
     <h2>This is a H2</h2>
     <h3>This is a H3</h3>
   </div>
