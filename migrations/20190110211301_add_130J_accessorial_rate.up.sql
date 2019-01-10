8fbc80018c761d74e7ad73206a1c4dfcd180e97c
Patrick <pstanger@gmail.com>
2019-01-10 21:16:32 +0000

    Adds rate for accessorial 130J

---
+++ migrations/20190110211301_add_130J_accessorial_rate.up.sql
@@ -0,0 +1,1 @@
+INSERT INTO tariff400ng_item_rates (id, created_at, updated_at, code, rate_cents, effective_date_lower, effective_date_upper) VALUES ('b3b0de94-eb1b-4a19-a623-d5cb9590dfcc', now(), now(), '130J', 21892, '2018-05-15', '2019-05-15');
