980b94631a2468b4191a72256d99cba34d6247a5
Matt Krump <matt@truss.works>
2019-01-10 16:41:39 +0000

    Add header styles in style guide #162514343

--- src/scenes/StyleGuide/index.js
+++ src/scenes/StyleGuide/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const StyleGuide = () => <div>Placeholder style guide</div>;
+const StyleGuide = () => (
+  <div>
+    <h1 style={{ 'border-bottom': '1px solid lightgrey' }}>Wizard Styles</h1>
+    <h2>This is a H2</h2>
+    <h3>This is a H3</h3>
+  </div>
+);
 
 export default StyleGuide;
