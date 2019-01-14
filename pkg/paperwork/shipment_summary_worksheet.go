d0ef4e83fdd015a32740bd404d07b5779350e4b0
Matt Krump <matt@truss.works>
2019-01-14 19:10:07 +0000

    Update ServiceMemberName position

--- pkg/paperwork/shipment_summary_worksheet.go
+++ pkg/paperwork/shipment_summary_worksheet.go
@@ -6,7 +6,7 @@ var ShipmentSummaryPage1Layout = FormLayout{
 	TemplateImagePath: "pkg/paperwork/formtemplates/shipment_summary_worksheet_page1.png",
 
 	FieldsLayout: map[string]FieldPos{
-		"ServiceMemberName": FormField(45, 38, 105, floatPtr(10), nil),
+		"ServiceMemberName": FormField(45, 42, 105, floatPtr(10), nil),
 	},
 }
 
