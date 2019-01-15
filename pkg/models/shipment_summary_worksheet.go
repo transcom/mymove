e1040e7ed2bb204439ee2cb90c454149f014c92a
Matt Krump <matt@truss.works>
2019-01-15 23:26:16 +0000

    Collect the values for Shipment Summary Worksheet into struct

--- pkg/models/shipment_summary_worksheet.go
+++ pkg/models/shipment_summary_worksheet.go
@@ -1,38 +1,166 @@
 package models
 
 import (
+	"context"
+	"fmt"
 	"github.com/gobuffalo/pop"
 	"github.com/gofrs/uuid"
+	"github.com/transcom/mymove/pkg/gen/internalmessages"
+	"time"
 )
+
+// FetchShipmentSummaryWorksheetFormValues fetches a single ShipmentSummaryWorksheetExtractor for a given Shipment ID
+func FetchShipmentSummaryWorksheetFormValues(db *pop.Connection, moveID uuid.UUID) (ShipmentSummaryWorksheetPage1Values, ShipmentSummaryWorksheetPage2Values, error) {
+	var err error
+	var ssfd shipmentSummaryFormData
+	var page1 ShipmentSummaryWorksheetPage1Values
+	page2 := ShipmentSummaryWorksheetPage2Values{}
+
+	ssfd, err = fetchDataShipmentSummaryWorksFormData(db, moveID)
+	if err != nil {
+		return page1, page2, err
+	}
+	page1 = formatValuesShipmentSummaryWorksheetFormPage1(ssfd)
+	return page1, page2, nil
+}
 
 // ShipmentSummaryWorksheetPage1Values is an object representing a Shipment Summary Worksheet
 // Convert dates to strings in order to avoid automatic formatting within forms.go
 type ShipmentSummaryWorksheetPage1Values struct {
-	ServiceMemberName        string `db:"service_member_name"`
+	ServiceMemberName        string
 	MaxSITStorageEntitlement string
+	PreferredPhone           string
+	PreferredEmail           string
+	DODId                    string
+	ServiceBranch            string
+	Rank                     string
+	OrdersNumber             string
+	IssuingAgency            string
+	OrderIssueDate           time.Time
+	OrdersType               internalmessages.OrdersType
+	DutyStationID            uuid.UUID
+	AuthorizedOrigin         DutyStation
+	NewDutyStationID         uuid.UUID
+	AuthorizedDestination    DutyStation
+	WeightAllotment          WeightAllotment
+	TotalWeightAllotment     int
 }
 
 // ShipmentSummaryWorksheetPage2Values is an object representing a Shipment Summary Worksheet
 type ShipmentSummaryWorksheetPage2Values struct {
 }
 
-// FetchShipmentSummaryWorksheetFormValues fetches a single ShipmentSummaryWorksheetExtractor for a given Shipment ID
-func FetchShipmentSummaryWorksheetFormValues(db *pop.Connection, moveID uuid.UUID) (ShipmentSummaryWorksheetPage1Values, ShipmentSummaryWorksheetPage2Values, error) {
-	var page1 ShipmentSummaryWorksheetPage1Values
-	var page2 ShipmentSummaryWorksheetPage2Values
+type shipmentSummaryFormData struct {
+	ServiceMember      ServiceMember
+	Order              Order
+	CurrentDutyStation DutyStation
+	NewDutyStation     DutyStation
+	WeightAllotment    WeightAllotment
+}
 
-	sql := ` SELECT
-				concat_ws(', ', concat_ws(' ', sm.last_name, sm.suffix), concat_ws(' ', sm.first_name, sm.middle_name)) AS service_member_name
-				FROM moves m
-				INNER JOIN orders o ON m.orders_id = o.id
-				INNER JOIN service_members sm ON o.service_member_id = sm.id
-				WHERE m.id = $1
-				`
-	err := db.RawQuery(sql, moveID).First(&page1)
+func fetchDataShipmentSummaryWorksFormData(db *pop.Connection, moveID uuid.UUID) (data shipmentSummaryFormData, err error) {
+	ssd := shipmentSummaryFormData{}
+	ids, err := getRequiredFields(err, db, moveID)
 	if err != nil {
-		return page1, page2, err
+		return ssd, err
 	}
+	ssd.Order, err = FetchOrder(db, ids.OrdersID)
+	if err != nil {
+		return ssd, err
+	}
+	ssd.ServiceMember, err = FetchServiceMember(db, ids.ServiceMemberID)
+	if err != nil {
+		return ssd, err
+	}
+	// TODO confirm context
+	ssd.CurrentDutyStation, err = FetchDutyStation(context.Background(), db, ids.ServiceMemberDutyStationID)
+	if err != nil {
+		return ssd, err
+	}
+	// TODO confirm context
+	ssd.NewDutyStation, err = FetchDutyStation(context.Background(), db, ssd.Order.NewDutyStationID)
+	if err != nil {
+		return ssd, err
+	}
+	rank := ServiceMemberRank(ids.ServiceMemberRank)
+	ssd.WeightAllotment = GetWeightAllotment(rank)
+	return ssd, nil
+}
+
+func formatValuesShipmentSummaryWorksheetFormPage1(data shipmentSummaryFormData) ShipmentSummaryWorksheetPage1Values {
+	page1 := ShipmentSummaryWorksheetPage1Values{}
 	page1.MaxSITStorageEntitlement = "90 days per each shipment"
 
-	return page1, page2, nil
+	// TODO ask about various pointer derefs
+	sm := data.ServiceMember
+	lastName := derefStringTypes(sm.LastName)
+	suffix := derefStringTypes(sm.Suffix)
+	firstName := derefStringTypes(sm.FirstName)
+	middleName := derefStringTypes(sm.MiddleName)
+	fullName := fmt.Sprintf("%s %s, %s %s", lastName, suffix, firstName, middleName)
+
+	page1.ServiceMemberName = fullName
+	page1.PreferredPhone = derefStringTypes(sm.Telephone)
+	page1.PreferredEmail = derefStringTypes(sm.PersonalEmail)
+	page1.DODId = derefStringTypes(sm.Edipi)
+	page1.ServiceBranch = derefStringTypes(sm.Affiliation)
+	page1.Rank = derefStringTypes(sm.Rank)
+
+	o := data.Order
+	page1.OrdersNumber = derefStringTypes(o.OrdersNumber)
+	page1.IssuingAgency = derefStringTypes(o.OrdersIssuingAgency)
+	page1.OrderIssueDate = o.IssueDate
+	page1.OrdersType = o.OrdersType
+
+	page1.AuthorizedOrigin = data.CurrentDutyStation
+	page1.AuthorizedDestination = data.NewDutyStation
+	page1.WeightAllotment = data.WeightAllotment
+	page1.TotalWeightAllotment = data.WeightAllotment.TotalWeightSelf +
+		data.WeightAllotment.ProGearWeight +
+		data.WeightAllotment.ProGearWeightSpouse
+	return page1
+}
+
+type requiredFields struct {
+	OrdersID                   uuid.UUID `db:"orders_id"`
+	ServiceMemberID            uuid.UUID `db:"service_member_id"`
+	ServiceMemberDutyStationID uuid.UUID `db:"duty_station_id"`
+	ServiceMemberRank          string    `db:"rank"`
+}
+
+func getRequiredFields(err error, db *pop.Connection, moveID uuid.UUID) (requiredFields, error) {
+	p := requiredFields{}
+	sql := `
+		SELECT orders_id,
+			   service_member_id,
+			   duty_station_id,
+			   rank
+		FROM moves m
+				 INNER JOIN orders o ON m.orders_id = o.id
+				 INNER JOIN service_members sm ON o.service_member_id = sm.id
+		WHERE m.id = $1`
+	err = db.RawQuery(sql, moveID).First(&p)
+	return p, err
+}
+
+func derefStringTypes(st interface{}) string {
+	switch v := st.(type) {
+	case *string:
+		if v != nil {
+			return *v
+		}
+	case string:
+		return v
+	case *ServiceMemberRank:
+		if v != nil {
+			return string(*v)
+		}
+		return ""
+	case *ServiceMemberAffiliation:
+		if v != nil {
+			return string(*v)
+		}
+		return ""
+	}
+	return ""
 }
--- pkg/models/shipment_summary_worksheet_test.go
+++ pkg/models/shipment_summary_worksheet_test.go
@@ -1,22 +1,113 @@
 package models_test
 
 import (
+	"fmt"
+	"github.com/gobuffalo/uuid"
+	"github.com/transcom/mymove/pkg/gen/internalmessages"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
+	"time"
 )
 
 func (suite *ModelSuite) TestFetchShipmentSummaryWorksheetFormValues() {
+	moveID, _ := uuid.NewV4()
+	firstName := "Marcus"
+	middleName := "Joseph"
+	lastName := "Jenkins"
+	suffix := "Jr."
+	fullName := fmt.Sprintf("%s %s, %s %s", lastName, suffix, firstName, middleName)
+	preferredPhoneNumber := "444-555-8888"
+	preferredEmail := "michael+ppm-expansion_1@truss.works"
+	maxSITStorageEntitlementDefault := "90 days per each shipment"
+	dodID := "1234567890"
+	serviceBranch := models.AffiliationAIRFORCE
+	rank := models.ServiceMemberRankE9
+	wtgEntitlements := models.WeightAllotment{
+		TotalWeightSelf:     13000,
+		ProGearWeight:       2000,
+		ProGearWeightSpouse: 500,
+	}
+	totalWeightEntitlement := 15500
+	ordersDate := time.Date(2018, time.December, 21, 0, 0, 0, 0, time.UTC)
+	print(ordersDate.Location())
+	ordersType := internalmessages.OrdersTypePERMANENTCHANGEOFSTATION
+	ordersNumber := "012345"
+	issuingBranch := string(serviceBranch)
+
+	fortBraggAssertions := testdatagen.Assertions{
+		Address: models.Address{
+			City:       "Fort Bragg",
+			State:      "NC",
+			PostalCode: "28310",
+		},
+		DutyStation: models.DutyStation{
+			Name: "Fort Bragg",
+		},
+	}
+	fortBragg := testdatagen.MakeDutyStation(suite.DB(), fortBraggAssertions)
+
+	fortBenningAssertions := testdatagen.Assertions{
+		Address: models.Address{
+			City:       "Fort Benning",
+			State:      "GA",
+			PostalCode: "31905",
+		},
+		DutyStation: models.DutyStation{
+			Name: "Fort Benning",
+		},
+	}
+	fortBenning := testdatagen.MakeDutyStation(suite.DB(), fortBenningAssertions)
+
 	move := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{
 		ServiceMember: models.ServiceMember{
-			FirstName:  models.StringPointer("Marcus"),
-			MiddleName: models.StringPointer("Joseph"),
-			LastName:   models.StringPointer("Jenkins"),
-			Suffix:     models.StringPointer("Jr."),
+			ID:            moveID,
+			FirstName:     models.StringPointer(firstName),
+			MiddleName:    models.StringPointer(middleName),
+			LastName:      models.StringPointer(lastName),
+			Suffix:        models.StringPointer(suffix),
+			Telephone:     models.StringPointer(preferredPhoneNumber),
+			PersonalEmail: models.StringPointer(preferredEmail),
+			Edipi:         models.StringPointer(dodID),
+			Affiliation:   &serviceBranch,
+			Rank:          &rank,
+			DutyStationID: &fortBragg.ID,
+		},
+		Order: models.Order{
+			IssueDate:           ordersDate,
+			OrdersType:          ordersType,
+			OrdersNumber:        models.StringPointer(ordersNumber),
+			NewDutyStationID:    fortBenning.ID,
+			OrdersIssuingAgency: &issuingBranch,
 		},
 	})
 	sswPage1, _, err := models.FetchShipmentSummaryWorksheetFormValues(suite.DB(), move.ID)
-	suite.NoError(err)
 
-	suite.Equal("Jenkins Jr., Marcus Joseph", sswPage1.ServiceMemberName)
-	suite.Equal("90 days per each shipment", sswPage1.MaxSITStorageEntitlement)
+	suite.NoError(err)
+	suite.Equal(fullName, sswPage1.ServiceMemberName)
+	suite.Equal(maxSITStorageEntitlementDefault, sswPage1.MaxSITStorageEntitlement)
+	suite.Equal(preferredPhoneNumber, sswPage1.PreferredPhone)
+	suite.Equal(preferredEmail, sswPage1.PreferredEmail)
+	suite.Equal(dodID, sswPage1.DODId)
+	suite.Equal(string(serviceBranch), sswPage1.ServiceBranch)
+	suite.Equal(string(rank), sswPage1.Rank)
+
+	suite.Equal(ordersNumber, sswPage1.OrdersNumber)
+	suite.Equal(issuingBranch, sswPage1.IssuingAgency)
+	suite.True(ordersDate.Equal(sswPage1.OrderIssueDate))
+	suite.Equal(ordersType, sswPage1.OrdersType)
+
+	suite.Equal(fortBragg.ID, sswPage1.AuthorizedOrigin.ID)
+	suite.Equal(fortBragg.Address.State, sswPage1.AuthorizedOrigin.Address.State)
+	suite.Equal(fortBragg.Address.City, sswPage1.AuthorizedOrigin.Address.City)
+	suite.Equal(fortBragg.Address.PostalCode, sswPage1.AuthorizedOrigin.Address.PostalCode)
+
+	suite.Equal(fortBenning.ID, sswPage1.AuthorizedDestination.ID)
+	suite.Equal(fortBenning.Address.State, sswPage1.AuthorizedDestination.Address.State)
+	suite.Equal(fortBenning.Address.City, sswPage1.AuthorizedDestination.Address.City)
+	suite.Equal(fortBenning.Address.PostalCode, sswPage1.AuthorizedDestination.Address.PostalCode)
+
+	suite.Equal(wtgEntitlements.TotalWeightSelf, sswPage1.WeightAllotment.TotalWeightSelf)
+	suite.Equal(wtgEntitlements.ProGearWeight, sswPage1.WeightAllotment.ProGearWeight)
+	suite.Equal(wtgEntitlements.ProGearWeightSpouse, sswPage1.WeightAllotment.ProGearWeightSpouse)
+	suite.Equal(totalWeightEntitlement, sswPage1.TotalWeightAllotment)
 }
