fc12b48120506417eddbe990c6c11a29f2400e87
Chris Gilmer <chris@truss.works>
2019-01-15 00:32:31 +0000

    Try out load testing framework locust.io

---
+++ load_testing/README.md
@@ -0,0 +1,23 @@
+# Load Testing
+
+## locust.io
+
+Getting started
+
+```sh
+brew install libev
+virtualenv .venv -p python3
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+In a separate window ensure that the app server is running with `make server_run`.
+
+Then run tests:
+
+```sh
+locust --host=http://milmovelocal:8080 -f load_testing/locustfile.py
+```
+
+Then open [http://localhost:8089](http://localhost:8089/) and enter the number of users to simulate and the hatch rate.
+Finally, hit the `Start swarming` button and wait for the tests to finish.
---
+++ load_testing/locustfile.py
@@ -0,0 +1,27 @@
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.login()
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        self.logout()
+
+    def login(self):
+        self.client.post("/devlocal-auth/new")
+
+    def logout(self):
+        self.client.post("/auth/logout")
+
+    @task(1)
+    def index(self):
+        self.client.get("/")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
