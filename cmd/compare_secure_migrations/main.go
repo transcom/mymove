243b77411ee71f13f0187815d12b8af9785b0edd
Chris Gilmer <chris@truss.works>
2019-01-18 17:50:00 +0000

    Compare objects by size as default method. Let users compare by md5 hash instead which requires downloading objects.  Include more flags and check those flags to ensure config is good before executing. Add logging.

--- cmd/compare_secure_migrations/main.go
+++ cmd/compare_secure_migrations/main.go
@@ -3,23 +3,44 @@ package main
 import (
 	"crypto/md5" // #nosec
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 	awssession "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+type errInvalidRegion struct {
+	Region string
+}
+
+func (e *errInvalidRegion) Error() string {
+	return fmt.Sprintf("invalid region %s", e.Region)
+}
+
+type errInvalidComparison struct {
+	Comparison string
+}
+
+func (e *errInvalidComparison) Error() string {
+	return fmt.Sprintf("invalid comparison %s", e.Comparison)
+}
 
 func initFlags(flag *pflag.FlagSet) {
 	flag.String("aws-s3-region", "", "AWS region used for S3 file storage")
-	flag.Int64("max-object-size", 10, "The maximum size of files to download in MB")
+	flag.String("comparison", "size", "Comparison used against files, either 'size' or 'md5'")
+	flag.Int64("max-object-size", 10, "The maximum size of files to download in MB for use with md5 comparison")
 }
 
 func hashObjectMd5(buff *aws.WriteAtBuffer) string {
@@ -32,6 +53,56 @@ func hashObjectMd5(buff *aws.WriteAtBuffer) string {
 	return hex.EncodeToString(hashInBytes[:16])
 }
 
+func stringSliceContains(stringSlice []string, value string) bool {
+	for _, x := range stringSlice {
+		if value == x {
+			return true
+		}
+	}
+	return false
+}
+
+func checkRegion(v *viper.Viper) error {
+
+	regions, ok := endpoints.RegionsForService(endpoints.DefaultPartitions(), endpoints.AwsPartitionID, endpoints.S3ServiceID)
+	if !ok {
+		return fmt.Errorf("could not find regions for service %s", endpoints.S3ServiceID)
+	}
+
+	r := v.GetString("aws-s3-region")
+	if len(r) == 0 {
+		return errors.Wrap(&errInvalidRegion{Region: r}, fmt.Sprintf("%s is invalid", "aws-s3-region"))
+	}
+
+	if _, ok := regions[r]; !ok {
+		return errors.Wrap(&errInvalidRegion{Region: r}, fmt.Sprintf("%s is invalid", "aws-s3-region"))
+	}
+
+	return nil
+}
+
+func checkComparison(v *viper.Viper) error {
+	if c := v.GetString("comparison"); len(c) == 0 || !stringSliceContains([]string{"size", "md5"}, c) {
+		return errors.Wrap(&errInvalidComparison{Comparison: c}, fmt.Sprintf("%s is invalid", "comparison"))
+	}
+	return nil
+}
+
+func checkConfig(v *viper.Viper) error {
+
+	err := checkRegion(v)
+	if err != nil {
+		return err
+	}
+
+	err = checkComparison(v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 
 	flag := pflag.CommandLine
@@ -42,6 +113,22 @@ func main() {
 	v.BindPFlags(flag)
 	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	v.AutomaticEnv()
+
+	var loggerConfig zap.Config
+
+	loggerConfig = zap.NewProductionConfig()
+
+	loggerConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		logger.Fatal("unable to build logger", zap.Error(err))
+	}
+
+	err = checkConfig(v)
+	if err != nil {
+		logger.Fatal("invalid configuration", zap.Error(err))
+	}
 
 	sess := awssession.Must(awssession.NewSession(&aws.Config{
 		Region: aws.String(v.GetString("aws-s3-region")),
@@ -59,11 +146,12 @@ func main() {
 		"transcom-ppp-app-prod-us-west-2",
 	}
 
-	hashCompare := map[string]map[string]string{}
+	compareMap := map[string]map[string]string{}
 
 	var wg sync.WaitGroup
 	var errs []error
 
+	comparison := v.GetString("comparison")
 	maxObjectSize := v.GetInt64("max-object-size")
 
 	for _, bucket := range bucketNames {
@@ -75,60 +163,68 @@ func main() {
 			errs = append(errs, err)
 		}
 
-		// Download and hash all objects concurrently for this bucket
+		// Download and compare all objects concurrently for this bucket
 		for _, item := range resp.Contents {
 			key := *item.Key
 
 			// Ignore directories and migrations larger than maxObjectSize
-			if *item.Size == 0 || *item.Size >= maxObjectSize*1024*1024 {
+			if comparison == "md5" && (*item.Size == 0 || *item.Size >= maxObjectSize*1024*1024) {
 				fmt.Println("SKIP", bucket, key, "size", *item.Size, "bytes")
 				continue
 			}
 
-			if _, ok := hashCompare[key]; !ok {
-				hashCompare[key] = map[string]string{}
+			if _, ok := compareMap[key]; !ok {
+				compareMap[key] = map[string]string{}
 			}
 
-			wg.Add(1)
-			go func(downloader *s3manager.Downloader, bucket string, objectName string, compare map[string]map[string]string, wg *sync.WaitGroup) {
-				var hash string
+			if comparison == "md5" {
+				wg.Add(1)
+				go func(downloader *s3manager.Downloader, bucket string, objectName string, compare map[string]map[string]string, wg *sync.WaitGroup) {
+					var hash string
 
-				// Create an in-memory buffer to write the object
-				buff := &aws.WriteAtBuffer{}
+					// Create an in-memory buffer to write the object
+					buff := &aws.WriteAtBuffer{}
 
-				// Download the object to the buffer
-				_, err := downloader.Download(buff,
-					&s3.GetObjectInput{
-						Bucket: aws.String(bucket),
-						Key:    aws.String(objectName),
-					})
+					// Download the object to the buffer
+					_, err := downloader.Download(buff,
+						&s3.GetObjectInput{
+							Bucket: aws.String(bucket),
+							Key:    aws.String(objectName),
+						})
 
-				// Save errors for later
-				if err != nil {
-					errs = append(errs, err)
-					fmt.Println("ERROR", bucket, objectName, err)
-				}
+					// Save errors for later
+					if err != nil {
+						errs = append(errs, err)
+						fmt.Println("ERROR", bucket, objectName, err)
+					}
 
-				// Calculate the hash for the object
-				hash = hashObjectMd5(buff)
-				compare[objectName][bucket] = hash
-				wg.Done()
-			}(downloader, bucket, key, hashCompare, &wg)
+					// Calculate the hash for the object
+					hash = hashObjectMd5(buff)
+					compare[objectName][bucket] = hash
+					wg.Done()
+				}(downloader, bucket, key, compareMap, &wg)
+			} else {
+				compareMap[key][bucket] = strconv.FormatInt(*item.Size, 10)
+			}
 		}
 	}
 
 	wg.Wait()
 
-	// Compare hashses and print differences
-	for migration, v := range hashCompare {
+	// Compare files and print differences
+	for migration, v := range compareMap {
 		if v[bucketNames[0]] == v[bucketNames[1]] && v[bucketNames[1]] == v[bucketNames[2]] {
 			fmt.Println("Migration:", migration)
-			fmt.Println("\t", "Hash:", v[bucketNames[0]])
+			fmt.Println("\t", fmt.Sprintf("%s:", comparison), v[bucketNames[0]])
 		} else {
 			fmt.Println("Migration:", migration)
 			for _, bucket := range bucketNames {
-				hash := v[bucket]
-				fmt.Println("\t", bucket, "\t", hash)
+				comp := v[bucket]
+				if comp != "" {
+					fmt.Println("\t", bucket, "\t", comp)
+				} else {
+					fmt.Println("\t", bucket, "\tNot Found")
+				}
 			}
 		}
 	}
