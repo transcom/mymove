581e18a549e423c3e8ea9c001c7a75047a906531
Isaac Garfinkle <isaac@truss.works>
2019-01-19 03:57:23 +0000

    Remove issue-related code

--- src/scenes/SubmittedFeedback/api.js
+++
@@ -1,8 +0,0 @@
-import { getClient, checkResponse } from 'shared/Swagger/api';
-
-export async function IssuesIndex() {
-  const client = await getClient();
-  const response = await client.apis.issues.indexIssues();
-  checkResponse(response, 'failed to load issues index due to server error');
-  return response.body;
-}
