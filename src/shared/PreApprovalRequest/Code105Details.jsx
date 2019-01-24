f794ff0f66f4dedeff0fc8b694c880607fd20a7c
Donald Thai <donald@truss.works>
2019-01-24 23:00:09 +0000

    Added SwaggerField for item description

--- src/shared/PreApprovalRequest/Code105Details.jsx
+++ src/shared/PreApprovalRequest/Code105Details.jsx
@@ -1,9 +1,17 @@
 import React, { Fragment } from 'react';
+import { SwaggerField } from 'shared/JsonSchemaForm/JsonSchemaField';
+
 
 export const Code105Details = props => {
+  const { swagger } = props;
   return (
     <Fragment>
-      <div>More to come!</div>
+      <SwaggerField
+        fieldName="description"
+        className="three-quarter-width"
+        swagger={swagger}
+        required
+      />
     </Fragment>
   );
 };
