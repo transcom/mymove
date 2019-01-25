b2498d9d2aa5f0070cc79248104aa637f2cb7314
Donald Thai <donald@truss.works>
2019-01-25 00:06:05 +0000

    Prettier changes

--- src/shared/PreApprovalRequest/Code105Details.jsx
+++ src/shared/PreApprovalRequest/Code105Details.jsx
@@ -1,18 +1,12 @@
 import React, { Fragment } from 'react';
 import { SwaggerField } from 'shared/JsonSchemaForm/JsonSchemaField';
 
-
 export const Code105Details = props => {
   const { ship_line_item_schema } = props;
   return (
     <Fragment>
       <div>More to come!</div>
-      <SwaggerField
-        fieldName="description"
-        className="three-quarter-width"
-        swagger={ship_line_item_schema}
-        required
-      />
+      <SwaggerField fieldName="description" className="three-quarter-width" swagger={ship_line_item_schema} required />
     </Fragment>
   );
 };
