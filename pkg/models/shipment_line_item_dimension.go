f35b9dee86050311dda36dc2733615c895bfa3b8
Donald Thai <donald@truss.works>
2019-01-30 22:49:02 +0000

    Added created and updated at props

--- pkg/models/shipment_line_item_dimension.go
+++ pkg/models/shipment_line_item_dimension.go
@@ -1,14 +1,18 @@
 package models
 
 import (
+	"time"
+
 	"github.com/gofrs/uuid"
 	"github.com/transcom/mymove/pkg/unit"
 )
 
 // ShipmentLineItemDimension is an object representing dimensions of a shipment line item
 type ShipmentLineItemDimension struct {
-	ID     uuid.UUID `json:"id" db:"id"`
-	Length unit.Inch `json:"length" db:"length"`
-	Width  unit.Inch `json:"width" db:"width"`
-	Height unit.Inch `json:"height" db:"height"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	Length    unit.Inch `json:"length" db:"length"`
+	Width     unit.Inch `json:"width" db:"width"`
+	Height    unit.Inch `json:"height" db:"height"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
