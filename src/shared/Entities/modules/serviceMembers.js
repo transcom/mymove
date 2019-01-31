58c7a4338b5e090f21be358a3be38bc91ea57c21
Sara Bocciardi <sara@truss.works>
2019-01-31 17:50:50 +0000

    Pull serviceMember from proper location.

--- src/shared/Entities/modules/serviceMembers.js
+++ src/shared/Entities/modules/serviceMembers.js
@@ -44,6 +44,19 @@ export function selectServiceMember(state, serviceMemberId) {
   return get(state, `entities.serviceMembers.${serviceMemberId}`, {});
 }
 
+export function selectServiceMemberForMove(state, moveId) {
+  const move = get(state, `entities.moves.${moveId}`);
+  if (!move) {
+    return {};
+  }
+  const orders = get(state, `entities.orders.${move.orders_id}`);
+  if (!orders) {
+    return {};
+  }
+  const serviceMember = get(state, `entities.serviceMembers.${orders.service_member_id}`);
+  return serviceMember || {};
+}
+
 export function selectBackupContactForServiceMember(state, serviceMemberId) {
   return Object.values(state.entities.backupContacts).find(backupContact => {
     return backupContact.service_member_id === serviceMemberId;
