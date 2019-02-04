9abcda2b351525ed7a8e00a5f1403f3c6c82fa41
Arianna Kellogg <ari@truss.works>
2019-02-04 22:19:52 +0000

    Revert "removed bin folder"
    This reverts commit 37a76d5fae138d1c4218e569f4e8ce3371a3dae7.

---
+++ bin/copy_swagger_ui.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Copies the assets (other than xxx.html) into the public directory
+# internal.html & api.html are checked into public/swagger-ui and
+# will need to be manually updated based on node_modules/swagger-ui-dist/index.html
+# if it ever changes.
+cp node_modules/swagger-ui-dist/{*.js,*.css,*.png} public/swagger-ui
+cp node_modules/js-cookie/src/js.cookie.js public/swagger-ui
