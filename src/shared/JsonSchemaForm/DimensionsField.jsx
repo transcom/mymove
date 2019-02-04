47516b974a3c6eae2fe0302c0876a1a5bbd591ee
Rupinder Dhariwal <rd@truss.works>
2019-02-04 21:16:52 +0000

    require length width and height to be non zero for 105 B & E 400ng items

--- src/shared/JsonSchemaForm/DimensionsField.jsx
+++ src/shared/JsonSchemaForm/DimensionsField.jsx
@@ -33,6 +33,7 @@ export class DimensionsField extends Component {
                   swagger={get(this.props, 'swagger.properties.' + this.props.fieldName)}
                   hideLabel={true}
                   className="dimensions-form-input"
+                  required={this.props.isRequired}
                 />
               </td>
               <td width="11%" className="multiplication-sign">
@@ -44,6 +45,7 @@ export class DimensionsField extends Component {
                   swagger={get(this.props, 'swagger.properties.' + this.props.fieldName)}
                   hideLabel={true}
                   className="dimensions-form-input"
+                  required={this.props.isRequired}
                 />
               </td>
               <td width="11%" className="multiplication-sign">
@@ -55,6 +57,7 @@ export class DimensionsField extends Component {
                   swagger={get(this.props, 'swagger.properties.' + this.props.fieldName)}
                   hideLabel={true}
                   className="dimensions-form-input"
+                  required={this.props.isRequired}
                 />
               </td>
             </tr>
@@ -69,4 +72,5 @@ DimensionsField.propTypes = {
   fieldName: PropTypes.string.isRequired,
   labelText: PropTypes.string.isRequired,
   swagger: PropTypes.object.isRequired,
+  isRequired: PropTypes.bool.isRequired,
 };
