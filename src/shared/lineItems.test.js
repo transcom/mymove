45ed0eebe5e475d763f29afeaa8a4c9bcb8bf08a
lindsay t <techells22@gmail.com>
2019-02-13 03:53:15 +0000

    try and make Codacy happy

--- src/shared/lineItems.test.js
+++ src/shared/lineItems.test.js
@@ -4,7 +4,8 @@ describe('lineItems', () => {
   describe('displayBaseQuantityUnits', () => {
     it('display full pack, full unpack, origin and dest fee weight truncated to 0 decimal places', () => {
       const item105A = { tariff400ng_item: { code: '105A' }, quantity_1: 5000000 };
-      const item105C = { tariff400ng_item: { code: '105C' }, quantity_1: 2000000000 };
+      const item105C = { tariff400ng_item: { code: '105C' }, quantity_1: Number.MAX_SAFE_INTEGER };
+
       const item135A = { tariff400ng_item: { code: '135A' }, quantity_1: 50000 };
       const item135B = { tariff400ng_item: { code: '135B' }, quantity_1: 51111 };
       const itemQuantityNull = { tariff400ng_item: { code: '105A' }, quantity_1: null };
@@ -13,7 +14,7 @@ describe('lineItems', () => {
       const itemNull = null;
 
       expect(lineItems.displayBaseQuantityUnits(item105A)).toEqual('500 lbs');
-      expect(lineItems.displayBaseQuantityUnits(item105C)).toEqual('200,000 lbs');
+      expect(lineItems.displayBaseQuantityUnits(item105C)).toEqual('900,719,925,474 lbs');
       expect(lineItems.displayBaseQuantityUnits(item135A)).toEqual('5 lbs');
       expect(lineItems.displayBaseQuantityUnits(item135B)).toEqual('5 lbs');
       expect(lineItems.displayBaseQuantityUnits(itemQuantityNull)).toEqual('0 lbs');
