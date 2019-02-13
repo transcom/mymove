2c24d2bcb121a6971de62eb62412512dbfdca4bf
Rupinder Dhariwal <rd@truss.works>
2019-02-13 16:45:22 +0000

    Added additional tests to make sure swaggerfield props are checked as well

--- src/shared/JsonSchemaForm/DimensionsField.test.js
+++ src/shared/JsonSchemaForm/DimensionsField.test.js
@@ -1,4 +1,5 @@
 import { DimensionsField } from 'shared/JsonSchemaForm/DimensionsField';
+import { SwaggerField } from 'shared/JsonSchemaForm/JsonSchemaField';
 import { shallow } from 'enzyme/build';
 import React from 'react';
 
@@ -7,4 +8,17 @@ describe('given a dimension input', () => {
   let swagger = { a: 'test', b: 2 };
   let wrapper = shallow(<DimensionsField isRequired={true} fieldName={'test'} labelText={'test'} swagger={swagger} />);
   expect(wrapper.find('.dimensions-form').length).toBe(1);
+
+  it('should render SwaggerField with a required prop', () => {
+    let wrapper = shallow(
+      <DimensionsField isRequired={true} fieldName={'test'} labelText={'test'} swagger={swagger} />,
+    );
+    // prettier-ignore
+    expect(
+      wrapper
+        .find(SwaggerField)
+        .at(0)
+        .props().required
+    ).toBe(true);
+  });
 });
