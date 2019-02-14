c0b73298c9d8348d595af1520defa2bdd16b6d9d
Patrick <pstanger@gmail.com>
2019-02-14 18:03:21 +0000

    Uses error level logging

--- pkg/route/here_planner.go
+++ pkg/route/here_planner.go
@@ -166,7 +166,7 @@ func (p *herePlanner) LatLongTransitDistance(source LatLong, dest LatLong) (int,
 func (p *herePlanner) Zip5TransitDistance(source string, destination string) (int, error) {
 	distanceMiles, err := zip5TransitDistanceHelper(p, source, destination)
 	if err != nil {
-		p.logger.Info("Failed to calculate HERE route between ZIPs", zap.String("source", source), zap.String("destination", destination))
+		p.logger.Error("Failed to calculate HERE route between ZIPs", zap.String("source", source), zap.String("destination", destination))
 	}
 
 	return distanceMiles, err
