ccaf48ccb87d60c70e67af8cd428e9c5b5dab6f4
Chris Gilmer <chris@truss.works>
2019-02-15 00:13:38 +0000

    Begin specifying a task sequence for a service member

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet, task
+from locust import (HttpLocust, TaskSet, TaskSequence, task, seq_task)
 
 
 class AnonBehavior(TaskSet):
@@ -14,11 +14,11 @@ class AnonUser(HttpLocust):
     task_set = AnonBehavior
 
 
-class UserBehavior(TaskSet):
+class UserBehavior(TaskSequence):
 
-    csrf = None
-    user = None
+    login_gov_user = None
     token = None
+    user = {}
 
     def _get_csrf_token(self):
         """
@@ -26,39 +26,60 @@ class UserBehavior(TaskSet):
 
         The token is set as a cookie with the name `masked_gorilla_csrf`
         """
-        if self.csrf:
-            return self.csrf
         self.client.get('/')
-        self.csrf = self.client.cookies.get('masked_gorilla_csrf')
+        csrf = self.client.cookies.get('masked_gorilla_csrf')
+        self.client.headers.update({'x-csrf-token': csrf})
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
         self._get_csrf_token()
-        self.login()
 
     def on_stop(self):
         """ on_stop is called when the TaskSet is stopping """
-        self.logout()
+        pass
 
+    @seq_task(1)
     def login(self):
-        resp = self.client.post('/devlocal-auth/create',
-                                headers={'x-csrf-token': self.csrf})
+        resp = self.client.post('/devlocal-auth/create')
         try:
-            self.user = resp.json()
+            self.login_gov_user = resp.json()
             self.token = self.client.cookies.get('mil_session_token')
         except Exception:
-            print('CSRF Token:', self.csrf)
+            print('Headers:', self.client.headers)
             print(resp.content)
 
+    @seq_task(2)
+    def retriever_user(self):
+        resp = self.client.get("/internal/users/logged_in")
+        self.user = resp.json()
+        # check response for 200
+
+    @seq_task(3)
+    def create_service_member(self):
+        payload = {"id": self.user["id"]}
+        resp = self.client.post("/internal/service_members", json=payload)
+        service_member = resp.json()
+        self.user["service_member"] = service_member
+        # check response for 201
+
+    @seq_task(4)
+    def create_your_profile(self):
+        service_member_id = self.user["service_member"]["id"]
+        url = "/internal/service_members/" + service_member_id
+        profile = {
+            "affiliation": "NAVY",
+            "edipi": "3333333333",
+            "rank": "E_5",
+            "social_security_number": "333-33-3333",
+        }
+        self.client.patch(url, json=profile)
+
+    @seq_task(5)
     def logout(self):
         self.client.post("/auth/logout")
-        self.csrf = None
-        self.user = None
+        self.login_gov_user = None
         self.token = None
-
-    @task(1)
-    def index(self):
-        self.client.get("/")
+        self.user = {}
 
 
 class MilMoveUser(HttpLocust):
