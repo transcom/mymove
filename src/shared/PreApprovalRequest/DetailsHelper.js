d9f7be19d78f095a7822637955ec80fcfd139526
Isaac Garfinkle <isaac@truss.works>
2019-02-18 22:42:03 +0000

    Allow backwards compatibility, store properly in db.

--- src/shared/PreApprovalRequest/DetailsHelper.js
+++ src/shared/PreApprovalRequest/DetailsHelper.js
@@ -1,9 +1,11 @@
 import { DefaultDetails } from './DefaultDetails';
 import { Code105Details } from './Code105Details';
+import { has } from 'lodash';
 
 export function getDetailComponent(code, robustAccessorial, initialValues) {
   code = code ? code.toLowerCase() : '';
   if (initialValues && !initialValues.crate_dimensions) return DefaultDetails;
-  if ((code && code.startsWith('105b')) || (code.startsWith('105e') && robustAccessorial)) return Code105Details;
+  const hasDimensions = has(initialValues, 'crate_dimensions');
+  if ((code.startsWith('105b') || code.startsWith('105e')) && hasDimensions && robustAccessorial) return Code105Details;
   return DefaultDetails;
 }
--- src/shared/PreApprovalRequest/DetailsHelper.test.js
+++ src/shared/PreApprovalRequest/DetailsHelper.test.js
@@ -3,6 +3,8 @@ import { DefaultDetails } from './DefaultDetails';
 import { Code105Details } from './Code105Details';
 
 let featureFlag = false;
+let initialValuesWithoutCrateDimensions = {};
+let initialValuesWithCrateDimensions = { crate_dimensions: true };
 describe('testing getDetailComponent()', () => {
   describe('returns default details component', () => {
     const DetailComponent = getDetailComponent();
@@ -16,28 +18,28 @@ describe('testing getDetailComponent()', () => {
     //pass in known code item with feature flag off
     featureFlag = false;
 
-    let DetailComponent = getDetailComponent('105', featureFlag);
+    let DetailComponent = getDetailComponent('105', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
 
-    DetailComponent = getDetailComponent('105B', featureFlag);
+    DetailComponent = getDetailComponent('105B', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105B', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
 
-    DetailComponent = getDetailComponent('105E', featureFlag);
+    DetailComponent = getDetailComponent('105E', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105E', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
 
     //testing for non-existing code
-    DetailComponent = getDetailComponent('4A', featureFlag);
+    DetailComponent = getDetailComponent('4A', featureFlag, initialValuesWithCrateDimensions);
     it('for code 4A', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
 
-    DetailComponent = getDetailComponent('105D', featureFlag);
+    DetailComponent = getDetailComponent('105D', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105D', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
@@ -46,17 +48,17 @@ describe('testing getDetailComponent()', () => {
   describe('returns 105B/E details component with feature flag on', () => {
     featureFlag = true;
 
-    let DetailComponent = getDetailComponent('105', featureFlag);
+    let DetailComponent = getDetailComponent('105', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105', () => {
       expect(DetailComponent).toBe(Code105Details);
     });
 
-    DetailComponent = getDetailComponent('105B', featureFlag);
+    DetailComponent = getDetailComponent('105B', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105B', () => {
       expect(DetailComponent).toBe(Code105Details);
     });
 
-    DetailComponent = getDetailComponent('105E', featureFlag);
+    DetailComponent = getDetailComponent('105E', featureFlag, initialValuesWithCrateDimensions);
     it('for code 105E', () => {
       expect(DetailComponent).toBe(Code105Details);
     });
@@ -67,6 +69,16 @@ describe('testing getDetailComponent()', () => {
 
     let DetailComponent = getDetailComponent('105D', featureFlag);
     it('for code 105D', () => {
+      expect(DetailComponent).toBe(DefaultDetails);
+    });
+
+    DetailComponent = getDetailComponent('105B', featureFlag, initialValuesWithoutCrateDimensions);
+    it('for code 105B without crate dimensions', () => {
+      expect(DetailComponent).toBe(DefaultDetails);
+    });
+
+    DetailComponent = getDetailComponent('105E', featureFlag, initialValuesWithoutCrateDimensions);
+    it('for code 105E without crate dimensions', () => {
       expect(DetailComponent).toBe(DefaultDetails);
     });
   });
