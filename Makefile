3ba520e9b172c4279400eec63293910d677c5454
Jim Benton <jim@truss.works>
2019-02-19 19:25:49 +0000

    Clone database before each suite run.

--- Makefile
+++ Makefile
@@ -214,9 +214,8 @@ endif
 
 server_test: server_deps server_generate db_test_reset db_test_migrate
 	# Don't run tests in /cmd or /pkg/gen & pass `-short` to exclude long running tests
-	# Use -test.parallel 1 to test packages serially and avoid database collisions
 	# Disable test caching with `-count 1` - caching was masking local test failures
-	go test -p 1 -count 1 -short $$(go list ./... | grep -v \\/pkg\\/gen\\/ | grep -v \\/cmd\\/)
+	go test -count 1 -short $$(go list ./... | grep -v \\/pkg\\/gen\\/ | grep -v \\/cmd\\/)
 
 server_test_all: server_deps server_generate db_dev_reset db_dev_migrate
 	# Like server_test but runs extended tests that may hit external services.
--- pkg/testingsuite/pop_suite.go
+++ pkg/testingsuite/pop_suite.go
@@ -2,34 +2,105 @@ package testingsuite
 
 import (
 	"fmt"
-	"log"
-	"path/filepath"
-	"runtime"
-
+	"github.com/codegangsta/envy/lib"
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/validate"
+	"github.com/pkg/errors"
+	"log"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 // PopTestSuite is a suite for testing
 type PopTestSuite struct {
 	BaseTestSuite
-	db *pop.Connection
+	PackageName string
+	db          *pop.Connection
+}
+
+func cloneDatabase(source, destination string) error {
+	// #nosec G204
+	drop := exec.Command("dropdb", "-U", "postgres", "-h", "localhost", "--if-exists", destination)
+
+	if op, err := drop.CombinedOutput(); err != nil {
+		return errors.Wrapf(err, "failed to drop the database %s: %s", destination, op)
+	}
+	// #nosec G204
+	create := exec.Command("createdb", "-U", "postgres", "-h", "localhost", destination)
+
+	if op, err := create.CombinedOutput(); err != nil {
+		return errors.Wrapf(err, "failed to create the database %s: %s", destination, op)
+	}
+
+	// #nosec G204
+	dump := exec.Command("pg_dump", "-U", "postgres", "-h", "localhost", "-F", "c", source)
+	out, err := dump.StdoutPipe()
+	if err != nil {
+		return errors.Wrapf(err, "failed to dump the database %s", source)
+	}
+
+	// #nosec G204
+	restore := exec.Command("pg_restore", "-U", "postgres", "-h", "localhost", "-d", destination)
+	restore.Stdin = out
+
+	err = dump.Start()
+	if err != nil {
+		return errors.Wrap(err, "failed to run the dump cmd")
+	}
+
+	if op, err := restore.CombinedOutput(); err != nil {
+		return errors.Wrapf(err, "failed to start the restore cmd: %s", op)
+	}
+
+	err = dump.Wait()
+	if err != nil {
+		return errors.Wrap(err, "failed to wait for restore cmd")
+	}
+
+	return nil
 }
 
 // NewPopTestSuite returns a new PopTestSuite
 func NewPopTestSuite() PopTestSuite {
-	// Find root path of testingsuite package
-	_, b, _, _ := runtime.Caller(0)
-	basepath := filepath.Dir(b)
+	//pop.Debug = true
 
-	configLocation := filepath.Join(basepath, "../../config")
-	pop.AddLookupPaths(configLocation)
-	db, err := pop.Connect("test")
+	// Find out what package is calling this function, and use that to build the database name.
+	pc, _, _, _ := runtime.Caller(1)
+	caller := runtime.FuncForPC(pc)
+
+	fnName := strings.Replace(caller.Name(), "github.com/transcom/mymove/pkg/", "", 1)
+	pkg := strings.Split(fnName, ".")[0]
+	dbName := fmt.Sprintf("test_%s", strings.Replace(pkg, "/", "_", -1))
+
+	if err := cloneDatabase("test_db", dbName); err != nil {
+		log.Panicf("failed to clone database\n%#v", err)
+	}
+
+	db, err := pop.NewConnection(&pop.ConnectionDetails{
+		Dialect:  "postgres",
+		Database: dbName,
+		Host:     envy.MustGet("DB_HOST"),
+		Port:     envy.MustGet("DB_PORT"),
+		User:     envy.MustGet("DB_USER"),
+		Password: envy.MustGet("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	err = db.Open()
 	if err != nil {
 		log.Panic(err)
 	}
 
-	return PopTestSuite{db: db}
+	fmt.Println(dbName)
+
+	err = db.RawQuery("SELECT 1;").Exec()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return PopTestSuite{db: db, PackageName: pkg}
 }
 
 // DB returns a db connection
