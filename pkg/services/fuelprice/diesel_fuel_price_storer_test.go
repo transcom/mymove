82ee2eb727ff1da6a6bf613e6f1ede33f0e87388
Kim Allen <kim@truss.works>
2019-02-21 00:56:07 +0000

    Fix query using date_part; adjust years for currentDate and shipmentDate; remove unnecessary destroy

--- pkg/services/fuelprice/diesel_fuel_price_storer_test.go
+++ pkg/services/fuelprice/diesel_fuel_price_storer_test.go
@@ -41,38 +41,31 @@ func TestFuelPriceSuite(t *testing.T) {
 
 func (suite *FuelPriceServiceSuite) TestStoreFuelPrices() {
 	clock := clock.NewMock()
-	currentDate := clock.Now().AddDate(49, 0, 0)
+	currentDate := clock.Now().AddDate(40, 0, 0)
 	// create fuel prices in db for last 15 months
 	for month := 0; month < 15; month++ {
 		var shipmentDate time.Time
-
-		shipmentDate = currentDate.AddDate(40, -(month - 1), 0)
+		shipmentDate = currentDate.AddDate(0, -(month - 1), 0)
 		testdatagen.MakeDefaultFuelEIADieselPriceForDate(suite.DB(), shipmentDate)
 	}
 	// remove this month's data
 	fuelEIADeiselPrices := []models.FuelEIADieselPrice{}
 	queryForThisMonth := suite.DB().RawQuery(
-		"SELECT * FROM fuel_eia_diesel_prices WHERE (DATEPART(year, pub_date) = $1"+
-			"AND DATEPART(month, pub_date = $2))", currentDate.Year(), currentDate.Month())
+		"SELECT * FROM fuel_eia_diesel_prices WHERE (date_part('year', pub_date) = $1 "+
+			"AND date_part('month', pub_date) = $2)", currentDate.Year(), int(currentDate.Month()))
 	err := queryForThisMonth.All(&fuelEIADeiselPrices)
 	if err != nil {
 		suite.logger.Error(err.Error())
 	}
 
-	err = suite.DB().Destroy(&fuelEIADeiselPrices)
-	if err != nil {
-		suite.logger.Error("Error deleting eia diesel price", zap.Error(err))
-	}
-
 	//remove a different month's data (not next to this month)
 	queryForPriorMonth := suite.DB().RawQuery(
-		"SELECT * FROM fuel_eia_diesel_prices WHERE (DATEPART(yy, pub_date) = $1"+
-			"AND DATEPART(mm, pub_date = $2))", currentDate.AddDate(0, -5, 0).Year(), currentDate.AddDate(0, -5, 0).Month())
+		"SELECT * FROM fuel_eia_diesel_prices WHERE (date_part('year', pub_date) = $1 "+
+			"AND date_part('month', pub_date) = $2)", currentDate.AddDate(0, -5, 0).Year(), int(currentDate.AddDate(0, -5, 0).Month()))
 	err = queryForPriorMonth.All(&fuelEIADeiselPrices)
 	if err != nil {
 		suite.logger.Error(err.Error())
 	}
-
 	err = suite.DB().Destroy(&fuelEIADeiselPrices)
 	if err != nil {
 		suite.logger.Error("Error deleting eia diesel price", zap.Error(err))
@@ -85,13 +78,13 @@ func (suite *FuelPriceServiceSuite) TestStoreFuelPrices() {
 	suite.Empty(verrs.Errors, "validation error when creating diesel prices")
 
 	// check that the last twelve months have fuel data
-
+	resultingFuelEIADeiselPrices := []models.FuelEIADieselPrice{}
 	// check that the records are added back in for the months removed
-	err = queryForThisMonth.All(&fuelEIADeiselPrices)
+	err = queryForThisMonth.All(&resultingFuelEIADeiselPrices)
 	if err != nil {
 		suite.logger.Error(err.Error())
 	}
-	suite.NotEmpty(&fuelEIADeiselPrices)
+	suite.NotEmpty(&resultingFuelEIADeiselPrices)
 
 	err = queryForPriorMonth.All(&fuelEIADeiselPrices)
 	if err != nil {
