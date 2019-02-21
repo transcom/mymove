873d8563c37fd3664c465bda624cc2515fe27562
Isaac Garfinkle <isaac@truss.works>
2019-02-21 01:26:41 +0000

    Fix issue with new PARs not using dimensions form.

--- src/shared/PreApprovalRequest/DetailsHelper.js
+++ src/shared/PreApprovalRequest/DetailsHelper.js
@@ -4,8 +4,7 @@ import { has } from 'lodash';
 
 export function getDetailComponent(code, robustAccessorial, initialValues) {
   code = code ? code.toLowerCase() : '';
-  if (initialValues && !initialValues.crate_dimensions) return DefaultDetails;
-  const hasDimensions = has(initialValues, 'crate_dimensions');
+  const hasDimensions = !initialValues || has(initialValues, 'crate_dimensions');
   if ((code.startsWith('105b') || code.startsWith('105e')) && hasDimensions && robustAccessorial) return Code105Details;
   return DefaultDetails;
 }
