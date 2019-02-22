88436eb0c9cf882dbd48a890272db67544cc2a69
Rupinder Dhariwal <rd@truss.works>
2019-02-22 22:19:16 +0000

    refactor the logic that checks which 105 item view to render when a pre approval request is edited or created. Took out has and replaced it with get call becuase has was checking for property instead of value.

--- src/shared/PreApprovalRequest/DetailsHelper.js
+++ src/shared/PreApprovalRequest/DetailsHelper.js
@@ -1,13 +1,16 @@
 import { DefaultForm } from './DefaultForm';
 import { Code105Form } from './Code105Form';
-import { has } from 'lodash';
+import { get } from 'lodash';
 import { Code105Details } from './Code105Details';
 import { DefaultDetails } from './DefaultDetails';
 
 export function getFormComponent(code, robustAccessorial, initialValues) {
   code = code ? code.toLowerCase() : '';
-  const hasDimensions = !initialValues || has(initialValues, 'crate_dimensions');
-  if ((code.startsWith('105b') || code.startsWith('105e')) && hasDimensions && robustAccessorial) return Code105Form;
+  const hasCrateDimensions = get(initialValues, 'crate_dimensions', false);
+  const isNew = !initialValues;
+  if (robustAccessorial && (code.startsWith('105b') || code.startsWith('105e'))) {
+    if (isNew || hasCrateDimensions) return Code105Form;
+  }
   return DefaultForm;
 }
 
