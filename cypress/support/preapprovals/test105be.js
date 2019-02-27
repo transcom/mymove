dd79b8d041055416f683c90c810db27efe78bcea
lindsay t <techells22@gmail.com>
2019-02-27 15:26:37 +0000

    refactor 105be helper functions

--- cypress/support/preapprovals/test105be.js
+++ cypress/support/preapprovals/test105be.js
@@ -1,68 +1,61 @@
 /* global cy */
-export function addOrigional105b() {
-  cy
-    .get('.add-request')
-    .contains('Add a request')
-    .click();
+export function addOrigional105be() {
+  clickAddPAR();
+  cy.selectTariff400ngItem('105B');
+  enterInput('quantity_1', 12);
+  enterTextarea('notes', 'notes notes 105B');
+  clickSaveButtonAndWait();
 
-  cy.selectTariff400ngItem('Pack Reg Crate');
-
-  cy
-    .get('input[name="quantity_1"]')
-    .first()
-    .type('12', { force: true });
-
-  cy
-    .get('textarea[name="notes"]')
-    .first()
-    .type('notes notes', { force: true });
-
-  cy
-    .get('button')
-    .contains('Save & Close')
-    .click();
+  clickAddPAR();
+  cy.selectTariff400ngItem('105E');
+  enterInput('quantity_1', 90);
+  enterTextarea('notes', 'notes notes 105E');
+  clickSaveButtonAndWait();
 }
 
-export function add105b() {
+export function add105be() {
+  clickAddPAR();
+  cy.selectTariff400ngItem('105B');
+  enterTextarea('description', 'description description 105B');
+  addDimensions('item', 30);
+  addDimensions('crate');
+  enterTextarea('notes', 'notes notes');
+  clickSaveButtonAndWait();
+
+  clickAddPAR();
+  cy.selectTariff400ngItem('105E');
+  enterTextarea('description', 'description description 105E');
+  addDimensions('item', 40);
+  addDimensions('crate', 50);
+  enterTextarea('notes', 'notes notes');
+  clickSaveButtonAndWait();
+}
+
+function clickAddPAR() {
   cy
     .get('.add-request')
     .contains('Add a request')
-    .click();
-
-  cy.selectTariff400ngItem('105B');
-
-  cy
-    .get('textarea[name="description"]')
-    .first()
-    .type('description description', { force: true });
-
-  addDimensions('item', 30);
-  addDimensions('crate');
-
-  cy
-    .get('textarea[name="notes"]')
-    .first()
-    .type('notes notes', { force: true });
-
-  cy
-    .get('button')
-    .contains('Save & Close')
     .click();
 }
 
 function addDimensions(type, value = 25) {
-  cy
-    .get(`input[name="${type}_dimensions.length"]`)
-    .first()
-    .type(value, { force: true });
+  enterInput(`${type}_dimensions.length`, value);
+  enterInput(`${type}_dimensions.width`, value);
+  enterInput(`${type}_dimensions.height`, value);
+}
 
-  cy
-    .get(`input[name="${type}_dimensions.width"]`)
-    .first()
-    .type(value, { force: true });
+function enterTextarea(type, value) {
+  cy.get(`textarea[name="${type}"]`).type(value, { force: true });
+}
 
+function enterInput(type, value) {
+  cy.get(`input[name="${type}"]`).type(value, { force: true });
+}
+
+function clickSaveButtonAndWait() {
   cy
-    .get(`input[name="${type}_dimensions.height"]`)
-    .first()
-    .type(value, { force: true });
+    .get('button')
+    .contains('Save & Close')
+    .click();
+  cy.wait('@accessorialsCheck');
 }
