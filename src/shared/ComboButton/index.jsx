d238d433939ec2d31bb1334439641919c54d8634
Matt Krump <matt@truss.works>
2019-02-28 20:21:46 +0000

    Only display tooltip when there is text

--- src/shared/ComboButton/index.jsx
+++ src/shared/ComboButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './index.css';
@@ -6,15 +6,13 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faCaretDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
 
 const ComboButton = ({ toolTipText, isDisabled }) => (
-  <Fragment>
-    <span className="button-tooltip tooltip">
-      <button disabled={isDisabled}>
-        Approve&nbsp;&nbsp;&nbsp;
-        <FontAwesomeIcon className="icon" icon={faCaretDown} />
-      </button>
-      <span className="tooltiptext">{toolTipText}</span>
-    </span>
-  </Fragment>
+  <span className="combo-button button-tooltip tooltip">
+    <button disabled={isDisabled}>
+      Approve
+      <FontAwesomeIcon className="combo-button-icon" icon={faCaretDown} />
+    </button>
+    {toolTipText && <span className="tooltiptext">{toolTipText}</span>}
+  </span>
 );
 
 ComboButton.propTypes = {
