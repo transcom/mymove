f6843c27573f19962709ca2dae70a2c269a357f0
Aileen Chen <aileen@dds.mil>
2019-03-05 17:17:41 +0000

    Remove dupe local migration records

--- local_migrations/20190304203050_mar4_dps_users.sql
+++ local_migrations/20190304203050_mar4_dps_users.sql
@@ -3,8 +3,3 @@
 -- intend to apply on production, but do not include any sensitive data.
 
 INSERT INTO public.dps_users VALUES ('f408b4fc-33db-4920-9118-52a5707af73b', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
-INSERT INTO public.dps_users VALUES ('18d0049c-82d1-4a5b-b0fe-a962ddb481a6', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
-INSERT INTO public.dps_users VALUES ('065d4f48-63f3-4e03-afe2-4af769d76d6a', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
-INSERT INTO public.dps_users VALUES ('c9aa6620-cc61-46d7-a2a5-a52312bb422f', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
-INSERT INTO public.dps_users VALUES ('54728067-edb4-4877-b679-011df41a2ace', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
-INSERT INTO public.dps_users VALUES ('926af5d7-86c4-4dc4-bbfa-ac2ea3064a91', 'test@example.com', '2019-03-04 20:37:01.943686', '2019-03-04 20:37:01.943686');
