2526428b12cf1d619dc6a9e23d20e7c4c6852009
Donald Thai <donald@truss.works>
2019-03-13 17:00:12 -0700

    Removed bold element

--- src/shared/JsonSchemaForm/DimensionsField.jsx
+++ src/shared/JsonSchemaForm/DimensionsField.jsx
@@ -9,7 +9,7 @@ export class DimensionsField extends Component {
     return (
       <FormSection name={this.props.fieldName}>
         <label htmlFor={this.props.fieldName} className="usa-input-label">
-          <b>{this.props.labelText}</b>
+          {this.props.labelText}
         </label>
         <table className="dimensions-form">
           <tbody>
