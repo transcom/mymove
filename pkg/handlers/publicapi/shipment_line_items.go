4bd0e9a24136879bfddb8607e8cda14db558c7ac
Donald Thai <donald@truss.works>
2019-03-19 16:46:32 -0700

    Added comment about 35A special functionality

--- pkg/handlers/publicapi/shipment_line_items.go
+++ pkg/handlers/publicapi/shipment_line_items.go
@@ -316,6 +316,7 @@ func (h UpdateShipmentLineItemHandler) Handle(params accessorialop.UpdateShipmen
 	tariff400ngItemID := uuid.Must(uuid.FromString(params.Payload.Tariff400ngItemID.String()))
 	tariff400ngItem, err := models.FetchTariff400ngItem(h.DB(), tariff400ngItemID)
 	shipment := shipmentLineItem.Shipment
+	// 35A has special functionality to update ActualAmountCents if it is not filled out and state is approved
 	canUpdate35A := tariff400ngItem.Code == "35A" && shipmentLineItem.ActualAmountCents == nil
 
 	if !tariff400ngItem.RequiresPreApproval {
