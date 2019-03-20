954d0a360de5778997fe00360a7a81bf5de1903f
Donald Thai <donald@truss.works>
2019-03-20 12:30:06 -0700

    Refactored 35 details to format actual amount text

--- src/shared/PreApprovalRequest/Code35Details.jsx
+++ src/shared/PreApprovalRequest/Code35Details.jsx
@@ -3,12 +3,14 @@ import { formatCents } from 'shared/formatters';
 
 export const Code35Details = props => {
   const row = props.shipmentLineItem;
+  const actAmtValue = formatCents(row.actual_amount_cents);
+  const actAmtText = actAmtValue ? '$' + actAmtValue : '--';
   return (
     <td details-cy={`${row.tariff400ng_item.code}-details`}>
       {row.description} <br />
       {row.reason} <br />
       Est. not to exceed: ${formatCents(row.estimate_amount_cents)} <br />
-      Actual Amount: ${`${formatCents(row.actual_amount_cents)}` || `--`} <br />
+      Actual Amount: {actAmtText} <br />
       {row.notes}
     </td>
   );
