b1391314852ad83bba95b12336ac0d562678f140
Donald Thai <donald@truss.works>
2019-03-20 15:15:47 -0700

    Refactored to use string interpolation

--- src/shared/PreApprovalRequest/Code35Details.jsx
+++ src/shared/PreApprovalRequest/Code35Details.jsx
@@ -4,13 +4,12 @@ import { formatCents } from 'shared/formatters';
 export const Code35Details = props => {
   const row = props.shipmentLineItem;
   const actAmtValue = formatCents(row.actual_amount_cents);
-  const actAmtText = actAmtValue ? '$' + actAmtValue : '--';
   return (
     <td details-cy={`${row.tariff400ng_item.code}-details`}>
       {row.description} <br />
       {row.reason} <br />
       Est. not to exceed: ${formatCents(row.estimate_amount_cents)} <br />
-      Actual amount: {actAmtText} <br />
+      Actual amount: {`${actAmtValue ? `$${actAmtValue}` : `--`}`} <br />
       {row.notes}
     </td>
   );
