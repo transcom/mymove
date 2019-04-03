2a4cc8e1bedcc7156600149f091423ce8b062ec2
lindsay t <techells22@gmail.com>
2019-04-03 20:16:35 +0000

    update testing move records show flag

---
+++ local_migrations/20190403150700_update-moves-show-flag.sql
@@ -0,0 +1,5 @@
+-- Local test migration.
+-- This will be run on development environments. It should mirror what you
+-- intend to apply on production, but do not include any sensitive data.
+
+-- * Leaving empty- modifying production moves show status not needed on development *
