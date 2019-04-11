eb90777761e76044c7ef47e54417543d81d6b09d
Mikena Wood <mikena@truss.works>
2019-04-11 17:14:41 +0000

    remove scac constraint

--- migrations/20190408234448_add_unique_constraint_on_scac.up.sql
+++
@@ -1,4 +0,0 @@
--- Make standard_carrier_alpha_code on transportation_service_providers unique --
-
-ALTER TABLE transportation_service_providers
-  ADD CONSTRAINT unique_standard_carrier_alpha_code UNIQUE (standard_carrier_alpha_code);
