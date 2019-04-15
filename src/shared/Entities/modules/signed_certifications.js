83c88a7956c7850e4bd2a749370c82d1e2943ff3
Matt Krump <matt@truss.works>
2019-04-15 15:46:07 +0000

    Disable shipment summary worksheet button when req data is missing

--- src/shared/Entities/modules/signed_certifications.js
+++ src/shared/Entities/modules/signed_certifications.js
@@ -1,11 +1,14 @@
 import { swaggerRequest } from 'shared/Swagger/request';
 import { getClient } from 'shared/Swagger/api';
+import { filter } from 'lodash';
 
-export const Label = 'SignedCertifications.createSignedCertification';
+export const createSignedCertificationLabel = 'SignedCertifications.createSignedCertification';
+export const getSignedCertificationsLabel = 'SignedCertifications.indexSignedCertifications';
 
 export function createSignedCertification(
   moveId,
   payload /*shape: {personally_procured_move_id, shipment_id, certification_text, signature, date, certification_type}*/,
+  label = createSignedCertificationLabel,
 ) {
   return swaggerRequest(
     getClient,
@@ -16,6 +19,20 @@ export function createSignedCertification(
         ...payload,
       },
     },
-    { Label },
+    { label },
   );
+}
+
+export function getSignedCertification(moveId, label = getSignedCertificationsLabel) {
+  return swaggerRequest(getClient, 'certification.indexSignedCertification', { moveId }, { label });
+}
+
+export function selectPaymentRequestCertificationForMove(state, moveId) {
+  const signedCertifications = filter(state.entities.signedCertifications, cert => {
+    return cert.certification_type === 'PPM_PAYMENT' && cert.move_id === moveId;
+  });
+  if (!Array.isArray(signedCertifications) || !signedCertifications.length) {
+    return {};
+  }
+  return signedCertifications[0];
 }
