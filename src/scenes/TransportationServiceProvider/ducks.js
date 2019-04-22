b09c94e096db9f75e44c9444b7480f400d591e88
Sara Bocciardi <sara@truss.works>
2019-04-22 21:56:46 +0000

    We can probably just kill this reducer now, but not quite yet...

--- src/scenes/TransportationServiceProvider/ducks.js
+++ src/scenes/TransportationServiceProvider/ducks.js
@@ -1,14 +1,6 @@
 import { isNull } from 'lodash';
-import * as ReduxHelpers from 'shared/ReduxHelpers';
 import { getEntitlements } from 'shared/entitlements.js';
 import { selectShipment } from 'shared/Entities/modules/shipments';
-
-// MULTIPLE-RESOURCE ACTION TYPES
-const loadTspDependenciesType = 'LOAD_TSP_DEPENDENCIES';
-
-// MULTIPLE-RESOURCE ACTION TYPES
-
-const LOAD_TSP_DEPENDENCIES = ReduxHelpers.generateAsyncActionTypes(loadTspDependenciesType);
 
 // SINGLE-RESOURCE ACTION CREATORS
 
@@ -33,37 +25,11 @@ const initialState = {
   storageInTransitsAreLoading: false,
   storageInTransitsHasLoadSuccess: false,
   storageInTransitsHasLoadError: null,
-  shipment: {},
-  loadTspDependenciesHasSuccess: false,
-  loadTspDependenciesHasError: null,
   flashMessage: false,
 };
 
 export function tspReducer(state = initialState, action) {
   switch (action.type) {
-    // MULTIPLE-RESOURCE ACTION TYPES
-    //
-    // These action types typically dispatch to other actions above to
-    // perform their work and exist to encapsulate when multiple requests
-    // need to be made in response to a user action.
-
-    // ALL TSP DEPENDENCIES
-    case LOAD_TSP_DEPENDENCIES.start:
-      return Object.assign({}, state, {
-        loadTspDependenciesHasSuccess: false,
-        loadTspDependenciesHasError: false,
-      });
-    case LOAD_TSP_DEPENDENCIES.success:
-      return Object.assign({}, state, {
-        loadTspDependenciesHasSuccess: true,
-        loadTspDependenciesHasError: false,
-      });
-    case LOAD_TSP_DEPENDENCIES.failure:
-      return Object.assign({}, state, {
-        loadTspDependenciesHasSuccess: false,
-        loadTspDependenciesHasError: true,
-        error: action.error.message,
-      });
     default:
       return state;
   }
