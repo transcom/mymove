8fb86530768f9870f03cb6b9694d8f7bc2ea10cd
Chris Gilmer <chris@truss.works>
2019-04-23 13:43:26 +0000

    Update ports tests

--- pkg/cli/ports_test.go
+++ pkg/cli/ports_test.go
@@ -1,6 +1,15 @@
 package cli
 
+import "github.com/spf13/pflag"
+
+func InitPorts(flag *pflag.FlagSet) {
+	InitAuthFlags(flag)
+	InitDPSFlags(flag)
+	InitDatabaseFlags(flag)
+	InitPortFlags(flag)
+}
+
 func (suite *cliTestSuite) TestConfigPorts() {
-	suite.Setup(InitPortFlags)
+	suite.Setup(InitPorts)
 	suite.Nil(CheckPorts(suite.viper))
 }
