b93a1fc325c22bd0e3f67646813e22b90b9fa0bc
Donald Thai <donald@truss.works>
2019-04-24 21:20:55 +0000

    new details component for item 125

---
+++ src/shared/PreApprovalRequest/Code125Details.jsx
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { formatDate } from 'shared/formatters';
+
+export const Code125Details = props => {
+  const row = props.shipmentLineItem;
+  return (
+    <td details-cy={`${row.tariff400ng_item.code}-details`}>
+      {row.reason} <br />
+      {`Date of service: ${formatDate(row.date)}`} <br />
+      {row.time && (
+        <Fragment>
+          {`Time of service: ${row.time}`} <br />
+        </Fragment>
+      )}
+      <br />
+      {row.address.street_address_1} <br />
+      {row.address.street_address_2 && (
+        <Fragment>
+          {row.address.street_address_2} <br />
+        </Fragment>
+      )}
+      {row.address.city}, {row.address.state} {row.address.postal_code} <br />
+    </td>
+  );
+};
