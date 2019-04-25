6676e531ff851be3311657f6ab74e8281053010a
Kim Allen <kim@truss.works>
2019-04-25 18:24:52 +0000

    Add and refactor integration tests for entering actual_start_date; start adding validation code

--- src/shared/StorageInTransit/PlaceInSit.jsx
+++ src/shared/StorageInTransit/PlaceInSit.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { capitalize } from 'lodash';
 import { connect } from 'react-redux';
+import { isValid, isSubmitting } from 'redux-form';
 import { bindActionCreators } from 'redux';
 
 import { formatDate4DigitYear } from 'shared/formatters';
 import PlaceInSitForm from './PlaceInSitForm.jsx';
+import { updateSitPlaceIntoSit } from 'shared/Entities/modules/storageInTransits';
+import { formName as PlaceInSitFormName } from 'shared/StorageInTransit/PlaceInSitForm.jsx';
 import './StorageInTransit.css';
 
 export class PlaceInSit extends Component {
@@ -39,17 +42,18 @@ export class PlaceInSit extends Component {
         <div className="usa-grid-full align-center-vertical">
           <div className="usa-width-one-half">
             <p className="cancel-link">
-              <a className="usa-button-secondary" onClick={this.closeForm}>
+              <a className="usa-button-secondary" data-cy="place-into-sit-cancel" onClick={this.closeForm}>
                 Cancel
               </a>
             </p>
           </div>
           <div className="usa-width-one-half align-right">
             <button
-              className="button usa-button-primary storage-in-transit-request-form-send-request-button"
+              className="button usa-button-primary"
+              data-cy="place-in-sit-button"
               disabled={!this.props.formEnabled}
             >
-              Approve
+              Place Into SIT
             </button>
           </div>
         </div>
@@ -58,14 +62,17 @@ export class PlaceInSit extends Component {
   }
 }
 
-PlaceInSit.propTypes = { sit: PropTypes.object.isRequired };
+PlaceInSit.propTypes = {
+  sit: PropTypes.object.isRequired,
+  formEnabled: PropTypes.bool.isRequired,
+};
 
 function mapStateToProps(state) {
-  return {};
+  return { formEnabled: isValid(PlaceInSitFormName)(state) && !isSubmitting(PlaceInSitFormName)(state) };
 }
 function mapDispatchToProps(dispatch) {
   // Bind an action, which submit the form by its name
-  return bindActionCreators({}, dispatch);
+  return bindActionCreators({ updateSitPlaceIntoSit }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlaceInSit);
