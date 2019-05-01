437e06009cf922b839104b1cba5626eaa4ade44c
Chris Gilmer <chris@truss.works>
2019-05-01 18:41:11 +0000

    Update the demo to check all apps

--- load_testing/demo.py
+++ load_testing/demo.py
@@ -7,8 +7,10 @@ import requests
 
 class Demo(object):
 
-    def __init__(self, url):
+    def __init__(self, url, user_type="milmove", session_token_name="mil_session_token"):
         self.session = requests.Session()
+        self.user_type = user_type
+        self.session_token_name = session_token_name
         self.url = url
         self.csrf = None
         self.user = None
@@ -32,17 +34,31 @@ class Demo(object):
         """
         resp = self.session.post(urllib.parse.urljoin(self.url, 'devlocal-auth/create'),
                                  headers={'x-csrf-token': self._get_csrf_token()},
-                                 data={"userType": "milmove"})
+                                 data={"userType": self.user_type})
         try:
             self.user = resp.json()
         except Exception:
             print(resp.content)
-        self.token = self.session.cookies.get('mil_session_token')
+        self.token = self.session.cookies.get(self.session_token_name)
+
+    def get_logged_in_user(self):
+        """
+        Get the logged in user information
+        """
+        resp = self.session.get(urllib.parse.urljoin(self.url, 'internal/users/logged_in'))
+        self.logged_in = resp.json()
 
 
 if __name__ == "__main__":
 
-    demo = Demo('http://milmovelocal:8080')
-    demo.create_user()
-    print('User', demo.user)
-    print('Token', demo.token)
+    milmove_demo = Demo('http://milmovelocal:8080', user_type="milmove", session_token_name="mil_session_token")
+    office_demo = Demo('http://officelocal:8080', user_type="office", session_token_name="office_session_token")
+    tsp_demo = Demo('http://tsplocal:8080', user_type="tsp", session_token_name="tsp_session_token")
+    for demo in [milmove_demo, office_demo, tsp_demo]:
+        print(demo.url)
+        demo.create_user()
+        print('User', demo.user)
+        print('Token', demo.token)
+        demo.get_logged_in_user()
+        print('LoggedIn', demo.logged_in)
+        print()
