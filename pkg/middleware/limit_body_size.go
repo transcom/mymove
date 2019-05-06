eaa0dd37dbf27723f2fc1b91abe6a4efceb1156d
pjdufour-truss <patrick.dufour@truss.works>
2019-05-06 17:09:04 +0000

    better debug

--- pkg/middleware/limit_body_size.go
+++ pkg/middleware/limit_body_size.go
@@ -2,11 +2,13 @@ package middleware
 
 import (
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
 // LimitBodySize is a middleware
 func LimitBodySize(maxBodySize int64, logger Logger) func(inner http.Handler) http.Handler {
-	logger.Debug("LimitBodySize Middleware used")
+	logger.Debug("LimitBodySize Middleware used", zap.Int64("max-body-size", maxBodySize))
 	return func(inner http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			r.Body = http.MaxBytesReader(w, r.Body, maxBodySize)
