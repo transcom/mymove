6fd93c9796e755f3f7c132fc427553decb71d73b
Donald Thai <donald@truss.works>
2019-05-08 17:24:40 +0000

    Re-arrange and rename hhg list

--- src/scenes/Office/QueueList.jsx
+++ src/scenes/Office/QueueList.jsx
@@ -18,20 +18,28 @@ export default class QueueList extends Component {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/queues/hhg_accepted" activeClassName="usa-current">
-              <span>Accepted HHGs</span>
+            <NavLink to="#hhgshipments" activeClassName="usa-current">
+              <span>HHG shipments:</span>
             </NavLink>
+            <ul class="usa-sidenav-sub_list">
+              <li>
+                <NavLink to="/queues/hhg_accepted" activeClassName="usa-current">
+                  <span>Accepted</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/queues/hhg_delivered" activeClassName="usa-current">
+                  <span>Delivered</span>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/queues/hhg_completed" activeClassName="usa-current">
+                  <span>Completed</span>
+                </NavLink>
+              </li>
+            </ul>
           </li>
-          <li>
-            <NavLink to="/queues/hhg_delivered" activeClassName="usa-current">
-              <span>Delivered HHGs</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/queues/hhg_completed" activeClassName="usa-current">
-              <span>Completed HHGs</span>
-            </NavLink>
-          </li>
+
           <li>
             <NavLink to="/queues/all" activeClassName="usa-current">
               <span>All Moves</span>
