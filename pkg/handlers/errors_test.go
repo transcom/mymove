a8a186e25e002b303a22e3f0d1e4237743f2601f
Rupinder Dhariwal <rd@truss.works>
2019-05-08 19:51:41 +0000

    refactor tests so error object is created organically from pop interactions

--- pkg/handlers/errors_test.go
+++ pkg/handlers/errors_test.go
@@ -6,13 +6,20 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 
-	"github.com/lib/pq"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 
+	"github.com/gofrs/uuid"
+
 	"github.com/transcom/mymove/pkg/logging/hnyzap"
+	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testingsuite"
 )
+
+type fakeModel struct {
+	ID   uuid.UUID
+	Name string
+}
 
 type ErrorsSuite struct {
 	testingsuite.PopTestSuite
@@ -37,27 +44,28 @@ func TestErrorsSuite(t *testing.T) {
 }
 
 func (suite *ErrorsSuite) TestResponseForErrorWhenASQLErrorIsEncountered() {
-	err := &pq.Error{}
 	var actual middleware.Responder
-	errTypes := []string{
-		"connection_exception",
-		"invalid_escape_character",
-		"integrity_constraint_violation",
-		"invalid_schema_name",
-		"foreign_key_violation",
-		"undefined_table",
-		"disk_full",
-		"too_many_columns",
-		"index_corrupted",
-		"invalid_transaction_state",
-	}
+	var signedCertification []*models.SignedCertification
+	var noTableModel []*fakeModel
 
-	for _, errT := range errTypes {
-		err.Message = errT
+	// invalid column
+	errInvalidColumn := suite.DB().Where("move_iid = $1", "123").All(&signedCertification)
+
+	// invalid arguments
+	errInvalidArguments := suite.DB().Where("id in (?) and foo = ?", 1, 2, 3, "bar").All(&signedCertification)
+
+	// invalid table
+	errNoTable := suite.DB().Where("1=1").First(noTableModel)
+
+	//slice to hold all errors and assert against
+	errs := []error{errInvalidColumn, errNoTable, errInvalidArguments}
+
+	for _, err := range errs {
 		actual = ResponseForError(suite.logger, err)
 		res, ok := actual.(*ErrResponse)
 		suite.True(ok)
 		suite.Equal(res.Code, 500)
 		suite.Equal(res.Err.Error(), SQLErrMessage)
 	}
+
 }
