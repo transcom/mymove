24646c0d702a9884610e81382cd0dd5033de688a
Ron Cruz <Ronolibert@gmail.com>
2019-05-09 20:10:16 +0000

    Add tests for claim_access_code

--- pkg/services/accesscode/claim_access_code.go
+++ pkg/services/accesscode/claim_access_code.go
@@ -1,8 +1,12 @@
 package accesscode
 
 import (
+	"log"
+	"time"
+
 	"github.com/gobuffalo/pop"
 	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
 
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
@@ -18,22 +22,51 @@ func NewAccessCodeClaimer(db *pop.Connection) services.AccessCodeClaimer {
 	return &claimAccessCode{db}
 }
 
-// ClaimAccessCode validates an access code based upon the code and move type. A valid access
-// code is assumed to have no `service_member_id`
-func (v claimAccessCode) ClaimAccessCode(code string, serviceMemberID uuid.UUID) (*models.AccessCode, error) {
+// FetchAccessCode gets an access code based upon the code given to determine whether or not it is a used code
+func (v claimAccessCode) FetchAccessCode(code string) (*models.AccessCode, error) {
 	ac := models.AccessCode{}
 
 	err := v.DB.
 		Where("code = ?", code).
 		First(&ac)
 
-	// service object for fetching an access code by code and put into a model
-
-	// service object for updating instance in the DB
-
 	if err != nil {
 		return &ac, err
 	}
 
 	return &ac, nil
+}
+
+// ClaimAccessCode validates an access code based upon the code and move type. A valid access
+// code is assumed to have no `service_member_id`
+func (v claimAccessCode) ClaimAccessCode(code string, serviceMemberID uuid.UUID) (*models.AccessCode, error) {
+	accessCode, err := v.FetchAccessCode(code)
+
+	if err != nil {
+		return &models.AccessCode{}, errors.Wrap(err, "Unable to find access code")
+	}
+
+	if accessCode.ServiceMemberID != nil {
+		log.Print(err)
+		return &models.AccessCode{}, errors.New("Access code already claimed")
+	}
+
+	transactionErr := v.DB.Transaction(func(connection *pop.Connection) error {
+		claimedAtTime := time.Now()
+		accessCode.ClaimedAt = &claimedAtTime
+		accessCode.ServiceMemberID = &serviceMemberID
+
+		verrs, err := connection.ValidateAndSave(accessCode)
+		if err != nil || verrs.HasAny() {
+			return errors.New("error claiming access code")
+		}
+
+		return nil
+	})
+
+	if transactionErr != nil {
+		return &models.AccessCode{}, errors.Wrap(transactionErr, "Unable to claim access code")
+	}
+
+	return accessCode, nil
 }
