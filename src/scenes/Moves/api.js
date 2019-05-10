a5b002b5001b926e83f22e01caf98e8face0018e
Donald Thai <donald@truss.works>
2019-05-10 14:51:59 -0700

    remove loadMove and updateMove from other files

--- src/scenes/Moves/api.js
+++ src/scenes/Moves/api.js
@@ -1,24 +1,5 @@
 import { getClient, checkResponse } from 'shared/Swagger/api';
 
-export async function GetMove(moveId) {
-  const client = await getClient();
-  const response = await client.apis.moves.showMove({
-    moveId,
-  });
-  checkResponse(response, 'failed to get move due to server error');
-  return response.body;
-}
-
-export async function UpdateMove(moveId, payload /*shape: { selected_move_type }*/) {
-  const client = await getClient();
-  const response = await client.apis.moves.patchMove({
-    moveId,
-    patchMovePayload: payload,
-  });
-  checkResponse(response, 'failed to update move due to server error');
-  return response.body;
-}
-
 export async function SubmitMoveForApproval(moveId, payload) {
   const client = await getClient();
   const response = await client.apis.moves.submitMoveForApproval({
