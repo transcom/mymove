f4d47a82dfb26d1725eeb8e6de00a50a482a25d0
Mikena Wood <mikena@truss.works>
2019-05-10 18:36:24 +0000

    add list tests

--- pkg/services/user/office_user_list_fetcher_test.go
+++ pkg/services/user/office_user_list_fetcher_test.go
@@ -1,26 +1,60 @@
 package user
 
 import (
-	"github.com/transcom/mymove/pkg/db/query"
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/gofrs/uuid"
+
 	"github.com/transcom/mymove/pkg/models"
-	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
+type testOfficeUserListQueryBuilder struct {
+	fakeFetchMany func(model interface{}) error
+}
+
+func (t *testOfficeUserListQueryBuilder) FetchMany(model interface{}, filters map[string]interface{}) error {
+	m := t.fakeFetchMany(model)
+	return m
+}
+
 func (suite *UserServiceSuite) TestFetchOfficeUserList() {
-	testdatagen.MakeDefaultOfficeUser(suite.DB())
-	email := "test@example.com"
-	assertions := testdatagen.Assertions{OfficeUser: models.OfficeUser{Email: email}}
-	user2 := testdatagen.MakeOfficeUser(suite.DB(), assertions)
-	builder := query.NewPopQueryBuilder(suite.DB())
-	fetcher := NewOfficeUserListFetcher(builder)
-	filters := map[string]interface{}{
-		"id": user2.ID,
-		"email": email,
-	}
+	suite.T().Run("if the user it fetched, it should be returned", func(t *testing.T){
+		id , err := uuid.NewV4()
+		suite.NoError(err)
+		fakeFetchMany := func(model interface{}) error {
+			value := reflect.ValueOf(model).Elem()
+			value.Set(reflect.Append(value, reflect.ValueOf(models.OfficeUser{ID: id})))
+			return nil
+		}
+		builder := &testOfficeUserListQueryBuilder{
+			fakeFetchMany: fakeFetchMany,
+		}
+		fetcher := NewOfficeUserListFetcher(builder)
+		filters := map[string]interface{}{
+			"id": id,
+		}
 
-	officeUsers, err := fetcher.FetchOfficeUserList(filters)
+		officeUsers, err := fetcher.FetchOfficeUserList(filters)
 
-	suite.NoError(err)
-	suite.Len(officeUsers, 1)
-	suite.Equal(officeUsers[0].Email, email)
+		suite.NoError(err)
+		suite.Equal(id, officeUsers[0].ID)
+	})
+
+	suite.T().Run("if there is an error, we get it with no office users", func(t *testing.T) {
+		fakeFetchMany := func(model interface{}) error {
+			return errors.New("Fetch error")
+		}
+		builder := &testOfficeUserListQueryBuilder{
+			fakeFetchMany: fakeFetchMany,
+		}
+		fetcher := NewOfficeUserListFetcher(builder)
+
+		officeUsers, err := fetcher.FetchOfficeUserList(map[string]interface{}{})
+
+		suite.Error(err)
+		suite.Equal(err.Error(), "Fetch error")
+		suite.Equal(models.OfficeUsers(nil), officeUsers)
+	})
 }
