37b020df36aeb1b52ef7d64d59217ddbde41a279
Ron Cruz <Ronolibert@gmail.com>
2019-05-20 16:47:01 +0000

    Add radio button component and the rest of the input fields

--- src/scenes/Moves/Ppm/WeightTicket.jsx
+++ src/scenes/Moves/Ppm/WeightTicket.jsx
@@ -3,6 +3,8 @@ import { reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
+import { uniqueId } from 'lodash';
+
 import PPMPaymentRequestActionBtns from './PPMPaymentRequestActionBtns';
 import WizardHeader from '../WizardHeader';
 import { ProgressTimeline, ProgressTimelineStep } from 'shared/ProgressTimeline';
@@ -13,11 +15,17 @@ import Uploader from 'shared/Uploader';
 
 class WeightTicket extends Component {
   state = {
-    value: '',
+    value: 'Yes',
   };
+
   labelIdle = 'Drag & drop or <span class="filepond--label-action">click to upload upload empty weight ticket</span>';
 
+  handleRadioChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
   render() {
+    const { value } = this.state;
     const { schema } = this.props;
     return (
       <Fragment>
@@ -39,12 +47,35 @@ class WeightTicket extends Component {
             value={this.state.value}
             required
           />
-          {this.state.value !== '' && (
-            <div className="uploader-box">
-              <Uploader options={{ allowMultiple: true, labelIdle: this.labelIdle }} />
-            </div>
-          )}
           <SwaggerField fieldName="vehicle_nickname" swagger={schema} required />
+          <div className="dashed-divider" />
+          <SwaggerField fieldName="empty_weight" swagger={schema} title="Empty Weight" required />
+          <Uploader options={{ labelIdle: this.labelIdle }} />
+          <SwaggerField fieldName="full_weight" swagger={schema} title="Full Weight" required />
+          <Uploader options={{ labelIdle: this.labelIdle }} />
+          <SwaggerField fieldName="weight_ticket_date" swagger={schema} required />
+          <div className="dashed-divider" />
+
+          <p className="radio-group-header">Do you have more weight tickets for another vehicle or trip?</p>
+          <RadioBtn
+            inputClassName="inline_radio"
+            labelClassName="inline_radio"
+            label="Yes"
+            value="Yes"
+            name="additional_weight_ticket"
+            checked={value === 'Yes'}
+            onChange={this.handleRadioChange}
+          />
+
+          <RadioBtn
+            inputClassName="inline_radio"
+            labelClassName="inline_radio"
+            label="No"
+            value="No"
+            name="additional_weight_ticket"
+            checked={value === 'No'}
+            onChange={this.handleRadioChange}
+          />
 
           {/* TODO: change onclick handler to go to next page in flow */}
           <PPMPaymentRequestActionBtns onClick={() => {}} nextBtnLabel="Save & Add Another" />
@@ -53,6 +84,25 @@ class WeightTicket extends Component {
     );
   }
 }
+const RadioBtn = ({ name, label, onChange, value, checked, inputClassName, labelClassName }) => {
+  const radioId = uniqueId(label);
+  return (
+    <>
+      <input
+        className={inputClassName}
+        id={radioId}
+        type="radio"
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChange}
+      />
+      <label className={labelClassName} htmlFor={radioId}>
+        {label}
+      </label>
+    </>
+  );
+};
 
 const formName = 'weight_ticket_wizard';
 WeightTicket = reduxForm({
