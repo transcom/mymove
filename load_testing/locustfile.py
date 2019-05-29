910232fb4262c501ce314f0665ed4e1f08ffa071
Chris Gilmer <chris@truss.works>
2019-05-29 22:53:15 +0000

    Local testing

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -7,16 +7,16 @@ from apps import TSPUserBehavior
 
 
 class AnonUser(HttpLocust):
-    # host = "http://milmovelocal:8080"
-    host = "https://my.experimental.move.mil"
+    host = "http://milmovelocal:8080"
+    # host = "https://my.experimental.move.mil"
     # weight = 5  # 5x more likely than other users
     weight = 1
     task_set = AnonBehavior
 
 
 class MilMoveUser(HttpLocust):
-    # host = "http://milmovelocal:8080"
-    host = "https://my.experimental.move.mil"
+    host = "http://milmovelocal:8080"
+    # host = "https://my.experimental.move.mil"
     weight = 1
     task_set = MilMoveUserBehavior
     min_wait = 1000
@@ -24,8 +24,8 @@ class MilMoveUser(HttpLocust):
 
 
 class OfficeUser(HttpLocust):
-    # host = "http://officelocal:8080"
-    host = "https://office.experimental.move.mil"
+    host = "http://officelocal:8080"
+    # host = "https://office.experimental.move.mil"
     weight = 1
     task_set = OfficeUserBehavior
     min_wait = 1000
@@ -33,8 +33,8 @@ class OfficeUser(HttpLocust):
 
 
 class TSPUser(HttpLocust):
-    # host = "http://tsplocal:8080"
-    host = "https://tsp.experimental.move.mil"
+    host = "http://tsplocal:8080"
+    # host = "https://tsp.experimental.move.mil"
     weight = 1
     task_set = TSPUserBehavior
     min_wait = 1000
