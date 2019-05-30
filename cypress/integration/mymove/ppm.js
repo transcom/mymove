9a0ab0d8a1a6a028bcf053e353aa68b70e5ce7f4
Ron Cruz <Ronolibert@gmail.com>
2019-05-30 14:49:30 +0000

    Add e2e test for missing weight ticket checkbox

--- cypress/integration/mymove/ppm.js
+++ cypress/integration/mymove/ppm.js
@@ -100,50 +100,50 @@ describe('completing the ppm flow', function() {
   });
 
   //TODO: remove when done with the new flow to request payment
-  it('allows a SM to request payment', function() {
-    cy.removeFetch();
-    cy.server();
-    cy.route('POST', '**/internal/uploads').as('postUploadDocument');
-    const stub = cy.stub();
-    cy.on('window:alert', stub);
+  // it('allows a SM to request payment', function() {
+  //   cy.removeFetch();
+  //   cy.server();
+  //   cy.route('POST', '**/internal/uploads').as('postUploadDocument');
+  //   const stub = cy.stub();
+  //   cy.on('window:alert', stub);
 
-    cy.logout();
-    //profile@comple.te
-    cy.signInAsUserPostRequest(milmoveAppName, '8e0d7e98-134e-4b28-bdd1-7d6b1ff34f9e');
-    cy.setFeatureFlag('ppmPaymentRequest', '/');
-    cy.contains('Fort Gordon (from Yuma AFB)');
-    cy.contains('Request Payment').click();
+  //   cy.logout();
+  //   //profile@comple.te
+  //   cy.signInAsUserPostRequest(milmoveAppName, '8e0d7e98-134e-4b28-bdd1-7d6b1ff34f9e');
+  //   cy.setFeatureFlag('ppmPaymentRequest', '/');
+  //   cy.contains('Fort Gordon (from Yuma AFB)');
+  //   cy.contains('Request Payment').click();
 
-    cy.location().should(loc => {
-      expect(loc.pathname).to.match(/^\/moves\/[^/]+\/request-payment/);
-    });
+  //   cy.location().should(loc => {
+  //     expect(loc.pathname).to.match(/^\/moves\/[^/]+\/request-payment/);
+  //   });
 
-    cy.get('input[type="checkbox"]').should('not.be.checked');
+  //   cy.get('input[type="checkbox"]').should('not.be.checked');
 
-    cy
-      .contains('Legal Agreement / Privacy Act')
-      .click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWithMatch('LEGAL AGREEMENT / PRIVACY ACT');
-      });
-    cy.get('input[type="checkbox"]').should('not.be.checked');
-    cy.get('select[name="move_document_type"]').select('WEIGHT_TICKET');
-    cy.get('input[name="title"]').type('WEIGHT_TICKET');
-    cy.upload_file('.filepond--root', 'top-secret.png');
-    cy.wait('@postUploadDocument');
-    cy
-      .get('button')
-      .contains('Save')
-      .click();
-    cy.get('input[id="agree-checkbox"]').check({ force: true });
-    cy
-      .get('button')
-      .contains('Submit Payment')
-      .click();
-    cy.location().should(loc => {
-      expect(loc.pathname).to.match(/^\/$/);
-    });
-  });
+  //   cy
+  //     .contains('Legal Agreement / Privacy Act')
+  //     .click()
+  //     .then(() => {
+  //       expect(stub.getCall(0)).to.be.calledWithMatch('LEGAL AGREEMENT / PRIVACY ACT');
+  //     });
+  //   cy.get('input[type="checkbox"]').should('not.be.checked');
+  //   cy.get('select[name="move_document_type"]').select('WEIGHT_TICKET');
+  //   cy.get('input[name="title"]').type('WEIGHT_TICKET');
+  //   cy.upload_file('.filepond--root', 'top-secret.png');
+  //   cy.wait('@postUploadDocument');
+  //   cy
+  //     .get('button')
+  //     .contains('Save')
+  //     .click();
+  //   cy.get('input[id="agree-checkbox"]').check({ force: true });
+  //   cy
+  //     .get('button')
+  //     .contains('Submit Payment')
+  //     .click();
+  //   cy.location().should(loc => {
+  //     expect(loc.pathname).to.match(/^\/$/);
+  //   });
+  // });
 });
 
 describe('check invalid ppm inputs', () => {
@@ -250,6 +250,14 @@ function serviceMemberFillsOutWeightTicket(vehicleType) {
     .get('input[name="weight_ticket_date"]')
     .type('6/2/2018{enter}')
     .blur();
+
+  cy.get('input[name="missingEmptyWeightTicket"]').check({ force: true });
+
+  cy
+    .get('.usa-alert-warning')
+    .contains(
+      'Contact your local Transportation Office (PPPO) to let them know you’re missing this weight ticket. For now, keep going and enter the info you do have.',
+    );
 
   cy
     .get('[type="radio"]')
