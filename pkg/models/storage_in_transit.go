51145db1fc5ac8b8107398d67cbbf85b76298bff
Kim Allen <kim@truss.works>
2019-05-31 18:52:08 +0000

    Order by location first so that Origin is first and then by estimated Start date

--- pkg/models/storage_in_transit.go
+++ pkg/models/storage_in_transit.go
@@ -106,7 +106,7 @@ func (s *StorageInTransit) Validate(tx *pop.Connection) (*validate.Errors, error
 func FetchStorageInTransitsOnShipment(tx *pop.Connection, shipmentID uuid.UUID) (StorageInTransits, error) {
 	storageInTransits := StorageInTransits{}
 
-	err := tx.Eager("WarehouseAddress").Where("shipment_id = $1", shipmentID).Order("actual_start_date").All(&storageInTransits)
+	err := tx.Eager("WarehouseAddress").Where("shipment_id = $1", shipmentID).Order("location desc").Order("estimated_start_date").All(&storageInTransits)
 
 	if err != nil {
 		return nil, err
