d4ad7f2efe5f5aea4aa230bba96d4fbf6d4934a6
Chris Gilmer <chris@truss.works>
2019-06-07 17:57:58 +0000

    Remove test that was left over after rebase

--- pkg/auth/authentication/devlocal_test.go
+++ pkg/auth/authentication/devlocal_test.go
@@ -10,10 +10,6 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/gofrs/uuid"
-	"github.com/pkg/errors"
-
-	"github.com/transcom/mymove/pkg/auth"
 	"github.com/transcom/mymove/pkg/models"
 )
 
@@ -651,38 +647,4 @@ func (suite *AuthSuite) TestCreateAndLoginUserHandlerFromAdminToTsp() {
 	}
 
 	suite.Equal(rr.Result().Header.Get("Location"), "http://tsp.example.com:1234/")
-}
-
-func (suite *AuthSuite) TestCreateAndLoginUserHandler() {
-	t := suite.T()
-
-	fakeToken := "some_token"
-	fakeUUID, _ := uuid.FromString("39b28c92-0506-4bef-8b57-e39519f42dc2")
-	callbackPort := 1234
-
-	req := httptest.NewRequest("POST", fmt.Sprintf("http://%s/devlocal-auth/new", MilTestHost), nil)
-	session := auth.Session{
-		ApplicationName: auth.MilApp,
-		IDToken:         fakeToken,
-		UserID:          fakeUUID,
-		Hostname:        MilTestHost,
-	}
-	ctx := auth.SetSessionInRequestContext(req, &session)
-	req = req.WithContext(ctx)
-
-	authContext := NewAuthContext(suite.logger, fakeLoginGovProvider(suite.logger), "http", callbackPort)
-	handler := NewCreateAndLoginUserHandler(authContext, suite.DB(), "fake key", false)
-
-	rr := httptest.NewRecorder()
-	handler.ServeHTTP(rr, req.WithContext(ctx))
-
-	suite.Equal(http.StatusSeeOther, rr.Code, "handler returned wrong status code")
-	if status := rr.Code; status != http.StatusSeeOther {
-		t.Errorf("handler returned wrong status code: got %v wanted %v", status, http.StatusSeeOther)
-	}
-
-	cookies := rr.Result().Cookies()
-	if _, err := getCookie("mil_session_token", cookies); err != nil {
-		t.Error("could not find session token in response")
-	}
 }
