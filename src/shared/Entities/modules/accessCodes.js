1d924183bc339ad625f37177c2f8e6763f23f94e
ralren <ren@truss.works>
2019-06-13 12:18:44 +0000

    Create the swagger request for fetchAccessCode

--- src/shared/Entities/modules/accessCodes.js
+++ src/shared/Entities/modules/accessCodes.js
@@ -2,7 +2,13 @@ import { swaggerRequest } from 'shared/Swagger/request';
 import { getPublicClient } from 'shared/Swagger/api';
 
 const validateAccessCodeLabel = 'AccessCodes.getAccessCode';
+const fetchAccessCodeLabel = 'AccessCodes.getAccessCodeForServiceMember';
 const claimAccessCodeLabel = 'AccessCodes.updateAccessCodeForServiceMember';
+
+export function fetchAccessCode(label = fetchAccessCodeLabel) {
+  const swaggerTag = 'accessscode.fetchAccessCode';
+  return swaggerRequest(getPublicClient, swaggerTag, {}, { label });
+}
 
 export function validateAccessCode(code, label = validateAccessCodeLabel) {
   const swaggerTag = 'accesscode.validateAccessCode';
