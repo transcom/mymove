f31ca1b10783d62bf49c0287de79966a6b845172
ralren <ren@truss.works>
2019-06-27 15:40:09 +0000

    Remove unique constrait from requries access code to service member id of access codes

--- migrations/20190422163230_create_access_code.up.sql
+++ migrations/20190422163230_create_access_code.up.sql
@@ -1,8 +1,12 @@
-CREATE TABLE access_codes (
+CREATE TABLE access_codes
+(
 	id uuid PRIMARY KEY,
-	service_member_id uuid REFERENCES service_members(id),
+	service_member_id uuid UNIQUE REFERENCES service_members(id),
 	code text UNIQUE NOT NULL,
 	move_type text NOT NULL,
-	created_at timestamp with time zone NOT NULL DEFAULT NOW(),
-	claimed_at timestamp with time zone
+	created_at timestamp
+	with time zone NOT NULL DEFAULT NOW
+	(),
+	claimed_at timestamp
+	with time zone
 );
