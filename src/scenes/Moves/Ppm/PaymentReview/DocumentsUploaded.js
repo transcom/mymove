b61c18a277f8cf38d08f532f743b1aeaae4332af
Ron Cruz <Ronolibert@gmail.com>
2019-06-28 13:54:27 +0000

    Fixed unit tests to account for new props

--- src/scenes/Moves/Ppm/PaymentReview/DocumentsUploaded.js
+++ src/scenes/Moves/Ppm/PaymentReview/DocumentsUploaded.js
@@ -18,7 +18,7 @@ export class DocumentsUploaded extends Component {
     showDocs: false,
   };
 
-  defaultProps = {
+  static defaultProps = {
     showLinks: false,
     showToggleDocs: false,
     inReviewPage: false,
@@ -52,58 +52,56 @@ export class DocumentsUploaded extends Component {
       return null;
     }
     return (
-      <>
-        <div
-          className="doc-summary-container"
-          data-cy="documents-uploaded"
-          style={{ paddingBottom: !inReviewPage && !showDocs ? '1em' : null, marginTop: !inReviewPage ? '1em' : null }}
-        >
-          <div style={{ display: 'flex', alignItems: 'baseline', marginRight: 5 }}>
-            {!inReviewPage && (
-              <img
-                alt="documents added checkmark"
-                src={docsAddedCheckmarkImg}
-                style={{ alignSelf: 'center', marginRight: 5 }}
-              />
-            )}
-            {this.renderHeader()}
-            {showToggleDocs && (
-              <a style={{ paddingLeft: '1em' }} onClick={this.toggleShowDocs}>
-                {showDocs ? 'Hide' : 'Show'}
-              </a>
-            )}
-          </div>
-          {(showDocs || inReviewPage) && (
-            <>
-              <h4>{weightTicketDocs.length} sets of weight tickets</h4>
-              <div className="tickets">
-                {weightTicketDocs.map((ticket, index) => (
-                  <WeightTicketListItem key={ticket.id} num={index} {...ticket} />
-                ))}
-              </div>
-              {showLinks && (
-                <Link data-cy="weight-ticket-link" to={`/moves/${moveId}/ppm-weight-ticket`}>
-                  <FontAwesomeIcon className="icon link-blue" icon={faPlusCircle} /> Add weight ticket
-                </Link>
-              )}
-              <hr id="doc-summary-separator" />
-              <h4>
-                {expenseDocs.length} expense{expenseDocs.length > 1 ? 's' : ''}
-              </h4>
-              <div className="tickets">
-                {formatExpenseDocs(expenseDocs).map(expense => <ExpenseTicketListItem key={expense.id} {...expense} />)}
-              </div>
-              {showLinks && (
-                <div className="add-expense-link">
-                  <Link data-cy="expense-link" to={`/moves/${moveId}/ppm-expenses`}>
-                    <FontAwesomeIcon className="icon link-blue" icon={faPlusCircle} /> Add expense
-                  </Link>
-                </div>
-              )}
-            </>
+      <div
+        className="doc-summary-container"
+        data-cy="documents-uploaded"
+        style={{ paddingBottom: !inReviewPage && !showDocs ? '1em' : null, marginTop: !inReviewPage ? '1em' : null }}
+      >
+        <div style={{ display: 'flex', alignItems: 'baseline', marginRight: 5 }}>
+          {!inReviewPage && (
+            <img
+              alt="documents added checkmark"
+              src={docsAddedCheckmarkImg}
+              style={{ alignSelf: 'center', marginRight: 5 }}
+            />
+          )}
+          {this.renderHeader()}
+          {showToggleDocs && (
+            <a style={{ paddingLeft: '1em' }} onClick={this.toggleShowDocs}>
+              {showDocs ? 'Hide' : 'Show'}
+            </a>
           )}
         </div>
-      </>
+        {(showDocs || inReviewPage) && (
+          <>
+            <h4>{weightTicketDocs.length} sets of weight tickets</h4>
+            <div className="tickets">
+              {weightTicketDocs.map((ticket, index) => (
+                <WeightTicketListItem key={ticket.id} num={index} {...ticket} />
+              ))}
+            </div>
+            {showLinks && (
+              <Link data-cy="weight-ticket-link" to={`/moves/${moveId}/ppm-weight-ticket`}>
+                <FontAwesomeIcon className="icon link-blue" icon={faPlusCircle} /> Add weight ticket
+              </Link>
+            )}
+            <hr id="doc-summary-separator" />
+            <h4>
+              {expenseDocs.length} expense{expenseDocs.length > 1 ? 's' : ''}
+            </h4>
+            <div className="tickets">
+              {formatExpenseDocs(expenseDocs).map(expense => <ExpenseTicketListItem key={expense.id} {...expense} />)}
+            </div>
+            {showLinks && (
+              <div className="add-expense-link">
+                <Link data-cy="expense-link" to={`/moves/${moveId}/ppm-expenses`}>
+                  <FontAwesomeIcon className="icon link-blue" icon={faPlusCircle} /> Add expense
+                </Link>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     );
   }
 }
--- src/scenes/Moves/Ppm/PaymentReview/DocumentsUploaded.test.js
+++ src/scenes/Moves/Ppm/PaymentReview/DocumentsUploaded.test.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import { DocumentsUploaded } from './DocumentsUploaded';
 import { mount } from 'enzyme';
-import Alert from 'shared/Alert';
 
-function generateWrapper(allDocuments, mockGetMoveDocumentsForMove) {
-  return mount(<DocumentsUploaded allDocuments={allDocuments} getMoveDocumentsForMove={mockGetMoveDocumentsForMove} />);
+const initialProps = {
+  moveId: 0,
+  expenseDocs: [],
+  weightTicketDocs: [],
+  getMoveDocumentsForMove: jest.fn(),
+};
+function generateWrapper(props) {
+  return mount(<DocumentsUploaded {...initialProps} {...props} />);
 }
 
 describe('DocumentsUploaded Alert', () => {
+  const documentsUploadedContainer = '[data-cy="documents-uploaded"]';
   describe('No documents uploaded', () => {
     it('component does not render', () => {
-      const mockGetMoveDocumentsForMove = jest.fn();
-      const wrapper = generateWrapper([], mockGetMoveDocumentsForMove);
-
-      expect(wrapper.find(Alert).length).toEqual(0);
+      const wrapper = generateWrapper();
+      expect(wrapper.find(documentsUploadedContainer).length).toEqual(0);
     });
   });
+
   describe('One document uploaded', () => {
     it('component renders', () => {
       const mockGetMoveDocumentsForMove = jest.fn();
-      const wrapper = generateWrapper(['document'], mockGetMoveDocumentsForMove);
-
+      const wrapper = generateWrapper({
+        getMoveDocumentsForMove: mockGetMoveDocumentsForMove,
+        expenseDocs: [{}],
+      });
       expect(mockGetMoveDocumentsForMove).toHaveBeenCalled();
-      expect(wrapper.find(Alert).length).toEqual(1);
-      expect(wrapper.find(Alert).text()).toContain('1 document added');
+      expect(wrapper.find(documentsUploadedContainer).length).toEqual(1);
+      expect(wrapper.find(documentsUploadedContainer).text()).toContain('1 document added');
     });
   });
   describe('More than one document uploaded', () => {
     it('component renders and text uses "documents" instead of "document"', () => {
       const mockGetMoveDocumentsForMove = jest.fn();
-      const wrapper = generateWrapper(['document 1', 'document 2'], mockGetMoveDocumentsForMove);
+      const wrapper = generateWrapper({
+        getMoveDocumentsForMove: mockGetMoveDocumentsForMove,
+        expenseDocs: [{}],
+        weightTicketDocs: [{}],
+      });
 
       expect(mockGetMoveDocumentsForMove).toHaveBeenCalled();
-      expect(wrapper.find(Alert).length).toEqual(1);
-      expect(wrapper.find(Alert).text()).toContain('2 documents added');
+      expect(wrapper.find(documentsUploadedContainer).length).toEqual(1);
+      expect(wrapper.find(documentsUploadedContainer).text()).toContain('2 documents added');
     });
   });
 });
