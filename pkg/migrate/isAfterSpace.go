e99d8db8654cf29f7c07c654004bb300b85bed8e
Chris Gilmer <chris@truss.works>
2019-07-03 21:12:13 +0000

    Add test for isAfterSpace

--- pkg/migrate/isAfterSpace.go
+++ pkg/migrate/isAfterSpace.go
@@ -1,5 +1,8 @@
 package migrate
 
+// isAfterSpace returns true if the index directly follows a space character
+//
+// The function also returns true if the index is at the beginning of the buffer
 func isAfterSpace(in *Buffer, i int) bool {
 	if i == 0 {
 		return true
