2dcab2f8b951528f7c45d1cfc159a687ad404306
Isaac Garfinkle <isaac@truss.works>
2019-07-17 23:14:33 +0000

    Fix more e2e data

--- pkg/testdatagen/scenario/e2ebasic.go
+++ pkg/testdatagen/scenario/e2ebasic.go
@@ -1003,6 +1003,7 @@ func (e e2eBasicScenario) Run(db *pop.Connection, loader *uploader.Uploader, log
 			OrdersTypeDetail:    &typeDetail,
 			DepartmentIndicator: models.StringPointer("AIR_FORCE"),
 			TAC:                 models.StringPointer("99"),
+			SAC:                 models.StringPointer("SAC"),
 		},
 		TrafficDistributionList: models.TrafficDistributionList{
 			ID:                uuid.FromStringOrNil("d17e2e3e-9bff-4bb0-b301-f97ad03350c1"),
@@ -1386,6 +1387,7 @@ func (e e2eBasicScenario) Run(db *pop.Connection, loader *uploader.Uploader, log
 			OrdersTypeDetail:    &typeDetail,
 			DepartmentIndicator: models.StringPointer("AIR_FORCE"),
 			TAC:                 models.StringPointer("99"),
+			SAC:                 models.StringPointer("SAC"),
 		},
 		TrafficDistributionList: models.TrafficDistributionList{
 			ID:                uuid.FromStringOrNil("cb49e75e-7897-4a01-8cff-c13ae85ca5ba"),
