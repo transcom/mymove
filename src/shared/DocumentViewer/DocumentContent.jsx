c0d3f36034a353af0db3be2f5bde7004c3d9c5f6
Kim Allen <kim@truss.works>
2019-07-23 23:59:43 +0000

    Show link if Firefox

--- src/shared/DocumentViewer/DocumentContent.jsx
+++ src/shared/DocumentViewer/DocumentContent.jsx
@@ -4,19 +4,29 @@ import PropTypes from 'prop-types';
 import './index.css';
 import styles from './DocumentContent.module.scss';
 
+const downloadOnlyView = (contentType, filename, url) => (
+  <div className="pdf-placeholder">
+    {filename && <span className="filename">{filename}</span>}
+    This PDF can be{' '}
+    <a target="_blank" rel="noopener noreferrer" href={url}>
+      viewed here
+    </a>
+    .
+  </div>
+);
+
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
 const DocumentContent = ({ contentType, filename, url }) => (
   <div className="page">
     {contentType === 'application/pdf' ? (
-      <object className={styles.pdf} data={url} type="application/pdf" alt="document upload">
-        <div className="pdf-placeholder">
-          {filename && <span className="filename">{filename}</span>}
-          This PDF can be{' '}
-          <a target="_blank" rel="noopener noreferrer" href={url}>
-            viewed here
-          </a>
-          .
-        </div>
-      </object>
+      isFirefox ? (
+        downloadOnlyView(contentType, filename, url)
+      ) : (
+        <object className={styles.pdf} data={url} type="application/pdf" alt="document upload">
+          {downloadOnlyView(contentType, filename, url)}
+        </object>
+      )
     ) : (
       <img src={url} width="100%" height="100%" alt="document upload" />
     )}
