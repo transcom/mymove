99bb930674798dad35cf5199dde942983341c21d
Rupinder Dhariwal <rd@truss.works>
2019-07-24 18:25:30 +0000

    apply fix to logic that was only extracting first line of Copy statement

--- pkg/migrate/Exec.go
+++ pkg/migrate/Exec.go
@@ -29,18 +29,22 @@ func Exec(inputReader io.Reader, tx *pop.Connection, wait time.Duration) error {
 
 	statements := make(chan string, 1000)
 	go SplitStatements(lines, statements)
-	counter := 0
 	for stmt := range statements {
 		//if it is COPY statement then assume rest of the statements are part of copy and execute then as part of stdin
 		match := copyStdinPattern.FindStringSubmatch(stmt)
-
 		if match != nil {
 			//create buffer of remaining statements
-			var copyStmts Buffer
+			copyStmts := NewBuffer()
+			var isCopy []string
 
 			//range over the channel again
 			for copyStmt := range statements {
-				isCopy := copyStdinPattern.FindStringSubmatch(copyStmt)
+
+				//only do this once, we are looking for first instance of copy
+				//with an assumption that copy is always the last statement
+				if isCopy == nil {
+					isCopy = copyStdinPattern.FindStringSubmatch(copyStmt)
+				}
 
 				//skip all non copy statements
 				if isCopy == nil {
@@ -52,10 +56,11 @@ func Exec(inputReader io.Reader, tx *pop.Connection, wait time.Duration) error {
 					}
 				}
 			}
+			copyStmts.Close()
 
 			// See test to understand regex
 			var errCopyFromStdin error
-			_, errCopyFromStdin = execCopyFromStdin(&copyStmts, counter, match[4], parseColumns(match[6]), tx, wait)
+			_, errCopyFromStdin = execCopyFromStdin(copyStmts, 0, match[4], parseColumns(match[6]), tx, wait)
 			if errCopyFromStdin != nil {
 				return errors.Wrap(errCopyFromStdin, "error copying from stdin")
 			}
@@ -68,7 +73,6 @@ func Exec(inputReader io.Reader, tx *pop.Connection, wait time.Duration) error {
 		if errExec != nil {
 			return errors.Wrapf(errExec, "error executing statement: %q", stmt)
 		}
-		counter++
 	}
 	return nil
 }
