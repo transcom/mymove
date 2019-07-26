ec9f841e8e5e31528033568ddd88783924ef7493
Chris Gilmer <chris@truss.works>
2019-07-26 21:23:08 +0000

    Complete test coverage for prepareCopyFromStdin

--- pkg/migrate/prepareCopyFromStdin_test.go
+++ pkg/migrate/prepareCopyFromStdin_test.go
@@ -9,13 +9,29 @@ func (suite *MigrateSuite) TestPrepareCopyFromStdinWithSchema() {
 	listOfColumns := "id, performance_period_start, performance_period_end, traffic_distribution_list_id, quality_band, offer_count, best_value_score, transportation_service_provider_id, created_at, updated_at, rate_cycle_start, rate_cycle_end, linehaul_rate, sit_rate"
 	columns := parseColumns(listOfColumns)
 
-	suite.DB().Transaction(func(tx *pop.Connection) error {
+	err := suite.DB().Transaction(func(tx *pop.Connection) error {
 		stmt, err := prepareCopyFromStdin(tableName, columns, tx)
 		suite.Nil(err)
 		suite.NotNil(stmt)
 		suite.Nil(stmt.Close())
 		return nil
 	})
+	suite.NoError(err)
+}
+
+func (suite *MigrateSuite) TestPrepareCopyFromStdinWithBadSchema() {
+	tableName := "public.bad.transportation_service_provider_performances"
+	listOfColumns := "id, performance_period_start, performance_period_end, traffic_distribution_list_id, quality_band, offer_count, best_value_score, transportation_service_provider_id, created_at, updated_at, rate_cycle_start, rate_cycle_end, linehaul_rate, sit_rate"
+	columns := parseColumns(listOfColumns)
+
+	err := suite.DB().Transaction(func(tx *pop.Connection) error {
+		stmt, err := prepareCopyFromStdin(tableName, columns, tx)
+		suite.NotNil(err)
+		suite.Nil(stmt)
+		return err
+	})
+	suite.NotNil(err)
+	suite.Equal("error preparing copy from stdin statement: pq: relation \"public.bad.transportation_service_provider_performances\" does not exist", err.Error())
 }
 
 func (suite *MigrateSuite) TestPrepareCopyFromStdinWithoutSchema() {
@@ -23,11 +39,27 @@ func (suite *MigrateSuite) TestPrepareCopyFromStdinWithoutSchema() {
 	listOfColumns := "id, performance_period_start, performance_period_end, traffic_distribution_list_id, quality_band, offer_count, best_value_score, transportation_service_provider_id, created_at, updated_at, rate_cycle_start, rate_cycle_end, linehaul_rate, sit_rate"
 	columns := parseColumns(listOfColumns)
 
-	suite.DB().Transaction(func(tx *pop.Connection) error {
+	err := suite.DB().Transaction(func(tx *pop.Connection) error {
 		stmt, err := prepareCopyFromStdin(tableName, columns, tx)
 		suite.Nil(err)
 		suite.NotNil(stmt)
 		suite.Nil(stmt.Close())
 		return nil
 	})
+	suite.NoError(err)
+}
+
+func (suite *MigrateSuite) TestPrepareCopyFromStdinWithoutSchemaFail() {
+	tableName := "bad_transportation_service_provider_performances"
+	listOfColumns := "id, performance_period_start, performance_period_end, traffic_distribution_list_id, quality_band, offer_count, best_value_score, transportation_service_provider_id, created_at, updated_at, rate_cycle_start, rate_cycle_end, linehaul_rate, sit_rate"
+	columns := parseColumns(listOfColumns)
+
+	err := suite.DB().Transaction(func(tx *pop.Connection) error {
+		stmt, err := prepareCopyFromStdin(tableName, columns, tx)
+		suite.NotNil(err)
+		suite.Nil(stmt)
+		return err
+	})
+	suite.NotNil(err)
+	suite.Equal("error preparing copy from stdin statement: pq: relation \"bad_transportation_service_provider_performances\" does not exist", err.Error())
 }
