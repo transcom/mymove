02fdab62c7e14b8a943f20e62bcfc3ce3e2c18ee
Donald Thai <donald@truss.works>
2019-07-30 20:23:19 +0000

    helper payload functions

---
+++ pkg/handlers/adminapi/addresses.go
@@ -0,0 +1,48 @@
+package adminapi
+
+import (
+	"github.com/go-openapi/swag"
+
+	"github.com/transcom/mymove/pkg/gen/adminmessages"
+	"github.com/transcom/mymove/pkg/models"
+)
+
+func addressModelFromPayload(rawAddress *adminmessages.Address) *models.Address {
+	if rawAddress == nil {
+		return nil
+	}
+	return &models.Address{
+		StreetAddress1: *rawAddress.StreetAddress1,
+		StreetAddress2: rawAddress.StreetAddress2,
+		StreetAddress3: rawAddress.StreetAddress3,
+		City:           *rawAddress.City,
+		State:          *rawAddress.State,
+		PostalCode:     *rawAddress.PostalCode,
+		Country:        rawAddress.Country,
+	}
+}
+
+func payloadForAddressModel(a *models.Address) *adminmessages.Address {
+	if a == nil {
+		return nil
+	}
+	return &adminmessages.Address{
+		StreetAddress1: swag.String(a.StreetAddress1),
+		StreetAddress2: a.StreetAddress2,
+		StreetAddress3: a.StreetAddress3,
+		City:           swag.String(a.City),
+		State:          swag.String(a.State),
+		PostalCode:     swag.String(a.PostalCode),
+		Country:        a.Country,
+	}
+}
+
+func updateAddressWithPayload(a *models.Address, payload *adminmessages.Address) {
+	a.StreetAddress1 = *payload.StreetAddress1
+	a.StreetAddress2 = payload.StreetAddress2
+	a.StreetAddress3 = payload.StreetAddress3
+	a.City = *payload.City
+	a.State = *payload.State
+	a.PostalCode = *payload.PostalCode
+	a.Country = payload.Country
+}
