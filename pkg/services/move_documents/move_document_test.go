d2873d4dc3be840da0ab5910090eb888c8e5bc0f
Matt Krump <matt@truss.works>
2019-07-31 15:07:02 +0000

    Remove old server tests

--- pkg/services/move_documents/move_document_test.go
+++ pkg/services/move_documents/move_document_test.go
@@ -3,6 +3,8 @@ package movedocument
 import (
 	"testing"
 
+	"go.uber.org/zap"
+
 	"github.com/stretchr/testify/suite"
 
 	"github.com/transcom/mymove/pkg/testingsuite"
@@ -20,6 +22,7 @@ func (suite *MoveDocumentServiceSuite) SetupTest() {
 func TestMoveDocumentUpdaterServiceSuite(t *testing.T) {
 	hs := &MoveDocumentServiceSuite{
 		PopTestSuite: testingsuite.NewPopTestSuite(testingsuite.CurrentPackage().Suffix("move_document_service")),
+		logger:       zap.NewNop(), // Use a no-op logger during testing
 	}
 	suite.Run(t, hs)
 }
