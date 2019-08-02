8ad380a510800c5a169069ab88d6c4da8653e85b
Donald Thai <donald@truss.works>
2019-08-02 21:05:49 +0000

    payload for transportation offices for adminmessages

--- pkg/handlers/adminapi/offices.go
+++ pkg/handlers/adminapi/offices.go
@@ -6,8 +6,21 @@ import (
 	officeop "github.com/transcom/mymove/pkg/gen/adminapi/adminoperations/office"
 	"github.com/transcom/mymove/pkg/gen/adminmessages"
 	"github.com/transcom/mymove/pkg/handlers"
+	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
 )
+
+func payloadForOfficeModel(o models.TransportationOffice) *adminmessages.TransportationOffice {
+	return &adminmessages.TransportationOffice{
+		ID:         handlers.FmtUUID(o.ID),
+		Name:       handlers.FmtString(o.Name),
+		Address:    payloadForAddressModel(&o.Address),
+		Gbloc:      o.Gbloc,
+		PhoneLines: payloadForPhoneLines(o.PhoneLines),
+		Latitude:   o.Latitude,
+		Longitude:  o.Longitude,
+	}
+}
 
 // IndexOfficesHandler returns a list of office users via GET /office_users
 type IndexOfficesHandler struct {
