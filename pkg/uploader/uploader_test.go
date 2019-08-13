badcd49734d1cccdfdd80889a9513754d7451bad
Kara Louie <kara@truss.works>
2019-08-13 15:12:10 +0000

    Add backend test

--- pkg/uploader/uploader_test.go
+++ pkg/uploader/uploader_test.go
@@ -119,6 +119,18 @@ func (suite *UploaderSuite) TestUploadFromLocalFileZeroLength() {
 	suite.Nil(upload, "returned an upload when erroring")
 }
 
+func (suite *UploaderSuite) TestTooLargeUploadFromLocalFile() {
+	document := testdatagen.MakeDefaultDocument(suite.DB())
+
+	up := uploader.NewUploader(suite.DB(), suite.logger, suite.storer)
+	file := suite.fixture("largepdf.pdf")
+
+	upload, verrs, err := up.CreateUploadForDocument(&document.ID, document.ServiceMember.UserID, file, uploader.AllowedTypesPDF)
+	suite.Equal(err, uploader.ErrTooLarge)
+	suite.False(verrs.HasAny(), "failed to validate upload")
+	suite.Nil(upload, "returned an upload when erroring")
+}
+
 func (suite *UploaderSuite) helperNewTempFile() (afero.File, error) {
 	outputFile, err := suite.fs.TempFile("/tmp/milmoves/", "TestCreateUploadNoDocument")
 	if err != nil {
