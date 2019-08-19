3ed1e7cb4eb78ebd7b431081551dbb1381417713
Matt Krump <matt@truss.works>
2019-08-19 22:24:06 +0000

    Add rotation bar

---
+++ src/shared/RotationBar/RotationBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import leftRotation from 'shared/images/left-rotation.png';
+import rightRotation from 'shared/images/right-rotation.png';
+import PropTypes from 'prop-types';
+import styles from './RotationBar.module.scss';
+
+export class RotationBar extends Component {
+  render() {
+    console.log(styles);
+    return (
+      <div className={styles['rotation-bar']}>
+        <button onClick={this.props.onLeftButtonClick}>
+          <img src={leftRotation} alt="rotate-left" />
+        </button>
+        <button onClick={this.props.onRightButtonClick}>
+          <img src={rightRotation} alt="rotate-right" />
+        </button>
+      </div>
+    );
+  }
+}
+
+RotationBar.propTypes = {
+  onLeftButtonClick: PropTypes.func,
+  onRightButtonClick: PropTypes.func,
+};
