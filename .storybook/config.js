23ebfed17c3dac17dd7a52c7c3e77a0a54993d13
John Gedeon <john@truss.works>
2019-08-22 18:21:22 +0000

    Change require to allow IE11 to load

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/stories', true, /\.stories\.js$/);
+//const req = require.context('../src/stories', true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  require('../src/stories/index.stories.js');
+  //req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
