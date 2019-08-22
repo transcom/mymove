befa24445890a5c2248f0942ac5c4ed9a43c5e92
John Gedeon <john@truss.works>
2019-08-22 20:36:32 +0000

    Remove comments

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,7 @@
 import { configure } from '@storybook/react';
 
-// automatically import all files ending in *.stories.js
-//const req = require.context('../src/stories', true, /\.stories\.js$/);
 function loadStories() {
   require('../src/stories/index.stories.js');
-  // This does not work in IE 11
-  //req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
