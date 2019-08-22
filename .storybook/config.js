7e1fc48f683b42bb1cb771307bd1e4e9b325c3c0
John Gedeon <john@truss.works>
2019-08-22 20:17:00 +0000

    Add clarifying comment on code change

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ import { configure } from '@storybook/react';
 //const req = require.context('../src/stories', true, /\.stories\.js$/);
 function loadStories() {
   require('../src/stories/index.stories.js');
+  // This does not work in IE 11
   //req.keys().forEach(filename => req(filename));
 }
 
