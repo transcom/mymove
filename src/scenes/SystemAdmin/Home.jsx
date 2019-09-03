6cf3de0760977dbc58e3bb01d3a3fd5f86c465b1
Garrett Martin <garrett@truss.works>
2019-09-03 19:10:28 +0000

    Add admin index page for electronic orders

--- src/scenes/SystemAdmin/Home.jsx
+++ src/scenes/SystemAdmin/Home.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Menu from './Menu';
 import UserList from './UserList';
 import OfficeList from './OfficeList';
+import ElectronicOrderList from './ElectronicOrderList';
 import UserShow from './UserShow';
 import styles from './Home.module.scss';
 
@@ -26,6 +27,7 @@ const Home = () => (
     <Admin dataProvider={dataProvider} history={history} appLayout={AdminLayout}>
       <Resource name="office_users" list={UserList} show={UserShow} />
       <Resource name="offices" list={OfficeList} />
+      <Resource name="electronic_orders" list={ElectronicOrderList} />
     </Admin>
   </div>
 );
