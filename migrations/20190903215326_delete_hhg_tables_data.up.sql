1506d93db95d88ef3d5d304e269aeafb51fa148b
Donald Thai <donald@truss.works>
2019-09-05 20:57:35 +0000

    latest update for script to delete HHG data

--- migrations/20190903215326_delete_hhg_tables_data.up.sql
+++ migrations/20190903215326_delete_hhg_tables_data.up.sql
@@ -1,7 +1,32 @@
--- drop table shipment_line_items;
--- drop table invoices;
--- ALTER TABLE move_documents DROP COLUMN shipment_id;
-drop table shipments;
+BEGIN;
+drop table if EXISTS shipment_line_items;
+drop table if EXISTS shipment_line_item_dimensions;
+drop table if EXISTS shipment_offers;
+drop table if EXISTS service_agents;
+drop table if EXISTS shipment_recalculates;
+drop table if EXISTS shipment_recalculate_logs;
+drop table if EXISTS storage_in_transits;
+drop table if EXISTS storage_in_transit_number_trackers;
+drop table if EXISTS gbl_number_trackers;
+
+ALTER TABLE move_documents DROP COLUMN IF EXISTS shipment_id;
+ALTER TABLE invoices DROP COLUMN IF EXISTS shipment_id;
+ALTER TABLE signed_certifications DROP COLUMN IF EXISTS shipment_id;
+
+-- setting all tsp users to disabled
+-- deleting records from the TDL, TSP and TSPP that relates to the user
+UPDATE tsp_users SET disabled = true;
+UPDATE transportation_service_providers SET enrolled = false WHERE id in (select transportation_service_provider_id from tsp_users);
+
+-- referencing the TDLs that need to be deleted
+select id into tdlIdTemp from transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users);
+
+DELETE FROM transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users);
+
+DELETE FROM traffic_distribution_lists WHERE id in (select traffic_distribution_list_id from transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users));
+
+-- finally dropping the shipments
+drop table IF EXISTS shipments;
 
 
 -- select m.selected_move_type, * from shipment_line_items sli
@@ -22,3 +47,5 @@ Query 1 OK: DROP TABLE
 move id
 aa8ab3e4-709c-43f3-bf0e-bf0e8e7d16e9
 */
+
+COMMIT;
