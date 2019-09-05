f29023bfb6dff72fb405c01374cefc99abbca564
Garrett Martin <garrett@truss.works>
2019-09-05 15:16:20 +0000

    Test cleanup!

--- src/scenes/SystemAdmin/TitleizedField.test.js
+++ src/scenes/SystemAdmin/TitleizedField.test.js
@@ -5,15 +5,9 @@ import TitleizedField from './TitleizedField';
 
 describe('TitleizedField', () => {
   it('transforms text properly', () => {
-    const div = document.createElement('div');
-    let record = { issuer: 'coast-guard' };
-    let field = shallow(<TitleizedField source="issuer" record={record} />, div);
+    const record = { issuer: 'coast-guard' };
+    const field = shallow(<TitleizedField source="issuer" record={record} />);
 
     expect(field.text()).toEqual('Coast Guard');
-
-    record = { issuer: 'Army' };
-    field = shallow(<TitleizedField source="issuer" record={record} />, div);
-
-    expect(field.text()).toEqual('Army');
   });
 });
