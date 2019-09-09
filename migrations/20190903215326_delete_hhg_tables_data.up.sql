746fd86e8441967b8626c30d8b0bab1df4319560
lindsay t <techells22@gmail.com>
2019-09-09 17:34:04 +0000

    tsp_users

--- migrations/20190903215326_delete_hhg_tables_data.up.sql
+++ migrations/20190903215326_delete_hhg_tables_data.up.sql
@@ -1,32 +1,28 @@
 BEGIN;
-drop table if EXISTS shipment_line_items;
-drop table if EXISTS shipment_line_item_dimensions;
-drop table if EXISTS shipment_offers;
-drop table if EXISTS service_agents;
-drop table if EXISTS shipment_recalculates;
-drop table if EXISTS shipment_recalculate_logs;
-drop table if EXISTS storage_in_transits;
-drop table if EXISTS storage_in_transit_number_trackers;
-drop table if EXISTS gbl_number_trackers;
+
+DROP TABLE IF EXISTS shipment_line_items;
+DROP TABLE IF EXISTS shipment_line_item_dimensions;
+DROP TABLE IF EXISTS shipment_offers;
+DROP TABLE IF EXISTS service_agents;
+DROP TABLE IF EXISTS shipment_recalculates;
+DROP TABLE IF EXISTS shipment_recalculate_logs;
+DROP TABLE IF EXISTS storage_in_transits;
+DROP TABLE IF EXISTS storage_in_transit_number_trackers;
+DROP TABLE IF EXISTS gbl_number_trackers;
+DROP TABLE IF EXISTS blackout_dates;
 
 ALTER TABLE move_documents DROP COLUMN IF EXISTS shipment_id;
 ALTER TABLE invoices DROP COLUMN IF EXISTS shipment_id;
 ALTER TABLE signed_certifications DROP COLUMN IF EXISTS shipment_id;
 
--- setting all tsp users to disabled
--- deleting records from the TDL, TSP and TSPP that relates to the user
-UPDATE tsp_users SET disabled = true;
-UPDATE transportation_service_providers SET enrolled = false WHERE id in (select transportation_service_provider_id from tsp_users);
+-- remove tsp_users table and disable associated tsp_users in users table
+ALTER TABLE tsp_users DROP CONSTRAINT IF EXISTS tsp_users_transportation_service_provider_id_fkey;
+UPDATE USERS SET disabled = TRUE WHERE id IN (SELECT user_id FROM tsp_users);
+DROP TABLE tsp_users;
 
--- referencing the TDLs that need to be deleted
-select id into TEMP andtdlIdTemp from transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users);
-
-DELETE FROM transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users);
-
-DELETE FROM traffic_distribution_lists WHERE id in (select traffic_distribution_list_id from transportation_service_provider_performances WHERE transportation_service_provider_id in (select transportation_service_provider_id from tsp_users));
 
 -- finally dropping the shipments
-drop table IF EXISTS shipments;
+DROP TABLE IF EXISTS shipments;
 
 
 COMMIT;
