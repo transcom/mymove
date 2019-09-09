0d1f142091d067ca05a5ca578bb6261c56ba3e79
lindsay t <techells22@gmail.com>
2019-09-09 20:02:15 +0000

    deleting move and sm rows

--- migrations/20190903215326_delete_hhg_tables_data.up.sql
+++ migrations/20190903215326_delete_hhg_tables_data.up.sql
@@ -20,9 +20,36 @@ ALTER TABLE tsp_users DROP CONSTRAINT IF EXISTS tsp_users_transportation_service
 UPDATE USERS SET disabled = TRUE WHERE id IN (SELECT user_id FROM tsp_users);
 DROP TABLE tsp_users;
 
+-- removing hhg moves
+-- documents
+DELETE FROM weight_ticket_set_documents WHERE move_document_id IN (SELECT md.id FROM move_documents md INNER JOIN moves m ON m.id = md.move_id AND m.selected_move_type = 'HHG');
+DELETE FROM moving_expense_documents WHERE move_document_id IN (SELECT md.id FROM move_documents md INNER JOIN moves m ON m.id = md.move_id AND m.selected_move_type = 'HHG');
+
+DELETE FROM move_documents WHERE move_id IN (SELECT id FROM moves where selected_move_type = 'HHG');
+DELETE FROM signed_certifications WHERE move_id IN (SELECT id FROM moves where selected_move_type = 'HHG');
 
 -- finally dropping the shipments
 DROP TABLE IF EXISTS shipments;
+
+DELETE FROM moves WHERE selected_move_type = 'HHG';
+
+
+
+-- service members
+DELETE FROM access_codes WHERE service_member_id IN (select sm.id from service_members sm inner join orders o on sm.id = o.service_member_id inner join moves m on m.orders_id = o.id WHERE m.selected_move_type = 'HHG');
+DELETE FROM backup_contacts WHERE service_member_id IN (select sm.id from service_members sm inner join orders o on sm.id = o.service_member_id inner join moves m on m.orders_id = o.id WHERE m.selected_move_type = 'HHG');
+DELETE FROM orders WHERE service_member_id IN (select sm.id from service_members sm inner join orders o on sm.id = o.service_member_id inner join moves m on m.orders_id = o.id WHERE m.selected_move_type = 'HHG');
+
+
+-- delete don't drop...
+DROP TABLE IF EXISTS service_members sm inner join orders o on sm.id = o.service_member_id inner join moves m on m.orders_id = o.id WHERE m.selected_move_type = 'HHG';
+
+
+
+
+
+
+
 
 
 COMMIT;
