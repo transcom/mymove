b72a39b7d97d79cc19e2d8aa68f0e457bdd221ee
Garrett Martin <garrett@truss.works>
2019-09-10 20:46:43 +0000

    Fix order on list page and fix email field

--- src/scenes/SystemAdmin/UserList.jsx
+++ src/scenes/SystemAdmin/UserList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { List, EmailField, Datagrid, TextField, BooleanField } from 'react-admin';
+import { List, Datagrid, TextField, BooleanField } from 'react-admin';
 import AdminPagination from './AdminPagination';
 
 const UserList = props => (
   <List {...props} pagination={<AdminPagination />} perPage={25}>
     <Datagrid rowClick="show">
-      <EmailField source="email" />
-      <TextField source="first_name" />
       <TextField source="id" />
+      <TextField source="email" />
+      <TextField source="first_name" />
       <TextField source="last_name" />
       <BooleanField source="disabled" label="Deactivated" />
     </Datagrid>
