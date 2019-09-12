7473124668e466d43bb77b8dd0f39c38eab67223
Isaac Garfinkle <isaac@truss.works>
2019-09-12 01:10:04 +0000

    Dropdown to select TO

--- src/scenes/SystemAdmin/UserCreate.jsx
+++ src/scenes/SystemAdmin/UserCreate.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Create, SimpleForm, TextInput, required, regex } from 'react-admin';
+import { Create, SimpleForm, TextInput, ReferenceInput, AutocompleteInput, required, regex } from 'react-admin';
 
 const phoneValidators = [required(), regex(/^[2-9]\d{2}-\d{3}-\d{4}$/, 'Invalid phone number, should be 000-000-0000')];
-const uuidValidators = [
-  required(),
-  regex(
-    /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/,
-    'Invalid uuid format, should be 12345678-1234-1234-1234-1234567890ab',
-  ),
-];
+
+const valueMatcher = (input, suggestion, getOptionText) => {
+  return input.toLowerCase().trim() === getOptionText(suggestion).toLowerCase();
+};
 
 const UserCreate = props => (
   <Create {...props}>
@@ -18,7 +15,9 @@ const UserCreate = props => (
       <TextInput source="last_name" validate={required()} />
       <TextInput source="email" validate={required()} />
       <TextInput source="telephone" validate={phoneValidators} />
-      <TextInput source="transportation_office_id" validate={uuidValidators} />
+      <ReferenceInput label="Transportation Office" reference="offices" source="transportation_office_id" perPage={500}>
+        <AutocompleteInput optionText="name" inputValueMatcher={valueMatcher} />
+      </ReferenceInput>
     </SimpleForm>
   </Create>
 );
