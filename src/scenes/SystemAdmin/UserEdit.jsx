5cd22128b36912232e82cbbbd83ce9a98063ef68
Garrett Martin <garrett@truss.works>
2019-09-16 16:21:04 +0000

    Add validations to edit form

--- src/scenes/SystemAdmin/UserEdit.jsx
+++ src/scenes/SystemAdmin/UserEdit.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Edit, SimpleForm, TextInput, DisabledInput } from 'react-admin';
+import { phoneValidators } from './form_validators';
+import { Edit, SimpleForm, TextInput, DisabledInput, required } from 'react-admin';
 
 const UserEdit = props => (
   <Edit {...props}>
     <SimpleForm>
       <DisabledInput source="id" />
       <DisabledInput source="email" />
-      <TextInput source="first_name" />
+      <TextInput source="first_name" validate={required()} />
       <TextInput source="middle_initials" />
-      <TextInput source="last_name" />
-      <TextInput source="telephone" />
+      <TextInput source="last_name" validate={required()} />
+      <TextInput source="telephone" validate={phoneValidators} />
     </SimpleForm>
   </Edit>
 );
