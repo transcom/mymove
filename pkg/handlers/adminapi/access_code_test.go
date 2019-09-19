0a6f83098f77f036c380a412a63e2ee8e340f9e5
Matt Krump <matt@truss.works>
2019-09-19 20:24:31 +0000

    Add additional argument to TestIndexAccessCodesHandler

--- pkg/handlers/adminapi/access_code_test.go
+++ pkg/handlers/adminapi/access_code_test.go
@@ -75,6 +75,7 @@ func (suite *HandlerSuite) TestIndexAccessCodesHandler() {
 		accessCodeListFetcher.On("FetchAccessCodeList",
 			mock.Anything,
 			mock.Anything,
+			mock.Anything,
 		).Return(nil, expectedError).Once()
 		handler := IndexAccessCodesHandler{
 			HandlerContext:        handlers.NewHandlerContext(suite.DB(), suite.TestLogger()),
