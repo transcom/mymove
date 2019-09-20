ca1e5c3d4b2db66b421fdd8796269fcfcdd140eb
Donald Thai <donald@truss.works>
2019-09-20 21:44:06 +0000

    refactor query to disable users that are not in admin or office tables

--- migrations/20190919173143_delete_hhg_tables_data.up.sql
+++ migrations/20190919173143_delete_hhg_tables_data.up.sql
@@ -25,7 +25,11 @@ ALTER TABLE invoices DROP COLUMN IF EXISTS shipment_id;
 ALTER TABLE signed_certifications DROP COLUMN IF EXISTS shipment_id;
 
 -- remove tsp_users table and disable associated tsp_users in users table
-UPDATE USERS SET disabled = TRUE WHERE id IN (SELECT user_id FROM tsp_users);
+-- make sure that users that are also office users are not disabled
+UPDATE USERS SET disabled = TRUE
+	WHERE id IN (select user_id from tsp_users where user_id is not null)
+		and id NOT IN (select user_id from office_users where user_id is not null)
+		and id NOT IN (select user_id from admin_users where user_id is not null);
 DROP TABLE tsp_users;
 
 -- removing hhg moves
