89abd184e07fc643c7267fab79f31ffa05fca567
Garrett Martin <garrett@blvrd.co>
2019-09-26 10:05:54 -0500

    Revert "Remove Liz from experimental, staging, and prod"

--- local_migrations/20190925190920_disable_liz.up.sql
+++
@@ -1,4 +0,0 @@
--- Local test migration.
--- This will be run on development environments.
--- It should mirror what you intend to apply on prod/staging/experimental
--- DO NOT include any sensitive data.
