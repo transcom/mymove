dfb83bd9fefff15819949002b0bd6f3c8af17218
lindsay t <techells22@gmail.com>
2019-09-30 15:28:37 +0000

    add skipped file

--- pkg/models/duty_station_names.go
+++ pkg/models/duty_station_names.go
@@ -8,11 +8,11 @@ import (
 
 // DutyStationName represents a military duty station for a specific affiliation
 type DutyStationName struct {
-	ID            uuid.UUID  `json:"id" db:"id"`
-	CreatedAt     time.Time  `json:"created_at" db:"created_at"`
-	UpdatedAt     time.Time  `json:"updated_at" db:"updated_at"`
-	Name          string     `json:"name" db:"name"`
-	DutyStationID uuid.UUID  `json:"duty_station_id" db:"duty_station_id"`
+	ID            uuid.UUID   `json:"id" db:"id"`
+	CreatedAt     time.Time   `json:"created_at" db:"created_at"`
+	UpdatedAt     time.Time   `json:"updated_at" db:"updated_at"`
+	Name          string      `json:"name" db:"name"`
+	DutyStationID uuid.UUID   `json:"duty_station_id" db:"duty_station_id"`
 	DutyStation   DutyStation `belongs_to:"duty_stations"`
 }
 
