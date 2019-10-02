a508b5464423d636a190a620c2b155169595058e
Jacqueline McKinney <jacqueline@truss.works>
2019-10-02 15:09:51 +0000

    draft parsing function, untested

--- cmd/parse_ratengine_data_ghc/main.go
+++ cmd/parse_ratengine_data_ghc/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-
-	"github.com/transcom/mymove/pkg/models"
-	"github.com/transcom/mymove/pkg/testdatagen"
+	"strconv"
 )
 import "go.uber.org/zap"
 import "github.com/tealeg/xlsx"
@@ -34,26 +32,105 @@ import "github.com/tealeg/xlsx"
         5a) Access. and Add. Prices
 	    5b) Price Escalation Discount
 
+
+--------------------------------------------------------------------------
+
+0: 	Guide to Pricing Rate Table
+1: 	Total Evaluated Price
+2: 	Submission Checklist
+3: 	1a) Directions
+4: 	1b) Service Areas
+5: 	Domestic Price Tabs >>>
+6: 	2a) Domestic Linehaul Prices
+7: 	2b) Dom. Service Area Prices
+8: 	2c) Other Domestic Prices
+9: 	International Prices Tables >>>
+10: 3a) OCONUS to OCONUS Prices
+11: 3b) CONUS to OCONUS Prices
+12: 3c) OCONUS to CONUS Prices
+13: 3d) Other International Prices
+14: 3e) Non-Standard Loc'n Prices
+15:	Other Price Tables
+16: 4a) Mgmt., Coun., Trans. Prices
+17: 5a) Access. and Add. Prices
+18: 5b) Price Escalation Discount
+19: Domestic  Linehaul Data
+20: Domestic Move Count
+21: Domestic Avg Weight
+22: Domestic Avg Milage
+23: Domestic Price Calculation >>>
+24: Domestic Linehaul Calculation
+25: Domestic SA Price Calculation
+26: NTS Packing Calculation
+27: Int'l Price Calculation >>>
+28: OCONUS to OCONUS Calculation
+29: CONUS to OCONUS Calculation
+30: OCONUS to CONUS Calculation
+31: Other Int'l Prices Calculation
+32: Non-Standard Loc'n Calculation
+33: Other Calculations >>>
+34: Mgmt., Coun., Trans., Calc
+35: Access. and Add. Calculation
+
+
  *************************************************************************/
+
+
 
 
 func help() {
 
 }
 
+
+
 func main() {
 	logger, err := zap.NewDevelopment()
 
+	/*
 	config := flag.String("config-dir", "config", "The location of server config files")
 	env := flag.String("env", "development", "The environment to run in, which configures the database.")
 	test := flag.Bool("test", false, "Whether to generate testy mcTest emails")
+	*/
+	help := flag.Bool("help", false, "Display help/usage info")
+	all := flag.Bool("all", false, "True, if parsing entire Rate Engine GHC XLSX")
+	display := flag.Bool("display", false, "True, if display output of parsed info")
+	filename := flag.String("filename", "", "Filename including path of the XLSX to parse for Rate Engine GHC import")
+
 	flag.Parse()
 
-	fmt.Printf("File written to %s\n", path)
+	//fmt.Printf("File written to %s\n", path)
+	fmt.Printf("Importing file %s\n", filename)
+
+	if filename != nil {
+		parseDomesticLinehaulPrices(*filename)
+	}
 
 }
 
-func parseDomesticLinehaulPrices() {
+// A safe way to get a cell from a slice of cells, returning empty string if not found
+func getCell(cells []*xlsx.Cell, i int) string {
+	if len(cells) > i {
+		return cells[i].String()
+	}
+
+	return ""
+}
+
+func getInt(from string) int  {
+	if from == "" {
+		return 0
+	}
+	i, err := strconv.Atoi(from)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
+
+
+func parseDomesticLinehaulPrices(parseFile string) error {
 
 	/*
 	weightBands
@@ -67,12 +144,7 @@ func parseDomesticLinehaulPrices() {
 		ColLettersToIndex
 	*/
 
-	type seasonType string
-
-	const (
-		PeakSeason    seasonType = "Peak"
-		NonPeakSeason seasonType = "NonPeak"
-	)
+	rateTypes := []string{"NonPeak", "Peak"}
 
 	weightBandNumCellsExpected := 10 //cells per band verify against weightBandNumCells
 	weightBandCountExpected := 3 //expected number of weight bands verify against weightBandCount
@@ -172,50 +244,70 @@ func parseDomesticLinehaulPrices() {
 	weightBandNumCells := len(milesRanges) //
 
 
-	var domesticLineHaulPrice struct {
+	type domesticLineHaulPrice struct {
 		serviceAreaNumber int
 		originServiceArea string
 		serviceSchedule int
-		season seasonType
+		season string
 		weightBand weightBand
 		milesRange milesRange
-		optionPeriodYearCount int
+		optionPeriodYearCount int //the escalation type
+		rate string
 	}
 
-}
+
+	if weightBandNumCells != weightBandNumCellsExpected {
+		fmt.Errorf("Exepected %d columns per weight band, found %d defined in golang parser\n", weightBandNumCellsExpected, weightBandNumCells)
+	}
+
+	if weightBandCount != weightBandCountExpected {
+		fmt.Errorf("Exepected %d weight bands, found %d defined in golang parser\n", weightBandCountExpected, weightBandCount)
+	}
 
 
-// ParseStations parses a spreadsheet of duty stations into DutyStationRow structs
-func (b *MigrationBuilder) parseStations(path string) ([]DutyStationWrapper, error) {
-	var stations []DutyStationWrapper
-
-	xlFile, err := xlsx.OpenFile(path)
+	xlFile, err := xlsx.OpenFile(parseFile)
 	if err != nil {
-		return stations, err
+		return err
 	}
 
-	// Skip the first header row
-	dataRows := xlFile.Sheets[0].Rows[1:]
+	feeColIndexStart := 6 // start at column 6 to get the rates
+	colIndex := feeColIndexStart
+
+	dataRows := xlFile.Sheets[6].Rows[14:]
 	for _, row := range dataRows {
-		parsed := DutyStationWrapper{
-			TransportationOfficeName: getCell(row.Cells, 8),
-			DutyStation: models.DutyStation{
-				Name:        getCell(row.Cells, 0),
-				Affiliation: affiliationMap[getCell(row.Cells, 1)],
-				Address: models.Address{
-					StreetAddress1: getCell(row.Cells, 2),
-					StreetAddress2: stringPointer(getCell(row.Cells, 3)),
-					StreetAddress3: stringPointer(getCell(row.Cells, 4)),
-					City:           getCell(row.Cells, 5),
-					State:          getCell(row.Cells, 6),
-					PostalCode:     floatFormatter(getCell(row.Cells, 7)),
-					Country:        stringPointer("United States"),
-				},
-			},
+		// For number of baseline + escalation years
+		colIndex = feeColIndexStart
+		numEscalationYears := 5
+		for escalation := 0; escalation < numEscalationYears; escalation++ {
+			// For each rate season
+			for _, r := range rateTypes {
+				// For each weight band
+				for _, w := range weightBands {
+					// For each milage range
+					for _, m := range milesRanges {
+						domesticLineHaulPrice := domesticLineHaulPrice{
+							serviceAreaNumber: getInt(getCell(row.Cells, 2)),
+							originServiceArea: getCell(row.Cells, 3),
+							serviceSchedule: getInt(getCell(row.Cells, 4)),
+							season: r,
+							weightBand: w,
+							milesRange: m,
+							optionPeriodYearCount: escalation,
+							rate: getCell(row.Cells, colIndex),
+						}
+						colIndex++
+						fmt.Printf("%v ", domesticLineHaulPrice)
+					}
+				}
+				colIndex++ // skip 1 column (empty column) before starting next rate type
+
+			}
 		}
-		stations = append(stations, parsed)
 	}
 
-	return stations, nil
+	//
+
+	return nil
 }
+
 
