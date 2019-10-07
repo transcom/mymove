9731a5501fb1b551c89990b5c07968d9cefc8e98
Reggie Riser <reggie@truss.works>
2019-10-07 21:53:57 +0000

    Added models/tests for re_contracts and re_contract_years

---
+++ pkg/models/re_contracts_test.go
@@ -0,0 +1,34 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+)
+
+func (suite *ModelSuite) TestReContractValidations() {
+	suite.T().Run("test valid ReContract", func(t *testing.T) {
+		newID, _ := uuid.NewV4()
+		validReContract := models.ReContract{
+			ID:        newID,
+			Code:      "ABC",
+			Name:      "ABC, Inc.",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		expErrors := map[string][]string{}
+		suite.verifyValidationErrors(&validReContract, expErrors)
+	})
+
+	suite.T().Run("test empty ReContract", func(t *testing.T) {
+		emptyReContract := &models.ReContract{}
+		expErrors := map[string][]string{
+			"code": {"Code can not be blank."},
+			"name": {"Name can not be blank."},
+		}
+		suite.verifyValidationErrors(emptyReContract, expErrors)
+	})
+}
