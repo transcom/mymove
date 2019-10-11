5fee063cdda8a737d7da9d2195b89fb6c65618cf
Garrett Martin <garrett@truss.works>
2019-10-11 20:55:14 +0000

    Pass strings to error

--- cmd/ecs-service-logs/main.go
+++ cmd/ecs-service-logs/main.go
@@ -464,7 +464,7 @@ func showFunction(cmd *cobra.Command, args []string) error {
 		endTimeUnix = aws.Int64(endTime.Unix() * 1000)     // milliseconds
 
 		if *startTimeUnix > *endTimeUnix {
-			return &errInvalidTimeRange{StartTime: startTime, EndTime: endTime}
+			return &errInvalidTimeRange{StartTime: startTime.String(), EndTime: endTime.String()}
 		}
 	}
 
