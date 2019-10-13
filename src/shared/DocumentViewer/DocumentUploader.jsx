db7c3bed15faf6dd6f366c435e5152986697415a
Sara Bocciardi <sara@truss.works>
2019-10-13 02:47:38 +0000

    Update grid for document viewer.

--- src/shared/DocumentViewer/DocumentUploader.jsx
+++ src/shared/DocumentViewer/DocumentUploader.jsx
@@ -87,8 +87,8 @@ export class DocumentUploader extends Component {
     return (
       <Fragment>
         {this.state.moveDocumentCreateError && (
-          <div className="usa-grid">
-            <div className="usa-width-one-whole error-message">
+          <div className="grid-row">
+            <div className="grid-col-12 error-message">
               <Alert type="error" heading="An error occurred">
                 Something went wrong contacting the server.
               </Alert>
