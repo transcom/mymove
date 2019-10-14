fe4c886ffd8266b564056fb79bb08bc6a503e6db
Matt Krump <matt@truss.works>
2019-10-14 22:00:13 +0000

    Create bare muxer for local dev fileserver

--- pkg/middleware/security_headers.go
+++ pkg/middleware/security_headers.go
@@ -10,9 +10,9 @@ var securityHeaders = map[string]string{
 	"strict-transport-security": "max-age=31536000; includeSubdomains; preload",
 	// Sets headers to prevent rendering our page in an iframe, prevents clickjacking
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-	"X-Frame-Options": "sameorigin",
+	"X-Frame-Options": "deny",
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/frame-ancestors
-	"Content-Security-Policy": `frame-ancestors 'self'`,
+	"Content-Security-Policy": "frame-ancestors 'none'",
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
 	"X-XSS-Protection": "1; mode=block",
 }
