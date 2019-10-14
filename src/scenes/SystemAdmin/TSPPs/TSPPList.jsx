39f54081401e336580cb5308c4b37d4369f3bdb5
Donald Thai <donald@truss.works>
2019-10-14 18:02:31 +0000

    switched to dateField to show dates

--- src/scenes/SystemAdmin/TSPPs/TSPPList.jsx
+++ src/scenes/SystemAdmin/TSPPs/TSPPList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Datagrid, List, TextField } from 'react-admin';
+import { Datagrid, List, TextField, DateField } from 'react-admin';
 import AdminPagination from 'scenes/SystemAdmin/shared/AdminPagination';
 
 const TSPPList = props => (
@@ -8,10 +8,10 @@ const TSPPList = props => (
       <TextField source="id" reference="transportation_service_provider_performances" />
       <TextField source="traffic_distribution_list_id" reference="transportation_service_provider_performances" />
       <TextField source="transportation_service_provider_id" reference="transportation_service_provider_performances" />
-      <TextField source="performance_period_start" reference="transportation_service_provider_performances" />
-      <TextField source="performance_period_end" reference="transportation_service_provider_performances" />
-      <TextField source="rate_cycle_start" reference="transportation_service_provider_performances" />
-      <TextField source="rate_cycle_end" reference="transportation_service_provider_performances" />
+      <DateField source="performance_period_start" reference="transportation_service_provider_performances" />
+      <DateField source="performance_period_end" reference="transportation_service_provider_performances" />
+      <DateField source="rate_cycle_start" reference="transportation_service_provider_performances" />
+      <DateField source="rate_cycle_end" reference="transportation_service_provider_performances" />
     </Datagrid>
   </List>
 );
