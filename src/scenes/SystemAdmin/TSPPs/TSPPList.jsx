21bc4fc55fad31fa5fb51ee4889d59529482e0bf
Donald Thai <donald@truss.works>
2019-10-14 19:16:32 +0000

    add filter to tspp list ui

--- src/scenes/SystemAdmin/TSPPs/TSPPList.jsx
+++ src/scenes/SystemAdmin/TSPPs/TSPPList.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Datagrid, List, TextField, DateField } from 'react-admin';
+import { Datagrid, List, TextField, DateField, Filter, TextInput } from 'react-admin';
 import AdminPagination from 'scenes/SystemAdmin/shared/AdminPagination';
+import styles from 'scenes/SystemAdmin/Home.module.scss';
+
+const TSPPFilter = props => (
+  <Filter {...props} className={styles['system-admin-filters']}>
+    <TextInput
+      label="Traffic distribution list id"
+      source="traffic_distribution_list_id"
+      reference="transportation_service_provider_performances"
+    />
+    <TextInput
+      label="Transportation service provider id"
+      source="transportation_service_provider_id"
+      reference="transportation_service_provider_performances"
+    />
+  </Filter>
+);
 
 const TSPPList = props => (
-  <List {...props} pagination={<AdminPagination />} perPage={25} bulkActionButtons={false}>
+  <List {...props} pagination={<AdminPagination />} perPage={25} filters={<TSPPFilter />} bulkActionButtons={false}>
     <Datagrid>
       <TextField source="id" reference="transportation_service_provider_performances" />
       <TextField source="traffic_distribution_list_id" reference="transportation_service_provider_performances" />
