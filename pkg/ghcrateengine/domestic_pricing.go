0848cfca7d1b58bea217320a46b4b657aa88fa69
Kim Allen <kim@truss.works>
2019-10-16 19:19:49 +0000

    Convert pseudocode and existing query to raw queries

--- pkg/ghcrateengine/domestic_pricing.go
+++ pkg/ghcrateengine/domestic_pricing.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/transcom/mymove/pkg/models"
+
 	"github.com/gobuffalo/pop"
 	"github.com/gofrs/uuid"
 	"github.com/pkg/errors"
@@ -22,74 +24,105 @@ type DomesticServicePricingData struct {
 	ServiceCode   string // may change to Service when model is available
 }
 
-func lookupDomesticLinehaulRate(db *pop.Connection, d DomesticServicePricingData) (rate unit.Millicents, err error) {
-	// TODO: check/correct syntax && implement when models are created
-	// query := db.Where(
-	// 	"is_peak_period = d.IsPeakPeriod").Join(
-	// 	"service_areas sa", "sa.id=re_domestic_linehaul_prices.service_area_id").Join(
-	// 	"re_contracts c", "re_contract.id=re_domestic_linehaul_prices.contract_id").Join(
-	// 	"re_contract_years cy", "re_contract.id=cy.contract_id").Where(
-	// 		"sa.id=d.ServiceAreaID").Where(
-	// 		"weight gte weight_lower").Where(
-	// 			"weight lte weight_upper").Where(
-	// 				"distance gte miles_lower").Where(
-	// 					"distance lte miles_upper").Where(
-	// 										"re_contracts.code=?", d.ContractCode).Where(
-	// 											"d.MoveDate gte cy.start_date").Where(
-	// 												"d.MoveDate lte cy.end_date")
+func lookupDomesticLinehaulRate(db *pop.Connection, pricingData DomesticServicePricingData) (rate unit.Millicents, err error) {
 
-	// domesticLinehaulPricing := models.DomesticLinehaulPricing{}
-	// err := db.query.All(&domesticLinehaulPricing)
-	// if err != nil {
-	// 	return err
-	// }
+	domesticLinehaulPrice := models.ReDomesticLinehaulPrice{}
+	query := `
+		select price_cents from re_domestic_linehaul_prices lh
+		inner join re_domestic_service_areas sa on sa.id = lh.domestic_service_area_id
+		inner join re_contracts on re_contracts.id = lh.contract_id
+		inner join re_contract_years on re_contracts.id = re_contract_years.contract_id
+		where lh.is_peak_period = $1
+		and $2 between lh.weight_lower and lh.weight_upper
+		and $3 between lh.miles_lower and lh.miles_upper
+		and $4 between re_contract_year.start_date and re_contract_year.end_date
+		and $5 re_contracts.code = $6
+	`
+	err = db.RawQuery(
+		query, pricingData.IsPeakPeriod, pricingData.Weight, pricingData.Distance, pricingData.MoveDate, pricingData.ContractCode).First(
+		&domesticLinehaulPrice)
+	if err != nil {
+		return rate, errors.Wrap(err, "Fetch domestic other price failed")
+	}
 
-	rate = 272700 // stubbed
+	//rate = domesticLinehaulPrice.PriceMillicents TODO: uncomment and remove stubbed rate when connected to db
 
-	return rate, err
+	//return rate, err
+	stubbedRate := unit.Millicents(272700) // stubbed
+
+	return stubbedRate, err
 }
 
-func lookupDomesticServiceAreaPrice(db *pop.Connection, pricingData DomesticServicePricingData) (cost unit.Cents, err error) {
-	// select cost from re_domestic_rate_area_prices
-	// joined with re_service_area, re_contracts, re_contract_years, re_services
-	// where:
-	// re_service_area.id = pricingData.ServiceAreaID
-	// re_services.code = pricingData.ServiceCode
-	// re_contracts.code = pricingData.ContractCode
-	// is_peak_period = pricingData.IsPeakPeriod
-	// move date is between start and end dates of contract year
+func lookupDomesticServiceAreaPrice(db *pop.Connection, pricingData DomesticServicePricingData) (rate unit.Cents, err error) {
+	domesticServiceAreaPrice := models.ReDomesticServiceAreaPrice{}
+
+	query := `
+		select price_price from re_domestic_service_area_prices dsap
+		inner join re_domestic_service_areas sa on sa.id = dsap.domestic_service_area_id
+		inner join re_contracts on re_contracts.id = dsap.contract_id
+		inner join re_contract_years on re_contracts.id = re_contract_years.contract_id
+		inner join re_services on re_services.id = dsap.services_id
+		where sa.id = $1
+		and re_services.code = $2
+		and re_contracts.code = $3
+		and dsap.is_peak_period = $4
+		and $5 between re_contract_year.start_date and re_contract_year.end_date;'
+	`
+	err = db.RawQuery(
+		query, pricingData.ServiceAreaID, pricingData.ServiceAreaID, pricingData.ContractCode, pricingData.IsPeakPeriod, pricingData.MoveDate).First(
+		&domesticServiceAreaPrice)
+	if err != nil {
+		return rate, errors.Wrap(err, "Fetch domestic other price failed")
+	}
+	rate = domesticServiceAreaPrice.PriceCents
+	//return rate, err TODO: uncomment and remove stubbed rate when connected to db
 
 	stubbedRate, err := unit.Cents(689), nil
 	return stubbedRate, err
 }
 
-func lookupDomesticOtherPrice(db *pop.Connection, pricingData DomesticServicePricingData) (cost unit.Cents, err error) {
-	serviceCode := "SITPD" // stubbed service code
-	var stubbedRate unit.Cents
+func lookupDomesticOtherPrice(db *pop.Connection, pd DomesticServicePricingData) (rate unit.Cents, err error) {
+	var domesticOtherPrice models.ReDomesticOtherPrice
+	domesticServiceArea := models.ReDomesticServiceArea{}
 
-	if pricingData.ServiceCode == serviceCode {
-		//SIT PD Schedule
-		// select cost from re_domestic_other_prices
-		// joined with re_service_area, re_contracts, re_contract_years, re_services
-		// where:
-		// re_domestic_service_areas.s_i_t_p_d_schedule = schedule
-		// re_services.code = pricingData.ServiceCode
-		// re_contracts.code = pricingData.ContractCode
-		// is_peak_period = pricingData.IsPeakPeriod
-		// move date is between start and end dates of contract year
-		stubbedRate, err = 23440, nil
+	var schedule int
+	sitPDServiceCode := "SITPD" // stubbed service code
+	packServiceCode := "DPK"    // stubbed service code
+	unpackServiceCode := "DUPK" // stubbed service code
+
+	err = db.Q().Where(fmt.Sprintf("id = %s", pd.ServiceAreaID)).Last(&domesticServiceArea)
+	if pd.ServiceCode == sitPDServiceCode {
+		schedule = domesticServiceArea.SITPDSchedule
+	} else if pd.ServiceCode == packServiceCode || pd.ServiceCode == unpackServiceCode {
+		schedule = domesticServiceArea.ServiceSchedule
 	} else {
-		// PACK AND UNPACK
-		// select cost from re_domestic_other_prices
-		// joined with re_service_area, re_contracts, re_contract_years, re_services
-		// where:
-		// re_domestic_service_areas.services_schedule = schedule
-		// re_services.code = pricingData.ServiceCode
-		// re_contracts.code = pricingData.ContractCode
-		// is_peak_period = pricingData.IsPeakPeriod
-		// move date is between start and end dates of contract year
-		stubbedRate, err = 6333, nil
+		// throw error??
+		return rate, errors.Wrap(err, "must be pack, unpack, or SIT P/D service")
 	}
+
+	query := `
+		select per_unit_price from re_domestic_other_prices
+         inner join re_contracts on re_domestic_other_prices.contract_id = re_contracts.id
+         inner join re_contract_years on re_contracts.id = re_contract_years.contract_id
+		 inner join re_services on re_services.id = re_domestic_other_prices.services_id
+         where re_domestic_other_prices.schedule = $1
+		  and re_contracts.code = $2
+		  and $3 between re_contract_years.start_date and re_contract_years.end_date
+		  and re_domestic_linehaul_prices.is_peak_period = $4
+		  and $5 between re_domestic_linehaul_prices.weight_lower and re_domestic_linehaul_prices.weight_upper
+		  and re_domestic_service_areas.id = $6;
+	`
+
+	err = db.RawQuery(
+		query, schedule, pd.ContractCode, pd.MoveDate, pd.IsPeakPeriod, pd.Weight, pd.ServiceAreaID).First(
+		&domesticOtherPrice)
+	if err != nil {
+		return rate, errors.Wrap(err, "Fetch domestic other price failed")
+	}
+
+	//return domesticOtherPrice.PerUnitPrice, err TODO: uncomment and remove stubbed rate when connected to db
+
+	stubbedRate, err := unit.Cents(23440), nil
 
 	return stubbedRate, err
 }
