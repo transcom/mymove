2d96cb4dd25a56cd76a39fc24056d47a7ed4da73
lindsay t <techells22@gmail.com>
2019-10-17 14:35:22 +0000

    add address handler showAddress

--- pkg/handlers/internalapi/addresses.go
+++ pkg/handlers/internalapi/addresses.go
@@ -1,10 +1,15 @@
 package internalapi
 
 import (
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
+	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 
+	addressop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/addresses"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
+	"github.com/transcom/mymove/pkg/handlers"
 	"github.com/transcom/mymove/pkg/models"
 )
 
@@ -47,4 +52,23 @@ func updateAddressWithPayload(a *models.Address, payload *internalmessages.Addre
 	a.State = *payload.State
 	a.PostalCode = *payload.PostalCode
 	a.Country = payload.Country
+}
+
+// ShowAddressHandler returns an address
+type ShowAddressHandler struct {
+	handlers.HandlerContext
+}
+
+// Handle returns a address given an addressId
+func (h ShowAddressHandler) Handle(params addressop.ShowAddressParams) middleware.Responder {
+	logger := h.LoggerFromRequest(params.HTTPRequest)
+	addressID, err := uuid.FromString(params.AddressID.String())
+
+	address := models.FetchAddressByID(h.DB(), &addressID)
+	if err != nil {
+		logger.Error("Finding address", zap.Error(err))
+	}
+
+	addressPayload := payloadForAddressModel(address)
+	return addressop.NewShowAddressOK().WithPayload(addressPayload)
 }
