681d73603de86d760385fcdd36b40ba0ab62245a
Sara Bocciardi <sara@truss.works>
2019-10-21 15:52:54 +0000

    Fix footer.

--- src/shared/Footer/index.jsx
+++ src/shared/Footer/index.jsx
@@ -4,74 +4,86 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="usa-footer usa-footer-medium" role="contentinfo">
-      <div className="usa-grid usa-footer-return-to-top" />
-      <div className="usa-footer-primary-section">
-        <div className="usa-grid-full">
-          <nav className="usa-footer-nav">
-            <ul className="usa-unstyled-list">
-              <li className="usa-width-one-fourth usa-footer-primary-content">
-                <a className="usa-footer-primary-link" href="https://www.move.mil/">
-                  Move.mil
-                </a>
-              </li>
-              <li className="usa-width-one-fourth usa-footer-primary-content">
-                <a className="usa-footer-primary-link" href="mailto:transcom.scott.tcj5j4.mbx.ppcf@mail.mil">
-                  Help Me
-                </a>
-              </li>
-              <li className="usa-width-one-fourth usa-footer-primary-content">
-                <Link className="usa-footer-primary-link" to="/accessibility">
-                  Accessibility
-                </Link>
-              </li>
-              <li className="usa-width-one-fourth usa-footer-primary-content">
-                <Link className="usa-footer-primary-link" to="/privacy-and-security-policy">
-                  Privacy and Security Policy
-                </Link>
-              </li>
-            </ul>
-          </nav>
+    <footer className="usa-footer" role="contentinfo">
+      <div className="grid-container usa-footer__return-to-top" />
+      <div className="usa-footer__primary-section">
+        <div className="usa-footer__primary-container grid-row">
+          <div className="grid-col-12">
+            <nav className="usa-footer__nav">
+              <ul className="grid-row grid-gap">
+                <li className="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
+                  <a className="usa-footer__primary-link" href="https://www.move.mil/">
+                    Move.mil
+                  </a>
+                </li>
+                <li className="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
+                  <a className="usa-footer__primary-link" href="mailto:transcom.scott.tcj5j4.mbx.ppcf@mail.mil">
+                    Help Me
+                  </a>
+                </li>
+                <li className="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
+                  <Link className="usa-footer__primary-link" to="/accessibility">
+                    Accessibility
+                  </Link>
+                </li>
+                <li className="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
+                  <Link className="usa-footer__primary-link" to="/privacy-and-security-policy">
+                    Privacy and Security Policy
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
-      <div className="usa-footer-secondary_section">
-        <div className="usa-grid">
-          <div className="usa-footer-logo usa-width-one-half">
-            <a href="https://www.ustranscom.mil/">
-              <img
-                className="usa-footer-logo-img _fix-ie-11-height"
-                src={transcomEmblem}
-                alt="United States Transportation Command Emblem"
-              />
-              <br />
-              <h3 className="usa-footer-big-logo-heading">USTRANSCOM</h3>
-              <span>United States Transportation Command</span>
-              <br />
-            </a>
-          </div>
-          <div className="usa-footer-contact-links usa-width-one-half">
-            <a
-              className="usa-link-twitter"
-              href="https://twitter.com/us_transcom"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>Twitter</span>
-            </a>
-            <a
-              className="usa-link-facebook"
-              href="https://www.facebook.com/USTRANSCOM/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span>Facebook</span>
-            </a>
-            <address>
-              <h3 className="usa-footer-contact-heading">Contact Us</h3>
-              <p>
-                <a href="https://move.mil/customer-service">Customer service</a>
-              </p>
-            </address>
+      <div className="usa-footer__secondary-section">
+        <div className="grid-container">
+          <div className="grid-row grid-gap">
+            <div className="usa-footer__logo grid-row mobile-lg:grid-col-6 mobile-lg:grid-gap-2">
+              <div className="mobile-lg:grid-col-auto">
+                <img
+                  className="usa-footer__logo-img _fix-ie-11-height"
+                  src={transcomEmblem}
+                  alt="United States Transportation Command Emblem"
+                />
+              </div>
+              <div className="mobile-lg:grid-col-auto">
+                <a href="https://www.ustranscom.mil/">
+                  <h3 className="usa-footer__logo-heading">USTRANSCOM</h3>
+                  <span>United States Transportation Command</span>
+                </a>
+              </div>
+            </div>
+            <div className="usa-footer__contact-links mobile-lg:grid-col-6">
+              <div className="usa-footer__social-links grid-row grid-gap-1">
+                <div className="grid-col-auto">
+                  <a
+                    className="usa-social-link usa-social-link--twitter"
+                    href="https://twitter.com/us_transcom"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Twitter</span>
+                  </a>
+                </div>
+                <div className="grid-col-auto">
+                  <a
+                    className="usa-social-link usa-social-link--facebook"
+                    href="https://www.facebook.com/USTRANSCOM/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Facebook</span>
+                  </a>
+                </div>
+              </div>
+              <h3 className="usa-footer__contact-heading">Contact Us</h3>
+              <address className="usa-footer__address">
+                <div className="usa-footer__contact-info grid-row grid-gap">
+                  <a href="https://move.mil/customer-service">Customer service</a>
+                </div>
+              </address>
+            </div>
           </div>
         </div>
       </div>
