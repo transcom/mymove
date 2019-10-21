c887991538eec52b6188b4674ad9b469380e12c1
Sara Bocciardi <sara@truss.works>
2019-10-21 20:18:16 +0000

    Fix layout for legalese page.

--- src/scenes/Moves/Ppm/CustomerAgreementLegalese.js
+++ src/scenes/Moves/Ppm/CustomerAgreementLegalese.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './CustomerAgreeementLegalese.css';
 
 function CustomerAgreementLegalese(props) {
   function goBack() {
@@ -7,42 +6,47 @@ function CustomerAgreementLegalese(props) {
   }
 
   return (
-    <div className="usa-grid customer-agreement-legalese-container">
-      <div>
-        <a onClick={goBack} className="usa-link">
-          {'<'} Back
-        </a>
-      </div>
-      <h2>Customer Agreement</h2>
-      <p style={{ marginBottom: '20px' }}>
-        Before submitting your payment request, please carefully read the following:
-      </p>
-      <div className="usa-grid-full customer-agreement-legalese-text">
-        <h4>LEGAL AGREEMENT / PRIVACY ACT</h4>
-        <h4>Financial Liability:</h4>
-        If this shipment(s) incurs costs above the allowance I am entitled to, I will pay the difference to the
-        government, or consent to the collection from my pay as necessary to cover all excess costs associated by this
-        shipment(s).
-        <h4>Advance Obligation:</h4>
-        <p>
-          I understand that the maximum advance allowed is based on the estimated weight and scheduled departure date of
-          my shipment(s). In the event, less weight is moved or my move occurs on a different scheduled departure date,
-          I may have to remit the difference with the balance of my incentive disbursement and/or from the collection of
-          my pay as may be necessary.
-        </p>
-        <p>
-          I understand that the maximum advance allowed is based on the estimated weight and scheduled departure date of
-          my shipment(s). In the event, less weight is moved or my move occurs on a different scheduled departure date,
-          I may have to remit the difference with the balance of my incentive disbursement and/or from the collection of
-          my pay as may be necessary. If I receive an advance for my PPM shipment, I agree to furnish weight tickets
-          within 45 days of final delivery to my destination. I understand that failure to furnish weight tickets within
-          this timeframe may lead to the collection of my pay as necessary to cover the cost of the advance.
-        </p>
-      </div>
-      <div className="usa-grid button-bar">
-        <button className="usa-button usa-button--outline" onClick={goBack}>
-          Back
-        </button>
+    <div className="grid-container usa-prose site-prose customer-agreement-legalese-container">
+      <div className="grid-row">
+        <div className="grid-col-12">
+          <div>
+            <a onClick={goBack} className="usa-link">
+              {'<'} Back
+            </a>
+          </div>
+          <h2>Customer Agreement</h2>
+          <p style={{ marginBottom: '20px' }}>
+            Before submitting your payment request, please carefully read the following:
+          </p>
+          <div className="usa-grid-full customer-agreement-legalese-text">
+            <h4>LEGAL AGREEMENT / PRIVACY ACT</h4>
+            <h4>Financial Liability:</h4>
+            If this shipment(s) incurs costs above the allowance I am entitled to, I will pay the difference to the
+            government, or consent to the collection from my pay as necessary to cover all excess costs associated by
+            this shipment(s).
+            <h4>Advance Obligation:</h4>
+            <p>
+              I understand that the maximum advance allowed is based on the estimated weight and scheduled departure
+              date of my shipment(s). In the event, less weight is moved or my move occurs on a different scheduled
+              departure date, I may have to remit the difference with the balance of my incentive disbursement and/or
+              from the collection of my pay as may be necessary.
+            </p>
+            <p>
+              I understand that the maximum advance allowed is based on the estimated weight and scheduled departure
+              date of my shipment(s). In the event, less weight is moved or my move occurs on a different scheduled
+              departure date, I may have to remit the difference with the balance of my incentive disbursement and/or
+              from the collection of my pay as may be necessary. If I receive an advance for my PPM shipment, I agree to
+              furnish weight tickets within 45 days of final delivery to my destination. I understand that failure to
+              furnish weight tickets within this timeframe may lead to the collection of my pay as necessary to cover
+              the cost of the advance.
+            </p>
+          </div>
+          <div className="usa-grid button-bar">
+            <button className="usa-button usa-button--outline" onClick={goBack}>
+              Back
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
