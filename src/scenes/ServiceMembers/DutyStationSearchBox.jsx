913f2f79502d4555405f3553bb139d963a5b0bbe
Sara Bocciardi <sara@truss.works>
2019-10-21 17:49:29 +0000

    Editing styles for duty station upload.

--- src/scenes/ServiceMembers/DutyStationSearchBox.jsx
+++ src/scenes/ServiceMembers/DutyStationSearchBox.jsx
@@ -98,7 +98,7 @@ export class DutyStationSearchBox extends Component {
     const defaultTitle = 'Name of Duty Station:';
     const inputContainerClasses = classNames({ 'usa-input-error': errorMsg });
     const searchBoxHeaderClasses = classNames({ 'duty-station-header': errorMsg });
-    const dutyInputClasses = classNames('duty-input-box', 'usa-input', {
+    const dutyInputClasses = classNames('duty-input-box', {
       [this.props.input.name]: true,
       'duty-input-box-error': errorMsg,
     });
@@ -107,6 +107,35 @@ export class DutyStationSearchBox extends Component {
     // if there is no duty station, that object will have a null uuid
     const isEmptyStation = get(this.props, 'input.value.id', NULL_UUID) === NULL_UUID;
     const title = this.props.title || defaultTitle;
+    const uswdsBlack = '#565c65';
+    const customStyles = {
+      control: provided => ({
+        ...provided,
+        borderRadius: '0px',
+        borderColor: uswdsBlack,
+        padding: '0.1rem',
+        maxWidth: '30rem',
+        ':hover': {
+          ...styles[':hover'],
+          borderColor: uswdsBlack,
+        },
+      }),
+      dropdownIndicator: provided => ({
+        ...provided,
+        color: uswdsBlack,
+        ':hover': {
+          ...styles[':hover'],
+          color: uswdsBlack,
+        },
+      }),
+      indicatorSeparator: provided => ({
+        ...provided,
+        backgroundColor: uswdsBlack,
+      }),
+      placeholder: provided => ({
+        color: uswdsBlack,
+      }),
+    };
     return (
       <Fragment>
         <div className="duty-station-search usa-form-group">
@@ -133,6 +162,7 @@ export class DutyStationSearchBox extends Component {
               value={isEmptyStation ? null : this.props.input.value}
               noOptionsMessage={this.noOptionsMessage}
               placeholder="Start typing a duty station..."
+              styles={customStyles}
             />
             {!isEmptyStation && (
               <p className={locationClasses}>
