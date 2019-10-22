08eb88227755bd68b2cda49c2004efa7ac390bcc
Reggie Riser <reggie@truss.works>
2019-10-22 16:22:27 +0000

    Added testdatagen functions for making test data; reorganized test code accordingly

--- pkg/services/ghcrateengine/price_domestic_linehaul_test.go
+++ pkg/services/ghcrateengine/price_domestic_linehaul_test.go
@@ -6,21 +6,17 @@ import (
 
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
+	"github.com/transcom/mymove/pkg/testdatagen"
 	"github.com/transcom/mymove/pkg/unit"
-)
-
-const (
-	testContractCode = "TEST"
-	testContractYear = 2019
 )
 
 func (suite *GHCRateEngineServiceSuite) TestPriceDomesticLinehaul() {
 	suite.setupDomesticLinehaulData()
-	domesticLinehaulPricer := NewDomesticLinehaulPricer(suite.DB(), suite.logger, testContractCode)
+	domesticLinehaulPricer := NewDomesticLinehaulPricer(suite.DB(), suite.logger, testdatagen.DefaultContractCode)
 
 	suite.T().Run("success within peak period", func(t *testing.T) {
 		pricingData := services.DomesticServicePricingData{
-			MoveDate:    time.Date(testContractYear, peakStart.month, peakStart.day, 0, 0, 0, 0, time.UTC),
+			MoveDate:    time.Date(testdatagen.TestYear, peakStart.month, peakStart.day, 0, 0, 0, 0, time.UTC),
 			Distance:    unit.Miles(1200),
 			Weight:      unit.Pound(4000),
 			ServiceArea: "004",
@@ -34,7 +30,7 @@ func (suite *GHCRateEngineServiceSuite) TestPriceDomesticLinehaul() {
 	suite.T().Run("success within non-peak period", func(t *testing.T) {
 		nonPeakDate := peakStart.addDate(0, -1)
 		pricingData := services.DomesticServicePricingData{
-			MoveDate:    time.Date(testContractYear, nonPeakDate.month, nonPeakDate.day, 0, 0, 0, 0, time.UTC),
+			MoveDate:    time.Date(testdatagen.TestYear, nonPeakDate.month, nonPeakDate.day, 0, 0, 0, 0, time.UTC),
 			Distance:    unit.Miles(1200),
 			Weight:      unit.Pound(4000),
 			ServiceArea: "004",
@@ -47,7 +43,7 @@ func (suite *GHCRateEngineServiceSuite) TestPriceDomesticLinehaul() {
 
 	suite.T().Run("weight below minimum", func(t *testing.T) {
 		pricingData := services.DomesticServicePricingData{
-			MoveDate:    time.Date(testContractYear, peakStart.month, peakStart.day, 0, 0, 0, 0, time.UTC),
+			MoveDate:    time.Date(testdatagen.TestYear, peakStart.month, peakStart.day, 0, 0, 0, 0, time.UTC),
 			Distance:    unit.Miles(1200),
 			Weight:      minDomesticWeight / 2,
 			ServiceArea: "004",
@@ -60,7 +56,7 @@ func (suite *GHCRateEngineServiceSuite) TestPriceDomesticLinehaul() {
 
 	suite.T().Run("date outside of valid contract year", func(t *testing.T) {
 		pricingData := services.DomesticServicePricingData{
-			MoveDate:    time.Date(testContractYear-1, time.January, 1, 0, 0, 0, 0, time.UTC),
+			MoveDate:    time.Date(testdatagen.TestYear-1, time.January, 1, 0, 0, 0, 0, time.UTC),
 			Distance:    unit.Miles(1200),
 			Weight:      unit.Pound(4000),
 			ServiceArea: "004",
@@ -72,52 +68,37 @@ func (suite *GHCRateEngineServiceSuite) TestPriceDomesticLinehaul() {
 }
 
 func (suite *GHCRateEngineServiceSuite) setupDomesticLinehaulData() {
-	contract := models.ReContract{
-		Code: testContractCode,
-		Name: "Test Contract",
-	}
-	suite.MustSave(&contract)
+	contractYear := testdatagen.MakeReContractYear(suite.DB(),
+		testdatagen.Assertions{
+			ReContractYear: models.ReContractYear{
+				Escalation:           1.0197,
+				EscalationCompounded: 1.04071,
+			},
+		})
 
-	contractYear := models.ReContractYear{
-		ContractID:           contract.ID,
-		Name:                 "Base Period Year 3",
-		StartDate:            time.Date(testContractYear, time.January, 1, 0, 0, 0, 0, time.UTC),
-		EndDate:              time.Date(testContractYear, time.December, 31, 0, 0, 0, 0, time.UTC),
-		Escalation:           1.0197,
-		EscalationCompounded: 1.04071,
-	}
-	suite.MustSave(&contractYear)
+	serviceArea := testdatagen.MakeReDomesticServiceArea(suite.DB(),
+		testdatagen.Assertions{
+			ReDomesticServiceArea: models.ReDomesticServiceArea{
+				ServiceArea: "004",
+			},
+		})
 
-	serviceArea := models.ReDomesticServiceArea{
-		BasePointCity:    "Birmingham",
-		State:            "AL",
-		ServiceArea:      "004",
-		ServicesSchedule: 2,
-		SITPDSchedule:    2,
-	}
-	suite.MustSave(&serviceArea)
-
-	linehaulPricePeak := models.ReDomesticLinehaulPrice{
-		ContractID:            contract.ID,
+	baseLinehaulPrice := models.ReDomesticLinehaulPrice{
+		ContractID:            contractYear.Contract.ID,
 		WeightLower:           500,
 		WeightUpper:           4999,
 		MilesLower:            1001,
 		MilesUpper:            1500,
 		IsPeakPeriod:          true,
 		DomesticServiceAreaID: serviceArea.ID,
-		PriceMillicents:       5000, // 0.050
 	}
+
+	linehaulPricePeak := baseLinehaulPrice
+	linehaulPricePeak.PriceMillicents = 5000 // 0.050
 	suite.MustSave(&linehaulPricePeak)
 
-	linehaulPriceNonPeak := models.ReDomesticLinehaulPrice{
-		ContractID:            contract.ID,
-		WeightLower:           500,
-		WeightUpper:           4999,
-		MilesLower:            1001,
-		MilesUpper:            1500,
-		IsPeakPeriod:          false,
-		DomesticServiceAreaID: serviceArea.ID,
-		PriceMillicents:       4500, // 0.045
-	}
+	linehaulPriceNonPeak := baseLinehaulPrice
+	linehaulPriceNonPeak.IsPeakPeriod = false
+	linehaulPriceNonPeak.PriceMillicents = 4500 // 0.045
 	suite.MustSave(&linehaulPriceNonPeak)
 }
