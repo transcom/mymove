ec31b16eff726f335a92cba5b7e94232ad5e1702
Matt Krump <1036969+mkrump@users.noreply.github.com>
2019-10-23 10:46:17 -0600

    Add cmdline flag to enable pprof debugging routes (for local dev) (#2621)
    * Add cmdline flag to enable pprof debugging routes for local dev

---
+++ pkg/cli/debug.go
@@ -0,0 +1,21 @@
+package cli
+
+import (
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	//DebugPProfFlag enables the pprof debugging endpoints
+	DebugPProfFlag string = "debug-pprof"
+)
+
+//  InitDebugFlags initializes the Debug command line flags
+func InitDebugFlags(flag *pflag.FlagSet) {
+	flag.Bool(DebugPProfFlag, false, "Enables the go pprof debugging endpoints")
+}
+
+// CheckDebugFlags validates command line flags
+func CheckDebugFlags(v *viper.Viper) error {
+	return nil
+}
