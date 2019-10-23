cdfe8b03e5234394d20af851f6dc78c651b0049e
Kim Allen <kim@truss.works>
2019-10-23 18:49:55 +0000

    Add service for pricing domestic per weight services

--- pkg/services/ghc_rate_engine.go
+++ pkg/services/ghc_rate_engine.go
@@ -11,3 +11,10 @@ import (
 type DomesticLinehaulPricer interface {
 	PriceDomesticLinehaul(moveDate time.Time, distance unit.Miles, weight unit.Pound, serviceArea string) (unit.Cents, error)
 }
+
+// DomesticPerWeightPricer domestic prices: origin and destination service area, SIT day 1, SIT Addt'l days
+// domestic other prices: pack, unpack, and sit p/d costs for a GHC move
+//go:generate mockery -name DomesticPerWeightPricer
+type DomesticPerWeightPricer interface {
+	PricePerWeightService(moveDate time.Time, distance unit.Miles, weight unit.Pound, serviceArea string, isDomesticOtherService bool) (unit.Cents, error)
+}
