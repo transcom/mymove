40466a457fed965bf4fd4cbe301b05c661857616
John Gedeon <john@truss.works>
2019-10-31 01:02:26 +0000

    Add test and fixture for service areas

---
+++ cmd/parse_ratengine_data_ghc/parse_service_areas_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/tealeg/xlsx"
+)
+
+// Test_parseServiceAreas
+func (suite *ParseRateEngineGHCXLSXSuite) Test_parseServiceAreas() {
+	initDataSheetInfo()
+	params := paramConfig{
+		processAll:   false,
+		showOutput:   false,
+		xlsxFilename: stringPointer("fixtures/pricing_template_2019-09-19_fake-data.xlsx"),
+		xlsxSheets:   []string{"4"},
+		saveToFile:   true,
+		runTime:      time.Now(),
+		runVerify:    true,
+	}
+
+	xlsxFile, err := xlsx.OpenFile(*params.xlsxFilename)
+	params.xlsxFile = xlsxFile
+	if err != nil {
+		log.Fatalf("Failed to open file %s with error %v\n", *params.xlsxFilename, err)
+	}
+
+	const sheetIndex int = 4
+	err = parseServiceAreas(params, sheetIndex, suite.tableFromSliceCreator)
+	suite.NoError(err, "parseDomesticServiceAreas function failed")
+
+	outputFilename := xlsxDataSheets[sheetIndex].generateOutputFilename(sheetIndex, params.runTime)
+
+	const goldenFilename string = "4_1b_service_areas_golden.csv"
+	suite.helperTestExpectedFileOutput(goldenFilename, outputFilename)
+}
