d3034f1a7ac8f8c75addcdaf6ea3c3447347dce4
Garrett Martin <garrett@truss.works>
2019-11-01 19:44:59 +0000

    Change active default to false and add indexes

--- migrations/20191029161504_add_active_column.up.sql
+++ migrations/20191029161504_add_active_column.up.sql
@@ -1,14 +1,14 @@
 ALTER TABLE users
-ADD COLUMN active BOOLEAN NOT NULL DEFAULT TRUE;
+ADD COLUMN active BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE office_users
-ADD COLUMN active BOOLEAN NOT NULL DEFAULT TRUE;
+ADD COLUMN active BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE admin_users
-ADD COLUMN active BOOLEAN NOT NULL DEFAULT TRUE;
+ADD COLUMN active BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE dps_users
-ADD COLUMN active BOOLEAN NOT NULL DEFAULT TRUE;
+ADD COLUMN active BOOLEAN NOT NULL DEFAULT FALSE;
 
 UPDATE users
 SET active = NOT deactivated;
@@ -21,3 +21,8 @@ SET active = NOT deactivated;
 
 UPDATE dps_users
 SET active = NOT deactivated;
+
+CREATE INDEX users_active_idx ON public.users USING btree (active);
+CREATE INDEX office_users_active_idx ON public.office_users USING btree (active);
+CREATE INDEX admin_users_active_idx ON public.admin_users USING btree (active);
+CREATE INDEX dps_users_active_idx ON public.dps_users USING btree (active);
