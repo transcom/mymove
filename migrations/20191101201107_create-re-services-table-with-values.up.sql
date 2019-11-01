7061aeaa089398de8812b11454b11afec5ab2b80
Kim Allen <kim@truss.works>
2019-11-01 21:18:11 +0000

    Add migration to add values to re_services table

---
+++ migrations/20191101201107_create-re-services-table-with-values.up.sql
@@ -0,0 +1,39 @@
+INSERT INTO re_services
+(id, code, name, created_at, updated_at)
+INSERT INTO re_services
+(id, code, name, created_at, updated_at)
+VALUES
+('1130e612-94eb-49a7-973d-72f33685e551', 'SMSR', 'Shipment Mgmt Services', now(), now()),
+('9dc919da-9b66-407b-9f17-05c0f03fcb50', 'COSR', 'Counseling Services', now(), now()),
+('8d600f25-1def-422d-b159-617c7d59156e', 'DLH', 'Dom. Linehaul', now(), now()),
+('2bc3e5cb-adef-46b1-bde9-55570bfdd43e', 'DODP', 'Dom. O/D Price', now(), now()),
+('998beda7-e390-4a83-b15e-578a24326937', 'DFSIT', 'Dom. O/D 1st Day SIT', now(), now()),
+('05eb6ff1-5cf6-4918-b887-8260dda6b9fe', 'DASIT', 'Dom. O/D Add''l SIT', now(), now()),
+('d1a4f062-0ca3-4387-8f8e-3dd20493d0b7', 'DSPD', 'Dom. SIT Pickup/Delivery', now(), now()),
+('bdea5a8d-f15f-47d2-85c9-bba5694802ce', 'DPK', 'Dom. Packing', now(), now()),
+('15f01bc1-0754-4341-8e0f-25c8f04d5a77', 'DUP', 'Dom. Unpacking', now(), now()),
+('68417bd7-4a9d-4472-941e-2ba6aeaf15f4', 'DCRT', 'Dom. Crating', now(), now()),
+('fc14935b-ebd3-4df3-940b-f30e71b6a56c', 'DUCR', 'Dom. Uncrating', now(), now()),
+('d979e8af-501a-44bb-8532-2799753a5810', 'DSSR', 'Dom. Shuttle Service', now(), now()),
+('56bb94cd-f160-4239-a028-b31ffc641eb7', 'IOOLH', 'Int''l O->O Shipping & LH', now(), now()),
+('133bc44a-2c0f-4ff9-a61d-9f2dad349d14', 'IOOUB', 'Int''l O->O UB', now(), now()),
+('07051352-4715-49b5-88e7-045b7541919d', 'ICOLH', 'Int''l C->O Shipping & LH', now(), now()),
+('16949697-6171-47aa-bcd4-479995cc5206', 'ICOUB', 'Int''l C->O UB', now(), now()),
+('d0bb2cae-838a-4fc7-8efc-f7c6ad57431d', 'IOCLH', 'Int''l O->C Shipping & LH', now(), now()),
+('7f14f357-f2ae-42b9-b834-5ab24c2cd2af', 'IOCUB', 'Int''l O->C UB', now(), now()),
+('67ba1eaf-6ffd-49de-9a69-497be7789877', 'IHPK', 'Int''l HHG Pack', now(), now()),
+('56e91c2d-015d-4243-9657-3ed34867abaa', 'IHUP', 'Int''l HHG Unpack', now(), now()),
+('ae84d292-f885-4138-86e2-b451855ffbf2', 'IUBPK', 'Int''l UB Pack', now(), now()),
+('f2739142-97d1-40f3-a8f4-6a9daf390806', 'IUBUP', 'Int''l UB Unpack', now(), now()),
+('b488bf85-ea5e-49c8-ba5c-e2fa278ac806', 'IFSIT', 'Int''l 1st Day SIT', now(), now()),
+('bd424e45-397b-4766-9712-de4ae3a2da36', 'IASIT', 'Int''l Add''l Day SIT', now(), now()),
+('6f4f6e31-0675-4051-b659-89832259f390', 'ISPD', 'Int''l SIT Pickup / Delivery', now(), now()),
+('4b85962e-25d3-4485-b43c-2497c4365598', 'ISPD', 'Int''l SIT Pickup / Delivery', now(), now()),
+('86203d72-7f7c-49ff-82f0-5b95e4958f60', 'ICRT', 'Int''l. Crating', now(), now()),
+('4132416b-b1aa-42e7-98f2-0ac0a03e8a31', 'IUCR', 'Int''l. Uncrating', now(), now()),
+('624a97c5-dfbf-4da9-a6e9-526b4f95af8d', 'ISSR', 'Int''l. Shuttle Service', now(), now()),
+('7e1c4f99-0054-4fac-a302-1a07a1daf58e', 'NSTH', 'NonStd HHG', now(), now()),
+('a68fa051-b09d-43f3-8290-f56fc89a4fe8', 'NSTUB', 'NonStd UB', now(), now());
+
+
+
