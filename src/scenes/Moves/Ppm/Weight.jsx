77ce90a452263f55fc0fe3438afd031972a53591
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2019-11-11 20:06:33 +0000

    Add incentive box that updates as changes occur in the weight estimation slider.

--- src/scenes/Moves/Ppm/Weight.jsx
+++ src/scenes/Moves/Ppm/Weight.jsx
@@ -96,10 +96,6 @@ export class PpmWeight extends Component {
     });
   };
 
-  onWeightSliderSelection = value => {
-    console.log(value);
-  };
-
   onWeightSelected() {
     const { currentPpm, originDutyStationZip } = this.props;
     this.props.getPpmWeightEstimate(
@@ -109,6 +105,55 @@ export class PpmWeight extends Component {
       currentPpm.destination_postal_code,
       this.state.pendingPpmWeight,
     );
+  }
+
+  chooseVehicleIcon(currentEstimate) {
+    if (currentEstimate < 500) {
+      return <img className="icon" src="/static/media/car-gray.4405e309.svg" alt="car-gray" />;
+    }
+    if (currentEstimate >= 500 && currentEstimate < 1500) {
+      return <img className="icon" src="/static/media/trailer-gray.fbfa9bc3.svg" alt="trailer-gray" />;
+    }
+    if (currentEstimate >= 1500) {
+      return <img className="icon" src="/static/media/truck-gray.55075f90.svg" alt="truck-gray" />;
+    }
+  }
+
+  chooseEstimateText(currentEstimate) {
+    if (currentEstimate < 500) {
+      return <p>Just a few things. One trip in a car.</p>;
+    }
+    if (currentEstimate >= 500 && currentEstimate < 1000) {
+      return <p>Studio apartment, minimal stuff. A large car, a pickup, a van, or a car with trailer.</p>;
+    }
+    if (currentEstimate >= 1000 && currentEstimate < 2000) {
+      return <p>1-2 rooms, light furniture. A pickup, a van, or a car with a small or medium trailer.</p>;
+    }
+    if (currentEstimate >= 2000 && currentEstimate < 3000) {
+      return (
+        <p>2-3 rooms, some bulky items. Cargo van, small or medium moving truck, medium or large cargo trailer.</p>
+      );
+    }
+    if (currentEstimate >= 3000 && currentEstimate < 4000) {
+      return <p>3-4 rooms. Small to medium moving truck, or a couple of trips.</p>;
+    }
+    if (currentEstimate >= 4000 && currentEstimate < 5000) {
+      return <p>4+ rooms, or just a lot of large, heavy things. Medium or large moving truck, or multiple trips.</p>;
+    }
+    if (currentEstimate >= 5000 && currentEstimate < 6000) {
+      return <p>Many rooms, many things, lots of them heavy. Medium or large moving truck, or multiple trips.</p>;
+    }
+    if (currentEstimate >= 6000 && currentEstimate < 7000) {
+      return <p>Large house, a lot of things. The biggest rentable moving trucks, or multiple trips or vehicles.</p>;
+    }
+    if (currentEstimate >= 7000) {
+      return (
+        <p>
+          A large house or small palace, many heavy or bulky items. Multiple trips using large vehicles, or hire
+          professional movers.
+        </p>
+      );
+    }
   }
 
   render() {
@@ -143,6 +188,28 @@ export class PpmWeight extends Component {
                   [this.props.entitlement.weight]: `${this.props.entitlement.weight.toLocaleString()} lbs`,
                 }}
               />
+              {hasEstimateError && (
+                <Fragment>
+                  <div className="error-message">
+                    <Alert type="warning" heading="Could not retrieve estimate">
+                      There was an issue retrieving an estimate for your incentive. You still qualify, but need to talk
+                      with your local transportation office which you can look up on{' '}
+                      <a href="move.mil" className="usa-link">
+                        move.mil
+                      </a>
+                    </Alert>
+                  </div>
+                </Fragment>
+              )}
+            </div>
+            <div className="incentive-estimate-box border radius-lg border-base">
+              {this.chooseVehicleIcon(this.state.pendingPpmWeight)}
+              {this.chooseEstimateText(this.state.pendingPpmWeight)}
+              <h4>Your incentive for moving {this.state.pendingPpmWeight} lbs:</h4>
+              <h3 className="incentive-range-text">
+                {formatCentsRange(incentive_estimate_min, incentive_estimate_max)}
+              </h3>
+              <p className="text-gray-50">Final payment will be based on the weight you actually move.</p>
             </div>
           </div>
         )}
