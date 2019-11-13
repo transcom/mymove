f1b35b676e7161e55ea47c91e474a98f03c0dbfb
ralren <ren@truss.works>
2019-11-13 21:43:05 +0000

    Sub date with at

---
+++ local_migrations/20191113212903_add_available_to_prime_at_and_submitted_counseling_info_at.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE move_task_orders
+	ADD COLUMN submitted_counseling_info_at date,
+	ADD COLUMN available_to_prime_at date;
--- local_migrations/20191113212903_add_available_to_prime_date_and_submitted_counseling_info_date.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE move_task_orders
-	ADD COLUMN submitted_counseling_info_date date,
-	ADD COLUMN available_to_prime_date date;
