113ba6f86cc5a35c1ea05674f5f5f523f9ecb409
Kim Allen <kim@truss.works>
2019-11-25 22:54:15 +0000

    Add failure test for payment request service

--- pkg/services/payment_request/payment_request_creator_test.go
+++ pkg/services/payment_request/payment_request_creator_test.go
@@ -1,6 +1,7 @@
 package paymentrequest
 
 import (
+	"github.com/gofrs/uuid"
 	"testing"
 
 	"github.com/transcom/mymove/pkg/models"
@@ -22,5 +23,17 @@ func (suite *PaymentRequestServiceSuite) TestCreatePaymentRequest() {
 		_, verrs, err := creator.CreatePaymentRequest(&paymentRequest)
 		suite.NoError(err)
 		suite.NoVerrs(verrs)
+	})
+
+	// Bad move task order ID
+	suite.T().Run("Given an non-existent move task order id, the create should fail", func(t *testing.T) {
+		creator := NewPaymentRequestCreator(suite.DB())
+		mtoID, _ := uuid.FromString("0aee14dd-b5ea-441a-89ad-db4439fa4ea2")
+		invalidPaymentRequest := models.PaymentRequest{
+			MoveTaskOrderID: mtoID,
+			IsFinal:         false,
+		}
+		_, _, err := creator.CreatePaymentRequest(&invalidPaymentRequest)
+		suite.Error(err)
 	})
 }
