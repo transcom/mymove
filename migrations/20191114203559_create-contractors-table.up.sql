4947f2dda8d25c9fdbe528df9acde3f3c7a2756d
Ron Cruz <Ronolibert@gmail.com>
2019-11-25 14:54:24 +0000

    Add basic contract validation test and change text to varchar

--- migrations/20191114203559_create-contractors-table.up.sql
+++ migrations/20191114203559_create-contractors-table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE contractor
 	id UUID PRIMARY KEY,
     created_at timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    name text NOT NULL,
-    contract_number text UNIQUE NOT NULL,
-	type text NOT NULL
+    name varchar(80) NOT NULL,
+    contract_number varchar(80) UNIQUE NOT NULL,
+	type varchar(80) NOT NULL
 );
