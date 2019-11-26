6c637ce1620e2ca704907daecabaecaaf80dc2f6
Garrett Martin <garrett@truss.works>
2019-11-26 15:28:14 +0000

    Fix comments

--- pkg/services/fetch/list_fetcher.go
+++ pkg/services/fetch/list_fetcher.go
@@ -13,19 +13,19 @@ type listFetcher struct {
 	builder listQueryBuilder
 }
 
-// FetchOfficeUserList uses the passed query builder to fetch a list of office users
+// FetchRecordList uses the passed query builder to fetch a list of records
 func (o *listFetcher) FetchRecordList(model interface{}, filters []services.QueryFilter, associations services.QueryAssociations, pagination services.Pagination, ordering services.QueryOrder) error {
 	error := o.builder.FetchMany(model, filters, associations, pagination, ordering)
 	return error
 }
 
-// FetchRecordCount uses the passed query builder to count office users
+// FetchRecordCount uses the passed query builder to count records
 func (o *listFetcher) FetchRecordCount(model interface{}, filters []services.QueryFilter) (int, error) {
 	count, error := o.builder.Count(model, filters)
 	return count, error
 }
 
-// NewListFetcher returns an implementation of OfficeUserListFetcher
+// NewListFetcher returns an implementation of ListFetcher
 func NewListFetcher(builder listQueryBuilder) services.ListFetcher {
 	return &listFetcher{builder}
 }
