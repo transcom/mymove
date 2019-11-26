ce8c2958dfa1832af29f41719fb9baca92d88cb3
Mazdak Atighi <maz@truss.works>
2019-11-26 19:54:49 +0000

    MB-607: ma fix formatting

--- migrations/20191125211848_create_roles_table.up.sql
+++ migrations/20191125211848_create_roles_table.up.sql
@@ -1,11 +1,9 @@
-create table roles
-(
+CREATE TABLE roles (
 	id integer PRIMARY KEY,
 	type text
-)
+);
 
-create table user_role
-(
+CREATE TABLE user_role (
 	FOREIGN KEY (user_id) REFERENCES users (id),
 	FOREIGN KEY (role_id) REFERENCES roles (id)
-)
+);
