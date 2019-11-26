63dd513a7089e03ef913a744e03f09b7ac252d42
Mazdak Atighi <maz@truss.works>
2019-11-26 21:13:13 +0000

    MB-607: ma add missing commas

--- migrations/20191125211848_create_roles_table.up.sql
+++ migrations/20191125211848_create_roles_table.up.sql
@@ -4,8 +4,8 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE user_role (
-	users_id uuid NOT NULL
-	roles_id integer NOT NULL
+	users_id uuid NOT NULL,
+	roles_id integer NOT NULL,
 	FOREIGN KEY (users_id) REFERENCES users (id),
 	FOREIGN KEY (roles_id) REFERENCES roles (id)
 );
