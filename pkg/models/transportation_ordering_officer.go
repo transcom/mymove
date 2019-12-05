d768dac4c71e4d03b06b1a8f9caec0991f998b55
Matt Krump <matt@truss.works>
2019-12-05 20:43:39 +0000

    add too model

--- pkg/models/transportation_ordering_officer.go
+++ pkg/models/transportation_ordering_officer.go
@@ -1,5 +1,15 @@
-package main
+package models
 
-func main() {
+import (
+	"time"
 
+	"github.com/gofrs/uuid"
+)
+
+type TransportationOrderingOfficer struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	UserID    uuid.UUID `json:"user_id" db:"user_id"`
+	User      User      `belongs_to:"users"`
 }
