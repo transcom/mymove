e6d147dbb8da455ffa788308d2ef794dbdce31a4
Jim Benton <jim@truss.works>
2019-12-06 21:31:00 +0000

    Add uniqueness constraints and indexes.

--- migrations/20191205190530_gbloc-assignments.up.sql
+++ migrations/20191205190530_gbloc-assignments.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE gblocs
 (
     id uuid primary key,
-    code text NOT NULL,
+    code text NOT NULL UNIQUE,
     name text NOT NULL,
     city text NOT NULL,
     state_abbreviation text NOT NULL,
@@ -9,12 +9,17 @@ CREATE TABLE gblocs
     updated_at timestamp without time zone NOT NULL
 );
 
+CREATE INDEX gblocs_code_idx ON gblocs (code);
+
 CREATE TABLE gbloc_state_assignments
 (
     id uuid primary key,
     gbloc_id uuid references gblocs(id),
-    state_name text NOT NULL,
-    state_abbreviation text NOT NULL,
+    state_name text NOT NULL UNIQUE,
+    state_abbreviation text NOT NULL UNIQUE,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
 );
+
+CREATE INDEX gbloc_state_assignments_state_name_idx ON gbloc_state_assignments (state_name);
+CREATE INDEX gbloc_state_assignments_state_abbreviation_idx ON gbloc_state_assignments (state_abbreviation);
