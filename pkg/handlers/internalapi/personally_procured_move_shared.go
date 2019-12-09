8614c10e6fc5341849b0623d74379d9a517bcf17
lindsay t <techells22@gmail.com>
2019-12-09 05:11:04 +0000

    get new_duty_station postal_code from orders rec

---
+++ pkg/handlers/internalapi/personally_procured_move_shared.go
@@ -0,0 +1,38 @@
+package internalapi
+
+import (
+	"github.com/gobuffalo/pop"
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
+	"github.com/transcom/mymove/pkg/services/pagination"
+	"github.com/transcom/mymove/pkg/services/query"
+)
+
+// GetDestionDutyStationPostalCode returns the postal code associated with orders->new_duty_station->address
+func GetDestionDutyStationPostalCode(db *pop.Connection, ordersID uuid.UUID) (string, error) {
+	queryBuilder := query.NewQueryBuilder(db)
+
+	var orders models.Orders
+	filters := []services.QueryFilter{
+		query.NewQueryFilter("id", "=", ordersID),
+	}
+	associations := query.NewQueryAssociations([]services.QueryAssociation{
+		query.NewQueryAssociation("NewDutyStation.Address"),
+	})
+	page, perPage := pagination.DefaultPage(), pagination.DefaultPerPage()
+	pagination := pagination.NewPagination(&page, &perPage)
+	ordering := query.NewQueryOrder(nil, nil)
+
+	err := queryBuilder.FetchMany(&orders, filters, associations, pagination, ordering)
+	if err != nil {
+		return "", err
+	}
+
+	if len(orders) == 0 {
+		return "", models.ErrFetchNotFound
+	}
+
+	return orders[0].NewDutyStation.Address.PostalCode, nil
+}
---
+++ pkg/handlers/internalapi/personally_procured_move_shared_test.go
@@ -0,0 +1,56 @@
+package internalapi
+
+import (
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/gen/internalmessages"
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *HandlerSuite) setupPersonallyProcuredMoveSharedTest(orderID uuid.UUID) {
+	address := models.Address{
+		StreetAddress1: "some address",
+		City:           "city",
+		State:          "state",
+		PostalCode:     "12345",
+	}
+	suite.MustSave(&address)
+
+	stationName := "New Duty Station"
+	station := models.DutyStation{
+		Name:        stationName,
+		Affiliation: internalmessages.AffiliationAIRFORCE,
+		AddressID:   address.ID,
+		Address:     address,
+	}
+	suite.MustSave(&station)
+
+	_ = testdatagen.MakeOrder(suite.DB(), testdatagen.Assertions{
+		Order: models.Order{
+			ID:               orderID,
+			NewDutyStationID: station.ID,
+		},
+	})
+}
+
+func (suite *HandlerSuite) TestGetDestionDutyStationPostalCode() {
+	orderID := uuid.Must(uuid.NewV4())
+	invalidID, _ := uuid.FromString("00000000-0000-0000-0000-000000000000")
+	suite.setupPersonallyProcuredMoveSharedTest(orderID)
+
+	tests := []struct {
+		lookupID  uuid.UUID
+		resultZip string
+		resultErr error
+	}{
+		{lookupID: orderID, resultZip: "12345", resultErr: nil},
+		{lookupID: invalidID, resultZip: "", resultErr: models.ErrFetchNotFound},
+	}
+
+	for _, ts := range tests {
+		destinationZip, err := GetDestionDutyStationPostalCode(suite.DB(), ts.lookupID)
+		suite.Equal(ts.resultErr, err, "Wrong resultErr: %s", ts.lookupID)
+		suite.Equal(ts.resultZip, destinationZip, "Wrong moveID: %s", ts.lookupID)
+	}
+}
