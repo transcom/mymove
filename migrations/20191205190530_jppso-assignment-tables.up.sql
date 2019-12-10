cfa41f2dc9da2c022bf5e65a68558c4afc26ccfa
Jim Benton <jim@truss.works>
2019-12-10 23:21:55 +0000

    Revise migrations based on feedback.

--- migrations/20191205190530_jppso-assignment-tables.up.sql
+++ migrations/20191205190530_jppso-assignment-tables.up.sql
@@ -1,25 +1,23 @@
-CREATE TABLE gblocs
+CREATE TABLE jppso_regions
 (
     id uuid primary key,
     code text NOT NULL UNIQUE,
     name text NOT NULL,
-    city text NOT NULL,
-    state_abbreviation text NOT NULL,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
 );
 
-CREATE INDEX gblocs_code_idx ON gblocs (code);
+CREATE INDEX jppso_regions_code_idx ON jppso_regions (code);
 
-CREATE TABLE gbloc_state_assignments
+CREATE TABLE jppso_region_state_assignments
 (
     id uuid primary key,
-    gbloc_id uuid references gblocs(id),
+    jppso_region_id uuid references jppso_regions(id),
     state_name text NOT NULL UNIQUE,
     state_abbreviation text NOT NULL UNIQUE,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
 );
 
-CREATE INDEX gbloc_state_assignments_state_name_idx ON gbloc_state_assignments (state_name);
-CREATE INDEX gbloc_state_assignments_state_abbreviation_idx ON gbloc_state_assignments (state_abbreviation);
+CREATE INDEX jppso_state_assignments_state_name_idx ON jppso_region_state_assignments (state_name);
+CREATE INDEX jppso_state_assignments_state_abbreviation_idx ON jppso_region_state_assignments (state_abbreviation);
