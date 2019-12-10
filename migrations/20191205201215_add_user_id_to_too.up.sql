a0f2d1c5eb8684b0f7b05fe6e9f30d9735a1facc
Matt Krump <matt@truss.works>
2019-12-10 23:07:09 +0000

    fix conflicting names in migration

--- migrations/20191205201215_add_user_id_to_too.up.sql
+++ migrations/20191205201215_add_user_id_to_too.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE transportation_ordering_officers
     ADD COLUMN user_id uuid;
 ALTER TABLE transportation_ordering_officers
-    ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES users (id);
+    ADD CONSTRAINT transportation_ordering_officers_user_id_fk FOREIGN KEY (user_id) REFERENCES users (id);
 ALTER TABLE transportation_ordering_officers
-    ADD CONSTRAINT user_id_ukey UNIQUE (user_id);
+    ADD CONSTRAINT transportation_ordering_officers_user_id_ukey UNIQUE (user_id);
