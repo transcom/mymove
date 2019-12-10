317b2016a913fed6bbf98021a5e4b9a5de379f3a
ralren <ren@truss.works>
2019-12-10 17:38:16 +0000

    Forgot to limit varchar

--- migrations/20191210171518_create_move_task_orders.up.sql
+++ migrations/20191210171518_create_move_task_orders.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE move_task_orders
 (
 	id uuid PRIMARY KEY NOT NULL,
 	move_order_id uuid REFERENCES move_orders,
-	reference_id varchar,
+	reference_id varchar(255),
 	is_available_to_prime bool NOT NULL,
 	is_cancelled bool NOT NULL,
 	created_at timestamp WITH TIME ZONE NOT NULL,
