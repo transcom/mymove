aabc716cd97bc164eecae78af0616f948036bf13
Donald Thai <donald@truss.works>
2019-12-11 09:21:20 -0800

    creator and creator test

---
+++ pkg/services/mto_service_item/mto_service_item_creator.go
@@ -0,0 +1,32 @@
+package mtoserviceitem
+
+import (
+	"fmt"
+
+	"github.com/gobuffalo/validate"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
+)
+
+type createMTOServiceItemQueryBuilder interface {
+	CreateOne(model interface{}) (*validate.Errors, error)
+}
+
+type mtoServiceItemCreator struct {
+	builder createMTOServiceItemQueryBuilder
+}
+
+func (o *mtoServiceItemCreator) CreateMTOServiceItem(serviceItem *models.MTOServiceItem) (*models.MTOServiceItem, *validate.Errors, error) {
+	verrs, err := o.builder.CreateOne(serviceItem)
+	fmt.Println(verrs, err)
+	if verrs != nil || err != nil {
+		return nil, verrs, err
+	}
+
+	return serviceItem, nil, nil
+}
+
+func NewMTOServiceItemCreator(builder createMTOServiceItemQueryBuilder) services.MTOServiceItemCreator {
+	return &mtoServiceItemCreator{builder}
+}
