d3a284cdd7b5a0ddc7b44a7341f9963bcc0261aa
ralren <ren@truss.works>
2019-12-11 14:16:21 +0000

    Remove extra library

--- pkg/models/customer.go
+++ pkg/models/customer.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gobuffalo/validate"
 	"github.com/gobuffalo/validate/validators"
 	"github.com/gofrs/uuid"
-	"time"
 )
 
 // Customer is an object representing data for a customer
