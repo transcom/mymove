9931913c7e562b8c8a7ece573e66683f19b81147
Donald Thai <donald@truss.works>
2019-12-11 09:08:14 -0800

    interface for mto service item creator

---
+++ pkg/services/mto_service_item.go
@@ -0,0 +1,15 @@
+package services
+
+import (
+	"github.com/gobuffalo/validate"
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+)
+
+
+// MTOServiceItemCreator is the exported interface for creating a mto service item
+//go:generate mockery -name MTOServiceItemCreator
+type MTOServiceItemCreator interface {
+	CreateMTOServiceItem(serviceItem *models.MTOServiceItem) (*models.MTOServiceItem, *validate.Errors, error)
+}
