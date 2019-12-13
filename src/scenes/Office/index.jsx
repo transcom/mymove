4343ffda054b5794cbdf48a4c689df6ac242a50e
Matt Krump <1036969+mkrump@users.noreply.github.com>
2019-12-13 09:29:16 -0700

    Redirect TOO on signup (local dev only) (#3177)
    * Redirect office users not in office_users table to verification page on signup (feature flagged to local dev only).

--- src/scenes/Office/index.jsx
+++ src/scenes/Office/index.jsx
@@ -30,6 +30,7 @@ const ScratchPad = lazy(() => import('shared/ScratchPad'));
 const CustomerDetails = lazy(() => import('./TOO/customerDetails'));
 const TOO = lazy(() => import('./TOO/too'));
 const TIO = lazy(() => import('./TIO/tio'));
+const TOOVerificationInProgress = lazy(() => import('./TOO/tooVerificationInProgress'));
 const PaymentRequestShow = lazy(() => import('./TIO/paymentRequestShow'));
 const PaymentRequestIndex = lazy(() => import('./TIO/paymentRequestIndex'));
 
@@ -163,6 +164,7 @@ export class OfficeWrapper extends Component {
                   <Switch>
                     {too && <PrivateRoute path="/too/placeholder" component={TOO} />}
                     {too && <PrivateRoute path="/too/customer/:customerId/details" component={CustomerDetails} />}
+                    {too && <Route path="/verification-in-progress" component={TOOVerificationInProgress} />}
                     {tio && <PrivateRoute path="/tio/placeholder" component={TIO} />}
                     {tio && <PrivateRoute path="/payment_requests/:id" component={PaymentRequestShow} />}
                     {tio && <PrivateRoute path="/payment_requests" component={PaymentRequestIndex} />}
