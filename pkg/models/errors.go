abaf0398b4d11f74ba97f1163b978eb1ee085a01
Donald Thai <donald@truss.works>
2019-12-16 16:27:23 -0800

    add new error string for foreign key constraints

--- pkg/models/errors.go
+++ pkg/models/errors.go
@@ -40,5 +40,9 @@ var ErrInvalidTransition = errors.New("INVALID_TRANSITION")
 // This is ugly, but the best we can do with go's Postgresql adapter
 const RecordNotFoundErrorString = "sql: no rows in result set"
 
+// ViolatesForeignKeyConstraint is part of the error string that is returned when postgres could not find any
+// matching IDs for a record creating/updating
+const ViolatesForeignKeyConstraint = "violates foreign key constraint"
+
 // uniqueConstraintViolationErrorPrefix This is the error we get back from dbConnection.Create()
 const uniqueConstraintViolationErrorPrefix = "pq: duplicate key value violates unique constraint"
