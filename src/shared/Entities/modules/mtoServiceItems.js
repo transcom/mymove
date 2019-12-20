e412a7af4488b8aca4734d34c116083d487ade31
Donald Thai <donald@truss.works>
2019-12-19 18:54:24 -0800

    selectors for mto service items

--- src/shared/Entities/modules/mtoServiceItems.js
+++ src/shared/Entities/modules/mtoServiceItems.js
@@ -1,6 +1,7 @@
 import { swaggerRequest } from 'shared/Swagger/request';
 import { getGHCClient } from 'shared/Swagger/api';
-import { filter } from 'lodash';
+import { selectMoveTaskOrders } from 'shared/Entities/modules/moveTaskOrders';
+import { get, filter } from 'lodash';
 
 const getMTOServiceItemsOperation = 'mtoServiceItem.listMTOServiceItems';
 const mtoServiceItemsSchemaKey = 'mtoServiceItems';
@@ -12,8 +13,12 @@ export function getMTOServiceItems(
   return swaggerRequest(getGHCClient, getMTOServiceItemsOperation, { moveTaskOrderID }, { label, schemaKey });
 }
 
-export function selectMTOServiceItems(state, moveTaskOrderIds = new Set()) {
-  return filter(state.entities.mtoServiceItem, item => {
-    return moveTaskOrderIds.has(item.moveTaskOrderID);
-  });
+export function selectMTOServiceItem(state, mtoServiceItemId) {
+  return get(state, `entities.mtoServiceItem.${mtoServiceItemId}`, {});
+}
+
+export function selectMTOServiceItems(state, moveOrderId) {
+  const moveTaskOrders = selectMoveTaskOrders(state, moveOrderId);
+
+  return filter(state.entities.mtoServiceItem, item => moveTaskOrders.find(mto => mto.id === item.moveTaskOrderID));
 }
