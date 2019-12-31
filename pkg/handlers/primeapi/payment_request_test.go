e17adfe8e2e3ab28f209db632f8a6e7f0caa8e8d
Jacqueline McKinney <jacqueline@truss.works>
2019-12-31 00:15:47 +0000

    add create payment playload audit test

--- pkg/handlers/primeapi/payment_request_test.go
+++ pkg/handlers/primeapi/payment_request_test.go
@@ -9,17 +9,18 @@ import (
 
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	"github.com/gofrs/uuid"
-
-	"github.com/transcom/mymove/pkg/gen/primemessages"
-	"github.com/transcom/mymove/pkg/testdatagen"
-
+	"github.com/transcom/mymove/pkg/auth"
 	paymentrequestop "github.com/transcom/mymove/pkg/gen/primeapi/primeoperations/payment_requests"
+	"github.com/transcom/mymove/pkg/gen/primemessages"
 	"github.com/transcom/mymove/pkg/handlers"
 	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services/audit"
 	"github.com/transcom/mymove/pkg/services/mocks"
+	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
 func (suite *HandlerSuite) TestCreatePaymentRequestHandler() {
@@ -198,5 +199,62 @@ func (suite *HandlerSuite) TestCreatePaymentRequestHandler() {
 
 		response := handler.Handle(params)
 		suite.IsType(&paymentrequestop.CreatePaymentRequestBadRequest{}, response)
+	})
+
+	suite.T().Run("successful create payment request payload audit", func(t *testing.T) {
+
+		req := httptest.NewRequest("POST", fmt.Sprintf("/payment_requests"), nil)
+		req = suite.AuthenticateUserRequest(req, requestUser)
+
+		serviceItemID1, _ := uuid.FromString("1b7b134a-7c44-45f2-9114-bb0831cc5db3")
+		serviceItemID2, _ := uuid.FromString("119f0a05-34d7-4d86-9745-009c0707b4c2")
+		params := paymentrequestop.CreatePaymentRequestParams{
+			HTTPRequest: req,
+			Body: &primemessages.CreatePaymentRequestPayload{
+				IsFinal:         swag.Bool(false),
+				MoveTaskOrderID: *handlers.FmtUUID(moveTaskOrderID),
+				ServiceItems: []*primemessages.ServiceItem{
+					{
+						ID: *handlers.FmtUUID(serviceItemID1),
+						Params: []*primemessages.ServiceItemParamsItems0{
+							{
+								Key:   "weight",
+								Value: "1234",
+							},
+							{
+								Key:   "pickup",
+								Value: "2019-12-16",
+							},
+						},
+					},
+					{
+						ID: *handlers.FmtUUID(serviceItemID2),
+						Params: []*primemessages.ServiceItemParamsItems0{
+							{
+								Key:   "weight",
+								Value: "5678",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		session := auth.Session{}
+
+		// Capture creation attempt in audit log
+		zapFields, err := audit.Capture(&params.Body, nil, suite.TestLogger(), &session, params.HTTPRequest)
+
+		var eventType string
+		for _, field := range zapFields {
+			if field.Key == "event_type" {
+				eventType = field.String
+			}
+		}
+		suite.Nil(err, "No error for audit.Capture call")
+		if assert.NotEmpty(t, zapFields) {
+			assert.Equal(t, "event_type", zapFields[0].Key)
+			assert.Equal(t, "audit_post_payment_requests", eventType)
+		}
 	})
 }
