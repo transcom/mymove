6a5948cca57d8503449f57bab17cf1584a73cedd
Matt Krump <matt@truss.works>
2020-01-04 00:02:14 +0000

    WIP / EOD: Working with compound where clause in pop

--- pkg/services/users_roles/users_roles.go
+++ pkg/services/users_roles/users_roles.go
@@ -1,6 +1,8 @@
 package usersroles
 
 import (
+	"log"
+
 	"github.com/gobuffalo/pop"
 	"github.com/gofrs/uuid"
 
@@ -21,15 +23,88 @@ func NewUsersRolesCreator(db *pop.Connection) services.UserRoleAssociator {
 //AssociateUserRoles associates a given user with a set of roles
 func (u usersRolesCreator) AssociateUserRoles(userID uuid.UUID, rs []roles.RoleType) ([]models.UsersRoles, error) {
 	var usersRoles []models.UsersRoles
-	usersRoles, err := u.fetchUnassociatedRoles(userID, rs)
+	allRoles := roles.Roles{}
+	err := u.db.All(&allRoles)
+	log.Println("allRoles", allRoles)
 	if err != nil {
 		return usersRoles, err
 	}
-	err = u.db.Create(usersRoles)
+	dbRoles, err := u.FetchUserRoles(userID)
 	if err != nil {
 		return usersRoles, err
 	}
-	return usersRoles, err
+	log.Println("dbRoles", dbRoles)
+	log.Println("rs", rs)
+	toAdd := Difference(rs, dbRoles)
+	log.Println("ToADD", toAdd)
+	toRemove := Difference(dbRoles, rs)
+	rToAdd := UserRoles(allRoles, toAdd, userID)
+	err = u.db.Create(rToAdd)
+	if err != nil {
+		return usersRoles, err
+	}
+	rToRemove := UserRoles(allRoles, toRemove, userID)
+	var rIDSTOdelete []uuid.UUID
+	for _, m := range rToRemove {
+		rIDSTOdelete = append(rIDSTOdelete, m.RoleID)
+	}
+	var ur []models.UsersRoles
+	err = u.db.Where("role_id in (?)", rIDSTOdelete).Where("user_id = ?", userID).All(&ur)
+	if err != nil {
+		return usersRoles, err
+	}
+	log.Println("ToRemove", toRemove)
+	err = u.db.Destroy(ur)
+	if err != nil {
+		return usersRoles, err
+	}
+	return usersRoles, nil
+}
+
+func UserRoles(all roles.Roles, filterOn []roles.RoleType, userID uuid.UUID) []models.UsersRoles {
+	var output []models.UsersRoles
+	for _, s := range filterOn {
+		for _, role := range all {
+			if s == role.RoleType {
+				record := models.UsersRoles{
+					UserID: userID,
+					RoleID: role.ID,
+				}
+				output = append(output, record)
+			}
+		}
+	}
+	return output
+}
+
+// Set Difference: A - B
+func Difference(a, b []roles.RoleType) (diff []roles.RoleType) {
+	m := make(map[roles.RoleType]bool)
+
+	for _, item := range b {
+		m[item] = true
+	}
+
+	for _, item := range a {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+	return
+}
+
+func (u usersRolesCreator) FetchUserRoles(userID uuid.UUID) ([]roles.RoleType, error) {
+	// select all roles not already associated with this user
+	var userRoles []roles.RoleType
+	err := u.db.RawQuery(
+		`SELECT role_type
+		FROM roles
+		JOIN users_roles ur ON roles.id = ur.role_id
+		WHERE user_id = $1`, userID).All(&userRoles)
+	if err != nil {
+		return userRoles, err
+	}
+	return userRoles, nil
 }
 
 func (u usersRolesCreator) fetchUnassociatedRoles(userID uuid.UUID, rs []roles.RoleType) ([]models.UsersRoles, error) {
@@ -49,6 +124,7 @@ func (u usersRolesCreator) fetchUnassociatedRoles(userID uuid.UUID, rs []roles.R
 		FROM roles
 				 JOIN users_roles ur ON roles.id = ur.role_id
 		WHERE user_id = $1)`, userID).All(&userRoles)
+	log.Println("fetchUnassociatedRoles.userRoles", userRoles)
 	if err != nil {
 		return userRoles, err
 	}
--- pkg/services/users_roles/users_roles_test.go
+++ pkg/services/users_roles/users_roles_test.go
@@ -1,6 +1,10 @@
 package usersroles
 
 import (
+	"log"
+
+	"github.com/gobuffalo/pop"
+
 	"github.com/gofrs/uuid"
 
 	"github.com/transcom/mymove/pkg/models"
@@ -77,4 +81,156 @@ func (suite *UsersRolesServiceSuite) TestAssociateUserRolesTwice() {
 	suite.NoError(err)
 	suite.Require().Len(user.Roles, 1)
 	suite.Equal(user.Roles[0].ID, role1.ID)
+}
+
+func (suite *UsersRolesServiceSuite) TestAssociateUserRolesRemove() {
+	officeUser := testdatagen.MakeDefaultOfficeUser(suite.DB())
+	id1, _ := uuid.NewV4()
+	role1 := roles.Role{
+		ID:       id1,
+		RoleType: "role1",
+	}
+	id2, _ := uuid.NewV4()
+	role2 := roles.Role{
+		ID:       id2,
+		RoleType: "role2",
+	}
+	rs := roles.Roles{role1, role2}
+	err := suite.DB().Create(rs)
+	origRoleTypes := []roles.RoleType{role1.RoleType}
+	suite.NoError(err)
+	urc := NewUsersRolesCreator(suite.DB())
+
+	_, err = urc.AssociateUserRoles(*officeUser.UserID, origRoleTypes)
+	suite.NoError(err)
+
+	// remove role1 and add role2
+	newRoleTypes := []roles.RoleType{role2.RoleType}
+	_, err = urc.AssociateUserRoles(*officeUser.UserID, newRoleTypes)
+	suite.NoError(err)
+
+	ur := models.UsersRoles{}
+	n, err := suite.DB().Count(&ur)
+	suite.NoError(err)
+	suite.Equal(1, n)
+
+	user := models.User{}
+	err = suite.DB().Eager("Roles").Find(&user, officeUser.UserID)
+	suite.NoError(err)
+	suite.Require().Len(user.Roles, 1)
+	suite.Equal(user.Roles[0].ID, role2.ID)
+}
+
+func (suite *UsersRolesServiceSuite) TestAssociateUserRolesMultiple() {
+	officeUser := testdatagen.MakeDefaultOfficeUser(suite.DB())
+	id1, _ := uuid.NewV4()
+	role1 := roles.Role{
+		ID:       id1,
+		RoleType: "role1",
+	}
+	id2, _ := uuid.NewV4()
+	role2 := roles.Role{
+		ID:       id2,
+		RoleType: "role2",
+	}
+	rs := roles.Roles{role1, role2}
+	err := suite.DB().Create(rs)
+	origRoleTypes := []roles.RoleType{role1.RoleType, role2.RoleType}
+	suite.NoError(err)
+	urc := NewUsersRolesCreator(suite.DB())
+
+	_, err = urc.AssociateUserRoles(*officeUser.UserID, origRoleTypes)
+	suite.NoError(err)
+
+	rsOut := roles.Roles{}
+	err = suite.DB().Where("role_type in (?)", []string{"role1", "role2"}).All(&rsOut)
+	suite.NoError(err)
+	log.Println(rsOut)
+
+	var ur []models.UsersRoles
+	pop.Debug = true
+	err = suite.DB().Where("role_id in (?)", []uuid.UUID{rs[0].ID, rs[1].ID}).Where("user_id = ?", officeUser.UserID).All(&ur)
+	pop.Debug = false
+	suite.NoError(err)
+	log.Println("ur", ur)
+}
+
+func (suite *UsersRolesServiceSuite) TestToDelete() {
+	database := []roles.RoleType{"B", "C"}
+	input := []roles.RoleType{"A", "B"}
+
+	toDelete := Difference(database, input)
+	suite.Equal(toDelete, []roles.RoleType{"C"})
+}
+
+func (suite *UsersRolesServiceSuite) TestToAdd() {
+	database := []roles.RoleType{"B", "C"}
+	input := []roles.RoleType{"A", "B"}
+
+	toAdd := Difference(input, database)
+	suite.Equal(toAdd, []roles.RoleType{"A"})
+}
+
+func (suite *UsersRolesServiceSuite) TestFetchUserRoles() {
+	officeUser := testdatagen.MakeDefaultOfficeUser(suite.DB())
+	id1, _ := uuid.NewV4()
+	role1 := roles.Role{
+		ID:       id1,
+		RoleType: "role1",
+	}
+	rs := roles.Roles{role1}
+	err := suite.DB().Create(rs)
+	suite.NoError(err)
+	log.Println(*officeUser.UserID)
+	log.Println(role1.ID)
+	userRole := models.UsersRoles{
+		UserID: *officeUser.UserID,
+		RoleID: role1.ID,
+	}
+	err = suite.DB().Create(&userRole)
+	suite.NoError(err)
+
+	urc := usersRolesCreator{db: suite.DB()}
+	urs, err := urc.FetchUserRoles(*officeUser.UserID)
+	log.Println(urs)
+	suite.NoError(err)
+
+	suite.Len(urs, 1)
+}
+
+func (suite *UsersRolesServiceSuite) TestGenerateUsersRoles() {
+	id1, _ := uuid.NewV4()
+	role1 := roles.Role{
+		ID:       id1,
+		RoleType: "role1",
+	}
+	id2, _ := uuid.NewV4()
+	role2 := roles.Role{
+		ID:       id2,
+		RoleType: "role2",
+	}
+	id3, _ := uuid.NewV4()
+	role3 := roles.Role{
+		ID:       id3,
+		RoleType: "role3",
+	}
+	allRoles := roles.Roles{role1, role2, role3}
+
+	filterOn := []roles.RoleType{"role1", "role2"}
+
+	userID, _ := uuid.NewV4()
+
+	urs := UserRoles(allRoles, filterOn, userID)
+
+	expected := []models.UsersRoles{{
+		UserID: userID,
+		RoleID: role1.ID},
+		{
+			UserID: userID,
+			RoleID: role2.ID,
+		},
+	}
+
+	suite.Len(urs, 2)
+	suite.Equal(urs, expected)
 }
