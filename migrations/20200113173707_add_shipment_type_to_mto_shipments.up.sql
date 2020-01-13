640147954f5dddd9b485594b8032220d88748d70
ralren <ren@truss.works>
2020-01-13 17:46:03 +0000

    Add shipment type to model and db

---
+++ migrations/20200113173707_add_shipment_type_to_mto_shipments.up.sql
@@ -0,0 +1,8 @@
+CREATE TYPE mto_shipment_type AS ENUM (
+'HHG',
+'INTERNATIONAL HHG',
+'INTERNATIONAL UB'
+    );
+
+ALTER TABLE mto_shipments
+	ADD COLUMN shipment_type mto_shipment_type;
--- pkg/models/mto_shipments.go
+++ pkg/models/mto_shipments.go
@@ -11,27 +11,37 @@ import (
 	"github.com/transcom/mymove/pkg/unit"
 )
 
+// MTOShipmentType represents the type of shipments the mto shipment is
+type MTOShipmentType string
+
+const (
+	MTOShipmentTypeHHG              MTOShipmentType = "HHG"
+	MTOShipmentTypeInternationalHHG MTOShipmentType = "International HHG"
+	MTOShipmentTypeInternationalUB  MTOShipmentType = "International UB"
+)
+
 // MTOShipment is an object representing data for a move task order shipment
 type MTOShipment struct {
-	ID                               uuid.UUID     `db:"id"`
-	MoveTaskOrder                    MoveTaskOrder `belongs_to:"move_task_orders"`
-	MoveTaskOrderID                  uuid.UUID     `db:"move_task_order_id"`
-	ScheduledPickupDate              *time.Time    `db:"scheduled_pickup_date"`
-	RequestedPickupDate              *time.Time    `db:"requested_pickup_date"`
-	CustomerRemarks                  *string       `db:"customer_remarks"`
-	PickupAddress                    Address       `belongs_to:"addresses"`
-	PickupAddressID                  uuid.UUID     `db:"pickup_address_id"`
-	DestinationAddress               Address       `belongs_to:"addresses"`
-	DestinationAddressID             uuid.UUID     `db:"destination_address_id"`
-	SecondaryPickupAddress           *Address      `belongs_to:"addresses"`
-	SecondaryPickupAddressID         *uuid.UUID    `db:"secondary_pickup_address_id"`
-	SecondaryDeliveryAddress         *Address      `belongs_to:"addresses"`
-	SecondaryDeliveryAddressID       *uuid.UUID    `db:"secondary_delivery_address_id"`
-	PrimeEstimatedWeight             *unit.Pound   `db:"prime_estimated_weight"`
-	PrimeEstimatedWeightRecordedDate *time.Time    `db:"prime_estimated_weight_recorded_date"`
-	PrimeActualWeight                *unit.Pound   `db:"prime_actual_weight"`
-	CreatedAt                        time.Time     `db:"created_at"`
-	UpdatedAt                        time.Time     `db:"updated_at"`
+	ID                               uuid.UUID       `db:"id"`
+	MoveTaskOrder                    MoveTaskOrder   `belongs_to:"move_task_orders"`
+	MoveTaskOrderID                  uuid.UUID       `db:"move_task_order_id"`
+	ScheduledPickupDate              *time.Time      `db:"scheduled_pickup_date"`
+	RequestedPickupDate              *time.Time      `db:"requested_pickup_date"`
+	CustomerRemarks                  *string         `db:"customer_remarks"`
+	PickupAddress                    Address         `belongs_to:"addresses"`
+	PickupAddressID                  uuid.UUID       `db:"pickup_address_id"`
+	DestinationAddress               Address         `belongs_to:"addresses"`
+	DestinationAddressID             uuid.UUID       `db:"destination_address_id"`
+	SecondaryPickupAddress           *Address        `belongs_to:"addresses"`
+	SecondaryPickupAddressID         *uuid.UUID      `db:"secondary_pickup_address_id"`
+	SecondaryDeliveryAddress         *Address        `belongs_to:"addresses"`
+	SecondaryDeliveryAddressID       *uuid.UUID      `db:"secondary_delivery_address_id"`
+	PrimeEstimatedWeight             *unit.Pound     `db:"prime_estimated_weight"`
+	PrimeEstimatedWeightRecordedDate *time.Time      `db:"prime_estimated_weight_recorded_date"`
+	PrimeActualWeight                *unit.Pound     `db:"prime_actual_weight"`
+	ShipmentType                     MTOShipmentType `db:"shipment_type"`
+	CreatedAt                        time.Time       `db:"created_at"`
+	UpdatedAt                        time.Time       `db:"updated_at"`
 }
 
 // MTOShipments is a list of mto shipments
