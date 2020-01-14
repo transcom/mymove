034072c193017422ed6c1f710cbe1a2f4e916de2
Deep Marreddy <deep@truss.works>
2020-01-14 17:06:31 +0000

    Change precision and scale for numeric data type

--- migrations/20200106225354_alter_factor_hundredths_type.up.sql
+++ migrations/20200106225354_alter_factor_hundredths_type.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE re_shipment_type_prices RENAME COLUMN factor_hundredths TO factor;
-ALTER TABLE re_shipment_type_prices ALTER COLUMN factor TYPE numeric(6,5);
+ALTER TABLE re_shipment_type_prices ALTER COLUMN factor TYPE numeric(3,2);
