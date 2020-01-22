3ea76ee2dff4f0ca3003f787154fb36bcc532e8c
Garrett Martin <garrett@truss.works>
2020-01-22 17:47:22 +0000

    remove unnecessary check from service object

--- pkg/services/mto_shipment/mto_shipment_status_updater.go
+++ pkg/services/mto_shipment/mto_shipment_status_updater.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
-	"github.com/transcom/mymove/pkg/services/query"
 )
 
 type UpdateMTOShipmentStatusQueryBuilder interface {
@@ -19,13 +18,7 @@ type mtoShipmentStatusUpdater struct {
 }
 
 func (o *mtoShipmentStatusUpdater) UpdateMTOShipmentStatus(id uuid.UUID, status string) (*validate.Errors, error) {
-	var shipment models.MTOShipment
-	filters := []services.QueryFilter{query.NewQueryFilter("id", "=", id.String())}
-	err := o.builder.FetchOne(&shipment, filters)
-
-	if err != nil {
-		return nil, err
-	}
+	shipment := models.MTOShipment{}
 
 	switch status {
 	case "APPROVED":
