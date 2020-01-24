79ee23266b5fa907cf19eac062763b30ad70281b
Donald Thai <donald@truss.works>
2020-01-23 17:01:19 -0800

    refactored failing tests after upgrading to latest packages

--- src/scenes/SystemAdmin/Home.test.jsx
+++ src/scenes/SystemAdmin/Home.test.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import restProvider from 'ra-data-simple-rest';
 import Home from './Home';
-import { HashRouter as Router } from 'react-router-dom';
 
 const dataProvider = restProvider('http://admin/v1/...');
 
 describe('AdminHome tests', () => {
   describe('AdminHome component', () => {
     let wrapper;
-    wrapper = mount(
-      <Router>
-        <Home dataProvider={dataProvider} />
-      </Router>,
-    );
+    wrapper = shallow(<Home dataProvider={dataProvider} />);
 
     it('renders without crashing', () => {
       expect(wrapper.find('.admin-system-wrapper').length).toEqual(1);
