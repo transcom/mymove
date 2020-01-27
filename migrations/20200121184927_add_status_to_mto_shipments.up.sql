2e584f6ec75e11bb0cb99275f22ea80fa03c925e
Garrett Martin <garrett@truss.works>
2020-01-27 18:23:05 +0000

    Migration adjustments

--- migrations/20200121184927_add_status_to_mto_shipments.up.sql
+++ migrations/20200121184927_add_status_to_mto_shipments.up.sql
@@ -4,5 +4,8 @@ CREATE TYPE mto_shipment_status AS ENUM (
 'REJECTED'
     );
 
-ALTER TABLE mto_shipments
-	ADD COLUMN status mto_shipment_status NOT NULL DEFAULT 'SUBMITTED';
+ALTER TABLE mto_shipments ADD COLUMN status mto_shipment_status;
+
+UPDATE mto_shipments SET status = 'SUBMITTED';
+
+ALTER TABLE mto_shipments ALTER COLUMN status SET DEFAULT 'SUBMITTED';
