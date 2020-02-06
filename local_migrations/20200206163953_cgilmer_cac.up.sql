afb36454df0cbce2108cb03533d7df167f628c4b
Chris Gilmer <chris@truss.works>
2020-02-06 16:42:13 +0000

    Add cgilmer cac to migrations

---
+++ local_migrations/20200206163953_cgilmer_cac.up.sql
@@ -0,0 +1,4 @@
+-- Local test migration.
+-- This will be run on development environments.
+-- It should mirror what you intend to apply on prod/staging/experimental
+-- DO NOT include any sensitive data.
