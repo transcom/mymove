27dea57ef201c9c6d9daf2218cb575ced4ef254e
Donald Thai <donald@truss.works>
2020-02-07 20:14:08 +0000

    new shared ApproveRejectModal component

---
+++ src/shared/ApproveRejectModal/index.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class ApproveRejectModal extends Component {
+  state = {
+    showRejectionInput: false,
+    rejectBtnIsDisabled: false,
+    rejectionReason: null,
+  };
+
+  handleRejectionChange = rejectionReason => {
+    this.setState({ rejectionReason })
+
+    if (!rejectionReason) {
+      this.setState({ rejectBtnIsDisabled: true })
+    } else {
+      this.setState({ rejectBtnIsDisabled: false })
+    }
+  }
+
+  handleRejectionClick = () => {
+    if (!this.state.showRejectionInput) {
+      this.setState({ showRejectionInput: true, rejectBtnIsDisabled: true })
+    } else if (!this.state.rejectionReason) {
+      console.error("Rejection reason empty. Please fill out rejection reason.")
+    } else {
+      this.props.rejectBtnOnClick(this.state.rejectionReason)
+    }
+  }
+
+  render() {
+    const { approveBtnOnClick, hideModal } = this.props
+    return !hideModal && (<>
+      <div>
+        <button onClick={approveBtnOnClick}>Approve</button>
+        <button onClick={this.handleRejectionClick} disabled={this.state.rejectBtnIsDisabled}>Reject</button>
+      </div>
+      <div>
+        {this.state.showRejectionInput && (<label>
+          Rejection reason
+          <input name="rejectionReason" onChange={(event) => this.handleRejectionChange(event.target.value)}></input>
+        </label>)}
+      </div>
+    </>)
+  }
+}
+
+ApproveRejectModal.propTypes = {
+  /** REQUIRED. Function that is passed in to the onClick prop of the approve button */
+  approveBtnOnClick: PropTypes.func.isRequired,
+  /** REQUIRED. Function that is passed in to the onClick prop of the reject button */
+  rejectBtnOnClick: PropTypes.func.isRequired,
+  /** OPTIONAL. Boolean to hide the modal or not */
+  hideModal: PropTypes.bool,
+};
+
+ApproveRejectModal.defaultProps = {
+  hideModal: false,
+}
