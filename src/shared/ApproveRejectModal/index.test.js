59fabc680e7a2b3363c7605b51ed9140f0767d39
Donald Thai <donald@truss.works>
2020-02-10 18:50:14 -0800

    refactor and add tests

--- src/shared/ApproveRejectModal/index.test.js
+++ src/shared/ApproveRejectModal/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { ApproveRejectModal } from '.';
 
 // throw errors if component throws warning or errors
@@ -16,11 +16,62 @@ describe('AcceptRejectModal component test', () => {
     expect(wrapper.find('Fragment')).toHaveLength(1);
   });
 
-  it('renders nothing without crashing with required props and hideModal prop', () => {
+  it('renders nothing without crashing with required props and showModal prop false', () => {
     const wrapper = shallow(
-      <ApproveRejectModal hideModal={true} approveBtnOnClick={jest.fn()} rejectBtnOnClick={jest.fn()} />,
+      <ApproveRejectModal showModal={false} approveBtnOnClick={jest.fn()} rejectBtnOnClick={jest.fn()} />,
     );
     expect(wrapper.find('Fragment')).toHaveLength(0);
+  });
+
+  it('handleApproveClick() is called', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(<ApproveRejectModal approveBtnOnClick={mockFn} rejectBtnOnClick={jest.fn()} />);
+    wrapper.find('button[children="Approve"]').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('handleRejectionClick() is called', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(<ApproveRejectModal approveBtnOnClick={jest.fn()} rejectBtnOnClick={mockFn} />);
+    wrapper.setState({
+      showRejectionToggleBtn: false,
+      showRejectionInput: true,
+      rejectBtnIsDisabled: false,
+      rejectionReason: 'rejected',
+    });
+    wrapper.find('button[children="Reject"]').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  it('handleRejectionChange() is called', () => {
+    const wrapper = shallow(<ApproveRejectModal approveBtnOnClick={jest.fn()} rejectBtnOnClick={jest.fn()} />);
+    wrapper.setState({
+      showRejectionToggleBtn: false,
+      showRejectionInput: true,
+      rejectBtnIsDisabled: true,
+    });
+    const spyFn = jest.spyOn(wrapper.instance(), 'handleRejectionChange');
+    wrapper.find('input').simulate('change', { target: { value: 'foo' } });
+    expect(spyFn).toHaveBeenCalled();
+  });
+
+  it('handleRejectionCancelClick() is called', () => {
+    const wrapper = shallow(<ApproveRejectModal approveBtnOnClick={jest.fn()} rejectBtnOnClick={jest.fn()} />);
+    wrapper.setState({
+      showRejectionToggleBtn: false,
+      showRejectionInput: true,
+      rejectBtnIsDisabled: true,
+    });
+    const spyFn = jest.spyOn(wrapper.instance(), 'handleRejectionCancelClick');
+    wrapper.instance().handleRejectionCancelClick();
+    expect(spyFn).toHaveBeenCalled();
+  });
+
+  it('handleRejectionToggleClick() is called', () => {
+    let wrapper = mount(<ApproveRejectModal approveBtnOnClick={jest.fn()} rejectBtnOnClick={jest.fn()} />);
+    const spyFn = jest.spyOn(wrapper.instance(), 'handleRejectionToggleClick');
+    wrapper.instance().handleRejectionToggleClick();
+    expect(spyFn).toHaveBeenCalled();
   });
 
   // Negative tests
