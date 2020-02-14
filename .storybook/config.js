6e1d2f2e238a846c20a64dc3e6be05a1de7afaa1
John Gedeon <john@truss.works>
2020-02-14 23:16:31 +0000

    Remove broken DateAndLocation story

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,6 @@ import 'uswds/dist/css/uswds.css';
 function loadStories() {
   require('../src/stories/index.stories.js');
   require('../src/stories/statusTimeLine.stories.js');
-  require('../src/stories/dateAndLocation.stories.js');
 }
 
 configure(loadStories, module);
