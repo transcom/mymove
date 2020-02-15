199fe9d68084b3fb2b073c0038341d5d4f6e726f
lindsay t <techells22@gmail.com>
2020-02-15 23:11:00 +0000

    get incentive from ppm vs currentPpm

--- src/scenes/Landing/MoveSummary/SubmittedPpmMoveDetails.jsx
+++ src/scenes/Landing/MoveSummary/SubmittedPpmMoveDetails.jsx
@@ -13,7 +13,7 @@ const SubmittedPpmMoveDetails = props => {
     : '';
   const advanceString = ppm.has_requested_advance ? `Advance Requested: $${formatCents(advance.requested_amount)}` : '';
   const hasSitString = `Temp. Storage: ${ppm.days_in_storage} days ${privateStorageString}`;
-  const incentiveRange = formatCentsRange(ppm.currentPpm.incentive_estimate_min, ppm.currentPpm.incentive_estimate_max);
+  const incentiveRange = formatCentsRange(ppm.incentive_estimate_min, ppm.incentive_estimate_max);
   return (
     <div className="titled_block">
       <div className="title">Details</div>
