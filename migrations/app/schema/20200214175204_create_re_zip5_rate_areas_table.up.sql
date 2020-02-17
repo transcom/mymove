ebe959c5c9aae8146d4845fd76816dc509ce3e8d
Jacqueline McKinney <jacqueline@truss.works>
2020-02-17 22:00:50 +0000

    add indexes to sql

--- migrations/app/schema/20200214175204_create_re_zip5_rate_areas_table.up.sql
+++ migrations/app/schema/20200214175204_create_re_zip5_rate_areas_table.up.sql
@@ -10,7 +10,12 @@ create table re_zip5_rate_areas
     updated_at timestamp not null
 );
 
+CREATE INDEX ON re_zip5_rate_areas (rate_area_id);
+CREATE INDEX ON re_zip5_rate_areas (zip5);
+
 alter table re_zip3s
     add rate_area_id uuid null
      constraint re_zip3s_rate_area_id_fkey references re_rate_areas,
-    add has_multiple_rate_areas boolean not null default false
+    add has_multiple_rate_areas boolean not null default false;
+
+CREATE INDEX ON re_zip3s (rate_area_id);
