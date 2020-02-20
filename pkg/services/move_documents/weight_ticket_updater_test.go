ff518f0a26d2c41c71b10ee5a7dd76080a41443d
Kim Allen <kim@truss.works>
2020-02-20 23:38:59 +0000

    Add a test for the service that updates weight tickets to check for updates to make and model, without validations

--- pkg/services/move_documents/weight_ticket_updater_test.go
+++ pkg/services/move_documents/weight_ticket_updater_test.go
@@ -419,3 +419,103 @@ func (suite *MoveDocumentServiceSuite) TestNetWeightAfterManualOverride() {
 	suite.Require().Equal(*updateMoveDocPayload.FullWeight-*updateMoveDocPayload.EmptyWeight, int64(*updatedPpm.NetWeight))
 
 }
+
+func (suite *MoveDocumentServiceSuite) TestMakeAndModelUpdate() {
+	wtu := WeightTicketUpdater{suite.DB(), moveDocumentStatusUpdater{}}
+	officeUser := testdatagen.MakeDefaultOfficeUser(suite.DB())
+	session := &auth.Session{
+		ApplicationName: auth.OfficeApp,
+		UserID:          *officeUser.UserID,
+		OfficeUserID:    officeUser.ID,
+	}
+
+	emptyWeight1 := unit.Pound(1000)
+	fullWeight1 := unit.Pound(2500)
+	// made up net weight (as if office user overrode weight tickets)
+	netWeight1 := unit.Pound(10000)
+	wtDate := time.Date(2019, 05, 11, 0, 0, 0, 0, time.UTC)
+	ppm := testdatagen.MakePPM(suite.DB(), testdatagen.Assertions{PersonallyProcuredMove: models.PersonallyProcuredMove{NetWeight: &netWeight1}})
+	move := ppm.Move
+	sm := ppm.Move.Orders.ServiceMember
+	moveDocument := testdatagen.MakeMoveDocument(suite.DB(),
+		testdatagen.Assertions{
+			MoveDocument: models.MoveDocument{
+				MoveID:                   move.ID,
+				Move:                     move,
+				PersonallyProcuredMoveID: &ppm.ID,
+				MoveDocumentType:         models.MoveDocumentTypeWEIGHTTICKETSET,
+				Status:                   models.MoveDocumentStatusOK,
+			},
+			Document: models.Document{
+				ServiceMemberID: sm.ID,
+				ServiceMember:   sm,
+			},
+		})
+
+	vehicleNickname := "My Box Truck"
+	weightTicketSetDocument := models.WeightTicketSetDocument{
+		MoveDocumentID:           moveDocument.ID,
+		MoveDocument:             moveDocument,
+		EmptyWeight:              &emptyWeight1,
+		EmptyWeightTicketMissing: false,
+		FullWeight:               &fullWeight1,
+		FullWeightTicketMissing:  false,
+		VehicleNickname:          &vehicleNickname,
+		WeightTicketSetType:      models.WeightTicketSetTypeBOXTRUCK,
+		WeightTicketDate:         &testdatagen.NextValidMoveDate,
+		TrailerOwnershipMissing:  false,
+	}
+	verrs, err := suite.DB().ValidateAndCreate(&weightTicketSetDocument)
+	suite.NoVerrs(verrs)
+	suite.NoError(err)
+
+	vehicleMake := "Radio Flyer"
+	vehicleModel := "Wagon"
+	emptyWeight := (int64)(1000)
+	fullWeight := (int64)(2500)
+	weightTicketSetType := internalmessages.WeightTicketSetTypeCAR
+
+	updateMoveDocPayload := &internalmessages.MoveDocumentPayload{
+		ID:                  handlers.FmtUUID(moveDocument.ID),
+		MoveID:              handlers.FmtUUID(move.ID),
+		Title:               handlers.FmtString("super_awesome.pdf"),
+		Notes:               handlers.FmtString("This document is super awesome."),
+		Status:              internalmessages.MoveDocumentStatusOK,
+		VehicleNickname:     &vehicleNickname,
+		VehicleMake:         &vehicleMake,
+		VehicleModel:        &vehicleModel,
+		EmptyWeight:         &emptyWeight,
+		FullWeight:          &fullWeight,
+		WeightTicketSetType: &weightTicketSetType,
+		MoveDocumentType:    internalmessages.MoveDocumentTypeWEIGHTTICKETSET,
+		WeightTicketDate:    handlers.FmtDate(wtDate),
+	}
+
+	originalMoveDocument, err := models.FetchMoveDocument(suite.DB(), session, moveDocument.ID, false)
+	suite.Nil(err)
+	umd, verrs, err := wtu.Update(updateMoveDocPayload, originalMoveDocument, session)
+	suite.NotNil(umd)
+	suite.NoVerrs(verrs)
+	suite.Nil(err)
+	md, err := models.FetchMoveDocument(suite.DB(), session, moveDocument.ID, false)
+	suite.Nil(err)
+
+	suite.Require().NotNil(md.WeightTicketSetDocument)
+	suite.Require().Equal(moveDocument.ID.String(), md.ID.String(), "expected move doc ids to match")
+	suite.Require().Equal("super_awesome.pdf", md.Title)
+	suite.Require().Equal("This document is super awesome.", *md.Notes)
+	suite.Require().Equal(models.MoveDocumentStatusOK, md.Status)
+	suite.Require().Equal(vehicleNickname, *md.WeightTicketSetDocument.VehicleNickname)
+	suite.Require().Equal(vehicleMake, *md.WeightTicketSetDocument.VehicleMake)
+	suite.Require().Equal(vehicleModel, *md.WeightTicketSetDocument.VehicleModel)
+	suite.Require().Equal(models.WeightTicketSetTypeCAR, md.WeightTicketSetDocument.WeightTicketSetType)
+	suite.Require().Equal(unit.Pound(1000), *md.WeightTicketSetDocument.EmptyWeight)
+	suite.Require().Equal(unit.Pound(2500), *md.WeightTicketSetDocument.FullWeight)
+	actualWtDate := *md.WeightTicketSetDocument.WeightTicketDate
+	suite.Require().Equal(wtDate.UTC(), actualWtDate.UTC())
+	updatedPpm := models.PersonallyProcuredMove{}
+	err = suite.DB().Where(`id = $1`, ppm.ID).First(&updatedPpm)
+	suite.Require().Nil(err)
+	suite.Require().Equal(*updateMoveDocPayload.FullWeight-*updateMoveDocPayload.EmptyWeight, int64(*updatedPpm.NetWeight))
+
+}
