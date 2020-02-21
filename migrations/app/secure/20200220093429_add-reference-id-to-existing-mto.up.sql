4b4c55a311ba7a59345b835a7faf419bb239653e
Ron Cruz <Ronolibert@gmail.com>
2020-02-21 22:33:54 +0000

    Backfill existing MTOs with unique reference ids

--- migrations/app/secure/20200220093429_add-reference-id-to-existing-mto.up.sql
+++ migrations/app/secure/20200220093429_add-reference-id-to-existing-mto.up.sql
@@ -2,4 +2,32 @@
 -- This will be run on development environments.
 -- It should mirror what you intend to apply on prod/staging/experimental
 -- DO NOT include any sensitive data.
-UPDATE move_task_orders SET reference_id='1234-4321' WHERE id='5d4b25bb-eb04-4c03-9a81-ee0398cb779e';
+LOCK TABLE move_task_orders IN SHARE MODE;
+
+DO
+$do$
+    DECLARE
+        current_move_task_order_id UUID;
+        new_ref_id                 varchar(10);
+        ref_id_count               int;
+    BEGIN
+        -- loop over all move task orders with reference_id null
+        FOR current_move_task_order_id IN SELECT id FROM move_task_orders WHERE reference_id IS NULL
+            LOOP
+                LOOP
+                    -- generate a random reference_id based on xxxx-xxxx
+                    SELECT floor(random() * 9000 + 1000) || '-' || floor(random() * 9000 + 1000) INTO new_ref_id;
+
+                    -- look up to see if reference_id is already being used by an MTO
+                    SELECT COUNT(*) INTO ref_id_count FROM move_task_orders WHERE reference_id = new_ref_id;
+
+                    -- if there are no collisions then break out of loop
+                    IF ref_id_count = 0 THEN
+                        EXIT;
+                    END IF;
+                END LOOP;
+
+                UPDATE move_task_orders SET reference_id = new_ref_id WHERE id = current_move_task_order_id;
+            END LOOP;
+    END
+$do$;
