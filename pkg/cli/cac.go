20e29e16b86b34e6cd790c8837cfbf1f86471c4f
Chris Gilmer <chris@truss.works>
2020-02-21 19:23:14 +0000

    Use new package for pkcs11 code instead of transcom/nom

--- pkg/cli/cac.go
+++ pkg/cli/cac.go
@@ -10,8 +10,7 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/tcnksm/go-input"
-
-	"github.com/transcom/nom/pkg/pkcs11"
+	pkcsigner "pault.ag/go/pksigner"
 )
 
 type ErrInvalidPath struct {
@@ -82,19 +81,19 @@ func CheckCAC(v *viper.Viper) error {
 
 // GetCACStore retrieves the CAC store
 // Call 'defer store.Close()' after retrieving the store
-func GetCACStore(v *viper.Viper) (*pkcs11.Store, error) {
+func GetCACStore(v *viper.Viper) (*pkcsigner.Store, error) {
 	pkcs11ModulePath := v.GetString(PKCS11ModuleFlag)
 	tokenLabel := v.GetString(TokenLabelFlag)
 	certLabel := v.GetString(CertLabelFlag)
 	keyLabel := v.GetString(KeyLabelFlag)
-	pkcsConfig := pkcs11.Config{
+	pkcsConfig := pkcsigner.Config{
 		Module:           pkcs11ModulePath,
 		CertificateLabel: certLabel,
 		PrivateKeyLabel:  keyLabel,
 		TokenLabel:       tokenLabel,
 	}
 
-	store, errPKCS11New := pkcs11.New(pkcsConfig)
+	store, errPKCS11New := pkcsigner.New(pkcsConfig)
 	if errPKCS11New != nil {
 		return nil, errPKCS11New
 	}
