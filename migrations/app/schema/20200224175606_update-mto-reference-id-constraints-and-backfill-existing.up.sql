0e7a2f3acb7e6a2f37376ee580db5db9f1d82ebd
Ron Cruz <Ronolibert@gmail.com>
2020-02-24 17:57:54 +0000

    Added new migration to ensure backfilling migration runs on all environments

--- migrations/app/schema/20200224175606_update-mto-reference-id-constraints-and-backfill-existing.up.sql
+++ migrations/app/schema/20200224175606_update-mto-reference-id-constraints-and-backfill-existing.up.sql
@@ -6,15 +6,15 @@ $do$
         current_move_task_order_id UUID;
         new_ref_id                 varchar(10);
         ref_id_count               int;
-		ref_id_p1				   varchar(4);
-		ref_id_p2				   varchar(4);
+        ref_id_p1				   varchar(4);
+        ref_id_p2				   varchar(4);
     BEGIN
         -- loop over all move task orders with reference_id null
         FOR current_move_task_order_id IN SELECT id FROM move_task_orders WHERE reference_id IS NULL OR reference_id=''
             LOOP
                 LOOP
-					ref_id_p1 := floor(random() * 9999);
-					ref_id_p2 := floor(random() * 9999);
+                    ref_id_p1 := floor(random() * 9999);
+                    ref_id_p2 := floor(random() * 9999);
                     -- generate a random reference_id based on xxxx-xxxx
                     SELECT  LPAD(ref_id_p1, 4, '0') || '-' || LPAD(ref_id_p2, 4, '0')  INTO new_ref_id;
 
@@ -35,4 +35,4 @@ $do$;
 
 ALTER TABLE move_task_orders
     ADD CONSTRAINT reference_id_unique_key UNIQUE (reference_id),
-	ALTER COLUMN reference_id SET NOT NULL;
+    ALTER COLUMN reference_id SET NOT NULL;
