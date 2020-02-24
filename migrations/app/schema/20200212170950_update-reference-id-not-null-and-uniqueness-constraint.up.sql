0815d95f88af5a241bc0122456580183ec51b7e0
Ron Cruz <Ronolibert@gmail.com>
2020-02-24 17:34:05 +0000

    Leftpad reference_id between 0 - 9999

--- migrations/app/schema/20200212170950_update-reference-id-not-null-and-uniqueness-constraint.up.sql
+++ migrations/app/schema/20200212170950_update-reference-id-not-null-and-uniqueness-constraint.up.sql
@@ -6,13 +6,17 @@ $do$
         current_move_task_order_id UUID;
         new_ref_id                 varchar(10);
         ref_id_count               int;
+		ref_id_p1				   varchar(4);
+		ref_id_p2				   varchar(4);
     BEGIN
         -- loop over all move task orders with reference_id null
         FOR current_move_task_order_id IN SELECT id FROM move_task_orders WHERE reference_id IS NULL OR reference_id=''
             LOOP
                 LOOP
+					ref_id_p1 := floor(random() * 9999);
+					ref_id_p2 := floor(random() * 9999);
                     -- generate a random reference_id based on xxxx-xxxx
-                    SELECT floor(random() * 9000 + 1000) || '-' || floor(random() * 9000 + 1000) INTO new_ref_id;
+                    SELECT  LPAD(ref_id_p1, 4, '0') || '-' || LPAD(ref_id_p2, 4, '0')  INTO new_ref_id;
 
                     -- look up to see if reference_id is already being used by an MTO
                     SELECT COUNT(*) INTO ref_id_count FROM move_task_orders WHERE reference_id = new_ref_id;
