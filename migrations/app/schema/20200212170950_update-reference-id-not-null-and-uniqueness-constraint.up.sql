6f9065da8dffb843339e993cc04ccdce18d92b59
Ron Cruz <Ronolibert@gmail.com>
2020-02-24 17:08:35 +0000

    Consolidate reference_id migrations

--- migrations/app/schema/20200212170950_update-reference-id-not-null-and-uniqueness-constraint.up.sql
+++ migrations/app/schema/20200212170950_update-reference-id-not-null-and-uniqueness-constraint.up.sql
@@ -1,3 +1,34 @@
+LOCK TABLE move_task_orders IN SHARE MODE;
+
+DO
+$do$
+    DECLARE
+        current_move_task_order_id UUID;
+        new_ref_id                 varchar(10);
+        ref_id_count               int;
+    BEGIN
+        -- loop over all move task orders with reference_id null
+        FOR current_move_task_order_id IN SELECT id FROM move_task_orders WHERE reference_id IS NULL OR reference_id=''
+            LOOP
+                LOOP
+                    -- generate a random reference_id based on xxxx-xxxx
+                    SELECT floor(random() * 9000 + 1000) || '-' || floor(random() * 9000 + 1000) INTO new_ref_id;
+
+                    -- look up to see if reference_id is already being used by an MTO
+                    SELECT COUNT(*) INTO ref_id_count FROM move_task_orders WHERE reference_id = new_ref_id;
+
+                    -- if there are no collisions then break out of loop
+                    IF ref_id_count = 0 THEN
+                        EXIT;
+                    END IF;
+                END LOOP;
+
+                UPDATE move_task_orders SET reference_id = new_ref_id WHERE id = current_move_task_order_id;
+            END LOOP;
+    END
+$do$;
+
+
 ALTER TABLE move_task_orders
     ADD CONSTRAINT reference_id_unique_key UNIQUE (reference_id),
 	ALTER COLUMN reference_id SET NOT NULL;
