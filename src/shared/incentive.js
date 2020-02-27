933ce3b36495938c32d98e723da0234d68a5309b
lindsay t <techells22@gmail.com>
2020-02-27 19:02:10 +0000

    reverse order of fallback - add tests

--- src/shared/incentive.js
+++ src/shared/incentive.js
@@ -5,10 +5,10 @@ export function hasShortHaulError(rateEngineError) {
 }
 
 export function formatIncentiveRange(ppm) {
-  let incentiveRange = formatCentsRange(ppm.currentPpm.incentive_estimate_min, ppm.currentPpm.incentive_estimate_max);
+  let incentiveRange = formatCentsRange(ppm.incentive_estimate_min, ppm.incentive_estimate_max);
   // work around for for ppm redux storage in multiple places...
   if (incentiveRange === '') {
-    incentiveRange = formatCentsRange(ppm.incentive_estimate_min, ppm.incentive_estimate_max);
+    incentiveRange = formatCentsRange(ppm.currentPpm.incentive_estimate_min, ppm.currentPpm.incentive_estimate_max);
   }
   return incentiveRange;
 }
