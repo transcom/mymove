286d9e63f3d726aa8873f02750762d52c3956849
Rupinder Dhariwal <rd@truss.works>
2020-03-02 21:04:59 +0000

    use image id instead of digest id

--- cmd/ecs-deploy/task_def.go
+++ cmd/ecs-deploy/task_def.go
@@ -469,14 +469,14 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 	imageList, describeImageErr := serviceECR.DescribeImages(&ecr.DescribeImagesInput{
 		ImageIds: []*ecr.ImageIdentifier{
 			{
-				ImageDigest: aws.String(recordedImageDigest),
+				ImageDigest: aws.String(ecrImage.RegistryID),
 			},
 		},
 		RegistryId:     aws.String(ecrImage.RegistryID),
 		RepositoryName: aws.String(ecrImage.RepositoryName),
 	})
 	if describeImageErr != nil {
-		quit(logger, nil, fmt.Errorf("unable to retriev image from %q: %w", imageURI, err))
+		quit(logger, nil, fmt.Errorf("unable to retrieve image from %q: %w", recordedImageDigest, err))
 	}
 	// We should find atleast one image with this criteria
 	if len(imageList.ImageDetails) < 1 {
