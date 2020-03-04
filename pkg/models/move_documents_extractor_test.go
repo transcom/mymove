63c8c50fe08e4b7e3e6274cb6412b92c1457812e
Kim Allen <kim@truss.works>
2020-03-04 02:47:21 +0000

    Start to rewrite test for fetch of documents

--- pkg/models/move_documents_extractor_test.go
+++ pkg/models/move_documents_extractor_test.go
@@ -1,54 +1,152 @@
 package models_test
 
 import (
-	"time"
-
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
-func (suite *ModelSuite) TestFetchAllMoveDocumentsForMove() {
-	// When: there is a move and move document
+//func (suite *ModelSuite) TestFetchAllMoveDocumentsForMove() {
+//	// When: there is a move and move document
+//	move := testdatagen.MakeDefaultMove(suite.DB())
+//	sm := move.Orders.ServiceMember
+//
+//	assertions := testdatagen.Assertions{
+//		MoveDocument: models.MoveDocument{
+//			MoveID: move.ID,
+//			Move:   move,
+//		},
+//		Document: models.Document{
+//			ServiceMemberID: sm.ID,
+//			ServiceMember:   sm,
+//		},
+//	}
+//
+//	testdatagen.MakeMoveDocument(suite.DB(), assertions)
+//	testdatagen.MakeMovingExpenseDocument(suite.DB(), assertions)
+//
+//	moveDocument2 := testdatagen.MakeMoveDocument(suite.DB(), assertions)
+//	weightTicketCarAssertions := testdatagen.Assertions{
+//		WeightTicketSetDocument: models.WeightTicketSetDocument{
+//			VehicleMake:              models.StringPointer("Honda"),
+//			VehicleModel:             models.StringPointer("Civic"),
+//			WeightTicketSetType:      models.WeightTicketSetTypeCAR,
+//			MoveDocument: models.MoveDocument{
+//				ID: moveDocument2.ID,
+//				MoveID: move.ID,
+//				Move: move,
+//				MoveDocumentType: models.MoveDocumentTypeWEIGHTTICKET,
+//				Document: models.Document{
+//					ServiceMemberID: sm.ID,
+//					ServiceMember:   sm,
+//				},
+//			},
+//
+//		},
+//	}
+//	carWeightTicketSetDocument := testdatagen.MakeWeightTicketSetDocument(suite.DB(), weightTicketCarAssertions)
+//	moveDocument2.WeightTicketSetDocument = &carWeightTicketSetDocument
+//
+//	moveDocument3 := testdatagen.MakeMoveDocument(suite.DB(), assertions)
+//	weightTicketTruckAssertions := testdatagen.Assertions{
+//		WeightTicketSetDocument: models.WeightTicketSetDocument{
+//			MoveDocument: models.MoveDocument{
+//				ID:               moveDocument3.ID,
+//				MoveID: move.ID,
+//				Move: move,
+//				MoveDocumentType: models.MoveDocumentTypeWEIGHTTICKET,
+//				Document: models.Document{
+//					ServiceMemberID: sm.ID,
+//					ServiceMember:   sm,
+//				},
+//			},
+//		},
+//	}
+//	truckWeightTicketSetDocument := testdatagen.MakeWeightTicketSetDocument(suite.DB(), weightTicketTruckAssertions)
+//	moveDocument3.WeightTicketSetDocument = &truckWeightTicketSetDocument
+//
+//	deletedAt := time.Date(2019, 8, 7, 0, 0, 0, 0, time.UTC)
+//	deleteAssertions := testdatagen.Assertions{
+//		MoveDocument: models.MoveDocument{
+//			MoveID:    move.ID,
+//			Move:      move,
+//			DeletedAt: &deletedAt,
+//		},
+//		Document: models.Document{
+//			ServiceMemberID: sm.ID,
+//			ServiceMember:   sm,
+//			DeletedAt:       &deletedAt,
+//		},
+//	}
+//	testdatagen.MakeMoveDocument(suite.DB(), deleteAssertions)
+//
+//	docs, err := move.FetchAllMoveDocumentsForMove(suite.DB(), false)
+//	if suite.NoError(err) {
+//		suite.Len(docs, 3)
+//	}
+//}
+
+func (suite *ModelSuite) TestFetchAllMoveDocumentsForMove2() {
+	// Create move an SM on which to attach move docs
 	move := testdatagen.MakeDefaultMove(suite.DB())
 	sm := move.Orders.ServiceMember
 
-	assertions := testdatagen.Assertions{
-		MoveDocument: models.MoveDocument{
-			MoveID: move.ID,
-			Move:   move,
-		},
-		Document: models.Document{
-			ServiceMemberID: sm.ID,
-			ServiceMember:   sm,
-		},
+	moveDoc1 := suite.documentMakerHelper(move, sm, models.MoveDocumentTypeWEIGHTTICKET)
+	carWeightTicketSetDocument := models.WeightTicketSetDocument{
+		VehicleMake:         models.StringPointer("Honda"),
+		VehicleModel:        models.StringPointer("Civic"),
+		WeightTicketSetType: models.WeightTicketSetTypeBOXTRUCK,
+		MoveDocumentID:      moveDoc1.ID,
+		MoveDocument:        moveDoc1,
 	}
 
-	testdatagen.MakeMoveDocument(suite.DB(), assertions)
-	testdatagen.MakeMovingExpenseDocument(suite.DB(), assertions)
-	testdatagen.MakeWeightTicketSetDocument(suite.DB(), assertions)
+	testdatagen.MakeWeightTicketSetDocument(suite.DB(), testdatagen.Assertions{
+		WeightTicketSetDocument: carWeightTicketSetDocument,
+	})
 
-	deletedAt := time.Date(2019, 8, 7, 0, 0, 0, 0, time.UTC)
-	deleteAssertions := testdatagen.Assertions{
-		MoveDocument: models.MoveDocument{
-			MoveID:    move.ID,
-			Move:      move,
-			DeletedAt: &deletedAt,
-		},
-		Document: models.Document{
-			ServiceMemberID: sm.ID,
-			ServiceMember:   sm,
-			DeletedAt:       &deletedAt,
-		},
+	moveDoc2 := suite.documentMakerHelper(move, sm, models.MoveDocumentTypeWEIGHTTICKET)
+	truckWeightTicketSetDocument := models.WeightTicketSetDocument{
+		VehicleNickname:     models.StringPointer("Hank the Tank"),
+		WeightTicketSetType: models.WeightTicketSetTypeBOXTRUCK,
+		MoveDocumentID:      moveDoc2.ID,
+		MoveDocument:        moveDoc2,
 	}
-	testdatagen.MakeMoveDocument(suite.DB(), deleteAssertions)
+
+	testdatagen.MakeWeightTicketSetDocument(suite.DB(), testdatagen.Assertions{
+		WeightTicketSetDocument: truckWeightTicketSetDocument,
+	})
 
 	docs, err := move.FetchAllMoveDocumentsForMove(suite.DB(), false)
-	if suite.NoError(err) {
-		suite.Len(docs, 3)
-	}
+	suite.NoError(err)
 
-	allDocs, err2 := move.FetchAllMoveDocumentsForMove(suite.DB(), false)
-	if suite.NoError(err2) {
-		suite.Len(allDocs, 3)
+	// Check car weight ticket values
+	carDoc := docs[0]
+	suite.Equal(models.MoveDocumentTypeWEIGHTTICKET, carDoc.MoveDocumentType)
+	suite.Equal(carWeightTicketSetDocument.VehicleMake, carDoc.VehicleMake)
+	suite.Equal(carWeightTicketSetDocument.VehicleModel, carDoc.VehicleModel)
+
+	truckDoc := docs[1]
+	suite.Equal(models.MoveDocumentTypeWEIGHTTICKET, truckDoc.MoveDocumentType)
+	suite.Equal(truckWeightTicketSetDocument.VehicleNickname, truckDoc.VehicleNickname)
+
+	// Create move documents with expense docs
+
+}
+
+func (suite *ModelSuite) documentMakerHelper(move models.Move, sm models.ServiceMember, moveDocumentType models.MoveDocumentType) models.MoveDocument {
+	// Create a Document and MoveDocument for a given move
+	moveDocAssertions := testdatagen.Assertions{
+		MoveDocument: models.MoveDocument{
+			MoveID:           move.ID,
+			Move:             move,
+			MoveDocumentType: models.MoveDocumentTypeWEIGHTTICKET,
+		},
+		Document: models.Document{
+			ServiceMemberID: sm.ID,
+			ServiceMember:   sm,
+		},
 	}
+	// Create move document and weight ticket
+	moveDoc := testdatagen.MakeMoveDocument(suite.DB(), moveDocAssertions)
+
+	return moveDoc
 }
