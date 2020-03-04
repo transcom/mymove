49973c6f63be5d540a5228d54899a78f1b30c8d3
Shauna Keating <shauna@truss.works>
2020-03-04 16:05:59 -0500

    adds headers page markup to the stories file

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -8,7 +8,7 @@ import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
+storiesOf('Components/Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
@@ -17,3 +17,15 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+  storiesOf('Global Styles/Typography', module)
+    .add('Headers', () =>
+      <div>
+        <p>h1</p><h1>Public Sans 40/48</h1>
+        <p>h2</p><h2>Public Sans 28/34</h2>
+        <p>h3</p><h3>Public Sans 21/23</h3>
+        <p>h4</p><h4>Public Sans 17/23</h4>
+        <p>h5</p><h5>Public Sans 15/20</h5>
+        <p>h6</p><h6>Public Sans 13/16</h6>
+      </div>
+    );
