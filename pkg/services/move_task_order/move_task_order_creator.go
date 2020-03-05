46443bc793088219804f37f89c441fe7cb9ffa13
Donald Thai <donald@truss.works>
2020-03-05 17:07:53 +0000

    mto creator service object and test

---
+++ pkg/services/move_task_order/move_task_order_creator.go
@@ -0,0 +1,96 @@
+package movetaskorder
+
+import (
+	"strings"
+	"time"
+
+	"github.com/gobuffalo/uuid"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/validate"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
+	mtoservicehelper "github.com/transcom/mymove/pkg/services/move_task_order/shared"
+)
+
+type createMoveTaskOrderQueryBuilder interface {
+	CreateOne(model interface{}) (*validate.Errors, error)
+}
+
+type moveTaskOrderCreator struct {
+	builder createMoveTaskOrderQueryBuilder
+	db      *pop.Connection
+}
+
+// CreateMoveTaskOrder creates an MTO Service Item
+func (o *moveTaskOrderCreator) CreateMoveTaskOrder(moveTaskOrder *models.MoveTaskOrder) (*models.MoveTaskOrder, *validate.Errors, error) {
+	// generate reference id if empty
+	if strings.TrimSpace(moveTaskOrder.ReferenceID) == "" {
+		referenceID, err := mtoservicehelper.GenerateReferenceID(o.db)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		moveTaskOrder.ReferenceID = referenceID
+	}
+
+	moveTaskOrder.CreatedAt = time.Now()
+	moveTaskOrder.UpdatedAt = time.Now()
+
+	verrs, err := o.builder.CreateOne(moveTaskOrder)
+	if verrs != nil || err != nil {
+		return nil, verrs, err
+	}
+
+	// create default server items as well
+	err = o.createDefaultServiceItems(moveTaskOrder)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return moveTaskOrder, nil, nil
+}
+
+// NewMoveTaskOrderCreator returns a new MTO service item creator
+func NewMoveTaskOrderCreator(builder createMoveTaskOrderQueryBuilder, db *pop.Connection) services.MoveTaskOrderCreator {
+	return &moveTaskOrderCreator{builder, db}
+}
+
+func (o *moveTaskOrderCreator) createDefaultServiceItems(moveTaskOrder *models.MoveTaskOrder) error {
+	var reServices []models.ReService
+	err := o.db.Where("code in (?)", []string{"MS", "CS"}).All(&reServices)
+
+	if err != nil {
+		return err
+	}
+
+	defaultServiceItems := make(map[uuid.UUID]models.MTOServiceItem)
+	for _, reService := range reServices {
+		defaultServiceItems[reService.ID] = models.MTOServiceItem{
+			ReServiceID:     reService.ID,
+			MoveTaskOrderID: moveTaskOrder.ID,
+		}
+	}
+
+	// Remove the ones that exist on the mto
+	for _, item := range moveTaskOrder.MTOServiceItems {
+		for _, reService := range reServices {
+			if item.ReServiceID == reService.ID {
+				delete(defaultServiceItems, reService.ID)
+			}
+		}
+	}
+
+	for _, serviceItem := range defaultServiceItems {
+		_, err := o.db.ValidateAndCreate(&serviceItem)
+
+		if err != nil {
+			return err
+		}
+
+		moveTaskOrder.MTOServiceItems = append(moveTaskOrder.MTOServiceItems, serviceItem)
+	}
+
+	return nil
+}
