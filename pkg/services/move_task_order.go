0c889742604907c1424047caab4936ff2f252a66
Donald Thai <donald@truss.works>
2020-03-05 17:09:20 +0000

    refactor move task order model and service interface

--- pkg/services/move_task_order.go
+++ pkg/services/move_task_order.go
@@ -1,12 +1,19 @@
 package services
 
 import (
+	"github.com/gobuffalo/validate"
 	"github.com/gofrs/uuid"
 
 	movetaskorderops "github.com/transcom/mymove/pkg/gen/primeapi/primeoperations/move_task_order"
 
 	"github.com/transcom/mymove/pkg/models"
 )
+
+// MoveTaskOrderCreator is the service object interface for FetchMoveTaskOrder
+//go:generate mockery -name MoveTaskOrderCreator
+type MoveTaskOrderCreator interface {
+	CreateMoveTaskOrder(moveTaskOrder *models.MoveTaskOrder) (*models.MoveTaskOrder, *validate.Errors, error)
+}
 
 // MoveTaskOrderFetcher is the service object interface for FetchMoveTaskOrder
 //go:generate mockery -name MoveTaskOrderFetcher
