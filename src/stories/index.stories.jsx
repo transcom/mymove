c35709d0e90043156381b9ce0de1cc1e165eb7ba
Shauna Keating <shauna@truss.works>
2020-03-06 16:26:53 -0500

    resolves eslint errors

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -18,30 +18,50 @@ storiesOf('Components/Button', module)
     </Button>
   ));
 
-  storiesOf('Global Styles/Typography', module)
-    .add('Headers', () =>
-      <div>
-        <p>h1</p><h1>Public Sans 40/48</h1>
-        <p>h2</p><h2>Public Sans 28/34</h2>
-        <p>h3</p><h3>Public Sans 21/23</h3>
-        <p>h4</p><h4>Public Sans 17/23</h4>
-        <p>h5</p><h5>Public Sans 15/20</h5>
-        <p>h6</p><h6>Public Sans 13/16</h6>
-      </div>
-    )
-    .add('Text', () =>
-      <div>
-        <p>p</p><p>Public Sans 15/18</p>
-        <p>p small</p><small>Public Sans 13/16</small>
-      </div>
-    )
-    .add('Links', () =>
-      <div>
-        <p>a</p><a href="https://materializecss.com/sass.html">USWDS blue-warm-60v</a>
-        <p>a:hover</p><a className="hover" href="https://materializecss.com/sass.html">USWDS blue-warm-60v</a>
-        <p>a:visted</p><a className="visited"  href="#">USWDS bg-violet-warm-60</a>
-        <p>a:disabled</p><a className="disabled" >This link is disabled</a>
-        <p>a:focus</p><a className="focus" >This link is focused</a>
-        <p>a small</p><small><a href="https://materializecss.com/sass.html">USWDS blue-warm-60v 14/16</a></small>
-      </div>
-    );
+storiesOf('Global Styles/Typography', module)
+  .add('Headers', () => (
+    <div>
+      <p>h1</p>
+      <h1>Public Sans 40/48</h1>
+      <p>h2</p>
+      <h2>Public Sans 28/34</h2>
+      <p>h3</p>
+      <h3>Public Sans 21/23</h3>
+      <p>h4</p>
+      <h4>Public Sans 17/23</h4>
+      <p>h5</p>
+      <h5>Public Sans 15/20</h5>
+      <p>h6</p>
+      <h6>Public Sans 13/16</h6>
+    </div>
+  ))
+  .add('Text', () => (
+    <div>
+      <p>p</p>
+      <p>Public Sans 15/18</p>
+      <p>p small</p>
+      <small>Public Sans 13/16</small>
+    </div>
+  ))
+  .add('Links', () => (
+    <div>
+      <p>a</p>
+      <a href="https://materializecss.com/sass.html">USWDS blue-warm-60v</a>
+      <p>a:hover</p>
+      <a className="hover" href="https://materializecss.com/sass.html">
+        USWDS blue-warm-60v
+      </a>
+      <p>a:visted</p>
+      <a className="visited" href="#">
+        USWDS bg-violet-warm-60
+      </a>
+      <p>a:disabled</p>
+      <a className="disabled">This link is disabled</a>
+      <p>a:focus</p>
+      <a className="focus">This link is focused</a>
+      <p>a small</p>
+      <small>
+        <a href="https://materializecss.com/sass.html">USWDS blue-warm-60v 14/16</a>
+      </small>
+    </div>
+  ));
