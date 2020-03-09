713a03e6303d16bccc79404695f3e1793fc60ca8
Shauna Keating <shauna@truss.works>
2020-03-09 11:29:30 -0400

    removes a typo at the beginning of the placeholder text

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -40,7 +40,7 @@ storiesOf('Global Styles/Typography', module)
       <p>p</p>
       <p>Public Sans 15/23 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>p small</p>
-      <p><small>Public Sans 13/18 WFaucibus in ornare quam viverra orci sagittis eu volutpat odio. Felis imperdiet proin fermentum leo vel orci. Egestas sed sed risus pretium quam vulputate. Consectetur libero id faucibus nisl. Ipsum dolor sit amet consectetur adipiscing elit. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan.</small></p>
+      <p><small>Public Sans 13/18 Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Felis imperdiet proin fermentum leo vel orci. Egestas sed sed risus pretium quam vulputate. Consectetur libero id faucibus nisl. Ipsum dolor sit amet consectetur adipiscing elit. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan.</small></p>
     </div>
   ))
   .add('Links', () => (
