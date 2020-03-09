af73e179ede7c19194e9eb282d7ed217f852f35e
Shauna Keating <shauna@truss.works>
2020-03-09 13:15:16 -0400

    adds markup for color swatches

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -23,25 +23,89 @@ storiesOf('Button', module)
     </Button>
   ));
 
-  const colors = () => {
-    return (
-      <ul>
-        {Object.keys(colors).map((color) => (
-          <li>
-            <span
-              style={
-                backgroundColor: colors[color],
-                display: 'block',
-                height: '4em',
-                marginBottom: '0.3em',
-                borderRadius: '5px',
-                border: '1px solid lightgray'
-              }
-            />
-          <span>{color}</span><br /> // color name
-          <span>{colors[color]}</span> <br /> // hex value
-        </li>
-        )
-      )
-    )
-  }
+  storiesOf('Global|Colors', module).add('all', () => (
+      <div style={ padding:'20px', }>
+        <>
+          <h3>Brand Colors</h3>
+          <ColorGroup group={filterGroup('brand')} />
+        </>
+        <>
+          <h3>Background Colors</h3>
+          <ColorGroup group={filterGroup('background')} />
+        </>
+        <>
+          <h3>Base Colors</h3>
+          <ColorGroup group={filterGroup('base')} />
+        </>
+        <>
+          <h3>Alert Colors</h3>
+          <ColorGroup group={filterGroup('alert')} />
+        </>
+        <>
+          <h3>Accent Colors</h3>
+          <ColorGroup group={filterGroup('accent')} />
+        </>
+      </div>
+  ));
+
+  // Convert the color key to the color variable name.
+  const colorVariable = (color) => {
+    const array = color.split('-')[1].split(/(?=[A-Z])/);
+    return `$color-${array.join('-').toLowerCase()}`;
+  };
+
+  // Convert the color key to the color proper name.
+  const colorName = (color) => {
+    const array = color.split('-')[1].split(/(?=[A-Z])/);
+    return `${array.join(' ').toLowerCase()}`;
+  };
+
+  // A component for displaying individual color swatches.
+  const Color = ({ color }) => (
+    <li
+      style={
+        borderRadius: '5px',
+        border: '1px solid lightgray',
+        padding: '5px'
+      }
+    >
+      <span
+        style={
+          backgroundColor: colors[color],
+          display: 'block',
+          height: '4em',
+          marginBottom: '0.3em',
+          borderRadius: '5px',
+          border: '1px solid lightgray'
+        }
+      />
+      <span style=>{colorName(color)}</span>{' '}
+      <br />
+      <span>{colorVariable(color)}</span> <br />
+      <span>{colors[color]}</span> <br />
+    </li>
+  );
+
+  Color.propTypes = {
+    color: PropTypes.string.isRequired
+  };
+
+  // A component for displaying a group of colors.
+  const ColorGroup = ({ group }) => (
+    <ul
+      style={
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(120px, 175px))',
+        gridGap: '20px',
+        marginBottom: '40px'
+      }
+    >
+      {group.map((color) => {
+        return <Color color={color} key={color} />;
+      })}
+    </ul>
+  );
+
+  ColorGroup.propTypes = {
+    group: PropTypes.array.isRequired
+  };
