8f3854888a66f5688447e8e2869ea09873c0662a
Shauna Keating <shauna@truss.works>
2020-03-11 15:34:25 -0400

    imports react-uswds button component

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import { Button } from '@trussworks/react-uswds';
 
 storiesOf('Components/Button', module)
   .add('default', () => (
