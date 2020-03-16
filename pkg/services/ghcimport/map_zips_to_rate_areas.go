68560479b5a63ed3e771178033e840f0ca113e37
Harsha Murthy <harsha@truss.works>
2020-03-16 19:37:32 +0000

    Complete mapZipsToRateAreas mapZip3s

--- pkg/services/ghcimport/map_zips_to_rate_areas.go
+++ pkg/services/ghcimport/map_zips_to_rate_areas.go
@@ -2,65 +2,100 @@ package ghcimport
 
 import (
 	"fmt"
-	"github.com/transcom/mymove/pkg/models"
+	"os"
 
 	"github.com/gobuffalo/pop"
-	//"github.com/gobuffalo/validate"
-	//"github.com/gofrs/uuid"
-	//"github.com/pkg/errors"
-	//
-	//"github.com/transcom/mymove/pkg/models"
+	"github.com/gocarina/gocsv"
+
+	"github.com/transcom/mymove/pkg/models"
 )
 
-func (gre *GHCRateEngineImporter) mapZipsToRateAreas(db *pop.Connection) error {
-	//fmt.Println("Test mapZipsToRateAreas function")
-	var err error
-	err = gre.mapZip3s(dbTx)
+//Zip3Fixture stores zip and rate and area information from tariff400ng_zip3s_fixture.csv
+type Zip3Fixture struct {
+	Zip      string `csv:"zip"`
+	RateArea string `csv:"rate_area"`
+}
+
+//Zip5Fixture stores zip and rate and area information from tariff400ng_zip5_rate_areas_fixture.csv
+type Zip5Fixture struct {
+	Zip      string `csv:"zip"`
+	RateArea string `csv:"rate_area"`
+}
+
+func (gre *GHCRateEngineImporter) mapZipsToRateAreas(dbTx *pop.Connection) error {
+	// Maps re_zip3s to re_rate_areas based on tariff400ng_zip3s_fixture.csv
+	err := gre.mapZip3s(dbTx)
 	if err != nil {
-		return fmt.Errorf("mapZipsToRateAreas failed to map: %w", err)
+		return fmt.Errorf("mapZip3s failed: %w", err)
 	}
 
-	err = gre.mapZip5s(dbTx)
+	// Creates re_zip5_rate_areas records from tariff400ng_zip5_rate_areas_fixture.csv
+	err = gre.createZip5s(dbTx)
 	if err != nil {
-		return fmt.Errorf("mapZipsToRateAreas failed to map: %w", err)
+		return fmt.Errorf("createZip5s failed: %w", err)
 	}
+
 	return nil
 }
 
-func (gre *GHCRateEngineImporter) mapZip3s(db *pop.Connection) error {
+func (gre *GHCRateEngineImporter) mapZip3s(dbTx *pop.Connection) error {
+	csvFile, err := os.OpenFile("pkg/services/ghcimport/fixtures/tariff400ng_zip3s_fixture.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer csvFile.Close()
 
-	gre.domesticRateAreaToIDMap
+	zip3RateAreas := []*Zip3Fixture{}
 
+	err = gocsv.UnmarshalFile(csvFile, &zip3RateAreas)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal file: %w", err)
+	}
 
-	// Load /fixtures/tariff400ng_zip3s_fixture.csv and for each row
-		// var zip3 = fixture zip3
-		// var rate_area = fixture rate_area
-
-		// Fetch the existing zip3 record for the current zip3 in this CSV line
+	for _, zip3RateArea := range zip3RateAreas {
 		var reZip3 models.ReZip3
-		err := db.Where("zip3 = $1", zip3FromCsv).First(&reZip3)
-		// TODO: Update rate_area_id and has_multiple_rate_areas
-		reZip3.RateAreaID = // look up the rate area id in the domesticRateAreaToIDMap for the rate area in this CSV line
-		reZip3.HasMultipleRateAreas = // true or false depending on whether "ZIP" is in there
-		verrs, err := db.ValidateAndUpdate(&reZip3)
+		err = dbTx.Where("zip3 = ?", zip3RateArea.Zip).First(&reZip3)
+		if err != nil {
+			return fmt.Errorf("failed to find ReZip3 record with zip %s: %w", zip3RateArea.Zip, err)
+		}
 
+		if zip3RateArea.RateArea == "ZIP" {
+			reZip3.RateAreaID = nil
+			reZip3.HasMultipleRateAreas = true
+			verrs, err := dbTx.ValidateAndUpdate(&reZip3)
+			if err != nil {
+				return fmt.Errorf("failed to update %v: %v", reZip3, err)
+			}
+			if verrs.HasAny() {
+				return fmt.Errorf("failed to validate %v: %v", reZip3, verrs)
+			}
+		} else {
+			rateAreaID, found := gre.domesticRateAreaToIDMap[zip3RateArea.RateArea]
+			if !found {
+				return fmt.Errorf("failed to map %s rate area to ID", zip3RateArea.RateArea)
+			}
+			reZip3.RateAreaID = &rateAreaID
+			reZip3.HasMultipleRateAreas = false
+			verrs, err := dbTx.ValidateAndUpdate(&reZip3)
+			if err != nil {
+				return fmt.Errorf("failed to update %v: %v", reZip3, err)
+			}
+			if verrs.HasAny() {
+				return fmt.Errorf("failed to validate %v: %v", reZip3, verrs)
+			}
+		}
+	}
 
-	// Load re_zip3s and find record where re_zip3s zip3 == fixture zip3
-			// if fixture rate_area == 'ZIP' set re_zip3s has_multiple_rate_areas to true
-			// else
-				// load re_rate_areas and find re_rate_areas code == fixture rate_area
-				// set re_zip3s rate_area_id to re_rate_areas id
-				// set re_zip3s has_multiple_rate_areas to false
 	return nil
 }
 
-func (gre *GHCRateEngineImporter) mapZip5s(db *pop.Connection) error {
-	// Load /fixtures/tariff400ng_zip5s_rate_areas_fixture.csv and for each row
-	// var zip5 = fixture zip5
-	// var rate_area = fixture rate_area
+func (gre *GHCRateEngineImporter) createZip5s(db *pop.Connection) error {
+	//TODO 1: Load /fixtures/tariff400ng_zip5s_rate_areas_fixture.csv
+	//TODO 2: Iterate over each row in CSV fixture
+	//TODO 3: Store zip5 and rate_area value from each row of the CSV
+	//TODO 4: Create a new record in re_zip5s_rate_areas table for each row of the CSV fixture
+	//TODO 5: Store zip5 value in record
+	//TODO 6: Find the corresponding re_rate_areas record and associate it with the new re_zip5s record
 
-	// Create a new record in re_zip5_rate_areas where re_zip5_rate_areas zip5 == fixture zip5
-	// load re_rate_areas and find re_rate_areas code == fixture rate_area
-	// set re_zip5_rate_areas rate_area_id to re_rate_areas id
 	return nil
 }
