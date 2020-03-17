094645f96188c9f71bbbd21a5466abae59be2e29
ralren <ren@truss.works>
2020-03-17 23:01:37 +0000

    Deal with zero values and make them null instead

--- migrations/app/schema/20200313212635_alter_move_task_orders_nullability.up.sql
+++ migrations/app/schema/20200313212635_alter_move_task_orders_nullability.up.sql
@@ -1,2 +1,5 @@
 ALTER TABLE move_task_orders
     ALTER COLUMN move_order_id SET NOT NULL;
+
+UPDATE move_task_orders SET ppm_type = NULL WHERE ppm_type = '';
+UPDATE move_task_orders SET ppm_estimated_weight = NULL WHERE ppm_estimated_weight = 0;
