eb5e32a0ef683f181415874e128060349c7829ba
Harsha Murthy <harsha@truss.works>
2020-03-20 14:36:08 +0000

    Add migration for missing zip3s

---
+++ migrations/app/schema/20200319211333_add_missing_zip3s_to_tariff400ng_zip3s_table.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 533, 'Milwaukee', 'WI', 872, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 536, 'Madison', 'WI', 868, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 555, 'Montrose', 'WI', 416, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 771, 'Houston', 'TX', 765, 'US68', 6, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 909, 'Los Angeles', 'CA', 57, 'US88', 2, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 995, 'Kodiak', 'AK', 22, 'US8050500', 14, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 998, 'Wrangell', 'AK', 22, 'US8030400', 14, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 999, 'Petersburg', 'AK', 22, 'US8030400', 14, now(), now());
