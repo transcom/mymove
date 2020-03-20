c399c56f8038ceaeaf12b5568061f6e10403c368
Harsha Murthy <harsha@truss.works>
2020-03-20 16:54:28 +0000

    Add explicit UUIDs

--- migrations/app/schema/20200319211333_add_missing_zip3s_to_tariff400ng_zip3s_table.up.sql
+++ migrations/app/schema/20200319211333_add_missing_zip3s_to_tariff400ng_zip3s_table.up.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 533, 'Milwaukee', 'WI', 872, 'US32', 7, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 536, 'Madison', 'WI', 868, 'US32', 7, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 555, 'Montrose', 'WI', 416, 'US32', 7, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 771, 'Houston', 'TX', 765, 'US68', 6, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 909, 'Los Angeles', 'CA', 57, 'US88', 2, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 995, 'Kodiak', 'AK', 22, 'US8050500', 14, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 998, 'Wrangell', 'AK', 22, 'US8030400', 14, now(), now());
-INSERT INTO public.tariff400ng_zip3s VALUES (uuid_generate_v4(), 999, 'Petersburg', 'AK', 22, 'US8030400', 14, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('D81B54D6-C8FC-4655-9807-9BB96D34DE2E', 533, 'Milwaukee', 'WI', 872, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('F3ED73E9-B036-49F8-BAA3-775EA127FBB1', 536, 'Madison', 'WI', 868, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('E198909A-89CC-4241-9D5A-1D94470145F6', 555, 'Montrose', 'WI', 416, 'US32', 7, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('746E6E65-9416-4243-930F-479887A29F1D', 771, 'Houston', 'TX', 765, 'US68', 6, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('7EB7E45C-1FB7-4719-A736-772D3D3F0408', 909, 'Los Angeles', 'CA', 57, 'US88', 2, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('E94B7825-648B-46AC-A6C8-43C92B7E9E31', 995, 'Kodiak', 'AK', 22, 'US8050500', 14, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('B0568066-747F-4079-ADF7-D99DFE368962', 998, 'Wrangell', 'AK', 22, 'US8030400', 14, now(), now());
+INSERT INTO public.tariff400ng_zip3s VALUES ('A9336E38-8554-4A8F-90BF-AE03A00DB801', 999, 'Petersburg', 'AK', 22, 'US8030400', 14, now(), now());
