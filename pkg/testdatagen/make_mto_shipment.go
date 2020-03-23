bfa19100a6fca9677f0e0aa2f64aa674f9c47208
Shimona Carvalho <shimona@truss.works>
2020-03-23 23:32:17 +0000

    Added test to catch this issue

--- pkg/testdatagen/make_mto_shipment.go
+++ pkg/testdatagen/make_mto_shipment.go
@@ -70,3 +70,64 @@ func MakeMTOShipment(db *pop.Connection, assertions Assertions) models.MTOShipme
 
 	return MTOShipment
 }
+
+// MakeMTOShipmentMinimal creates a single MTOShipment with a minimal set of data as could be possible
+// through milmove UI.
+func MakeMTOShipmentMinimal(db *pop.Connection, assertions Assertions) models.MTOShipment {
+	moveTaskOrder := assertions.MoveTaskOrder
+	if isZeroUUID(moveTaskOrder.ID) {
+		moveTaskOrder = MakeMoveTaskOrder(db, assertions)
+	}
+	pickupAddress := assertions.MTOShipment.PickupAddress
+	if isZeroUUID(pickupAddress.ID) {
+		pickupAddress = MakeAddress(db, assertions)
+	}
+	destinationAddress := assertions.MTOShipment.DestinationAddress
+	if isZeroUUID(destinationAddress.ID) {
+		destinationAddress = MakeAddress2(db, assertions)
+	}
+
+	shipmentType := models.MTOShipmentTypeHHG
+	if assertions.MTOShipment.ShipmentType != "" {
+		shipmentType = assertions.MTOShipment.ShipmentType
+	}
+
+	// mock remarks
+	remarks := "please treat gently"
+	rejectionReason := "shipment not good enough"
+
+	// mock weights
+	actualWeight := unit.Pound(980)
+
+	// mock dates
+	requestedPickupDate := time.Date(TestYear, time.March, 15, 0, 0, 0, 0, time.UTC)
+
+	MTOShipment := models.MTOShipment{
+		MoveTaskOrder:            moveTaskOrder,
+		MoveTaskOrderID:          moveTaskOrder.ID,
+		RequestedPickupDate:      &requestedPickupDate,
+		CustomerRemarks:          &remarks,
+		PickupAddress:            pickupAddress,
+		PickupAddressID:          pickupAddress.ID,
+		DestinationAddress:       destinationAddress,
+		DestinationAddressID:     destinationAddress.ID,
+		PrimeActualWeight:        &actualWeight,
+		SecondaryPickupAddress:   &pickupAddress,
+		SecondaryDeliveryAddress: &destinationAddress,
+		ShipmentType:             shipmentType,
+		Status:                   "SUBMITTED",
+		RejectionReason:          &rejectionReason,
+	}
+
+	if assertions.MTOShipment.Status == models.MTOShipmentStatusApproved {
+		approvedDate := time.Date(TestYear, time.March, 20, 0, 0, 0, 0, time.UTC)
+		MTOShipment.ApprovedDate = &approvedDate
+	}
+
+	// Overwrite values with those from assertions
+	mergeModels(&MTOShipment, assertions.MTOShipment)
+
+	mustCreate(db, &MTOShipment)
+
+	return MTOShipment
+}
