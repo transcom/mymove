e159672e6791751b2779ea3c9d1aed18c6987211
Donald Thai <donald@truss.works>
2020-03-24 22:59:36 +0000

    Update column comment.

--- migrations/app/schema/20200323215437_mto_service_items_add_dimensions.up.sql
+++ migrations/app/schema/20200323215437_mto_service_items_add_dimensions.up.sql
@@ -11,8 +11,8 @@ ALTER TABLE "mto_service_items"
     ADD COLUMN "crate_dimension_id" uuid REFERENCES "mto_service_item_dimensions";
 
 -- comments on columns
-COMMENT ON COLUMN "mto_service_item_dimensions"."length_thousandth_inches" IS 'Length of a crate or item. 1000 thousand inches (thou) = 1 inch.';
-COMMENT ON COLUMN "mto_service_item_dimensions"."height_thousandth_inches" IS 'Height of a crate or item. 1000 thousand inches (thou) = 1 inch.';
-COMMENT ON COLUMN "mto_service_item_dimensions"."width_thousandth_inches" IS 'Width of a crate or item. 1000 thousand inches (thou) = 1 inch.';
+COMMENT ON COLUMN "mto_service_item_dimensions"."length_thousandth_inches" IS 'Length of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
+COMMENT ON COLUMN "mto_service_item_dimensions"."height_thousandth_inches" IS 'Height of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
+COMMENT ON COLUMN "mto_service_item_dimensions"."width_thousandth_inches" IS 'Width of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
 COMMENT ON COLUMN "mto_service_items"."item_dimension_id" IS 'Dimensions for the item. Ex: Crating a decorated horse head and taking dimensions of the item.';
 COMMENT ON COLUMN "mto_service_items"."crate_dimension_id" IS 'Dimensions for the crate. Ex: Crating a decorated horse head and taking dimensions of the create.';
