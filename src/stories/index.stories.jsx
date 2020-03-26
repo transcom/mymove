460c99bd5b832ed56333ece7be3f31092878d938
Shauna Keating <shauna@truss.works>
2020-03-26 11:22:11 -0400

    wraps table cells and their labels with a div to make it easier to style the layout of the storybook page

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -219,36 +219,46 @@ storiesOf('Components|Containers', module).add('all', () => (
 storiesOf('Components|Tables', module)
   .add('Table Elements', () => (
     <div style={{ padding: '20px' }}>
-      <code>cell-bg</code>
-      <table>
-        <tr>
-          <td>Type something</td>
-        </tr>
-      </table>
-      <code>td:hover</code>
-      <table>
-        <tr>
-          <td class="hover">Type something</td>
-        </tr>
-      </table>
-      <code>td:locked</code>
-      <table>
-        <tr>
-          <td class="locked">Type something</td>
-        </tr>
-      </table>
-      <code>td-numeric</code>
-      <table>
-        <tr>
-          <td>Type something</td>
-        </tr>
-      </table>
-      <code>th</code>
-      <table>
-        <tr>
-          <th>Type something</th>
-        </tr>
-      </table>
+      <div className="sb-table-wrapper">
+        <code>cell-bg</code>
+        <table>
+          <tr>
+            <td>Table Cell Content</td>
+          </tr>
+        </table>
+      </div>
+      <div className="sb-table-wrapper">
+        <code>td:hover</code>
+        <table>
+          <tr>
+            <td className="hover">Table Cell Content</td>
+          </tr>
+        </table>
+      </div>
+      <div className="sb-table-wrapper">
+        <code>td:locked</code>
+        <table>
+          <tr>
+            <td className="locked">Table Cell Content</td>
+          </tr>
+        </table>
+      </div>
+      <div className="sb-table-wrapper">
+        <code>td-numeric</code>
+        <table>
+          <tr>
+            <td>Table Cell Content</td>
+          </tr>
+        </table>
+      </div>
+      <div className="sb-table-wrapper">
+        <code>th</code>
+        <table>
+          <tr>
+            <th>Table Cell Content</th>
+          </tr>
+        </table>
+      </div>
     </div>
   ))
   .add('Basic Tables', () => (
