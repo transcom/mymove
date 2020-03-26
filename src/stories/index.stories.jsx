fb99b9a3c846255159457733a7a8b4bd9354bb65
Shauna Keating <shauna@truss.works>
2020-03-26 12:02:55 -0400

    adds styling to display table cell blocks instorybook in a way that uses the space well

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -225,89 +225,111 @@ storiesOf('Components|Tables', module)
         <div className="sb-table-wrapper">
           <code>cell-bg</code>
           <table>
-            <tr>
-              <td>Table Cell Content</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Table Cell Content</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>td:hover</code>
           <table>
-            <tr>
-              <td className="hover">Table Cell Content</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td className="hover">Table Cell Content</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>td:locked</code>
           <table>
-            <tr>
-              <td className="locked">Table Cell Content</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td className="locked">Table Cell Content</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>td-numeric</code>
           <table>
-            <tr>
-              <td>Table Cell Content</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Table Cell Content</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th</code>
           <table>
-            <tr>
-              <th>Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th>Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th—sortAscending</code>
           <table>
-            <tr>
-              <th className="sortAscending">Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th className="sortAscending">Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th—sortDescending</code>
           <table>
-            <tr>
-              <th className="sortDescending">Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th className="sortDescending">Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th—numeric</code>
           <table>
-            <tr>
-              <th>Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th>Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th—small</code>
           <table className="table--small">
-            <tr>
-              <th>Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th>Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>th—small—numeric</code>
           <table className="table--small">
-            <tr>
-              <th className="--numeric">Table Cell Content</th>
-            </tr>
+            <thead>
+              <tr>
+                <th className="--numeric">Table Cell Content</th>
+              </tr>
+            </thead>
           </table>
         </div>
         <div className="sb-table-wrapper">
           <code>td—filter</code>
           <table>
-            <tr>
-              <td className="--filter">Table Cell Content</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td className="--filter">Table Cell Content</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
