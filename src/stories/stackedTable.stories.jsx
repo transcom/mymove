ae4f588533fdcdcf5eca7c951656eb62661aa31b
Ron Cruz <Ronolibert@gmail.com>
2020-03-27 11:04:10 +0000

    Add storybook example for icon buttons to be styled later

--- src/stories/stackedTable.stories.jsx
+++ src/stories/stackedTable.stories.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
-// import { action } from '@storybook/addon-actions';
+import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
-// import { ReactComponent as EditIcon } from 'shared/images/edit-24px.svg';
+import { Button } from '@trussworks/react-uswds';
+import { ReactComponent as EditIcon } from 'shared/images/edit-24px.svg';
 import { StackedTable, StackedTableRow, StackedTableHeader, StackedTableData } from '../components/StackedTable';
 
 const StackedTableExample = () => (
   <div>
     <StackedTable fullWidth>
       <StackedTableRow>
-        <StackedTableHeader>Orders Number</StackedTableHeader>
-        <StackedTableData>999999999</StackedTableData>
+        <StackedTableHeader>Table Header</StackedTableHeader>
+        <StackedTableData>Table Data</StackedTableData>
       </StackedTableRow>
       <StackedTableRow>
-        <StackedTableHeader>Authorized entitlement</StackedTableHeader>
-        <StackedTableData>999999999</StackedTableData>
+        <StackedTableHeader>Table Header</StackedTableHeader>
+        <StackedTableData>Table Data</StackedTableData>
       </StackedTableRow>
     </StackedTable>
   </div>
 );
 
-storiesOf('Components|StackedTable', module).add('default', () => <StackedTableExample />);
+const StackedTableWithButtons = () => (
+  <div>
+    <StackedTable fullWidth>
+      <StackedTableRow>
+        <StackedTableHeader>Table Header</StackedTableHeader>
+        <StackedTableData>
+          Table Data
+          <Button className="usa-button--icon" onClick={action('should open edit form')}>
+            <span className="icon">
+              <EditIcon />
+            </span>
+            <span>Icon button text</span>
+          </Button>
+        </StackedTableData>
+      </StackedTableRow>
+      <StackedTableRow>
+        <StackedTableHeader>Table Header</StackedTableHeader>
+        <StackedTableData>
+          Table Data
+          <Button className="usa-button--icon" onClick={action('should open edit form')}>
+            <span className="icon">
+              <EditIcon />
+            </span>
+            <span>Icon button text</span>
+          </Button>
+        </StackedTableData>
+      </StackedTableRow>
+    </StackedTable>
+  </div>
+);
+
+storiesOf('Components|StackedTable', module)
+  .add('default', () => <StackedTableExample />)
+  .add('with buttons to edit', () => <StackedTableWithButtons />);
