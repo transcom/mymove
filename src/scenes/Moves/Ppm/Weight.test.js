0c56f1765f235b5ceb75099fc6394c726da4c311
lindsay t <techells22@gmail.com>
2020-03-28 15:28:04 +0000

    update tests

--- src/scenes/Moves/Ppm/Weight.test.js
+++ src/scenes/Moves/Ppm/Weight.test.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { PpmWeight } from './Weight';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 
 describe('Weight', () => {
+  const moveDate = moment().add(7, 'day');
   const minProps = {
+    selectedWeightInfo: { min: 0, max: 0 },
     hasLoadSuccess: true,
-    match: { params: { moveId: 'someID' } },
-    orders: { id: 1 },
-    loadPPMs: jest.fn(),
-    currentPPM: {},
-    getPpmWeightEstimate: jest.fn(),
     entitlement: {
       weight: 0,
       pro_gear: 0,
       pro_gear_spouse: 0,
     },
+    currentPPM: {
+      original_move_date: moveDate,
+      pickup_postal_code: '00000',
+    },
+    tempCurrentPPM: {
+      original_move_date: moveDate,
+      pickup_postal_code: '00000',
+    },
+    orders: { id: 1 },
+    getPpmWeightEstimate: jest.fn(),
   };
   it('Component renders', () => {
     expect(shallow(<PpmWeight {...minProps} />).length).toEqual(1);
@@ -22,31 +30,25 @@ describe('Weight', () => {
 
   describe('Test estimate icon', () => {
     let wrapper;
-    const iconAndTextProps = {
-      currentPPM: {},
-      orders: { id: 1 },
-      getPpmWeightEstimate: jest.fn(),
-    };
-    const match = { params: { moveId: 'someID' } };
     describe('Move under 500 lbs', () => {
       it('Should show car icon for 499 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={499} match={match} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 499 }} />);
         expect(wrapper.find({ 'data-cy': 'vehicleIcon' }).prop('src')).toEqual('car-gray.svg');
       });
     });
     describe('Move between 500 lbs and 1499 lbs', () => {
       it('Should show trailer icon for 500 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={500} match={match} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 500 }} />);
         expect(wrapper.find({ 'data-cy': 'vehicleIcon' }).prop('src')).toEqual('trailer-gray.svg');
       });
       it('Should show trailer icon for 1499 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={1499} match={match} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 1499 }} />);
         expect(wrapper.find({ 'data-cy': 'vehicleIcon' }).prop('src')).toEqual('trailer-gray.svg');
       });
     });
     describe('Move 1500 lbs or greater', () => {
       it('Should show truck icon for 1500 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={1500} match={match} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 1500 }} />);
         expect(wrapper.find({ 'data-cy': 'vehicleIcon' }).prop('src')).toEqual('truck-gray.svg');
       });
     });
@@ -54,27 +56,22 @@ describe('Weight', () => {
 
   describe('Test estimate text', () => {
     let wrapper;
-    const iconAndTextProps = {
-      currentPPM: {},
-      orders: { id: 1 },
-      getPpmWeightEstimate: jest.fn(),
-    };
 
     describe('Move under 500 lbs', () => {
       it('Should show text for 499 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={499} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 499 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual('Just a few things. One trip in a car.');
       });
     });
     describe('Move between 500 lbs and 1000 lbs', () => {
       it('Should show text for 500 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={500} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 500 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Studio apartment, minimal stuff. A large car, a pickup, a van, or a car with trailer.',
         );
       });
       it('Should show text for 999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Studio apartment, minimal stuff. A large car, a pickup, a van, or a car with trailer.',
         );
@@ -82,13 +79,13 @@ describe('Weight', () => {
     });
     describe('Move between 1000 lbs and 2000 lbs', () => {
       it('Should show text for 1000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={1000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 1000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '1-2 rooms, light furniture. A pickup, a van, or a car with a small or medium trailer.',
         );
       });
       it('Should show text for 1999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={1999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 1999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '1-2 rooms, light furniture. A pickup, a van, or a car with a small or medium trailer.',
         );
@@ -96,13 +93,13 @@ describe('Weight', () => {
     });
     describe('Move between 2000 lbs and 3000 lbs', () => {
       it('Should show text for 2000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={2000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 2000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '2-3 rooms, some bulky items. Cargo van, small or medium moving truck, medium or large cargo trailer.',
         );
       });
       it('Should show text for 2999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={2999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 2999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '2-3 rooms, some bulky items. Cargo van, small or medium moving truck, medium or large cargo trailer.',
         );
@@ -110,13 +107,13 @@ describe('Weight', () => {
     });
     describe('Move between 3000 lbs and 4000 lbs', () => {
       it('Should show text for 3000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={3000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 3000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '3-4 rooms. Small to medium moving truck, or a couple of trips.',
         );
       });
       it('Should show text for 3999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={3999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 3999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '3-4 rooms. Small to medium moving truck, or a couple of trips.',
         );
@@ -124,13 +121,13 @@ describe('Weight', () => {
     });
     describe('Move between 4000 lbs and 5000 lbs', () => {
       it('Should show text for 4000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={4000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 4000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '4+ rooms, or just a lot of large, heavy things. Medium or large moving truck, or multiple trips.',
         );
       });
       it('Should show text for 4999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={4999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 4999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           '4+ rooms, or just a lot of large, heavy things. Medium or large moving truck, or multiple trips.',
         );
@@ -138,13 +135,13 @@ describe('Weight', () => {
     });
     describe('Move between 5000 lbs and 6000 lbs', () => {
       it('Should show text for 5000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={5000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 5000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Many rooms, many things, lots of them heavy. Medium or large moving truck, or multiple trips.',
         );
       });
       it('Should show text for 5999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={5999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 5999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Many rooms, many things, lots of them heavy. Medium or large moving truck, or multiple trips.',
         );
@@ -152,13 +149,13 @@ describe('Weight', () => {
     });
     describe('Move between 6000 lbs and 7000 lbs', () => {
       it('Should show text for 6000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={6000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 6000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Large house, a lot of things. The biggest rentable moving trucks, or multiple trips or vehicles.',
         );
       });
       it('Should show text for 6999 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={6999} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 6999 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'Large house, a lot of things. The biggest rentable moving trucks, or multiple trips or vehicles.',
         );
@@ -166,7 +163,7 @@ describe('Weight', () => {
     });
     describe('Move 7000 lbs or over', () => {
       it('Should show text for 7000 lbs', () => {
-        wrapper = shallow(<PpmWeight {...minProps} {...iconAndTextProps} currentWeight={7000} />);
+        wrapper = shallow(<PpmWeight {...minProps} currentPPM={{ weight_estimate: 7000 }} />);
         expect(wrapper.find({ 'data-cy': 'estimateText' }).text()).toEqual(
           'A large house or small palace, many heavy or bulky items. Multiple trips using large vehicles, or hire professional movers.',
         );
