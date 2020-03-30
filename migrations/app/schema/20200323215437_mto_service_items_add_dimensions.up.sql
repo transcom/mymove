364349266078d53581c569d9b1ba6fd704016280
Donald Thai <donald@truss.works>
2020-03-30 13:16:09 -0700

    add dimension type to distinguish from item and crate

--- migrations/app/schema/20200323215437_mto_service_items_add_dimensions.up.sql
+++ migrations/app/schema/20200323215437_mto_service_items_add_dimensions.up.sql
@@ -1,18 +1,21 @@
+-- create dimensions table
+-- row is deleted if the mto_service_item is deleted
+-- mto service item can only have an item or crate dimension
+CREATE TYPE "dimension_type" AS ENUM ('ITEM', 'CRATE');
 CREATE TABLE "mto_service_item_dimensions" (
     "id" uuid PRIMARY KEY NOT NULL,
+    "mto_service_item_id" uuid NOT NULL REFERENCES "mto_service_items" ON DELETE CASCADE,
+    "type" "dimension_type" NOT NULL,
     "length_thousandth_inches" INTEGER NOT NULL,
     "height_thousandth_inches" INTEGER NOT NULL,
     "width_thousandth_inches" INTEGER NOT NULL,
     "created_at" timestamptz NOT NULL,
-    "updated_at" timestamptz NOT NULL);
-
-ALTER TABLE "mto_service_items"
-    ADD COLUMN "item_dimension_id" uuid REFERENCES "mto_service_item_dimensions",
-    ADD COLUMN "crate_dimension_id" uuid REFERENCES "mto_service_item_dimensions";
+    "updated_at" timestamptz NOT NULL,
+    UNIQUE ("mto_service_item_id", "type")
+);
 
 -- comments on columns
+COMMENT ON COLUMN "mto_service_item_dimensions"."type" IS 'Specify what is being measured, the item itself or crate. Eg. Measuring dimensions for a decorated horse head, ITEM type.';
 COMMENT ON COLUMN "mto_service_item_dimensions"."length_thousandth_inches" IS 'Length of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
 COMMENT ON COLUMN "mto_service_item_dimensions"."height_thousandth_inches" IS 'Height of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
 COMMENT ON COLUMN "mto_service_item_dimensions"."width_thousandth_inches" IS 'Width of a crate or item. 1000 thousandth inches (thou) = 1 inch.';
-COMMENT ON COLUMN "mto_service_items"."item_dimension_id" IS 'Dimensions for the item. Ex: Crating a decorated horse head and taking dimensions of the item.';
-COMMENT ON COLUMN "mto_service_items"."crate_dimension_id" IS 'Dimensions for the crate. Ex: Crating a decorated horse head and taking dimensions of the create.';
