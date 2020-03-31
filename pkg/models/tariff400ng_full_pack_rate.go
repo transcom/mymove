85ec573bf1f94ce81c6f64a1a69268f62d961abc
noelle barber <noelle@truss.works>
2020-03-31 21:39:49 +0000

    group validators

--- pkg/models/tariff400ng_full_pack_rate.go
+++ pkg/models/tariff400ng_full_pack_rate.go
@@ -34,9 +34,9 @@ func (t *Tariff400ngFullPackRate) Validate(tx *pop.Connection) (*validate.Errors
 	return validate.Validate(
 		&validators.IntIsPresent{Field: t.Schedule, Name: "Schedule"},
 		&validators.IntIsPresent{Field: t.WeightLbsUpper.Int(), Name: "WeightLbsUpper"},
-		&validators.IntIsGreaterThan{Field: t.RateCents.Int(), Name: "RateCents", Compared: -1},
 		&validators.IntIsLessThan{Field: t.WeightLbsLower.Int(), Name: "WeightLbsLower",
 			Compared: t.WeightLbsUpper.Int()},
+		&validators.IntIsGreaterThan{Field: t.RateCents.Int(), Name: "RateCents", Compared: -1},
 		&validators.TimeIsPresent{Field: t.EffectiveDateLower, Name: "EffectiveDateLower"},
 		&validators.TimeIsPresent{Field: t.EffectiveDateUpper, Name: "EffectiveDateUpper"},
 		&validators.TimeAfterTime{
