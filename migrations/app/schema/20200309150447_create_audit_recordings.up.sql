6ad8b317b0ade8da0b9954d61a3f09e182a92769
Garrett Martin <garrett@truss.works>
2020-04-01 18:48:59 +0000

    some changes to audit_recordings schema

--- migrations/app/schema/20200309150447_create_audit_recordings.up.sql
+++ migrations/app/schema/20200309150447_create_audit_recordings.up.sql
@@ -1,17 +1,23 @@
+CREATE TYPE audit_type AS ENUM (
+	'adhoc',
+	'general'
+);
+
 CREATE TABLE audit_recordings
 (
 	id uuid PRIMARY KEY NOT NULL,
+	human_readable_id text,
 	event_name text,
 	first_name text,
 	last_name text,
 	email text,
 	record_data json,
-	record_type text,
-	payload json,
-	metadata json,
+	record_table text,
+	changed_columns json,
 	move_id uuid REFERENCES moves,
 	user_id uuid REFERENCES users,
 	client_cert_id uuid REFERENCES client_certs,
+	audit_type audit_type,
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
