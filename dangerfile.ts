51d9a656b3db95e96f3185ca9a6f17737b8c3f74
John Gedeon <john@truss.works>
2020-04-03 20:23:07 +0000

    Check for story id in PR title

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,9 +1,15 @@
 import { includes } from 'lodash';
 import { danger, warn, fail } from 'danger';
 
-// No PR is too small to include a description of why you made a change
-if (danger.github && danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+if (danger.github) {
+  // No PR is too small to include a description of why you made a change
+  if (danger.github.pr.body.length < 10) {
+    warn('Please include a description of your PR changes.');
+  }
+  // PRs should have a Jira ID in the title
+  if (!danger.github.pr.title.match(/^MB-\d+/)) {
+    warn('Please include the Jira ID at the start of the title with the format MB-123');
+  }
 }
 
 // load all modified and new files
