4f3de2409dfd57826686ccae9b03d5d287a58f86
John Gedeon <john@truss.works>
2020-04-03 20:12:49 +0000

    Update dangerfile.ts for new prettier rules

--- dangerfile.ts
+++ dangerfile.ts
@@ -10,15 +10,15 @@ if (danger.github && danger.github.pr.body.length < 10) {
 const allFiles = danger.git.modified_files.concat(danger.git.created_files);
 
 // Request changes to app code to also include changes to tests.
-const hasAppChanges = allFiles.some(p => !!p.match(/src\/.*\.jsx?/));
-const hasTestChanges = allFiles.some(p => !!p.match(/src\/.*\.test\.jsx?/));
+const hasAppChanges = allFiles.some((path) => !!path.match(/src\/.*\.jsx?/));
+const hasTestChanges = allFiles.some((path) => !!path.match(/src\/.*\.test\.jsx?/));
 if (hasAppChanges && !hasTestChanges) {
   warn('This PR does not include changes to unit tests, even though it affects app code.');
 }
 
 // Require new src/components files to include changes to storybook
-const hasComponentChanges = danger.git.created_files.some(p => includes(p, 'src/components'));
-const hasStorybookChanges = allFiles.some(p => includes(p, 'src/stories'));
+const hasComponentChanges = danger.git.created_files.some((path) => includes(path, 'src/components'));
+const hasStorybookChanges = allFiles.some((path) => includes(path, 'src/stories'));
 
 if (hasComponentChanges && !hasStorybookChanges) {
   fail('This PR does not include changes to storybook, even though it affects component code.');
