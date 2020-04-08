976439f514c7de57124f210f8d796cc0bc35de8a
Shauna Keating <shauna@truss.works>
2020-04-08 14:47:21 -0400

    adjusts styling of stacked table header

--- src/stories/stackedTable.stories.jsx
+++ src/stories/stackedTable.stories.jsx
@@ -3,10 +3,24 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@trussworks/react-uswds';
 import { ReactComponent as EditIcon } from 'shared/icon/edit.svg';
+import { ReactComponent as DocsIcon } from 'shared/icon/documents.svg';
 import { StackedTable, StackedTableRow, StackedTableHeader, StackedTableData } from '../components/StackedTable';
 
 const StackedTableExample = () => (
   <div className="table--stacked">
+    <div className="display-flex">
+      <div>
+        <h4>Orders</h4>
+      </div>
+      <div>
+        <Button className="usa-button--icon" onClick={action('clicked')}>
+          <span className="icon">
+            <DocsIcon />
+          </span>
+          <span>View orders</span>
+        </Button>
+      </div>
+    </div>
     <StackedTable fullWidth>
       <caption>
         <h4>Orders</h4>
@@ -27,10 +41,20 @@ const StackedTableExample = () => (
 
 const StackedTableWithButtons = () => (
   <div className="table--stacked table--stacked-wbuttons">
-    <StackedTable fullWidth>
-      <caption>
+    <div className="display-flex">
+      <div>
         <h4>Orders</h4>
-      </caption>
+      </div>
+      <div>
+        <Button className="usa-button--icon" onClick={action('clicked')}>
+          <span className="icon">
+            <DocsIcon />
+          </span>
+          <span>View orders</span>
+        </Button>
+      </div>
+    </div>
+    <StackedTable fullWidth>
       <col style={{ width: '25%' }} />
       <col style={{ width: '75%' }} />
       <StackedTableRow>
