41db310f1bd31da26eaff27301388d30f7dee81f
Ron Cruz <Ronolibert@gmail.com>
2020-04-10 12:28:17 +0000

    Update prime-api-client to utilize feature flag support-endpoints instead of separate support subdomain

--- pkg/auth/cookie.go
+++ pkg/auth/cookie.go
@@ -17,14 +17,13 @@ import (
 
 // ApplicationServername is a collection of all the servernames for the application
 type ApplicationServername struct {
-	MilServername     string
-	OfficeServername  string
-	AdminServername   string
-	OrdersServername  string
-	DpsServername     string
-	SddcServername    string
-	PrimeServername   string
-	SupportServername string
+	MilServername    string
+	OfficeServername string
+	AdminServername  string
+	OrdersServername string
+	DpsServername    string
+	SddcServername   string
+	PrimeServername  string
 }
 
 type errInvalidHostname struct {
