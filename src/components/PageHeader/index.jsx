2ec7f58df31e218cf469117d53cb9ac991c65239
Shauna Keating <shauna@truss.works>
2020-04-14 12:56:53 -0400

    adds styling for the name section on the left side of the page header and the information in the right side of the page header

--- src/components/PageHeader/index.jsx
+++ src/components/PageHeader/index.jsx
@@ -7,14 +7,26 @@ const cx = classNames.bind(styles);
 const PageHeader = () => (
   <div className={cx('page-header')}>
     <div>
-      <h2>
-        Smith, Kerry
-        <span data-testid="tag" className="usa-tag usa-tag--cyan usa-tag--large">
-          #ABC123K
-        </span>
-      </h2>
+      <div className={cx('name-block')}>
+        <h2>Smith, Kerry</h2>
+        <span className="usa-tag usa-tag--cyan usa-tag--large">#ABC123K</span>
+      </div>
+      <div>officer information</div>
     </div>
-    <div>right</div>
+    <div className={cx('info-block')}>
+      <div>
+        <p>Authorized origin</p>
+        <h4>JBSA Lackland</h4>
+      </div>
+      <div>
+        <p>Authorized destination</p>
+        <h4>JB Lewis-McChord</h4>
+      </div>
+      <div>
+        <p>Report by</p>
+        <h4>27 Mar 2020</h4>
+      </div>
+    </div>
   </div>
 );
 
