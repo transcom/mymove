f5f1318b738050e5c661040e30b3f8665fc2cc17
Garrett Martin <garrett@truss.works>
2020-04-15 16:09:55 +0000

    Fix test and remove GovBanner component

--- src/pages/TOO/accessorials.jsx
+++ src/pages/TOO/accessorials.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { GovBanner } from '@trussworks/react-uswds';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const Accessorials = () => {
   return (
     <>
-      <GovBanner />
       <h1>This is where we will put our accessorial components!</h1>
     </>
   );
--- src/pages/TOO/accessorials.test.jsx
+++ src/pages/TOO/accessorials.test.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { GovBanner } from '@trussworks/react-uswds';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { history, store } from 'shared/store';
 import Accessorials from './accessorials';
 
 describe('Accessorials', () => {
-  const wrapper = shallow(<Accessorials />);
-
-  it('should render the GovBanner', () => {
-    expect(wrapper.find(<GovBanner />).length).toEqual(1);
-  });
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Accessorials />
+      </ConnectedRouter>
+    </Provider>,
+  );
 
   it('should render the h1', () => {
-    expect(wrapper.text()).toContain('This is where we will put our accessorial components!');
+    expect(wrapper.contains(<h1>This is where we will put our accessorial components!</h1>)).toBe(true);
   });
 });
