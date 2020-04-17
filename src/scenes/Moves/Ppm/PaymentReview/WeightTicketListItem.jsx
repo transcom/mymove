f16ab49f0992fdc95af9f8a820ddcbd8b7822c79
Arianna Kellogg <ari@truss.works>
2020-04-17 23:15:09 +0000

    MB-1783 update variable to match naming convention

--- src/scenes/Moves/Ppm/PaymentReview/WeightTicketListItem.jsx
+++ src/scenes/Moves/Ppm/PaymentReview/WeightTicketListItem.jsx
@@ -65,7 +65,7 @@ class WeightTicketListItem extends Component {
     const isInfected = this.areUploadsInfected(uploads);
     const showWeightTicketIcon = weight_ticket_set_type !== 'PRO_GEAR';
     const showVehicleNickname = weight_ticket_set_type === 'BOX_TRUCK' || 'PRO_GEAR';
-    const showMakeModel = weight_ticket_set_type === 'CAR' || 'CAR_TRAILER';
+    const showVehicleMakeModel = weight_ticket_set_type === 'CAR' || 'CAR_TRAILER';
     return (
       <div className="ticket-item" style={{ display: 'flex' }}>
         {/* size of largest of the images */}
@@ -82,7 +82,7 @@ class WeightTicketListItem extends Component {
         <div style={{ flex: 1 }}>
           <div className="weight-li-item-container">
             <h4>
-              {showMakeModel && (
+              {showVehicleMakeModel && (
                 <>
                   {vehicle_make} {vehicle_model}
                 </>
