6c8e867338b87201208cabce3cd4613dc1e4a05b
Moncef Belyamani <moncef@truss.works>
2020-04-17 21:13:16 -0400

    Add guard rails for session config

--- pkg/cli/session.go
+++ pkg/cli/session.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
@@ -22,10 +23,46 @@ const (
 // InitSessionFlags initializes SessionFlags command line flags
 func InitSessionFlags(flag *pflag.FlagSet) {
 	flag.Duration(SessionIdleTimeoutInMinutesFlag, (time.Duration(SessionIdleTimeoutInMinutes) * time.Minute), "Session idle timeout in minutes")
-	flag.Duration(SessionLifetimeInHoursFlag, (time.Duration(SessionLifetimeInHours) * time.Hour), "Session absoluty expiry in hours")
+	flag.Duration(SessionLifetimeInHoursFlag, (time.Duration(SessionLifetimeInHours) * time.Hour), "Session absolute expiry in hours")
 }
 
 // CheckSession validates session command line flags
 func CheckSession(v *viper.Viper) error {
+	if err := ValidateSessionTimeout(v, SessionIdleTimeoutInMinutesFlag); err != nil {
+		return err
+	}
+
+	if err := ValidateSessionLifetime(v, SessionLifetimeInHoursFlag); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ValidateSessionTimeout validates session idle timeout
+func ValidateSessionTimeout(v *viper.Viper, flagname string) error {
+	environment := v.GetString(EnvironmentFlag)
+	timeout := v.GetDuration(flagname)
+
+	if environment == EnvironmentProd && (timeout < 15 || timeout > 60) {
+		return errors.Errorf("%s must be an integer between 15 and 60", SessionIdleTimeoutInMinutesFlag)
+	}
+
+	return nil
+}
+
+// ValidateSessionLifetime validates session lifetime
+func ValidateSessionLifetime(v *viper.Viper, flagname string) error {
+	environment := v.GetString(EnvironmentFlag)
+	lifetime := v.GetDuration(flagname)
+
+	if environment == EnvironmentProd && lifetime < 12 {
+		return errors.Errorf("%s must be at least 12 hours in production", SessionLifetimeInHoursFlag)
+	}
+
+	if lifetime < 1 {
+		return errors.Errorf("%s must be at least 1", SessionLifetimeInHoursFlag)
+	}
+
 	return nil
 }
