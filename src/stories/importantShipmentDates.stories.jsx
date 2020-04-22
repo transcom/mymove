b8bb10c7c31198a3b8477f6ed562c31b9f0e0b35
Garrett Martin <garrett@truss.works>
2020-04-22 14:51:54 +0000

    Move shipment dates component to more specific storybook category

--- src/stories/importantShipmentDates.stories.jsx
+++ src/stories/importantShipmentDates.stories.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 
 import ImportantShipmentDates from '../components/Office/ImportantShipmentDates';
 
-storiesOf('Components|ImportantShipmentDates', module).add('default', () => {
+storiesOf('TXO|ImportantShipmentDates', module).add('default', () => {
   return (
     <ImportantShipmentDates requestedPickupDate="Thursday, 26 Mar 2020" scheduledPickupDate="Friday, 27 Mar 2020" />
   );
