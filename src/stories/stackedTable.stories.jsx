e190d65f9482fe3c6afb339ec2af74ccd1c400bc
John Gedeon <john@truss.works>
2020-04-23 20:06:46 +0000

    Add example of stacked table with some editable rows

--- src/stories/stackedTable.stories.jsx
+++ src/stories/stackedTable.stories.jsx
@@ -101,6 +101,99 @@ const StackedTableWithButtons = () => (
     </table>
   </div>
 );
+const StackedTableWithSomeButtons = () => (
+  <div>
+    <div className="stackedtable-header">
+      <div>
+        <h4>Title Goes here</h4>
+      </div>
+    </div>
+    <table className="table--stacked table--stacked-wbuttons">
+      <colgroup>
+        <col style={{ width: '25%' }} />
+        <col style={{ width: '75%' }} />
+      </colgroup>
+      <tbody>
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <StackedTableRowForm
+          initialValues={{ madeUpField: '' }}
+          validationSchema={Yup.object({
+            madeUpField: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+          })}
+          onSubmit={action('Made Up Field Submit')}
+          onReset={action('Made Up Field Cancel')}
+          id="madeUpField"
+          name="madeUpField"
+          type="text"
+          label="Made Up Field"
+        />
+        <StackedTableRowForm
+          initialValues={{ madeUpField2: 'Other data' }}
+          validationSchema={Yup.object({
+            madeUpField2: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+          })}
+          onSubmit={action('Made Up Field 2 Submit')}
+          onReset={action('Made Up Field 2 Cancel')}
+          id="madeUpField2"
+          name="madeUpField2"
+          type="text"
+          label="Made Up Field 2"
+        />
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <StackedTableRowForm
+          initialValues={{ madeUpField3: 'More Data' }}
+          validationSchema={Yup.object({
+            madeUpField3: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+          })}
+          onSubmit={action('Made Up Field 3 Submit')}
+          onReset={action('Made Up Field 3 Cancel')}
+          id="madeUpField3"
+          name="madeUpField3"
+          type="text"
+          label="Made Up Field 3"
+        />
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+        <StackedTableRowForm
+          initialValues={{ madeUpField4: 'More Data' }}
+          validationSchema={Yup.object({
+            madeUpField4: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+          })}
+          onSubmit={action('Made Up Field 4 Submit')}
+          onReset={action('Made Up Field 4 Cancel')}
+          id="madeUpField4"
+          name="madeUpField4"
+          type="text"
+          label="Made Up Field 4"
+        />
+        <tr>
+          <th scope="row">Made Up Read Only Row</th>
+          <td>999999999</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
 
 storiesOf('Components|StackedTable', module)
   .add('default', () => (
@@ -111,5 +204,10 @@ storiesOf('Components|StackedTable', module)
   .add('with buttons to edit', () => (
     <div style={{ padding: `20px`, background: `#f0f0f0` }}>
       <StackedTableWithButtons />
+    </div>
+  ))
+  .add('with some buttons to edit', () => (
+    <div style={{ padding: `20px`, background: `#f0f0f0` }}>
+      <StackedTableWithSomeButtons />
     </div>
   ));
