dc538af6cdf63216322926e9028ee4c9cb3f7737
Arianna Kellogg <ari@truss.works>
2020-04-23 23:50:52 +0000

    MB-2421 add period after zip code hint

--- src/scenes/Moves/Ppm/DateAndLocation.jsx
+++ src/scenes/Moves/Ppm/DateAndLocation.jsx
@@ -162,7 +162,7 @@ export class DateAndLocation extends Component {
           <div style={{ marginTop: '0.5rem' }}>
             <span className="grey">
               The ZIP code for {currentOrders && currentOrders.new_duty_station.name} is{' '}
-              {currentOrders && currentOrders.new_duty_station.address.postal_code}{' '}
+              {currentOrders && currentOrders.new_duty_station.address.postal_code}.
             </span>
           </div>
           <SwaggerField fieldName="has_sit" swagger={this.props.schema} component={YesNoBoolean} />
