7458551aa15ecc784b721dad1750243e47214941
Sandy Wright <sandy@truss.works>
2020-04-27 12:09:18 -0500

    MB-2320 Add func to check for non-modifiable mto shipment fields

--- pkg/handlers/primeapi/mto_shipment.go
+++ pkg/handlers/primeapi/mto_shipment.go
@@ -1,8 +1,12 @@
 package primeapi
 
 import (
+	"time"
+
 	"github.com/go-openapi/runtime/middleware"
 	"go.uber.org/zap"
+
+	"github.com/transcom/mymove/pkg/models"
 
 	"github.com/transcom/mymove/pkg/gen/primemessages"
 
@@ -33,6 +37,82 @@ func ConvertToMTOShipment(updates *primemessages.PutMTOShipment) *primemessages.
 	return shipment
 }
 
+// UpdateMTOShipmentModel checks that only the fields that can be updated were passed into the payload,
+// then grabs the model
+func UpdateMTOShipmentModel(payload *primemessages.MTOShipment) (*models.MTOShipment, error) {
+	fieldsInError := make([]string, 0)
+
+	if payload.MoveTaskOrderID != "0" && payload.MoveTaskOrderID != "00000000-0000-0000-0000-000000000000" {
+		fieldsInError = append(fieldsInError, "moveTaskOrderID")
+	}
+
+	createdAt := time.Time(payload.CreatedAt)
+	if !createdAt.IsZero() {
+		fieldsInError = append(fieldsInError, "createdAt")
+	}
+
+	updatedAt := time.Time(payload.UpdatedAt)
+	if !updatedAt.IsZero() {
+		fieldsInError = append(fieldsInError, "updatedAt")
+	}
+
+	primeEstimatedWeightRecordedDate := time.Time(payload.PrimeEstimatedWeightRecordedDate)
+	if !primeEstimatedWeightRecordedDate.IsZero() {
+		fieldsInError = append(fieldsInError, "primeEstimatedWeightRecordedDate")
+	}
+
+	approvedDate := time.Time(payload.ApprovedDate)
+	if !approvedDate.IsZero() {
+		fieldsInError = append(fieldsInError, "approvedDate")
+	}
+
+	if payload.Status != "" {
+		fieldsInError = append(fieldsInError, "status")
+	}
+	if payload.RejectionReason != nil {
+		fieldsInError = append(fieldsInError, "rejectionReason")
+	}
+	if payload.CustomerRemarks != nil {
+		fieldsInError = append(fieldsInError, "customerRemarks")
+	}
+
+	if payload.Agents != nil {
+		var mtoShipmentIDErr bool
+		var createdAtErr bool
+		var updatedAtErr bool
+
+		for _, agent := range payload.Agents {
+			if agent.MtoShipmentID != "0" && agent.MtoShipmentID != "00000000-0000-0000-0000-000000000000" &&
+				agent.MtoShipmentID != payload.ID && !mtoShipmentIDErr {
+				mtoShipmentIDErr = true
+				fieldsInError = append(fieldsInError, "agents:mtoShipmentID")
+			}
+			createdAt := time.Time(agent.CreatedAt)
+			if !createdAt.IsZero() {
+				createdAtErr = true
+				fieldsInError = append(fieldsInError, "agents:createdAt")
+			}
+			updatedAt := time.Time(agent.UpdatedAt)
+			if !updatedAt.IsZero() {
+				updatedAtErr = true
+				fieldsInError = append(fieldsInError, "agents:updatedAt")
+			}
+
+			if mtoShipmentIDErr && createdAtErr && updatedAtErr {
+				break // we've found all the errors we're gonna find here
+			}
+		}
+	}
+
+	if len(fieldsInError) > 0 {
+		// todo return error
+	}
+
+	mtoShipment := payloads.MTOShipmentModel(payload)
+
+	return mtoShipment, nil
+}
+
 // UpdateMTOShipmentHandler is the handler to update MTO shipments
 type UpdateMTOShipmentHandler struct {
 	handlers.HandlerContext
@@ -44,7 +124,7 @@ func (h UpdateMTOShipmentHandler) Handle(params mtoshipmentops.UpdateMTOShipment
 	logger := h.LoggerFromRequest(params.HTTPRequest)
 
 	shipmentPayload := ConvertToMTOShipment(params.Body)
-	mtoShipment := payloads.MTOShipmentModel(shipmentPayload)
+	mtoShipment, _ := UpdateMTOShipmentModel(shipmentPayload)
 	eTag := params.IfMatch
 	logger.Info("primeapi.UpdateMTOShipmentHandler info", zap.String("pointOfContact", params.Body.PointOfContact))
 
