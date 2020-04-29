579c45aa8cc7ebacd6c9c6ba48a86cbc74a9c507
Garrett Martin <garrett@truss.works>
2020-04-29 18:39:17 +0000

    Add service item count to request service item table

--- src/components/RequestedServiceItemsTable/index.jsx
+++ src/components/RequestedServiceItemsTable/index.jsx
@@ -1,83 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ServiceItemTableHasImg from '../ServiceItemTableHasImg';
-// import PropTypes from 'prop-types';
-// import classNames from 'classnames/bind';
-// import styles from './index.module.scss';
-//
-// const cx = classNames.bind(styles);
 
-const RequestedServiceItemsTable = () => (
-  <ServiceItemTableHasImg
-    serviceItems={[
-      {
-        id: 'abc-123',
-        dateRequested: '20 Nov 2020',
-        serviceItem: 'Dom. Origin 1st Day SIT',
-        code: 'DOMSIT',
-        details: {
-          text: {
-            ZIP: '60612',
-            Reason: "here's the reason",
-          },
-          imgURL: null,
-        },
-      },
-      {
-        id: 'abc-1234',
-        dateRequested: '22 Nov 2020',
-        serviceItem: 'Dom. Destination 1st Day SIT',
-        code: 'DDFSIT',
-        details: {
-          text: {
-            'First available delivery date': '22 Nov 2020',
-            'First customer contact': '22 Nov 2020 12:00pm',
-            'Second customer contact': '22 Nov 2020 12:00pm',
-          },
-          imgURL: null,
-        },
-      },
-      {
-        id: 'cba-123',
-        dateRequested: '22 Nov 2020',
-        serviceItem: 'Dom. Origin Shuttle Service',
-        code: 'DOSHUT',
-        details: {
-          text: {
-            'Reason for request': "Here's the reason",
-            'Estimated weight': '3,500lbs',
-          },
-          imgURL: null,
-        },
-      },
-      {
-        id: 'cba-1234',
-        dateRequested: '22 Nov 2020',
-        serviceItem: 'Dom. Destination Shuttle Service',
-        code: 'DDSHUT',
-        details: {
-          text: {
-            'Reason for request': "Here's the reason",
-            'Estimated weight': '3,500lbs',
-          },
-          imgURL: null,
-        },
-      },
-      {
-        id: 'abc12345',
-        dateRequested: '22 Nov 2020',
-        serviceItem: 'Dom. Crating',
-        code: 'DCRT',
-        details: {
-          text: {
-            Description: "Here's the description",
-            'Item dimensions': '84"x26"x42"',
-            'Crate dimensions': '110"x36"x54"',
-          },
-          imgURL: 'https://live.staticflickr.com/4735/24289917967_27840ed1af_b.jpg',
-        },
-      },
-    ]}
-  />
-);
+const RequestedServiceItemsTable = ({ serviceItems }) => {
+  return (
+    <>
+      {/* eslint-disable-next-line */}
+      <h3>Requested service items ({serviceItems.length})</h3>
+      <ServiceItemTableHasImg serviceItems={serviceItems} />
+    </>
+  );
+};
+
+RequestedServiceItemsTable.propTypes = {
+  serviceItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      dateRequested: PropTypes.string,
+      serviceItem: PropTypes.string,
+      code: PropTypes.string,
+      details: PropTypes.object,
+    }),
+  ).isRequired,
+};
 
 export default RequestedServiceItemsTable;
