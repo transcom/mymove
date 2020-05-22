6a7ddd9c5dd199253109bd44dd0bbe36cd99b7e0
Kim Allen <kim@truss.works>
2020-05-22 19:35:46 +0000

    Apply service in sit estimate (while lacking ppm info)

--- pkg/handlers/internalapi/personally_procured_move_sit_estimate.go
+++ pkg/handlers/internalapi/personally_procured_move_sit_estimate.go
@@ -3,6 +3,8 @@ package internalapi
 import (
 	"time"
 
+	"github.com/transcom/mymove/pkg/services/ppmservices"
+
 	"github.com/transcom/mymove/pkg/models"
 
 	"github.com/go-openapi/runtime/middleware"
@@ -41,19 +43,17 @@ func (h ShowPPMSitEstimateHandler) Handle(params ppmop.ShowPPMSitEstimateParams)
 	if err != nil {
 		return handlers.ResponseForError(logger, err)
 	}
-	//TODO: run rate engine to determine winning parameters and therefore proper discount rate to use in SIT calculation
+
+	calculator := ppmservices.NewEstimateCalculator(h.DB(), logger, h.Planner())
+	// TODO: pass in through params the active PPM or PPM ID to run the calculations
+	tempPPM := models.PersonallyProcuredMove{}
+	costDetails, err := calculator.CalculateEstimatedCostDetails(&tempPPM, move.ID)
+	cost := rateengine.GetWinningCostMove(costDetails)
 
 	sitZip3 := rateengine.Zip5ToZip3(destinationZip)
 	cwtWeight := unit.Pound(params.WeightEstimate).ToCWT()
 	originalMoveDateTime := time.Time(params.OriginalMoveDate)
 
-	lhDiscount, sitDiscount, err := models.PPMDiscountFetch(h.DB(),
-		logger,
-		move,
-		params.OriginZip,
-		destinationZip,
-		time.Time(params.OriginalMoveDate),
-	)
 	if err != nil {
 		return handlers.ResponseForError(logger, err)
 	}
@@ -64,7 +64,7 @@ func (h ShowPPMSitEstimateHandler) Handle(params ppmop.ShowPPMSitEstimateParams)
 	}
 
 	// Swagger returns int64 when using the integer type
-	sitCharge := int64(sitComputation.ApplyDiscount(lhDiscount, sitDiscount))
+	sitCharge := int64(sitComputation.ApplyDiscount(cost.LHDiscount, cost.SITDiscount))
 
 	ppmSitEstimate := internalmessages.PPMSitEstimate{
 		Estimate: &sitCharge,
