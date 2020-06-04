53aba6e4640457fc78657c045bb897226d72f654
Shauna Keating <shauna@truss.works>
2020-06-04 10:34:33 -0400

    sets up document viewer menu component

--- src/components/DocumentViewer/menu.jsx
+++ src/components/DocumentViewer/menu.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 
 import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
 import { ReactComponent as DocMenu } from 'shared/icon/doc-menu.svg';
+import styles from './index.module.scss';
+
+const cx = classNames.bind(styles);
 
 const DocViewerMenu = () => (
-  <div className="doc-viewer--menu doc-viewer--menu--collapsed">
-    <XLightIcon />
+  <div className={cx('doc-viewer--menu doc-viewer--menu--collapsed')}>
     <DocMenu />
+    <XLightIcon />
+    <div className={cx('thumbnail-container')}>test</div>
   </div>
 );
 
