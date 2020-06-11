b60f9bee0e832a643c217672a1e92fd23b6a587f
Shauna Keating <shauna@truss.works>
2020-06-11 12:33:02 -0400

    adds styling for menu header

--- src/components/DocumentViewer/index.jsx
+++ src/components/DocumentViewer/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-// import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
+import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
 import { ReactComponent as DocMenu } from 'shared/icon/doc-menu.svg';
 import { Button } from '@trussworks/react-uswds';
 // import fakeDoc from 'shared/images/fake-doc.png';
@@ -16,10 +16,16 @@ const testThumbnail = (
 
 const DocViewerMenu = ({ menuClass }) => (
   <div className={`${styles.docViewerMenu} ${menuClass}`}>
-    <div className={styles.menuControls}>
-      <Button unstyled>
-        <DocMenu />
-      </Button>
+    <div className={styles.menuHeader}>
+      <h3>Documents</h3>
+      <div className={styles.menuControls}>
+        <Button unstyled className={styles.menuOpen}>
+          <DocMenu />
+        </Button>
+        <Button unstyled className={styles.menuClose}>
+          <XLightIcon />
+        </Button>
+      </div>
     </div>
     <div className={styles.thumbnailContainer}>
       {testThumbnail}
