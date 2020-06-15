bd35155747cc02b063402a88496047538f552fb6
Ron Cruz <Ronolibert@gmail.com>
2020-06-15 05:22:05 +0000

    Tests for available to prime and not available to prime cases

--- pkg/services/move_task_order/move_task_order_fetcher_test.go
+++ pkg/services/move_task_order/move_task_order_fetcher_test.go
@@ -1,8 +1,11 @@
 package movetaskorder_test
 
 import (
-	. "github.com/transcom/mymove/pkg/services/move_task_order"
+	"testing"
+	"time"
 
+	"github.com/transcom/mymove/pkg/models"
+	. "github.com/transcom/mymove/pkg/services/move_task_order"
 	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
@@ -42,4 +45,46 @@ func (suite *MoveTaskOrderServiceSuite) TestListMoveTaskOrdersFetcher() {
 	suite.NotNil(expectedMTO.ReferenceID)
 	suite.Nil(expectedMTO.AvailableToPrimeAt)
 	suite.False(expectedMTO.IsCanceled)
+}
+
+func (suite *MoveTaskOrderServiceSuite) TestListAllMoveTaskOrdersFetcher() {
+	suite.T().Run("all move task orders", func(t *testing.T) {
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{})
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{})
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{})
+
+		mtoFetcher := NewMoveTaskOrderFetcher(suite.DB())
+
+		moveTaskOrders, err := mtoFetcher.ListAllMoveTaskOrders(false, nil)
+		suite.NoError(err)
+
+		mto := moveTaskOrders[0]
+
+		suite.Equal(len(moveTaskOrders), 3)
+		suite.Nil(mto.AvailableToPrimeAt)
+	})
+
+	suite.T().Run("all move task orders that are available to prime", func(t *testing.T) {
+		time1 := time.Now()
+		time2 := time.Now()
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{
+			MoveTaskOrder: models.MoveTaskOrder{
+				AvailableToPrimeAt: &time1,
+			},
+		})
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{
+			MoveTaskOrder: models.MoveTaskOrder{
+				AvailableToPrimeAt: &time2,
+			},
+		})
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{})
+		testdatagen.MakeMoveTaskOrder(suite.DB(), testdatagen.Assertions{})
+
+		mtoFetcher := NewMoveTaskOrderFetcher(suite.DB())
+
+		moveTaskOrders, err := mtoFetcher.ListAllMoveTaskOrders(true, nil)
+		suite.NoError(err)
+
+		suite.Equal(len(moveTaskOrders), 2)
+	})
 }
