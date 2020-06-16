93f5f501a0057c47db43a16709eeb7452e68aeaf
lindsay t <techells22@gmail.com>
2020-06-16 20:15:55 +0000

    update initDODCertificates func

--- cmd/send_to_gex/main.go
+++ cmd/send_to_gex/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -15,6 +14,7 @@ import (
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
+	"github.com/transcom/mymove/pkg/cli"
 	"github.com/transcom/mymove/pkg/logging"
 	"github.com/transcom/mymove/pkg/server"
 	"github.com/transcom/mymove/pkg/services/invoice"
@@ -96,48 +96,44 @@ func main() {
 
 //TODO: Infra will work to refactor and reduce duplication (also found in cmd/milmove/main.go)
 func initDODCertificates(v *viper.Viper, logger *zap.Logger) ([]tls.Certificate, *x509.CertPool, error) {
-
-	tlsCert := v.GetString("move-mil-dod-tls-cert")
-	if len(tlsCert) == 0 {
-		return make([]tls.Certificate, 0), nil, errors.Errorf("%s is missing", "move-mil-dod-tls-cert")
+	tlsCertString := v.GetString(cli.MoveMilDoDTLSCertFlag)
+	tlsCerts := cli.ParseCertificates(tlsCertString)
+	if len(tlsCerts) == 0 {
+		return make([]tls.Certificate, 0), nil, errors.Errorf("%s is missing certificate PEM block", cli.MoveMilDoDTLSCertFlag)
+	}
+	if len(tlsCerts) > 1 {
+		return make([]tls.Certificate, 0), nil, errors.Errorf("%s has too many certificate PEM blocks", cli.MoveMilDoDTLSCertFlag)
 	}
 
-	caCert := v.GetString("move-mil-dod-ca-cert")
-	if len(caCert) == 0 {
-		return make([]tls.Certificate, 0), nil, errors.Errorf("%s is missing", "move-mil-dod-ca-cert")
+	logger.Info(fmt.Sprintf("certificate chain from %s parsed", cli.MoveMilDoDTLSCertFlag), zap.Any("count", len(tlsCerts)))
+	fmt.Println(len(tlsCerts))
+
+	caCertString := v.GetString(cli.MoveMilDoDCACertFlag)
+	caCerts := cli.ParseCertificates(caCertString)
+	if len(caCerts) == 0 {
+		return make([]tls.Certificate, 0), nil, errors.Errorf("%s is missing certificate PEM block", cli.MoveMilDoDTLSCertFlag)
 	}
+	logger.Info(fmt.Sprintf("certificate chain from %s parsed", cli.MoveMilDoDCACertFlag), zap.Any("count", len(caCerts)))
 
-	//Append move.mil cert with CA certificate chain
-	cert := bytes.Join(
-		[][]byte{
-			[]byte(tlsCert),
-			[]byte(caCert),
-		},
-		[]byte("\n"),
-	)
+	//Append move.mil cert with intermediate CA to create a validate certificate chain
+	cert := strings.Join(append(append(make([]string, 0), tlsCerts...), caCerts...), "\n")
 
-	key := []byte(v.GetString("move-mil-dod-tls-key"))
-	if len(key) == 0 {
-		return make([]tls.Certificate, 0), nil, errors.Errorf("%s is missing", "move-mil-dod-tls-key")
-	}
-
-	keyPair, err := tls.X509KeyPair(cert, key)
+	key := v.GetString(cli.MoveMilDoDTLSKeyFlag)
+	keyPair, err := tls.X509KeyPair([]byte(cert), []byte(key))
 	if err != nil {
-		return make([]tls.Certificate, 0), nil, errors.Wrap(err, "failed to parse DOD keypair for server")
+		return make([]tls.Certificate, 0), nil, errors.Wrap(err, "failed to parse DOD x509 keypair for server")
 	}
 
-	pathToPackage := v.GetString("dod-ca-package")
-	if len(pathToPackage) == 0 {
-		return make([]tls.Certificate, 0), nil, errors.Wrap(&errInvalidPKCS7{Path: pathToPackage}, fmt.Sprintf("%s is missing", "dod-ca-package"))
-	}
+	logger.Info("DOD keypair", zap.Any("certificates", len(keyPair.Certificate)))
 
+	pathToPackage := v.GetString(cli.DoDCAPackageFlag)
 	pkcs7Package, err := ioutil.ReadFile(pathToPackage) // #nosec
 	if err != nil {
-		return make([]tls.Certificate, 0), nil, errors.Wrap(err, fmt.Sprintf("%s is invalid", "dod-ca-package"))
+		return make([]tls.Certificate, 0), nil, errors.Wrap(err, fmt.Sprintf("%s is invalid", cli.DoDCAPackageFlag))
 	}
 
 	if len(pkcs7Package) == 0 {
-		return make([]tls.Certificate, 0), nil, errors.Wrap(&errInvalidPKCS7{Path: pathToPackage}, fmt.Sprintf("%s is an empty file", "dod-ca-package"))
+		return make([]tls.Certificate, 0), nil, errors.Wrap(&cli.ErrInvalidPKCS7{Path: pathToPackage}, fmt.Sprintf("%s is an empty file", cli.DoDCAPackageFlag))
 	}
 
 	dodCACertPool, err := server.LoadCertPoolFromPkcs7Package(pkcs7Package)
