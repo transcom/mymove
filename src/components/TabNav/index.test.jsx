fabedc0698837f333c5c1d76d4e4f6009a97155f
Duncan Spencer <duncan@truss.works>
2020-06-16 19:17:28 +0000

    correct TabNav component test

--- src/components/TabNav/index.test.jsx
+++ src/components/TabNav/index.test.jsx
@@ -7,16 +7,17 @@ describe('TabNav', () => {
   it('should render the tab navigation', () => {
     const options = [
       <a href="/test">
-        <span>Link 1</span>
+        <span className="tab-title">Link 1</span>
+        <Tag>5</Tag>
       </a>,
       <a href="/test2">
-        <span>Link 2</span>
+        <span className="tab-title">Link 2</span>
       </a>,
     ];
     const wrapper = shallow(<TabNav items={options} />);
-    expect(wrapper.find('.tab-title').first().text()).toBe('Option 1');
+    expect(wrapper.find('.tab-title').first().text()).toBe('Link 1');
     expect(wrapper.find(Tag).length).toBe(1);
-    expect(wrapper.find(Tag).children().text()).toBe('2');
-    expect(wrapper.find('.tab-title').last().text()).toBe('Option 3');
+    expect(wrapper.find(Tag).children().text()).toBe('5');
+    expect(wrapper.find('.tab-title').last().text()).toBe('Link 2');
   });
 });
