535b4ad1845195fd9c44f0166b0a096da08c7bf1
Shauna Keating <shauna@truss.works>
2020-06-16 15:14:01 -0400

    creates files for content portion for the doc viewer, begins to set up component in jsx file

---
+++ src/components/DocumentViewer/content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
+// import { ReactComponent as DocMenu } from 'shared/icon/doc-menu.svg';
+// import { Button } from '@trussworks/react-uswds';
+// import fakeDoc from 'shared/images/fake-doc.png';
+import styles from './content.module.scss';
+
+const DocViewerContent = () => <div className={`${styles.docViewerContent}`} />;
+
+export default DocViewerContent;
