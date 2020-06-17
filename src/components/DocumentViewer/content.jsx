eac8ad5f79e7ba69aa8ce7cae963a93206a3ed2f
Shauna Keating <shauna@truss.works>
2020-06-17 10:10:22 -0400

    adds alt attibute to doc image

--- src/components/DocumentViewer/content.jsx
+++ src/components/DocumentViewer/content.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 // import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
 // import { ReactComponent as DocMenu } from 'shared/icon/doc-menu.svg';
 // import { Button } from '@trussworks/react-uswds';
-// import fakeDoc from 'shared/images/fake-doc.png';
+import fakeDoc from 'shared/images/fake-doc.png';
 import styles from './content.module.scss';
 
 const DocViewerContent = () => (
   <div className={`${styles.docViewerContent}`}>
-    <div className={`${styles.titleBar}`}>test</div>
+    <div className={`${styles.titleBar}`}>I am title bar</div>
+    <div className={`${styles.docArea}`}>
+      <img src={`${fakeDoc}`} alt=" " />
+    </div>
+    <div className={`${styles.docControls}`}>I am document controls</div>
   </div>
 );
 
