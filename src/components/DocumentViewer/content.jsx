34f58536f739acd1f869a8717c56615e5fdb30ff
Shauna Keating <shauna@truss.works>
2020-06-18 22:40:04 +0000

    adds svg icon assets for doc control buttons, styles them and creates mixin for coloration on unstyled icon buttons

--- src/components/DocumentViewer/content.jsx
+++ src/components/DocumentViewer/content.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-// import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
 import { ReactComponent as ExternalLink } from 'shared/icon/external-link.svg';
+import { ReactComponent as ZoomIn } from 'shared/icon/zoom-in.svg';
+import { ReactComponent as ZoomOut } from 'shared/icon/zoom-out.svg';
+import { ReactComponent as RotateLeft } from 'shared/icon/rotate-counter-clockwise.svg';
+import { ReactComponent as RotateRight } from 'shared/icon/rotate-clockwise.svg';
+import { ReactComponent as ArrowLeft } from 'shared/icon/arrow-left.svg';
+import { ReactComponent as ArrowRight } from 'shared/icon/arrow-right.svg';
 import { Button } from '@trussworks/react-uswds';
 import fakeDoc from 'shared/images/fake-doc.png';
 import styles from './content.module.scss';
@@ -16,9 +21,34 @@ const DocViewerContent = () => (
       </Button>
     </div>
     <div className={`${styles.docArea}`}>
+      <div className={`${styles.docNav}`}>
+        <Button unstyled>
+          <ArrowLeft />
+        </Button>
+        <Button unstyled>
+          <ArrowRight />
+        </Button>
+      </div>
       <img src={`${fakeDoc}`} alt=" " />
     </div>
-    <div className={`${styles.docControls}`}>I am document controls</div>
+    <div className={`${styles.docControls}`}>
+      <Button unstyled>
+        <RotateLeft />
+        Rotate left
+      </Button>
+      <Button unstyled>
+        <RotateRight />
+        Rotate right
+      </Button>
+      <Button unstyled>
+        <ZoomOut />
+        Zoom out
+      </Button>
+      <Button unstyled>
+        <ZoomIn />
+        Zoom in
+      </Button>
+    </div>
   </div>
 );
 
