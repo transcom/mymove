ca2f4c8e1d58fc349013040c933b2361fa5ce8b1
noelle barber <noelle@truss.works>
2020-06-18 16:50:07 +0000

    Revert "Removed conversion to int/float as part of parsing for COPY statements"
    This reverts commit 4c1cb1382254b2217d2e6e3f9ed279502e720289.

--- pkg/migrate/lineToValues.go
+++ pkg/migrate/lineToValues.go
@@ -1,6 +1,7 @@
 package migrate
 
 import (
+	"strconv"
 	"strings"
 )
 
@@ -10,6 +11,10 @@ func lineToValues(line string) []interface{} {
 	for _, s := range parts {
 		if s == "\\N" {
 			values = append(values, nil)
+		} else if n, err := strconv.Atoi(s); err == nil {
+			values = append(values, int64(n))
+		} else if f, err := strconv.ParseFloat(s, 64); err == nil {
+			values = append(values, f)
 		} else {
 			values = append(values, s)
 		}
