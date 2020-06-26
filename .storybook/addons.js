cb5e05d0fde6b0914db930b02c744653b282644a
Shauna Keating <shauna@truss.works>
2020-06-26 19:43:51 +0000

    changes the addon order so Abstract is first

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,5 @@
+import 'storybook-addons-abstract/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@dump247/storybook-state/register';
-import 'storybook-addons-abstract/register';
