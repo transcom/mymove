b1d3336612e3f5540e1abb229b615a8ea9a5ab08
Shauna Keating <shauna@truss.works>
2020-07-01 18:40:17 +0000

    removes reference to storiesOf in doc viewer components

---
+++ src/components/documentViewer/Content/ContentArea.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import DocViewerContent from './Content';
+
+export default {
+  title: 'Components|Document Viewer',
+};
+
+export const ContentArea = () => <DocViewerContent />;
---
+++ src/components/documentViewer/Menu/Menu.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import DocViewerMenu from './Menu';
+
+export default {
+  title: 'Components|Document Viewer',
+};
+
+export const Menu = () => <DocViewerMenu />;
