37bb419f8b3a9eba7e00d5ee9261cb8de064546f
Shauna Keating <shauna@truss.works>
2020-07-01 19:10:17 +0000

    moves tags to their own stories file in csf format

--- src/stories/index.stories.jsx
+++ src/stories/index.stories.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
-import { Tag } from '@trussworks/react-uswds';
 
 import colors from '../shared/styles/colors.scss';
-
-import { ReactComponent as AlertIcon } from 'shared/icon/alert.svg';
 
 const filterGroup = (filter) => Object.keys(colors).filter((color) => color.indexOf(filter) === 0);
 
@@ -186,22 +183,3 @@ storiesOf('Global|Typography', module)
       </small>
     </div>
   ));
-
-// Tags
-
-storiesOf('Components|Tags', module).add('all', () => (
-  <div id="tags" style={{ padding: '20px' }}>
-    <hr />
-    <h3>Tags</h3>
-    <Tag>New</Tag>
-    <Tag className="usa-tag--green">Authorized</Tag>
-    <Tag className="usa-tag--red">Rejected</Tag>
-    <Tag className="usa-tag--yellow">Pending</Tag>
-    <Tag className="usa-tag--alert">
-      <AlertIcon />
-    </Tag>
-    <Tag className="usa-tag--teal">INTL</Tag>
-    <Tag>3</Tag>
-    <Tag className="usa-tag--cyan usa-tag--large">#ABC123K</Tag>
-  </div>
-));
