1048becfba1f86d0b31d5ea333eeac98d3e325fb
Shauna Keating <shauna@truss.works>
2020-07-01 21:00:13 +0000

    converts tab nav stories file to CSF format

--- src/stories/tabNav.stories.jsx
+++ src/stories/tabNav.stories.jsx
@@ -1,44 +1,47 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { Tag } from '@trussworks/react-uswds';
 
 import TabNav from '../components/TabNav';
 
-storiesOf('Components|TabNav', module)
-  .addDecorator(withKnobs)
-  .add('default', () => (
-    <TabNav
-      items={[
-        <a href="#" className="usa-current usa-nav__link" role="tab">
+export default {
+  title: 'Components|Tab Navigation',
+  decorators: [withKnobs],
+};
+
+export const Default = () => (
+  <TabNav
+    items={[
+      <a href="#" className="usa-current usa-nav__link" role="tab">
+        <span className="tab-title">Move details</span>
+      </a>,
+      <a href="#" className="usa-nav__link" role="tab">
+        <span className="tab-title">Move task order</span>
+      </a>,
+      <a href="#" className="usa-nav__link" role="tab">
+        <span className="tab-title">Payment requests</span>
+      </a>,
+    ]}
+    role="navigation"
+  />
+);
+
+export const withTag = () => (
+  <TabNav
+    items={[
+      <>
+        <a href="#" className="usa-nav__link" role="tab">
           <span className="tab-title">Move details</span>
-        </a>,
-        <a href="#" className="usa-nav__link" role="tab">
-          <span className="tab-title">Move task order</span>
-        </a>,
-        <a href="#" className="usa-nav__link" role="tab">
-          <span className="tab-title">Payment requests</span>
-        </a>,
-      ]}
-      role="navigation"
-    />
-  ))
-  .add('withTag', () => (
-    <TabNav
-      items={[
-        <>
-          <a href="#" className="usa-nav__link" role="tab">
-            <span className="tab-title">Move details</span>
-            <Tag>2</Tag>
-          </a>
-        </>,
-        <a href="#" className="usa-current usa-nav__link" role="tab">
-          <span className="tab-title">Move task order</span>
-        </a>,
-        <a href="#" className="usa-nav__link" role="tab">
-          <span className="tab-title">Payment requests</span>
-        </a>,
-      ]}
-      role="navigation"
-    />
-  ));
+          <Tag>2</Tag>
+        </a>
+      </>,
+      <a href="#" className="usa-current usa-nav__link" role="tab">
+        <span className="tab-title">Move task order</span>
+      </a>,
+      <a href="#" className="usa-nav__link" role="tab">
+        <span className="tab-title">Payment requests</span>
+      </a>,
+    ]}
+    role="navigation"
+  />
+);
