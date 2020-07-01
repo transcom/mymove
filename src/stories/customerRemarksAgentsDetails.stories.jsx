88310b3b0a46b6bc295d83a9dc05875b5d8b2e47
Shauna Keating <shauna@truss.works>
2020-07-01 21:37:30 +0000

    converts customer remarks agent details  to CSF

--- src/stories/customerRemarksAgentsDetails.stories.jsx
+++ src/stories/customerRemarksAgentsDetails.stories.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { withKnobs, text, object } from '@storybook/addon-knobs';
 
 import CustomerRemarksAgentsDetails from 'components/CustomerRemarksAgentsDetails';
 
-storiesOf('TOO/TIO Components|CustomerRemarksAgentsDetails', module)
-  .addDecorator(withKnobs)
-  .add('with no details', () => {
-    return (
-      <div id="containers" style={{ padding: '20px' }}>
-        <CustomerRemarksAgentsDetails />
-      </div>
-    );
-  })
-  .add('with details', () => {
-    const customerRemarks = 'This is a customer remark.';
-    const releasingAgent = {
-      firstName: 'FirstName',
-      lastName: 'LastName',
-      phone: '(111) 111-1111',
-      email: 'test@test.com',
-    };
-    const receivingAgent = {
-      firstName: 'FirstName',
-      lastName: 'LastName',
-      phone: '(111) 111-1111',
-      email: 'test@test.com',
-    };
+export default {
+  title: 'TOO&#47;TIO Components|CustomerRemarksAgentsDetails',
+  decorator: withKnobs,
+};
 
-    return (
-      <div id="containers" style={{ padding: '20px' }}>
-        <CustomerRemarksAgentsDetails
-          customerRemarks={text('customerRemarks', customerRemarks)}
-          releasingAgent={object('releasingAgent', releasingAgent)}
-          receivingAgent={object('receivingAgent', receivingAgent)}
-        />
-      </div>
-    );
-  });
+export const WithNoDetails = () => (
+  <div id="containers" style={{ padding: '20px' }}>
+    <CustomerRemarksAgentsDetails />
+  </div>
+);
+
+const customerRemarks = 'This is a customer remark.';
+const releasingAgent = {
+  firstName: 'FirstName',
+  lastName: 'LastName',
+  phone: '(111) 111-1111',
+  email: 'test@test.com',
+};
+const receivingAgent = {
+  firstName: 'FirstName',
+  lastName: 'LastName',
+  phone: '(111) 111-1111',
+  email: 'test@test.com',
+};
+export const WithDetails = () => (
+  <div id="containers" style={{ padding: '20px' }}>
+    <CustomerRemarksAgentsDetails
+      customerRemarks={text('customerRemarks', customerRemarks)}
+      releasingAgent={object('releasingAgent', releasingAgent)}
+      receivingAgent={object('receivingAgent', receivingAgent)}
+    />
+  </div>
+);
