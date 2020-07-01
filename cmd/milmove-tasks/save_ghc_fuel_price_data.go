3458ab2d55fbff5ba4a850d6d47ec72fd26a1a8e
Harsha Murthy <harsha@truss.works>
2020-07-01 15:44:39 +0000

    Ship it

--- cmd/milmove-tasks/save_ghc_fuel_price_data.go
+++ cmd/milmove-tasks/save_ghc_fuel_price_data.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	awssession "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -76,6 +80,40 @@ func saveGHCFuelPriceData(cmd *cobra.Command, args []string) error {
 		logger.Fatal("invalid configuration", zap.Error(err))
 	}
 
+	var session *awssession.Session
+	if v.GetBool(cli.DbIamFlag) {
+		c, errorConfig := cli.GetAWSConfig(v, v.GetBool(cli.VerboseFlag))
+		if errorConfig != nil {
+			logger.Fatal(errors.Wrap(errorConfig, "error creating aws config").Error())
+		}
+		s, errorSession := awssession.NewSession(c)
+		if errorSession != nil {
+			logger.Fatal(errors.Wrap(errorSession, "error creating aws session").Error())
+		}
+		session = s
+	}
+
+	var dbCreds *credentials.Credentials
+	if v.GetBool(cli.DbIamFlag) {
+		if session != nil {
+			// We want to get the credentials from the logged in AWS session rather than create directly,
+			// because the session conflates the environment, shared, and container metdata config
+			// within NewSession.  With stscreds, we use the Secure Token Service,
+			// to assume the given role (that has rds db connect permissions).
+			dbIamRole := v.GetString(cli.DbIamRoleFlag)
+			logger.Info(fmt.Sprintf("assuming AWS role %q for db connection", dbIamRole))
+			dbCreds = stscreds.NewCredentials(session, dbIamRole)
+		}
+	}
+
+	// Create a connection to the DB
+	dbConnection, err := cli.InitDatabase(v, dbCreds, logger)
+	if err != nil {
+		// No connection object means that the configuraton failed to validate and we should not startup
+		// A valid connection object that still has an error indicates that the DB is not up and we should not startup
+		logger.Fatal("Connecting to DB", zap.Error(err))
+	}
+
 	eiaURL := v.GetString(cli.EIAURLFlag)
 	eiaKey := v.GetString(cli.EIAKeyFlag)
 	newDieselFuelPriceInfo := ghcdieselfuelprice.NewDieselFuelPriceInfo(eiaURL, eiaKey, ghcdieselfuelprice.FetchEIAData, logger)
@@ -83,6 +121,11 @@ func saveGHCFuelPriceData(cmd *cobra.Command, args []string) error {
 	err = newDieselFuelPriceInfo.RunFetcher()
 	if err != nil {
 		logger.Fatal("error returned by RunFetcher function in ghcdieselfuelprice service", zap.Error(err))
+	}
+
+	err = newDieselFuelPriceInfo.RunStorer(dbConnection)
+	if err != nil {
+		logger.Fatal("error returned by RunStorer function in ghcdieselfuelprice service", zap.Error(err))
 	}
 	return nil
 }
