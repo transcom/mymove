a498df2cf5e0a762806a9c18406fe3aa557a23a1
pearl-truss <pearl@truss.works>
2020-07-02 21:39:00 +0000

    added detailed note on use of the hardcoded/default contract code

--- pkg/payment_request/service_param_value_lookups/service_area_origin_lookup.go
+++ pkg/payment_request/service_param_value_lookups/service_area_origin_lookup.go
@@ -55,6 +55,14 @@ func (r ServiceAreaOriginLookup) lookup(keyData *ServiceItemParamKeyData) (strin
 	query := db.Q().Join("re_zip3s", "re_zip3s.domestic_service_area_id = re_domestic_service_areas.id").
 		Join("re_contracts", "re_contracts.id = re_domestic_service_areas.contract_id").
 		Where("re_zip3s.zip3 = ?", zip3).
+		/*
+			DefaultContractCode, TRUSS_TEST is temporarily being used here because:
+			MTO and mtoServiceItem are not linked or associated with the contract record
+			MTO currently has a contractor_id but not a contract_id. 
+			If it'll will be possible for a MTO to contain service items that are associated with different contracts
+			It would be ideal for the mtoServiceItem records to contain a contract code that can then passed
+			to this query. Otherwise the MTO could contain the contract_id or contract_code
+		*/
 		Where("re_contracts.code = ?", ghcrateengine.DefaultContractCode)
 
 	err = query.First(&domesticServiceArea)
