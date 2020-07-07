71e81fcd3ffd4e5f2cd3f63e682bfad0e0546257
Suzanne Rozier <suz@truss.works>
2020-07-07 16:24:35 +0000

    Fix terser plugin options

--- terser-rescript.js
+++ terser-rescript.js
@@ -2,6 +2,7 @@
 const TerserPlugin = require('terser-webpack-plugin');
 
 // modify Terser settings in webpack config
+// defaults taken from https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpack.config.js
 module.exports = (webpackConfig) => {
   return {
     ...webpackConfig,
@@ -9,24 +10,23 @@ module.exports = (webpackConfig) => {
       ...webpackConfig.optimization,
       minimizer: [
         new TerserPlugin({
-          terserOptions: { parse: { ecma: 8 } },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-            drop_console: true,
-          },
-          mangle: { safari10: true },
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true,
+          terserOptions: {
+            parse: { ecma: 8 },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+            },
+            mangle: { safari10: true },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
           },
           parallel: 2,
-          cache: true,
           sourceMap: false,
-          extractComments: false,
         }),
       ],
     },
