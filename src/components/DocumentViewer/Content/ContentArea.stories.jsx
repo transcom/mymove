947b57f5d3d7fbafea7645d1a472217daddcadee
Suzanne Rozier <suz@truss.works>
2020-07-09 18:35:36 +0000

    Loki skip doc viewer test

--- src/components/DocumentViewer/Content/ContentArea.stories.jsx
+++ src/components/DocumentViewer/Content/ContentArea.stories.jsx
@@ -6,6 +6,7 @@ import DocViewerContent from './Content';
 
 export default {
   title: 'Components|Document Viewer|Content',
+  parameters: { loki: { skip: true } },
 };
 
 export const ContentArea = () => <DocViewerContent fileType="pdf" filePath={pdf} />;
