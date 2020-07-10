cec19a31aa93ed7b8cd8ab75f4d85600b6b373e7
Arianna Kellogg <ari@truss.works>
2020-07-10 23:47:47 +0000

    MB-3158 add ContactInfoFields component to storybook, rendering Releasing and Receiving agent in HHG setup form

--- src/components/Customer/HHGDetailsForm.jsx
+++ src/components/Customer/HHGDetailsForm.jsx
@@ -5,6 +5,7 @@ import { Formik } from 'formik';
 import { Form } from '../form/Form';
 import { DatePickerInput, TextInput } from '../form/fields';
 import { AddressFields } from '../form/AddressFields';
+import { ContactInfoFields } from '../form/ContactInfoFields/ContactInfoFields';
 
 // eslint-disable-next-line
 export const HHGDetailsForm = ({ initialValues }) => {
@@ -13,8 +14,10 @@ export const HHGDetailsForm = ({ initialValues }) => {
       <Form>
         <DatePickerInput name="requestedPickupDate" label="Requested pickup date" />
         <AddressFields legend="Pickup location" />
+        <ContactInfoFields legend="Releasing agent" />
         <DatePickerInput name="requestedDeliveryDate" label="Requested delivery date" />
         <AddressFields legend="Delivery location" />
+        <ContactInfoFields legend="Receiving agent" />
         <TextInput name="remarks" label="Remarks" />
       </Form>
     </Formik>
---
+++ src/components/form/ContactInfoFields/ContactInfoFields.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Fieldset, Label, TextInput } from '@trussworks/react-uswds';
+
+export const ContactInfoFields = ({ legend }) => {
+  return (
+    <Fieldset legend={legend}>
+      <Label hint="(optional)" htmlFor="first-name">
+        First name
+      </Label>
+      <TextInput id="first-name" name="first-name" type="text" />
+      <Label hint="(optional)" htmlFor="last-name">
+        Last name
+      </Label>
+      <TextInput id="last-name" name="last-name" type="text" />
+      <Label hint="(optional)" htmlFor="phone">
+        Phone
+      </Label>
+      <TextInput id="phone" name="phone" type="text" />
+      <Label hint="(optional)" htmlFor="state">
+        Email
+      </Label>
+      <TextInput id="email" name="email" type="text" />
+    </Fieldset>
+  );
+};
+
+ContactInfoFields.propTypes = {
+  legend: PropTypes.string,
+};
+
+ContactInfoFields.defaultProps = {
+  legend: '',
+};
+
+export default ContactInfoFields;
