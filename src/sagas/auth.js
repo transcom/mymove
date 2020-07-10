b1737bad60d8b0a6bf3f62f4a1e90e8294f44899
Suzanne Rozier <suz@truss.works>
2020-07-10 19:17:26 +0000

    Add sagas, getLoggedInUser saga

---
+++ src/sagas/auth.js
@@ -0,0 +1,41 @@
+import { takeLatest, put, call } from 'redux-saga/effects';
+import { normalize } from 'normalizr';
+
+import { generateAsyncActions } from 'shared/ReduxHelpers';
+import { GetIsLoggedIn, GetLoggedInUser } from 'shared/User/api';
+import { ordersArray } from 'shared/Entities/schema';
+import { addEntities } from 'shared/Entities/actions';
+
+const getLoggedInUserType = 'GET_LOGGED_IN_USER';
+
+const getLoggedInActions = generateAsyncActions(getLoggedInUserType);
+
+export function* fetchUser() {
+  try {
+    const isLoggedIn = yield call(GetIsLoggedIn);
+    if (isLoggedIn) {
+      try {
+        const user = yield call(GetLoggedInUser);
+        if (user.service_member) {
+          const data = normalize(user.service_member.orders, ordersArray);
+          const filtered = { ...data.entities.addresses };
+          yield put(addEntities(filtered));
+        }
+
+        yield put(getLoggedInActions.success(user));
+      } catch (e) {
+        put(getLoggedInActions.error(e));
+      }
+    } else {
+      put(getLoggedInActions.error('User is not logged in'));
+    }
+  } catch (e) {
+    put(getLoggedInActions.error(e));
+  }
+
+  yield;
+}
+
+export default function* watchFetchUser() {
+  yield takeLatest('GET_LOGGED_IN_USER_START', fetchUser);
+}
