6140989eb58037f265924bb8fcb4c036f01714da
Duncan Spencer <duncan@truss.works>
2020-07-10 14:10:32 +0000

    Add close button, flexbox, spacing and shadow

--- src/components/ReviewServiceItems/ReviewServiceItems.jsx
+++ src/components/ReviewServiceItems/ReviewServiceItems.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from '@trussworks/react-uswds';
 
 import styles from './ReviewServiceItems.module.scss';
 
 import { ServiceItemCardsShape } from 'types/serviceItemCard';
+import { ReactComponent as XLightIcon } from 'shared/icon/x-light.svg';
 
-const ReviewServiceItems = ({ header, serviceItemCards }) => {
+const ReviewServiceItems = ({ header, serviceItemCards, handleClose }) => {
   // const [curServiceItemCard] = useState(serviceItemCards[0]);
   const [curCardIndex] = useState(0);
   const totalCards = serviceItemCards.length;
@@ -16,6 +18,9 @@ const ReviewServiceItems = ({ header, serviceItemCards }) => {
   return (
     <div data-testid="ReviewServiceItems" className={styles.ReviewServiceItems}>
       <div className={styles.top}>
+        <Button data-testid="closeSidebar" type="button" onClick={handleClose} unstyled className={styles.sidebarClose}>
+          <XLightIcon />
+        </Button>
         <div className={styles.eyebrowTitle}>{`${curCardIndex + 1} OF ${totalCards} ITEMS`}</div>
         <h2 className={styles.header}>{header}</h2>
       </div>
@@ -28,6 +33,7 @@ const ReviewServiceItems = ({ header, serviceItemCards }) => {
 ReviewServiceItems.propTypes = {
   header: PropTypes.string,
   serviceItemCards: ServiceItemCardsShape.isRequired,
+  handleClose: PropTypes.func.isRequired,
 };
 
 ReviewServiceItems.defaultProps = {
