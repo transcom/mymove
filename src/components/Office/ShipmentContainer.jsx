870416ecab6651d3d623160093c4fdf5948a5a61
Donald Thai <donald@truss.works>
2020-07-11 00:07:14 +0000

    refactor of shipment container

--- src/components/Office/ShipmentContainer.jsx
+++ src/components/Office/ShipmentContainer.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
-import classNames from 'classnames/bind';
+import classNames from 'classnames';
 import * as PropTypes from 'prop-types';
 
-import styles from './shipmentContainer.module.scss';
+import styles from './ShipmentContainer.module.scss';
 
 import { SHIPMENT_TYPE } from 'shared/constants';
-
-const cx = classNames.bind(styles);
+import { ShipmentTypeOneOf } from 'types/shipment';
 
 const ShipmentContainer = ({ className, children, shipmentType }) => {
-  const containerClasses = cx('container', 'shipment-container', {
-    'container--accent--hhg':
-      shipmentType === SHIPMENT_TYPE.HHG ||
-      shipmentType === SHIPMENT_TYPE.HHG_SHORTHAUL_DOMESTIC ||
-      shipmentType === SHIPMENT_TYPE.HHG_LONGHAUL_DOMESTIC,
-    'container--accent--nts': shipmentType === SHIPMENT_TYPE.NTS,
-  });
+  const containerClasses = classNames(
+    'container',
+    styles.shipmentContainer,
+    {
+      'container--accent--hhg':
+        shipmentType === SHIPMENT_TYPE.HHG ||
+        shipmentType === SHIPMENT_TYPE.HHG_SHORTHAUL_DOMESTIC ||
+        shipmentType === SHIPMENT_TYPE.HHG_LONGHAUL_DOMESTIC,
+      'container--accent--nts': shipmentType === SHIPMENT_TYPE.NTS,
+    },
+    className,
+  );
 
-  return <div className={`${containerClasses} ${className}`}>{children}</div>;
+  return <div className={`${containerClasses}`}>{children}</div>;
 };
 
 ShipmentContainer.propTypes = {
   className: PropTypes.string,
   children: PropTypes.element.isRequired,
   /** Describes the type of shipment container. */
-  shipmentType: PropTypes.oneOf([
-    SHIPMENT_TYPE.HHG,
-    SHIPMENT_TYPE.HHG_SHORTHAUL_DOMESTIC,
-    SHIPMENT_TYPE.HHG_LONGHAUL_DOMESTIC,
-    SHIPMENT_TYPE.NTS,
-  ]),
+  shipmentType: ShipmentTypeOneOf,
 };
 
 ShipmentContainer.defaultProps = {
