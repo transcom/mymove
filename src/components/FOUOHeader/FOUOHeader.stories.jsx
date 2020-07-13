af402329c00e1ac03a3f74d8b4d62572844aeffe
Shauna Keating <shauna@truss.works>
2020-07-13 20:23:33 +0000

    adds stories file to the FOUO Header component folder

---
+++ src/components/FOUOHeader/FOUOHeader.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import FOUOHeader from './index';
+
+export default {
+  title: 'Components|Headers|FOUO Header',
+};
+
+export const ForOfficialUseOnlyHeader = () => <FOUOHeader />;
