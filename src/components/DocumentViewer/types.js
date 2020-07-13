cc1ab39d0c5444747a5db28f26d501579bb0cbd6
Shauna Keating <shauna@truss.works>
2020-07-13 21:00:02 +0000

    moves doc viewer folder to get git to recognize the folder's case change

--- src/components/DocumentViewer/types.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const FileShape = PropTypes.shape({
-  filename: PropTypes.node.isRequired,
-  filePath: PropTypes.string.isRequired,
-  fileType: PropTypes.string.isRequired,
-});
-
-export const FilesShape = PropTypes.arrayOf(FileShape);
