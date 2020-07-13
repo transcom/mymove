025131eba98c6d7be991b2941c9bef8af47f39f1
Shauna Keating <shauna@truss.works>
2020-07-13 20:56:22 +0000

    adjusts headers stories

---
+++ src/components/MilMoveHeader/MilMoveHeader.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MilMoveHeader from './index';
+
+export default {
+  title: 'Components|Headers|MilMove Header',
+};
+
+export const Milmove = () => <MilMoveHeader />;
