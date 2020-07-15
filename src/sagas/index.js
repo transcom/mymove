f616c58a499c2c75c05a64f063ccfc1b498bd7db
Suzanne Rozier <suz@truss.works>
2020-07-15 14:59:08 +0000

    Only include auth saga in root

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,8 +1,7 @@
 import { all } from 'redux-saga/effects';
 
-import watchSwaggerRequests from './request';
 import watchFetchUser from './auth';
 
 export default function* rootSaga() {
-  yield all([watchSwaggerRequests(), watchFetchUser()]);
+  yield all([watchFetchUser()]);
 }
