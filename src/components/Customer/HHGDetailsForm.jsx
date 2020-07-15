1acbdfce9f857cd0854336499bd062432d578d7b
Kim Allen <kim@truss.works>
2020-07-15 00:52:30 +0000

    Add non-functional radios for choosing if hasDeliveryAddress

--- src/components/Customer/HHGDetailsForm.jsx
+++ src/components/Customer/HHGDetailsForm.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
+import { Fieldset, Radio } from '@trussworks/react-uswds';
 
 import { Form } from '../form/Form';
 import { DatePickerInput, TextInput } from '../form/fields';
@@ -8,23 +9,58 @@ import { AddressFields } from '../form/AddressFields/AddressFields';
 import { ContactInfoFields } from '../form/ContactInfoFields/ContactInfoFields';
 import { WizardPage } from '../../shared/WizardPage';
 
-export const HHGDetailsForm = ({ initialValues, pageKey, pageList }) => {
-  return (
-    <Formik initialValues={initialValues}>
-      <WizardPage pageKey={pageKey} pageList={pageList} handleSubmit={() => {}}>
-        <Form>
-          <DatePickerInput name="requestedPickupDate" label="Requested pickup date" id="requested-pickup-date" />
-          <AddressFields initialValues={initialValues.pickupLocation} legend="Pickup location" />
-          <ContactInfoFields legend="Releasing agent" />
-          <DatePickerInput name="requestedDeliveryDate" label="Requested delivery date" id="requested-delivery-date" />
-          <AddressFields initialValues={initialValues.deliveryLocation} legend="Delivery location" />
-          <ContactInfoFields legend="Receiving agent" />
-          <TextInput name="remarks" label="Remarks" id="requested-delivery-date" />
-        </Form>
-      </WizardPage>
-    </Formik>
-  );
-};
+class HHGDetailsForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasDeliveryAddress: false,
+    };
+  }
+
+  handleChangeHasDeliveryAddress = () => {
+    // not currently getting hit
+    this.setState((prevState) => {
+      return { hasDeliveryAddress: !prevState.hasDeliveryAddress };
+    });
+  };
+
+  render() {
+    const { initialValues, pageKey, pageList } = this.props;
+    const { hasDeliveryAddress } = this.state;
+    return (
+      <Formik initialValues={initialValues}>
+        <WizardPage pageKey={pageKey} pageList={pageList} handleSubmit={() => {}}>
+          <Form>
+            <DatePickerInput name="requestedPickupDate" label="Requested pickup date" id="requested-pickup-date" />
+            <AddressFields initialValues={initialValues.pickupLocation} legend="Pickup location" />
+            <ContactInfoFields initialValues={initialValues.releasingAgent} legend="Releasing agent" />
+            <DatePickerInput
+              name="requestedDeliveryDate"
+              label="Requested delivery date"
+              id="requested-delivery-date"
+            />
+            <Fieldset legend="Delivery location">
+              <Radio label="Yes" checked={hasDeliveryAddress} onChange={this.handleChangeHasDeliveryAddress} />
+              <Radio label="No" checked={!hasDeliveryAddress} onChange={this.handleChangeHasDeliveryAddress} />
+              {hasDeliveryAddress ? (
+                <AddressFields initialValues={initialValues.deliveryLocation} />
+              ) : (
+                <>
+                  <div>We can use the zip of your new duty station.</div>
+                  <div>[City], [State] [New duty station zip]</div>
+                </>
+              )}
+            </Fieldset>
+            <ContactInfoFields initialValues={initialValues.receivingAgent} legend="Receiving agent" />
+            <Fieldset legend="Remarks">
+              <TextInput name="remarks" label="Remarks" id="requested-delivery-date" />
+            </Fieldset>
+          </Form>
+        </WizardPage>
+      </Formik>
+    );
+  }
+}
 
 HHGDetailsForm.propTypes = {
   pageKey: PropTypes.string.isRequired,
@@ -45,6 +81,18 @@ HHGDetailsForm.propTypes = {
       city: PropTypes.string,
       state: PropTypes.string,
       zip: PropTypes.string,
+    }),
+    releasingAgent: PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      phone: PropTypes.string,
+      email: PropTypes.string,
+    }),
+    receivingAgent: PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      phone: PropTypes.string,
+      email: PropTypes.string,
     }),
     remarks: PropTypes.string,
   }),
--- src/pages/MyMove/HHGMoveSetup.jsx
+++ src/pages/MyMove/HHGMoveSetup.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { HHGDetailsForm } from 'components/Customer/HHGDetailsForm';
+import HHGDetailsFormComponent from 'components/Customer/HHGDetailsForm';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class HHGMoveSetup extends Component {
@@ -10,7 +10,7 @@ export class HHGMoveSetup extends Component {
     return (
       <div>
         <h3>Now lets arrange details for the professional movers</h3>
-        <HHGDetailsForm pageList={pageList} pageKey={pageKey} />
+        <HHGDetailsFormComponent pageList={pageList} pageKey={pageKey} />
       </div>
     );
   }
