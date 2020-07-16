f28ab819fe17c722cad6e6110b6cffe97290379a
Kim Allen <kim@truss.works>
2020-07-16 00:16:46 +0000

    Add test for when has delivery address is true

--- src/components/Customer/HHGDetailsForm.test.jsx
+++ src/components/Customer/HHGDetailsForm.test.jsx
@@ -8,8 +8,13 @@ describe('HHGDetailsForm component', () => {
     const wrapper = mount(<HHGDetailsForm pageList={['page1', 'anotherPage/:foo/:bar']} pageKey="page1" />);
     expect(wrapper.find('HHGDetailsForm').length).toBe(1);
     expect(wrapper.find('DatePickerInput').length).toBe(2);
-    expect(wrapper.find('AddressFields').length).toBe(2);
+    expect(wrapper.find('AddressFields').length).toBe(1);
     expect(wrapper.find('ContactInfoFields').length).toBe(2);
     expect(wrapper.find('TextInput').length).toBe(1);
+  });
+  it('renders second address field when has delivery date', () => {
+    const wrapper = mount(<HHGDetailsForm pageList={['page1', 'anotherPage/:foo/:bar']} pageKey="page1" />);
+    wrapper.setState({ hasDeliveryAddress: true });
+    expect(wrapper.find('AddressFields').length).toBe(2);
   });
 });
