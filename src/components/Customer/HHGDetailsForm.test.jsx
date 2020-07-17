89adde7ee801c9b8201565e58a1188c03ad2ca6f
Arianna Kellogg <ari@truss.works>
2020-07-17 23:29:45 +0000

    MB-3158 add more specific check for Remarks textinput client test

--- src/components/Customer/HHGDetailsForm.test.jsx
+++ src/components/Customer/HHGDetailsForm.test.jsx
@@ -10,7 +10,7 @@ describe('HHGDetailsForm component', () => {
     expect(wrapper.find('DatePickerInput').length).toBe(2);
     expect(wrapper.find('AddressFields').length).toBe(1);
     expect(wrapper.find('ContactInfoFields').length).toBe(2);
-    expect(wrapper.find('TextInput').length).toBe(1);
+    expect(wrapper.find('input[name="remarks"]').length).toBe(1);
   });
   it('renders second address field when has delivery date', () => {
     const wrapper = mount(<HHGDetailsForm pageList={['page1', 'anotherPage/:foo/:bar']} pageKey="page1" />);
