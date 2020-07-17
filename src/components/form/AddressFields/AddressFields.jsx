e24f6896714e5bc0ea1cac0cd8f0c9e9688a27eb
Arianna Kellogg <ari@truss.works>
2020-07-17 23:22:17 +0000

    MB-3158 update from data-cy to data-testid, clean up naming in HHGMoveSetup

--- src/components/form/AddressFields/AddressFields.jsx
+++ src/components/form/AddressFields/AddressFields.jsx
@@ -6,17 +6,17 @@ export const AddressFields = ({ legend, className }) => {
   return (
     <Fieldset legend={legend} className={className}>
       <Label htmlFor="mailing-address-1">Street address 1</Label>
-      <TextInput id="mailing-address-1" data-cy="mailingAddress1" name="mailing-address-1" type="text" />
+      <TextInput id="mailing-address-1" data-testid="mailingAddress1" name="mailing-address-1" type="text" />
       <Label hint=" (optional)" htmlFor="mailing-address-2">
         Street address 2
       </Label>
-      <TextInput id="mailing-address-2" data-cy="mailingAddress2" name="mailing-address-2" type="text" />
+      <TextInput id="mailing-address-2" data-testid="mailingAddress2" name="mailing-address-2" type="text" />
       <Label htmlFor="city">City</Label>
-      <TextInput id="city" data-cy="city" name="city" type="text" />
+      <TextInput id="city" data-testid="city" name="city" type="text" />
       <Label htmlFor="state">State</Label>
-      <TextInput id="state" data-cy="state" name="state" type="text" />
+      <TextInput id="state" data-testid="state" name="state" type="text" />
       <Label htmlFor="zip">ZIP</Label>
-      <TextInput id="zip" data-cy="zip" inputSize="medium" name="zip" pattern="[\d]{5}(-[\d]{4})?" type="text" />
+      <TextInput id="zip" data-testid="zip" inputSize="medium" name="zip" pattern="[\d]{5}(-[\d]{4})?" type="text" />
     </Fieldset>
   );
 };
--- src/components/form/AddressFields/AddressFields.test.jsx
+++ src/components/form/AddressFields/AddressFields.test.jsx
@@ -7,10 +7,10 @@ const wrapper = mount(<AddressFields />);
 
 describe('AddressFields component', () => {
   it('renders', () => {
-    expect(wrapper.find('input[data-cy="mailingAddress1"]').length).toBe(1);
-    expect(wrapper.find('input[data-cy="mailingAddress2"]').length).toBe(1);
-    expect(wrapper.find('input[data-cy="city"]').length).toBe(1);
-    expect(wrapper.find('input[data-cy="state"]').length).toBe(1);
-    expect(wrapper.find('input[data-cy="zip"]').length).toBe(1);
+    expect(wrapper.find('input[data-testid="mailingAddress1"]').length).toBe(1);
+    expect(wrapper.find('input[data-testid="mailingAddress2"]').length).toBe(1);
+    expect(wrapper.find('input[data-testid="city"]').length).toBe(1);
+    expect(wrapper.find('input[data-testid="state"]').length).toBe(1);
+    expect(wrapper.find('input[data-testid="zip"]').length).toBe(1);
   });
 });
