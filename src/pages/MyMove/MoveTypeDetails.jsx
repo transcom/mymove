89daa17171e5233897fa9e087c5e2e97f5b31462
Arianna Kellogg <ari@truss.works>
2020-07-23 23:38:13 +0000

    MB-3105 add static text for move type tips

--- src/pages/MyMove/MoveTypeDetails.jsx
+++ src/pages/MyMove/MoveTypeDetails.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 export const MoveTypeDetails = () => (
-  <div>
-    <h2>Figure out your move details</h2>
-    <p>Handy tips to help you decide how to move</p>
+  <div className="usa-grid">
+    <div className="grid-row">
+      <div className="grid-col">
+        <h1 className="sm-heading">Figure out your move details</h1>
+        <p>Handy tips to help you decide how to move</p>
+        <p>Your weight allowance is ___ lbs.</p>
+        <h2 className="sm-heading">One move, several parts</h2>
+        <p>
+          It’s common to move some things yourself and have professional movers do the rest. You can also move things to
+          or from more than one location.
+        </p>
+        <h2 className="sm-heading">Keep important things with you</h2>
+        <p>
+          It’s a smart idea to move essential items, heirlooms, and irreplaceable things yourself. Select a PPM
+          (personally procured move) to do that.
+        </p>
+        <h2 className="sm-heading">Spread out your pickup dates</h2>
+        <p>
+          The easiest way to get the right boxes to the right location is to request different pickup and delivery days
+          for different loads.
+        </p>
+      </div>
+    </div>
   </div>
 );
 
