7a2057aed60909afaea75251b204cc4148ea68f9
Suzanne Rozier <suz@truss.works>
2020-07-28 22:56:48 +0000

    Flesh out updater tests

--- pkg/services/payment_request/payment_request_status_updater_test.go
+++ pkg/services/payment_request/payment_request_status_updater_test.go
@@ -25,13 +25,21 @@ func (suite *PaymentRequestServiceSuite) TestUpdatePaymentRequestStatus() {
 		suite.NoError(err)
 	})
 
-	suite.T().Run("Should return an InvalidInputError if the payment request has service items that have not been reviewed", func(t *testing.T) {
+	suite.T().Run("Should return an InvalidInputError if the payment request has any service items that have not been reviewed", func(t *testing.T) {
 		paymentRequest := testdatagen.MakeDefaultPaymentRequest(suite.DB())
 
 		psiCost := unit.Cents(10000)
 		testdatagen.MakePaymentServiceItem(suite.DB(), testdatagen.Assertions{
 			PaymentServiceItem: models.PaymentServiceItem{
 				PriceCents: &psiCost,
+				Status:     models.PaymentServiceItemStatusRequested,
+			},
+			PaymentRequest: paymentRequest,
+		})
+		testdatagen.MakePaymentServiceItem(suite.DB(), testdatagen.Assertions{
+			PaymentServiceItem: models.PaymentServiceItem{
+				PriceCents: &psiCost,
+				Status:     models.PaymentServiceItemStatusApproved,
 			},
 			PaymentRequest: paymentRequest,
 		})
@@ -42,6 +50,32 @@ func (suite *PaymentRequestServiceSuite) TestUpdatePaymentRequestStatus() {
 		_, err := updater.UpdatePaymentRequestStatus(&paymentRequest, etag.GenerateEtag(paymentRequest.UpdatedAt))
 		suite.Error(err)
 		suite.IsType(services.InvalidInputError{}, err)
+	})
+
+	suite.T().Run("Should update and return no error if the payment request has service items that have all been reviewed", func(t *testing.T) {
+		paymentRequest := testdatagen.MakeDefaultPaymentRequest(suite.DB())
+
+		psiCost := unit.Cents(10000)
+		testdatagen.MakePaymentServiceItem(suite.DB(), testdatagen.Assertions{
+			PaymentServiceItem: models.PaymentServiceItem{
+				PriceCents: &psiCost,
+				Status:     models.PaymentServiceItemStatusApproved,
+			},
+			PaymentRequest: paymentRequest,
+		})
+		testdatagen.MakePaymentServiceItem(suite.DB(), testdatagen.Assertions{
+			PaymentServiceItem: models.PaymentServiceItem{
+				PriceCents: &psiCost,
+				Status:     models.PaymentServiceItemStatusDenied,
+			},
+			PaymentRequest: paymentRequest,
+		})
+
+		paymentRequest.Status = models.PaymentRequestStatusReviewed
+		updater := NewPaymentRequestStatusUpdater(builder)
+
+		_, err := updater.UpdatePaymentRequestStatus(&paymentRequest, etag.GenerateEtag(paymentRequest.UpdatedAt))
+		suite.NoError(err)
 	})
 
 	suite.T().Run("Should return a PreconditionFailedError with a stale etag", func(t *testing.T) {
