0785e98ac68c89c5783841ddfc1c1f3bd423f13e
Donald Thai <donald@truss.works>
2020-07-29 19:24:37 +0000

    update ReviewDetailsCard.stories.jsx based on refactor

--- src/components/Office/ReviewServiceItems/ReviewDetailsCard.stories.jsx
+++ src/components/Office/ReviewServiceItems/ReviewDetailsCard.stories.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 import ReviewDetailsCard from './ReviewDetailsCard';
+import NeedsReview from './NeedsReview';
+import RejectRequest from './RejectRequest';
 
 export default {
-  title: 'TOO/TIO Components|ReviewServiceItems',
+  title: 'TOO/TIO Components|ReviewServiceItems/ReviewDetails',
   component: ReviewDetailsCard,
 };
 
-export const ReviewDetails = () => <ReviewDetailsCard />;
+export const ReviewDetailsWithNoValues = () => <ReviewDetailsCard />;
+
+export const ReviewDetailsWithNeedsReview = () => (
+  <ReviewDetailsCard acceptedAmount={1234} rejectedAmount={1234} requestedAmount={1234}>
+    <NeedsReview numberOfItems={1} />
+  </ReviewDetailsCard>
+);
+
+export const ReviewDetailsWithRejectRequest = () => (
+  <ReviewDetailsCard acceptedAmount={1234} rejectedAmount={1234} requestedAmount={1234}>
+    <RejectRequest />
+  </ReviewDetailsCard>
+);
