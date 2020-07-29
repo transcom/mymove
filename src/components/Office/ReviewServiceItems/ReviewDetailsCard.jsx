9332b7f6ac7a869a84cf74ef5731fc4658b04426
Donald Thai <donald@truss.works>
2020-07-29 19:24:15 +0000

    Refactor ReviewDetailsCard.jsx to take in props

--- src/components/Office/ReviewServiceItems/ReviewDetailsCard.jsx
+++ src/components/Office/ReviewServiceItems/ReviewDetailsCard.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
-import { Button } from '@trussworks/react-uswds';
+import PropTypes from 'prop-types';
 
 import styles from './ReviewDetailsCard.module.scss';
 
 import { toDollarString } from 'shared/formatters';
 
 /** This component represents a Payment Request Review Details Card shown at the end of navigation */
-const ReviewDetailsCard = () => {
+const ReviewDetailsCard = ({ children, requestedAmount, acceptedAmount, rejectedAmount }) => {
   return (
     <div data-testid="ReviewDetailsCard" className={styles.ReviewDetailsCard}>
       <h4 className={styles.cardHeader}>Review details</h4>
       <dl>
         <dt>Requested</dt>
-        <dd data-testid="requested">{toDollarString(1234.12)}</dd>
+        <dd data-testid="requested">{toDollarString(requestedAmount)}</dd>
 
         <dt>Accepted</dt>
-        <dd data-testid="accepted">{toDollarString(1234.12)}</dd>
+        <dd data-testid="accepted">{toDollarString(acceptedAmount)}</dd>
 
         <dt>Rejected</dt>
-        <dd data-testid="rejected">{toDollarString(1234.12)}</dd>
+        <dd data-testid="rejected">{toDollarString(rejectedAmount)}</dd>
       </dl>
 
-      <div data-testid="NeedsReview" className={styles.needsReview}>
-        <div className={styles.header}>One item still needs your review</div>
-        <div>Accept or reject all service items, then authorized payment.</div>
-        <Button type="button" secondary className={styles.button}>
-          Finish review
-        </Button>
-      </div>
+      {children}
     </div>
   );
 };
 
-ReviewDetailsCard.propTypes = {};
+ReviewDetailsCard.propTypes = {
+  children: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+  requestedAmount: PropTypes.number,
+  acceptedAmount: PropTypes.number,
+  rejectedAmount: PropTypes.number,
+};
 
-ReviewDetailsCard.defaultProps = {};
+ReviewDetailsCard.defaultProps = {
+  children: null,
+  requestedAmount: 0,
+  acceptedAmount: 0,
+  rejectedAmount: 0,
+};
 
 export default ReviewDetailsCard;
