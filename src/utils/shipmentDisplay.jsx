e46176ca160a4255e9e982556b53e48489b6adc1
Duncan <52669884+duncan-truss@users.noreply.github.com>
2020-07-30 16:25:13 -0400

    [MB-3460] Make non-postal code destination address parts optional in TOO components (#4511)
    * format shipment destination address that may only include postal code
    
    * format destination address in approval preview component and test
    
    * Update shipment approval preview story to reflect postal code only
    
    * fix data-testid and replace br with nbsp

---
+++ src/utils/shipmentDisplay.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+export default function formatAddress(address) {
+  const { street_address_1, city, state, postal_code } = address;
+  return (
+    <>
+      {street_address_1 && <>{street_address_1},&nbsp;</>}
+      {city ? `${city}, ${state} ${postal_code}` : postal_code}
+    </>
+  );
+}
