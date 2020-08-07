8b7ab4c4d697e7b58cb5b27495eb4b5b2ce99f13
Arianna Kellogg <ari@truss.works>
2020-08-07 00:13:49 +0000

    MB-3105 reverting change to getWorkflowRoutes that was unrelated to my story and was only made to try and make other unrelated failing tests pass

--- src/scenes/MyMove/getWorkflowRoutes.jsx
+++ src/scenes/MyMove/getWorkflowRoutes.jsx
@@ -78,7 +78,7 @@ const isCurrentMoveSubmitted = ({ move }) => {
 
 const pages = {
   '/service-member/:serviceMemberId/conus-status': {
-    isInFlow: always,
+    isInFlow: inGhcFlow,
     isComplete: ({ sm }) => sm.is_profile_complete || every([sm.rank, sm.edipi, sm.affiliation]),
     render: (key, pages, description, props) => ({ match }) => {
       return (
