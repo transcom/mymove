90cc1ed3fcc2769346be39f0156d2e2834250c77
Moncef Belyamani <moncef@truss.works>
2020-08-13 13:57:18 +0000

    Revert "Remove convert.go"
    This reverts commit 9fb3d680ef941c77b5a8e5695bba5b7bb2db80e4.

--- pkg/cli/featureflag.go
+++ pkg/cli/featureflag.go
@@ -10,12 +10,18 @@ const (
 	FeatureFlagAccessCode string = "feature-flag-access-code"
 	// FeatureFlagRoleBasedAuth is the role-based-auth feature flag
 	FeatureFlagRoleBasedAuth string = "feature-flag-role-based-auth"
+	// FeatureFlagConvertPPMsToGHC is the convert-ppms-to-ghc feature flag
+	FeatureFlagConvertPPMsToGHC string = "feature-flag-convert-ppms-to-ghc"
+	// FeatureFlagConvertProfileOrdersToGHC is the convert-ppms-to-ghc feature flag
+	FeatureFlagConvertProfileOrdersToGHC string = "feature-flag-convert-profile-orders-to-ghc"
 )
 
 // InitFeatureFlags initializes FeatureFlags command line flags
 func InitFeatureFlags(flag *pflag.FlagSet) {
 	flag.Bool(FeatureFlagAccessCode, false, "Flag (bool) to enable requires-access-code")
 	flag.Bool(FeatureFlagRoleBasedAuth, false, "Flag (bool) to enable role-based-auth")
+	flag.Bool(FeatureFlagConvertPPMsToGHC, false, "Flag (bool) to enable convert-ppms-to-ghc")
+	flag.Bool(FeatureFlagConvertProfileOrdersToGHC, false, "Flag (bool) to enable convert-profile-orders-to-ghc")
 }
 
 // CheckFeatureFlag validates Verbose command line flags
