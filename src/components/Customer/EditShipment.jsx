58766e5007b50d9f6bd656cc4b240cd6e9f35197
Ron Cruz <Ronolibert@gmail.com>
2020-08-14 02:58:04 +0000

    Mobile view of edit shipment first pass

--- src/components/Customer/EditShipment.jsx
+++ src/components/Customer/EditShipment.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { arrayOf, string, bool, shape, func } from 'prop-types';
-import { get, isEmpty } from 'lodash';
-import { connect } from 'react-redux';
+import { string, shape, func } from 'prop-types';
+import { isEmpty } from 'lodash';
 import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
-import { Fieldset, Radio, Label } from '@trussworks/react-uswds';
+import { Fieldset, Radio, Label, Textarea, Button } from '@trussworks/react-uswds';
 
 import { Form } from '../form/Form';
-import { DatePickerInput, TextInput } from '../form/fields';
+import { DatePickerInput } from '../form/fields';
 import { AddressFields } from '../form/AddressFields/AddressFields';
 import { ContactInfoFields } from '../form/ContactInfoFields/ContactInfoFields';
 
@@ -15,6 +14,8 @@ import { MTOAgentType } from 'shared/constants';
 import { formatSwaggerDate } from 'shared/formatters';
 import Checkbox from 'shared/Checkbox';
 import { validateDate } from 'utils/formikValidators';
+
+import './EditShipment.scss';
 
 const PickupAddressSchema = Yup.object().shape({
   mailingAddress1: Yup.string().required('Required'),
@@ -51,11 +52,38 @@ const HHGDetailsFormSchema = Yup.object().shape({
   receivingAgent: AgentSchema,
   remarks: Yup.string(),
 });
+
+const Divider = ({ className }) => <hr className={className} />;
+
+Divider.propTypes = {
+  className: string,
+};
+
+Divider.defaultProps = {
+  className: '',
+};
+
+const Hint = ({ className, children, ...props }) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <div {...props} className={`usa-hint ${className}`}>
+    {children}
+  </div>
+);
+
+Hint.propTypes = {
+  className: string,
+  children: React.Children.isRequired,
+};
+
+Hint.defaultProps = {
+  className: '',
+};
+
 class EditShipment extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hasDeliveryAddress: false,
+      hasDeliveryAddress: true,
       initialValues: {},
     };
   }
@@ -182,9 +210,14 @@ class EditShipment extends Component {
   render() {
     const { newDutyStationAddress } = this.props;
     const { hasDeliveryAddress, initialValues, useCurrentResidence } = this.state;
-    const fieldsetClasses = 'margin-top-2';
     return (
-      <div className="grid-container usa-prose">
+      <div className="grid-container">
+        <div className="margin-top" style={{ backgroundColor: '#aacdec', width: 51, textAlign: 'center' }}>
+          HHG
+        </div>
+        <h2 className="margin-top-1" style={{ fontSize: 28 }}>
+          When and where will you move this shipment?
+        </h2>
         <Formik
           initialValues={initialValues}
           enableReinitialize
@@ -192,30 +225,33 @@ class EditShipment extends Component {
           validateOnChange
           validationSchema={HHGDetailsFormSchema}
         >
-          {({ values, dirty, isValid }) => (
+          {({ values }) => (
             <Form>
-              <Fieldset legend="Pickup date" className={fieldsetClasses}>
+              <Fieldset legend="Pickup date" className="fieldset-legend margin-top-4">
                 <Field
                   as={DatePickerInput}
                   name="requestedPickupDate"
                   label="Requested pickup date"
+                  labelClassName="margin-top-2"
                   id="requestedPickupDate"
                   value={values.requestedPickupDate}
                   validate={validateDate}
                 />
               </Fieldset>
-              <span className="usa-hint" id="pickupDateHint">
-                Your movers will confirm this date or one shortly before or after.
-              </span>
-
+              <Hint className="margin-top-1 margin-bottom-4" id="pickupDateHint">
+                Movers will contact you to schedule the actual pickup date. That date should fall within 7 days of your
+                requested date. Tip: Avoid scheduling multiple shipments on the same day.{' '}
+              </Hint>
+              <Divider className="margin-bottom-4" />
               <AddressFields
+                className="margin-bottom-3 fieldset-legend"
                 name="pickupLocation"
                 legend="Pickup location"
-                className={fieldsetClasses}
                 renderExistingAddressCheckbox={() => (
                   <Checkbox
                     data-testid="useCurrentResidence"
                     label="Use my current residence address"
+                    className="margin-top-3"
                     name="useCurrentResidence"
                     checked={useCurrentResidence}
                     onChange={() => this.handleUseCurrentResidenceChange(values)}
@@ -223,14 +259,19 @@ class EditShipment extends Component {
                 )}
                 values={values.pickupLocation}
               />
+              <div className="usa-hint">
+                If you have more things at another pickup location, you can schedule for them later.
+              </div>
+              <hr className="margin-top-4 margin-bottom-4" />
               <ContactInfoFields
+                className="fieldset-legend margin-bottom-5"
                 name="releasingAgent"
                 legend="Releasing agent"
-                className={fieldsetClasses}
                 subtitle="Who can allow the movers to take your stuff if you're not there?"
                 values={values.releasingAgent}
               />
-              <Fieldset legend="Delivery date" className={fieldsetClasses}>
+              <Divider className="margin-bottom-6" />
+              <Fieldset legend="Delivery date" className="fieldset-legend">
                 <DatePickerInput
                   name="requestedDeliveryDate"
                   label="Requested delivery date"
@@ -238,12 +279,14 @@ class EditShipment extends Component {
                   value={values.requestedDeliveryDate}
                   validate={validateDate}
                 />
-                <span className="usa-hint" id="deliveryDateHint">
-                  Your movers will confirm this date or one shortly before or after.
-                </span>
+                <Hint className="margin-top-1">
+                  Shipments can take several weeks to arrive, depending on how far they&apos;re going. Your movers will
+                  contact you close to the date you select to coordinate delivery
+                </Hint>
               </Fieldset>
-              <Fieldset legend="Delivery location" className={fieldsetClasses}>
-                <Label>Do you know your delivery address?</Label>
+              <Divider className="margin-top-4 margin-bottom-4" />
+              <Fieldset legend="Delivery location" className="fieldset-legend">
+                <Label className="margin-top-3 margin-bottom-1">Do you know your delivery address?</Label>
                 <div className="display-flex margin-top-1">
                   <Radio
                     id="has-delivery-address"
@@ -261,34 +304,72 @@ class EditShipment extends Component {
                   />
                 </div>
                 {hasDeliveryAddress ? (
-                  <AddressFields name="deliveryLocation" className={fieldsetClasses} values={values.deliveryLocation} />
+                  <AddressFields name="deliveryLocation" values={values.deliveryLocation} />
                 ) : (
                   <>
-                    <div className={fieldsetClasses}>We can use the zip of your new duty station.</div>
+                    <div>We can use the zip of your new duty station.</div>
                     <div>
                       {newDutyStationAddress.city}, {newDutyStationAddress.state} {newDutyStationAddress.postal_code}
                     </div>
                   </>
                 )}
               </Fieldset>
+              <Divider className="margin-top-4 margin-bottom-4" />
               <ContactInfoFields
+                className="fieldset-legend"
                 name="receivingAgent"
                 legend="Receiving agent"
-                className={fieldsetClasses}
                 subtitle="Who can take delivery for you if the movers arrive and you're not there?"
                 values={values.receivingAgent}
               />
-              <Fieldset legend="Remarks" className={fieldsetClasses}>
-                <TextInput
+              <Divider className="margin-top-4 margin-bottom-4" />
+              <Fieldset legend="Remarks" className="fieldset-legend">
+                <div className="margin-top-3 margin-bottom-1" style={{ fontSize: 13 }}>
+                  <strong>Is there anything special about this shipment that the movers should know?</strong>
+                </div>
+                <div
+                  className="shadow-0"
+                  style={{
+                    padding: 15,
+                    borderRadius: 4,
+                    border: '1px solid black',
+                    backgroundColor: '#f9f9f9',
+                    fontSize: 13,
+                  }}
+                >
+                  <strong>Examples</strong>
+                  <ul style={{ padding: 0, listStylePosition: 'inside', margin: 0 }}>
+                    <li>Things that might need special handling</li>
+                    <li>Access info for a location</li>
+                    <li>Weapons or alcohol</li>
+                  </ul>
+                </div>
+
+                <Textarea
                   label="Anything else you would like us to know?"
                   labelHint="(optional)"
                   data-testid="remarks"
                   name="remarks"
                   id="remarks"
-                  maxLength={1500}
+                  placeholder="This is 500 characters of customer remarks placeholder"
+                  maxLength={500}
+                  style={{ resize: 'vertical' }}
+                  type="textarea"
                   value={values.remarks}
                 />
               </Fieldset>
+              <Divider className="margin-top-6 margin-bottom-3" />
+              <Hint>
+                You can change details for your HHG shipment when you talk to your move counselor or the person
+                who&apos;s your point of contact with the movers. You can also edit in MilMove up to 24 hours before
+                your final pickup date.
+              </Hint>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Button>Save</Button>
+                <Button className="usa-button--unstyled">
+                  <span>Cancel</span>
+                </Button>
+              </div>
             </Form>
           )}
         </Formik>
