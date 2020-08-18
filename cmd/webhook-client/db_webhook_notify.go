61ef814d31f30bcc5713a2e1a406e5c26ef7f15f
Shimona Carvalho <shimona@truss.works>
2020-08-18 19:56:33 +0000

    Refactored db-webhook-notify code for autochecking - first pass

--- cmd/webhook-client/db_webhook_notify.go
+++ cmd/webhook-client/db_webhook_notify.go
@@ -4,7 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"time"
 
+	"github.com/gobuffalo/pop"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -13,30 +19,20 @@ import (
 	"github.com/transcom/mymove/pkg/models"
 )
 
-func initDbWebhookNotifyFlags(flag *pflag.FlagSet) {
+const (
+	// PeriodFlag indicates how often to check the db in seconds
+	PeriodFlag string = "period"
+)
 
+func initDbWebhookNotifyFlags(flag *pflag.FlagSet) {
+	flag.Int(PeriodFlag, 5, "Period in secs to check for notifications")
 	flag.SortFlags = false
 }
 
-func dbWebhookNotify(cmd *cobra.Command, args []string) error {
-	v := viper.New()
-	// Temp url to use when making requests
-	basePath := "/support/v1/webhook-notify"
-
-	err := ParseFlags(cmd, v, args)
-	if err != nil {
-		return err
-	}
-
-	db, logger, err := InitRootConfig(v)
-	if err != nil {
-		logger.Error("invalid configuration", zap.Error(err))
-		return err
-	}
-
+func checkDatabase(db *pop.Connection, logger Logger, runtime *WebhookRuntime) error {
 	// Read notifications
 	notifications := []models.WebhookNotification{}
-	err = db.All(&notifications)
+	err := db.All(&notifications)
 	if err != nil {
 		logger.Error("Error:", zap.Error(err))
 		return err
@@ -61,20 +57,10 @@ func dbWebhookNotify(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	// Create the client and open the cacStore
-	runtime, cacStore, errCreateClient := CreateClient(v)
-
-	if errCreateClient != nil {
-		logger.Error("Error creating client:", zap.Error(errCreateClient))
-		return errCreateClient
-	}
-
-	// Defer closing the store until after the api call has completed
-	if cacStore != nil {
-		defer cacStore.Close()
-	}
-
 	// Make the API call
+	// Temp url to use when making requests
+	basePath := "/support/v1/webhook-notify"
+
 	runtime.BasePath = basePath
 	resp, body, err := runtime.Post(json)
 	// Check for error making the request
@@ -91,6 +77,81 @@ func dbWebhookNotify(cmd *cobra.Command, args []string) error {
 	}
 
 	logger.Info("Request complete: ", zap.String("Status", resp.Status))
-	fmt.Println(body)
+	fmt.Println(fmt.Sprintf("%s", body))
+
+	return nil
+}
+
+func dbListen(db *pop.Connection, logger Logger, runtime *WebhookRuntime, periodInSeconds int) error {
+
+	t := time.Tick(time.Duration(periodInSeconds) * time.Second)
+	r := rand.New(rand.NewSource(99)) /// todo delete
+
+	checkDatabase(db, logger, runtime)
+	for range t {
+		//Download the current contents of the URL and do something with it
+		checkDatabase(db, logger, runtime)
+		jitterTime := r.Int31n(2000)
+		fmt.Printf("Checked at at %s, add jitter %d s\n", time.Now(), jitterTime)
+
+		// add a bit of jitter
+		jitter := time.Duration(jitterTime) * time.Millisecond
+		time.Sleep(jitter)
+	}
+	return nil
+}
+
+func dbWebhookNotify(cmd *cobra.Command, args []string) error {
+	v := viper.New()
+
+	err := ParseFlags(cmd, v, args)
+	if err != nil {
+		return err
+	}
+
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		changed := ""
+		if f.Changed {
+			changed = "user-defined"
+		}
+		fmt.Println(f.Name, ": \t", f.Value, " \t", changed)
+	})
+
+	db, logger, err := InitRootConfig(v)
+	if err != nil {
+		logger.Error("invalid configuration", zap.Error(err))
+		return err
+	}
+
+	// Create the client and open the cacStore
+	runtime, cacStore, errCreateClient := CreateClient(v)
+
+	if errCreateClient != nil {
+		logger.Error("Error creating client:", zap.Error(errCreateClient))
+		return errCreateClient
+	}
+
+	// Defer closing the store until after the api call has completed
+	if cacStore != nil {
+		defer cacStore.Close()
+	}
+
+	go dbListen(db, logger, runtime, v.GetInt(PeriodFlag))
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	// Creates an empty context object
+	if err = db.Close(); err == nil {
+		logger.Info("Db connection closed")
+	} else {
+		logger.Error("Db connection close failed", zap.Error(err))
+	}
+
+	log.Println("Listener exiting")
 	return nil
 }
