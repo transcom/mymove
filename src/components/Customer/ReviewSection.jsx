c08f2bd8f4a303e6b67677341e73e8897e7ba96f
Arianna Kellogg <ari@truss.works>
2020-08-19 23:00:37 +0000

    MB-3447 cleanup in aisle ReviewSection

--- src/components/Customer/ReviewSection.jsx
+++ src/components/Customer/ReviewSection.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { get } from 'lodash';
 import PropTypes from 'prop-types';
-
-// import Address from './Address';
-
-// import { formatDateSM } from 'shared/formatters';
-// import { getFullSMName } from 'utils/moveSetupFlow';
 
 import 'scenes/Review/Review.css';
 
@@ -44,34 +38,6 @@ const ReviewSection = ({ fieldData, title, editLink }) => {
     </div>
   );
 };
-
-/*
-  <table className="table--stacked review-section">
-    <colgroup>
-      <col style={{ width: '25%' }} />
-      <col style={{ width: '75%' }} />
-    </colgroup>
-    <tbody>
-      <tr>
-        <th scope="row">Contact info</th>
-      </tr>
-      <tr>
-        <th scope="row">Best contact phone</th>
-        <td>{get(serviceMember, 'telephone')}</td>
-      </tr>
-      <tr>
-        <th scope="row">Personal email</th>
-        <td>{get(serviceMember, 'personal_email')}</td>
-      </tr>
-      <tr>
-        <th scope="row">Current mailing address</th>
-        <td>
-          <Address address={get(serviceMember, 'residential_address')} />
-        </td>
-      </tr>
-    </tbody>
-  </table>
-*/
 
 ReviewSection.defaultProps = {
   title: '',
