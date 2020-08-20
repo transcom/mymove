e674f09fff34897e9a20704e154d01febab901ba
Duncan Spencer <duncan@truss.works>
2020-08-20 16:55:39 +0000

    remove eslint disable styles import

--- src/components/Office/RequestedServiceItemsTable/RequestedServiceItemsTable.jsx
+++ src/components/Office/RequestedServiceItemsTable/RequestedServiceItemsTable.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line no-unused-vars
 import styles from './RequestedServiceItemsTable.module.scss';
 
 import ServiceItemTableHasImg from 'components/ServiceItemTableHasImg';
