8e5ef81136b2e186a82cb6a1fe916d12cc404940
Jeri Sommers 🐍 <sojeri@truss.works>
2020-08-21 06:11:57 +0000

    try disabling shared memory to see if it helps the hanging

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -24,6 +24,16 @@ Cypress.Screenshot.defaults({
   capture: 'fullPage',
 });
 
+cy.on('before:browser:launch', (browser = {}, args) => {
+  // Disable shared memory when running headless since running out of memory can cause cypress to hang indefinitely
+  // https://github.com/cypress-io/cypress/issues/8206
+  if (config.env.headless && browser.name === 'chrome') {
+    args.push('--disable-dev-shm-usage');
+  } else if (config.env.headless && browser.name === 'electron') {
+    args['disable-dev-shm-usage'] = true;
+  }
+});
+
 afterEach(function () {
   if (this.currentTest.state === 'failed') {
     // Take another screenshot so we get the full page
