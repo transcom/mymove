69ab973606efd47a05b99df47641672d740c1181
Arianna Kellogg <ari@truss.works>
2020-08-24 23:51:51 +0000

    MB-3447 replaced GridContainer with ReviewSection, rendering most rows

--- src/pages/MyMove/HHGShipmentSummary.jsx
+++ src/pages/MyMove/HHGShipmentSummary.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes, { string } from 'prop-types';
-import { get, isEmpty } from 'lodash';
-import { GridContainer, Grid } from '@trussworks/react-uswds';
+// import { get, isEmpty } from 'lodash';
+import { get } from 'lodash';
+// import { GridContainer, Grid } from '@trussworks/react-uswds';
+
+import ReviewSection from '../../components/Customer/ReviewSection';
 
 import Address from 'scenes/Review/Address';
 import { formatDateSM } from 'shared/formatters';
@@ -17,36 +20,42 @@ export default function HHGShipmentSummary(props) {
   const pickupLocation = get(mtoShipment, 'pickupAddress', {});
   const agents = get(mtoShipment, 'agents', {});
   const releasingAgent = Object.values(agents).find((agent) => agent.agentType === MTOAgentType.RELEASING);
-
+  const releasingAgentFullName = getFullAgentName(releasingAgent);
   const requestedDeliveryDate = get(mtoShipment, 'requestedDeliveryDate', '');
   const dropoffLocation = get(mtoShipment, 'destinationAddress', {});
   const receivingAgent = Object.values(agents).find((agent) => agent.agentType === MTOAgentType.RECEIVING);
+  const receivingAgentFullName = getFullAgentName(receivingAgent);
   const remarks = get(mtoShipment, 'customerRemarks', '');
 
-  // <ReviewSection fieldData={hhgShipmentData} title="Shipment [x]: HHG" editLink='' />
+  const hhgPickupLocation = <Address address={pickupLocation} />;
 
-  /*
-const hhgShipmentData = [
-  { label: 'Expected departure', value: },
-  { label: 'Starting ZIP', value: },
-  { label: 'Storage (SIT)', value: }, // val: "No" || "Yes, [x] days"
-  { label: 'Destination ZIP', value: },
-  { label: 'HHG shipment weight' }, // subheading, needs to be editable, but has the same styling as the rest...
-  { label: 'Estimated weight', value: },
-  { label: 'Estimated incentive', value: }, // val: [x] lbs
-  { label: 'Requested pickup date', value: },
-  { label: 'Pickup location', value: },
-  { label: 'Releasing agent', value: },
-  { label: 'Requested delivery date', value: },
-  { label: 'Destination', value: },
-  { label: 'Receiving agent', value: },
-  { label: 'Remarks', value: },
-  { label: '', value: },
-];
-*/
+  const destination = <Address address={dropoffLocation} />;
+
+  const hhgShipmentData = [
+    { label: 'Expected departure', value: '' },
+    { label: 'Starting ZIP', value: '' },
+    { label: 'Storage (SIT)', value: '' }, // val: "No" || "Yes, [x] days"
+    { label: 'Destination ZIP', value: '' },
+    { label: 'HHG shipment weight' }, // subheading, needs to be editable, but has the same styling as the rest...
+    { label: 'Estimated weight', value: '' },
+    { label: 'Estimated incentive', value: '' }, // val: [x] lbs
+    { label: 'Requested pickup date', value: formatDateSM(requestedPickupDate) },
+    { label: 'Pickup location', value: hhgPickupLocation },
+    { label: 'Releasing agent', value: releasingAgentFullName }, // optional field
+    { label: 'Requested delivery date', value: formatDateSM(requestedDeliveryDate) },
+    { label: 'Destination', value: destination },
+    { label: 'Receiving agent', value: receivingAgentFullName }, // optional field
+    { label: 'Remarks', value: remarks },
+  ];
 
   return (
     <div data-testid="hhg-summary" className="review-content">
+      <ReviewSection fieldData={hhgShipmentData} title="Shipment [x]: HHG" editLink="" />
+    </div>
+  );
+}
+
+/*
       <GridContainer>
         <h3>Shipment - Government moves all of your stuff (HHG)</h3>
         <Grid row>
@@ -128,9 +137,7 @@ const hhgShipmentData = [
           <Grid tablet={{ col: true }} />
         </Grid>
       </GridContainer>
-    </div>
-  );
-}
+*/
 
 HHGShipmentSummary.propTypes = {
   mtoShipment: PropTypes.shape({
