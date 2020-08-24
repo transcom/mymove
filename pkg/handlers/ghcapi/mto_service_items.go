78cad190cfe1a4df366e2dfd60f0de3ba69b649c
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2020-08-24 14:28:02 +0000

    Change output text to say mto service item.

--- pkg/handlers/ghcapi/mto_service_items.go
+++ pkg/handlers/ghcapi/mto_service_items.go
@@ -180,7 +180,7 @@ func (h UpdateMTOServiceItemStatusHandler) Handle(params mtoserviceitemop.Update
 	if err != nil {
 		switch err.(type) {
 		case services.NotFoundError:
-			payload := payloadForClientError("Unknown UUID(s)", "Unknown UUID(s) used to update a payment request ", h.GetTraceID())
+			payload := payloadForClientError("Unknown UUID(s)", "Unknown UUID(s) used to update a mto service item", h.GetTraceID())
 			return mtoserviceitemop.NewUpdateMTOServiceItemStatusNotFound().WithPayload(payload)
 		case services.PreconditionFailedError:
 			return mtoserviceitemop.NewUpdateMTOServiceItemStatusPreconditionFailed().WithPayload(&ghcmessages.Error{Message: handlers.FmtString(err.Error())})
