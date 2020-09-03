4f3c455e57e5490248751992b6422dd684b08a64
pearl-truss <pearl@truss.works>
2020-09-03 20:25:03 +0000

    wip added pass at generating payment service item and isa header

--- pkg/services/invoice/ghc_payment_request_invoice_generator.go
+++ pkg/services/invoice/ghc_payment_request_invoice_generator.go
@@ -1,6 +1,11 @@
 package invoice
 
 import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/gobuffalo/pop"
 	"github.com/transcom/mymove/pkg/models"
 
 	ediinvoice "github.com/transcom/mymove/pkg/edi/invoice"
@@ -9,14 +14,51 @@ import (
 
 // GHCPaymentRequestInvoiceGenerator is a service object to turn payment requests into 858s
 type GHCPaymentRequestInvoiceGenerator struct {
+	db *pop.Connection
 }
 
+const dateFormat = "060102"
+const timeFormat = "1504"
+
 // Generate method takes a payment request and returns an Invoice858C
-func (g GHCPaymentRequestInvoiceGenerator) Generate(paymentRequest models.PaymentRequest) (ediinvoice.Invoice858C, error) {
+func (g GHCPaymentRequestInvoiceGenerator) Generate(paymentRequest models.PaymentRequest, sendProductionInvoice bool) (ediinvoice.Invoice858C, error) {
 	// TODO: probably need to check if the MTO is loaded on the paymentRequest that is passed in, not sure what is more in line with go standards to error out if it's not there or look it up.
 	// TODO: seems ReferenceID is a *string but cannot be saved as nil, do we need to validate it's not nil here
 
+	currentTime := time.Now()
+
+	// TODO: interchangeControlNumber, err := icnSequencer.NextVal()
+	// if err != nil {
+	// 	return Invoice858C{}, errors.Wrap(err, fmt.Sprintf("Failed to get next Interchange Control Number"))
+	// }
+	var usageIndicator string
+	if sendProductionInvoice {
+		usageIndicator = "P"
+	} else {
+		usageIndicator = "T"
+	}
+
 	var edi858 ediinvoice.Invoice858C
+	isa := edisegment.ISA{
+		AuthorizationInformationQualifier: "00", // No authorization information
+		AuthorizationInformation:          "0084182369",
+		SecurityInformationQualifier:      "00", // No security information
+		SecurityInformation:               "_   _",
+		InterchangeSenderIDQualifier:      "ZZ",
+		InterchangeSenderID:               "GOVDPIBS",
+		InterchangeReceiverIDQualifier:    "12",
+		InterchangeReceiverID:             "8004171844",
+		InterchangeDate:                   currentTime.Format(dateFormat),
+		InterchangeTime:                   currentTime.Format(timeFormat),
+		InterchangeControlStandards:       "U",
+		InterchangeControlVersionNumber:   "00401",
+		InterchangeControlNumber:          100001272,
+		AcknowledgementRequested:          0,
+		UsageIndicator:                    usageIndicator, // T for test, P for production
+		ComponentElementSeparator:         "|",
+	}
+	edi858.ISA = append(edi858.ISA, &isa)
+
 	bx := edisegment.BX{
 		TransactionSetPurposeCode:    "00",
 		TransactionMethodTypeCode:    "J",
@@ -33,5 +75,80 @@ func (g GHCPaymentRequestInvoiceGenerator) Generate(paymentRequest models.Paymen
 	}
 	edi858.Header = append(edi858.Header, &paymentRequestNumberSegment)
 
+	var paymentServiceItems models.PaymentServiceItems
+	error := g.db.Q().
+		Eager("MTOServiceItem.ReService").
+		Where("payment_request_id", paymentRequest.ID).
+		All(&paymentServiceItems)
+	if error != nil {
+		return edi858, fmt.Errorf("Could not find payment service items: %w", error)
+	}
+
+	paymentServiceItemSegments, err := generatePaymentServiceItemSegments(paymentServiceItems)
+	if err != nil {
+		return edi858, fmt.Errorf("Could not generate payment service item segments: %w", err)
+	}
+	edi858.ServiceItems = append(edi858.ServiceItems, &paymentServiceItemSegments)
+	}
+
 	return edi858, nil
+}
+
+func generatePaymentServiceItemSegments(paymentRequest) ([]edisegment.Segment, error) {
+	//Initialize empty collection of segments
+	var segments []edisegment.Segment
+
+	// Iterate over payment service items
+	for idx, serviceItem := range paymentServiceItems {
+		// Initialize empty edisegment
+		var tariffSegments []edisegment.Segment
+
+		hierarchicalIDNumber := strconv.Itoa(idx + 1)
+		// Build and put together the segments
+		hlSegment := edisegment.HL{
+			HierarchicalIDNumber:  hierarchicalIDNumber, // may need to change if sending multiple payment request in a single edi
+			HierarchicalLevelCode: "|",
+		}
+
+		n9Segment := edisegment.N9{
+			ReferenceIdentificationQualifier: "PO",
+			// pending creation of shorter identifier for payment service item
+			// https://dp3.atlassian.net/browse/MB-3718
+			ReferenceIdentification: serviceItem.ID.String(),
+		}
+
+		// TODO: add another n9 for SIT
+		// TODO: add a L5 segment/definition
+
+		var weight models.PaymentServiceItemParam
+		// TODO: update to have a case statement as different service items may or may not have weight
+		// and the distance key can differ (zip3 v zip5, and distances for SIT)
+		error := g.db.Q().
+			Join("service_item_param_key sk", "payment_service_item_params.service_item_param_key_id = sk.id").
+			Where("payment_service_item_id", serviceItem.ID).
+			Where("sk.key = ?", ServiceItemParamNameWeightBilledActual)
+		First(&weight)
+		var distance models.PaymentServiceItemParam
+		error := g.db.Q().
+			Join("service_item_param_key sk", "payment_service_item_params.service_item_param_key_id = sk.id").
+			Where("payment_service_item_id", serviceItem.ID).
+			Where("sk.key = ?", ServiceItemParamNameDistanceZip3)
+		First(&distance)
+		if error != nil {
+			return edi858, fmt.Errorf("Could not lookup PaymentServiceItemParam: %w", error)
+		}
+		l0Segment := edisegment.L0{
+			LadingLineItemNumber:   hierarchicalIDNumber,
+			BilledRatedAsQuantity:  distance.Value,
+			BilledRatedAsQualifier: "DM",
+			Weight:                 weight.Value,
+			WeightQualifier:        "B",
+			WeightUnitCode:         "L",
+		}
+
+		tariffSegments = append(tariffSegments, hlSegment, n9Segment, l0Segment)
+
+		segments = append(segments, tariffSegments...)
+
+		return segments, nil;
 }
--- pkg/services/invoice/ghc_payment_request_invoice_generator_test.go
+++ pkg/services/invoice/ghc_payment_request_invoice_generator_test.go
@@ -2,6 +2,7 @@ package invoice
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -29,12 +30,40 @@ func TestGHCInvoiceSuite(t *testing.T) {
 }
 
 func (suite *GHCInvoiceSuite) TestGenerateGHCInvoice() {
-	suite.T().Run("addes bx header line", func(t *testing.T) {
+	const testDateFormat = "060102"
+	const testTimeFormat = "1504"
+	currentTime := time.Now()
+
+	suite.T().Run("adds isa header line", func(t *testing.T) {
 		paymentRequest := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
-		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest)
+		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest, false)
 		suite.NoError(err)
-		suite.IsType(&edisegment.BX{}, result.Header[0])
-		bx := result.Header[0].(*edisegment.BX)
+		suite.IsType(&edisegment.ISA{}, result.Header[0])
+		isa := result.Header[0].(*edisegment.ISA)
+		suite.Equal("00", isa.AuthorizationInformationQualifier)
+		suite.Equal("0084182369", isa.AuthorizationInformation)
+		suite.Equal("00", isa.SecurityInformationQualifier)
+		suite.Equal("_   _", isa.SecurityInformation)
+		suite.Equal("ZZ", isa.InterchangeSenderIDQualifier)
+		suite.Equal("GOVDPIBS", isa.InterchangeSenderID)
+		suite.Equal("12", isa.InterchangeReceiverIDQualifier)
+		suite.Equal("8004171844", isa.InterchangeReceiverID)
+		suite.Equal(currentTime.Format(testDateFormat), isa.InterchangeDate)
+		suite.Equal(currentTime.Format(testTimeFormat), isa.InterchangeTime)
+		suite.Equal("U", isa.InterchangeControlStandards)
+		suite.Equal("00401", isa.InterchangeControlVersionNumber)
+		suite.Equal(100001272, isa.InterchangeControlNumber)
+		suite.Equal("0", isa.AcknowledgementRequested)
+		suite.Equal("T", isa.UsageIndicator)
+		suite.Equal("|", isa.ComponentElementSeparator)
+	})
+
+	suite.T().Run("adds bx header line", func(t *testing.T) {
+		paymentRequest := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
+		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest, false)
+		suite.NoError(err)
+		suite.IsType(&edisegment.BX{}, result.Header[1])
+		bx := result.Header[1].(*edisegment.BX)
 		suite.Equal("00", bx.TransactionSetPurposeCode)
 		suite.Equal("J", bx.TransactionMethodTypeCode)
 		suite.Equal("PP", bx.ShipmentMethodOfPayment)
@@ -45,17 +74,17 @@ func (suite *GHCInvoiceSuite) TestGenerateGHCInvoice() {
 
 	suite.T().Run("adds payment request number to header", func(t *testing.T) {
 		paymentRequest := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
-		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest)
+		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest, false)
 		suite.NoError(err)
-		suite.IsType(&edisegment.N9{}, result.Header[1])
-		n9 := result.Header[1].(*edisegment.N9)
+		suite.IsType(&edisegment.N9{}, result.Header[2])
+		n9 := result.Header[2].(*edisegment.N9)
 		suite.Equal("CN", n9.ReferenceIdentificationQualifier)
 		suite.Equal(paymentRequest.PaymentRequestNumber, n9.ReferenceIdentification)
 	})
 
 	suite.T().Run("does not error out creating EDI from Invoice858", func(t *testing.T) {
 		paymentRequest := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
-		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest)
+		result, err := GHCPaymentRequestInvoiceGenerator{}.Generate(paymentRequest, false)
 		suite.NoError(err)
 		_, err = result.EDIString()
 		suite.NoError(err)
