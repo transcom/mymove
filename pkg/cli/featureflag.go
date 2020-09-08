37c28b457334367196fbe01e7145d11c958c2a6e
Moncef Belyamani <monfresh@users.noreply.github.com>
2020-09-08 10:04:03 -0400

    MB-3667 Create order with all fields at same time (#4629)
    * MB-3667 Create order with all fields at same time
    
    During onboarding, we store the service member's rank and origin duty
    station on the `service_members` table. However, on the TOO page,
    those fields are assumed to come from the `Orders` table. Since those
    fields are expected to be on the `Orders` table, they should be
    populated at the same time as the order is first created. This was not
    the case. The way those fields were added to the Order was via a
    temporary hack that was enabled in some environments, but not others.
    
    During the moves and MTOs consolidation, we were able to remove one of
    the temporary conversion functions. In this PR, we are able to
    remove the remaining conversion-related code entirely by adding those
    missing order fields to the order creation in the orders handler.

--- pkg/cli/featureflag.go
+++ pkg/cli/featureflag.go
@@ -8,14 +8,11 @@ import (
 const (
 	// FeatureFlagAccessCode is the access-code feature flag
 	FeatureFlagAccessCode string = "feature-flag-access-code"
-	// FeatureFlagConvertProfileOrdersToGHC is the convert-ppms-to-ghc feature flag
-	FeatureFlagConvertProfileOrdersToGHC string = "feature-flag-convert-profile-orders-to-ghc"
 )
 
 // InitFeatureFlags initializes FeatureFlags command line flags
 func InitFeatureFlags(flag *pflag.FlagSet) {
 	flag.Bool(FeatureFlagAccessCode, false, "Flag (bool) to enable requires-access-code")
-	flag.Bool(FeatureFlagConvertProfileOrdersToGHC, false, "Flag (bool) to enable convert-profile-orders-to-ghc")
 }
 
 // CheckFeatureFlag validates Verbose command line flags
