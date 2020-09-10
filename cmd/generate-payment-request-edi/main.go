4bc3914f5f790455c36e3628aff1c1b3dea91b2b
Harsha Murthy <harsha@truss.works>
2020-09-10 21:59:00 +0000

    Generate Payment Request EDI 858 CLI Utility WIP

---
+++ cmd/generate-payment-request-edi/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+
+	"github.com/transcom/mymove/pkg/cli"
+	"github.com/transcom/mymove/pkg/logging"
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services/invoice"
+)
+
+// Call this from command line with go run ./cmd/generate-payment-request-edi/ --payment-request-number
+// Must use a payment request that is submitted, but not yet approved for payment (that does not already have a submitted invoice)
+
+func checkConfig(v *viper.Viper, logger logger) error {
+
+	logger.Debug("checking config")
+
+	err := cli.CheckDatabase(v, logger)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initFlags(flag *pflag.FlagSet) {
+
+	// // Scenario config
+	flag.String("payment-request-number", "", "The payment request number to generate an EDI for")
+
+	// DB Config
+	cli.InitDatabaseFlags(flag)
+
+	// Verbose
+	cli.InitVerboseFlags(flag)
+
+	// Don't sort flags
+	flag.SortFlags = false
+}
+
+func main() {
+	flag := pflag.CommandLine
+	initFlags(flag)
+	parseErr := flag.Parse(os.Args[1:])
+	if parseErr != nil {
+		log.Fatal("failed to parse flags", zap.Error(parseErr))
+	}
+
+	v := viper.New()
+	bindErr := v.BindPFlags(flag)
+	if bindErr != nil {
+		log.Fatal("failed to bind flags", zap.Error(bindErr))
+	}
+	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	v.AutomaticEnv()
+
+	dbEnv := v.GetString(cli.DbEnvFlag)
+
+	logger, err := logging.Config(dbEnv, v.GetBool(cli.VerboseFlag))
+	if err != nil {
+		log.Fatalf("Failed to initialize Zap logging due to %v", err)
+	}
+	zap.ReplaceGlobals(logger)
+
+	fmt.Println("logger: ", logger)
+
+	err = checkConfig(v, logger)
+	if err != nil {
+		logger.Fatal("invalid configuration", zap.Error(err))
+	}
+
+	// DB connection
+	dbConnection, err := cli.InitDatabase(v, nil, logger)
+	if err != nil {
+		// No connection object means that the configuraton failed to validate and we should not startup
+		// A valid connection object that still has an error indicates that the DB is not up and we should not startup
+		logger.Fatal("Connecting to DB", zap.Error(err))
+	}
+
+	paymentRequestNumber := v.GetString("payment-request-number")
+	var paymentRequest models.PaymentRequest
+
+	err = dbConnection.Where("payment_request_number = ?", paymentRequestNumber).First(&paymentRequest)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	generator := invoice.GHCPaymentRequestInvoiceGenerator{DB: dbConnection}
+	edi858c, err := generator.Generate(paymentRequest, false)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result, err := edi858c.EDIString()
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	fmt.Println(result)
+}
