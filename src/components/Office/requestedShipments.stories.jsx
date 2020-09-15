f4fa228a4a50f7ee0cf53b7b28c5192163030dc7
Suzanne Rozier <suz@truss.works>
2020-09-15 16:38:34 +0000

    Flex behavior for shipment cards

--- src/components/Office/requestedShipments.stories.jsx
+++ src/components/Office/requestedShipments.stories.jsx
@@ -64,6 +64,64 @@ const shipments = [
   },
   {
     approvedDate: '0001-01-01',
+    createdAt: '2020-06-10T15:58:02.404029Z',
+    customerRemarks: 'please treat gently',
+    destinationAddress: {
+      city: 'Fairfield',
+      country: 'US',
+      eTag: 'MjAyMC0wNi0xMFQxNTo1ODowMi4zODk0MTJa',
+      id: '672ff379-f6e3-48b4-a87d-796713f8f997',
+      postal_code: '94535',
+      state: 'CA',
+      street_address_1: '987 Any Avenue',
+      street_address_2: 'P.O. Box 9876',
+      street_address_3: 'c/o Some Person',
+    },
+    eTag: 'MjAyMC0wNi0xMFQxNTo1ODowMi40MDQwMzFa',
+    id: 'cd01a5b8-9b44-4511-8a8d-edb60f2a4aea',
+    moveTaskOrderID: '9c7b255c-2981-4bf8-839f-61c7458e2b4d',
+    pickupAddress: {
+      city: 'Beverly Hills',
+      country: 'US',
+      eTag: 'MjAyMC0wNi0xMFQxNTo1ODowMi4zODQ3Njla',
+      id: '1686751b-ab36-43cf-b3c9-c0f467d13c19',
+      postal_code: '90210',
+      state: 'CA',
+      street_address_1: '123 Any Street',
+      street_address_2: 'P.O. Box 12345',
+      street_address_3: 'c/o Some Person',
+    },
+    rejectionReason: 'shipment not good enough',
+    requestedPickupDate: '2018-03-15',
+    scheduledPickupDate: '2018-03-16',
+    secondaryDeliveryAddress: {
+      city: 'Beverly Hills',
+      country: 'US',
+      eTag: 'MjAyMC0wNi0xMFQxNTo1ODowMi4zOTkzMlo=',
+      id: '15e8f6cc-e1d7-44b2-b1e0-fcb3d6442831',
+      postal_code: '90210',
+      state: 'CA',
+      street_address_1: '123 Any Street',
+      street_address_2: 'P.O. Box 12345',
+      street_address_3: 'c/o Some Person',
+    },
+    secondaryPickupAddress: {
+      city: 'Beverly Hills',
+      country: 'US',
+      eTag: 'MjAyMC0wNi0xMFQxNTo1ODowMi4zOTM4OTZa',
+      id: '9b79e0c3-8ed5-4fb8-aa36-95845707d8ee',
+      postal_code: '90210',
+      state: 'CA',
+      street_address_1: '123 Any Street',
+      street_address_2: 'P.O. Box 12345',
+      street_address_3: 'c/o Some Person',
+    },
+    shipmentType: 'HHG',
+    status: 'SUBMITTED',
+    updatedAt: '2020-06-10T15:58:02.404031Z',
+  },
+  {
+    approvedDate: '0001-01-01',
     createdAt: '2020-06-10T15:58:02.431993Z',
     customerRemarks: 'please treat gently',
     destinationAddress: {
@@ -243,7 +301,7 @@ storiesOf('TOO/TIO Components|RequestedShipments', module)
       </div>
     );
   })
-  .add('with two shipments', () => {
+  .add('with multiple shipments', () => {
     return (
       <div style={{ position: 'relative', padding: '20px' }}>
         <RequestedShipments
@@ -272,7 +330,7 @@ storiesOf('TOO/TIO Components|RequestedShipments', module)
       </div>
     );
   })
-  .add('with two approved shipments', () => {
+  .add('with multiple approved shipments', () => {
     return (
       <div style={{ position: 'relative', padding: '20px' }}>
         <RequestedShipments
