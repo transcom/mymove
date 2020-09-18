b7d802c7e218ef7001439bfde9972c1a39dfa38d
Donald Thai <donald@truss.works>
2020-09-18 00:08:51 +0000

    refactor fetcher to omit soft deleted documents

--- pkg/services/payment_request/payment_request_fetcher_test.go
+++ pkg/services/payment_request/payment_request_fetcher_test.go
@@ -2,6 +2,7 @@ package paymentrequest
 
 import (
 	"testing"
+	"time"
 
 	"github.com/transcom/mymove/pkg/testdatagen"
 
@@ -20,6 +21,71 @@ func (suite *PaymentRequestServiceSuite) TestFetchPaymentRequest() {
 
 		suite.NoError(err)
 		suite.Equal(pr.ID, paymentRequest.ID)
+	})
+
+	suite.T().Run("returns payment request with proof of service docs", func(t *testing.T) {
+
+		fetcher := NewPaymentRequestFetcher(suite.DB())
+
+		pr := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
+		posd := testdatagen.MakeProofOfServiceDoc(suite.DB(), testdatagen.Assertions{
+			ProofOfServiceDoc: models.ProofOfServiceDoc{
+				PaymentRequestID: pr.ID,
+			},
+		})
+		u := testdatagen.MakeDefaultUpload(suite.DB())
+		pu := testdatagen.MakePrimeUpload(suite.DB(), testdatagen.Assertions{
+			PrimeUpload: models.PrimeUpload{
+				ProofOfServiceDocID: posd.ID,
+				UploadID:            u.ID,
+			},
+		})
+
+		paymentRequest, err := fetcher.FetchPaymentRequest(pr.ID)
+
+		suite.NoError(err)
+		suite.Equal(pr.ID, paymentRequest.ID)
+
+		suite.Len(paymentRequest.ProofOfServiceDocs, 1)
+		suite.Equal(posd.ID, paymentRequest.ProofOfServiceDocs[0].ID)
+
+		suite.Len(paymentRequest.ProofOfServiceDocs[0].PrimeUploads, 1)
+		suite.Equal(pu.ID, paymentRequest.ProofOfServiceDocs[0].PrimeUploads[0].ID)
+
+		suite.Equal(u.ID, paymentRequest.ProofOfServiceDocs[0].PrimeUploads[0].UploadID)
+	})
+
+	suite.T().Run("returns payment request without soft deleted proof of service docs", func(t *testing.T) {
+
+		fetcher := NewPaymentRequestFetcher(suite.DB())
+
+		pr := testdatagen.MakePaymentRequest(suite.DB(), testdatagen.Assertions{})
+		posd := testdatagen.MakeProofOfServiceDoc(suite.DB(), testdatagen.Assertions{
+			ProofOfServiceDoc: models.ProofOfServiceDoc{
+				PaymentRequestID: pr.ID,
+			},
+		})
+		deletedAt := time.Now()
+		u := testdatagen.MakeUpload(suite.DB(), testdatagen.Assertions{
+			Upload: models.Upload{DeletedAt: &deletedAt},
+		})
+		testdatagen.MakePrimeUpload(suite.DB(), testdatagen.Assertions{
+			PrimeUpload: models.PrimeUpload{
+				ProofOfServiceDocID: posd.ID,
+				UploadID:            u.ID,
+				DeletedAt:           &deletedAt,
+			},
+		})
+
+		paymentRequest, err := fetcher.FetchPaymentRequest(pr.ID)
+
+		suite.NoError(err)
+		suite.Equal(pr.ID, paymentRequest.ID)
+
+		suite.Len(paymentRequest.ProofOfServiceDocs, 1)
+		suite.Equal(posd.ID, paymentRequest.ProofOfServiceDocs[0].ID)
+
+		suite.Len(paymentRequest.ProofOfServiceDocs[0].PrimeUploads, 0)
 	})
 
 	suite.T().Run("if there is an error, we get it with zero payment request", func(t *testing.T) {
