8dcda88cde93ab14be35315dbdcd49293bdb8c93
Sandy Wright <sandy@truss.works>
2020-09-25 16:05:04 -0500

    MB-4236 Add stale eTag and 404 tests

--- pkg/handlers/primeapi/mto_agent_test.go
+++ pkg/handlers/primeapi/mto_agent_test.go
@@ -30,12 +30,10 @@ func (suite *HandlerSuite) TestUpdateMTOAgentHandler() {
 	phone := "555-456-7890"
 
 	newAgent := models.MTOAgent{
-		ID:            agent.ID,
-		MTOShipmentID: agent.MTOShipmentID,
-		FirstName:     &firstName,
-		LastName:      &lastName,
-		Email:         &email,
-		Phone:         &phone,
+		FirstName: &firstName,
+		LastName:  &lastName,
+		Email:     &email,
+		Phone:     &phone,
 	}
 
 	// Create handler and request
@@ -46,7 +44,7 @@ func (suite *HandlerSuite) TestUpdateMTOAgentHandler() {
 	req := httptest.NewRequest("PUT", fmt.Sprintf("/mto-shipments/%s/agents/%s", agent.MTOShipmentID.String(), agent.ID.String()), nil)
 
 	eTag := etag.GenerateEtag(agent.UpdatedAt)
-	//var updatedETag string
+	var updatedETag string
 
 	// Test a successful request + update
 	suite.T().Run("200 - OK response", func(t *testing.T) {
@@ -67,18 +65,33 @@ func (suite *HandlerSuite) TestUpdateMTOAgentHandler() {
 
 		// Check values
 		agentOK := response.(*mtoshipmentops.UpdateMTOAgentOK)
-		suite.Equal(agentOK.Payload.ID, agent.ID)
-		suite.Equal(agentOK.Payload.MtoShipmentID, agent.MTOShipmentID)
+		suite.Equal(agentOK.Payload.ID.String(), agent.ID.String())
+		suite.Equal(agentOK.Payload.MtoShipmentID.String(), agent.MTOShipmentID.String())
+		suite.Equal(string(agentOK.Payload.AgentType), string(agent.MTOAgentType)) // wasn't updated, should be original value
 		suite.Equal(agentOK.Payload.FirstName, newAgent.FirstName)
 		suite.Equal(agentOK.Payload.LastName, newAgent.LastName)
 		suite.Equal(agentOK.Payload.Email, newAgent.Email)
 		suite.Equal(agentOK.Payload.Phone, newAgent.Phone)
-		//updatedETag = agentOK.Payload.ETag
+		updatedETag = agentOK.Payload.ETag
 	})
 
 	// Test stale eTag
 	suite.T().Run("412 - Precondition failed response", func(t *testing.T) {
+		// Let's test with the same valid values, but the original, expired eTag:
+		payload := payloads.MTOAgent(&newAgent)
+		params := mtoshipmentops.UpdateMTOAgentParams{
+			HTTPRequest:   req,
+			AgentID:       *handlers.FmtUUID(agent.ID),
+			MtoShipmentID: *handlers.FmtUUID(agent.MTOShipmentID),
+			Body:          payload,
+			IfMatch:       eTag, // stale
+		}
+		// Run swagger validations
+		suite.NoError(params.Body.Validate(strfmt.Default))
 
+		// Run handler and check response
+		response := handler.Handle(params)
+		suite.IsType(&mtoshipmentops.UpdateMTOAgentPreconditionFailed{}, response)
 	})
 
 	// Test invalid IDs in the body vs. path values
@@ -93,7 +106,24 @@ func (suite *HandlerSuite) TestUpdateMTOAgentHandler() {
 
 	// Test not found response
 	suite.T().Run("404 - Not found response", func(t *testing.T) {
+		payload := payloads.MTOAgent(&newAgent)
+		params := mtoshipmentops.UpdateMTOAgentParams{
+			HTTPRequest:   req,
+			AgentID:       *handlers.FmtUUID(agent.MTOShipmentID), // instead of agent.ID
+			MtoShipmentID: *handlers.FmtUUID(agent.MTOShipmentID),
+			Body:          payload,
+			IfMatch:       updatedETag,
+		}
+		// Run swagger validations
+		suite.NoError(params.Body.Validate(strfmt.Default))
 
+		// Run handler and check response
+		response := handler.Handle(params)
+		suite.IsType(&mtoshipmentops.UpdateMTOAgentNotFound{}, response)
+
+		// Check error message for the incorrect ID
+		agentNotFound := response.(*mtoshipmentops.UpdateMTOAgentNotFound)
+		suite.Contains(*agentNotFound.Payload.Detail, agent.MTOShipmentID.String())
 	})
 
 	// Test not Prime-available (not found response)
