e7e45f287b110a319ac3fc71a3187391d14ea4fa
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2020-09-30 20:16:24 +0000

    Remove CreateMTOServiceItem handler from GHCAPI

--- pkg/handlers/ghcapi/mto_service_items.go
+++ pkg/handlers/ghcapi/mto_service_items.go
@@ -2,12 +2,10 @@ package ghcapi
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gobuffalo/validate"
 	"github.com/gofrs/uuid"
-	"github.com/pkg/errors"
 	"go.uber.org/zap"
 
 	mtoserviceitemop "github.com/transcom/mymove/pkg/gen/ghcapi/ghcoperations/mto_service_item"
@@ -34,91 +32,6 @@ func payloadForValidationError(title string, detail string, instance uuid.UUID, 
 		InvalidFields: handlers.NewValidationErrorsResponse(validationErrors).Errors,
 		ClientError:   *payloadForClientError(title, detail, instance),
 	}
-}
-
-// CreateMTOServiceItemHandler struct that describes creating a mto service item handler
-type CreateMTOServiceItemHandler struct {
-	handlers.HandlerContext
-	services.MTOServiceItemCreator
-}
-
-// Handle handler that creates a mto service item
-func (h CreateMTOServiceItemHandler) Handle(params mtoserviceitemop.CreateMTOServiceItemParams) middleware.Responder {
-	var errs []string
-	session, logger := h.SessionAndLoggerFromRequest(params.HTTPRequest)
-
-	moveTaskOrderID, err := uuid.FromString(params.MoveTaskOrderID.String())
-	if err != nil {
-		errs = append(errs, fmt.Errorf("UUID Parsing for %s: %w", "MoveTaskOrderID", err).Error())
-	}
-
-	reServiceID, err := uuid.FromString(params.CreateMTOServiceItemBody.ReServiceID.String())
-	if err != nil {
-		errs = append(errs, fmt.Errorf("UUID Parsing for %s: %w", "ReServiceID", err).Error())
-	}
-
-	mtoShipmentID, err := uuid.FromString(params.CreateMTOServiceItemBody.MtoShipmentID.String())
-	if err != nil {
-		errs = append(errs, fmt.Errorf("UUID Parsing for %s: %w", "MtoShipmentID", err).Error())
-	}
-
-	// return any parsing errors for uuids
-	if len(errs) > 0 {
-		parsingError := strings.Join(errs, "\n")
-		logger.Error(parsingError)
-		payload := payloadForValidationError("UUID(s) parsing error", parsingError, h.GetTraceID(), validate.NewErrors())
-
-		return mtoserviceitemop.NewCreateMTOServiceItemUnprocessableEntity().WithPayload(payload)
-	}
-
-	serviceItem := models.MTOServiceItem{
-		MoveTaskOrderID: moveTaskOrderID,
-		ReServiceID:     reServiceID,
-		MTOShipmentID:   &mtoShipmentID,
-	}
-
-	// Capture creation attempt in audit log
-	_, err = audit.Capture(&serviceItem, nil, logger, session, params.HTTPRequest)
-	if err != nil {
-		logger.Error("Auditing service error for service item creation.", zap.Error(err))
-		return mtoserviceitemop.NewCreateMTOServiceItemInternalServerError()
-	}
-
-	createdServiceItems, verrs, err := h.MTOServiceItemCreator.CreateMTOServiceItem(&serviceItem)
-	if verrs != nil && verrs.HasAny() {
-		logger.Error("Error validating mto service item: ", zap.Error(verrs))
-		payload := payloadForValidationError(handlers.ValidationErrMessage, "The information you provided is invalid.", h.GetTraceID(), verrs)
-
-		return mtoserviceitemop.NewCreateMTOServiceItemUnprocessableEntity().WithPayload(payload)
-	}
-
-	// return any errors
-	if err != nil {
-		logger.Error("Error creating mto service item: ", zap.Error(err))
-
-		if strings.Contains(errors.Cause(err).Error(), models.ViolatesForeignKeyConstraint) {
-			payload := payloadForClientError("Unknown UUID(s)", "Unknown UUID(s) used to create a mto service item.", h.GetTraceID())
-
-			return mtoserviceitemop.NewCreateMTOServiceItemNotFound().WithPayload(payload)
-		}
-
-		return mtoserviceitemop.NewCreateMTOServiceItemInternalServerError()
-	}
-
-	_, err = event.TriggerEvent(event.Event{
-		EventKey:        event.MTOServiceItemCreateEventKey,
-		MtoID:           (*createdServiceItems)[0].MoveTaskOrderID,
-		UpdatedObjectID: (*createdServiceItems)[0].ID,
-		Request:         params.HTTPRequest,
-		EndpointKey:     event.GhcCreateMTOServiceItemEndpointKey,
-		DBConnection:    h.DB(),
-		HandlerContext:  h,
-	})
-	if err != nil {
-		logger.Error("ghcapi.CreateMTOServiceItemHandler could not generate the event")
-	}
-	serviceItemsPayload := payloads.MTOServiceItemModels(*createdServiceItems)
-	return mtoserviceitemop.NewCreateMTOServiceItemCreated().WithPayload(serviceItemsPayload[0])
 }
 
 // UpdateMTOServiceItemStatusHandler struct that describes updating service item status
