859a0a93bfa3e06b4954e0cb7c1eaacac53b4982
Shauna Keating <shauna@truss.works>
2020-10-07 21:20:49 +0000

    adds div to wrap forms we want to wrap in special customer onbaording containers on desktop and styles for it

--- src/components/Customer/EditShipment.jsx
+++ src/components/Customer/EditShipment.jsx
@@ -292,142 +292,144 @@ class EditShipment extends Component {
           validationSchema={HHGDetailsFormSchema}
         >
           {({ values, isValid, dirty, isSubmitting, handleChange }) => (
-            <Form>
-              <div className="container">
-                <Fieldset legend="Pickup date" className="margin-top-4">
-                  <Field
-                    as={DatePickerInput}
-                    name="requestedPickupDate"
-                    label="Requested pickup date"
-                    labelClassName={`margin-top-2 ${styles['small-bold']}`}
-                    id="requestedPickupDate"
-                    value={values.requestedPickupDate}
+            <div className="wrapper-co">
+              <Form>
+                <div className="container-co">
+                  <Fieldset legend="Pickup date" className="margin-top-4">
+                    <Field
+                      as={DatePickerInput}
+                      name="requestedPickupDate"
+                      label="Requested pickup date"
+                      labelClassName={`margin-top-2 ${styles['small-bold']}`}
+                      id="requestedPickupDate"
+                      value={values.requestedPickupDate}
+                      validate={validateDate}
+                    />
+                    <Hint className="margin-top-1" id="pickupDateHint">
+                      Movers will contact you to schedule the actual pickup date. That date should fall within 7 days of
+                      your requested date. Tip: Avoid scheduling multiple shipments on the same day.{' '}
+                    </Hint>
+                  </Fieldset>
+                </div>
+                <AddressFields
+                  className="margin-bottom-3"
+                  name="pickupAddress"
+                  legend="Pickup location"
+                  renderExistingAddressCheckbox={() => (
+                    <div className="margin-y-2">
+                      <Checkbox
+                        data-testid="useCurrentResidence"
+                        label="Use my current residence address"
+                        name="useCurrentResidence"
+                        checked={useCurrentResidence}
+                        onChange={() => this.handleUseCurrentResidenceChange(values)}
+                      />
+                    </div>
+                  )}
+                  values={values.pickupAddress}
+                />
+                <Hint>If you have more things at another pickup location, you can schedule for them later.</Hint>
+                <hr className="margin-top-4 margin-bottom-4" />
+                <ContactInfoFields
+                  className="margin-bottom-5"
+                  name="releasingAgent"
+                  legend="Releasing agent"
+                  hintText="Optional"
+                  subtitle="Who can allow the movers to take your stuff if you're not there?"
+                  subtitleClassName="margin-top-3"
+                  values={values.releasingAgent}
+                />
+                <Divider className="margin-bottom-6" />
+                <Fieldset legend="Delivery date">
+                  <DatePickerInput
+                    name="requestedDeliveryDate"
+                    label="Requested delivery date"
+                    labelClassName={`${styles['small-bold']}`}
+                    id="requestedDeliveryDate"
+                    value={values.requestedDeliveryDate}
                     validate={validateDate}
                   />
-                  <Hint className="margin-top-1" id="pickupDateHint">
-                    Movers will contact you to schedule the actual pickup date. That date should fall within 7 days of
-                    your requested date. Tip: Avoid scheduling multiple shipments on the same day.{' '}
+                  <Hint className="margin-top-1">
+                    Shipments can take several weeks to arrive, depending on how far they&apos;re going. Your movers
+                    will contact you close to the date you select to coordinate delivery
                   </Hint>
                 </Fieldset>
-              </div>
-              <AddressFields
-                className="margin-bottom-3"
-                name="pickupAddress"
-                legend="Pickup location"
-                renderExistingAddressCheckbox={() => (
-                  <div className="margin-y-2">
-                    <Checkbox
-                      data-testid="useCurrentResidence"
-                      label="Use my current residence address"
-                      name="useCurrentResidence"
-                      checked={useCurrentResidence}
-                      onChange={() => this.handleUseCurrentResidenceChange(values)}
+                <Divider className="margin-top-4 margin-bottom-4" />
+                <Fieldset legend="Delivery location">
+                  <Label className="margin-top-3 margin-bottom-1">Do you know your delivery address?</Label>
+                  <div className="display-flex margin-top-1">
+                    <Radio
+                      className="margin-right-3"
+                      id="has-delivery-address"
+                      label="Yes"
+                      name="hasDeliveryAddress"
+                      onChange={this.handleChangeHasDeliveryAddress}
+                      checked={hasDeliveryAddress}
+                    />
+                    <Radio
+                      id="no-delivery-address"
+                      label="No"
+                      name="hasDeliveryAddress"
+                      checked={!hasDeliveryAddress}
+                      onChange={this.handleChangeHasDeliveryAddress}
                     />
                   </div>
-                )}
-                values={values.pickupAddress}
-              />
-              <Hint>If you have more things at another pickup location, you can schedule for them later.</Hint>
-              <hr className="margin-top-4 margin-bottom-4" />
-              <ContactInfoFields
-                className="margin-bottom-5"
-                name="releasingAgent"
-                legend="Releasing agent"
-                hintText="Optional"
-                subtitle="Who can allow the movers to take your stuff if you're not there?"
-                subtitleClassName="margin-top-3"
-                values={values.releasingAgent}
-              />
-              <Divider className="margin-bottom-6" />
-              <Fieldset legend="Delivery date">
-                <DatePickerInput
-                  name="requestedDeliveryDate"
-                  label="Requested delivery date"
-                  labelClassName={`${styles['small-bold']}`}
-                  id="requestedDeliveryDate"
-                  value={values.requestedDeliveryDate}
-                  validate={validateDate}
+                  {hasDeliveryAddress && <AddressFields name="destinationAddress" values={values.destinationAddress} />}
+                </Fieldset>
+                <Divider className="margin-top-4 margin-bottom-4" />
+                <ContactInfoFields
+                  name="receivingAgent"
+                  legend="Receiving agent"
+                  hintText="Optional"
+                  subtitleClassName="margin-top-3"
+                  subtitle="Who can take delivery for you if the movers arrive and you're not there?"
+                  values={values.receivingAgent}
                 />
-                <Hint className="margin-top-1">
-                  Shipments can take several weeks to arrive, depending on how far they&apos;re going. Your movers will
-                  contact you close to the date you select to coordinate delivery
-                </Hint>
-              </Fieldset>
-              <Divider className="margin-top-4 margin-bottom-4" />
-              <Fieldset legend="Delivery location">
-                <Label className="margin-top-3 margin-bottom-1">Do you know your delivery address?</Label>
-                <div className="display-flex margin-top-1">
-                  <Radio
-                    className="margin-right-3"
-                    id="has-delivery-address"
-                    label="Yes"
-                    name="hasDeliveryAddress"
-                    onChange={this.handleChangeHasDeliveryAddress}
-                    checked={hasDeliveryAddress}
-                  />
-                  <Radio
-                    id="no-delivery-address"
-                    label="No"
-                    name="hasDeliveryAddress"
-                    checked={!hasDeliveryAddress}
-                    onChange={this.handleChangeHasDeliveryAddress}
-                  />
-                </div>
-                {hasDeliveryAddress && <AddressFields name="destinationAddress" values={values.destinationAddress} />}
-              </Fieldset>
-              <Divider className="margin-top-4 margin-bottom-4" />
-              <ContactInfoFields
-                name="receivingAgent"
-                legend="Receiving agent"
-                hintText="Optional"
-                subtitleClassName="margin-top-3"
-                subtitle="Who can take delivery for you if the movers arrive and you're not there?"
-                values={values.receivingAgent}
-              />
-              <Divider className="margin-top-4 margin-bottom-4" />
-              <Fieldset hintText="Optional" legend="Remarks">
-                <div className={`${styles['small-bold']} margin-top-3 margin-bottom-1`}>
-                  Is there anything special about this shipment that the movers should know?
-                </div>
-                <div className={`${styles['hhg-examples-container']}`}>
-                  <strong>Examples</strong>
-                  <ul>
-                    <li>Things that might need special handling</li>
-                    <li>Access info for a location</li>
-                    <li>Weapons or alcohol</li>
-                  </ul>
-                </div>
+                <Divider className="margin-top-4 margin-bottom-4" />
+                <Fieldset hintText="Optional" legend="Remarks">
+                  <div className={`${styles['small-bold']} margin-top-3 margin-bottom-1`}>
+                    Is there anything special about this shipment that the movers should know?
+                  </div>
+                  <div className={`${styles['hhg-examples-container']}`}>
+                    <strong>Examples</strong>
+                    <ul>
+                      <li>Things that might need special handling</li>
+                      <li>Access info for a location</li>
+                      <li>Weapons or alcohol</li>
+                    </ul>
+                  </div>
 
-                <Textarea
-                  label="Anything else you would like us to know?"
-                  data-testid="remarks"
-                  name="customerRemarks"
-                  className={`${styles.remarks}`}
-                  placeholder="500 characters"
-                  maxLength={500}
-                  type="textarea"
-                  value={values.customerRemarks}
-                  onChange={handleChange}
-                />
-              </Fieldset>
-              <Divider className="margin-top-6 margin-bottom-3" />
-              <Hint className="margin-bottom-2">
-                You can change details for your HHG shipment when you talk to your move counselor or the person
-                who&apos;s your point of contact with the movers. You can also edit in MilMove up to 24 hours before
-                your final pickup date.
-              </Hint>
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <Button
-                  disabled={isSubmitting || (!isValid && !dirty) || (isValid && !dirty)}
-                  onClick={() => this.submitMTOShipment(values)}
-                >
-                  <span>Save</span>
-                </Button>
-                <Button className={`${styles['cancel-button']}`} onClick={goBack}>
-                  <span>Cancel</span>
-                </Button>
-              </div>
-            </Form>
+                  <Textarea
+                    label="Anything else you would like us to know?"
+                    data-testid="remarks"
+                    name="customerRemarks"
+                    className={`${styles.remarks}`}
+                    placeholder="500 characters"
+                    maxLength={500}
+                    type="textarea"
+                    value={values.customerRemarks}
+                    onChange={handleChange}
+                  />
+                </Fieldset>
+                <Divider className="margin-top-6 margin-bottom-3" />
+                <Hint className="margin-bottom-2">
+                  You can change details for your HHG shipment when you talk to your move counselor or the person
+                  who&apos;s your point of contact with the movers. You can also edit in MilMove up to 24 hours before
+                  your final pickup date.
+                </Hint>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <Button
+                    disabled={isSubmitting || (!isValid && !dirty) || (isValid && !dirty)}
+                    onClick={() => this.submitMTOShipment(values)}
+                  >
+                    <span>Save</span>
+                  </Button>
+                  <Button className={`${styles['cancel-button']}`} onClick={goBack}>
+                    <span>Cancel</span>
+                  </Button>
+                </div>
+              </Form>
+            </div>
           )}
         </Formik>
       </div>
