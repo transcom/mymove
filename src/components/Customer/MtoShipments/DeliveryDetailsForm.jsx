82472dc1ab5f276fdb1e1d1f1a8612eaad13098c
Jeri Sommers 🐍 <sojeri@truss.works>
2020-10-07 20:16:38 +0000

    add placeholder files

---
+++ src/components/Customer/MtoShipments/DeliveryDetailsForm.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class DeliveryDetailsForm extends Component {
+  componentDidMount() {
+    return new Error('not implemented');
+  }
+
+  render() {
+    return new Error('not implemented');
+  }
+}
+
+export default { DeliveryDetailsForm };
---
+++ src/components/Customer/MtoShipments/PickupDetails.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class PickupDetailsForm extends Component {
+  componentDidMount() {
+    return new Error('not implemented');
+  }
+
+  render() {
+    return new Error('not implemented');
+  }
+}
+
+export default { PickupDetailsForm };
