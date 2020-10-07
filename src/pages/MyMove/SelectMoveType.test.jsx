dafcd8c63b959a3219adb939234c76c48f72f635
Jeri Sommers 🐍 <sojeri@truss.works>
2020-10-07 18:14:48 +0000

    use more react-y way of setting state in tests

--- src/pages/MyMove/SelectMoveType.test.jsx
+++ src/pages/MyMove/SelectMoveType.test.jsx
@@ -6,23 +6,19 @@ import { SHIPMENT_OPTIONS } from 'shared/constants';
 import { SelectMoveType } from 'pages/MyMove/SelectMoveType';
 
 describe('SelectMoveType', () => {
-  let defaultProps;
-
-  beforeEach(() => {
-    defaultProps = {
-      pageList: ['page1', 'anotherPage/:foo/:bar'],
-      pageKey: 'page1',
-      match: { isExact: false, path: '', url: '' },
-      updateMove: jest.fn(),
-      push: jest.fn(),
-      loadMTOShipments: jest.fn(),
-      move: { id: 'mockId', status: 'DRAFT' },
-      selectedMoveType: SHIPMENT_OPTIONS.PPM,
-      isPpmSelectable: true,
-      isHhgSelectable: true,
-      shipmentNumber: 4,
-    };
-  });
+  const defaultProps = {
+    pageList: ['page1', 'anotherPage/:foo/:bar'],
+    pageKey: 'page1',
+    match: { isExact: false, path: '', url: '' },
+    updateMove: jest.fn(),
+    push: jest.fn(),
+    loadMTOShipments: jest.fn(),
+    move: { id: 'mockId', status: 'DRAFT' },
+    selectedMoveType: SHIPMENT_OPTIONS.PPM,
+    isPpmSelectable: true,
+    isHhgSelectable: true,
+    shipmentNumber: 4,
+  };
 
   it('should render radio buttons with PPM selected', () => {
     // eslint-disable-next-line react/jsx-props-no-spreading
@@ -35,9 +31,8 @@ describe('SelectMoveType', () => {
   });
 
   it('should render radio buttons with HHG selected', () => {
-    defaultProps.selectedMoveType = SHIPMENT_OPTIONS.HHG;
     // eslint-disable-next-line react/jsx-props-no-spreading
-    const wrapper = mount(<SelectMoveType {...defaultProps} />);
+    const wrapper = mount(<SelectMoveType {...defaultProps} selectedMoveType={SHIPMENT_OPTIONS.HHG} />);
     expect(wrapper.find(Radio).length).toBe(2);
 
     expect(wrapper.find(Radio).at(1).text()).toContain('The government packs for me and moves me');
@@ -46,10 +41,8 @@ describe('SelectMoveType', () => {
   });
 
   it('should disable PPM form option if PPM is already submitted', () => {
-    defaultProps.isPpmSelectable = false;
-
     // eslint-disable-next-line react/jsx-props-no-spreading
-    const wrapper = mount(<SelectMoveType {...defaultProps} />);
+    const wrapper = mount(<SelectMoveType {...defaultProps} isPpmSelectable={false} />);
 
     // PPM button should be disabled on page load and should contained updated text
     const actualComponentText = wrapper.text();
@@ -59,10 +52,8 @@ describe('SelectMoveType', () => {
   });
 
   it('should disable HHG form option if move is already submitted', () => {
-    defaultProps.isHhgSelectable = false;
-
     // eslint-disable-next-line react/jsx-props-no-spreading
-    const wrapper = mount(<SelectMoveType {...defaultProps} />);
+    const wrapper = mount(<SelectMoveType {...defaultProps} isHhgSelectable={false} />);
 
     // HHG button should be disabled on page load and should contained updated text
     const actualComponentText = wrapper.text();
