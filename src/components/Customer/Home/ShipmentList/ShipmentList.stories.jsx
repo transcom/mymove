ff97e4fb48de8d3e3fda0e97d3156ac7fdcd22f9
Ron Cruz <Ronolibert@gmail.com>
2020-10-08 17:42:26 +0000

    Fix merge conflicts

--- src/components/Customer/Home/ShipmentList/ShipmentList.stories.jsx
+++ src/components/Customer/Home/ShipmentList/ShipmentList.stories.jsx
@@ -8,7 +8,7 @@ import { SHIPMENT_OPTIONS } from 'shared/constants';
 export const Basic = () => (
   <div className="grid-container">
     <h3>Single Shipment</h3>
-    <ShipmentList shipments={[{ id: '#0001', shipmentType: SHIPMENT_OPTIONS.PPM }]} onShipmentClick={() => {}} />
+    <ShipmentList shipments={[{ id: '0001', shipmentType: SHIPMENT_OPTIONS.PPM }]} onShipmentClick={() => {}} />
     <br />
     <h3>Multiple shipments</h3>
     <ShipmentList
