ef844e781fe940428dfa63e13fc36f38d725f48b
Shauna Keating <shauna@truss.works>
2020-10-12 19:24:17 +0000

    adds css module content and sets up stories file

---
+++ src/components/Customer/Containers/SectionWrapper.stories.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import { SectionWrapperComponent as SectionWrapper } from './SectionWrapper';
+
+export default {
+  title: 'Customer Components | Section Wrapper',
+};
+
+export const SectionWrapper = () => <SectionWrapper />;
