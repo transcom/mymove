62a0e649ea207347d718a965b0d8974d9a0c06e6
Shauna Keating <shauna@truss.works>
2020-10-12 19:31:37 +0000

    adds in content of section wrapper component

---
+++ src/components/Customer/Containers/SectionWrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './SectionWrapper.module.scss';
+
+const cx = classNames.bind(styles);
+
+const SectionWrapper = () => (
+  <div className={cx('container-co')}>
+  </div>
+);
+
+export default SectionWrapper;
