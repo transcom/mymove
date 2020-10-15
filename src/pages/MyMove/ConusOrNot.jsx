779a5a77db75e8083883b6d41aa3e6aead394e2f
Arianna Kellogg <ari@truss.works>
2020-10-15 19:13:05 +0000

    MB-4351 using cardText property to show helper text

--- src/pages/MyMove/ConusOrNot.jsx
+++ src/pages/MyMove/ConusOrNot.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Radio } from '@trussworks/react-uswds';
 import { func, PropTypes } from 'prop-types';
 
 import SelectableCard from 'components/Customer/SelectableCard';
@@ -11,53 +10,37 @@ import { CONUS_STATUS } from 'shared/constants';
 export class ConusOrNot extends Component {
   render() {
     const { setLocation, conusStatus } = this.props;
-    const selectableCardDefaultProps = {
-      name: 'OCONUS',
-    };
+    const selectableCardDefaultProps = {};
+    const oconusCardText = `Starts or ends in Alaska, Hawaii, or International locations`;
+    /* 'Starts or ends in Alaska, Hawaii, or International locations'
+        BOLD 'MilMove does not support OCONUS moves yet.' BOLD
+        'Contact your current transportation office to set up your move.'; */
 
     return (
       <div className="grid-row">
         <div className="grid-col">
           <h1 className="sm-heading">Where are you moving?</h1>
-          <Radio
+          <SelectableCard
+            {...selectableCardDefaultProps} // eslint-disable-line
             id={CONUS_STATUS.CONUS}
             label="CONUS"
             value={CONUS_STATUS.CONUS}
-            name="conusStatus"
             onChange={(e) => setLocation(e.target.value)}
+            name="conusStatus"
             checked={conusStatus === CONUS_STATUS.CONUS}
+            cardText="Starts and ends in the continental US"
           />
-          <span className="usa-hint" id="conusStatus">
-            Starts and ends in the continental US
-          </span>
-          <Radio
+          <SelectableCard
+            {...selectableCardDefaultProps} // eslint-disable-line
             id={CONUS_STATUS.OCONUS}
             label="OCONUS"
             value={CONUS_STATUS.OCONUS}
             onChange={(e) => setLocation(e.target.value)}
             name="conusStatus"
             checked={conusStatus === CONUS_STATUS.OCONUS}
+            disabled
+            cardText={oconusCardText}
           />
-          <span className="usa-hint" id="conusStatus">
-            Starts or ends in Alaska, Hawaii, or International locations
-          </span>
-          <div>
-            <SelectableCard
-              {...selectableCardDefaultProps} // eslint-disable-line
-              label="CONUS"
-            />
-          </div>
-          <div>
-            <SelectableCard
-              {...selectableCardDefaultProps} // eslint-disable-line
-              label="OCONUS"
-            />
-          </div>
-          {conusStatus === CONUS_STATUS.OCONUS && (
-            <div>
-              MilMove does not support OCONUS moves yet. Contact your current transportation office to set up your move.
-            </div>
-          )}
         </div>
       </div>
     );
