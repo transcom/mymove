1c6e3c2f26f10a60e51dcb40eed7bdae1ed21f3e
Ron Cruz <Ronolibert@gmail.com>
2020-10-15 15:06:14 +0000

    Uncomment tests

--- pkg/services/mto_shipment/mto_shipment_creator_test.go
+++ pkg/services/mto_shipment/mto_shipment_creator_test.go
@@ -3,6 +3,8 @@ package mtoshipment
 import (
 	"testing"
 
+	"github.com/transcom/mymove/pkg/unit"
+
 	mtoserviceitem "github.com/transcom/mymove/pkg/services/mto_service_item"
 
 	"github.com/gobuffalo/pop"
@@ -36,7 +38,7 @@ func (t *testMTOShipmentQueryBuilder) Transaction(fn func(tx *pop.Connection) er
 }
 
 func (suite *MTOShipmentServiceSuite) TestCreateMTOShipmentRequest() {
-	//mtoShipment := testdatagen.MakeDefaultMTOShipment(suite.DB())
+	mtoShipment := testdatagen.MakeDefaultMTOShipment(suite.DB())
 	builder := query.NewQueryBuilder(suite.DB())
 	createNewBuilder := func(db *pop.Connection) createMTOShipmentQueryBuilder {
 		return builder
@@ -52,88 +54,88 @@ func (suite *MTOShipmentServiceSuite) TestCreateMTOShipmentRequest() {
 	}
 
 	// Invalid ID fields set
-	//suite.T().Run("invalid IDs found", func(t *testing.T) {
-	//	// The default shipment created will have IDs filled in for subobjects, but let's make sure one is set anyway:
-	//	moveTaskOrderID := mtoShipment.MoveTaskOrderID
-	//	if mtoShipment.PickupAddress != nil && mtoShipment.PickupAddress.ID != uuid.Nil {
-	//		mtoShipment.PickupAddress.ID = moveTaskOrderID
-	//	}
-	//	createdShipment, err := creator.CreateMTOShipment(&mtoShipment, nil)
-	//
-	//	suite.Nil(createdShipment)
-	//	suite.Error(err)
-	//	suite.IsType(services.InvalidInputError{}, err)
-	//	invalidErr := err.(services.InvalidInputError)
-	//	suite.NotNil(invalidErr.ValidationErrors)
-	//	suite.NotEmpty(invalidErr.ValidationErrors)
-	//})
-	//
-	//// Happy path
-	//suite.T().Run("If the shipment is created successfully it should be returned", func(t *testing.T) {
-	//	mtoShipment := clearShipmentIDFields(&mtoShipment)
-	//	serviceItemsList := models.MTOServiceItems{}
-	//
-	//	createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
-	//
-	//	suite.NoError(err)
-	//	suite.NotNil(createdShipment)
-	//	suite.Equal(models.MTOShipmentStatusDraft, createdShipment.Status)
-	//})
-	//
-	//suite.T().Run("If the shipment is created successfully with submitted status it should be returned", func(t *testing.T) {
-	//	mtoShipment := clearShipmentIDFields(&mtoShipment)
-	//	mtoShipment.Status = models.MTOShipmentStatusSubmitted
-	//	serviceItemsList := models.MTOServiceItems{}
-	//
-	//	createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
-	//
-	//	suite.NoError(err)
-	//	suite.NotNil(createdShipment)
-	//	suite.Equal(models.MTOShipmentStatusSubmitted, createdShipment.Status)
-	//})
-	//
-	//suite.T().Run("If the shipment has mto service items", func(t *testing.T) {
-	//	testdatagen.MakeReService(suite.DB(), testdatagen.Assertions{
-	//		ReService: models.ReService{
-	//			Code: models.ReServiceCodeDDSHUT,
-	//			Name: "ReServiceCodeDDSHUT",
-	//		},
-	//	})
-	//
-	//	testdatagen.MakeReService(suite.DB(), testdatagen.Assertions{
-	//		ReService: models.ReService{
-	//			Code: models.ReServiceCodeDOFSIT,
-	//			Name: "ReServiceCodeDOFSIT",
-	//		},
-	//	})
-	//
-	//	serviceItemsList := []models.MTOServiceItem{
-	//		{
-	//			MoveTaskOrderID: mtoShipment.MoveTaskOrder.ID,
-	//			MoveTaskOrder:   mtoShipment.MoveTaskOrder,
-	//			ReService: models.ReService{
-	//				Code: models.ReServiceCodeDDSHUT,
-	//			},
-	//		},
-	//		{
-	//			MoveTaskOrderID: mtoShipment.MoveTaskOrder.ID,
-	//			MoveTaskOrder:   mtoShipment.MoveTaskOrder,
-	//			ReService: models.ReService{
-	//				Code: models.ReServiceCodeDOFSIT,
-	//			},
-	//		},
-	//	}
-	//
-	//	mtoShipment := clearShipmentIDFields(&mtoShipment)
-	//	weight := unit.Pound(2)
-	//	mtoShipment.PrimeEstimatedWeight = &weight // for DDSHUT service item type
-	//	createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
-	//
-	//	suite.NoError(err)
-	//	suite.NotNil(createdShipment)
-	//	suite.NotNil(createdShipment.MTOServiceItems, "Service Items are empty")
-	//	suite.Equal(createdShipment.MTOServiceItems[0].MTOShipmentID, &createdShipment.ID, "Service items are not the same")
-	//})
+	suite.T().Run("invalid IDs found", func(t *testing.T) {
+		// The default shipment created will have IDs filled in for subobjects, but let's make sure one is set anyway:
+		moveTaskOrderID := mtoShipment.MoveTaskOrderID
+		if mtoShipment.PickupAddress != nil && mtoShipment.PickupAddress.ID != uuid.Nil {
+			mtoShipment.PickupAddress.ID = moveTaskOrderID
+		}
+		createdShipment, err := creator.CreateMTOShipment(&mtoShipment, nil)
+
+		suite.Nil(createdShipment)
+		suite.Error(err)
+		suite.IsType(services.InvalidInputError{}, err)
+		invalidErr := err.(services.InvalidInputError)
+		suite.NotNil(invalidErr.ValidationErrors)
+		suite.NotEmpty(invalidErr.ValidationErrors)
+	})
+
+	// Happy path
+	suite.T().Run("If the shipment is created successfully it should be returned", func(t *testing.T) {
+		mtoShipment := clearShipmentIDFields(&mtoShipment)
+		serviceItemsList := models.MTOServiceItems{}
+
+		createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
+
+		suite.NoError(err)
+		suite.NotNil(createdShipment)
+		suite.Equal(models.MTOShipmentStatusDraft, createdShipment.Status)
+	})
+
+	suite.T().Run("If the shipment is created successfully with submitted status it should be returned", func(t *testing.T) {
+		mtoShipment := clearShipmentIDFields(&mtoShipment)
+		mtoShipment.Status = models.MTOShipmentStatusSubmitted
+		serviceItemsList := models.MTOServiceItems{}
+
+		createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
+
+		suite.NoError(err)
+		suite.NotNil(createdShipment)
+		suite.Equal(models.MTOShipmentStatusSubmitted, createdShipment.Status)
+	})
+
+	suite.T().Run("If the shipment has mto service items", func(t *testing.T) {
+		testdatagen.MakeReService(suite.DB(), testdatagen.Assertions{
+			ReService: models.ReService{
+				Code: models.ReServiceCodeDDSHUT,
+				Name: "ReServiceCodeDDSHUT",
+			},
+		})
+
+		testdatagen.MakeReService(suite.DB(), testdatagen.Assertions{
+			ReService: models.ReService{
+				Code: models.ReServiceCodeDOFSIT,
+				Name: "ReServiceCodeDOFSIT",
+			},
+		})
+
+		serviceItemsList := []models.MTOServiceItem{
+			{
+				MoveTaskOrderID: mtoShipment.MoveTaskOrder.ID,
+				MoveTaskOrder:   mtoShipment.MoveTaskOrder,
+				ReService: models.ReService{
+					Code: models.ReServiceCodeDDSHUT,
+				},
+			},
+			{
+				MoveTaskOrderID: mtoShipment.MoveTaskOrder.ID,
+				MoveTaskOrder:   mtoShipment.MoveTaskOrder,
+				ReService: models.ReService{
+					Code: models.ReServiceCodeDOFSIT,
+				},
+			},
+		}
+
+		mtoShipment := clearShipmentIDFields(&mtoShipment)
+		weight := unit.Pound(2)
+		mtoShipment.PrimeEstimatedWeight = &weight // for DDSHUT service item type
+		createdShipment, err := creator.CreateMTOShipment(mtoShipment, serviceItemsList)
+
+		suite.NoError(err)
+		suite.NotNil(createdShipment)
+		suite.NotNil(createdShipment.MTOServiceItems, "Service Items are empty")
+		suite.Equal(createdShipment.MTOServiceItems[0].MTOShipmentID, &createdShipment.ID, "Service items are not the same")
+	})
 
 	suite.T().Run("If the move already has a submitted NTS shipment, it should return a validation error", func(t *testing.T) {
 		ntsShipment := testdatagen.MakeMTOShipment(suite.DB(), testdatagen.Assertions{
