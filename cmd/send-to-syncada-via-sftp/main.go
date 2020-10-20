0f09156675041775996546af0186c98d9b1d4964
Harsha Murthy <harsha@truss.works>
2020-10-20 15:05:20 +0000

    Remve SSH host key check

--- cmd/send-to-syncada-via-sftp/main.go
+++ cmd/send-to-syncada-via-sftp/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -51,39 +50,6 @@ func initFlags(flag *pflag.FlagSet) {
 	flag.SortFlags = false
 }
 
-func getHostKey(host string) ssh.PublicKey {
-	// parse OpenSSH known_hosts file
-	// ssh or use ssh-keyscan to get initial key
-	file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var hostKey ssh.PublicKey
-	for scanner.Scan() {
-		fields := strings.Split(scanner.Text(), " ")
-		if len(fields) != 3 {
-			continue
-		}
-		if strings.Contains(fields[0], host) {
-			var err error
-			hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
-			if err != nil {
-				log.Fatalf("error parsing %q: %v", fields[2], err)
-			}
-			break
-		}
-	}
-
-	if hostKey == nil {
-		log.Fatalf("no hostkey found for %s", host)
-	}
-
-	return hostKey
-}
-
 func main() {
 	flag := pflag.CommandLine
 	initFlags(flag)
@@ -121,16 +87,13 @@ func main() {
 	port := v.GetString(cli.SyncadaSFTPPortFlag)
 	syncadaInboundDirectory := v.GetString(cli.SyncadaSFTPInboundDirectoryFlag)
 
-	// get host public key
-	hostKey := getHostKey(remote)
-
 	config := &ssh.ClientConfig{
 		User: userID,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
 		// HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		HostKeyCallback: ssh.FixedHostKey(hostKey),
+		// HostKeyCallback: ssh.FixedHostKey(hostKey),
 	}
 
 	// connect
