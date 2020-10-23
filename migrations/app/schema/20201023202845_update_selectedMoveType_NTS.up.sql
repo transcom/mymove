5f687b0b0060140e71ff99961bd304579a372680
Jeri Sommers 🐍 <sojeri@truss.works>
2020-10-23 20:42:27 +0000

    add migration incl column comment update to reflect new NTS raw value for move.selectedMoveType

---
+++ migrations/app/schema/20201023202845_update_selectedMoveType_NTS.up.sql
@@ -0,0 +1,8 @@
+UPDATE
+    moves
+SET
+    selected_move_type = 'HHG_INTO_NTS_DOMESTIC'
+WHERE
+    selected_move_type = 'NTS';
+
+COMMENT ON COLUMN moves.selected_move_type IS 'The type of Move the customer is choosing. Allowed values are HHG, PPM, UB, POV, HHG_INTO_NTS_DOMESTIC, HHG_OUTOF_NTS_DOMESTIC HHG_PPM (but only HHG and PPM appear to be used currently).';
