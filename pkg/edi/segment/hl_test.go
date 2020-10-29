58c3be5154fe35db9b5e066c8366588ee550ee47
John Gedeon <john@truss.works>
2020-10-29 23:22:04 +0000

    Add test for min and max of HierarchicalIDNumber

--- pkg/edi/segment/hl_test.go
+++ pkg/edi/segment/hl_test.go
@@ -28,4 +28,22 @@ func (suite *SegmentSuite) TestValidateHL() {
 		suite.ValidateError(err, "HierarchicalLevelCode", "oneof")
 		suite.ValidateErrorLen(err, 3)
 	})
+
+	suite.T().Run("validate failure 2", func(t *testing.T) {
+		hl := validHL
+		hl.HierarchicalIDNumber = "" // alphanum takes precidence over min
+
+		err := suite.validator.Struct(hl)
+		suite.ValidateError(err, "HierarchicalIDNumber", "alphanum")
+		suite.ValidateErrorLen(err, 1)
+	})
+
+	suite.T().Run("validate failure 3", func(t *testing.T) {
+		hl := validHL
+		hl.HierarchicalIDNumber = "0123456789ABCDF" // max
+
+		err := suite.validator.Struct(hl)
+		suite.ValidateError(err, "HierarchicalIDNumber", "max")
+		suite.ValidateErrorLen(err, 1)
+	})
 }
