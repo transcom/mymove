6b5753cc3c022d2215984c6ca8db5e4832415aed
Donald Thai <donald@truss.works>
2020-10-30 09:47:42 -0700

    define move status options

---
+++ src/constants/queues.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const MOVE_STATUS_OPTIONS = {
+  'New move': 'New move',
+  'Approvals requested': 'Approvals requested',
+  'Move approved': 'Move approved',
+};
