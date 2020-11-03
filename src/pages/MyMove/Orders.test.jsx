b19a904c8668d273b27867871e4bce23fa68219d
Suzanne Rozier <suz@truss.works>
2020-11-03 15:55:28 +0000

    Clean up test

--- src/pages/MyMove/Orders.test.jsx
+++ src/pages/MyMove/Orders.test.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-// eslint-disable-next-line import/no-named-as-default
-import Orders from './Orders';
+import ConnectedOrders from './Orders';
 
 import { MockProviders } from 'testUtils';
 
 describe('Orders page', () => {
-  it('renders', () => {});
-  const initialState = {
-    swaggerInternal: {
-      spec: {
-        definitions: { CreateUpdateOrders: { properties: { orders_type: { enum: [], 'x-display-value': [] } } } },
+  it('renders', () => {
+    const initialState = {
+      swaggerInternal: {
+        spec: {
+          definitions: { CreateUpdateOrders: { properties: { orders_type: { enum: [], 'x-display-value': [] } } } },
+        },
       },
-    },
-  };
-  const wrapper = mount(
-    <MockProviders initialState={initialState} initialEntries={['/']}>
-      <Orders pages={[]} pageKey="" updateOrders={jest.fn()} />
-    </MockProviders>,
-  );
-  expect(wrapper.length).toEqual(1);
+      serviceMember: {
+        currentServiceMember: {
+          id: 'testServiceMember123',
+        },
+      },
+    };
+    const wrapper = mount(
+      <MockProviders initialState={initialState} initialEntries={['/']}>
+        <ConnectedOrders pages={[]} pageKey="" updateOrders={jest.fn()} />
+      </MockProviders>,
+    );
+    expect(wrapper.length).toEqual(1);
+  });
 });
