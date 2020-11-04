ba5139b635c93a4095d17fea2328308bfbdfa89d
Arianna Kellogg <ari@truss.works>
2020-11-04 22:20:54 +0000

    MB-4267 add client test checking for existence of HelperSubmittedPPM

--- src/pages/MyMove/Home/index.test.jsx
+++ src/pages/MyMove/Home/index.test.jsx
@@ -119,6 +119,10 @@ describe('Home component', () => {
       it('renders the SubmittedMove helper', () => {
         expect(wrapper.find('HelperSubmittedMove').exists()).toBe(true);
       });
+
+      it('renders the SubmittedPPM helper', () => {
+        expect(wrapper.find('HelperSubmittedPPM').exists()).toBe(true);
+      });
     });
 
     describe('for HHG moves', () => {
