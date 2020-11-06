b250e30f369919d62f46f93512873b19b536c024
Shimona Carvalho <shimona@truss.works>
2020-11-06 19:02:02 +0000

    Tidied up some comments

--- cmd/webhook-client/webhook/webhook_test.go
+++ cmd/webhook-client/webhook/webhook_test.go
@@ -230,10 +230,12 @@ func (suite *WebhookClientTestingSuite) Test_EngineRunInactiveSub() {
 	// notification.Status in the model with SENT or FAILED accordingly
 
 	// EXPECTED BEHAVIOR IN THIS TEST
-	// We're going to make 2 Payment.Update events and one
-	// Payment.Create event. We will have 1 active subscription for Payment.Update
-	// and 1 inactive subscription for Payment.Create.
-	// Therefore we expect only the 2 Payment.Update notifications to get sent.
+	// We're going to make 2 PaymentUpdate events and one
+	// PaymentCreate event.
+	// We will have an active subscription for PaymentUpdate
+	// and a disabled subscription for PaymentCreate.
+	// Therefore we expect only the 2 PaymentUpdate notifications to get SENT.
+	// And the PaymentCreate notification to be SKIPPED.
 
 	// SETUP SCENARIO
 	engine, notifications, subscriptions := setupEngineRun(suite)
