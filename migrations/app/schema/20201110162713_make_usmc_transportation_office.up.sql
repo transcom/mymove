dd3288da43a474d9605b42e1996e096b9214ad9f
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2020-11-10 18:04:30 +0000

    Create migration to insert Camp LeJeune transportation office with USMC GBLOC. Also insert phone line records (copies of the non USMC specific LeJeune entry)

---
+++ migrations/app/schema/20201110162713_make_usmc_transportation_office.up.sql
@@ -0,0 +1,10 @@
+--
+-- Data for Name: transportation_offices; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+INSERT INTO public.transportation_offices VALUES ('ccf50409-9d03-4cac-a931-580649f1647a', 'aa899628-dabb-4724-8e4a-b4579c1550e0', 'Camp LeJeune (USMC)', '0f631e20-4edf-457d-8de8-431b4e17f5ae', 34.668,-77.327866, 'Monday; Tuesday; Wednesday and Friday 0730 -1600; Thursday 0730 - 1500', 'Walk-In Help; Briefings; Computer Lab; Appointments; QA Inspections', NULL, '2020-11-10 17:47:34.148582', '2020-11-10 17:47:34.148582', 'USMC');
+--
+-- Data for Name: office_emails; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+INSERT INTO public.office_phone_lines VALUES ('c20bbee8-72a8-4510-9dcb-20757e05ad36', 'ccf50409-9d03-4cac-a931-580649f1647a', '(910) 451-2377', NULL, false, 'voice', '2020-11-10 17:47:34.148582', '2020-11-10 17:47:34.148582');
+INSERT INTO public.office_phone_lines VALUES ('3f677f42-5191-4548-b50c-b94a3a48d99c', 'ccf50409-9d03-4cac-a931-580649f1647a', '750-4377', NULL, true, 'voice', '2020-11-10 17:47:34.148582', '2020-11-10 17:47:34.148582');
+
