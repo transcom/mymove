01591444379006cf2d921e79e2088a282ac6fc0e
Donald Thai <donald@truss.works>
2020-11-11 00:32:27 +0000

    setup payment request queue for pagination

--- src/pages/Office/PaymentRequestQueue/PaymentRequestQueue.jsx
+++ src/pages/Office/PaymentRequestQueue/PaymentRequestQueue.jsx
@@ -103,15 +103,15 @@ const columns = [
 
 const PaymentRequestQueue = ({ history }) => {
   const [paramFilters, setParamFilters] = useState([]);
-  const [selectedPage, setSelectedPage] = useState(1);
-  const [controlledPerPage, setControlledPerPage] = useState(20);
-  const [controlledPageCount, setControlledPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPageSize, setCurrentPageSize] = useState(20);
+  const [pageCount, setPageCount] = React.useState(0);
 
   const {
-    queuePaymentRequestsResult: { perPage = 20, totalCount = 0, queuePaymentRequests = [] },
+    queuePaymentRequestsResult: { totalCount = 0, queuePaymentRequests = [], page = 1, perPage = 20 },
     isLoading,
     isError,
-  } = usePaymentRequestQueueQueries({ filters: paramFilters, page: selectedPage, perPage: controlledPerPage });
+  } = usePaymentRequestQueueQueries({ filters: paramFilters, currentPage, currentPageSize });
 
   // react-table setup below
 
@@ -133,26 +133,20 @@ const PaymentRequestQueue = ({ history }) => {
     canPreviousPage,
     canNextPage,
     pageOptions,
-    pageCount,
     gotoPage,
     nextPage,
     previousPage,
     setPageSize,
-    showPagination,
     state: { filters, pageIndex, pageSize },
   } = useTable(
     {
       columns: tableColumns,
       data: tableData,
-      initialState: { hiddenColumns: ['id'], pageIndex: 0, pageSize: perPage },
+      initialState: { hiddenColumns: ['id'], pageSize: perPage, pageIndex: page - 1 },
       defaultColumn, // Be sure to pass the defaultColumn option
       manualFilters: true,
-      manualPagination: true, // Tell the usePagination
-      // hook that we'll handle our own data fetching
-      // This means we'll also have to provide our own
-      // pageCount.
-      pageCount: controlledPageCount,
-      showPagination: true,
+      manualPagination: true,
+      pageCount,
     },
     useFilters,
     usePagination,
@@ -162,11 +156,11 @@ const PaymentRequestQueue = ({ history }) => {
   useEffect(() => {
     if (!isLoading && !isError) {
       setParamFilters(filters);
-      setSelectedPage(pageIndex + 1);
-      setControlledPerPage(pageSize);
-      setControlledPageCount(Math.ceil(totalCount / pageSize));
+      setCurrentPage(pageIndex + 1);
+      setCurrentPageSize(pageSize);
+      setPageCount(Math.ceil(totalCount / pageSize));
     }
-  }, [filters, isLoading, isError, pageIndex, totalCount, pageSize]);
+  }, [filters, pageIndex, pageSize, isLoading, isError, totalCount]);
 
   if (isLoading) return <LoadingPlaceholder />;
   if (isError) return <SomethingWentWrong />;
@@ -179,42 +173,24 @@ const PaymentRequestQueue = ({ history }) => {
     <GridContainer containerSize="widescreen" className={styles.PaymentRequestQueue}>
       <h1>{`Payment requests (${totalCount})`}</h1>
       <div className={styles.tableContainer}>
-        <>
-          <pre>
-            <code>
-              {JSON.stringify(
-                {
-                  pageIndex,
-                  pageSize,
-                  pageCount,
-                  pageOptions,
-                  canNextPage,
-                  canPreviousPage,
-                },
-                null,
-                2,
-              )}
-            </code>
-          </pre>
-        </>
         <Table
           handleClick={handleClick}
-          showPagination={showPagination}
+          gotoPage={gotoPage}
+          setPageSize={setPageSize}
+          nextPage={nextPage}
+          previousPage={previousPage}
           getTableProps={getTableProps}
           getTableBodyProps={getTableBodyProps}
           headerGroups={headerGroups}
           rows={rows}
           prepareRow={prepareRow}
+          showPagination
           canPreviousPage={canPreviousPage}
           canNextPage={canNextPage}
-          pageOptions={pageOptions}
-          pageCount={pageCount}
-          gotoPage={gotoPage}
-          nextPage={nextPage}
-          previousPage={previousPage}
-          setPageSize={setPageSize}
-          pageSize={pageSize}
           pageIndex={pageIndex}
+          pageSize={pageSize}
+          pageCount={pageCount}
+          pageOptions={pageOptions}
         />
       </div>
     </GridContainer>
