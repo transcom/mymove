b68fd50dab2da7444d9a261cd2d32760001c280e
Duncan Spencer <duncan@truss.works>
2020-11-12 22:43:22 +0000

    change naming to not reuse cte names

--- migrations/app/schema/20201112222339_create_user_records_for_office_and_admin_users_without.up.sql
+++ migrations/app/schema/20201112222339_create_user_records_for_office_and_admin_users_without.up.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Finds office users who have never signed in so we must create a user record
 -- for them that can be updated on first sign in
-WITH never_signed_in AS (
+WITH office_never_signed_in AS (
     SELECT *
     FROM office_users
              LEFT JOIN users ON office_users.email = users.login_gov_email
@@ -9,24 +9,24 @@ WITH never_signed_in AS (
 )
 INSERT INTO users (id, login_gov_email, created_at, updated_at)
 SELECT uuid_generate_v4(), email, now(), now()
-FROM never_signed_in;
+FROM office_never_signed_in;
 
 -- Now that we've created the user record, update the office_user record with
 -- the new user.id
-WITH associate_user AS (
+WITH office_associate_user AS (
     SELECT users.*
     FROM office_users
     JOIN users ON office_users.email = users.login_gov_email
     WHERE office_users.user_id IS NULL
 )
 UPDATE office_users
-SET user_id = associate_user.id
-FROM associate_user
-WHERE office_users.email = associate_user.login_gov_email;
+SET user_id = office_associate_user.id
+FROM office_associate_user
+WHERE office_users.email = office_associate_user.login_gov_email;
 
 -- Finds admin users who have never signed in so we must create a user record
 -- for them that can be updated on first sign in
-WITH never_signed_in AS (
+WITH admin_never_signed_in AS (
     SELECT *
     FROM admin_users
              LEFT JOIN users ON admin_users.email = users.login_gov_email
@@ -34,17 +34,17 @@ WITH never_signed_in AS (
 )
 INSERT INTO users (id, login_gov_email, created_at, updated_at)
 SELECT uuid_generate_v4(), email, now(), now()
-FROM never_signed_in;
+FROM admin_never_signed_in;
 
 -- Now that we've created the user record, update the admin_user record with
 -- the new user.id
-WITH associate_user AS (
+WITH admin_associate_user AS (
     SELECT users.*
     FROM admin_users
     JOIN users ON admin_users.email = users.login_gov_email
     WHERE admin_users.user_id IS NULL
 )
 UPDATE admin_users
-SET user_id = associate_user.id
-FROM associate_user
-WHERE admin_users.email = associate_user.login_gov_email;
+SET user_id = admin_associate_user.id
+FROM admin_associate_user
+WHERE admin_users.email = admin_associate_user.login_gov_email;
