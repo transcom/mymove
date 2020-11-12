bb5915453ee4609d25580552b9fff8fe03ceca11
Duncan Spencer <duncan@truss.works>
2020-11-12 22:31:09 +0000

    Add migration to create user records for office and admin users without them

---
+++ migrations/app/schema/20201112222339_create_user_records_for_office_and_admin_users_without.up.sql
@@ -0,0 +1,50 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- Finds office users who have never signed in so we must create a user record
+-- for them that can be updated on first sign in
+WITH never_signed_in AS (
+    SELECT *
+    FROM office_users
+             LEFT JOIN users ON office_users.email = users.login_gov_email
+    WHERE users.login_gov_email IS NULL
+)
+INSERT INTO users (id, login_gov_email, created_at, updated_at)
+SELECT uuid_generate_v4(), email, now(), now()
+FROM never_signed_in;
+
+-- Now that we've created the user record, update the office_user record with
+-- the new user.id
+WITH associate_user AS (
+    SELECT users.*
+    FROM office_users
+    JOIN users ON office_users.email = users.login_gov_email
+    WHERE office_users.user_id IS NULL
+)
+UPDATE office_users
+SET user_id = associate_user.id
+FROM associate_user
+WHERE office_users.email = associate_user.login_gov_email;
+
+-- Finds admin users who have never signed in so we must create a user record
+-- for them that can be updated on first sign in
+WITH never_signed_in AS (
+    SELECT *
+    FROM admin_users
+             LEFT JOIN users ON admin_users.email = users.login_gov_email
+    WHERE users.login_gov_email IS NULL
+)
+INSERT INTO users (id, login_gov_email, created_at, updated_at)
+SELECT uuid_generate_v4(), email, now(), now()
+FROM never_signed_in;
+
+-- Now that we've created the user record, update the admin_user record with
+-- the new user.id
+WITH associate_user AS (
+    SELECT users.*
+    FROM admin_users
+    JOIN users ON admin_users.email = users.login_gov_email
+    WHERE admin_users.user_id IS NULL
+)
+UPDATE admin_users
+SET user_id = associate_user.id
+FROM associate_user
+WHERE admin_users.email = associate_user.login_gov_email;
