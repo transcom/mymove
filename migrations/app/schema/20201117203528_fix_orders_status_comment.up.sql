e434adf1d0fb6625f76d2798b8366465af54cfa5
lindsay t <techells22@gmail.com>
2020-11-17 21:31:33 +0000

    update wording

--- migrations/app/schema/20201117203528_fix_orders_status_comment.up.sql
+++ migrations/app/schema/20201117203528_fix_orders_status_comment.up.sql
@@ -1,1 +1,1 @@
-COMMENT ON COLUMN orders.status IS 'The status of the orders. Values can be: DRAFT, SUBMITTED, APPROVED, CANCELED';
+COMMENT ON COLUMN orders.status IS 'The status of the orders. Allowed values are DRAFT, SUBMITTED, APPROVED, CANCELED.';
