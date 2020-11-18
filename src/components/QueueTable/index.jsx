6bf366e9b06e1dfca2085da1f82795577598fc48
Shauna Keating <shauna@truss.works>
2020-11-18 22:04:17 +0000

    deletes custom icons for th document viewer

--- src/components/QueueTable/index.jsx
+++ src/components/QueueTable/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from '@trussworks/react-uswds';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
