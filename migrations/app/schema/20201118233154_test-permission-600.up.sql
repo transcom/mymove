678531b004fbe54a6cbde87414d52045c4c9bdf8
andrea <andrea@truss.works>
2020-11-19 22:58:35 +0000

    deploy migration to experimental env

--- migrations/app/schema/20201118233154_test-permission-600.up.sql
+++ migrations/app/schema/20201118233154_test-permission-600.up.sql
@@ -1,1 +1,8 @@
-COMMENT ON COLUMN orders.status IS 'TESTING CHANGE The status of the orders. Allowed values are DRAFT, SUBMITTED, APPROVED, CANCELED.';
+-- Test migration to check for permission file change with a fake office
+--
+-- Data for Name: addresses; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.transportation_offices
+VALUES
+	('6b083e9b-acfc-4c7a-8e04-2e442e0785b5', NULL, 'AAA AFB', '2c3209df-6568-476b-82e0-df762c040c03', 39.8137283, -84.0537415, 'Monday - Friday 0730-1630; Closed Weekends and Federal Holidays', 'Walk-In Help; Briefings; Appointments; QA Inspections', NULL, '2020-11-19 14:27:38.564964', '2020-11-19 14:27:38.564964');
