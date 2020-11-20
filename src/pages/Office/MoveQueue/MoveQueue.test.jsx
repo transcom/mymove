da9242e0622ac77c136c26c11400df9e0c81b910
Donald Thai <donald@truss.works>
2020-11-20 00:26:16 +0000

    add mock for useUserQueries

--- src/pages/Office/MoveQueue/MoveQueue.test.jsx
+++ src/pages/Office/MoveQueue/MoveQueue.test.jsx
@@ -6,6 +6,15 @@ import MoveQueue from './MoveQueue';
 import { MockProviders } from 'testUtils';
 
 jest.mock('hooks/queries', () => ({
+  useUserQueries: () => {
+    return {
+      isLoading: false,
+      isError: false,
+      data: {
+        office_user: { transportation_office: { gbloc: 'TEST' } },
+      },
+    };
+  },
   useMovesQueueQueries: () => {
     return {
       isLoading: false,
