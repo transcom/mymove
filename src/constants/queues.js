fba87cfef6f006dae78db6801836ee3fc0f345ce
Donald Thai <donald@truss.works>
2020-11-23 21:51:58 +0000

    remove unneeded options

--- src/constants/queues.js
+++ src/constants/queues.js
@@ -15,15 +15,6 @@ export const BRANCH_OPTIONS = [
   { value: '', label: 'All' },
   { value: 'ARMY', label: 'Army' },
   { value: 'NAVY', label: 'Navy' },
-  { value: 'MARINES', label: 'Marine Corps' },
-  { value: 'AIR_FORCE', label: 'Air Force' },
-  { value: 'COAST_GUARD', label: 'Coast Guard' },
-];
-
-export const BRANCH_OPTIONS_NO_MARINES = [
-  { value: '', label: 'All' },
-  { value: 'ARMY', label: 'Army' },
-  { value: 'NAVY', label: 'Navy' },
   { value: 'AIR_FORCE', label: 'Air Force' },
   { value: 'COAST_GUARD', label: 'Coast Guard' },
 ];
