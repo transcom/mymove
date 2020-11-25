027f462361eb4acd1923ba4cf719d77346560e31
Donald Thai <donald@truss.works>
2020-11-25 19:22:36 +0000

    refactor entitlement test for new expected values

--- pkg/models/entitlements_test.go
+++ pkg/models/entitlements_test.go
@@ -2,25 +2,21 @@ package models_test
 
 import (
 	"github.com/transcom/mymove/pkg/models"
+	"testing"
 )
 
 func (suite *ModelSuite) TestGetEntitlementWithValidValues() {
 	E1 := models.ServiceMemberRankE1
 
-	// When: E1 has dependents and spouse gear
-	E1FullLoad, err := models.GetEntitlement(E1, true, true)
-	suite.NoError(err)
-	suite.Assertions.Equal(10500, E1FullLoad)
-	// When: E1 doesn't have dependents or spouse gear
-	E1Solo, err := models.GetEntitlement(E1, false, false)
-	suite.NoError(err)
-	suite.Assertions.Equal(7000, E1Solo)
-	// When: E1 doesn't have dependents but has spouse gear - impossible state
-	E1FakeSpouse, err := models.GetEntitlement(E1, false, true)
-	suite.NoError(err)
-	suite.Assertions.Equal(7000, E1FakeSpouse)
-	// When: E1 has dependents but no spouse gear
-	E1DivorcedWithKids, err := models.GetEntitlement(E1, true, false)
-	suite.NoError(err)
-	suite.Assertions.Equal(10000, E1DivorcedWithKids)
+	suite.T().Run("E1 with dependents", func(t *testing.T) {
+		E1FullLoad, err := models.GetEntitlement(E1, true)
+		suite.NoError(err)
+		suite.Assertions.Equal(8000, E1FullLoad)
+	})
+
+	suite.T().Run("E1 without dependents", func(t *testing.T) {
+		E1Solo, err := models.GetEntitlement(E1, false)
+		suite.NoError(err)
+		suite.Assertions.Equal(5000, E1Solo)
+	})
 }
