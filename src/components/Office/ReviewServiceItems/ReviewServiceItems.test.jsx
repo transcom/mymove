4e90ba2d38028cc60585161ec9a1a985c6a9f88b
noelle barber <noelle@truss.works>
2020-11-25 16:01:36 +0000

    pass clear selection client test

--- src/components/Office/ReviewServiceItems/ReviewServiceItems.test.jsx
+++ src/components/Office/ReviewServiceItems/ReviewServiceItems.test.jsx
@@ -387,6 +387,10 @@ describe('ReviewServiceItems component', () => {
     );
     const approvedAmount = componentWithInitialValues.find('[data-testid="approvedAmount"]');
 
+    it('calculates the approved sum for items with initial values', () => {
+      expect(approvedAmount.text()).toEqual('$1,050.25');
+    });
+
     it('can clear approved amount for an item', async () => {
       const approveInput = mountedComponent.find(`input[name="status"][value="APPROVED"]`);
       expect(approveInput.length).toBe(1);
@@ -395,7 +399,6 @@ describe('ReviewServiceItems component', () => {
         approveInput.simulate('change');
       });
       mountedComponent.update();
-      expect(approvedAmount.text()).toEqual('$1,050.25');
 
       const clearSelectionButton = mountedComponent.find('button[data-testid="clearStatusButton"]');
       expect(clearSelectionButton.length).toBe(1);
@@ -405,11 +408,8 @@ describe('ReviewServiceItems component', () => {
       });
       mountedComponent.update();
 
-      expect(approvedAmount.text()).toEqual(0);
-    });
-
-    it('calculates the approved sum for items with initial values', () => {
-      expect(approvedAmount.text()).toEqual('$1,050.25');
+      const clearedAmount = mountedComponent.find('[data-testid="approvedAmount"]');
+      expect(clearedAmount.text()).toEqual('$0.00');
     });
   });
 
