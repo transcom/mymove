7a018837378f8683c7dbf2a330d9635f8522ad43
Shauna Keating <shauna@truss.works>
2020-11-30 20:40:04 +0000

    adds code to cypress plugins index page that premits pa11y and lighthouse to run in the cypress browser

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,3 +15,20 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+
+const {
+  lighthouse,
+  pa11y,
+  prepareAudit
+} = require("cypress-audit");
+
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(), // calling the function is important
+    pa11y: pa11y(), // calling the function is important
+  });
+};
