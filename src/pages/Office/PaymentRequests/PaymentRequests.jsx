9f482e6d0f98ab624ee204cb4dc670cd9d5b23eb
noelle barber <noelle@truss.works>
2020-12-01 19:18:58 +0000

    correct TIO flow tests

--- src/pages/Office/PaymentRequests/PaymentRequests.jsx
+++ src/pages/Office/PaymentRequests/PaymentRequests.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PaymentRequests = () => {
   return (
-    <div className="grid-container-desktop-lg" data-testid="payment-requests">
+    <div className="grid-container-desktop-lg" data-testid="PaymentRequests">
       <h1>Payment requests</h1>
       <div className="container" />
     </div>
