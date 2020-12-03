d2a4c185898fd859abdd19b2fec5dea736c32b1f
noelle barber <noelle@truss.works>
2020-12-03 16:55:43 +0000

    rever review service items flow button

--- src/pages/Office/PaymentRequests/PaymentRequests.jsx
+++ src/pages/Office/PaymentRequests/PaymentRequests.jsx
@@ -1,32 +1,12 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Button } from '@trussworks/react-uswds';
 
-import { MatchShape, HistoryShape } from 'types/router';
-
-const PaymentRequests = ({ history, match }) => {
-  const { moveOrderId } = match.params;
-  const paymentRequestId = history.location.state.detail;
-
-  const handleClick = () => {
-    history.push(`/moves/${moveOrderId}/payment-requests/${paymentRequestId}`);
-  };
-
+const PaymentRequests = () => {
   return (
     <div className="grid-container-desktop-lg" data-testid="PaymentRequests">
       <h1>Payment requests</h1>
-      <div className="container">
-        <Button data-testid="ReviewServiceItems" onClick={handleClick}>
-          Review service items
-        </Button>
-      </div>
+      <div className="container" />
     </div>
   );
 };
 
-PaymentRequests.propTypes = {
-  history: HistoryShape.isRequired,
-  match: MatchShape.isRequired,
-};
-
-export default withRouter(PaymentRequests);
+export default PaymentRequests;
