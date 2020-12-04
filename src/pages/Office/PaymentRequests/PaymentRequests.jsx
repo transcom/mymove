68d7d16379e459b0559f666b2c8f7975bc685fc9
noelle barber <noelle@truss.works>
2020-12-04 17:20:36 +0000

    comment pseudocode

--- src/pages/Office/PaymentRequests/PaymentRequests.jsx
+++ src/pages/Office/PaymentRequests/PaymentRequests.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Button } from '@trussworks/react-uswds';
 
-const PaymentRequests = () => {
+import { MatchShape, HistoryShape } from 'types/router';
+
+const PaymentRequests = ({ history, match }) => {
+  const { paymentRequestID, moveLocator } = match.params;
+  // values.id = payment request id
+
+  const handleClick = () => {
+    history.push(`/moves/${moveLocator}/payment-requests/${paymentRequestID}`);
+  };
+
   return (
     <div className="grid-container-desktop-lg" data-testid="PaymentRequests">
       <h1>Payment requests</h1>
-      <div className="container" />
+      <div className="container">
+        <Button data-testid="ReviewServiceItems" onClick={handleClick}>
+          Review service items
+        </Button>
+      </div>
     </div>
   );
 };
 
-export default PaymentRequests;
+// include an array list to iterate over for all Payment Requests in the move
+// tie appearance of button to status. status == 'APPROVED' && (render the card)
+
+PaymentRequests.propTypes = {
+  history: HistoryShape.isRequired,
+  match: MatchShape.isRequired,
+};
+
+export default withRouter(PaymentRequests);
