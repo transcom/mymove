18eac865fed5be56cf1c8bccf43ed5b41e547677
Kim Allen <kim@truss.works>
2020-12-07 23:43:28 +0000

    Add preset-create-react-app

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.jsx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
 };
