3fb9e8926b866e52dadaab44b3f860f90c91e837
Reggie Riser <reggie@truss.works>
2020-12-08 17:41:23 +0000

    Fixed mixed up field names in comments

--- migrations/app/schema/20201207223002_create_zip3_distances.up.sql
+++ migrations/app/schema/20201207223002_create_zip3_distances.up.sql
@@ -15,9 +15,9 @@ CREATE UNIQUE INDEX zip3_distances_unique_zip3s ON zip3_distances (from_zip3, to
 
 COMMENT ON TABLE zip3_distances IS 'Stores the distances between zip3 pairs; there should only be one record for any zip3 pair, with from_zip3 always alphabetically before to_zip3.';
 COMMENT ON COLUMN zip3_distances.id IS 'UUID that uniquely identifies the record.';
-COMMENT ON COLUMN zip3_distances.from_zip3 IS 'The starting zip3; this should always be alphabetically before from_zip3.';
-COMMENT ON COLUMN zip3_distances.to_zip3 IS 'The ending zip3; this should always be alphabetically after to_zip3.';
-COMMENT ON COLUMN zip3_distances.distance_miles IS 'The distance in miles between the from_zip3 and to_zip3.';
+COMMENT ON COLUMN zip3_distances.from_zip3 IS 'The starting zip3; this should always be alphabetically before to_zip3.';
+COMMENT ON COLUMN zip3_distances.to_zip3 IS 'The ending zip3; this should always be alphabetically after from_zip3.';
+COMMENT ON COLUMN zip3_distances.distance_miles IS 'The distance in miles between from_zip3 and to_zip3.';
 COMMENT ON COLUMN zip3_distances.created_at IS 'Timestamp when the record was first created.';
 COMMENT ON COLUMN zip3_distances.updated_at IS 'Timestamp when the record was first updated.';
 COMMENT ON CONSTRAINT zip3_distances_ordering ON zip3_distances IS 'Ensures that from_zip3 is always alphabetically before to_zip3.';
