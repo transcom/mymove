f48df0bb76fc48fb7eb28c57d16ad9157faf172f
Kim Allen <kim@truss.works>
2020-12-08 01:10:11 +0000

    Add addons we're using to main.js; remove addons.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,8 @@ module.exports = {
   stories: ['../src/**/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
     {
       name: '@storybook/addon-docs',
       options: {
