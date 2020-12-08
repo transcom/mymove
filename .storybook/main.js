4675f37f10af29d7a3cf767cd1541ca67a98a620
Kim Allen <kim@truss.works>
2020-12-08 18:48:10 +0000

    Move new dependencies from dependencies to devDependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,11 @@ module.exports = {
   stories: ['../src/**/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-a11y',
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport',
     {
       name: '@storybook/addon-docs',
       options: {
