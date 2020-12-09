7de4ad3151ef2131c9fd52bb0b625e0aaf183936
Kim Allen <kim@truss.works>
2020-12-09 19:41:05 +0000

    Remove babel-loader and depend on it from webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,16 @@ module.exports = {
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-a11y',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-    '@storybook/addon-links',
-    'storybook-addons-abstract',
-    '@dump247/storybook-state',
     {
       name: '@storybook/addon-docs',
       options: {
         configureJSX: true,
       },
     },
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+    'storybook-addons-abstract',
+    '@dump247/storybook-state',
   ],
 };
