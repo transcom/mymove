00011000c4d5dc9ac52c4601763a91996b9920be
Ron Cruz <Ronolibert@gmail.com>
2020-12-11 21:13:09 +0000

    Convert update move order to transaction and add authorized weight to update payload definition

--- pkg/services/move_order/move_order_updater.go
+++ pkg/services/move_order/move_order_updater.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gobuffalo/validate/v3"
 	"github.com/gofrs/uuid"
 
+	"github.com/transcom/mymove/pkg/etag"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
 	"github.com/transcom/mymove/pkg/services/query"
@@ -34,44 +35,70 @@ func (s *moveOrderUpdater) UpdateMoveOrder(moveOrderID uuid.UUID, eTag string, m
 		return nil, services.NewNotFoundError(moveOrder.ID, "while looking for moveOrder")
 	}
 
-	if moveOrder.OriginDutyStationID != existingOrder.OriginDutyStationID {
-		originDutyStation, fetchErr := models.FetchDutyStation(s.db, *moveOrder.OriginDutyStationID)
-		if fetchErr != nil {
-			return nil, services.NewInvalidInputError(moveOrder.ID, fetchErr, nil, "unable to find origin duty station")
+	transactionError := s.db.Transaction(func(tx *pop.Connection) error {
+		existingETag := etag.GenerateEtag(existingOrder.UpdatedAt)
+		if existingETag != eTag {
+			return query.StaleIdentifierError{StaleIdentifier: eTag}
 		}
-		existingOrder.OriginDutyStationID = moveOrder.OriginDutyStationID
-		existingOrder.OriginDutyStation = &originDutyStation
-	}
 
-	if moveOrder.NewDutyStationID != existingOrder.NewDutyStationID {
-		newDutyStation, fetchErr := models.FetchDutyStation(s.db, *&moveOrder.NewDutyStationID)
-		if fetchErr != nil {
-			return nil, services.NewInvalidInputError(moveOrder.ID, fetchErr, nil, "unable to find destination duty station")
+		//if moveOrder.Entitlement.Au
+		//existingOrder.Entitlement.DBAuthorizedWeight = &weight
+		//err = tx.Save(existingOrder.Entitlement)
+		if err != nil {
+			return err
 		}
-		existingOrder.NewDutyStationID = moveOrder.NewDutyStationID
-		existingOrder.NewDutyStation = newDutyStation
-	}
 
-	existingOrder.IssueDate = moveOrder.IssueDate
-	existingOrder.ReportByDate = moveOrder.ReportByDate
-	existingOrder.OrdersType = moveOrder.OrdersType
-	existingOrder.OrdersTypeDetail = moveOrder.OrdersTypeDetail
-	existingOrder.OrdersNumber = moveOrder.OrdersNumber
-	existingOrder.TAC = moveOrder.TAC
-	existingOrder.SAC = moveOrder.SAC
-	existingOrder.DepartmentIndicator = moveOrder.DepartmentIndicator
 
-	verrs, err := s.builder.UpdateOne(existingOrder, &eTag)
-
-	if verrs != nil && verrs.HasAny() {
-		return nil, services.NewInvalidInputError(moveOrder.ID, err, verrs, "")
-	}
-
-	if err != nil {
-		switch err.(type) {
-		case query.StaleIdentifierError:
-			return &models.Order{}, services.NewPreconditionFailedError(moveOrder.ID, err)
+		if moveOrder.OriginDutyStationID != existingOrder.OriginDutyStationID {
+			originDutyStation, fetchErr := models.FetchDutyStation(s.db, *moveOrder.OriginDutyStationID)
+			if fetchErr != nil {
+				return services.NewInvalidInputError(moveOrder.ID, fetchErr, nil, "unable to find origin duty station")
+			}
+			existingOrder.OriginDutyStationID = moveOrder.OriginDutyStationID
+			existingOrder.OriginDutyStation = &originDutyStation
 		}
+
+		if moveOrder.NewDutyStationID != existingOrder.NewDutyStationID {
+			newDutyStation, fetchErr := models.FetchDutyStation(s.db, *&moveOrder.NewDutyStationID)
+			if fetchErr != nil {
+				return services.NewInvalidInputError(moveOrder.ID, fetchErr, nil, "unable to find destination duty station")
+			}
+			existingOrder.NewDutyStationID = moveOrder.NewDutyStationID
+			existingOrder.NewDutyStation = newDutyStation
+		}
+
+		existingOrder.IssueDate = moveOrder.IssueDate
+		existingOrder.ReportByDate = moveOrder.ReportByDate
+		existingOrder.OrdersType = moveOrder.OrdersType
+		existingOrder.OrdersTypeDetail = moveOrder.OrdersTypeDetail
+		existingOrder.OrdersNumber = moveOrder.OrdersNumber
+		existingOrder.TAC = moveOrder.TAC
+		existingOrder.SAC = moveOrder.SAC
+		existingOrder.DepartmentIndicator = moveOrder.DepartmentIndicator
+
+		verrs, err := s.builder.UpdateOne(existingOrder, &eTag)
+
+		if verrs != nil && verrs.HasAny() {
+			return services.NewInvalidInputError(moveOrder.ID, err, verrs, "")
+		}
+
+		if err != nil {
+			switch err.(type) {
+			case query.StaleIdentifierError:
+				return services.NewPreconditionFailedError(moveOrder.ID, err)
+			}
+		}
+
+		return nil
+	})
+
+
+
+	if transactionError != nil {
+		if t, ok := transactionError.(query.StaleIdentifierError); ok {
+			return nil, services.NewPreconditionFailedError(existingOrder.ID, t)
+		}
+		return nil, services.NewQueryError("moveOrder", transactionError, "")
 	}
 
 	return existingOrder, err
