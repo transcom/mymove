d6fd20490baa3232a86e6a34dd55f6985a61cc87
Suzanne Rozier <suz@truss.works>
2020-12-14 14:50:31 +0000

    Update SB

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,17 @@ module.exports = {
   stories: ['../src/**/*.stories.jsx'],
   addons: [
     // '@storybook/addon-a11y',
-    '@storybook/addon-actions',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
     '@storybook/addon-knobs',
     '@storybook/addon-links',
-    'storybook-addons-abstract',
+    // 'storybook-addons-abstract',
     '@dump247/storybook-state',
   ],
 };
