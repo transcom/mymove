673dbaa72ba0d92cf9e33ba3fa7bdfbf7acbb225
Duncan Spencer <duncan@truss.works>
2020-12-21 22:48:41 +0000

    update getMove spec, handler, and move fetcher service

--- pkg/gen/ghcmessages/move.go
+++ pkg/gen/ghcmessages/move.go
@@ -17,6 +17,14 @@ import (
 // swagger:model Move
 type Move struct {
 
+	// available to prime at
+	// Format: date-time
+	AvailableToPrimeAt *strfmt.DateTime `json:"availableToPrimeAt,omitempty"`
+
+	// contractor Id
+	// Format: uuid
+	ContractorID *strfmt.UUID `json:"contractorId,omitempty"`
+
 	// created at
 	// Format: date-time
 	CreatedAt strfmt.DateTime `json:"created_at,omitempty"`
@@ -28,9 +36,19 @@ type Move struct {
 	// locator
 	Locator string `json:"locator,omitempty"`
 
-	// orders id
+	// orders Id
 	// Format: uuid
-	OrdersID strfmt.UUID `json:"orders_id,omitempty"`
+	OrdersID strfmt.UUID `json:"ordersId,omitempty"`
+
+	// reference Id
+	ReferenceID *string `json:"referenceId,omitempty"`
+
+	// status
+	Status MoveStatus `json:"status,omitempty"`
+
+	// submitted at
+	// Format: date-time
+	SubmittedAt *strfmt.DateTime `json:"submitted_at,omitempty"`
 
 	// updated at
 	// Format: date-time
@@ -40,6 +58,14 @@ type Move struct {
 // Validate validates this move
 func (m *Move) Validate(formats strfmt.Registry) error {
 	var res []error
+
+	if err := m.validateAvailableToPrimeAt(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateContractorID(formats); err != nil {
+		res = append(res, err)
+	}
 
 	if err := m.validateCreatedAt(formats); err != nil {
 		res = append(res, err)
@@ -53,6 +79,14 @@ func (m *Move) Validate(formats strfmt.Registry) error {
 		res = append(res, err)
 	}
 
+	if err := m.validateStatus(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateSubmittedAt(formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.validateUpdatedAt(formats); err != nil {
 		res = append(res, err)
 	}
@@ -60,6 +94,32 @@ func (m *Move) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+func (m *Move) validateAvailableToPrimeAt(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.AvailableToPrimeAt) { // not required
+		return nil
+	}
+
+	if err := validate.FormatOf("availableToPrimeAt", "body", "date-time", m.AvailableToPrimeAt.String(), formats); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Move) validateContractorID(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.ContractorID) { // not required
+		return nil
+	}
+
+	if err := validate.FormatOf("contractorId", "body", "uuid", m.ContractorID.String(), formats); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -95,7 +155,36 @@ func (m *Move) validateOrdersID(formats strfmt.Registry) error {
 		return nil
 	}
 
-	if err := validate.FormatOf("orders_id", "body", "uuid", m.OrdersID.String(), formats); err != nil {
+	if err := validate.FormatOf("ordersId", "body", "uuid", m.OrdersID.String(), formats); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *Move) validateStatus(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.Status) { // not required
+		return nil
+	}
+
+	if err := m.Status.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("status")
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (m *Move) validateSubmittedAt(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.SubmittedAt) { // not required
+		return nil
+	}
+
+	if err := validate.FormatOf("submitted_at", "body", "date-time", m.SubmittedAt.String(), formats); err != nil {
 		return err
 	}
 
