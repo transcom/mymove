8ff8b6c6394c505ef6f8fe6a0518e42917d70bee
Donald Thai <donald@truss.works>
2020-12-22 21:19:38 +0000

    useOrdersDocumentQueries() update to take in locator instead of move order id

--- src/hooks/queries.js
+++ src/hooks/queries.js
@@ -6,6 +6,7 @@ import {
   getMTOShipments,
   getMTOServiceItems,
   getMoveOrder,
+  getMove,
   getMoveTaskOrderList,
   getDocument,
   getMovesQueue,
@@ -18,6 +19,7 @@ import {
   PAYMENT_REQUESTS,
   MTO_SHIPMENTS,
   MTO_SERVICE_ITEMS,
+  MOVES,
   MOVE_ORDERS,
   MOVE_PAYMENT_REQUESTS,
   MOVE_TASK_ORDERS,
@@ -138,9 +140,16 @@ export const useMoveOrderQueries = (moveOrderId) => {
   };
 };
 
-export const useOrdersDocumentQueries = (moveOrderId) => {
+export const useOrdersDocumentQueries = (moveCode) => {
   // Get the orders info so we can get the uploaded_orders_id (which is a document id)
-  const { data: { moveOrders } = {}, ...moveOrderQuery } = useQuery([MOVE_ORDERS, moveOrderId], getMoveOrder);
+  const { data: move, ...moveQuery } = useQuery([MOVES, moveCode], getMove);
+
+  const moveOrderId = move?.ordersId;
+
+  // get orders
+  const { data: { moveOrders } = {}, ...moveOrderQuery } = useQuery([MOVE_ORDERS, moveOrderId], getMoveOrder, {
+    enabled: !!moveOrderId,
+  });
 
   const orders = moveOrders && moveOrders[`${moveOrderId}`];
   // eslint-disable-next-line camelcase
@@ -156,9 +165,10 @@ export const useOrdersDocumentQueries = (moveOrderId) => {
     },
   );
 
-  const { isLoading, isError, isSuccess } = getQueriesStatus([moveOrderQuery, ordersDocumentsQuery]);
+  const { isLoading, isError, isSuccess } = getQueriesStatus([moveQuery, moveOrderQuery, ordersDocumentsQuery]);
 
   return {
+    move,
     moveOrders,
     documents,
     upload,
