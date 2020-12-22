2b72f26d1844fe889dd0f60fd4883649d4a3cc7e
John Gedeon <john@truss.works>
2020-12-22 23:06:28 +0000

    Update test

--- pkg/route/rand_mcnally_distance_test.go
+++ pkg/route/rand_mcnally_distance_test.go
@@ -15,16 +15,16 @@ func (suite *GHCTestSuite) TestRandMcNallyZip3Distance() {
 		suite.Equal(24, distance)
 	})
 
-	suite.T().Run("pickupZip is greater than destinationZip", func(t *testing.T) {
+	suite.T().Run("fromZip3 is greater than toZip3", func(t *testing.T) {
 		distance, err := randMcNallyZip3Distance(suite.DB(), "011", "010")
 		suite.NoError(err)
 		suite.Equal(24, distance)
 	})
 
-	suite.T().Run("pickupZip is the same as destinationZip", func(t *testing.T) {
+	suite.T().Run("fromZip3 is the same as toZip3", func(t *testing.T) {
 		distance, err := randMcNallyZip3Distance(suite.DB(), "010", "010")
 		suite.Equal(0, distance)
 		suite.NotNil(err)
-		suite.Equal("pickupZip (010) cannot be the same as destinationZip (010)", err.Error())
+		suite.Equal("fromZip3 (010) cannot be the same as toZip3 (010)", err.Error())
 	})
 }
