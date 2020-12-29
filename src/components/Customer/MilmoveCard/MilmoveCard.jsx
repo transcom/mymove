04ac66565c2bb7ae58118c29540418029d73dddc
Ron Cruz <Ronolibert@gmail.com>
2020-12-29 22:08:55 +0000

    Added styling to milmove card component

--- src/components/Customer/MilmoveCard/MilmoveCard.jsx
+++ src/components/Customer/MilmoveCard/MilmoveCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardBody } from '@trussworks/react-uswds';
+import { Card, CardBody, CardMedia } from '@trussworks/react-uswds';
 
 import * as styles from './MilmoveCard.module.scss';
 
@@ -8,14 +8,11 @@ import HHGShipmentImg from 'shared/images/hiveboxx-OoiWpdFC0Rw-unsplash.jpg';
 import MoveCounselorImg from 'shared/images/christina-wocintechchat-com-LQ1t-8Ms5PY-unsplash.jpg';
 import MovingTruckImg from 'shared/images/arron-choi-kRK1Bne4xEw-unsplash.jpg';
 
-const Divider = () => <div className={styles.divider} />;
+const Divider = () => <div className={styles.Divider} />;
 
 const MilmoveCard = () => (
-  <Card>
-    <CardHeader>
-      <h2>Hold on to things you’ll need quickly</h2>
-    </CardHeader>
-    <CardBody>
+  <Card containerProps={{ className: styles.CardContainer }}>
+    <CardBody className={styles.CardBody}>
       <SectionA />
       <Divider />
       <SectionB />
@@ -28,53 +25,52 @@ const MilmoveCard = () => (
 );
 
 const SectionA = () => (
-  <>
-    <img src={PPMShipmentImg} alt="PPM Shipment" />
+  <div className={styles.Section}>
+    <h3>Hold on to things you’ll need quickly</h3>
+    <CardMedia>
+      <img src={PPMShipmentImg} alt="PPM Shipment" />
+    </CardMedia>
+    <p>Hand-carry important documents — ID, medical info, orders, school records, etc.sss</p>
     <p>
-      Hand-carry important documents — ID, medical info, orders, school records, etc.
-      <br />
       Pack a set of things that you’ll need when you arrive — clothes, electronics, chargers, cleaning supplies, etc.
       Valuables that can’t be replaced are also a good idea.
-      <br />
-      To be paid for moving these things, select a PPM shipment.
     </p>
-  </>
+    <p>To be paid for moving these things, select a PPM shipment.</p>
+  </div>
 );
 
 const SectionB = () => (
-  <>
-    <h2>One move, several parts</h2>
+  <div className={styles.Section}>
+    <h3>One move, several parts</h3>
     <img src={HHGShipmentImg} alt="HHG Shipment" />
     <p>It’s common to move a few things yourself and have professional movers pack and move the rest.</p>
     <p>
       You can have things picked up or delivered to more than one place — your home and an office, for example. But
       multiple shipments make it easier to go over weight and end up paying for part of your move yourself.
     </p>
-  </>
+  </div>
 );
 
 const SectionC = () => (
-  <>
-    <h2>Talk to your move counselor</h2>
+  <div className={styles.Section}>
+    <h3>Talk to your move counselor</h3>
     <img src={MoveCounselorImg} alt="Move counselor meeting" />
     <p>
       A session with a move counselor is free. Counselors have a lot of experience with military moves and can steer you
       through complicated situations.
     </p>
-    <p>
-      Your counselor can identify:
-      <ul>
-        <li>belongings that won&apos;t count against your weight allowance</li>
-        <li>excess weight, excess distance, and other things that can cost you money</li>
-        <li>things to make your move easier</li>
-      </ul>
-    </p>
-  </>
+    <p className={styles.CounselorListHeader}>Your counselor can identify:</p>
+    <ul>
+      <li>belongings that won&apos;t count against your weight allowance</li>
+      <li>excess weight, excess distance, and other things that can cost you money</li>
+      <li>things to make your move easier</li>
+    </ul>
+  </div>
 );
 
 const SectionD = () => (
-  <>
-    <h2>Talk to your movers</h2>
+  <div className={styles.Section}>
+    <h3>Talk to your movers</h3>
     <img src={MovingTruckImg} alt="Moving truck" />
     <p>
       If you have any shipments using professional movers, you&apos;ll be referred to a point of contact for your move.
@@ -83,6 +79,6 @@ const SectionD = () => (
     <p>
       It’s OK if things change after you submit your move info. Your movers or your counselor will make things work.
     </p>
-  </>
+  </div>
 );
 export default MilmoveCard;
--- src/shared/User/SignIn.jsx
+++ src/shared/User/SignIn.jsx
@@ -8,45 +8,42 @@ import MilmoveCard from 'components/Customer/MilmoveCard/MilmoveCard';
 const SignIn = ({ context, location }) => {
   const error = qs.parse(location.search).error;
   return (
-    <>
-      <div className="grid-container usa-prose">
-        <div className="grid-row">
-          <div className="grid-col-8 grid-offset-2">
-            {error && (
-              <div>
-                <Alert type="error" heading="An error occurred">
-                  There was an error during your last sign in attempt. Please try again.
-                  <br />
-                  Error code: {error}
-                </Alert>
+    <div className="grid-container usa-prose">
+      <div className="grid-row">
+        <div className="grid-col-8 grid-offset-2">
+          {error && (
+            <div>
+              <Alert type="error" heading="An error occurred">
+                There was an error during your last sign in attempt. Please try again.
                 <br />
-              </div>
-            )}
-            <h1 className="align-center">Welcome to {context.siteName}!</h1>
-            <p>This is a new system from USTRANSCOM to support the relocation of families during PCS.</p>
-            {context.showLoginWarning && (
-              <div>
-                <p>
-                  Right now, use of this system is by invitation only. If you haven't received an invitation, please go
-                  to{' '}
-                  <a href="https://eta.sddc.army.mil/ETASSOPortal/default.aspx" className="usa-link">
-                    DPS
-                  </a>{' '}
-                  to schedule your move.
-                </p>
-                <p>Over the coming months, we'll be rolling this new tool out to more and more people. Stay tuned.</p>
-              </div>
-            )}
-            <div className="align-center">
-              <a href="/auth/login-gov" className="usa-button usa-button-big">
-                Sign in
-              </a>
+                Error code: {error}
+              </Alert>
+              <br />
             </div>
+          )}
+          <h1 className="align-center">Welcome to {context.siteName}!</h1>
+          <p>This is a new system from USTRANSCOM to support the relocation of families during PCS.</p>
+          {context.showLoginWarning && (
+            <div>
+              <p>
+                Right now, use of this system is by invitation only. If you haven't received an invitation, please go to{' '}
+                <a href="https://eta.sddc.army.mil/ETASSOPortal/default.aspx" className="usa-link">
+                  DPS
+                </a>{' '}
+                to schedule your move.
+              </p>
+              <p>Over the coming months, we'll be rolling this new tool out to more and more people. Stay tuned.</p>
+            </div>
+          )}
+          <div className="align-center">
+            <a href="/auth/login-gov" className="usa-button usa-button-big">
+              Sign in
+            </a>
           </div>
         </div>
       </div>
       <MilmoveCard />
-    </>
+    </div>
   );
 };
 
