d2820af39ee49e6c962db270db14c2e2e48ada01
Carter Jones <carter@truss.works>
2020-12-30 07:49:37 +0000

    create a low privileged postgres role for crud operations

---
+++ migrations/app/schema/20201230070859_create_crud_role.up.sql
@@ -0,0 +1,24 @@
+-- Create a new role named "crud" (CREATE READ UPDATE DELETE).
+CREATE ROLE crud WITH LOGIN;
+GRANT rds_iam TO crud;
+
+-- Set a password when running this locally.
+DO
+$do$
+BEGIN
+  IF NOT EXISTS (
+    SELECT -- SELECT list can stay empty for this
+    FROM   pg_catalog.pg_roles
+    WHERE  rolname = 'rdsadmin') THEN
+    ALTER USER crud WITH PASSWORD 'mysecretpassword';
+  END IF;
+END
+$do$;
+
+-- Modify existing tables and sequences.
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO crud;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO crud;
+
+-- Modify future tables and sequences.
+ALTER DEFAULT PRIVILEGES GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO crud;
+ALTER DEFAULT PRIVILEGES GRANT USAGE ON SEQUENCES TO crud;
