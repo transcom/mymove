30c4d35b2f1cd89cca44a934fbc4d69d83385129
Ron Cruz <Ronolibert@gmail.com>
2020-12-30 21:45:15 +0000

    Used exdent and inset props for different milmove card scenarios

--- src/components/Customer/MilmoveCard/MilmoveCard.jsx
+++ src/components/Customer/MilmoveCard/MilmoveCard.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { CardGroup, Card, CardHeader, CardBody } from '@trussworks/react-uswds';
-import { node, string } from 'prop-types';
+import { Card } from '@trussworks/react-uswds';
+import { bool, node } from 'prop-types';
 
 import * as styles from './MilmoveCard.module.scss';
 
-const MilmoveCard = ({ headerText, children }) => (
-  <CardGroup>
-    <Card containerProps={{ className: styles.CardContainer }}>
-      <CardHeader>
+const MilmoveCard = ({ children, headerFirst }) => (
+  <Card containerProps={{ className: styles.CardContainer }} headerFirst={headerFirst}>
+    <>{children}</>
+    {/* <CardHeader>
         <h3>{headerText}</h3>
       </CardHeader>
-      <CardBody className={styles.CardBody}>{children}</CardBody>
-    </Card>
-  </CardGroup>
+      <CardBody className={styles.CardBody}>{children}</CardBody> */}
+  </Card>
 );
 
 MilmoveCard.propTypes = {
-  headerText: string.isRequired,
   children: node.isRequired,
+  headerFirst: bool,
+};
+
+MilmoveCard.defaultProps = {
+  headerFirst: true,
 };
 
 export default MilmoveCard;
--- src/components/Customer/MilmoveCard/MilmoveCard.stories.jsx
+++ src/components/Customer/MilmoveCard/MilmoveCard.stories.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CardHeader, CardGroup, CardMedia, CardBody } from '@trussworks/react-uswds';
 
 import PPMShipmentImg from '../../../images/ppm-shipment.jpg';
 
@@ -9,16 +10,48 @@ export default {
   component: MilmoveCard,
 };
 
-export const Basic = () => (
+export const IndentedMilmoveCard = () => (
   <div style={{ maxWidth: 675 }}>
-    <MilmoveCard headerText="Hold on to things you’ll need quickly">
-      <img src={PPMShipmentImg} alt="PPM Shipment" />
-      <p>Hand-carry important documents — ID, medical info, orders, school records, etc.</p>
-      <p>
-        Pack a set of things that you’ll need when you arrive — clothes, electronics, chargers, cleaning supplies, etc.
-        Valuables that can’t be replaced are also a good idea.
-      </p>
-      <p>To be paid for moving these things, select a PPM shipment.</p>
-    </MilmoveCard>
+    <CardGroup>
+      <MilmoveCard>
+        <CardHeader>
+          <h3>Hold on to things you’ll need quickly</h3>
+        </CardHeader>
+        <CardMedia inset>
+          <img src={PPMShipmentImg} alt="PPM Shipment" />
+        </CardMedia>
+        <CardBody>
+          <p>Hand-carry important documents — ID, medical info, orders, school records, etc.</p>
+          <p>
+            Pack a set of things that you’ll need when you arrive — clothes, electronics, chargers, cleaning supplies,
+            etc. Valuables that can’t be replaced are also a good idea.
+          </p>
+          <p>To be paid for moving these things, select a PPM shipment.</p>
+        </CardBody>
+      </MilmoveCard>
+    </CardGroup>
+  </div>
+);
+
+export const ExdentedMilmoveCard = () => (
+  <div style={{ maxWidth: 675 }}>
+    <CardGroup>
+      <MilmoveCard>
+        <CardHeader exdent>
+          <h3>Hold on to things you’ll need quickly</h3>
+        </CardHeader>
+        <CardMedia exdent>
+          <img src={PPMShipmentImg} alt="PPM Shipment" />
+        </CardMedia>
+        <CardBody exdent>
+          <p>Hand-carry important documents — ID, medical info, orders, school records, etc.</p>
+          <p>
+            Pack a set of things that you’ll need when you arrive — clothes, electronics, chargers, cleaning supplies,
+            etc. Valuables that can’t be replaced are also a good idea.
+          </p>
+          <p>To be paid for moving these things, select a PPM shipment.</p>
+        </CardBody>
+      </MilmoveCard>
+    </CardGroup>
   </div>
 );
