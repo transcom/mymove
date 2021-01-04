8adf12e1a0f398a84eb5494395f898e133dc448f
Erin Stanfill <erin@truss.works>
2021-01-04 20:53:23 +0000

    update client cert for Erin CAC

--- migrations/app/secure/20210104155733_erin_update_cac.up.sql
+++ migrations/app/secure/20210104155733_erin_update_cac.up.sql
@@ -1,4 +1,13 @@
--- Local test migration.
--- This will be run on development environments.
--- It should mirror what you intend to apply on prod/staging/experimental
--- DO NOT include any sensitive data.
+
+-- This migration allows a CAC cert to have read/write access to all orders and the prime API.
+-- The Orders API and the Prime API use client certificate authentication. Only certificates
+-- signed by a trusted CA (such as DISA) are allowed which includes CACs.
+-- Using a person's CAC as the certificate is a convenient way to permit a
+-- single trusted individual to interact with the Orders API and the Prime API. Eventually
+-- this CAC certificate should be removed.
+-- Updating sha256 for new cac
+UPDATE client_certs
+SET sha256_digest = '03fcb44f906f6dcb655cf4d4ddbbf694942b49b61eb87988f8b9d3b474d3db4d',
+	subject = 'CN=tinyels,OU=DoD+OU=PKI+OU=CONTRACTOR,O=U.S. Government,C=US',
+	updated_at = NOW()
+WHERE id = 'a8744ea1-38ee-458b-ba28-e8d04b4f76c3';
