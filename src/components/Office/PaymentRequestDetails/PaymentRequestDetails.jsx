0edac0ad4d02d2f8833e28c18dc3ba62d3db94a2
JP <jenniferlynparsons@gmail.com>
2021-01-07 17:09:27 -0500

    PaymentRequestDetails cleanup

--- src/components/Office/PaymentRequestDetails/PaymentRequestDetails.jsx
+++ src/components/Office/PaymentRequestDetails/PaymentRequestDetails.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { ShipmentOptionsOneOf } from '../../../types/shipment';
+import { PaymentServiceItemShape } from '../../../types/index';
 
 import styles from './PaymentRequestDetails.module.scss';
 
+import { PAYMENT_SERVICE_ITEM_STATUS } from 'shared/constants';
 import { formatCents, toDollarString } from 'shared/formatters';
 
 const PaymentRequestDetails = ({ serviceItems }) => {
@@ -15,7 +15,9 @@ const PaymentRequestDetails = ({ serviceItems }) => {
         {/* TODO this div will become dynamic based on different shipment types */}
         <div className={styles.shipmentType}>
           <div className={styles.basicServiceType} />
-          <h3>Basic Service Items ({serviceItems.length} items)</h3>
+          <h3>
+            Basic Service Items ({serviceItems.length} {serviceItems.length > 1 ? 'items' : 'item'})
+          </h3>
         </div>
       </div>
       <table className="table--stacked">
@@ -32,26 +34,26 @@ const PaymentRequestDetails = ({ serviceItems }) => {
           </tr>
         </thead>
         <tbody>
-          {serviceItems.map((item, i) => {
+          {serviceItems.map((item) => {
             return (
               // eslint-disable-next-line react/no-array-index-key
-              <tr key={i}>
+              <tr key={item.id}>
                 <td>{item.serviceItemName}</td>
                 <td>{toDollarString(formatCents(item.priceCents))}</td>
                 <td>
-                  {item.status === 'PENDING' && (
+                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.PENDING && (
                     <div className={styles.needsReview}>
                       <FontAwesomeIcon icon="exclamation-circle" />
                       <span>Needs Review</span>
                     </div>
                   )}
-                  {item.status === 'APPROVED' && (
+                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.APPROVED && (
                     <div className={styles.accepted}>
                       <FontAwesomeIcon icon="check" />
                       <span>Accepted</span>
                     </div>
                   )}
-                  {item.status === 'DENIED' && (
+                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.DENIED && (
                     <div className={styles.rejected}>
                       <FontAwesomeIcon icon="times" />
                       <span>Rejected</span>
@@ -68,16 +70,7 @@ const PaymentRequestDetails = ({ serviceItems }) => {
 };
 
 PaymentRequestDetails.propTypes = {
-  serviceItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      createAt: PropTypes.string,
-      mtoServiceItemID: PropTypes.string,
-      priceCents: PropTypes.number,
-      status: PropTypes.string,
-      shipmentType: ShipmentOptionsOneOf,
-    }),
-  ).isRequired,
+  serviceItems: PaymentServiceItemShape.isRequired,
 };
 
 export default PaymentRequestDetails;
--- src/components/Office/PaymentRequestDetails/PaymentRequestDetails.test.jsx
+++ src/components/Office/PaymentRequestDetails/PaymentRequestDetails.test.jsx
@@ -73,6 +73,25 @@ const basicPaymentRequest = {
   ],
 };
 
+const basicPaymentRequestOneServiceItem = {
+  id: '29474c6a-69b6-4501-8e08-670a12512e5f',
+  createdAt: '2020-12-01T00:00:00.000Z',
+  moveTaskOrderID: 'f8c2f97f-99e7-4fb1-9cc4-473debd04dbc',
+  paymentRequestNumber: '1843-9061-2',
+  status: 'REVIEWED',
+  serviceItems: [
+    {
+      id: '09474c6a-69b6-4501-8e08-670a12512a5f',
+      createdAt: '2020-12-01T00:00:00.000Z',
+      mtoServiceItemID: 'f8c2f97f-99e7-4fb1-9cc4-473debd24dbc',
+      priceCents: 2000001,
+      status: 'APPROVED',
+      shipmentType: null,
+      serviceItemName: 'Move management',
+    },
+  ],
+};
+
 const testMoveLocator = 'AF7K1P';
 
 describe('PaymentRequestDetails', () => {
@@ -84,8 +103,23 @@ describe('PaymentRequestDetails', () => {
     );
 
     it('renders the service items', async () => {
-      await wrapper.update();
       expect(wrapper.find('td')).toBeTruthy();
+    });
+
+    it('renders the expected table title', () => {
+      expect(wrapper.text().includes('Basic Service Items (6 items)')).toBeTruthy();
+    });
+  });
+
+  describe('When given a single basic service item', () => {
+    const wrapper = mount(
+      <MockProviders initialEntries={[`/moves/${testMoveLocator}/payment-requests`]}>
+        <PaymentRequestDetails serviceItems={basicPaymentRequestOneServiceItem.serviceItems} />
+      </MockProviders>,
+    );
+
+    it('renders the expected table title', () => {
+      expect(wrapper.text().includes('Basic Service Items (1 item)')).toBeTruthy();
     });
   });
 });
