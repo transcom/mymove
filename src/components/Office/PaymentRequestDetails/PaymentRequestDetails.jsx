4767880ea2785d326b4662e04b9fba23d9b0376f
Donald Thai <donald@truss.works>
2021-01-08 19:09:17 +0000

    hide basic service items if none

--- src/components/Office/PaymentRequestDetails/PaymentRequestDetails.jsx
+++ src/components/Office/PaymentRequestDetails/PaymentRequestDetails.jsx
@@ -15,62 +15,64 @@ const PaymentRequestDetails = ({ serviceItems }) => {
   );
 
   return (
-    <div className={styles.PaymentRequestDetails}>
-      <div className="stackedtable-header">
-        {/* TODO this div will become dynamic based on different shipment types */}
-        <div className={styles.shipmentType}>
-          <div className={styles.basicServiceType} />
-          <h3>
-            Basic Service Items ({basicServiceItems.length} {basicServiceItems.length > 1 ? 'items' : 'item'})
-          </h3>
+    basicServiceItems.length > 0 && (
+      <div className={styles.PaymentRequestDetails}>
+        <div className="stackedtable-header">
+          {/* TODO this div will become dynamic based on different shipment types */}
+          <div className={styles.shipmentType}>
+            <div className={styles.basicServiceType} />
+            <h3>
+              Basic Service Items ({basicServiceItems.length} {basicServiceItems.length > 1 ? 'items' : 'item'})
+            </h3>
+          </div>
         </div>
+        <table className="table--stacked">
+          <colgroup>
+            <col style={{ width: '50%' }} />
+            <col style={{ width: '25%' }} />
+            <col style={{ width: '25%' }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>Service Item</th>
+              <th className="align-right">Amount</th>
+              <th className="align-right">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {basicServiceItems.map((item) => {
+              return (
+                // eslint-disable-next-line react/no-array-index-key
+                <tr key={item.id}>
+                  <td>{item.mtoServiceItemName}</td>
+                  <td>{toDollarString(formatCents(item.priceCents))}</td>
+                  <td>
+                    {item.status === PAYMENT_SERVICE_ITEM_STATUS.REQUESTED && (
+                      <div className={styles.needsReview}>
+                        <FontAwesomeIcon icon="exclamation-circle" />
+                        <span>Needs Review</span>
+                      </div>
+                    )}
+                    {item.status === PAYMENT_SERVICE_ITEM_STATUS.APPROVED && (
+                      <div className={styles.accepted}>
+                        <FontAwesomeIcon icon="check" />
+                        <span>Accepted</span>
+                      </div>
+                    )}
+                    {item.status === PAYMENT_SERVICE_ITEM_STATUS.DENIED && (
+                      <div className={styles.rejected}>
+                        <FontAwesomeIcon icon="times" />
+                        <span>Rejected</span>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-      <table className="table--stacked">
-        <colgroup>
-          <col style={{ width: '50%' }} />
-          <col style={{ width: '25%' }} />
-          <col style={{ width: '25%' }} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>Service Item</th>
-            <th className="align-right">Amount</th>
-            <th className="align-right">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {basicServiceItems.map((item) => {
-            return (
-              // eslint-disable-next-line react/no-array-index-key
-              <tr key={item.id}>
-                <td>{item.mtoServiceItemName}</td>
-                <td>{toDollarString(formatCents(item.priceCents))}</td>
-                <td>
-                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.REQUESTED && (
-                    <div className={styles.needsReview}>
-                      <FontAwesomeIcon icon="exclamation-circle" />
-                      <span>Needs Review</span>
-                    </div>
-                  )}
-                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.APPROVED && (
-                    <div className={styles.accepted}>
-                      <FontAwesomeIcon icon="check" />
-                      <span>Accepted</span>
-                    </div>
-                  )}
-                  {item.status === PAYMENT_SERVICE_ITEM_STATUS.DENIED && (
-                    <div className={styles.rejected}>
-                      <FontAwesomeIcon icon="times" />
-                      <span>Rejected</span>
-                    </div>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    )
   );
 };
 
