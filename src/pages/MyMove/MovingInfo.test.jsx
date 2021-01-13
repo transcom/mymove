677dc221300c0e678446f5a3ca48db8aef1ccf82
Yan Zhao <yan.zhao@truss.works>
2021-01-13 20:14:51 +0000

    Add Entitlement to Service Member Orders, Componentize MovingInfo, Update Tests
    - add the entitlement to the payload for service member orders
    - change MovingInfo to a component to use fetchLatestOrders
    - update MovingInfo tests

--- src/pages/MyMove/MovingInfo.test.jsx
+++ src/pages/MyMove/MovingInfo.test.jsx
@@ -3,12 +3,20 @@ import { mount } from 'enzyme';
 
 import { MovingInfo } from './MovingInfo';
 
-const wrapper = mount(<MovingInfo />);
+const testProps = {
+  entitlementWeight: 7000,
+  fetchLatestOrders: jest.fn(),
+  serviceMemberId: '1234567890',
+};
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const wrapper = mount(<MovingInfo {...testProps} />);
 
 describe('MovingInfo component', () => {
   it('renders', () => {
     expect(wrapper.find('MovingInfo').length).toBe(1);
     expect(wrapper.text()).toContain('Tips for planning your shipments');
+    expect(wrapper.text()).toContain('7,000 lbs');
     expect(wrapper.find('[data-testid="shipmentsHeader"]').length).toBe(1);
     expect(wrapper.find('[data-testid="shipmentsSubHeader"]').length).toBe(4);
   });
