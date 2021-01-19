a9cea03027e3667b85839106b839edc23de2629b
Marty Boren <marty@truss.works>
2021-01-19 21:07:27 +0000

    Combine ALTER TABLEs & follow fk naming convention

--- migrations/app/schema/20210112002844_add_sit_addresses.up.sql
+++ migrations/app/schema/20210112002844_add_sit_addresses.up.sql
@@ -2,12 +2,10 @@
 ALTER TABLE mto_service_items
     ADD COLUMN sit_destination_final_address_id uuid,
     ADD COLUMN sit_origin_original_address_id uuid,
-    ADD COLUMN sit_origin_actual_address_id uuid;
-
-ALTER TABLE mto_service_items
-    ADD CONSTRAINT sit_destination_final_address_fk FOREIGN KEY (sit_destination_final_address_id) REFERENCES addresses (id),
-    ADD CONSTRAINT sit_origin_original_address_fk FOREIGN KEY (sit_origin_original_address_id) REFERENCES addresses (id),
-    ADD CONSTRAINT sit_origin_actual_address_fk FOREIGN KEY (sit_origin_actual_address_id) REFERENCES addresses (id);
+    ADD COLUMN sit_origin_actual_address_id uuid,
+    ADD CONSTRAINT mto_service_items_sit_destination_final_address_id_fkey FOREIGN KEY (sit_destination_final_address_id) REFERENCES addresses (id),
+    ADD CONSTRAINT mto_service_items_sit_origin_original_address_id_fkey FOREIGN KEY (sit_origin_original_address_id) REFERENCES addresses (id),
+    ADD CONSTRAINT mto_service_items_sit_origin_actual_address_id_fkey FOREIGN KEY (sit_origin_actual_address_id) REFERENCES addresses (id);
 
 -- Column comments
 COMMENT ON COLUMN mto_service_items.sit_destination_final_address_id IS 'Final delivery address for Destination SIT';
