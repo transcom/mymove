4103d6438c3f6745daacd1fc97814d487c767ee4
Sandy Wright <sandy@truss.works>
2021-01-22 13:50:09 -0600

    MB-6175 Add tests for new MTO ShowHide service object

--- pkg/services/move_task_order/move_task_order_updater_test.go
+++ pkg/services/move_task_order/move_task_order_updater_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/transcom/mymove/pkg/services/mocks"
+
 	"github.com/transcom/mymove/pkg/etag"
 	movetaskorderops "github.com/transcom/mymove/pkg/gen/primeapi/primeoperations/move_task_order"
 	"github.com/transcom/mymove/pkg/models"
@@ -56,5 +61,91 @@ func (suite *MoveTaskOrderServiceSuite) TestMoveTaskOrderUpdater_UpdatePostCouns
 
 		suite.Error(err)
 		suite.IsType(services.PreconditionFailedError{}, err)
+	})
+}
+
+func (suite *MoveTaskOrderServiceSuite) TestMoveTaskOrderUpdater_ShowHide() {
+	// Set up a default move:
+	move := testdatagen.MakeDefaultMove(suite.DB())
+
+	// Set up the necessary updater objects:
+	queryBuilder := query.NewQueryBuilder(suite.DB())
+	updater := NewMoveTaskOrderUpdater(suite.DB(), queryBuilder, mtoserviceitem.NewMTOServiceItemCreator(queryBuilder))
+
+	// Case: Move successfully activated
+	suite.T().Run("Success - Set show field to true", func(t *testing.T) {
+		show := true
+		updatedMove, err := updater.ShowHide(move.ID, &show)
+
+		suite.NotNil(updatedMove)
+		suite.NoError(err)
+		suite.Equal(updatedMove.ID, move.ID)
+		suite.Equal(*updatedMove.Show, show)
+	})
+
+	// Case: Move successfully deactivate
+	suite.T().Run("Success - Set show field to false", func(t *testing.T) {
+		show := false
+		updatedMove, err := updater.ShowHide(move.ID, &show)
+
+		suite.NotNil(updatedMove)
+		suite.NoError(err)
+		suite.Equal(updatedMove.ID, move.ID)
+		suite.Equal(*updatedMove.Show, show)
+	})
+
+	// Case: Move UUID not found in DB
+	suite.T().Run("Fail - Move not found", func(t *testing.T) {
+		show := true
+		badMoveID := uuid.FromStringOrNil("00000000-0000-0000-0000-000000000001")
+		updatedMove, err := updater.ShowHide(badMoveID, &show)
+
+		suite.Nil(updatedMove)
+		suite.Error(err)
+		suite.IsType(services.NotFoundError{}, err)
+		suite.Contains(err.Error(), badMoveID.String())
+	})
+
+	// Case: Show input value is nil, not True or False
+	suite.T().Run("Fail - Nil value in show field", func(t *testing.T) {
+		updatedMove, err := updater.ShowHide(move.ID, nil)
+
+		suite.Nil(updatedMove)
+		suite.Error(err)
+		suite.IsType(services.InvalidInputError{}, err)
+		suite.Contains(err.Error(), "The 'show' field must be either True or False - it cannot be empty")
+	})
+
+	// Case: Invalid input found while updating the move
+	// TODO: Is there a way to mock ValidateUpdate so that these tests actually mean something?
+	suite.T().Run("Fail - Invalid input found on move", func(t *testing.T) {
+		mockUpdater := mocks.MoveTaskOrderUpdater{}
+		mockUpdater.On("ShowHide",
+			mock.Anything, // our arguments aren't important here because there's no specific way to trigger this error
+			mock.Anything,
+		).Return(nil, services.InvalidInputError{})
+
+		show := true
+		updatedMove, err := mockUpdater.ShowHide(move.ID, &show)
+
+		suite.Nil(updatedMove)
+		suite.Error(err)
+		suite.IsType(services.InvalidInputError{}, err)
+	})
+
+	// Case: Query error encountered while updating the move
+	suite.T().Run("Fail - Query error", func(t *testing.T) {
+		mockUpdater := mocks.MoveTaskOrderUpdater{}
+		mockUpdater.On("ShowHide",
+			mock.Anything, // our arguments aren't important here because there's no specific way to trigger this error
+			mock.Anything,
+		).Return(nil, services.QueryError{})
+
+		show := true
+		updatedMove, err := mockUpdater.ShowHide(move.ID, &show)
+
+		suite.Nil(updatedMove)
+		suite.Error(err)
+		suite.IsType(services.QueryError{}, err)
 	})
 }
