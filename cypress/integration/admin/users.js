61ab978b94e21de503763c5943aa4ef4e961230a
Emily Mahanna <emahanna@truss.works>
2021-01-22 17:26:31 +0000

    Fix cypress test

--- cypress/integration/admin/users.js
+++ cypress/integration/admin/users.js
@@ -7,13 +7,15 @@ describe('Users Page', function () {
 
   it('successfully navigates to users page', function () {
     cy.signInAsNewAdminUser();
-    cy.get('a[href*="customer_users"]').click();
+    cy.get('a[href*="users"]').click();
     cy.url().should('eq', adminBaseURL + '/system/users');
-    cy.get('header').contains('Customer users');
+    cy.get('header').contains('Users');
 
     const columnLabels = ['Id', 'Email', 'Active', 'Created at'];
     columnLabels.forEach((label) => {
       cy.get('table').contains(label);
     });
+
+    //TODO test search functionality
   });
 });
