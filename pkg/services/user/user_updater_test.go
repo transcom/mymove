0d01c41d53fb88a806bf789b5b32730705abe2a7
Abigail Young <abigail@truss.works>
2021-01-22 19:53:27 +0000

    add tests for new user updater service object

---
+++ pkg/services/user/user_updater_test.go
@@ -0,0 +1,47 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/transcom/mymove/pkg/gen/adminmessages"
+	"github.com/transcom/mymove/pkg/services/query"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *UserServiceSuite) TestUserUpdater() {
+	builder := query.NewQueryBuilder(suite.DB())
+	updater := NewUserUpdater(builder)
+	active := true
+	inactive := false
+
+	activeUser := testdatagen.MakeDefaultUser(suite.DB())
+
+	suite.T().Run("Deactivate a user successfully", func(t *testing.T) {
+		payload := adminmessages.UserUpdatePayload{
+			Active: &inactive,
+		}
+
+		// Take our existing active user and change their Active status to False
+		updatedUser, verr, err := updater.UpdateUser(activeUser.ID, &payload)
+
+		suite.Nil(verr)
+		suite.Nil(err)
+		suite.False(updatedUser.Active)
+
+	})
+
+	suite.T().Run("Activate a user successfully", func(t *testing.T) {
+		payload := adminmessages.UserUpdatePayload{
+			Active: &active,
+		}
+
+		// Take our existing inactive user and change their Active status to True
+		updatedUser, verr, err := updater.UpdateUser(activeUser.ID, &payload)
+
+		suite.Nil(verr)
+		suite.Nil(err)
+		suite.True(updatedUser.Active)
+
+	})
+
+}
