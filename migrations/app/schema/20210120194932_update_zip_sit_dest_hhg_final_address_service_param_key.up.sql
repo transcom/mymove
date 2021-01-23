e769e6154d357828a0447ba5ed10ec2f90898ec8
John Gedeon <john@truss.works>
2021-01-23 01:43:43 +0000

    Update updated_at field for changed rows

--- migrations/app/schema/20210120194932_update_zip_sit_dest_hhg_final_address_service_param_key.up.sql
+++ migrations/app/schema/20210120194932_update_zip_sit_dest_hhg_final_address_service_param_key.up.sql
@@ -1,9 +1,11 @@
 -- Rename ZipSITAddress to ZipSITDestHHGFinalAddress and update description
 UPDATE service_item_param_keys
-  SET key = 'ZipSITDestHHGFinalAddress', description = 'SIT Final Destination Address ZIP from MTOServiceItem'
+  SET key = 'ZipSITDestHHGFinalAddress',
+    description = 'SIT Final Destination Address ZIP from MTOServiceItem',
+    updated_at = now()
   WHERE key = 'ZipSITAddress';
 
 -- Update ZipDestAddress description to clarify its source is the MTOShipment
 UPDATE service_item_param_keys
-  SET description = 'Destination address ZIP from MTOShipment'
+  SET description = 'Destination address ZIP from MTOShipment', updated_at = now()
   WHERE key = 'ZipDestAddress';
