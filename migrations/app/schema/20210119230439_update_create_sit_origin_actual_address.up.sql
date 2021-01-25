08ebffb708cc6c240a6efeefcfd14b521d703ab6
Jacqueline McKinney <jacqueline@truss.works>
2021-01-25 16:14:17 +0000

    add indexes to foreign keys

--- migrations/app/schema/20210119230439_update_create_sit_origin_actual_address.up.sql
+++ migrations/app/schema/20210119230439_update_create_sit_origin_actual_address.up.sql
@@ -5,6 +5,10 @@ ALTER TABLE mto_service_items
     ADD CONSTRAINT mto_service_items_sit_origin_hhg_original_address_id_fkey FOREIGN KEY (sit_origin_hhg_original_address_id) REFERENCES addresses (id),
     ADD CONSTRAINT mto_service_items_sit_origin_hhg_actual_address_id_fkey FOREIGN KEY (sit_origin_hhg_actual_address_id) REFERENCES addresses (id);
 
+-- Create index
+CREATE INDEX on mto_service_items (sit_origin_hhg_original_address_id);
+CREATE INDEX on mto_service_items (sit_origin_hhg_actual_address_id);
+
 -- Column comments
 COMMENT ON COLUMN mto_service_items.sit_origin_hhg_original_address_id IS 'HHG Original pickup address, using Origin SIT';
-COMMENT ON COLUMN mto_service_items.sit_origin_hhg_actual_address_id IS 'HHG New pickup address, using Origin SIT';
+COMMENT ON COLUMN mto_service_items.sit_origin_hhg_actual_address_id IS 'HHG (new) Actual pickup address, using Origin SIT';
