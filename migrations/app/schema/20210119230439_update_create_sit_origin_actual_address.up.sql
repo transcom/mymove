d10b498ef1426bf6ca637dbdb5bed77f88c13fa5
Jacqueline McKinney <jacqueline@truss.works>
2021-01-25 16:49:34 +0000

    make statements more succint in sql

--- migrations/app/schema/20210119230439_update_create_sit_origin_actual_address.up.sql
+++ migrations/app/schema/20210119230439_update_create_sit_origin_actual_address.up.sql
@@ -1,9 +1,7 @@
 -- Column add
 ALTER TABLE mto_service_items
-    ADD COLUMN sit_origin_hhg_original_address_id uuid,
-    ADD COLUMN sit_origin_hhg_actual_address_id uuid,
-    ADD CONSTRAINT mto_service_items_sit_origin_hhg_original_address_id_fkey FOREIGN KEY (sit_origin_hhg_original_address_id) REFERENCES addresses (id),
-    ADD CONSTRAINT mto_service_items_sit_origin_hhg_actual_address_id_fkey FOREIGN KEY (sit_origin_hhg_actual_address_id) REFERENCES addresses (id);
+    ADD COLUMN sit_origin_hhg_original_address_id uuid CONSTRAINT mto_service_items_sit_origin_hhg_original_address_id_fkey REFERENCES addresses (id),
+    ADD COLUMN sit_origin_hhg_actual_address_id uuid CONSTRAINT mto_service_items_sit_origin_hhg_actual_address_id_fkey REFERENCES addresses (id);
 
 -- Create index
 CREATE INDEX on mto_service_items (sit_origin_hhg_original_address_id);
