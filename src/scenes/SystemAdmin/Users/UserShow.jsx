d5afa10d2d03f1ef50674f02b9bc5b266e0fb963
Namibia <namibia@truss.works>
2021-01-27 19:50:58 +0000

    add fields to show page

--- src/scenes/SystemAdmin/Users/UserShow.jsx
+++ src/scenes/SystemAdmin/Users/UserShow.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Show, SimpleShowLayout, TextField } from 'react-admin';
+import { Show, SimpleShowLayout, TextField, BooleanField, DateField } from 'react-admin';
 
 const UserShow = (props) => {
   return (
     <Show {...props} title="title">
       <SimpleShowLayout>
         <TextField source="loginGovEmail" label="user email" />
+        <BooleanField source="active" />
         <TextField source="currentAdminSessionId" label="user current admin session ID" />
         <TextField source="currentOfficeSessionId" label="user current office session ID" />
         <TextField source="currentMilSessionId" label="user current mil session ID" />
+        <DateField source="createdAt" showTime />
+        <DateField source="updatedAt" showTime />
       </SimpleShowLayout>
     </Show>
   );
