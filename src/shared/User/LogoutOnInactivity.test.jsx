c92041df5e705090719d2cace2d501997d4dc5d2
Moncef Belyamani <moncef@truss.works>
2021-01-28 16:29:17 -0500

    Remove eslint disable lines

--- src/shared/User/LogoutOnInactivity.test.jsx
+++ src/shared/User/LogoutOnInactivity.test.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-only-tests/no-only-tests */
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
