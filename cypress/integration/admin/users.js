40a90da86a407ebfa7857e0effd827007d37f9ec
Namibia <namibia@truss.works>
2021-01-28 17:23:04 +0000

    add test

--- cypress/integration/admin/users.js
+++ cypress/integration/admin/users.js
@@ -17,3 +17,26 @@ describe('Users Page', function () {
     });
   });
 });
+
+describe('Users Details Show Page', function () {
+  // after(() => {
+
+  // });
+
+  // Is there a way to pass in any id instead of a specific ID in the URL
+  // pullout columnLabels var
+  //  Make a note that this test is dependent on the previous test
+  it('pulls up details page for a user', function () {
+    cy.signInAsNewAdminUser();
+    cy.get('a[href*="system/users"]').click();
+    cy.url().should('eq', adminBaseURL + '/system/users');
+    // Fix the line below to get any id for any user in the list of users
+    // cy.get('a[href*="system/users/${id.first()}/show"]').click();
+    cy.get('a[href*="/show"]').first().click();
+
+    const pageContent = ['user ID', 'user email', 'Active', 'Created at', 'Updated at'];
+    pageContent.forEach((string) => {
+      cy.get('input').contains(string);
+    });
+  });
+});
