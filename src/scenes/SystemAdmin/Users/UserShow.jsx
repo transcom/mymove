aad87b3fd3e289b6e010ffad5a7be82909a766fc
Namibia <namibia@truss.works>
2021-02-01 14:44:42 +0000

    put userShow page back in SystemAdmin

---
+++ src/scenes/SystemAdmin/Users/UserShow.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Show, SimpleShowLayout, TextField, BooleanField, DateField } from 'react-admin';
+
+const UserShow = (props) => {
+  return (
+    <Show {...props} title="title" data-testid="user-show-detail">
+      <SimpleShowLayout>
+        <TextField source="id" label="User ID" />
+        <TextField source="loginGovEmail" label="User email" />
+        <BooleanField source="active" />
+        <TextField source="currentAdminSessionId" label="User current admin session ID" />
+        <TextField source="currentOfficeSessionId" label="User current office session ID" />
+        <TextField source="currentMilSessionId" label="User current mil session ID" />
+        <DateField source="createdAt" showTime />
+        <DateField source="updatedAt" showTime />
+      </SimpleShowLayout>
+    </Show>
+  );
+};
+
+export default UserShow;
