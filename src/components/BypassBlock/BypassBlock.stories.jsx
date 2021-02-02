df691ef0a3399e50ef7a77162310c976226ffc2a
Shauna Keating <shauna@truss.works>
2021-02-02 20:01:11 +0000

    adds markup for the bypass block stories page

--- src/components/BypassBlock/BypassBlock.stories.jsx
+++ src/components/BypassBlock/BypassBlock.stories.jsx
@@ -6,6 +6,14 @@ export default {
   title: 'Components/Headers/Bypass Block',
 };
 
-export const Milmove = () => (
-  <BypassBlock />
+export const BypassBlockLink = () => (
+  <div>
+    <BypassBlock />
+    <nav>
+      Sample Navigation
+      <a href="#">Link</a>
+      <a href="#">Link</a>
+      <a href="#">Link</a>
+    </nav>
+  </div>
 );
--- src/components/BypassBlock/index.jsx
+++ src/components/BypassBlock/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { string } from 'prop-types';
+
 import styles from './index.module.scss';
 
 const BypassBlock = ({ anchorLink }) => (
   <div className={styles.bypassBlock}>
-    <a href={anchorLink} >Skip to Content</a>
+    <a href={anchorLink}>Skip to Content</a>
   </div>
 );
 
-BypassBlock.defaultProps = {
-  anchorLink: '#main',
-};
-
 BypassBlock.propTypes = {
   anchorLink: string,
+};
+
+BypassBlock.defaultProps = {
+  anchorLink: '#main',
 };
 
 export default BypassBlock;
