4d2a8dc7f811439c1873b9a332b986341fd3777d
Shauna Keating <shauna@truss.works>
2021-02-02 19:52:21 +0000

    adds stories page for bypass block

---
+++ src/components/BypassBlock/BypassBlock.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BypassBlock from './index';
+
+export default {
+  title: 'Components/Headers/Bypass Block',
+};
+
+export const Milmove = () => (
+  <BypassBlock />
+);
