e63532adbc9dec2ca29e392177ee8cf3933c4802
Reggie Riser <reggie@truss.works>
2021-02-03 22:40:50 +0000

    Refactored pickup/delivery pricer code to keep things DRY

--- pkg/services/ghcrateengine/domestic_origin_sit_pickup_pricer.go
+++ pkg/services/ghcrateengine/domestic_origin_sit_pickup_pricer.go
@@ -1,8 +1,6 @@
 package ghcrateengine
 
 import (
-	"fmt"
-	"math"
 	"time"
 
 	"github.com/gobuffalo/pop/v5"
@@ -24,70 +22,8 @@ func NewDomesticOriginSITPickupPricer(db *pop.Connection) services.DomesticOrigi
 }
 
 // Price determines the price for domestic origin SIT pickup
-func (p domesticOriginSITPickupPricer) Price(contractCode string, requestedPickupDate time.Time, isPeakPeriod bool,
-	weight unit.Pound, serviceArea string, sitSchedule int, zipSITOriginOriginal string, zipSITOriginActual string, distance unit.Miles) (unit.Cents, error) {
-
-	if weight < minDomesticWeight {
-		return 0, fmt.Errorf("weight of %d less than the minimum of %d", weight, minDomesticWeight)
-	}
-
-	if len(zipSITOriginOriginal) < 5 {
-		return unit.Cents(0), fmt.Errorf("invalid SIT origin original postal code of %s", zipSITOriginOriginal)
-	}
-	zip3Original := zipSITOriginOriginal[:3]
-
-	if len(zipSITOriginActual) < 5 {
-		return unit.Cents(0), fmt.Errorf("invalid SIT origin actual postal code of %s", zipSITOriginActual)
-	}
-	zip3Actual := zipSITOriginActual[:3]
-
-	// Three different pricing scenarios below.
-
-	// 1) Zip3 to same zip3
-	if zip3Original == zip3Actual {
-		// Do a normal shorthaul calculation
-		shorthaulPricer := NewDomesticShorthaulPricer(p.db)
-		totalPriceCents, err := shorthaulPricer.Price(contractCode, requestedPickupDate, distance, weight, serviceArea)
-		if err != nil {
-			return unit.Cents(0), fmt.Errorf("could not price shorthaul: %w", err)
-		}
-
-		return totalPriceCents, nil
-	}
-
-	// Zip3s must be different at this point.  Now examine distance.
-
-	// 2) Zip3 to different zip3 and > 50 miles
-	if distance > 50 {
-		// Do a normal linehaul calculation
-		linehaulPricer := NewDomesticLinehaulPricer(p.db)
-		totalPriceCents, err := linehaulPricer.Price(contractCode, requestedPickupDate, isPeakPeriod, distance, weight, serviceArea)
-		if err != nil {
-			return unit.Cents(0), fmt.Errorf("could not price linehaul: %w", err)
-		}
-
-		return totalPriceCents, nil
-	}
-
-	// Zip3s must be different at this point and distance is <= 50.
-
-	// 3) Zip3 to different zip3 and <= 50 miles
-
-	// Rate comes from the domestic other price table based on SIT schedule
-	domOtherPrice, err := fetchDomOtherPrice(p.db, contractCode, models.ReServiceCodeDOPSIT, sitSchedule, isPeakPeriod)
-	if err != nil {
-		return unit.Cents(0), fmt.Errorf("could not fetch domestic origin SIT pickup rate: %w", err)
-	}
-	contractYear, err := fetchContractYear(p.db, domOtherPrice.ContractID, requestedPickupDate)
-	if err != nil {
-		return unit.Cents(0), fmt.Errorf("could not fetch contract year: %w", err)
-	}
-
-	baseTotalPrice := domOtherPrice.PriceCents.Float64() * weight.ToCWTFloat64()
-	escalatedTotalPrice := baseTotalPrice * contractYear.EscalationCompounded
-	totalPriceCents := unit.Cents(math.Round(escalatedTotalPrice))
-
-	return totalPriceCents, nil
+func (p domesticOriginSITPickupPricer) Price(contractCode string, requestedPickupDate time.Time, isPeakPeriod bool, weight unit.Pound, serviceArea string, sitSchedule int, zipSITOriginOriginal string, zipSITOriginActual string, distance unit.Miles) (unit.Cents, error) {
+	return priceDomesticPickupDeliverySIT(p.db, models.ReServiceCodeDOPSIT, contractCode, requestedPickupDate, isPeakPeriod, weight, serviceArea, sitSchedule, zipSITOriginOriginal, zipSITOriginActual, distance)
 }
 
 // PriceUsingParams determines the price for domestic origin SIT pickup given PaymentServiceItemParams
--- pkg/services/ghcrateengine/pricer_helpers_test.go
+++ pkg/services/ghcrateengine/pricer_helpers_test.go
@@ -1,9 +1,11 @@
 package ghcrateengine
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
 	"github.com/transcom/mymove/pkg/unit"
 )
 
@@ -74,6 +76,100 @@ func (suite *GHCRateEngineServiceSuite) Test_priceDomesticAdditionalDaysSit() {
 	suite.T().Run("not finding a contract year record", func(t *testing.T) {
 		twoYearsLaterPickupDate := ddasitTestRequestedPickupDate.AddDate(2, 0, 0)
 		_, err := priceDomesticAdditionalDaysSit(suite.DB(), models.ReServiceCodeDDASIT, DefaultContractCode, twoYearsLaterPickupDate, ddasitTestIsPeakPeriod, ddasitTestWeight, ddasitTestServiceArea, ddasitTestNumberOfDaysInSIT)
+		suite.Error(err)
+		suite.Contains(err.Error(), "could not fetch contract year")
+	})
+}
+
+func (suite *GHCRateEngineServiceSuite) Test_priceDomesticPickupDeliverySITSameZip3s() {
+	dshZipDest := "30907"
+	dshZipSITDest := "30901" // same zip3
+	dshDistance := unit.Miles(15)
+
+	suite.T().Run("destination golden path for same zip3s", func(t *testing.T) {
+		suite.setupDomesticServiceAreaPrice(models.ReServiceCodeDSH, dddsitTestServiceArea, dddsitTestIsPeakPeriod, dddsitTestDomesticServiceAreaBasePriceCents, dddsitTestEscalationCompounded)
+		priceCents, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dshZipDest, dshZipSITDest, dshDistance)
+		suite.NoError(err)
+		expectedPrice := unit.Cents(53187) // dddsitTestDomesticServiceAreaBasePriceCents * (dddsitTestWeight / 100) * distance * dddsitTestEscalationCompounded
+		suite.Equal(expectedPrice, priceCents)
+	})
+
+	suite.T().Run("invalid service code", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeCS, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dshZipDest, dshZipSITDest, dshDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "unsupported pickup/delivery SIT code")
+	})
+
+	suite.T().Run("invalid weight", func(t *testing.T) {
+		badWeight := unit.Pound(250)
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, badWeight, dddsitTestServiceArea, dddsitTestSchedule, dshZipDest, dshZipSITDest, dshDistance)
+		suite.Error(err)
+		expectedError := fmt.Sprintf("weight of %d less than the minimum", badWeight)
+		suite.Contains(err.Error(), expectedError)
+	})
+
+	suite.T().Run("bad destination zip", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, "309", dshZipSITDest, dshDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "invalid destination postal code")
+	})
+
+	suite.T().Run("bad SIT destination zip", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dshZipDest, "1234", dshDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "invalid SIT destination postal code")
+	})
+
+	suite.T().Run("error from shorthaul pricer", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, "BOGUS", dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dshZipDest, dshZipSITDest, dshDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "could not price shorthaul")
+	})
+}
+
+func (suite *GHCRateEngineServiceSuite) Test_priceDomesticPickupDeliverySIT50PlusMilesDiffZip3s() {
+	dlhZipDest := "30907"
+	dlhZipSITDest := "36106"       // different zip3
+	dlhDistance := unit.Miles(305) // > 50 miles
+
+	suite.T().Run("destination golden path for > 50 miles with different zip3s", func(t *testing.T) {
+		suite.setupDomesticLinehaulPrice(dddsitTestServiceArea, dddsitTestIsPeakPeriod, dddsitTestWeightLower, dddsitTestWeightUpper, dddsitTestMilesLower, dddsitTestMilesUpper, dddsitTestDomesticLinehaulBasePriceMillicents, dddsitTestEscalationCompounded)
+		priceCents, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dlhZipDest, dlhZipSITDest, dlhDistance)
+		suite.NoError(err)
+		expectedPriceMillicents := unit.Millicents(45944438) // dddsitTestDomesticLinehaulBasePriceMillicents * (dddsitTestWeight / 100) * distance * dddsitTestEscalationCompounded
+		expectedPrice := expectedPriceMillicents.ToCents()
+		suite.Equal(expectedPrice, priceCents)
+	})
+
+	suite.T().Run("error from linehaul pricer", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, "BOGUS", dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, dlhZipDest, dlhZipSITDest, dlhDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "could not price linehaul")
+	})
+}
+
+func (suite *GHCRateEngineServiceSuite) Test_priceDomesticPickupDeliverySIT50MilesOrLessDiffZip3s() {
+	domOtherZipDest := "30907"
+	domOtherZipSITDest := "29801"      // different zip3
+	domOtherDistance := unit.Miles(37) // <= 50 miles
+
+	suite.T().Run("destination golden path for <= 50 miles with different zip3s", func(t *testing.T) {
+		suite.setupDomesticOtherPrice(models.ReServiceCodeDDDSIT, dddsitTestSchedule, dddsitTestIsPeakPeriod, dddsitTestDomesticOtherBasePriceCents, dddsitTestEscalationCompounded)
+		priceCents, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, domOtherZipDest, domOtherZipSITDest, domOtherDistance)
+		suite.NoError(err)
+		expectedPrice := unit.Cents(58355) // dddsitTestDomesticOtherBasePriceCents * (dddsitTestWeight / 100) * dddsitTestEscalationCompounded
+		suite.Equal(expectedPrice, priceCents)
+	})
+
+	suite.T().Run("not finding a rate record", func(t *testing.T) {
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, "BOGUS", dddsitTestRequestedPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, domOtherZipDest, domOtherZipSITDest, domOtherDistance)
+		suite.Error(err)
+		suite.Contains(err.Error(), "could not fetch domestic destination SIT delivery rate")
+	})
+
+	suite.T().Run("not finding a contract year record", func(t *testing.T) {
+		twoYearsLaterPickupDate := dddsitTestRequestedPickupDate.AddDate(2, 0, 0)
+		_, err := priceDomesticPickupDeliverySIT(suite.DB(), models.ReServiceCodeDDDSIT, testdatagen.DefaultContractCode, twoYearsLaterPickupDate, dddsitTestIsPeakPeriod, dddsitTestWeight, dddsitTestServiceArea, dddsitTestSchedule, domOtherZipDest, domOtherZipSITDest, domOtherDistance)
 		suite.Error(err)
 		suite.Contains(err.Error(), "could not fetch contract year")
 	})
