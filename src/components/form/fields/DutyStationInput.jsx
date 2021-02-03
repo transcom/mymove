8e4be17d10d609b569ebb068a5545febd46eacdf
Ron Cruz <Ronolibert@gmail.com>
2021-02-03 16:08:52 +0000

    MB-6596 Fix prop validation eslint errors

--- src/components/form/fields/DutyStationInput.jsx
+++ src/components/form/fields/DutyStationInput.jsx
@@ -3,13 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { DutyStationSearchBox } from 'scenes/ServiceMembers/DutyStationSearchBox';
-import { DutyStationShape } from 'types/dutyStation';
 
 // TODO: refactor component when we can to make it more user friendly with Formik
 export const DutyStationInput = (props) => {
-  // eslint-disable-next-line react/prop-types
   const { label, name, displayAddress } = props;
-  // eslint-disable-next-line no-unused-vars
   const [field, meta, helpers] = useField(props);
 
   const errorString = meta.value?.name ? meta.error?.name || meta.error : '';
@@ -32,16 +29,12 @@ export const DutyStationInput = (props) => {
 DutyStationInput.propTypes = {
   // label displayed for input
   label: PropTypes.string.isRequired,
-  // duty station value
-  // eslint-disable-next-line react/no-unused-prop-types
-  value: DutyStationShape,
   // name is for the input
   name: PropTypes.string.isRequired,
   displayAddress: PropTypes.bool,
 };
 
 DutyStationInput.defaultProps = {
-  value: {},
   displayAddress: true,
 };
 
