2ae58677f808f101385d2426d2e5f38538b0a51b
Duncan <52669884+duncan-truss@users.noreply.github.com>
2021-02-12 17:16:25 -0500

    [mb-6434] Orders document viewer benchmark network device conditions (#5902)
    * setup config and command line args with scenario names
    
    * network comparison test table results with seperate configs
    
    * add CPU slowdown config and handle promise rejection errors

--- puppeteer/benchmarking.js
+++ puppeteer/benchmarking.js
@@ -1,67 +1,69 @@
-const puppeteer = require('puppeteer');
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+const commander = require('commander');
+const Conf = require('conf');
+const debug = require('debug')('debug');
 
-// Gets the total request time based on the responseEnd - requestStart
-// in secs
-const getTotalRequestTime = (navigationEntries = []) => {
-  if (navigationEntries.length === 0) {
-    return null;
+const { schema } = require('./constants');
+const { totalDuration } = require('./scenarios');
+
+const config = new Conf({
+  cwd: __dirname, // saves the config file to the same dir of this script
+  schema,
+});
+
+const program = new commander.Command();
+
+const runAction = async ({ scenario, measurementType, host, verbose }) => {
+  if (verbose) {
+    debug.enabled = true;
   }
+  console.log(`Running scenario ${scenario} with measurement ${measurementType}`);
 
-  let firstRequestStart = null;
-  let lastResponseEnd = null;
+  if (measurementType === 'total-duration') {
+    const elapsedTime = await totalDuration(host, config.store, debug).catch(() => {
+      process.exit(1);
+    });
 
-  navigationEntries.forEach((entry) => {
-    if (entry.entryType === 'resource' || entry.entryType === 'navigation') {
-      if (firstRequestStart === null || entry.requestStart < firstRequestStart) {
-        firstRequestStart = entry.requestStart;
-      }
+    console.log(`${elapsedTime} secs from performance entries`);
+  } else if (measurementType === 'network-comparison') {
+    const results = {};
+    // await cannot be used inside of a forEach loop
+    // eslint-disable-next-line no-restricted-syntax
+    for (const speed of ['fast', 'medium', 'slow']) {
+      const configStore = {};
+      Object.assign(configStore, config.store, { network: speed });
+      console.log(`Running network test with ${speed} profile`);
 
-      if (lastResponseEnd === null || entry.responseEnd > lastResponseEnd) {
-        lastResponseEnd = entry.responseEnd;
-      }
+      // Running these tests in parallel would likely skew the results
+      // eslint-disable-next-line no-await-in-loop
+      const elapsedTime = await totalDuration(host, configStore, debug).catch(() => {
+        process.exit(1);
+      });
+      results[`${speed}`] = { 'duration (seconds)': elapsedTime };
     }
-  });
 
-  return (lastResponseEnd - firstRequestStart) / 1000;
+    console.table(results);
+  }
 };
 
-(async () => {
-  const waitOptions = { waitUntil: 'networkidle0' };
-  // for debugging, set headless to false
-  const launchOptions = { headless: true };
-  const browser = await puppeteer.launch(launchOptions);
-  const page = await browser.newPage();
+program
+  .command('run', { isDefault: true })
+  .description('runs a benchmark test')
+  .addOption(
+    new commander.Option('-s, --scenario <scenario>', 'scenario is the page or workflow being tested')
+      .default('too-orders-document-viewer')
+      .choices(['too-orders-document-viewer'])
+      .makeOptionMandatory(),
+  )
+  .addOption(
+    new commander.Option('-m --measurement-type <type>', 'specifies the kind of performance output metrics to measure')
+      .default('total-duration')
+      .choices(['total-duration', 'network-comparison']),
+  )
+  .addOption(
+    new commander.Option('-h --host <host>', 'base host url to use including port').default('http://officelocal:3000'),
+  )
+  .addOption(new commander.Option('-v --verbose', 'shows verbose debugging info').default(false))
+  .action(runAction);
 
-  // Set windows height en width
-  await page.setViewport({ width: 1600, height: 900 });
-  await page.goto('http://officelocal:3000/devlocal-auth/login', waitOptions);
-
-  // Login by clicking button for existing user
-  const loginBtnSelector = 'button[value="9bda91d2-7a0c-4de1-ae02-b8cf8b4b858b"]';
-  await page.waitForSelector(loginBtnSelector);
-  await Promise.all([page.click(loginBtnSelector), page.waitForNavigation(waitOptions)]);
-
-  // grab first table data for locator
-  const locatorSelector = 'td[data-testid="locator-0"]';
-  await page.waitForSelector(locatorSelector);
-  const element = await page.$(locatorSelector);
-  const locatorValue = await page.evaluate((el) => el.textContent, element);
-
-  // go to a document viewer, orders
-  await page.goto(`http://officelocal:3000/moves/${locatorValue}/orders`, waitOptions);
-
-  const docViewerContentSelector = 'div[data-testid="DocViewerContent"]';
-  await page.waitForSelector(docViewerContentSelector);
-
-  // Will return all http requests and navigation performance on last navigation
-  const navigationEntries = JSON.parse(
-    await page.evaluate(() => {
-      return JSON.stringify(performance.getEntries());
-    }),
-  );
-
-  // eslint-disable-next-line no-console
-  console.log(getTotalRequestTime(navigationEntries), 'secs from performance entries');
-
-  await browser.close();
-})();
+program.parse(process.argv);
