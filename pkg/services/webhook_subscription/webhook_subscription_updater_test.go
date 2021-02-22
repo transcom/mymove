b1975a2e3ee0e690354ac86fcdee9bec1c29f2cd
Namibia Torres <namibia@truss.works>
2021-02-22 17:50:05 +0000

    update tests

--- pkg/services/webhook_subscription/webhook_subscription_updater_test.go
+++ pkg/services/webhook_subscription/webhook_subscription_updater_test.go
@@ -3,6 +3,9 @@ package webhooksubscription
 import (
 	"testing"
 
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/services"
 	"github.com/transcom/mymove/pkg/services/query"
 	"github.com/transcom/mymove/pkg/testdatagen"
 )
@@ -33,17 +36,24 @@ func (suite *WebhookSubscriptionServiceSuite) TestWebhookSubscriptionUpdater() {
 		suite.Equal(webhookSubscriptionID, foundWebhookSubscription.ID)
 	})
 
-	// suite.T().Run("Fails to update - return empty webhookSubscription and error", func(t *testing.T) {
-	// 	// fakeID, err := uuid.NewV4()
-	// 	// suite.NoError(err)
+	suite.T().Run("Fails to find correct webhookSuscription - return empty webhookSubscription and error", func(t *testing.T) {
+		fakeID, _ := uuid.NewV4()
+		webhookSubscription.ID = fakeID
 
-	// 	// filters := []services.QueryFilter{query.NewQueryFilter("id", "=", fakeID.String())}
-	// 	// var foundWebhookSubscription models.WebhookSubscription
+		_, err := updater.UpdateWebhookSubscription(&webhookSubscription)
 
-	// 	fakeWebhookSubscription, err := updater.UpdateWebhookSubscription(&webhookSubscription, eTag)
+		suite.Error(err)
+		suite.IsType(services.NotFoundError{}, err)
+	})
 
-	// 	suite.Error(err)
-	// 	suite.Equal(models.WebhookSubscription{}, fakeWebhookSubscription)
-	// })
+	suite.T().Run("Fails to update - return empty webhookSubscription and error", func(t *testing.T) {
+		badWebhookSubscription := testdatagen.MakeDefaultWebhookSubscription(suite.DB())
+		fakeID, _ := uuid.NewV4()
+		badWebhookSubscription.SubscriberID = fakeID
+
+		_, err := updater.UpdateWebhookSubscription(&badWebhookSubscription)
+
+		suite.Error(err)
+	})
 
 }
