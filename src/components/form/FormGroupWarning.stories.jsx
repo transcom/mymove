526dd65ef377567651f50d2d04bef58a169848ba
Shauna Keating <shauna@truss.works>
2021-02-23 02:28:42 +0000

    tries to fix the import path issue

--- src/components/form/FormGroupWarning.stories.jsx
+++ src/components/form/FormGroupWarning.stories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+
 import FormGroupWarning from './FormGroupWarning';
 
 export default {
   title: 'Components/Form',
-  component: FormGroupWarning,
 };
 
-export const FormFieldWithWarning = () => (
+export const FormGroupWithWarning = () => (
   <FormGroupWarning inputLabel="Warning Message" warningMessage="Helpful warning message text go here" />
 );
