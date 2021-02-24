920454a224a2be586326d47e348a4f7d38692bdc
Shauna Keating <shauna@truss.works>
2021-02-24 18:08:18 +0000

    adds return to form warning jsx and adds .isrequired to proptypes

--- src/components/form/FormGroupWarning.jsx
+++ src/components/form/FormGroupWarning.jsx
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 import { Label, TextInput } from '@trussworks/react-uswds';
 
 export const FormGroupWarning = ({ inputLabel, warningMessage }) => {
-  <div>
-    <Label htmlFor="input-type-text">{inputLabel}</Label>
-    <TextInput id="input-type-text" name="input-type-text" type="text" validationStatus="warning" />
-    <em>
-      <p className="usa-hint">{warningMessage}</p>
-    </em>
-  </div>;
+  return (
+    <div>
+      <Label htmlFor="input-type-text">{inputLabel}</Label>
+      <TextInput id="input-type-text" name="input-type-text" type="text" validationStatus="warning" />
+      <em>
+        <p className="usa-hint">{warningMessage}</p>
+      </em>
+    </div>
+  );
 };
 
 FormGroupWarning.propTypes = {
-  inputLabel: PropTypes.string,
-  warningMessage: PropTypes.string,
+  inputLabel: PropTypes.string.isRequired,
+  warningMessage: PropTypes.string.isRequired,
 };
 
 export default FormGroupWarning;
