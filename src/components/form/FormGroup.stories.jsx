444506d445e209761042bef734d2c87e7f5ead77
Shauna Keating <shauna@truss.works>
2021-02-25 15:56:12 +0000

    makes hint italic in warning, adds correct markup for error state

--- src/components/form/FormGroup.stories.jsx
+++ src/components/form/FormGroup.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormGroup, Label, TextInput } from '@trussworks/react-uswds';
+import { FormGroup, Label, TextInput, ErrorMessage } from '@trussworks/react-uswds';
 
 export default {
   title: 'Components/Form',
@@ -15,12 +15,19 @@ export const FormGroupWithWarning = () => (
   <FormGroup className="warning">
     <Label htmlFor="input-type-text">Text input label</Label>
     <TextInput id="input-type-text" name="input-type-text" type="text" />
+    <p className="usa-hint">
+      This TAC does not appear in TGET, so it might not be valid. Make sure it matches what&apos;s on the orders before
+      you continue.
+    </p>
   </FormGroup>
 );
 
 export const FormGroupWithError = () => (
   <FormGroup error>
-    <Label htmlFor="input-type-text">Text input label</Label>
-    <TextInput id="input-type-text" name="input-type-text" type="text" />
+    <Label htmlFor="input-type-text" error>
+      Text input label
+    </Label>
+    <ErrorMessage>Helpful error message</ErrorMessage>
+    <TextInput id="input-type-text" name="input-type-text" type="text" validationStatus="error" />
   </FormGroup>
 );
