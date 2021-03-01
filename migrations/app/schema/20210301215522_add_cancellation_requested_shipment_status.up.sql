9e3fe730522191b903eaabb97e1add738b4d3176
Sandy Wright <sandy@truss.works>
2021-03-01 16:12:25 -0600

    MB-5490 Add migration for CANCELLATION_REQUESTED shipment status enum value

---
+++ migrations/app/schema/20210301215522_add_cancellation_requested_shipment_status.up.sql
@@ -0,0 +1,7 @@
+ALTER TYPE mto_shipment_status ADD VALUE 'CANCELLATION_REQUESTED';
+COMMENT ON COLUMN mto_shipments.status IS 'The status of a shipment. The list of statuses includes:
+1. DRAFT
+2. SUBMITTED
+3. APPROVED
+4. REJECTED
+5. CANCELLATION REQUESTED';
