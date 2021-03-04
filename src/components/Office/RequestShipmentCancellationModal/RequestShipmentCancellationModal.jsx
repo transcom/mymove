465eee4ffabe732fee1d6da16362178546d3de69
Emily Mahanna <emahanna@truss.works>
2021-03-04 23:29:18 +0000

    fix styling

--- src/components/Office/RequestShipmentCancellationModal/RequestShipmentCancellationModal.jsx
+++ src/components/Office/RequestShipmentCancellationModal/RequestShipmentCancellationModal.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@trussworks/react-uswds';
 
-import styles from './RequestShipmentCancellationModal.module.scss';
-
 import Modal, { ModalTitle, ModalClose, ModalActions, connectModal } from 'components/Modal/Modal';
 
 export const RequestShipmentCancellationModal = ({ onClose, onSubmit }) => (
   <Modal>
-    <ModalClose className={styles.closeButton} handleClick={onClose} />
+    <ModalClose handleClick={onClose} />
     <ModalTitle>
       <h3>Request shipment cancellation</h3>
     </ModalTitle>
@@ -17,10 +15,10 @@ export const RequestShipmentCancellationModal = ({ onClose, onSubmit }) => (
       whether or not service items have been charged to the shipment yet.
     </p>
     <ModalActions>
-      <Button secondary className={styles.cancelButton} type="button" onClick={onClose} data-testid="modalBackButton">
+      <Button className="usa-button--tertiary" type="button" onClick={onClose} data-testid="modalBackButton">
         Back
       </Button>
-      <Button className={styles.requestButton} type="submit" onClick={onSubmit}>
+      <Button className="usa-button--destructive" type="submit" onClick={onSubmit}>
         Request Cancellation
       </Button>
     </ModalActions>
