b53740c1f8f309938a15b783fbeda1036edc7ad3
Felipe Lee <felipe@truss.works>
2021-03-05 15:23:29 -0600

    MB-6535 Update a few TAC validation tests to be table-driven

--- pkg/handlers/ghcapi/tac_test.go
+++ pkg/handlers/ghcapi/tac_test.go
@@ -14,41 +14,33 @@ func (suite *HandlerSuite) TestTacValidation() {
 	user := testdatagen.MakeOfficeUser(suite.DB(), testdatagen.Assertions{})
 	transportationAccountingCode := testdatagen.MakeTransportationAccountingCode(suite.DB(), testdatagen.Assertions{})
 
-	suite.T().Run("Successful Valid TAC validation", func(t *testing.T) {
-		tac := transportationAccountingCode.TAC
-		request := httptest.NewRequest("GET", fmt.Sprintf("/tac/valid?tac=%s", tac), nil)
-		request = suite.AuthenticateOfficeRequest(request, user)
-		params := tacop.TacValidationParams{
-			HTTPRequest: request,
-			Tac:         tac,
-		}
-		context := handlers.NewHandlerContext(suite.DB(), suite.TestLogger())
-		handler := TacValidationHandler{context}
-		response := handler.Handle(params)
+	tests := []struct {
+		tacCode string
+		isValid bool
+	}{
+		{tacCode: transportationAccountingCode.TAC, isValid: true},
+		{tacCode: "4EVR", isValid: false},
+	}
 
-		suite.Assertions.IsType(&tacop.TacValidationOK{}, response)
+	for _, tc := range tests {
+		suite.T().Run("Successful TAC validation", func(t *testing.T) {
+			request := httptest.NewRequest("GET", fmt.Sprintf("/tac/valid?tac=%s", tc.tacCode), nil)
+			request = suite.AuthenticateOfficeRequest(request, user)
+			params := tacop.TacValidationParams{
+				HTTPRequest: request,
+				Tac:         tc.tacCode,
+			}
+			context := handlers.NewHandlerContext(suite.DB(), suite.TestLogger())
+			handler := TacValidationHandler{context}
+			response := handler.Handle(params)
 
-		okResponse := response.(*tacop.TacValidationOK)
-		suite.True(*okResponse.Payload.IsValid)
-	})
+			suite.Assertions.IsType(&tacop.TacValidationOK{}, response)
 
-	suite.T().Run("Successful Invalid TAC validation", func(t *testing.T) {
-		tac := "4EVR"
-		request := httptest.NewRequest("GET", fmt.Sprintf("/tac/valid?tac=%s", tac), nil)
-		request = suite.AuthenticateOfficeRequest(request, user)
-		params := tacop.TacValidationParams{
-			HTTPRequest: request,
-			Tac:         tac,
-		}
-		context := handlers.NewHandlerContext(suite.DB(), suite.TestLogger())
-		handler := TacValidationHandler{context}
-		response := handler.Handle(params)
-
-		suite.Assertions.IsType(&tacop.TacValidationOK{}, response)
-
-		okResponse := response.(*tacop.TacValidationOK)
-		suite.False(*okResponse.Payload.IsValid)
-	})
+			okResponse := response.(*tacop.TacValidationOK)
+			suite.Equal(tc.isValid, *okResponse.Payload.IsValid,
+				"Expected %v validation to return %v, got %v", tc.tacCode, tc.isValid, *okResponse.Payload.IsValid)
+		})
+	}
 
 	suite.T().Run("Unknown user for TAC validation is unauthorized", func(t *testing.T) {
 		tac := "4EVR"
