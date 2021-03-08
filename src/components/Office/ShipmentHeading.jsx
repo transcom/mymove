c3860f73754f1cae3998f4313de525635f55d6d7
Abigail Young <abigail@truss.works>
2021-03-08 19:03:37 +0000

    Add request cancellation button and link styles

--- src/components/Office/ShipmentHeading.jsx
+++ src/components/Office/ShipmentHeading.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { Button } from '@trussworks/react-uswds';
 
 import { AddressShape } from '../../types/address';
 
@@ -19,10 +20,16 @@ function ShipmentHeading({ shipmentInfo }) {
   return (
     <div className={classNames(styles.shipmentHeading, 'shipment-heading')}>
       <h3 data-testid="office-shipment-heading-h3">{shipmentInfo.shipmentType}</h3>
-      <small>
-        {`${shipmentInfo.originCity}, ${shipmentInfo.originState} ${shipmentInfo.originPostalCode} to
+
+      <div className={styles.row}>
+        <small>
+          {`${shipmentInfo.originCity}, ${shipmentInfo.originState} ${shipmentInfo.originPostalCode} to
         ${formatDestinationAddress(shipmentInfo.destinationAddress)} on ${shipmentInfo.scheduledPickupDate}`}
-      </small>
+        </small>
+        <Button type="button" unstyled>
+          <small>Request Cancellation</small>
+        </Button>
+      </div>
     </div>
   );
 }
