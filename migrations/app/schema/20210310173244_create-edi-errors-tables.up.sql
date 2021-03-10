9c65f6e4444f2c8d6e5a858ac2e6a2925cb8ed1b
Jacqueline McKinney <jacqueline@truss.works>
2021-03-10 17:53:07 +0000

    edi errors database draft design

---
+++ migrations/app/schema/20210310173244_create-edi-errors-tables.up.sql
@@ -0,0 +1,45 @@
+create type edi_response_type as enum (
+    'EDI997',
+    'EDI824'
+    );
+
+create table edi_errors_acknowledgement_code_errors (
+    id uuid not null primary key,
+    code varchar,
+    description varchar,
+    source edi_response_type,
+    created_at timestamp not null,
+    updated_at timestamp not null,
+    deleted_at timestamp with time zone
+);
+
+create table edi_errors_technical_error_descriptions (
+     code varchar,
+     description varchar,
+     source edi_response_type,
+     id uuid not null primary key,
+     created_at timestamp not null,
+     updated_at timestamp not null,
+     deleted_at timestamp with time zone
+);
+
+create table edi_errors (
+    id uuid not null primary key,
+    payment_request_id uuid not null
+        constraint edi_errors_payment_request_id_fkey
+            references payment_requests,
+    edi_errors_technical_error_description_id uuid
+        constraint edi_errors_edi_errors_technical_error_description_id_fkey
+            references edi_errors_technical_error_descriptions,
+    edi_errors_acknowledgement_code_error_id uuid
+        constraint edi_errors_edi_errors_acknowledgement_code_error_id_fkey
+            references edi_errors_acknowledgement_code_errors,
+    created_at timestamp not null,
+    updated_at timestamp not null,
+    deleted_at timestamp with time zone
+);
+
+ALTER TYPE payment_request_status
+    ADD VALUE 'EDI_ERROR';
+
+COMMENT ON COLUMN payment_requests.status IS 'Track the status of the payment request through the system. PENDING by default at creation. Options: PENDING, REVIEWED, REVIEWED_AND_ALL_SERVICE_ITEMS_REJECTED, SENT_TO_GEX, RECEIVED_BY_GEX, PAID, EDI_ERROR';
