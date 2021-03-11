f59626003768e4f90d7167f7c83b2c2630ec2ef2
Donald Thai <donald@truss.works>
2021-03-11 01:32:07 +0000

    refactor data for storybook component

--- src/components/Office/ServiceItemCalculations/ServiceItemCalculations.stories.jsx
+++ src/components/Office/ServiceItemCalculations/ServiceItemCalculations.stories.jsx
@@ -15,4 +15,32 @@ export default {
   ],
 };
 
-export const Default = () => <ServiceItemCalculations />;
+const data = [
+  {
+    value: '85 cwt',
+    label: 'Billable weight (cwt)',
+    details: ['Shipment weight: 8,500 lbs', 'Estimated: 8,000'],
+  },
+  {
+    value: '2,337',
+    label: 'Mileage',
+    details: ['Zip 322 to Zip 919'],
+  },
+  {
+    value: '0.03',
+    label: 'Baseline linehaul price',
+    details: ['Domestic non-peak', 'Origin service area: 176', 'Pickup date: 24 Jan 2020'],
+  },
+  {
+    value: '1.033',
+    label: 'Price escalation factor',
+    details: null,
+  },
+  {
+    value: '$6.423',
+    label: ['Total amount requested'],
+    details: [],
+  },
+];
+
+export const Default = () => <ServiceItemCalculations calculations={data} />;
