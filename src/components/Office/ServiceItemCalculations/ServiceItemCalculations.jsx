55525a5816a8a044635b3c0943d6083d23a0ff20
Donald Thai <donald@truss.works>
2021-03-11 19:09:33 +0000

    remove unused commented out code and wrap label and details in div

--- src/components/Office/ServiceItemCalculations/ServiceItemCalculations.jsx
+++ src/components/Office/ServiceItemCalculations/ServiceItemCalculations.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './ServiceItemCalculations.module.scss';
-
-// import { formatCents, toDollarString } from 'shared/formatters';
-// import { PaymentServiceItemShape } from 'types';
 
 const ServiceItemCalculations = ({ calculations }) => {
   const appendSign = (index, length) => {
@@ -34,21 +30,23 @@ const ServiceItemCalculations = ({ calculations }) => {
                 {calc.value}
               </div>
               <hr />
-              <div className={styles.descriptionTitle}>{calc.label}</div>
-              <div className={styles.descriptionContent}>
-                {calc.details &&
-                  calc.details.map((detail, i) => {
-                    if (i === calc.details.length - 1) {
-                      return <React.Fragment key={detail}>{detail}</React.Fragment>;
-                    }
+              <div>
+                <div className={styles.descriptionTitle}>{calc.label}</div>
+                <div className={styles.descriptionContent}>
+                  {calc.details &&
+                    calc.details.map((detail, i) => {
+                      if (i === calc.details.length - 1) {
+                        return <React.Fragment key={detail}>{detail}</React.Fragment>;
+                      }
 
-                    // each item, add line breaks
-                    return (
-                      <React.Fragment key={detail}>
-                        {detail} <br />
-                      </React.Fragment>
-                    );
-                  })}
+                      // each item, add line breaks
+                      return (
+                        <React.Fragment key={detail}>
+                          {detail} <br />
+                        </React.Fragment>
+                      );
+                    })}
+                </div>
               </div>
             </div>
           );
@@ -57,71 +55,6 @@ const ServiceItemCalculations = ({ calculations }) => {
     </div>
   );
 };
-
-// Collection of calculations for the service item
-// const ServiceItemCalculations = () => {
-//   return (
-//     <div className={styles.ServiceItemCalculations}>
-//       <div className={styles.flexGrid}>
-//         <div className={styles.col}>
-//           <div className={styles.value}>85 cwt</div>
-//         </div>
-//         <div className={styles.col}>
-//           <div className={styles.value}>
-//             <span className={styles.multiplier}>X</span> 2,337
-//           </div>
-//         </div>
-//         <div className={styles.col}>
-//           <div className={styles.value}>
-//             <span className={styles.multiplier}>X</span> 0.03
-//           </div>
-//         </div>
-//         <div className={styles.col}>
-//           <div className={styles.value}>
-//             <span className={styles.multiplier}>X</span> 1.033
-//           </div>
-//         </div>
-//         <div className={styles.col}>
-//           <div className={styles.value}>
-//             <span className={styles.equal}>X</span> $6.423
-//           </div>
-//         </div>
-//       </div>
-//
-//       <hr />
-//       <div className={styles.flexGrid}>
-//         <div className={styles.col}>
-//           <div className={styles.descriptionTitle}>Billable weight (cwt)</div>
-//           <div className={styles.descriptionContent}>
-//             Shipment weight: 8,500 lbs <br /> Estimated: 8,000
-//           </div>
-//         </div>
-//
-//         <div className={styles.col}>
-//           <div className={styles.descriptionTitle}>Mileage</div>
-//           <div className={styles.descriptionContent}>Zip 322 to Zip 919</div>
-//         </div>
-//
-//         <div className={styles.col}>
-//           <div className={styles.descriptionTitle}>Baseline linehaul price</div>
-//           <div className={styles.descriptionContent}>
-//             Domestic non-peak <br /> Origin service area: 176 <br /> Pickup date: 24 Jan 2020
-//           </div>
-//         </div>
-//
-//         <div className={styles.col}>
-//           <div className={styles.descriptionTitle}>Price escalation factor</div>
-//           <div className={styles.descriptionContent} />
-//         </div>
-//
-//         <div className={styles.col}>
-//           <div className={styles.descriptionTitle}>Total amount request</div>
-//           <div className={styles.descriptionContent} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 ServiceItemCalculations.propTypes = {
   // collection of ordered calculations and last item is the Total amount requested
