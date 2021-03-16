3fdfeb2668b0a07154f61997fa8e1944369ebfc7
Suzanne Rozier <suz@truss.works>
2021-03-16 20:08:18 +0000

    Move validation schema to util file

--- src/components/form/AddressFields/AddressFields.stories.jsx
+++ src/components/form/AddressFields/AddressFields.stories.jsx
@@ -6,28 +6,11 @@ import { AddressFields } from './AddressFields';
 
 import { Form } from 'components/form/Form';
 import formStyles from 'styles/form.module.scss';
+import { requiredAddressSchema } from 'utils/validation';
 
 export default {
   title: 'Components/Fieldsets/AddressFields',
 };
-
-const requiredAddressSchema = Yup.object().shape({
-  street_address_1: Yup.string().required('Required'),
-  street_address_2: Yup.string(),
-  city: Yup.string().required('Required'),
-  state: Yup.string().length(2, 'Must use state abbreviation').required('Required'),
-  postal_code: Yup.string()
-    // RA Summary: eslint - security/detect-unsafe-regex - Denial of Service: Regular Expression
-    // RA: Locates potentially unsafe regular expressions, which may take a very long time to run, blocking the event loop
-    // RA: Per MilMove SSP, predisposing conditions are regex patterns from untrusted sources or unbounded matching.
-    // RA: The regex pattern is a constant string set at compile-time and it is bounded to 10 characters (zip code).
-    // RA Developer Status: Mitigated
-    // RA Validator Status:  Mitigated
-    // RA Modified Severity: N/A
-    // eslint-disable-next-line security/detect-unsafe-regex
-    .matches(/^(\d{5}([-]\d{4})?)$/, 'Must be valid zip code')
-    .required('Required'),
-});
 
 export const Basic = () => (
   <Formik
@@ -60,7 +43,7 @@ export const CurrentResidentialAddress = () => (
         postal_code: '',
       },
     }}
-    validationSchema={requiredAddressSchema}
+    validationSchema={Yup.object().shape({ residential_address: requiredAddressSchema.required() })}
   >
     {() => (
       <Form className={formStyles.form}>
@@ -81,7 +64,7 @@ export const CurrentResidentialAddressWithInitialValues = () => (
         postal_code: '10002',
       },
     }}
-    validationSchema={requiredAddressSchema}
+    validationSchema={Yup.object().shape({ residential_address: requiredAddressSchema.required() })}
   >
     {() => (
       <Form className={formStyles.form}>
