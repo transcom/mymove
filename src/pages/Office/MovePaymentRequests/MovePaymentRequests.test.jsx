e9816afcebc2af86c82c9917ed74f63c73d65812
hannaliebl <hannaliebl@users.noreply.github.com>
2021-03-17 17:19:15 +0000

    Move tests for MovePaymentRequests to react testing library, add tests for updating pending payment request count

--- src/pages/Office/MovePaymentRequests/MovePaymentRequests.test.jsx
+++ src/pages/Office/MovePaymentRequests/MovePaymentRequests.test.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, waitFor } from '@testing-library/react';
 
 import MovePaymentRequests from './MovePaymentRequests';
 
@@ -10,7 +11,10 @@ jest.mock('hooks/queries', () => ({
   useMovePaymentRequestsQueries: jest.fn(),
 }));
 
-const setUnapprovedShipmentCount = jest.fn();
+const testProps = {
+  setUnapprovedShipmentCount: jest.fn(),
+  setPendingPaymentRequestCount: jest.fn(),
+};
 
 const move = {
   contractor: {
@@ -83,46 +87,114 @@ const multiplePaymentRequests = {
   ],
 };
 
+const singleReviewedPaymentRequest = {
+  paymentRequests: [
+    {
+      id: '09474c6a-69b6-4501-8e08-670a12512e5f',
+      createdAt: '2020-12-01T00:00:00.000Z',
+      moveTaskOrderID: 'f8c2f97f-99e7-4fb1-9cc4-473debd04dbc',
+      paymentRequestNumber: '1843-9061-1',
+      status: 'REVIEWED',
+      moveTaskOrder: move,
+      serviceItems: [
+        {
+          id: '09474c6a-69b6-4501-8e08-670a12512a5f',
+          createdAt: '2020-12-01T00:00:00.000Z',
+          mtoServiceItemID: 'f8c2f97f-99e7-4fb1-9cc4-473debd24dbc',
+          priceCents: 2000001,
+          status: 'APPROVED',
+        },
+        {
+          id: '39474c6a-69b6-4501-8e08-670a12512a5f',
+          createdAt: '2020-12-01T00:00:00.000Z',
+          mtoServiceItemID: 'a8c2f97f-99e7-4fb1-9cc4-473debd24dbc',
+          priceCents: 4000001,
+          status: 'DENIED',
+          rejectionReason: 'Requested amount exceeds guideline',
+        },
+      ],
+      reviewedAt: '2020-12-01T00:00:00.000Z',
+    },
+  ],
+  mtoShipments: [
+    {
+      destinationAddress: { city: 'Princeton', state: 'NJ', postal_code: '08540' },
+      pickupAddress: { city: 'Boston', state: 'MA', postal_code: '02101' },
+    },
+  ],
+};
+
 const emptyPaymentRequests = {
   paymentRequests: [],
   mtoShipments: [],
 };
 
-describe('MovePaymentRequests', () => {
-  describe('multiple payment requests for move', () => {
-    useMovePaymentRequestsQueries.mockImplementation(() => multiplePaymentRequests);
+function renderMovePaymentRequests(props) {
+  return render(
+    <MockProviders initialEntries={[`/moves/L2BKD6/payment-requests`]}>
+      <MovePaymentRequests {...props} />
+    </MockProviders>,
+  );
+}
 
-    const testMoveCode = 'L2BKD6';
-    const component = mount(
-      <MockProviders initialEntries={[`/moves/${testMoveCode}/payment-requests`]}>
-        <MovePaymentRequests setUnapprovedShipmentCount={setUnapprovedShipmentCount} />
-      </MockProviders>,
-    );
+describe('MovePaymentRequests', () => {
+  describe('with multiple payment requests', () => {
+    beforeEach(() => {
+      useMovePaymentRequestsQueries.mockImplementation(() => multiplePaymentRequests);
+    });
 
     it('renders without errors', () => {
-      expect(component.find('h1').contains('Payment requests')).toBe(true);
+      const { getByText } = renderMovePaymentRequests(testProps);
+      expect(getByText('Payment requests')).toBeInTheDocument();
     });
 
-    it('renders mulitple payment requests', () => {
-      expect(component.find('PaymentRequestCard').length).toBe(2);
+    it('renders multiple payment requests', async () => {
+      const { getByText } = renderMovePaymentRequests(testProps);
+
+      await waitFor(() => {
+        multiplePaymentRequests.paymentRequests.forEach((pr) => {
+          expect(getByText(`Payment Request ${pr.paymentRequestNumber}`)).toBeInTheDocument();
+        });
+      });
     });
 
-    it('updates the unapproved shipments tag state', () => {
-      expect(setUnapprovedShipmentCount).toHaveBeenCalledWith(0);
+    it('updates the pending payment request count callback', async () => {
+      renderMovePaymentRequests(testProps);
+      await waitFor(() => {
+        expect(testProps.setPendingPaymentRequestCount).toHaveBeenCalledWith(1);
+      });
+    });
+
+    it('updates the unapproved shipments tag callback', async () => {
+      renderMovePaymentRequests(testProps);
+      await waitFor(() => {
+        expect(testProps.setUnapprovedShipmentCount).toHaveBeenCalledWith(0);
+      });
     });
   });
 
-  describe('no payment requests for move', () => {
-    useMovePaymentRequestsQueries.mockImplementation(() => emptyPaymentRequests);
+  describe('with one reviewed payment request', () => {
+    beforeEach(() => {
+      useMovePaymentRequestsQueries.mockImplementation(() => singleReviewedPaymentRequest);
+    });
+    it('updates the pending payment request count callback', async () => {
+      renderMovePaymentRequests(testProps);
+      await waitFor(() => {
+        expect(testProps.setPendingPaymentRequestCount).toHaveBeenCalledWith(0);
+      });
+    });
+  });
 
-    const component = mount(
-      <MockProviders initialEntries={[`/moves/FG7W32/payment-requests`]}>
-        <MovePaymentRequests setUnapprovedShipmentCount={setUnapprovedShipmentCount} />
-      </MockProviders>,
-    );
+  describe('with no payment requests for move', () => {
+    beforeEach(() => {
+      useMovePaymentRequestsQueries.mockImplementation(() => emptyPaymentRequests);
+    });
 
-    it('renders with empty message when no payment requests exist', () => {
-      expect(component.find('p').contains('No payment requests have been submitted for this move yet.')).toBe(true);
+    it('renders with empty message when no payment requests exist', async () => {
+      const { getByText } = renderMovePaymentRequests(testProps);
+      await waitFor(() => {
+        expect(getByText('No payment requests have been submitted for this move yet.')).toBeInTheDocument();
+      });
     });
   });
 });
