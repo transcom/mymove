be3f30bd41e507e66ccedb240badce93a6ca5af1
Yan Zhao <yan.zhao@truss.works>
2021-03-17 23:59:43 +0000

    Remove scss for ServiceMemberContactInfoFields
    - useRef for the uuid so that it doesn't change on every render
    - remove the scss since the styles can come from the form styles
    - use the Grid components to control the width

--- src/components/form/ServiceMemberContactInfoFields/index.jsx
+++ src/components/form/ServiceMemberContactInfoFields/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { func, node, string } from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { Checkbox, Label, Fieldset } from '@trussworks/react-uswds';
 
-import styles from './ServiceMemberContactInfoFields.module.scss';
-
+import formStyles from 'styles/form.module.scss';
 import TextField from 'components/form/fields/TextField';
 
 export const ServiceMemberContactInfoFields = ({
@@ -15,44 +14,52 @@ export const ServiceMemberContactInfoFields = ({
   name,
   render,
 }) => {
-  const ServiceMemberContactInfoFieldsUUID = uuidv4();
+  const ServiceMemberContactInfoFieldsUUID = useRef(uuidv4());
 
   return (
     <Fieldset legend={legend} className={className}>
       {render(
-        <div className={styles.ServiceMemberContactInfoFields}>
-          <TextField
-            className={styles.contactPhoneFields}
-            label="Best contact phone"
-            id={`phone_${ServiceMemberContactInfoFieldsUUID}`}
-            name={`${name}.phone`}
-            type="tel"
-            maxLength="10"
-          />
-          <TextField
-            className={styles.contactPhoneFields}
-            label="Alt. phone"
-            labelHint="Optional"
-            id={`alternatePhone_${ServiceMemberContactInfoFieldsUUID}`}
-            name={`${name}.alternatePhone`}
-            type="tel"
-            maxLength="10"
-          />
+        <>
+          <div className="grid-row grid-gap">
+            <div className="mobile-lg:grid-col-7">
+              <TextField
+                label="Best contact phone"
+                id={`phone_${ServiceMemberContactInfoFieldsUUID}`}
+                name={`${name}.phone`}
+                type="tel"
+                maxLength="10"
+              />
+            </div>
+          </div>
+          <div className="grid-row grid-gap">
+            <div className="mobile-lg:grid-col-7">
+              <TextField
+                label="Alt. phone"
+                labelHint="Optional"
+                id={`alternatePhone_${ServiceMemberContactInfoFieldsUUID}`}
+                name={`${name}.alternatePhone`}
+                type="tel"
+                maxLength="10"
+              />
+            </div>
+          </div>
           <TextField label="Personal email" id={`email_${ServiceMemberContactInfoFieldsUUID}`} name={`${name}.email`} />
           <Label>Preferred contact method</Label>
-          <Checkbox
-            id={`preferPhone_${ServiceMemberContactInfoFieldsUUID}`}
-            label="Phone"
-            name={`${name}.preferPhone`}
-            onChange={onChangePreferPhone}
-          />
-          <Checkbox
-            id={`prefer_email${ServiceMemberContactInfoFieldsUUID}`}
-            label="Email"
-            name={`${name}.preferEmail`}
-            onChange={onChangePreferEmail}
-          />
-        </div>,
+          <div className={formStyles.radioGroup}>
+            <Checkbox
+              id={`preferPhone_${ServiceMemberContactInfoFieldsUUID}`}
+              label="Phone"
+              name={`${name}.preferPhone`}
+              onChange={onChangePreferPhone}
+            />
+            <Checkbox
+              id={`prefer_email${ServiceMemberContactInfoFieldsUUID}`}
+              label="Email"
+              name={`${name}.preferEmail`}
+              onChange={onChangePreferEmail}
+            />
+          </div>
+        </>,
       )}
     </Fieldset>
   );
