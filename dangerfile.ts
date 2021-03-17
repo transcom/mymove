2091b3d43b87edd85424c0a8dfc932fb491811e4
Ron Cruz <Ronolibert@gmail.com>
2021-03-17 11:16:23 -0700

    Allow eslint-disable with rule to be a valid way to disable linter

--- dangerfile.ts
+++ dangerfile.ts
@@ -100,15 +100,15 @@ function doesLineHaveProhibitedOverride(disablingString) {
     .split(/[\s,]+/)
     .map((item) => item.trim());
   // disablingStringParts format: ['eslint-disable-next-line', 'no-jsx', 'no-default']
-  if (disablingStringParts[0] === 'eslint-disable') {
-    // fail because don't disable whole file please!
-    prohibitedOverrideMsg =
-      'Found `eslint-disable`. This disables the whole file, which is bad practice because it can allow security issues to slip in unnoticed. Please specify exact rules on a line by line basis, eg `eslint-disable-next-line no-underscore-dangle`.';
-  }
 
   if (disablingStringParts.length === 1) {
-    // fail because rule should be specified
-    prohibitedOverrideMsg = 'Must specify the rule you are disabling';
+    if (disablingStringParts[0] === 'eslint-disable') {
+      prohibitedOverrideMsg =
+        'Found `eslint-disable`. This disables the whole file, which is bad practice because it can allow security issues to slip in unnoticed. Please specify exact rules on a line by line basis, eg `eslint-disable-next-line no-underscore-dangle`.';
+    } else {
+      // fail because rule should be specified
+      prohibitedOverrideMsg = 'Must specify the rule you are disabling';
+    }
     return prohibitedOverrideMsg;
   }
 
