dd130734465da5bd4bbce26486b1df9974c5bc5e
Kim Allen <kim@truss.works>
2021-03-18 22:10:57 +0000

    Commit time-boxed linter fixes in BackupContact

--- src/scenes/ServiceMembers/BackupContact.jsx
+++ src/scenes/ServiceMembers/BackupContact.jsx
@@ -1,8 +1,8 @@
 import { get, pick } from 'lodash';
-import PropTypes from 'prop-types';
+import { func, shape, string } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getFormValues } from 'redux-form';
+import { getFormValues, reduxForm } from 'redux-form';
 
 import {
   updateServiceMember as updateServiceMemberAction,
@@ -15,15 +15,13 @@ import {
   getResponseError,
 } from 'services/internalApi';
 import { renderField, recursivelyAnnotateRequiredFields } from 'shared/JsonSchemaForm';
-import { reduxForm } from 'redux-form';
 import { no_op } from 'shared/utils';
-import WizardPage from 'shared/WizardPage';
+import ConnectedWizardPage from 'shared/WizardPage';
 import scrollToTop from 'shared/scrollToTop';
 import { selectServiceMemberFromLoggedInUser, selectBackupContacts } from 'store/entities/selectors';
 import requireCustomerState from 'containers/requireCustomerState/requireCustomerState';
 import { profileStates } from 'constants/customerStates';
-import { PageKeyShape, PageListShape } from 'types/customerShapes';
-
+import { PageKeyShape, PageListShape, BackupContactShape } from 'types/customerShapes';
 import SectionWrapper from 'components/Customer/SectionWrapper';
 
 import './BackupContact.css';
@@ -67,8 +65,8 @@ class ContactForm extends Component {
   }
 }
 
-const validateContact = (values, form) => {
-  let requiredErrors = {};
+const validateContact = (values) => {
+  const requiredErrors = {};
   ['name', 'email'].forEach((requiredFieldName) => {
     if (values[`${requiredFieldName}`] === undefined || values[`${requiredFieldName}`] === '') {
       requiredErrors[`${requiredFieldName}`] = 'Required.';
@@ -129,27 +127,26 @@ export class BackupContact extends Component {
 
             scrollToTop();
           });
-      } else {
-        return createBackupContactForServiceMember(serviceMemberId, payload)
-          .then((response) => {
-            updateBackupContact(response);
-          })
-          .then(() => getServiceMember(serviceMemberId))
-          .then((response) => {
-            updateServiceMember(response);
-          })
-          .catch((e) => {
-            // TODO - error handling - below is rudimentary error handling to approximate existing UX
-            // Error shape: https://github.com/swagger-api/swagger-js/blob/master/docs/usage/http-client.md#errors
-            const { response } = e;
-            const errorMessage = getResponseError(response, 'failed to create backup contact due to server error');
-            this.setState({
-              errorMessage,
-            });
-
-            scrollToTop();
-          });
       }
+      return createBackupContactForServiceMember(serviceMemberId, payload)
+        .then((response) => {
+          updateBackupContact(response);
+        })
+        .then(() => getServiceMember(serviceMemberId))
+        .then((response) => {
+          updateServiceMember(response);
+        })
+        .catch((e) => {
+          // TODO - error handling - below is rudimentary error handling to approximate existing UX
+          // Error shape: https://github.com/swagger-api/swagger-js/blob/master/docs/usage/http-client.md#errors
+          const { response } = e;
+          const errorMessage = getResponseError(response, 'failed to create backup contact due to server error');
+          this.setState({
+            errorMessage,
+          });
+
+          scrollToTop();
+        });
     }
 
     return Promise.resolve();
@@ -166,13 +163,13 @@ export class BackupContact extends Component {
     const { pages, pageKey } = this.props;
     const { isValid, isDirty, errorMessage } = this.state;
 
-    var [contact1] = this.props.currentBackupContacts;
+    const [contact1] = this.props.currentBackupContacts;
 
     // initialValues has to be null until there are values from the action since only the first values are taken
     const firstInitialValues = contact1 ? pick(contact1, ['name', 'email', 'telephone', 'permission']) : null;
 
     return (
-      <WizardPage
+      <ConnectedWizardPage
         handleSubmit={this.handleSubmit}
         pageList={pages}
         pageKey={pageKey}
@@ -187,15 +184,30 @@ export class BackupContact extends Component {
           handleSubmit={no_op}
           schema={this.props.schema}
         />
-      </WizardPage>
+      </ConnectedWizardPage>
     );
   }
 }
 
 BackupContact.propTypes = {
-  schema: PropTypes.object.isRequired,
+  schema: BackupContactShape.isRequired,
   pages: PageListShape.isRequired,
   pageKey: PageKeyShape.isRequired,
+  updateBackupContact: func.isRequired,
+  updateServiceMember: func.isRequired,
+  values: shape({
+    name: string,
+    telephone: string,
+    email: string,
+  }),
+};
+
+BackupContact.defaultProps = {
+  values: {
+    name: '',
+    telephone: '',
+    email: '',
+  },
 };
 
 const mapDispatchToProps = {
