887859761443d04370aa6a30b9bcf35eb7857408
pearl-truss <pearl@truss.works>
2021-03-19 16:25:22 +0000

    fixed formatting in migration

--- migrations/app/schema/20210318162232_create_edi_processing_table.up.sql
+++ migrations/app/schema/20210318162232_create_edi_processing_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TYPE edi_type AS ENUM (
-    '997',
-    '824',
+	'997',
+	'824',
 	'810',
 	'858'
 );
@@ -8,11 +8,11 @@ CREATE TYPE edi_type AS ENUM (
 CREATE TABLE edi_processing
 (
 	id uuid
-	    constraint edi_processing_pkey primary key,
+		constraint edi_processing_pkey primary key,
 	edi_type edi_type NOT NULL,
-    num_edis_processed INT NOT NULL,
+	num_edis_processed INT NOT NULL,
 	process_started_at timestamp NOT NULL,
 	process_ended_at timestamp NOT NULL,
-	created_at timestamp not null,
-    updated_at timestamp not null
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL
 );
