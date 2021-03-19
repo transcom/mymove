8d1393ee89fd87d0c9af3376d48417e7a64c7cf5
Marty Boren <marty@truss.works>
2021-03-19 00:43:58 +0000

    Make SyncadaReaderSFTPSession private & temporarily comment out deletion

--- pkg/services/invoice/syncada_sftp_reader.go
+++ pkg/services/invoice/syncada_sftp_reader.go
@@ -12,22 +12,22 @@ import (
 	"github.com/transcom/mymove/pkg/services"
 )
 
-// SyncadaReaderSFTPSession contains information to create a new Syncada SFTP session
-type SyncadaReaderSFTPSession struct {
+// syncadaReaderSFTPSession contains information to create a new Syncada SFTP session
+type syncadaReaderSFTPSession struct {
 	client services.SFTPClient
 	logger Logger
 }
 
 // InitNewSyncadaSFTPReaderSession initialize a NewSyncadaSFTPSession and return services.SyncadaSFTPReader
 func InitNewSyncadaSFTPReaderSession(client services.SFTPClient, logger Logger) services.SyncadaSFTPReader {
-	return &SyncadaReaderSFTPSession{
+	return &syncadaReaderSFTPSession{
 		client,
 		logger,
 	}
 }
 
 // FetchAndProcessSyncadaFiles downloads Syncada files with SFTP, processes them using the provided processor, and deletes them from the SFTP server if they were successfully processed
-func (s *SyncadaReaderSFTPSession) FetchAndProcessSyncadaFiles(syncadaPath string, lastRead time.Time, processor services.SyncadaFileProcessor) error {
+func (s *syncadaReaderSFTPSession) FetchAndProcessSyncadaFiles(syncadaPath string, lastRead time.Time, processor services.SyncadaFileProcessor) error {
 	fileList, err := s.client.ReadDir(syncadaPath)
 	if err != nil {
 		s.logger.Error("Error reading SFTP directory", zap.String("directory", syncadaPath))
@@ -55,17 +55,18 @@ func (s *SyncadaReaderSFTPSession) FetchAndProcessSyncadaFiles(syncadaPath strin
 				continue
 			}
 
-			err = s.client.Remove(filePath)
-			if err != nil {
-				s.logger.Error("Error while deleting Syncada file", zap.String("path", filePath))
-			}
+			// TODO Commenting this out until I figure out how to send a new EDI
+			//err = s.client.Remove(filePath)
+			//if err != nil {
+			//	s.logger.Error("Error while deleting Syncada file", zap.String("path", filePath))
+			//}
 		}
 	}
 
 	return nil
 }
 
-func (s *SyncadaReaderSFTPSession) downloadFile(path string) (string, error) {
+func (s *syncadaReaderSFTPSession) downloadFile(path string) (string, error) {
 	file, err := s.client.Open(path)
 	if err != nil {
 		// TODO this will happen all the time and is not a big deal, worth logging?
