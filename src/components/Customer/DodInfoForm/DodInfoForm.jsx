f20be67c051a615b7529d6c015d2991e08b443a2
Suzanne Rozier <suz@truss.works>
2021-03-23 16:23:24 +0000

    Add new components and tests for DOD Info page and form

---
+++ src/components/Customer/DodInfoForm/DodInfoForm.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { Form } from 'components/form/Form';
+import TextField from 'components/form/fields/TextField';
+import { DropdownInput } from 'components/form/fields/DropdownInput';
+import SectionWrapper from 'components/Customer/SectionWrapper';
+import formStyles from 'styles/form.module.scss';
+import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
+import { dropdownInputOptions } from 'shared/formatters';
+import { ORDERS_RANK_OPTIONS } from 'constants/orders';
+import { SERVICE_MEMBER_AGENCY_LABELS } from 'content/serviceMemberAgencies';
+
+const DodInfoForm = ({ initialValues, onBack, onSubmit }) => {
+  // TODO - EDIPI should only accept numeric input
+
+  const branchOptions = dropdownInputOptions(SERVICE_MEMBER_AGENCY_LABELS);
+  const rankOptions = dropdownInputOptions(ORDERS_RANK_OPTIONS);
+
+  const validationSchema = Yup.object().shape({
+    affiliation: Yup.string().required('Required'),
+    edipi: Yup.string().required('Required'),
+    rank: Yup.string().required('Required'),
+  });
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      {({ isValid, handleSubmit, isSubmitting }) => {
+        return (
+          <Form className={formStyles.form}>
+            <h1>Create your profile</h1>
+            <p>Before we can schedule your move, we need to know a little more about you.</p>
+            <SectionWrapper>
+              <DropdownInput
+                label="Branch of service"
+                name="affiliation"
+                id="affiliation"
+                required
+                options={branchOptions}
+              />
+              <TextField
+                label="DoD ID number"
+                name="edipi"
+                id="edipi"
+                required
+                maxLength="10"
+                inputMode="numeric"
+                pattern="[0-9]{10}"
+              />
+              <DropdownInput label="Rank" name="rank" id="rank" required options={rankOptions} />
+            </SectionWrapper>
+
+            <div className={formStyles.formActions}>
+              <WizardNavigation
+                onBackClick={onBack}
+                disableNext={!isValid || isSubmitting}
+                onNextClick={handleSubmit}
+              />
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+DodInfoForm.propTypes = {
+  initialValues: PropTypes.shape({
+    affiliation: PropTypes.string,
+    edipi: PropTypes.string,
+    rank: PropTypes.string,
+  }).isRequired,
+  onBack: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default DodInfoForm;
---
+++ src/components/Customer/DodInfoForm/DodInfoForm.stories.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import DodInfoForm from './DodInfoForm';
+
+export default {
+  title: 'Customer Components / Forms / DOD Info Form',
+};
+
+export const DefaultState = () => <DodInfoForm />;
