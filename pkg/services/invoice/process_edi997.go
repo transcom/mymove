9adfdf3b671234b556d01f167377af5a3e8d6aaf
pearl-truss <pearl@truss.works>
2021-03-25 03:16:10 +0000

    fixed and added unit test for validating 997 processing

--- pkg/services/invoice/process_edi997.go
+++ pkg/services/invoice/process_edi997.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gobuffalo/pop/v5"
 
 	ediResponse997 "github.com/transcom/mymove/pkg/edi/edi997"
+	edi "github.com/transcom/mymove/pkg/edi/invoice"
 	"github.com/transcom/mymove/pkg/services"
 )
 
@@ -30,33 +31,53 @@ func (e *edi997Processor) ProcessEDI997(stringEDI997 string) (ediResponse997.EDI
 	edi997 := ediResponse997.EDI{}
 	err := edi997.Parse(stringEDI997)
 	if err != nil {
-		// e.logger.Error()
 		return edi997, err
 	}
 	var paymentRequestNumber = "2"
+	errString := ""
+	ediHeader := edi.InvoiceResponseHeader{}
+	ediHeader.ISA = edi997.InterchangeControlEnvelope.ISA
+	ediHeader.GS = edi997.InterchangeControlEnvelope.FunctionalGroups[0].GS
+	// validate header fields
+	err = ValidateEDIHeader(ediHeader, paymentRequestNumber, e.logger)
+	if err != nil {
+		errString += err.Error()
+	}
 
-	// validate header messages
-	isa := edi997.InterchangeControlEnvelope.ISA
-	icn := isa.InterchangeControlNumber
-	if icn < 1 || icn > 1000000000 {
-		return edi997, fmt.Errorf("Invalid InterchangeControlNumber: %d for PaymentRequestNumber: %s", icn, paymentRequestNumber)
+	// validate 997 specific fields
+	for _, functionalGroup := range edi997.InterchangeControlEnvelope.FunctionalGroups {
+		for _, transactionSet := range functionalGroup.TransactionSets {
+
+			functionalGroupResponse := transactionSet.FunctionalGroupResponse
+			transactionSetResponses := functionalGroupResponse.TransactionSetResponses
+			ak1 := functionalGroupResponse.AK1
+			functionalIDCode := strings.TrimSpace(ak1.FunctionalIdentifierCode)
+			if functionalIDCode != "SI" {
+				msg := fmt.Sprintf("Invalid FunctionalIdentifierCode in AK1 segment: %s  for PaymentRequestNumber: %s", functionalIDCode, paymentRequestNumber)
+				e.logger.Error(msg)
+				errString += msg + "\n"
+			}
+			for _, transactionSetResponse := range transactionSetResponses {
+				ak2 := transactionSetResponse.AK2
+				transactionSetIdentifierCode := strings.TrimSpace(ak2.TransactionSetIdentifierCode)
+				if transactionSetIdentifierCode != "858" {
+					msg := fmt.Sprintf("Invalid TransactionSetIdentifierCode in AK2 segment: %s for PaymentRequestNumber: %s", transactionSetIdentifierCode, paymentRequestNumber)
+					e.logger.Error(msg)
+					errString += msg + "\n"
+				}
+
+				ak5 := transactionSetResponse.AK5
+				transactionSetAcknowledgmentCode := strings.TrimSpace(ak5.TransactionSetAcknowledgmentCode)
+				if transactionSetAcknowledgmentCode != "A" {
+					msg := fmt.Sprintf("Invalid TransactionSetAcknowledgmentCode in AK5 segment: %s for PaymentRequestNumber: %s", transactionSetAcknowledgmentCode, paymentRequestNumber)
+					e.logger.Error(msg)
+					errString += msg + "\n"
+				}
+			}
+		}
 	}
-	ackRequested := isa.AcknowledgementRequested
-	if ackRequested != 0 && ackRequested != 1 {
-		return edi997, fmt.Errorf("Invalid AcknowledgementRequested field: %d for PaymentRequestNumber: %s", ackRequested, paymentRequestNumber)
-	}
-	usageIndicator := strings.TrimSpace(isa.UsageIndicator)
-	if usageIndicator != "T" && usageIndicator != "P" {
-		return edi997, fmt.Errorf("Invalid UsageIndicator field: %s for PaymentRequestNumber: %s", usageIndicator, paymentRequestNumber)
-	}
-	gs := edi997.InterchangeControlEnvelope.FunctionalGroups[0].GS
-	functionalIDCode := strings.TrimSpace(gs.FunctionalIdentifierCode)
-	if functionalIDCode != "SI" {
-		return edi997, fmt.Errorf("Invalid FunctionalIdentifierCode field: %s for PaymentRequestNumber: %s", functionalIDCode, paymentRequestNumber)
-	}
-	gcn := gs.GroupControlNumber
-	if gcn < 1 || gcn > 1000000000 {
-		return edi997, fmt.Errorf("Invalid GroupControlNumber: %d for PaymentRequestNumber: %s", gcn, paymentRequestNumber)
+	if errString != "" {
+		return edi997, fmt.Errorf(errString)
 	}
 	return edi997, nil
 
@@ -68,4 +89,46 @@ func (e *edi997Processor) ProcessEDI997(stringEDI997 string) (ediResponse997.EDI
 	// 	return reviewedPaymentRequests, services.NewQueryError("PaymentRequests", err, fmt.Sprintf("Could not find reviewed payment requests: %s", err))
 	// }
 	// return reviewedPaymentRequests, err
+}
+
+// ValidateEDIHeader validates an EDI header for a 997 or 824
+func ValidateEDIHeader(ediHeader edi.InvoiceResponseHeader, paymentRequestNumber string, logger Logger) error {
+	returnErr := ""
+	isa := ediHeader.ISA
+	icn := isa.InterchangeControlNumber
+
+	if icn < 1 || icn > 1000000000 {
+		msg := fmt.Sprintf("Invalid InterchangeControlNumber in ISA segment: %d for PaymentRequestNumber: %s", icn, paymentRequestNumber)
+		logger.Error(msg)
+		returnErr += msg + "\n"
+	}
+	ackRequested := isa.AcknowledgementRequested
+	if ackRequested != 0 && ackRequested != 1 {
+		msg := fmt.Sprintf("Invalid AcknowledgementRequested in ISA segment: %d for PaymentRequestNumber: %s", ackRequested, paymentRequestNumber)
+		logger.Error(msg)
+		returnErr += msg + "\n"
+	}
+	usageIndicator := strings.TrimSpace(isa.UsageIndicator)
+	if usageIndicator != "T" && usageIndicator != "P" {
+		msg := fmt.Sprintf("Invalid UsageIndicator in ISA segment %s for PaymentRequestNumber: %s", usageIndicator, paymentRequestNumber)
+		logger.Error(msg)
+		returnErr += msg + "\n"
+	}
+	gs := ediHeader.GS
+	functionalIDCode := strings.TrimSpace(gs.FunctionalIdentifierCode)
+	if functionalIDCode != "SI" {
+		msg := fmt.Sprintf("Invalid FunctionalIdentifierCode in GS segment: %s for PaymentRequestNumber: %s", functionalIDCode, paymentRequestNumber)
+		logger.Error(msg)
+		returnErr += msg + "\n"
+	}
+	gcn := gs.GroupControlNumber
+	if gcn < 1 || gcn > 1000000000 {
+		msg := fmt.Sprintf("Invalid GroupControlNumber in GS segment: %d  for PaymentRequestNumber: %s", gcn, paymentRequestNumber)
+		logger.Error(msg)
+		returnErr += msg + "\n"
+	}
+	if returnErr != "" {
+		return fmt.Errorf(returnErr)
+	}
+	return nil
 }
--- pkg/services/invoice/process_edi997_test.go
+++ pkg/services/invoice/process_edi997_test.go
@@ -2,7 +2,10 @@ package invoice
 
 import (
 	"log"
+	"strings"
 	"testing"
+
+	edisegment "github.com/transcom/mymove/pkg/edi/segment"
 
 	"github.com/stretchr/testify/suite"
 
@@ -14,7 +17,6 @@ import (
 type ProcessEDI997Suite struct {
 	testingsuite.PopTestSuite
 	logger *zap.Logger
-	// icnSequencer sequence.Sequencer
 }
 
 func (suite *ProcessEDI997Suite) SetupTest() {
@@ -29,7 +31,6 @@ func TestProcessEDI997Suite(t *testing.T) {
 		PopTestSuite: testingsuite.NewPopTestSuite(testingsuite.CurrentPackage()),
 		logger:       zap.NewNop(), // Use a no-op logger during testing
 	}
-	// ts.icnSequencer = sequence.NewDatabaseSequencer(ts.DB(), ediinvoice.ICNSequenceName)
 
 	suite.Run(t, ts)
 	ts.PopTestSuite.TearDown()
@@ -56,70 +57,10 @@ IEA*1*000000022
 		suite.NoError(err)
 	})
 
-	suite.T().Run("fails when there's an invalid ICN", func(t *testing.T) {
-		sample997EDIString := `
-ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*2000000000*0*T*:
-GS*SI*8004171844*MILMOVE*20210217*152945*220001*X*004010
-ST*997*0001
-AK1*SI*100001251
-AK2*858*0001
-
-
-AK5*A
-AK9*A*1*1*1
-SE*6*0001
-GE*1*220001
-IEA*1*000000022
-`
-		_, err := edi997Processor.ProcessEDI997(sample997EDIString)
-		suite.Error(err, "fail to process 997")
-		suite.Contains(err.Error(), "Invalid InterchangeControlNumber")
-	})
-
-	suite.T().Run("fails when there's an invalid AcknowledgementRequested field", func(t *testing.T) {
-		sample997EDIString := `
-ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*000000022*8*T*:
-GS*SI*8004171844*MILMOVE*20210217*152945*220001*X*004010
-ST*997*0001
-AK1*SI*100001251
-AK2*858*0001
-
-
-AK5*A
-AK9*A*1*1*1
-SE*6*0001
-GE*1*220001
-IEA*1*000000022
-`
-		_, err := edi997Processor.ProcessEDI997(sample997EDIString)
-		suite.Error(err, "fail to process 997")
-		suite.Contains(err.Error(), "Invalid AcknowledgementRequested")
-	})
-
-	suite.T().Run("fails when there's an invalid UsageIndicator field", func(t *testing.T) {
-		sample997EDIString := `
-ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*000000022*0*A*:
-GS*SI*8004171844*MILMOVE*20210217*152945*220001*X*004010
-ST*997*0001
-AK1*SI*100001251
-AK2*858*0001
-
-
-AK5*A
-AK9*A*1*1*1
-SE*6*0001
-GE*1*220001
-IEA*1*000000022
-`
-		_, err := edi997Processor.ProcessEDI997(sample997EDIString)
-		suite.Error(err, "fail to process 997")
-		suite.Contains(err.Error(), "Invalid UsageIndicator")
-	})
-
-	suite.T().Run("fails when there's an invalid FunctionalIdentifierCode field", func(t *testing.T) {
+	suite.T().Run("successfully create a valid segments", func(t *testing.T) {
 		sample997EDIString := `
 ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*000000022*0*T*:
-GS*FA*8004171844*MILMOVE*20210217*152945*220001*X*004010
+GS*SI*8004171844*MILMOVE*20210217*152945*220001*X*004010
 ST*997*0001
 AK1*SI*100001251
 AK2*858*0001
@@ -131,28 +72,85 @@ SE*6*0001
 GE*1*220001
 IEA*1*000000022
 `
-		_, err := edi997Processor.ProcessEDI997(sample997EDIString)
-		suite.Error(err, "fail to process 997")
-		suite.Contains(err.Error(), "Invalid FunctionalIdentifierCode")
+		edi, err := edi997Processor.ProcessEDI997(sample997EDIString)
+		suite.NoError(err)
+		functionalGroup := edi.InterchangeControlEnvelope.FunctionalGroups[0]
+		transactionSet := functionalGroup.TransactionSets[0]
+		transactionSetResponses := transactionSet.FunctionalGroupResponse.TransactionSetResponses[0]
+		suite.IsType(edisegment.ISA{}, edi.InterchangeControlEnvelope.ISA)
+		suite.IsType(edisegment.IEA{}, edi.InterchangeControlEnvelope.IEA)
+		suite.IsType(edisegment.GS{}, functionalGroup.GS)
+		suite.IsType(edisegment.GE{}, functionalGroup.GE)
+		suite.IsType(edisegment.ST{}, transactionSet.ST)
+		suite.IsType(edisegment.SE{}, transactionSet.SE)
+		suite.IsType(edisegment.AK1{}, transactionSet.FunctionalGroupResponse.AK1)
+		suite.IsType(edisegment.AK9{}, transactionSet.FunctionalGroupResponse.AK9)
+		suite.IsType(edisegment.AK2{}, transactionSetResponses.AK2)
+		suite.IsType(edisegment.AK5{}, transactionSetResponses.AK5)
 	})
 
-	suite.T().Run("fails when there's an invalid GroupControlNumber field", func(t *testing.T) {
+	suite.T().Run("fails when there are validation errors on EDI fields", func(t *testing.T) {
 		sample997EDIString := `
-ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*000000022*0*T*:
-GS*SI*8004171844*MILMOVE*20210217*152945*22000000001*X*004010
+ISA*00*          *00*          *12*8004171844     *ZZ*MILMOVE        *210217*1530*U*00401*2000000000*8*A*:
+GS*FA*8004171844*MILMOVE*20210217*152945*22000000001*X*004010
 ST*997*0001
-AK1*SI*100001251
-AK2*858*0001
-
-
-AK5*A
+AK1*FA*100001251
+AK2*909*0001
+AK3*ab*123
+AK4*1*2*3*4*MM*bad data goes here 89
+AK3*ab*124
+AK4*1*2*3*4*MM*bad data goes here 100
+AK5*Q
 AK9*A*1*1*1
 SE*6*0001
+ST*997*0002
+AK1*FA*100001251
+AK2*900*0001
+AK3*ab*123
+AK4*1*2*3*4*MM*bad data goes here 90
+AK5*B
+SE*6*0002
 GE*1*220001
+GS*FA*8004171844*MILMOVE*20210217*152945*220002*X*004010
+ST*997*0001
+AK1*VV*100001251
+AK2*123*0001
+AK3*ab*123
+AK4*1*2*3*4*MM*bad data goes here 93
+AK5*C
+AK9*A*1*1*1
+SE*6*0001
+GE*1*220002
 IEA*1*000000022
 `
 		_, err := edi997Processor.ProcessEDI997(sample997EDIString)
 		suite.Error(err, "fail to process 997")
-		suite.Contains(err.Error(), "Invalid GroupControlNumber")
+		errString := err.Error()
+		actualErrors := strings.Split(errString, "\n")
+		testData := []struct {
+			TestName         string
+			ExpectedErrorMsg string
+		}{
+			{TestName: "Invalid ICN", ExpectedErrorMsg: "Invalid InterchangeControlNumber in ISA"},
+			{TestName: "Invalid AcknowledgementRequested", ExpectedErrorMsg: "Invalid AcknowledgementRequested in ISA"},
+			{TestName: "Invalid UsageIndicator", ExpectedErrorMsg: "Invalid UsageIndicator in ISA"},
+			{TestName: "Invalid FunctionalIdentifierCode", ExpectedErrorMsg: "Invalid FunctionalIdentifierCode in GS"},
+			{TestName: "Invalid GroupControlNumber", ExpectedErrorMsg: "Invalid GroupControlNumber in GS"},
+			{TestName: "Invalid FunctionalIdentifierCode", ExpectedErrorMsg: "Invalid FunctionalIdentifierCode in AK1"},
+			{TestName: "Invalid TransactionSetIdentifierCode", ExpectedErrorMsg: "Invalid TransactionSetIdentifierCode in AK2"},
+			{TestName: "Invalid TransactionSetAcknowledgmentCode", ExpectedErrorMsg: "Invalid TransactionSetAcknowledgmentCode in AK5"},
+			{TestName: "Second AK1 failure for Invalid FunctionalIdentifierCode", ExpectedErrorMsg: "Invalid FunctionalIdentifierCode in AK1"},
+			{TestName: "Second AK2 Invalid TransactionSetIdentifierCode", ExpectedErrorMsg: "Invalid TransactionSetIdentifierCode in AK2"},
+			{TestName: "Second AK5 Invalid TransactionSetAcknowledgmentCode", ExpectedErrorMsg: "Invalid TransactionSetAcknowledgmentCode in AK5"},
+			{TestName: "Third (in second functionalGroupEnvelope) AK1 failure for Invalid FunctionalIdentifierCode", ExpectedErrorMsg: "Invalid FunctionalIdentifierCode in AK1"},
+			{TestName: "Third (in second functionalGroupEnvelope) AK2 Invalid TransactionSetIdentifierCode", ExpectedErrorMsg: "Invalid TransactionSetIdentifierCode in AK2"},
+			{TestName: "Third (in second functionalGroupEnvelope) AK5 Invalid TransactionSetAcknowledgmentCode", ExpectedErrorMsg: "Invalid TransactionSetAcknowledgmentCode in AK5"},
+		}
+
+		for i, data := range testData {
+			suite.T().Run(data.TestName, func(t *testing.T) {
+				suite.Contains(actualErrors[i], data.ExpectedErrorMsg)
+			})
+		}
 	})
 }
