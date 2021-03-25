3f2cd224911bd3b9e4710663d87c120dc9c75f7a
John Gedeon <john@truss.works>
2021-03-25 05:37:35 +0000

    Correct sql migration typo

--- migrations/app/schema/20210323204715_add_payment_request_to_interchange_control_number.up.sql
+++ migrations/app/schema/20210323204715_add_payment_request_to_interchange_control_number.up.sql
@@ -4,8 +4,8 @@ CREATE TABLE payment_request_to_interchange_control_numbers
     payment_request_id uuid NOT NULL,
     interchange_control_number int NOT NULL,
     constraint payment_request_id_interchange_control_number_unique_key unique (payment_request_id, interchange_control_number)
-)
+);
 
-COMMENT ON COLUMN payment_request_to_interchange_control_numbers.id is 'The id of this record';
-COMMENT ON COLUMN payment_request_to_interchange_control_numbers.payment_request_id is 'The id of the associated payment request';
-COMMENT ON COLUMN payment_request_to_interchange_control_numbers.interchange_control_number is 'The interchange control number generated in the out going EDI 858 invoice';
+COMMENT ON COLUMN payment_request_to_interchange_control_numbers.id IS 'The id of this record';
+COMMENT ON COLUMN payment_request_to_interchange_control_numbers.payment_request_id IS 'The id of the associated payment request';
+COMMENT ON COLUMN payment_request_to_interchange_control_numbers.interchange_control_number IS 'The interchange control number generated in the out going EDI 858 invoice';
