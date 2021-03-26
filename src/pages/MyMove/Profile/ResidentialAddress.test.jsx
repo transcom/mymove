c8eaf75e8729d5fc010f2fd3b42ea224bd21773a
Felipe Lee <felipe@truss.works>
2021-03-26 13:33:08 -0500

    MB-5319 Add/update tests for ResidentialAddress page

--- src/pages/MyMove/Profile/ResidentialAddress.test.jsx
+++ src/pages/MyMove/Profile/ResidentialAddress.test.jsx
@@ -1,11 +1,181 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { mount } from 'enzyme';
 import * as reactRedux from 'react-redux';
 import { push } from 'connected-react-router';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { MockProviders } from 'testUtils';
-import ConnectedResidentialAddress from 'scenes/ServiceMembers/ResidentialAddress';
+import ConnectedResidentialAddress, { ResidentialAddress } from 'pages/MyMove/Profile/ResidentialAddress';
+import { customerRoutes } from 'constants/routes';
+import { patchServiceMember } from 'services/internalApi';
+import { ValidateZipRateData } from 'shared/api';
+
+jest.mock('services/internalApi', () => ({
+  ...jest.requireActual('services/internalApi'),
+  patchServiceMember: jest.fn(),
+}));
+
+jest.mock('shared/api', () => ({
+  ...jest.requireActual('shared/api'),
+  ValidateZipRateData: jest.fn(),
+}));
+
+describe('ResidentialAddress page', () => {
+  const fakeAddress = {
+    street_address_1: '235 Prospect Valley Road SE',
+    street_address_2: '',
+    city: 'El Paso',
+    state: 'TX',
+    postal_code: '79912',
+  };
+
+  const blankAddress = Object.fromEntries(Object.keys(fakeAddress).map((k) => [k, '']));
+
+  const generateTestProps = (address) => ({
+    updateServiceMember: jest.fn(),
+    push: jest.fn(),
+    serviceMember: {
+      id: 'testServiceMemberId',
+      residential_address: address,
+    },
+  });
+
+  it('renders the ResidentialAddressForm', async () => {
+    const testProps = generateTestProps(blankAddress);
+
+    const { queryByRole } = render(<ResidentialAddress {...testProps} />);
+
+    await waitFor(() => {
+      expect(queryByRole('heading', { name: 'Current residence', level: 1 })).toBeInTheDocument();
+    });
+  });
+
+  it('validates zip code using api endpoint', async () => {
+    const testProps = generateTestProps(blankAddress);
+
+    ValidateZipRateData.mockImplementation(() => ({
+      valid: true,
+    }));
+
+    const { findByLabelText } = render(<ResidentialAddress {...testProps} />);
+
+    const postalCodeInput = await findByLabelText('ZIP');
+
+    const postalCode = '99999';
+
+    userEvent.type(postalCodeInput, postalCode);
+    userEvent.tab();
+
+    await waitFor(() => {
+      expect(ValidateZipRateData).toHaveBeenCalledWith(postalCode, 'origin');
+    });
+  });
+
+  it('back button submits the form and goes to the Current duty station step', async () => {
+    const testProps = generateTestProps(fakeAddress);
+
+    ValidateZipRateData.mockImplementation(() => ({
+      valid: true,
+    }));
+    patchServiceMember.mockImplementation(() => Promise.resolve(testProps.serviceMember));
+
+    const { getByRole } = render(<ResidentialAddress {...testProps} />);
+
+    const backButton = getByRole('button', { name: 'Back' });
+    expect(backButton).toBeInTheDocument();
+    userEvent.click(backButton);
+
+    await waitFor(() => {
+      expect(patchServiceMember).toHaveBeenCalled();
+    });
+
+    expect(testProps.updateServiceMember).toHaveBeenCalledWith(testProps.serviceMember);
+    expect(testProps.push).toHaveBeenCalledWith(customerRoutes.CURRENT_DUTY_STATION_PATH);
+  });
+
+  it('next button submits the form and goes to the Backup address step', async () => {
+    const testProps = generateTestProps(fakeAddress);
+
+    ValidateZipRateData.mockImplementation(() => ({
+      valid: true,
+    }));
+    patchServiceMember.mockImplementation(() => Promise.resolve(testProps.serviceMember));
+
+    const { getByRole } = render(<ResidentialAddress {...testProps} />);
+
+    const submitButton = getByRole('button', { name: 'Next' });
+    expect(submitButton).toBeInTheDocument();
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(patchServiceMember).toHaveBeenCalled();
+    });
+
+    expect(testProps.updateServiceMember).toHaveBeenCalledWith(testProps.serviceMember);
+    expect(testProps.push).toHaveBeenCalledWith(customerRoutes.BACKUP_ADDRESS_PATH);
+  });
+
+  it('shows an error if the ValidateZipRateData API returns an error', async () => {
+    const testProps = generateTestProps(fakeAddress);
+
+    ValidateZipRateData.mockImplementation(() => ({
+      valid: false,
+    }));
+    patchServiceMember.mockImplementation(() => Promise.resolve(testProps.serviceMember));
+
+    const { getByRole, findByRole } = render(<ResidentialAddress {...testProps} />);
+
+    const submitButton = getByRole('button', { name: 'Next' });
+    expect(submitButton).toBeInTheDocument();
+    userEvent.click(submitButton);
+
+    const alert = await findByRole('alert');
+
+    expect(alert).toHaveTextContent(
+      'Sorry, we don’t support that zip code yet. Please contact your local PPPO for assistance.',
+    );
+    expect(patchServiceMember).not.toHaveBeenCalled();
+    expect(testProps.updateServiceMember).not.toHaveBeenCalled();
+    expect(testProps.push).not.toHaveBeenCalled();
+  });
+
+  it('shows an error if the patchServiceMember API returns an error', async () => {
+    const testProps = generateTestProps(fakeAddress);
+
+    ValidateZipRateData.mockImplementation(() => ({
+      valid: true,
+    }));
+    patchServiceMember.mockImplementation(() =>
+      // Disable this rule because makeSwaggerRequest does not throw an error if the API call fails
+      // eslint-disable-next-line prefer-promise-reject-errors
+      Promise.reject({
+        message: 'A server error occurred saving the service member',
+        response: {
+          body: {
+            detail: 'A server error occurred saving the service member',
+          },
+        },
+      }),
+    );
+
+    const { getByRole, queryByText } = render(<ResidentialAddress {...testProps} />);
+
+    const submitButton = getByRole('button', { name: 'Next' });
+    expect(submitButton).toBeInTheDocument();
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(patchServiceMember).toHaveBeenCalled();
+    });
+
+    expect(queryByText('A server error occurred saving the service member')).toBeInTheDocument();
+    expect(testProps.updateServiceMember).not.toHaveBeenCalled();
+    expect(testProps.push).not.toHaveBeenCalled();
+  });
+
+  afterEach(jest.resetAllMocks);
+});
 
 describe('requireCustomerState ResidentialAddress', () => {
   const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
@@ -18,11 +188,8 @@ describe('requireCustomerState ResidentialAddress', () => {
   });
 
   const props = {
-    pages: ['first'],
-    pageKey: '1',
-    userEmail: 'my@email.com',
-    schema: { my: 'schema' },
     updateServiceMember: jest.fn(),
+    push: jest.fn(),
   };
 
   it('dispatches a redirect if the current state is earlier than the "DUTY STATION COMPLETE" state', () => {
@@ -58,7 +225,7 @@ describe('requireCustomerState ResidentialAddress', () => {
     );
 
     expect(wrapper.exists()).toBe(true);
-    expect(mockDispatch).toHaveBeenCalledWith(push('/service-member/current-duty'));
+    expect(mockDispatch).toHaveBeenCalledWith(push(customerRoutes.CURRENT_DUTY_STATION_PATH));
   });
 
   it('does not redirect if the current state equals the "DUTY STATION COMPLETE" state', () => {
@@ -99,6 +266,7 @@ describe('requireCustomerState ResidentialAddress', () => {
     expect(wrapper.exists()).toBe(true);
     expect(mockDispatch).not.toHaveBeenCalled();
   });
+
   it('does not redirect if the current state is after the "DUTY STATION COMPLETE" state and profile is not complete', () => {
     const mockState = {
       entities: {
