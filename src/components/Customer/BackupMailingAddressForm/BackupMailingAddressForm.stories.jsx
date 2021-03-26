05ea37207cd9db210f3cb381a203504ac9f9b3b8
Felipe Lee <felipe@truss.works>
2021-03-26 18:17:13 -0500

    MB-5320 Add stories showing example of using BackupMailingAddressForm

---
+++ src/components/Customer/BackupMailingAddressForm/BackupMailingAddressForm.stories.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import BackupMailingAddressForm from './BackupMailingAddressForm';
+
+export default {
+  title: 'Customer Components / Forms / BackupMailingAddressForm',
+  component: BackupMailingAddressForm,
+  argTypes: {
+    onSubmit: {
+      action: 'submit form',
+    },
+    onBack: {
+      action: 'go back',
+    },
+  },
+};
+
+export const DefaultState = (argTypes) => (
+  <BackupMailingAddressForm
+    initialValues={{
+      residential_address: {
+        street_address_1: '',
+        street_address_2: '',
+        city: '',
+        state: '',
+        postal_code: '',
+      },
+    }}
+    onBack={argTypes.onBack}
+    onSubmit={argTypes.onSubmit}
+  />
+);
+
+export const WithCustomValidators = (argTypes) => (
+  <BackupMailingAddressForm
+    initialValues={{
+      residential_address: {
+        street_address_1: '',
+        street_address_2: '',
+        city: '',
+        state: '',
+        postal_code: '',
+      },
+    }}
+    onBack={argTypes.onBack}
+    onSubmit={argTypes.onSubmit}
+    validators={{
+      city: (value) => (value === 'Nowhere' ? 'No one lives there' : ''),
+      postalCode: (value) => (value !== '99999' ? 'ZIP code must be 99999' : ''),
+    }}
+  />
+);
