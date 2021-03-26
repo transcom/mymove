d3ccf9dcc5cec60d0b8fc510147385cfd9e90a31
Ron Cruz <Ronolibert@gmail.com>
2021-03-26 17:08:25 +0000

    Commit for demo

--- eslint/no-unapproved-annotation.js
+++ eslint/no-unapproved-annotation.js
@@ -23,32 +23,32 @@ const create = (context) => ({
     node.comments.forEach((comment) => {
       const commentValue = comment.value.trim();
       const result = disableRegex.exec(commentValue);
+      // result.groups.ruleId && // Specifies a rule that we need annotations for
 
       if (
         result && // It's a eslint-disable comment
-        rulesRequiringAnnotation.has(result.groups.ruleId) && // Specifies a rule that we need annotations for
         context.getCommentsBefore(comment).length
       ) {
-        console.log('what', result.groups);
         const validatorComment = context
           .getCommentsBefore(comment)
           .map(({ value }) => value.trim())
           .filter((value) => value.includes(VALIDATOR_LABEL))[0];
-        const [, validatorStatusValue] = validatorComment.split(':');
-
-        if (!validatorStatusOptions.has(validatorStatusValue.trim())) {
-          context.report({
-            // Can't set it at the given location as the warning
-            // will be ignored due to the disable comment
-            loc: {
-              start: {
-                ...comment.loc.start,
-                column: -1,
+        if (validatorComment) {
+          const [, validatorStatusValue] = validatorComment.split(':');
+          if (!validatorStatusOptions.has(validatorStatusValue.trim())) {
+            context.report({
+              // Can't set it at the given location as the warning
+              // will be ignored due to the disable comment
+              loc: {
+                start: {
+                  ...comment.loc.start,
+                  column: -1,
+                },
+                end: comment.loc.end,
               },
-              end: comment.loc.end,
-            },
-            messageId: MESSAGE_ID,
-          });
+              messageId: MESSAGE_ID,
+            });
+          }
         }
       }
     });
