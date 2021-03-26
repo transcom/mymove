44e071bd523b5f70ddeb93f16abd8a765a38a79a
Donald Thai <donald@truss.works>
2021-03-26 22:52:01 +0000

    - update shipment type

--- migrations/app/schema/20210326215029_edit-shipment-type-shorthaul.up.sql
+++ migrations/app/schema/20210326215029_edit-shipment-type-shorthaul.up.sql
@@ -1,1 +1,1 @@
-UPDATE mto_shipments SET shipment_type = 'HHG_SHORTHAUL_DOMESTIC' WHERE "id" = '01b9671e-b268-4906-967b-ba661a1d3933';
+UPDATE mto_shipments SET shipment_type = 'HHG_SHORTHAUL_DOMESTIC' WHERE "id" = 'e6eca75f-451e-49a7-b36d-0826a083268d';
