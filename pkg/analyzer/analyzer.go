d714b62d942f558ebf50592790ce8e0b1d5f2447
Ron Cruz <Ronolibert@gmail.com>
2021-03-29 11:08:06 +0000

    Finish custom linter poc

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -4,61 +4,71 @@ import (
 	"go/ast"
 	"strings"
 
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+
 	"golang.org/x/tools/go/analysis"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "goprintffuncname",
-	Doc:  "Checks that printf-like functions are named with `f` at the end.",
-	Run:  run,
+	Name:     "goprintffuncname",
+	Doc:      "Checks that printf-like functions are named with `f` at the end.",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	inspect := func(node ast.Node) bool {
-		funcDecl, ok := node.(*ast.FuncDecl)
-		if !ok {
-			return true
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		funcDecl := node.(*ast.FuncDecl)
+
+		if res := funcDecl.Type.Results; res != nil && len(res.List) != 0 {
+			return
 		}
 
 		params := funcDecl.Type.Params.List
-		if len(params) != 2 { // [0] must be format (string), [1] must be args (...interface{})
-			return true
+		if len(params) < 2 { // [0] must be format (string), [1] must be args (...interface{})
+			return
 		}
 
-		firstParamType, ok := params[0].Type.(*ast.Ident)
+		formatParamType, ok := params[len(params)-2].Type.(*ast.Ident)
 		if !ok { // first param type isn't identificator so it can't be of type "string"
-			return true
+			return
 		}
 
-		if firstParamType.Name != "string" { // first param (format) type is not string
-			return true
+		if formatParamType.Name != "string" { // first param (format) type is not string
+			return
 		}
 
-		secondParamType, ok := params[1].Type.(*ast.Ellipsis)
+		if formatParamNames := params[len(params)-2].Names; len(formatParamNames) == 0 || formatParamNames[len(formatParamNames)-1].Name != "format" {
+			return
+		}
+
+		argsParamType, ok := params[len(params)-1].Type.(*ast.Ellipsis)
 		if !ok { // args are not ellipsis (...args)
-			return true
+			return
 		}
 
-		elementType, ok := secondParamType.Elt.(*ast.InterfaceType)
+		elementType, ok := argsParamType.Elt.(*ast.InterfaceType)
 		if !ok { // args are not of interface type, but we need interface{}
-			return true
+			return
 		}
 
 		if elementType.Methods != nil && len(elementType.Methods.List) != 0 {
-			return true // has >= 1 method in interface, but we need an empty interface "interface{}"
+			return // has >= 1 method in interface, but we need an empty interface "interface{}"
 		}
 
 		if strings.HasSuffix(funcDecl.Name.Name, "f") {
-			return true
+			return
 		}
 
 		pass.Reportf(node.Pos(), "printf-like formatting function '%s' should be named '%sf'",
 			funcDecl.Name.Name, funcDecl.Name.Name)
-		return true
-	}
+	})
 
-	for _, f := range pass.Files {
-		ast.Inspect(f, inspect)
-	}
 	return nil, nil
 }
