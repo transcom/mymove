d756b33bc946cdf4c1d3650c229b5ea61c67a35e
Jacqueline McKinney <jacqueline@truss.works>
2021-03-30 22:37:01 +0000

    added UT for EdiErrorsTechnicalErrorDescription

--- pkg/models/edi_errors_send_to_syncada_error.go
+++ pkg/models/edi_errors_send_to_syncada_error.go
@@ -1,7 +1,24 @@
 package models
 
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
 // EdiErrorsSendToSyncadaError records when the send of an EDI 858 fails to send to syncada
 type EdiErrorsSendToSyncadaError struct {
+	ID                         uuid.UUID                                 `json:"id" db:"id"`
+	CreatedAt                  time.Time                                 `json:"created_at" db:"created_at"`
+	UpdatedAt                  time.Time                                 `json:"updated_at" db:"updated_at"`
+	EdiErrorID                 uuid.UUID                                 `json:"edi_error_id" db:"edi_error_id"`
+	EdiError                   EdiError                                  `belongs_to:"edi_errors"`
+	PaymentRequestID           uuid.UUID                                 `json:"payment_request_id" db:"payment_request_id"`
+	PaymentRequest             PaymentRequest                            `belongs_to:"payment_requests"`
+	InterchangeControlNumberID *uuid.UUID                                `json:"interchange_control_number_id" db:"interchange_control_number_id"`
+	InterchangeControlNumber   *PaymentRequestToInterchangeControlNumber `belongs_to:"payment_request_to_interchange_control_numbers"`
+	Description                string                                    `json:"description" db:"description"`
+	EDIType                    EDIType                                   `json:"edi_type" db:"edi_type"`
 }
 
 // EdiErrorsSendToSyncadaErrors list of EdiErrorsSendToSyncadaError
--- pkg/models/edi_errors_technical_error_description.go
+++ pkg/models/edi_errors_technical_error_description.go
@@ -1,8 +1,12 @@
 package models
 
 import (
+	"strings"
 	"time"
 
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/gofrs/uuid"
 )
 
@@ -19,7 +23,26 @@ type EdiErrorsTechnicalErrorDescription struct {
 	InterchangeControlNumber   PaymentRequestToInterchangeControlNumber `belongs_to:"payment_request_to_interchange_control_numbers"`
 	Code                       string                                   `json:"code" db:"code"`
 	Description                string                                   `json:"description" db:"description"`
-	Source                     EDIType                                  `json:"source" db:"source"`
+	EDIType                    EDIType                                  `json:"edi_type" db:"edi_type"`
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+func (e *EdiErrorsTechnicalErrorDescription) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	var vs []validate.Validator
+	vs = append(vs, &validators.StringInclusion{Field: string(e.EDIType), Name: "EDIType", List: []string{
+		string(EDI810),
+		string(EDI824),
+		string(EDI858),
+		string(EDI997),
+	}})
+	vs = append(vs, &validators.UUIDIsPresent{Field: e.EdiErrorID, Name: "EdiErrorID"})
+	vs = append(vs, &validators.UUIDIsPresent{Field: e.PaymentRequestID, Name: "PaymentRequestID"})
+	vs = append(vs, &validators.UUIDIsPresent{Field: e.InterchangeControlNumberID, Name: "InterchangeControlNumberID"})
+	if strings.TrimSpace(e.Code) == "" && strings.TrimSpace(e.Description) == "" {
+		vs = append(vs, &validators.StringIsPresent{Field: e.Code, Name: "Code", Message: "Code or Description must be present"})
+		vs = append(vs, &validators.StringIsPresent{Field: e.Description, Name: "Description", Message: "Code or Description must be present"})
+	}
+	return validate.Validate(vs...), nil
 }
 
 // EdiErrorsTechnicalErrorDescriptions is a list of EDI Technical Error Descriptions (TEDs)
