1e4de45cd2d157a478dc439b2550e1e7dbfdced7
Ron Cruz <Ronolibert@gmail.com>
2021-03-31 15:25:47 +0000

    Add base files for custom linter

---
+++ eslint/no-unapproved-annotation.js
@@ -0,0 +1,65 @@
+const MESSAGE_ID = 'no-unapproved-annotation';
+
+const messages = {
+  [MESSAGE_ID]: 'Requires annotation approval from an ISSO',
+};
+
+// eslint-disable-next-line security/detect-unsafe-regex
+const disableRegex = /^eslint-disable(?:-next-line|-line)?(?<ruleId>$|(?:\s+(?:@(?:[\w-]+\/){1,2})?[\w-]+)?)/;
+
+const validatorStatusOptions = new Set([
+  'RA Accepted',
+  'Return to Developer',
+  'Known Issue',
+  'Mitigated',
+  'False Positive',
+  'Bad Practice',
+]);
+
+// const rulesRequiringAnnotation = new Set(['no-console', 'security', 'react']);
+
+const VALIDATOR_LABEL = 'RA Validator Status:';
+const create = (context) => ({
+  Program: (node) => {
+    node.comments.forEach((comment) => {
+      const commentValue = comment.value.trim();
+      const result = disableRegex.exec(commentValue);
+      // result.groups.ruleId && // Specifies a rule that we need annotations for
+
+      if (
+        result && // It's a eslint-disable comment
+        context.getCommentsBefore(comment).length
+      ) {
+        const validatorComment = context
+          .getCommentsBefore(comment)
+          .map(({ value }) => value.trim())
+          .filter((value) => value.includes(VALIDATOR_LABEL))[0];
+        if (validatorComment) {
+          const [, validatorStatusValue] = validatorComment.split(':');
+          if (!validatorStatusOptions.has(validatorStatusValue.trim())) {
+            context.report({
+              // Can't set it at the given location as the warning
+              // will be ignored due to the disable comment
+              loc: {
+                start: {
+                  ...comment.loc.start,
+                  column: -1,
+                },
+                end: comment.loc.end,
+              },
+              messageId: MESSAGE_ID,
+            });
+          }
+        }
+      }
+    });
+  },
+});
+
+module.exports = {
+  create,
+  meta: {
+    type: 'suggestion',
+    messages,
+  },
+};
