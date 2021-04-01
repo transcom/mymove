9a2bb5737ce06e9b463a2d8945940510fda6e9c1
Ron Cruz <Ronolibert@gmail.com>
2021-04-01 17:58:31 +0000

    Added additional bypassable rules and added test cases for custom rules

---
+++ tests/no-explicit-eslint-disable.js
@@ -0,0 +1,32 @@
+const { RuleTester } = require('eslint');
+
+const rule = require('../eslint/no-explicit-eslint-disable');
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-explicit-eslint-disable', rule, {
+  valid: [
+    '// eslint-disable no-console',
+    '// eslint-disable react/jsx-props-no-spreading',
+    '/* eslint-disable no-console */',
+  ],
+
+  invalid: [
+    {
+      code: '// eslint-disable',
+      errors: [{ message: 'Please specify the rule(s) you want to disable.' }],
+    },
+    {
+      code: '// eslint-disable-next-line',
+      errors: [{ message: 'Please specify the rule(s) you want to disable.' }],
+    },
+    {
+      code: '/* eslint-disable */',
+      errors: [{ message: 'Please specify the rule(s) you want to disable.' }],
+    },
+    {
+      code: '/* eslint-disable-next-line */',
+      errors: [{ message: 'Please specify the rule(s) you want to disable.' }],
+    },
+  ],
+});
