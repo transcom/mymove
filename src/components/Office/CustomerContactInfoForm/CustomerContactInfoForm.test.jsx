dd8580c890c8fbfe606bed67ef015a66c15435d7
JP <jenniferlynparsons@gmail.com>
2021-04-09 08:51:53 -0400

    Add checkbox rendering test

--- src/components/Office/CustomerContactInfoForm/CustomerContactInfoForm.test.jsx
+++ src/components/Office/CustomerContactInfoForm/CustomerContactInfoForm.test.jsx
@@ -34,6 +34,7 @@ describe('CustomerContactInfoForm Component', () => {
 
     await waitFor(() => {
       expect(screen.getByText('Contact info')).toBeInstanceOf(HTMLHeadingElement);
+      expect(screen.getByLabelText('This is not the person named on the orders.')).toBeInstanceOf(HTMLInputElement);
       expect(screen.getByLabelText('First name')).toBeInstanceOf(HTMLInputElement);
       expect(screen.getByLabelText('First name')).toBeRequired();
 
@@ -64,65 +65,4 @@ describe('CustomerContactInfoForm Component', () => {
       expect(screen.getAllByLabelText('Email')[1]).toBeRequired();
     });
   });
-
-  // it('validates the customer phone field', async () => {
-  //   render(<CustomerContactInfoForm {...testProps} />);
-  //   userEvent.type(screen.getByLabelText('Best contact phone'), '12345');
-  //   userEvent.tab();
-
-  //   await waitFor(() => {
-  //     expect(screen.getByText('Number must have 10 digits and a valid area code')).toBeInTheDocument();
-  //   });
-  // });
-
-  // it('validates the customer email field', async () => {
-  //   render(<CustomerContactInfoForm {...testProps} />);
-  //   userEvent.type(screen.getByLabelText('Email'), 'sample@');
-  //   userEvent.tab();
-
-  //   await waitFor(() => {
-  //     expect(screen.getByText('Must be a valid email address')).toBeInTheDocument();
-  //   });
-  // });
-
-  // it('shows an error message when trying to submit an invalid form', async () => {
-  //   render(<CustomerContactInfoForm {...testProps} />);
-  //   const submitBtn = screen.getByRole('button', { name: 'Save' });
-
-  //   userEvent.click(submitBtn);
-
-  //   await waitFor(() => {
-  //     expect(screen.getAllByText('Required').length).toBe(2);
-  //   });
-
-  //   expect(testProps.onSubmit).not.toHaveBeenCalled();
-  // });
-
-  // it('submits a form when it is valid', async () => {
-  //   render(<CustomerContactInfoForm {...testProps} />);
-  //   const submitBtn = screen.getByRole('button', { name: 'Next' });
-
-  //   userEvent.type(screen.getByLabelText('Phone'), '555-555-5555');
-  //   userEvent.type(screen.getByLabelText('Email'), 'test@sample.com');
-  //   userEvent.click(screen.getByLabelText('Email'));
-  //   userEvent.click(submitBtn);
-
-  //   await waitFor(() => {
-  //     expect(testProps.onSubmit).toHaveBeenCalled();
-  //   });
-  // });
-
-  // it('calls the cancel handler when cancel button is clicked', async () => {
-  //   render(<CustomerContactInfoForm {...testProps} />);
-  //   const backBtn = screen.getByRole('button', { name: 'Cancel' });
-
-  //   userEvent.type(screen.getByLabelText('Phone'), '555-555-1111');
-  //   userEvent.type(screen.getByLabelText('Email'), 'test@sample.com');
-  //   userEvent.click(screen.getByLabelText('Email'));
-  //   userEvent.click(backBtn);
-
-  //   await waitFor(() => {
-  //     expect(testProps.onBack).toHaveBeenCalled();
-  //   });
-  // });
 });
