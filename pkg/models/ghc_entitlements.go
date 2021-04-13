815cfc9122968afdf1e3e962a1d9ce91b6034c6d
Donald Thai <donald@truss.works>
2021-04-13 15:41:58 -0700

    new fields for entitlements model

--- pkg/models/ghc_entitlements.go
+++ pkg/models/ghc_entitlements.go
@@ -14,11 +14,13 @@ type Entitlement struct {
 	NonTemporaryStorage   *bool     `db:"non_temporary_storage"`
 	PrivatelyOwnedVehicle *bool     `db:"privately_owned_vehicle"`
 	//DBAuthorizedWeight is AuthorizedWeight when not null
-	DBAuthorizedWeight *int             `db:"authorized_weight"`
-	weightAllotment    *WeightAllotment `db:"-"`
-	StorageInTransit   *int             `db:"storage_in_transit"`
-	CreatedAt          time.Time        `db:"created_at"`
-	UpdatedAt          time.Time        `db:"updated_at"`
+	DBAuthorizedWeight                           *int             `db:"authorized_weight"`
+	weightAllotment                              *WeightAllotment `db:"-"`
+	StorageInTransit                             *int             `db:"storage_in_transit"`
+	RequiredMedicalEquipmentWeight               *int             `db:"required_medical_equipment_weight"`
+	OrganizationalClothingAndIndividualEquipment *bool            `db:"organizational_clothing_and_individual_equipment"`
+	CreatedAt                                    time.Time        `db:"created_at"`
+	UpdatedAt                                    time.Time        `db:"updated_at"`
 }
 
 // SetWeightAllotment sets the weight allotment
