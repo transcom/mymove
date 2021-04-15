0673ecd0313cf0fd16d1bfb6769309f80b2930ea
Namibia Torres <namibia@truss.works>
2021-04-15 16:11:55 -0700

    add a couple of spaces for consistency.

--- migrations/app/schema/20210407223734_add_service_counselor_role.up.sql
+++ migrations/app/schema/20210407223734_add_service_counselor_role.up.sql
@@ -3,4 +3,4 @@ COMMENT ON COLUMN roles.role_type IS 'These are the names of the roles in snake 
 
 INSERT INTO roles (id, role_type, created_at, updated_at, role_name)
 VALUES
-('010bdae1-8ebe-44c9-b8ee-8c4477fae2a6', 'services_counselor', now(),now(),'Services Counselor');
+('010bdae1-8ebe-44c9-b8ee-8c4477fae2a6', 'services_counselor', now(), now(), 'Services Counselor');
