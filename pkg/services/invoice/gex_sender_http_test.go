a2cf5af9f6927d444e0f8ac4cd04e1d716f9a2ac
Marty Boren <marty@truss.works>
2021-04-19 20:26:27 +0000

    Test filename and channel being sent to GEX

--- pkg/services/invoice/gex_sender_http_test.go
+++ pkg/services/invoice/gex_sender_http_test.go
@@ -51,3 +51,21 @@ func (suite *GexSuite) TestSendToGexHTTP_Call() {
 	expectedStatus = http.StatusInternalServerError
 	suite.Equal(expectedStatus, resp.StatusCode)
 }
+
+func (suite *GexSuite) TestSendToGexHTTP_QueryParams() {
+	ediString := ""
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	resp, err := NewGexSenderHTTP(mockServer.URL, "test_channel", false, nil, "", "").
+		SendToGex(ediString, "test_filename")
+	if resp == nil || err != nil {
+		suite.T().Fatal(err, "Failed mock request")
+	}
+	expectedStatus := http.StatusOK
+	suite.Equal(expectedStatus, resp.StatusCode)
+
+	// Make sure we sent a request with correct channel and filename query parameters
+	suite.Contains(resp.Request.URL.RawQuery, "channel=test_channel")
+	suite.Contains(resp.Request.URL.RawQuery, "fname=test_filename")
+}
