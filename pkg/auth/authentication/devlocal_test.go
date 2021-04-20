ae06cbbeeaf58bef83bfbf91103f1d011773c56a
Duncan Spencer <duncan@truss.works>
2021-04-20 22:09:32 +0000

    add gbloc to devlocal office user transportation office, create roles factory for tests

--- pkg/auth/authentication/devlocal_test.go
+++ pkg/auth/authentication/devlocal_test.go
@@ -21,8 +21,10 @@ import (
 
 	"github.com/transcom/mymove/pkg/auth"
 	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/models/roles"
 	"github.com/transcom/mymove/pkg/services"
 	"github.com/transcom/mymove/pkg/services/query"
+	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
 func getCookie(name string, cookies []*http.Cookie) (*http.Cookie, error) {
@@ -75,53 +77,72 @@ func (suite *AuthSuite) TestCreateUserHandlerMilMove() {
 func (suite *AuthSuite) TestCreateUserHandlerOffice() {
 	t := suite.T()
 
+	// These roles are created during migrations but our test suite truncates all tables
+	testdatagen.MakePPMOfficeRole(suite.DB())
+	testdatagen.MakeTOORole(suite.DB())
+	testdatagen.MakeTIORole(suite.DB())
+	testdatagen.MakeServicesCounselorRole(suite.DB())
+
 	appnames := ApplicationTestServername()
 	callbackPort := 1234
-
-	// Exercise all variables in the office user
-	form := url.Values{}
-	form.Add("userType", "PPM office")
-	form.Add("firstName", "Carol")
-	form.Add("lastName", "X")
-	form.Add("telephone", "222-222-2222")
-	form.Add("email", "office_user@example.com")
-
-	req := httptest.NewRequest("POST", fmt.Sprintf("http://%s/devlocal-auth/create", appnames.OfficeServername), strings.NewReader(form.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
-	req.ParseForm()
 
 	sessionManagers := setupSessionManagers()
 	officeSession := sessionManagers[2]
 	authContext := NewAuthContext(suite.logger, fakeLoginGovProvider(suite.logger), "http", callbackPort, sessionManagers)
 	handler := NewCreateUserHandler(authContext, suite.DB(), appnames)
 
-	rr := httptest.NewRecorder()
-	officeSession.LoadAndSave(handler).ServeHTTP(rr, req)
+	for _, newOfficeUser := range []struct {
+		userType string
+		roleType roles.RoleType
+		email    string
+	}{{userType: PPMOfficeUserType, roleType: roles.RoleTypePPMOfficeUsers, email: "ppm_office_user@example.com"}, {userType: TOOOfficeUserType, roleType: roles.RoleTypeTOO, email: "too_office_user@example.com"}, {userType: TIOOfficeUserType, roleType: roles.RoleTypeTIO, email: "tio_office_user@example.com"}} {
+		// Exercise all variables in the office user
+		form := url.Values{}
+		form.Add("userType", newOfficeUser.userType)
+		form.Add("firstName", "Carol")
+		form.Add("lastName", "X")
+		form.Add("telephone", "222-222-2222")
+		form.Add("email", newOfficeUser.email)
 
-	suite.Equal(http.StatusOK, rr.Code, "handler returned wrong status code")
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v wanted %v", status, http.StatusOK)
+		req := httptest.NewRequest("POST", fmt.Sprintf("http://%s/devlocal-auth/create", appnames.OfficeServername), strings.NewReader(form.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+		req.ParseForm()
+
+		rr := httptest.NewRecorder()
+		officeSession.LoadAndSave(handler).ServeHTTP(rr, req)
+
+		suite.Equal(http.StatusOK, rr.Code, "handler returned wrong status code")
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v wanted %v", status, http.StatusOK)
+		}
+
+		cookies := rr.Result().Cookies()
+		if _, err := getCookie("office_session_token", cookies); err != nil {
+			t.Error("could not find session token in response")
+		}
+
+		user := models.User{}
+		err := json.Unmarshal(rr.Body.Bytes(), &user)
+		if err != nil {
+			t.Error("Could not unmarshal json data into User model.", err)
+		}
+
+		officeUser, err := models.FetchOfficeUserByEmail(suite.DB(), user.LoginGovEmail)
+		if err != nil {
+			t.Error("Could not find office user for this user.", err)
+		}
+
+		err = suite.DB().Load(officeUser, "TransportationOffice", "User.Roles")
+		if err != nil {
+			t.Error("Could not load transportation office for this user")
+		}
+
+		suite.Equal("Carol", officeUser.FirstName)
+		suite.Equal("X", officeUser.LastName)
+		suite.Equal("222-222-2222", officeUser.Telephone)
+		suite.Equal(newOfficeUser.roleType, officeUser.User.Roles[0].RoleType)
+		suite.Equal("LKNQ", officeUser.TransportationOffice.Gbloc)
 	}
-
-	cookies := rr.Result().Cookies()
-	if _, err := getCookie("office_session_token", cookies); err != nil {
-		t.Error("could not find session token in response")
-	}
-
-	user := models.User{}
-	err := json.Unmarshal(rr.Body.Bytes(), &user)
-	if err != nil {
-		t.Error("Could not unmarshal json data into User model.", err)
-	}
-
-	officeUser, err := models.FetchOfficeUserByEmail(suite.DB(), user.LoginGovEmail)
-	if err != nil {
-		t.Error("Could not find office user for this user.", err)
-	}
-
-	suite.Equal(officeUser.FirstName, "Carol")
-	suite.Equal(officeUser.LastName, "X")
-	suite.Equal(officeUser.Telephone, "222-222-2222")
 }
 
 func (suite *AuthSuite) TestCreateUserHandlerDPS() {
@@ -213,6 +234,10 @@ func (suite *AuthSuite) TestCreateUserHandlerAdmin() {
 	if err := queryBuilder.FetchOne(&adminUser, filters); err != nil {
 		t.Error("Couldn't find admin user record")
 	}
+
+	suite.Equal("Leo", adminUser.FirstName)
+	suite.Equal("Spaceman", adminUser.LastName)
+	suite.Equal(models.SystemAdminRole, adminUser.Role)
 }
 
 func (suite *AuthSuite) TestCreateAndLoginUserHandlerFromMilMoveToMilMove() {
