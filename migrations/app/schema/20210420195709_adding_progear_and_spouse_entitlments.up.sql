f7451004b2be77dd70b5f612b7c64a64bab2cae3
Donald Thai <donald@truss.works>
2021-04-21 21:11:10 +0000

    update spouse_pro_gear_weight to pro_gear_weight_spouse for consistency

--- migrations/app/schema/20210420195709_adding_progear_and_spouse_entitlments.up.sql
+++ migrations/app/schema/20210420195709_adding_progear_and_spouse_entitlments.up.sql
@@ -2,5 +2,5 @@ ALTER TABLE entitlements
     ADD COLUMN pro_gear_weight int4;
 COMMENT ON COLUMN entitlements.pro_gear_weight IS 'This is equipment a member needs for the performance of official duties at the next or a later destination. Members are given a weight allowance for progear that is separate from their normal weight allowance.';
 ALTER TABLE entitlements
-    ADD COLUMN spouse_pro_gear_weight int4;
-COMMENT ON COLUMN entitlements.spouse_pro_gear_weight IS 'This is equipment a member''s spouse needs for the performance of official duties at the next or a later destination. Members are given a weight allowance for progear that is separate from their normal weight allowance.';
+    ADD COLUMN pro_gear_weight_spouse int4;
+COMMENT ON COLUMN entitlements.pro_gear_weight_spouse IS 'This is equipment a member''s spouse needs for the performance of official duties at the next or a later destination. Members are given a weight allowance for progear that is separate from their normal weight allowance.';
