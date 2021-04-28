54833b4cb3c10f5bbf0b5af36a43bf21b89cbd78
Sandy Wright <sandy@truss.works>
2021-04-28 21:43:25 +0000

    MB-7947 Remove test to wait 5s to change transportation office

--- cypress/integration/admin/officeUsers.js
+++ cypress/integration/admin/officeUsers.js
@@ -144,13 +144,7 @@ describe('Office Users Edit Page', function () {
 
     cy.get('input[id="firstName"]').clear().type('Edit');
     cy.get('input[id="lastName"]').clear().type('CypressUser');
-    cy.get('input[id="transportationOfficeId"]').type('Scott AFB');
-    cy.wait(5000); // we have to wait for the autocomplete to give us something
-    cy.get('div[role="tooltip"] div[role="listbox"] li')
-      .first()
-      .then(($opt) => {
-        $opt.trigger('click');
-      });
+    cy.get('input[id="transportationOfficeId"]').should('not.be.disabled');
 
     // set the user to the active status they did NOT have before
     cy.get('#active').click();
