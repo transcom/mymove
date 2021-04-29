59e36e63185361bb87218e96d168df3359184e72
Shauna Keating <shauna@truss.works>
2021-04-29 18:56:32 +0000

    adds mock providers to footer stories file

--- src/components/Customer/Footer/index.stories.jsx
+++ src/components/Customer/Footer/index.stories.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import Footer from './index';
 
+import { MockProviders } from 'testUtils';
+
 export default {
   title: 'Customer Components / Page Footer',
   component: Footer,
+  decorators: [
+    (Story) => (
+      <MockProviders>
+        <Story />
+      </MockProviders>
+    ),
+  ],
 };
 
 export const Basic = () => <Footer />;
