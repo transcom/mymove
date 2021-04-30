79fa55af254e1fe840b79b9df3273b096019bf26
Ron Cruz <Ronolibert@gmail.com>
2021-04-30 17:40:18 +0000

    Edit lint error messages to point to the wiki

--- pkg/ato-linter/ato.go
+++ pkg/ato-linter/ato.go
@@ -106,13 +106,13 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 		containsDisablingGosecNoAnnotation := containsGosecNoAnnotation(comments.List)
 		if containsDisablingGosecNoAnnotation {
-			pass.Reportf(node.Pos(), "Disabling of gosec must have an annotation associated with it. Please visit https://docs.google.com/document/d/1qiBNHlctSby0RZeaPzb-afVxAdA9vlrrQgce00zjDww/edit#heading=h.b2vss780hqfi")
+			pass.Reportf(node.Pos(), "Disabling of gosec must have an annotation associated with it. Please visit https://github.com/transcom/mymove/wiki/guide-to-static-analysis-annotations-for-disabled-Linters#guide-to-static-analysis-annotations-for-disabled-linters")
 			return
 		}
 
 		containsAnnotationNotApproved := containsAnnotationNotApproved(comments.List)
 		if containsAnnotationNotApproved {
-			pass.Reportf(node.Pos(), "Please add the truss-is3 team as reviewers for this PR and ping the ISSO in #static-code-review Slack. Add label ‘needs-is3-review’ to this PR")
+			pass.Reportf(node.Pos(), "Please add the truss-is3 team as reviewers for this PR and ping the ISSO in #static-code-review Slack. Add label ‘needs-is3-review’ to this PR. For more info see https://github.com/transcom/mymove/wiki/guide-to-static-analysis-security-workflow#guide-to-static-analysis-security-workflow")
 			return
 		}
 	})
--- testdata/src/linter_tests/ato.go
+++ testdata/src/linter_tests/ato.go
@@ -1,9 +1,9 @@
 package linter_tests
 
-// #nosec // want "Please visit https://docs.google.com/document/d/1qiBNHlctSby0RZeaPzb-afVxAdA9vlrrQgce00zjDww/edit#heading=h.b2vss780hqfi"
+// #nosec // want "Please visit https://github.com/transcom/mymove/wiki/guide-to-static-analysis-annotations-for-disabled-Linters#guide-to-static-analysis-annotations-for-disabled-linters"
 func nosecShouldHaveAnnotation() {}
 
-//RA Summary: [linter] - [linter type code] - [Linter summary] // want "Please add the truss-is3 team as reviewers for this PR and ping the ISSO in #static-code-review Slack. Add label ‘needs-is3-review’ to this PR"
+//RA Summary: [linter] - [linter type code] - [Linter summary] // want "Please add the truss-is3 team as reviewers for this PR and ping the ISSO in #static-code-review Slack. Add label ‘needs-is3-review’ to this PR. For more info see https://github.com/transcom/mymove/wiki/guide-to-static-analysis-security-workflow#guide-to-static-analysis-security-workflow"
 //RA: <Why did the linter flag this line of code?>
 //RA: <Why is this line of code valuable?>
 //RA: <What mitigates the risk of negative impact?>
