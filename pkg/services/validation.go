ab175b11c951cace7d66bf4878e4c7cdff35dea0
Sandy Wright <sandy@truss.works>
2021-04-30 17:09:41 -0500

    MB-7989 Add validation helper functions to new validation.go file

---
+++ pkg/services/validation.go
@@ -0,0 +1,33 @@
+package services
+
+import "time"
+
+// SetOptionalDateTimeField sets the correct new value for the updated date field. Can be nil.
+func SetOptionalDateTimeField(newDate *time.Time, oldDate *time.Time) *time.Time {
+	// check if the user wanted to keep this field the same:
+	if newDate == nil {
+		return oldDate
+	}
+
+	// check if the user wanted to nullify the value in this field:
+	if newDate.IsZero() {
+		return nil
+	}
+
+	return newDate // return the new intended value
+}
+
+// SetOptionalStringField sets the correct new value for the updated string field. Can be nil.
+func SetOptionalStringField(newString *string, oldString *string) *string {
+	// check if the user wanted to keep this field the same:
+	if newString == nil {
+		return oldString
+	}
+
+	// check if the user wanted to nullify the value in this field:
+	if *newString == "" {
+		return nil
+	}
+
+	return newString // return the new intended value
+}
