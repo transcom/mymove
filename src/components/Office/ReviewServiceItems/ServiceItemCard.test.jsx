64c0cb509974fb9efbab1fa34f6bd0b01f19871e
Donald Thai <donald@truss.works>
2021-05-05 16:04:17 -0700

    tests to test the new interactions

--- src/components/Office/ReviewServiceItems/ServiceItemCard.test.jsx
+++ src/components/Office/ReviewServiceItems/ServiceItemCard.test.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { act } from 'react-dom/test-utils';
+import { render, fireEvent, act, waitFor } from '@testing-library/react';
 import { mount } from 'enzyme';
 
 import testParams from '../ServiceItemCalculations/serviceItemTestParams';
@@ -70,6 +70,76 @@ describe('ServiceItemCard component', () => {
     it('does not render calculations toggle when the service item calculations are not implemented', () => {
       const component = mount(<ServiceItemCard {...basicServiceItemCard} />);
       expect(component.find('button[data-testid="toggleCalculations"]').exists()).toBe(false);
+    });
+
+    // using react testing library to test dom interactions
+    it('save button is disabled when rejection reason is empty', () => {
+      const { getByTestId } = render(<ServiceItemCard {...basicServiceItemCard} />);
+      const rejectRadio = getByTestId('rejectRadio');
+
+      waitFor(() => {
+        fireEvent.click(rejectRadio);
+      });
+
+      expect(getByTestId('rejectionSaveButton')).toHaveAttribute('disabled');
+    });
+
+    // using react testing library to test dom interactions
+    it('edit reason button link show after saving', async () => {
+      const { getByTestId } = render(<ServiceItemCard {...basicServiceItemCard} />);
+      const rejectRadio = getByTestId('rejectRadio');
+
+      // Click on reject radio and fill in text area
+      await waitFor(() => {
+        fireEvent.click(rejectRadio);
+      });
+
+      await waitFor(() => {
+        fireEvent.change(getByTestId('textarea'), {
+          target: { value: 'Rejected just because.' },
+        });
+      });
+
+      expect(getByTestId('rejectionSaveButton').hasAttribute('disabled')).toBeFalsy();
+
+      // Save
+      await waitFor(() => {
+        fireEvent.click(getByTestId('rejectionSaveButton'));
+      });
+
+      expect(getByTestId('editReasonButton')).toBeTruthy();
+      expect(getByTestId('rejectionReasonReadOnly').textContent).toBe('Rejected just because.');
+    });
+
+    // using react testing library to test dom interactions
+    it('edit existing rejection reason', async () => {
+      const data = {
+        ...basicServiceItemCard,
+        status: PAYMENT_SERVICE_ITEM_STATUS.DENIED,
+        rejectionReason: 'Rejected just because.',
+      };
+      const { getByTestId } = render(<ServiceItemCard {...data} />);
+
+      expect(getByTestId('editReasonButton')).toBeTruthy();
+      expect(getByTestId('rejectionReasonReadOnly').textContent).toBe('Rejected just because.');
+
+      // Click on Edit reason button, edit text area and save
+      await waitFor(() => {
+        fireEvent.click(getByTestId('editReasonButton'));
+      });
+
+      await waitFor(() => {
+        fireEvent.change(getByTestId('textarea'), {
+          target: { value: 'Edited rejection reason.' },
+        });
+      });
+
+      await waitFor(() => {
+        fireEvent.click(getByTestId('rejectionSaveButton'));
+      });
+
+      expect(getByTestId('editReasonButton')).toBeTruthy();
+      expect(getByTestId('rejectionReasonReadOnly').textContent).toBe('Edited rejection reason.');
     });
   });
 
