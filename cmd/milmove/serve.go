7ab542064bf4c5170e5e72ae4a4f1cda4c25b463
pearl-truss <pearl@truss.works>
2021-05-07 18:55:08 +0000

    revert change to serge.go

--- cmd/milmove/serve.go
+++ cmd/milmove/serve.go
@@ -598,7 +598,7 @@ func serveFunction(cmd *cobra.Command, args []string) error {
 	if len(gexURL) == 0 {
 		// this spins up a local test server
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusForbidden)
+			w.WriteHeader(http.StatusOK)
 		}))
 		gexRequester = invoice.NewGexSenderHTTP(
 			server.URL,
