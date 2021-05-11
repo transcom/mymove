09f2c51d9a23789aeced4363c2069057c542a543
Marty Boren <marty@truss.works>
2021-05-11 23:10:21 +0000

    Add failing tests for SQL migration splitting

--- pkg/migrate/SplitStatements_test.go
+++ pkg/migrate/SplitStatements_test.go
@@ -69,6 +69,129 @@ func TestSplitStatementsCopyFromStdin(t *testing.T) {
 	require.Equal(t, i, 12)
 }
 
+func TestSplitStatementsCommentMultipleQuotes(t *testing.T) {
+	// Load the fixture with the sql example
+	fixture := "./fixtures/commentWithMultipleQuotes.sql"
+	f, err := os.Open(fixture)
+	defer func() {
+		if fixtureCloseErr := f.Close(); fixtureCloseErr != nil {
+			t.Error("Failed to close fixture", zap.Error(fixtureCloseErr))
+		}
+	}()
+	require.Nil(t, err)
+	lines := make(chan string, 1000)
+	dropComments := true
+	dropSearchPath := true
+	go func() {
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			lines <- ReadInSQLLine(scanner.Text(), dropComments, dropSearchPath)
+		}
+		close(lines)
+	}()
+
+	wait := 10 * time.Millisecond
+	statements := make(chan string, 1000)
+	go SplitStatements(lines, statements, wait)
+
+	expectedStmt := []string{
+		"COMMENT ON COLUMN public.office_emails.label IS 'The department the email gets sent to. For example, ''Customer Service''';",
+		"COMMENT ON COLUMN public.office_emails.created_at IS 'Lots of quotes ''''within a string.''''';",
+		"COMMENT ON COLUMN public.office_emails.updated_at IS 'Unbalanced quotes at end of string''';",
+	}
+
+	i := 0
+	for stmt := range statements {
+		require.Equal(t, expectedStmt[i], stmt)
+		i++
+	}
+	require.Equal(t, i, 3)
+}
+func TestSplitStatementsCopyFromStdinWithQuotes(t *testing.T) {
+
+	// Load the fixture with the sql example
+	fixture := "./fixtures/copyFromStdinQuotes.sql"
+	f, err := os.Open(fixture)
+	defer func() {
+		if fixtureCloseErr := f.Close(); fixtureCloseErr != nil {
+			t.Error("Failed to close fixture", zap.Error(fixtureCloseErr))
+		}
+	}()
+	require.Nil(t, err)
+	lines := make(chan string, 1000)
+	dropComments := true
+	dropSearchPath := true
+	go func() {
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			lines <- ReadInSQLLine(scanner.Text(), dropComments, dropSearchPath)
+		}
+		close(lines)
+	}()
+
+	wait := 10 * time.Millisecond
+	statements := make(chan string, 1000)
+	go SplitStatements(lines, statements, wait)
+
+	expectedStmt := []string{
+		"COPY public.addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at, street_address_3, country) FROM stdin;",
+		"00000000-0000-0000-0000-000000000001    123 Any St         Ellsworth AFB   SD      57706   2018-05-28 14:27:38.959754      2018-05-28 14:27:38.959755      \\N      United States",
+		"00000000-0000-0000-0000-000000000002    123 O'Connell         Fort Carson     CO      80913   2018-05-28 14:27:39.06161       2018-05-28 14:27:39.061611      \\N      United States",
+		"00000000-0000-0000-0000-000000000003    123 Q St       Hill Air Force Base     UT      84056   2018-05-28 14:27:39.104893      2018-05-28 14:27:39.104894      \\N      United States",
+		"\\.",
+		"COPY public.addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at, street_address_3, country) FROM stdin;",
+		"00000000-0000-0000-0000-000000000001    123 Any St         Ellsworth AFB   SD      57706   2018-05-28 14:27:38.959754      2018-05-28 14:27:38.959755      \\N      United States",
+		"00000000-0000-0000-0000-000000000002    123 O'Connell         Fort Carson     CO      80913   2018-05-28 14:27:39.06161       2018-05-28 14:27:39.061611      \\N      United States",
+		"00000000-0000-0000-0000-000000000003    123 Q St       Hill Air Force Base     UT      84056   2018-05-28 14:27:39.104893      2018-05-28 14:27:39.104894      \\N      United States",
+		"\\.",
+	}
+
+	i := 0
+	for stmt := range statements {
+		require.Equal(t, expectedStmt[i], stmt)
+		i++
+	}
+	require.Equal(t, i, 10)
+}
+func TestSplitStatementsCopyFromStdinWithSemicolons(t *testing.T) {
+
+	// Load the fixture with the sql example
+	fixture := "./fixtures/copyFromStdinSemicolons.sql"
+	f, err := os.Open(fixture)
+	defer func() {
+		if fixtureCloseErr := f.Close(); fixtureCloseErr != nil {
+			t.Error("Failed to close fixture", zap.Error(fixtureCloseErr))
+		}
+	}()
+	require.Nil(t, err)
+	lines := make(chan string, 1000)
+	dropComments := true
+	dropSearchPath := true
+	go func() {
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			lines <- ReadInSQLLine(scanner.Text(), dropComments, dropSearchPath)
+		}
+		close(lines)
+	}()
+
+	wait := 10 * time.Millisecond
+	statements := make(chan string, 1000)
+	go SplitStatements(lines, statements, wait)
+
+	expectedStmt := []string{
+		"COPY public.addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at, street_address_3, country) FROM stdin;",
+		"00000000-0000-0000-0000-000000000001    123 Any St;         Ellsworth AFB   SD      57706   2018-05-28 14:27:38.959754      2018-05-28 14:27:38.959755      \\N      United States",
+		"\\.",
+	}
+
+	i := 0
+	for stmt := range statements {
+		require.Equal(t, expectedStmt[i], stmt)
+		i++
+	}
+	require.Equal(t, i, 6)
+}
 func TestSplitStatementsCopyFromStdinMultiple(t *testing.T) {
 
 	// Load the fixture with the sql example
