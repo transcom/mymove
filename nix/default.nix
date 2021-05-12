c6e03122956fc4272c99d461cf639a487e629ddc
Reggie Riser <reggie@truss.works>
2021-05-12 16:39:49 +0000

    Changed version reference to go 1.16/1.16.4

--- nix/default.nix
+++ nix/default.nix
@@ -39,7 +39,7 @@ in buildEnv {
 
     (import (builtins.fetchGit {
       # Descriptive name to make the store path easier to identify
-      name = "go-1.15.8";
+      name = "go-1.16.4";
       url = "https://github.com/NixOS/nixpkgs/";
       ref = "refs/heads/nixpkgs-unstable";
       rev = "860a644e0d07d769d39c4f89cac8c5d4ca686161";
