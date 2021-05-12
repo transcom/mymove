97c65fb9949d0df26edbbb6c5745c8a5d606d396
Yan Zhao <yan.zhao@truss.works>
2021-05-12 03:17:35 +0000

    Fix Console Errors From Services CounselingEditShipmentDetails
    - fix the match argument
    - add a blank handler and correct the parameter name

--- src/pages/Office/ServicesCounselingEditShipmentDetails/ServicesCounselingEditShipmentDetails.jsx
+++ src/pages/Office/ServicesCounselingEditShipmentDetails/ServicesCounselingEditShipmentDetails.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import { matchPath, useHistory, useLocation, useParams } from 'react-router-dom';
 import { GridContainer } from '@trussworks/react-uswds';
 
 import ServicesCounselingShipmentForm from 'components/Office/ServicesCounselingShipmentForm/ServicesCounselingShipmentForm';
 import { SHIPMENT_OPTIONS } from 'shared/constants';
 import { useMoveDetailsQueries } from 'hooks/queries';
-import { MatchShape } from 'types/customerShapes';
 import LoadingPlaceholder from 'shared/LoadingPlaceholder';
 import SomethingWentWrong from 'shared/SomethingWentWrong';
 
-const ServicesCounselingEditShipmentDetails = ({ match }) => {
+const ServicesCounselingEditShipmentDetails = () => {
   const { moveCode, shipmentId } = useParams();
   const history = useHistory();
   const { order, mtoShipments, isLoading, isError } = useMoveDetailsQueries(moveCode);
+  const { pathname } = useLocation();
 
   if (isLoading) return <LoadingPlaceholder />;
   if (isError) return <SomethingWentWrong />;
@@ -21,22 +21,17 @@ const ServicesCounselingEditShipmentDetails = ({ match }) => {
   const matchingShipment = mtoShipments.filter((shipment) => shipment.id === shipmentId)[0];
   const weightAllotment = { ...allowances, total_weight_self: allowances.authorizedWeight };
 
-  /*
-  console.log(`order:`);
-  console.log(order);
-  console.log(`mtoShipments`);
-  console.log(mtoShipments);
-  console.log(`customer:`);
-  console.log(customer);
-  console.log(allowances);
-  */
+  const updateMtoShipmentHandler = () => {};
 
   return (
     <GridContainer>
       <ServicesCounselingShipmentForm
-        match={match}
+        match={matchPath(pathname, {
+          isExact: true,
+          path: '/moves/:moveId/:shipmentId',
+        })}
         history={history}
-        updateMtoShipment={() => {}}
+        updateMTOShipment={updateMtoShipmentHandler}
         isCreatePage={false}
         currentResidence={customer.current_address}
         newDutyStationAddress={order.destinationDutyStation}
@@ -46,14 +41,6 @@ const ServicesCounselingEditShipmentDetails = ({ match }) => {
       />
     </GridContainer>
   );
-};
-
-ServicesCounselingEditShipmentDetails.propTypes = {
-  match: MatchShape,
-};
-
-ServicesCounselingEditShipmentDetails.defaultProps = {
-  match: { isExact: false, params: { moveID: '' } },
 };
 
 export default ServicesCounselingEditShipmentDetails;
