df2192f767711dd29878190b92e4cbe0a9d0ff2d
hannaliebl <hannaliebl@users.noreply.github.com>
2021-05-12 17:14:07 +0000

    Add migration to add deleted_at column to mto_shipments table and add DeletedAt field to corresponding model

---
+++ migrations/app/schema/20210512164642_add_deleted_at_to_mto_shipments_table.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE mto_shipments ADD COLUMN deleted_at timestamp with time zone;
--- pkg/models/mto_shipments.go
+++ pkg/models/mto_shipments.go
@@ -99,6 +99,7 @@ type MTOShipment struct {
 	Distance                         *unit.Miles       `db:"distance"`
 	CreatedAt                        time.Time         `db:"created_at"`
 	UpdatedAt                        time.Time         `db:"updated_at"`
+	DeletedAt                        *time.Time        `db:"deleted_at"`
 }
 
 // MTOShipments is a list of mto shipments
