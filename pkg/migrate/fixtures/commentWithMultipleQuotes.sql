4fe297091d2c8b6772cedc728377d59b6590154b
Marty Boren <marty@truss.works>
2021-05-12 20:32:34 +0000

    Fix test file
    This got out of sync with the corresponding test

--- pkg/migrate/fixtures/commentWithMultipleQuotes.sql
+++ pkg/migrate/fixtures/commentWithMultipleQuotes.sql
@@ -1,4 +1,4 @@
-COMMENT ON COLUMN public.office_emails.label IS 'The department the email gets sent to. For example, Customer Service';
+COMMENT ON COLUMN public.office_emails.label IS 'The department the email gets sent to. For example, ''Customer Service''';
 COMMENT ON COLUMN public.office_emails.updated_at IS '''triple quotes at start';
 COMMENT ON COLUMN public.office_emails.created_at IS 'Lots of quotes ''''within a string.''''';
 COMMENT ON COLUMN public.office_emails.updated_at IS 'Unbalanced quotes at end of string''';
