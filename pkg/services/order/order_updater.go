254fc32b1a72d0cdf8e6021553c0fe189fc600eb
Donald Thai <donald@truss.works>
2021-05-13 21:04:48 +0000

    - refactor UpdateOrder to simplify operations

--- pkg/services/order/order_updater.go
+++ pkg/services/order/order_updater.go
@@ -3,144 +3,89 @@ package order
 import (
 	"github.com/gobuffalo/pop/v5"
 	"github.com/gobuffalo/validate/v3"
-
-	"github.com/transcom/mymove/pkg/etag"
-
+	"github.com/gofrs/uuid"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
-	"github.com/transcom/mymove/pkg/services/query"
 )
 
 type orderUpdater struct {
 	db *pop.Connection
-	orderFetcher
 }
 
 // NewOrderUpdater creates a new struct with the service dependencies
 func NewOrderUpdater(db *pop.Connection) services.OrderUpdater {
-	return &orderUpdater{db, orderFetcher{db}}
+	return &orderUpdater{db}
 }
 
 // UpdateOrder updates the Order model
-func (s *orderUpdater) UpdateOrder(eTag string, order models.Order) (*models.Order, error) {
-	existingOrder, err := s.orderFetcher.FetchOrder(order.ID)
-	if err != nil {
-		return nil, services.NewNotFoundError(order.ID, "while looking for order")
-	}
+func (s *orderUpdater) UpdateOrder(order models.Order) (*models.Order, error) {
+	handleError := func(verrs *validate.Errors, err error) error {
+		if verrs != nil && verrs.HasAny() {
+			return services.NewInvalidInputError(order.ID, nil, verrs, "")
+		}
+		if err != nil {
+			return err
+		}
 
-	existingETag := etag.GenerateEtag(existingOrder.UpdatedAt)
-	if existingETag != eTag {
-		return nil, services.NewPreconditionFailedError(order.ID, query.StaleIdentifierError{StaleIdentifier: eTag})
+		return nil
 	}
 
 	transactionError := s.db.Transaction(func(tx *pop.Connection) error {
+		var verrs *validate.Errors
+		var err error
 
-		if order.ServiceMember.Affiliation != nil {
-			existingOrder.ServiceMember.Affiliation = order.ServiceMember.Affiliation
-			err = tx.Save(&existingOrder.ServiceMember)
-			if err != nil {
-				return err
-			}
-		}
-
-		if entitlement := order.Entitlement; entitlement != nil {
-			if entitlement.DBAuthorizedWeight != nil {
-				existingOrder.Entitlement.DBAuthorizedWeight = entitlement.DBAuthorizedWeight
-			}
-
-			if entitlement.DependentsAuthorized != nil {
-				existingOrder.Entitlement.DependentsAuthorized = entitlement.DependentsAuthorized
-			}
-
-			// TODO - Should we always update? Seems like we should consider fields that are not passed in for this Patch operation...
-			// TODO - Make these fields required since they're not nullable?
-			existingOrder.Entitlement.ProGearWeight = entitlement.ProGearWeight
-			existingOrder.Entitlement.ProGearWeightSpouse = entitlement.ProGearWeightSpouse
-			existingOrder.Entitlement.RequiredMedicalEquipmentWeight = entitlement.RequiredMedicalEquipmentWeight
-			existingOrder.Entitlement.OrganizationalClothingAndIndividualEquipment = entitlement.OrganizationalClothingAndIndividualEquipment
-
-			var verrs *validate.Errors
-			verrs, err = tx.ValidateAndUpdate(existingOrder.Entitlement)
-			if verrs != nil && verrs.HasAny() {
-				return services.NewInvalidInputError(order.ID, nil, verrs, "")
-			}
-			if err != nil {
-				return err
-			}
-		}
-
-		if order.OriginDutyStationID != existingOrder.OriginDutyStationID {
-			originDutyStation, fetchErr := models.FetchDutyStation(s.db, *order.OriginDutyStationID)
-			if fetchErr != nil {
-				return services.NewInvalidInputError(order.ID, fetchErr, nil, "unable to find origin duty station")
-			}
-			existingOrder.OriginDutyStationID = order.OriginDutyStationID
-			existingOrder.OriginDutyStation = &originDutyStation
-
-			existingOrder.ServiceMember.DutyStationID = &originDutyStation.ID
-			existingOrder.ServiceMember.DutyStation = originDutyStation
-
-			err = tx.Save(&existingOrder.ServiceMember)
-
-			if err != nil {
-				return err
-			}
-		}
-
-		if order.NewDutyStationID != existingOrder.NewDutyStationID {
-			newDutyStation, fetchErr := models.FetchDutyStation(s.db, order.NewDutyStationID)
-			if fetchErr != nil {
-				return services.NewInvalidInputError(order.ID, fetchErr, nil, "unable to find destination duty station")
-			}
-			existingOrder.NewDutyStationID = order.NewDutyStationID
-			existingOrder.NewDutyStation = newDutyStation
-		}
-
+		// update service member
 		if order.Grade != nil {
-			existingOrder.Grade = order.Grade
+			// keep grade and rank in sync
+			order.ServiceMember.Rank = (*models.ServiceMemberRank)(order.Grade)
+		}
 
-			existingOrder.ServiceMember.Rank = (*models.ServiceMemberRank)(order.Grade)
+		verrs, err = tx.ValidateAndUpdate(&order.ServiceMember)
+		if e := handleError(verrs, err); e != nil {
+			return e
+		}
 
-			err = tx.Save(&existingOrder.ServiceMember)
-
-			if err != nil {
-				return err
+		// update entitlement
+		if order.Entitlement != nil {
+			verrs, err = tx.ValidateAndUpdate(order.Entitlement)
+			if e := handleError(verrs, err); e != nil {
+				return e
 			}
 		}
 
-		if order.OrdersTypeDetail != nil {
-			existingOrder.OrdersTypeDetail = order.OrdersTypeDetail
+		if order.OriginDutyStationID != nil {
+			// TODO refactor to use service objects to fetch duty station
+			var originDutyStation models.DutyStation
+			originDutyStation, err = models.FetchDutyStation(s.db, *order.OriginDutyStationID)
+			if e := handleError(verrs, err); e != nil {
+				return e
+			}
+			order.OriginDutyStationID = &originDutyStation.ID
+			order.OriginDutyStation = &originDutyStation
+
+			order.ServiceMember.DutyStationID = &originDutyStation.ID
+			order.ServiceMember.DutyStation = originDutyStation
+
+			verrs, err = tx.ValidateAndUpdate(&order.ServiceMember)
+			if e := handleError(verrs, err); e != nil {
+				return e
+			}
 		}
 
-		if order.TAC != nil {
-			existingOrder.TAC = order.TAC
+		if order.NewDutyStationID != uuid.Nil {
+			// TODO refactor to use service objects to fetch duty station
+			var newDutyStation models.DutyStation
+			newDutyStation, err = models.FetchDutyStation(s.db, order.NewDutyStationID)
+			if e := handleError(verrs, err); e != nil {
+				return e
+			}
+			order.NewDutyStationID = newDutyStation.ID
+			order.NewDutyStation = newDutyStation
 		}
 
-		if order.SAC != nil {
-			existingOrder.SAC = order.SAC
-		}
-
-		if order.OrdersNumber != nil {
-			existingOrder.OrdersNumber = order.OrdersNumber
-		}
-
-		if order.DepartmentIndicator != nil {
-			existingOrder.DepartmentIndicator = order.DepartmentIndicator
-		}
-
-		existingOrder.IssueDate = order.IssueDate
-		existingOrder.ReportByDate = order.ReportByDate
-		existingOrder.OrdersType = order.OrdersType
-
-		// optimistic locking handled before transaction block
-		verrs, updateErr := tx.ValidateAndUpdate(existingOrder)
-
-		if verrs != nil && verrs.HasAny() {
-			return services.NewInvalidInputError(order.ID, err, verrs, "")
-		}
-
-		if updateErr != nil {
-			return updateErr
+		verrs, err = tx.ValidateAndUpdate(&order)
+		if e := handleError(verrs, err); e != nil {
+			return e
 		}
 
 		return nil
@@ -150,5 +95,5 @@ func (s *orderUpdater) UpdateOrder(eTag string, order models.Order) (*models.Ord
 		return nil, transactionError
 	}
 
-	return existingOrder, err
+	return &order, nil
 }
