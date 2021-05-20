6579ba4a0e1ddcec583541134cda1c30f2e17f63
hannaliebl <hannaliebl@users.noreply.github.com>
2021-05-20 20:06:05 +0000

    Add tests for rest of fields

--- src/pages/Office/CustomerInfo/CustomerInfo.test.jsx
+++ src/pages/Office/CustomerInfo/CustomerInfo.test.jsx
@@ -33,6 +33,24 @@ describe('CustomerInfo', () => {
         <CustomerInfo customer={mockCustomer} isLoading={false} isError={false} />{' '}
       </MockProviders>,
     );
-    expect(screen.getByLabelText('First name').value).toEqual('John');
+    expect(screen.getByLabelText('First name').value).toEqual(mockCustomer.first_name);
+    expect(screen.getByLabelText(/Middle name/i).value).toEqual(mockCustomer.middle_name);
+    expect(screen.getByLabelText('Last name').value).toEqual(mockCustomer.last_name);
+    expect(screen.getByLabelText(/Suffix/i).value).toEqual(mockCustomer.suffix);
+    // to get around the two inputs labeled "Phone" on the screen
+    expect(screen.getByDisplayValue(mockCustomer.phone).value).toEqual(mockCustomer.phone);
+    expect(screen.getByDisplayValue(mockCustomer.backup_contact.phone).value).toEqual(
+      mockCustomer.backup_contact.phone,
+    );
+    // to get around the two inputs labeled "Email" on the screen
+    expect(screen.getByDisplayValue(mockCustomer.email).value).toEqual(mockCustomer.email);
+    expect(screen.getByDisplayValue(mockCustomer.backup_contact.email).value).toEqual(
+      mockCustomer.backup_contact.email,
+    );
+    expect(screen.getByLabelText('Address 1').value).toEqual(mockCustomer.current_address.street_address_1);
+    expect(screen.getByLabelText('City').value).toEqual(mockCustomer.current_address.city);
+    expect(screen.getByLabelText('State').value).toEqual(mockCustomer.current_address.state);
+    expect(screen.getByLabelText('ZIP').value).toEqual(mockCustomer.current_address.postal_code);
+    expect(screen.getByLabelText('Name').value).toEqual(mockCustomer.backup_contact.name);
   });
 });
