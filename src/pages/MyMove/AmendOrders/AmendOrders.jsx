2f68b17c7339bd4bb1fa90df11783a570c808933
Marty Boren <marty@truss.works>
2021-06-11 19:53:32 +0000

    Temporarily connect amended orders uploader to uploaded_orders
    I added props and wired things up so you can see the service member's
    uploaded documents, and use the uploader component.
    
    Once we make the amended orders document available, we'll need to
    update this to use that instead.

--- src/pages/MyMove/AmendOrders/AmendOrders.jsx
+++ src/pages/MyMove/AmendOrders/AmendOrders.jsx
@@ -1,21 +1,68 @@
-import { React, createRef } from 'react';
+import { React, createRef, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { GridContainer, Grid } from '@trussworks/react-uswds';
+import { connect } from 'react-redux';
 
+import Hint from 'components/Hint';
 import SectionWrapper from 'components/Customer/SectionWrapper';
 import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
 import UploadsTable from 'components/UploadsTable/UploadsTable';
 import ScrollToTop from 'components/ScrollToTop';
 import FileUpload from 'components/FileUpload/FileUpload';
-import { UploadsShape } from 'types/customerShapes';
+import { UploadsShape, OrdersShape } from 'types/customerShapes';
+import { getOrdersForServiceMember, createUploadForDocument, deleteUpload } from 'services/internalApi';
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
+import {
+  selectCurrentOrders,
+  selectServiceMemberFromLoggedInUser,
+  selectUploadsForCurrentOrders,
+} from 'store/entities/selectors';
+import { updateOrders as updateOrdersAction } from 'store/entities/actions';
 
-const AmendOrders = ({ uploads }) => {
-  const handleDelete = () => {};
-  const handleUpload = () => {};
-  const onChange = () => {};
-  const handleSave = () => {};
-  const handleCancel = () => {};
-
+const AmendOrders = ({ uploads, updateOrders, serviceMemberId, currentOrders }) => {
+  const [isLoading, setLoading] = useState(true);
   const filePondEl = createRef();
+
+  const handleDelete = (uploadId) => {
+    return deleteUpload(uploadId).then(() => {
+      // TODO Temporarily using the original uploaded orders, will change to use amended orders once that is available
+      getOrdersForServiceMember(serviceMemberId).then((response) => {
+        updateOrders(response);
+      });
+    });
+  };
+  const handleUpload = (file) => {
+    // TODO Temporarily using the original uploaded orders, will change to use amended orders once that is available
+    const documentId = currentOrders?.uploaded_orders?.id;
+    return createUploadForDocument(file, documentId);
+  };
+  const handleUploadComplete = () => {
+    // TODO Temporarily using the original uploaded orders, will change to use amended orders once that is available
+    getOrdersForServiceMember(serviceMemberId).then((response) => {
+      updateOrders(response);
+    });
+  };
+
+  const onChange = () => {
+    filePondEl.current?.removeFiles();
+    handleUploadComplete();
+  };
+
+  const handleSave = () => {
+    // push(generalRoutes.HOME_PATH);
+  };
+  const handleCancel = () => {
+    // push(generalRoutes.HOME_PATH);
+  };
+
+  useEffect(() => {
+    getOrdersForServiceMember(serviceMemberId).then((response) => {
+      updateOrders(response);
+      setLoading(false);
+    });
+  });
+
+  if (isLoading) return <LoadingPlaceholder />;
 
   return (
     <GridContainer>
@@ -32,8 +79,8 @@ const AmendOrders = ({ uploads }) => {
       <Grid row>
         <Grid col desktop={{ col: 8, offset: 2 }}>
           <SectionWrapper>
-            <h1>Upload your orders</h1>
-            <p>PDF, JPG, or PNG only. Maximum file size 25MB. Each page must be clear and legible</p>
+            <h5>Upload orders</h5>
+            <Hint>PDF, JPG, or PNG only. Maximum file size 25MB. Each page must be clear and legible</Hint>
             {uploads && uploads.length > 0 && (
               <>
                 <br />
@@ -57,34 +104,35 @@ const AmendOrders = ({ uploads }) => {
 };
 
 AmendOrders.propTypes = {
+  serviceMemberId: PropTypes.string.isRequired,
+  updateOrders: PropTypes.func.isRequired,
+  currentOrders: OrdersShape,
   uploads: UploadsShape,
+  // push: PropTypes.func.isRequired,
 };
 
-// Temporary fake data
 AmendOrders.defaultProps = {
-  uploads: [
-    {
-      bytes: 9043,
-      content_type: 'image/png',
-      created_at: '2021-06-08T23:10:59.442Z',
-      filename: 'orders.png',
-      id: '2e5b2961-ca0c-4820-aa9d-a693c631b068',
-      status: 'PROCESSING',
-      updated_at: '2021-06-08T23:10:59.442Z',
-      url:
-        '/storage/user/11a6ca88-cf5a-4cfc-b4db-9a4cd0285bf3/uploads/2e5b2961-ca0c-4820-aa9d-a693c631b068?contentType=image%2Fpng',
-    },
-    {
-      bytes: 9043,
-      content_type: 'image/png',
-      created_at: '2021-06-08T23:11:31.885Z',
-      filename: 'orders.png',
-      id: '74455c13-f2d6-47bb-9cd1-e1fb712e3d0f',
-      status: 'PROCESSING',
-      updated_at: '2021-06-08T23:11:31.885Z',
-      url:
-        '/storage/user/11a6ca88-cf5a-4cfc-b4db-9a4cd0285bf3/uploads/74455c13-f2d6-47bb-9cd1-e1fb712e3d0f?contentType=image%2Fpng',
-    },
-  ],
+  uploads: [],
+  currentOrders: {},
 };
-export default AmendOrders;
+
+function mapStateToProps(state) {
+  const serviceMember = selectServiceMemberFromLoggedInUser(state);
+  const serviceMemberId = serviceMember?.id;
+  const currentOrders = selectCurrentOrders(state);
+
+  const props = {
+    serviceMemberId,
+    currentOrders,
+    uploads: selectUploadsForCurrentOrders(state),
+  };
+
+  return props;
+}
+
+const mapDispatchToProps = {
+  // TODO we might need a new action to handle updating amended orders
+  updateOrders: updateOrdersAction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AmendOrders);
