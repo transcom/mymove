2e3cf18f50cd400fcf99e2e21b6019cf56e7adc0
Donald Thai <donald@truss.works>
2021-06-16 20:59:30 +0000

    hide the Request diversion button if shipment is cancelled

--- src/components/Office/ShipmentAddresses/ShipmentAddresses.jsx
+++ src/components/Office/ShipmentAddresses/ShipmentAddresses.jsx
@@ -11,6 +11,8 @@ import DataPoint from '../../DataPoint/index';
 
 import styles from './ShipmentAddresses.module.scss';
 
+import { shipmentStatuses } from 'constants/shipments';
+
 const ShipmentAddresses = ({
   pickupAddress,
   destinationAddress,
@@ -25,13 +27,15 @@ const ShipmentAddresses = ({
         columnHeaders={[
           'Authorized addresses',
           <div className={styles.rightAlignButtonWrapper}>
-            <Button
-              type="button"
-              onClick={() => handleDivertShipment(shipmentInfo.shipmentID, shipmentInfo.ifMatchEtag)}
-              unstyled
-            >
-              Request diversion
-            </Button>
+            {shipmentInfo.shipmentStatus !== shipmentStatuses.CANCELED && (
+              <Button
+                type="button"
+                onClick={() => handleDivertShipment(shipmentInfo.shipmentID, shipmentInfo.ifMatchEtag)}
+                unstyled
+              >
+                Request diversion
+              </Button>
+            )}
           </div>,
         ]}
         dataRow={[formatAddress(originDutyStation), formatAddress(destinationDutyStation)]}
@@ -55,6 +59,7 @@ ShipmentAddresses.propTypes = {
   shipmentInfo: PropTypes.shape({
     shipmentID: PropTypes.string.isRequired,
     ifMatchEtag: PropTypes.string.isRequired,
+    shipmentStatus: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/components/Office/ShipmentAddresses/ShipmentAddresses.test.jsx
+++ src/components/Office/ShipmentAddresses/ShipmentAddresses.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ShipmentAddresses from './ShipmentAddresses';
@@ -33,13 +33,47 @@ const testProps = {
   shipmentInfo: {
     shipmentID: '456',
     ifMatchEtag: 'abc123',
+    shipmentStatus: 'APPROVED',
+  },
+};
+
+const cancelledShipment = {
+  pickupAddress: {
+    city: 'Fairfax',
+    state: 'VA',
+    postal_code: '12345',
+    street_address_1: '123 Fake Street',
+    street_address_2: '',
+    street_address_3: '',
+    country: 'USA',
+  },
+  destinationAddress: {
+    city: 'Boston',
+    state: 'MA',
+    postal_code: '01054',
+    street_address_1: '5 Main Street',
+    street_address_2: '',
+    street_address_3: '',
+    country: 'USA',
+  },
+  destinationDutyStation: {
+    street_address_1: '',
+    city: 'Fort Irwin',
+    state: 'CA',
+    postal_code: '92310',
+  },
+  handleDivertShipment: jest.fn(),
+  shipmentInfo: {
+    shipmentID: '456',
+    ifMatchEtag: 'abc123',
+    shipmentStatus: 'CANCELED',
   },
 };
 
 describe('ShipmentAddresses', () => {
   it('calls props.handleDivertShipment on request diversion button click', async () => {
-    const { getByRole } = render(<ShipmentAddresses {...testProps} />);
-    const requestDiversionBtn = getByRole('button', { name: 'Request diversion' });
+    render(<ShipmentAddresses {...testProps} />);
+    const requestDiversionBtn = screen.getByRole('button', { name: 'Request diversion' });
 
     userEvent.click(requestDiversionBtn);
     await waitFor(() => {
@@ -48,6 +82,15 @@ describe('ShipmentAddresses', () => {
         testProps.shipmentInfo.shipmentID,
         testProps.shipmentInfo.ifMatchEtag,
       );
+    });
+  });
+
+  it('hides the request diversion button for a cancelled shipment', async () => {
+    render(<ShipmentAddresses {...cancelledShipment} />);
+    const requestDiversionBtn = screen.queryByRole('button', { name: 'Request diversion' });
+
+    await waitFor(() => {
+      expect(requestDiversionBtn).toBeNull();
     });
   });
 });
