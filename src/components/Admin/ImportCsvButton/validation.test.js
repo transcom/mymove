96b7c2c2d556e8e1c5bc735d84b03f3aa8dac532
Sandy Wright <sandy@truss.works>
2021-06-28 18:13:31 +0000

    Add tests for parseRoles

---
+++ src/components/Admin/ImportCsvButton/validation.test.js
@@ -0,0 +1,50 @@
+import { parseRoles } from './validation';
+
+import { roleTypes, adminOfficeRoles } from 'constants/userRoles';
+
+describe('checkRequiredFields', () => {
+  it('success: does nothing if all fields provided', () => {});
+
+  it('fail: throws an error if fields are missing', () => {});
+});
+
+describe('checkTelephone', () => {
+  it('success: does nothing if telephone is valid', () => {});
+
+  it('fail: throws an error if telephone is invalid', () => {});
+});
+
+describe('parseRoles', () => {
+  const servicesCounselorAdminRole = adminOfficeRoles.filter(
+    (role) => role.roleType === roleTypes.SERVICES_COUNSELOR,
+  )[0];
+  const tooAdminRole = adminOfficeRoles.filter((role) => role.roleType === roleTypes.TOO)[0];
+
+  it('fail: throws an error if there are no roles', () => {
+    function parseEmptyRoles() {
+      parseRoles('');
+    }
+    expect(parseEmptyRoles).toThrowError('Unable to parse roles for row.');
+  });
+
+  it('success: parses one role into an array of len 1', () => {
+    const roles = roleTypes.SERVICES_COUNSELOR;
+    const rolesArray = parseRoles(roles);
+    expect(rolesArray).toHaveLength(1);
+    expect(rolesArray).toContainEqual(servicesCounselorAdminRole);
+  });
+
+  it('success: parses multiple roles into an array', () => {
+    const roles = `${roleTypes.SERVICES_COUNSELOR}, ${roleTypes.TOO}`;
+    const rolesArray = parseRoles(roles);
+    expect(rolesArray).toHaveLength(2);
+    expect(rolesArray).toEqual(expect.arrayContaining([servicesCounselorAdminRole, tooAdminRole]));
+  });
+
+  it('fail: throws an error if there is an invalid role', () => {
+    function parseInvalidRoles() {
+      parseRoles('test_role');
+    }
+    expect(parseInvalidRoles).toThrowError('Invalid roles provided for row.');
+  });
+});
