661f0418d62451f3344526b330cbf8b1de424388
Emily Mahanna <emahanna@truss.works>
2021-06-29 17:15:14 +0000

    fix comments on columns

--- migrations/app/schema/20210628223755_add_actual_and_estimated_weight_to_mto_service_items.up.sql
+++ migrations/app/schema/20210628223755_add_actual_and_estimated_weight_to_mto_service_items.up.sql
@@ -4,5 +4,5 @@ ALTER TABLE mto_service_items
     ADD COLUMN actual_weight integer;
 
 -- Column comments
-COMMENT ON COLUMN mto_service_items.estimated_weight IS 'The guessed weight for a shuttling service item. Relevant for DOSHUT service items.';
-COMMENT ON COLUMN mto_service_items.actual_weight IS 'The measured weight for a shuttling service item. Relevant for DOSHUT service items.';
+COMMENT ON COLUMN mto_service_items.estimated_weight IS 'An estimate of how much weight from a shipment will be included in a shuttling (DDSHUT & DOSHUT) service item.';
+COMMENT ON COLUMN mto_service_items.actual_weight IS 'Provided by the movers, based on weight tickets. Relevant for shuttling (DDSHUT & DOSHUT) service items.';
