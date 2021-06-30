8365b47015f5ecb01a95686b0e0a3e6200e8ef9e
Kyle Hill <kyle@truss.works>
2021-06-30 19:32:42 +0000

    Adds test case for approved, amended orders

--- src/pages/MyMove/Home/index.test.jsx
+++ src/pages/MyMove/Home/index.test.jsx
@@ -307,7 +307,7 @@ describe('Home component', () => {
       });
     });
 
-    describe('for amended orders', () => {
+    describe('for unapproved amended orders', () => {
       const submittedAt = new Date();
       const orders = {
         id: 'testOrder123',
@@ -334,6 +334,36 @@ describe('Home component', () => {
 
       it('renders the HelperAmendedOrders helper', () => {
         expect(wrapper.find('HelperAmendedOrders').exists()).toBe(true);
+      });
+    });
+
+    describe('for approved amended orders', () => {
+      const submittedAt = new Date();
+      const orders = {
+        id: 'testOrder123',
+        new_duty_station: {
+          name: 'Test Duty Station',
+        },
+      };
+      const uploadedOrderDocuments = [{ id: 'testDocument354', filename: 'testOrder1.pdf' }];
+      const uploadedAmendedOrderDocuments = [{ id: 'testDocument987', filename: 'testOrder2.pdf' }];
+      const move = { id: 'testMoveId', status: 'APPROVED', submitted_at: submittedAt };
+      const currentPpm = { id: 'mockCombo' };
+      const wrapper = mount(
+        <MockProviders initialEntries={['/']}>
+          <Home
+            {...defaultProps}
+            orders={orders}
+            uploadedOrderDocuments={uploadedOrderDocuments}
+            uploadedAmendedOrderDocuments={uploadedAmendedOrderDocuments}
+            move={move}
+            currentPpm={currentPpm}
+          />
+        </MockProviders>,
+      );
+
+      it('does not render the HelperAmendedOrders helper', () => {
+        expect(wrapper.find('HelperAmendedOrders').exists()).toBe(false);
       });
     });
   });
