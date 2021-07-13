a8f2f198702474bdd86473e2b50e97a5ccc43dfb
Yan Zhao <yan.zhao@truss.works>
2021-07-13 23:42:20 +0000

    Initial Take at Converting EditOrders to a Functional Component

--- src/pages/MyMove/EditOrders.jsx
+++ src/pages/MyMove/EditOrders.jsx
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react';
+import React, { createRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { get } from 'lodash';
@@ -7,7 +7,13 @@ import { push } from 'connected-react-router';
 import Alert from 'shared/Alert';
 import { withContext } from 'shared/AppContext';
 import scrollToTop from 'shared/scrollToTop';
-import { getOrdersForServiceMember, patchOrders, createUploadForDocument, deleteUpload } from 'services/internalApi';
+import {
+  getResponseError,
+  getOrdersForServiceMember,
+  patchOrders,
+  createUploadForDocument,
+  deleteUpload,
+} from 'services/internalApi';
 import { updateOrders as updateOrdersAction } from 'store/entities/actions';
 import { setFlashMessage as setFlashMessageAction } from 'store/flash/actions';
 import {
@@ -23,37 +29,34 @@ import { OrdersShape, HistoryShape } from 'types/customerShapes';
 import { EntitlementShape, ExistingUploadsShape } from 'types';
 import 'scenes/Review/Review.css';
 
-class EditOrders extends Component {
-  constructor(props) {
-    super(props);
+const EditOrders = ({
+  currentOrders,
+  serviceMemberId,
+  updateOrders,
+  existingUploads,
+  moveIsApproved,
+  spouseHasProGear,
+  history,
+  schema,
+  setFlashMessage,
+  entitlement,
+}) => {
+  const filePondEl = createRef();
+  const [serverError, setServerError] = useState(null);
 
-    this.filePondEl = createRef();
-  }
-
-  componentDidMount() {
-    const { serviceMemberId, updateOrders } = this.props;
-    getOrdersForServiceMember(serviceMemberId).then((response) => {
-      updateOrders(response);
-    });
-  }
-
-  handleUploadFile = (file) => {
-    const { currentOrders } = this.props;
+  const handleUploadFile = (file) => {
     const documentId = currentOrders?.uploaded_orders?.id;
     return createUploadForDocument(file, documentId);
   };
 
-  handleUploadComplete = () => {
-    const { serviceMemberId, updateOrders } = this.props;
-    this.filePondEl.current?.removeFiles();
+  const handleUploadComplete = () => {
+    filePondEl.current?.removeFiles();
     return getOrdersForServiceMember(serviceMemberId).then((response) => {
       updateOrders(response);
     });
   };
 
-  handleDeleteFile = (uploadId) => {
-    const { serviceMemberId, updateOrders } = this.props;
-
+  const handleDeleteFile = (uploadId) => {
     return deleteUpload(uploadId).then(() => {
       getOrdersForServiceMember(serviceMemberId).then((response) => {
         updateOrders(response);
@@ -61,9 +64,7 @@ class EditOrders extends Component {
     });
   };
 
-  submitOrders = (fieldValues) => {
-    const { setFlashMessage, entitlement, updateOrders, currentOrders, spouseHasProGear, history } = this.props;
-
+  const submitOrders = (fieldValues) => {
     let entitlementCouldChange = false;
 
     const fromFormSpouseHasProGear = (fieldValues.has_dependents && fieldValues.spouse_has_pro_gear) || false;
@@ -94,47 +95,50 @@ class EditOrders extends Component {
 
         history.goBack();
       })
-      .catch(() => {
+      .catch((e) => {
+        // TODO - error handling - below is rudimentary error handling to approximate existing UX
+        // Error shape: https://github.com/swagger-api/swagger-js/blob/master/docs/usage/http-client.md#errors
+        const { response } = e;
+        const errorMessage = getResponseError(response, 'failed to update orders due to server error');
+        setServerError(errorMessage);
+
         scrollToTop();
       });
   };
 
-  render() {
-    const { error, schema, currentOrders, existingUploads, moveIsApproved } = this.props;
-    return (
-      <div className="usa-grid">
-        {error && (
-          <div className="usa-width-one-whole error-message">
-            <Alert type="error" heading="An error occurred">
-              {error.message}
-            </Alert>
-          </div>
-        )}
-        {moveIsApproved && (
-          <div className="usa-width-one-whole error-message">
-            <Alert type="warning" heading="Your move is approved">
-              To make a change to your orders, you will need to contact your local PPPO office.
-            </Alert>
-          </div>
-        )}
-        {!moveIsApproved && (
-          <div className="usa-width-one-whole">
-            <EditOrdersForm
-              initialValues={currentOrders}
-              onSubmit={this.submitOrders}
-              schema={schema}
-              filePondEl={this.filePondEl}
-              createUpload={this.handleUploadFile}
-              onUploadComplete={this.handleUploadComplete}
-              existingUploads={existingUploads}
-              onDelete={this.handleDeleteFile}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="usa-grid">
+      {serverError && (
+        <div className="usa-width-one-whole error-message">
+          <Alert type="error" heading="An error occurred">
+            {serverError}
+          </Alert>
+        </div>
+      )}
+      {moveIsApproved && (
+        <div className="usa-width-one-whole error-message">
+          <Alert type="warning" heading="Your move is approved">
+            To make a change to your orders, you will need to contact your local PPPO office.
+          </Alert>
+        </div>
+      )}
+      {!moveIsApproved && (
+        <div className="usa-width-one-whole">
+          <EditOrdersForm
+            initialValues={currentOrders}
+            onSubmit={submitOrders}
+            schema={schema}
+            filePondEl={filePondEl}
+            createUpload={handleUploadFile}
+            onUploadComplete={handleUploadComplete}
+            existingUploads={existingUploads}
+            onDelete={handleDeleteFile}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 EditOrders.propTypes = {
   moveIsApproved: PropTypes.bool.isRequired,
@@ -145,16 +149,12 @@ EditOrders.propTypes = {
   history: HistoryShape.isRequired,
   entitlement: EntitlementShape.isRequired,
   existingUploads: ExistingUploadsShape,
-  error: PropTypes.shape({
-    message: PropTypes.string.isRequired,
-  }),
   schema: PropTypes.shape({}),
   spouseHasProGear: PropTypes.bool,
 };
 
 EditOrders.defaultProps = {
   existingUploads: [],
-  error: null,
   spouseHasProGear: false,
   schema: {},
 };
@@ -169,7 +169,6 @@ function mapStateToProps(state) {
     currentOrders,
     serviceMemberId,
     existingUploads: uploads,
-    error: get(state, 'orders.error'),
     moveIsApproved: selectMoveIsApproved(state),
     isPpm: selectHasCurrentPPM(state),
     schema: get(state, 'swaggerInternal.spec.definitions.CreateUpdateOrders', {}),
