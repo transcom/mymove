72c832a725e2d3b76b8599ab15502aaa9df051e5
Moncef Belyamani <moncef@truss.works>
2021-07-14 01:32:24 +0000

    MB-8665 Move order validations to order_updater
    When we first added validations to prevent a TOO from submitting a move
    while certain order fields were missing, we made the mistake of putting
    the validations in the Order model as opposed to the service. Back then,
    we only had one generic service for updating orders, so it might have
    made sense at the time.
    
    Since then, we've added more complexity, such as requiring separate
    validations for counselors versus TOOs, and allowing customers to
    upload amended orders even if the move has made it past service
    counseling but hasn't been updated by the TOO yet.
    
    Instead of trying to fit all of these different scenarios into a
    generic validation in the Order model, it now makes more sense to
    only add the validation where it applies, which is only when a TOO is
    updating an order and when they are approving a move.

---
+++ pkg/services/order/validation.go
@@ -0,0 +1,88 @@
+package order
+
+import (
+	"fmt"
+
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
+
+	"github.com/transcom/mymove/pkg/gen/internalmessages"
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
+)
+
+// Validator is the interface for the various validations we might want to
+// define.
+type Validator interface {
+	Validate(order *models.Order) error
+}
+
+type validatorFunc func(*models.Order) error
+
+func (fn validatorFunc) Validate(order *models.Order) error {
+	return fn(order)
+}
+
+// ValidateOrder accepts a range of validator functions and runs each one
+func ValidateOrder(order *models.Order, checks ...Validator) (result error) {
+	verrs := validate.NewErrors()
+	for _, checker := range checks {
+		if err := checker.Validate(order); err != nil {
+			switch e := err.(type) {
+			case *validate.Errors:
+				// accumulate validation errors
+				verrs.Append(e)
+			default:
+				// non-validation errors have priority,
+				// and short-circuit doing any further checks
+				return err
+			}
+		}
+	}
+	if verrs.HasAny() {
+		result = services.NewInvalidInputError(order.ID, nil, verrs, "")
+	}
+	return result
+}
+
+// StringIsPresentAfterSubmission checks presence of fields after an order has been submitted
+// This validation only applies when a TOO is updating orders.
+type StringIsPresentAfterSubmission struct {
+	Name  string
+	Field *string
+}
+
+// IsValid adds an error if the field is blank
+func (validate *StringIsPresentAfterSubmission) IsValid(errors *validate.Errors) {
+	if validate.Field == nil || *validate.Field == "" {
+		errors.Add(validators.GenerateKey(validate.Name), fmt.Sprintf("%s cannot be blank.", validate.Name))
+	}
+}
+
+// OrdersTypeDetailIsPresentAfterSubmission validates that orders type field is present
+type OrdersTypeDetailIsPresentAfterSubmission struct {
+	Name  string
+	Field *internalmessages.OrdersTypeDetail
+}
+
+// IsValid adds an error if the string value is blank.
+func (validate *OrdersTypeDetailIsPresentAfterSubmission) IsValid(errors *validate.Errors) {
+	if validate.Field == nil || string(*validate.Field) == "" {
+		errors.Add(validators.GenerateKey(validate.Name), fmt.Sprintf("%s cannot be blank.", validate.Name))
+	}
+}
+
+// CheckRequiredFields ensures the presence of certain order fields before a TOO
+// can approve a move or update an order.
+func CheckRequiredFields() Validator {
+	return validatorFunc(func(order *models.Order) error {
+		verrs := validate.Validate(
+			&StringIsPresentAfterSubmission{Name: "TransportationAccountingCode", Field: order.TAC},
+			&StringIsPresentAfterSubmission{Name: "DepartmentIndicator", Field: order.DepartmentIndicator},
+			&StringIsPresentAfterSubmission{Name: "OrdersNumber", Field: order.OrdersNumber},
+			&OrdersTypeDetailIsPresentAfterSubmission{Name: "OrdersTypeDetail", Field: order.OrdersTypeDetail},
+		)
+
+		return verrs
+	})
+}
