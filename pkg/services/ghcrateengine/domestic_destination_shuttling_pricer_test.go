eeca90bde53b2ae16d237eb3163eed86fd306398
pearl-truss <pearl@truss.works>
2021-07-14 02:41:59 +0000

    added destination shuttling pricer

---
+++ pkg/services/ghcrateengine/domestic_destination_shuttling_pricer_test.go
@@ -0,0 +1,48 @@
+package ghcrateengine
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *GHCRateEngineServiceSuite) setupDomesticDestinationShuttlingServiceItem() models.PaymentServiceItem {
+	return testdatagen.MakeDefaultPaymentServiceItemWithParams(
+		suite.DB(),
+		models.ReServiceCodeDDSHUT,
+		[]testdatagen.CreatePaymentServiceItemParams{
+			{
+				Key:     models.ServiceItemParamNameContractCode,
+				KeyType: models.ServiceItemParamTypeString,
+				Value:   testdatagen.DefaultContractCode,
+			},
+			{
+				Key:     models.ServiceItemParamNameRequestedPickupDate,
+				KeyType: models.ServiceItemParamTypeDate,
+				Value:   dofsitTestRequestedPickupDate.Format(DateParamFormat),
+			},
+			{
+				Key:     models.ServiceItemParamNameServicesScheduleOrigin,
+				KeyType: models.ServiceItemParamTypeInteger,
+				Value:   strconv.Itoa(servicesScheduleDest),
+			},
+			{
+				Key:     models.ServiceItemParamNameWeightActual,
+				KeyType: models.ServiceItemParamTypeInteger,
+				Value:   "1400",
+			},
+			{
+				Key:     models.ServiceItemParamNameWeightBilledActual,
+				KeyType: models.ServiceItemParamTypeInteger,
+				Value:   fmt.Sprintf("%d", int(testWeight)),
+			},
+			{
+				Key:     models.ServiceItemParamNameWeightEstimated,
+				KeyType: models.ServiceItemParamTypeInteger,
+				Value:   "1400",
+			},
+		},
+	)
+}
