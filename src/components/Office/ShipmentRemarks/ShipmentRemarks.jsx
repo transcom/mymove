2a538578c3e147c7a31b087560fcc1cdd2ae44d0
hannaliebl <hannaliebl@users.noreply.github.com>
2021-07-16 16:51:47 +0000

    Remove margin from last remarks container

--- src/components/Office/ShipmentRemarks/ShipmentRemarks.jsx
+++ src/components/Office/ShipmentRemarks/ShipmentRemarks.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import classnames from 'classnames';
 import * as PropTypes from 'prop-types';
 
 import DataPointGroup from '../../DataPointGroup/index';
 import DataPoint from '../../DataPoint/index';
+import styles from '../ShipmentDetails/ShipmentDetails.module.scss';
 
 const ShipmentRemarks = ({ title, remarks }) => {
   return (
-    <DataPointGroup className="maxw-tablet">
+    <DataPointGroup className={classnames('maxw-tablet', styles.ShipmentRemarks)}>
       <DataPoint columnHeaders={[title]} dataRow={[remarks]} />
     </DataPointGroup>
   );
