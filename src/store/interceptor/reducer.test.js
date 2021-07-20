e7ce68852265c5fcde5866c3958568f1996eafd1
Kyle Hill <kyle@truss.works>
2021-07-20 16:56:36 +0000

    Adds logic to reducer to handle INTERCEPT_RESPONSE events

--- src/store/interceptor/reducer.test.js
+++ src/store/interceptor/reducer.test.js
@@ -1,7 +1,46 @@
+import { interceptResponse } from './actions';
 import interceptorReducer, { initialState } from './reducer';
+
+const SHORT_RESPONSE_GAP_MS = 100;
+const LONG_RESPONSE_GAP_MS = 4000;
 
 describe('interceptorReducer', () => {
   it('returns the initial state by default', () => {
     expect(interceptorReducer(undefined, undefined)).toEqual(initialState);
+  });
+
+  describe('INTERCEPT_RESPONSE', () => {
+    it('updates state if an error was intercepted', () => {
+      const oldState = {
+        hasRecentError: false,
+        timestamp: Date.now() - SHORT_RESPONSE_GAP_MS,
+      };
+
+      const newState = interceptorReducer(oldState, interceptResponse(true));
+      expect(newState.hasRecentError).toEqual(true);
+      expect(newState.timestamp - oldState.timestamp).toBeGreaterThanOrEqual(SHORT_RESPONSE_GAP_MS);
+    });
+
+    it('maintains hasRecentError value if a recent response was intercepted', () => {
+      const oldState = {
+        hasRecentError: true,
+        timestamp: Date.now() - SHORT_RESPONSE_GAP_MS,
+      };
+
+      const newState = interceptorReducer(oldState, interceptResponse(false));
+      expect(newState.hasRecentError).toEqual(true);
+      expect(newState.timestamp - oldState.timestamp).toBeGreaterThanOrEqual(SHORT_RESPONSE_GAP_MS);
+    });
+
+    it('updates hasRecentError value if timestamp is stale', () => {
+      const oldState = {
+        hasRecentError: true,
+        timestamp: Date.now() - LONG_RESPONSE_GAP_MS,
+      };
+
+      const newState = interceptorReducer(oldState, interceptResponse(false));
+      expect(newState.hasRecentError).toEqual(false);
+      expect(newState.timestamp - oldState.timestamp).toBeGreaterThanOrEqual(LONG_RESPONSE_GAP_MS);
+    });
   });
 });
