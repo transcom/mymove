f69203ac229c5f538b6ffe56689cd1ac2db770bf
Carter Jones <carter@truss.works>
2021-07-21 12:54:32 +0000

    add another aws document reference

--- migrations/app/schema/20210720183133_create_rds_pgaudit_role.up.sql
+++ migrations/app/schema/20210720183133_create_rds_pgaudit_role.up.sql
@@ -3,4 +3,5 @@ SET ROLE master;
 
 -- Create a new role named "rds_pgaudit" as indicated by the AWS documentation:
 -- https://aws.amazon.com/premiumsupport/knowledge-center/rds-postgresql-pgaudit/
+-- https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.PostgreSQL.CommonDBATasks.html#Appendix.PostgreSQL.CommonDBATasks.pgaudit
 CREATE ROLE rds_pgaudit;
