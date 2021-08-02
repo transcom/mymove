2b96485eb7a14219bed98fd01c484191c2ca091f
Marty Boren <marty@truss.works>
2021-08-02 20:34:06 +0000

    Add tests for unit.CubicFeet

--- pkg/unit/volume_test.go
+++ pkg/unit/volume_test.go
@@ -5,13 +5,35 @@ import (
 )
 
 func Test_CubicFeetStringConversion(t *testing.T) {
-	// baseQuantity := BaseQuantity(10000)
-	// result := baseQuantity.String()
+	cubicFeet := CubicFeet(10.0)
+	result := cubicFeet.String()
 
-	// expected := "10000"
-	// if result != expected {
-	// 	t.Errorf("wrong string of BaseQuantity: expected %s, got %s", expected, result)
-	// }
+	expected := "10.00"
+	if result != expected {
+		t.Errorf("wrong string of BaseQuantity: expected %s, got %s", expected, result)
+	}
 }
 
-// add function testing parse from string
+func Test_CubicFeetFromStringSuccess(t *testing.T) {
+	result, err := CubicFeetFromString("11.5")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := CubicFeet(11.5)
+	if result != expected {
+		t.Errorf("Incorrectly parsed CubicFeet string: expected %f, got %f", expected, result)
+	}
+}
+
+func Test_CubicFeetFromStringFailure(t *testing.T) {
+	result, err := CubicFeetFromString("11.5")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := CubicFeet(11.5)
+	if result != expected {
+		t.Errorf("Incorrectly parsed CubicFeet string: expected %f, got %f", expected, result)
+	}
+}
