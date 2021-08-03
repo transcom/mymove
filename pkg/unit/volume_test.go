ff769b0491bf946352bfdc6b8d5056f7ac8a7bbd
Marty Boren <marty@truss.works>
2021-08-03 19:35:27 +0000

    Remove unused function

--- pkg/unit/volume_test.go
+++ pkg/unit/volume_test.go
@@ -13,27 +13,3 @@ func Test_CubicFeetStringConversion(t *testing.T) {
 		t.Errorf("wrong string of BaseQuantity: expected %s, got %s", expected, result)
 	}
 }
-
-func Test_CubicFeetFromStringSuccess(t *testing.T) {
-	result, err := CubicFeetFromString("11.5")
-	if err != nil {
-		t.Error(err)
-	}
-
-	expected := CubicFeet(11.5)
-	if result != expected {
-		t.Errorf("Incorrectly parsed CubicFeet string: expected %f, got %f", expected, result)
-	}
-}
-
-func Test_CubicFeetFromStringFailure(t *testing.T) {
-	result, err := CubicFeetFromString("11.5")
-	if err != nil {
-		t.Error(err)
-	}
-
-	expected := CubicFeet(11.5)
-	if result != expected {
-		t.Errorf("Incorrectly parsed CubicFeet string: expected %f, got %f", expected, result)
-	}
-}
