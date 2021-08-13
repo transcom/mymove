990708d386e40c67a51573473ef4fde40027e441
Duncan Spencer <duncan@truss.works>
2021-08-13 20:52:59 +0000

    add test for undefined and zero estimated weight values

--- src/utils/hasRiskOfExcess.test.js
+++ src/utils/hasRiskOfExcess.test.js
@@ -12,4 +12,12 @@ describe('hasRiskOfExcess', () => {
   it('returns false when estimated weight is less than 90% of weight allowancew', () => {
     expect(hasRiskOfExcess(89, 100)).toEqual(false);
   });
+
+  it('returns false when estimated weight is undefined', () => {
+    expect(hasRiskOfExcess(undefined, 100)).toEqual(false);
+  });
+
+  it('returns false when estimated weight is zero', () => {
+    expect(hasRiskOfExcess(0, 100)).toEqual(false);
+  });
 });
