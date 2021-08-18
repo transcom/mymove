57a875be59e6d8a49e2bf917f2457d6925daf898
pearl-truss <pearl@truss.works>
2021-08-18 16:04:00 +0000

    updated unit test

--- src/components/ShipmentList/ShipmentList.test.jsx
+++ src/components/ShipmentList/ShipmentList.test.jsx
@@ -1,23 +1,23 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
 
 import ShipmentList from '.';
 
-const shipments = [
-  { id: 'ID-1', shipmentType: 'PPM' },
-  { id: 'ID-2', shipmentType: 'HHG' },
-  { id: 'ID-3', shipmentType: 'HHG_INTO_NTS_DOMESTIC' },
-  { id: 'ID-4', shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
-];
-const onShipmentClick = jest.fn();
-const defaultProps = {
-  shipments,
-  onShipmentClick,
-  moveSubmitted: false,
-};
-
 describe('ShipmentList component', () => {
+  const shipments = [
+    { id: 'ID-1', shipmentType: 'PPM' },
+    { id: 'ID-2', shipmentType: 'HHG' },
+    { id: 'ID-3', shipmentType: 'HHG_INTO_NTS_DOMESTIC' },
+    { id: 'ID-4', shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+  ];
+  const onShipmentClick = jest.fn();
+  const defaultProps = {
+    shipments,
+    onShipmentClick,
+    moveSubmitted: false,
+  };
   it('renders ShipmentList with shipments', () => {
     const wrapper = mount(<ShipmentList {...defaultProps} />);
     expect(wrapper.find('ShipmentListItem').length).toBe(4);
@@ -48,5 +48,37 @@ describe('ShipmentList component', () => {
     expect(shipmentOneId[0]).toEqual(shipments[0].id);
     expect(shipmentTwoId[0]).toEqual(shipments[1].id);
     expect(shipmentThreeId[0]).toEqual(shipments[2].id);
+  });
+});
+
+describe('BillableWeightCard', () => {
+  it('renders maximum billable weight, total billable weight, weight requested and weight allowance', () => {
+    const shipments = [
+      { id: '0001', shipmentType: 'HHG', billableWeightCap: '5,600' },
+      { id: '0002', shipmentType: 'HHG', billableWeightCap: '3,200', reweigh: { id: '1234' } },
+      { id: '0003', shipmentType: 'HHG', billableWeightCap: '3,400' },
+    ];
+    const entitlements = [
+      { id: '1234', shipmentId: '0001', authorizedWeight: '4,600' },
+      { id: '12346', shipmentId: '0002', authorizedWeight: '4,600' },
+      { id: '12347', shipmentId: '0003', authorizedWeight: '4,600' },
+    ];
+    const defaultProps = {
+      shipments,
+      entitlements,
+      moveSubmitted: false,
+      showShipmentWeight: true,
+    };
+
+    const { getByText } = render(<ShipmentList {...defaultProps} />);
+
+    // flags
+    expect(getByText('Over weight')).toBeInTheDocument();
+    expect(getByText('Missing weight')).toBeInTheDocument();
+
+    // weights
+    expect(getByText(`${shipments[0].billableWeightCap} lbs`)).toBeInTheDocument();
+    expect(getByText(`${shipments[1].billableWeightCap} lbs`)).toBeInTheDocument();
+    expect(getByText(`${shipments[2].billableWeightCap} lbs`)).toBeInTheDocument();
   });
 });
