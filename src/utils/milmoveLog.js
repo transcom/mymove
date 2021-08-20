97bb72b3a6f920b6940e29921f11a19ac62d04bf
John Gedeon <john@truss.works>
2021-08-20 19:39:06 +0000

    Add annotation for disabling no-unused-vars in milmoveLog

--- src/utils/milmoveLog.js
+++ src/utils/milmoveLog.js
@@ -9,9 +9,16 @@ export const MILMOVE_LOG_LEVEL = {
 
 // milmoveLog: wrapper log to re-direct logging to something other than console.log/warn/error
 // use the levels set by MILMOVE_LOG_LEVEL
-// TODO There is an ADR in the works to figure out what the final solution will be.
-// TODO https://github.com/transcom/mymove/pull/7007
-// TODO for now removing all use of console.warn console.error console.debug console.log console.info
-// TODO this new function to be used re-direct logs to the final destination
+//
+// RA Summary: eslint - no-unused-vars - Should not have unused variables
+// RA: The method parameters 'level' and 'message' are not used within the method
+// RA: This method is a place holder so it is a no-op There is an ADR in the works to figure out what the final solution will be.
+// RA: https://github.com/transcom/mymove/pull/7007
+// RA: for now removing all use of console.warn console.error console.debug console.log console.info
+// RA: this new function to be used re-direct logs to the final destination
+// RA Developer Status: Known Issue
+// RA Validator Status: {RA Accepted, Return to Developer, Known Issue, Mitigated, False Positive, Bad Practice}
+// RA Validator:
+// RA Modified Severity:
 // eslint-disable-next-line no-unused-vars
 export function milmoveLog(level, message) {}
