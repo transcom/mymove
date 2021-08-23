9844cb974ad2b7ca8bfa46630a0b9af2f424384b
Sandy Wright <sandy@truss.works>
2021-08-23 15:22:44 -0500

    MB-9020 Upd auth Logger to be more standard

--- pkg/auth/authentication/logger.go
+++ pkg/auth/authentication/logger.go
@@ -6,6 +6,7 @@ import (
 
 // Logger is an interface that describes the logging requirements of this package.
 type Logger interface {
+	Debug(msg string, fields ...zap.Field)
 	Info(msg string, fields ...zap.Field)
 	Error(msg string, fields ...zap.Field)
 }
