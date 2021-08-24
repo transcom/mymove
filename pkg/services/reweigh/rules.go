358ded019e12abb7598be952eff826e8547e9040
Namibia Torres <namibia@truss.works>
2021-08-23 17:20:53 -0700

    add rules and validations

--- pkg/services/reweigh/rules.go
+++ pkg/services/reweigh/rules.go
@@ -1,1 +1,102 @@
 package reweigh
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
+)
+
+// checkShipmentID checks that the user can't change the shipment ID
+func checkShipmentID() reweighValidator {
+	return reweighValidatorFunc(func(_ context.Context, newReweigh models.Reweigh, oldReweigh *models.Reweigh, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+		if oldReweigh == nil {
+			if newReweigh.ShipmentID == uuid.Nil {
+				verrs.Add("ShipmentID", "Shipment ID is required")
+			}
+		} else {
+			if newReweigh.ShipmentID != uuid.Nil && newReweigh.ShipmentID != oldReweigh.ShipmentID {
+				verrs.Add("ShipmentID", "cannot be updated")
+			}
+		}
+		return verrs
+	})
+}
+
+// checkReweighID checks that the user can't change the reweigh ID
+func checkReweighID() reweighValidator {
+	return reweighValidatorFunc(func(_ context.Context, newReweigh models.Reweigh, oldReweigh *models.Reweigh, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+		if oldReweigh == nil {
+			if newReweigh.ID != uuid.Nil {
+				verrs.Add("ID", "cannot manually set a new reweigh's UUID")
+			}
+		} else {
+			if newReweigh.ID != oldReweigh.ID {
+				return services.NewImplementationError(
+					fmt.Sprintf("the newReweigh ID (%s) must match oldReweigh ID (%s).", newReweigh.ID, oldReweigh.ID),
+				)
+			}
+		}
+		return verrs
+	})
+}
+
+// checkRequiredFields checks that the required fields are included
+func checkRequiredFields() reweighValidator {
+	return reweighValidatorFunc(func(_ context.Context, reweigh models.Reweigh, oldReweigh *models.Reweigh, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+
+		var requestedAt time.Time
+		var requestedBy models.ReweighRequester
+
+		// Set any pre-existing values as the baseline:
+		if oldReweigh != nil {
+			requestedAt = oldReweigh.RequestedAt
+			requestedBy = oldReweigh.RequestedBy
+		}
+
+		// Override pre-existing values with anything sent in for the update/create:
+		if !reweigh.RequestedAt.IsZero() {
+			requestedAt = reweigh.RequestedAt
+		}
+		if reweigh.RequestedBy != "" {
+			requestedBy = reweigh.RequestedBy
+		}
+
+		// Check that we have something in the RequestedAt field:
+		// I WANT TO SAY: IF TIME.TIME is not empty for requestedAt
+		if requestedAt.IsZero() {
+			verrs.Add("requestedAt", "cannot be empty")
+		}
+
+		// Check that we have something in the RequestedBy field:
+		if requestedBy == "" {
+			verrs.Add("requestedBy", "cannot be blank")
+		}
+
+		return verrs
+	})
+}
+
+//checks that the shipment associated with the reweigh is available to Prime
+func checkPrimeAvailability(checker services.MoveTaskOrderChecker) reweighValidator {
+	return reweighValidatorFunc(func(ctx context.Context, newReweigh models.Reweigh, oldReweigh *models.Reweigh, shipment *models.MTOShipment) error {
+		if shipment == nil {
+			return services.NewNotFoundError(newReweigh.ID, "while looking for Prime-available Shipment")
+		}
+
+		isAvailable, err := checker.MTOAvailableToPrime(shipment.MoveTaskOrderID)
+		if !isAvailable || err != nil {
+			return services.NewNotFoundError(
+				newReweigh.ID, fmt.Sprintf("while looking for Prime-available Shipment with id: %s", shipment.ID))
+		}
+		return nil
+	})
+}
--- pkg/services/reweigh/validation.go
+++ pkg/services/reweigh/validation.go
@@ -1,1 +1,25 @@
 package reweigh
+
+import (
+	"context"
+
+	"github.com/transcom/mymove/pkg/models"
+)
+
+// checks business requirements
+// reweighValidator describes a method for checking business requirements
+type reweighValidator interface {
+	// Validate checks the newReweigh for adherence to business rules. The
+	// oldReweigh parameter is expected to be nil in creation use cases.
+	// It is safe to return a *validate.Errors with zero added errors as
+	// a success case.
+	Validate(c context.Context, newReweigh models.Reweigh, oldReweigh *models.Reweigh, shipment *models.MTOShipment) error
+}
+
+// reweighValidatorFunc is an adapter type for converting a function into an implementation of reweighValidator
+type reweighValidatorFunc func(context.Context, models.Reweigh, *models.Reweigh, *models.MTOShipment) error
+
+// Validate fulfills the reweighValidator interface
+func (fn reweighValidatorFunc) Validate(ctx context.Context, newer models.Reweigh, older *models.Reweigh, ship *models.MTOShipment) error {
+	return fn(ctx, newer, older, ship)
+}
