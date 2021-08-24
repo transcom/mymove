52e44703ca139d1cec47360700e559699b802a12
Namibia Torres <namibia@truss.works>
2021-08-24 22:33:12 +0000

    update creator to search if shipment ID exists

--- pkg/services/reweigh/reweigh_creator.go
+++ pkg/services/reweigh/reweigh_creator.go
@@ -25,14 +25,18 @@ func NewReweighsCreator(db *pop.Connection) services.ReweighCreator {
 
 // CreateReweigh creates a reweigh
 func (f *reweighCreator) CreateReweigh(reweigh *models.Reweigh) (*models.Reweigh, error) {
+	// Get existing shipment and agents information for validation
+	mtoShipment := &models.MTOShipment{}
+	err := f.db.Find(mtoShipment, reweigh.ShipmentID)
+	if err == sql.ErrNoRows {
+		return nil, services.NewNotFoundError(reweigh.ShipmentID, "while looking for MTOShipment")
+	}
+
 	verrs, err := f.db.ValidateAndCreate(reweigh)
 
 	if verrs != nil && verrs.HasAny() {
 		return nil, services.NewInvalidInputError(uuid.Nil, err, verrs, "Invalid input found while creating the reweigh.")
 	} else if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, services.NewNotFoundError(reweigh.ShipmentID, "while looking for MTOShipment")
-		}
 		// If the error is something else (this is unexpected), we create a QueryError
 		return nil, services.NewQueryError("Reweigh", err, "")
 	}
