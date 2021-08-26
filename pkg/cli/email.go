fab1241db06e3610b21f42443e44d3b7e02ccecc
Sandy Wright <sandy@truss.works>
2021-08-26 10:46:41 -0500

    Update sys admin email to dp3-alerts@truss.works

--- pkg/cli/email.go
+++ pkg/cli/email.go
@@ -25,7 +25,7 @@ func InitEmailFlags(flag *pflag.FlagSet) {
 	flag.String(EmailBackendFlag, "local", "Email backend to use, either 'ses' or 'local'")
 	flag.String(AWSSESRegionFlag, "", "AWS region used for SES")
 	flag.String(AWSSESDomainFlag, "", "Domain used for SES")
-	flag.String(SysAdminEmail, "sandy+flag@truss.works", "Email address for the system administrators")
+	flag.String(SysAdminEmail, "dp3-alerts@truss.works", "Email address for the system administrators")
 }
 
 // CheckEmail validates Email command line flags
