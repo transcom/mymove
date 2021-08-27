73e651676c9db0bb47216f4ccc89fc80c3be9744
Ron Cruz <Ronolibert@gmail.com>
2021-08-27 18:32:41 +0000

    Used EditBillableWeight component in ShipmentCard

--- src/components/Office/BillableWeight/ShipmentCard/ShipmentCard.jsx
+++ src/components/Office/BillableWeight/ShipmentCard/ShipmentCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { string, number, shape } from 'prop-types';
 import classnames from 'classnames';
-import { Button } from '@trussworks/react-uswds';
+
+import EditBillableWeight from '../EditBillableWeight/EditBillableWeight';
 
 import styles from './ShipmentCard.module.scss';
 
@@ -65,7 +66,7 @@ export default function ShipmentCard({
       <footer>
         <h3>Billable weight</h3>
         <span>{formatWeight(billableWeight)}</span>
-        <Button className={styles.editBtn}>Edit</Button>
+        <EditBillableWeight />
       </footer>
     </ShipmentContainer>
   );
