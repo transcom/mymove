e5316333cd8a7987a6bf7c83be0ffeb15259944e
Felipe Lee <felipe@truss.works>
2021-08-27 21:07:47 +0000

    MB-9239 Fix ShipmentContainer.test.jsx to pass missing required props

--- src/components/Office/ShipmentContainer/ShipmentContainer.test.jsx
+++ src/components/Office/ShipmentContainer/ShipmentContainer.test.jsx
@@ -5,25 +5,33 @@ import ShipmentHeading from '../ShipmentHeading/ShipmentHeading';
 
 import ShipmentContainer from './ShipmentContainer';
 
+import { shipmentStatuses } from 'constants/shipments';
 import { SHIPMENT_OPTIONS } from 'shared/constants';
 
 const headingInfo = {
-  shipmentType: 'Household Goods',
-  originCity: 'San Antonio',
-  originState: 'TX',
-  originPostalCode: '98421',
-  destinationCity: 'Tacoma',
-  destinationState: 'WA',
-  destinationPostalCode: '98421',
-  scheduledPickupDate: '27 Mar 2020',
-  reweigh: { id: '00000000-0000-0000-0000-000000000000' },
+  shipmentInfo: {
+    shipmentID: '1',
+    shipmentStatus: shipmentStatuses.SUBMITTED,
+    shipmentType: 'Household Goods',
+    originCity: 'San Antonio',
+    originState: 'TX',
+    originPostalCode: '98421',
+    destinationCity: 'Tacoma',
+    destinationState: 'WA',
+    destinationPostalCode: '98421',
+    scheduledPickupDate: '27 Mar 2020',
+    reweigh: { id: '00000000-0000-0000-0000-000000000000' },
+    ifMatchEtag: 'etag',
+    moveTaskOrderID: 'mtoID',
+  },
+  handleShowCancellationModal: jest.fn(),
 };
 
 describe('Shipment Container', () => {
   it('renders the container successfully', () => {
     const wrapper = shallow(
       <ShipmentContainer>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('[data-testid="ShipmentContainer"]').exists()).toBe(true);
@@ -31,7 +39,7 @@ describe('Shipment Container', () => {
   it('renders a child component passed to it', () => {
     const wrapper = shallow(
       <ShipmentContainer>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find(ShipmentHeading).length).toBe(1);
@@ -39,21 +47,21 @@ describe('Shipment Container', () => {
   it('renders a container with className container--accent--hhg', () => {
     let wrapper = shallow(
       <ShipmentContainer shipmentType={SHIPMENT_OPTIONS.HHG}>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('.container--accent--hhg').length).toBe(1);
 
     wrapper = shallow(
       <ShipmentContainer shipmentType={SHIPMENT_OPTIONS.HHG_SHORTHAUL_DOMESTIC}>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('.container--accent--hhg').length).toBe(1);
 
     wrapper = shallow(
       <ShipmentContainer shipmentType={SHIPMENT_OPTIONS.HHG_LONGHAUL_DOMESTIC}>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('.container--accent--hhg').length).toBe(1);
@@ -61,7 +69,7 @@ describe('Shipment Container', () => {
   it('renders a container with className container--accent--nts', () => {
     const wrapper = shallow(
       <ShipmentContainer shipmentType={SHIPMENT_OPTIONS.NTS}>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('.container--accent--nts').length).toBe(1);
@@ -69,7 +77,7 @@ describe('Shipment Container', () => {
   it('renders a container with className container--accent--ntsr', () => {
     const wrapper = shallow(
       <ShipmentContainer shipmentType={SHIPMENT_OPTIONS.NTSR}>
-        <ShipmentHeading shipmentInfo={headingInfo} />
+        <ShipmentHeading {...headingInfo} />
       </ShipmentContainer>,
     );
     expect(wrapper.find('.container--accent--ntsr').length).toBe(1);
