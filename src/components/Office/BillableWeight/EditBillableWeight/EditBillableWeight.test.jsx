d5f1cf7e69dab063140071d32ccd9311035e5114
Ron Cruz <Ronolibert@gmail.com>
2021-08-27 18:02:23 +0000

    Added unit tests to EditBillableWeight component

---
+++ src/components/Office/BillableWeight/EditBillableWeight/EditBillableWeight.test.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import EditBillableWeight from './EditBillableWeight';
+
+import { formatWeight } from 'shared/formatters';
+
+describe('EditBillableWeight', () => {
+  it('renders edit button intially', () => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const defaultProps = {
+      weightAllowance: 8000,
+      estimatedWeight: 13750,
+    };
+
+    render(<EditBillableWeight {...defaultProps} />);
+
+    expect(screen.getByRole('button', { name: 'Edit' })).toBeInTheDocument();
+  });
+
+  it('clicking edit button shows different view', () => {
+    const defaultProps = {
+      weightAllowance: 8000,
+      estimatedWeight: 13750,
+    };
+
+    render(<EditBillableWeight {...defaultProps} />);
+
+    fireEvent.click(screen.getByRole('button', { name: 'Edit' }));
+    expect(screen.queryByText('Edit')).toBeNull();
+    // weights
+    expect(screen.getByText(formatWeight(defaultProps.weightAllowance))).toBeInTheDocument();
+    expect(screen.getByText(formatWeight(defaultProps.estimatedWeight))).toBeInTheDocument();
+    // buttons
+    expect(screen.getByRole('button', { name: 'Save changes' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+  });
+
+  it('should be able to toggle between views', () => {
+    const defaultProps = {
+      weightAllowance: 8000,
+      estimatedWeight: 13750,
+    };
+
+    render(<EditBillableWeight {...defaultProps} />);
+    fireEvent.click(screen.getByRole('button', { name: 'Edit' }));
+    expect(screen.queryByText('Edit')).toBeNull();
+    expect(screen.getByRole('button', { name: 'Save changes' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+
+    fireEvent.click(screen.getByRole('button', { name: 'Cancel' }));
+    expect(screen.queryByText('Edit')).toBeInTheDocument();
+    expect(screen.queryByText('Save changes')).toBeNull();
+    expect(screen.queryByText('Cancel')).toBeNull();
+  });
+});
