3a2b87dd774a3fd0c62cd453ef135dfbdfdf5c73
pearl-truss <pearl@truss.works>
2021-08-30 14:01:26 +0000

    fix typo

--- migrations/app/schema/20210826153944_add_tio_remarks_to_moves_table.up.sql
+++ migrations/app/schema/20210826153944_add_tio_remarks_to_moves_table.up.sql
@@ -3,4 +3,4 @@ ALTER TABLE moves
     ADD COLUMN tio_remarks text;
 
 -- Column comments
-COMMENT ON COLUMN moves.tio_remarks IS 'Remarks at TIO has on a move';
+COMMENT ON COLUMN moves.tio_remarks IS 'Remarks a TIO has on a move';
