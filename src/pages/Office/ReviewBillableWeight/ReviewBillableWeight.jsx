7fd1c5cd62529780057e5dbdeb10f78aed7766f5
Yan Zhao <yan.zhao@truss.works>
2021-08-31 18:43:45 +0000

    Start Filling out the Review Billable Weight Page
    - add a document viewer
    - retrieve orders just as a starting point
    - add the document sidebar

--- src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
+++ src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { Button } from '@trussworks/react-uswds';
+import { useParams } from 'react-router-dom';
+
+import DocumentViewer from 'components/DocumentViewer/DocumentViewer';
+import DocumentViewerSidebar from 'pages/Office/DocumentViewerSidebar/DocumentViewerSidebar';
+import { useOrdersDocumentQueries } from 'hooks/queries';
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
+import SomethingWentWrong from 'shared/SomethingWentWrong';
 
 export default function ReviewBillableWeight() {
-  return <div>Review Billable Weight page</div>;
+  const { moveCode } = useParams();
+
+  const { upload, isLoading, isError } = useOrdersDocumentQueries(moveCode);
+
+  if (isLoading) return <LoadingPlaceholder />;
+  if (isError) return <SomethingWentWrong />;
+
+  const documentsForViewer = Object.values(upload);
+
+  return (
+    <>
+      <DocumentViewer files={documentsForViewer} />
+      <DocumentViewerSidebar title="Review weights" subtitle="Edit max billableweight" onClose={() => {}}>
+        <DocumentViewerSidebar.Content>Hello</DocumentViewerSidebar.Content>
+        <DocumentViewerSidebar.Footer>
+          <Button>Button</Button>
+        </DocumentViewerSidebar.Footer>
+      </DocumentViewerSidebar>
+    </>
+  );
 }
