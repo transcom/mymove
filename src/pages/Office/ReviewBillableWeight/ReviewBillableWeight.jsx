771ab23f3b00a0053a87bfd7bf2f8b0122f19252
Yan Zhao <yan.zhao@truss.works>
2021-08-31 23:55:20 +0000

    Wire Up the X Button on the Review Billable Weight Page
    - make the x button go to the payment requests page
    - create constant to use for the payment requests page

--- src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
+++ src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button } from '@trussworks/react-uswds';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import { generatePath } from 'react-router';
 
+import { tioRoutes } from 'constants/routes';
 import styles from 'pages/Office/ServicesCounselingMoveDocumentWrapper/ServicesCounselingMoveDocumentWrapper.module.scss';
 import DocumentViewer from 'components/DocumentViewer/DocumentViewer';
 import DocumentViewerSidebar from 'pages/Office/DocumentViewerSidebar/DocumentViewerSidebar';
@@ -11,6 +13,7 @@ import SomethingWentWrong from 'shared/SomethingWentWrong';
 
 export default function ReviewBillableWeight() {
   const { moveCode } = useParams();
+  const history = useHistory();
 
   const { upload, isLoading, isError } = useOrdersDocumentQueries(moveCode);
 
@@ -19,13 +22,17 @@ export default function ReviewBillableWeight() {
 
   const documentsForViewer = Object.values(upload);
 
+  const handleClose = () => {
+    history.push(generatePath(tioRoutes.PAYMENT_REQUESTS_PATH, { moveCode }));
+  };
+
   return (
     <div className={styles.DocumentWrapper}>
       <div className={styles.embed}>
         <DocumentViewer files={documentsForViewer} />
       </div>
       <div className={styles.sidebar}>
-        <DocumentViewerSidebar title="Review weights" subtitle="Edit max billableweight" onClose={() => {}}>
+        <DocumentViewerSidebar title="Review weights" subtitle="Edit max billableweight" onClose={handleClose}>
           <DocumentViewerSidebar.Content>Hello</DocumentViewerSidebar.Content>
           <DocumentViewerSidebar.Footer>
             <Button>Button</Button>
