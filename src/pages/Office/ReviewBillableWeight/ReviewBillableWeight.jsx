37d8eb97d5da878ea9c5c903c59fe949c357c106
Yan Zhao <yan.zhao@truss.works>
2021-08-31 19:23:51 +0000

    Apply Some Styling to ReviewBillableWeight Page
    - apply a little bit of styling to the billable weight page to make it look a little nicer
    - fix a typo

--- src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
+++ src/pages/Office/ReviewBillableWeight/ReviewBillableWeight.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from '@trussworks/react-uswds';
 import { useParams } from 'react-router-dom';
 
+import styles from 'pages/Office/ServicesCounselingMoveDocumentWrapper/ServicesCounselingMoveDocumentWrapper.module.scss';
 import DocumentViewer from 'components/DocumentViewer/DocumentViewer';
 import DocumentViewerSidebar from 'pages/Office/DocumentViewerSidebar/DocumentViewerSidebar';
 import { useOrdersDocumentQueries } from 'hooks/queries';
@@ -19,14 +20,18 @@ export default function ReviewBillableWeight() {
   const documentsForViewer = Object.values(upload);
 
   return (
-    <>
-      <DocumentViewer files={documentsForViewer} />
-      <DocumentViewerSidebar title="Review weights" subtitle="Edit max billableweight" onClose={() => {}}>
-        <DocumentViewerSidebar.Content>Hello</DocumentViewerSidebar.Content>
-        <DocumentViewerSidebar.Footer>
-          <Button>Button</Button>
-        </DocumentViewerSidebar.Footer>
-      </DocumentViewerSidebar>
-    </>
+    <div className={styles.DocumentWrapper}>
+      <div className={styles.embed}>
+        <DocumentViewer files={documentsForViewer} />
+      </div>
+      <div className={styles.sidebar}>
+        <DocumentViewerSidebar title="Review weights" subtitle="Edit max billableweight" onClose={() => {}}>
+          <DocumentViewerSidebar.Content>Hello</DocumentViewerSidebar.Content>
+          <DocumentViewerSidebar.Footer>
+            <Button>Button</Button>
+          </DocumentViewerSidebar.Footer>
+        </DocumentViewerSidebar>
+      </div>
+    </div>
   );
 }
