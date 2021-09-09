4503afbc610e22241631289d9219af28bb5bc27c
Namibia Torres <namibia@truss.works>
2021-09-09 19:12:51 +0000

    add service suite and blank creator

---
+++ pkg/services/sit_extension/sit_service_test.go
@@ -0,0 +1,36 @@
+package sitextension
+
+import (
+	"testing"
+
+	"github.com/transcom/mymove/pkg/appcontext"
+
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
+
+	"github.com/transcom/mymove/pkg/testingsuite"
+)
+
+type SitExtensionServiceSuite struct {
+	testingsuite.PopTestSuite
+	logger *zap.Logger
+}
+
+// TestAppContext returns the AppContext for the test suite
+func (suite *SitExtensionServiceSuite) TestAppContext() appcontext.AppContext {
+	return appcontext.NewAppContext(suite.DB(), suite.logger)
+}
+
+func (suite *SitExtensionServiceSuite) SetupTest() {
+	err := suite.TruncateAll()
+	suite.FatalNoError(err)
+}
+
+func TestSitExtensionServiceSuite(t *testing.T) {
+	testService := &SitExtensionServiceSuite{
+		PopTestSuite: testingsuite.NewPopTestSuite(testingsuite.CurrentPackage()),
+		logger:       zap.NewNop(), // no-op logger used during testing
+	}
+	suite.Run(t, testService)
+	testService.PopTestSuite.TearDown()
+}
