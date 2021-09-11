717e9ae9357dd4c589051ba9a5d34d6cf6e8d2d0
Yan Zhao <yan.zhao@truss.works>
2021-09-11 00:26:09 +0000

    Add Tests for the Custom Hooks
    - add tests that test the custom hooks for calculating the total billable weight and the weight requested

--- src/hooks/custom.test.js
+++ src/hooks/custom.test.js
@@ -1,4 +1,10 @@
-import { includedStatusesForCalculatingWeights } from 'hooks/custom';
+import { renderHook } from '@testing-library/react-hooks';
+
+import {
+  includedStatusesForCalculatingWeights,
+  useCalculatedTotalBillableWeight,
+  useCalculatedWeightRequested,
+} from 'hooks/custom';
 import { shipmentStatuses } from 'constants/shipments';
 
 describe('includedStatusesForCalculatingWeights returns true for approved, diversion requested, or cancellation requested', () => {
@@ -13,5 +19,85 @@ describe('includedStatusesForCalculatingWeights returns true for approved, diver
     ['FAKE_STATUS', false],
   ])('checks if a shipment with status %s should be included: %b', (status, isIncluded) => {
     expect(includedStatusesForCalculatingWeights(status)).toBe(isIncluded);
+  });
+});
+
+describe('for all shipments that are approved, have a cancellation requested, or have a diversion requested', () => {
+  it('useCalculatedTotalBillableWeight returns the calculated billable weight', () => {
+    let mtoShipments = [
+      {
+        calculatedBillableWeight: 10,
+        status: shipmentStatuses.DRAFT,
+      },
+      {
+        calculatedBillableWeight: 500,
+        status: shipmentStatuses.APPROVED,
+      },
+      {
+        calculatedBillableWeight: 200,
+        status: shipmentStatuses.CANCELLATION_REQUESTED,
+      },
+      {
+        calculatedBillableWeight: 300,
+        status: shipmentStatuses.DIVERSION_REQUESTED,
+      },
+    ];
+
+    const { result, rerender } = renderHook(() => useCalculatedTotalBillableWeight(mtoShipments));
+
+    expect(result.current).toBe(1000);
+
+    mtoShipments = mtoShipments.concat([{ calculatedBillableWeight: 100, status: shipmentStatuses.APPROVED }]);
+    rerender();
+
+    expect(result.current).toBe(1100);
+  });
+
+  it('useCalculatedWeightRequested returns the calculated billable weight using the lower value between the prime actual weight and the reweigh weight', () => {
+    let mtoShipments = [
+      {
+        primeActualWeight: 10,
+        status: shipmentStatuses.DRAFT,
+        reweigh: {
+          weight: 5,
+        },
+      },
+      {
+        primeActualWeight: 2000,
+        status: shipmentStatuses.APPROVED,
+        reweigh: {
+          weight: 300,
+        },
+      },
+      {
+        primeActualWeight: 100,
+        status: shipmentStatuses.APPROVED,
+      },
+      {
+        primeActualWeight: 1000,
+        status: shipmentStatuses.CANCELLATION_REQUESTED,
+        reweigh: {
+          weight: 200,
+        },
+      },
+      {
+        primeActualWeight: 400,
+        status: shipmentStatuses.DIVERSION_REQUESTED,
+        reweigh: {
+          weight: 3000,
+        },
+      },
+    ];
+
+    const { result, rerender } = renderHook(() => useCalculatedWeightRequested(mtoShipments));
+
+    expect(result.current).toBe(1000);
+
+    mtoShipments = mtoShipments.concat([
+      { primeActualWeight: 100, status: shipmentStatuses.APPROVED, reweigh: { weight: 3000 } },
+    ]);
+    rerender();
+
+    expect(result.current).toBe(1100);
   });
 });
