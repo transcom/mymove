ae6a20aa5158e54cc509d64b1ade21d280456325
Jacqueline McK <1359520+jacquelineIO@users.noreply.github.com>
2021-09-15 10:29:32 -0500

    [MB-9309] reweigh lookup implemented for pricing (#7344)
    * reweigh lookup implemented
    * Update pkg/testdatagen/make_service_param.go
    
    Co-authored-by: Reggie Riser <4960757+reggieriser@users.noreply.github.com>
    
    * update to case statement

---
+++ pkg/payment_request/service_param_value_lookups/weight_reweigh_lookup_test.go
@@ -0,0 +1,29 @@
+package serviceparamvaluelookups
+
+import (
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/unit"
+)
+
+func (suite *ServiceParamValueLookupsSuite) TestWeightReweighLookup() {
+	key := models.ServiceItemParamNameWeightReweigh
+
+	suite.Run("reweigh weight is present on MTO Shipment", func() {
+		_, _, paramLookup := suite.setupTestMTOServiceItemWithReweigh(unit.Pound(1234), unit.Pound(1234), models.ReServiceCodeDLH, models.MTOShipmentTypeHHG)
+		valueStr, err := paramLookup.ServiceParamValue(suite.TestAppContext(), key)
+		suite.FatalNoError(err)
+		suite.Equal("1234", valueStr)
+	})
+
+	suite.Run("nil Shipment Reweigh", func() {
+		// Set the reweigh weight to nil
+		mtoServiceItem, paymentRequest, _ := suite.setupTestMTOServiceItemWithWeight(unit.Pound(1234), unit.Pound(450), models.ReServiceCodeDLH, models.MTOShipmentTypeHHG)
+
+		paramLookup, err := ServiceParamLookupInitialize(suite.TestAppContext(), suite.planner, mtoServiceItem.ID, paymentRequest.ID, paymentRequest.MoveTaskOrderID, nil)
+		suite.FatalNoError(err)
+
+		valueStr, err := paramLookup.ServiceParamValue(suite.TestAppContext(), key)
+		suite.NoError(err)
+		suite.Equal("", valueStr)
+	})
+}
