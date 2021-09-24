d5ad96f9a883ac3efd9428919815568422e9bdb9
YanZ777 <6477059+YanZ777@users.noreply.github.com>
2021-09-23 18:00:56 -0700

    MB-9265 add yellow alerts for missing weights (#7428)
    * Add alerts for missing shipment information
    
    - fix console warning about missing information
    - add icon for missing weight on the payment requests page
    - add alert on the edit max billable weight sidebar for missing shipment weight
    
    * Add tests to check for missing weight on the shipment list
    
    - refactor out the tests for the over weight and missing weight so that they are better isolated
    
    * Add tests for the billable weight card
    
    - refactor tests so that testing each of the different flags for missing weight and over weight is isolated
    
    * Refactor tests for better organization
    
    - wrap related tests in a describe so that it's easier to read in the test summary output
    
    * Add tests for missing shipment weight on the review billable weight page
    
    - add tests to check for the alert
    - fix logic with when the alert should show
    
    * Fix console error in tests
    
    * Add msising period to the alert message
    
    * Remove white background color from container
    
    - fix the white space that was showing up between alerts on the review billable weight page between alerts
    - auto format the styling
    
    * Fix the storybook stories so that it shows a better variety
    
    - add some props so that there is a shipment that does not show a missing weight or an over weight flag
    
    * Adjust storybook story so that the single shipment display doesn't have any flags

--- src/components/Office/BillableWeight/BillableWeightCard/BillableWeightCard.test.jsx
+++ src/components/Office/BillableWeight/BillableWeightCard/BillableWeightCard.test.jsx
@@ -7,29 +7,43 @@ import BillableWeightCard from './BillableWeightCard';
 import { formatWeight } from 'shared/formatters';
 
 describe('BillableWeightCard', () => {
-  const shipments = [
-    { id: '0001', shipmentType: 'HHG', calculatedBillableWeight: 6161, estimatedWeight: 5600 },
-    {
-      id: '0002',
-      shipmentType: 'HHG',
-      calculatedBillableWeight: 3200,
-      estimatedWeight: 5000,
-      reweigh: { id: '1234' },
-    },
-    { id: '0003', shipmentType: 'HHG', calculatedBillableWeight: 3400, estimatedWeight: 5000 },
-  ];
-
   const defaultProps = {
     maxBillableWeight: 13750,
     totalBillableWeight: 12460,
     weightRequested: 12260,
     weightAllowance: 8000,
-    shipments,
     onReviewWeights: jest.fn(),
   };
 
   it('renders maximum billable weight, total billable weight, weight requested and weight allowance', () => {
-    render(<BillableWeightCard {...defaultProps} />);
+    const shipments = [
+      {
+        id: '0001',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 2161,
+        estimatedWeight: 5600,
+        primeEstimatedWeight: 100,
+        reweigh: { id: '1234', weight: 40 },
+      },
+      {
+        id: '0002',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 3200,
+        estimatedWeight: 5000,
+        primeEstimatedWeight: 1000,
+        reweigh: { id: '1234', weight: 300 },
+      },
+      {
+        id: '0003',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 3400,
+        estimatedWeight: 5000,
+        primeEstimatedWeight: 200,
+        reweigh: { id: '1234', weight: 500 },
+      },
+    ];
+
+    render(<BillableWeightCard {...defaultProps} shipments={shipments} />);
 
     // labels
     expect(screen.getByText('Maximum billable weight')).toBeInTheDocument();
@@ -44,8 +58,8 @@ describe('BillableWeightCard', () => {
     expect(screen.getByText(formatWeight(defaultProps.weightAllowance))).toBeInTheDocument();
 
     // flags
-    expect(screen.getByText('Over weight')).toBeInTheDocument();
-    expect(screen.getByText('Missing weight')).toBeInTheDocument();
+    expect(screen.queryByText('Over weight')).not.toBeInTheDocument();
+    expect(screen.queryByText('Missing weight')).not.toBeInTheDocument();
 
     // shipment weights
     expect(screen.getByText(formatWeight(shipments[0].calculatedBillableWeight))).toBeInTheDocument();
@@ -53,8 +67,71 @@ describe('BillableWeightCard', () => {
     expect(screen.getByText(formatWeight(shipments[2].calculatedBillableWeight))).toBeInTheDocument();
   });
 
+  it('shows an over weight flag if a shipment is over weight', () => {
+    const shipments = [
+      {
+        id: '0001',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 6161,
+        estimatedWeight: 5600,
+        primeEstimatedWeight: 100,
+        reweigh: { id: '1234', weight: 40 },
+      },
+    ];
+
+    render(<BillableWeightCard {...defaultProps} shipments={shipments} />);
+
+    expect(screen.queryByText('Missing weight')).not.toBeInTheDocument();
+    expect(screen.getByText('Over weight')).toBeInTheDocument();
+  });
+
+  it('shows a missing weight flag if a shipment is missing the reweigh weight', () => {
+    const shipments = [
+      {
+        id: '0001',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 1161,
+        estimatedWeight: 5600,
+        primeEstimatedWeight: 100,
+        reweigh: { id: '1234' },
+      },
+    ];
+
+    render(<BillableWeightCard {...defaultProps} shipments={shipments} />);
+
+    expect(screen.queryByText('Over weight')).not.toBeInTheDocument();
+    expect(screen.getByText('Missing weight')).toBeInTheDocument();
+  });
+
+  it('shows a missing weight flag if a shipment is missing a prime estimated weight', () => {
+    const shipments = [
+      {
+        id: '0001',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 4161,
+        estimatedWeight: 5600,
+        reweigh: { id: '1234', weight: 40 },
+      },
+    ];
+
+    render(<BillableWeightCard {...defaultProps} shipments={shipments} />);
+
+    expect(screen.queryByText('Over weight')).not.toBeInTheDocument();
+    expect(screen.getByText('Missing weight')).toBeInTheDocument();
+  });
+
   it('implements the review weights handler when the review weights button is clicked', async () => {
-    render(<BillableWeightCard {...defaultProps} />);
+    const shipments = [
+      {
+        id: '0001',
+        shipmentType: 'HHG',
+        calculatedBillableWeight: 6161,
+        estimatedWeight: 5600,
+        primeEstimatedWeight: 100,
+        reweigh: { id: '1234', weight: 40 },
+      },
+    ];
+    render(<BillableWeightCard {...defaultProps} shipments={shipments} />);
 
     const reviewWeights = screen.getByRole('button', { name: 'Review weights' });
 
