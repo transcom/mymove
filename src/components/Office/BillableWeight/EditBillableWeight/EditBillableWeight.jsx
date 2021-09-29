1d2b2e52bb331a644d76401fdab7a82b33b784be
Yan Zhao <yan.zhao@truss.works>
2021-09-29 21:55:07 +0000

    Hide part of the hint text for the max billable weight if there is no estimated weight

--- src/components/Office/BillableWeight/EditBillableWeight/EditBillableWeight.jsx
+++ src/components/Office/BillableWeight/EditBillableWeight/EditBillableWeight.jsx
@@ -59,10 +59,12 @@ function MaxBillableWeightHintText({ weightAllowance, estimatedWeight }) {
         <strong data-testid="maxWeight-weightAllowance">{formatWeight(weightAllowance)}</strong>{' '}
         <span>| weight allowance</span>
       </div>
-      <div className={styles.hintText}>
-        <strong data-testid="maxWeight-estimatedWeight">{formatWeight(estimatedWeight * 1.1)}</strong>{' '}
-        <span>| 110% of total estimated weight</span>
-      </div>
+      {!Number.isNaN(estimatedWeight) && (
+        <div className={styles.hintText}>
+          <strong data-testid="maxWeight-estimatedWeight">{formatWeight(estimatedWeight * 1.1)}</strong>{' '}
+          <span>| 110% of total estimated weight</span>
+        </div>
+      )}
     </>
   );
 }
