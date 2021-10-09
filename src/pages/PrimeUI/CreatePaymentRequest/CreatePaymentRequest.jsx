25dc493af497c3df1af9eb5daa8e110902783304
Yan Zhao <yan.zhao@truss.works>
2021-10-09 01:00:46 +0000

    Reactify the payload
    - chnage from a relative import to an absolute import

--- src/pages/PrimeUI/CreatePaymentRequest/CreatePaymentRequest.jsx
+++ src/pages/PrimeUI/CreatePaymentRequest/CreatePaymentRequest.jsx
@@ -1,127 +1,123 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import LoadingPlaceholder from '../../../shared/LoadingPlaceholder';
-import SomethingWentWrong from '../../../shared/SomethingWentWrong';
-
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
+import SomethingWentWrong from 'shared/SomethingWentWrong';
+import SectionWrapper from 'components/Customer/SectionWrapper';
+import formStyles from 'styles/form.module.scss';
+import { ShipmentOptionsOneOf } from 'types/shipment';
+import { AgentShape } from 'types/agent';
+import { AddressShape } from 'types/address';
+import descriptionListStyles from 'styles/descriptionList.module.scss';
 import { usePrimeSimulatorGetMove } from 'hooks/queries';
 
-function parseServiceItem(serviceItem) {
-  let str = '';
+const ServiceItem = ({ serviceItem, shipmentServiceItemNumber }) => {
+  return (
+    <dl className={descriptionListStyles.descriptionList}>
+      <h3>Service Item {shipmentServiceItemNumber}</h3>
+      <div className={descriptionListStyles.row}>
+        <dt>ID:</dt>
+        <dd>{serviceItem.id}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Service Code:</dt>
+        <dd>{serviceItem.reServiceCode}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Service Name:</dt>
+        <dd>{serviceItem.reServiceName}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>eTag:</dt>
+        <dd>{serviceItem.eTag}</dd>
+      </div>
+    </dl>
+  );
+};
 
-  str = `ID: ${serviceItem.id}\n`;
-  str += `Service Code: ${serviceItem.reServiceCode}\n`;
-  str += `Service Name: ${serviceItem.reServiceName}\n`;
-  str += `eTag: ${serviceItem.eTag}\n`;
+ServiceItem.propTypes = {
+  serviceItem: PropTypes.shape({
+    id: PropTypes.string,
+    reServiceCode: PropTypes.string,
+    reServiceName: PropTypes.string,
+    eTag: PropTypes.string,
+  }).isRequired,
+  shipmentServiceItemNumber: PropTypes.number.isRequired,
+};
 
-  return str;
-}
+const Shipment = ({ shipment, shipmentNumber }) => {
+  return (
+    <dl className={descriptionListStyles.descriptionList}>
+      <h3>Shipment {shipmentNumber}</h3>
+      <div className={descriptionListStyles.row}>
+        <dt>Shipment ID:</dt>
+        <dd>{shipment.id}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Shipment eTag:</dt>
+        <dd>{shipment.eTag}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Requested Pickup Date:</dt>
+        <dd>{shipment.requestedPickupDate}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Actual Pickup Date:</dt>
+        <dd>{shipment.actualPickupDate}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Estimated Weight:</dt>
+        <dd>{shipment.primeEstimatedWeight}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Actual Weight:</dt>
+        <dd>{shipment.primeActualWeight}</dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Pickup Address:</dt>
+        <dd>
+          {shipment.pickupAddress.streetAddress1} {shipment.pickupAddress.streetAddress2} {shipment.pickupAddress.city}{' '}
+          {shipment.pickupAddress.state} {shipment.pickupAddress.postalCode}
+        </dd>
+      </div>
+      <div className={descriptionListStyles.row}>
+        <dt>Destination Address:</dt>
+        <dd>
+          {shipment.destinationAddress.streetAddress1} {shipment.destinationAddress.streetAddress2}{' '}
+          {shipment.destinationAddress.city} {shipment.destinationAddress.state}{' '}
+          {shipment.destinationAddress.postalCode}
+        </dd>
+      </div>
+    </dl>
+  );
+};
 
-function parseAddress(address) {
-  let str = '';
-
-  str += `\n\t${address.streetAddress1}\n\t${address.streetAddress2}\n\t${address.streetAddress2}\n`;
-  str += `\t${address.city}, ${address.state}, ${address.postalCode}\n`;
-
-  return str;
-}
-
-function parseMoveForPaymentRequest(moveTaskOrders) {
-  /*
-  ================================================
-  Move Code:
-  Move ID:
-  ------------------------------------------------
-  Move Service Items:
-  ------------------------------------------------
-  Shipments (#)
-  ************************************************
-  ID:
-  Shipment ID:
-  Shipment Type:
-  Shipment ETag:
-  Requested Pickup Date:
-  Actual Pickup Date:
-  Estimated Weight:
-  Actual Weight:
-  Reweigh Weight:
-  Pickup Address:
-  Destination Address:
-  ------------------------------------------------
-  Shipment Service Items:
-  Service Item ID:
-  Service Item Code:
-  Service Item Description:
-  ************************************************
-  */
-
-  let moveDetails = '';
-  let move = moveTaskOrders.moveTaskOrders;
-  const firstkey = Object.keys(move)[0];
-  move = move[firstkey];
-
-  const equalDivider = '=';
-  moveDetails += equalDivider.repeat(50);
-  moveDetails += '\n';
-  moveDetails += `Move Code: ${move.moveCode}\n`;
-  moveDetails += `Move ID: ${move.id}\n`;
-
-  const lineDivider = '-';
-  moveDetails += lineDivider.repeat(50);
-  moveDetails += '\n';
-
-  moveDetails += 'Move Service Items:\n';
-  moveDetails += lineDivider.repeat(50);
-  moveDetails += '\n';
-
-  const MoveServiceCodes = ['MS', 'CS'];
-  // Get MS and CS service items
-  for (let i = 0; i < move.mtoServiceItems.length; i += 1) {
-    if (MoveServiceCodes.includes(move.mtoServiceItems[i].reServiceCode)) {
-      moveDetails += parseServiceItem(move.mtoServiceItems[i]);
-      moveDetails += lineDivider.repeat(50);
-      moveDetails += '\n';
-    }
-  }
-
-  moveDetails += lineDivider.repeat(50);
-  moveDetails += '\n';
-
-  moveDetails += `Shipments (${move.mtoShipments.length}):\n`;
-  const starDivider = '*';
-  moveDetails += starDivider.repeat(50);
-  moveDetails += '\n';
-
-  for (let i = 0; i < move.mtoShipments.length; i += 1) {
-    const shipment = move.mtoShipments[i];
-    moveDetails += `Shipment ID: ${shipment.id}\n`;
-    moveDetails += `Shipment Type: ${shipment.shipmentType}\n`;
-    moveDetails += `Shipment eTag: ' ${shipment.eTag}\n`;
-    moveDetails += `Requested Pickup Date: ${shipment.requestedPickupDate}\n`;
-    moveDetails += `Actual Pickup Date: ${shipment.actualPickupDate}\n`;
-    moveDetails += `Estimated Weight: ${shipment.primeEstimatedWeight}\n`;
-    moveDetails += `Actual Weight: ${shipment.primeActualWeight}\n`;
-    moveDetails += `Pickup Address: ${parseAddress(shipment.pickupAddress)}\n`;
-    moveDetails += `Destination Address: ${parseAddress(shipment.destinationAddress)}\n`;
-    moveDetails += lineDivider.repeat(50);
-    moveDetails += '\n';
-
-    moveDetails += 'Shipment Service Items:\n';
-    moveDetails += lineDivider.repeat(50);
-    moveDetails += '\n';
-    for (let j = 0; j < move.mtoServiceItems.length; j += 1) {
-      if (move.mtoServiceItems[j].mtoShipmentID === shipment.id) {
-        moveDetails += parseServiceItem(move.mtoServiceItems[j]);
-        moveDetails += lineDivider.repeat(50);
-        moveDetails += '\n';
-      }
-    }
-    moveDetails += starDivider.repeat(50);
-    moveDetails += '\n';
-  }
-
-  return moveDetails;
-}
+Shipment.propTypes = {
+  shipment: PropTypes.shape({
+    id: PropTypes.string,
+    eTag: PropTypes.string,
+    shipmentType: ShipmentOptionsOneOf,
+    requestedPickupDate: PropTypes.string,
+    scheduledPickupDate: PropTypes.string,
+    actualPickupDate: PropTypes.string,
+    pickupAddress: AddressShape,
+    secondaryPickupAddress: AddressShape,
+    destinationAddress: AddressShape,
+    secondaryDeliveryAddress: AddressShape,
+    agents: PropTypes.arrayOf(AgentShape),
+    primeEstimatedWeight: PropTypes.number,
+    primeActualWeight: PropTypes.number,
+    diversion: PropTypes.bool,
+    counselorRemarks: PropTypes.string,
+    customerRemarks: PropTypes.string,
+    status: PropTypes.string,
+    reweigh: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+  shipmentNumber: PropTypes.number.isRequired,
+};
 
 const CreatePaymentRequest = () => {
   const { moveCode } = useParams();
@@ -131,8 +127,69 @@ const CreatePaymentRequest = () => {
   if (isLoading) return <LoadingPlaceholder />;
   if (isError) return <SomethingWentWrong />;
 
-  return <div style={{ 'white-space': 'pre' }}>{parseMoveForPaymentRequest(data)}</div>;
-  // return <div style={{ 'white-space': 'pre' }}>{JSON.stringify(data, null, '\t')}</div>;
+  const { moveTaskOrders } = data;
+  const moveTaskOrderKeys = Object.keys(moveTaskOrders);
+  const moveTaskOrder = moveTaskOrders[moveTaskOrderKeys[0]];
+  const { mtoShipments, mtoServiceItems } = moveTaskOrder;
+  const MoveServiceCodes = ['MS', 'CS'];
+
+  return (
+    <div className="grid-container usa-prose">
+      <div className="grid-row">
+        <div className="grid-col-12">
+          <SectionWrapper className={formStyles.formSection}>
+            <dl className={descriptionListStyles.descriptionList}>
+              <h2>Move</h2>
+              <div className={descriptionListStyles.row}>
+                <dt>Move Code:</dt>
+                <dd>{moveCode}</dd>
+              </div>
+              <div className={descriptionListStyles.row}>
+                <dt>Move Id:</dt>
+                <dd>{moveTaskOrder.id}</dd>
+              </div>
+            </dl>
+            <dl className={descriptionListStyles.descriptionList}>
+              <h2>Move Service Items</h2>
+              {mtoServiceItems.map((mtoServiceItem, mtoServiceItemIndex) => {
+                return (
+                  MoveServiceCodes.includes(mtoServiceItem.reServiceCode) && (
+                    <ServiceItem
+                      key={mtoServiceItem.id}
+                      serviceItem={mtoServiceItem}
+                      shipmentServiceItemNumber={mtoServiceItemIndex}
+                    />
+                  )
+                );
+              })}
+            </dl>
+            <dl className={descriptionListStyles.descriptionList}>
+              <h2>Shipments</h2>
+              {mtoShipments.map((mtoShipment, index) => {
+                return (
+                  <>
+                    <Shipment key={mtoShipment.id} shipment={mtoShipment} shipmentNumber={index} />
+                    <h2>Shipment Service Items</h2>
+                    {mtoServiceItems.map((mtoServiceItem, mtoServiceItemIndex) => {
+                      return (
+                        mtoServiceItem.mtoShipmentID === mtoShipment.id && (
+                          <ServiceItem
+                            key={mtoServiceItem.id}
+                            serviceItem={mtoServiceItem}
+                            shipmentServiceItemNumber={mtoServiceItemIndex}
+                          />
+                        )
+                      );
+                    })}
+                  </>
+                );
+              })}
+            </dl>
+          </SectionWrapper>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePaymentRequest;
