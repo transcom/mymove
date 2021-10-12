c7af5c0bb8e36a472de24c810e5bfdf49b6d8958
Roger Steve Ruiz <rogeruiz@truss.works>
2021-10-12 14:05:46 +0000

    Match how date strings are used in the Office app;
    This work might be better suited for TRA work, but the way the
    MilMove application formats dates is spread across various files rather
    than being formatted in a single place in various formats. Taking
    inspiration from the ServicesCounselingQueue, the AvailableMovesQueue
    adds the Time to the date string format that Office users expect to see
    in the application.

--- src/pages/PrimeUI/PrimeSimulatorAvailableMoves/PrimeSimulatorAvailableMoves.jsx
+++ src/pages/PrimeUI/PrimeSimulatorAvailableMoves/PrimeSimulatorAvailableMoves.jsx
@@ -6,19 +6,44 @@ import styles from './PrimeSimulatorAvailableMoves.module.scss';
 import { createHeader } from 'components/Table/utils';
 import TableQueue from 'components/Table/TableQueue';
 import { usePrimeSimulatorAvailableMovesQueries } from 'hooks/queries';
+// TODO: This is very clunky. There are shared/formatters and util/formatters
+// that determine dates. This way is a way to do it now, but this should be
+// refactored as part of TRA work to be done differently across the app.
+// For now though, I'm going to be using the `formatDateFromIso` function and
+// then leverage a constant for how the date should be formatted.
+import { formatDateFromIso } from 'shared/formatters';
+import { DATE_TIME_FORMAT_STRING } from 'shared/constants';
 import LoadingPlaceholder from 'shared/LoadingPlaceholder';
 import SomethingWentWrong from 'shared/SomethingWentWrong';
 
 const columnHeaders = () => [
   createHeader('ID', 'id'),
   createHeader('Move code', 'moveCode'),
-  createHeader('Created at', 'createdAt'),
-  createHeader('Updated at', 'updatedAt'),
+  createHeader(
+    'Created at',
+    (row) => {
+      return formatDateFromIso(row.createdAt, DATE_TIME_FORMAT_STRING);
+    },
+    { id: 'createdAt' },
+  ),
+  createHeader(
+    'Updated at',
+    (row) => {
+      return formatDateFromIso(row.updatedAt, DATE_TIME_FORMAT_STRING);
+    },
+    { id: 'updatedAt' },
+  ),
   createHeader('e-Tag', 'eTag'),
   createHeader('Order ID', 'orderID'),
   createHeader('Type', 'ppmType'),
   createHeader('Reference ID', 'referenceId'),
-  createHeader('Available to Prime at', 'availableToPrimeAt'),
+  createHeader(
+    'Available to Prime at',
+    (row) => {
+      return formatDateFromIso(row.availableToPrimeAt, DATE_TIME_FORMAT_STRING);
+    },
+    { id: 'availableToPrimeAt' },
+  ),
 ];
 
 const PrimeSimulatorAvailableMoves = () => {
