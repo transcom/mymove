2f278df29e43dd958e93e5ebf1f0d6a5588de34c
Yan Zhao <yan.zhao@truss.works>
2021-10-14 23:46:35 +0000

    Add in the upload table
    - put in the upload table into the upload payment request documents page
    - put in functions that don't do much for now

--- src/pages/PrimeUI/UploadPaymentRequestDocuments/UploadPaymentRequestDocuments.jsx
+++ src/pages/PrimeUI/UploadPaymentRequestDocuments/UploadPaymentRequestDocuments.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
-// import { useParams } from 'react-router-dom';
+import { React, createRef, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import { Grid, Alert } from '@trussworks/react-uswds';
+
+import SectionWrapper from 'components/Customer/SectionWrapper';
+import UploadsTable from 'components/UploadsTable/UploadsTable';
+import FileUpload from 'components/FileUpload/FileUpload';
+import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
 
 const UploadPaymentRequest = () => {
-  return <div>Payment upload</div>;
+  const filePondEl = createRef();
+  // const history = useHistory();
+  // const [serverError, setServerError] = useState(null);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const handleDelete = (uploadId) => {
+    setUploadedFiles(uploadedFiles.filter((file) => file.id !== uploadId));
+  };
+
+  const handleUpload = (file) => {
+    // console.log(file);
+    setUploadedFiles([...uploadedFiles, { file, id: file.lastModified }]);
+    return Promise.resolve();
+  };
+
+  const onChange = () => {
+    filePondEl.current?.removeFiles();
+  };
+
+  const handleSave = () => {};
+
+  const handleCancel = () => {};
+
+  return (
+    <>
+      {/*
+      {serverError && (
+        <Grid row>
+          <Grid col desktop={{ col: 8, offset: 2 }}>
+            <Alert type="error" heading="An error occurred">
+              {serverError}
+            </Alert>
+          </Grid>
+        </Grid>
+      )}
+      */}
+
+      <SectionWrapper>
+        <UploadsTable uploads={uploadedFiles} onDelete={handleDelete} />
+        <div>
+          <h2>Upload Payment Request Documents</h2>
+          <FileUpload
+            ref={filePondEl}
+            createUpload={handleUpload}
+            onChange={onChange}
+            labelIdle={
+              'Drag & drop or <span class="filepond--label-action">click to upload payment request documents</span>'
+            }
+          />
+        </div>
+        <WizardNavigation editMode disableNext={false} onNextClick={handleSave} onCancelClick={handleCancel} />
+      </SectionWrapper>
+    </>
+  );
 };
 
 export default UploadPaymentRequest;
