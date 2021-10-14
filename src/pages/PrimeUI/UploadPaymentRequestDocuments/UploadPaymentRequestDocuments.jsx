28b88f689df264cd28f80d2e64eff49e87f29668
Yan Zhao <yan.zhao@truss.works>
2021-10-14 01:34:44 +0000

    Add upload payment request documents page
    - create a new route constant
    - add route to the office index.js
    - create new component to represent the page

---
+++ src/pages/PrimeUI/UploadPaymentRequestDocuments/UploadPaymentRequestDocuments.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { useParams } from 'react-router-dom';
+
+const UploadPaymentRequest = () => {
+  return <div>Payment upload</div>;
+};
+
+export default UploadPaymentRequest;
