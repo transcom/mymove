154e4c1e922aac33f4f7624bc11ed6db8b229047
Marty Boren <marty@truss.works>
2021-10-18 19:02:36 +0000

    Whitespace changes

--- migrations/app/schema/20211015201518_financial_review_flag.up.sql
+++ migrations/app/schema/20211015201518_financial_review_flag.up.sql
@@ -6,9 +6,9 @@ ALTER TABLE moves
 ALTER TABLE moves
     ALTER COLUMN financial_review_requested SET DEFAULT FALSE;
 UPDATE moves set financial_review_requested = FALSE;
+
 ALTER TABLE moves
 	ALTER COLUMN financial_review_requested SET NOT NULL;
-
 
 COMMENT ON COLUMN moves.financial_review_requested IS 'This flag is set by office users when they believe a move may incur excess costs to the customer and should have Finance Office review. The government will query this field from the data warehouse, so changes to it may require coordination.';
 COMMENT ON COLUMN moves.financial_review_remarks IS 'Reason provided by an office user for requesting financial review. The government will query this field from the data warehouse, so changes to it may require coordination.';
