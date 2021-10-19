9c5fc8270f9076b64605a7ea5262cfe4f0e6be1c
Marty Boren <marty@truss.works>
2021-10-19 22:46:59 +0000

    Change timestamp col to include time zone

--- migrations/app/schema/20211015201518_financial_review_flag.up.sql
+++ migrations/app/schema/20211015201518_financial_review_flag.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE moves
 	ADD COLUMN financial_review_flag boolean NOT NULL DEFAULT FALSE,
 	ADD COLUMN financial_review_remarks text,
-	ADD COLUMN financial_review_flag_set_at timestamp;
+	ADD COLUMN financial_review_flag_set_at timestamp with time zone;
 
 COMMENT ON COLUMN moves.financial_review_flag IS 'This flag is set by office users when they believe a move may incur excess costs to the customer and should have Finance Office review. The government will query this field from the data warehouse, so changes to it may require coordination.';
 COMMENT ON COLUMN moves.financial_review_remarks IS 'Reason provided by an office user for requesting financial review. The government will query this field from the data warehouse, so changes to it may require coordination.';
