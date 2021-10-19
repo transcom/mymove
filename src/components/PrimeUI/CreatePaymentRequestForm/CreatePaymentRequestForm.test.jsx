f4020028702c21365ee6501a41b0bf92d3597d75
Duncan <52669884+duncan-truss@users.noreply.github.com>
2021-10-19 17:29:25 -0400

    [MB-9752] Prime simulator create payment request form integration (#7584)
    * add formik service item checkbox form with validations and errors
    
    * add useMutation to make create payment request api call with payload
    
    * navigate back to move details after successful payment request and display list of payment requests
    
    * replaced checkbox with react-uswds version without formik wrapper
    
    * override some label form styles
    
    * create approve move with shipments missing required payment request fields
    
    * add generic api error response alert when create payment request fails
    
    * add select all and deselect all service items functionality
    
    * break out create payment request form into it's own component
    
    * split out styles now that there are multiple components
    
    * initial form client tests
    
    * add test for deselection
    
    * add page display tests for move, shipment, and service items
    
    * create payment request error and succeess submission tests
    
    * use generatePath for route, set react-query query cache

---
+++ src/components/PrimeUI/CreatePaymentRequestForm/CreatePaymentRequestForm.test.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import * as Yup from 'yup';
+import userEvent from '@testing-library/user-event';
+
+import CreatePaymentRequestForm from './CreatePaymentRequestForm';
+
+describe('CreatePaymentRequestForm', () => {
+  // No need to test any other data setting here because all checkboxes are unset to start
+  const initialValues = {
+    serviceItems: [],
+  };
+
+  const createPaymentRequestSchema = Yup.object().shape({
+    serviceItems: Yup.array().of(Yup.string()).min(1),
+  });
+
+  const twoShipments = [
+    {
+      id: '1',
+      pickupAddress: { streetAddress1: '500 Main Street', city: 'New York', state: 'NY', postal_code: '10001' },
+    },
+    {
+      id: '2',
+      pickupAddress: { streetAddress1: '33 Bleeker Street', city: 'New York', state: 'NY', postal_code: '10002' },
+    },
+  ];
+
+  const basicAndShipmentsServiceItems = {
+    basic: [{ id: '3', reServiceCode: 'MS' }],
+    1: [{ id: '4', reServiceCode: 'DLH' }],
+    2: [{ id: '5', reServiceCode: 'FSC' }],
+  };
+
+  it('renders the form', async () => {
+    render(
+      <CreatePaymentRequestForm
+        initialValues={initialValues}
+        createPaymentRequestSchema={createPaymentRequestSchema}
+        mtoShipments={twoShipments}
+        groupedServiceItems={basicAndShipmentsServiceItems}
+        onSubmit={jest.fn()}
+        handleSelectAll={jest.fn()}
+      />,
+    );
+
+    // 1 move service item and 1 on each shipment
+    expect(screen.getAllByRole('checkbox', { name: 'Add to payment request' }).length).toEqual(3);
+    // 1 select all checkbox for each shipment
+    expect(screen.getAllByLabelText('Add all service items').length).toEqual(2);
+    expect(screen.getByRole('button', { type: 'submit' })).toBeDisabled();
+  });
+
+  it('enables the submit button when at least one service item is checked', async () => {
+    render(
+      <CreatePaymentRequestForm
+        initialValues={initialValues}
+        createPaymentRequestSchema={createPaymentRequestSchema}
+        mtoShipments={twoShipments}
+        groupedServiceItems={basicAndShipmentsServiceItems}
+        onSubmit={jest.fn()}
+        handleSelectAll={jest.fn()}
+      />,
+    );
+
+    userEvent.click(screen.getAllByRole('checkbox', { name: 'Add to payment request' })[0]);
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { type: 'submit' })).toBeEnabled();
+    });
+  });
+
+  it('displays the validation error when no service items are selected', async () => {
+    render(
+      <CreatePaymentRequestForm
+        initialValues={initialValues}
+        createPaymentRequestSchema={createPaymentRequestSchema}
+        mtoShipments={twoShipments}
+        groupedServiceItems={basicAndShipmentsServiceItems}
+        onSubmit={jest.fn()}
+        handleSelectAll={jest.fn()}
+      />,
+    );
+
+    const basicServiceItemInput = screen.getAllByRole('checkbox', { name: 'Add to payment request' })[0];
+    await userEvent.click(basicServiceItemInput);
+
+    await userEvent.click(basicServiceItemInput);
+
+    await waitFor(() => {
+      expect(screen.getByText('At least 1 service item must be added when creating a payment request'));
+      expect(screen.getByRole('button', { type: 'submit' })).toBeDisabled();
+    });
+  });
+
+  it('selects all service items of the shipment when select all is clicked', async () => {
+    const handleSelectAll = (shipmentID, values, setValues) => {
+      setValues({ serviceItems: ['4'] });
+    };
+
+    render(
+      <CreatePaymentRequestForm
+        initialValues={initialValues}
+        createPaymentRequestSchema={createPaymentRequestSchema}
+        mtoShipments={twoShipments}
+        groupedServiceItems={basicAndShipmentsServiceItems}
+        onSubmit={jest.fn()}
+        handleSelectAll={handleSelectAll}
+      />,
+    );
+
+    const shipmentSelectAllInput = screen.getAllByRole('checkbox', { name: 'Add all service items' })[0];
+    await userEvent.click(shipmentSelectAllInput);
+
+    await waitFor(() => {
+      expect(screen.getByRole('checkbox', { name: 'Add to payment request', checked: true })).toBeInTheDocument();
+      expect(screen.getByRole('button', { type: 'submit' })).toBeEnabled();
+    });
+  });
+
+  it('deselects all service items of the shipment when select all is unchecked', async () => {
+    const handleSelectAll = (shipmentID, values, setValues, event) => {
+      if (!event.target.checked) {
+        setValues({ serviceItems: [] });
+      } else {
+        setValues({ serviceItems: ['4'] });
+      }
+    };
+
+    render(
+      <CreatePaymentRequestForm
+        initialValues={initialValues}
+        createPaymentRequestSchema={createPaymentRequestSchema}
+        mtoShipments={twoShipments}
+        groupedServiceItems={basicAndShipmentsServiceItems}
+        onSubmit={jest.fn()}
+        handleSelectAll={handleSelectAll}
+      />,
+    );
+
+    const shipmentSelectAllInput = screen.getAllByRole('checkbox', { name: 'Add all service items' })[0];
+    await userEvent.click(shipmentSelectAllInput);
+    await userEvent.click(shipmentSelectAllInput);
+
+    await waitFor(() => {
+      const shipmentServiceItemInput = screen.queryAllByRole('checkbox', {
+        name: 'Add to payment request',
+        checked: true,
+      });
+      // all checkboxes are back to being unchecked
+      expect(shipmentServiceItemInput).toHaveLength(0);
+      expect(screen.getByText('At least 1 service item must be added when creating a payment request'));
+      expect(screen.getByRole('button', { type: 'submit' })).toBeDisabled();
+    });
+  });
+});
---
+++ src/pages/PrimeUI/CreatePaymentRequest/CreatePaymentRequest.test.jsx
@@ -0,0 +1,247 @@
+import React from 'react';
+import { act, render, screen, within, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { usePrimeSimulatorGetMove } from '../../../hooks/queries';
+import { createPaymentRequest } from '../../../services/primeApi';
+
+import CreatePaymentRequest from './CreatePaymentRequest';
+
+const mockUseHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn().mockReturnValue({ moveCodeOrID: 'LN4T89' }),
+  useHistory: () => ({
+    push: mockUseHistoryPush,
+  }),
+}));
+
+jest.mock('hooks/queries', () => ({
+  usePrimeSimulatorGetMove: jest.fn(),
+}));
+
+jest.mock('services/primeApi', () => ({
+  ...jest.requireActual('services/primeApi'),
+  createPaymentRequest: jest.fn(),
+}));
+
+const moveTaskOrder = {
+  id: '1',
+  moveCode: 'LN4T89',
+  mtoShipments: [
+    {
+      id: '2',
+      shipmentType: 'HHG',
+      requestedPickupDate: '2021-11-26',
+      pickupAddress: { streetAddress1: '100 1st Avenue', city: 'New York', state: 'NY', postalCode: '10001' },
+    },
+    {
+      id: '3',
+      shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      requestedPickupDate: '2021-12-01',
+      pickupAddress: { streetAddress1: '800 Madison Avenue', city: 'New York', state: 'NY', postalCode: '10002' },
+    },
+  ],
+  mtoServiceItems: [
+    { id: '4', reServiceCode: 'MS', reServiceName: 'Move management' },
+    { id: '5', reServiceCode: 'DLH', mtoShipmentID: '2', reServiceName: 'Domestic linehaul' },
+    { id: '6', reServiceCode: 'FSC', mtoShipmentID: '3', reServiceName: 'Fuel surcharge' },
+  ],
+};
+
+const moveReturnValue = {
+  moveTaskOrder,
+  isLoading: false,
+  isError: false,
+};
+
+describe('CreatePaymentRequest page', () => {
+  describe('check loading and error component states', () => {
+    const loadingReturnValue = {
+      moveTaskOrder: undefined,
+      isLoading: true,
+      isError: false,
+    };
+
+    const errorReturnValue = {
+      moveTaskOrder: undefined,
+      isLoading: false,
+      isError: true,
+    };
+
+    it('renders the loading placeholder when the query is still loading', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(loadingReturnValue);
+
+      render(<CreatePaymentRequest />);
+
+      expect(await screen.getByRole('heading', { name: 'Loading, please wait...', level: 2 }));
+    });
+
+    it('renders the Something Went Wrong component when the query has an error', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(errorReturnValue);
+
+      render(<CreatePaymentRequest />);
+
+      expect(await screen.getByText(/Something went wrong./));
+    });
+  });
+
+  describe('displaying move, shipment, and service item information', () => {
+    it('displays the move information and basic service items', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+
+      render(<CreatePaymentRequest />);
+
+      const moveHeading = screen.getByRole('heading', { name: 'Move', level: 2 });
+      expect(moveHeading).toBeInTheDocument();
+
+      const moveContainer = moveHeading.parentElement;
+      expect(within(moveContainer).getByText('Move Code:')).toBeInTheDocument();
+      expect(within(moveContainer).getByText(moveTaskOrder.moveCode)).toBeInTheDocument();
+      expect(within(moveContainer).getByText('Move Id:')).toBeInTheDocument();
+      expect(within(moveContainer).getByText('1')).toBeInTheDocument();
+
+      const moveServiceItems = screen.getByRole('heading', { name: 'Move Service Items', level: 2 });
+      expect(moveServiceItems).toBeInTheDocument();
+
+      const moveServiceItemsContainer = moveServiceItems.parentElement;
+      expect(
+        within(moveServiceItemsContainer).getByRole('heading', { name: 'Move management', level: 3 }),
+      ).toBeInTheDocument();
+      expect(within(moveServiceItemsContainer).getByText('Service Code:')).toBeInTheDocument();
+      expect(within(moveServiceItemsContainer).getByText('MS')).toBeInTheDocument();
+      expect(
+        within(moveServiceItemsContainer).getByRole('checkbox', { name: 'Add to payment request', checked: false }),
+      ).toBeInTheDocument();
+    });
+
+    it('displays the shipment information and shipment service items', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+
+      render(<CreatePaymentRequest />);
+
+      const shipmentsHeading = screen.getByRole('heading', { name: 'Shipments', level: 2 });
+      expect(shipmentsHeading).toBeInTheDocument();
+
+      const shipmentsContainer = shipmentsHeading.parentElement;
+      const hhgHeading = within(shipmentsContainer).getByRole('heading', { name: 'HHG shipment', level: 3 });
+
+      expect(hhgHeading).toBeInTheDocument();
+      const hhgContainer = hhgHeading.parentElement.parentElement;
+
+      expect(
+        within(hhgContainer).getByRole('checkbox', { name: 'Add all service items', checked: false }),
+      ).toBeInTheDocument();
+
+      expect(within(hhgContainer).getByRole('heading', { name: 'Domestic linehaul', level: 3 }));
+
+      expect(
+        within(hhgContainer).getByRole('checkbox', { name: 'Add to payment request', checked: false }),
+      ).toBeInTheDocument();
+
+      const ntsHeading = within(shipmentsContainer).getByRole('heading', { name: 'NTS shipment', level: 3 });
+      expect(ntsHeading).toBeInTheDocument();
+      const ntsContainer = ntsHeading.parentElement.parentElement;
+
+      expect(
+        within(ntsContainer).getByRole('checkbox', { name: 'Add all service items', checked: false }),
+      ).toBeInTheDocument();
+
+      expect(within(ntsContainer).getByRole('heading', { name: 'Fuel surcharge', level: 3 }));
+
+      expect(
+        within(ntsContainer).getByRole('checkbox', { name: 'Add to payment request', checked: false }),
+      ).toBeInTheDocument();
+    });
+
+    it('displays the submit button and hint text', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+
+      render(<CreatePaymentRequest />);
+
+      expect(screen.getByRole('button', { name: 'Submit Payment Request' })).toBeDisabled();
+      expect(
+        screen.getByText(
+          'At least one basic service item or shipment service item is required to create a payment request',
+        ),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('error alert display', () => {
+    it('displays the error alert when the api submission returns an error', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+      createPaymentRequest.mockRejectedValue({ response: { body: { title: 'Error title', detail: 'Error detail' } } });
+
+      render(<CreatePaymentRequest />);
+
+      const serviceItemInputs = screen.getAllByRole('checkbox', { name: 'Add to payment request' });
+      // avoiding linter pitfalls with async for loops
+      await userEvent.click(serviceItemInputs[0]);
+      await userEvent.click(serviceItemInputs[1]);
+      await userEvent.click(serviceItemInputs[2]);
+
+      await act(async () => {
+        await userEvent.click(screen.getByRole('button', { name: 'Submit Payment Request' }));
+      });
+
+      await waitFor(() => {
+        expect(screen.getByText('Error title')).toBeInTheDocument();
+        expect(screen.getByText('Error detail')).toBeInTheDocument();
+      });
+    });
+
+    it('displays the unknown error when none is provided', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+      createPaymentRequest.mockRejectedValue('malformed api error response');
+
+      render(<CreatePaymentRequest />);
+
+      const serviceItemInputs = screen.getAllByRole('checkbox', { name: 'Add to payment request' });
+      await userEvent.click(serviceItemInputs[0]);
+      await userEvent.click(serviceItemInputs[1]);
+      await userEvent.click(serviceItemInputs[2]);
+
+      await act(async () => {
+        await userEvent.click(screen.getByRole('button', { name: 'Submit Payment Request' }));
+      });
+
+      await waitFor(() => {
+        expect(screen.getByText('Unexpected error')).toBeInTheDocument();
+        expect(
+          screen.getByText(
+            'An unknown error has occurred, please check the state of the shipment and service items data for this move',
+          ),
+        ).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('successful submission of form', () => {
+    it('calls history router back to move details', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+      createPaymentRequest.mockReturnValue({
+        id: '7',
+        moveTaskOrderID: '1',
+        paymentRequestNumber: '1111-1111-1',
+        status: 'PENDING',
+        paymentServiceItems: [],
+      });
+
+      render(<CreatePaymentRequest />);
+
+      const serviceItemInputs = screen.getAllByRole('checkbox', { name: 'Add to payment request' });
+      await userEvent.click(serviceItemInputs[0]);
+      await userEvent.click(serviceItemInputs[1]);
+      await userEvent.click(serviceItemInputs[2]);
+
+      await act(async () => {
+        await userEvent.click(screen.getByRole('button', { name: 'Submit Payment Request' }));
+      });
+
+      await waitFor(() => {
+        expect(mockUseHistoryPush).toHaveBeenCalledWith('/simulator/moves/LN4T89/details');
+      });
+    });
+  });
+});
