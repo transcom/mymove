e147dc1fa734ecc6af77ba1c64ef8d60ada59597
Duncan Spencer <duncan@truss.works>
2021-10-21 16:31:12 +0000

    dynamic form display for origin and destination SIT service item creation

--- src/components/PrimeUI/CreateShipmentServiceItemForm/CreateShipmentServiceItemForm.jsx
+++ src/components/PrimeUI/CreateShipmentServiceItemForm/CreateShipmentServiceItemForm.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Formik } from 'formik';
-import { Dropdown } from '@trussworks/react-uswds';
+import { Button, Dropdown, Label } from '@trussworks/react-uswds';
 
 import { ShipmentShape } from '../../../types/shipment';
 import TextField from '../../form/fields/TextField';
 import { DatePickerInput } from '../../form/fields';
 import Shipment from '../Shipment/Shipment';
+import MaskedTextField from '../../form/fields/MaskedTextField';
+import { Form } from '../../form';
+import { AddressFields } from '../../form/AddressFields/AddressFields';
 
 const serviceItemTypeOptions = (
   <>
@@ -14,21 +17,97 @@ const serviceItemTypeOptions = (
   </>
 );
 
-const originSITForm = (
-  <>
-    <TextField name="reason" id="reason" label="Reason" />
-  </>
-);
+const originSITForm = (shipment) => {
+  const initialValues = {
+    moveTaskOrderID: shipment.moveTaskOrderID,
+    mtoShipmentID: shipment.id,
+    modelType: 'MTOServiceItemOriginSIT',
+    reServiceCode: 'DOFSIT',
+    reason: '',
+    sitPostalCode: '',
+    sitEntryDate: '',
+    sitDepartureDate: '',
+    sitHHGActualOrigin: {},
+  };
+  return (
+    <Formik initialValues={initialValues}>
+      <Form>
+        <input type="hidden" name="moveTaskOrderID" />
+        <input type="hidden" name="mtoShipmentID" />
+        <input type="hidden" name="modelType" />
+        <input type="hidden" name="reServiceCode" />
+        <TextField name="reason" id="reason" label="Reason" />
+        <MaskedTextField
+          id="sitPostalCode"
+          name="sitPostalCode"
+          label="SIT postal code"
+          mask="00000[{-}0000]"
+          placeholder="62225"
+        />
+        <DatePickerInput label="SIT Entry Date" name="sitEntryDate" />
+        <DatePickerInput label="SIT Departure Date" name="sitDepartureDate" />
+        <AddressFields legend="SIT HHG actual origin" name="sitHHGActualOrigin" />
+        <Button type="submit">Create service item</Button>
+      </Form>
+    </Formik>
+  );
+};
 
-const destinationSITForm = (
-  <>
-    <DatePickerInput label="First available delivery date" name="firstAvailableDeliveryDate1" />
-  </>
-);
+const destinationSITForm = (shipment) => {
+  const initialValues = {
+    moveTaskOrderID: shipment.moveTaskOrderID,
+    mtoShipmentID: shipment.id,
+    modelType: 'MTOServiceItemDestSIT',
+    reServiceCode: 'DDFSIT',
+    firstAvailableDeliveryDate1: '',
+    timeMilitary1: '',
+    firstAvailableDeliveryDate2: '',
+    timeMilitary2: '',
+    sitEntryDate: '',
+    sitDepartureDate: '',
+    sitDestinationFinalAddress: {},
+  };
+  return (
+    <Formik initialValues={initialValues}>
+      <Form>
+        <input type="hidden" name="moveTaskOrderID" />
+        <input type="hidden" name="mtoShipmentID" />
+        <input type="hidden" name="modelType" />
+        <input type="hidden" name="reServiceCode" />
+        <DatePickerInput label="First available delivery date" name="firstAvailableDeliveryDate1" />
+        <MaskedTextField
+          id="timeMilitary1"
+          name="timeMilitary1"
+          label="First available delivery time"
+          mask="0000{Z}"
+          placeholder="1400Z"
+        />
+        <DatePickerInput label="Second available delivery date" name="firstAvailableDeliveryDate2" />
+        <MaskedTextField
+          id="timeMilitary1"
+          name="timeMilitary2"
+          label="Second available delivery time"
+          mask="0000{Z}"
+          placeholder="1400Z"
+        />
+        <DatePickerInput label="SIT entry date" name="sitEntryDate" />
+        <DatePickerInput label="SIT departure date" name="sitDepartureDate" />
+        <AddressFields legend="SIT destination final address" name="sitHHGActualOrigin" />
+        <Button type="submit">Create service item</Button>
+      </Form>
+    </Formik>
+  );
+};
 
-const serviceItemFormLookup = {
-  MTOServiceItemOriginSIT: originSITForm,
-  MTOServiceItemDestSIT: destinationSITForm,
+const populateServiceItemForm = (serviceItemType, shipment) => {
+  switch (serviceItemType) {
+    case 'MTOServiceItemOriginSIT':
+      return originSITForm(shipment);
+    case 'MTOServiceItemDestSIT':
+      return destinationSITForm(shipment);
+    default:
+      return <></>;
+  }
 };
 
 const CreateShipmentServiceItemForm = ({ shipment }) => {
@@ -41,10 +120,11 @@ const CreateShipmentServiceItemForm = ({ shipment }) => {
   return (
     <>
       <Shipment shipment={shipment} />
-      <Dropdown name="serviceItemType" label="Service item type" onChange={handleServiceItemTypeChange}>
+      <Label htmlFor="serviceItemType">Service item type</Label>
+      <Dropdown id="serviceItemType" name="serviceItemType" onChange={handleServiceItemTypeChange}>
         {serviceItemTypeOptions}
       </Dropdown>
-      <Formik>{serviceItemFormLookup[selectedServiceItemType]}</Formik>
+      {populateServiceItemForm(selectedServiceItemType, shipment)}
     </>
   );
 };
