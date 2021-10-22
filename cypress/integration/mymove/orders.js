b68bf3d8a21c671d358f75919f8e1e0745f81ad6
Emily Mahanna <emahanna@truss.works>
2021-10-22 18:09:09 +0000

    Update integration tests

--- cypress/integration/mymove/orders.js
+++ cypress/integration/mymove/orders.js
@@ -34,14 +34,14 @@ describe('orders entry', function () {
 
     cy.get('label[for="hasDependentsNo"]').first().click();
 
-    // Choosing same current and destination duty station should block you from progressing and give an error
-    cy.selectDutyStation('Yuma AFB', 'new_duty_station');
+    // Choosing same current and destination duty location should block you from progressing and give an error
+    cy.selectDutyStation('Yuma AFB', 'new_duty_location');
     cy.get('.usa-error-message').contains(
-      'You entered the same duty station for your origin and destination. Please change one of them.',
+      'You entered the same duty location for your origin and destination. Please change one of them.',
     );
     cy.get('button[data-testid="wizardNextButton"]').should('be.disabled');
 
-    cy.selectDutyStation('NAS Fort Worth JRB', 'new_duty_station');
+    cy.selectDutyStation('NAS Fort Worth JRB', 'new_duty_location');
 
     cy.nextPage();
 
