d9cd4ff3e8c7c246ccadee6950cf67b604abdf8b
kennethchow-truss <kenneth.chow@truss.works>
2021-10-25 14:27:55 -0700

    Add tests
    (cherry picked from commit dcb9ab48d52c26b1dade85698ce238e701b6f8f2)

---
+++ src/pages/PrimeUI/MoveTaskOrder/MoveDetails.test.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { usePrimeSimulatorGetMove } from '../../../hooks/queries';
+
+import MoveDetails from './MoveDetails';
+
+import { MockProviders } from 'testUtils';
+
+const mockUseHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn().mockReturnValue({ moveCodeOrID: 'LN4T89' }),
+  useHistory: () => ({
+    push: mockUseHistoryPush,
+  }),
+}));
+
+jest.mock('hooks/queries', () => ({
+  usePrimeSimulatorGetMove: jest.fn(),
+}));
+
+jest.mock('services/primeApi', () => ({
+  ...jest.requireActual('services/primeApi'),
+  moveDetails: jest.fn(),
+}));
+
+const moveTaskOrder = {
+  id: '1',
+  moveCode: 'LN4T89',
+  mtoShipments: [
+    {
+      id: '2',
+      shipmentType: 'HHG',
+      requestedPickupDate: '2021-11-26',
+      pickupAddress: { streetAddress1: '100 1st Avenue', city: 'New York', state: 'NY', postalCode: '10001' },
+      destinationAddress: {
+        streetAddress1: '800 Madison Avenue',
+        streetAddress2: '900 Madison Avenue',
+        city: 'New York',
+        state: 'NY',
+        postalCode: '10002',
+      },
+    },
+    {
+      id: '3',
+      shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      requestedPickupDate: '2021-12-01',
+      pickupAddress: { streetAddress1: '800 Madison Avenue', city: 'New York', state: 'NY', postalCode: '10002' },
+      destinationAddress: {
+        streetAddress1: '800 Madison Avenue',
+        streetAddress2: '900 Madison Avenue',
+        city: 'New York',
+        state: 'NY',
+        postalCode: '10002',
+      },
+    },
+  ],
+  paymentRequests: [
+    {
+      id: '4a1b0048-ffe7-11eb-9a03-0242ac130003',
+      paymentRequestNumber: '5924-0164-1',
+    },
+  ],
+};
+
+const moveReturnValue = {
+  moveTaskOrder,
+  isLoading: false,
+  isError: false,
+};
+
+describe('PrimeUI MoveDetails page', () => {
+  describe('check move details page load', () => {
+    it('displays payment requests information', async () => {
+      usePrimeSimulatorGetMove.mockReturnValue(moveReturnValue);
+      render(
+        <MockProviders initialEntries={[`payment-request/${moveTaskOrder.paymentRequests[0].id}/upload`]}>
+          <MoveDetails />
+        </MockProviders>,
+      );
+
+      const paymentRequestsHeading = screen.getByRole('heading', { name: 'Payment Requests', level: 2 });
+      expect(paymentRequestsHeading).toBeInTheDocument();
+
+      const uploadButton = screen.getByText(/Upload Document/, { selector: 'a.usa-button' });
+      expect(uploadButton).toBeInTheDocument();
+    });
+  });
+});
