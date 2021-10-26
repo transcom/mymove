7adf953fd7ab85447f8986588b1e511caa72cd21
Roger Steve Ruiz <rogeruiz@truss.works>
2021-10-26 23:43:14 +0000

    Update AddressForm with <Update, Cancel> buttons

--- src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateAddressForm.jsx
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateAddressForm.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Formik } from 'formik';
+import { useHistory, useParams } from 'react-router-dom';
+import { generatePath } from 'react-router';
 import PropTypes from 'prop-types';
 import { FormGroup } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import classnames from 'classnames';
 
 import SectionWrapper from '../../../components/Customer/SectionWrapper';
 import { ResidentialAddressShape } from '../../../types/address';
 import { AddressFields } from '../../../components/form/AddressFields/AddressFields';
+import { primeSimulatorRoutes } from '../../../constants/routes';
 
 import { Form } from 'components/form/Form';
 import formStyles from 'styles/form.module.scss';
+import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
 import { requiredAddressSchema } from 'utils/validation';
 
 const PrimeUIShipmentUpdateAddressForm = ({
@@ -18,24 +21,41 @@ const PrimeUIShipmentUpdateAddressForm = ({
   addressLocation,
   onSubmit,
   updateShipmentAddressSchema,
-}) => (
-  <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={updateShipmentAddressSchema}>
-    {({ isValid, isSubmitting, errors }) => (
-      /* <Form className={classnames(styles.CreatePaymentRequestForm, formStyles.form)}> */
-      <Form className={classnames(formStyles.form)}>
-        <FormGroup error={errors != null && Object.keys(errors).length > 0 ? 1 : 0}>
-          <SectionWrapper className={formStyles.formSection}>
-            <h2>{addressLocation}</h2>
-            <AddressFields name="address" />
-          </SectionWrapper>
-          <Button aria-label="Update Shipment Address" type="submit" disabled={isSubmitting || !isValid}>
-            Update
-          </Button>
-        </FormGroup>
-      </Form>
-    )}
-  </Formik>
-);
+}) => {
+  const { moveCodeOrID } = useParams();
+  const history = useHistory();
+
+  const handleClose = () => {
+    history.push(generatePath(primeSimulatorRoutes.VIEW_MOVE_PATH, { moveCodeOrID }));
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={updateShipmentAddressSchema}>
+      {({ isValid, isSubmitting, handleSubmit, errors }) => (
+        /* <Form className={classnames(styles.CreatePaymentRequestForm, formStyles.form)}> */
+        <Form className={classnames(formStyles.form)}>
+          <FormGroup error={errors != null && Object.keys(errors).length > 0 ? 1 : 0}>
+            <SectionWrapper className={formStyles.formSection}>
+              <h2>{addressLocation}</h2>
+              <AddressFields name="address" />
+            </SectionWrapper>
+            <WizardNavigation
+              editMode
+              className={formStyles.formActions}
+              aria-label="Update Shipment Address"
+              type="submit"
+              disabled={isSubmitting || !isValid}
+              onCancelClick={handleClose}
+              onNextClick={handleSubmit}
+            >
+              Update
+            </WizardNavigation>
+          </FormGroup>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 PrimeUIShipmentUpdateAddressForm.propTypes = {
   initialValues: PropTypes.shape({
