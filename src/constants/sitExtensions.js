0f534a9d3d16967dd37e37ac7d8dd4deec64ea17
Emily Mahanna <emahanna@truss.works>
2021-10-27 16:23:52 +0000

    Update sitExtension reason

--- src/constants/sitExtensions.js
+++ src/constants/sitExtensions.js
@@ -20,7 +20,7 @@ export const sitExtensionReasons = {
   [SIT_EXTENSION_REASON.SERIOUS_ILLNESS_MEMBER]: 'Serious illness of the member',
   [SIT_EXTENSION_REASON.SERIOUS_ILLNESS_DEPENDENT]: 'Serious illness or death of a dependent',
   [SIT_EXTENSION_REASON.IMPENDING_ASSIGNEMENT]: 'Impending assignment to government quarters',
-  [SIT_EXTENSION_REASON.DIRECTED_TEMPORARY_DUTY]: 'Directed temporary duty after arrival at permanent duty station',
+  [SIT_EXTENSION_REASON.DIRECTED_TEMPORARY_DUTY]: 'Directed temporary duty after arrival at permanent duty location',
   [SIT_EXTENSION_REASON.NONAVAILABILITY_OF_CIVILIAN_HOUSING]: 'Nonavailability of suitable civilian housing',
   [SIT_EXTENSION_REASON.AWAITING_COMPLETION_OF_RESIDENCE]: 'Awaiting completion of residence under construction',
   [SIT_EXTENSION_REASON.OTHER]: 'Other reason',
