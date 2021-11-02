6d5e69ee0d9f84e09af8ce97c02498e4677d7d14
kennethchow-truss <kenneth.chow@truss.works>
2021-11-02 09:46:22 -0700

    Added banner

---
+++ src/pages/PrimeUI/PrimeBanner/PrimeBanner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PrimeBanner.module.scss';
+
+const PrimeBanner = () => {
+  return <div className={styles.PrimeBanner}>FOR INTERNAL TESTING PURPOSES ONLY</div>;
+};
+
+export default PrimeBanner;
