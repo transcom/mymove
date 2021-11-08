8dfcd2dc6b94caf80240e12d7a0cee97ef99dd3b
Marjo Poindexter <marjo@truss.works>
2021-11-08 19:52:05 +0000

    add new fields to models

--- migrations/app/schema/20211104212749_add_selected_tac_sac_to_mto_shipments.up.sql
+++ migrations/app/schema/20211104212749_add_selected_tac_sac_to_mto_shipments.up.sql
@@ -1,5 +1,5 @@
-CREATE TYPE loa_type AS ENUM ('hhg', 'nts');
+CREATE TYPE loa_type AS ENUM ('HHG', 'NTS');
 
 ALTER TABLE mto_shipments
-	ADD COLUMN tac loa_type,
-	ADD COLUMN sac loa_type;
+	ADD COLUMN tac_type loa_type,
+	ADD COLUMN sac_type loa_type;
