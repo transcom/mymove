b7faa5b535065385ee601d3a5ac7e0f91f0bf367
Namibia Torres <namibia@truss.works>
2021-11-08 18:19:39 +0000

    yaml address shape updates

--- src/components/Customer/ResidentialAddressForm/ResidentialAddressForm.jsx
+++ src/components/Customer/ResidentialAddressForm/ResidentialAddressForm.jsx
@@ -19,6 +19,7 @@ const ResidentialAddressForm = ({ formFieldsName, initialValues, onSubmit, onBac
   return (
     <Formik
       initialValues={initialValues}
+      initialValues={initialValues}
       onSubmit={onSubmit}
       validateOnChange={false}
       validateOnMount
