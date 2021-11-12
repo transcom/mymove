05a480dd2c1c41ba59c6ea396f2ff41f084908a2
Marty Boren <marty@truss.works>
2021-11-12 20:39:16 +0000

    Add index to orders.gbloc

--- migrations/app/schema/20211112193224_support_noninstallation_locations.up.sql
+++ migrations/app/schema/20211112193224_support_noninstallation_locations.up.sql
@@ -3,4 +3,7 @@ ALTER TABLE duty_stations
 
 ALTER TABLE orders
 	ADD COLUMN gbloc VARCHAR;
+
+CREATE INDEX orders_gbloc_idx ON orders (gbloc);
+
 COMMENT ON COLUMN orders.gbloc IS 'TIO and TOO users from transportation offices in this GBLOC will see these orders in their queue.';
