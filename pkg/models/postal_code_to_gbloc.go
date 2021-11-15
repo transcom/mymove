9e875911a8d8768107f08df76456a0c85955e43a
Marty Boren <marty@truss.works>
2021-11-15 18:25:23 +0000

    Add missing fields to postal_code_to_gbloc model
    CreatedAt/UpdatedAt were in the DB schema but not in the corresponding
    Go model.

--- pkg/models/postal_code_to_gbloc.go
+++ pkg/models/postal_code_to_gbloc.go
@@ -1,9 +1,13 @@
 package models
 
+import "time"
+
 // PostalCodeToGBLOC is a mapping from Postal Codes to GBLOCs
 type PostalCodeToGBLOC struct {
-	PostalCode string `db:"postal_code"`
-	GBLOC      string `db:"gbloc"`
+	PostalCode string    `db:"postal_code"`
+	GBLOC      string    `db:"gbloc"`
+	CreatedAt  time.Time `db:"created_at"`
+	UpdatedAt  time.Time `db:"updated_at"`
 }
 
 // PostalCodeToGBLOCs is not required by pop and may be deleted
