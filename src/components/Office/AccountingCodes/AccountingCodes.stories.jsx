4943649cdc1a1d1d6f075c54066b642909dfa2e6
Kyle Hill <kyle@truss.works>
2021-11-16 17:55:27 +0000

    AccountingCodes component created with stories, tests

--- src/components/Office/AccountingCodes/AccountingCodes.stories.jsx
+++ src/components/Office/AccountingCodes/AccountingCodes.stories.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Grid, GridContainer } from '@trussworks/react-uswds';
+import { Formik } from 'formik';
 
 import AccountingCodes from './AccountingCodes';
 
-import formStyles from 'styles/form.module.scss';
 import styles from 'pages/Office/ServicesCounselingMoveInfo/ServicesCounselingTab.module.scss';
+import { Form } from 'components/form/Form';
+import formStyles from 'styles/form.module.scss';
 
 export default {
   title: 'Office Components / Forms / ServicesCounselingShipmentForm / AccountingCodes',
@@ -14,9 +16,15 @@ export default {
       <GridContainer className={styles.gridContainer}>
         <Grid row>
           <Grid col desktop={{ col: 8, offset: 2 }}>
-            <form className={formStyles.form}>
-              <Story />
-            </form>
+            <Formik initialValues={{}}>
+              {() => {
+                return (
+                  <Form className={formStyles.form} style={{ maxWidth: 'none' }}>
+                    <Story />
+                  </Form>
+                );
+              }}
+            </Formik>
           </Grid>
         </Grid>
       </GridContainer>
@@ -24,5 +32,13 @@ export default {
   ],
 };
 
-// create shipment stories (form should not prefill customer data)
-export const ServicesCounselorView = () => <AccountingCodes />;
+export const AsRequired = () => <AccountingCodes optional={false} />;
+
+export const WithNoTACsOrSACs = () => <AccountingCodes />;
+WithNoTACsOrSACs.storyName = 'With No TACs or SACs';
+
+export const WithSingleCode = () => <AccountingCodes TACs={{ hhg: '1234 ' }} />;
+
+export const WithMultipleCodes = () => (
+  <AccountingCodes TACs={{ hhg: '1234', nts: '5678' }} SACs={{ hhg: '98765', nts: '000012345' }} />
+);
