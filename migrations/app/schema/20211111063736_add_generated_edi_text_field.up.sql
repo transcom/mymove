af47934ad0392caf83e9b40820b6efd21db73538
John Gedeon <john@truss.works>
2021-11-17 23:30:49 +0000

    fixup! Add generate_edi_858_text field to payment request migration

--- migrations/app/schema/20211111063736_add_generated_edi_text_field.up.sql
+++ migrations/app/schema/20211111063736_add_generated_edi_text_field.up.sql
@@ -1,3 +1,25 @@
--- add two new columns to entitlements table
-ALTER TABLE payment_requests ADD COLUMN generated_edi_858_text text;
-COMMENT ON COLUMN payment_requests.generated_edi_858_text IS 'Once a payment request is processed and an EDI 858 is generated it is stored in this column for reference in diagnosing issues'
+-- rename table to more acurately describe what is in it.
+ALTER TABLE payment_request_to_interchange_control_numbers RENAME TO payment_request_edis;
+ALTER TABLE payment_request_edis
+  RENAME CONSTRAINT payment_request_to_icns_payment_request_id_fkey TO payment_request_edi_to_payment_request_id_fkey;
+
+-- create view with name of old table to allow for zero downtime
+-- creating here means view will not have the new fields below
+CREATE VIEW payment_request_to_interchange_control_numbers AS SELECT * FROM payment_request_edis;
+COMMENT ON VIEW payment_request_to_interchange_control_numbers IS 'Is the old name of payment_request_edis table, should be dropped eventually after new code is rolled out';
+-- update edi_errors forigen key reference
+ALTER TABLE edi_errors
+  RENAME COLUMN interchange_control_number_id TO payment_request_edi_id;
+ALTER TABLE edi_errors
+  RENAME CONSTRAINT edi_errors_icn_id_fkey TO edi_errors_payment_request_edi_id_to_payment_request_edi_id_fkey;
+
+-- add new column to hold edi text
+ALTER TABLE payment_request_edis ADD COLUMN edi_text text;
+
+-- add new column for created and updated at times
+ALTER TABLE payment_request_edis ADD COLUMN created_at timestamp not null;
+ALTER TABLE payment_request_edis ADD COLUMN updated_at timestamp not null;
+
+-- Add comment for new column
+COMMENT ON COLUMN payment_request_edis.edi_text IS 'Contains the text of the edi that is represented by this EDI type, EDI ICN, and Payment Request ID';
+COMMENT ON COLUMN edi_errors.payment_request_edi_id IS 'ID for payment_request_edis associated with this error. This will identify the ICN EDI related to the payment request.';
