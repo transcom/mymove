d531868ea627e302fe6b5461ccabe18f9038823a
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2021-11-18 22:28:29 +0000

    Enable e2e test for services counselor and add a wait call so it happens successfully.

--- cypress/integration/office/servicescounseling/servicesCounselingFlows.js
+++ cypress/integration/office/servicescounseling/servicesCounselingFlows.js
@@ -18,6 +18,7 @@ describe('Services counselor user', () => {
     cy.intercept('**/ghc/v1/move-task-orders/**/status/service-counseling-completed').as(
       'patchServiceCounselingCompleted',
     );
+    cy.intercept('**/ghc/v1/moves/**/financial-review-flag').as('financialReviewFlagCompleted');
 
     const userId = 'a6c8663f-998f-4626-a978-ad60da2476ec';
     cy.apiSignInAsUser(userId, ServicesCounselorOfficeUserType);
@@ -65,25 +66,26 @@ describe('Services counselor user', () => {
     cy.contains('Move submitted.');
   });
 
-  // it('is able to flag a move for financial review', () => {
-  //   cy.wait(['@getSortedMoves']);
-  //   // It doesn't matter which move we click on in the queue.
-  //   cy.get('td').first().click();
-  //   cy.url().should('include', `details`);
-  //   cy.wait(['@getMoves', '@getOrders', '@getMTOShipments', '@getMTOServiceItems']);
-  //
-  //   // click to trigger financial review modal
-  //   cy.contains('Flag move for financial review').click();
+  it('is able to flag a move for financial review', () => {
+    cy.wait(['@getSortedMoves']);
+    // It doesn't matter which move we click on in the queue.
+    cy.get('td').first().click();
+    cy.url().should('include', `details`);
+    cy.wait(['@getMoves', '@getOrders', '@getMTOShipments', '@getMTOServiceItems']);
 
-  //   // Enter information in modal and submit
-  //   cy.get('label').contains('Yes').click();
-  //   cy.get('textarea').type('Because I said so...');
+    // click to trigger financial review modal
+    cy.contains('Flag move for financial review').click();
 
-  //   // Click save on the modal
-  //   cy.get('button').contains('Save').click();
+    // Enter information in modal and submit
+    cy.get('label').contains('Yes').click();
+    cy.get('textarea').type('Because I said so...');
 
-  //   // Verify sucess alert and tag
-  //   cy.contains('Move flagged for financial review.');
-  //   cy.contains('Flagged for financial review');
-  // });
+    // Click save on the modal
+    cy.get('button').contains('Save').click();
+    cy.wait(['@financialReviewFlagCompleted']);
+
+    // Verify sucess alert and tag
+    cy.contains('Move flagged for financial review.');
+    cy.contains('Flagged for financial review');
+  });
 });
