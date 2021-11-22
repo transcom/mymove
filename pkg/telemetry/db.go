2d1f1771f3eb3524768d856c5b30e97dcd01150e
Andrew Hobson <ahobson@truss.works>
2021-11-22 18:31:28 +0000

    Single callback for otel db stats, waitduration in ms, more stats
    * Instead of multiple otel async callbacks, use a single one and
      report on multiple db stats

--- pkg/telemetry/db.go
+++ pkg/telemetry/db.go
@@ -13,22 +13,6 @@ import (
 	"github.com/transcom/mymove/pkg/db/stats"
 )
 
-// Ideally the "go.opentelemetry.io/otel/semconv" package would
-// provide standard names for the stats below, but they don't seem to
-// See https://github.com/open-telemetry/opentelemetry-go/blob/main/semconv/v1.7.0/trace.go
-
-// https://pkg.go.dev/database/sql#DB.Stats InUse
-const dbPoolInUseName = "db.pool.inuse"
-const dbPoolInUseDesc = "The number of connections currently in use"
-
-// https://pkg.go.dev/database/sql#DB.Stats Idle
-const dbPoolIdleName = "db.pool.idle"
-const dbPoolIdleDesc = "The number of connections currently in use"
-
-// https://pkg.go.dev/database/sql#DB.Stats WaitDuration
-const dbWaitDurationName = "db.waitduration"
-const dbWaitDurationDesc = "The total time blocked waiting for a new connection"
-
 const dbTelemetryVersion = "0.1"
 
 // RegisterDBStatsObserver creates a custom metric that is updated
@@ -52,33 +36,85 @@ func RegisterDBStatsObserver(appCtx appcontext.AppContext, config *Config) error
 	lock.Lock()
 	defer lock.Unlock()
 
-	poolInUse, err := dbMeter.Int64ObservableUpDownCounter(dbPoolInUseName, instrument.WithDescription(dbPoolInUseDesc))
+	// Ideally the "go.opentelemetry.io/otel/semconv" package would
+	// provide standard names for the stats below, but they don't seem to
+	// See https://github.com/open-telemetry/opentelemetry-go/blob/main/semconv/v1.7.0/trace.go
+
+	// https://pkg.go.dev/database/sql#DB.Stats
+	poolInUse, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.pool.inuse",
+		instrument.WithDescription("The number of connections currently in use"))
 	if err != nil {
 		return err
 	}
 
-	poolIdle, err := dbMeter.Int64ObservableUpDownCounter(dbPoolIdleName, instrument.WithDescription(dbPoolIdleDesc))
+	// https://pkg.go.dev/database/sql#DB.Stats
+	poolIdle, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.pool.idle",
+		instrument.WithDescription("The number of idle connections"))
 	if err != nil {
 		return err
 	}
 
-	dbWait, err := dbMeter.Int64ObservableUpDownCounter(dbWaitDurationName, instrument.WithDescription(dbWaitDurationDesc))
+	// https://pkg.go.dev/database/sql#DB.Stats
+	dbWait, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.waitduration",
+		instrument.WithUnit("ms"),
+		instrument.WithDescription("Milliseconds blocked waiting for a new connection"))
 	if err != nil {
 		return err
 	}
 
+	maxIdleClosed, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.maxidleclosed",
+		instrument.WithDescription("The total number of connections closed due to SetMaxIdleConns"),
+	)
+	if err != nil {
+		return err
+	}
+
+	maxIdleTimeClosed, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.maxidletimeclosed",
+		instrument.WithDescription("The total number of connections closed due to SetConnMaxIdleTime"),
+	)
+	if err != nil {
+		return err
+	}
+
+	maxLifetimeClosed, err := dbMeter.Int64ObservableUpDownCounter(
+		"db.maxlifetimeclosed",
+		instrument.WithDescription("The total number of connections closed due to SetConnMaxLifetime"),
+	)
+	if err != nil {
+		return err
+	}
+
+	lastStats := time.Now()
 	_, err = dbMeter.RegisterCallback(
 		func(ctx context.Context, observer metric.Observer) error {
 			lock.Lock()
 			defer lock.Unlock()
+
+			now := time.Now()
+
+			// round to nearest second
+			diff := now.Sub(lastStats).Round(time.Second)
+			if diff < minDuration {
+				appCtx.Logger().Warn("Skipping db telemetry update")
+				return nil
+			}
 			dbStats, dberr := stats.DBStats(appCtx)
 			if dberr == nil {
 				observer.ObserveInt64(poolInUse, int64(dbStats.InUse))
 				observer.ObserveInt64(poolIdle, int64(dbStats.Idle))
-				observer.ObserveInt64(dbWait, int64(dbStats.WaitDuration))
+				observer.ObserveInt64(dbWait, int64(dbStats.WaitDuration.Milliseconds()))
+				observer.ObserveInt64(maxIdleClosed, dbStats.MaxIdleClosed)
+				observer.ObserveInt64(maxIdleTimeClosed, dbStats.MaxIdleTimeClosed)
+				observer.ObserveInt64(maxLifetimeClosed, dbStats.MaxLifetimeClosed)
+				lastStats = now
 			}
 			return dberr
-		}, poolInUse, poolIdle, dbWait)
+		}, poolInUse, poolIdle, dbWait, maxIdleClosed, maxIdleTimeClosed, maxLifetimeClosed)
 
 	if err != nil {
 		return err
