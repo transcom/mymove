33c052c3d1b18bcd50bb3d57082aeef30d2c32a5
Yan Zhao <yan.zhao@truss.works>
2021-11-22 22:50:33 +0000

    Implement close button functionality
    - add params and history and generate a path to push onto the history

--- src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweigh.jsx
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweigh.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Grid, GridContainer } from '@trussworks/react-uswds';
+import { generatePath } from 'react-router';
+import { useHistory, useParams } from 'react-router-dom';
 
 import PrimeUIShipmentUpdateReweighForm from './PrimeUIShipmentUpdateReweighForm';
 
 import styles from 'components/Office/CustomerContactInfoForm/CustomerContactInfoForm.module.scss';
+import { primeSimulatorRoutes } from 'constants/routes';
 
 const PrimeUIShipmentUpdateReweigh = () => {
+  const { moveCodeOrID } = useParams();
+  const history = useHistory();
+
   const handleSubmit = () => {};
 
-  const handleClose = () => {};
+  const handleClose = () => {
+    history.push(generatePath(primeSimulatorRoutes.VIEW_MOVE_PATH, { moveCodeOrID }));
+  };
 
   return (
     <div className={styles.tabContent}>
