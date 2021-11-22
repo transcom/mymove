72fa7019b28fefcc7c575a82aaa15cd1a4ffef83
Yan Zhao <yan.zhao@truss.works>
2021-11-22 21:08:57 +0000

    Add initial framework for the reweigh form

--- src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweigh.jsx
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweigh.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import PrimeUIShipmentUpdateReweighForm from './PrimeUIShipmentUpdateReweighForm';
 
+import styles from 'components/Office/CustomerContactInfoForm/CustomerContactInfoForm.module.scss';
+
 const PrimeUIShipmentUpdateReweigh = () => {
-  return <PrimeUIShipmentUpdateReweighForm />;
+  const handleSubmit = () => {};
+
+  const handleClose = () => {};
+
+  return (
+    <div className={styles.tabContent}>
+      <div className={styles.container}>
+        <GridContainer className={styles.gridContainer}>
+          <Grid row>
+            <Grid col desktop={{ col: 8, offset: 2 }}>
+              <PrimeUIShipmentUpdateReweighForm handleSubmit={handleSubmit} handleClose={handleClose} />
+            </Grid>
+          </Grid>
+        </GridContainer>
+      </div>
+    </div>
+  );
 };
 
 export default PrimeUIShipmentUpdateReweigh;
--- src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweighForm.jsx
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweighForm.jsx
@@ -1,7 +1,75 @@
 import React from 'react';
+import { func } from 'prop-types';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import { Textarea, Label } from '@trussworks/react-uswds';
 
-const ReweighForm = () => {
-  return <div>Reweigh form</div>;
+import SectionWrapper from 'components/Customer/SectionWrapper';
+import formStyles from 'styles/form.module.scss';
+import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
+import MaskedTextField from 'components/form/fields/MaskedTextField';
+
+const validationSchema = Yup.object({
+  reweighWeight: Yup.number().min(1, 'Authorized weight must be greater than or equal to 1').required('Required'),
+  reweighRemarks: Yup.string().required('Required'),
+});
+
+const ReweighForm = ({ handleSubmit, handleClose }) => {
+  const initialValues = {
+    reweighWeight: '',
+    reweighRemarks: '',
+  };
+
+  return (
+    <Formik enableReinitialize initialValues={initialValues} validationSchema={validationSchema}>
+      {({ handleChange, values, setTouched }) => (
+        <>
+          <SectionWrapper className={formStyles.formSection}>
+            <MaskedTextField
+              defaultValue="0"
+              inputTestId="textInput"
+              id="reweighWeight"
+              lazy={false} // immediate masking evaluation
+              label="Reweigh Weight"
+              mask={Number}
+              name="reweighWeight"
+              scale={0} // digits after point, 0 for integers
+              signed={false} // disallow negative
+              thousandsSeparator=","
+            >
+              {' '}
+              lbs
+            </MaskedTextField>
+            <Label htmlFor="remarks">Remarks</Label>
+            <Textarea
+              data-testid="remarks"
+              id="reweighRemarks"
+              maxLength={500}
+              onChange={handleChange}
+              placeholder=""
+              onBlur={() => setTouched({ reweighRemarks: true }, false)}
+              value={values.reweighRemarks}
+            />
+          </SectionWrapper>
+
+          <WizardNavigation
+            editMode
+            className={formStyles.formActions}
+            aria-label="Update Reweigh"
+            type="submit"
+            onCancelClick={handleClose}
+            onNextClick={handleSubmit}
+          />
+        </>
+      )}
+    </Formik>
+  );
+};
+
+// disableNext={isSubmitting || !isValid}
+ReweighForm.propTypes = {
+  handleSubmit: func.isRequired,
+  handleClose: func.isRequired,
 };
 
 export default ReweighForm;
