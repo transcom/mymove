4f9466759d5079ffe319d8713785095a87e0046a
Andrew Hobson <ahobson@truss.works>
2021-11-22 17:20:59 +0000

    Update db telemetry to use better otel practices

--- pkg/telemetry/db.go
+++ pkg/telemetry/db.go
@@ -5,6 +5,8 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/db/stats"
@@ -26,6 +28,8 @@ const dbPoolIdleDesc = "The number of connections currently in use"
 const dbWaitDurationName = "db.waitduration"
 const dbWaitDurationDesc = "The total time blocked waiting for a new connection"
 
+const dbTelemetryVersion = "0.1"
+
 // RegisterDBStatsObserver creates a custom metric that is updated
 // automatically using an observer
 func RegisterDBStatsObserver(appCtx appcontext.AppContext, config *Config) {
@@ -33,37 +37,51 @@ func RegisterDBStatsObserver(appCtx appcontext.AppContext, config *Config) {
 		return
 	}
 
-	metric.Must(meter).NewInt64UpDownCounterObserver(
+	meterProvider := global.GetMeterProvider()
+
+	dbMeter := meterProvider.Meter("github.com/transcom/mymove/db",
+		metric.WithInstrumentationVersion(dbTelemetryVersion))
+
+	_, err := dbMeter.NewInt64UpDownCounterObserver(
 		dbPoolInUseName,
 		func(_ context.Context, result metric.Int64ObserverResult) {
-			dbStats, err := stats.DBStats(appCtx)
-			if err == nil {
+			dbStats, dberr := stats.DBStats(appCtx)
+			if dberr == nil {
 				result.Observe(int64(dbStats.InUse),
 					attribute.String(dbPoolInUseName, dbPoolInUseDesc))
 			}
 		},
 		metric.WithDescription(dbPoolInUseDesc))
+	if err != nil {
+		appCtx.Logger().Fatal("Failed to start db instrumentation", zap.Error(err))
+	}
 
-	metric.Must(meter).NewInt64UpDownCounterObserver(
+	_, err = dbMeter.NewInt64UpDownCounterObserver(
 		dbPoolIdleName,
 		func(_ context.Context, result metric.Int64ObserverResult) {
-			dbStats, err := stats.DBStats(appCtx)
-			if err == nil {
+			dbStats, dberr := stats.DBStats(appCtx)
+			if dberr == nil {
 				result.Observe(int64(dbStats.Idle),
 					attribute.String(dbPoolIdleName, dbPoolIdleDesc))
 			}
 		},
 		metric.WithDescription(dbPoolIdleDesc))
+	if err != nil {
+		appCtx.Logger().Fatal("Failed to start db instrumentation", zap.Error(err))
+	}
 
-	metric.Must(meter).NewInt64UpDownCounterObserver(
+	_, err = dbMeter.NewInt64UpDownCounterObserver(
 		dbWaitDurationName,
 		func(_ context.Context, result metric.Int64ObserverResult) {
-			dbStats, err := stats.DBStats(appCtx)
-			if err == nil {
+			dbStats, dberr := stats.DBStats(appCtx)
+			if dberr == nil {
 				result.Observe(int64(dbStats.WaitDuration),
 					attribute.String(dbWaitDurationName, dbWaitDurationDesc))
 			}
 		},
 		metric.WithDescription(dbWaitDurationDesc))
+	if err != nil {
+		appCtx.Logger().Fatal("Failed to start db instrumentation", zap.Error(err))
+	}
 
 }
--- pkg/telemetry/init.go
+++ pkg/telemetry/init.go
@@ -43,13 +43,13 @@ type Config struct {
 
 const (
 	defaultCollectSeconds = 30
-	tracerName            = "io.opentelemetry.traces.milmove"
-	metricName            = "io.opentelemetry.metrics.milmove"
+	tracerName            = "github.com/transcom/mymove"
+	//	metricName            = "github.com/transcom/mymove"
 )
 
 var (
 	tracer trace.Tracer
-	meter  metric.Meter
+	// meter  metric.Meter
 )
 
 // Init currently the target for distributed tracing / opentelemetry is
@@ -176,7 +176,7 @@ func Init(logger *zap.Logger, config *Config) (shutdown func()) {
 	}
 
 	tracer = otel.Tracer(tracerName)
-	meter = global.Meter(metricName)
+	//meter = global.Meter(metricName)
 
 	return shutdown
 }
