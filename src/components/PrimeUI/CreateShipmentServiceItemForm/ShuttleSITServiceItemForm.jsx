3d1bcf315e67da01cd1b4b4b8341e5c0157bbd82
Donald Thai <donald@truss.works>
2021-11-23 16:25:30 -0800

    clean up shuttle form to submit input form data

--- src/components/PrimeUI/CreateShipmentServiceItemForm/ShuttleSITServiceItemForm.jsx
+++ src/components/PrimeUI/CreateShipmentServiceItemForm/ShuttleSITServiceItemForm.jsx
@@ -4,75 +4,28 @@ import { Button } from '@trussworks/react-uswds';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { formatDateForSwagger } from '../../../shared/dates';
-// import { formatAddressForPrimeAPI } from '../../../utils/formatters';
-
 import { Form } from 'components/form/Form';
 import TextField from 'components/form/fields/TextField';
 import { ShipmentShape } from 'types/shipment';
+import { DropdownInput } from 'components/form/fields';
+import { shuttleServiceItemCodeOptions, createServiceItemModelTypes } from 'constants/prime';
+import MaskedTextField from 'components/form/fields/MaskedTextField';
 
-const shuttleSITValidationSchema = Yup.object().shape({
-  /*
-  firstAvailableDeliveryDate1: Yup.date()
-    .typeError('Enter a complete date in DD MMM YYYY format (day, month, year).')
-    .required('Required'),
-  timeMilitary1: Yup.string()
-    .matches(/^(\d{4}Z)$/, 'Must be a valid military time (e.g. 1400Z)')
-    .required('Required'),
-  firstAvailableDeliveryDate2: Yup.date()
-    .typeError('Enter a complete date in DD MMM YYYY format (day, month, year).')
-    .required('Required'),
-  timeMilitary2: Yup.string()
-    .matches(/^(\d{4}Z)$/, 'Must be a valid military time (e.g. 1400Z)')
-    .required('Required'),
-  sitEntryDate: Yup.date()
-    .typeError('Enter a complete date in DD MMM YYYY format (day, month, year).')
-    .required('Required'),
-  sitDepartureDate: Yup.date().typeError('Enter a complete date in DD MMM YYYY format (day, month, year).'),
-  sitDestinationFinalAddress: addressSchema,
-  */
-  reason: '',
-});
+const shuttleSITValidationSchema = Yup.object().shape({});
 
 const ShuttleSITServiceItemForm = ({ shipment, submission }) => {
   const initialValues = {
     moveTaskOrderID: shipment.moveTaskOrderID,
     mtoShipmentID: shipment.id,
-    modelType: 'MTOServiceItemShuttle',
-    // TODO: This Model type has two different `reServiceCode` associated with
-    // it. This needs a Dropdown element associated with this form beyond what
-    // was borrowed from the DestinationSITServiceItemForm that inspired this
-    // file.
-    reServiceCode: ['DOSHUT', 'DDSHUT'],
+    modelType: createServiceItemModelTypes.MTOServiceItemShuttle,
   };
 
   const onSubmit = (values) => {
-    const {
-      firstAvailableDeliveryDate1,
-      /*
-      firstAvailableDeliveryDate2,
-      sitEntryDate,
-      sitDepartureDate,
-      sitDestinationFinalAddress,
-      */
-      ...serviceItemValues
-    } = values;
-    /*
-    firstAvailableDeliveryDate1: formatDateForSwagger(firstAvailableDeliveryDate1),
-    firstAvailableDeliveryDate2: formatDateForSwagger(firstAvailableDeliveryDate2),
-    sitEntryDate: formatDateForSwagger(sitEntryDate),
-    sitDepartureDate: sitDepartureDate ? formatDateForSwagger(sitDepartureDate) : null,
-    sitDestinationFinalAddress: sitDestinationFinalAddress.streetAddress1
-    ? formatAddressForPrimeAPI(sitDestinationFinalAddress)
-    : null,
-    ...serviceItemValues,
-    */
+    const { estimatedWeight, actualWeight, ...otherFields } = values;
     const body = {
-      reServiceCode: 'DOSHUT',
-      reason: '',
-      estimatedWeight: 0,
-      actualWeight: 0,
-      ...serviceItemValues,
+      estimatedWeight: Number.parseInt(estimatedWeight, 10),
+      actualWeight: Number.parseInt(actualWeight, 10),
+      ...otherFields,
     };
     submission({ body });
   };
@@ -80,13 +33,36 @@ const ShuttleSITServiceItemForm = ({ shipment, submission }) => {
   return (
     <Formik initialValues={initialValues} validationSchema={shuttleSITValidationSchema} onSubmit={onSubmit}>
       <Form>
-        <input type="hidden" name="moveTaskOrderID" />
-        <input type="hidden" name="mtoShipmentID" />
-        <input type="hidden" name="modelType" />
-        <input type="hidden" name="reServiceCode" />
+        <DropdownInput
+          label="Service Item Code"
+          name="reServiceCode"
+          id="reServiceCode"
+          required
+          options={shuttleServiceItemCodeOptions}
+        />
         <TextField name="reason" id="reason" label="Reason" />
-        <TextField name="estimatedWeight" id="estimatedWeight" label="Estimated Weight" />
-        <TextField name="actualWeight" id="actualWeight" label="Actual Weight" />
+        <MaskedTextField
+          data-testid="estimatedWeightInput"
+          defaultValue="0"
+          name="estimatedWeight"
+          label="Estimated weight (lbs)"
+          id="estimatedWeightInput"
+          mask={Number}
+          scale={0} // digits after point, 0 for integers
+          thousandsSeparator=","
+          lazy={false} // immediate masking evaluation
+        />
+        <MaskedTextField
+          data-testid="actualWeightInput"
+          defaultValue="0"
+          name="actualWeight"
+          label="Actual weight (lbs)"
+          id="actualWeightInput"
+          mask={Number}
+          scale={0} // digits after point, 0 for integers
+          thousandsSeparator=","
+          lazy={false} // immediate masking evaluation
+        />
         <Button type="submit">Create service item</Button>
       </Form>
     </Formik>
