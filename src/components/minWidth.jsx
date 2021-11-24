26531240c4c8b498d9bea5a737eba03eb5c0099c
Shauna Keating <shauna@truss.works>
2021-11-24 19:58:27 +0000

    removes superfluous aria form role from html form element

--- src/components/minWidth.jsx
+++ src/components/minWidth.jsx
@@ -2,11 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const minWidth = ({ children }) => (
-  <div style="min-width:1240px; overflow-x:scroll">
-    {children}
-  </div>
-);
+const minWidth = ({ children }) => <div style={{ minWidth: '1240px', overflowX: 'scroll' }}>{children}</div>;
 
 minWidth.propTypes = {
   children: PropTypes.node.isRequired,
