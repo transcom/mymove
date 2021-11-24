1326e23a7ffd56dec6051961c029f26faaa54297
Reggie Riser <reggie@truss.works>
2021-11-24 17:06:29 +0000

    Fixed failing client test

--- src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
+++ src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
@@ -39,7 +39,7 @@ describe('Shipment Service Items Table', () => {
     expect(wrapper.find('table.serviceItemsTable').exists()).toBe(true);
     expect(wrapper.find('h4').text()).toEqual('Service items for this shipment 6 items');
     expect(wrapper.find('th').text()).toEqual('Service item');
-    expect(wrapper.find('td').length).toBe(6);
+    expect(wrapper.find('td').length).toBe(5);
     expect(wrapper.find('td').at(0).text()).toEqual('Domestic linehaul');
     expect(wrapper.find('td').at(1).text()).toEqual('Fuel surcharge');
     expect(wrapper.find('td').at(2).text()).toEqual('Domestic origin price');
