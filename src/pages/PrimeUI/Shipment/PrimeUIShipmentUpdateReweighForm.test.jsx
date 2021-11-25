6d1fb2780fd6be534f0527c5db0773cc01228a4a
Yan Zhao <yan.zhao@truss.works>
2021-11-25 01:52:46 +0000

    Implement tests for the reweigh form

---
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweighForm.test.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import PrimeUIShipmentUpdateReweighForm from './PrimeUIShipmentUpdateReweighForm';
+
+describe('PrimeUIShipmentUpdateReweighForm', () => {
+  const testProps = {
+    initialValues: {
+      reweighWeight: '0',
+      reweighRemarks: '',
+    },
+    onSubmit: jest.fn().mockImplementation(() => Promise.resolve()),
+    handleClose: jest.fn(),
+  };
+
+  it('renders the form', async () => {
+    render(<PrimeUIShipmentUpdateReweighForm {...testProps} />);
+
+    expect(await screen.findByLabelText('Reweigh Weight')).toBeInstanceOf(HTMLInputElement);
+    expect(screen.getByTestId('remarks')).toBeInstanceOf(HTMLTextAreaElement);
+  });
+
+  it('submits the form when valid', async () => {
+    render(<PrimeUIShipmentUpdateReweighForm {...testProps} />);
+    const submitBtn = await screen.findByRole('button', { name: 'Save' });
+
+    expect(submitBtn).toBeDisabled();
+
+    const reweighInput = screen.getByLabelText('Reweigh Weight');
+    userEvent.clear(reweighInput);
+    userEvent.type(reweighInput, '123');
+    userEvent.type(screen.getByTestId('remarks'), 'test');
+
+    await waitFor(() => {
+      expect(submitBtn).not.toBeDisabled();
+    });
+    userEvent.click(submitBtn);
+
+    await waitFor(() => {
+      expect(testProps.onSubmit).toHaveBeenCalled();
+    });
+  });
+
+  it('shows an error message when the reweigh weight is 0', async () => {
+    render(<PrimeUIShipmentUpdateReweighForm {...testProps} />);
+
+    const alert = await screen.findByRole('alert');
+
+    expect(alert).toHaveTextContent('Authorized weight must be greater than or equal to 1');
+  });
+
+  it('implements the handleClose handler when the Cancel button is clicked', async () => {
+    render(<PrimeUIShipmentUpdateReweighForm {...testProps} />);
+    const cancelButton = await screen.findByRole('button', { name: 'Cancel' });
+
+    userEvent.click(cancelButton);
+
+    await waitFor(() => {
+      expect(testProps.handleClose).toHaveBeenCalled();
+    });
+  });
+});
