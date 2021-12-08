e7fc6d71615b2c305928393d79b61b6ff56b1255
Yan Zhao <yan.zhao@truss.works>
2021-12-08 01:49:41 +0000

    Convert tests from enzyme to RTL

--- src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
+++ src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
@@ -1,61 +1,71 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import ShipmentServiceItemsTable from './ShipmentServiceItemsTable';
 
 import { SHIPMENT_OPTIONS } from 'shared/constants';
 
 describe('Shipment Service Items Table', () => {
-  it('renders the hhg longhaul shipment type with service items', () => {
-    const wrapper = shallow(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.HHG_LONGHAUL_DOMESTIC} />);
-    expect(wrapper.find('table.serviceItemsTable').exists()).toBe(true);
-    expect(wrapper.find('h4').text()).toEqual('Service items for this shipment 6 items');
-    expect(wrapper.find('th').text()).toEqual('Service item');
-    expect(wrapper.find('td').length).toBe(6);
-    expect(wrapper.find('td').at(0).text()).toEqual('Domestic linehaul');
-    expect(wrapper.find('td').at(1).text()).toEqual('Fuel surcharge');
-    expect(wrapper.find('td').at(2).text()).toEqual('Domestic origin price');
-    expect(wrapper.find('td').at(3).text()).toEqual('Domestic destination price');
-    expect(wrapper.find('td').at(4).text()).toEqual('Domestic packing');
-    expect(wrapper.find('td').at(5).text()).toEqual('Domestic unpacking');
+  describe('renders the hhg longhaul shipment type with service items', () => {
+    it.each([
+      ['Domestic linehaul'],
+      ['Fuel surcharge'],
+      ['Domestic origin price'],
+      ['Domestic destination price'],
+      ['Domestic packing'],
+      ['Domestic unpacking'],
+    ])('expects %s to be in the document', async (serviceItem) => {
+      render(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.HHG_LONGHAUL_DOMESTIC} />);
+      expect(
+        await screen.findByRole('heading', { name: 'Service items for this shipment 6 items', level: 4 }),
+      ).toBeInTheDocument();
+      expect(screen.getByText(serviceItem)).toBeInTheDocument();
+    });
   });
 
-  it('renders the hhg shorthaul shipment type with service items', () => {
-    const wrapper = shallow(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.HHG_SHORTHAUL_DOMESTIC} />);
-    expect(wrapper.find('table.serviceItemsTable').exists()).toBe(true);
-    expect(wrapper.find('h4').text()).toEqual('Service items for this shipment 6 items');
-    expect(wrapper.find('th').text()).toEqual('Service item');
-    expect(wrapper.find('td').length).toBe(6);
-    expect(wrapper.find('td').at(0).text()).toEqual('Domestic shorthaul');
-    expect(wrapper.find('td').at(1).text()).toEqual('Fuel surcharge');
-    expect(wrapper.find('td').at(2).text()).toEqual('Domestic origin price');
-    expect(wrapper.find('td').at(3).text()).toEqual('Domestic destination price');
-    expect(wrapper.find('td').at(4).text()).toEqual('Domestic packing');
-    expect(wrapper.find('td').at(5).text()).toEqual('Domestic unpacking');
+  describe('renders the hhg shorthaul shipment type with service items', () => {
+    it.each([
+      ['Domestic shorthaul'],
+      ['Fuel surcharge'],
+      ['Domestic origin price'],
+      ['Domestic destination price'],
+      ['Domestic packing'],
+      ['Domestic unpacking'],
+    ])('expects %s to be in the document', async (serviceItem) => {
+      render(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.HHG_SHORTHAUL_DOMESTIC} />);
+      expect(
+        await screen.findByRole('heading', { name: 'Service items for this shipment 6 items', level: 4 }),
+      ).toBeInTheDocument();
+      expect(screen.getByText(serviceItem)).toBeInTheDocument();
+    });
   });
 
-  it('renders the nts shipment type with service items', () => {
-    const wrapper = shallow(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.NTS} />);
-    expect(wrapper.find('table.serviceItemsTable').exists()).toBe(true);
-    expect(wrapper.find('h4').text()).toEqual('Service items for this shipment 4 items');
-    expect(wrapper.find('th').text()).toEqual('Service item');
-    expect(wrapper.find('td').length).toBe(4);
-    expect(wrapper.find('td').at(0).text()).toEqual('Domestic linehaul');
-    expect(wrapper.find('td').at(1).text()).toEqual('Fuel surcharge');
-    expect(wrapper.find('td').at(2).text()).toEqual('Domestic origin price');
-    expect(wrapper.find('td').at(3).text()).toEqual('Domestic NTS packing');
+  describe('renders the nts shipment type with service items', () => {
+    it.each([['Domestic linehaul'], ['Fuel surcharge'], ['Domestic origin price'], ['Domestic NTS packing']])(
+      'expects %s to be in the document',
+      async (serviceItem) => {
+        render(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.NTS} />);
+        expect(
+          await screen.findByRole('heading', { name: 'Service items for this shipment 4 items', level: 4 }),
+        ).toBeInTheDocument();
+        expect(screen.getByText(serviceItem)).toBeInTheDocument();
+      },
+    );
   });
 
-  it('renders the nts release shipment type with service items', () => {
-    const wrapper = shallow(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.NTSR} />);
-    expect(wrapper.find('table.serviceItemsTable').exists()).toBe(true);
-    expect(wrapper.find('h4').text()).toEqual('Service items for this shipment 5 items');
-    expect(wrapper.find('th').text()).toEqual('Service item');
-    expect(wrapper.find('td').length).toBe(5);
-    expect(wrapper.find('td').at(0).text()).toEqual('Domestic linehaul');
-    expect(wrapper.find('td').at(1).text()).toEqual('Fuel surcharge');
-    expect(wrapper.find('td').at(2).text()).toEqual('Domestic origin price');
-    expect(wrapper.find('td').at(3).text()).toEqual('Domestic destination price');
-    expect(wrapper.find('td').at(4).text()).toEqual('Domestic unpacking');
+  describe('renders the nts release shipment type with service items', () => {
+    it.each([
+      ['Domestic linehaul'],
+      ['Fuel surcharge'],
+      ['Domestic origin price'],
+      ['Domestic destination price'],
+      ['Domestic unpacking'],
+    ])('expects %s to be in the document', async (serviceItem) => {
+      render(<ShipmentServiceItemsTable shipmentType={SHIPMENT_OPTIONS.NTSR} />);
+      expect(
+        await screen.findByRole('heading', { name: 'Service items for this shipment 5 items', level: 4 }),
+      ).toBeInTheDocument();
+      expect(screen.getByText(serviceItem)).toBeInTheDocument();
+    });
   });
 });
