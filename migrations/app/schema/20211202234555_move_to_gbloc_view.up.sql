43c6e4f7ef1f7a302afaf999b3158c0d319802fb
Marty Boren <marty@truss.works>
2021-12-07 17:12:00 -0800

    Update PostalCodeToGBLOC for TOO queue
    There were a few goofs that I made when creating this table
    that I discovered when I tried to actually use it. Fortunately,
    since nothing uses it yet, I don't have to worry about backwards
    compatibility in this migration, so it's easy to fix it up.
    
    1. I used a singular name: postal_code_to_gbloc. Pop prefers plural
       names. This is not a big deal to work around, but it's easy to fix.
    2. Pop requires tables to have an id column. This is a big one, and
       I had to add an id to all 40k rows to fix it ;_;
    
    I'm also creating a view that will be used in the TOO queue query.
    It maps every move_id to a GBLOC based on the postal code of its first
    shipment. I couldn't figure out how to express this query in Pop,
    without writing raw SQL (which I couldn't do because the query needed
    to be composable), but we don't use views for this anywhere else, so it
    might be better to go with a less surprising approach.

---
+++ migrations/app/schema/20211202234555_move_to_gbloc_view.up.sql
@@ -0,0 +1,25 @@
+-- Pop prefers tables to have plural names, this table isn't used yet,
+-- so we might as well rename it now before we have to worry about backwards
+-- compatibility.
+alter table postal_code_to_gbloc rename to postal_code_to_gblocs;
+
+-- This view finds the GBLOC for the first shipment of each move
+CREATE VIEW move_to_gbloc AS
+SELECT DISTINCT ON (sh.move_id) sh.move_id AS move_id, pctg.gbloc AS gbloc
+FROM mto_shipments sh
+		 JOIN addresses a ON sh.pickup_address_id = a.id
+		 JOIN postal_code_to_gblocs pctg ON a.postal_code = pctg.postal_code
+		 JOIN moves m ON sh.move_id = m.id
+		 JOIN orders o ON m.orders_id = o.id
+ORDER BY sh.move_id, sh.created_at;
+
+-- Add id column to postal_code_to_gblocs. This is required by Pop.
+alter table postal_code_to_gblocs
+	add column id uuid;
+
+-- need to circle back and update this with hardcoded IDs
+update postal_code_to_gbloc
+set id = uuid_generate_v4();
+
+alter table postal_code_to_gblocs drop constraint postal_code_to_gbloc_pkey;
+alter table postal_code_to_gblocs add primary key (id);
