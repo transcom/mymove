a512c0eed0ac293d50187c54914c55a33c2118af
hannaliebl <hannaliebl@users.noreply.github.com>
2021-12-13 18:59:35 +0000

    Use screen in tests

--- src/components/ButtonDropdown/ButtonDropdown.test.jsx
+++ src/components/ButtonDropdown/ButtonDropdown.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ButtonDropdown from './ButtonDropdown';
 
 describe('ButtonDropdown component', () => {
   it('renders the USWDS dropdown component without errors', () => {
-    const { queryByTestId } = render(
+    render(
       <ButtonDropdown onChange={() => {}}>
         <option>- Select - </option>
         <option value="value1">Option A</option>
@@ -15,12 +15,12 @@ describe('ButtonDropdown component', () => {
       </ButtonDropdown>,
     );
 
-    expect(queryByTestId('dropdown')).toBeInTheDocument();
+    expect(screen.queryByTestId('dropdown')).toBeInTheDocument();
   });
 
   it('calls onChange prop on option selection', () => {
     const onChangeSpy = jest.fn();
-    const { getByRole } = render(
+    render(
       <ButtonDropdown onChange={onChangeSpy}>
         <option>- Select - </option>
         <option value="value1">Option A</option>
@@ -28,7 +28,7 @@ describe('ButtonDropdown component', () => {
         <option value="value3">Option C</option>
       </ButtonDropdown>,
     );
-    const dropdown = getByRole('combobox');
+    const dropdown = screen.getByRole('combobox');
     userEvent.selectOptions(dropdown, 'value1');
 
     expect(onChangeSpy).toHaveBeenCalled();
