e5192e0028511c131f73336ff5ba9e72e8996283
Marty Boren <marty@truss.works>
2021-12-14 21:21:06 +0000

    Add more detail to comment

--- pkg/models/move_to_gblocs.go
+++ pkg/models/move_to_gblocs.go
@@ -3,6 +3,8 @@ package models
 import "github.com/gofrs/uuid"
 
 // MoveToGBLOC represents the view that associates each move ID with a GBLOC based on the postal code of its first shipment.
+// This view is used to encapsulate query logic that was impossible to express with Pop.
+// It will be used for the TOO and TIO queues, but not for Services Counseling.
 type MoveToGBLOC struct {
 	MoveID uuid.UUID `db:"move_id"`
 	Move   Move      `belongs_to:"moves" fk_id:"move_id"`
