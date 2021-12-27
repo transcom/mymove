5b0cef4e1f6eb465ee021440c8b3be1628a0da1a
eamahanna <emahanna@truss.works>
2021-12-27 17:11:06 +0000

    Add permissions to view

--- migrations/app/schema/20211221182830_origin_duty_location_to_gbloc_view.up.sql
+++ migrations/app/schema/20211221182830_origin_duty_location_to_gbloc_view.up.sql
@@ -6,3 +6,5 @@ FROM moves m
 		 JOIN duty_stations ds on ds.id = ord.origin_duty_station_id
 		 JOIN addresses a ON a.id = ds.address_id
 		 JOIN postal_code_to_gblocs pctg ON a.postal_code = pctg.postal_code;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON origin_duty_location_to_gbloc TO crud;
