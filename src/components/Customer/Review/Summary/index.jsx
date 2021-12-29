593dc286558365ef935bb5ed896a3cf240ac2a62
pearl-truss <pearl@truss.works>
2021-12-29 20:50:32 +0000

    format the rank outside of return

--- src/components/Customer/Review/Summary/index.jsx
+++ src/components/Customer/Review/Summary/index.jsx
@@ -235,6 +235,8 @@ export class Summary extends Component {
     const thirdSectionHasContent =
       showMoveSetup || showPPMShipmentSummary || (isReviewPage && (mtoShipments.length > 0 || currentPPM));
 
+    const formattedRank = get(schemaRank['x-display-value'], get(serviceMember, 'rank'));
+
     return (
       <>
         {entitlementWarning && (
@@ -254,7 +256,7 @@ export class Summary extends Component {
             onEditClick={this.handleEditClick}
             lastName={serviceMember.last_name}
             postalCode={serviceMember.residential_address.postalCode}
-            rank={get(schemaRank['x-display-value'], get(serviceMember, 'rank'))}
+            rank={formattedRank}
             state={serviceMember.residential_address.state}
             streetAddress1={serviceMember.residential_address.streetAddress1}
             streetAddress2={serviceMember.residential_address.streetAddress2}
