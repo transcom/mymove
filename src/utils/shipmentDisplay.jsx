d215f6ebbd3c95ccdfd22d8ad3fa0234ce5072a3
Yan Zhao <yan.zhao@truss.works>
2022-01-04 01:50:12 +0000

    Add utils to help with retrieval of accounting codes
    - add tests to verify the utils were written correctly

--- src/utils/shipmentDisplay.jsx
+++ src/utils/shipmentDisplay.jsx
@@ -2,7 +2,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-import { shipmentOptionLabels } from 'shared/constants';
+import { LOA_TYPE, shipmentOptionLabels } from 'shared/constants';
 import { shipmentStatuses, shipmentModificationTypes } from 'constants/shipments';
 
 export function formatAddress(address) {
@@ -13,6 +13,28 @@ export function formatAddress(address) {
       {city ? `${city}, ${state} ${postalCode}` : postalCode}
     </>
   );
+}
+
+export function retrieveTAC(tacType, ordersLOA) {
+  switch (tacType) {
+    case LOA_TYPE.HHG:
+      return ordersLOA.tac;
+    case LOA_TYPE.NTS:
+      return ordersLOA.ntsTAC;
+    default:
+      return ordersLOA.tac;
+  }
+}
+
+export function retrieveSAC(sacType, ordersLOA) {
+  switch (sacType) {
+    case LOA_TYPE.HHG:
+      return ordersLOA.sac;
+    case LOA_TYPE.NTS:
+      return ordersLOA.ntsSAC;
+    default:
+      return ordersLOA.sac;
+  }
 }
 
 export function formatAccountingCode(accountingCode, accountingCodeType) {
