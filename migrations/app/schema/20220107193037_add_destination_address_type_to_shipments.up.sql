b7ce46917e680ffcbd087fc3234759579a69aeac
Christopher Hui <christopher.kam.hui@gmail.com>
2022-01-07 22:43:19 +0000

    update comments to describe the migration

--- migrations/app/schema/20220107193037_add_destination_address_type_to_shipments.up.sql
+++ migrations/app/schema/20220107193037_add_destination_address_type_to_shipments.up.sql
@@ -1,9 +1,11 @@
--- This address type is ONLY applicable for destination addresses for retirees and separatees
-CREATE TYPE address_type AS ENUM (
+-- Types of shipment locations for destination addresses
+CREATE TYPE destination_address_type AS ENUM (
 	'HOME_OF_RECORD',
 	'HOME_OF_SELECTION',
 	'PLACE_ENTERED_ACTIVE_DUTY',
 	'OTHER'
 );
 
-ALTER TABLE mto_shipments ADD COLUMN destination_address_type address_type;
+-- The destination_address_type column is used to determine the type of location
+-- destination addresseses are for retirees and separatees
+ALTER TABLE mto_shipments ADD COLUMN destination_address_type destination_address_type;
