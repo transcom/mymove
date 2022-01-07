043fb7e6dfaea10ff3daafcb6d188a95e0fadf49
Christopher Hui <christopher.kam.hui@gmail.com>
2022-01-07 19:42:33 +0000

    add destination address type column to shipments table

---
+++ migrations/app/schema/20220107193037_add_destination_address_type_to_shipments.up.sql
@@ -0,0 +1,8 @@
+CREATE TYPE address_type AS ENUM (
+	'HOME_OF_RECORD',
+	'HOME_OF_SELECTION',
+	'PLACE_ENTERED_ACTIVE_DUTY',
+	'OTHER'
+);
+
+ALTER TABLE mto_shipments ADD COLUMN destination_address_type address_type;
