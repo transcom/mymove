97c082cade7ec26e9985c3d7f3435f0ed4416bae
Marty Boren <marty@truss.works>
2022-01-14 17:23:29 +0000

    Use NULL instead of empty string
    In both this and the previous migration, I inserted some
    transportation_offices with empty strings for the hours and
    services fields.
    I updated the inserts in this migration, and added updates to
    fix the records from the last migration.

--- migrations/app/schema/20220113172826_create_new_duty_locations.up.sql
+++ migrations/app/schema/20220113172826_create_new_duty_locations.up.sql
@@ -1,8 +1,17 @@
+-- Clean up some empty strings that should have been NULL from the last migration
+UPDATE transportation_offices
+SET hours = NULL
+WHERE hours = '';
+
+UPDATE transportation_offices
+SET services = NULL
+WHERE services = '';
+
 -- NAVSUP FLC San Diego
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('8ea692be-424a-4355-b1ff-1c72e4bee245', '2623 LeHardy St.', 'Bldg. 3376', 'San Diego', 'CA', '92136', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('27002d34-e9ea-4ef5-a086-f23d07c4088c', 'NAVSUP FLC San Diego', 'LKNQ', '8ea692be-424a-4355-b1ff-1c72e4bee245', 0, 0, '', now(), now());
+  VALUES ('27002d34-e9ea-4ef5-a086-f23d07c4088c', 'NAVSUP FLC San Diego', 'LKNQ', '8ea692be-424a-4355-b1ff-1c72e4bee245', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('183019a0-5ed5-4f65-90ba-546ade3cf723', 'n/a', 'San Diego', 'CA', '92136', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -12,7 +21,7 @@ INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_of
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('9672422e-6918-48ff-be43-becd025a0e52', 'Counseling Office', 'End of Route 238', 'Yorktown', 'VA', '23690', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('4ed2762d-73bc-4c62-bea9-725c5c64cb62', 'USCG Training Center Yorktown', 'AGFM', '9672422e-6918-48ff-be43-becd025a0e52', 0, 0, '', now(), now());
+  VALUES ('4ed2762d-73bc-4c62-bea9-725c5c64cb62', 'USCG Training Center Yorktown', 'AGFM', '9672422e-6918-48ff-be43-becd025a0e52', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('e749736a-3b40-475b-b83e-6525f280f9b4', 'n/a', 'Yorktown', 'VA', '23690', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -34,7 +43,7 @@ INSERT INTO duty_station_names (id, name, duty_station_id, created_at, updated_a
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('3b11a8c4-5e23-4afe-b00b-edda2be5d56d', 'USCG Tracen', '1 Munro Ave', 'Cape May', 'NJ', '08204', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('7ac0f374-b97a-4b98-9878-eabef89adff9', 'USCG Training Center Cape May', 'AGFM', '3b11a8c4-5e23-4afe-b00b-edda2be5d56d', 0, 0, '', now(), now());
+  VALUES ('7ac0f374-b97a-4b98-9878-eabef89adff9', 'USCG Training Center Cape May', 'AGFM', '3b11a8c4-5e23-4afe-b00b-edda2be5d56d', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('f21af5e6-52a8-4328-9bb8-1f5a972ebd01', 'n/a', 'Cape May', 'NJ', '08204', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -44,7 +53,7 @@ INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_of
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('b7f7367d-d632-462a-ab7c-688687533e11', 'USCG Base Boston Personnel Service Division', '427 Commercial St', 'Boston', 'MA', '02109', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('cf1addea-a4f9-4173-8506-2bb82a064cb7', 'USCG Base Boston', 'AGFM', 'b7f7367d-d632-462a-ab7c-688687533e11', 0, 0, '', now(), now());
+  VALUES ('cf1addea-a4f9-4173-8506-2bb82a064cb7', 'USCG Base Boston', 'AGFM', 'b7f7367d-d632-462a-ab7c-688687533e11', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('9870776a-8df9-481c-84ad-34b8c579c510', 'n/a', 'Boston', 'MA', '02109', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -54,7 +63,7 @@ INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_of
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('71e8ae89-de1b-456f-8555-e3499ff869b3', '1050 Register St.', NULL, 'Charleston', 'SC', '29405', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('b429ec35-eb8b-4c35-979b-e96cb9a9cbf7', 'USCG Base Charleston', 'AGFM', '71e8ae89-de1b-456f-8555-e3499ff869b3', 0, 0, '', now(), now());
+  VALUES ('b429ec35-eb8b-4c35-979b-e96cb9a9cbf7', 'USCG Base Charleston', 'AGFM', '71e8ae89-de1b-456f-8555-e3499ff869b3', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('5c2f4497-bc71-41ad-a5a1-f4be491df4cb', 'n/a', 'Charleston', 'SC', '29405', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -76,7 +85,7 @@ INSERT INTO duty_station_names (id, name, duty_station_id, created_at, updated_a
 INSERT INTO addresses (id, street_address_1, street_address_2, city, state, postal_code, created_at, updated_at)
   VALUES ('52b86914-a467-4d0d-8ff2-4d4cf9958915', '1001 South Seaside Ave', NULL, 'San Pedro', 'CA', '90731', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('f3ce3548-fb03-4e2e-b986-386a1adf2400', 'USCG Base Los Angeles/Long Beach', 'LKNQ', '52b86914-a467-4d0d-8ff2-4d4cf9958915', 0, 0, '', now(), now());
+  VALUES ('f3ce3548-fb03-4e2e-b986-386a1adf2400', 'USCG Base Los Angeles/Long Beach', 'LKNQ', '52b86914-a467-4d0d-8ff2-4d4cf9958915', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('e11ee632-869f-4eba-8d6a-01a4524668e5', 'n/a', 'San Pedro', 'CA', '90731', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
@@ -116,7 +125,7 @@ INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_of
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('9b5006fa-8812-4f9b-a73f-1e184f29719c', 'n/a', 'Seal Beach', 'CA', '90740', now(), now());
 INSERT INTO transportation_offices (id, name, gbloc, address_id, latitude, longitude, hours, created_at, updated_at)
-  VALUES ('a0de70b3-e7e9-4e47-8c7e-3f9f7ba4c5ab', 'NAVSUP FLC Seal Beach', 'LKNQ', '9b5006fa-8812-4f9b-a73f-1e184f29719c', 0, 0, '', now(), now());
+  VALUES ('a0de70b3-e7e9-4e47-8c7e-3f9f7ba4c5ab', 'NAVSUP FLC Seal Beach', 'LKNQ', '9b5006fa-8812-4f9b-a73f-1e184f29719c', 0, 0, NULL, now(), now());
 INSERT INTO addresses (id, street_address_1, city, state, postal_code, created_at, updated_at)
   VALUES ('f7c383a5-8faf-47db-8ea8-c2e356d7ba90', 'n/a', 'Seal Beach', 'CA', '90740', now(), now());
 INSERT INTO duty_locations (id, name, affiliation, address_id, transportation_office_id, provides_services_counseling, updated_at, created_at)
