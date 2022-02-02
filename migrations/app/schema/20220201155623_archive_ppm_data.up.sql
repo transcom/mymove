f8f2ea98de71ec412c6d6c9dadf91b68cfc2583b
pearl-truss <pearl@truss.works>
2022-02-02 20:22:49 +0000

    created remaining tables and inserts

--- migrations/app/schema/20220201155623_archive_ppm_data.up.sql
+++ migrations/app/schema/20220201155623_archive_ppm_data.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE archived_personally_procured_moves (
     id uuid PRIMARY KEY,
+    size varchar(255),
     move_id uuid REFERENCES moves,
     weight_estimate INT,
 	created_at timestamp without time zone,
@@ -41,6 +42,7 @@ CREATE TABLE archived_move_documents(
     document_id uuid REFERENCES documents,
     type varchar(255),
     status varchar(255),
+    move_document_type varchar(255),
     notes text,
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
@@ -57,7 +59,7 @@ CREATE INDEX ON archived_move_documents (personally_procured_move_id);
 CREATE TABLE archived_signed_certifications(
     id uuid PRIMARY KEY,
     submitting_user_id uuid REFERENCES users,
-    signed_certificate_move_id uuid REFERENCES moves,
+    move_id uuid REFERENCES moves,
     certification_text text,
     signature text,
     date timestamp,
@@ -111,81 +113,83 @@ CREATE INDEX ON archived_moving_expense_documents (id);
 CREATE INDEX ON archived_moving_expense_documents (move_document_id);
 
 INSERT INTO archived_personally_procured_moves (
-id, move_id, weight_estimate,
+id, size, move_id, weight_estimate,
 created_at, updated_at,
 pickup_postal_code, additional_pickup_postal_code,
 destination_postal_code, days_in_storage, has_additional_postal_code,
 advance_id, has_requested_advance, advance_worksheet_id,
-estimated_storage_reimbursement, mileage, planned_sit_max, sit_max, ppm_status,
+estimated_storage_reimbursement, mileage, planned_sit_max, sit_max, status,
 incentive_estimate_max, incentive_estimate_min,
 net_weight, original_move_date, actual_move_date,
 total_sit_cost, has_sit, submit_date, reviewed_date, approve_date,
 has_pro_gear, has_pro_gear_over_thousand)
-SELECT ppm.id, ppm.move_id, ppm.weight_estimate,
-ppm.created_at, ppm.updated_at,
-ppm.pickup_postal_code,
-ppm.additional_pickup_postal_code,
-ppm.destination_postal_code, ppm.days_in_storage,
-ppm.has_additional_postal_code, ppm.advance_id,
-ppm.has_requested_advance, ppm.advance_worksheet_id,
-ppm.estimated_storage_reimbursement, ppm.mileage,
-ppm.planned_sit_max, ppm.sit_max,
-ppm.status,
-ppm.incentive_estimate_max,
-ppm.incentive_estimate_min,
-ppm.net_weight, ppm.original_move_date,
-ppm.actual_move_date, ppm.total_sit_cost,
-ppm.has_sit, ppm.submit_date,
-ppm.reviewed_date, ppm.approve_date,
-ppm.has_pro_gear, ppm.has_pro_gear_over_thousand
-FROM personally_procured_moves ppm;
+SELECT id, size, move_id, weight_estimate,
+created_at, updated_at,
+pickup_postal_code,
+additional_pickup_postal_code,
+destination_postal_code, days_in_storage,
+has_additional_postal_code, advance_id,
+has_requested_advance, advance_worksheet_id,
+estimated_storage_reimbursement, mileage,
+planned_sit_max, sit_max,
+status,
+incentive_estimate_max,
+incentive_estimate_min,
+net_weight, original_move_date,
+actual_move_date, total_sit_cost,
+has_sit, submit_date,
+reviewed_date, approve_date,
+has_pro_gear, has_pro_gear_over_thousand
+FROM personally_procured_moves;
 
 INSERT INTO archived_signed_certifications(
 id, submitting_user_id, move_id,
 certification_text, signature, date, created_at,
 updated_at, certification_type, personally_procured_move_id)
-SELECT sc.id, sc.submitting_user_id, sc.move_id,
-sc.certification_text, sc.signature, sc.date,
-sc.created_at, sc.updated_at, sc.certification_type,
-sc.personally_procured_move_id
-FROM signed_certifcations sc;
+SELECT id, submitting_user_id, move_id,
+certification_text, signature, date,
+created_at, updated_at, certification_type,
+personally_procured_move_id
+FROM signed_certifications;
 
+INSERT INTO archived_move_documents(
+id, move_id, document_id,
+move_document_type, status, notes,
+updated_at, created_at,
+title, personally_procured_move_id, deleted_at)
+SELECT id, move_id,
+document_id, move_document_type, status, notes,
+updated_at, created_at,
+title, personally_procured_move_id, deleted_at
+FROM move_documents;
 
+INSERT INTO archived_weight_ticket_set_documents(
+id, weight_ticket_set_type, vehicle_nickname,
+move_document_id, empty_weight,
+empty_weight_ticket_missing, full_weight, full_weight_ticket_missing, weight_ticket_date,
+created_at, updated_at, trailer_ownership_missing,
+deleted_at, vehicle_make, vehicle_model
+)
+SELECT id, weight_ticket_set_type,
+vehicle_nickname, move_document_id,
+empty_weight,
+empty_weight_ticket_missing, full_weight,
+full_weight_ticket_missing, weight_ticket_date,
+created_at, updated_at, trailer_ownership_missing,
+deleted_at, vehicle_make, vehicle_model
+FROM weight_ticket_set_documents;
 
--- move_document_id, move_document_move_id, document_id,
--- move_document_type, move_document_status, notes,
--- move_document_updated_at, move_document_created_at,
--- title, move_document_ppm_id, move_document_deleted_at,
--- weight_ticket_set_document_id, weight_ticket_set_type, vehicle_nickname,
--- weight_ticket_set_move_document_id, empty_weight,
--- empty_weight_ticket_missing, full_weight, full_weight_ticket_missing, weight_ticket_date,
--- weight_ticket_set_document_created_at, weight_ticket_set_document_updated_at, trailer_ownership_missing,
--- weight_ticket_set_document_deleted_at, vehicle_make, vehicle_model,
--- moving_expense_document_id, moving_expense_document_move_document_id, moving_expense_type,
--- moving_expense_document_created_at, moving_expense_document_updated_at, requested_amount_cents,
--- payment_method, receipt_missing, storage_start_date, storage_end_date, moving_expense_document_deleted_at)
--- SELECT
-
--- md.id AS move_document_id, md.move_id AS move_document_move_id,
--- md.document_id, md.move_document_type, md.status AS move_document_status, md.notes,
--- md.updated_at AS move_document_updated_at, md.created_at AS move_document_created_at,
--- md.title, md.personally_procured_move_id AS move_document_ppm_id,
--- md.deleted_at AS move_document_deleted_at,
--- wtsd.id AS weight_ticket_set_document_id, wtsd.weight_ticket_set_type,
--- wtsd.vehicle_nickname, wtsd.move_document_id AS weight_ticket_set_move_document_id,
--- wtsd.empty_weight,
--- wtsd.empty_weight_ticket_missing, wtsd.full_weight,
--- wtsd.full_weight_ticket_missing, wtsd.weight_ticket_date,
--- wtsd.created_at AS weight_ticket_set_document_created_at,
--- wtsd.updated_at AS weight_ticket_set_document_updated_at, wtsd.trailer_ownership_missing,
--- wtsd.deleted_at AS weight_ticket_set_document_deleted_at,
--- wtsd.vehicle_make, wtsd.vehicle_model,
--- med.id AS moving_expense_document_id,
--- med.move_document_id AS moving_expense_document_move_document_id,
--- med.moving_expense_type,
--- med.created_at AS moving_expense_document_created_at,
--- med.updated_at AS moving_expense_document_updated_at,
--- med.requested_amount_cents,
--- med.payment_method, med.receipt_missing,
--- med.storage_start_date, med.storage_end_date,
--- med.deleted_at AS moving_expense_document_deleted_at
+INSERT INTO archived_moving_expense_documents(
+id, move_document_id, moving_expense_type,
+created_at, updated_at, requested_amount_cents,
+payment_method, receipt_missing, storage_start_date,
+storage_end_date, deleted_at
+)
+SELECT id, move_document_id,
+moving_expense_type,
+created_at, updated_at,
+requested_amount_cents,
+payment_method, receipt_missing,
+storage_start_date, storage_end_date,
+deleted_at
+from moving_expense_documents;
