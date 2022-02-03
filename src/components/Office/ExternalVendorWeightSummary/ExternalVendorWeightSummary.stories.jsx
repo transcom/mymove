a41e5812a18e6ac6935107d0f9b7c946134b5ac1
hannaliebl <hannaliebl@users.noreply.github.com>
2022-02-03 20:32:34 +0000

    Update component based on the requirement that NTS shipments do not contain and should not display shipment weights but should count toward external vendor shipment total

--- src/components/Office/ExternalVendorWeightSummary/ExternalVendorWeightSummary.stories.jsx
+++ src/components/Office/ExternalVendorWeightSummary/ExternalVendorWeightSummary.stories.jsx
@@ -16,27 +16,83 @@ export default {
   ],
 };
 
-export const WithMultipleShipments = () => (
+export const WithMultipleNTSRShipments = () => (
   <ExternalVendorWeightSummary
     shipments={[
       {
         ntsRecordedWeight: 1000,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
       },
       {
         ntsRecordedWeight: 2000,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
       },
       {
         ntsRecordedWeight: 1500,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
       },
     ]}
   />
 );
 
-export const WithOneShipment = () => (
+export const WithMultipleNTSShipments = () => (
+  // NTS shipments from external vendors don't have weights
+  <ExternalVendorWeightSummary
+    shipments={[
+      {
+        shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      },
+      {
+        shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      },
+      {
+        shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      },
+    ]}
+  />
+);
+
+export const WithMultipleShipmentsOfBothTypes = () => (
   <ExternalVendorWeightSummary
     shipments={[
       {
         ntsRecordedWeight: 1000,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
+      },
+      {
+        ntsRecordedWeight: 2000,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
+      },
+      {
+        shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      },
+      {
+        ntsRecordedWeight: 1500,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
+      },
+      {
+        shipmentType: 'HHG_INTO_NTS_DOMESTIC',
+      },
+    ]}
+  />
+);
+
+export const WithOneNTSShipment = () => (
+  <ExternalVendorWeightSummary
+    shipments={[
+      {
+        NTS: 'HHG_INTO_NTS_DOMESTIC',
+      },
+    ]}
+  />
+);
+
+export const WithOneNTSRShipment = () => (
+  <ExternalVendorWeightSummary
+    shipments={[
+      {
+        ntsRecordedWeight: 1500,
+        shipmentType: 'HHG_OUTOF_NTS_DOMESTIC',
       },
     ]}
   />
--- src/components/Office/ExternalVendorWeightSummary/ExternalVendorWeightSummary.test.jsx
+++ src/components/Office/ExternalVendorWeightSummary/ExternalVendorWeightSummary.test.jsx
@@ -5,21 +5,47 @@ import ExternalVendorWeightSummary from './ExternalVendorWeightSummary';
 
 import { MockProviders } from 'testUtils';
 
-const shipments = [{ ntsRecordedWeight: 1000 }, { ntsRecordedWeight: 500 }, { ntsRecordedWeight: 1500 }];
-
 describe('ExternalVendorWeightSummary component', () => {
-  it('renders with one shipment', () => {
+  it('renders with one NTS shipment', () => {
     render(
       <MockProviders>
-        <ExternalVendorWeightSummary shipments={[shipments[0]]} />
+        <ExternalVendorWeightSummary shipments={[{ shipmentType: 'HHG_INTO_NTS_DOMESTIC' }]} />
       </MockProviders>,
     );
 
     expect(screen.getByText('1 other shipment:')).toBeInTheDocument();
-    expect(screen.getByText('1,000 lbs')).toBeInTheDocument();
   });
 
-  it('renders with one shipment', () => {
+  it('renders with many NTS shipments', () => {
+    const shipments = [{ shipmentType: 'HHG_INTO_NTS_DOMESTIC' }, { shipmentType: 'HHG_INTO_NTS_DOMESTIC' }];
+    render(
+      <MockProviders>
+        <ExternalVendorWeightSummary shipments={shipments} />
+      </MockProviders>,
+    );
+
+    expect(screen.getByText('2 other shipments:')).toBeInTheDocument();
+  });
+
+  it('renders with one NTSR shipment', () => {
+    render(
+      <MockProviders>
+        <ExternalVendorWeightSummary
+          shipments={[{ ntsRecordedWeight: 1500, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' }]}
+        />
+      </MockProviders>,
+    );
+
+    expect(screen.getByText('1 other shipment:')).toBeInTheDocument();
+    expect(screen.getByText('1,500 lbs')).toBeInTheDocument();
+  });
+
+  it('renders with many NTSR shipments', () => {
+    const shipments = [
+      { ntsRecordedWeight: 1000, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+      { ntsRecordedWeight: 500, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+      { ntsRecordedWeight: 1500, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+    ];
     render(
       <MockProviders>
         <ExternalVendorWeightSummary shipments={shipments} />
@@ -27,6 +53,24 @@ describe('ExternalVendorWeightSummary component', () => {
     );
 
     expect(screen.getByText('3 other shipments:')).toBeInTheDocument();
+    expect(screen.getByText('3,000 lbs')).toBeInTheDocument();
+  });
+
+  it('renders with many NTSR and NTS shipments', () => {
+    const shipments = [
+      { ntsRecordedWeight: 1000, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+      { shipmentType: 'HHG_INTO_NTS_DOMESTIC' },
+      { ntsRecordedWeight: 500, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+      { shipmentType: 'HHG_INTO_NTS_DOMESTIC' },
+      { ntsRecordedWeight: 1500, shipmentType: 'HHG_OUTOF_NTS_DOMESTIC' },
+    ];
+    render(
+      <MockProviders>
+        <ExternalVendorWeightSummary shipments={shipments} />
+      </MockProviders>,
+    );
+
+    expect(screen.getByText('5 other shipments:')).toBeInTheDocument();
     expect(screen.getByText('3,000 lbs')).toBeInTheDocument();
   });
 });
