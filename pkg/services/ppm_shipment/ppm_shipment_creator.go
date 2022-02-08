e91fa907a98565558e24c6de321864117ef1e733
Namibia Torres <namibia@truss.works>
2022-02-08 05:31:06 +0000

    add service obect, rules, and validations

--- pkg/services/ppm_shipment/ppm_shipment_creator.go
+++ pkg/services/ppm_shipment/ppm_shipment_creator.go
@@ -1,32 +1,85 @@
 package ppm_shipment
 
 import (
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
 	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
+	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
-	mtoshipment "github.com/transcom/mymove/pkg/services/mto_shipment"
+	"github.com/transcom/mymove/pkg/services/fetch"
+	moverouter "github.com/transcom/mymove/pkg/services/move"
+	"github.com/transcom/mymove/pkg/services/mto_shipment"
+	"github.com/transcom/mymove/pkg/services/query"
 )
 
-builder := createMTOSHipmentQueryBuilder
-fetcher := services.Fetcher
-moveRouter := services.moveRouter
+type createMTOShipmentQueryBuilder interface {
+	FetchOne(appCtx appcontext.AppContext, model interface{}, filters []services.QueryFilter) error
+	CreateOne(appCtx appcontext.AppContext, model interface{}) (*validate.Errors, error)
+	UpdateOne(appCtx appcontext.AppContext, model interface{}, eTag *string) (*validate.Errors, error)
+}
 
-func createPPMShipment(appCtx appcontext.AppContext) error {
+// ppmShipmentCreator sets up the service object
+type ppmShipmentCreator struct {
+	checks []ppmShipmentValidator
+}
+
+// NewPPMShipmentCreator creates a new struct with the service dependencies
+func NewPPMShipmentCreator() services.PPMShipmentCreator {
+	return &ppmShipmentCreator{
+		checks: []ppmShipmentValidator{
+			checkShipmentID(),
+			checkPPMShipmentID(),
+			checkRequiredFields(),
+		},
+	}
+}
+
+// CreatePPMShipmentCheck passes a validator key to CreatePPMShipment
+func (f *ppmShipmentCreator) CreatePPMShipmentCheck(appCtx appcontext.AppContext, ppmShipment *models.PPMShipment) (*models.PPMShipment, error) {
+	return f.createPPMShipment(appCtx, ppmShipment, f.checks...)
+}
+
+func (f *ppmShipmentCreator) createPPMShipment(appCtx appcontext.AppContext, ppmShipment *models.PPMShipment, checks ...ppmShipmentValidator) (*models.PPMShipment, error) {
 	// Make sure we do this whole process in a transaction so partial changes do not get made committed
 	// in the event of an error.
+
+	builder := query.NewQueryBuilder()
+	fetcher := fetch.NewFetcher(builder)
+	moveRouter := moverouter.NewMoveRouter()
 	mtoShipmentCreator := mtoshipment.NewMTOShipmentCreator(builder, fetcher, moveRouter)
 	// Start a transaction that will create a Shipment, then create a PPM
 	transactionError := appCtx.NewTransaction(func(txnAppCtx appcontext.AppContext) error {
 		var err error
-		createShipment, err := mtoShipmentCreator.CreateMTOShipment(txnAppCtx) // PASS IN ALL THE OBJECTS CreateMTOShipment needs
+		// Might require us to pass in a service item rather than nil or empty service item
+		// var serviceItem models.ServiceItem
+		// NOTE: If the ppm requires a service item for pricing try passing in an HHG service item, then we can use the HHG service item here
+		createShipment, err := mtoShipmentCreator.CreateMTOShipment(txnAppCtx, &ppmShipment.Shipment, nil)
 		// Check that mtoshipment is created. If not, bail out.
-		// CREATE THE PPM_SHIPEMNT. Use the same txnAppCtx
-		// Create the model object for ppm_shipment and save that data to the DB
-		// Check that ppm_shipment is created. If not, bail out adn return the error object
+		if err != nil {
+			return apperror.NewQueryError("MTOShipment", err, "")
+		}
+
+		ppmShipment.ShipmentID = createShipment.ID
+		// Validate ppmShipment, and return an error
+		err = validatePPMShipment(txnAppCtx, *ppmShipment, nil, &ppmShipment.Shipment)
+		if err != nil {
+			return err
+		}
+		// Validate ppm shipment model object and save it to DB (create)
+		verrs, err := txnAppCtx.DB().ValidateAndCreate(ppmShipment)
+		// Check validation errors
+		if verrs != nil && verrs.HasAny() {
+			return apperror.NewInvalidInputError(uuid.Nil, err, verrs, "Invalid input found while creating the PPM shipment.")
+		} else if err != nil {
+			// If the error is something else (this is unexpected), we create a QueryError
+			return apperror.NewQueryError("PPM Shipment", err, "")
+		}
 
 		return err
 	})
 	if transactionError != nil {
-		return transactionError
+		return nil, transactionError
 	}
-	return nil
+	return ppmShipment, nil
 }
---
+++ pkg/services/ppm_shipment/rules.go
@@ -0,0 +1,73 @@
+package ppm_shipment
+
+import (
+	"fmt"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
+	"github.com/transcom/mymove/pkg/models"
+	"time"
+)
+
+// checkShipmentID checks that the user can't change the shipment ID
+func checkShipmentID() ppmShipmentValidator {
+	return ppmShipmentValidatorFunc(func(_ appcontext.AppContext, newPPMShipment models.PPMShipment, oldPPMShipment *models.PPMShipment, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+		if oldPPMShipment == nil {
+			if newPPMShipment.ShipmentID == uuid.Nil {
+				verrs.Add("ShipmentID", "Shipment ID is required")
+			}
+		} else {
+			if newPPMShipment.ShipmentID != uuid.Nil && newPPMShipment.ShipmentID != oldPPMShipment.ShipmentID {
+				verrs.Add("ShipmentID", "cannot be updated")
+			}
+		}
+		return verrs
+	})
+}
+
+// checkPPMShipmentID checks that the user can't change the PPMShipment ID
+func checkPPMShipmentID() ppmShipmentValidator {
+	return ppmShipmentValidatorFunc(func(_ appcontext.AppContext, newPPMShipment models.PPMShipment, oldPPMShipment *models.PPMShipment, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+		if oldPPMShipment == nil {
+			if newPPMShipment.ID != uuid.Nil {
+				verrs.Add("ID", "cannot manually set a new PPM Shipment's UUID")
+			}
+		} else {
+			if newPPMShipment.ID != oldPPMShipment.ID {
+				return apperror.NewImplementationError(
+					fmt.Sprintf("the newPPMShipment ID (%s) must match oldPPMShipment ID (%s).", newPPMShipment.ID, oldPPMShipment.ID),
+				)
+			}
+		}
+		return verrs
+	})
+}
+
+// checkRequiredFields checks that the required fields are included
+func checkRequiredFields() ppmShipmentValidator {
+	return ppmShipmentValidatorFunc(func(_ appcontext.AppContext, newPPMShipment models.PPMShipment, oldPPMShipment *models.PPMShipment, _ *models.MTOShipment) error {
+		verrs := validate.NewErrors()
+
+		var createdAt time.Time
+
+		// Set any pre-existing values as the baseline:
+		if oldPPMShipment != nil {
+			createdAt = oldPPMShipment.CreatedAt
+		}
+
+		// Override pre-existing values with anything sent in for the update/create:
+		if !newPPMShipment.CreatedAt.IsZero() {
+			createdAt = newPPMShipment.CreatedAt
+		}
+
+		// Check that we have something in the CreatedAt field:
+		if createdAt.IsZero() {
+			verrs.Add("requestedAt", "cannot be empty")
+		}
+
+		return verrs
+	})
+}
