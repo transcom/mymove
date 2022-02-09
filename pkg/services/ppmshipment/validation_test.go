9bb2876e53aa1bc7c42ec590706e71d4674fb4bd
Namibia Torres <namibia@truss.works>
2022-02-08 17:01:08 -0800

    set up validation tests once ppm updater is added

---
+++ pkg/services/ppmshipment/validation_test.go
@@ -0,0 +1,29 @@
+package ppmshipment
+
+//import (
+//	"time"
+//
+//	"github.com/gofrs/uuid"
+//
+//	"github.com/transcom/mymove/pkg/handlers"
+//	"github.com/transcom/mymove/pkg/models"
+//)
+//
+//func (suite *PPMShipmentSuite) TestUpdatePPMShipment() {
+//	ppmShipmentID, _ := uuid.NewV4()
+//	shipmentID, _ := uuid.NewV4()
+//	createdAt := time.Now()
+//
+//	oldPPM := models.PPMShipment{
+//
+//	}
+//
+//	newPPM := models.PPMShipment{
+//	}
+//
+//	updatedPPM := updatePPM(newPPM, &oldPPM)
+//
+//	// check the new data populates
+//	// Checking that the data that needs to stay the same, wasn't changed:
+//
+//}
