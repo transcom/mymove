cf7aeaefd4ed486b9971200005786e9ffb827d1f
Yan Zhao <yan.zhao@truss.works>
2022-02-10 02:10:36 +0000

    Fix naming so that it matches what is actually in the ordersInfo

--- src/components/Office/RequestedShipments/RequestedShipments.jsx
+++ src/components/Office/RequestedShipments/RequestedShipments.jsx
@@ -58,8 +58,8 @@ const RequestedShipments = ({
   const ordersLOA = {
     tac: ordersInfo.tacMDC,
     sac: ordersInfo.sacSDN,
-    ntsTac: ordersInfo.ntsTac,
-    ntsSac: ordersInfo.ntsSac,
+    ntsTac: ordersInfo.NTStac,
+    ntsSac: ordersInfo.NTSsac,
   };
 
   const shipmentDisplayInfo = (shipment, dutyStationPostal) => {
