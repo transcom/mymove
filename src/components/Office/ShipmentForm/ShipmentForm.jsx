fd24e07f52eeda257bab0037a29a8170a6993b1a
Yan Zhao <yan.zhao@truss.works>
2022-02-15 01:54:28 +0000

    Fix prop type validation
    - grab the values from the object for oneOf

--- src/components/Office/ShipmentForm/ShipmentForm.jsx
+++ src/components/Office/ShipmentForm/ShipmentForm.jsx
@@ -492,7 +492,7 @@ ShipmentForm.propTypes = {
   TACs: AccountingCodesShape,
   SACs: AccountingCodesShape,
   userRole: oneOf(officeRoles).isRequired,
-  orderType: oneOf(ORDERS_TYPE).isRequired,
+  orderType: oneOf(Object.values(ORDERS_TYPE)).isRequired,
 };
 
 ShipmentForm.defaultProps = {
