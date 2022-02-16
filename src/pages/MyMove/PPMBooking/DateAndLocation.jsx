28053cd6580af406a336d2129ab87b7e956431f8
Duncan Spencer <duncan@truss.works>
2022-02-16 15:34:17 +0000

    add shipment type tag, alert for submission errors, form values and update redux calls

--- src/pages/MyMove/PPMBooking/DateAndLocation.jsx
+++ src/pages/MyMove/PPMBooking/DateAndLocation.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generatePath, useHistory, useParams } from 'react-router-dom';
-import { GridContainer, Grid } from '@trussworks/react-uswds';
+import { GridContainer, Grid, Alert } from '@trussworks/react-uswds';
 
 import { MtoShipmentShape, ServiceMemberShape } from 'types/customerShapes';
 import { DutyStationShape } from 'types';
@@ -9,10 +9,14 @@ import { validatePostalCode } from 'utils/validation';
 import { customerRoutes, generalRoutes } from 'constants/routes';
 import { createMTOShipment, patchMTOShipment } from 'services/internalApi';
 import { SHIPMENT_OPTIONS } from 'shared/constants';
+import { updateMTOShipment } from 'sagas/entities';
+import ShipmentTag from 'components/ShipmentTag/ShipmentTag';
+import { shipmentTypes } from 'constants/shipments';
 
 const DateAndLocation = ({ mtoShipment, serviceMember, destinationDutyLocation }) => {
+  const [errorMessage, setErrorMessage] = useState();
   const history = useHistory();
-  const { moveId } = useParams();
+  const { moveId, shipmentNumber } = useParams();
 
   const isNewShipment = !mtoShipment?.id;
 
@@ -25,12 +29,16 @@ const DateAndLocation = ({ mtoShipment, serviceMember, destinationDutyLocation }
   };
 
   const handleSubmit = async (values, { setSubmitting }) => {
+    setErrorMessage(null);
+
     const createOrUpdateShipment = {
       moveTaskOrderID: moveId,
       shipmentType: SHIPMENT_OPTIONS.PPM,
       ppmShipment: {
         pickupPostalCode: values.pickupPostalCode,
+        hasSecondaryPickupPostalCode: values.hasSecondaryPickupPostalCode, // I think sending this is necessary so we know if the customer wants to clear their previously secondary ZIPs, or we could send nulls for those fields.
         destinationPostalCode: values.destinationPostalCode,
+        hasSecondaryDestinationPostalCode: values.hasSecondaryDestinationPostalCode,
         sitExpected: values.sitExpected,
         expectedDepartureDate: values.expectedDepartureDate,
       },
@@ -44,38 +52,40 @@ const DateAndLocation = ({ mtoShipment, serviceMember, destinationDutyLocation }
       createOrUpdateShipment.secondaryDestinationPostalCode = values.secondaryDestinationPostalCode;
     }
 
-    let newShipmentId;
     if (isNewShipment) {
       createMTOShipment(createOrUpdateShipment)
-        .then(() => {
+        .then((response) => {
           setSubmitting(false);
-          newShipmentId = '00000000-0000-0000-0000-000000000000'; // TODO: replace me
+          updateMTOShipment(response);
           history.push(
             generatePath(customerRoutes.SHIPMENT_PPM_ESTIMATED_WEIGHT_PATH, {
               moveId,
-              mtoShipmentId: newShipmentId,
+              mtoShipmentId: response.id,
             }),
           );
         })
         .catch(() => {
           setSubmitting(false);
+          setErrorMessage('There was an error attempting to create your shipment.');
         });
     } else {
       createOrUpdateShipment.id = mtoShipment.id;
       createOrUpdateShipment.ppmShipment.id = mtoShipment.ppmShipment?.id;
 
       patchMTOShipment(mtoShipment.id, createOrUpdateShipment, mtoShipment.eTag)
-        .then(() => {
+        .then((response) => {
           setSubmitting(false);
+          updateMTOShipment(response);
           history.push(
             generatePath(customerRoutes.SHIPMENT_PPM_ESTIMATED_WEIGHT_PATH, {
               moveId,
-              mtoShipmentId: mtoShipment?.id,
+              mtoShipmentId: response.id,
             }),
           );
         })
         .catch(() => {
           setSubmitting(false);
+          setErrorMessage('There was an error attempting to update your shipment.');
         });
     }
   };
@@ -84,7 +94,13 @@ const DateAndLocation = ({ mtoShipment, serviceMember, destinationDutyLocation }
     <GridContainer>
       <Grid row>
         <Grid col desktop={{ col: 8, offset: 2 }}>
+          <ShipmentTag shipmentType={shipmentTypes.PPM} shipmentNumber={shipmentNumber} />
           <h1>PPM date & location</h1>
+          {errorMessage && (
+            <Alert slim type="error">
+              {errorMessage}
+            </Alert>
+          )}
           <DateAndLocationForm
             mtoShipment={mtoShipment}
             serviceMember={serviceMember}
