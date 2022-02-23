6e827fea643b409c0746424433ef394a25f8a09d
Marjo Poindexter <marjo@truss.works>
2022-02-23 16:15:21 +0000

    refactor ShipmentInfoList to match the others

--- src/components/Office/DefinitionLists/ShipmentInfoList.jsx
+++ src/components/Office/DefinitionLists/ShipmentInfoList.jsx
@@ -31,6 +31,32 @@ const ShipmentInfoList = ({ className, shipment, warnIfMissing, errorIfMissing }
   });
   setDisplayFlags(errorIfMissing, warnIfMissing, null, null, shipment);
 
+  const requestedPickupDateElementFlags = getDisplayFlags('requestedPickupDate');
+  const requestedPickupDateElement = (
+    <div className={requestedPickupDateElementFlags.classes}>
+      <dt>Requested move date</dt>
+      <dd data-testid="requestedPickupDate">{requestedPickupDate && formatDate(requestedPickupDate, 'DD MMM YYYY')}</dd>
+    </div>
+  );
+
+  const pickupAddressElementFlags = getDisplayFlags('pickupAddress');
+  const pickupAddressElement = (
+    <div className={pickupAddressElementFlags.classes}>
+      <dt>Origin address</dt>
+      <dd data-testid="pickupAddress">{pickupAddress && formatAddress(pickupAddress)}</dd>
+    </div>
+  );
+
+  const secondaryPickupAddressElementFlags = getDisplayFlags('secondaryPickupAddress');
+  const secondaryPickupAddressElement = (
+    <div className={secondaryPickupAddressElementFlags.classes}>
+      <dt>Second pickup address</dt>
+      <dd data-testid="secondaryPickupAddress">
+        {secondaryPickupAddress ? formatAddress(secondaryPickupAddress) : '—'}
+      </dd>
+    </div>
+  );
+
   const destinationTypeFlags = getDisplayFlags('destinationType');
   const destinationTypeElement = (
     <div className={destinationTypeFlags.classes}>
@@ -39,11 +65,47 @@ const ShipmentInfoList = ({ className, shipment, warnIfMissing, errorIfMissing }
     </div>
   );
 
+  const destinationAddressElementFlags = getDisplayFlags('destinationAddress');
+  const destinationAddressElement = (
+    <div className={destinationAddressElementFlags.classes}>
+      <dt>Destination address</dt>
+      <dd data-testid="destinationAddress">{formatAddress(destinationAddress)}</dd>
+    </div>
+  );
+
+  const secondaryDeliveryAddressElementFlags = getDisplayFlags('secondaryDeliveryAddress');
+  const secondaryDeliveryAddressElement = (
+    <div className={secondaryDeliveryAddressElementFlags.classes}>
+      <dt>Second destination address</dt>
+      <dd data-testid="secondaryDeliveryAddress">
+        {secondaryDeliveryAddress ? formatAddress(secondaryDeliveryAddress) : '—'}
+      </dd>
+    </div>
+  );
+
+  const agentsElementFlags = getDisplayFlags('agents');
+  const agentsElement = agents
+    ? agents.map((agent) => (
+        <div className={agentsElementFlags.classes} key={`${agent.agentType}-${agent.email}`}>
+          <dt>{agent.agentType === 'RELEASING_AGENT' ? 'Releasing agent' : 'Receiving agent'}</dt>
+          <dd data-testid={agent.agentType}>{formatAgent(agent)}</dd>
+        </div>
+      ))
+    : null;
+
   const counselorRemarksElementFlags = getDisplayFlags('counselorRemarks');
   const counselorRemarksElement = (
     <div className={counselorRemarksElementFlags.classes}>
       <dt>Counselor remarks</dt>
       <dd data-testid="counselorRemarks">{counselorRemarks || '—'}</dd>
+    </div>
+  );
+
+  const customerRemarksElementFlags = getDisplayFlags('customerRemarks');
+  const customerRemarksElement = (
+    <div className={customerRemarksElementFlags.classes}>
+      <dt>Customer remarks</dt>
+      <dd data-testid="customerRemarks">{customerRemarks || '—'}</dd>
     </div>
   );
 
@@ -58,43 +120,15 @@ const ShipmentInfoList = ({ className, shipment, warnIfMissing, errorIfMissing }
       )}
       data-testid="shipment-info-list"
     >
-      <div className={styles.row}>
-        <dt>Requested move date</dt>
-        <dd>{formatDate(requestedPickupDate, 'DD MMM YYYY')}</dd>
-      </div>
-      <div className={styles.row}>
-        <dt>Origin address</dt>
-        <dd>{pickupAddress && formatAddress(pickupAddress)}</dd>
-      </div>
-      {secondaryPickupAddress && (
-        <div className={styles.row}>
-          <dt>Second pickup address</dt>
-          <dd>{formatAddress(secondaryPickupAddress)}</dd>
-        </div>
-      )}
-      <div className={styles.row}>
-        <dt>Destination address</dt>
-        <dd data-testid="destinationAddress">{formatAddress(destinationAddress)}</dd>
-      </div>
+      {requestedPickupDateElement}
+      {pickupAddressElement}
+      {secondaryPickupAddress && secondaryPickupAddressElement}
+      {destinationAddressElement}
       {displayDestinationType && destinationTypeElement}
-      {secondaryDeliveryAddress && (
-        <div className={styles.row}>
-          <dt>Second destination address</dt>
-          <dd>{formatAddress(secondaryDeliveryAddress)}</dd>
-        </div>
-      )}
-      {agents &&
-        agents.map((agent) => (
-          <div className={styles.row} key={`${agent.agentType}-${agent.email}`}>
-            <dt>{agent.agentType === 'RELEASING_AGENT' ? 'Releasing agent' : 'Receiving agent'}</dt>
-            <dd>{formatAgent(agent)}</dd>
-          </div>
-        ))}
+      {secondaryDeliveryAddress && secondaryDeliveryAddressElement}
+      {agents && agentsElement}
       {counselorRemarksElement}
-      <div className={styles.row}>
-        <dt>Customer remarks</dt>
-        <dd data-testid="customerRemarks">{customerRemarks || '—'}</dd>
-      </div>
+      {customerRemarks && customerRemarksElement}
     </dl>
   );
 };
