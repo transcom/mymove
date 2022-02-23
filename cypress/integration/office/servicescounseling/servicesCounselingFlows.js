bc2e8f074dfeca1f85aea1332926c8234a8e6a1a
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2022-02-23 17:44:39 +0000

    playing around with ordering to see how it plays in circleci

--- cypress/integration/office/servicescounseling/servicesCounselingFlows.js
+++ cypress/integration/office/servicescounseling/servicesCounselingFlows.js
@@ -164,6 +164,7 @@ describe('Services counselor user', () => {
     //Edit DependentsAuthorized
     cy.get('[data-testid="dependentsAuthorizedInput"]').click({ force: true });
     // Edit allowances page | Save
+    cy.get('[data-testid="scAllowancesSave"]').contains('Save').click();
     cy.wait(3000);
     cy.get('[data-testid="scAllowancesSave"]').contains('Save').click();
 
