f77eafed7d48df499377c550e6663a14defb34b4
Marty Boren <marty@truss.works>
2022-02-23 23:38:27 +0000

    Remove commented out function

--- pkg/handlers/internalapi/duty_stations.go
+++ pkg/handlers/internalapi/duty_stations.go
@@ -10,28 +10,6 @@ import (
 	"github.com/transcom/mymove/pkg/models"
 )
 
-// TODO
-//func payloadForDutyStationModel(station models.DutyLocation) *internalmessages.DutyLocationPayload {
-//	// If the station ID has no UUID then it isn't real data
-//	// Unlike other payloads the
-//	if station.ID == uuid.Nil {
-//		return nil
-//	}
-//	payload := internalmessages.DutyLocationPayload{
-//		ID:          handlers.FmtUUID(station.ID),
-//		CreatedAt:   handlers.FmtDateTime(station.CreatedAt),
-//		UpdatedAt:   handlers.FmtDateTime(station.UpdatedAt),
-//		Name:        swag.String(station.Name),
-//		Affiliation: station.Affiliation,
-//		AddressID:   handlers.FmtUUID(station.AddressID),
-//		Address:     payloads.Address(&station.Address),
-//	}
-//
-//	payload.TransportationOffice = payloads.TransportationOffice(station.TransportationOffice)
-//
-//	return &payload
-//}
-
 // SearchDutyStationsHandler returns a list of all issues
 type SearchDutyStationsHandler struct {
 	handlers.HandlerContext
