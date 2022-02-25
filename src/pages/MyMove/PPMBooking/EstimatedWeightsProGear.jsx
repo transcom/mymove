d56a0f727b202eb44e397557fc751388b996dae7
Felipe Lee <felipe@truss.works>
2022-02-25 23:58:20 +0000

    MB-10675 Add first pass at EstimatedWeightsProGear.jsx
    This page probably doesn't quite work, and it's unreachable at the moment haha.

---
+++ src/pages/MyMove/PPMBooking/EstimatedWeightsProGear.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { generatePath, useHistory, useParams } from 'react-router-dom';
+import { GridContainer, Grid, Alert } from '@trussworks/react-uswds';
+
+import { customerRoutes, generalRoutes } from 'constants/routes';
+import EstimatedWeightsProGearForm from 'components/Customer/PPMBooking/EstimatedWeightsProGearForm/EstimatedWeightsProGearForm';
+import { shipmentTypes } from 'constants/shipments';
+import ShipmentTag from 'components/ShipmentTag/ShipmentTag';
+import { getResponseError, patchMTOShipment } from 'services/internalApi';
+import { updateMTOShipment } from 'store/entities/actions';
+import { MtoShipmentShape, OrdersShape, ServiceMemberShape } from 'types/customerShapes';
+
+const EstimatedWeightsProGear = ({ orders, serviceMember, mtoShipment }) => {
+  const [errorMessage, setErrorMessage] = useState();
+  const history = useHistory();
+  const { moveId, shipmentNumber } = useParams();
+
+  const isFirstPass = mtoShipment.ppmShipment.estimatedWeight === null;
+  const mtoShipmentId = mtoShipment.id;
+
+  const handleBack = () => {
+    if (isFirstPass) {
+      history.push(generatePath(customerRoutes.SHIPMENT_EDIT_PATH, { moveId, mtoShipmentId }));
+    }
+
+    history.push(generalRoutes.HOME_PATH);
+  };
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    setErrorMessage(null);
+
+    const payload = {
+      ppmShipment: {
+        id: mtoShipment.ppmShipment.id,
+        estimatedWeight: values.estimatedWeight,
+        hasProGear: values.hasProGear,
+        proGearWeight: values.proGearWeight ? values.hasProGear : null,
+        spouseProGearWeight: values.spouseProGearWeight ? values.hasProGear : null,
+      },
+    };
+
+    patchMTOShipment(mtoShipment.id, payload, mtoShipment.eTag)
+      .then((response) => {
+        setSubmitting(false);
+        updateMTOShipment(response);
+        history.push(customerRoutes.SHIPMENT_PPM_ESTIMATED_INCENTIVE, { moveId, mtoShipmentId });
+      })
+      .catch((err) => {
+        setSubmitting(false);
+
+        setErrorMessage(getResponseError(err.response, 'Failed to update MTO shipment due to server error.'));
+      });
+  };
+
+  return (
+    <div>
+      <GridContainer>
+        <Grid row>
+          <Grid col desktop={{ col: 8, offset: 2 }}>
+            <ShipmentTag shipmentType={shipmentTypes.PPM} shipmentNumber={shipmentNumber} />
+            <h1>PPM date & location</h1>
+            {errorMessage && (
+              <Alert slim type="error">
+                {errorMessage}
+              </Alert>
+            )}
+            <EstimatedWeightsProGearForm
+              orders={orders}
+              serviceMember={serviceMember}
+              mtoShipment={mtoShipment}
+              onSubmit={handleSubmit}
+              onBack={handleBack}
+            />
+          </Grid>
+        </Grid>
+      </GridContainer>
+    </div>
+  );
+};
+
+EstimatedWeightsProGear.propTypes = {
+  orders: OrdersShape.isRequired,
+  serviceMember: ServiceMemberShape.isRequired,
+  mtoShipment: MtoShipmentShape.isRequired,
+};
+
+export default EstimatedWeightsProGear;
