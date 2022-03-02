d907de1c9eb0ab3a213a29c547798bb56bb0098b
pearl-truss <pearl@truss.works>
2022-03-02 20:49:52 +0000

    allow cypres to requery

--- cypress/integration/milmove/ppms/dateAndLocation.js
+++ cypress/integration/milmove/ppms/dateAndLocation.js
@@ -56,14 +56,12 @@ function invalidInputs() {
 
   // invalid postal codes
   cy.get('input[name="pickupPostalCode"]').clear().type('00000').blur();
-  cy.get('[class="usa-error-message"]').as('errorMessage');
   cy.get('@errorMessage').contains(
     "We don't have rates for this ZIP code. Please verify that you have entered the correct one. Contact support if this problem persists.",
   );
   cy.get('input[name="pickupPostalCode"]').clear().type('90210').blur();
   cy.get('@errorMessage').should('not.exist');
   cy.get('input[name="pickupPostalCode"]').clear().blur();
-  cy.get('[class="usa-error-message"]').as('errorMessage');
   cy.get('@errorMessage').contains('Required');
   cy.get('@errorMessage').next('input').should('have.id', 'pickupPostalCode');
   cy.get('input[name="pickupPostalCode"]').clear().type('90210').blur();
@@ -72,7 +70,6 @@ function invalidInputs() {
   // missing secondary pickup postal code
   cy.get('input[name="hasSecondaryPickupPostalCode"]').eq(0).check({ force: true });
   cy.get('input[name="secondaryPickupPostalCode"]').clear().blur();
-  cy.get('[class="usa-error-message"]').as('errorMessage');
   cy.get('@errorMessage').contains('Required');
   cy.get('@errorMessage').next('input').should('have.id', 'secondaryPickupPostalCode');
   cy.get('input[name="secondaryPickupPostalCode"]').clear().type('90210').blur();
@@ -81,7 +78,6 @@ function invalidInputs() {
   // missing secondary destination postal code
   cy.get('input[name="hasSecondaryDestinationPostalCode"]').eq(0).check({ force: true });
   cy.get('input[name="secondaryDestinationPostalCode"]').clear().blur();
-  cy.get('[class="usa-error-message"]').as('errorMessage');
   cy.get('@errorMessage').contains('Required');
   cy.get('@errorMessage').next('input').should('have.id', 'secondaryDestinationPostalCode');
   cy.get('input[name="secondaryDestinationPostalCode"]').clear().type('90210').blur();
