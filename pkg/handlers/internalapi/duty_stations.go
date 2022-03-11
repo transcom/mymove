18fca8d21246ea491259f5b1951740c93e682ba3
Roger Steve Ruiz <rogeruiz@truss.works>
2022-03-11 00:00:26 +0000

    Refactor SearchDutyStationsHandler

--- pkg/handlers/internalapi/duty_stations.go
+++ pkg/handlers/internalapi/duty_stations.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-openapi/runtime/middleware"
 	"go.uber.org/zap"
 
+	"github.com/transcom/mymove/pkg/appcontext"
 	stationop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/duty_locations"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
 	"github.com/transcom/mymove/pkg/handlers"
@@ -18,19 +19,21 @@ type SearchDutyStationsHandler struct {
 // TODO: temporary workaround until this file gets deleted entirely
 // Handle returns a list of stations based on the search query
 func (h SearchDutyStationsHandler) Handle(params stationop.SearchDutyLocationsParams) middleware.Responder {
-	appCtx := h.AppContextFromRequest(params.HTTPRequest)
+	return h.AuditableAppContextFromRequest(params.HTTPRequest,
+		func(appCtx appcontext.AppContext) middleware.Responder {
 
-	locations, err := models.FindDutyLocations(appCtx.DB(), params.Search)
-	if err != nil {
-		appCtx.Logger().Error("Finding duty stations", zap.Error(err))
-		return stationop.NewSearchDutyLocationsInternalServerError()
+			locations, err := models.FindDutyLocations(appCtx.DB(), params.Search)
+			if err != nil {
+				appCtx.Logger().Error("Finding duty stations", zap.Error(err))
+				return stationop.NewSearchDutyLocationsInternalServerError()
 
-	}
+			}
 
-	locationPayloads := make(internalmessages.DutyLocationsPayload, len(locations))
-	for i, location := range locations {
-		locationPayload := payloadForDutyLocationModel(location)
-		locationPayloads[i] = locationPayload
-	}
-	return stationop.NewSearchDutyLocationsOK().WithPayload(locationPayloads)
+			locationPayloads := make(internalmessages.DutyLocationsPayload, len(locations))
+			for i, location := range locations {
+				locationPayload := payloadForDutyLocationModel(location)
+				locationPayloads[i] = locationPayload
+			}
+			return stationop.NewSearchDutyLocationsOK().WithPayload(locationPayloads)
+		})
 }
