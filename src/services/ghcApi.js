0cf99f0d44c979316b6fd1fa8af271e46a9310db
Michael Valdes <michael.valde@truss.works>
2022-03-14 20:53:32 +0000

    Revert unnecessary var rename

--- src/services/ghcApi.js
+++ src/services/ghcApi.js
@@ -29,8 +29,8 @@ export async function getMove(key, locator) {
   return makeGHCRequest('move.getMove', { locator }, { normalize: false });
 }
 
-export async function getMoveHistory(key, moveCode) {
-  return makeGHCRequest('move.getMoveHistory', { locator: moveCode }, { normalize: false });
+export async function getMoveHistory(key, locator) {
+  return makeGHCRequest('move.getMoveHistory', { locator }, { normalize: false });
 }
 
 export async function getOrder(key, orderID) {
