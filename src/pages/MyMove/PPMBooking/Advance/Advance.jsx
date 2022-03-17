af6cc6d51338ad4ff08d3fab368090b33d33e3c1
pearl-truss <pearl@truss.works>
2022-03-17 13:28:47 +0000

    update the routing for back and continue buttons on the ppm advance page

---
+++ src/pages/MyMove/PPMBooking/Advance/Advance.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { generatePath, useHistory, useParams } from 'react-router-dom';
+import { GridContainer, Grid, Alert } from '@trussworks/react-uswds';
+
+import ppmBookingPageStyles from 'pages/MyMove/PPMBooking/PPMBooking.module.scss';
+import { customerRoutes } from 'constants/routes';
+import AdvanceForm from 'components/Customer/PPMBooking/Advance/AdvanceForm';
+import { shipmentTypes } from 'constants/shipments';
+import ShipmentTag from 'components/ShipmentTag/ShipmentTag';
+import { getResponseError, patchMTOShipment } from 'services/internalApi';
+import { updateMTOShipment } from 'store/entities/actions';
+import {
+  selectCurrentOrders,
+  selectMTOShipmentById,
+  selectServiceMemberFromLoggedInUser,
+} from 'store/entities/selectors';
+
+// update ppmShipment payload for AdvanceForm values
+// remove unnessary selectioins like order and service member
+// create a new advance route
+// use this component for the advance route
+const Advance = () => {
+  const [errorMessage, setErrorMessage] = useState();
+  const history = useHistory();
+  const { moveId, mtoShipmentId, shipmentNumber } = useParams();
+  const dispatch = useDispatch();
+
+  const serviceMember = useSelector((state) => selectServiceMemberFromLoggedInUser(state));
+  const orders = useSelector((state) => selectCurrentOrders(state));
+  const mtoShipment = useSelector((state) => selectMTOShipmentById(state, mtoShipmentId));
+
+  const handleBack = () => {
+    history.push(generatePath(customerRoutes.SHIPMENT_PPM_ESTIMATED_INCENTIVE_PATH, { moveId, mtoShipmentId }));
+  };
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    setErrorMessage(null);
+
+    const hasProGear = values.hasProGear === 'true';
+
+    const payload = {
+      ppmShipment: {
+        id: mtoShipment.ppmShipment.id,
+        estimatedWeight: Number(values.estimatedWeight),
+        hasProGear,
+        proGearWeight: hasProGear ? Number(values.proGearWeight) : null,
+        spouseProGearWeight: hasProGear ? Number(values.spouseProGearWeight) : null,
+      },
+    };
+
+    patchMTOShipment(mtoShipment.id, payload, mtoShipment.eTag)
+      .then((response) => {
+        setSubmitting(false);
+        dispatch(updateMTOShipment(response));
+        history.push(generatePath(customerRoutes.MOVE_REVIEW_PATH, { moveId }));
+      })
+      .catch((err) => {
+        setSubmitting(false);
+
+        setErrorMessage(getResponseError(err.response, 'Failed to update MTO shipment due to server error.'));
+      });
+  };
+
+  return (
+    <div className={ppmBookingPageStyles.PPMBookingPage}>
+      <GridContainer>
+        <Grid row>
+          <Grid col desktop={{ col: 8, offset: 2 }}>
+            <ShipmentTag shipmentType={shipmentTypes.PPM} shipmentNumber={shipmentNumber} />
+            <h1>Advances</h1>
+            {errorMessage && (
+              <Alert slim type="error">
+                {errorMessage}
+              </Alert>
+            )}
+            <AdvanceForm
+              orders={orders}
+              serviceMember={serviceMember}
+              mtoShipment={mtoShipment}
+              onSubmit={handleSubmit}
+              onBack={handleBack}
+            />
+          </Grid>
+        </Grid>
+      </GridContainer>
+    </div>
+  );
+};
+
+export default Advance;
