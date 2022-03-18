f3f4abed9397a0e18aaa9aa6c7c9d09bb51e34d8
Ronak Patel <ronak@truss.works>
2022-03-18 21:35:20 +0000

    Resolve sporadic ETag issue and clean up code

--- cypress/integration/milmove/ppms/estimatedIncentive.js
+++ cypress/integration/milmove/ppms/estimatedIncentive.js
@@ -4,11 +4,12 @@ describe('PPM Onboarding - Estimated Incentive', function () {
   });
 
   beforeEach(() => {
-    // cy.intercept('GET', '**/internal/moves/**/mto_shipments').as('getShipment');
+    cy.intercept('GET', '**/internal/moves/**/mto_shipments').as('getShipment');
     cy.intercept('PATCH', '**/internal/mto-shipments/**').as('patchShipment');
-    cy.logout();
+
     const userId = '4512dc8c-c777-444e-b6dc-7971e398f2dc';
     cy.apiSignInAsUser(userId);
+    cy.wait('@getShipment');
   });
 
   it('go to estimated incentives page', () => {
@@ -17,7 +18,7 @@ describe('PPM Onboarding - Estimated Incentive', function () {
   });
 
   it('mobile - go to estimated incentives page', () => {
-    cy.viewport(425, 875);
+    cy.viewport(479, 875);
     navigateToEstimatedIncentivePageAndVerifyAmount();
     cy.get('[data-testid="ppm-estimated-incentive-next"]').should('not.be.disabled').should('have.css', 'order', '1');
   });
@@ -25,19 +26,12 @@ describe('PPM Onboarding - Estimated Incentive', function () {
 
 function navigateToEstimatedIncentivePageAndVerifyAmount() {
   cy.get('[data-testid="shipment-list-item-container"]').click();
+  cy.wait('@getShipment');
 
-  // cy.url()
-  //   .should('include', '/edit')
-  //   .then((url) => {
-  //     const regex = /(.*)\/edit/;
-  //     const estimatedIncentivePath = `${url.match(regex)[1]}/estimated-incentive`;
-  //     cy.visit(estimatedIncentivePath);
-  //   });
-
-  cy.get('[data-testid="ppm-date-and-location-submit"]').contains('Save & Continue').click();
+  cy.get('[data-testid="ppm-date-and-location-submit"]').should('not.be.disabled').click();
   cy.wait('@patchShipment');
 
-  cy.get('[data-testid="ppm-estimated-weights-submit"]').click();
+  cy.get('[data-testid="ppm-estimated-weights-submit"]').should('not.be.disabled').click();
   cy.wait('@patchShipment');
 
   cy.get('[data-testid="ppm-estimated-incentive-amount-sentence"]').contains(/\$\d{1,3}(?:,\d{3})*? is/);
