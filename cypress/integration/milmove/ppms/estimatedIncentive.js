cd035a74e237ce2bdef3397a6e63678081775490
Ronak Patel <ronak@truss.works>
2022-03-21 18:19:17 +0000

    Add comments and reorganize code based on PR feedback

--- cypress/integration/milmove/ppms/estimatedIncentive.js
+++ cypress/integration/milmove/ppms/estimatedIncentive.js
@@ -6,25 +6,24 @@ describe('PPM Onboarding - Estimated Incentive', function () {
   beforeEach(() => {
     cy.intercept('GET', '**/internal/moves/**/mto_shipments').as('getShipment');
     cy.intercept('PATCH', '**/internal/mto-shipments/**').as('patchShipment');
-
-    const userId = '4512dc8c-c777-444e-b6dc-7971e398f2dc';
-    cy.apiSignInAsUser(userId);
-    cy.wait('@getShipment');
   });
 
   it('go to estimated incentives page', () => {
-    navigateToEstimatedIncentivePageAndVerifyAmount();
-    cy.get('[data-testid="ppm-estimated-incentive-next"]').should('not.be.disabled');
+    navigateToEstimatedIncentivePageAndVerifyFields(false);
   });
 
   it('mobile - go to estimated incentives page', () => {
     cy.viewport(479, 875);
-    navigateToEstimatedIncentivePageAndVerifyAmount();
-    cy.get('[data-testid="ppm-estimated-incentive-next"]').should('not.be.disabled').should('have.css', 'order', '1');
+    navigateToEstimatedIncentivePageAndVerifyFields(true);
   });
 });
 
-function navigateToEstimatedIncentivePageAndVerifyAmount() {
+function navigateToEstimatedIncentivePageAndVerifyFields(isMobile = false) {
+  // estimated_weights@ppm.unsubmitted
+  const userId = '4512dc8c-c777-444e-b6dc-7971e398f2dc';
+  cy.apiSignInAsUser(userId);
+  cy.wait('@getShipment');
+
   cy.get('[data-testid="shipment-list-item-container"]').click();
   cy.wait('@getShipment');
 
@@ -34,5 +33,17 @@ function navigateToEstimatedIncentivePageAndVerifyAmount() {
   cy.get('[data-testid="ppm-estimated-weights-submit"]').should('not.be.disabled').click();
   cy.wait('@patchShipment');
 
+  // checks the format of the incentive amount statment is `$<some comma-separated number without decimals> is`
   cy.get('[data-testid="ppm-estimated-incentive-amount-sentence"]').contains(/\$\d{1,3}(?:,\d{3})*? is/);
+
+  if (!isMobile) {
+    cy.get('[data-testid="ppm-estimated-incentive-next"]').should('not.be.disabled');
+  } else {
+    cy.get('[data-testid="ppm-estimated-incentive-next"]').should('not.be.disabled').should('have.css', 'order', '1');
+  }
+
+  cy.get('[data-testid="ppm-estimated-incentive-next"]').should('be.enabled').click();
+  cy.location().should((loc) => {
+    expect(loc.pathname).to.match(/^\/moves\/[^/]+\/shipments\/[^/]+\/advances/);
+  });
 }
