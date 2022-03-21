ca161590e15b01b09de6038d3e4c742a1b0f410d
Reggie Riser <reggie@truss.works>
2022-03-21 18:10:36 +0000

    Also removing jaynawallaceTRUSS's CAC access

--- migrations/app/schema/20220321142756_remove_cac_jacquelineio.up.sql
+++
@@ -1,2 +0,0 @@
--- Remove jacquelineIO's CAC using unique sha256 digest of the client cert
-DELETE FROM client_certs WHERE sha256_digest = 'f1c5a949a12dbd8ffa37faf0e4f4a6930a97534993d290b102117b6094e69d3b';
