03e0079d50510b750ddf10841a36a71062af79df
Namibia Torres <namibia@truss.works>
2022-03-23 12:45:18 -0700

    move validation tests to rules doc

--- pkg/services/ppmshipment/validation.go
+++ pkg/services/ppmshipment/validation.go
@@ -42,17 +42,7 @@ func validatePPMShipment(
 	if verrs.HasAny() {
 		result = apperror.NewInvalidInputError(newPPMShipment.ID, nil, verrs, "Invalid input found while validating the PPM shipment.")
 	}
-	if float64(*newPPMShipment.Advance) > float64(*newPPMShipment.EstimatedIncentive)*0.6 {
-		result = apperror.NewInvalidInputError(newPPMShipment.ID, nil, verrs, "Advance can not be greater than 60% of the estimated incentive")
-	}
 
-	if float64(*newPPMShipment.Advance) < float64(1) {
-		result = apperror.NewInvalidInputError(newPPMShipment.ID, nil, verrs, "Advance can not be  value less than 1")
-	}
-
-	if *newPPMShipment.AdvanceRequested && *newPPMShipment.Advance == 0 {
-		result = apperror.NewInvalidInputError(newPPMShipment.ID, nil, verrs, "An advance amount is required")
-	}
 	return result
 }
 
