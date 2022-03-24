b8007d2ec445320c04938190a7cc702cb8de49bd
Yan Zhao <yan.zhao@truss.works>
2022-03-24 01:52:48 +0000

    Add tests for both MoveHistoryDetailsSelector and PlainTextDetails

---
+++ src/pages/Office/MoveHistory/MoveHistoryDetailsSelector.test.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import MoveHistoryDetailsSelector from './MoveHistoryDetailsSelector';
+
+describe('MoveHistoryDetailsSelector', () => {
+  it.each([
+    ['counselingUpdateOrder', 'Labelled'],
+    ['updateOrder', 'Labelled'],
+    ['updateAllowance', 'Labelled'],
+    ['counselingUpdateAllowance', 'Labelled'],
+    ['updateMoveTaskOrder', 'Labelled'],
+    ['updateMTOShipment', 'Labelled'],
+    ['approveShipment', 'Approved shipment'],
+    ['requestShipmentDiversion', 'Requested diversion'],
+    ['updateMTOServiceItem', 'Service Items'],
+    ['updateMTOServiceItemStatus', 'Service item status'],
+    ['updateMoveTaskOrderStatus', '-'],
+    ['requestShipmentCancellation', 'Shipment cancelled'],
+    ['createOrders', '-'],
+    ['updateOrders', 'Labelled'],
+    ['uploadAmendedOrders', '-'],
+    ['submitMoveForApproval', '-'],
+    ['submitAmendedOrders', 'Labelled'],
+    ['createMTOShipment', '-'],
+    ['updateMTOShipmentAddress', 'Labelled'],
+    ['createMTOServiceItem', 'Service Items'],
+    ['default', '-'],
+  ])('for event name %s it renders %s', (eventName, text) => {
+    render(<MoveHistoryDetailsSelector eventName={eventName} />);
+
+    expect(screen.getByText(text, { exact: false })).toBeInTheDocument();
+  });
+
+  it.each([
+    [
+      'setFinancialReviewFlag',
+      'Move flagged for financial review',
+      [{ columnName: 'financial_review_flag', columnValue: 'true' }],
+    ],
+    [
+      'setFinancialReviewFlag',
+      'Move unflagged for financial review',
+      [{ columnName: 'financial_review_flag', columnValue: 'false' }],
+    ],
+  ])('for event name %s it renders %s', (eventName, text, changedValues) => {
+    render(<MoveHistoryDetailsSelector eventName={eventName} changedValues={changedValues} />);
+
+    expect(screen.getByText(text, { exact: false })).toBeInTheDocument();
+  });
+});
