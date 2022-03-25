77f28c9a7457ef7979caa393d2dbb19683c18b6f
Roger Steve Ruiz <rogeruiz@truss.works>
2022-03-25 16:06:07 +0000

    Refactor TacValidationHandler again;
    This refactor uses the `apperror` package rather than `fmt` to create
    Error messaging around what's happening in the handler.

--- pkg/handlers/ghcapi/tac.go
+++ pkg/handlers/ghcapi/tac.go
@@ -1,13 +1,13 @@
 package ghcapi
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/go-openapi/runtime/middleware"
 	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
 	tacop "github.com/transcom/mymove/pkg/gen/ghcapi/ghcoperations/tac"
 	"github.com/transcom/mymove/pkg/gen/ghcmessages"
 	"github.com/transcom/mymove/pkg/handlers"
@@ -23,22 +23,29 @@ type TacValidationHandler struct {
 func (h TacValidationHandler) Handle(params tacop.TacValidationParams) middleware.Responder {
 	return h.AuditableAppContextFromRequestWithErrors(params.HTTPRequest,
 		func(appCtx appcontext.AppContext) (middleware.Responder, error) {
-			rollbackErr := fmt.Errorf("TAC validation error")
-
 			if appCtx.Session() == nil {
-				return tacop.NewTacValidationUnauthorized(), rollbackErr
+				sessionErr := apperror.NewSessionError(
+					"user is not authorized",
+				)
+				appCtx.Logger().Error(sessionErr.Error())
+				return tacop.NewTacValidationUnauthorized(), sessionErr
 			}
 
 			if !appCtx.Session().IsOfficeApp() || !appCtx.Session().IsOfficeUser() {
-				return tacop.NewTacValidationForbidden(), rollbackErr
+				sessionOfficeErr := apperror.NewSessionError(
+					"user is not authenticated with TOO office role",
+				)
+				appCtx.Logger().Error(sessionOfficeErr.Error())
+				return tacop.NewTacValidationForbidden(), sessionOfficeErr
 			}
 
 			db := appCtx.DB()
 			isValid, err := db.Where("tac = $1", strings.ToUpper(params.Tac)).Exists(&models.TransportationAccountingCode{})
 
 			if err != nil {
-				appCtx.Logger().Error("Error looking for transportation accounting code", zap.Error(err))
-				return tacop.NewTacValidationInternalServerError(), rollbackErr
+				appCtx.Logger().
+					Error("Error looking for transportation accounting code", zap.Error(err))
+				return tacop.NewTacValidationInternalServerError(), err
 			}
 
 			tacValidationPayload := &ghcmessages.TacValid{
