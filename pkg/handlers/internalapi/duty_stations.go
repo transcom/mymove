e61b876d8abbcc0b716c4bc98cecfb21b4a8e53b
Roger Steve Ruiz <rogeruiz@truss.works>
2022-03-28 19:32:56 +0000

    Refactor SearchDutyStationsHandler

--- pkg/handlers/internalapi/duty_stations.go
+++ pkg/handlers/internalapi/duty_stations.go
@@ -2,9 +2,11 @@ package internalapi
 
 import (
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/gofrs/uuid"
 	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
 	stationop "github.com/transcom/mymove/pkg/gen/internalapi/internaloperations/duty_locations"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
 	"github.com/transcom/mymove/pkg/handlers"
@@ -19,13 +21,14 @@ type SearchDutyStationsHandler struct {
 // TODO: temporary workaround until this file gets deleted entirely
 // Handle returns a list of stations based on the search query
 func (h SearchDutyStationsHandler) Handle(params stationop.SearchDutyLocationsParams) middleware.Responder {
-	return h.AuditableAppContextFromRequest(params.HTTPRequest,
-		func(appCtx appcontext.AppContext) middleware.Responder {
+	return h.AuditableAppContextFromRequestWithErrors(params.HTTPRequest,
+		func(appCtx appcontext.AppContext) (middleware.Responder, error) {
 
 			locations, err := models.FindDutyLocations(appCtx.DB(), params.Search)
 			if err != nil {
-				appCtx.Logger().Error("Finding duty stations", zap.Error(err))
-				return stationop.NewSearchDutyLocationsInternalServerError()
+				dutyStationErr := apperror.NewNotFoundError(uuid.Nil, "Finding duty stations")
+				appCtx.Logger().Error(dutyStationErr.Error(), zap.Error(err))
+				return stationop.NewSearchDutyLocationsInternalServerError(), dutyStationErr
 
 			}
 
@@ -34,6 +37,6 @@ func (h SearchDutyStationsHandler) Handle(params stationop.SearchDutyLocationsPa
 				locationPayload := payloadForDutyLocationModel(location)
 				locationPayloads[i] = locationPayload
 			}
-			return stationop.NewSearchDutyLocationsOK().WithPayload(locationPayloads)
+			return stationop.NewSearchDutyLocationsOK().WithPayload(locationPayloads), nil
 		})
 }
