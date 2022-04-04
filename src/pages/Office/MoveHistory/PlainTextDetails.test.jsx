d68d3e7022d7e5e6dec7d753fc9aeb4be254eb35
Yan Zhao <yan.zhao@truss.works>
2022-04-04 18:44:23 +0000

    Make the other shipment types a bit more display friendly
    - add tests for the historyLogUIDisplayName as well

--- src/pages/Office/MoveHistory/PlainTextDetails.test.jsx
+++ src/pages/Office/MoveHistory/PlainTextDetails.test.jsx
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react';
 
 import PlainTextDetails from './PlainTextDetails';
 
+import { shipmentOptionToDisplay } from 'constants/historyLogUIDisplayName';
+
 describe('PlainTextDetails', () => {
   it.each([
     ['approveShipment', 'Approved shipment', {}, {}],
@@ -10,7 +12,36 @@ describe('PlainTextDetails', () => {
     ['updateMTOServiceItemStatus', 'Service item status', {}, {}],
     ['setFinancialReviewFlag', 'Move flagged for financial review', {}, { financial_review_flag: 'true' }],
     ['setFinancialReviewFlag', 'Move unflagged for financial review', {}, { financial_review_flag: 'false' }],
-    ['requestShipmentCancellation', 'Requested cancellation for HHG Shipment', { shipment_type: 'HHG' }, {}],
+    [
+      'requestShipmentCancellation',
+      `Requested cancellation for ${shipmentOptionToDisplay.HHG} shipment`,
+      { shipment_type: 'HHG' },
+      {},
+    ],
+    [
+      'requestShipmentCancellation',
+      `Requested cancellation for ${shipmentOptionToDisplay.HHG_OUTOF_NTS_DOMESTIC} shipment`,
+      { shipment_type: 'HHG_OUTOF_NTS_DOMESTIC' },
+      {},
+    ],
+    [
+      'requestShipmentCancellation',
+      `Requested cancellation for ${shipmentOptionToDisplay.HHG_INTO_NTS_DOMESTIC} shipment`,
+      { shipment_type: 'HHG_INTO_NTS_DOMESTIC' },
+      {},
+    ],
+    [
+      'requestShipmentCancellation',
+      `Requested cancellation for ${shipmentOptionToDisplay.PPM} shipment`,
+      { shipment_type: 'PPM' },
+      {},
+    ],
+    [
+      'requestShipmentCancellation',
+      `Requested cancellation for ${shipmentOptionToDisplay.HHG_SHORTHAUL_DOMESTIC} shipment`,
+      { shipment_type: 'HHG_SHORTHAUL_DOMESTIC' },
+      {},
+    ],
     ['updateMoveTaskOrderStatus', 'Created Move Task Order (MTO)', {}, { status: 'APPROVED' }],
     ['updateMoveTaskOrderStatus', 'Rejected Move Task Order (MTO)', {}, { status: 'REJECTED' }],
   ])('for event name %s it renders %s', (eventName, text, oldValues, changedValues) => {
