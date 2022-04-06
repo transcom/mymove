2865b579a9dfa8e61e73453c7d646c888d610ae3
Felipe Lee <felipe@truss.works>
2022-04-06 19:50:28 +0000

    MB-12031 Set faker locale to be en_US

--- src/utils/test/factories/base.js
+++ src/utils/test/factories/base.js
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker';
 import { perBuild } from '@jackfranklin/test-data-bot';
 
+faker.setLocale('en_US');
+
 const fake = (callback) => {
   return perBuild(() => callback(faker));
 };
