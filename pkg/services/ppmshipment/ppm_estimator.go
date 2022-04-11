0cddc6b6d2f759b7e1d8503192158b1066f6dcb9
Namibia Torres <namibia@truss.works>
2022-04-11 14:28:35 -0700

    Fix test and estimator

--- pkg/services/ppmshipment/ppm_estimator.go
+++ pkg/services/ppmshipment/ppm_estimator.go
@@ -33,37 +33,18 @@ func (f *estimatePPM) estimateIncentive(appCtx appcontext.AppContext, oldPPMShip
 	if newPPMShipment.ID.IsNil() {
 		return nil, err
 	}
-	//oldPPMShipment, err := models.FetchPPMShipmentFromMTOShipmentID(appCtx.DB(), mtoShipmentID)
-	//if err != nil {
-	//	return nil, err
-	//}
-	// if etag.GenerateEtag(oldPPMShipment.UpdatedAt) != eTag {
-	// 	return nil, apperror.NewPreconditionFailedError(ppmShipment.ID, nil)
-	// }
 
-	// PPM Shipment Created:
-	// 1.  Check that shipment status is in DRAFT
 	// Use Case: Calculating the final incentive in the closeout flow, what is the status then?
 	// Might not need to block on teh PPM status.
 	if newPPMShipment.Status != models.PPMShipmentStatusDraft {
 		return nil, err
 	}
 	// 2. Check that all the fields we need are present.
-	err = validatePPMShipment(appCtx, *newPPMShipment, nil, &newPPMShipment.Shipment, checks...)
+	err = validatePPMShipment(appCtx, *newPPMShipment, &oldPPMShipment, &oldPPMShipment.Shipment, checks...)
 	if err != nil {
 		return nil, err
 	}
 
-	//if transactionError != nil {
-	//	return nil, transactionError
-	//}
-
-	// PPM Shipment Updated:
-	// 2. Check that all or some required fields are not present OR have not changed.
-	// 3. Skip the estimator and return log message with reason
-	// 4. Check that all the required fields are present AND have been updated to a value
-	//verrs := validate.NewErrors()
-	// 3. Move on to estimate incentive
 	if newPPMShipment.ExpectedDepartureDate == oldPPMShipment.ExpectedDepartureDate && newPPMShipment.PickupPostalCode == oldPPMShipment.PickupPostalCode && newPPMShipment.DestinationPostalCode == oldPPMShipment.DestinationPostalCode && newPPMShipment.AdvanceRequested == oldPPMShipment.AdvanceRequested && newPPMShipment.EstimatedWeight == oldPPMShipment.EstimatedWeight {
 		return oldPPMShipment.EstimatedIncentive, nil
 	}
@@ -71,14 +52,13 @@ func (f *estimatePPM) estimateIncentive(appCtx appcontext.AppContext, oldPPMShip
 	newPPMShipment.Advance = nil
 	// TODO: Call the pricer to calculate the incentive
 	newPPMShipment.EstimatedIncentive = models.Int32Pointer(int32(1000000))
+	// Saving Esimtated INcentive to DB
 	verrs, err := appCtx.DB().ValidateAndSave(newPPMShipment)
-
 	if verrs != nil && verrs.HasAny() {
 		return nil, apperror.NewInvalidInputError(newPPMShipment.ID, err, verrs, "Invalid input found while creating the Estimated Incentive.")
 	} else if err != nil {
 		return nil, apperror.NewQueryError("EstimatedIncentive", err, "")
 	}
+
 	return newPPMShipment.EstimatedIncentive, nil
-	// 4. Check that all the required fields are present AND have been updated to a value
-	// 5. estimate the incentive again and set old advance to nil, and advance_requested to false, if they existed:
 }
--- pkg/services/ppmshipment/ppm_estimator_test.go
+++ pkg/services/ppmshipment/ppm_estimator_test.go
@@ -1,41 +1,36 @@
 package ppmshipment
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/gofrs/uuid"
 
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/testdatagen"
 	"github.com/transcom/mymove/pkg/unit"
 )
 
-func createNewDefaultPPMShipment() *models.PPMShipment {
-	estimatedWeight := unit.Pound(5000)
-	ppmShipment := models.PPMShipment{
-		PickupPostalCode:      "20636",
-		DestinationPostalCode: "94040",
-		EstimatedWeight:       &estimatedWeight,
-	}
-	return &ppmShipment
-}
-
 func (suite *PPMShipmentSuite) TestEstimatedIncentive() {
 	suite.Run("Estimated Incentive - Success", func() {
 		oldPPMShipment := testdatagen.MakeDefaultPPMShipment(suite.DB())
 		ppmEstimator := NewEstimatePPM()
-		//ppmShipmentUpdater := NewPPMShipmentUpdater(ppmEstimator)
-		// Compare that the required fields have changed
-		// Check that we have an estimated incentive
-		newPPM := createNewDefaultPPMShipment()
-		ppmEstimate, err := ppmEstimator.EstimateIncentiveWithDefaultChecks(suite.AppContextForTest(), oldPPMShipment, newPPM)
-		//updatedPPMShipment, err := ppmShipmentUpdater.UpdatePPMShipmentWithDefaultCheck(suite.AppContextForTest(), newPPM, oldPPMShipment.ShipmentID)
-		// Might need to check that the estimated incentive is saved to the DB. LOOK AT EXAMPLES in other tests
-		fmt.Println("❤️")
-		fmt.Println(ppmEstimate)
-		fmt.Println("🔓")
+
+		estimatedWeight := unit.Pound(5000)
+		newPPM := models.PPMShipment{
+			ID:                    uuid.FromStringOrNil("575c25aa-b4eb-4024-9597-43483003c773"),
+			ShipmentID:            oldPPMShipment.ShipmentID,
+			Status:                "DRAFT",
+			ExpectedDepartureDate: time.Time(time.Date(2022, 12, 11, 0, 0, 0, 0, time.UTC)),
+			PickupPostalCode:      "20636",
+			DestinationPostalCode: "94040",
+			EstimatedWeight:       &estimatedWeight,
+		}
+
+		ppmEstimate, err := ppmEstimator.EstimateIncentiveWithDefaultChecks(suite.AppContextForTest(), oldPPMShipment, &newPPM)
 		suite.NilOrNoVerrs(err)
 		suite.NotEqualValues(oldPPMShipment.PickupPostalCode, newPPM.PickupPostalCode)
 		suite.NotEqualValues(oldPPMShipment.EstimatedWeight, newPPM.EstimatedWeight)
-		suite.Equal(int32(100000), ppmEstimate)
+		suite.Equal(int32(1000000), *ppmEstimate)
 	})
 	// Add unhappy path tests
 }
