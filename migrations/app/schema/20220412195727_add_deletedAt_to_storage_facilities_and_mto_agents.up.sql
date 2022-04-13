d480e7e0a2b8c7672f5923af5a00cfbad5596175
pearl-truss <pearl@truss.works>
2022-04-13 13:51:23 +0000

    add comments to alter table migration

--- migrations/app/schema/20220412195727_add_deletedAt_to_storage_facilities_and_mto_agents.up.sql
+++ migrations/app/schema/20220412195727_add_deletedAt_to_storage_facilities_and_mto_agents.up.sql
@@ -3,3 +3,6 @@ ADD deleted_at timestamp with time zone;
 
 ALTER TABLE storage_facilities
 ADD deleted_at timestamp with time zone;
+
+COMMENT ON COLUMN mto_agents.deleted_at IS 'Indicates whether the mto agent has been soft deleted or not, and when it was soft deleted.';
+COMMENT ON COLUMN storage_facilities.deleted_at IS 'Indicates whether the storage facility has been soft deleted or not, and when it was soft deleted.';
