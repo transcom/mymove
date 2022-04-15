43fb6ebc57637eb4430e512ad3214b7d10220568
leia-sefkin <leia.sefkin@truss.works>
2022-04-15 21:53:47 +0000

    remove access code from frontend

--- src/scenes/SystemAdmin/Home.jsx
+++ src/scenes/SystemAdmin/Home.jsx
@@ -5,7 +5,6 @@ import { createBrowserHistory } from 'history';
 import React from 'react';
 import Menu from './shared/Menu';
 import FOUOHeader from 'components/FOUOHeader';
-import AccessCodeList from './AccessCodes/AccessCodeList';
 import UploadShow from './Uploads/UploadShow';
 import OfficeUserList from 'pages/Admin/OfficeUsers/OfficeUserList';
 import OfficeUserShow from 'pages/Admin/OfficeUsers/OfficeUserShow';
@@ -95,8 +94,7 @@ const Home = () => (
         show={TSPPShow}
       />
       <Resource name="electronic_orders" options={{ label: 'Electronic orders' }} list={ElectronicOrderList} />
-      <Resource name="access_codes" options={{ label: 'Access codes' }} list={AccessCodeList} />
-      <Resource name="uploads" options={{ label: 'Search Upload by ID' }} show={UploadShow} />
+      å <Resource name="uploads" options={{ label: 'Search Upload by ID' }} show={UploadShow} />
       <Resource name="organizations" />
       <Resource name="notifications" options={{ label: 'Notifications' }} list={NotificationList} />
       <Resource
