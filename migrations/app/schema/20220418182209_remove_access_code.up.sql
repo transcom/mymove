7cb6a04d6a13e2dcefa6c44d9780b8a8fd183354
leia-sefkin <leia.sefkin@truss.works>
2022-04-18 21:38:22 +0000

    archiving data before removal

--- migrations/app/schema/20220418182209_remove_access_code.up.sql
+++ migrations/app/schema/20220418182209_remove_access_code.up.sql
@@ -1,3 +1,12 @@
-DROP TABLE IF EXISTS ACCESS_CODES CASCADE;
+CREATE TABLE archived_access_codes (
+  LIKE access_codes
+  INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES
+);
 
-ALTER TABLE SERVICE_MEMBERS DROP IF EXISTS REQUIRES_ACCESS_CODE CASCADE;
+ALTER TABLE archived_access_codes ADD CONSTRAINT archived_access_codes_service_member_id FOREIGN KEY (service_member_id) REFERENCES service_members(id);
+
+INSERT INTO archived_access_codes SELECT * FROM access_codes;
+
+DROP TABLE access_codes CASCADE;
+
+ALTER TABLE service_members DROP IF EXISTS requires_access_code CASCADE;
