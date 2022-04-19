2735a3cf8a6a50dbc4a02576a244cc38f3aaf05c
Yan Zhao <yan.zhao@truss.works>
2022-04-19 22:56:42 +0000

    Remove unused methods and their associated tests
    - all these methods were not being used anywhere else in repo
    - remove any associated tests

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,20 +3,10 @@ import path from 'path';
 import moment from 'moment';
 import numeral from 'numeral';
 
-import { SHIPMENT_OPTIONS } from 'shared/constants';
-import { DEPARTMENT_INDICATOR_LABELS, DEPARTMENT_INDICATOR_OPTIONS } from 'constants/departmentIndicators';
+import { DEPARTMENT_INDICATOR_OPTIONS } from 'constants/departmentIndicators';
 import { SERVICE_MEMBER_AGENCY_LABELS } from 'content/serviceMemberAgencies';
-import { SERVICE_COUNSELING_MOVE_STATUS_OPTIONS, MOVE_STATUS_OPTIONS } from 'constants/queues';
 import { ORDERS_TYPE_OPTIONS, ORDERS_TYPE_DETAILS_OPTIONS } from 'constants/orders';
 import { PAYMENT_REQUEST_STATUS_LABELS } from 'constants/paymentRequestStatus';
-
-export function formatNumber(num) {
-  if (!Number.isFinite(num)) {
-    return '';
-  }
-
-  return num.toLocaleString();
-}
 
 /**
  * Formats number into a dollar string. Eg. $1,234.12
@@ -29,20 +19,6 @@ export function toDollarString(num) {
   return numeral(num).format('$0,0.00');
 }
 
-// Format user-entered base quantity into base quantity, e.g. 16.7000 -> 167000
-export function formatToBaseQuantity(baseQuantity) {
-  return parseFloat(String(baseQuantity).replace(',', '')) * 10000;
-}
-
-// Format a base quantity into a user-friendly number, e.g. 167000 -> 16.7
-export function convertFromBaseQuantity(baseQuantity) {
-  if (!Number.isFinite(baseQuantity)) {
-    return null;
-  }
-
-  return baseQuantity / 10000;
-}
-
 // Format a thousandth of an inch into an inch, e.g. 16700 -> 16.7
 export function convertFromThousandthInchToInch(thousandthInch) {
   if (!Number.isFinite(thousandthInch)) {
@@ -52,35 +28,9 @@ export function convertFromThousandthInchToInch(thousandthInch) {
   return thousandthInch / 1000;
 }
 
-// Format a dimensions object length, width and height to inches
-export function formatToDimensionsInches(dimensions) {
-  if (!dimensions) {
-    return undefined;
-  }
-
-  return {
-    length: convertFromThousandthInchToInch(dimensions.length),
-    width: convertFromThousandthInchToInch(dimensions.width),
-    height: convertFromThousandthInchToInch(dimensions.height),
-  };
-}
-
 // Format user-entered dimension into base dimension, e.g. 15.25 -> 15250
 export function formatToThousandthInches(val) {
   return parseFloat(String(val).replace(',', '')) * 1000;
-}
-
-// Format dimensions object length, width and height to base dimensions
-export function formatDimensionsToThousandthInches(dimensions) {
-  if (!dimensions) {
-    return undefined;
-  }
-
-  return {
-    length: formatToThousandthInches(dimensions.length),
-    width: formatToThousandthInches(dimensions.width),
-    height: formatToThousandthInches(dimensions.height),
-  };
 }
 
 // Service Member Formatters
@@ -155,49 +105,12 @@ export function formatDateFromIso(date, outputFormat) {
   return formatDate(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ', outputFormat);
 }
 
-export function formatDate4DigitYear(date) {
-  if (date) {
-    return moment(date).format('DD-MMM-YYYY');
-  }
-  return undefined;
-}
-
-export function formatTime(date) {
-  if (date) {
-    return moment(date).format('HH:mm');
-  }
-  return undefined;
-}
-
 // Format a date and include its time, e.g. 03-Jan-2018 21:23
 export function formatDateTime(date) {
   if (date) {
     return moment(date).format('DD-MMM-YY HH:mm');
   }
   return undefined;
-}
-
-// Format a date, include its time and timezone, e.g. 03-Jan-2018 21:23 ET
-export function formatDateTimeWithTZ(date) {
-  if (!date) return undefined;
-
-  // This gets us a date string that includes the browser timezone
-  // e.g. Mon Apr 22 2019 09:08:10 GMT-0500 (Central Daylight Time)
-  // If this looks a bit strange, it's a workaround for IE11 not
-  // supporting the timeZoneName: 'short' option in Date.toLocaleString
-  const newDateString = String(moment(date).toDate());
-  const longZone = newDateString.substring(newDateString.lastIndexOf('(') + 1, newDateString.lastIndexOf(')'));
-  let shortZone = longZone
-    .split(' ')
-    .map((word) => word[0])
-    .join('');
-
-  // Converting timezones like CDT and EST to CT and ET
-  if (shortZone.length === 3 && shortZone !== 'UTC') {
-    shortZone = shortZone.slice(0, 1) + shortZone.slice(2, 3);
-  }
-
-  return `${moment(date, moment.ISO_8601, true).format('DD-MMM-YY HH:mm')} ${shortZone}`;
 }
 
 export function formatTimeAgo(date) {
@@ -216,22 +129,6 @@ export const formatToOrdinal = (n) => {
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
 
-// Map shipment types to friendly display names for mto shipments
-export const mtoShipmentTypeToFriendlyDisplay = (shipmentType) => {
-  switch (shipmentType) {
-    case SHIPMENT_OPTIONS.HHG:
-      return 'Household goods';
-    case SHIPMENT_OPTIONS.NTSR:
-      return 'NTS release';
-    case SHIPMENT_OPTIONS.HHG_LONGHAUL_DOMESTIC:
-      return 'Household goods longhaul domestic';
-    case SHIPMENT_OPTIONS.HHG_SHORTHAUL_DOMESTIC:
-      return 'Household goods shorthaul domestic';
-    default:
-      return shipmentType;
-  }
-};
-
 export const departmentIndicatorReadable = (departmentIndicator, missingText) => {
   if (!departmentIndicator) {
     return missingText;
@@ -239,20 +136,8 @@ export const departmentIndicatorReadable = (departmentIndicator, missingText) =>
   return DEPARTMENT_INDICATOR_OPTIONS[`${departmentIndicator}`] || departmentIndicator;
 };
 
-export const departmentIndicatorLabel = (departmentIndicator) => {
-  return DEPARTMENT_INDICATOR_LABELS[`${departmentIndicator}`] || departmentIndicator;
-};
-
 export const serviceMemberAgencyLabel = (agency) => {
   return SERVICE_MEMBER_AGENCY_LABELS[`${agency}`] || agency;
-};
-
-export const moveStatusLabel = (status) => {
-  return MOVE_STATUS_OPTIONS.find((option) => option.value === `${status}`)?.label || status;
-};
-
-export const serviceCounselingMoveStatusLabel = (status) => {
-  return SERVICE_COUNSELING_MOVE_STATUS_OPTIONS.find((option) => option.value === `${status}`)?.label || status;
 };
 
 export const ordersTypeReadable = (ordersType, missingText) => {
@@ -291,33 +176,6 @@ export const dropdownInputOptions = (options) => {
   return Object.entries(options).map(([key, value]) => ({ key, value }));
 };
 
-// adds commas to numberString w/o removeing .0000 from the end of the string or rounding
-export const addCommasToNumberString = (numOrString, decimalPlaces = 0) => {
-  let numToFixed = numOrString;
-  if (!numOrString || numOrString === '0') {
-    numToFixed = (0).toFixed(decimalPlaces);
-  }
-
-  const str = numToFixed.toString();
-  const [wholeNum, decimalNum] = str.split('.');
-  const wholeNumInt = parseInt(wholeNum, 10);
-  if (decimalNum) {
-    return `${wholeNumInt.toLocaleString()}.${decimalNum}`;
-  }
-  return wholeNumInt.toLocaleString();
-};
-
-// truncate a number and return appropiate decimal places... (watch out for negitive numbers: floor(-5.1) === -6)
-// see test for examples of how this works
-export const truncateNumber = (num, decimalPlaces = 0) => {
-  if (!num) return num;
-
-  const floatNum = parseFloat(num).toFixed(4);
-  const scale = 10 ** decimalPlaces;
-  const truncatedNbr = Math.floor(floatNum * scale) / scale;
-  return truncatedNbr.toFixed(decimalPlaces).toString();
-};
-
 // Formats the numeric age input to a human readable string. Eg. 1.5 = 1 day, 2.5 = 2 days
 export const formatAgeToDays = (age) => {
   if (age < 1) {
@@ -328,19 +186,6 @@ export const formatAgeToDays = (age) => {
   }
   return `${Math.floor(age)} days`;
 };
-
-// Format orders type (ex: PERMANENT_CHANGE_OF_STATION => Permanent change of station)
-export function formatOrderType(orderType) {
-  return orderType
-    .split('_')
-    .map((str, i) => {
-      if (i === 0) {
-        return str[0] + str.slice(1).toLowerCase();
-      }
-      return str.toLowerCase();
-    })
-    .join(' ');
-}
 
 // Format dates for customer app (ex. 25 Dec 2020)
 export function formatCustomerDate(date) {
@@ -454,29 +299,12 @@ export function formatCents(cents, minimumFractionDigits = 2, maximumFractionDig
   return (cents / 100).toLocaleString(undefined, { minimumFractionDigits, maximumFractionDigits });
 }
 
-// Format base quantity as cents
-export function formatBaseQuantityAsDollars(baseQuantity) {
-  return formatCents(baseQuantity / 100);
-}
-
 export function formatCentsRange(min, max) {
   if (!Number.isFinite(min) || !Number.isFinite(max)) {
     return '';
   }
 
   return `$${formatCents(min)} - ${formatCents(max)}`;
-}
-
-// Format a base quantity into a user-friendly number string, e.g. 167000 -> "16.7000"
-export function formatFromBaseQuantity(baseQuantity) {
-  if (!Number.isFinite(baseQuantity)) {
-    return '';
-  }
-
-  return (baseQuantity / 10000).toLocaleString(undefined, {
-    minimumFractionDigits: 4,
-    maximumFractionDigits: 4,
-  });
 }
 
 // Formats a numeric value amount in the default locale with configurable options
