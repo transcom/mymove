7b31d9969130b1003977640e4c167d98b787dcfb
pearl-truss <pearl@truss.works>
2022-04-22 14:21:56 +0000

    clean up storybook stories for shipmentList

--- src/components/ShipmentList/ShipmentList.stories.jsx
+++ src/components/ShipmentList/ShipmentList.stories.jsx
@@ -4,85 +4,6 @@ import ShipmentList from './ShipmentList';
 
 import { SHIPMENT_OPTIONS } from 'shared/constants';
 
-export const Basic = () => (
-  <div className="grid-container">
-    <h3>Single Shipment</h3>
-    <ShipmentList shipments={[{ id: '0001', shipmentType: SHIPMENT_OPTIONS.PPM }]} onShipmentClick={() => {}} />
-    <br />
-    <h3>Multiple shipments</h3>
-    <ShipmentList
-      shipments={[
-        { id: '0001', shipmentType: SHIPMENT_OPTIONS.HHG },
-        { id: '0002', shipmentType: SHIPMENT_OPTIONS.NTS },
-        { id: '0003', shipmentType: SHIPMENT_OPTIONS.PPM },
-      ]}
-      onShipmentClick={() => {}}
-    />
-  </div>
-);
-
-export const ShipmentListWithWeights = () => (
-  <div className="grid-container">
-    <h3>Single Shipment</h3>
-    <ShipmentList
-      shipments={[
-        {
-          id: '0001',
-          shipmentType: SHIPMENT_OPTIONS.HHG,
-          calculatedBillableWeight: 4600,
-          estimatedWeight: 5000,
-          primeEstimatedWeight: 300,
-          reweigh: { id: '1236', weight: 200 },
-        },
-      ]}
-      showShipmentWeight
-    />
-    <br />
-    <h3>Multiple shipments</h3>
-    <ShipmentList
-      shipments={[
-        { id: '0001', shipmentType: SHIPMENT_OPTIONS.HHG, calculatedBillableWeight: 6161, estimatedWeight: 5600 },
-        { id: '0002', shipmentType: SHIPMENT_OPTIONS.HHG, calculatedBillableWeight: 3200, reweigh: { id: '1234' } },
-        {
-          id: '0003',
-          shipmentType: SHIPMENT_OPTIONS.HHG,
-          calculatedBillableWeight: 3400,
-          estimatedWeight: 5000,
-          primeEstimatedWeight: 300,
-          reweigh: { id: '1236', weight: 200 },
-        },
-      ]}
-      showShipmentWeight
-    />
-  </div>
-);
-
-export const ShipmentListWithPPMs = () => (
-  <div className="grid-container">
-    <h3>Incomplete and Complete PPMs</h3>
-    <ShipmentList
-      shipments={[
-        {
-          id: '0001',
-          shipmentType: SHIPMENT_OPTIONS.PPM,
-          ppmShipment: {
-            id: 'completePPM',
-            advanceRequested: false,
-          },
-        },
-        {
-          id: '0002',
-          shipmentType: SHIPMENT_OPTIONS.PPM,
-          ppmShipment: {
-            id: 'completePPM',
-            advanceRequested: null,
-          },
-        },
-      ]}
-    />
-  </div>
-);
-
 export default {
   title: 'Components / ShipmentList',
   component: ShipmentList,
@@ -164,3 +85,26 @@ WithWeightsMultiple.args = {
   showShipmentWeight: true,
 };
 WithWeightsMultiple.decorators = generateDecorator('Multiple Shipments');
+
+export const WithPPMs = Template.bind({});
+WithPPMs.args = {
+  shipments: [
+    {
+      id: '0001',
+      shipmentType: SHIPMENT_OPTIONS.PPM,
+      ppmShipment: {
+        id: 'completePPM',
+        advanceRequested: false,
+      },
+    },
+    {
+      id: '0002',
+      shipmentType: SHIPMENT_OPTIONS.PPM,
+      ppmShipment: {
+        id: 'completePPM',
+        advanceRequested: null,
+      },
+    },
+  ],
+};
+WithPPMs.decorators = generateDecorator('With PPM Shipments');
