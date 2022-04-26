2c298b81c8eb0cd8e4adfc150cbe140876da6383
kennethchow-truss <kenneth.chow@truss.works>
2022-04-26 09:48:23 -0700

    fixed front end test

--- src/constants/moveHistoryEventTemplate.test.js
+++ src/constants/moveHistoryEventTemplate.test.js
@@ -72,9 +72,11 @@ describe('moveHistoryEventTemplate', () => {
   describe('when given a Create basic service item history record', () => {
     const item = {
       action: 'INSERT',
-      context: {
-        name: 'Domestic linehaul',
-      },
+      context: [
+        {
+          name: 'Domestic linehaul',
+        },
+      ],
       eventName: 'updateMoveTaskOrderStatus',
       tableName: 'mto_service_items',
     };
