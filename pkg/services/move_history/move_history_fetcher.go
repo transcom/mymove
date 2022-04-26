b3640104dcac5a188c4d4eada24b1f0f65619858
Roger Steve Ruiz <rogeruiz@truss.works>
2022-04-26 18:38:05 +0000

    Bring in the new SQL statement from comment;
    See the comment here.
    https://github.com/transcom/mymove/pull/8411/files#r856439856
    
    Co-authored-by: Donald Thai <donald@truss.works>

--- pkg/services/move_history/move_history_fetcher.go
+++ pkg/services/move_history/move_history_fetcher.go
@@ -58,25 +58,32 @@ func (f moveHistoryFetcher) FetchMoveHistory(appCtx appcontext.AppContext, param
 	),
 	orders AS (
 		SELECT
-			orders.*,
-			lookup_dl_origin_name."name" AS origin_duty_location_name,
-			lookup_dl_new_name."name" AS new_duty_location_name
+			orders.*
 		FROM
 			orders
 		JOIN moves ON moves.orders_id = orders.id
-		JOIN duty_locations AS lookup_dl_origin_name ON orders.origin_duty_location_id = lookup_dl_origin_name.id
-		JOIN duty_locations AS lookup_dl_new_name ON orders.new_duty_location_id = lookup_dl_new_name.id
 		WHERE orders.id = (SELECT moves.orders_id FROM moves)
 	),
+	-- Context is null if empty, {}, object
+    -- Joining the jsonb changed_data for every record to surface duty location ids.
+    -- Left join duty_locations since we don't expect origin/new duty locations to change every time.
+    -- Convert changed_data.origin_duty_location_id and changed_data.new_duty_location_id to UUID type to take advantage of indexing.
 	orders_logs AS (
 		SELECT
 			audit_history.*,
-			json_build_object('origin_duty_location_name', orders.origin_duty_location_name, 'new_duty_location_name', orders.new_duty_location_name)::TEXT AS context,
-			NULL AS context_id
+			NULLIF(
+				jsonb_strip_nulls(
+					jsonb_build_object('origin_duty_location_name', old_duty.name, 'new_duty_location_name', new_duty.name)
+				)::TEXT, '{}'::TEXT
+			) AS context,
+ 			NULL AS context_id
 		FROM
 			audit_history
 		JOIN orders ON orders.id = audit_history.object_id
 			AND audit_history."table_name" = 'orders'
+		JOIN jsonb_to_record(audit_history.changed_data) as c(origin_duty_location_id TEXT, new_duty_location_id TEXT) on TRUE
+		LEFT JOIN duty_locations AS old_duty on uuid(c.origin_duty_location_id) = old_duty.id
+		LEFT JOIN duty_locations AS new_duty on uuid(c.new_duty_location_id) = new_duty.id
 	),
 	service_items AS (
 		SELECT
