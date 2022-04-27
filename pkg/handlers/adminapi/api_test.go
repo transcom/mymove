1e6b855f854b041327f52c251f089847951469b7
Sandy Wright <sandy@truss.works>
2022-04-27 15:12:38 -0500

    Remove conflict with package name suite

--- pkg/handlers/adminapi/api_test.go
+++ pkg/handlers/adminapi/api_test.go
@@ -19,8 +19,8 @@ type HandlerSuite struct {
 }
 
 // AfterTest completes tests by trying to close open files
-func (suite *HandlerSuite) AfterTest() {
-	for _, file := range suite.TestFilesToClose() {
+func (hs *HandlerSuite) AfterTest() {
+	for _, file := range hs.TestFilesToClose() {
 		//RA Summary: gosec - errcheck - Unchecked return value
 		//RA: Linter flags errcheck error: Ignoring a method's return value can cause the program to overlook unexpected states and conditions.
 		//RA: Functions with unchecked return values in the file are used to close a local server connection to ensure a unit test server is not left running indefinitely
