4d555bbebbd0aa906c3bb0048aa6ed7ca5f4725a
Namibia Torres <namibia@truss.works>
2022-05-06 07:53:10 -0700

    comment out ppmservices to see if it breaks anything.

--- pkg/services/ppmservices.go
+++ pkg/services/ppmservices.go
@@ -1,16 +1,17 @@
 package services
 
-import (
-	"github.com/gofrs/uuid"
-
-	"github.com/transcom/mymove/pkg/appcontext"
-	"github.com/transcom/mymove/pkg/rateengine"
-
-	"github.com/transcom/mymove/pkg/models"
-)
-
-// EstimateCalculator is the exported interface for calculating the PPM estimate
-//go:generate mockery --name EstimateCalculator --disable-version-string
-type EstimateCalculator interface {
-	CalculateEstimates(appCtx appcontext.AppContext, ppm *models.PersonallyProcuredMove, moveID uuid.UUID) (int64, rateengine.CostComputation, error)
-}
+//
+//import (
+//	"github.com/gofrs/uuid"
+//
+//	"github.com/transcom/mymove/pkg/appcontext"
+//	"github.com/transcom/mymove/pkg/rateengine"
+//
+//	"github.com/transcom/mymove/pkg/models"
+//)
+//
+//// EstimateCalculator is the exported interface for calculating the PPM estimate
+////go:generate mockery --name EstimateCalculator --disable-version-string
+//type EstimateCalculator interface {
+//	CalculateEstimates(appCtx appcontext.AppContext, ppm *models.PersonallyProcuredMove, moveID uuid.UUID) (int64, rateengine.CostComputation, error)
+//}
