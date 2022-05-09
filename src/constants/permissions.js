5a2f8196c66cb24dbafd6b0e40aaa2035976ebee
PDickeyTruss <pdickey@truss.works>
2022-05-09 15:20:37 -0600

    Add initial permission consts

---
+++ src/constants/permissions.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const permissionTypes = {
+  editShipment: 'edit.shipment',
+  editOrders: 'edit.orders',
+  editMaxBillableWeight: 'edit.max_billable_weight',
+  editFinancialReviewFlag: 'edit.financial_review_flag',
+};
