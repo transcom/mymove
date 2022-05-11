759f4e8bafc0a048cb79f2c34e4ff0e8e200b59c
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-11 21:55:48 +0000

    Treat blank ModifiedBy as MilMove system;
    In planning today, @kennethchow-truss mentioned that we could just check
    for the lack of data for ModifiedBy to show that the MilMove system has
    modified the database. This is an implementation of that plan.

--- src/pages/Office/MoveHistory/ModifiedBy.jsx
+++ src/pages/Office/MoveHistory/ModifiedBy.jsx
@@ -3,20 +3,34 @@ import { string } from 'prop-types';
 
 import styles from './ModifiedBy.module.scss';
 
-const ModifiedBy = ({ firstName, lastName, email, phone }) => (
-  <div className={styles.ModifiedBy}>
-    {lastName && firstName && (
-      <span className={styles.name}>
-        {lastName}, {firstName}
-      </span>
-    )}
-    {email && phone && (
-      <div className={styles.contactInfo}>
-        {email} | {phone}
+const ModifiedBy = ({ firstName, lastName, email, phone }) => {
+  // If an event is modified by the MilMove system, it will contain no
+  // information. This is used to idetify moves that were modified by the
+  // MilMove system itself.
+  if (firstName === '' && lastName === '' && email === '' && phone === '') {
+    const systemName = 'MilMove';
+    return (
+      <div className={styles.ModifiedBy}>
+        <span className={styles.name}>{systemName}</span>
       </div>
-    )}
-  </div>
-);
+    );
+  }
+
+  return (
+    <div className={styles.ModifiedBy}>
+      {lastName && firstName && (
+        <span className={styles.name}>
+          {lastName}, {firstName}
+        </span>
+      )}
+      {email && phone && (
+        <div className={styles.contactInfo}>
+          {email} | {phone}
+        </div>
+      )}
+    </div>
+  );
+};
 
 ModifiedBy.defaultProps = {
   firstName: '',
