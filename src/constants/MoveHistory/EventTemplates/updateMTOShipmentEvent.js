f236493f47279345a6d280382ab13738f621c59b
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-19 23:01:28 +0000

    Add all the event templates we need;
    This list was generated from the working Array example in the
    moveHistoryEventTemplates.js.
    
    Co-Authored-By: Yan Zhao <yan.zhao@truss.works>

---
+++ src/constants/MoveHistory/EventTemplates/updateMTOShipmentEvent.js
@@ -0,0 +1,18 @@
+import detailsTypes from 'constants/MoveHistory/DetailsColumn/Types';
+import moveHistoryOperations from 'constants/MoveHistory/Operations';
+import dbActions from 'constants/MoveHistory/Database/Actions';
+import dbTables from 'constants/MoveHistory/Database/Tables';
+
+export default {
+  action: dbActions.UPDATE,
+  eventName: moveHistoryOperations.updateMTOShipment,
+  tableName: dbTables.mto_shipments,
+  detailsType: detailsTypes.LABELED,
+  getEventNameDisplay: () => 'Updated shipment',
+  getDetailsLabeledDetails: (historyRecord) => {
+    return {
+      shipment_type: historyRecord.oldValues.shipment_type,
+      ...historyRecord.changedValues,
+    };
+  },
+};
