913ae3bfe3ec18ca16c3ddb57ae7d2c3aa6d4fb7
Yan Zhao <yan.zhao@truss.works>
2022-05-19 18:19:29 +0000

    Remove name from the default export since it's no longer needed

--- src/constants/MoveHistory/Database/orders.js
+++ src/constants/MoveHistory/Database/orders.js
@@ -6,11 +6,9 @@ import {
 } from 'constants/orders';
 
 // This is to map the human-readable text to the options
-const optionFields = {
+export default {
   ...ORDERS_BRANCH_OPTIONS,
   ...ORDERS_TYPE_DETAILS_OPTIONS,
   ...ORDERS_TYPE_OPTIONS,
   ...ORDERS_RANK_OPTIONS,
 };
-
-export default optionFields;
