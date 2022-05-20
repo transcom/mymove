84442a9f57c6f35d72f8d801f0d31d94e7adcc84
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-20 05:54:11 +0000

    Refactor createStandardServiceItem event

--- src/constants/MoveHistory/EventTemplates/index.js
+++ src/constants/MoveHistory/EventTemplates/index.js
@@ -8,7 +8,7 @@ export { default as createMTOShipmentAgent } from './createMTOShipmentAgent';
 export { default as createOrders } from './createOrders';
 export { default as createPaymentRequestReweighUpdate } from './createPaymentRequestReweighUpdate';
 export { default as createPaymentRequestShipmentUpdate } from './createPaymentRequestShipmentUpdate';
-// export { default as createStandardServiceItem } from './createStandardServiceItem';
+export { default as createStandardServiceItem } from './createStandardServiceItem';
 // export { default as requestShipmentCancellation } from './requestShipmentCancellation';
 // export { default as requestShipmentDiversion } from './requestShipmentDiversion';
 // export { default as requestShipmentReweigh } from './requestShipmentReweigh';
