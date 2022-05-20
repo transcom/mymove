bc20589fa48d283bba8ae0455d37c8263ba7ca8d
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-20 13:30:56 +0000

    Refactor submitMoveForApproval event;
    This test file was removed here as there we will be tackling it in
    another ticket, MB-12471.

--- src/constants/MoveHistory/EventTemplates/submitMoveForApproval.js
+++ src/constants/MoveHistory/EventTemplates/submitMoveForApproval.js
@@ -1,31 +1,11 @@
-import detailsTypes from 'constants/MoveHistory/UIDisplay/DetailsTypes';
-import dbTables from 'constants/MoveHistory/Database/Tables';
+import o from 'constants/MoveHistory/UIDisplay/Operations';
+import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 
 export default {
-  action: null,
-  eventName: null,
-  tableName: null,
-  detailsType: detailsTypes.PLAIN_TEXT,
-  getEventNameDisplay: ({ tableName }) => {
-    switch (tableName) {
-      case dbTables.orders:
-        return 'Updated order';
-      case dbTables.mto_service_items:
-        return 'Updated service item';
-      case dbTables.entitlements:
-        return 'Updated allowances';
-      case dbTables.payment_requests:
-        return 'Updated payment request';
-      case dbTables.mto_shipments:
-      case dbTables.mto_agents:
-      case dbTables.addresses:
-        return 'Updated shipment';
-      case dbTables.moves:
-      default:
-        return 'Updated move';
-    }
-  },
-  getDetailsPlainText: () => {
-    return '-';
-  },
+  action: '*',
+  eventName: o.submitMoveForApproval,
+  tableName: '*',
+  detailsType: d.PLAIN_TEXT,
+  getEventNameDisplay: () => 'Submitted move',
+  getDetailsPlainText: () => '-',
 };
