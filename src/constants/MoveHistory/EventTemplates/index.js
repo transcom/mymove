355ec61f1b80420f869de02c00673be8e68e06fc
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-20 15:59:51 +0000

    Refactor updatePaymentRequestStatus event;
    This test file was removed because we're working on tests like these in
    another ticket, MB-12471.

--- src/constants/MoveHistory/EventTemplates/index.js
+++ src/constants/MoveHistory/EventTemplates/index.js
@@ -26,7 +26,7 @@ export { default as updateMTOShipmentAgent } from './updateMTOShipmentAgent';
 export { default as updateMTOShipmentDeprecatePaymentRequest } from './updateMTOShipmentDeprecatePaymentRequest';
 export { default as updateOrder } from './updateOrder';
 export { default as updatePaymentRequest } from './updatePaymentRequest';
-// export { default as updatePaymentRequestStatus } from './updatePaymentRequestStatus';
+export { default as updatePaymentRequestStatus } from './updatePaymentRequestStatus';
 // export { default as updateServiceItemStatus } from './updateServiceItemStatus';
 export { default as updateMTOReviewedBillableWeightsAt } from './updateMTOReviewedBillableWeightsAt';
 export { default as uploadAmendedOrders } from './uploadAmendedOrders';
