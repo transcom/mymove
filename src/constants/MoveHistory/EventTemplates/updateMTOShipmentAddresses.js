cae679d2042dd5da565831328347fe27a3ac36b4
Roger Steve Ruiz <rogeruiz@truss.works>
2022-05-20 15:14:53 +0000

    Refactor updateMTOShipmentAddresses event

--- src/constants/MoveHistory/EventTemplates/updateMTOShipmentAddresses.js
+++ src/constants/MoveHistory/EventTemplates/updateMTOShipmentAddresses.js
@@ -1,31 +1,43 @@
-import detailsTypes from 'constants/MoveHistory/UIDisplay/DetailsTypes';
-import dbTables from 'constants/MoveHistory/Database/Tables';
+import o from 'constants/MoveHistory/UIDisplay/Operations';
+import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import a from 'constants/MoveHistory/Database/Actions';
+import t from 'constants/MoveHistory/Database/Tables';
+import { formatMoveHistoryFullAddress } from 'utils/formatters';
 
 export default {
-  action: null,
-  eventName: null,
-  tableName: null,
-  detailsType: detailsTypes.PLAIN_TEXT,
-  getEventNameDisplay: ({ tableName }) => {
-    switch (tableName) {
-      case dbTables.orders:
-        return 'Updated order';
-      case dbTables.mto_service_items:
-        return 'Updated service item';
-      case dbTables.entitlements:
-        return 'Updated allowances';
-      case dbTables.payment_requests:
-        return 'Updated payment request';
-      case dbTables.mto_shipments:
-      case dbTables.mto_agents:
-      case dbTables.addresses:
-        return 'Updated shipment';
-      case dbTables.moves:
-      default:
-        return 'Updated move';
+  action: a.UPDATE,
+  eventName: o.updateMTOShipment,
+  tableName: t.addresses,
+  detailsType: d.LABELED,
+  getEventNameDisplay: () => 'Updated shipment',
+  getDetailsLabeledDetails: ({ oldValues, changedValues, context }) => {
+    let newChangedValues = {
+      street_address_1: oldValues.street_address_1,
+      street_address_2: oldValues.street_address_2,
+      city: oldValues.city,
+      state: oldValues.state,
+      postal_code: oldValues.postal_code,
+      ...changedValues,
+    };
+
+    const address = formatMoveHistoryFullAddress(newChangedValues);
+
+    const addressType = context.filter((contextObject) => contextObject.address_type)[0].address_type;
+
+    let addressLabel = '';
+    if (addressType === 'pickupAddress') {
+      addressLabel = 'pickup_address';
+    } else if (addressType === 'destinationAddress') {
+      addressLabel = 'destination_address';
     }
-  },
-  getDetailsPlainText: () => {
-    return '-';
+
+    newChangedValues = {
+      shipment_type: context[0]?.shipment_type,
+      ...changedValues,
+    };
+
+    newChangedValues[addressLabel] = address;
+
+    return newChangedValues;
   },
 };
