e8a4a0a231a46e288fd720eb03fa07bdfaf46eee
kennethchow-truss <kenneth.chow@truss.works>
2022-05-24 11:16:21 -0700

    Fixing the payment_request

--- src/constants/MoveHistory/EventTemplates/reweighPaymentRequest.test.js
+++ src/constants/MoveHistory/EventTemplates/reweighPaymentRequest.test.js
@@ -6,8 +6,10 @@ describe('reweighs update', () => {
   const item = {
     action: 'UPDATE',
     eventName: o.updateReweigh,
-    tableName: 'reweighs',
-    context: [{ shipment_type: 'HHG', payment_request_number: '0288-7994-1' }],
+    tableName: 'payment_requests',
+    context: [{ shipment_type: 'HHG' }],
+    changedValues: { recalculation_of_payment_request_id: '1234' },
+    oldValues: { payment_request_number: '0288-7994-1' },
   };
   it('correctly matches the reweigh payment request', () => {
     const result = getTemplate(item);
