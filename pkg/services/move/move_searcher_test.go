71c5ed4d65624cc1fe6b757302f2f21ad77dccaf
Marty Boren <marty@truss.works>
2022-05-25 04:28:05 +0000

    Fix failing server tests
    I was trying to share some db records between tests, but they were
    getting killed by transactions between the tests, so i just recreated
    them in each test.

--- pkg/services/move/move_searcher_test.go
+++ pkg/services/move/move_searcher_test.go
@@ -6,31 +6,53 @@ import (
 )
 
 func (suite *MoveServiceSuite) TestMoveSearch() {
-	firstMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
-		Locator: "AAAAAA",
-	}})
-	secondMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
-		Locator: "BBBBBB",
-	}})
 	searcher := NewMoveSearcher()
 
 	suite.Run("search with no filters should fail", func() {
+		testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "AAAAAA",
+		}})
+		testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "BBBBBB",
+		}})
+
 		_, err := searcher.SearchMoves(suite.AppContextForTest(), nil, nil)
 		suite.Error(err)
 	})
 	suite.Run("search with valid locator", func() {
+		firstMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "AAAAAA",
+		}})
+		testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "BBBBBB",
+		}})
+
 		moves, err := searcher.SearchMoves(suite.AppContextForTest(), &firstMove.Locator, nil)
 		suite.NoError(err)
 		suite.Len(moves, 1)
 		suite.Equal(firstMove.Locator, moves[0].Locator)
 	})
 	suite.Run("search with valid DOD ID", func() {
+		testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "AAAAAA",
+		}})
+		secondMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "BBBBBB",
+		}})
+
 		moves, err := searcher.SearchMoves(suite.AppContextForTest(), nil, secondMove.Orders.ServiceMember.Edipi)
 		suite.NoError(err)
 		suite.Len(moves, 1)
 		suite.Equal(secondMove.Locator, moves[0].Locator)
 	})
 	suite.Run("search with both DOD ID and locator filters should fail", func() {
+		firstMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "AAAAAA",
+		}})
+		secondMove := testdatagen.MakeMove(suite.DB(), testdatagen.Assertions{Move: models.Move{
+			Locator: "BBBBBB",
+		}})
+
 		// Search for Locator of one move and DOD ID of another move
 		_, err := searcher.SearchMoves(suite.AppContextForTest(), &firstMove.Locator, secondMove.Orders.ServiceMember.Edipi)
 		suite.Error(err)
