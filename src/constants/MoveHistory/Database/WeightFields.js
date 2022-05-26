a66fa14d49891a6e51fc15ef1bbbe931b2359067
kennethchow-truss <kenneth.chow@truss.works>
2022-05-26 09:52:35 -0700

    Weight fields

--- src/constants/MoveHistory/Database/WeightFields.js
+++ src/constants/MoveHistory/Database/WeightFields.js
@@ -4,5 +4,6 @@ export default {
   authorized_weight: 'authorized_weight',
   pro_gear_weight: 'pro_gear_weight',
   pro_gear_weight_spouse: 'pro_gear_weight_spouse',
+  prime_actual_weight: 'prime_actual_weight',
   required_medical_equipment_weight: 'required_medical_equipment_weight',
 };
