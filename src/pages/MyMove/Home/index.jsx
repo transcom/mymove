e85053025188adecb53ceea90e0b11292a85ac15
Roger Steve Ruiz <rogeruiz@truss.works>
2022-06-03 14:52:17 +0000

    Refactor Class declarations & sort them;
    This change was needed because of ESLint warnings around
    `class-methods-use-this` and `react/sort-comp`.

--- src/pages/MyMove/Home/index.jsx
+++ src/pages/MyMove/Home/index.jsx
@@ -71,6 +71,13 @@ Description.defaultProps = {
 };
 
 export class Home extends Component {
+  static sortAllShipments = (mtoShipments) => {
+    const allShipments = JSON.parse(JSON.stringify(mtoShipments));
+    allShipments.sort((a, b) => moment(a.createdAt) - moment(b.createdAt));
+
+    return allShipments;
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -95,6 +102,11 @@ export class Home extends Component {
       getSignedCertification(move.id);
     }
   }
+
+  static handlePrintLegalese = (e) => {
+    e.preventDefault();
+    window.print();
+  };
 
   get hasOrders() {
     const { orders, uploadedOrderDocuments } = this.props;
@@ -286,18 +298,6 @@ export class Home extends Component {
       mtoShipmentId: shipmentId,
     });
     history.push(path);
-  };
-
-  sortAllShipments = (mtoShipments) => {
-    const allShipments = JSON.parse(JSON.stringify(mtoShipments));
-    allShipments.sort((a, b) => moment(a.createdAt) - moment(b.createdAt));
-
-    return allShipments;
-  };
-
-  handlePrintLegalese = (e) => {
-    e.preventDefault();
-    window.print();
   };
 
   render() {
