7fd23833bc642cb3d834f6a32d4937052d4a26f9
Roger Steve Ruiz <rogeruiz@truss.works>
2022-06-08 20:29:08 +0000

    Update test and app to use a data-testid;
    Rather than an HTML element like the h4.

--- cypress/integration/milmove/ppms/review.js
+++ cypress/integration/milmove/ppms/review.js
@@ -101,7 +101,7 @@ function verifyPPMShipmentCard(shipmentCardFields, isEditable = false) {
   cy.get('@moveSetup')
     .next()
     .within(() => {
-      cy.get('h4').contains('PPM 1');
+      cy.get('[data-testid="ShipmentCardNumber"]').contains('PPM 1');
 
       if (isEditable) {
         cy.get('button').contains('Edit');
