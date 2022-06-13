162a0fcc246cc7a429885fc1366796a7aafd944d
Yan Zhao <yan.zhao@truss.works>
2022-06-13 22:19:10 +0000

    Revert "Adjust createPaymentRequest event to catch more cases"
    This reverts commit 8d66996af961b6d1378ae26c65a41f34c9010273.

--- src/constants/MoveHistory/EventTemplates/createPaymentRequest.js
+++ src/constants/MoveHistory/EventTemplates/createPaymentRequest.js
@@ -1,10 +1,11 @@
+import o from 'constants/MoveHistory/UIDisplay/Operations';
 import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
 
 export default {
   action: a.INSERT,
-  eventName: '*',
+  eventName: o.createPaymentRequest,
   tableName: t.payment_requests,
   detailsType: d.LABELED_PAYMENT_REQUEST,
   getEventNameDisplay: ({ changedValues }) => `Submitted payment request ${changedValues?.payment_request_number}`,
