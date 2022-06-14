5a8820037a103c1a7c4d598c517f3693d698ba54
Namibia Torres <namibia@truss.works>
2022-06-14 16:07:33 -0700

    refactor about.js

--- cypress/integration/milmove/ppms/about.js
+++ cypress/integration/milmove/ppms/about.js
@@ -5,7 +5,7 @@ import {
   signInAndNavigateToAboutPageWithoutAdvance,
 } from '../../../support/ppmCustomerShared';
 
-describe('About Your PPM - With Advances', function () {
+describe('About Your PPM', function () {
   before(() => {
     cy.prepareCustomerApp();
   });
@@ -16,12 +16,13 @@ describe('About Your PPM - With Advances', function () {
   });
   // Get another user for desktop
   const viewportType = [
-    { viewport: 'desktop', isMobile: false, userId: 'c28b2eb1-975f-49f7-b8a3-c7377c0da908' }, // readyToFinish2@ppm.approved
-    { viewport: 'mobile', isMobile: true, userId: 'c28b2eb1-975f-49f7-b8a3-c7377c0da908' }, // readyToFinish2@ppm.approved
+    { viewport: 'desktop', isMobile: false },
+    { viewport: 'mobile', isMobile: true },
   ];
 
   viewportType.forEach(({ viewport, isMobile, userId }) => {
     it(`can upload with an advance - ${viewport}`, () => {
+      userId = 'c28b2eb1-975f-49f7-b8a3-c7377c0da908'; // readyToFinish2@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
@@ -29,24 +30,10 @@ describe('About Your PPM - With Advances', function () {
       signInAndNavigateToAboutPageWithAdvance(userId);
     });
   });
-});
-
-describe('About Your PPM - Without Advances', function () {
-  before(() => {
-    cy.prepareCustomerApp();
-  });
-
-  beforeEach(() => {
-    cy.intercept('GET', '**/internal/moves/**/mto_shipments').as('getShipment');
-    cy.intercept('PATCH', '**/internal/mto-shipments/**').as('patchShipment');
-  });
-  const viewportType = [
-    { viewport: 'desktop', isMobile: false, userId: 'cde987a1-a717-4a61-98b5-1f05e2e0844d' }, // readyToFinish@ppm.approved
-    { viewport: 'mobile', isMobile: true, userId: 'cde987a1-a717-4a61-98b5-1f05e2e0844d' }, // readyToFinish@ppm.approved
-  ];
 
   viewportType.forEach(({ viewport, isMobile, userId }) => {
     it(`can upload without an advance - ${viewport}`, () => {
+      userId = 'cde987a1-a717-4a61-98b5-1f05e2e0844d'; // readyToFinish@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
