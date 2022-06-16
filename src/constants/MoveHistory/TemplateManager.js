bc0369eec482fca3d132a9702712312bb224f04d
Yan Zhao <yan.zhao@truss.works>
2022-06-16 21:46:01 +0000

    Remove defaulting of the action and the table name for the template manager
    - events will fill out the table name and template manager as needed

--- src/constants/MoveHistory/TemplateManager.js
+++ src/constants/MoveHistory/TemplateManager.js
@@ -5,9 +5,9 @@ import * as eventTemplates from 'constants/MoveHistory/EventTemplates';
 const allMoveHistoryEventTemplates = [];
 
 const registerTemplate = ({
-  action = '*',
+  action,
   eventName,
-  tableName = '*',
+  tableName,
   detailsType = detailsTypes.PLAIN_TEXT,
   getEventNameDisplay = () => {
     return 'Undefined event type';
