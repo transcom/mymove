7823f495ab9eb77586f88c3b4e4c67b9fac3e215
Michael Valdes <michael.valde@truss.works>
2022-06-21 16:59:39 +0000

    Extract function navigateToMove for TOO tests

--- cypress/integration/office/txo/tooFlows.js
+++ cypress/integration/office/txo/tooFlows.js
@@ -1,4 +1,5 @@
 import { TOOOfficeUserType } from '../../../support/constants';
+import { navigateToMove } from './tooNavigateToMove';
 
 describe('TOO user', () => {
   before(() => {
@@ -442,20 +443,3 @@ describe('TOO user', () => {
     cy.contains('The service member is unable to move into their new home at the expected time');
   });
 });
-
-function navigateToMove(moveLocator) {
-  // TOO Moves queue
-  cy.wait(['@getSortedOrders']);
-  cy.get('input[name="locator"]').as('moveCodeFilterInput');
-
-  // type in move code/locator to filter
-  cy.get('@moveCodeFilterInput').type(moveLocator).blur();
-  cy.get('tbody > tr').as('results');
-
-  // click result to navigate to move details page
-  cy.get('@results').first().click();
-  cy.url().should('include', `/moves/${moveLocator}/details`);
-
-  // Move Details page
-  cy.wait(['@getMoves', '@getOrders', '@getMTOShipments', '@getMTOServiceItems']);
-}
