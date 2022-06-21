bb2dab6c22dbfbec7352077c220802d9175986ba
Michael Valdes <michael.valde@truss.works>
2022-06-21 15:13:56 +0000

    Fix flaky moveSearch test
    A wait was added at the end of the final test in order to ensure that
    the page is finished loading before moving on to the next test.

--- cypress/integration/office/qaecsr/moveSearch.js
+++ cypress/integration/office/qaecsr/moveSearch.js
@@ -8,6 +8,10 @@ describe('QAE/CSR Move Search', () => {
   beforeEach(() => {
     cy.intercept('**/ghc/v1/swagger.yaml').as('getGHCClient');
     cy.intercept('**/ghc/v1/moves/search').as('getSearchResults');
+    cy.intercept('**/ghc/v1/move/**').as('getMoves');
+    cy.intercept('**/ghc/v1/orders/**').as('getOrders');
+    cy.intercept('**/ghc/v1/move_task_orders/**/mto_shipments').as('getMTOShipments');
+    cy.intercept('**/ghc/v1/move_task_orders/**/mto_service_items').as('getMTOServiceItems');
 
     const userId = '2419b1d6-097f-4dc4-8171-8f858967b4db';
     cy.apiSignInAsUser(userId, QAECSROfficeUserType);
@@ -27,6 +31,7 @@ describe('QAE/CSR Move Search', () => {
     // click result to navigate to move details page
     cy.get('@results').first().click();
     cy.url().should('include', `/moves/${moveLocator}/details`);
+    cy.wait(['@getMoves', '@getOrders', '@getMTOShipments', '@getMTOServiceItems']);
   });
   it('is able to search by DOD ID', () => {
     const moveLocator = 'QAEHLP';
@@ -46,5 +51,6 @@ describe('QAE/CSR Move Search', () => {
     // click result to navigate to move details page
     cy.get('@results').first().click();
     cy.url().should('include', `/moves/${moveLocator}/details`);
+    cy.wait(['@getMoves', '@getOrders', '@getMTOShipments', '@getMTOServiceItems']);
   });
 });
