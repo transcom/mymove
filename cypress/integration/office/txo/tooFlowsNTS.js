3b95df6ad3218e3eb8b54f8266c6f51e9a56d209
Michael Valdes <michael.valde@truss.works>
2022-06-21 15:24:55 +0000

    Fix flaky tooFlowsNTS tests
    I added some steps to the navigateToMove function to make it match other
    similar methods in other TOO tests.

--- cypress/integration/office/txo/tooFlowsNTS.js
+++ cypress/integration/office/txo/tooFlowsNTS.js
@@ -423,7 +423,14 @@ function editTacSac() {
 function navigateToMove(moveLocator) {
   // TOO Moves queue
   cy.wait(['@getSortedOrders']);
-  cy.contains(moveLocator).click();
+  cy.get('input[name="locator"]').as('moveCodeFilterInput');
+
+  // type in move code/locator to filter
+  cy.get('@moveCodeFilterInput').type(moveLocator).blur();
+  cy.get('tbody > tr').as('results');
+
+  // click result to navigate to move details page
+  cy.get('@results').first().click();
   cy.url().should('include', `/moves/${moveLocator}/details`);
 
   // Move Details page
