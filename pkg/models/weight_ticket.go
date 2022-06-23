4d73e8e128d9eec98abdd57702aaa9bb396b85e2
Felipe Lee <felipe@truss.works>
2022-06-23 23:25:32 +0000

    MB-12926 Make Empty/Full doc fields on `WeightTicket` concrete types
    I'd initially made them pointers to try to let us be flexible and make things simpler, but in talking about validation more thoroughly, it just added work. If we ever do need to change things, we can switch this back.

--- pkg/models/weight_ticket.go
+++ pkg/models/weight_ticket.go
@@ -9,8 +9,7 @@ import (
 )
 
 // WeightTicket represents the weight tickets and related data for a single trip of a PPM Shipment. Each trip should be
-// its own record. EmptyDocumentID and FullDocumentID are pointers, but based on our plan for how we'll be using this
-// model, they should be filled in. Mainly made pointers to give us wiggle room if our implementation changes.
+// its own record.
 type WeightTicket struct {
 	ID                                uuid.UUID   `json:"id" db:"id"`
 	PPMShipmentID                     uuid.UUID   `json:"ppm_shipment_id" db:"ppm_shipment_id"`
@@ -21,12 +20,12 @@ type WeightTicket struct {
 	VehicleDescription                *string     `json:"vehicle_description" db:"vehicle_description"`
 	EmptyWeight                       *unit.Pound `json:"empty_weight" db:"empty_weight"`
 	HasEmptyWeightTicket              *bool       `json:"has_empty_weight_ticket" db:"has_empty_weight_ticket"`
-	EmptyDocumentID                   *uuid.UUID  `json:"empty_document_id" db:"empty_document_id"`
-	EmptyDocument                     *Document   `belongs_to:"documents" fk_id:"empty_document_id"`
+	EmptyDocumentID                   uuid.UUID   `json:"empty_document_id" db:"empty_document_id"`
+	EmptyDocument                     Document    `belongs_to:"documents" fk_id:"empty_document_id"`
 	FullWeight                        *unit.Pound `json:"full_weight" db:"full_weight"`
 	HasFullWeightTicket               *bool       `json:"has_full_weight_ticket" db:"has_full_weight_ticket"`
-	FullDocumentID                    *uuid.UUID  `json:"full_document_id" db:"full_document_id"`
-	FullDocument                      *Document   `belongs_to:"documents" fk_id:"full_document_id"`
+	FullDocumentID                    uuid.UUID   `json:"full_document_id" db:"full_document_id"`
+	FullDocument                      Document    `belongs_to:"documents" fk_id:"full_document_id"`
 	OwnsTrailer                       *bool       `json:"owns_trailer" db:"owns_trailer"`
 	TrailerMeetsCriteria              *bool       `json:"trailer_meets_criteria" db:"trailer_meets_criteria"`
 	ProofOfTrailerOwnershipDocumentID *uuid.UUID  `json:"proof_of_trailer_ownership_document_id" db:"proof_of_trailer_ownership_document_id"`
