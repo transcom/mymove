1537bcc9cac56669a92f7f3b10e8854419c6bcbd
Marty Boren <marty@truss.works>
2022-06-23 22:24:46 +0000

    Implement DeleteCustomerSupportRemark service

--- pkg/services/customer_support_remarks/customer_support_remarks_deleter.go
+++ pkg/services/customer_support_remarks/customer_support_remarks_deleter.go
@@ -1,7 +1,14 @@
 package customersupportremarks
 
 import (
+	"database/sql"
+	"errors"
+
 	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/apperror"
+	"github.com/transcom/mymove/pkg/db/utilities"
+	"github.com/transcom/mymove/pkg/models"
 
 	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/services"
@@ -15,5 +22,35 @@ func NewCustomerSupportRemarkDeleter() services.CustomerSupportRemarkDeleter {
 }
 
 func (o customerSupportRemarkDeleter) DeleteCustomerSupportRemark(appCtx appcontext.AppContext, customerSupportRemarkID uuid.UUID) error {
+	var remark models.CustomerSupportRemark
+	err := appCtx.DB().Q().Scope(utilities.ExcludeDeletedScope()).Find(&remark, customerSupportRemarkID)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return apperror.NewNotFoundError(customerSupportRemarkID, "attempting to delete CustomerSupportRemark")
+		default:
+			return apperror.NewQueryError("CustomerSupportRemark", err, "")
+		}
+	}
+
+	transactionError := appCtx.NewTransaction(func(txnAppCtx appcontext.AppContext) error {
+		err := utilities.SoftDestroy(appCtx.DB(), &remark)
+		// TODO is this error handling here necessary? can we just bubble the error up?
+		if err != nil {
+			switch err.Error() {
+			case "error updating model":
+				return apperror.NewUnprocessableEntityError("while updating model")
+			case "this model does not have deleted_at field":
+				return apperror.NewPreconditionFailedError(remark.ID, errors.New("model or sub table missing deleted_at field"))
+			default:
+				return apperror.NewInternalServerError("failed attempt to soft delete model")
+			}
+		}
+		return nil
+	})
+
+	if transactionError != nil {
+		return transactionError
+	}
 	return nil
 }
--- pkg/services/customer_support_remarks/customer_support_remarks_deleter_test.go
+++ pkg/services/customer_support_remarks/customer_support_remarks_deleter_test.go
@@ -1,1 +1,42 @@
 package customersupportremarks
+
+import (
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/apperror"
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *CustomerSupportRemarksSuite) TestCustomerSupportRemarkDeleter() {
+	deleter := NewCustomerSupportRemarkDeleter()
+
+	suite.Run("delete existing remark", func() {
+		remark := testdatagen.MakeDefaultCustomerSupportRemark(suite.DB())
+		suite.NoError(deleter.DeleteCustomerSupportRemark(suite.AppContextForTest(), remark.ID))
+		var dbRemark models.CustomerSupportRemark
+		err := suite.DB().Find(&dbRemark, remark.ID)
+		suite.NoError(err)
+		suite.NotNil(dbRemark.DeletedAt)
+	})
+
+	suite.Run("Returns an error when delete non-existent remark", func() {
+		uuid := uuid.Must(uuid.NewV4())
+		err := deleter.DeleteCustomerSupportRemark(suite.AppContextForTest(), uuid)
+		suite.Error(err)
+		suite.IsType(apperror.NotFoundError{}, err)
+
+	})
+
+	suite.Run("Returns an error when attempting to delete an already deleted remark", func() {
+		remark := testdatagen.MakeDefaultCustomerSupportRemark(suite.DB())
+		suite.NoError(deleter.DeleteCustomerSupportRemark(suite.AppContextForTest(), remark.ID))
+		var dbRemark models.CustomerSupportRemark
+		err := suite.DB().Find(&dbRemark, remark.ID)
+		suite.NoError(err)
+		suite.NotNil(dbRemark.DeletedAt)
+		err = deleter.DeleteCustomerSupportRemark(suite.AppContextForTest(), remark.ID)
+		suite.Error(err)
+		suite.IsType(apperror.NotFoundError{}, err)
+	})
+}
