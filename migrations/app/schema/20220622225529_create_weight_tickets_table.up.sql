7534886208fa8c216044e4d23f2a14d459c31987
Felipe Lee <felipe@truss.works>
2022-06-23 19:20:25 +0000

    MB-12926 Add ppm shipment id index & add comments on fk cols
    Also move `created_at`, `updated_at`, and `deleted_at` columns to the end of the `CREATE TABLE` statement

--- migrations/app/schema/20220622225529_create_weight_tickets_table.up.sql
+++ migrations/app/schema/20220622225529_create_weight_tickets_table.up.sql
@@ -1,12 +1,9 @@
 CREATE TABLE weight_tickets
 (
-	id uuid PRIMARY KEY NOT NULL,
+	id uuid PRIMARY KEY,
 	ppm_shipment_id uuid NOT NULL
 		CONSTRAINT weight_tickets_ppm_shipments_id_fkey
 		REFERENCES ppm_shipments,
-	created_at timestamp NOT NULL,
-	updated_at timestamp NOT NULL,
-	deleted_at timestamptz,
 	vehicle_description varchar,
 	empty_weight int,
 	has_empty_weight_ticket bool,
@@ -22,16 +19,24 @@ CREATE TABLE weight_tickets
 	trailer_meets_criteria bool,
 	proof_of_trailer_ownership_document_id uuid
 		CONSTRAINT weight_tickets_proof_of_trailer_ownership_document_id_fkey
-		REFERENCES documents
+		REFERENCES documents,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL,
+	deleted_at timestamptz
 );
 
+CREATE INDEX weight_tickets_ppm_shipment_id_idx ON weight_tickets USING hash (ppm_shipment_id);
 CREATE INDEX weight_tickets_deleted_at_idx ON weight_tickets USING btree (deleted_at);
 
 COMMENT on TABLE weight_tickets IS 'Stores weight ticket docs associated with a trip for a PPM shipment.';
+COMMENT on COLUMN weight_tickets.ppm_shipment_id IS 'The ID of the PPM shipment that this set of weight tickets is for.';
 COMMENT on COLUMN weight_tickets.vehicle_description IS 'Stores a description of the vehicle used for the trip. E.g. make/model, type of truck/van, etc.';
 COMMENT on COLUMN weight_tickets.empty_weight IS 'Stores the weight of the vehicle when empty.';
 COMMENT on COLUMN weight_tickets.has_empty_weight_ticket IS 'Indicates if the customer has a weight ticket for the vehicle weight when empty.';
+COMMENT on COLUMN weight_tickets.empty_document_id IS 'The ID of the document that is associated with the user uploads containing the empty vehicle weight.';
 COMMENT on COLUMN weight_tickets.full_weight IS 'Stores the weight of the vehicle when full.';
 COMMENT on COLUMN weight_tickets.has_full_weight_ticket IS 'Indicates if the customer has a weight ticket for the vehicle weight when full.';
+COMMENT on COLUMN weight_tickets.empty_document_id IS 'The ID of the document that is associated with the user uploads containing the full vehicle weight.';
 COMMENT on COLUMN weight_tickets.owns_trailer IS 'Indicates if the customer used a trailer they own for the move.';
 COMMENT on COLUMN weight_tickets.trailer_meets_criteria IS 'Indicates if the trailer that the customer used meets all the criteria to be claimable.';
+COMMENT on COLUMN weight_tickets.proof_of_trailer_ownership_document_id IS 'The ID of the document that is associated with the user uploads containing the proof of trailer ownership.';
