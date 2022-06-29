e839924c85d46ba1f71867cfadf2114d9cdd8f3f
Michael Valdes <michael.valde@truss.works>
2022-06-29 15:11:51 +0000

    Add a countdown timer to log out on inactivity

--- src/layout/LogoutOnInactivity.jsx
+++ src/layout/LogoutOnInactivity.jsx
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useIdleTimer } from 'react-idle-timer';
 
 import Alert from 'shared/Alert';
 
 const maxIdleTimeInSeconds = 14 * 60;
-// const maxWarningTimeBeforeTimeoutInSeconds = 60;
+const maxWarningTimeBeforeTimeoutInSeconds = 60;
 const maxIdleTimeInMilliseconds = maxIdleTimeInSeconds * 1000;
 const keepAliveEndpoint = '/internal/users/logged_in';
 
 const LogoutOnInactivity = () => {
   const [isIdle, setIsIdle] = useState(false);
+  const [timeLeftInSeconds, setTimeLeftInSeconds] = useState(maxWarningTimeBeforeTimeoutInSeconds);
+  const timerRef = useRef(null);
 
   const handleOnActive = () => {
     setIsIdle(false);
+    clearInterval(timerRef.current);
+    setTimeLeftInSeconds(maxWarningTimeBeforeTimeoutInSeconds);
     fetch(keepAliveEndpoint);
   };
 
   const handleOnIdle = () => {
     setIsIdle(true);
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(() => {
+      setTimeLeftInSeconds((current) => {
+        return current - 1;
+      });
+    }, 1000);
   };
 
   useIdleTimer({
@@ -28,14 +38,24 @@ const LogoutOnInactivity = () => {
     events: ['blur', 'focus', 'mousedown', 'touchstart', 'MSPointerDown'],
   });
 
+  useEffect(() => {
+    if (isIdle) {
+      if (timeLeftInSeconds <= 0) {
+        clearInterval(timerRef.current);
+        // log out
+      }
+    }
+  }, [isIdle, timeLeftInSeconds]);
+
   return (
     <div data-testid="logoutOnInactivityWrapper">
-      <h1>Time left in seconds: {}</h1>
+      <h1>Time left in seconds: {timeLeftInSeconds}</h1>
       <h1>Idle: {isIdle.toString()}</h1>
       {isIdle && (
         <div data-testid="logoutAlert">
           <Alert data-testid="logoutAlert" type="warning" heading="Inactive user">
-            You have been inactive and will be logged out in {} seconds unless you touch or click on the page.
+            You have been inactive and will be logged out in {timeLeftInSeconds} seconds unless you touch or click on
+            the page.
           </Alert>
         </div>
       )}
