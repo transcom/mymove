badea8b747350b55f4f37a655f60dacfafd90a4b
Michael Valdes <michael.valde@truss.works>
2022-06-29 15:16:51 +0000

    Add logout functionality to LogoutOnInactivity

--- src/layout/LogoutOnInactivity.jsx
+++ src/layout/LogoutOnInactivity.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useIdleTimer } from 'react-idle-timer';
+import { useSelector, useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 
+import { logOut } from 'store/auth/actions';
+import { selectIsLoggedIn } from 'store/auth/selectors';
 import Alert from 'shared/Alert';
+import { LogoutUser } from 'utils/api';
 
 const maxIdleTimeInSeconds = 14 * 60;
 const maxWarningTimeBeforeTimeoutInSeconds = 60;
@@ -12,22 +18,29 @@ const LogoutOnInactivity = () => {
   const [isIdle, setIsIdle] = useState(false);
   const [timeLeftInSeconds, setTimeLeftInSeconds] = useState(maxWarningTimeBeforeTimeoutInSeconds);
   const timerRef = useRef(null);
+  const history = useHistory();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
 
   const handleOnActive = () => {
-    setIsIdle(false);
-    clearInterval(timerRef.current);
-    setTimeLeftInSeconds(maxWarningTimeBeforeTimeoutInSeconds);
-    fetch(keepAliveEndpoint);
+    if (isLoggedIn) {
+      setIsIdle(false);
+      clearInterval(timerRef.current);
+      setTimeLeftInSeconds(maxWarningTimeBeforeTimeoutInSeconds);
+      fetch(keepAliveEndpoint);
+    }
   };
 
   const handleOnIdle = () => {
-    setIsIdle(true);
-    clearInterval(timerRef.current);
-    timerRef.current = setInterval(() => {
-      setTimeLeftInSeconds((current) => {
-        return current - 1;
-      });
-    }, 1000);
+    if (isLoggedIn) {
+      setIsIdle(true);
+      clearInterval(timerRef.current);
+      timerRef.current = setInterval(() => {
+        setTimeLeftInSeconds((current) => {
+          return current - 1;
+        });
+      }, 1000);
+    }
   };
 
   useIdleTimer({
@@ -36,28 +49,46 @@ const LogoutOnInactivity = () => {
     onActive: handleOnActive,
     onIdle: handleOnIdle,
     events: ['blur', 'focus', 'mousedown', 'touchstart', 'MSPointerDown'],
+    startOnMount: true,
   });
 
   useEffect(() => {
     if (isIdle) {
-      if (timeLeftInSeconds <= 0) {
+      if (isLoggedIn && timeLeftInSeconds <= 0) {
         clearInterval(timerRef.current);
-        // log out
+        dispatch(push(logOut));
+        LogoutUser().then((r) => {
+          const redirectURL = r.body;
+          if (redirectURL) {
+            window.location.href = redirectURL;
+          } else {
+            history.push({
+              pathname: '/sign-in',
+              state: { hasLoggedOut: true },
+            });
+          }
+        });
       }
     }
-  }, [isIdle, timeLeftInSeconds]);
+  }, [isIdle, isLoggedIn, timeLeftInSeconds, history, dispatch]);
 
   return (
     <div data-testid="logoutOnInactivityWrapper">
-      <h1>Time left in seconds: {timeLeftInSeconds}</h1>
-      <h1>Idle: {isIdle.toString()}</h1>
-      {isIdle && (
-        <div data-testid="logoutAlert">
-          <Alert data-testid="logoutAlert" type="warning" heading="Inactive user">
-            You have been inactive and will be logged out in {timeLeftInSeconds} seconds unless you touch or click on
-            the page.
-          </Alert>
-        </div>
+      {isLoggedIn && (
+        <>
+          {/* testing - to remove */}
+          <h1>Time left in seconds: {timeLeftInSeconds}</h1>
+          <h1>Idle: {isIdle.toString()}</h1>
+          <h1>Logged in: {isLoggedIn.toString()}</h1>
+          {isIdle && (
+            <div data-testid="logoutAlert">
+              <Alert data-testid="logoutAlert" type="warning" heading="Inactive user">
+                You have been inactive and will be logged out in {timeLeftInSeconds} seconds unless you touch or click
+                on the page.
+              </Alert>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
