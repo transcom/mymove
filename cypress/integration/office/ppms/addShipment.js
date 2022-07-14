ec84266429e6daf91d65159574d9b8560da658fa
JP <jenniferlynparsons@gmail.com>
2022-07-14 12:59:25 -0400

    Add additional intercepts to confirm page load

--- cypress/integration/office/ppms/addShipment.js
+++ cypress/integration/office/ppms/addShipment.js
@@ -64,7 +64,7 @@ describe('Services counselor user', () => {
     cy.get('input[name="spouseProGearWeight"]').type(500).blur();
 
     cy.get('button[data-testid="submitForm"]:enabled').click();
-    cy.wait('@createShipment');
+    cy.wait(['@createShipment', '@getMTOShipments', '@getMoves', '@getOrders']);
 
     // Fill out page two
     cy.get('input[name="advance"]').clear().type('2100').blur();
