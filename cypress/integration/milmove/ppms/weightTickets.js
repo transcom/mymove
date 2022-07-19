9cb53d42845e91d84524fa311ff7fb4604cb270b
Pearl Truss <pearl@truss.works>
2022-07-19 14:48:38 +0000

    update viewports to test flows with different users

--- cypress/integration/milmove/ppms/weightTickets.js
+++ cypress/integration/milmove/ppms/weightTickets.js
@@ -15,13 +15,12 @@ describe('Weight Tickets', function () {
   });
 
   const viewportType = [
-    { viewport: 'desktop', isMobile: false },
-    { viewport: 'mobile', isMobile: true },
+    { viewport: 'desktop', isMobile: false, userId: '88007896-6ae7-4600-866a-873d3bc67fd3' }, // actualPPMDateZIPAdvanceDone@ppm.approved
+    { viewport: 'mobile', isMobile: true, userId: '88007896-6ae7-4600-866a-873d3bc67fd3' }, // actualPPMDateZIPAdvanceDone@ppm.approved
   ];
 
   viewportType.forEach(({ viewport, isMobile, userId }) => {
     it(`validation errors - ${viewport}`, () => {
-      userId = '88007896-6ae7-4600-866a-873d3bc67fd3'; // actualPPMDateZIPAdvanceDone@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
@@ -32,7 +31,6 @@ describe('Weight Tickets', function () {
 
   viewportType.forEach(({ viewport, isMobile, userId }) => {
     it(`proceed with weight ticket documents - ${viewport}`, () => {
-      userId = '88007896-6ae7-4600-866a-873d3bc67fd3'; // actualPPMDateZIPAdvanceDone@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
@@ -42,9 +40,14 @@ describe('Weight Tickets', function () {
     });
   });
 
-  viewportType.forEach(({ viewport, isMobile, userId }) => {
+  // TODO: update mobile user to a unique user
+  const viewportType2 = [
+    { viewport: 'desktop', isMobile: false, userId: '22dba194-3d9a-49c6-8328-718dd945292f' }, // actualPPMDateZIPAdvanceDone2@ppm.approved
+    { viewport: 'mobile', isMobile: true, userId: '88007896-6ae7-4600-866a-873d3bc67fd3' }, // actualPPMDateZIPAdvanceDone@ppm.approved
+  ];
+
+  viewportType2.forEach(({ viewport, isMobile, userId }) => {
     it(`proceed without weight tickets (constructed weight) - ${viewport}`, () => {
-      userId = '22dba194-3d9a-49c6-8328-718dd945292f'; // actualPPMDateZIPAdvanceDone2@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
@@ -53,9 +56,14 @@ describe('Weight Tickets', function () {
     });
   });
 
-  viewportType.forEach(({ viewport, isMobile, userId }) => {
+  // TODO: update mobile user to a unique user
+  const viewportType3 = [
+    { viewport: 'desktop', isMobile: false, userId: '9ec731d8-f347-4d34-8b54-4ce9e6ea3282' }, // actualPPMDateZIPAdvanceDone3@ppm.approved
+    { viewport: 'mobile', isMobile: true, userId: '88007896-6ae7-4600-866a-873d3bc67fd3' }, // actualPPMDateZIPAdvanceDone@ppm.approved
+  ];
+
+  viewportType3.forEach(({ viewport, isMobile, userId }) => {
     it(`proceed with claiming trailer - ${viewport}`, () => {
-      userId = '9ec731d8-f347-4d34-8b54-4ce9e6ea3282'; // actualPPMDateZIPAdvanceDone3@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
@@ -64,9 +72,14 @@ describe('Weight Tickets', function () {
     });
   });
 
-  viewportType.forEach(({ viewport, isMobile, userId }) => {
+  // TODO: update mobile user to a unique user
+  const viewportType4 = [
+    { viewport: 'desktop', isMobile: false, userId: '2a0146c4-ec9a-4efc-a94c-6c2849c3e167' }, // actualPPMDateZIPAdvanceDone3@ppm.approved
+    { viewport: 'mobile', isMobile: true, userId: '88007896-6ae7-4600-866a-873d3bc67fd3' }, // actualPPMDateZIPAdvanceDone@ppm.approved
+  ];
+
+  viewportType4.forEach(({ viewport, isMobile, userId }) => {
     it(`proceed without claiming trailer - ${viewport}`, () => {
-      userId = '2a0146c4-ec9a-4efc-a94c-6c2849c3e167'; // actualPPMDateZIPAdvanceDone4@ppm.approved
       if (isMobile) {
         setMobileViewport();
       }
