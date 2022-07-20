333c3b9e8f8394a3d5e797536cde5af1e6d63bfe
PDickeyTruss <pdickey@truss.works>
2022-07-20 00:13:42 +0000

    Fronted side of cancel eval report

--- src/pages/Office/EvaluationReports/EvaluationReports.jsx
+++ src/pages/Office/EvaluationReports/EvaluationReports.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { Button, Grid, GridContainer } from '@trussworks/react-uswds';
+
+import evaluationReportsStyles from './EvaluationReports.module.scss';
 
 import { useEvaluationReportsQueries } from 'hooks/queries';
 import ShipmentEvaluationReports from 'components/Office/EvaluationReportTable/ShipmentEvaluationReports';
 import EvaluationReportTable from 'components/Office/EvaluationReportTable/EvaluationReportTable';
 import LoadingPlaceholder from 'shared/LoadingPlaceholder';
 import SomethingWentWrong from 'shared/SomethingWentWrong';
+import Alert from 'shared/Alert';
 
 const EvaluationReports = () => {
   const { moveCode } = useParams();
+  const location = useLocation();
+
   const { shipmentEvaluationReports, counselingEvaluationReports, shipments, isLoading, isError } =
     useEvaluationReportsQueries(moveCode);
 
@@ -22,6 +27,11 @@ const EvaluationReports = () => {
 
   return (
     <GridContainer>
+      {location.state?.showDeleteSuccess && (
+        <div className={evaluationReportsStyles.alert}>
+          <Alert type="success">Your report has been canceled</Alert>
+        </div>
+      )}
       <Grid row>
         <h1>Quality assurance reports</h1>
       </Grid>
--- src/pages/Office/ShipmentEvaluationReport/ShipmentEvaluationReport.jsx
+++ src/pages/Office/ShipmentEvaluationReport/ShipmentEvaluationReport.jsx
@@ -1,56 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'styles/office.scss';
 import { GridContainer, Grid, Button } from '@trussworks/react-uswds';
 import classnames from 'classnames';
-import { useParams } from 'react-router';
+import { useParams, useHistory } from 'react-router';
+import { queryCache, useMutation } from 'react-query';
 
 import styles from '../TXOMoveInfo/TXOTab.module.scss';
 
 import shipmentEvaluationReportStyles from './ShipmentEvaluationReport.module.scss';
 
-// import Alert from 'shared/Alert';
+import ConnectedDeleteEvaluationReportConfirmationModal from 'components/ConfirmationModals/DeleteEvaluationReportConfirmationModal';
+import { deleteEvaluationReport } from 'services/ghcApi';
+import { SHIPMENT_EVALUATION_REPORTS } from 'constants/queryKeys';
 
 const mtoRefId = 'TODO'; // move?.referenceId
 
 const ShipmentEvaluationReport = () => {
   const { moveCode, reportId } = useParams();
+  const history = useHistory();
 
+  const [deleteEvaluationReportMutation] = useMutation(deleteEvaluationReport, {
+    onSuccess: async () => {
+      await queryCache.invalidateQueries([SHIPMENT_EVALUATION_REPORTS, moveCode]);
+    },
+  });
+
+  const [isDeleteModelOpen, setIsDeleteModelOpen] = useState(false);
+
+  const toggleCancelModel = () => {
+    setIsDeleteModelOpen(!isDeleteModelOpen);
+  };
+
+  const cancelReport = async () => {
+    // Close the modal
+    setIsDeleteModelOpen(!isDeleteModelOpen);
+
+    // Mark as deleted in database
+    await deleteEvaluationReportMutation(reportId);
+
+    // Reroute back to eval report page, include flag to know to show alert
+    history.push(`/moves/${moveCode}/evaluation-reports`, { showDeleteSuccess: true });
+  };
   return (
-    <div className={classnames(styles.tabContent, shipmentEvaluationReportStyles.tabContent)}>
-      <GridContainer>
-        <Grid row>
-          <Grid col desktop={{ col: 8, offset: 2 }}>
-            {/* {showDeletionSuccess && <Alert type="success">Your remark has been deleted.</Alert>} */}
-            <h1>Shipment report</h1>
-            <div className={styles.pageHeaderDetails}>
-              <h6>REPORT ID #{reportId}</h6>
-              <h6>MOVE CODE {moveCode}</h6>
-              <h6>MTO REFERENCE ID {mtoRefId}</h6>
-            </div>
+    <>
+      <ConnectedDeleteEvaluationReportConfirmationModal
+        isOpen={isDeleteModelOpen}
+        closeModal={toggleCancelModel}
+        submitModal={cancelReport}
+      />
+      <div className={classnames(styles.tabContent, shipmentEvaluationReportStyles.tabContent)}>
+        <GridContainer>
+          <Grid row>
+            <Grid col desktop={{ col: 8, offset: 2 }}>
+              {/* {showDeletionSuccess && <Alert type="success">Your remark has been deleted.</Alert>} */}
+              <h1>Shipment report</h1>
+              <div className={styles.pageHeaderDetails}>
+                <h6>REPORT ID #{reportId}</h6>
+                <h6>MOVE CODE {moveCode}</h6>
+                <h6>MTO REFERENCE ID {mtoRefId}</h6>
+              </div>
+            </Grid>
           </Grid>
-        </Grid>
-      </GridContainer>
-      <GridContainer className={shipmentEvaluationReportStyles.cardContainer}>
-        <Grid row>
-          <Grid col desktop={{ col: 8, offset: 2 }}>
-            <h2>Shipment information</h2>
+        </GridContainer>
+        <GridContainer className={shipmentEvaluationReportStyles.cardContainer}>
+          <Grid row>
+            <Grid col desktop={{ col: 8, offset: 2 }}>
+              <h2>Shipment information</h2>
+            </Grid>
           </Grid>
-        </Grid>
-      </GridContainer>
+        </GridContainer>
 
-      <GridContainer className={shipmentEvaluationReportStyles.cardContainer}>
-        <Grid row>
-          <Grid col desktop={{ col: 8, offset: 2 }}>
-            <h2>Evaluation form</h2>
+        <GridContainer className={shipmentEvaluationReportStyles.cardContainer}>
+          <Grid row>
+            <Grid col desktop={{ col: 8, offset: 2 }}>
+              <h2>Evaluation form</h2>
+            </Grid>
           </Grid>
-        </Grid>
-      </GridContainer>
-      <div style={{ display: 'flex', float: 'right' }}>
-        <Button className="usa-button--unstyled">Cancel</Button>
-        <Button className="usa-button--secondary">Save draft</Button>
-        <Button type="submit">Submit</Button>
+        </GridContainer>
+        <div style={{ display: 'flex', float: 'right' }}>
+          <Button className="usa-button--unstyled" onClick={toggleCancelModel}>
+            Cancel
+          </Button>
+          <Button className="usa-button--secondary">Save draft</Button>
+          <Button type="submit">Submit</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
