6bf1e5ee85dbc4452aa88e44042f5505e3ca3cf7
Felipe Lee <felipe@truss.works>
2022-07-21 02:12:43 +0000

    MB-13177 Add MovingExpense table with base validation logic & tests
    There is an existing type called `MovingExpenseType`, which was used for the old `MovingExpenseDocument` model, so we can't use that name in the new type for our new model. I thought about renaming the old one since we're going to get rid of it, but it has a lot of references, so for the sake of getting this model done, I created the new type with a slightly different name (`MovingExpenseReceiptType`). I still used the shorter name at the table level and for the model field itself because I figure when we get rid of the old type, we can rename the new type more easily. If it's preferred though, we can just use the new type name for the field itself too at both the table and model level.

---
+++ pkg/models/moving_expense.go
@@ -0,0 +1,108 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/unit"
+)
+
+// MovingExpenseReceiptType represents types of different moving expenses
+type MovingExpenseReceiptType string
+
+const (
+	// MovingExpenseReceiptTypeContractedExpense captures enum value "CONTRACTED_EXPENSE"
+	MovingExpenseReceiptTypeContractedExpense MovingExpenseReceiptType = "CONTRACTED_EXPENSE"
+	// MovingExpenseReceiptTypeOil captures enum value "OIL"
+	MovingExpenseReceiptTypeOil MovingExpenseReceiptType = "OIL"
+	// MovingExpenseReceiptTypePackingMaterials captures enum value "PACKING_MATERIALS"
+	MovingExpenseReceiptTypePackingMaterials MovingExpenseReceiptType = "PACKING_MATERIALS"
+	// MovingExpenseReceiptTypeRentalEquipment captures enum value "RENTAL_EQUIPMENT"
+	MovingExpenseReceiptTypeRentalEquipment MovingExpenseReceiptType = "RENTAL_EQUIPMENT"
+	// MovingExpenseReceiptTypeStorage captures enum value "STORAGE"
+	MovingExpenseReceiptTypeStorage MovingExpenseReceiptType = "STORAGE"
+	// MovingExpenseReceiptTypeTolls captures enum value "TOLLS"
+	MovingExpenseReceiptTypeTolls MovingExpenseReceiptType = "TOLLS"
+	// MovingExpenseReceiptTypeWeighingFees captures enum value "WEIGHING_FEES"
+	MovingExpenseReceiptTypeWeighingFees MovingExpenseReceiptType = "WEIGHING_FEES"
+	// MovingExpenseReceiptTypeOther captures enum value "OTHER"
+	MovingExpenseReceiptTypeOther MovingExpenseReceiptType = "OTHER"
+)
+
+// MovingExpenseStatus represents the status of an order record's lifecycle
+type MovingExpenseStatus string
+
+const (
+	// MovingExpenseStatusApproved captures enum value "APPROVED"
+	MovingExpenseStatusApproved MovingExpenseStatus = "APPROVED"
+	// MovingExpenseStatusExcluded captures enum value "EXCLUDED"
+	MovingExpenseStatusExcluded MovingExpenseStatus = "EXCLUDED"
+	// MovingExpenseStatusRejected captures enum value "REJECTED"
+	MovingExpenseStatusRejected MovingExpenseStatus = "REJECTED"
+)
+
+type MovingExpense struct {
+	ID                uuid.UUID                 `json:"id" db:"id"`
+	PPMShipmentID     uuid.UUID                 `json:"ppm_shipment_id" db:"ppm_shipment_id"`
+	PPMShipment       PPMShipment               `belongs_to:"ppm_shipments" fk_id:"ppm_shipment_id"`
+	DocumentID        uuid.UUID                 `json:"document_id" db:"document_id"`
+	Document          Document                  `belongs_to:"documents" fk_id:"document_id"`
+	CreatedAt         time.Time                 `json:"created_at" db:"created_at"`
+	UpdatedAt         time.Time                 `json:"updated_at" db:"updated_at"`
+	DeletedAt         *time.Time                `json:"deleted_at" db:"deleted_at"`
+	MovingExpenseType *MovingExpenseReceiptType `json:"moving_expense_type" db:"moving_expense_type"`
+	Description       *string                   `json:"description" db:"description"`
+	PaidWithGTCC      *bool                     `json:"paid_with_gtcc" db:"paid_with_gtcc"`
+	Amount            *unit.Cents               `json:"amount" db:"amount"`
+	MissingReceipt    *bool                     `json:"missing_receipt" db:"missing_receipt"`
+	Status            *MovingExpenseStatus      `json:"status" db:"status"`
+	SITStartDate      *time.Time                `json:"sit_start_date" db:"sit_start_date"`
+	SITEndDate        *time.Time                `json:"sit_end_date" db:"sit_end_date"`
+}
+
+type MovingExpenses []MovingExpense
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate,
+// pop.ValidateAndUpdate) method. This should contain validation that is for data integrity. Business validation should
+// occur in service objects.
+func (m *MovingExpense) Validate(_ *pop.Connection) (*validate.Errors, error) {
+	var movingExpenseType *string
+
+	if m.MovingExpenseType != nil {
+		movingExpenseType = StringPointer(string(*m.MovingExpenseType))
+	}
+
+	var status *string
+
+	if m.Status != nil {
+		status = StringPointer(string(*m.Status))
+	}
+
+	return validate.Validate(
+		&validators.UUIDIsPresent{Name: "PPMShipmentID", Field: m.PPMShipmentID},
+		&validators.UUIDIsPresent{Name: "DocumentID", Field: m.DocumentID},
+		&OptionalTimeIsPresent{Name: "DeletedAt", Field: m.DeletedAt},
+		&OptionalStringInclusion{Name: "MovingExpenseType", Field: movingExpenseType, List: []string{
+			string(MovingExpenseReceiptTypeContractedExpense),
+			string(MovingExpenseReceiptTypeOil),
+			string(MovingExpenseReceiptTypePackingMaterials),
+			string(MovingExpenseReceiptTypeRentalEquipment),
+			string(MovingExpenseReceiptTypeStorage),
+			string(MovingExpenseReceiptTypeTolls),
+			string(MovingExpenseReceiptTypeWeighingFees),
+			string(MovingExpenseReceiptTypeOther),
+		}},
+		&StringIsNilOrNotBlank{Name: "Description", Field: m.Description},
+		&OptionalStringInclusion{Name: "Status", Field: status, List: []string{
+			string(MovingExpenseStatusApproved),
+			string(MovingExpenseStatusExcluded),
+			string(MovingExpenseStatusRejected),
+		}},
+		&OptionalTimeIsPresent{Name: "SITStartDate", Field: m.SITStartDate},
+		&OptionalTimeIsPresent{Name: "SITEndDate", Field: m.SITEndDate},
+	), nil
+}
---
+++ pkg/models/moving_expense_test.go
@@ -0,0 +1,103 @@
+package models_test
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *ModelSuite) TestMovingExpenseValidation() {
+	blankExpenseType := models.MovingExpenseReceiptType("")
+	blankStatusType := models.MovingExpenseStatus("")
+	validExpenseTypes := strings.Join([]string{
+		string(models.MovingExpenseReceiptTypeContractedExpense),
+		string(models.MovingExpenseReceiptTypeOil),
+		string(models.MovingExpenseReceiptTypePackingMaterials),
+		string(models.MovingExpenseReceiptTypeRentalEquipment),
+		string(models.MovingExpenseReceiptTypeStorage),
+		string(models.MovingExpenseReceiptTypeTolls),
+		string(models.MovingExpenseReceiptTypeWeighingFees),
+		string(models.MovingExpenseReceiptTypeOther),
+	}, ", ")
+
+	validStatuses := strings.Join([]string{
+		string(models.MovingExpenseStatusApproved),
+		string(models.MovingExpenseStatusExcluded),
+		string(models.MovingExpenseStatusRejected),
+	}, ", ")
+
+	testCases := map[string]struct {
+		movingExpense models.MovingExpense
+		expectedErrs  map[string][]string
+	}{
+		"Successful create": {
+			movingExpense: models.MovingExpense{
+				PPMShipmentID: uuid.Must(uuid.NewV4()),
+				DocumentID:    uuid.Must(uuid.NewV4()),
+			},
+			expectedErrs: nil,
+		},
+		"Missing UUIDs": {
+			movingExpense: models.MovingExpense{},
+			expectedErrs: map[string][]string{
+				"ppmshipment_id": {"PPMShipmentID can not be blank."},
+				"document_id":    {"DocumentID can not be blank."},
+			},
+		},
+		"Optional fields are valid": {
+			movingExpense: models.MovingExpense{
+				PPMShipmentID:     uuid.Must(uuid.NewV4()),
+				DocumentID:        uuid.Must(uuid.NewV4()),
+				DeletedAt:         models.TimePointer(time.Time{}),
+				MovingExpenseType: &blankExpenseType,
+				Description:       models.StringPointer(""),
+				Status:            &blankStatusType,
+				SITStartDate:      models.TimePointer(time.Time{}),
+				SITEndDate:        models.TimePointer(time.Time{}),
+			},
+			expectedErrs: map[string][]string{
+				"deleted_at":          {"DeletedAt can not be blank."},
+				"moving_expense_type": {fmt.Sprintf("MovingExpenseType is not in the list [%s].", validExpenseTypes)},
+				"description":         {"Description can not be blank."},
+				"status":              {fmt.Sprintf("Status is not in the list [%s].", validStatuses)},
+				"sitstart_date":       {"SITStartDate can not be blank."},
+				"sitend_date":         {"SITEndDate can not be blank."},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		name := name
+		tc := tc
+
+		suite.Run(name, func() {
+			suite.verifyValidationErrors(&tc.movingExpense, tc.expectedErrs)
+		})
+	}
+
+	suite.Run("Can create a moving expense", func() {
+		// This test is meant to be a base smoke test to make sure this model/table works
+		ppmShipment := testdatagen.MakeApprovedPPMShipmentWithActualInfo(suite.DB(), testdatagen.Assertions{})
+
+		serviceMember := ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMember
+
+		document := testdatagen.MakeDocument(suite.DB(), testdatagen.Assertions{
+			Document: models.Document{
+				ServiceMemberID: serviceMember.ID,
+				ServiceMember:   serviceMember,
+			},
+		})
+
+		movingExpense := models.MovingExpense{
+			PPMShipmentID: ppmShipment.ID,
+			DocumentID:    document.ID,
+		}
+
+		suite.MustCreate(&movingExpense)
+	})
+}
