15f5f1718d7222f454dae8cb060e34f569ceb7d3
Marty Boren <marty@truss.works>
2022-07-25 19:52:14 +0000

    Replace shipment number with shortened shipment ID

--- src/components/Office/EvaluationReportTable/ShipmentEvaluationReports.jsx
+++ src/components/Office/EvaluationReportTable/ShipmentEvaluationReports.jsx
@@ -8,19 +8,11 @@ import styles from './ShipmentEvaluationReports.module.scss';
 
 const ShipmentEvaluationReports = ({ shipments, reports }) => {
   const sortedShipments = shipments.sort((a, b) => moment(a.createdAt) - moment(b.createdAt));
-  const shipmentNumbersByType = {};
 
   const shipmentRows = sortedShipments.map((shipment) => {
-    const { shipmentType } = shipment;
-    if (shipmentNumbersByType[shipmentType]) {
-      shipmentNumbersByType[shipmentType] += 1;
-    } else {
-      shipmentNumbersByType[shipmentType] = 1;
-    }
-    const shipmentNumber = shipmentNumbersByType[shipmentType];
     return (
       <div key={shipment.id} className={styles.shipmentRow}>
-        <EvaluationReportShipmentInfo shipment={shipment} shipmentNumber={shipmentNumber} />
+        <EvaluationReportShipmentInfo shipment={shipment} />
         <EvaluationReportTable reports={reports.filter((r) => r.shipmentID === shipment.id)} />
       </div>
     );
