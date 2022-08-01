87be811520a7a4c72c9ff73eed17daea755f6dda
PDickeyTruss <pdickey@truss.works>
2022-08-01 23:32:29 +0000

    Fix shipment eval form storybook

--- src/components/Office/ShipmentEvaluationForm/ShipmentEvaluationForm.stories.jsx
+++ src/components/Office/ShipmentEvaluationForm/ShipmentEvaluationForm.stories.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { MemoryRouter, Route } from 'react-router';
 
 import ShipmentEvaluationForm from './ShipmentEvaluationForm';
 
+import { MockProviders } from 'testUtils';
+
 export default {
   title: 'Office Components/ShipmentEvaluationForm',
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/moves/AMDORD/customer-support-remarks']}>
+        <Route path="/moves/:moveCode/customer-support-remarks">
+          <MockProviders>
+            <div style={{ padding: '40px', width: '850px' }}>
+              <Story />
+            </div>
+          </MockProviders>
+        </Route>
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export const Default = () => <ShipmentEvaluationForm />;
