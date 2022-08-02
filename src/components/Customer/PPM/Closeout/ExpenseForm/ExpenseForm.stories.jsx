3a7cba515019af32845a5db8d5710edae62d637a
Pearl Truss <pearl@truss.works>
2022-08-02 15:11:07 +0000

    add story for sit

--- src/components/Customer/PPM/Closeout/ExpenseForm/ExpenseForm.stories.jsx
+++ src/components/Customer/PPM/Closeout/ExpenseForm/ExpenseForm.stories.jsx
@@ -99,3 +99,42 @@ ExistingExpenses.args = {
   onUploadDelete: mockUploadDelete,
   tripNumber: '1',
 };
+
+export const SITExpenses = Template.bind({});
+SITExpenses.args = {
+  mtoShipment: {
+    id: 'f3c29ac7-823a-496a-90dd-b7ab0d4b0ece',
+    moveTaskOrderId: 'e9864ee5-56e7-401d-9a7b-a5ea9a83bdea',
+    shipmentType: SHIPMENT_OPTIONS.PPM,
+    ppmShipment: {
+      id: '343bb456-63af-4f76-89bd-7403094a5c4d',
+    },
+  },
+  expense: {
+    id: '32ecb311-edbe-4fd4-96ee-bd693113f3f3',
+    ppmShipmentId: '343bb456-63af-4f76-89bd-7403094a5c4d',
+    receiptType: 'storage',
+    description: '10x10 storage pod',
+    missingReceipt: false,
+    paidWithGTCC: false,
+    amount: 1600,
+    sitStartDate: '2022-09-23',
+    sitEndDate: '2022-12-25',
+    receiptDocument: {
+      uploads: [
+        {
+          id: 'db4713ae-6087-4330-8b0d-926b3d65c454',
+          created_at: '2022-06-10T12:59:30.000Z',
+          bytes: 204800,
+          url: 'some/path/to/',
+          filename: 'expenseReceipt.pdf',
+          content_type: 'application/pdf',
+        },
+      ],
+    },
+  },
+  onCreateUpload: mockCreateUploadSuccess,
+  onUploadComplete: mockUploadComplete,
+  onUploadDelete: mockUploadDelete,
+  tripNumber: '1',
+};
