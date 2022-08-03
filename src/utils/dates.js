c8a6cb0f6a945fa6358dfab3a85d80df17c533c3
Pearl Truss <pearl@truss.works>
2022-08-03 18:24:14 +0000

    use utc for dates

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
 
 export default function numOfDaysBetweenDates(date1, date2) {
-  const mDate1 = moment(date1);
-  const mDate2 = moment(date2);
+  const mDate1 = moment.utc(date1);
+  const mDate2 = moment.utc(date2);
 
   return mDate2.diff(mDate1, 'days');
 }
