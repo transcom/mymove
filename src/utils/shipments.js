3557bf03aa21195f7b20683d6bc71abf2ffcbda6
Felipe Lee <felipe@truss.works>
2022-08-06 00:49:11 +0000

    MB-11853 Add function to test if a given weight ticket is complete
    Also added a few factories for creating uploads, documents, and weight tickets. None of them are super robust, but they'll serve until we truly implement factories in the future.

--- src/utils/shipments.js
+++ src/utils/shipments.js
@@ -8,6 +8,26 @@ export function isPPMShipmentComplete(mtoShipment) {
   return false;
 }
 
+// isWeightTicketComplete - checks that the required fields for a weight ticket have valid data
+// to check if the weight ticket can be considered complete. For the purposes of this function,
+// any data is enough to consider some fields valid.
+export function isWeightTicketComplete(weightTicket) {
+  const hasValidEmptyWeight = weightTicket.emptyWeight != null && weightTicket.emptyWeight >= 0;
+
+  const hasTrailerDocUpload = weightTicket.proofOfTrailerOwnershipDocument.uploads.length > 0;
+  const needsTrailerUpload = weightTicket.ownsTrailer && weightTicket.trailerMeetsCriteria;
+  const trailerNeedsMet = needsTrailerUpload ? hasTrailerDocUpload : true;
+
+  return !!(
+    weightTicket.vehicleDescription &&
+    hasValidEmptyWeight &&
+    weightTicket.emptyDocument.uploads.length > 0 &&
+    weightTicket.fullWeight > 0 &&
+    weightTicket.fullDocument.uploads.length > 0 &&
+    trailerNeedsMet
+  );
+}
+
 export function hasCompleteWeightTickets(weightTickets) {
   if (!weightTickets?.length) {
     return false;
--- src/utils/shipments.test.js
+++ src/utils/shipments.test.js
@@ -1,6 +1,10 @@
-import { isPPMShipmentComplete, hasCompleteWeightTickets } from './shipments';
+import { v4 } from 'uuid';
+
+import { isPPMShipmentComplete, isWeightTicketComplete, hasCompleteWeightTickets } from './shipments';
 
 import { SHIPMENT_OPTIONS } from 'shared/constants';
+import createDocumentWithoutUploads from 'utils/test/factories/document';
+import { createCompleteWeightTicket, createCompleteWeightTicketWithTrailer } from 'utils/test/factories/weightTicket';
 
 describe('shipments utils', () => {
   describe('isPPMShipmentComplete', () => {
@@ -80,6 +84,39 @@ describe('shipments utils', () => {
       expect(isPPMShipmentComplete(incompletePPMShipment)).toBe(false);
     });
   });
+
+  describe('isWeightTicketComplete', () => {
+    const serviceMemberId = v4();
+
+    const completeWeightTicket = createCompleteWeightTicket({ serviceMemberId });
+    const completeWeightTicketWithTrailer = createCompleteWeightTicketWithTrailer({ serviceMemberId });
+
+    const emptyDocumentWithoutUploads = createDocumentWithoutUploads({ serviceMemberId });
+    const fullDocumentWithoutUploads = createDocumentWithoutUploads({ serviceMemberId });
+    const trailerDocumentWithoutUploads = createDocumentWithoutUploads({ serviceMemberId });
+
+    it.each([
+      [false, 'vehicle description is missing', { ...completeWeightTicket, vehicleDescription: null }],
+      [false, 'empty weight is missing', { ...completeWeightTicket, emptyWeight: null }],
+      [false, 'empty document has no uploads', { ...completeWeightTicket, emptyDocument: emptyDocumentWithoutUploads }],
+      [false, 'full weight is missing', { ...completeWeightTicket, fullWeight: null }],
+      [false, 'full document has no uploads', { ...completeWeightTicket, fullDocument: fullDocumentWithoutUploads }],
+      [
+        false,
+        'owns trailer but missing trailer uploads',
+        {
+          ...completeWeightTicketWithTrailer,
+          proofOfTrailerOwnershipDocument: trailerDocumentWithoutUploads,
+        },
+      ],
+      [true, 'all required data is present (no trailer)', completeWeightTicket],
+      [true, 'all required data is present (empty weight === 0)', { ...completeWeightTicket, emptyWeight: 0 }],
+      [true, 'all required data is present (with trailer)', completeWeightTicketWithTrailer],
+    ])('returns %s if %s', (expectedValue, scenarioDescription, weightTicket) => {
+      expect(isWeightTicketComplete(weightTicket)).toEqual(expectedValue);
+    });
+  });
+
   describe('hasCompleteWeightTickets', () => {
     it('returns false when there are no weight tickets', () => {
       expect(hasCompleteWeightTickets()).toBe(false);
