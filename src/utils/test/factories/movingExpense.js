b272265bb210b10dd0c4ca6eb22d53752a4cc9bf
Felipe Lee <felipe@truss.works>
2022-08-24 00:28:21 +0000

    MB-13352 Add factories for movingExpenses and proGearWeightTickets

---
+++ src/utils/test/factories/movingExpense.js
@@ -0,0 +1,73 @@
+import moment from 'moment';
+import { v4 } from 'uuid';
+
+import { movingExpenseTypes } from 'constants/ppmShipmentDocument';
+import { createDocumentWithoutUploads } from 'utils/test/factories/document';
+import createUpload from 'utils/test/factories/upload';
+
+const createBaseMovingExpense = ({ serviceMemberId, creationDate = new Date() } = {}, fieldOverrides = {}) => {
+  const createdAtDate = creationDate.toISOString();
+
+  const smId = serviceMemberId || v4();
+  const document = createDocumentWithoutUploads({ serviceMemberId: smId });
+
+  return {
+    id: v4(),
+    ppmShipmentId: v4(),
+    documentId: document.id,
+    document,
+    movingExpenseType: null,
+    description: null,
+    paidWithGtcc: null,
+    amount: null,
+    missingReceipt: null,
+    status: null,
+    reason: null,
+    sitStartDate: null,
+    sitEndDate: null,
+    createdAt: createdAtDate,
+    updatedAt: createdAtDate,
+    eTag: window.btoa(createdAtDate),
+    ...fieldOverrides,
+  };
+};
+
+const createCompleteMovingExpense = ({ serviceMemberId, creationDate } = {}, fieldOverrides = {}) => {
+  const fullFieldOverrides = {
+    movingExpenseType: movingExpenseTypes.PACKING_MATERIALS,
+    description: 'Medium and large boxes',
+    paidWithGtcc: false,
+    amount: '7500',
+    missingReceipt: false,
+    ...fieldOverrides,
+  };
+
+  const movingExpense = createBaseMovingExpense({ serviceMemberId, creationDate }, fullFieldOverrides);
+
+  if (movingExpense.document.uploads.length === 0) {
+    movingExpense.document.uploads.push(createUpload({ fileName: 'expense.pdf' }));
+  }
+
+  if (movingExpense.createdAt === movingExpense.updatedAt) {
+    const updatedAt = moment(movingExpense.createdAt).add(1, 'hour').toISOString();
+
+    movingExpense.updatedAt = updatedAt;
+    movingExpense.eTag = window.btoa(updatedAt);
+  }
+
+  return movingExpense;
+};
+
+const createCompleteSITMovingExpense = ({ serviceMemberId, creationDate } = {}, fieldOverrides = {}) => {
+  const fullFieldOverrides = {
+    movingExpenseType: movingExpenseTypes.STORAGE,
+    description: 'Storage while away',
+    sitStartDate: '2022-09-15',
+    sitEndDate: '2022-09-20',
+    ...fieldOverrides,
+  };
+
+  return createCompleteMovingExpense({ serviceMemberId, creationDate }, fullFieldOverrides);
+};
+
+export { createBaseMovingExpense, createCompleteMovingExpense, createCompleteSITMovingExpense };
