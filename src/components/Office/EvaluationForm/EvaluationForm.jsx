d4f43210a80753ff1e708f1948975e7cb975d92c
Amanda P <amandap@truss.works>
2022-08-24 09:57:54 -0400

    Update to plural when 'physical' is selected

--- src/components/Office/EvaluationForm/EvaluationForm.jsx
+++ src/components/Office/EvaluationForm/EvaluationForm.jsx
@@ -250,7 +250,7 @@ const EvaluationForm = ({ evaluationReport }) => {
                             <DropdownInput
                               id="hour"
                               name="hour"
-                              label="Hour"
+                              label="Hours"
                               className={styles.hourPicker}
                               onChange={(e) => {
                                 setFieldValue('hour', e.target.value);
@@ -262,7 +262,7 @@ const EvaluationForm = ({ evaluationReport }) => {
                             <DropdownInput
                               id="minute"
                               name="minute"
-                              label="Minute"
+                              label="Minutes"
                               className={styles.minutePicker}
                               onChange={(e) => {
                                 setFieldValue('minute', e.target.value);
