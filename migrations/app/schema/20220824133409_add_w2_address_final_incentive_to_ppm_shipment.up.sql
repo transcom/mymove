07d7581ff225d893e32f4a5c445036bce2b9d682
Pearl Truss <pearl@truss.works>
2022-08-24 15:01:00 +0000

    add migration for creating an w2 address id column on ppm shipments table

---
+++ migrations/app/schema/20220824133409_add_w2_address_final_incentive_to_ppm_shipment.up.sql
@@ -0,0 +1,6 @@
+ALTER table ppm_shipments
+    ADD COLUMN w2_address_id uuid CONSTRAINT ppm_shipments_address_id_fkey REFERENCES addresses (id);
+
+CREATE INDEX on ppm_shipments (w2_address_id);
+
+COMMENT ON COLUMN ppm_shipments.w2_address_id IS "Customer's address to receive their W2 tax form";
