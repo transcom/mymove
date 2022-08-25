9e893aea70e1f6f756a6ca59287da4c2ea4635f0
Marty Boren <marty@truss.works>
2022-08-25 22:08:34 +0000

    Update forbidden page styles
    I think these are 95% accurate to the designs now. The fonts feel a bit different
    but the numbers seem right, so I'm not sure what's going on.
    This might be Good Enough.

--- src/pages/Office/Forbidden/Forbidden.jsx
+++ src/pages/Office/Forbidden/Forbidden.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router';
-import { Button, Grid, GridContainer } from '@trussworks/react-uswds';
+import { Button } from '@trussworks/react-uswds';
+
+import styles from './Forbidden.module.scss';
 
 const Forbidden = () => {
   const history = useHistory();
   const { moveCode } = useParams();
-  // TODO need to figure out how to style this thing
-  // TODO grid kinda works buuut it has some awkward breakpoints and really
-  // TODO i just need to make a dang box a fixed width and center it, seems like
-  // TODO a lot of extra markup for that.
   return (
-    <GridContainer>
-      <Grid row>
-        <Grid col desktop={{ col: 8, offset: 2 }}>
-          <h1>Sorry, you can&apos;t access this page</h1>
-          <p>This page is only available to authorized users</p>
-          <p>
-            You are not signed in to MilMove in a role that gives you access. If you believe you should have access,
-            contact your administrator.
-          </p>
-          <Button onClick={() => history.push(`/moves/${moveCode}/details`)}>Go to move details</Button>
-        </Grid>
-      </Grid>
-    </GridContainer>
+    <div className={styles.forbidden}>
+      <h1>Sorry, you can&apos;t access this page</h1>
+      <p className={styles.subHeading}>This page is only available to authorized users</p>
+      <p className={styles.explanation}>
+        You are not signed in to MilMove in a role that gives you access. If you believe you should have access, contact
+        your administrator.
+      </p>
+      <Button type="button" onClick={() => history.push(`/moves/${moveCode}/details`)}>
+        Go to move details
+      </Button>
+    </div>
   );
 };
 
