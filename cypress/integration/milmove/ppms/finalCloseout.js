9c6d938ed67da3c026dcd06483691209ba3ca7d9
Felipe Lee <felipe@truss.works>
2022-08-30 01:30:22 +0000

    MB-13352 Update final closeout cypress tests for money format changes

--- cypress/integration/milmove/ppms/finalCloseout.js
+++ cypress/integration/milmove/ppms/finalCloseout.js
@@ -35,12 +35,12 @@ describe('Final Closeout', function () {
 function verifyFinalIncentiveAndTotals() {
   // TODO: Once we get back final incentive, set a value in the testdatagen func
   //  createMoveWithPPMShipmentReadyForFinalCloseout and check for it here.
-  cy.get('h2').contains('Your final estimated incentive: $0');
+  cy.get('h2').contains('Your final estimated incentive: $0.00');
 
   cy.get('li').contains('4,000 lbs total net weight');
 
   // TODO: Once we get moving expenses and pro gear back, check for those here as well.
 
   cy.get('li').contains('0 lbs of pro-gear');
-  cy.get('li').contains('$0 in expenses claimed');
+  cy.get('li').contains('$0.00 in expenses claimed');
 }
