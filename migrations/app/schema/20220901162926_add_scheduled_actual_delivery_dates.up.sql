b6f11a66ba28c633a608a5060413bab83fe413ad
Marty Boren <marty@truss.works>
2022-09-01 19:15:31 +0000

    Small formatting update for migration

--- migrations/app/schema/20220901162926_add_scheduled_actual_delivery_dates.up.sql
+++ migrations/app/schema/20220901162926_add_scheduled_actual_delivery_dates.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE mto_shipments
 	ADD COLUMN scheduled_delivery_date date,
-    add column actual_delivery_date date;
+	ADD COLUMN actual_delivery_date    date;
 
 COMMENT ON COLUMN mto_shipments.scheduled_delivery_date IS 'The delivery date the Prime contractor schedules for a shipment after consultation with the customer';
 COMMENT ON COLUMN mto_shipments.actual_delivery_date IS 'The actual date that the shipment was delivered to the destination address by the Prime';
