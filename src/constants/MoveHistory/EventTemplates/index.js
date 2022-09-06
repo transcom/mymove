77adb9d1faedca5eff2c28cb1253d3519814e27b
Michael Mayo <mmayo@truss.works>
2022-09-06 13:57:47 +0000

    Add event template to display reweigh weights in move history table
    MB-12015: Added an event template to display updates to reweigh weights on the move history table. This also included modifying the field mappings and weight fields to get the template to display the correct labels and weight units.

--- src/constants/MoveHistory/EventTemplates/index.js
+++ src/constants/MoveHistory/EventTemplates/index.js
@@ -37,6 +37,7 @@ export { default as updateOrder } from './updateOrder';
 export { default as updatePaymentRequest } from './updatePaymentRequest';
 export { default as updatePaymentRequestJobRunner } from './updatePaymentRequestJobRunner';
 export { default as updatePaymentRequestStatus } from './updatePaymentRequestStatus';
+export { default as updateReweighWeight } from './updateReweighWeight';
 export { default as updateServiceItemStatus } from './updateServiceItemStatus';
 export { default as updateMTOReviewedBillableWeightsAt } from './updateMTOReviewedBillableWeightsAt';
 export { default as uploadAmendedOrders } from './uploadAmendedOrders';
