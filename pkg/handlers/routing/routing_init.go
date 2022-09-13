94180d79aad03c35b559b19fa95fd7abad312c31
Andrew Hobson <ahobson@truss.works>
2022-09-13 18:40:09 +0000

    Shoehorn in debugging output for current sessions

--- pkg/handlers/routing/routing_init.go
+++ pkg/handlers/routing/routing_init.go
@@ -1,7 +1,9 @@
 package routing
 
 import (
+	"context"
 	"encoding/hex"
+	"encoding/json"
 	"net/http"
 	"net/http/pprof"
 	"path"
@@ -248,7 +250,7 @@ func InitRouting(appCtx appcontext.AppContext, redisPool *redis.Pool,
 	root.Use(sessionCookieMiddleware)
 	root.Use(middleware.RequestLogger(appCtx.Logger()))
 
-	debug := root.PathPrefix("/debug/pprof/").Subrouter()
+	debug := root.PathPrefix("/debug/pprof").Subrouter()
 	debug.Use(userAuthMiddleware)
 	if routingConfig.ServeDebugPProf {
 		appCtx.Logger().Info("Enabling pprof routes")
@@ -263,6 +265,67 @@ func InitRouting(appCtx appcontext.AppContext, redisPool *redis.Pool,
 		debug.HandleFunc("/trace", pprof.Trace).Methods("GET")
 		debug.Handle("/threadcreate", pprof.Handler("threadcreate")).Methods("GET")
 		debug.HandleFunc("/symbol", pprof.Symbol).Methods("GET")
+		debug.HandleFunc("/sessions", func(w http.ResponseWriter, r *http.Request) {
+			m := make(map[string]map[string]auth.Session)
+
+			msm := routingConfig.HandlerConfig.GetMilSessionManager()
+			msessions := make(map[string]auth.Session)
+			err := msm.Iterate(r.Context(), func(ctx context.Context) error {
+				obj := msm.Get(ctx, "session")
+				session, ok := obj.(auth.Session)
+				if ok {
+					token := msm.Token(ctx)
+					msessions[token] = session
+				}
+				return nil
+			})
+			if err != nil {
+				appCtx.Logger().Error("Error iteration mil sessions", zap.Error(err))
+			}
+			m[routingConfig.HandlerConfig.AppNames().MilServername] = msessions
+
+			osm := routingConfig.HandlerConfig.GetOfficeSessionManager()
+			osessions := make(map[string]auth.Session)
+			err = osm.Iterate(r.Context(), func(ctx context.Context) error {
+				obj := osm.Get(ctx, "session")
+				session, ok := obj.(auth.Session)
+				if ok {
+					token := osm.Token(ctx)
+					osessions[token] = session
+				}
+				return nil
+			})
+			if err != nil {
+				appCtx.Logger().Error("Error iteration office sessions", zap.Error(err))
+			}
+
+			m[routingConfig.HandlerConfig.AppNames().OfficeServername] = osessions
+
+			asm := routingConfig.HandlerConfig.GetAdminSessionManager()
+			asessions := make(map[string]auth.Session)
+			err = asm.Iterate(r.Context(), func(ctx context.Context) error {
+				obj := asm.Get(ctx, "session")
+				session, ok := obj.(auth.Session)
+				if ok {
+					token := asm.Token(ctx)
+					asessions[token] = session
+				}
+				return nil
+			})
+			if err != nil {
+				appCtx.Logger().Error("Error iteration admin sessions", zap.Error(err))
+			}
+			m[routingConfig.HandlerConfig.AppNames().AdminServername] = asessions
+
+			w.Header().Add("Content-type", "application/json")
+
+			err = json.NewEncoder(w).Encode(m)
+			if err != nil {
+				appCtx.Logger().Error("redis write error", zap.Error(err))
+				http.Error(w, "redis write error", http.StatusInternalServerError)
+				return
+			}
+		}).Methods("GET")
 	} else {
 		debug.HandleFunc("/", http.NotFound).Methods("GET")
 	}
