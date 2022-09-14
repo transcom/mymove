92605f5d9eb5f67fd203a5b0df5b53a4073b078f
Pearl Truss <pearl@truss.works>
2022-09-14 18:13:26 +0000

    add migration to drop unneeded tables and columns

---
+++ migrations/app/schema/20220914175516_deprecate_move_docs_and_moving_expense_docs.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE moves DROP COLUMN IF EXISTS move_documents;
+DROP TABLE IF EXISTS moving_expense_documents;
+DROP TABLE IF EXISTS weight_ticket_set_documents;
+DROP TABLE IF EXISTS move_documents;
