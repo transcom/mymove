765714e3b8c42de63b2b5f11eadeafc12ade4098
Roger Steve Ruiz <rogeruiz@truss.works>
2022-09-16 23:03:46 +0000

    This is working locally, so trying in CI now

--- pkg/services/query/query_fetcher.go
+++ pkg/services/query/query_fetcher.go
@@ -2,8 +2,7 @@ package query
 
 import (
 	"fmt"
-	"os"
-	"path"
+	"path/filepath"
 
 	"github.com/qustavo/dotsql"
 )
@@ -11,17 +10,19 @@ import (
 // getSQLScriptFilePath returns the path for the SQL scripts directory to be used
 // by the QueryFetcher package
 func getSQLScriptFilePath(fileName string) (string, error) {
-	sqlDir := "pkg/services/query/sql_scripts"
+	sqlDir := "pkg/services/query/sql_scripts/"
 
-	cwd, err := os.Getwd()
+	// Let's try and get the absolute path based on the construction of the SQL
+	// directory as we know it at the root of the project, then append the file
+	// name and the extension at the end. The Abs() function will return the
+	// correct path here since it joins the current working directory with the
+	// path that we give it to see if it is there. But this doesn't guarantee
+	// that the file is actually there or if the path is unique according to
+	// the documentation in the path/filepath package.
+	queryLocation, err := filepath.Abs(sqlDir + fileName + ".sql")
+
 	if err != nil {
-		return "", err
-	}
-
-	queryLocation := path.Join(cwd, sqlDir, fileName+".sql")
-
-	if queryLocation == cwd {
-		return "", fmt.Errorf("The SQL scripts directory was not found at %s", sqlDir)
+		return "", fmt.Errorf("There was an error getting the absolute path for %s. %s", fileName, err)
 	}
 
 	return queryLocation, nil
