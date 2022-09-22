64f594eaa71723de111148f642297bb804095c80
Amina Foon <amina@truss.works>
2022-09-22 15:19:09 -0400

    add service counseling allowance change template

---
+++ src/constants/MoveHistory/EventTemplates/updateAllowanceCounseling.js
@@ -0,0 +1,15 @@
+import o from 'constants/MoveHistory/UIDisplay/Operations';
+import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import a from 'constants/MoveHistory/Database/Actions';
+import t from 'constants/MoveHistory/Database/Tables';
+
+export default {
+  action: a.UPDATE,
+  eventName: o.counselingUpdateAllowance,
+  tableName: t.entitlements,
+  detailsType: d.LABELED,
+  getEventNameDisplay: () => 'Updated allowances',
+  getDetailsLabeledDetails: (historyRecord) => {
+    return { ...historyRecord.changedValues };
+  },
+};
