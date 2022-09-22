ec730502538bd7a8ed6217609202012925505790
Roger Steve Ruiz <rogeruiz@truss.works>
2022-09-22 14:55:08 +0000

    Adding the database triggers;
    Because we're using a non "MasterUser" for DMS RDS authentication. We've
    copied the commands into this SQL file from the documentation.
    
    [=> The documentation](https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.RDSPostgreSQL.NonMasterUser)
    
    We are still currently validating the right approach here since we have
    to test this locally and in experimental.
    
    Co-Authored-By: Chikara Takahashi <chikara@truss.works>
    Co-Authored-By: Jessie Tamburino <jesset@truss.works>

--- migrations/app/schema/20220921194349_add_advana_dms_integration_psql_user.up.sql
+++ migrations/app/schema/20220921194349_add_advana_dms_integration_psql_user.up.sql
@@ -34,3 +34,49 @@ GRANT USAGE, UPDATE ON ALL SEQUENCES IN SCHEMA public TO dms_crud;
 -- 2. when this runs in Docker, it will apply to the `postgres` role.
 ALTER DEFAULT PRIVILEGES GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO dms_crud;
 ALTER DEFAULT PRIVILEGES GRANT USAGE, UPDATE ON SEQUENCES TO dms_crud;
+
+
+--- Taken from this link here: https://docs.aws.amazon.com/dms/latest/userguide/CHAP_Source.PostgreSQL.html#CHAP_Source.PostgreSQL.RDSPostgreSQL.NonMasterUser
+
+
+create table public.awsdms_ddl_audit
+(
+  c_key    bigserial primary key,
+  c_time   timestamp,    -- Informational
+  c_user   varchar(64),  -- Informational: current_user
+  c_txn    varchar(16),  -- Informational: current transaction
+  c_tag    varchar(24),  -- Either 'CREATE TABLE' or 'ALTER TABLE' or 'DROP TABLE'
+  c_oid    integer,      -- For future use - TG_OBJECTID
+  c_name   varchar(64),  -- For future use - TG_OBJECTNAME
+  c_schema varchar(64),  -- For future use - TG_SCHEMANAME. For now - holds current_schema
+  c_ddlqry  text         -- The DDL query associated with the current DDL event
+)
+
+
+CREATE OR REPLACE FUNCTION public.awsdms_intercept_ddl()
+  RETURNS event_trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+  AS $$
+  declare _qry text;
+BEGIN
+  if (tg_tag='CREATE TABLE' or tg_tag='ALTER TABLE' or tg_tag='DROP TABLE') then
+         SELECT current_query() into _qry;
+         insert into public.awsdms_ddl_audit
+         values
+         (
+         default,current_timestamp,current_user,cast(TXID_CURRENT()as varchar(16)),tg_tag,0,'',current_schema,_qry
+         );
+         delete from public.awsdms_ddl_audit;
+end if;
+END;
+$$;
+
+--- This needs to be run as the dms_crud
+CREATE EVENT TRIGGER awsdms_intercept_ddl ON ddl_command_end
+EXECUTE PROCEDURE public.awsdms_intercept_ddl();
+
+
+--- All users and roles need access to the events
+GRANT ALL ON public.awsdms_ddl_audit TO public;
+GRANT ALL ON public.awsdms_ddl_audit_c_key_seq TO public;
