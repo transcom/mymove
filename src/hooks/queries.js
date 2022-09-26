52837b17a069a073d7d66ee5d9fffffeb0743bb4
PDickeyTruss <pdickey@truss.works>
2022-09-26 07:25:33 -0800

    Add/update frontend tests

--- src/hooks/queries.js
+++ src/hooks/queries.js
@@ -408,7 +408,7 @@ export const useEvaluationReportQueries = (reportID) => {
     enabled: !!shipmentID,
   });
 
-  const { data: reportViolations, ...reportViolationsQuery } = useQuery(
+  const { data: reportViolations = [], ...reportViolationsQuery } = useQuery(
     [REPORT_VIOLATIONS, reportID],
     getReportViolationsByReportID,
     {
