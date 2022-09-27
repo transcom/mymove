7171bbb81a0e1525d9aebdef2de93e7e1c35631e
Namibia Torres <namibia@truss.works>
2022-09-27 16:06:10 -0700

    update migration with correct enum type

--- migrations/app/schema/20220927000358_add_status_and_reason_to_weight_ticket.up.sql
+++ migrations/app/schema/20220927000358_add_status_and_reason_to_weight_ticket.up.sql
@@ -1,11 +1,5 @@
-CREATE TYPE ppm_shipment_status_3 AS ENUM (
-    'APPROVE',
-	'EXCLUDE',
-	'REJECT'
-	);
-
 ALTER TABLE weight_tickets
-    ADD COLUMN status ppm_shipment_status_3,
+    ADD COLUMN status ppm_document_status,
     ADD COLUMN reason varchar;
 
 COMMENT ON COLUMN weight_tickets.status IS 'Status of the ppm shipment set by the Service Counselor.';
