1fdcfb4b42ed22b0e65de7f2ea735a10ffc7bf4f
ruizajtruss <ruizaj@truss.works>
2022-09-28 22:47:55 +0000

    changed test description

--- src/constants/MoveHistory/EventTemplates/deleteShipment.test.js
+++ src/constants/MoveHistory/EventTemplates/deleteShipment.test.js
@@ -1,14 +1,14 @@
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/deleteShipment';
 
-describe('when given an Approved shipment history record', () => {
+describe('When a service counselor deletes a shipment', () => {
   const item = {
     action: 'UPDATE',
     eventName: 'deleteShipment',
     oldValues: { shipment_type: 'HHG' },
     tableName: 'mto_shipments',
   };
-  it('correctly matches the Approved shipment event', () => {
+  it('correctly displays a message indicating a HHG shipment was deleted', () => {
     const result = getTemplate(item);
     expect(result).toMatchObject(e);
     expect(result.getDetailsPlainText(item)).toEqual('HHG shipment deleted');
