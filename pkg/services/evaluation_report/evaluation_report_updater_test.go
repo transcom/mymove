d55270ef47d0c969813549654647e53711d3fcc7
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2022-09-29 16:32:19 +0000

    Make sure we remove existing reportViolations when the user sets observedViolations to false.

--- pkg/services/evaluation_report/evaluation_report_updater_test.go
+++ pkg/services/evaluation_report/evaluation_report_updater_test.go
@@ -159,6 +159,27 @@ func (suite EvaluationReportSuite) TestUpdateEvaluationReport() {
 		suite.Equal(report.Remarks, updatedReport.Remarks)
 		suite.Nil(updatedReport.SubmittedAt)
 	})
+
+	suite.Run("saving report with pre-existing violations should delete them if violationsObserved is false", func() {
+		report := testdatagen.MakeEvaluationReport(suite.DB(), testdatagen.Assertions{})
+		testdatagen.MakeReportViolation(suite.DB(), testdatagen.Assertions{ReportViolation: models.ReportViolation{
+			ReportID:    report.ID,
+			Violation:   models.PWSViolation{},
+			ViolationID: uuid.UUID{},
+		}})
+		report.ViolationsObserved = swag.Bool(false)
+
+		// do the update
+		err := updater.UpdateEvaluationReport(suite.AppContextForTest(), &report, report.OfficeUserID, etag.GenerateEtag(report.UpdatedAt))
+		suite.NoError(err)
+
+		var reportViolations models.ReportViolations
+		err = suite.DB().Where("report_id = ?", report.ID).All(&reportViolations)
+		suite.NoError(err)
+		// we shouldn't find any report violations, which means this object should have a 0 length
+		suite.Equal(0, len(reportViolations))
+	})
+
 	suite.Run("saving report does not overwrite readonly fields", func() {
 		// Create a report and save it to the database
 		move := testdatagen.MakeDefaultMove(suite.DB())
