9251db690cb78e7da37586375ab04dcffd861e31
Ronak Patel <ronak@truss.works>
2022-09-29 20:37:17 +0000

    Make scroll to top behavior happen instantly instead of smoothly scorlling

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -9,7 +9,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scroll({ top: 0, left: 0, behavior: 'instant' });
   }, [pathname]);
 
   return null;
