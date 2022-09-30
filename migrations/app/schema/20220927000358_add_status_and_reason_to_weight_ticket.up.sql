a567c4f9059d9ed4a764832585ccbe2828bea027
Namibia Torres <namibia@truss.works>
2022-09-29 17:17:06 -0700

    update migration - descriptions for both fields

--- migrations/app/schema/20220927000358_add_status_and_reason_to_weight_ticket.up.sql
+++ migrations/app/schema/20220927000358_add_status_and_reason_to_weight_ticket.up.sql
@@ -2,5 +2,5 @@ ALTER TABLE weight_tickets
     ADD COLUMN status ppm_document_status,
     ADD COLUMN reason varchar;
 
-COMMENT ON COLUMN weight_tickets.status IS 'Status of the ppm shipment set by the Service Counselor.';
-COMMENT ON COLUMN weight_tickets.reason IS 'Reason for selecting a status of exclude or reject.';
+COMMENT ON COLUMN weight_tickets.status IS 'Status of the weight ticket, e.g. APPROVED.';
+COMMENT ON COLUMN weight_tickets.reason IS 'Contains the reason a weight ticket is excluded or rejected; otherwise null.';
