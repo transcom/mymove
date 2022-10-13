b992a31a481ba039bf410551fed1b32272c2b22d
Michael Valdes <michael.valdes@truss.works>
2022-10-13 16:42:49 +0000

    Refactor updateOrder event per ADR 74

--- src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.jsx
+++ src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.jsx
@@ -1,26 +1,30 @@
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import React from 'react';
+
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
+import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+
+const formatChangedValues = (historyRecord) => {
+  let newChangedValues;
+
+  if (historyRecord.context) {
+    newChangedValues = {
+      ...historyRecord.changedValues,
+      ...historyRecord.context[0],
+    };
+  } else {
+    newChangedValues = historyRecord.changedValues;
+  }
+
+  return { ...historyRecord, changedValues: newChangedValues };
+};
 
 export default {
   action: a.UPDATE,
   eventName: '*',
   tableName: t.orders,
-  detailsType: d.LABELED,
   getEventNameDisplay: () => 'Updated orders',
-  getDetailsLabeledDetails: (historyRecord) => {
-    let newChangedValues;
-
-    if (historyRecord.context) {
-      newChangedValues = {
-        ...historyRecord.changedValues,
-        ...historyRecord.context[0],
-      };
-    } else {
-      newChangedValues = historyRecord.changedValues;
-    }
-
-    // merge context with change values for only this event
-    return newChangedValues;
+  getDetails: (historyRecord) => {
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
   },
 };
--- src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.test.jsx
+++ src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.test.jsx
@@ -1,9 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
 import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/updateOrders/updateOrder';
 
 describe('when given an Order update history record', () => {
-  const item = {
+  const historyRecord = {
     action: 'UPDATE',
     eventName: 'updateOrder',
     tableName: 'orders',
@@ -12,14 +14,14 @@ describe('when given an Order update history record', () => {
     context: [{ old_duty_location_name: 'old name', new_duty_location_name: 'new name' }],
   };
   it('correctly matches the Update orders event', () => {
-    const result = getTemplate(item);
+    const result = getTemplate(historyRecord);
     expect(result).toMatchObject(e);
+    render(result.getDetails(historyRecord));
     // expect to have merged context and changedValues
-    expect(result.getDetailsLabeledDetails({ context: item.context, changedValues: item.changedValues })).toEqual({
-      old_duty_location_id: 'ID1',
-      new_duty_location_id: 'ID2',
-      old_duty_location_name: 'old name',
-      new_duty_location_name: 'new name',
-    });
+    expect(screen.queryByText('ID1')).not.toBeInTheDocument();
+    expect(screen.queryByText('ID2')).not.toBeInTheDocument();
+    expect(screen.queryByText('old name')).not.toBeInTheDocument();
+    expect(screen.getByText('New duty location name')).toBeInTheDocument();
+    expect(screen.getByText('new name', { exact: false })).toBeInTheDocument();
   });
 });
