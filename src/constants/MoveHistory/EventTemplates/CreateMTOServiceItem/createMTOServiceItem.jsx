ec1e240570381d27b2e6f323af2f165766613fdd
Michael Valdes <michael.valdes@truss.works>
2022-10-14 20:29:36 +0000

    Refactor createMTOServiceItem template

--- src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItem.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItem.jsx
@@ -1,21 +1,25 @@
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
+import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+
+const formatChangedValues = (historyRecord) => {
+  const newChangedValues = {
+    shipment_type: historyRecord.context[0]?.shipment_type,
+    service_item_name: historyRecord.context[0]?.name,
+    ...historyRecord.changedValues,
+  };
+  return { ...historyRecord, changedValues: newChangedValues };
+};
 
 export default {
   action: a.INSERT,
   eventName: o.createMTOServiceItem,
   tableName: t.mto_service_items,
-  detailsType: d.LABELED,
   getEventNameDisplay: () => 'Requested service item',
-  getDetailsLabeledDetails: ({ changedValues, context }) => {
-    const newChangedValues = {
-      shipment_type: context[0]?.shipment_type,
-      service_item_name: context[0]?.name,
-      ...changedValues,
-    };
-
-    return newChangedValues;
+  getDetails: (historyRecord) => {
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
   },
 };
--- src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItem.test.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItem.test.jsx
@@ -1,11 +1,13 @@
-import getTemplate from 'constants/MoveHistory/TemplateManager';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
+import { render, screen } from '@testing-library/react';
+
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
 import e from 'constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItem';
+import getTemplate from 'constants/MoveHistory/TemplateManager';
+import o from 'constants/MoveHistory/UIDisplay/Operations';
 
 describe('when given a Create basic service item history record', () => {
-  const item = {
+  const historyRecord = {
     action: a.INSERT,
     changedValues: {
       reason: 'Test',
@@ -20,15 +22,20 @@ describe('when given a Create basic service item history record', () => {
     eventName: o.createMTOServiceItem,
     tableName: t.mto_service_items,
   };
+  const template = getTemplate(historyRecord);
   it('correctly matches the create service item event', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    expect(result.getEventNameDisplay()).toEqual('Requested service item');
-    expect(result.getDetailsLabeledDetails(item)).toMatchObject({
-      service_item_name: 'Domestic uncrating',
-      shipment_type: 'HHG',
-      reason: 'Test',
-      status: 'SUBMITTED',
+    expect(template).toMatchObject(e);
+    expect(template.getEventNameDisplay()).toEqual('Requested service item');
+  });
+  describe('when given a specific set of details', () => {
+    it.each([
+      ['service_item_name', 'Domestic uncrating'],
+      ['shipment_type', 'HHG'],
+      ['reason', 'Test'],
+      ['status', 'SUBMITTED'],
+    ])('dislpays the proper details value for %s', async (label, value) => {
+      render(template.getDetails(historyRecord));
+      expect(screen.getByText(value, { exact: false })).toBeInTheDocument();
     });
   });
 });
