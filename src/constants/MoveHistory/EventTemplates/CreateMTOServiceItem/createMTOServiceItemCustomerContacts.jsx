caa2c7086a4a2c6c8bfd5f784cf3a1f7c7c97323
Michael Valdes <michael.valdes@truss.works>
2022-10-14 20:47:38 +0000

    Refactor createMTOServiceItemCustomerContacts

--- src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItemCustomerContacts.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItemCustomerContacts.jsx
@@ -1,25 +1,27 @@
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
+import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+
+const formatChangedValues = (historyRecord) => {
+  const { type, time_military: timeMilitary } = historyRecord.changedValues;
+  const deliveryTimeOrder = type === 'FIRST' ? 'first_available_delivery_time' : 'second_available_delivery_time';
+  const newChangedValues = {
+    ...historyRecord.changedValues,
+  };
+
+  newChangedValues[deliveryTimeOrder] = timeMilitary;
+  return { ...historyRecord, changedValues: newChangedValues };
+};
 
 export default {
   action: a.INSERT,
   eventName: o.createMTOServiceItem,
   tableName: t.mto_service_item_customer_contacts,
-  detailsType: d.LABELED,
   getEventNameDisplay: () => 'Requested service item',
-  getDetailsLabeledDetails: ({ changedValues }) => {
-    const { type, time_military: timeMilitary } = changedValues;
-
-    const deliveryTimeOrder = type === 'FIRST' ? 'first_available_delivery_time' : 'second_available_delivery_time';
-
-    const newChangedValues = {
-      ...changedValues,
-    };
-
-    newChangedValues[deliveryTimeOrder] = timeMilitary;
-
-    return newChangedValues;
+  getDetails: (historyRecord) => {
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
   },
 };
