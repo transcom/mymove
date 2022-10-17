ceb73003466e3d03ec0aa8563b568aa0a1e6d669
Katy Chambers <katyc@truss.works>
2022-10-17 23:31:44 +0000

    Another pass at adding shipment ids

--- src/constants/MoveHistory/EventTemplates/createStandardServiceItem.js
+++ src/constants/MoveHistory/EventTemplates/createStandardServiceItem.js
@@ -11,6 +11,8 @@ export default {
   detailsType: d.PLAIN_TEXT,
   getEventNameDisplay: () => 'Approved service item',
   getDetailsPlainText: (historyRecord) => {
-    return `${shipmentTypes[historyRecord.context[0]?.shipment_type]} shipment, ${historyRecord.context[0]?.name}`;
+    const shipmentType = historyRecord.context[0]?.shipment_type;
+    const shipmentIdDisplay = historyRecord.context[0].shipment_id_abbr.toUpperCase();
+    return `${shipmentTypes[shipmentType]} shipment #${shipmentIdDisplay}, ${historyRecord.context[0]?.name}`;
   },
 };
