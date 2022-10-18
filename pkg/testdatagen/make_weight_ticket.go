000f595bc7cc4fabb8846f9c5e8bf71a070459d1
Duncan Spencer <duncan@truss.works>
2022-10-18 20:46:21 +0000

    basic ppm shipment document fetcher tests

--- pkg/testdatagen/make_weight_ticket.go
+++ pkg/testdatagen/make_weight_ticket.go
@@ -17,7 +17,7 @@ func MakeMinimalWeightTicket(db *pop.Connection, assertions Assertions) models.W
 	// so we'll look at assertions.WeightTicket.<Document>
 	emptyDocument := GetOrCreateDocument(db, assertions.WeightTicket.EmptyDocument, assertions)
 	fullDocument := GetOrCreateDocument(db, assertions.WeightTicket.FullDocument, assertions)
-	trailerDocument := GetOrCreateDocument(db, assertions.WeightTicket.FullDocument, assertions)
+	trailerDocument := GetOrCreateDocument(db, assertions.WeightTicket.ProofOfTrailerOwnershipDocument, assertions)
 
 	newWeightTicket := models.WeightTicket{
 		PPMShipmentID:                     ppmShipment.ID,
