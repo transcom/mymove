3ffa405b734f0cbeabea12c02937e1f4d7874db3
Michael Valdes <michael.valdes@truss.works>
2022-10-19 16:19:13 +0000

    Refactor updateOrder.test

--- src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.test.jsx
+++ src/constants/MoveHistory/EventTemplates/updateOrders/updateOrder.test.jsx
@@ -13,16 +13,19 @@ describe('when given an Order update history record', () => {
     changedValues: { old_duty_location_id: 'ID1', new_duty_location_id: 'ID2', has_dependents: 'false' },
     context: [{ old_duty_location_name: 'old name', new_duty_location_name: 'new name' }],
   };
-  it('correctly matches the Update orders event', () => {
-    const result = getTemplate(historyRecord);
-    expect(result).toMatchObject(e);
-    render(result.getDetails(historyRecord));
-    // expect to have merged context and changedValues
-    expect(screen.queryByText('ID1')).not.toBeInTheDocument();
-    expect(screen.queryByText('ID2')).not.toBeInTheDocument();
-    expect(screen.queryByText('old name')).not.toBeInTheDocument();
-    expect(screen.getByText('New duty location name')).toBeInTheDocument();
-    expect(screen.getByText('new name', { exact: false })).toBeInTheDocument();
-    expect(screen.getByText('Dependents included')).toBeInTheDocument();
+  it('correctly matches to the proper template', () => {
+    const template = getTemplate(historyRecord);
+    expect(template).toMatchObject(e);
+  });
+  describe('When given a specific set of details for updated orders', () => {
+    it.each([
+      ['New duty location name', ': new name'],
+      ['Dependents included', ': false'],
+    ])('displays the proper details value for %s', async (label, value) => {
+      const template = getTemplate(historyRecord);
+      render(template.getDetails(historyRecord));
+      expect(screen.getByText(label)).toBeInTheDocument();
+      expect(screen.getByText(value)).toBeInTheDocument();
+    });
   });
 });
