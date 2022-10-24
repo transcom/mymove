1f92484c549e4472d2980d2d2936804a3d97c427
ruizajtruss <ruizaj@truss.works>
2022-10-24 23:59:40 +0000

    refactored template and testing

---
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOServiceItem/updateMTOServiceItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import o from 'constants/MoveHistory/UIDisplay/Operations';
+import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import a from 'constants/MoveHistory/Database/Actions';
+import t from 'constants/MoveHistory/Database/Tables';
+import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+
+const formatChangedValues = (historyRecord) => {
+  const { changedValues, context } = historyRecord;
+  let newChangedValues = changedValues;
+
+  if (historyRecord.context) {
+    newChangedValues = {
+      shipment_type: context[0]?.shipment_type,
+      shipment_id_display: context[0]?.shipment_id_abbr.toUpperCase(),
+      service_item_name: context[0]?.name,
+      ...newChangedValues,
+    };
+  }
+
+  return { ...historyRecord, changedValues: newChangedValues };
+};
+
+export default {
+  action: a.UPDATE,
+  eventName: o.updateMTOServiceItem,
+  tableName: t.mto_service_items,
+  detailsType: d.LABELED,
+  getEventNameDisplay: () => 'Updated service item',
+  getDetails: (historyRecord) => {
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
+  },
+};
