e73d75f43081923d49bdee528cb46ea5306b1cde
Reggie Riser <reggie@truss.works>
2022-10-24 21:48:07 +0000

    Refactored assets to use go's built-in embed capability

--- pkg/notifications/move_submitted.go
+++ pkg/notifications/move_submitted.go
@@ -16,9 +16,9 @@ import (
 )
 
 var (
-	moveSubmittedRawTextTemplate = string(assets.MustAsset("pkg/notifications/templates/move_submitted_template.txt"))
+	moveSubmittedRawTextTemplate = string(assets.MustAsset("notifications/templates/move_submitted_template.txt"))
 	moveSubmittedTextTemplate    = text.Must(text.New("text_template").Parse(moveSubmittedRawTextTemplate))
-	moveSubmittedRawHTMLTemplate = string(assets.MustAsset("pkg/notifications/templates/move_submitted_template.html"))
+	moveSubmittedRawHTMLTemplate = string(assets.MustAsset("notifications/templates/move_submitted_template.html"))
 	moveSubmittedHTMLTemplate    = html.Must(html.New("text_template").Parse(moveSubmittedRawHTMLTemplate))
 )
 
