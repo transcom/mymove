eae20a6192135f96c8cde3fcacf43edfcdeadb79
Duncan Spencer <duncan@truss.works>
2022-10-24 17:21:59 +0000

    update column comments

--- migrations/app/schema/20221020162508_rename_pro_gear_weight_document_columns.up.sql
+++ migrations/app/schema/20221020162508_rename_pro_gear_weight_document_columns.up.sql
@@ -15,3 +15,7 @@ ALTER TABLE progear_weight_tickets
 
 ALTER TABLE progear_weight_tickets
 	RENAME full_document_id TO document_id;
+
+COMMENT on COLUMN progear_weight_tickets.weight IS 'Stores the weight of the the pro-gear in pounds.';
+COMMENT on COLUMN progear_weight_tickets.document_id IS 'The ID of the document that is associated with the user uploads containing the pro-gear weight.';
+
