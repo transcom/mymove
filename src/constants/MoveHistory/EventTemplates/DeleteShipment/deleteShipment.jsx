260e58353562b1899604ac97b09ce0e2aa2e5406
ruizajtruss <ruizaj@truss.works>
2022-10-26 03:28:55 +0000

    removing unused import

--- src/constants/MoveHistory/EventTemplates/DeleteShipment/deleteShipment.jsx
+++ src/constants/MoveHistory/EventTemplates/DeleteShipment/deleteShipment.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 import a from 'constants/MoveHistory/Database/Actions';
 import o from 'constants/MoveHistory/UIDisplay/Operations';
 import t from 'constants/MoveHistory/Database/Tables';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import { shipmentTypes as s } from 'constants/shipments';
 
 export default {
   action: a.UPDATE,
   eventName: o.deleteShipment,
   tableName: t.mto_shipments,
-  detailsType: d.PLAIN_TEXT,
   getEventNameDisplay: () => 'Deleted Shipment',
   getDetails: ({ context }) => (
     <>
