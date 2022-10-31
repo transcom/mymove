10560fc6cc329b319ffbf3febdb4b6179094fd40
ruizajtruss <ruizaj@truss.works>
2022-10-31 23:51:15 +0000

    refactored testing

--- src/constants/MoveHistory/EventTemplates/UpdateMTOShipmentStatus/updateMTOShipmentStatus.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOShipmentStatus/updateMTOShipmentStatus.test.jsx
@@ -1,29 +1,40 @@
-import t from 'constants/MoveHistory/TemplateManager';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
+import { screen, render } from '@testing-library/react';
+
 import e from 'constants/MoveHistory/EventTemplates/UpdateMTOShipmentStatus/updateMTOShipmentStatus';
+import getTemplate from 'constants/MoveHistory/TemplateManager';
 
 describe('when Prime user cancels a shipment', () => {
-  const item = {
-    action: 'UPDATE',
-    eventName: o.updateMTOShipmentStatus,
-    tableName: 'mto_shipments',
-    detailsType: d.LABELED,
-  };
   const historyRecord = {
+    action: 'UPDATE',
+    eventName: 'updateMTOShipmentStatus',
+    tableName: 'mto_shipments',
     context: [
       {
-        shipment_type: 'HHG shipment',
-        shipment_id_abbr: 'a1b2c',
+        shipment_id_abbr: 'acf7b',
+        shipment_type: 'HHG',
       },
     ],
-    changedValues: {
-      status: 'CANCELED',
-    },
+    changedValues: { status: 'CANCELED' },
   };
-  it('correctly displays shipment type and status', () => {
-    const result = t(item);
-    result.getDetailsLabeledDetails(historyRecord);
-    expect(result).toMatchObject(e);
+
+  it('matches the given history record to the expected template', () => {
+    const template = getTemplate(historyRecord);
+    expect(template).toMatchObject(e);
+  });
+
+  it('displays the proper header for the given history record', () => {
+    const template = getTemplate(historyRecord);
+
+    render(template.getDetails(historyRecord));
+    expect(screen.getByText('HHG shipment #ACF7B')).toBeInTheDocument();
+  });
+
+  describe('When given a specific set of details for a cancelled shipment', () => {
+    it.each([['Status', ': CANCELED']])('displays the proper details value for %s', async (label, value) => {
+      const template = getTemplate(historyRecord);
+      render(template.getDetails(historyRecord));
+      expect(screen.getByText(label)).toBeInTheDocument();
+      expect(screen.getByText(value)).toBeInTheDocument();
+    });
   });
 });
