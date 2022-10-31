481bc2b6b8e49fe9802e5220f7fe945a6e333384
ruizajtruss <ruizaj@truss.works>
2022-10-31 21:01:18 +0000

    test refactor

--- src/constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/updateMoveTaskOrderStatus.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/updateMoveTaskOrderStatus.test.jsx
@@ -1,8 +1,10 @@
+import { render, screen } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/updateMoveTaskOrderStatus';
 
 describe('when given a Move approved history record', () => {
-  const item = {
+  const historyRecord = {
     action: 'UPDATE',
     changedValues: {
       available_to_prime_at: '2022-04-13T15:21:31.746028+00:00',
@@ -11,25 +13,41 @@ describe('when given a Move approved history record', () => {
     eventName: 'updateMoveTaskOrderStatus',
     tableName: 'moves',
   };
-  it('correctly matches the Update move task order status event', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    expect(result.getEventNameDisplay(item)).toEqual('Approved move');
-    expect(result.getDetailsPlainText(item)).toEqual('Created Move Task Order (MTO)');
+
+  it('correctly matches the Update move task order to the proper template', () => {
+    const template = getTemplate(historyRecord);
+    expect(template).toMatchObject(e);
+  });
+
+  it('displays the correct value in the event name and details column', () => {
+    const template = getTemplate(historyRecord);
+
+    render(template.getEventNameDisplay(historyRecord));
+    render(template.getDetails(historyRecord));
+    expect(screen.getByText('Approved move'));
+    expect(screen.getByText('Created Move Task Order (MTO)'));
   });
 });
 
 describe('when given a Move status update history record', () => {
-  const item = {
+  const historyRecord = {
     action: 'UPDATE',
     changedValues: { status: 'CANCELED' },
     eventName: 'updateMoveTaskOrderStatus',
     tableName: 'moves',
   };
+
   it('correctly matches the Update move task order status event', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    expect(result.getEventNameDisplay(item)).toEqual('Move status updated');
-    expect(result.getDetailsPlainText(item)).toEqual('-');
+    const template = getTemplate(historyRecord);
+    expect(template).toMatchObject(e);
+  });
+
+  it('displays the correct value in the event name and details column', () => {
+    const template = getTemplate(historyRecord);
+
+    render(template.getEventNameDisplay(historyRecord));
+    render(template.getDetails(historyRecord));
+    expect(screen.getByText('Move status updated'));
+    expect(screen.getByText('-'));
   });
 });
