3892e27d368a6276c61f3251a284a946cedf9acc
ruizajtruss <ruizaj@truss.works>
2022-10-31 23:39:06 +0000

    refactored template

--- src/constants/MoveHistory/EventTemplates/UpdateMTOShipmentStatus/updateMTOShipmentStatus.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOShipmentStatus/updateMTOShipmentStatus.jsx
@@ -1,19 +1,26 @@
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
+import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+
+const formatChangedValues = (historyRecord) => {
+  const { context, changedValues } = historyRecord;
+
+  const newChangedValues = {
+    ...changedValues,
+    shipment_type: context[0]?.shipment_type,
+    shipment_id_display: context[0]?.shipment_id_abbr.toUpperCase(),
+  };
+
+  return { ...historyRecord, changedValues: newChangedValues };
+};
 
 export default {
   action: a.UPDATE,
   eventName: o.updateMTOShipmentStatus,
   tableName: t.mto_shipments,
-  detailsType: d.LABELED,
   getEventNameDisplay: () => 'Updated shipment',
-  getDetailsLabeledDetails: ({ changedValues, context }) => {
-    return {
-      shipment_type: context[0]?.shipment_type,
-      shipment_id_display: context[0]?.shipment_id_abbr.toUpperCase(),
-      ...changedValues,
-    };
-  },
+  getDetails: (historyRecord) => <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />,
 };
