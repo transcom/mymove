a0574da553dec42c3a91e0aef13df46440499a1f
ruizajtruss <ruizaj@truss.works>
2022-10-31 19:04:40 +0000

    testing refactor

--- src/constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/createBasicServiceItem.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/createBasicServiceItem.test.jsx
@@ -1,8 +1,10 @@
+import { screen, render } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/UpdateMoveTaskOrderStatus/createBasicServiceItem';
 
 describe('when given a Create basic service item history record', () => {
-  const item = {
+  const historyRecord = {
     action: 'INSERT',
     context: [
       {
@@ -12,10 +14,18 @@ describe('when given a Create basic service item history record', () => {
     eventName: 'updateMoveTaskOrderStatus',
     tableName: 'mto_service_items',
   };
-  it('correctly matches the Create basic service item event', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    expect(result.getEventNameDisplay(result)).toEqual('Approved service item');
-    expect(result.getDetailsPlainText(item)).toEqual('Move management');
+
+  it('correctly matches the Create basic service item template', () => {
+    const template = getTemplate(historyRecord);
+
+    expect(template).toMatchObject(e);
+    expect(template.getEventNameDisplay(template)).toEqual('Approved service item');
+  });
+
+  it('displays the expected details column value', () => {
+    const template = getTemplate(historyRecord);
+
+    render(template.getDetails(historyRecord));
+    expect(screen.getByText('Move management')).toBeInTheDocument();
   });
 });
