0e2b19a08c0b485a693ec937ed53d319bec3c046
Namibia Torres <namibia@truss.works>
2022-11-02 12:03:18 +0000

    add creator test

---
+++ pkg/services/progear_weight_ticket/progear_weight_ticket_creator_test.go
@@ -0,0 +1,60 @@
+package progearweightticket
+
+import (
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/apperror"
+	"github.com/transcom/mymove/pkg/auth"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+func (suite *ProgearWeightTicketSuite) TestProgearWeightTicketCreator() {
+	suite.Run("Successfully creates a ProgearWeightTicket", func() {
+		serviceMember := testdatagen.MakeDefaultServiceMember(suite.DB())
+		session := &auth.Session{
+			ServiceMemberID: serviceMember.ID,
+		}
+
+		ppmShipment := testdatagen.MakeMinimalDefaultPPMShipment(suite.DB())
+		progearWeightTicketCreator := NewProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), ppmShipment.ID)
+
+		suite.Nil(err)
+		suite.NotNil(progearWeightTicket)
+		suite.Equal(ppmShipment.ID, progearWeightTicket.PPMShipmentID)
+		suite.NotNil(progearWeightTicket.EmptyDocumentID)
+		suite.NotNil(progearWeightTicket.FullDocumentID)
+		suite.Equal(serviceMember.ID, progearWeightTicket.EmptyDocument.ServiceMemberID)
+		suite.Equal(serviceMember.ID, progearWeightTicket.FullDocument.ServiceMemberID)
+
+		// ADD a test for constructed weight if progear weight ticket document is missing
+	})
+
+	suite.Run("Fails when an invalid ppmShipmentID is used", func() {
+		serviceMember := testdatagen.MakeDefaultServiceMember(suite.DB())
+		session := &auth.Session{
+			ServiceMemberID: serviceMember.ID,
+		}
+
+		progearWeightTicketCreator := NewProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), uuid.Nil)
+
+		suite.Nil(progearWeightTicket)
+		suite.ErrorContains(err, "Valid PPMShipmentID is missing")
+	})
+
+	suite.Run("Invalid serviceMemberID", func() {
+		session := &auth.Session{
+			ServiceMemberID: uuid.Nil,
+		}
+		ppmShipment := testdatagen.MakeMinimalDefaultPPMShipment(suite.DB())
+
+		progearWeightTicketCreator := NewProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), ppmShipment.ID)
+
+		suite.Nil(progearWeightTicket)
+		suite.NotNil(err)
+		suite.IsType(apperror.InvalidInputError{}, err)
+		suite.Equal("Invalid input due to invalid ServiceMemberID", err.Error())
+	})
+}
