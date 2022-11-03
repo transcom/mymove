e896493b7bda02c471f1b52f92b7a70e2a886f8f
ruizajtruss <ruizaj@truss.works>
2022-11-03 23:26:54 +0000

    refactored test

--- src/constants/MoveHistory/EventTemplates/CreateUpload/proofOfServiceDocUpload.test.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateUpload/proofOfServiceDocUpload.test.jsx
@@ -1,21 +1,24 @@
+import { screen, render } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/CreateUpload/proofOfServiceDocUpload';
-import a from 'constants/MoveHistory/Database/Actions';
-import t from 'constants/MoveHistory/Database/Tables';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
 
 describe('when given a proof of service document upload history record', () => {
-  const item = {
-    action: a.INSERT,
-    eventName: o.createUpload,
-    tableName: t.proof_of_service_docs,
+  const historyRecord = {
+    action: 'INSERT',
+    eventName: 'createUpload',
+    tableName: 'proof_of_service_docs',
     context: [{ payment_request_number: '1234-5678-1' }],
   };
   it('correctly matches the proof of service document upload event', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    expect(result.getDetailsPlainText(item)).toEqual(
-      'Proof of service document uploaded for payment request 1234-5678-1',
-    );
+    const template = getTemplate(historyRecord);
+    expect(template).toMatchObject(e);
+  });
+
+  it('displays the proper details message with the correct request number', () => {
+    const template = getTemplate(historyRecord);
+
+    render(template.getDetails(historyRecord));
+    expect(screen.getByText('Proof of service document uploaded for payment request 1234-5678-1')).toBeInTheDocument();
   });
 });
