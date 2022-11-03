2a2892da9838771abfe89ce57feefdde98c78d6c
ruizajtruss <ruizaj@truss.works>
2022-11-03 21:08:32 +0000

    template refactor

--- src/constants/MoveHistory/EventTemplates/CreatePaymentRequest/createPaymentRequest.jsx
+++ src/constants/MoveHistory/EventTemplates/CreatePaymentRequest/createPaymentRequest.jsx
@@ -1,34 +1,42 @@
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
+import LabeledPaymentRequestDetails from 'pages/Office/MoveHistory/LabeledPaymentRequestDetails';
+
+const getLabeledPaymentRequestDetails = (context) => {
+  let moveServices = '';
+  const shipmentServices = {};
+
+  context.forEach((serviceItem) => {
+    if (serviceItem.name === 'Move management' || serviceItem.name === 'Counseling') {
+      moveServices += `, ${serviceItem.name}`;
+    } else {
+      const shipmentId = serviceItem.shipment_id;
+      if (shipmentServices[shipmentId]) {
+        const { serviceItems } = shipmentServices[shipmentId];
+        shipmentServices[shipmentId].serviceItems = `${serviceItems}, ${serviceItem.name}`;
+      } else {
+        shipmentServices[shipmentId] = {
+          serviceItems: serviceItem.name,
+          shipmentType: serviceItem.shipment_type,
+          shipmentIdAbbr: serviceItem.shipment_id_abbr.toUpperCase(),
+          shipmentId,
+        };
+      }
+    }
+  });
+
+  return { moveServices: moveServices.slice(2), shipmentServices: Object.values(shipmentServices) };
+};
 
 export default {
   action: a.INSERT,
   eventName: o.createPaymentRequest,
   tableName: t.payment_requests,
-  detailsType: d.LABELED_PAYMENT_REQUEST,
   getEventNameDisplay: ({ changedValues }) => `Submitted payment request ${changedValues?.payment_request_number}`,
-  getLabeledPaymentRequestDetails: (context) => {
-    let moveServices = '';
-    const shipmentServices = {};
-    context.forEach((serviceItem) => {
-      if (serviceItem.name === 'Move management' || serviceItem.name === 'Counseling') {
-        moveServices += `, ${serviceItem.name}`;
-      } else {
-        const shipmentId = serviceItem.shipment_id;
-        if (shipmentServices[shipmentId]) {
-          const { serviceItems } = shipmentServices[shipmentId];
-          shipmentServices[shipmentId].serviceItems = `${serviceItems}, ${serviceItem.name}`;
-        } else {
-          shipmentServices[shipmentId] = {
-            serviceItems: serviceItem.name,
-            shipmentType: serviceItem.shipment_type,
-            shipmentId,
-          };
-        }
-      }
-    });
-    return { moveServices: moveServices.slice(2), shipmentServices: Object.values(shipmentServices) };
-  },
+  getDetails: ({ context }) => (
+    <LabeledPaymentRequestDetails context={context} getLabeledPaymentRequestDetails={getLabeledPaymentRequestDetails} />
+  ),
 };
