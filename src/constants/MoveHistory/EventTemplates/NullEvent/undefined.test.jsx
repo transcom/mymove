d2e3f11fa332223075dab208fa540be73e7a636c
ruizajtruss <ruizaj@truss.works>
2022-11-05 00:02:56 +0000

    testing refactor

--- src/constants/MoveHistory/EventTemplates/NullEvent/undefined.test.jsx
+++ src/constants/MoveHistory/EventTemplates/NullEvent/undefined.test.jsx
@@ -1,15 +1,33 @@
+import { screen, render } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
 import e from 'constants/MoveHistory/EventTemplates/NullEvent/undefined';
 
 describe('when given an unidentifiable move history record', () => {
-  const item = {
-    action: 'UPDATE',
-    eventName: 'testEventName',
-    tableName: 'mto_agents',
-  };
-  it('correctly matches the Undefined move history event', () => {
-    const result = getTemplate(item);
-    expect(result).toEqual(e);
-    expect(result.getEventNameDisplay(item)).toEqual('Updated shipment');
+  describe('depending on the table name', () => {
+    it.each([
+      ['Updated shipment', 'mto_shipments'],
+      ['Updated shipment', 'mto_agents'],
+      ['Updated shipment', 'addresses'],
+      ['Updated move', 'moves'],
+      ['Updated payment request', 'payment_requests'],
+      ['Updated allowances', 'entitlements'],
+      ['Updated service item', 'mto_service_items'],
+      ['Updated order', 'orders'],
+    ])('it displays the `%s` event name for the table `%s`', async (eventDisplayName, table) => {
+      const historyRecord = {
+        action: null,
+        eventName: 'testname',
+        tableName: `${table}`,
+      };
+
+      const template = getTemplate(historyRecord);
+
+      expect(template).toEqual(e);
+      render(template.getEventNameDisplay(historyRecord));
+      expect(screen.getByText(eventDisplayName)).toBeInTheDocument();
+      render(template.getDetails(historyRecord));
+      expect(screen.getByText('-')).toBeInTheDocument();
+    });
   });
 });
