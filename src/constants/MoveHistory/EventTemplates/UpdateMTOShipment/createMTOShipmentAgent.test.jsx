1668b21197705a938702eb11e2a3ec3dbfd05067
ruizajtruss <ruizaj@truss.works>
2022-11-07 20:51:56 +0000

    test refactor

--- src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/createMTOShipmentAgent.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/createMTOShipmentAgent.test.jsx
@@ -1,14 +1,13 @@
+import { screen, render } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
 import e from 'constants/MoveHistory/EventTemplates/UpdateMTOShipment/createMTOShipmentAgent';
 
-describe('when given an mto shipment agents insert with mto agents table history record', () => {
-  const item = {
+describe('when given a historyRecord that updates a receiving/releasing agent', () => {
+  const historyRecord = {
     action: 'INSERT',
-    eventName: o.updateMTOShipment,
+    eventName: 'updateMTOShipment',
     tableName: 'mto_agents',
-    detailsType: d.LABELED,
     changedValues: {
       email: 'grace@email.com',
       first_name: 'Grace',
@@ -19,102 +18,62 @@ describe('when given an mto shipment agents insert with mto agents table history
     context: [{ shipment_type: 'HHG', shipment_id_abbr: 'a1b2c' }],
   };
 
-  const item2 = {
+  const historyRecord2 = {
     action: 'INSERT',
-    eventName: o.createMTOShipment,
+    eventName: 'updateMTOShipment',
     tableName: 'mto_agents',
-    detailsType: d.LABELED,
     changedValues: {
       email: 'catalina@email.com',
       first_name: 'Catalina',
       last_name: 'Washington',
       phone: '999-999-9999',
-      agent_type: 'RELEASING_AGENT',
+      agent_type: 'RECEIVING_AGENT',
     },
     context: [{ shipment_type: 'HHG', shipment_id_abbr: 'a1b2c' }],
   };
 
-  it('correctly matches the insert mto shipment agent event for releasing agents when shipment updated', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    // expect to have formatted the agent correctly
-    expect(
-      result.getDetailsLabeledDetails({
-        changedValues: item.changedValues,
-        context: item.context,
-      }),
-    ).toEqual({
-      releasing_agent: 'Grace Griffin, 555-555-5555, grace@email.com',
-      email: 'grace@email.com',
-      first_name: 'Grace',
-      last_name: 'Griffin',
-      phone: '555-555-5555',
-      agent_type: 'RELEASING_AGENT',
-      shipment_type: 'HHG',
-      shipment_id_display: 'A1B2C',
+  it('matches the events to the correct template', () => {
+    const template = getTemplate(historyRecord);
+    const template2 = getTemplate(historyRecord2);
+
+    expect(template).toMatchObject(e);
+    expect(template2).toMatchObject(e);
+  });
+
+  it('displays the proper shipment title in the details column', () => {
+    const template = getTemplate(historyRecord);
+    const template2 = getTemplate(historyRecord2);
+
+    render(template.getDetails(historyRecord));
+    render(template2.getDetails(historyRecord2));
+    expect(screen.getAllByText('HHG shipment #A1B2C'));
+  });
+
+  describe('it displayes the proper labeled details for the given releasing agent', () => {
+    it.each([
+      ['Releasing agent', ': Grace Griffin, 555-555-5555, grace@email.com'],
+      ['First name', ': Grace'],
+      ['Last name', ': Griffin'],
+    ])('should display the label `%s` for the value `%s`', async (label, value) => {
+      const template = getTemplate(historyRecord);
+
+      render(template.getDetails(historyRecord));
+      expect(screen.getByText(label)).toBeInTheDocument();
+      expect(screen.getByText(value)).toBeInTheDocument();
     });
   });
 
-  it('correctly matches the insert mto shipment agent event for releasing agents when shipment created', () => {
-    const result = getTemplate(item2);
-    expect(result).toMatchObject(e);
-    // expect to have formatted the agent correctly
-    expect(
-      result.getDetailsLabeledDetails({
-        changedValues: item2.changedValues,
-        context: item2.context,
-      }),
-    ).toEqual({
-      releasing_agent: 'Catalina Washington, 999-999-9999, catalina@email.com',
-      email: 'catalina@email.com',
-      first_name: 'Catalina',
-      last_name: 'Washington',
-      phone: '999-999-9999',
-      agent_type: 'RELEASING_AGENT',
-      shipment_type: 'HHG',
-      shipment_id_display: 'A1B2C',
-    });
-  });
+  describe('it displayes the proper labeled details for the given receiving agent', () => {
+    it.each([
+      ['Receiving agent', ': Catalina Washington, 999-999-9999, catalina@email.com'],
+      ['First name', ': Catalina'],
+      ['Last name', ': Washington'],
+    ])('should display the label `%s` for the value `%s`', async (label, value) => {
+      const template = getTemplate(historyRecord2);
 
-  it('correctly matches the insert mto shipment agent event for receiving agents when shipment updated', () => {
-    const result = getTemplate(item);
-    expect(result).toMatchObject(e);
-    // expect to have formatted the agent correctly
-    expect(
-      result.getDetailsLabeledDetails({
-        changedValues: { ...item.changedValues, agent_type: 'RECEIVING_AGENT' },
-        context: item.context,
-      }),
-    ).toEqual({
-      receiving_agent: 'Grace Griffin, 555-555-5555, grace@email.com',
-      email: 'grace@email.com',
-      first_name: 'Grace',
-      last_name: 'Griffin',
-      phone: '555-555-5555',
-      agent_type: 'RECEIVING_AGENT',
-      shipment_type: 'HHG',
-      shipment_id_display: 'A1B2C',
-    });
-  });
-
-  it('correctly matches the insert mto shipment agent event for receiving agents when shipment created', () => {
-    const result = getTemplate(item2);
-    expect(result).toMatchObject(e);
-    // expect to have formatted the agent correctly
-    expect(
-      result.getDetailsLabeledDetails({
-        changedValues: { ...item2.changedValues, agent_type: 'RECEIVING_AGENT' },
-        context: item2.context,
-      }),
-    ).toEqual({
-      receiving_agent: 'Catalina Washington, 999-999-9999, catalina@email.com',
-      email: 'catalina@email.com',
-      first_name: 'Catalina',
-      last_name: 'Washington',
-      phone: '999-999-9999',
-      agent_type: 'RECEIVING_AGENT',
-      shipment_type: 'HHG',
-      shipment_id_display: 'A1B2C',
+      render(template.getDetails(historyRecord2));
+      expect(screen.getByText(label)).toBeInTheDocument();
+      expect(screen.getByText(value)).toBeInTheDocument();
     });
   });
 });
