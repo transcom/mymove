c79176b24a215173df12b3359c3d5fd23a3e55b5
ruizajtruss <ruizaj@truss.works>
2022-11-07 19:17:18 +0000

    test constants

--- src/constants/MoveHistory/EventTemplates/UpdateMTOStatusServiceCounselingCompleted/updateMTOStatusServiceCounselingCompleted.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOStatusServiceCounselingCompleted/updateMTOStatusServiceCounselingCompleted.test.jsx
@@ -2,15 +2,12 @@ import { screen, render } from '@testing-library/react';
 
 import e from 'constants/MoveHistory/EventTemplates/UpdateMTOStatusServiceCounselingCompleted/updateMTOStatusServiceCounselingCompleted';
 import getTemplate from 'constants/MoveHistory/TemplateManager';
-import a from 'constants/MoveHistory/Database/Actions';
-import t from 'constants/MoveHistory/Database/Tables';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
 
 describe('When given a completed services counseling for a move', () => {
   const historyRecord = {
-    action: a.UPDATE,
-    eventName: o.updateMTOStatusServiceCounselingCompleted,
-    tableName: t.moves,
+    action: 'UPDATE',
+    eventName: 'updateMTOStatusServiceCounselingCompleted',
+    tableName: 'moves',
   };
   it('correctly matches the update mto status services counseling completed event to the proper template', () => {
     const template = getTemplate(historyRecord);
