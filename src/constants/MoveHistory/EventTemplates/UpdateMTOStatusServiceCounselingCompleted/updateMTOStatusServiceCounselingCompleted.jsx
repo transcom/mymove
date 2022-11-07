3e397beccd56d6255cddbed25f6dbd2adbee5852
ruizajtruss <ruizaj@truss.works>
2022-11-07 19:07:39 +0000

    refactored template

--- src/constants/MoveHistory/EventTemplates/UpdateMTOStatusServiceCounselingCompleted/updateMTOStatusServiceCounselingCompleted.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOStatusServiceCounselingCompleted/updateMTOStatusServiceCounselingCompleted.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
 
@@ -7,9 +8,6 @@ export default {
   action: a.UPDATE,
   eventName: o.updateMTOStatusServiceCounselingCompleted,
   tableName: t.moves,
-  detailsType: d.PLAIN_TEXT,
-  getEventNameDisplay: () => `Updated shipment`,
-  getDetailsPlainText: () => {
-    return `Counseling Completed`;
-  },
+  getEventNameDisplay: () => 'Updated shipment',
+  getDetailsPlainText: () => <> Counseling Completed </>,
 };
