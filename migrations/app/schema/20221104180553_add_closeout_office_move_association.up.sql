e6ca1c10ac7fd07d87b50b1de4685e71112625eb
PDickeyTruss <pdickey@truss.works>
2022-11-08 09:46:56 -0900

    Add comment on closeout office id column

--- migrations/app/schema/20221104180553_add_closeout_office_move_association.up.sql
+++ migrations/app/schema/20221104180553_add_closeout_office_move_association.up.sql
@@ -1,1 +1,3 @@
 ALTER TABLE moves ADD COLUMN closeout_office_id uuid REFERENCES transportation_offices(id);
+
+COMMENT on COLUMN moves.closeout_office_id IS 'The ID of the assiociated transportation office that is the closeout office for a move.';
