ba3e1561da29f46d8aedf5db2a3e08173334c7d7
Amina Foon <amina@truss.works>
2022-11-07 20:00:53 -0500

    refactor updateMTOShipmentPrimeEstimatedExcessWeight template and test
    Refactor updateMTOShipmentPrimeEstimatedExcessWeight template and test

--- src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/updateMTOShipmentPrimeEstimatedExcessWeight.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/updateMTOShipmentPrimeEstimatedExcessWeight.jsx
@@ -1,4 +1,5 @@
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
+import React from 'react';
+
 import o from 'constants/MoveHistory/UIDisplay/Operations';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
@@ -7,11 +8,12 @@ export default {
   action: a.UPDATE,
   eventName: o.updateMTOShipment,
   tableName: t.moves,
-  detailsType: d.PLAIN_TEXT,
   getEventNameDisplay: () => 'Updated move',
-  getDetailsPlainText: (historyRecord) => {
-    return historyRecord.changedValues?.excess_weight_qualified_at
-      ? 'Flagged for excess weight, total estimated weight > 90% weight allowance'
-      : '-';
-  },
+  getDetails: (historyRecord) => (
+    <div>
+      {historyRecord.changedValues.excess_weight_qualified_at
+        ? 'Flagged for excess weight, total estimated weight > 90% weight allowance'
+        : '-'}
+    </div>
+  ),
 };
--- src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/updateMTOShipmentPrimeEstimatedExcessWeight.test.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateMTOShipment/updateMTOShipmentPrimeEstimatedExcessWeight.test.jsx
@@ -1,24 +1,28 @@
+import { screen, render } from '@testing-library/react';
+
 import getTemplate from 'constants/MoveHistory/TemplateManager';
-import d from 'constants/MoveHistory/UIDisplay/DetailsTypes';
-import o from 'constants/MoveHistory/UIDisplay/Operations';
 import updateMTOShipmentPrimeEstimatedExcessWeight from 'constants/MoveHistory/EventTemplates/UpdateMTOShipment/updateMTOShipmentPrimeEstimatedExcessWeight';
 
 describe("when Prime user updates a shipment's estimated to one that exceeds the weight limit", () => {
-  const item = {
+  const historyRecord = {
     action: 'UPDATE',
-    eventName: o.updateMTOShipment,
+    eventName: 'updateMTOShipment',
     tableName: 'moves',
-    detailsType: d.PLAIN_TEXT,
     eventNameDisplay: 'Updated move',
     changedValues: {
       excess_weight_qualified_at: '2022-09-07T21:26:47.833768+00:00',
     },
   };
   it('correctly matches the Update mto shipment event', () => {
-    const result = getTemplate(item);
+    const result = getTemplate(historyRecord);
     expect(result).toMatchObject(updateMTOShipmentPrimeEstimatedExcessWeight);
-    expect(result.getDetailsPlainText(item)).toEqual(
-      'Flagged for excess weight, total estimated weight > 90% weight allowance',
-    );
+    expect(result.getEventNameDisplay()).toEqual('Updated move');
+  });
+  describe('it displays the proper labeled details for the component', () => {
+    const result = getTemplate(historyRecord);
+    render(result.getDetails(historyRecord));
+    expect(
+      screen.getByText('Flagged for excess weight, total estimated weight > 90% weight allowance'),
+    ).toBeInTheDocument();
   });
 });
