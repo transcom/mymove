56e6b925728f83c16d4f7488c10cab6fb06c116a
ruizajtruss <ruizaj@truss.works>
2022-11-10 00:38:56 +0000

    added missing event name

--- src/constants/MoveHistory/EventTemplates/UpdateAddress/createAddress.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateAddress/createAddress.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { formatMoveHistoryFullAddress } from 'utils/formatters';
 import a from 'constants/MoveHistory/Database/Actions';
+import o from 'constants/MoveHistory/UIDisplay/Operations';
 import t from 'constants/MoveHistory/Database/Tables';
 import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
 import ADDRESS_TYPE from 'constants/MoveHistory/Database/AddressTypes';
@@ -25,7 +26,7 @@ const formatChangedValues = (historyRecord) => {
 
 export default {
   action: a.INSERT,
-  eventName: '*',
+  eventName: o.updateMTOShipment,
   tableName: t.addresses,
   getEventNameDisplay: () => 'Updated address',
   getDetails: (historyRecord) => <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />,
