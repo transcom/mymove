eefec75cf5860e6efd344204f6931d065ca924b3
Andrew Hobson <ahobson@truss.works>
2022-11-17 16:21:28 +0000

    Remove unused logger from client cert service suite

--- pkg/services/clientcert/client_cert_service_test.go
+++ pkg/services/clientcert/client_cert_service_test.go
@@ -4,21 +4,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
-	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/testingsuite"
 )
 
 type ClientCertServiceSuite struct {
 	*testingsuite.PopTestSuite
-	logger *zap.Logger
 }
 
 func TestUserSuite(t *testing.T) {
 
 	ts := &ClientCertServiceSuite{
 		PopTestSuite: testingsuite.NewPopTestSuite(testingsuite.CurrentPackage(), testingsuite.WithPerTestTransaction()),
-		logger:       zap.NewNop(), // Use a no-op logger during testing
 	}
 	suite.Run(t, ts)
 	ts.PopTestSuite.TearDown()
