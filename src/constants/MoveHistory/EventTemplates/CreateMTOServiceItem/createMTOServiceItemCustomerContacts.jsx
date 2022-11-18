bb4e3eba663d1ca55818298fbfd2ac434d833af6
ruizajtruss <ruizaj@truss.works>
2022-11-18 22:45:57 +0000

    included shipment label and fixed field mapping bug

--- src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItemCustomerContacts.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateMTOServiceItem/createMTOServiceItemCustomerContacts.jsx
@@ -4,15 +4,28 @@ import o from 'constants/MoveHistory/UIDisplay/Operations';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
 import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
+import { getMtoShipmentLabel } from 'utils/formatMtoShipment';
 
 const formatChangedValues = (historyRecord) => {
+  const { changedValues } = historyRecord;
   const { type, time_military: timeMilitary } = historyRecord.changedValues;
+
   const deliveryTimeOrder = type === 'FIRST' ? 'first_available_delivery_time' : 'second_available_delivery_time';
+  const deliveryDateOrder = type === 'FIRST' ? 'first_available_delivery_date' : 'second_available_delivery_date';
   const newChangedValues = {
-    ...historyRecord.changedValues,
+    ...getMtoShipmentLabel(historyRecord),
+    ...changedValues,
   };
 
-  newChangedValues[deliveryTimeOrder] = timeMilitary;
+  if (type === 'FIRST') {
+    newChangedValues[deliveryTimeOrder] = timeMilitary;
+  } else {
+    newChangedValues[deliveryTimeOrder] = timeMilitary;
+    newChangedValues[deliveryDateOrder] = changedValues.first_available_delivery_date;
+    delete newChangedValues.first_available_delivery_date;
+  }
+  // eslint-disable-next-line no-console
+  console.log({ ...historyRecord, changedValues: newChangedValues });
   return { ...historyRecord, changedValues: newChangedValues };
 };
 
