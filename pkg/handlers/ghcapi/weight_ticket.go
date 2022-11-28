e786a4d1f5eedef43e3a3ccc3a8832b895268533
Namibia Torres <namibia@truss.works>
2022-11-28 15:30:21 -0800

    add error handling to update in weight_ticket.go

--- pkg/handlers/ghcapi/weight_ticket.go
+++ pkg/handlers/ghcapi/weight_ticket.go
@@ -1,14 +1,20 @@
 package ghcapi
 
 import (
+	"fmt"
+
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
 	weightticketops "github.com/transcom/mymove/pkg/gen/ghcapi/ghcoperations/ppm"
+	"github.com/transcom/mymove/pkg/gen/ghcmessages"
 	"github.com/transcom/mymove/pkg/handlers"
 	"github.com/transcom/mymove/pkg/handlers/ghcapi/internal/payloads"
 	"github.com/transcom/mymove/pkg/services"
+	mtoshipment "github.com/transcom/mymove/pkg/services/mto_shipment"
 )
 
 // UpdateWeightTicketHandler
@@ -22,11 +28,61 @@ func (h UpdateWeightTicketHandler) Handle(params weightticketops.UpdateWeightTic
 		func(appCtx appcontext.AppContext) (middleware.Responder, error) {
 			payload := params.UpdateWeightTicketPayload
 
+			ppmshipmentID := uuid.FromStringOrNil(params.PpmShipmentID.String())
+			_, err := mtoshipment.FindShipment(appCtx, ppmshipmentID)
+			// Can't find original weight ticket
+			if err != nil {
+				appCtx.Logger().Error("ghcapi.UpdateShipmentHandler", zap.Error(err))
+				switch err.(type) {
+				case apperror.NotFoundError:
+					return weightticketops.NewUpdateWeightTicketNotFound(), err
+				default:
+					msg := fmt.Sprintf("%v | Instance: %v", handlers.FmtString(err.Error()), h.GetTraceIDFromRequest(params.HTTPRequest))
+
+					return weightticketops.NewUpdateWeightTicketInternalServerError().WithPayload(
+						&ghcmessages.Error{Message: &msg},
+					), err
+				}
+			}
+
 			weightTicket := payloads.WeightTicketModelFromUpdate(payload)
 
 			weightTicket.ID = uuid.FromStringOrNil(params.WeightTicketID.String())
 
-			updatedWeightTicket, _ := h.weighTicketUpdater.UpdateWeightTicket(appCtx, *weightTicket, params.IfMatch)
+			handleError := func(err error) (middleware.Responder, error) {
+				appCtx.Logger().Error("ghcapi.UpdateWeightTicketHandler", zap.Error(err))
+
+				switch e := err.(type) {
+				case apperror.NotFoundError:
+					return weightticketops.NewUpdateWeightTicketNotFound(), err
+				case apperror.InvalidInputError:
+					return weightticketops.NewUpdateWeightTicketUnprocessableEntity().WithPayload(
+						payloadForValidationError(
+							handlers.ValidationErrMessage,
+							err.Error(),
+							h.GetTraceIDFromRequest(params.HTTPRequest),
+							e.ValidationErrors,
+						),
+					), err
+				case apperror.PreconditionFailedError:
+					msg := fmt.Sprintf("%v | Instance: %v", handlers.FmtString(err.Error()), h.GetTraceIDFromRequest(params.HTTPRequest))
+					return weightticketops.NewUpdateWeightTicketPreconditionFailed().WithPayload(
+						&ghcmessages.Error{Message: &msg},
+					), err
+				default:
+					msg := fmt.Sprintf("%v | Instance: %v", handlers.FmtString(err.Error()), h.GetTraceIDFromRequest(params.HTTPRequest))
+
+					return weightticketops.NewUpdateWeightTicketInternalServerError().WithPayload(
+						&ghcmessages.Error{Message: &msg},
+					), err
+				}
+			}
+
+			updatedWeightTicket, err := h.weighTicketUpdater.UpdateWeightTicket(appCtx, *weightTicket, params.IfMatch)
+
+			if err != nil {
+				return handleError(err)
+			}
 
 			returnPayload := payloads.WeightTicket(h.FileStorer(), updatedWeightTicket)
 
