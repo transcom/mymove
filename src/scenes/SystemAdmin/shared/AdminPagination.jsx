45b7d4d46abe2c30116d5c8ace5429f7e9c0085c
DymonMoore <dmoore@truss.works>
2022-11-30 10:51:01 -0500

    Fixed compile issues

--- src/scenes/SystemAdmin/shared/AdminPagination.jsx
+++ src/scenes/SystemAdmin/shared/AdminPagination.jsx
@@ -3,14 +3,14 @@ import { Pagination, useListContext } from 'react-admin';
 import styles from './AdminPagination.module.scss';
 
 const AdminPagination = () => {
-const {isLoading, total} = useListContext()
+  const { isLoading, total } = useListContext();
   !isLoading && total === 0 ? (
     <div className={styles['no-results']}>
       There are no results for this access code. Please check your entry to make sure you entered the correct letter
       combination.
     </div>
   ) : (
-    <Pagination rowsPerPageOptions={[]} {...isLoading, total} />
+    <Pagination rowsPerPageOptions={[]} {...isLoading} {...total} />
   );
-  }
+};
 export default AdminPagination;
