f6ad233c9297a6a89aa080e3f0b3203e64d2387d
DymonMoore <dmoore@truss.works>
2022-12-05 16:46:35 -0500

    Added new fields to eval report comfirmation model.

--- src/components/Office/DefinitionLists/EvaluationReportList.jsx
+++ src/components/Office/DefinitionLists/EvaluationReportList.jsx
@@ -45,16 +45,19 @@ const EvaluationReportList = ({ evaluationReport }) => {
             </>
           }
         />
-        <PreviewRow
-          isShown={evaluationReport.travelTimeMinutes >= 0}
-          label="Travel time to inspection"
-          data={convertToHoursAndMinutes(evaluationReport.travelTimeMinutes)}
-        />
-        <PreviewRow
-          isShown={evaluationReport.evaluationLengthMinutes >= 0}
-          label="Evaluation length"
-          data={convertToHoursAndMinutes(evaluationReport.evaluationLengthMinutes)}
-        />
+        {evaluationReport.inspectionType === 'PHYSICAL' && (
+          <>
+            <PreviewRow label="Time departed for evaluation" data={evaluationReport.timeDepart} />
+            <PreviewRow label="Time evaluation started" data={evaluationReport.evalStart} />
+            <PreviewRow label="Time evaluation ended" data={evaluationReport.evalEnd} />
+          </>
+        )}
+        {evaluationReport.inspectionType !== 'PHYSICAL' && (
+          <>
+            <PreviewRow label="Time evaluation started" data={evaluationReport.evalStart} />
+            <PreviewRow label="Time evaluation ended" data={evaluationReport.evalEnd} />
+          </>
+        )}
       </dl>
     </div>
   );
