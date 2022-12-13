b29b10f9b540116c8531a8109ffc7ba70b96362c
StuTruss <stuartb@truss.works>
2022-12-13 18:28:06 +0000

    add trais + buildAddress func

---
+++ pkg/factory/address_factory.go
@@ -0,0 +1,52 @@
+package factory
+
+import (
+	"github.com/go-openapi/swag"
+	"github.com/gobuffalo/pop/v6"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+// BuildAddress creates a single Address and associated service member.
+func BuildAddress(db *pop.Connection, customs []Customization, traits []Trait) models.Address {
+	customs = setupCustomizations(customs, traits)
+
+	// Find address assertion and convert to models address
+	var cAddress models.Address
+	if result := findValidCustomization(customs, User); result != nil {
+		cAddress = result.Model.(models.Address)
+	}
+
+	// Create default Address
+	address := models.Address{
+		StreetAddress1: "123 Any Street",
+		StreetAddress2: swag.String("P.O. Box 12345"),
+		StreetAddress3: swag.String("c/o Some Person"),
+		City:           "Beverly Hills",
+		State:          "CA",
+		PostalCode:     "90210",
+		Country:        swag.String("US"),
+	}
+
+	// Overwrite values with those from customizations
+	testdatagen.MergeModels(&address, cAddress)
+
+	// If db is false, it's a stub. No need to create in database.
+	if db != nil {
+		mustCreate(db, &address)
+	}
+
+	return address
+}
+
+// BuildDefaultAddress makes an Address with default values
+func BuildDefaultAddress(db *pop.Connection) models.Address {
+	FetchOrBuildTariff400ngZip3(db, nil, nil)
+	return BuildAddress(db, nil, nil)
+}
+
+// BuildStubbedAddress returns a stubbed address without saving it to the DB
+func BuildStubbedAddress(customs []Customization, traits []Trait) models.Address {
+	return BuildAddress(nil, customs, traits)
+}
--- pkg/factory/traits.go
+++ pkg/factory/traits.go
@@ -1,6 +1,10 @@
 package factory
 
-import "github.com/transcom/mymove/pkg/models"
+import (
+	"github.com/go-openapi/swag"
+
+	"github.com/transcom/mymove/pkg/models"
+)
 
 // GetTraitNavy is a sample GetTraitFunc
 func GetTraitNavy() []Customization {
@@ -21,6 +25,57 @@ func GetTraitArmy() []Customization {
 		{
 			Model: models.ServiceMember{
 				Affiliation: &army,
+			},
+		},
+	}
+}
+
+// GetTraitAddress2 is a sample GetTraitFunc
+func GetTraitAddress2() []Customization {
+	return []Customization{
+		{
+			Model: models.Address{
+				StreetAddress1: "987 Any Avenue",
+				StreetAddress2: swag.String("P.O. Box 9876"),
+				StreetAddress3: swag.String("c/o Some Person"),
+				City:           "Fairfield",
+				State:          "CA",
+				PostalCode:     "94535",
+				Country:        swag.String("US"),
+			},
+		},
+	}
+}
+
+// GetTraitAddress3 is a sample GetTraitFunc
+func GetTraitAddress3() []Customization {
+	return []Customization{
+		{
+			Model: models.Address{
+				StreetAddress1: "987 Other Avenue",
+				StreetAddress2: swag.String("P.O. Box 1234"),
+				StreetAddress3: swag.String("c/o Another Person"),
+				City:           "Des Moines",
+				State:          "IA",
+				PostalCode:     "50309",
+				Country:        swag.String("US"),
+			},
+		},
+	}
+}
+
+// GetTraitAddress4 is a sample GetTraitFunc
+func GetTraitAddress4() []Customization {
+	return []Customization{
+		{
+			Model: models.Address{
+				StreetAddress1: "987 Over There Avenue",
+				StreetAddress2: swag.String("P.O. Box 1234"),
+				StreetAddress3: swag.String("c/o Another Person"),
+				City:           "Houston",
+				State:          "TX",
+				PostalCode:     "77083",
+				Country:        swag.String("US"),
 			},
 		},
 	}
