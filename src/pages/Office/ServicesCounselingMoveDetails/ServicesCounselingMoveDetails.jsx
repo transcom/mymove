5353aaf3e09d78228e271a8c07a38a8755f0cab2
belle <belle@truss.works>
2022-12-15 23:46:25 +0000

    Trying to fix 1 failing cypress test on adding ppm shipment

--- src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
+++ src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
@@ -52,7 +52,6 @@ const ServicesCounselingMoveDetails = ({ infoSavedAlert, setUnapprovedShipmentCo
 
   let counselorCanEdit;
   let counselorCanEditNonPPM;
-  let counselorCanEditGeneral;
 
   const sections = useMemo(() => {
     return ['shipments', 'orders', 'allowances', 'customer-info'];
@@ -170,7 +169,6 @@ const ServicesCounselingMoveDetails = ({ infoSavedAlert, setUnapprovedShipmentCo
     });
 
     const counselorCanReview = ppmShipmentsInfo.length > 0;
-    counselorCanEditGeneral = move.status === MOVE_STATUSES.NEEDS_SERVICE_COUNSELING;
     counselorCanEdit = move.status === MOVE_STATUSES.NEEDS_SERVICE_COUNSELING && ppmShipmentsOtherStatuses.length > 0;
     counselorCanEditNonPPM =
       move.status === MOVE_STATUSES.NEEDS_SERVICE_COUNSELING && shipmentsInfo.shipmentType !== 'PPM';
@@ -420,7 +418,7 @@ const ServicesCounselingMoveDetails = ({ infoSavedAlert, setUnapprovedShipmentCo
             <DetailsPanel
               className={scMoveDetailsStyles.noPaddingBottom}
               editButton={
-                (counselorCanEdit || counselorCanEditNonPPM || counselorCanEditGeneral) && (
+                (counselorCanEdit || counselorCanEditNonPPM) && (
                   <ButtonDropdown data-testid="addShipmentButton" onChange={handleButtonDropdownChange}>
                     <option value="">Add a new shipment</option>
                     <option data-testid="hhgOption" value={SHIPMENT_OPTIONS_URL.HHG}>
