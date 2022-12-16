44fd8807abc6a80c5a0b3d80cb8cce9c3108f091
Scott Ries <scottmries@truss.works>
2022-12-16 15:54:00 +0000

    Add ReviewExpense component, story, styles, and tests.

---
+++ src/components/Office/PPM/ReviewExpense/ReviewExpense.jsx
@@ -0,0 +1,217 @@
+import React, { useState } from 'react';
+import { number } from 'prop-types';
+import { Formik } from 'formik';
+import classnames from 'classnames';
+import { Form, FormGroup, Label, Radio, Textarea } from '@trussworks/react-uswds';
+import * as Yup from 'yup';
+import moment from 'moment';
+
+import PPMHeaderSummary from '../PPMHeaderSummary/PPMHeaderSummary';
+
+import styles from './ReviewExpense.module.scss';
+
+import { formatCents } from 'utils/formatters';
+import { PPMShipmentShape, ExpenseShape } from 'types/shipment';
+import Fieldset from 'shared/Fieldset';
+import { DatePickerInput } from 'components/form/fields';
+import MaskedTextField from 'components/form/fields/MaskedTextField/MaskedTextField';
+import formStyles from 'styles/form.module.scss';
+import approveRejectStyles from 'styles/approveRejectControls.module.scss';
+import ppmDocumentStatus from 'constants/ppms';
+import { expenseTypeLabels, expenseTypes } from 'constants/ppmExpenseTypes';
+
+const validationSchema = Yup.object().shape({
+  amount: Yup.number().required('Enter the expense amount').min(1, 'Enter an expense amount greater than $0.00'),
+  sitStartDate: Yup.string().required(),
+  sitEndDate: Yup.string().required(),
+  reason: Yup.string()
+    .when('status', {
+      is: ppmDocumentStatus.REJECTED,
+      then: (schema) => schema.required('Add a reason why this receipt is rejected'),
+    })
+    .when('status', {
+      is: ppmDocumentStatus.EXCLUDED,
+      then: (schema) => schema.required('Add a reason why this receipt is excluded'),
+    }),
+  status: Yup.string().required('Reviewing this receipt is required'),
+});
+
+export default function ReviewExpense({ ppmShipment, expense, expenseNumber, ppmNumber }) {
+  const [canEditReason, setCanEditReason] = useState(true);
+
+  const { movingExpenseType, description, amount, paidWithGtcc, sitStartDate, sitEndDate, status, reason } =
+    expense || {};
+
+  const initialValues = {
+    movingExpenseType: movingExpenseType || '',
+    description: description || '',
+    amount: amount ? `${formatCents(amount)}` : '',
+    paidWithGtcc: paidWithGtcc ? 'true' : 'false',
+    sitStartDate: sitStartDate || '',
+    sitEndDate: sitEndDate || '',
+    status: status || '',
+    reason: reason || '',
+  };
+
+  const daysInSIT = sitStartDate && sitEndDate ? moment(sitEndDate).diff(moment(sitStartDate), 'days') : '##';
+
+  const expenseName = movingExpenseType === expenseTypes.STORAGE ? 'Storage' : 'Receipt';
+  return (
+    <div className={classnames(styles.container, 'container--accent--ppm')}>
+      <Formik initialValues={initialValues} validationSchema={validationSchema}>
+        {({ handleChange, values }) => {
+          const handleAcceptChange = (event) => {
+            handleChange(event);
+            setCanEditReason(true);
+          };
+
+          return (
+            <Form className={classnames(formStyles.form, styles.ReviewExpense)}>
+              <PPMHeaderSummary ppmShipment={ppmShipment} ppmNumber={ppmNumber} />
+              <hr />
+              <h3 className={styles.expenseNumber}>
+                {expenseName} {expenseNumber}
+              </h3>
+              <legend className={classnames('usa-label', styles.label)}>Expense type</legend>
+              <div className={styles.displayValue}>{expenseTypeLabels[movingExpenseType]}</div>
+              <legend className={classnames('usa-label', styles.label)}>Description</legend>
+              <div className={styles.displayValue}>{description}</div>
+              <MaskedTextField
+                defaultValue="0"
+                name="amount"
+                label="Amount"
+                id="amount"
+                mask={Number}
+                scale={2} // digits after point, 0 for integers
+                radix="." // fractional delimiter
+                mapToRadix={['.']} // symbols to process as radix
+                padFractionalZeros // if true, then pads zeros at end to the length of scale
+                signed={false} // disallow negative
+                thousandsSeparator=","
+                lazy={false} // immediate masking evaluation
+                prefix="$"
+              />
+              {movingExpenseType === expenseTypes.STORAGE && (
+                <>
+                  <DatePickerInput name="sitStartDate" label="Start date" />
+                  <DatePickerInput name="sitEndDate" label="End date" />
+                  <legend className={classnames('usa-label', styles.label)}>Total days in SIT</legend>
+                  <div className={styles.displayValue} data-testid="days-in-sit">
+                    {daysInSIT}
+                  </div>
+                </>
+              )}
+              <h3 className={styles.reviewHeader}>
+                Review {expenseName.toLowerCase()} {expenseNumber}
+              </h3>
+              <p>Add a review for this {expenseName.toLowerCase()}</p>
+              <Fieldset>
+                <div
+                  className={classnames(approveRejectStyles.statusOption, {
+                    [approveRejectStyles.selected]: values.status === ppmDocumentStatus.APPROVED,
+                  })}
+                >
+                  <Radio
+                    id={`accept-${expense?.id}`}
+                    checked={values.status === ppmDocumentStatus.APPROVED}
+                    value={ppmDocumentStatus.APPROVED}
+                    name="status"
+                    label="Accept"
+                    onChange={handleAcceptChange}
+                    data-testid="acceptRadio"
+                  />
+                </div>
+                <div
+                  className={classnames(approveRejectStyles.statusOption, styles.exclude, {
+                    [approveRejectStyles.selected]: values.status === ppmDocumentStatus.EXCLUDED,
+                  })}
+                >
+                  <Radio
+                    id={`exclude-${expense?.id}`}
+                    checked={values.status === ppmDocumentStatus.EXCLUDED}
+                    value={ppmDocumentStatus.EXCLUDED}
+                    name="status"
+                    label="Exclude"
+                    onChange={handleChange}
+                    data-testid="excludeRadio"
+                  />
+
+                  {values.status === ppmDocumentStatus.EXCLUDED && (
+                    <FormGroup className={styles.reason}>
+                      <Label htmlFor={`excludeReason-${expense?.id}`}>Reason</Label>
+                      {!canEditReason && (
+                        <p data-testid="exclusionReasonReadOnly">{expense?.reason || values.reason}</p>
+                      )}
+
+                      {canEditReason && (
+                        <>
+                          <Textarea
+                            id={`excludeReason-${expense?.id}`}
+                            name="reason"
+                            onChange={handleChange}
+                            value={values.reason}
+                            placeholder="Type something"
+                          />
+                          <div className={styles.hint}>500 characters</div>
+                        </>
+                      )}
+                    </FormGroup>
+                  )}
+                </div>
+                <div
+                  className={classnames(approveRejectStyles.statusOption, styles.reject, {
+                    [approveRejectStyles.selected]: values.status === ppmDocumentStatus.REJECTED,
+                  })}
+                >
+                  <Radio
+                    id={`reject-${expense?.id}`}
+                    checked={values.status === ppmDocumentStatus.REJECTED}
+                    value={ppmDocumentStatus.REJECTED}
+                    name="status"
+                    label="Reject"
+                    onChange={handleChange}
+                    data-testid="rejectRadio"
+                  />
+
+                  {values.status === ppmDocumentStatus.REJECTED && (
+                    <FormGroup className={styles.reason}>
+                      <Label htmlFor={`rejectReason-${expense?.id}`}>Reason</Label>
+                      {!canEditReason && (
+                        <p data-testid="rejectionReasonReadOnly">{expense?.reason || values.reason}</p>
+                      )}
+
+                      {canEditReason && (
+                        <>
+                          <Textarea
+                            id={`rejectReason-${expense?.id}`}
+                            name="reason"
+                            onChange={handleChange}
+                            value={values.reason}
+                            placeholder="Type something"
+                          />
+                          <div className={styles.hint}>500 characters</div>
+                        </>
+                      )}
+                    </FormGroup>
+                  )}
+                </div>
+              </Fieldset>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+}
+
+ReviewExpense.propTypes = {
+  expense: ExpenseShape,
+  ppmShipment: PPMShipmentShape,
+  expenseNumber: number.isRequired,
+  ppmNumber: number.isRequired,
+};
+
+ReviewExpense.defaultProps = {
+  expense: undefined,
+  ppmShipment: undefined,
+};
---
+++ src/components/Office/PPM/ReviewExpense/ReviewExpense.test.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+
+import ReviewExpense from './ReviewExpense';
+
+import { expenseTypes } from 'constants/ppmExpenseTypes';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const defaultProps = {
+  ppmShipment: {
+    id: '32ecb311-edbe-4fd4-96ee-bd693113f3f3',
+    actualPickupPostalCode: '90210',
+    actualMoveDate: '2022-04-30',
+    actualDestinationPostalCode: '94611',
+    hasReceivedAdvance: true,
+    advanceAmountReceived: 60000,
+  },
+  expenseNumber: 1,
+  ppmNumber: 1,
+};
+
+const expenseRequiredProps = {
+  expense: {
+    id: '32ecb311-edbe-4fd4-96ee-bd693113f3f3',
+    ppmShipmentId: '343bb456-63af-4f76-89bd-7403094a5c4d',
+    movingExpenseType: expenseTypes.PACKING_MATERIALS,
+    description: 'boxes, tape, bubble wrap',
+    paidWithGtcc: false,
+    amount: 12345,
+  },
+};
+
+const storageProps = {
+  expense: {
+    ...expenseRequiredProps.expense,
+    movingExpenseType: expenseTypes.STORAGE,
+    sitStartDate: '15 Dec 2022',
+    sitEndDate: '25 Dec 2022',
+  },
+};
+
+describe('ReviewExpenseForm component', () => {
+  describe('displays form', () => {
+    it('renders blank form on load with defaults', async () => {
+      render(<ReviewExpense {...defaultProps} />);
+
+      await waitFor(() => {
+        expect(screen.getByRole('heading', { level: 3, name: 'Receipt 1' })).toBeInTheDocument();
+      });
+
+      expect(screen.getByText('Expense type')).toBeInTheDocument();
+      expect(screen.getByText('Description')).toBeInTheDocument();
+      expect(screen.getByLabelText('Amount')).toBeInstanceOf(HTMLInputElement);
+
+      expect(screen.getByRole('heading', { level: 3, name: 'Review receipt 1' })).toBeInTheDocument();
+
+      expect(screen.getByText('Add a review for this receipt')).toBeInTheDocument();
+
+      expect(screen.getByLabelText('Accept')).toBeInstanceOf(HTMLInputElement);
+      expect(screen.getByLabelText('Exclude')).toBeInstanceOf(HTMLInputElement);
+      expect(screen.getByLabelText('Reject')).toBeInstanceOf(HTMLInputElement);
+    });
+
+    it('populates edit form with existing expense values', async () => {
+      render(<ReviewExpense {...defaultProps} {...expenseRequiredProps} />);
+
+      await waitFor(() => {
+        expect(screen.getByText('Packing materials')).toBeInTheDocument();
+      });
+      expect(screen.getByText('boxes, tape, bubble wrap')).toBeInTheDocument();
+
+      expect(screen.getByLabelText('Amount')).toHaveDisplayValue('123.45');
+    });
+
+    it('shows SIT fields when expense type is Storage', async () => {
+      render(<ReviewExpense {...defaultProps} {...storageProps} />);
+      await waitFor(() => {
+        expect(screen.getByLabelText('Start date')).toBeInstanceOf(HTMLInputElement);
+      });
+      expect(screen.getByLabelText('End date')).toBeInstanceOf(HTMLInputElement);
+      expect(screen.getByText('Total days in SIT')).toBeInTheDocument();
+    });
+
+    it('populates edit form with existing storage values', async () => {
+      render(<ReviewExpense {...defaultProps} {...storageProps} />);
+      await waitFor(() => {
+        expect(screen.getByLabelText('Start date')).toHaveDisplayValue('15 Dec 2022');
+      });
+      expect(screen.getByLabelText('End date')).toHaveDisplayValue('25 Dec 2022');
+    });
+
+    it('correctly displays days in SIT', async () => {
+      render(<ReviewExpense {...defaultProps} {...storageProps} />);
+      await waitFor(() => {
+        expect(screen.getByTestId('days-in-sit')).toHaveTextContent('10');
+      });
+    });
+  });
+});
