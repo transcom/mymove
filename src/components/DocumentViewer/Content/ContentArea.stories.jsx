ffe452a02a48457266a3bf8627414a720b7114d1
Pearl Truss <pearl@truss.works>
2022-12-19 20:32:03 +0000

    add xls example to storybook

--- src/components/DocumentViewer/Content/ContentArea.stories.jsx
+++ src/components/DocumentViewer/Content/ContentArea.stories.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import pdf from '../sample.pdf';
+import xls from '../sample.xls';
 
 import DocViewerContent from './Content';
 
 export default {
   title: 'Components/Document Viewer/Content',
+  component: DocViewerContent,
   parameters: {
     happo: false,
   },
+  decorators: [
+    (Story) => (
+      <GridContainer>
+        <Grid row>
+          <Grid col desktop={{ col: 8, offset: 2 }}>
+            <Story />
+          </Grid>
+        </Grid>
+      </GridContainer>
+    ),
+  ],
 };
 
-export const ContentArea = () => <DocViewerContent fileType="pdf" filePath={pdf} />;
+const Template = (args) => <DocViewerContent {...args} />;
+
+export const PdfContent = Template.bind({});
+PdfContent.args = {
+  fileType: 'pdf',
+  filePath: pdf,
+};
+
+export const XlsContent = Template.bind({});
+XlsContent.args = {
+  fileType: 'xls',
+  filePath: xls,
+};
