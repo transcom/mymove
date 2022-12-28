8f7448339c36090d453ba8eebb984a7220a8c187
pearl-truss <pearl@truss.works>
2022-12-28 17:17:08 +0000

    add document viewer to ReviewDocuments page with stylign and test

--- src/pages/Office/PPM/ReviewDocuments/ReviewDocuments.jsx
+++ src/pages/Office/PPM/ReviewDocuments/ReviewDocuments.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default function ReviewDocuments() {
-  return <h1>Placeholder page for review documents</h1>;
-}
+import styles from './ReviewDocuments.module.scss';
+
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
+import SomethingWentWrong from 'shared/SomethingWentWrong';
+import { MatchShape } from 'types/router';
+import DocumentViewer from 'components/DocumentViewer/DocumentViewer';
+import { useMoveDetailsQueries } from 'hooks/queries';
+
+export const ReviewDocuments = ({ match }) => {
+  const { shipmentId, moveCode } = match.params;
+  const { mtoShipments, isLoading, isError } = useMoveDetailsQueries(moveCode);
+
+  if (isLoading) return <LoadingPlaceholder />;
+  if (isError) return <SomethingWentWrong />;
+
+  const { ppmShipment } = mtoShipments.filter((shipment) => shipment.id === shipmentId)[0];
+  let uploads = [];
+  ppmShipment?.weightTickets?.forEach((weightTicket) => {
+    uploads = uploads.concat(weightTicket.emptyDocument.uploads);
+    uploads = uploads.concat(weightTicket.fullDocument.uploads);
+  });
+
+  return (
+    <div data-testid="ReviewDocuments" className={styles.ReviewDocuments}>
+      <div className={styles.embed}>
+        <DocumentViewer files={uploads} />
+      </div>
+    </div>
+  );
+};
+
+ReviewDocuments.propTypes = {
+  match: MatchShape.isRequired,
+};
+
+export default withRouter(ReviewDocuments);
