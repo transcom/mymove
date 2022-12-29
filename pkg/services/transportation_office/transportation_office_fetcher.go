8b370ce11a0fbc2b5c8d4cbab020cea51697ebc6
DymonMoore <dmoore@truss.works>
2022-12-29 18:23:56 +0000

    Added find transportation office method to fetcher

--- pkg/services/transportation_office/transportation_office_fetcher.go
+++ pkg/services/transportation_office/transportation_office_fetcher.go
@@ -3,7 +3,9 @@ package transportationoffice
 import (
 	"database/sql"
 
+	"github.com/gobuffalo/pop/v6"
 	"github.com/gofrs/uuid"
+	"github.com/pkg/errors"
 	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/apperror"
 	"github.com/transcom/mymove/pkg/models"
@@ -18,7 +20,7 @@ func NewTransportationOfficesFetcher() services.TransportationOfficesFetcher {
 }
 
 func (o transportationOfficesFetcher) GetTransportationOffices(appCtx appcontext.AppContext, search string) (*models.TransportationOffices, error) {
-	officeList, err := models.FindTransportationOffice(appCtx.DB(), search)
+	officeList, err := FindTransportationOffice(appCtx.DB(), search)
 
 	if err != nil {
 		switch err {
@@ -30,4 +32,38 @@ func (o transportationOfficesFetcher) GetTransportationOffices(appCtx appcontext
 	}
 
 	return &officeList, nil
+}
+
+func FindTransportationOffice(tx *pop.Connection, search string) (models.TransportationOffices, error) {
+	var officeList []models.TransportationOffice
+
+	// The % operator filters out strings that are below this similarity threshold
+	err := tx.Q().RawQuery("SET pg_trgm.similarity_threshold = 0.03").Exec()
+	if err != nil {
+		return officeList, err
+	}
+
+	// TODO: copied over from duty_location query, what else can be simplified or left behind? is there a better way to grab name, address?
+	// eager loading doesn't make sense - can't do subqueries there easily
+	sqlQuery := `
+	with names as (select office.id as transportation_office_id, office.name, similarity(office.name, $1) as sim
+	from transportation_offices as office inner join addresses a2 on office.address_id = a2.id
+	where name % $1
+	limit 5)
+select office.*
+	from names n inner join transportation_offices office on n.transportation_office_id = office.id
+	group by office.id
+	order by max(n.sim) desc, office.name
+	limit 5`
+
+	query := tx.Q().RawQuery(sqlQuery, search)
+	if err := query.All(&officeList); err != nil {
+		if errors.Cause(err).Error() != models.RecordNotFoundErrorString {
+			return officeList, err
+		}
+	}
+	for i, _ := range officeList {
+		tx.Load(&officeList[i], "Address")
+	}
+	return officeList, nil
 }
