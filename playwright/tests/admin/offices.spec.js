74f54738a1de14b8c093ef6609de0ff2543d79e0
Andrew Hobson <ahobson@truss.works>
2022-12-30 16:30:03 +0000

    Convert admin offices test to playwright

---
+++ playwright/tests/admin/offices.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { test, expect } = require('../utils/adminTest');
+
+test.describe('Offices Page', () => {
+  test('successfully navigates to offices page', async ({ page, adminPage }) => {
+    await adminPage.signInAsNewAdminUser();
+    await page.getByRole('menuitem', { name: 'Offices' }).click();
+
+    expect(page.url()).toContain('/system/offices');
+    await expect(page.locator('header')).toContainText('Offices');
+    await expect(page.getByLabel('Search by Office Name')).toBeEditable();
+
+    const columnLabels = ['Id', 'Name', 'Latitude', 'Longitude', 'Gbloc'];
+    for (const label of columnLabels) {
+      await expect(page.locator('table')).toContainText(label);
+    }
+  });
+});
