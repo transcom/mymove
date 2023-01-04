0fc87493584b54c708769fb934bfe22d36951d0e
DymonMoore <dmoore@truss.works>
2023-01-03 20:12:47 -0500

    Added server tests to check transportation office fetcher finds correct office and sorts results in order

---
+++ pkg/services/transportation_office/transportation_office_fetcher_test.go
@@ -0,0 +1,83 @@
+package transportationoffice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+	"github.com/transcom/mymove/pkg/testingsuite"
+)
+
+type TransportationOfficeServiceSuite struct {
+	*testingsuite.PopTestSuite
+}
+
+func TestTransportationOfficeServiceSuite(t *testing.T) {
+
+	ts := &TransportationOfficeServiceSuite{
+		PopTestSuite: testingsuite.NewPopTestSuite(
+			testingsuite.CurrentPackage(),
+			testingsuite.WithPerTestTransaction(),
+		),
+	}
+	suite.Run(t, ts)
+	ts.PopTestSuite.TearDown()
+}
+
+func (suite *TransportationOfficeServiceSuite) Test_SearchTransportationOffice() {
+
+	transportationOffice := testdatagen.MakeTransportationOffice(suite.DB(), testdatagen.Assertions{
+		TransportationOffice: models.TransportationOffice{
+			Name:             "LRC Fort Knox",
+			ProvidesCloseout: true,
+		},
+	})
+	office, err := FindTransportationOffice(suite.AppContextForTest(), "LRC Fort Knox")
+
+	suite.NoError(err)
+	suite.Equal(transportationOffice.Name, office[0].Name)
+	suite.Equal(transportationOffice.Address.ID, office[0].Address.ID)
+	suite.Equal(transportationOffice.Gbloc, office[0].Gbloc)
+
+}
+
+func (suite *TransportationOfficeServiceSuite) Test_SearchWithNoTransportationOffices() {
+
+	office, err := FindTransportationOffice(suite.AppContextForTest(), "LRC Fort Knox")
+	suite.NoError(err)
+	suite.Len(office, 0)
+}
+
+func (suite *TransportationOfficeServiceSuite) Test_SortedTransportationOffices() {
+
+	transportationOffice1 := testdatagen.MakeTransportationOffice(suite.DB(), testdatagen.Assertions{
+		TransportationOffice: models.TransportationOffice{
+			Name:             "JPPSO",
+			ProvidesCloseout: true,
+		},
+	})
+
+	transportationOffice3 := testdatagen.MakeTransportationOffice(suite.DB(), testdatagen.Assertions{
+		TransportationOffice: models.TransportationOffice{
+			Name:             "SO",
+			ProvidesCloseout: true,
+		},
+	})
+
+	transportationOffice2 := testdatagen.MakeTransportationOffice(suite.DB(), testdatagen.Assertions{
+		TransportationOffice: models.TransportationOffice{
+			Name:             "PPSO",
+			ProvidesCloseout: true,
+		},
+	})
+
+	office, err := FindTransportationOffice(suite.AppContextForTest(), "JPPSO")
+
+	suite.NoError(err)
+	suite.Equal(transportationOffice1.Name, office[0].Name)
+	suite.Equal(transportationOffice2.Name, office[1].Name)
+	suite.Equal(transportationOffice3.Name, office[2].Name)
+
+}
