1b43dae38465046fdc6500a0f98837aa0bb3f66f
DymonMoore <dmoore@truss.works>
2023-01-03 20:14:08 -0500

    Updated transportation office fetcher interface

--- pkg/services/transportation_office.go
+++ pkg/services/transportation_office.go
@@ -1,14 +1,11 @@
 package services
 
 import (
-	"github.com/gofrs/uuid"
-
 	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/models"
 )
 
 //go:generate mockery --name TransportationOfficesFetcher --disable-version-string
 type TransportationOfficesFetcher interface {
-	GetTransportationOffice(appCtx appcontext.AppContext, transportationOfficeID uuid.UUID, includeOnlyPPMCloseoutOffices bool) (*models.TransportationOffice, error)
-	GetTransportationOffices(appCtx appcontext.AppContext) (*models.TransportationOffices, error)
+	GetTransportationOffices(appCtx appcontext.AppContext, search string) (*models.TransportationOffices, error)
 }
