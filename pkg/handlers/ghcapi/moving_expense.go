dd84661ec688a55d07fc23d405c119a33f1a2908
Felipe Lee <felipe@truss.works>
2023-01-05 00:22:45 +0000

    MB-13827 Add error handling to ghc update moving expense handler

--- pkg/handlers/ghcapi/moving_expense.go
+++ pkg/handlers/ghcapi/moving_expense.go
@@ -3,9 +3,12 @@ package ghcapi
 import (
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 
 	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/apperror"
 	movingexpenseops "github.com/transcom/mymove/pkg/gen/ghcapi/ghcoperations/ppm"
+	"github.com/transcom/mymove/pkg/gen/ghcmessages"
 	"github.com/transcom/mymove/pkg/handlers"
 	"github.com/transcom/mymove/pkg/handlers/ghcapi/internal/payloads"
 	"github.com/transcom/mymove/pkg/services"
@@ -19,13 +22,47 @@ type UpdateMovingExpenseHandler struct {
 
 func (h UpdateMovingExpenseHandler) Handle(params movingexpenseops.UpdateMovingExpenseParams) middleware.Responder {
 	return h.AuditableAppContextFromRequestWithErrors(params.HTTPRequest, func(appCtx appcontext.AppContext) (middleware.Responder, error) {
-		payload := params.UpdateMovingExpense
+		if !appCtx.Session().IsOfficeApp() {
+			return movingexpenseops.NewUpdateMovingExpenseForbidden(), apperror.NewSessionError("Request should come from the office app.")
+		}
 
-		movingExpense := payloads.MovingExpenseModelFromUpdate(payload)
+		movingExpense := payloads.MovingExpenseModelFromUpdate(params.UpdateMovingExpense)
 
 		movingExpense.ID = uuid.FromStringOrNil(params.MovingExpenseID.String())
 
-		updatedMovingExpense, _ := h.movingExpenseUpdater.UpdateMovingExpense(appCtx, *movingExpense, params.IfMatch)
+		updatedMovingExpense, err := h.movingExpenseUpdater.UpdateMovingExpense(appCtx, *movingExpense, params.IfMatch)
+
+		if err != nil {
+			appCtx.Logger().Error("ghcapi.UpdateMovingExpenseHandler error", zap.Error(err))
+
+			switch e := err.(type) {
+			case apperror.NotFoundError:
+				return movingexpenseops.NewUpdateMovingExpenseNotFound(), nil
+			case apperror.QueryError:
+				if e.Unwrap() != nil {
+					// If you can unwrap, log the error (usually a pq error) for better debugging
+					appCtx.Logger().Error(
+						"ghcapi.UpdateMovingExpenseHandler error",
+						zap.Error(e.Unwrap()),
+					)
+				}
+
+				return movingexpenseops.NewUpdateMovingExpenseInternalServerError(), nil
+			case apperror.PreconditionFailedError:
+				return movingexpenseops.NewUpdateMovingExpensePreconditionFailed().WithPayload(&ghcmessages.Error{Message: handlers.FmtString(err.Error())}), nil
+			case apperror.InvalidInputError:
+				return movingexpenseops.NewUpdateMovingExpenseUnprocessableEntity().WithPayload(
+					payloadForValidationError(
+						handlers.ValidationErrMessage,
+						err.Error(),
+						h.GetTraceIDFromRequest(params.HTTPRequest),
+						e.ValidationErrors,
+					),
+				), nil
+			default:
+				return movingexpenseops.NewUpdateMovingExpenseInternalServerError(), nil
+			}
+		}
 
 		returnPayload := payloads.MovingExpense(h.FileStorer(), updatedMovingExpense)
 
