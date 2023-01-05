3f0c010285db8c208831e7ecda19c229dd088045
Roger Steve Ruiz <rogeruiz@truss.works>
2023-01-05 22:54:25 +0000

    Refactoring HERE to use API Key;
    This is an initial commit which will be continued at a later date.

--- pkg/route/here_planner_test.go
+++ pkg/route/here_planner_test.go
@@ -105,7 +105,7 @@ func (suite *HereFullSuite) SetupTest() {
 	geocodeEndpoint := os.Getenv("HERE_MAPS_GEOCODE_ENDPOINT")
 	routingEndpoint := os.Getenv("HERE_MAPS_ROUTING_ENDPOINT")
 	testAppID := os.Getenv("HERE_MAPS_APP_ID")
-	testAppCode := os.Getenv("HERE_MAPS_APP_CODE")
+	testAppCode := os.Getenv("HERE_MAPS_API_KEY")
 	if len(geocodeEndpoint) == 0 || len(routingEndpoint) == 0 ||
 		len(testAppID) == 0 || len(testAppCode) == 0 {
 		suite.Fail("You must set HERE_... environment variables to run this test")
