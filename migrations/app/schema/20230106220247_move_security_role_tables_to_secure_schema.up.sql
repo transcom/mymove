3a61cfe0ca0bbb355726974257898b787b79be1c
Deep Marreddy <deep@truss.works>
2023-01-13 21:05:50 +0000

    wip

---
+++ migrations/app/schema/20230106220247_move_security_role_tables_to_secure_schema.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE admin_users SET SCHEMA schema_secure;
+ALTER TABLE archived_signed_certifications SET SCHEMA schema_secure;
+ALTER TABLE client_certs SET SCHEMA schema_secure;
+ALTER TABLE users SET SCHEMA schema_secure;
+ALTER TABLE users_roles SET SCHEMA schema_secure;
---
+++ migrations/app/schema/20230109195101_set-search-path-order.up.sql
@@ -0,0 +1,3 @@
+SET search_path TO "$user", "public", "schema_secure";
+ALTER ROLE postgres SET search_path = "$user", "public", "schema_secure";
+
