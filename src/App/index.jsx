f93d639709b01ef4eae43a5ccb3a7ed8af46540c
Amina Foon <amina@truss.works>
2023-01-13 17:50:46 -0500

    set defaults to reflect previous and expected behavior

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -41,11 +41,11 @@ const officeQueryConfig = new QueryClient({
       // do not re-query on window refocus
       refetchOnWindowFocus: false,
       // onError: noop, // TODO - log errors?
-      // networkMode: 'offlineFirst', // restoring previous-behavior. Without this, it will be paused without a network
+      networkMode: 'offlineFirst', // restoring previous-behavior. Without this, it will be paused without a network
     },
     mutations: {
       // onError: noop, // TODO - log errors?
-      // networkMode: 'offlineFirst', // restoring previous-behavior. Without this, it will be paused without a network
+      networkMode: 'offlineFirst', // restoring previous-behavior. Without this, it will be paused without a network
     },
   },
 });
