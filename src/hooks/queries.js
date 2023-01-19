f54a3aa953c1e825f8c0f0667bc477891c7bd5b3
belle <belle@truss.works>
2023-01-19 00:24:14 +0000

    Cleaned up code in queries.js, added min-width in the reviewdocumentssidepanel css to fix storybook alignment issue

--- src/hooks/queries.js
+++ src/hooks/queries.js
@@ -184,24 +184,7 @@ export const usePPMShipmentDocsQueries = (shipmentID) => {
   const { data: mtoShipment, ...mtoShipmentQuery } = useQuery([MTO_SHIPMENT, shipmentID], getMTOShipmentByID);
 
   const ppmShipmentId = mtoShipment?.ppmShipment?.id;
-  const { data: weightTickets, ...weightTicketsQuery } = useQuery([WEIGHT_TICKETS, ppmShipmentId], getWeightTickets, {
-    enabled: !!ppmShipmentId,
-  });
 
-  const { isLoading, isError, isSuccess } = getQueriesStatus([mtoShipmentQuery, weightTicketsQuery]);
-  return {
-    mtoShipment,
-    weightTickets,
-    isLoading,
-    isError,
-    isSuccess,
-  };
-};
-
-export const useQueries = (shipmentID) => {
-  const { data: mtoShipment, ...mtoShipmentQuery } = useQuery([MTO_SHIPMENT, shipmentID], getMTOShipmentByID);
-
-  const ppmShipmentId = mtoShipment?.ppmShipment?.id;
   const { data: weightTickets, ...weightTicketsQuery } = useQuery([WEIGHT_TICKETS, ppmShipmentId], getWeightTickets, {
     enabled: !!ppmShipmentId,
   });
