c593ada6f855700b020730e95bdbd3d587c532dc
Andrew Hobson <ahobson@truss.works>
2023-01-23 14:13:59 +0000

    Some playwright refactoring

--- playwright/tests/utils/baseTest.js
+++ playwright/tests/utils/baseTest.js
@@ -24,28 +24,19 @@ export class BaseTestPage {
   }
 
   /**
-   * @param {string} inputData
-   * @param {string} fieldName
-   * @param {string} classSelector
-   * @param {number} nth
-   */
-  async genericSelect(inputData, fieldName, classSelector, nth) {
-    // fieldName is passed as a classname to the react-select component,
-    // so select for it if provided
-    const actualClassSelector = fieldName ? `${classSelector}.${fieldName}` : classSelector;
-    await this.page.locator(`${actualClassSelector} input[type="text"]`).type(inputData);
-
-    // Click on the first presented option
-    await this.page.locator(classSelector).locator('div[class*="option"]').nth(nth).click();
-  }
-
-  /**
    * @param {string} dutyLocationName
    * @param {string} fieldName
    * @param {number} nth
    */
   async selectDutyLocation(dutyLocationName, fieldName, nth = 0) {
-    return this.genericSelect(dutyLocationName, fieldName, '.duty-input-box', nth);
+    // fieldName is passed as a classname to the react-select component,
+    // so select for it if provided
+    const classSelector = '.duty-input-box';
+    const actualClassSelector = fieldName ? `${classSelector}.${fieldName}` : classSelector;
+    await this.page.locator(`${actualClassSelector} input[type="text"]`).type(dutyLocationName);
+
+    // Click on the first presented option
+    await this.page.locator(classSelector).locator('div[class*="option"]').nth(nth).click();
   }
 
   /**
