6079a1fcc480fa681d9137fd55573d64fe990811
PDickeyTruss <pdickey@truss.works>
2023-02-02 17:22:35 +0000

    Fix for admin pagination per upgrade change

--- src/scenes/SystemAdmin/shared/AdminPagination.jsx
+++ src/scenes/SystemAdmin/shared/AdminPagination.jsx
@@ -4,13 +4,13 @@ import styles from './AdminPagination.module.scss';
 
 const AdminPagination = () => {
   const { isLoading, total } = useListContext();
-  !isLoading && total === 0 ? (
+  return !isLoading && total === 0 ? (
     <div className={styles['no-results']}>
       There are no results for this access code. Please check your entry to make sure you entered the correct letter
       combination.
     </div>
   ) : (
-    <Pagination rowsPerPageOptions={[]} {...isLoading} {...total} />
+    <Pagination rowsPerPageOptions={[]} isLoading={isLoading} total={total} />
   );
 };
 export default AdminPagination;
--- src/scenes/SystemAdmin/shared/AdminPagination.test.js
+++ src/scenes/SystemAdmin/shared/AdminPagination.test.js
@@ -3,30 +3,36 @@ import { shallow } from 'enzyme';
 
 import AdminPagination from './AdminPagination';
 
+import { useListContext } from 'react-admin';
+
+jest.mock('react-admin', () => ({
+  ...jest.requireActual('react-admin'),
+  useListContext: jest.fn(),
+}));
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 describe('AdminPagination', () => {
   it('Returns "no results" text when the filter has no matches', () => {
-    const props = {
+    useListContext.mockImplementation(() => ({
       total: 0,
       isLoading: false,
-      count: 0,
-      rowsPerPage: 25,
-    };
-    const pagination = shallow(<AdminPagination {...props} />);
+    }));
+
+    const pagination = shallow(<AdminPagination />);
+
     const noResultsDiv = pagination.find('.no-results');
     expect(noResultsDiv).toHaveLength(1);
-    pagination.setProps({
-      total: 1,
-    });
-    expect(pagination.exists('.no-results')).toEqual(false);
   });
+
   it('Does not return "no results" text when the component is still loading', () => {
-    const props = {
+    useListContext.mockImplementation(() => ({
       total: 0,
       isLoading: true,
-      count: 0,
-      rowsPerPage: 25,
-    };
-    const pagination = shallow(<AdminPagination {...props} />);
+    }));
+    const pagination = shallow(<AdminPagination />);
     // const noResultsDiv = pagination.find('.no-results');
     expect(pagination.find('.no-results')).toHaveLength(0);
   });
