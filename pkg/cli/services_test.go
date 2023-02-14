c0edfb7dfec2c14d9205468e1f6ff1a873ef5816
Andrew Hobson <ahobson@truss.works>
2023-02-14 17:19:22 +0000

    Switch services_test to use locally provided config

--- pkg/cli/services_test.go
+++ pkg/cli/services_test.go
@@ -1,6 +1,9 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigServices() {
+	// create fake data for the eia test
+	// use https://pkg.go.dev/testing#B.Setenv for cleanup
+	suite.T().Setenv("SERVE_API_INTERNAL", "true")
 	suite.Setup(InitServiceFlags, []string{})
 	suite.NoError(CheckServices(suite.viper))
 }
