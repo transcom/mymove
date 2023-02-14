dfe69928f04fda2c3d56008258482fffd2b7622d
Andrew Hobson <ahobson@truss.works>
2023-02-14 14:08:51 +0000

    Switch csrf_test to use provided env values

--- pkg/cli/csrf_test.go
+++ pkg/cli/csrf_test.go
@@ -1,6 +1,8 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigCSRF() {
+	// dummy generated with openssl rand -hex 32
+	suite.T().Setenv("CSRF_AUTH_KEY", "84ba3ca1ade09785ff4a8f3abf12fbd841a504262baff9fe6d86ab1bdede86d9")
 	suite.Setup(InitCSRFFlags, []string{})
 	suite.NoError(CheckCSRF(suite.viper))
 }
