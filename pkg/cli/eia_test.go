72953dda52cd9ecce569d44fc80385abdb75c2cd
Andrew Hobson <ahobson@truss.works>
2023-02-14 17:18:05 +0000

    Switch eia_test to use locally provided config

--- pkg/cli/eia_test.go
+++ pkg/cli/eia_test.go
@@ -1,6 +1,11 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigEIA() {
+	// create fake data for the eia test
+	// use https://pkg.go.dev/testing#B.Setenv for cleanup
+	// dummy generated with openssl rand -hex 16
+	suite.T().Setenv("EIA_KEY",
+		"2bc8b0f2d13dd4f02f654650b6782ef0")
 	suite.Setup(InitEIAFlags, []string{})
 	suite.NoError(CheckEIA(suite.viper))
 }
