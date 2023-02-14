6da1ad4fd35eb33dce1e45a5b01d6a81f3353692
Andrew Hobson <ahobson@truss.works>
2023-02-14 16:43:37 +0000

    Switch iws_test to use provided env values

--- pkg/cli/iws_test.go
+++ pkg/cli/iws_test.go
@@ -1,6 +1,9 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigIWS() {
+	// create fake data for the iws test
+	// use https://pkg.go.dev/testing#B.Setenv for cleanup
+	suite.T().Setenv("IWS_RBS_HOST", "iws.rbs.example.com")
 	suite.Setup(InitIWSFlags, []string{})
 	suite.NoError(CheckIWS(suite.viper))
 }
