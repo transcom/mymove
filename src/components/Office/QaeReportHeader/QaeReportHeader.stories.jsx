c9af51a856ae981dbfb820b43092784aee81c3a8
PDickeyTruss <pdickey@truss.works>
2023-02-16 20:15:15 +0000

    Fix all office storybook stories that used routing

--- src/components/Office/QaeReportHeader/QaeReportHeader.stories.jsx
+++ src/components/Office/QaeReportHeader/QaeReportHeader.stories.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { MemoryRouter, Route } from 'react-router';
 
 import QaeReportHeader from './QaeReportHeader';
 
 import EVALUATION_REPORT_TYPE from 'constants/evaluationReports';
+import { MockRouting } from 'testUtils';
+import { qaeCSRRoutes } from 'constants/routes';
 
 export default {
   title: 'Office Components/QaeReportHeader',
   component: QaeReportHeader,
   decorators: [
     (Story) => (
-      <MemoryRouter initialEntries={['/moves/AMDORD/evaluation-reports/ab30c135-1d6d-4a0d-a6d5-f408474f6ee2']}>
-        <Route path="/moves/:moveCode/evaluation-reports/:reportId">
-          <div style={{ padding: '40px', width: '900px', minWidth: '900px' }}>
-            <Story />
-          </div>
-        </Route>
-      </MemoryRouter>
+      <MockRouting
+        path={qaeCSRRoutes.BASE_EVALUATION_REPORT_PATH}
+        params={{ moveCode: 'AMDORD', reportId: 'ab30c135-1d6d-4a0d-a6d5-f408474f6ee2' }}
+      >
+        <div style={{ padding: '40px', width: '900px', minWidth: '900px' }}>
+          <Story />
+        </div>
+      </MockRouting>
     ),
   ],
 };
