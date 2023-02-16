58864f3420dd5512cba593ea67ab5085b1b66094
Ronak Patel <ronak@truss.works>
2023-02-16 17:17:13 +0000

    Update filtering functionality

--- pkg/services/order/order_fetcher.go
+++ pkg/services/order/order_fetcher.go
@@ -371,7 +371,7 @@ func submittedAtFilter(submittedAt *time.Time) QueryOption {
 func requestedMoveDateFilter(requestedMoveDate *string) QueryOption {
 	return func(query *pop.Query) {
 		if requestedMoveDate != nil {
-			query.Where("mto_shipments.requested_pickup_date = ?", *requestedMoveDate)
+			query.Where("(mto_shipments.requested_pickup_date = ? OR ppm_shipments.expected_departure_date = ?)", *requestedMoveDate, *requestedMoveDate)
 		}
 	}
 }
