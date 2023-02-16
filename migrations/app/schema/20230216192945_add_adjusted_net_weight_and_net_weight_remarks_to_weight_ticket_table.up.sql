5fe1bf3afa4694ab093c37350a648e5330d0b916
ruizajtruss <ruizaj@truss.works>
2023-02-16 19:31:03 +0000

    renamed migration

--- migrations/app/schema/20230216192945_add_adjusted_net_weight_and_net_weight_remarks_to_weight_ticket_table.up.sql
+++ migrations/app/schema/20230216192945_add_adjusted_net_weight_and_net_weight_remarks_to_weight_ticket_table.up.sql
@@ -3,6 +3,6 @@ ALTER TABLE weight_tickets
 ADD COLUMN adjusted_net_weight int,
 ADD COLUMN net_weight_remarks varchar;
 
---Column Comments
+-- Column Comments
 COMMENT ON COLUMN weight_tickets.adjusted_net_weight IS 'Stores the net weight of the vehicle';
 COMMENT ON COLUMN weight_tickets.net_weight_remarks IS 'Stores remarks explaining any edits made to the net weight';
