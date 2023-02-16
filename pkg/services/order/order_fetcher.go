9b8f6183df098b9072460131337f1b468c14b8be
Ronak Patel <ronak@truss.works>
2023-02-16 20:10:31 +0000

    Update casing for sql statement

--- pkg/services/order/order_fetcher.go
+++ pkg/services/order/order_fetcher.go
@@ -457,7 +457,7 @@ func sortOrder(sort *string, order *string, ppmCloseoutGblocs bool) QueryOption 
 		"submittedAt":             "moves.submitted_at",
 		"originDutyLocation":      "origin_dl.name",
 		"destinationDutyLocation": "dest_dl.name",
-		"requestedMoveDate":       "case when coalesce(min(mto_shipments.requested_pickup_date), 'infinity') <= coalesce(min(ppm_shipments.expected_departure_date), 'infinity') THEN min(mto_shipments.requested_pickup_date) ELSE min(ppm_shipments.expected_departure_date) end",
+		"requestedMoveDate":       "CASE WHEN COALESCE(MIN(mto_shipments.requested_pickup_date), 'infinity') <= COALESCE(MIN(ppm_shipments.expected_departure_date), 'infinity') THEN MIN(mto_shipments.requested_pickup_date) ELSE MIN(ppm_shipments.expected_departure_date) END",
 		"originGBLOC":             "origin_to.gbloc",
 		"ppmType":                 "moves.ppm_type",
 		"closeoutLocation":        "closeout_to.name",
