c96bc2142ed21771f4571556269780ba75731ee0
Michael Valdes <michael.valdes@truss.works>
2023-02-24 21:03:15 +0000

    Add weight displays to review shipment weights

--- src/pages/Office/ServicesCounselingReviewShipmentWeights/ServicesCounselingReviewShipmentWeights.jsx
+++ src/pages/Office/ServicesCounselingReviewShipmentWeights/ServicesCounselingReviewShipmentWeights.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import { useReviewShipmentWeightsQuery } from 'hooks/queries';
+import WeightDisplay from 'components/Office/WeightDisplay/WeightDisplay';
+import { useCalculatedEstimatedWeight, useCalculatedWeightRequested } from 'hooks/custom';
 
 const ServicesCounselingReviewShipmentWeights = ({ moveCode }) => {
-  useReviewShipmentWeightsQuery(moveCode);
-  return <h1>Review shipment weights</h1>;
+  const { orders, mtoShipments } = useReviewShipmentWeightsQuery(moveCode);
+  const estimatedWeightTotal = useCalculatedEstimatedWeight(mtoShipments);
+  const moveWeightTotal = useCalculatedWeightRequested(mtoShipments);
+  const order = Object.values(orders)?.[0];
+
+  return (
+    <GridContainer>
+      <Grid row>
+        <h1>Review shipment weights</h1>
+      </Grid>
+      <Grid row>
+        <WeightDisplay heading="Weight allowance" weightValue={order.entitlement.totalWeight} />
+        <WeightDisplay heading="Estimated weight (total)" weightValue={estimatedWeightTotal} />
+        <WeightDisplay heading="Max billable weight" weightValue={order.entitlement.authorizedWeight} />
+        <WeightDisplay heading="Move weight (total)" weightValue={moveWeightTotal} />
+      </Grid>
+    </GridContainer>
+  );
 };
 
 export default ServicesCounselingReviewShipmentWeights;
