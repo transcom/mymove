488ff4e07f00b7076e39e3173a1de8a3495a1738
Michael Valdes <michael.valdes@truss.works>
2023-02-24 23:27:24 +0000

    Add tab content styles

--- src/pages/Office/ServicesCounselingReviewShipmentWeights/ServicesCounselingReviewShipmentWeights.jsx
+++ src/pages/Office/ServicesCounselingReviewShipmentWeights/ServicesCounselingReviewShipmentWeights.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, GridContainer, Tag } from '@trussworks/react-uswds';
 
+import styles from '../TXOMoveInfo/TXOTab.module.scss';
+
 import { useReviewShipmentWeightsQuery } from 'hooks/queries';
 import WeightDisplay from 'components/Office/WeightDisplay/WeightDisplay';
 import { useCalculatedEstimatedWeight, useCalculatedWeightRequested } from 'hooks/custom';
@@ -13,19 +15,21 @@ const ServicesCounselingReviewShipmentWeights = ({ moveCode }) => {
   const order = Object.values(orders)?.[0];
 
   return (
-    <GridContainer>
-      <Grid row>
-        <h1>Review shipment weights</h1>
-      </Grid>
-      <Grid row>
-        <WeightDisplay heading="Weight allowance" weightValue={order.entitlement.totalWeight} />
-        <WeightDisplay heading="Estimated weight (total)" weightValue={estimatedWeightTotal} />
-        <WeightDisplay heading="Max billable weight" weightValue={order.entitlement.authorizedWeight}>
-          {hasRiskOfExcess(estimatedWeightTotal, order.entitlement.totalWeight) && <Tag>Risk of excess</Tag>}
-        </WeightDisplay>
-        <WeightDisplay heading="Move weight (total)" weightValue={moveWeightTotal} />
-      </Grid>
-    </GridContainer>
+    <div className={styles.tabContent}>
+      <GridContainer>
+        <Grid row>
+          <h1>Review shipment weights</h1>
+        </Grid>
+        <Grid row>
+          <WeightDisplay heading="Weight allowance" weightValue={order.entitlement.totalWeight} />
+          <WeightDisplay heading="Estimated weight (total)" weightValue={estimatedWeightTotal} />
+          <WeightDisplay heading="Max billable weight" weightValue={order.entitlement.authorizedWeight}>
+            {hasRiskOfExcess(estimatedWeightTotal, order.entitlement.totalWeight) && <Tag>Risk of excess</Tag>}
+          </WeightDisplay>
+          <WeightDisplay heading="Move weight (total)" weightValue={moveWeightTotal} />
+        </Grid>
+      </GridContainer>
+    </div>
   );
 };
 
