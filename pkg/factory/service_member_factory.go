2b026459092d3ce8a198b770368fc6f373e5756f
StuTruss <stuartb@truss.works>
2023-03-01 23:03:20 +0000

    add BuildServiceMember function + tests

---
+++ pkg/factory/service_member_factory.go
@@ -0,0 +1,60 @@
+package factory
+
+import (
+	"github.com/gobuffalo/pop/v6"
+
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/testdatagen"
+)
+
+// BuildServiceMember creates a single ServiceMember
+// Also creates, if not provided:
+// - Residential Address of the ServiceMember
+// - User
+func BuildServiceMember(db *pop.Connection, customs []Customization, traits []Trait) models.ServiceMember {
+	customs = setupCustomizations(customs, traits)
+
+	// Find ServiceMember customization and extract the custom ServiceMember
+	var cServiceMember models.ServiceMember
+	if result := findValidCustomization(customs, ServiceMember); result != nil {
+		cServiceMember = result.Model.(models.ServiceMember)
+		if result.LinkOnly {
+			return cServiceMember
+		}
+	}
+
+	// Find/create the user model
+	user := BuildUser(db, customs, traits)
+
+	// Find/create the address model
+	currentAddress := BuildAddress(db, customs, traits)
+
+	email := "leo_spaceman_sm@example.com"
+	agency := models.AffiliationARMY
+	rank := models.ServiceMemberRankE1
+
+	randomEdipi := RandomEdipi()
+
+	serviceMember := models.ServiceMember{
+		UserID:               user.ID,
+		User:                 user,
+		Edipi:                models.StringPointer(randomEdipi),
+		Affiliation:          &agency,
+		FirstName:            models.StringPointer("Leo"),
+		LastName:             models.StringPointer("Spacemen"),
+		Telephone:            models.StringPointer("212-123-4567"),
+		PersonalEmail:        &email,
+		ResidentialAddressID: &currentAddress.ID,
+		ResidentialAddress:   &currentAddress,
+		Rank:                 &rank,
+	}
+
+	// Overwrite values with those from customizations
+	testdatagen.MergeModels(&serviceMember, cServiceMember)
+
+	if db != nil {
+		mustCreate(db, &serviceMember)
+	}
+
+	return serviceMember
+}
