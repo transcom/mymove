a39e409421c92303513ace0702e942010ce16b1f
Andrew Hobson <ahobson@truss.works>
2023-03-10 21:25:58 +0000

    Major refactor of server test perTestTransaction logic
    * Refactor to put almost all the perTestTransaction logic in
      pop_suite_txn.go
    * No need for a custom store. Once we register go-txdb, we can let the
      usual pop process connect using the custom driver
    * Use a manual savepoint for preloaded data instead of trying to use
      sqlx.Beginx. We can do that because we know all connections will be
      inside a transaction thanks to go-txdb.
    * Ensure that if a test process is reused for multiple packages, it
      uses the same database since go-txdb registers a db URL once

--- pkg/testingsuite/pop_suite_txn.go
+++ pkg/testingsuite/pop_suite_txn.go
@@ -1,17 +1,20 @@
 package testingsuite
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"time"
 
 	"github.com/DATA-DOG/go-txdb"
 	"github.com/gobuffalo/pop/v6"
-	"github.com/jmoiron/sqlx"
 )
+
+// per process tracking of which dbNum is used
+// see findOrCreatePerTestTransactionDb
+var lockedDbNum int
 
 // use a driver that sqlx knows uses a dollar binding like postgres
 // https://github.com/jmoiron/sqlx/blob/master/bind.go
@@ -43,21 +46,26 @@ func (suite *PopTestSuite) PreloadData(f func()) {
 	}
 	// establish a database connection for the suite that will
 	// rollback when closed
-	suite.preloadedTxnDb = suite.openTxnDb("preload_data")
+	suite.preloadedTxnDb = suite.openTxnPopConnection("preload_data")
 	// set lowPrivConn so suite.DB() will return the pop connection
-	suite.lowPrivConn = suite.txnPopConnection(suite.preloadedTxnDb)
+	// inside the PreloadData `f`
+	suite.lowPrivConn = suite.preloadedTxnDb
 	defer func() {
 		// ensure we unset the connection so that after preloading
-		// data a new pop connection will be established
+		// data a new savepoint can be establised
 		suite.lowPrivConn = nil
 	}()
 	suite.T().Cleanup(func() {
+		// the test context is done, so we can close this db
+		// connection (which will rollback the go-txdb transaction)
 		err := suite.preloadedTxnDb.Close()
 		if err != nil {
-			log.Panic(err)
+			log.Fatalf("Preload db close failed: %v", err)
 		}
 		suite.preloadedTxnDb = nil
 	})
+
+	// call the user provided preload function
 	f()
 }
 
@@ -69,25 +77,33 @@ func (suite *PopTestSuite) perTestTransactionDB() *pop.Connection {
 	// create a new txdb db connection per test
 	suite.txnMutex.Lock()
 	defer suite.txnMutex.Unlock()
+	testingName := suite.T().Name()
 	// set up the pop connection for PreloadData that will
 	// rollback at the end of the test
 	if suite.preloadedTxnDb != nil {
-		// At this point, PreloadData has been called, so we want to
-		// start a savepoint. This is what go-txdb does for us. See
-		// https://github.com/DATA-DOG/go-txdb/blob/master/db.go#L195
 		if suite.lowPrivConn == nil {
-			tx, err := suite.preloadedTxnDb.Beginx()
+			// At this point, PreloadData has been called, so we want to
+			// start a savepoint if this is a new connection. Pop does
+			// not support nested transaction transparently, but we
+			// know we are in a transaction because we are using
+			// go-txdb, so we can manually create and rollback a
+			// savepoint (aka a postgres nested transaction)
+			err := suite.preloadedTxnDb.RawQuery("SAVEPOINT preload_data").Exec()
 			if err != nil {
-				log.Panic(err)
+				log.Fatalf("Cannot start preload savepoint `preload_data`: %v", err)
 			}
-			suite.lowPrivConn = suite.txnPopConnection(suite.preloadedTxnDb)
+
+			// set lowPrivConn so that suite.DB() will use it
+			suite.lowPrivConn = suite.preloadedTxnDb
+
 			// ensure the savepoint is rolled back when this test
 			// context (suite.T()) is finished
 			suite.T().Cleanup(func() {
+				// reset lowPrivConn now that this test is done
 				suite.lowPrivConn = nil
-				err = tx.Rollback()
+				err := suite.preloadedTxnDb.RawQuery("ROLLBACK TO SAVEPOINT preload_data").Exec()
 				if err != nil {
-					log.Panic(err)
+					log.Fatalf("Preload savepoint `preload_data` rollback failed: %v", err)
 				}
 			})
 		}
@@ -95,17 +111,15 @@ func (suite *PopTestSuite) perTestTransactionDB() *pop.Connection {
 	}
 
 	// At this point, we know this is a transactional test that is not
-	// using PreloadData, so we can establish a new connection using
-	// go-txdb.
-	testingName := suite.T().Name()
+	// using PreloadData, so we can establish an entirely new
+	// connection
 	popConn, ok := suite.txnTestDb[testingName]
 	if ok {
 		return popConn
 	}
-	// create a brand new database for this test so it is
-	// completely isolated
-	db := suite.openTxnDb(testingName)
-	popConn = suite.txnPopConnection(db)
+
+	// openTxnPopConnection will wind up opening a go-txdb connection
+	popConn = suite.openTxnPopConnection(testingName)
 	suite.txnTestDb[testingName] = popConn
 
 	// To prevent accidental dependencies between tests, check and
@@ -146,7 +160,10 @@ func (suite *PopTestSuite) perTestTransactionDB() *pop.Connection {
 		log.Panic("Multiple test databases active simultaneously, use PreloadData: " + names)
 	}
 	suite.T().Cleanup(func() {
+		// when this test context finishes, remove it from tracking
 		delete(suite.txnTestDb, testingName)
+		// ensure we close the db connection (which will roll back the
+		// go-txdb transaction)
 		err := popConn.Close()
 		if err != nil {
 			log.Panic(err)
@@ -155,31 +172,36 @@ func (suite *PopTestSuite) perTestTransactionDB() *pop.Connection {
 	return popConn
 }
 
-// txnPopConnection wraps the sqlx.DB in a *pop.Connection
-func (suite *PopTestSuite) txnPopConnection(db *sqlx.DB) *pop.Connection {
-	popConn, err := pop.NewConnection(suite.lowPrivConnDetails)
+// openTxnPopConnection opens a pop connection for the provided test name
+func (suite *PopTestSuite) openTxnPopConnection(testNameOverride string) *pop.Connection {
+	packageName := suite.PackageName.String()
+	testName := testNameOverride
+	if testName == "" {
+		testName = suite.T().Name()
+	}
+	// when connecting using txdb, the URL needs to look like
+	// something for pop, but does not have to point to a real db
+	dbSanitizer := regexp.MustCompile("[^a-zA-Z0-9_]")
+	txnFakeURL := "postgres:///" + dbSanitizer.ReplaceAllString(packageName+"_"+testName, "_")
+	txnPopDeets := pop.ConnectionDetails{
+		Driver:  fakePopSqlxDriverName,
+		Dialect: suite.lowPrivConnDetails.Dialect,
+		URL:     txnFakeURL,
+	}
+	popConn, err := pop.NewConnection(&txnPopDeets)
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("Error creating new pop connection: %v", err)
 	}
-	suiteStore := &PopSuiteTxnStore{
-		db,
-		popConn,
+	err = popConn.Open()
+	if err != nil {
+		log.Fatalf("Error opening new pop connection: %v", err)
 	}
-	popConn.Store = suiteStore
 	return popConn
 }
 
-// findOrCreatePerTestTransactionDb tries to reuse a pool of databases so
-// a clone doesn't have to be created, which greatly speeds up the
-// tests. Because it is used for tests that rollback a transaction at
-// the end of the test, the db can be reused.
-func (suite *PopTestSuite) findOrCreatePerTestTransactionDb() {
-	packageName := suite.PackageName.String()
-	suite.pgConnDetails.Options["application_name"] = packageName
-	suite.lowPrivConnDetails.Options["application_name"] = packageName
+func (suite *PopTestSuite) tryAdvisoryLock(dbNum int) bool {
 	// lockStart is an arbitrary number, it could be anything
-	lockStart := 10000
-	dbNum := 1
+	const lockStart = 10000
 
 	// Use an advisory lock to hold a database until the
 	// connection is closed at the end of the package test suite
@@ -187,23 +209,72 @@ func (suite *PopTestSuite) findOrCreatePerTestTransactionDb() {
 	//
 	// See https://www.postgresql.org/docs/current/functions-admin.html#FUNCTIONS-ADVISORY-LOCKS
 	var lock bool
-	for {
-		lockQuery := fmt.Sprintf("SELECT pg_try_advisory_lock(%d)", lockStart+dbNum)
-		err := suite.pgConn.RawQuery(lockQuery).First(&lock)
-		if err != nil {
-			log.Panic(err)
+	lockQuery := fmt.Sprintf("SELECT pg_try_advisory_lock(%d)", lockStart+dbNum)
+	err := suite.pgConn.RawQuery(lockQuery).First(&lock)
+	if err != nil {
+		log.Panicf("Error trying to get advisory lock: %v", err)
+	}
+	return lock
+}
+
+// findOrCreatePerTestTransactionDb tries to reuse a pool of databases so
+// a clone doesn't have to be created, which greatly speeds up the
+// tests. Because it is used for tests that rollback a transaction at
+// the end of the test, the db can be reused.
+func (suite *PopTestSuite) findOrCreatePerTestTransactionDb() {
+	pid := os.Getpid()
+	packageName := suite.PackageName.String()
+	suite.pgConnDetails.Options["application_name"] = packageName
+	suite.lowPrivConnDetails.Options["application_name"] = packageName
+	suite.lowPrivConnDetails.Driver = fakePopSqlxDriverName
+	// when doing per test transaction, high priv conn should never be used
+	suite.highPrivConnDetails.Database = "UNUSED"
+	if containsString(sql.Drivers(), fakePopSqlxDriverName) {
+		// This is only called from NewPopTestSuite, which could be
+		// be called multiple times per process if a package has
+		// multiple test suites in it.
+		//
+		// Go runs each package's test in its own process
+		//
+		// We have already registered the fake txdb driver in this
+		// process, so we have already been here before. Once the the
+		// fake txdb driver has been registered for a db url, we
+		// cannot change it, so we need to wait for that db to become
+		// available again
+
+		// use a non blocking advisory lock so we don't get into a
+		// database deadlock situation
+		for {
+			if suite.tryAdvisoryLock(lockedDbNum) {
+				break
+			}
+			// if we didn't get the lock, sleep until we do
+			log.Printf("TXNDB: Waiting for locked db %s in pid %d",
+				suite.lowPrivConnDetails.Database, pid)
+			time.Sleep(time.Second)
 		}
-		if lock {
+
+		testDbName := fmt.Sprintf("%s_%d", suite.dbNameTemplate, lockedDbNum)
+		suite.lowPrivConnDetails.Database = testDbName
+		log.Printf("TXNDB: package %s will RE-use database %s in pid %d", packageName,
+			suite.lowPrivConnDetails.Database, pid)
+		return
+	}
+
+	// we are not reusing a database, so get a lock on a new database
+	dbNum := 1
+
+	for {
+		if suite.tryAdvisoryLock(dbNum) {
 			break
 		}
 		dbNum++
 	}
+
+	lockedDbNum = dbNum
 	// now we have a lock on dbNum until the pgConn closes
-	templateDbName := suite.dbNameTemplate
 	// the test databases used here look like test_db_1, test_db_2, etc
-	testDbName := fmt.Sprintf("%s_%d", templateDbName, dbNum)
-	// when doing per test transaction, high priv conn should never be used
-	suite.highPrivConnDetails.Database = "UNUSED"
+	testDbName := fmt.Sprintf("%s_%d", suite.dbNameTemplate, lockedDbNum)
 	suite.lowPrivConnDetails.Database = testDbName
 
 	// Try to figure out if we need to recreate the test db from the
@@ -212,7 +283,7 @@ func (suite *PopTestSuite) findOrCreatePerTestTransactionDb() {
 	// can reuse
 	mtimeQuery := "SELECT (pg_stat_file('base/'||oid ||'/PG_VERSION')).modification FROM pg_database WHERE datname = ?"
 	var templateMtime time.Time
-	err := suite.pgConn.RawQuery(mtimeQuery, templateDbName).First(&templateMtime)
+	err := suite.pgConn.RawQuery(mtimeQuery, suite.dbNameTemplate).First(&templateMtime)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -228,40 +299,29 @@ func (suite *PopTestSuite) findOrCreatePerTestTransactionDb() {
 		if err != nil {
 			log.Panic(err)
 		}
-		err = cloneDatabase(suite.pgConn, templateDbName, testDbName)
+		err = cloneDatabase(suite.pgConn, suite.dbNameTemplate, testDbName)
 		if err != nil {
 			log.Panic(err)
 		}
 	}
-}
 
-// openTxnDb sets up the sqlx.DB for this test that will rollback
-// all changes when the db is closed
-func (suite *PopTestSuite) openTxnDb(name string) *sqlx.DB {
-	packageName := suite.PackageName.String()
-
+	// See https://github.com/DATA-DOG/go-txdb for more information
+	// about how txdb works and why we need to register a fake driver
+	// and then connect to a fake database name
 	s := "postgres://%s:%s@%s:%s/%s?%s"
-	dataSourceName := fmt.Sprintf(s, suite.lowPrivConnDetails.User,
+	dataSourceName := fmt.Sprintf(s,
+		suite.lowPrivConnDetails.User,
 		suite.lowPrivConnDetails.Password,
 		suite.lowPrivConnDetails.Host,
 		suite.lowPrivConnDetails.Port,
 		suite.lowPrivConnDetails.Database,
 		suite.lowPrivConnDetails.OptionsString(""))
 
-	// See https://github.com/DATA-DOG/go-txdb for more information
-	// about how txdb works and why we need to register a fake driver
-	// and then connect to a fake database name
-	if !containsString(sql.Drivers(), fakePopSqlxDriverName) {
-		txdb.Register(fakePopSqlxDriverName, "postgres", dataSourceName)
-	}
-
-	dbSanitizer := regexp.MustCompile("[^a-zA-Z0-9_]")
-	fakeDbName := dbSanitizer.ReplaceAllString(packageName+"_"+name, "_")
-
-	suite.lowPrivConnDetails.Driver = fakePopSqlxDriverName
-	suite.lowPrivConnDetails.Options["application_name"] = fakeDbName
-	suite.lowPrivConnDetails.Database = fakeDbName
-	return sqlx.MustOpen(fakePopSqlxDriverName, fakeDbName)
+	log.Printf("TXNDB: package %s will use database %s in pid %d", packageName,
+		suite.lowPrivConnDetails.Database, pid)
+	// Register will panic if the same driver is registered more than
+	// once in the same process, so we've checked for that up above
+	txdb.Register(fakePopSqlxDriverName, "postgres", dataSourceName)
 }
 
 // tearDownTxnTest closes the db connection established for this test
@@ -269,65 +329,4 @@ func (suite *PopTestSuite) tearDownTxnTest() {
 	if !suite.usePerTestTransaction {
 		return
 	}
-}
-
-// PopSuiteTxnStore is a pop.Store that uses go-txdb to wrap the
-// connection in a transaction that will be rolled back when closed
-type PopSuiteTxnStore struct {
-	*sqlx.DB
-	popConn *pop.Connection
-}
-
-// Commit the transaction if it exists
-// Needed to implement the pop.store interface, but won't actually be called
-func (store *PopSuiteTxnStore) Commit() error {
-	return nil
-}
-
-// Rollback the transaction if it exists
-// Needed to implement the pop.store interface, but won't actually be called
-func (store *PopSuiteTxnStore) Rollback() error {
-	return nil
-}
-
-// Transaction starts a pop.Transaction
-func (store *PopSuiteTxnStore) Transaction() (*pop.Tx, error) {
-	return store.TransactionContext(context.Background())
-}
-
-// Close closes any open transactions and db connections
-func (store *PopSuiteTxnStore) Close() error {
-	if store.popConn != nil {
-		err := store.DB.Close()
-		if err != nil {
-			log.Fatalf("DB close failed: %v", err)
-		}
-		store.DB = nil
-		store.popConn = nil
-	}
-	return nil
-}
-
-// TransactionContext returns the current pop.Transaction
-func (store *PopSuiteTxnStore) TransactionContext(ctx context.Context) (*pop.Tx, error) {
-	return store.TransactionContextOptions(ctx, nil)
-}
-
-// TransactionContextOptions returns the current pop.Transaction
-func (store *PopSuiteTxnStore) TransactionContextOptions(ctx context.Context, opts *sql.TxOptions) (*pop.Tx, error) {
-	tx, err := store.DB.BeginTxx(ctx, opts)
-	if err != nil {
-		return nil, fmt.Errorf("could not create new transaction %w", err)
-	}
-	t := &pop.Tx{
-		ID: seededRand.Int(),
-		Tx: tx,
-	}
-	// Fake out POP!
-	// Because we are using go-txdb to manage the transactions, we can
-	// handle nested transactions. Setting TX on the pop connection
-	// means the connection can only have a single TX, which breaks
-	// some tests
-	store.popConn.TX = nil
-	return t, nil
 }
