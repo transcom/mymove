f2939784eec1f0f020cc76e6805b723ec85ca6de
PDickeyTruss <pdickey@truss.works>
2023-03-17 13:31:09 -0800

    Cleanup test util functions/params and documentation

--- src/testUtils.jsx
+++ src/testUtils.jsx
@@ -8,8 +8,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { configureStore } from 'shared/store';
 import PermissionProvider from 'components/Restricted/PermissionProvider';
 
-// Helper function to create a react-router memory router with the provided options
-const createMockRouter = ({ path, params, routes, initialEntries, children }) => {
+/** Helper function to create a react-router `MemoryRouter` with the provided options */
+const createMockRouter = ({ path, params, routes, children }) => {
+  // Add the current path (or /) to the routes array
   const mockRoutes = [
     {
       path: path || '/',
@@ -17,72 +18,33 @@ const createMockRouter = ({ path, params, routes, initialEntries, children }) =>
     },
   ];
 
+  // include any additional routes provided
   if (routes && routes.length > 0) mockRoutes.push(...routes);
 
   const router = createMemoryRouter(mockRoutes, {
-    initialEntries: [initialEntries || generatePath(path, params)] || ['/'],
+    initialEntries: [generatePath(path, params)] || ['/'],
   });
 
   return router;
 };
 
+/** Wrap the provided children with a react query client provider
+ * @param {object} client - A react query client to use. If not provided, a new client will be created.
+ * @returns {object} - The react query client provider
+ * */
 export const ReactQueryWrapper = ({ children, client }) => {
   const queryClient = client || new QueryClient();
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
 
-/**
- * Render the `ui` with mock routing in place, setup using the `options`. Most common options are `path` and `params`.
- * Enables rendered components to use routing hooks like `useParams` and `useLocation`.
- *
- * @param {*} ui - The component to be rendered using react-testing-library
- * @param {*} options - Routing options used to create a mock router. Prefered options are `path` and `params`. Other options are for extended use cases and supporting existiong patterns.
- * @returns {*} - The result of the render call from react-testing-library
- */
-export const renderWithRouter = (ui, options) => {
-  return render(<MockRouting {...options}>{ui}</MockRouting>);
-};
-
-export const renderWithProviders = (ui, options) => {
-  return render(
-    <ReactQueryWrapper>
-      <MockProviders {...options}>{ui}</MockProviders>
-    </ReactQueryWrapper>,
-  );
-};
-
-/**
- * Renders class components with a `routing` prop. For use with components that use the `withRouter` HOC.
- * Adds a mock router prop to the component using the provided path and params in options to mock behavior of `withRouter`.
- *
- * @param {*} ui - The component to be rendered using react-testing-library
- * @param {*} options - Routing options used to create a mock router. Prefered options are `path` and `params`. Other options are for extended use cases and supporting existiong patterns.
- * @returns {*} - The result of the render call from react-testing-library
- */
-export const renderWithRouterProp = (ui, options) => {
-  const path = options?.path || '/';
-  const params = options?.params || {};
-  const navigate = options?.navigate || jest.fn();
-  const search = options?.search || '';
-
-  const pathname = generatePath(path, params);
-  const router = { location: { pathname, search }, params, navigate };
-
-  const routingOptions = { ...options, path, params, navigate, search };
-  if (options?.includeProviders)
-    return renderWithProviders(React.cloneElement(ui, { router: { ...router } }), routingOptions);
-
-  return renderWithRouter(React.cloneElement(ui, { router: { ...router } }), routingOptions);
-};
-
 /** Wrap the provided children with a mock router using the provided options. */
-export const MockRouting = ({ children, path, params, initialEntries, routes }) => {
-  const mockRouter = createMockRouter({ path, params, routes, initialEntries, children });
+export const MockRouterProvider = ({ children, path, params, routes }) => {
+  const mockRouter = createMockRouter({ path, params, routes, children });
 
   return <RouterProvider router={mockRouter} />;
 };
 
-MockRouting.propTypes = {
+MockRouterProvider.propTypes = {
   children: node.isRequired,
   params: shape({}),
   path: string,
@@ -96,7 +58,7 @@ MockRouting.propTypes = {
   ),
 };
 
-MockRouting.defaultProps = {
+MockRouterProvider.defaultProps = {
   params: {},
   path: '/',
   routes: null,
@@ -105,16 +67,15 @@ MockRouting.defaultProps = {
 /** Wrap the three most common mock providers (permission, redux, and router) around the provided children */
 export const MockProviders = ({
   children,
-  initialState,
-  initialEntries,
-  path,
-  params,
-  routes,
-  permissions,
-  currentUserId,
-  client,
+  initialState, // redux
+  path, // routing
+  params, // routing
+  routes, // routing
+  permissions, // permissions
+  currentUserId, // permissions
+  client, // react query
 }) => {
-  const mockRouter = createMockRouter({ path, params, routes, initialEntries, children });
+  const mockRouter = createMockRouter({ path, params, routes, children });
   const mockStore = configureStore(initialState);
 
   return (
@@ -135,6 +96,14 @@ MockProviders.propTypes = {
   path: string,
   permissions: arrayOf(string),
   currentUserId: string,
+  routes: arrayOf(
+    shape({
+      path: string,
+      element: node,
+      children: arrayOf(shape({})),
+      caseSensitive: bool,
+    }),
+  ),
 };
 
 MockProviders.defaultProps = {
@@ -143,6 +112,47 @@ MockProviders.defaultProps = {
   path: '/',
   permissions: [],
   currentUserId: null,
+  routes: null,
+};
+
+/**
+ * Render the `ui` with mock routing in place, setup using the `options`. Most common options are `path` and `params`.
+ * Enables rendered components to use routing hooks like `useParams` and `useLocation`.
+ *
+ * @param {*} ui - The component to be rendered using react-testing-library
+ * @param {*} options - Routing options used to create a mock router. Prefered options are `path` and `params`. Other options are for extended use cases and supporting existiong patterns.
+ * @returns {*} - The result of the render call from react-testing-library
+ */
+export const renderWithRouter = (ui, options) => {
+  return render(<MockRouterProvider {...options}>{ui}</MockRouterProvider>);
+};
+
+export const renderWithProviders = (ui, options) => {
+  return render(<MockProviders {...options}>{ui}</MockProviders>);
+};
+
+/**
+ * Renders class components with a `router` prop. For use with components that use the `withRouter` HOC.
+ * Sets up mock routing and adds a mock `router` prop to the component using the provided path and params in options to mock behavior of `withRouter`.
+ *
+ * @param {*} ui - The component to be rendered using react-testing-library
+ * @param {*} options - Routing options used to create a mock router. Prefered options are `path` and `params`. Other options are for extended use cases and supporting existiong patterns.
+ * @returns {*} - The result of the render call from react-testing-library
+ */
+export const renderWithRouterProp = (ui, options) => {
+  const path = options?.path || '/';
+  const params = options?.params || {};
+  const navigate = options?.navigate || jest.fn();
+  const search = options?.search || '';
+
+  const pathname = generatePath(path, params);
+  const router = { location: { pathname, search }, params, navigate };
+
+  const routingOptions = { ...options, path, params, navigate, search };
+  if (options?.includeProviders)
+    return renderWithProviders(React.cloneElement(ui, { router: { ...router } }), routingOptions);
+
+  return renderWithRouter(React.cloneElement(ui, { router: { ...router } }), routingOptions);
 };
 
 export const mockPage = (path, name) => {
