94e680b0f8a8ff6f1a252697772ad45458b4e83b
Marty Boren <marty@truss.works>
2023-03-28 18:04:04 +0000

    Add comments to new columns

--- migrations/app/schema/20230324203740_add_secondary_address_radio_states.up.sql
+++ migrations/app/schema/20230324203740_add_secondary_address_radio_states.up.sql
@@ -1,3 +1,6 @@
-alter table mto_shipments
-	add column has_secondary_pickup_address bool,
-    add column has_secondary_delivery_address bool;
+ALTER TABLE mto_shipments
+	ADD COLUMN has_secondary_pickup_address   bool,
+	ADD COLUMN has_secondary_delivery_address bool;
+
+COMMENT ON COLUMN mto_shipments.has_secondary_pickup_address IS 'False if the shipment does not have a secondary pickup address. This column exists to make it possible to tell whether a shipment update should delete an address or not modify it.';
+COMMENT ON COLUMN mto_shipments.has_secondary_delivery_address IS 'False if the shipment does not have a secondary delivery address. This column exists to make it possible to tell whether a shipment update should delete an address or not modify it.';
