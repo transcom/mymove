e5279b03dde5388d83374b3b2def6d752f6da1d7
ruizajtruss <ruizaj@truss.works>
2023-04-08 00:58:48 +0000

    storybook migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 const path = require('path');
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
@@ -18,12 +14,12 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-links',
     '@dump247/storybook-state',
+    '@storybook/addon-mdx-gfm',
   ],
   webpackFinal: async (config) => {
     config.resolve.modules = config.resolve.modules || [];
     config.resolve.modules.push(path.resolve(__dirname, '../src'));
     config.resolve.modules.push('node_modules');
-
     return config;
   },
   refs: {
@@ -33,7 +29,10 @@ module.exports = {
     },
   },
   framework: {
-    name: '@storybook/react',
+    name: '@storybook/react-webpack5',
     options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
