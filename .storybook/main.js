d0db59dc71143ffc6edd797cd0ce8356026070f5
ruizajtruss <ruizaj@truss.works>
2023-04-10 20:40:04 +0000

    storybook auto migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,5 @@
 const path = require('path');
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/*.stories.jsx'],
   addons: [
     '@storybook/preset-create-react-app',
@@ -18,12 +14,12 @@ module.exports = {
     '@storybook/addon-knobs',
     '@storybook/addon-links',
     '@dump247/storybook-state',
+    '@storybook/addon-mdx-gfm',
   ],
   webpackFinal: async (config) => {
     config.resolve.modules = config.resolve.modules || [];
     config.resolve.modules.push(path.resolve(__dirname, '../src'));
     config.resolve.modules.push('node_modules');
-
     return config;
   },
   refs: {
@@ -31,5 +27,12 @@ module.exports = {
       title: 'ReactUSWDS',
       url: 'https://trussworks.github.io/react-uswds/',
     },
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
   },
 };
