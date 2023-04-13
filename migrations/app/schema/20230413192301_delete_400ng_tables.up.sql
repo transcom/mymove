715573117c1af6f94221b27c18967cb18ab73977
DymonMoore <dmoore@truss.works>
2023-04-13 19:23:30 +0000

    Add migration to delete unused 400ng tables.

---
+++ migrations/app/schema/20230413192301_delete_400ng_tables.up.sql
@@ -0,0 +1,7 @@
+DROP TABLE tariff400ng_full_pack_rates IF EXISTS
+DROP TABLE tariff400ng_full_unpack_rates IF EXISTS
+DROP TABLE tariff400ng_item_rates IF EXISTS
+DROP TABLE tariff400ng_items IF EXISTS
+DROP TABLE tariff400ng_linehaul_rates IF EXISTS
+DROP TABLE tariff400ng_service_areas IF EXISTS
+DROP TABLE tariff400ng_shorthaul_rates IF EXISTS
