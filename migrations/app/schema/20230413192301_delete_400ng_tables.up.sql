aaf8f81277bccbb77e6986e80f8f1173f451c053
DymonMoore <dmoore@truss.works>
2023-04-14 14:21:00 +0000

    Remove if exists

--- migrations/app/schema/20230413192301_delete_400ng_tables.up.sql
+++ migrations/app/schema/20230413192301_delete_400ng_tables.up.sql
@@ -1,7 +1,7 @@
-DROP TABLE tariff400ng_full_pack_rates IF EXISTS
-DROP TABLE tariff400ng_full_unpack_rates IF EXISTS
-DROP TABLE tariff400ng_item_rates IF EXISTS
-DROP TABLE tariff400ng_items IF EXISTS
-DROP TABLE tariff400ng_linehaul_rates IF EXISTS
-DROP TABLE tariff400ng_service_areas IF EXISTS
-DROP TABLE tariff400ng_shorthaul_rates IF EXISTS
+DROP TABLE tariff400ng_full_pack_rates;
+DROP TABLE tariff400ng_full_unpack_rates;
+DROP TABLE tariff400ng_item_rates;
+DROP TABLE tariff400ng_items;
+DROP TABLE tariff400ng_linehaul_rates;
+DROP TABLE tariff400ng_service_areas;
+DROP TABLE tariff400ng_shorthaul_rates;
