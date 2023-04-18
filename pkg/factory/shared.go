6e9952732ce2598a1abb5052055a6d195bc04008
StuTruss <stuartb@truss.works>
2023-04-18 15:55:21 +0000

    add BuildMtoServiceItemDimension

--- pkg/factory/shared.go
+++ pkg/factory/shared.go
@@ -44,6 +44,7 @@ var DutyLocation CustomType = "DutyLocation"
 var Entitlement CustomType = "Entitlement"
 var Move CustomType = "Move"
 var MTOServiceItem CustomType = "MTOServiceItem"
+var MTOServiceItemDimension CustomType = "MTOServiceItemDimension"
 var MTOShipment CustomType = "MTOShipment"
 var Notification CustomType = "Notification"
 var OfficePhoneLine CustomType = "OfficePhoneLine"
@@ -67,36 +68,37 @@ var UsersRoles CustomType = "UsersRoles"
 
 // defaultTypesMap allows us to assign CustomTypes for most default types
 var defaultTypesMap = map[string]CustomType{
-	"models.Address":               Address,
-	"models.AdminUser":             AdminUser,
-	"models.BackupContact":         BackupContact,
-	"models.Contractor":            Contractor,
-	"models.CustomerSupportRemark": CustomerSupportRemark,
-	"models.Document":              Document,
-	"models.DutyLocation":          DutyLocation,
-	"models.Entitlement":           Entitlement,
-	"models.Move":                  Move,
-	"models.MTOServiceItem":        MTOServiceItem,
-	"models.MTOShipment":           MTOShipment,
-	"models.Notification":          Notification,
-	"models.OfficePhoneLine":       OfficePhoneLine,
-	"models.OfficeUser":            OfficeUser,
-	"models.Order":                 Order,
-	"models.Organization":          Organization,
-	"models.PPMShipment":           PPMShipment,
-	"models.PostalCodeToGBLOC":     PostalCodeToGBLOC,
-	"models.ReService":             ReService,
-	"models.ServiceItemParamKey":   ServiceItemParamKey,
-	"models.ServiceMember":         ServiceMember,
-	"models.SignedCertification":   SignedCertification,
-	"models.StorageFacility":       StorageFacility,
-	"models.Tariff400ngZip3":       Tariff400ngZip3,
-	"models.TransportationOffice":  TransportationOffice,
-	"models.Upload":                Upload,
-	"models.UserUpload":            UserUpload,
-	"models.User":                  User,
-	"models.UsersRoles":            UsersRoles,
-	"roles.Role":                   Role,
+	"models.Address":                 Address,
+	"models.AdminUser":               AdminUser,
+	"models.BackupContact":           BackupContact,
+	"models.Contractor":              Contractor,
+	"models.CustomerSupportRemark":   CustomerSupportRemark,
+	"models.Document":                Document,
+	"models.DutyLocation":            DutyLocation,
+	"models.Entitlement":             Entitlement,
+	"models.Move":                    Move,
+	"models.MTOServiceItem":          MTOServiceItem,
+	"models.MTOServiceItemDimension": MTOServiceItemDimension,
+	"models.MTOShipment":             MTOShipment,
+	"models.Notification":            Notification,
+	"models.OfficePhoneLine":         OfficePhoneLine,
+	"models.OfficeUser":              OfficeUser,
+	"models.Order":                   Order,
+	"models.Organization":            Organization,
+	"models.PPMShipment":             PPMShipment,
+	"models.PostalCodeToGBLOC":       PostalCodeToGBLOC,
+	"models.ReService":               ReService,
+	"models.ServiceItemParamKey":     ServiceItemParamKey,
+	"models.ServiceMember":           ServiceMember,
+	"models.SignedCertification":     SignedCertification,
+	"models.StorageFacility":         StorageFacility,
+	"models.Tariff400ngZip3":         Tariff400ngZip3,
+	"models.TransportationOffice":    TransportationOffice,
+	"models.Upload":                  Upload,
+	"models.UserUpload":              UserUpload,
+	"models.User":                    User,
+	"models.UsersRoles":              UsersRoles,
+	"roles.Role":                     Role,
 }
 
 // Instead of nesting structs, we create specific CustomTypes here to give devs
