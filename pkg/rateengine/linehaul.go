dbc4d40f639eb5a05e3cc6d84c310dfc5c13f275
Reggie Riser <reggie@truss.works>
2023-04-27 20:15:58 +0000

    Deleting/deprecating old rate engine code; leaving some exported parts since referenced by shipment summary code we may want to reference

--- pkg/rateengine/linehaul.go
+++ pkg/rateengine/linehaul.go
@@ -1,18 +1,11 @@
 package rateengine
 
 import (
-	"time"
-
-	"github.com/pkg/errors"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
-	"github.com/transcom/mymove/pkg/appcontext"
-	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/unit"
 )
 
 // LinehaulCostComputation represents the results of a computation.
+// Deprecated: This is part of the old pre-GHC rate engine.
 type LinehaulCostComputation struct {
 	BaseLinehaul              unit.Cents
 	OriginLinehaulFactor      unit.Cents
@@ -21,119 +14,4 @@ type LinehaulCostComputation struct {
 	LinehaulChargeTotal       unit.Cents
 	Mileage                   int
 	FuelSurcharge             FeeAndRate
-}
-
-// Scale scales a cost computation by a multiplicative factor
-func (c *LinehaulCostComputation) Scale(factor float64) {
-	c.BaseLinehaul = c.BaseLinehaul.MultiplyFloat64(factor)
-	c.OriginLinehaulFactor = c.OriginLinehaulFactor.MultiplyFloat64(factor)
-	c.DestinationLinehaulFactor = c.DestinationLinehaulFactor.MultiplyFloat64(factor)
-	c.ShorthaulCharge = c.ShorthaulCharge.MultiplyFloat64(factor)
-	c.LinehaulChargeTotal = c.LinehaulChargeTotal.MultiplyFloat64(factor)
-}
-
-// MarshalLogObject allows LinehaulCostComputation to be logged by Zap.
-func (c LinehaulCostComputation) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
-	encoder.AddInt("BaseLinehaul", c.BaseLinehaul.Int())
-	encoder.AddInt("OriginLinehaulFactor", c.OriginLinehaulFactor.Int())
-	encoder.AddInt("DestinationLinehaulFactor", c.DestinationLinehaulFactor.Int())
-	encoder.AddInt("ShorthaulCharge", c.ShorthaulCharge.Int())
-
-	return nil
-}
-
-// Determine the Base Linehaul (BLH)
-func (re *RateEngine) baseLinehaul(appCtx appcontext.AppContext, mileage int, weight unit.Pound, date time.Time) (baseLinehaulChargeCents unit.Cents, err error) {
-	return unit.Cents(0), err
-}
-
-// Determine the Linehaul Factors (OLF and DLF)
-func (re *RateEngine) linehaulFactors(appCtx appcontext.AppContext, cwt unit.CWT, zip3 string, date time.Time) (linehaulFactorCents unit.Cents, err error) {
-	return unit.Cents(0), nil
-}
-
-// Determine Shorthaul (SH) Charge (ONLY applies if shipment moves 800 miles and less)
-func (re *RateEngine) shorthaulCharge(appCtx appcontext.AppContext, mileage int, cwt unit.CWT, date time.Time) (shorthaulChargeCents unit.Cents, err error) {
-	if mileage >= 800 {
-		return 0, nil
-	}
-	appCtx.Logger().Info("Shipment qualifies for shorthaul fee",
-		zap.String("moveLocator", re.move.Locator),
-		zap.Int("miles", mileage),
-	)
-
-	return unit.Cents(0), nil
-}
-
-// Determine Linehaul Charge (LC) TOTAL
-// Formula: LC= [BLH + OLF + DLF + [SH]
-func (re *RateEngine) linehaulChargeComputation(appCtx appcontext.AppContext, weight unit.Pound, originZip5 string, destinationZip5 string, distanceMiles int, pickupDate time.Time) (cost LinehaulCostComputation, err error) {
-	cwt := weight.ToCWT()
-	originZip3 := Zip5ToZip3(originZip5)
-	destinationZip3 := Zip5ToZip3(destinationZip5)
-
-	cost.Mileage = distanceMiles
-
-	cost.BaseLinehaul, err = re.baseLinehaul(appCtx, distanceMiles, weight, pickupDate)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to determine base linehaul charge")
-	}
-	cost.OriginLinehaulFactor, err = re.linehaulFactors(appCtx, cwt, originZip3, pickupDate)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to determine origin linehaul factor")
-	}
-	cost.DestinationLinehaulFactor, err = re.linehaulFactors(appCtx, cwt, destinationZip3, pickupDate)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to determine destination linehaul factor")
-	}
-	cost.ShorthaulCharge, err = re.shorthaulCharge(appCtx, distanceMiles, cwt, pickupDate)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to determine shorthaul charge")
-	}
-
-	cost.LinehaulChargeTotal = cost.BaseLinehaul +
-		cost.OriginLinehaulFactor +
-		cost.DestinationLinehaulFactor +
-		cost.ShorthaulCharge
-
-	appCtx.Logger().Info("Linehaul charge total calculated",
-		zap.String("moveLocator", re.move.Locator),
-		zap.Int("linehaul total", cost.LinehaulChargeTotal.Int()),
-		zap.Int("linehaul", cost.BaseLinehaul.Int()),
-		zap.Int("origin lh factor", cost.OriginLinehaulFactor.Int()),
-		zap.Int("destination lh factor", cost.DestinationLinehaulFactor.Int()),
-		zap.Int("shorthaul", cost.ShorthaulCharge.Int()),
-	)
-
-	return cost, err
-}
-
-// Calculate the fuel surcharge and return the result
-func (re *RateEngine) fuelSurchargeComputation(appCtx appcontext.AppContext, totalLinehaulCost unit.Cents, bookDate time.Time) (fuelSurcharge FeeAndRate, err error) {
-	fuelEIADieselPriceSlice := []models.FuelEIADieselPrice{}
-
-	// Changing the format of the date to remove the time portion so it plays nicely with db
-	bookDateString := bookDate.Format("2006-01-02")
-
-	query := appCtx.DB().Where("rate_start_date <= ?", bookDateString).Where("rate_end_date >= ?", bookDateString)
-	err1 := query.All(&fuelEIADieselPriceSlice)
-	if err1 != nil {
-		appCtx.Logger().Error(err1.Error())
-	}
-
-	// We expect to only retrieve one value from the FuelEIADieselPrice table. There should be only one valid date range for a given bookDate.
-	// If we get more than one, something is wrong.
-	if len(fuelEIADieselPriceSlice) > 1 {
-		appCtx.Logger().Error("Got back multiple values from FuelEIADieselPrice when we should have only gotten one.")
-	}
-
-	if len(fuelEIADieselPriceSlice) == 0 {
-		appCtx.Logger().Error("Query failed to find an applicable FuelEIADieselPrice")
-	}
-
-	fuelEIADieselPrice := fuelEIADieselPriceSlice[0]
-	fuelSurchargePercentage := float64(fuelEIADieselPrice.BaselineRate) / 100
-	fee := totalLinehaulCost.MultiplyFloat64(fuelSurchargePercentage)
-
-	return FeeAndRate{Fee: unit.Cents(fee), Rate: fuelEIADieselPrice.EIAPricePerGallonMillicents}, err
 }
--- pkg/rateengine/nonlinehaul.go
+++ pkg/rateengine/nonlinehaul.go
@@ -1,187 +1,21 @@
 package rateengine
 
 import (
-	"math"
-	"time"
-
-	"github.com/pkg/errors"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
-	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/unit"
 )
 
 // FeeAndRate holds the rate lookup and calculated fee (non-discounted)
+// Deprecated: This is part of the old pre-GHC rate engine.
 type FeeAndRate struct {
 	Fee  unit.Cents
 	Rate unit.Millicents
 }
 
 // NonLinehaulCostComputation represents the results of a computation.
+// Deprecated: This is part of the old pre-GHC rate engine.
 type NonLinehaulCostComputation struct {
 	OriginService      FeeAndRate
 	DestinationService FeeAndRate
 	Pack               FeeAndRate
 	Unpack             FeeAndRate
-}
-
-// SITComputation represents the parts of the SIT calculation (needs to be separable to apply correct discount rates).
-type SITComputation struct {
-	SITPart            unit.Cents
-	LinehaulPart       unit.Cents
-	NonDiscountedTotal unit.Cents
-}
-
-// Scale scales a cost computation by a multiplicative factor
-func (c *NonLinehaulCostComputation) Scale(factor float64) {
-	c.OriginService.Fee = c.OriginService.Fee.MultiplyFloat64(factor)
-	c.DestinationService.Fee = c.DestinationService.Fee.MultiplyFloat64(factor)
-	c.Pack.Fee = c.Pack.Fee.MultiplyFloat64(factor)
-	c.Unpack.Fee = c.Unpack.Fee.MultiplyFloat64(factor)
-}
-
-// ApplyDiscount will apply the linehaul and SIT discounts to the appropriate parts of the SIT computation.
-func (s SITComputation) ApplyDiscount(linehaulDiscount unit.DiscountRate, sitDiscount unit.DiscountRate) unit.Cents {
-	return sitDiscount.Apply(s.SITPart).AddCents(linehaulDiscount.Apply(s.LinehaulPart))
-}
-
-// MarshalLogObject allows SITComputation to be logged by Zap.
-func (s SITComputation) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
-	encoder.AddInt("SITPart", s.SITPart.Int())
-	encoder.AddInt("LinehaulPart", s.LinehaulPart.Int())
-	encoder.AddInt("NonDiscountedTotal", s.NonDiscountedTotal.Int())
-
-	return nil
-}
-
-// serviceFeeCents returns the NON-DISCOUNTED rate in millicents with the fee
-func (re *RateEngine) serviceFeeCents(appCtx appcontext.AppContext, cwt unit.CWT, zip3 string, date time.Time) (FeeAndRate, error) {
-	rateCents := unit.Cents(0)
-	feeCents := rateCents.Multiply(cwt.Int())
-	return FeeAndRate{Fee: feeCents, Rate: rateCents.ToMillicents()}, nil
-}
-
-// fullPackCents Returns the NON-DISCOUNTED rate in millicents with the fee
-func (re *RateEngine) fullPackCents(appCtx appcontext.AppContext, cwt unit.CWT, zip3 string, date time.Time) (FeeAndRate, error) {
-	fullPackRate := unit.Cents(0)
-
-	return FeeAndRate{Fee: fullPackRate.Multiply(cwt.Int()), Rate: fullPackRate.ToMillicents()}, nil
-}
-
-// fullUnpackCents Returns the NON-DISCOUNTED rate in millicents with the fee
-func (re *RateEngine) fullUnpackCents(appCtx appcontext.AppContext, cwt unit.CWT, zip3 string, date time.Time) (FeeAndRate, error) {
-	fullUnpackRate := 0
-
-	return FeeAndRate{Fee: unit.Cents(math.Round(float64(cwt.Int()*fullUnpackRate) / 1000.0)), Rate: unit.Millicents(fullUnpackRate)}, nil
-}
-
-// SitCharge calculates the SIT charge based on various factors.
-// Note: Assumes the caller will apply any SIT discount rate as needed (no discounts applied here).
-func (re *RateEngine) SitCharge(appCtx appcontext.AppContext, cwt unit.CWT, daysInSIT int, zip3 string, date time.Time, isPPM bool) (SITComputation, error) {
-	if daysInSIT == 0 {
-		return SITComputation{}, nil
-	} else if daysInSIT < 0 {
-		return SITComputation{}, errors.New("requested SitCharge for negative days in SIT")
-	}
-
-	effectiveCWT := cwt
-	if !isPPM {
-		// An HHG uses a minimum weight of 1000 pounds.
-		// TODO: If an HHG shipment is delivered partially out of SIT (split deliveries), 1000 lb min does not apply.
-		minCWT := unit.Pound(1000).ToCWT()
-		if cwt < minCWT {
-			effectiveCWT = minCWT
-		}
-	}
-
-	// Both PPMs and HHGs use 185A and 185B in the same way.
-	sitPart := unit.Cents(0)
-	additionalDays := daysInSIT - 1
-	if additionalDays > 0 {
-		sitPart = sitPart.AddCents(unit.Cents(0))
-	}
-
-	zapFields := []zap.Field{
-		zap.Int("cwt", cwt.Int()),
-		zap.Int("days", daysInSIT),
-		zap.String("zip3", zip3),
-		zap.Time("date", date),
-		zap.Bool("isPPM", isPPM),
-		zap.Int("effectiveCWT", effectiveCWT.Int()),
-	}
-
-	var linehaulPart unit.Cents
-	if isPPM {
-		// PPM SIT formula:
-		//   (185A SIT first day rate * CWT) +
-		//   (185B SIT additional day rate * additional days * CWT) +
-		//   210A SIT PD 30 miles or less for SIT PD schedule of service area +
-		//   225A SIT PD Self/Mini Storage for services schedule of service area
-		rate210ACents := unit.Cents(0)
-		sitPart = sitPart.AddCents(rate210ACents)
-
-		rate225ACents := unit.Cents(0)
-		linehaulPart = rate225ACents
-
-		zapFields = append(zapFields,
-			zap.Int("210A", rate210ACents.Int()),
-			zap.Int("225A", rate225ACents.Int()))
-	}
-	// TODO: The rest of the HHG scenarios are as follows (to be added to the 185A and 185B parts):
-	//   * 30 miles or less from original delivery address to final delivery address (block 18 on GBL):
-	//       (185A SIT first day rate * CWT) +
-	//       (185B SIT additional day rate * additional days * CWT)
-	//       210A SIT PD 30 miles or less for SIT PD schedule of service area
-	//   * Between 31 and 50 miles from original delivery address to final delivery address (block 18 on GBL):
-	//       (185A SIT first day rate * CWT) +
-	//	     (185B SIT additional day rate * additional days * CWT)
-	//       210A SIT PD 30 miles or less for SIT PD schedule of service area +
-	//       210B SIT PD 30 to 50 miles SIT PD schedule of service area
-	//   * Over 50 miles from original delivery address to final delivery address (block 18 on GBL):
-	//       (185A SIT first day rate * CWT) +
-	//	     (185B SIT additional day rate * additional days * CWT)
-	//       210C SIT PD over 50 miles SIT PD schedule of service area
-
-	sitComputation := SITComputation{
-		SITPart:            sitPart,
-		LinehaulPart:       linehaulPart,
-		NonDiscountedTotal: sitPart.AddCents(linehaulPart),
-	}
-
-	zapFields = append(zapFields, zap.Object("sit computation", sitComputation), zap.String("moveLocator", re.move.Locator))
-	appCtx.Logger().Info("sit calculation", zapFields...)
-
-	return sitComputation, nil
-}
-
-func (re *RateEngine) nonLinehaulChargeComputation(appCtx appcontext.AppContext, weight unit.Pound, originZip5 string, destinationZip5 string, date time.Time) (cost NonLinehaulCostComputation, err error) {
-	cwt := weight.ToCWT()
-	originZip3 := Zip5ToZip3(originZip5)
-	destinationZip3 := Zip5ToZip3(destinationZip5)
-	cost.OriginService, err = re.serviceFeeCents(appCtx, cwt, originZip3, date)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to  determine origin service fee")
-	}
-	cost.DestinationService, err = re.serviceFeeCents(appCtx, cwt, destinationZip3, date)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to  determine destination service fee")
-	}
-	cost.Pack, err = re.fullPackCents(appCtx, cwt, originZip3, date)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to  determine full pack cost")
-	}
-	cost.Unpack, err = re.fullUnpackCents(appCtx, cwt, destinationZip3, date)
-	if err != nil {
-		return cost, errors.Wrap(err, "Failed to  determine full unpack cost")
-	}
-
-	appCtx.Logger().Info("Non-Linehaul charge total calculated",
-		zap.String("moveLocator", re.move.Locator),
-		zap.Int("origin service fee", cost.OriginService.Fee.Int()),
-		zap.Int("destination service fee", cost.DestinationService.Fee.Int()),
-		zap.Int("pack fee", cost.Pack.Fee.Int()),
-		zap.Int("unpack fee", cost.Unpack.Fee.Int()))
-
-	return cost, nil
 }
