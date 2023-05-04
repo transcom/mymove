14d66ea5b5658763ff4a6c02fdd92e9325648b38
Andrew Hobson <ahobson@truss.works>
2023-05-04 16:29:48 +0000

    Create BaseRoutingSuite with helpers, convert RoutingSuite to use it

---
+++ pkg/handlers/routing/base_routing_suite.go
@@ -0,0 +1,191 @@
+package routing
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"path"
+	"time"
+
+	"github.com/spf13/afero"
+
+	"github.com/transcom/mymove/pkg/auth"
+	"github.com/transcom/mymove/pkg/handlers"
+	"github.com/transcom/mymove/pkg/handlers/authentication"
+	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/notifications"
+	"github.com/transcom/mymove/pkg/telemetry"
+	"github.com/transcom/mymove/pkg/testingsuite"
+)
+
+type BaseRoutingSuite struct {
+	handlers.BaseHandlerTestSuite
+	indexContent  string
+	routingConfig *Config
+}
+
+func NewBaseRoutingSuite() BaseRoutingSuite {
+	return BaseRoutingSuite{
+		BaseHandlerTestSuite: handlers.NewBaseHandlerTestSuite(
+			notifications.NewStubNotificationSender("milmovelocal"),
+			testingsuite.CurrentPackage(),
+			testingsuite.WithPerTestTransaction()),
+		indexContent: "<html></html>",
+	}
+}
+
+// override HandlerConfig to use the version saved in routing config
+// so the same session manager(s) are used
+func (suite *BaseRoutingSuite) HandlerConfig() handlers.HandlerConfig {
+	return suite.RoutingConfig().HandlerConfig
+}
+
+func (suite *BaseRoutingSuite) RoutingConfig() *Config {
+	// need to ensure we have only one routing config so the same
+	// session managers are re-used
+	if suite.routingConfig != nil {
+		return suite.routingConfig
+	}
+	// ensure the routing config is reset when the test context is finished
+	suite.T().Cleanup(func() { suite.routingConfig = nil })
+	// Test that we can initialize routing and serve the index file
+	handlerConfig := suite.BaseHandlerTestSuite.HandlerConfig()
+	handlerConfig.SetAppNames(handlers.ApplicationTestServername())
+
+	fakeLoginGovProvider := authentication.NewLoginGovProvider("fakeHostname", "secret_key", suite.Logger())
+
+	authContext := authentication.NewAuthContext(suite.Logger(), fakeLoginGovProvider, "http", 80)
+
+	fakeFs := afero.NewMemMapFs()
+	fakeBase := "fakebase"
+	f, err := fakeFs.Create(path.Join(fakeBase, "index.html"))
+	suite.FatalNoError(err)
+	_, err = f.Write([]byte(suite.indexContent))
+	suite.FatalNoError(err)
+
+	suite.routingConfig = &Config{
+		FileSystem:    fakeFs,
+		HandlerConfig: handlerConfig,
+		AuthContext:   authContext,
+		BuildRoot:     fakeBase,
+
+		// include all these as true to increase test coverage
+		ServeSwaggerUI:      true,
+		ServePrime:          true,
+		ServeSupport:        true,
+		ServeDebugPProf:     true,
+		ServeAPIInternal:    true,
+		ServeAdmin:          true,
+		ServePrimeSimulator: true,
+		ServeGHC:            true,
+		ServeDevlocalAuth:   true,
+		ServeOrders:         true,
+	}
+
+	return suite.routingConfig
+}
+
+func (suite *BaseRoutingSuite) SetupSiteHandler() http.Handler {
+	return suite.SetupCustomSiteHandler(suite.RoutingConfig())
+}
+
+func (suite *BaseRoutingSuite) SetupCustomSiteHandler(routingConfig *Config) http.Handler {
+	siteHandler, err := InitRouting(suite.AppContextForTest(), nil, routingConfig, &telemetry.Config{})
+	suite.FatalNoError(err)
+	return siteHandler
+}
+
+func (suite *BaseRoutingSuite) setupRequestSession(req *http.Request, user models.User, hostname string) {
+	app, err := auth.ApplicationName(hostname, suite.HandlerConfig().AppNames())
+	suite.FatalNoError(err)
+	sessionManager := suite.HandlerConfig().SessionManagers().SessionManagerForApplication(app)
+
+	fakeAuthContext, err := sessionManager.Load(context.Background(), "")
+	suite.NoError(err)
+
+	fakeSession := auth.Session{
+		ApplicationName: app,
+		Hostname:        hostname,
+	}
+
+	suite.FatalNotNil(user.LoginGovUUID)
+	suite.FatalFalse(user.LoginGovUUID.IsNil())
+	userIdentity, err := models.FetchUserIdentity(suite.DB(), user.LoginGovUUID.String())
+	suite.FatalNoError(err)
+
+	// use AuthorizeKnownUser which also sets up various things in the
+	// Session, including Permissions
+	authentication.AuthorizeKnownUser(fakeAuthContext, suite.AppContextWithSessionForTest(&fakeSession),
+		userIdentity, sessionManager)
+
+	sessionManager.Put(fakeAuthContext, "session", &fakeSession)
+	// ignore expiry for this test
+	// need to call commit ourselves to get the session token to put
+	// into the cookie
+	token, _, err := sessionManager.Commit(fakeAuthContext)
+	suite.NoError(err)
+	sessionCookie := sessionManager.SessionCookie()
+	cookie := &http.Cookie{
+		Name:     sessionCookie.Name,
+		Value:    token,
+		Path:     sessionCookie.Path,
+		Domain:   sessionCookie.Domain,
+		Secure:   sessionCookie.Secure,
+		HttpOnly: sessionCookie.HttpOnly,
+		SameSite: sessionCookie.SameSite,
+	}
+	cookie.Expires = time.Unix(1, 0)
+	cookie.MaxAge = -1
+
+	req.Header.Add("cookie", cookie.String())
+}
+
+func (suite *BaseRoutingSuite) SetupAdminRequestSession(req *http.Request, adminUser models.AdminUser) {
+	suite.setupRequestSession(req, adminUser.User, suite.HandlerConfig().AppNames().AdminServername)
+}
+
+func (suite *BaseRoutingSuite) SetupMilRequestSession(req *http.Request, serviceMember models.ServiceMember) {
+	suite.setupRequestSession(req, serviceMember.User, suite.HandlerConfig().AppNames().MilServername)
+}
+
+func (suite *BaseRoutingSuite) SetupOfficeRequestSession(req *http.Request, officeUser models.OfficeUser) {
+	suite.setupRequestSession(req, officeUser.User, suite.HandlerConfig().AppNames().OfficeServername)
+}
+
+func (suite *BaseRoutingSuite) NewAdminRequest(method string, relativePath string, body io.Reader) *http.Request {
+	return httptest.NewRequest(method,
+		fmt.Sprintf("http://%s%s", suite.HandlerConfig().AppNames().AdminServername, relativePath),
+		body)
+}
+
+func (suite *BaseRoutingSuite) NewAuthenticatedAdminRequest(method string, relativePath string, body io.Reader, adminUser models.AdminUser) *http.Request {
+	req := suite.NewAdminRequest(method, relativePath, body)
+	suite.SetupAdminRequestSession(req, adminUser)
+	return req
+}
+
+func (suite *BaseRoutingSuite) NewMilRequest(method string, relativePath string, body io.Reader) *http.Request {
+	return httptest.NewRequest(method,
+		fmt.Sprintf("http://%s%s", suite.HandlerConfig().AppNames().MilServername, relativePath),
+		body)
+}
+
+func (suite *BaseRoutingSuite) NewAuthenticatedMilRequest(method string, relativePath string, body io.Reader, serviceMember models.ServiceMember) *http.Request {
+	req := suite.NewMilRequest(method, relativePath, body)
+	suite.SetupMilRequestSession(req, serviceMember)
+	return req
+}
+
+func (suite *BaseRoutingSuite) NewOfficeRequest(method string, relativePath string, body io.Reader) *http.Request {
+	return httptest.NewRequest(method,
+		fmt.Sprintf("http://%s%s", suite.HandlerConfig().AppNames().OfficeServername, relativePath),
+		body)
+}
+
+func (suite *BaseRoutingSuite) NewAuthenticatedOfficeRequest(method string, relativePath string, body io.Reader, officeUser models.OfficeUser) *http.Request {
+	req := suite.NewOfficeRequest(method, relativePath, body)
+	suite.SetupOfficeRequestSession(req, officeUser)
+	return req
+}
--- pkg/handlers/routing/routing_init_test.go
+++ pkg/handlers/routing/routing_init_test.go
@@ -1,183 +1,73 @@
 package routing
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"path"
 	"testing"
-	"time"
 
-	"github.com/spf13/afero"
 	"github.com/stretchr/testify/suite"
 
-	"github.com/transcom/mymove/pkg/auth"
 	"github.com/transcom/mymove/pkg/factory"
 	"github.com/transcom/mymove/pkg/gen/internalmessages"
-	"github.com/transcom/mymove/pkg/handlers"
-	"github.com/transcom/mymove/pkg/handlers/authentication"
-	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/models/roles"
-	"github.com/transcom/mymove/pkg/notifications"
-	"github.com/transcom/mymove/pkg/telemetry"
-	"github.com/transcom/mymove/pkg/testingsuite"
 )
 
 type RoutingSuite struct {
-	handlers.BaseHandlerTestSuite
-	routingConfig *Config
+	BaseRoutingSuite
 }
 
 func TestRoutingSuite(t *testing.T) {
 	hs := &RoutingSuite{
-		BaseHandlerTestSuite: handlers.NewBaseHandlerTestSuite(notifications.NewStubNotificationSender("milmovelocal"), testingsuite.CurrentPackage(), testingsuite.WithPerTestTransaction()),
+		NewBaseRoutingSuite(),
 	}
 	suite.Run(t, hs)
 	hs.PopTestSuite.TearDown()
 }
 
-var appNames = auth.ApplicationServername{
-	MilServername:    "mil.example.com",
-	OfficeServername: "office.example.com",
-	AdminServername:  "admin.example.com",
-	PrimeServername:  "prime.example.com",
-}
-
-const indexContent = "<html></html>"
-
-func (suite *RoutingSuite) SetupTest() {
-	// Test that we can initialize routing and serve the index file
-	handlerConfig := suite.HandlerConfig()
-	handlerConfig.SetAppNames(appNames)
-
-	fakeLoginGovProvider := authentication.NewLoginGovProvider("fakeHostname", "secret_key", suite.Logger())
-
-	authContext := authentication.NewAuthContext(suite.Logger(), fakeLoginGovProvider, "http", 80)
-
-	fakeFs := afero.NewMemMapFs()
-	fakeBase := "fakebase"
-	f, err := fakeFs.Create(path.Join(fakeBase, "index.html"))
-	suite.NoError(err)
-	_, err = f.Write([]byte(indexContent))
-	suite.NoError(err)
-
-	suite.routingConfig = &Config{
-		FileSystem:    fakeFs,
-		HandlerConfig: handlerConfig,
-		AuthContext:   authContext,
-		BuildRoot:     fakeBase,
-
-		// include all these as true to increase test coverage
-		ServeSwaggerUI:      true,
-		ServePrime:          true,
-		ServeSupport:        true,
-		ServeDebugPProf:     true,
-		ServeAPIInternal:    true,
-		ServeAdmin:          true,
-		ServePrimeSimulator: true,
-		ServeGHC:            true,
-		ServeDevlocalAuth:   true,
-		ServeOrders:         true,
-	}
-}
-
 func (suite *RoutingSuite) TestBasicRoutingInit() {
 
-	h, err := InitRouting(suite.AppContextForTest(), nil, suite.routingConfig, &telemetry.Config{})
-	suite.NoError(err)
-
-	req := httptest.NewRequest("GET", fmt.Sprintf("http://%s/", appNames.MilServername), nil)
+	req := suite.NewMilRequest("GET", "/", nil)
 	rr := httptest.NewRecorder()
-	h.ServeHTTP(rr, req)
+	suite.SetupSiteHandler().ServeHTTP(rr, req)
 
 	suite.Equal(http.StatusOK, rr.Code)
-	suite.Equal(indexContent, rr.Body.String())
+	suite.Equal(suite.indexContent, rr.Body.String())
 }
 
 func (suite *RoutingSuite) TestServeGHC() {
 
-	h, err := InitRouting(suite.AppContextForTest(), nil, suite.routingConfig, &telemetry.Config{})
-	suite.NoError(err)
-
 	user := factory.BuildUser(suite.DB(), nil, nil)
+	routingConfig := suite.RoutingConfig()
+	siteHandler := suite.SetupCustomSiteHandler(routingConfig)
 
 	// make the request without auth
-	// getting auth working here in the test is a good bit more work.
-	// Will have that in a future PR
-	req := httptest.NewRequest("GET", fmt.Sprintf("http://%s/ghc/v1/customer/%s", appNames.MilServername, user.ID.String()), nil)
+	req := suite.NewMilRequest("GET", fmt.Sprintf("/ghc/v1/customer/%s", user.ID.String()), nil)
 	rr := httptest.NewRecorder()
-	h.ServeHTTP(rr, req)
+	siteHandler.ServeHTTP(rr, req)
 	suite.Equal(http.StatusUnauthorized, rr.Code)
 
 	// make the request with GHC routing turned off
-	suite.routingConfig.ServeGHC = false
-	h, err = InitRouting(suite.AppContextForTest(), nil, suite.routingConfig, &telemetry.Config{})
-	suite.NoError(err)
-	req = httptest.NewRequest("GET", fmt.Sprintf("http://%s/ghc/v1/customer/%s", appNames.MilServername, user.ID.String()), nil)
+	routingConfig.ServeGHC = false
+	noghcHandler := suite.SetupCustomSiteHandler(routingConfig)
+	req = suite.NewMilRequest("GET", fmt.Sprintf("/ghc/v1/customer/%s", user.ID.String()), nil)
 	rr = httptest.NewRecorder()
-	h.ServeHTTP(rr, req)
+	noghcHandler.ServeHTTP(rr, req)
 	// if the API is not enabled, the routing will be served by the
 	// SPA handler, sending back the index page, which will have the
 	// javascript SPA routing
 	suite.Equal(http.StatusOK, rr.Code)
-	suite.Equal(indexContent, rr.Body.String())
-}
-
-func (suite *RoutingSuite) setupOfficeRequestSession(req *http.Request, officeUser models.OfficeUser) {
-
-	fakeAuthContext := context.Background()
-	sessionManager := suite.routingConfig.HandlerConfig.SessionManagers().Office
-	fakeAuthContext, err := sessionManager.Load(fakeAuthContext, "")
-	suite.NoError(err)
-
-	fakeSession := auth.Session{
-		ApplicationName: auth.Application(auth.OfficeApp),
-		Hostname:        suite.HandlerConfig().AppNames().OfficeServername,
-	}
-
-	userIdentity, err := models.FetchUserIdentity(suite.DB(), officeUser.User.LoginGovUUID.String())
-	suite.Assert().NoError(err)
-
-	// use AuthorizeKnownUser which also sets up various things in the
-	// Session, including Permissions
-	authentication.AuthorizeKnownUser(fakeAuthContext, suite.AppContextWithSessionForTest(&fakeSession),
-		userIdentity, sessionManager)
-
-	sessionManager.Put(fakeAuthContext, "session", fakeSession)
-	// ignore expiry for this test
-	// need to call commit ourselves to get the session token to put
-	// into the cookie
-	token, _, err := sessionManager.Commit(fakeAuthContext)
-	suite.NoError(err)
-	sessionCookie := sessionManager.SessionCookie()
-	cookie := &http.Cookie{
-		Name:     sessionCookie.Name,
-		Value:    token,
-		Path:     sessionCookie.Path,
-		Domain:   sessionCookie.Domain,
-		Secure:   sessionCookie.Secure,
-		HttpOnly: sessionCookie.HttpOnly,
-		SameSite: sessionCookie.SameSite,
-	}
-	cookie.Expires = time.Unix(1, 0)
-	cookie.MaxAge = -1
-
-	req.Header.Add("cookie", cookie.String())
+	suite.Equal(suite.indexContent, rr.Body.String())
 }
 
 func (suite *RoutingSuite) TestOfficeLoggedInEndpoint() {
-	h, err := InitRouting(suite.AppContextForTest(), nil, suite.routingConfig, &telemetry.Config{})
-	suite.NoError(err)
-
 	officeUser := factory.BuildOfficeUserWithRoles(suite.DB(), factory.GetTraitActiveOfficeUser(),
 		[]roles.RoleType{roles.RoleTypeTIO, roles.RoleTypeServicesCounselor})
-	req := httptest.NewRequest("GET", fmt.Sprintf("http://%s/internal/users/logged_in", appNames.OfficeServername), nil)
-	suite.setupOfficeRequestSession(req, officeUser)
+	req := suite.NewAuthenticatedOfficeRequest("GET", "/internal/users/logged_in", nil, officeUser)
 
 	rr := httptest.NewRecorder()
-	h.ServeHTTP(rr, req)
+	suite.SetupSiteHandler().ServeHTTP(rr, req)
 	suite.Equal(http.StatusOK, rr.Code)
 
 	var userPayload internalmessages.LoggedInUserPayload
