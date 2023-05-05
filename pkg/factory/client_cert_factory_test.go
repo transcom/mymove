db8fd2d5141ce518fdd4bac598fb7226d36c5466
Andrew Hobson <ahobson@truss.works>
2023-05-05 14:16:16 +0000

    Update BuildClientCert factory and tests

--- pkg/factory/client_cert_factory_test.go
+++ pkg/factory/client_cert_factory_test.go
@@ -1,108 +1,133 @@
 package factory
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+
 	"github.com/gofrs/uuid"
 
 	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/models/roles"
 )
 
-const defaultSHA256Digest string = "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b"
-const defaultSubject string = "CN=example-user,OU=DoD+OU=PKI+OU=CONTRACTOR,O=U.S. Government,C=US"
-const customSubject string = "CN=custom-user,OU=DoD+OU=PKI+OU=CONTRACTOR,O=U.S. Government,C=US"
-
 func (suite *FactorySuite) TestBuildClientCert() {
-	suite.Run("Successful creation of default user", func() {
-		// Under test:       BuildClientCert
-		// Mocked:           None
-		// Set up:           Create a ClientCert with no customizations or traits
-		// Expected outcome: ClientCert should be created with default values
+	suite.Run("Successful creation of default clientCert", func() {
+		// Under test:      BuildClientCert
+		// Mocked:          None
+		// Set up:          Create a clientCert with no customizations or traits
+		// Expected outcome:ClientCert should be created with default values
 
-		certificate := BuildClientCert(suite.DB(), nil, nil)
-		suite.Equal(defaultSHA256Digest, certificate.Sha256Digest)
-		suite.Equal(defaultSubject, certificate.Subject)
-		suite.NotNil(certificate.UserID)
+		// FUNCTION UNDER TEST
+		clientCert := BuildClientCert(suite.DB(), nil, nil)
+
+		// VALIDATE RESULTS
+		suite.NotEmpty(clientCert.Sha256Digest)
+		suite.Contains(clientCert.Subject, "OU=AppClientTLS/CN=factory-")
+		suite.True(clientCert.AllowPrime)
 	})
 
-	suite.Run("Successful creation of user with customization", func() {
-		// Under test:       BuildClientCert
-		// Set up:           Create a ClientCert with a customized subject and no trait
-		// Expected outcome: ClientCert should be created with default SHA256 digest and custom subject with no user association
-		certificate := BuildClientCert(suite.DB(), []Customization{
+	suite.Run("Successful creation of prime clientCert", func() {
+		// Under test:      BuildPrimeClientCert
+		// Mocked:          None
+		// Set up:          Create a clientCert with no customizations or traits
+		// Expected outcome:ClientCert should be created with default
+		// values, associated with an active user with the prime role
+
+		// FUNCTION UNDER TEST
+		clientCert := BuildPrimeClientCert(suite.DB())
+
+		// VALIDATE RESULTS
+		suite.NotEmpty(clientCert.Sha256Digest)
+		suite.Contains(clientCert.Subject, "OU=AppClientTLS/CN=factory-")
+		suite.True(clientCert.AllowPrime)
+		var user models.User
+		suite.NoError(suite.DB().Eager("Roles").Find(&user, clientCert.UserID))
+		suite.NotEmpty(user.Roles)
+		suite.Equal(1, len(user.Roles), user.Roles)
+		suite.Equal(roles.RoleTypePrime, user.Roles[0].RoleType)
+	})
+
+	suite.Run("Successful creation of customized clientCert", func() {
+		// Under test:      BuildClientCert
+		// Mocked:          None
+		// Set up:          Create a clientCert with customization
+		// Expected outcome:ClientCert should be created with customized values
+
+		// SETUP
+		// Create a custom clientCert to compare values
+		s := sha256.Sum256([]byte("custom"))
+		custClientCert := models.ClientCert{
+			Sha256Digest: hex.EncodeToString(s[:]),
+			Subject:      "CustomSubject",
+			AllowPrime:   false,
+		}
+
+		customUser := BuildUser(suite.DB(), nil, nil)
+
+		// FUNCTION UNDER TEST
+		clientCert := BuildClientCert(suite.DB(), []Customization{
 			{
-				Model: models.ClientCert{
-					Subject: customSubject,
-				},
+				Model: custClientCert,
 			},
-		}, nil)
-		suite.Equal(defaultSHA256Digest, certificate.Sha256Digest)
-		suite.Equal(customSubject, certificate.Subject)
-		suite.NotNil(certificate.UserID)
-	})
-
-	suite.Run("Successful creation of user with trait", func() {
-		// Under test:       BuildClientCert
-		// Set up:           Create a Client Certificate with a trait
-		// Expected outcome: User should be created with with default SHA256,
-		// Subject, and an associated UserID
-
-		user := BuildUser(suite.DB(), nil, nil)
-
-		certificate := BuildClientCert(suite.DB(), []Customization{
 			{
-				Model:    user,
+				Model:    customUser,
 				LinkOnly: true,
 			},
 		}, nil)
-		suite.Equal(defaultSHA256Digest, certificate.Sha256Digest)
-		suite.Equal(defaultSubject, certificate.Subject)
-		suite.Equal(user.ID, certificate.UserID)
+
+		// VALIDATE RESULTS
+		suite.Equal(custClientCert.Sha256Digest, clientCert.Sha256Digest)
+		suite.Equal(custClientCert.Subject, clientCert.Subject)
+		suite.Equal(custClientCert.AllowPrime, clientCert.AllowPrime)
+		suite.Equal(customUser.ID, clientCert.UserID)
 	})
 
-	suite.Run("Successful creation of user with both", func() {
-		// Under test:       BuildClientCert
-		// Set up:           Create a Client Certificate with a customized subject and active trait
-		// Expected outcome: Cert should be created with default Sha256Digest, custom subject, and an associated UserID
+	suite.Run("Successful return of linkOnly clientCert", func() {
+		// Under test:      BuildClientCert
+		// Set up:          Create a clientCert and pass in a linkOnly clientCert
+		// Expected outcome:No new clientCert should be created
 
-		certificate := BuildClientCert(suite.DB(), []Customization{
-			{
-				Model: models.ClientCert{
-					Subject: customSubject,
-				},
-			}}, []Trait{
-			GetTraitActiveUser,
-		})
-		suite.Equal(defaultSHA256Digest, certificate.Sha256Digest)
-		suite.Equal(customSubject, certificate.Subject)
-		suite.NotNil(certificate.UserID)
-	})
-
-	suite.Run("Successful creation of stubbed user", func() {
-		// Under test:       BuildClientCert
-		// Set up:           Create a customized user, but don't pass in a db
-		// Expected outcome: Client Certifiate should be created with email and active status
-		//                   No user should be created in database
+		// Check num clientCerts
 		precount, err := suite.DB().Count(&models.ClientCert{})
 		suite.NoError(err)
 
-		user := models.User{
-			ID: uuid.Must(uuid.NewV4()),
+		customCert := models.ClientCert{
+			ID:           uuid.Must(uuid.NewV4()),
+			Sha256Digest: hex.EncodeToString([]byte("LinkOnly")),
+			Subject:      "LinkOnly",
 		}
-		certificate := BuildClientCert(nil, []Customization{
+
+		clientCert := BuildClientCert(suite.DB(), []Customization{
 			{
-				Model: models.ClientCert{
-					Subject: customSubject,
-				},
-			},
-			{
-				Model: user,
+				Model:    customCert,
+				LinkOnly: true,
 			},
 		}, nil)
-
-		suite.Equal(customSubject, certificate.Subject)
-		suite.Equal(user.ID, certificate.UserID)
-		// Count how many certificates are in the DB, no certificates should have been created.
 		count, err := suite.DB().Count(&models.ClientCert{})
-		suite.NoError(err)
 		suite.Equal(precount, count)
+		suite.NoError(err)
+
+		suite.Equal(customCert.ID, clientCert.ID)
+		suite.Equal(customCert.Sha256Digest, clientCert.Sha256Digest)
+		suite.Equal(customCert.Subject, clientCert.Subject)
+	})
+
+	suite.Run("Two clientCerts should not be created", func() {
+		// Under test:      FetchOrBuildDevlocalClientCert
+		// Set up:          Create a clientCert with no customized state or traits
+		// Expected outcome:Only 1 clientCert should be created
+		count, potentialErr := suite.DB().Where(`sha256_digest=$1`, devlocalSha256Digest).Count(&models.ClientCert{})
+		suite.NoError(potentialErr)
+		suite.Zero(count)
+
+		firstClientCert := FetchOrBuildDevlocalClientCert(suite.DB())
+
+		secondClientCert := FetchOrBuildDevlocalClientCert(suite.DB())
+
+		suite.Equal(firstClientCert.ID, secondClientCert.ID)
+
+		existingClientCertCount, err := suite.DB().Where(`sha256_digest=$1`, devlocalSha256Digest).Count(&models.ClientCert{})
+		suite.NoError(err)
+		suite.Equal(1, existingClientCertCount)
 	})
 }
