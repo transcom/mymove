15bdb48b8c4c482ebc307089bcfebcb89213fe9b
Michael Mayo <mmayo@truss.works>
2023-05-10 18:09:08 +0000

    Adding foreign key ids to identify the correct columns on the service item and customer contact tables

--- pkg/models/service_items_customer_contacts.go
+++ pkg/models/service_items_customer_contacts.go
@@ -6,12 +6,16 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-// ServiceItemsCustomerContacts represents an MTOServiceItem and a MTOServiceItemCustomerContact
+// ServiceItemsCustomerContacts represents a relationship between a MTOServiceItem and a MTOServiceItemCustomerContact.
 type ServiceItemsCustomerContacts struct {
-	ID                              uuid.UUID                     `db:"id"`
-	MTOServiceItemID                uuid.UUID                     `db:"mtoservice_item_id"`
+	ID uuid.UUID `db:"id"`
+	// For many-to-many relationships, Pop will query for an id field on the join table based off of the names of the originating models.
+	// since MTOServiceItem and MTOServiceItemCustomerContact have four capital letters in front,
+	// Pop will not put an underscore between the "mto" and "service" in both ids. The columns on this table must exclude that underscore.
+	// This is why the db and fk_id flags are different.
+	MTOServiceItemID                uuid.UUID                     `db:"mtoservice_item_id" fk_id:"mto_service_item_id"`
 	MTOServiceItem                  MTOServiceItem                `belongs_to:"mto_service_items" db:"-"`
-	MTOServiceItemCustomerContactID uuid.UUID                     `db:"mtoservice_item_customer_contact_id"`
+	MTOServiceItemCustomerContactID uuid.UUID                     `db:"mtoservice_item_customer_contact_id" fk_id:"mto_service_item_customer_contact_id"`
 	MTOServiceItemCustomerContact   MTOServiceItemCustomerContact `belongs_to:"mto_service_items_customer_contacts" db:"-"`
 	CreatedAt                       time.Time                     `db:"created_at"`
 	UpdateAt                        time.Time                     `db:"updated_at"`
