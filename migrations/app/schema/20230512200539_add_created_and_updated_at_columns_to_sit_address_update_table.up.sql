02c38ecec6f1272c8392dd79879d630eeffdfb91
ruizajtruss <ruizaj@truss.works>
2023-05-12 22:00:00 +0000

    updated columns

--- migrations/app/schema/20230512200539_add_created_and_updated_at_columns_to_sit_address_update_table.up.sql
+++ migrations/app/schema/20230512200539_add_created_and_updated_at_columns_to_sit_address_update_table.up.sql
@@ -1,4 +1,5 @@
---Add new columns
+--Add new columns and remove reason column
 ALTER TABLE sit_address_updates
 ADD COLUMN created_at timestamp not null,
-ADD COLUMN updated_at timestamp not null;
+ADD COLUMN updated_at timestamp not null,
+DROP COLUMN reason;
