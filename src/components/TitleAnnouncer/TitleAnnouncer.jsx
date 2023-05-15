2203af96086f923ec3a8e209263c30fb2cd2fe5a
Scott Ries <scottmries@truss.works>
2023-05-15 17:17:04 +0000

    Update class name.

--- src/components/TitleAnnouncer/TitleAnnouncer.jsx
+++ src/components/TitleAnnouncer/TitleAnnouncer.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import styles from 'components/TitleAnnouncer/TitleAnnouncer.module.scss';
 
-const TitleAnnouncer = () => <div id="title-announcer" aria-live="polite" className={styles.TitleAnnouncer} />;
+const TitleAnnouncer = () => <div id="title-announcer" aria-live="polite" className={styles.HiddenTitleAnnouncer} />;
 
 export default TitleAnnouncer;
