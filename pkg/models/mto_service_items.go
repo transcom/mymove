21309e1b43692e0c2fb1e4a5dc9d3ab1822dfc0b
Michael Mayo <mmayo@truss.works>
2023-05-16 15:21:04 +0000

    Reapplying changes reverted in merge conflict resolution

--- pkg/models/mto_service_items.go
+++ pkg/models/mto_service_items.go
@@ -51,7 +51,7 @@ type MTOServiceItem struct {
 	EstimatedWeight                 *unit.Pound                    `db:"estimated_weight"`
 	ActualWeight                    *unit.Pound                    `db:"actual_weight"`
 	Dimensions                      MTOServiceItemDimensions       `has_many:"mto_service_item_dimensions" fk_id:"mto_service_item_id"`
-	CustomerContacts                MTOServiceItemCustomerContacts `has_many:"mto_service_item_customer_contacts" fk_id:"mto_service_item_id"`
+	CustomerContacts                MTOServiceItemCustomerContacts `many_to_many:"service_items_customer_contacts"`
 	SITAddressUpdates               SITAddressUpdates              `has_many:"sit_address_updates" fk_id:"mto_service_item_id"`
 	CreatedAt                       time.Time                      `db:"created_at"`
 	UpdatedAt                       time.Time                      `db:"updated_at"`
