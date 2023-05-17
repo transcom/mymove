577b4c859ae0448d7092019299ad393ef09752c8
Amina Foon <amina@truss.works>
2023-05-17 19:10:17 -0400

    Make the form for editing the shipment address and update stories

---
+++ src/components/Office/ServiceItemUpdateModal/EditSitAddressChangeForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import classnames from 'classnames';
+import { Alert } from '@trussworks/react-uswds';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { formatAddress } from '../../../utils/shipmentDisplay';
+import DataTableWrapper from '../../DataTableWrapper/index';
+import DataTable from '../../DataTable/index';
+
+import styles from './ServiceItemUpdateModal.module.scss';
+
+import AddressFields from 'components/form/AddressFields/AddressFields';
+
+const EditSitAddressChangeForm = ({ initialAddress }) => {
+  return (
+    <div className={styles.editSitAddressChangeForm}>
+      <DataTableWrapper className={classnames('maxw-tablet', 'table--data-point-group', styles.initialAddress)}>
+        <DataTable columnHeaders={['Initial SIT delivery address']} dataRow={[formatAddress(initialAddress)]} />
+      </DataTableWrapper>
+      <h3>Final SIT delivery</h3>
+      <Alert type="warning">Approvals over 50 miles will result in updated pricing for this shipment.</Alert>
+      <AddressFields name="initialAddress" />
+    </div>
+  );
+};
+
+export default EditSitAddressChangeForm;
--- src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.stories.jsx
+++ src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.stories.jsx
@@ -1,22 +1,59 @@
 import React from 'react';
 
 import ConnectedServiceItemUpdateModal, { ServiceItemUpdateModal } from './ServiceItemUpdateModal';
+import EditSitAddressChangeForm from './EditSitAddressChangeForm';
+
+const destinationSIT = {
+  id: 'abc123',
+  code: 'DDDSIT',
+  submittedAt: '2020-11-20',
+  serviceItem: 'Domestic destination SIT',
+  details: {
+    reason: "Customer's housing at base is not ready",
+    firstCustomerContact: { timeMilitary: '1200Z', firstAvailableDeliveryDate: '2020-09-15' },
+    secondCustomerContact: { timeMilitary: '2300Z', firstAvailableDeliveryDate: '2020-09-21' },
+    serviceItem: 'Domestic Destination SIT',
+  },
+};
+
+const initialAddress = {
+  city: 'Fairfax',
+  state: 'VA',
+  postalCode: '12345',
+  streetAddress1: '123 Fake Street',
+  streetAddress2: '',
+  streetAddress3: '',
+  country: 'USA',
+};
 
 export default {
   title: 'Office Components/ServiceItemUpdateModal',
+  component: ServiceItemUpdateModal,
+  args: {
+    closeModal: () => {},
+    onSave: () => {},
+    isOpen: true,
+    serviceItem: destinationSIT,
+  },
+  argTypes: {
+    onClose: { action: 'close button clicked' },
+    onSubmit: { action: 'submit button clicked' },
+  },
 };
 
-const defaultProps = {
-  closeModal: () => {},
-  onSave: () => {},
-  title: 'Edit Service Item',
+const Template = (args) => <ServiceItemUpdateModal {...args} />;
+export const ServiceItemUpdateModalStory = Template.bind({});
+ServiceItemUpdateModalStory.args = {
+  title: 'Title',
 };
 
-const Template = () => <ServiceItemUpdateModal {...defaultProps} />;
-export const ServiceItemUpdateModalStory = () => Template.bind();
-
+// Creates template of the modal
 const ConnectedTemplate = (args) => <ConnectedServiceItemUpdateModal {...args} />;
-export const ConnectedModal = ConnectedTemplate.bind({});
-ConnectedModal.args = {
-  isOpen: true,
+// Story for Editing service item address
+export const EditServiceItemAddress = ConnectedTemplate.bind({});
+EditServiceItemAddress.args = {
+  title: 'Edit Service Item',
+  content: <EditSitAddressChangeForm initialAddress={initialAddress} />,
 };
+
+// To-do: Setup story for Reviewing Service Item requests
