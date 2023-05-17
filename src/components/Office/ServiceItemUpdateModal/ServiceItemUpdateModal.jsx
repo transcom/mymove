9ae264f1e3238c6e027916a0038cad74528d228e
Amina Foon <amina@truss.works>
2023-05-17 19:08:42 -0400

    create all the shared portions of the service item modals

--- src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.jsx
+++ src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Label, Textarea, Tag } from '@trussworks/react-uswds';
+import { Button, Label, Textarea } from '@trussworks/react-uswds';
 import { Formik, Field } from 'formik';
+
+import { ServiceItemDetailsShape } from '../../../types/serviceItems';
+import ServiceItemDetails from '../ServiceItemDetails/ServiceItemDetails';
+
+import styles from './ServiceItemUpdateModal.module.scss';
 
 import Modal, { ModalActions, ModalClose, ModalTitle, connectModal } from 'components/Modal/Modal';
 import { Form } from 'components/form';
+import ShipmentTag from 'components/ShipmentTag/ShipmentTag';
+import { formatDateFromIso } from 'utils/formatters';
 
-// const SitAddressInfo = () => {
-//   return <div>Tersting</div>;
-// };
-
+const ServiceItemDetail = ({ serviceItem }) => {
+  const { id, code, submittedAt, details } = serviceItem;
+  return (
+    <table className={styles.serviceItemDetails}>
+      <tr key={id}>
+        <td className={styles.nameDateContainer}>
+          <p className={styles.serviceItemName}>{serviceItem.serviceItem}</p>
+          <p>{formatDateFromIso(submittedAt, 'DD MMM YYYY')}</p>
+        </td>
+        <td className={styles.detailsContainer}>
+          <ServiceItemDetails code={code} details={details} />
+        </td>
+      </tr>
+    </table>
+  );
+};
 /**
- * @description This componment is thee modal used for when a TOO edits the address for a Service item
+ * @description This componment is the modal used for when a TOO edits the address for a Service item
  * or reviews a service item request from a the prime.
  */
-export const ServiceItemUpdateModal = ({ onSave, closeModal, title }) => {
+export const ServiceItemUpdateModal = ({ onSave, closeModal, title, serviceItem, content }) => {
   const initialValues = {
     officeRemarks: '',
     newAddress: {
@@ -26,16 +45,21 @@ export const ServiceItemUpdateModal = ({ onSave, closeModal, title }) => {
     },
   };
   return (
-    <Modal>
+    <Modal className={styles.serviceItemUpdateModal}>
       <div>
-        <Tag>HHG</Tag>
+        <ShipmentTag shipmentType="HHG" />
         <ModalClose handleClick={() => closeModal()} />
       </div>
-      <ModalTitle>{title}</ModalTitle>
+      <ModalTitle className={styles.titleSection}>
+        <h2>{title}</h2>
+      </ModalTitle>
+      <ServiceItemDetail serviceItem={serviceItem} />
       <Formik onSubmit={(e) => onSave(e)} initialValues={initialValues}>
         {({ isValid }) => {
           return (
             <Form>
+              <h3>SIT delivery address</h3>
+              {content}
               <Label htmlFor="officeRemarks">Office remarks</Label>
               <Field as={Textarea} data-testid="officeRemarks" label="No" name="officeRemarks" id="officeRemarks" />
               <ModalActions>
@@ -56,6 +80,12 @@ ServiceItemUpdateModal.propTypes = {
   closeModal: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
+  serviceItem: ServiceItemDetailsShape.isRequired,
+  content: PropTypes.element,
+};
+
+ServiceItemUpdateModal.defaultProps = {
+  content: <div />,
 };
 
 ServiceItemUpdateModal.displayName = 'ServiceItemUpdateModal';
