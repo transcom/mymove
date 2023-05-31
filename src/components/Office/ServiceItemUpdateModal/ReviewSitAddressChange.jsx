d27daba82bf04712c5ec0284f3aa7994cbb2ca4b
ruizajtruss <ruizaj@truss.works>
2023-05-31 22:13:26 +0000

    styling is setup - need to add conditional logic to yellow tag

--- src/components/Office/ServiceItemUpdateModal/ReviewSitAddressChange.jsx
+++ src/components/Office/ServiceItemUpdateModal/ReviewSitAddressChange.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
-// import classnames from 'classnames';
+import classnames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Alert } from '@trussworks/react-uswds';
 import * as PropTypes from 'prop-types';
+
+import styles from './ServiceItemUpdateModal.module.scss';
 
 import DataTable from 'components/DataTable/index';
 // import DataTableWrapper from 'components/DataTableWrapper/index';
 import { formatAddress } from 'utils/shipmentDisplay';
 import { AddressShape } from 'types';
-// import styles from 'components/Office/ShipmentForm/ShipmentForm';
+import DataTableWrapper from 'components/DataTableWrapper';
+// import DataTableWrapper from 'components/DataTableWrapper';
 
 const ReviewSitAddressChange = ({ sitAddressUpdate }) => {
   const { oldAddress, newAddress, contractorRemarks, distance } = sitAddressUpdate;
 
   return (
-    <div>
-      <Alert type="warning">
+    <>
+      <Alert type="warning" style={{ marginBottom: '1rem' }}>
         Requested final SIT delivery address is {distance} miles from the initial SIT delivery address. Approvals over
         50 miles will result in updated pricing for this shipment.
       </Alert>
-      <DataTable
-        columnHeaders={['Initial SIT delivery address', 'Requested final SIT delivery address']}
-        dataRow={[formatAddress(oldAddress), formatAddress(newAddress)]}
-        icon={<FontAwesomeIcon icon="arrow-right" />}
-      />
-      <DataTable
-        columnHeaders={['Update request details']}
-        dataRow={[
-          <>
-            <b>Contractor remarks:</b> {contractorRemarks}
-          </>,
-        ]}
-      />
-    </div>
+      <DataTableWrapper className={classnames('maxw-tablet', 'table--data-point-group', styles.reviewAddressChange)}>
+        <DataTable
+          columnHeaders={['Initial SIT delivery address', 'Requested final SIT delivery address']}
+          dataRow={[formatAddress(oldAddress), formatAddress(newAddress)]}
+          icon={<FontAwesomeIcon icon="arrow-right" />}
+        />
+        <DataTable
+          columnHeaders={['Update request details']}
+          dataRow={[
+            <>
+              <b>Contractor remarks:</b> {contractorRemarks}
+            </>,
+          ]}
+        />
+      </DataTableWrapper>
+    </>
   );
 };
 
--- src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.jsx
+++ src/components/Office/ServiceItemUpdateModal/ServiceItemUpdateModal.jsx
@@ -87,9 +87,11 @@ export const ServiceItemUpdateModal = ({
           return (
             <Form className={formStyles.form}>
               <div className={styles.sitPanelForm}>
-                <div className={styles.modalReviewHeader}>
-                  <h3>SIT delivery address</h3>
-                  <Tag data-testid="sitAddressUpdateTag">UPDATE REQUESTED</Tag>
+                <div className={styles.modalHeader}>
+                  <h3 className={styles.modalReviewHeader}>SIT delivery address</h3>
+                  <Tag data-testid="sitAddressUpdateTag" className={styles.sitAddressUpdateTag}>
+                    UPDATE REQUESTED
+                  </Tag>
                 </div>
                 {content}
                 <Label htmlFor="officeRemarks">Office remarks</Label>
