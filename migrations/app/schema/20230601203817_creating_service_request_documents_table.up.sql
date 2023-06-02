2cfc093ccc63083e988e856d623f69a65ef9ba45
pearl-truss <pearl@truss.works>
2023-06-02 14:28:32 +0000

    modify service request documents table to remove unneeded upload_id column

--- migrations/app/schema/20230601203817_creating_service_request_documents_table.up.sql
+++ migrations/app/schema/20230601203817_creating_service_request_documents_table.up.sql
@@ -2,15 +2,12 @@ CREATE TABLE service_request_documents
 (
 	id uuid PRIMARY KEY,
 	mto_service_item_id uuid NOT NULL CONSTRAINT service_request_documents_mto_service_item_id_fkey REFERENCES mto_service_items (id),
-	upload_id uuid NOT NULL
-        constraint service_request_documents_upload_id_fkey references uploads,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
-    constraint service_request_documents_unique_key unique (mto_service_item_id, upload_id)
+    constraint service_request_documents_unique_key unique (mto_service_item_id)
 );
 
 -- Column Comments
-COMMENT on TABLE service_request_documents IS 'Stores uploads from the Prime that represent proof of a service item request';
+COMMENT on TABLE service_request_documents IS 'Associates uploads from the Prime that represent proof of a service item request';
 COMMENT on COLUMN service_request_documents.id IS 'uuid that represents this entity';
 COMMENT on COLUMN service_request_documents.mto_service_item_id IS 'Foreign key of the mto_service_items table';
-COMMENT on COLUMN service_request_documents.upload_id IS 'Foreign key of the uploads table';
