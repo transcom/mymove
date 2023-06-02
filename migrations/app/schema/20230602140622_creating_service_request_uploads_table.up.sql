ecc57455f496bc52dd2a59c841c32062ec825c60
pearl-truss <pearl@truss.works>
2023-06-02 16:17:06 +0000

    update migrations to match PR feedback

--- migrations/app/schema/20230602140622_creating_service_request_uploads_table.up.sql
+++ migrations/app/schema/20230602140622_creating_service_request_uploads_table.up.sql
@@ -1,7 +1,6 @@
 create table service_request_document_uploads
 (
-    id uuid not null
-        primary key,
+    id uuid primary key,
     service_request_documents_id uuid
         constraint service_request_documents_service_request_documents_id_fkey
             references service_request_documents not null,
@@ -16,4 +15,6 @@ create table service_request_document_uploads
 -- Column Comments
 COMMENT on TABLE service_request_document_uploads IS 'Stores uploads from the Prime that represent proof of a service item request';
 COMMENT on COLUMN service_request_document_uploads.id IS 'uuid that represents this entity';
+COMMENT on COLUMN service_request_document_uploads.contractor_id IS 'uuid that represents the contractor who provided the upload';
+COMMENT on COLUMN service_request_document_uploads.service_request_documents_id IS 'uuid that represents the associated service request document';
 COMMENT on COLUMN service_request_document_uploads.upload_id IS 'Foreign key of the uploads table';
