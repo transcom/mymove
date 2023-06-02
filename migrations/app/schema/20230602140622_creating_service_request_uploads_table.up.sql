ea287cb3455c17f79b22268b09b9a2efa4ab45bc
pearl-truss <pearl@truss.works>
2023-06-02 14:27:30 +0000

    create table for service request uploads

---
+++ migrations/app/schema/20230602140622_creating_service_request_uploads_table.up.sql
@@ -0,0 +1,19 @@
+create table service_request_document_uploads
+(
+    id uuid not null
+        primary key,
+    service_request_documents_id uuid
+        constraint service_request_documents_service_request_documents_id_fkey
+            references service_request_documents not null,
+    contractor_id uuid not null constraint service_request_documents_contractor_id_fkey references contractors,
+    upload_id uuid not null constraint service_request_documents_uploads_id_fkey references upload,
+    created_at timestamp not null,
+    updated_at timestamp not null,
+    deleted_at timestamp with time zone
+
+);
+
+-- Column Comments
+COMMENT on TABLE service_request_document_uploads IS 'Stores uploads from the Prime that represent proof of a service item request';
+COMMENT on COLUMN service_request_document_uploads.id IS 'uuid that represents this entity';
+COMMENT on COLUMN service_request_document_uploads.upload_id IS 'Foreign key of the uploads table';
