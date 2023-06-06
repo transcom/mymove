7b460fc55d283450ae4ddb95afdbd5a978e07a1d
Ronak Patel <ronak@truss.works>
2023-06-06 19:23:40 +0000

    Add migrations to introduce new param and remove old one for DDDSIT

---
+++ migrations/app/schema/20230605174844_delete_ZipDestAddress_for_DDDSIT_service_item.up.sql
@@ -0,0 +1,2 @@
+DELETE FROM service_params
+WHERE service_id = (SELECT id FROM re_services WHERE code = 'DDDSIT') AND service_item_param_key_id = (SELECT id FROM service_item_param_keys WHERE key = 'ZipDestAddress');
