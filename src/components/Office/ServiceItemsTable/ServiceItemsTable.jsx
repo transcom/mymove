8dc928323673ca993ddcc0f52c5e42f6cb8de67c
ruizajtruss <ruizaj@truss.works>
2023-06-07 16:52:26 +0000

    making alert full length of row

--- src/components/Office/ServiceItemsTable/ServiceItemsTable.jsx
+++ src/components/Office/ServiceItemsTable/ServiceItemsTable.jsx
@@ -69,7 +69,7 @@ const ServiceItemsTable = ({
           )}
         {ALLOWED_SIT_ADDRESS_UPDATE_SI_CODES.includes(code) && makeVisible && (
           <tr key={index}>
-            <td style={{ border: 'none', paddingLeft: '20.85rem', paddingBottom: '0' }} colSpan={3}>
+            <td style={{ border: 'none', paddingBottom: '0' }} colSpan={3}>
               <Alert type={alertType} slim>
                 {alertMessage}
               </Alert>
