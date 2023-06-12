04d3071a067780c3b23f19cea628b7aae59cc2f7
Yan Zhao <yan.zhao@truss.works>
2023-06-12 19:16:07 +0000

    fix: Remove constraint completely without adding a new one.

--- migrations/app/schema/20230612172652_alter_service_request_documents_table_change_contstraints.up.sql
+++ migrations/app/schema/20230612172652_alter_service_request_documents_table_change_contstraints.up.sql
@@ -1,4 +1,2 @@
 ALTER TABLE service_request_documents
-	DROP CONSTRAINT service_request_documents_unique_key,
-	ADD upload_id uuid CONSTRAINT service_request_documents_uploads_id_fkey REFERENCES uploads,
-	ADD CONSTRAINT proof_of_service_docs_unique_key UNIQUE (mto_service_item_id, upload_id);
+	DROP CONSTRAINT service_request_documents_unique_key;
