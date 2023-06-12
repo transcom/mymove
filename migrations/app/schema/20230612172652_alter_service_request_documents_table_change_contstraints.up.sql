9de9c9772cc9a423ab5daa4a4a3684da78bc0721
Yan Zhao <yan.zhao@truss.works>
2023-06-12 20:40:54 +0000

    feat: Add constraint on the upload_id for the service request documents uploads table.

--- migrations/app/schema/20230612172652_alter_service_request_documents_table_change_contstraints.up.sql
+++ migrations/app/schema/20230612172652_alter_service_request_documents_table_change_contstraints.up.sql
@@ -1,2 +1,6 @@
 ALTER TABLE service_request_documents
 	DROP CONSTRAINT service_request_documents_unique_key;
+
+ALTER TABLE service_request_document_uploads
+	ADD CONSTRAINT service_request_documents_uploads_unique_key UNIQUE (upload_id);
+
