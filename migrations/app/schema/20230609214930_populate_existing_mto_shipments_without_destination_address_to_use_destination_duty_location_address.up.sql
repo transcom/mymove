537a6b50a663ec53c35b7c8d7a368a69ff282d25
PDickeyTruss <pdickey@truss.works>
2023-06-14 07:18:53 -0800

    Change left joins to inner joins to prevent null address creation when a shipment has no move

--- migrations/app/schema/20230609214930_populate_existing_mto_shipments_without_destination_address_to_use_destination_duty_location_address.up.sql
+++ migrations/app/schema/20230609214930_populate_existing_mto_shipments_without_destination_address_to_use_destination_duty_location_address.up.sql
@@ -9,10 +9,10 @@ DECLARE
 
 BEGIN
 	FOR rec IN SELECT mto_shipments.id, city, state, postal_code, country FROM mto_shipments
-		LEFT JOIN moves ON mto_shipments.move_id = moves.id
-		LEFT JOIN orders ON moves.orders_id = orders.id
-		LEFT JOIN duty_locations ON orders.new_duty_location_id = duty_locations.id
-		LEFT JOIN addresses ON duty_locations.address_id = addresses.id
+		INNER JOIN moves ON mto_shipments.move_id = moves.id
+		INNER JOIN orders ON moves.orders_id = orders.id
+		INNER JOIN duty_locations ON orders.new_duty_location_id = duty_locations.id
+		INNER JOIN addresses ON duty_locations.address_id = addresses.id
 		WHERE destination_address_id IS NULL AND shipment_type = 'HHG'
 
 		LOOP
