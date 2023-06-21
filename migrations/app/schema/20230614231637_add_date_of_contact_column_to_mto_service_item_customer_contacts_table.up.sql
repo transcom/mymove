0f15773e02e5bdc1a9e80b11fa8bdf16b587d2a0
ruizajtruss <ruizaj@truss.works>
2023-06-21 23:35:29 +0000

    updated comment in migration

--- migrations/app/schema/20230614231637_add_date_of_contact_column_to_mto_service_item_customer_contacts_table.up.sql
+++ migrations/app/schema/20230614231637_add_date_of_contact_column_to_mto_service_item_customer_contacts_table.up.sql
@@ -3,4 +3,5 @@ ALTER TABLE mto_service_item_customer_contacts
 ADD COLUMN date_of_contact timestamptz NOT NULL;
 
 -- Comment On Column
-COMMENT ON COLUMN mto_service_item_customer_contacts.date_of_contact IS 'The date of attempted contact with the customer by the prime';
+COMMENT ON COLUMN mto_service_item_customer_contacts.date_of_contact IS 'The date of attempted contact with the customer by the prime corresponding to the time_military column';
+COMMENT ON COLUMN mto_service_item_customer_contacts.time_military IS 'The time of attempted contact with the customer by the prime, in military format (HHMMZ), corresponding to the date_of_contact column';
