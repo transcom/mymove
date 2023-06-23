acb3df0fd8f8d67e8ab039953b2401e91002b4dd
ruizajtruss <ruizaj@truss.works>
2023-06-23 03:56:43 +0000

    edited migration to handle null values

--- migrations/app/schema/20230614231637_add_date_of_contact_column_to_mto_service_item_customer_contacts_table.up.sql
+++ migrations/app/schema/20230614231637_add_date_of_contact_column_to_mto_service_item_customer_contacts_table.up.sql
@@ -1,6 +1,14 @@
 -- New Column
 ALTER TABLE mto_service_item_customer_contacts
-ADD COLUMN date_of_contact timestamptz NOT NULL;
+ADD COLUMN date_of_contact timestamptz NULL;
+
+-- Copying data for null columns
+UPDATE mto_service_item_customer_contacts SET
+	date_of_contact = first_available_delivery_date
+WHERE date_of_contact IS NULL;
+
+-- Setting column to not null after updating null values
+ALTER TABLE mto_service_item_customer_contacts ALTER COLUMN date_of_contact SET NOT NULL;
 
 -- Comment On Column
 COMMENT ON COLUMN mto_service_item_customer_contacts.date_of_contact IS 'The date of attempted contact with the customer by the prime corresponding to the time_military column';
