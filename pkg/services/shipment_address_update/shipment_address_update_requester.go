d3c9aa11a79dc0e13d81597771bad7bada7d0af7
Marty Boren <marty@truss.works>
2023-06-28 21:30:32 +0000

    Make sure NewAddress is populated in response to first request

--- pkg/services/shipment_address_update/shipment_address_update_requester.go
+++ pkg/services/shipment_address_update/shipment_address_update_requester.go
@@ -172,6 +172,7 @@ func (f *shipmentAddressUpdateRequester) RequestShipmentDeliveryAddressUpdate(ap
 		return nil, err
 	}
 	addressUpdate.NewAddressID = address.ID
+	addressUpdate.NewAddress = *address
 
 	contract, err := serviceparamvaluelookups.FetchContract(appCtx, *shipment.MoveTaskOrder.AvailableToPrimeAt)
 	if err != nil {
