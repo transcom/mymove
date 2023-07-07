c5965148b82cbef0cb58811cea379c5ff08945e9
Michael Mayo <mmayo@truss.works>
2023-07-07 21:26:05 +0000

    Removed required status for first name prop

--- src/types/agent.js
+++ src/types/agent.js
@@ -2,7 +2,7 @@ import { string, shape } from 'prop-types';
 
 export const AgentShape = shape({
   agentType: string.isRequired,
-  firstName: string.isRequired,
+  firstName: string,
   lastName: string,
   phone: string,
   email: string,
