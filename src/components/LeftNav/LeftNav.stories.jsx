7b7fcce10450bca4f1f6ab7a9dd7b49546431f0d
Arianna Kellogg <ari@truss.works>
2023-07-10 18:43:01 +0000

    MB-16267 reorder LeftNav storybook basic, add example with alert on approved shipments

--- src/components/LeftNav/LeftNav.stories.jsx
+++ src/components/LeftNav/LeftNav.stories.jsx
@@ -13,8 +13,26 @@ export default {
 
 export const Basic = () => (
   <div id="l-nav" style={{ padding: '20px', background: '#f0f0f0' }}>
-    <LeftNav sections={['allowances', 'requested-shipments', 'orders', 'customer-info']}>
+    <LeftNav sections={['requested-shipments', 'orders', 'allowances', 'customer-info']}>
       <LeftNavTag associatedSectionName="requested-shipments" showTag className="usa-tag usa-tag--alert">
+        <FontAwesomeIcon icon="exclamation" />
+      </LeftNavTag>
+
+      <LeftNavTag associatedSectionName="orders" showTag className="usa-tag--teal">
+        INTL
+      </LeftNavTag>
+
+      <LeftNavTag associatedSectionName="customer-info" showTag>
+        3
+      </LeftNavTag>
+    </LeftNav>
+  </div>
+);
+
+export const WithAlert = () => (
+  <div id="l-nav" style={{ padding: '20px', background: '#f0f0f0' }}>
+    <LeftNav sections={['approved-shipments', 'orders', 'allowances', 'customer-info']}>
+      <LeftNavTag associatedSectionName="approved-shipments" showTag className="usa-tag usa-tag--alert">
         <FontAwesomeIcon icon="exclamation" />
       </LeftNavTag>
 
