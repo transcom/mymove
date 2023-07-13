e9ead02cb3e758112a0df7bae9e9fde92c94ab0e
belle <belle@truss.works>
2023-07-13 17:53:22 +0000

    WIP rules.go, writing a check to prevent payment requests for same service item with status of pending or paid

--- pkg/services/payment_request/rules.go
+++ pkg/services/payment_request/rules.go
@@ -31,3 +31,37 @@ func checkMTOIDMatchesServiceItemMTOID() paymentRequestValidator {
 		return nil
 	})
 }
+
+func checkStatusOfPaymentRequest() paymentRequestValidator {
+	return paymentRequestValidatorFunc(func(_ appcontext.AppContext, paymentRequest models.PaymentRequest, oldPaymentRequest *models.PaymentRequest) error {
+		var paymentRequestServiceItems = paymentRequest.PaymentServiceItems
+		for _, paymentRequestServiceItem := range paymentRequestServiceItems {
+			// fmt.Println(paymentRequest.PaymentServiceItems[0].MTOServiceItemID)
+			// fmt.Println(paymentRequestServiceItem.MTOServiceItemID)
+			// if paymentRequest.PaymentServiceItems[0].MTOServiceItemID == paymentRequestServiceItem.MTOServiceItemID && (paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPending || paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPaid) {
+			if paymentRequest.PaymentServiceItems[0].MTOServiceItemID == paymentRequestServiceItem.MTOServiceItemID && paymentRequestServiceItem.MTOServiceItemID != uuid.Nil {
+
+				// if paymentRequestServiceItem.Status == models.PaymentServiceItemStatusRequested || paymentRequestServiceItem.Status == models.PaymentServiceItemStatusPaid {
+
+				// 	return apperror.NewConflictError(paymentRequestServiceItem.MTOServiceItemID, "Conflict Error: Payment Request for service item is already paid or requested")
+				// }
+				// if paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPending || paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPaid {
+				// }
+				// if paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPending || paymentRequestServiceItem.PaymentRequest.Status == models.PaymentRequestStatusPaid {
+				return apperror.NewConflictError(paymentRequestServiceItem.MTOServiceItemID, "Conflict Error: Payment Request for service item is already paid or requested")
+				// }
+				// if paymentRequestServiceItem.MTOServiceItem.ReService.Code == models.ReServiceCodeDDASIT || paymentRequestServiceItem.MTOServiceItem.ReService.Code == models.ReServiceCodeDOASIT {
+				//     //   newStart, newEnd, newErr := getStartAndEndParams(paymentRequestServiceItem.PaymentServiceItemParams)
+				// 	newStart, newEnd, _ := getStartAndEndParams(paymentRequestServiceItem.PaymentServiceItemParams)
+				// 	if paymentRequestServiceItem.Status == models.PaymentServiceItemStatusRequested || paymentRequestServiceItem.Status == models.PaymentServiceItemStatusPaid {
+				// 		// start, end, err := getStartAndEndParams(paymentRequestServiceItem.PaymentServiceItemParams)
+				// 		start, end, _ := getStartAndEndParams(paymentRequestServiceItem.PaymentServiceItemParams)
+				// 		fmt.Println(start, end, newStart, newEnd)
+
+				// 		// inTimeFrame := time.After(start) && time.Before(end)
+				// 	}
+			}
+		}
+		return nil
+	})
+}
