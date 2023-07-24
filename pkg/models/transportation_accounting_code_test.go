51721a46e88d7b516c07f391bfef7c3ed9638618
Brandon Lenz <brandon@truss.works>
2023-07-24 13:25:26 -0400

    Invalid TAC unit test

--- pkg/models/transportation_accounting_code_test.go
+++ pkg/models/transportation_accounting_code_test.go
@@ -6,13 +6,25 @@ import (
 	"github.com/transcom/mymove/pkg/models"
 )
 
-func (suite *ModelSuite) Test_ValidateTac() {
+func (suite *ModelSuite) Test_ValidTac() {
 	tac := models.TransportationAccountingCode{
 		ID:  uuid.Must(uuid.NewV4()),
 		TAC: "TheTac",
 	}
 
 	expErrors := map[string][]string{}
+
+	suite.verifyValidationErrors(&tac, expErrors)
+}
+
+func (suite *ModelSuite) Test_InvalidTac() {
+	tac := models.TransportationAccountingCode{
+		ID: uuid.Must(uuid.NewV4()),
+	}
+
+	expErrors := map[string][]string{
+		"TAC": {"TAC can not be blank."},
+	}
 
 	suite.verifyValidationErrors(&tac, expErrors)
 }
