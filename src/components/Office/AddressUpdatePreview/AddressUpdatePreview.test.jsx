9903046f0718effa7bc866ad655290ae54b29d93
PDickeyTruss <pdickey@truss.works>
2023-07-25 23:45:42 +0000

    Fix broken stub test

--- src/components/Office/AddressUpdatePreview/AddressUpdatePreview.test.jsx
+++ src/components/Office/AddressUpdatePreview/AddressUpdatePreview.test.jsx
@@ -5,7 +5,15 @@ import AddressUpdatePreview from './AddressUpdatePreview';
 
 describe('AddressUpdatePreview', () => {
   it('does a thing', () => {
-    render(<AddressUpdatePreview />);
+    render(
+      <AddressUpdatePreview
+        deliveryAddressUpdate={{
+          originalAddress: { city: '', state: '', postalCode: '', streetAddress1: '', streetAddress2: '' },
+          newAddress: { city: '', state: '', postalCode: '', streetAddress1: '', streetAddress2: '' },
+          contractorRemarks: '',
+        }}
+      />,
+    );
 
     expect(true).toBe(true);
   });
