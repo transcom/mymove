404edf1ff391d96099b352f003721f61f77d1057
DymonMoore <dmoore@truss.works>
2023-07-26 20:17:38 +0000

    fix failing client tests.

--- src/pages/MyMove/Agreement.test.jsx
+++ src/pages/MyMove/Agreement.test.jsx
@@ -67,7 +67,7 @@ describe('Agreement page', () => {
     submitMoveForApproval.mockRejectedValueOnce({ errors: { signature: 'Signature can not be blank.' } });
 
     render(<Agreement {...testProps} />);
-    await userEvent.type(screen.getByLabelText('Signature'), 'Sofia Clark-Nuñez');
+    await userEvent.type(screen.getByLabelText('SIGNATURE'), 'Sofia Clark-Nuñez');
     await userEvent.click(screen.getByRole('button', { name: 'Complete' }));
 
     await waitFor(() => {
