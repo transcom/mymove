a2fc9cb0f6288f82177ef1151e585ca17185bcfc
PDickeyTruss <pdickey@truss.works>
2023-07-26 21:24:50 +0000

    Wire up address update review form to use and validate form fields

--- src/components/Office/ShipmentAddressUpdateReviewRequestModal/ShipmentAddressUpdateReviewRequestModal.jsx
+++ src/components/Office/ShipmentAddressUpdateReviewRequestModal/ShipmentAddressUpdateReviewRequestModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Textarea, Label, FormGroup, Radio } from '@trussworks/react-uswds'; // Tag Label
 import { Formik, Field } from 'formik';
+import * as Yup from 'yup';
 
 import styles from './ShipmentAddressUpdateReviewRequestModal.module.scss';
 
@@ -10,6 +11,11 @@ import formStyles from 'styles/form.module.scss';
 import AddressUpdatePreview from 'components/Office/AddressUpdatePreview/AddressUpdatePreview';
 import ShipmentTag from 'components/ShipmentTag/ShipmentTag';
 
+const formSchema = Yup.object().shape({
+  addressUpdate: Yup.string().required('Required'),
+  officeRemarks: Yup.string().required('Required'),
+});
+
 export const ShipmentAddressUpdateReviewRequestModal = ({ deliveryAddressUpdate, shipmentType, onClose }) => {
   return (
     <Modal>
@@ -18,38 +24,54 @@ export const ShipmentAddressUpdateReviewRequestModal = ({ deliveryAddressUpdate,
         <ShipmentTag shipmentType={shipmentType} />
         <h2 className={styles.modalTitle}>Review request</h2>
       </ModalTitle>
-      <Formik>
-        <Form className={formStyles.form}>
-          <div className={styles.modalbody}>
-            <AddressUpdatePreview deliveryAddressUpdate={deliveryAddressUpdate} shipmentType={shipmentType} />
-            <FormGroup>
-              <h4>Review Request</h4>
-              <Label>Approve address change?</Label>
-              <div data-testid="reviewSITAddressUpdateForm">
-                <Field
-                  as={Radio}
-                  label="Yes"
-                  id="acceptAddressUpdate"
-                  name="sitAddressUpdate"
-                  value="YES"
-                  type="radio"
-                />
-                <Field as={Radio} label="No" id="rejectAddressUpdate" name="sitAddressUpdate" value="NO" type="radio" />
+      <Formik
+        initialValues={{ addressUpdate: '', officeRemarks: '' }}
+        onSubmit={() => {}}
+        validateOnMount
+        validationSchema={formSchema}
+      >
+        {({ isValid }) => {
+          return (
+            <Form className={formStyles.form}>
+              <div className={styles.modalbody}>
+                <AddressUpdatePreview deliveryAddressUpdate={deliveryAddressUpdate} shipmentType={shipmentType} />
+                <FormGroup>
+                  <h4>Review Request</h4>
+                  <Label>Approve address change?</Label>
+                  <div>
+                    <Field
+                      as={Radio}
+                      label="Yes"
+                      id="acceptAddressUpdate"
+                      name="addressUpdate"
+                      value="YES"
+                      type="radio"
+                    />
+                    <Field
+                      as={Radio}
+                      label="No"
+                      id="rejectAddressUpdate"
+                      name="addressUpdate"
+                      value="NO"
+                      type="radio"
+                    />
+                  </div>
+                </FormGroup>
+                <Label htmlFor="officeRemarks">Office remarks</Label>
+                <p style={{ fontSize: 'small' }}>Office remarks will be sent to the contractor.</p>
+                <Field as={Textarea} data-testid="officeRemarks" label="No" name="officeRemarks" id="officeRemarks" />
               </div>
-            </FormGroup>
-            <Label htmlFor="officeRemarks">Office remarks</Label>
-            <p style={{ fontSize: 'small' }}>Office remarks will be sent to the contractor.</p>
-            <Field as={Textarea} data-testid="officeRemarks" label="No" name="officeRemarks" id="officeRemarks" />
-          </div>
-          <ModalActions>
-            <Button type="submit" disabled={false}>
-              Save
-            </Button>
-            <Button type="button" secondary onClick={() => onClose()}>
-              Cancel
-            </Button>
-          </ModalActions>
-        </Form>
+              <ModalActions>
+                <Button type="submit" disabled={!isValid}>
+                  Save
+                </Button>
+                <Button type="button" secondary onClick={onClose}>
+                  Cancel
+                </Button>
+              </ModalActions>
+            </Form>
+          );
+        }}
       </Formik>
     </Modal>
   );
