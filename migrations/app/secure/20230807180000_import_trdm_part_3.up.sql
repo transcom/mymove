a3286fa839f2c7bf4e6db229c672865b5b31b4e1
Marty Boren <marty@truss.works>
2023-08-07 18:50:31 +0000

    Split migration into multiple files to appease virus scanner
    The virus scanner we use on S3 has a max file size of 250MB
    The production migration was much larger than that, so I had to split
    it into 3 smaller migrations.

---
+++ migrations/app/secure/20230807180000_import_trdm_part_3.up.sql
@@ -0,0 +1,7 @@
+-- Local test migration.
+-- This will be run on development environments.
+-- It should mirror what you intend to apply on loadtest/demo/exp/stg/prd
+-- DO NOT include any sensitive data.
+
+-- We had to split this migration into multiple parts because it was too large for the virus scanner.
+-- Locally, we only create a few records, so we don't need anything in parts 2 and 3 of this migration.
