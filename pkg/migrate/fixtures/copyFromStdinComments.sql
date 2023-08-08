78f7e567ac01ff5bb5951348c80d126042713919
Marty Boren <marty@truss.works>
2023-08-08 23:03:25 +0000

    Fix SQL parser to allow -- and SQL keywords in COPY FROM stdin data

---
+++ pkg/migrate/fixtures/copyFromStdinComments.sql
@@ -0,0 +1,4 @@
+-- this is a comment
+COPY public.re_services (id, code, name, created_at, updated_at, priority) FROM stdin;
+fc050966-171e-4ab5-a4fd-e81947ce9a19	14	Tw -- elve	2020-03-23 16:31:50.313853	2020-03-23 16:31:50.313853	1
+\.
