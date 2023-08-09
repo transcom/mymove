030001f0b8e4fe91dac82540b8ca06aba931d02a
Marty Boren <marty@truss.works>
2023-08-09 16:19:02 +0000

    Add unique pkey index to transportation_codes.id
    Also move TAC index creation after loading data.

--- migrations/app/secure/20230807180000_import_trdm_part_3.up.sql
+++ migrations/app/secure/20230807180000_import_trdm_part_3.up.sql
@@ -4,4 +4,10 @@
 -- DO NOT include any sensitive data.
 
 -- We had to split this migration into multiple parts because it was too large for the virus scanner.
--- Locally, we only create a few records, so we don't need anything in parts 2 and 3 of this migration.
+-- Locally, we only create a few records, so we don't need to load any data in parts 2 and 3 of this migration.
+
+-- Create indices after loading data
+CREATE INDEX transportation_accounting_codes_tac_idx ON transportation_accounting_codes (tac);
+
+ALTER TABLE transportation_accounting_codes
+	ADD CONSTRAINT transportation_acounting_codes_pkey PRIMARY KEY (id);
