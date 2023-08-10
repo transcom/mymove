6f6a667f3ef6eac430fb687646c28733c855be18
Michael Mayo <mmayo@truss.works>
2023-08-10 15:13:20 +0000

    Modified url for email link

--- pkg/notifications/move_issued_to_prime.go
+++ pkg/notifications/move_issued_to_prime.go
@@ -76,7 +76,7 @@ func (m MoveIssuedToPrime) emails(appCtx appcontext.AppContext) ([]emailContent,
 	}
 
 	htmlBody, textBody, err := m.renderTemplates(appCtx, moveIssuedToPrimeEmailData{
-		MilitaryOneSourceLink:        "https://www.militaryonesource.mil/benefits/militaryinstallations-local-transportation-office/",
+		MilitaryOneSourceLink:        "https://installations.militaryonesource.mil/search?program-service=2/view-by=ALL",
 		OriginDutyLocation:           originDutyLocation,
 		DestinationDutyLocation:      orders.NewDutyLocation.Name,
 		ProvidesGovernmentCounseling: providesGovernmentCounseling,
