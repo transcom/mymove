bc33a5e7c279ba87039c01f7c7d3157889c590bc
Yan Zhao <yan.zhao@truss.works>
2023-08-11 19:56:04 +0000

    test: use suite.T().Setenv instead of os.Setenv since we want setting the environment variables to be temporary

--- pkg/cli/gex_sftp_test.go
+++ pkg/cli/gex_sftp_test.go
@@ -1,28 +1,19 @@
 package cli
 
-import (
-	"os"
-)
-
 func (suite *cliTestSuite) TestConfigGEXSFTP() {
 	suite.Setup(InitGEXSFTPFlags, []string{})
 
-	err := os.Setenv("GEX_SFTP_PORT", "1234")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_PORT", "1234")
 
-	err = os.Setenv("GEX_SFTP_IP_ADDRESS", "127.0.0.1")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_IP_ADDRESS", "127.0.0.1")
 
-	err = os.Setenv("GEX_SFTP_USER_ID", "FAKE_USER_ID")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_USER_ID", "FAKE_USER_ID")
 
-	err = os.Setenv("GEX_SFTP_PASSWORD", "FAKE PASSWORD")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_PASSWORD", "FAKE PASSWORD")
 
 	// generated fake host key to pass parser used following command and only saved the pub key
 	//   ssh-keygen -q -N "" -t ecdsa -f /tmp/ssh_host_ecdsa_key
-	err = os.Setenv("GEX_SFTP_HOST_KEY", "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBI+M4xIGU6D4On+Wxz9k/QT12TieNvaXA0lvosnW135MRQzwZp5VDThQ6Vx7yhp18shgjEIxFHFTLxpmUc6JdMc= fake@localhost")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_HOST_KEY", "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBI+M4xIGU6D4On+Wxz9k/QT12TieNvaXA0lvosnW135MRQzwZp5VDThQ6Vx7yhp18shgjEIxFHFTLxpmUc6JdMc= fake@localhost")
 
 	suite.NoError(CheckGEXSFTP(suite.viper))
 }
