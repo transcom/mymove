8614fb9d735d941fdbf1ebabd58f068b2e4c6696
Yan Zhao <yan.zhao@truss.works>
2023-08-11 19:51:52 +0000

    test: swap things over to use suite.T().Setenv instead of os.Setenv since we want the setting of environment variables to be temporary

--- pkg/cli/gex_sftp_test.go
+++ pkg/cli/gex_sftp_test.go
@@ -5,23 +5,18 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
-	"os"
 )
 
 func (suite *cliTestSuite) TestConfigGEXSFTP() {
 	suite.Setup(InitGEXSFTPFlags, []string{})
 
-	err := os.Setenv("GEX_SFTP_PORT", "1234")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_PORT", "1234")
 
-	err = os.Setenv("GEX_SFTP_IP_ADDRESS", "127.0.0.1")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_IP_ADDRESS", "127.0.0.1")
 
-	err = os.Setenv("GEX_SFTP_USER_ID", "FAKE_USER_ID")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_USER_ID", "FAKE_USER_ID")
 
-	err = os.Setenv("GEX_SFTP_PASSWORD", "FAKE PASSWORD")
-	suite.Require().Nil(err)
+	suite.T().Setenv("GEX_SFTP_PASSWORD", "FAKE PASSWORD")
 
 	// Generate a key for testing
 	bitSize := 4096
@@ -37,12 +32,12 @@ func (suite *cliTestSuite) TestConfigGEXSFTP() {
 		},
 	)
 
-	err = os.Setenv("GEX_PRIVATE_KEY", string(keyPEM))
+	suite.T().Setenv("GEX_PRIVATE_KEY", string(keyPEM))
 	suite.Require().Nil(err)
 
 	// generated fake host key to pass parser used following command and only saved the pub key
 	//   ssh-keygen -q -N "" -t ecdsa -f /tmp/ssh_host_ecdsa_key
-	err = os.Setenv("GEX_SFTP_HOST_KEY", "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBI+M4xIGU6D4On+Wxz9k/QT12TieNvaXA0lvosnW135MRQzwZp5VDThQ6Vx7yhp18shgjEIxFHFTLxpmUc6JdMc= fake@localhost")
+	suite.T().Setenv("GEX_SFTP_HOST_KEY", "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBI+M4xIGU6D4On+Wxz9k/QT12TieNvaXA0lvosnW135MRQzwZp5VDThQ6Vx7yhp18shgjEIxFHFTLxpmUc6JdMc= fake@localhost")
 	suite.Require().Nil(err)
 
 	suite.Run("pass without errors if the environment is set up with valid values", func() {
@@ -51,16 +46,14 @@ func (suite *cliTestSuite) TestConfigGEXSFTP() {
 
 	suite.Run("fail with an error when given an invalid private key", func() {
 		// Override what was set earlier with a fake key
-		err = os.Setenv("GEX_PRIVATE_KEY", "FAKEKEY")
-		suite.Require().Nil(err)
+		suite.T().Setenv("GEX_PRIVATE_KEY", "FAKEKEY")
 
 		suite.Error(CheckGEXSFTP(suite.viper), "no key found")
 	})
 
 	suite.Run("fail with an error when given an invalid host key", func() {
 		// Override what was set earlier with a fake key
-		err = os.Setenv("GEX_SFTP_HOST_KEY", "FAKEKEY")
-		suite.Require().Nil(err)
+		suite.T().Setenv("GEX_SFTP_HOST_KEY", "FAKEKEY")
 
 		suite.Error(CheckGEXSFTP(suite.viper), "no key found")
 	})
