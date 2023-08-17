d6fcff3e149c6db9825c3e404a973f3346677ad3
Daniel Jordan <daniel.jordan@caci.com>
2023-08-17 15:08:40 +0000

    cleaned up some code and added comments

--- pkg/handlers/authentication/auth.go
+++ pkg/handlers/authentication/auth.go
@@ -495,8 +495,6 @@ func NewLogoutHandler(ac Context, hc handlers.HandlerConfig) LogoutHandler {
 // !Needs to be finalized after sessions.
 func (h LogoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	appCtx := h.AppContextFromRequest(r)
-	fmt.Println(appCtx.Session().IDToken)
-	fmt.Println(appCtx.Session().AccessToken)
 	if appCtx.Session() != nil {
 		sessionManager := h.SessionManagers().SessionManagerForApplication(appCtx.Session().ApplicationName)
 		if sessionManager == nil {
@@ -507,26 +505,10 @@ func (h LogoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		redirectURL := h.landingURL(appCtx.Session())
 		if appCtx.Session().IDToken != "" {
 			var logoutURL string
-			// clearing okta.mil sessions
+			// clearing okta.mil sessions by clearing Access Token & ID Token
+			// this is shown in a sample app here: https://github.com/okta/samples-golang/blob/master/okta-hosted-login/main.go
 			appCtx.Session().AccessToken = ""
 			appCtx.Session().IDToken = ""
-			// All users logged in via devlocal-auth will have this IDToken. We
-			// don't want to make a call to okta.mil for a logout URL as it will
-			// fail for devlocal-auth'ed users.
-			if appCtx.Session().IDToken == "devlocal" {
-				logoutURL = redirectURL
-			} else {
-				// provider, err := okta.GetOktaProviderForRequest(r)
-				// if err != nil {
-				// 	appCtx.Logger().Error("get provider", zap.Error(err))
-				// 	http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-				// 	return
-				// }
-				// logoutURL, err = h.oktaProvider.LogoutURL(*provider, redirectURL)
-				// if err != nil {
-				// 	appCtx.Logger().Error("failed to retrieve logout url from provider")
-				// }
-			}
 			if appCtx.Session().UserID != uuid.Nil {
 				err := resetUserCurrentSessionID(appCtx)
 				if err != nil {
