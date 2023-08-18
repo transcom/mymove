df427cedae193f6954c94086b33dd5189e469116
Roger Steve Ruiz <rogeruiz@truss.works>
2023-08-18 17:42:49 +0000

    Upgrading Bootstrap to 5.2.3;
    This upgrade was done in order to fix the bug here, twbs/boostrap#35297.

--- pkg/handlers/testharnessapi/api.go
+++ pkg/handlers/testharnessapi/api.go
@@ -47,7 +47,7 @@ func NewDefaultBuilder(handlerConfig handlers.HandlerConfig) http.Handler {
 				t := template.Must(template.New("users").Parse(`
 				  <html>
 				  <head>
-					<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 				  </head>
 				  <body class="py-4">
 					<div class="container">
@@ -78,7 +78,7 @@ func NewBuilderList(handlerConfig handlers.HandlerConfig) http.Handler {
 			t := template.Must(template.New("actions").Parse(`
 	  <html>
 	  <head>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 	  </head>
 	  <body class="py-4">
 		<div class="container">
