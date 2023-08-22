809059c282b0307359ef84a37a4bf1b7b597c22b
Roger Steve Ruiz <rogeruiz@truss.works>
2023-08-22 00:25:45 +0000

    Fixing the tests by accounting for the new errors;
    This was the only change I had to make to the tests to get things to pass. We
    still need tests around this new calculation.

--- pkg/services/ghcrateengine/domestic_destination_pricer_test.go
+++ pkg/services/ghcrateengine/domestic_destination_pricer_test.go
@@ -226,7 +226,8 @@ func (suite *GHCRateEngineServiceSuite) TestPriceDomesticDestination() {
 		)
 
 		suite.Error(err)
-		suite.Equal("Could not lookup contract year: "+models.RecordNotFoundErrorString, err.Error())
+		suite.Equal("Could not look up escalated price: could not lookup contract year: "+models.RecordNotFoundErrorString, err.Error())
+
 	})
 
 	suite.Run("fail when is weight below minimum and shipment isn't a PPM", func() {
