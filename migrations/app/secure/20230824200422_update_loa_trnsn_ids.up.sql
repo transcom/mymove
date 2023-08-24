b0c135c343dfcf6c714847e00de1b652bcc71992
Brandon Lenz <brandon@truss.works>
2023-08-24 21:28:10 +0000

    Update dummy data to have a 3-digit loa_trnsn_id

--- migrations/app/secure/20230824200422_update_loa_trnsn_ids.up.sql
+++ migrations/app/secure/20230824200422_update_loa_trnsn_ids.up.sql
@@ -4,7 +4,7 @@
 -- DO NOT include any sensitive data.
 
 -- Update loa_trnsn_id column constraint
-ALTER TABLE lines_of_accounting ALTER COLUMN loa_trnsn_id TYPE char (2);
+ALTER TABLE lines_of_accounting ALTER COLUMN loa_trnsn_id TYPE char (3);
 -- Update lines_of_accounting with updated loa_trnsn_id values, mapped by loa_sys_id
 UPDATE lines_of_accounting AS loas SET
 	loa_trnsn_id = updated.loa_trnsn_id
@@ -16,6 +16,6 @@ FROM (VALUES
 	(10005, 'C1'),
 	(10006, 'AB'),
 	(10007, NULL),
-	(10008, 'CC')
+	(10008, 'C12')
 ) AS updated(loa_sys_id, loa_trnsn_id)
 WHERE updated.loa_sys_id = loas.loa_sys_id;
