d68edaa749e4acab982e61846001c075aedeabab
Ronak Patel <ronak@truss.works>
2023-08-24 17:38:37 +0000

    Add missing semicolon

--- migrations/app/schema/20230822201206_add_fields_for_sit_to_mto_service_items.up.sql
+++ migrations/app/schema/20230822201206_add_fields_for_sit_to_mto_service_items.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE mto_service_items
 	ADD COLUMN sit_customer_contacted date,
-	ADD COLUMN sit_requested_delivery date
+	ADD COLUMN sit_requested_delivery date;
 
 -- Comment On Column
 COMMENT ON COLUMN mto_service_items.sit_customer_contacted IS 'The date when the customer contacted the prime for a delivery out of SIT';
