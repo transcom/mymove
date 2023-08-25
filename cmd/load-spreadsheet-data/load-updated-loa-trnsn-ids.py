aa2f0d3d720381ff6869918dd196a5b444193a82
Brandon Lenz <brandon@truss.works>
2023-08-25 18:14:23 +0000

    Greatly simplify filename generation

--- cmd/load-spreadsheet-data/load-updated-loa-trnsn-ids.py
+++ cmd/load-spreadsheet-data/load-updated-loa-trnsn-ids.py
@@ -1,8 +1,6 @@
 import os, sys, pandas as pd
 from datetime import datetime
 
-ZERO = '0'
-
 if len(sys.argv) < 2:
     sys.exit("Input file required.")
 
@@ -10,14 +8,7 @@ if len(sys.argv) < 2:
 # Generate to gitignored tmp directory to prevent committing secure data
 current_dir = os.getcwd()
 destination_directory = 'tmp/generated-secure-migrations'
-now = datetime.now()
-year = str(now.year).rjust(4, ZERO)
-month = str(now.month).rjust(2, ZERO)
-day = str(now.day).rjust(2, ZERO)
-hour = str(now.hour).rjust(2, ZERO)
-minute = str(now.minute).rjust(2, ZERO)
-second = str(now.second).rjust(2, ZERO)
-filename = f'{year}{month}{day}{hour}{minute}{second}_update_loa_trnsn_ids.up.sql'
+filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_update_loa_trnsn_ids.up.sql"
 secure_migration_filename = (
     f'{current_dir}/{destination_directory}/{filename}'
 )
