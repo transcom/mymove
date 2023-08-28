171a7f05a1c7e20be3c3d1538a12221ddcc25ec3
Scott Ries <scottmries@truss.works>
2023-08-28 12:31:18 -0400

    Update migrations/app/secure/20230808155723_add_ns_mayport.up.sql
    Co-authored-by: Roger Steve Ruiz <rogeruiz@truss.works>

--- migrations/app/secure/20230808155723_add_ns_mayport.up.sql
+++ migrations/app/secure/20230808155723_add_ns_mayport.up.sql
@@ -1,4 +1,4 @@
 --- This migration is only expected to run in the STG environment. The other environments have a blank file.
 
 UPDATE addresses SET postal_code = '32233' WHERE id = '430c50b4-cc87-42a6-9921-2010aa6d4ed3';
-UPDATE duty_locations SET name = 'NS Mayport, FL 32233', provides_services_counseling = 'TRUE' where address_id = '430c50b4-cc87-42a6-9921-2010aa6d4ed3'
+UPDATE duty_locations SET name = 'NS Mayport, FL 32233', provides_services_counseling = 'TRUE' where address_id = '430c50b4-cc87-42a6-9921-2010aa6d4ed3';
