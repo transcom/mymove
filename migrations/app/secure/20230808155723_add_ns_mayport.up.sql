15f0083657fd44798e552eadcaa321ef8eaf9d6a
Scott Ries <scottmries@truss.works>
2023-08-28 16:54:14 +0000

    Revert to inserts.

--- migrations/app/secure/20230808155723_add_ns_mayport.up.sql
+++ migrations/app/secure/20230808155723_add_ns_mayport.up.sql
@@ -1,4 +1,2 @@
---- This migration is only expected to run in the STG environment. The other environments have a blank file.
-
-UPDATE addresses SET postal_code = '32233' WHERE id = '430c50b4-cc87-42a6-9921-2010aa6d4ed3';
-UPDATE duty_locations SET name = 'NS Mayport, FL 32233', provides_services_counseling = 'TRUE' where address_id = '430c50b4-cc87-42a6-9921-2010aa6d4ed3';
+INSERT INTO addresses (id, street_address_1, city, state, postal_code, updated_at, created_at) VALUES ('430c50b4-cc87-42a6-9921-2010aa6d4ed3', 'n/a', 'Mayport', 'FL', '32233', now(), now());
+INSERT INTO duty_locations (id, address_id, name, affiliation, transportation_office_id, updated_at, created_at, provides_services_counseling) VALUES ('3bfe8068-f9d1-4c21-ac19-1c0a60dcd326', '430c50b4-cc87-42a6-9921-2010aa6d4ed3', 'NS Mayport, FL 32233', 'NAVY', 'f5ab88fe-47f8-4b58-99af-41067d6cb60d', now(), now(), 'TRUE');
