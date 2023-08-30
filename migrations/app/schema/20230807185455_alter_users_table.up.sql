4757948296efc18a7bb28df798be39319156a5aa
Daniel Jordan <daniel.jordan@caci.com>
2023-08-30 19:25:34 +0000

    update to rerun tests

--- migrations/app/schema/20230807185455_alter_users_table.up.sql
+++ migrations/app/schema/20230807185455_alter_users_table.up.sql
@@ -1,4 +1,4 @@
--- commenting these out due to introduction of these columns at an earlier migration file during change from login.gov to Okta
+-- commenting these out due to introduction of these columns at an earlier migration file during change from login.gov to Okta, this is no longer needed
 
 -- alter table users
 -- add column okta_email text,
