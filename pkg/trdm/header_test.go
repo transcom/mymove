77f94b1184577d0e766dc3d1dd3a4761236995e8
cameroncaci <cameron.jewell@caci.com>
2023-09-06 18:29:35 +0000

    Test cleanup, moved test cert and private key to factory

--- pkg/trdm/header_test.go
+++ pkg/trdm/header_test.go
@@ -1,33 +1,16 @@
 package trdm_test
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"math/big"
-
+	"github.com/transcom/mymove/pkg/factory"
 	"github.com/transcom/mymove/pkg/trdm"
 )
 
 func (suite *TRDMSuite) TestGenerateSignedHeader() {
-	// Ceate an x509 template to be used for creating a new certificate. This template is lacking a lot of information, but provides just enough
-	// to satisfy the test's needs
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(1),
-	}
-
-	// Generate private key
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	// Generate public cert, private key, and error
+	cert, key, err := factory.Generatex509CertAndSecret()
 	suite.NoError(err)
-
-	// Generate public certificate
-	certByte, err := x509.CreateCertificate(rand.Reader, ca, ca, &key.PublicKey, key)
-	suite.NoError(err)
-	certificate, err := x509.ParseCertificate(certByte)
-	suite.NoError(err)
-
 	// Pass certificate and key for header signing
-	_, err = trdm.GenerateSignedHeader(certificate, key)
+	_, err = trdm.GenerateSignedHeader(cert, key)
 	suite.NoError(err)
 }
 
