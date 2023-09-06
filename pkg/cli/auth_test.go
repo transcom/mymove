2cbc6754b59dd171d48019afe28fb5b57c6a49bb
deandreJones <deandre.jones@caci.com>
2023-09-06 19:37:25 +0000

    remove setEnvs flags

--- pkg/cli/auth_test.go
+++ pkg/cli/auth_test.go
@@ -1,9 +1,7 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigAuth() {
-	suite.T().Setenv("okta-customer-client-id", "someValue")
-	suite.T().Setenv("okta-office-client-id", "someValue")
-	suite.T().Setenv("okta-admin-client-id", "someValue")
+
 	suite.Setup(InitAuthFlags, []string{})
 	suite.NoError(CheckAuth(suite.viper))
 }
