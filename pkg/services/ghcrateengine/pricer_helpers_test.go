edfa93628748804b706f84098829e159b3ed5cd4
deandreJones <136509766+deandreJones@users.noreply.github.com>
2023-09-05 19:43:15 -0500

    Update pricer_helpers_test.go

--- pkg/services/ghcrateengine/pricer_helpers_test.go
+++ pkg/services/ghcrateengine/pricer_helpers_test.go
@@ -553,8 +553,7 @@ func (suite *GHCRateEngineServiceSuite) Test_priceDomesticShuttling() {
 		_, _, err := priceDomesticShuttling(suite.AppContextForTest(), models.ReServiceCodeDDSHUT, testdatagen.DefaultContractCode, twoYearsLaterPickupDate, ddshutTestWeight, ddshutTestServiceSchedule)
 
 		suite.Error(err)
-		suite.Contains(err.Error(), "unable to calculate escalated total price: could not lookup contract year")
-
+suite.Contains(err.Error(), "could not calculate escalated price: could not lookup contract year")
 	})
 }
 func (suite *GHCRateEngineServiceSuite) Test_priceDomesticCrating() {
