b440953daac27d8139392accf85601dd32835c44
deandreJones <deandre.jones@caci.com>
2023-09-06 19:07:20 +0000

    E-04371  set Env vars

--- pkg/cli/auth_test.go
+++ pkg/cli/auth_test.go
@@ -1,6 +1,9 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigAuth() {
+	suite.T().Setenv("okta-customer-client-id", "someValue")
+	suite.T().Setenv("okta-office-client-id", "someValue")
+	suite.T().Setenv("okta-admin-client-id", "someValue")
 	suite.Setup(InitAuthFlags, []string{})
 	suite.NoError(CheckAuth(suite.viper))
 }
