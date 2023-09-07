eda149b8dc0193847dbdf131f99d30f694910198
deandreJones <deandre.jones@caci.com>
2023-09-07 14:06:10 +0000

    original migration files

--- migrations/app/schema/20201117222339_create_user_records_for_office_and_admin_users_without.up.sql
+++ migrations/app/schema/20201117222339_create_user_records_for_office_and_admin_users_without.up.sql
@@ -12,10 +12,10 @@ WHERE email IN ('example1@truss.works','example2@truss.works','example3@truss.wo
 WITH office_never_signed_in AS (
     SELECT *
     FROM office_users
-             LEFT JOIN users ON office_users.email = users.okta_email
-    WHERE users.okta_email IS NULL
+             LEFT JOIN users ON office_users.email = users.login_gov_email
+    WHERE users.login_gov_email IS NULL
 )
-INSERT INTO users (id, okta_email, active, created_at, updated_at)
+INSERT INTO users (id, login_gov_email, active, created_at, updated_at)
 SELECT uuid_generate_v4(), email, TRUE, now(), now()
 FROM office_never_signed_in;
 
@@ -24,23 +24,23 @@ FROM office_never_signed_in;
 WITH office_associate_user AS (
     SELECT users.*
     FROM office_users
-    JOIN users ON office_users.email = users.okta_email
+    JOIN users ON office_users.email = users.login_gov_email
     WHERE office_users.user_id IS NULL
 )
 UPDATE office_users
 SET user_id = office_associate_user.id
 FROM office_associate_user
-WHERE office_users.email = office_associate_user.okta_email;
+WHERE office_users.email = office_associate_user.login_gov_email;
 
 -- Finds admin users who have never signed in so we must create a user record
 -- for them that can be updated on first sign in
 WITH admin_never_signed_in AS (
     SELECT *
     FROM admin_users
-             LEFT JOIN users ON admin_users.email = users.okta_email
-    WHERE users.okta_email IS NULL
+             LEFT JOIN users ON admin_users.email = users.login_gov_email
+    WHERE users.login_gov_email IS NULL
 )
-INSERT INTO users (id, okta_email, active, created_at, updated_at)
+INSERT INTO users (id, login_gov_email, active, created_at, updated_at)
 SELECT uuid_generate_v4(), email, TRUE, now(), now()
 FROM admin_never_signed_in;
 
@@ -49,10 +49,10 @@ FROM admin_never_signed_in;
 WITH admin_associate_user AS (
     SELECT users.*
     FROM admin_users
-    JOIN users ON admin_users.email = users.okta_email
+    JOIN users ON admin_users.email = users.login_gov_email
     WHERE admin_users.user_id IS NULL
 )
 UPDATE admin_users
 SET user_id = admin_associate_user.id
 FROM admin_associate_user
-WHERE admin_users.email = admin_associate_user.okta_email;
+WHERE admin_users.email = admin_associate_user.login_gov_email;
--- migrations/app/secure/20190328175938_update-beta-super-users.up.sql
+++ migrations/app/secure/20190328175938_update-beta-super-users.up.sql
@@ -3,4 +3,4 @@
 -- intend to apply on production, but do not include any sensitive data.
 
 -- Below is an example for easy reference for future migrations (no updates to dev data actually needed)
--- UPDATE users SET is_superuser = TRUE, updated_at = now() WHERE okta_email = 'officeuser1@example.com';
+-- UPDATE users SET is_superuser = TRUE, updated_at = now() WHERE login_gov_email = 'officeuser1@example.com';
