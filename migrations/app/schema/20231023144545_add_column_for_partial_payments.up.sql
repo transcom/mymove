e90e43b0301e73d635c2d6800981a73e72830c5e
Daniel Jordan <daniel.jordan@caci.com>
2023-10-23 19:32:43 +0000

    added _cents to column name to be more descriptive on expected value

--- migrations/app/schema/20231023144545_add_column_for_partial_payments.up.sql
+++ migrations/app/schema/20231023144545_add_column_for_partial_payments.up.sql
@@ -1,7 +1,7 @@
 -- adding column for partial payments
 -- this is stored as an array of integers in cents to account for potential multiple payment requests
 ALTER TABLE payment_requests
-ADD COLUMN partial_payment integer[];
+ADD COLUMN partial_payment_cents integer[];
 
 -- Column comments
-COMMENT ON COLUMN payment_requests.partial_payment IS 'Partial payment for payment requests stored in cents.';
+COMMENT ON COLUMN payment_requests.partial_payment_cents IS 'Partial payment for payment requests stored in cents.';
