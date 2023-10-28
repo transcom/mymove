2b74a18b0306bca3202c4e34405708895e7771f1
AaronW <aaron.weatherford@caci.com>
2023-10-27 19:44:38 -0500

    more testing work
    -corrected some faulty syntax in the tests
    -added another test in the entire shipment onboarding tests

--- playwright/tests/my/mymove/onboarding.spec.js
+++ playwright/tests/my/mymove/onboarding.spec.js
@@ -33,7 +33,7 @@ test('A customer can go through onboarding', async ({ page, customerPage }) => {
   await customerPage.waitForPage.onboardingDutyLocation();
   await page.getByLabel('What is your current duty location?').fill('Scott AFB');
   await page.keyboard.press('Backspace'); // tests if backspace clears the duty location field
-  await expect(page.getByLabel('New duty location')).toBeEmpty();
+  await expect(page.getByLabel('What is your current duty location?')).toBeEmpty();
   await page.getByLabel('What is your current duty location?').fill('Scott AFB');
   // 'mark' is not yet supported by react testing library
   // https://github.com/testing-library/dom-testing-library/issues/1150
