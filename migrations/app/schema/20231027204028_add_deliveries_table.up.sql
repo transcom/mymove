147a5bec86f98c06d5ef41dedb73f7d88f151a68
Daniel Jordan <daniel.jordan@caci.com>
2023-11-02 15:41:22 +0000

    added comments to migration file

--- migrations/app/schema/20231027204028_add_deliveries_table.up.sql
+++ migrations/app/schema/20231027204028_add_deliveries_table.up.sql
@@ -13,3 +13,8 @@ CREATE TABLE deliveries
 -- adding indexes on these columns to accelerate database queries that involve these columns
 CREATE INDEX ON deliveries (mto_shipment_id);
 CREATE INDEX ON deliveries (mto_service_item_id);
+
+-- Column comments
+COMMENT ON COLUMN deliveries.mto_shipment_id IS 'Shipment that the delivery applies to.';
+COMMENT ON COLUMN deliveries.mto_service_item_id IS 'Service item that the delivery applies to.';
+COMMENT ON COLUMN deliveries.delivery_weight_pounds IS 'The number of pounds that the delivery is, can be partial or full weight.';
