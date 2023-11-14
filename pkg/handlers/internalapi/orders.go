16515bb5ff4faeebb547eaadd3da436ac7732f97
Logan Cunningham <logan.cunningham@caci.com>
2023-11-14 15:17:29 +0000

    weightAllotment

--- pkg/handlers/internalapi/orders.go
+++ pkg/handlers/internalapi/orders.go
@@ -168,7 +168,15 @@ func (h CreateOrdersHandler) Handle(params ordersop.CreateOrdersParams) middlewa
 				StorageInTransit:     models.IntPointer(sitDaysAllowance),
 				ProGearWeight:        weightAllotment.ProGearWeight,
 				ProGearWeightSpouse:  weightAllotment.ProGearWeightSpouse,
+				weightAllotment:      weight,
+				WeightAllotment:      weight,
 			}
+
+			/*
+				IF you get that to work you'll still have to add conditionals for all the places the entitlement is used because it
+				isn't inheritly clear if it's using the spouse weight or not. So you'll be creating new variables and conditionals
+				in move_dats.go, move_weights, and move_submitted, etc
+			*/
 
 			if saveEntitlementErr := appCtx.DB().Save(&entitlement); saveEntitlementErr != nil {
 				return handlers.ResponseForError(appCtx.Logger(), saveEntitlementErr), saveEntitlementErr
