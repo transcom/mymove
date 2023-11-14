7d7644b7ad77856328ca8e6e67de0b6e4fe0a881
Maria Traskowsky <Maria.Traskowsky@caci.com>
2023-11-14 21:25:59 +0000

    Added tests for not rendering tooltip on any service item that is not resubmitted

--- src/components/Office/ServiceItemsTable/ResubmittedServiceItemDetails.test.jsx
+++ src/components/Office/ServiceItemsTable/ResubmittedServiceItemDetails.test.jsx
@@ -99,4 +99,85 @@ describe('ServiceItemsTable', () => {
     resultString += 'SIT Postal Code\nNew: 09876 \nPrevious: 67890\n\n';
     expect(toolTip.props().text).toBe(resultString);
   });
+
+  it('does not render a tooltip for a service item that has not been resubmitted', () => {
+    const defaultProps = {
+      handleUpdateMTOServiceItemStatus: jest.fn(),
+      handleShowRejectionDialog: jest.fn(),
+      handleRequestSITAddressUpdateModal: jest.fn(),
+      handleShowEditSitAddressModal: jest.fn(),
+      handleShowEditSitEntryDateModal: jest.fn(),
+      serviceItemAddressUpdateAlert: {
+        makeVisible: false,
+        alertMessage: '',
+        alertType: '',
+      },
+    };
+
+    const serviceItems = [
+      {
+        id: 'abc123',
+        mtoShipmentID: 'xyz789',
+        submittedAt: '2020-11-20',
+        serviceItem: 'Domestic Origin 1st Day SIT',
+        code: 'DOFSIT',
+        details: {
+          pickupPostalCode: '11111',
+          reason: 'This is the reason',
+        },
+      },
+    ];
+    const history = {
+      isLoading: false,
+      isError: false,
+      queueResult: {
+        data: [
+          {
+            action: 'INSERT',
+            eventName: 'updateMTOServiceItem',
+            actionTstampTx: '2022-03-09T15:33:38.579Z',
+            changedValues: {
+              reason: 'New reason in test code',
+              status: 'SUBMITTED',
+              id: 'abc12345',
+              pickup_postal_code: '54321',
+              sit_entry_date: '2023-01-01',
+              sit_postal_code: '09876',
+            },
+            objectId: 'historyObjectInServiceItemsTableTest',
+          },
+        ],
+      },
+      isSuccess: true,
+    };
+
+    useMovePaymentRequestsQueries.mockReturnValue(multiplePaymentRequests);
+    useGHCGetMoveHistory.mockReturnValue(history);
+
+    const approvedWrapper = mount(
+      <MockProviders permissions={[permissionTypes.updateMTOServiceItem]}>
+        <ServiceItemsTable
+          {...defaultProps}
+          serviceItems={serviceItems}
+          statusForTableType={SERVICE_ITEM_STATUS.APPROVED}
+        />
+      </MockProviders>,
+    );
+
+    const toolTipAccepted = approvedWrapper.find('ToolTip').at(0);
+    expect(toolTipAccepted.exists()).toBe(false);
+
+    const rejectedWrapper = mount(
+      <MockProviders permissions={[permissionTypes.updateMTOServiceItem]}>
+        <ServiceItemsTable
+          {...defaultProps}
+          serviceItems={serviceItems}
+          statusForTableType={SERVICE_ITEM_STATUS.REJECTED}
+        />
+      </MockProviders>,
+    );
+
+    const toolTipRejected = rejectedWrapper.find('ToolTip').at(0);
+    expect(toolTipRejected.exists()).toBe(false);
+  });
 });
