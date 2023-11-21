29062cd570cfb53cab78f1b67715fc1362a08ebc
TevinAdams <tevin.adams@caci.com>
2023-11-21 16:53:43 +0000

    Removed DB script

--- migrations/app/schema/20231106173413_add_weight_column_to_payment_request.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE payment_requests
-ADD COLUMN requested_weight_amount integer;
-
-COMMENT ON COLUMN payment_requests.requested_weight_amount IS 'The desired weight amount for the payment request.';
