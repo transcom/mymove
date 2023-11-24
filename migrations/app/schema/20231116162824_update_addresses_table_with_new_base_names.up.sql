fd710f030431f55ea07b4d1fd27da8916666ef36
Maria Traskowsky <Maria.Traskowsky@caci.com>
2023-11-24 20:01:43 +0000

    Update augusta

--- migrations/app/schema/20231116162824_update_addresses_table_with_new_base_names.up.sql
+++ migrations/app/schema/20231116162824_update_addresses_table_with_new_base_names.up.sql
@@ -5,7 +5,7 @@ UPDATE addresses SET city = 'Fort Liberty' WHERE city in ('Fort Bragg', 'FORT BR
 UPDATE addresses SET city = 'Fort Johnson' WHERE city in ('Fort Polk South', 'Fort Polk') and postal_code = '71459';
 UPDATE addresses SET city = 'Fort Moore' WHERE city IN ('Fort Benning', 'FORT BENNING') AND postal_code IN ('31905', '31995');
 UPDATE addresses SET city = 'Fort Gregg-Adams' WHERE city in ('Fort Lee', 'FORT LEE') AND postal_code = '23801';
-UPDATE addresses SET city = 'Fort Eisenhower' WHERE city = 'Fort Gordon' AND postal_code = '30813';
+UPDATE addresses SET city = 'Fort Eisenhower' WHERE city in ('Fort Gordon', 'Augusta') AND postal_code = '30813';
 
 -- add row into duty_location_names for fort barfoot
 INSERT INTO duty_location_names (id, name, duty_location_id, created_at, updated_at)
