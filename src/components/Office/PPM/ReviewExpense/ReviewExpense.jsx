77f77e3a268ac5e82e873f192362e7bd86c42b4d
= <landan.parker@caci.com>
2023-11-27 08:33:53 -0600

    Fixed tests

--- src/components/Office/PPM/ReviewExpense/ReviewExpense.jsx
+++ src/components/Office/PPM/ReviewExpense/ReviewExpense.jsx
@@ -101,7 +101,7 @@ export default function ReviewExpense({
   };
 
   const titleCase = (input) => input.charAt(0).toUpperCase() + input.slice(1);
-  const allCase = (input) => input.split(' ').map(titleCase).join(' ');
+  const allCase = (input) => input?.split(' ').map(titleCase).join(' ') ?? '';
   const expenseName = initialValues.movingExpenseType.toLowerCase().replace('_', ' ');
   return (
     <div className={classnames(styles.container, 'container--accent--ppm')}>
