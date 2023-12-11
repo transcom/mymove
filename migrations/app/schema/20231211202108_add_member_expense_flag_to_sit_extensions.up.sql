957d63b06b93419de0977cdc6a6e11f1fe8786d7
Brooklyn Welsh <brooklyn.welsh@caci.com>
2023-12-11 15:33:03 -0500

    Added comment to migration.

--- migrations/app/schema/20231211202108_add_member_expense_flag_to_sit_extensions.up.sql
+++ migrations/app/schema/20231211202108_add_member_expense_flag_to_sit_extensions.up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE sit_extensions
 ADD COLUMN members_expense BOOLEAN DEFAULT FALSE;
+COMMENT on TABLE sit_extensions.members_expense IS 'Whether or not the service member is responsible for expenses of SIT (i.e. if SIT extension request was denied).';
