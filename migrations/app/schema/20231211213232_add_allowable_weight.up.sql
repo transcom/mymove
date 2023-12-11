56e265d363e7a37a49f68ae0c858fca88d1e9ec7
joeydoyecaci <joseph.doye@caci.com>
2023-12-11 22:02:00 +0000

    Added db migration for reimbursable weight column

---
+++ migrations/app/schema/20231211213232_add_allowable_weight.up.sql
@@ -0,0 +1,5 @@
+-- Adding new field to weight_tickets to allow capture of the maximum reimbursable benefit amount for PPM shipments.
+ALTER TABLE public.weight_tickets ADD reimbursable_weight int4 NOT NULL DEFAULT 0;
+
+-- Column comments
+COMMENT ON COLUMN public.weight_tickets.reimbursable_weight IS 'Stores the allowable weight of the vehicle';
