27430d234a025b31986154bc10e860ed793ab747
Brooklyn Welsh <brooklyn.welsh@caci.com>
2023-12-13 19:13:54 -0500

    Fixed backend code so that DB flag is now properly set. Also added new Mocks for MTOServiceItemUpdater.

--- pkg/services/mocks/SITExtensionDenier.go
+++ pkg/services/mocks/SITExtensionDenier.go
@@ -16,25 +16,25 @@ type SITExtensionDenier struct {
 	mock.Mock
 }
 
-// DenySITExtension provides a mock function with given fields: appCtx, shipmentID, sitExtensionID, officeRemarks, eTag
-func (_m *SITExtensionDenier) DenySITExtension(appCtx appcontext.AppContext, shipmentID uuid.UUID, sitExtensionID uuid.UUID, officeRemarks *string, eTag string) (*models.MTOShipment, error) {
-	ret := _m.Called(appCtx, shipmentID, sitExtensionID, officeRemarks, eTag)
+// DenySITExtension provides a mock function with given fields: appCtx, shipmentID, sitExtensionID, officeRemarks, convertToMembersExpense, eTag
+func (_m *SITExtensionDenier) DenySITExtension(appCtx appcontext.AppContext, shipmentID uuid.UUID, sitExtensionID uuid.UUID, officeRemarks *string, convertToMembersExpense bool, eTag string) (*models.MTOShipment, error) {
+	ret := _m.Called(appCtx, shipmentID, sitExtensionID, officeRemarks, convertToMembersExpense, eTag)
 
 	var r0 *models.MTOShipment
 	var r1 error
-	if rf, ok := ret.Get(0).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, string) (*models.MTOShipment, error)); ok {
-		return rf(appCtx, shipmentID, sitExtensionID, officeRemarks, eTag)
+	if rf, ok := ret.Get(0).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, bool, string) (*models.MTOShipment, error)); ok {
+		return rf(appCtx, shipmentID, sitExtensionID, officeRemarks, convertToMembersExpense, eTag)
 	}
-	if rf, ok := ret.Get(0).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, string) *models.MTOShipment); ok {
-		r0 = rf(appCtx, shipmentID, sitExtensionID, officeRemarks, eTag)
+	if rf, ok := ret.Get(0).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, bool, string) *models.MTOShipment); ok {
+		r0 = rf(appCtx, shipmentID, sitExtensionID, officeRemarks, convertToMembersExpense, eTag)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*models.MTOShipment)
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, string) error); ok {
-		r1 = rf(appCtx, shipmentID, sitExtensionID, officeRemarks, eTag)
+	if rf, ok := ret.Get(1).(func(appcontext.AppContext, uuid.UUID, uuid.UUID, *string, bool, string) error); ok {
+		r1 = rf(appCtx, shipmentID, sitExtensionID, officeRemarks, convertToMembersExpense, eTag)
 	} else {
 		r1 = ret.Error(1)
 	}
