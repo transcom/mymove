83b9d352446aac0cbb5b53b070aa2bef741fe924
Tae Jung <tae.jung@caci.com>
2023-12-29 11:50:53 -0600

    sql fix

--- migrations/app/schema/20231211202108_add_member_expense_flags.up.sql
+++ migrations/app/schema/20231211202108_add_member_expense_flags.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE sit_extensions
-ADD COLUMN customer_expense BOOLEAN DEFAULT false NOT NULL;
+ADD COLUMN customer_expense BOOLEAN DEFAULT FALSE;
 COMMENT on COLUMN sit_extensions.customer_expense IS 'Denotes that the TOO rejected this extension request AND converted it to member''s expense (could be used in MTO view/history to show exactly when a shipment was converted)';
 
 ALTER TABLE mto_service_items
-ADD COLUMN customer_expense BOOLEAN DEFAULT FALSE NOT NULL;
+ADD COLUMN customer_expense BOOLEAN DEFAULT FALSE;
 COMMENT on COLUMN mto_service_items.customer_expense IS 'Whether or not the service member is responsible for expenses of SIT (i.e. if SIT extension request was denied). Only applicable to DOFSIT items.';
