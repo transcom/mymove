2f99b8dd4138fd22b9e14b0d9a4e024644a0c2ba
antgmann <anthony.mann@caci.com>
2023-12-29 20:17:23 +0000

    pdfcpu config errors resolved, now pdfcpuwrapper config error

--- pkg/paperwork/generator.go
+++ pkg/paperwork/generator.go
@@ -12,7 +12,9 @@ import (
 	"github.com/disintegration/imaging"
 	"github.com/jung-kurt/gofpdf"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
-	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+
+	// "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
 	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 	"go.uber.org/zap"
@@ -36,13 +38,13 @@ const (
 type Generator struct {
 	fs        *afero.Afero
 	uploader  *uploader.Uploader
-	pdfConfig *pdfcpu.Configuration
+	pdfConfig *model.Configuration
 	workDir   string
 	pdfLib    PDFLibrary
 }
 
 type pdfCPUWrapper struct {
-	*pdfcpu.Configuration
+	*model.Configuration
 }
 
 // Merge merges files
@@ -92,7 +94,7 @@ func convertTo8BitPNG(in io.Reader, out io.Writer) error {
 func NewGenerator(uploader *uploader.Uploader) (*Generator, error) {
 	afs := uploader.Storer.FileSystem()
 
-	pdfConfig := pdfcpu.NewDefaultConfiguration()
+	pdfConfig := model.NewDefaultConfiguration()
 	pdfCPU := pdfCPUWrapper{Configuration: pdfConfig}
 
 	directory, err := afs.TempDir("", "generator")
