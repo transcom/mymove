da812f0296093f166c1c6e6416b2b60134f06ca4
cameroncaci <cameron.jewell@caci.com>
2024-01-02 21:51:00 +0000

    added diversion support for reweigh creator

--- pkg/services/reweigh/reweigh_updater.go
+++ pkg/services/reweigh/reweigh_updater.go
@@ -12,8 +12,6 @@ import (
 	"github.com/transcom/mymove/pkg/etag"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
-	mtoshipment "github.com/transcom/mymove/pkg/services/mto_shipment"
-	"github.com/transcom/mymove/pkg/unit"
 )
 
 // reweighUpdater needs to be updates to have checks for validation
@@ -57,7 +55,7 @@ func (f *reweighUpdater) updateDivertedShipmentReweigh(appCtx appcontext.AppCont
 	var newReweigh *models.Reweigh
 
 	txnError := appCtx.NewTransaction(func(txnAppCtx appcontext.AppContext) error {
-		shipmentIDs, lowestWeight, err := f.getDivertedShipmentIDsAndLowestWeight(txnAppCtx, reweigh)
+		shipmentIDs, lowestWeight, err := getDivertedShipmentIDsAndLowestWeight(txnAppCtx, reweigh)
 		if err != nil {
 			return err
 		}
@@ -96,39 +94,6 @@ func (f *reweighUpdater) updateStandardReweigh(appCtx appcontext.AppContext, rew
 		return nil, txnError
 	}
 	return reweigh, nil
-}
-
-func (f *reweighUpdater) getDivertedShipmentIDsAndLowestWeight(appCtx appcontext.AppContext, reweigh *models.Reweigh) ([]uuid.UUID, *unit.Pound, error) {
-	var shipmentIDs []uuid.UUID
-	var lowestWeight *unit.Pound
-	shipmentFetcher := mtoshipment.NewMTOShipmentFetcher()
-	reweighFetcher := NewReweighFetcher()
-
-	// Find out if this shipment is part of a diversion chain, and if so gather all associated shipments
-	associatedDivertedShipments, err := shipmentFetcher.GetDiversionChain(appCtx, reweigh.ShipmentID)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Create an array of UUIDs, dereferenced from associatedDivertedShipments
-	for _, shipment := range *associatedDivertedShipments {
-		shipmentIDs = append(shipmentIDs, shipment.ID)
-	}
-	reweighs, err := reweighFetcher.ListReweighsByShipmentIDs(appCtx, shipmentIDs)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Find the lowest reweigh
-	for _, tempReweighVar := range reweighs {
-		// Check for valid reweigh weight, if we have set a lowest weight yet, and if so then if the new weight is lower than our lowest
-		if tempReweighVar.Weight != nil && (lowestWeight == nil || *tempReweighVar.Weight < *lowestWeight) {
-			// If valid and lower than our lowest, we set the new weight here
-			lowestWeight = tempReweighVar.Weight
-			//shipmentIdWithLowestWeight = shipmentID
-		}
-	}
-	return shipmentIDs, lowestWeight, nil
 }
 
 func (f *reweighUpdater) updateReweighsInChain(appCtx appcontext.AppContext, requestedReweigh *models.Reweigh, shipmentIDs []uuid.UUID, eTag string, checks ...reweighValidator) (*models.Reweigh, error) {
