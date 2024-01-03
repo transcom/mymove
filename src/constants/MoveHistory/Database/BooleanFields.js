295138aca7e86a160f1dd0c4285dc891e77f99cc
Paul Stonebraker <paul.stonebraker@caci.com>
2024-01-03 13:47:53 +0000

    fix boolean fields casing

--- src/constants/MoveHistory/Database/BooleanFields.js
+++ src/constants/MoveHistory/Database/BooleanFields.js
@@ -4,6 +4,6 @@ export default {
   dependents_authorized: 'dependents_authorized',
   has_dependents: 'has_dependents',
   has_requested_advance: 'has_requested_advance',
-  owns_trailer: 'Trailer',
-  trailer_meets_criteria: 'Trailer meets criteria',
+  owns_trailer: 'owns_trailer',
+  trailer_meets_criteria: 'trailer_meets_criteria',
 };
