28f4b844c835e345d34e7a143e28331d16cb93cb
Michael Inthavongsay <m.inthavongsay@caci.com>
2024-01-10 15:42:34 +0000

    removed commented out code

--- pkg/handlers/primeapi/move_task_order.go
+++ pkg/handlers/primeapi/move_task_order.go
@@ -260,15 +260,6 @@ func (h DownloadMoveOrderHandler) Handle(params movetaskorderops.DownloadMoveOrd
 			}
 
 			move := moves[len(moves)-1]
-
-			//generator, err := paperwork.NewMoveOrderPdfGenerator(appCtx, h.FileStorer())
-			if err != nil {
-				appCtx.Logger().Error("Error: NewMoveOrderPdfGenerator", zap.Error(err))
-				return movetaskorderops.NewDownloadMoveOrderInternalServerError().WithPayload(
-					payloads.InternalServerError(nil, h.GetTraceIDFromRequest(params.HTTPRequest))), err
-			}
-
-			//outputFile, err := generator.GeneratePdf(paperwork.DownloadAll, move)
 			outputFile, err := h.PrimeDownloadMoveUploadPDFGenerator.GenerateDownloadMoveUserUploadPDF(appCtx, services.DownloadMoveOrderUploadTypeAll, move)
 
 			if err != nil {
