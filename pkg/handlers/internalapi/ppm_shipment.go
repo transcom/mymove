84e33de4dd148fa9d8ccb125d0dbdea35f68ad5d
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-01-12 21:26:07 +0000

    Fixed bad ShipmentID var.

--- pkg/handlers/internalapi/ppm_shipment.go
+++ pkg/handlers/internalapi/ppm_shipment.go
@@ -125,7 +125,6 @@ func (h SubmitPPMShipmentDocumentationHandler) Handle(params ppmops.SubmitPPMShi
 			)
 			if err != nil {
 				appCtx.Logger().Error("problem sending email to user", zap.Error(err))
-				return handlers.ResponseForError(appCtx.Logger(), err), err
 			}
 
 			return ppmops.NewSubmitPPMShipmentDocumentationOK().WithPayload(returnPayload), nil
@@ -252,7 +251,7 @@ func (h ResubmitPPMShipmentDocumentationHandler) Handle(params ppmops.ResubmitPP
 			}
 
 			err = h.NotificationSender().SendNotification(appCtx,
-				notifications.NewPpmPacketEmail(ppmShipment.ShipmentID),
+				notifications.NewPpmPacketEmail(ppmShipment.ID),
 			)
 			if err != nil {
 				appCtx.Logger().Error("problem sending email to user", zap.Error(err))
