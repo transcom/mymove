53a3a1940195bfa3e0f82fa453e52878069dee78
Maria Traskowsky <Maria.Traskowsky@caci.com>
2024-01-14 21:03:23 +0000

    remove comment from route.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -10,7 +10,6 @@ export const customerRoutes = {
   DOD_INFO_PATH: '/service-member/dod-info',
   NAME_PATH: '/service-member/name',
   CONTACT_INFO_PATH: '/service-member/contact-info',
-  // CURRENT_DUTY_LOCATION_PATH: '/service-member/current-duty',
   CURRENT_ADDRESS_PATH: '/service-member/current-address',
   BACKUP_ADDRESS_PATH: '/service-member/backup-address',
   BACKUP_CONTACTS_PATH: '/service-member/backup-contact',
