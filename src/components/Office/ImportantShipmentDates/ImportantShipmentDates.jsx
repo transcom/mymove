b0c654a8a976cc48bb8eda0a34e478430cca5571
AaronW <aaron.weatherford@caci.com>
2024-01-16 20:37:19 +0000

    fixed an issue where a wrapper div was getting populated when there were not child tables to display

--- src/components/Office/ImportantShipmentDates/ImportantShipmentDates.jsx
+++ src/components/Office/ImportantShipmentDates/ImportantShipmentDates.jsx
@@ -18,25 +18,29 @@ const ImportantShipmentDates = ({
 }) => {
   const emDash = '\u2014';
   return (
-    <div className={classnames('maxw-tablet', styles.shipmentDatesContainer)}>
-      <DataTableWrapper className="table--data-point-group">
-        {requiredDeliveryDate && (
-          <DataTable columnHeaders={['Required Delivery Date']} dataRow={[requiredDeliveryDate || emDash]} />
-        )}
-        {requestedPickupDate && scheduledDeliveryDate && actualPickupDate && (
-          <DataTable
-            columnHeaders={['Requested pick up date', 'Scheduled pick up date', 'Actual pick up date']}
-            dataRow={[requestedPickupDate || emDash, scheduledPickupDate || emDash, actualPickupDate || emDash]}
-          />
-        )}
-        {requestedDeliveryDate && scheduledDeliveryDate && actualDeliveryDate && (
-          <DataTable
-            columnHeaders={['Requested delivery date', 'Scheduled delivery date', 'Actual delivery date']}
-            dataRow={[requestedDeliveryDate || emDash, scheduledDeliveryDate || emDash, actualDeliveryDate || emDash]}
-          />
-        )}
-      </DataTableWrapper>
-    </div>
+    (requiredDeliveryDate ||
+      (requestedPickupDate && scheduledPickupDate && actualPickupDate) ||
+      (scheduledDeliveryDate && requestedDeliveryDate && actualDeliveryDate)) && (
+      <div className={classnames('maxw-tablet', styles.shipmentDatesContainer)}>
+        <DataTableWrapper className="table--data-point-group">
+          {requiredDeliveryDate && (
+            <DataTable columnHeaders={['Required Delivery Date']} dataRow={[requiredDeliveryDate || emDash]} />
+          )}
+          {requestedPickupDate && scheduledPickupDate && actualPickupDate && (
+            <DataTable
+              columnHeaders={['Requested pick up date', 'Scheduled pick up date', 'Actual pick up date']}
+              dataRow={[requestedPickupDate || emDash, scheduledPickupDate || emDash, actualPickupDate || emDash]}
+            />
+          )}
+          {requestedDeliveryDate && scheduledDeliveryDate && actualDeliveryDate && (
+            <DataTable
+              columnHeaders={['Requested delivery date', 'Scheduled delivery date', 'Actual delivery date']}
+              dataRow={[requestedDeliveryDate || emDash, scheduledDeliveryDate || emDash, actualDeliveryDate || emDash]}
+            />
+          )}
+        </DataTableWrapper>
+      </div>
+    )
   );
 };
 
