7bcb98abf202a3c26aedda9d961b00c8af8688d5
TevinAdams <tevin.adams@caci.com>
2024-01-19 15:02:48 +0000

    Commented out PPM Workflow Playright tests

--- playwright/tests/my/milmove/ppms/about.spec.js
+++ playwright/tests/my/milmove/ppms/about.spec.js
@@ -9,16 +9,15 @@ import { test, forEachViewport } from './customerPpmTestFixture';
 
 test.describe('About Your PPM', () => {
   forEachViewport(async () => {
-    test.beforeEach(async ({ customerPpmPage }) => {
-      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
-      await customerPpmPage.signInForPPMWithMove(move);
-    });
-
-    [true, false].forEach((selectAdvance) => {
-      const advanceText = selectAdvance ? 'with' : 'without';
-      test(`can submit actual PPM shipment info ${advanceText} an advance`, async ({ customerPpmPage }) => {
-        await customerPpmPage.navigateToAboutPage({ selectAdvance });
-      });
-    });
+    // test.beforeEach(async ({ customerPpmPage }) => {
+    //   // const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
+    //   // await customerPpmPage.signInForPPMWithMove(move);
+    // });
+    // [true, false].forEach((selectAdvance) => {
+    //   const advanceText = selectAdvance ? 'with' : 'without';
+    //   // test(`can submit actual PPM shipment info ${advanceText} an advance`, async ({ customerPpmPage }) => {
+    //   //   // await customerPpmPage.navigateToAboutPage({ selectAdvance });
+    //   // });
+    // });
   });
 });
