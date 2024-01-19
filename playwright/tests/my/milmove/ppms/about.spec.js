5b74ddc1d6946e276edf60a5c9b48dd6adf0da32
TevinAdams <tevin.adams@caci.com>
2024-01-19 17:11:37 +0000

    commenting out playwright tests

--- playwright/tests/my/milmove/ppms/about.spec.js
+++ playwright/tests/my/milmove/ppms/about.spec.js
@@ -3,21 +3,21 @@
  * non best-practices, in particular: heavy use of `page.locator`
  * instead of `page.getBy*`.
  */
-
+// Need to rework once PPM workflow is restored
 // @ts-check
-import { test, forEachViewport } from './customerPpmTestFixture';
+// import { test, forEachViewport } from './customerPpmTestFixture';
 
-test.describe('About Your PPM', () => {
-  forEachViewport(async () => {
-    // test.beforeEach(async ({ customerPpmPage }) => {
-    //   // const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
-    //   // await customerPpmPage.signInForPPMWithMove(move);
-    // });
-    // [true, false].forEach((selectAdvance) => {
-    //   const advanceText = selectAdvance ? 'with' : 'without';
-    //   // test(`can submit actual PPM shipment info ${advanceText} an advance`, async ({ customerPpmPage }) => {
-    //   //   // await customerPpmPage.navigateToAboutPage({ selectAdvance });
-    //   // });
-    // });
-  });
-});
+// test.describe('About Your PPM', () => {
+//   forEachViewport(async () => {
+//     // test.beforeEach(async ({ customerPpmPage }) => {
+//     //   // const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
+//     //   // await customerPpmPage.signInForPPMWithMove(move);
+//     // });
+//     // [true, false].forEach((selectAdvance) => {
+//     //   const advanceText = selectAdvance ? 'with' : 'without';
+//     //   // test(`can submit actual PPM shipment info ${advanceText} an advance`, async ({ customerPpmPage }) => {
+//     //   //   // await customerPpmPage.navigateToAboutPage({ selectAdvance });
+//     //   // });
+//     // });
+//   });
+// });
