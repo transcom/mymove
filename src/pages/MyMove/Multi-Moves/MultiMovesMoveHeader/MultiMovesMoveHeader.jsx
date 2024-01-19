5da27d27c18e399b3f76056488d152c635bca9b5
Daniel Jordan <daniel.jordan@caci.com>
2024-01-19 21:37:44 +0000

    adding tests for move header

--- src/pages/MyMove/Multi-Moves/MultiMovesMoveHeader/MultiMovesMoveHeader.jsx
+++ src/pages/MyMove/Multi-Moves/MultiMovesMoveHeader/MultiMovesMoveHeader.jsx
@@ -7,7 +7,7 @@ import styles from './MultiMovesMoveHeader.module.scss';
 const MultiMovesMoveHeader = ({ title }) => {
   return (
     <div className={styles.moveHeaderContainer}>
-      <FontAwesomeIcon icon={faTruck} />
+      <FontAwesomeIcon icon={faTruck} data-testid="truck-icon" />
       <h3>{title}</h3>
     </div>
   );
