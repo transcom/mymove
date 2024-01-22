7b7e2f5ed96371883e0fdf7f41aae82bd24190a4
Daniel Jordan <daniel.jordan@caci.com>
2024-01-22 17:57:02 +0000

    tests

--- src/pages/MyMove/Multi-Moves/MultiMovesMoveContainer/MultiMovesMoveContainer.test.jsx
+++ src/pages/MyMove/Multi-Moves/MultiMovesMoveContainer/MultiMovesMoveContainer.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; // For expect assertions
 
 import { mockMovesPCS } from '../MultiMovesTestData';
@@ -13,6 +13,7 @@ describe('MultiMovesMoveContainer', () => {
   it('renders current move list correctly', () => {
     render(<MultiMovesMoveContainer moves={mockCurrentMoves} />);
 
+    expect(screen.getByTestId('move-info-container')).toBeInTheDocument();
     expect(screen.getByText('#MOVECO')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Go to Move' })).toBeInTheDocument();
   });
@@ -20,9 +21,25 @@ describe('MultiMovesMoveContainer', () => {
   it('renders previous move list correctly', () => {
     render(<MultiMovesMoveContainer moves={mockPreviousMoves} />);
 
-    expect(screen.getByText('#SAMPLE')).toBeInTheDocument();
-    expect(screen.getByText('#EXAMPL')).toBeInTheDocument();
-    // should render two buttons
+    expect(screen.queryByText('#SAMPLE')).toBeInTheDocument();
+    expect(screen.queryByText('#EXAMPL')).toBeInTheDocument();
     expect(screen.getAllByRole('button', { name: 'Download' })).toHaveLength(2);
+  });
+
+  it('expands and collapses moves correctly', () => {
+    render(<MultiMovesMoveContainer moves={mockCurrentMoves} />);
+
+    // Initially, the move details should not be visible
+    expect(screen.queryByText('Shipment')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId('expand-icon'));
+
+    // Now, the move details should be visible
+    expect(screen.getByText('Shipments')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId('expand-icon'));
+
+    // The move details should be hidden again
+    expect(screen.queryByText('Shipments')).not.toBeInTheDocument();
   });
 });
