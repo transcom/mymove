d8b62b8f3420347389e3f4d874b28327eaf7596e
Ricky Mettler <ricky.mettler@caci.com>
2024-01-22 16:41:55 +0000

    adding comments

--- pkg/services/sit_status/shipment_sit_status.go
+++ pkg/services/sit_status/shipment_sit_status.go
@@ -129,7 +129,8 @@ func (f shipmentSITStatus) CalculateShipmentSITStatus(appCtx appcontext.AppConte
 		sitCustomerContacted = currentSIT.SITCustomerContacted
 		sitRequestedDelivery = currentSIT.SITRequestedDelivery
 
-		oldServiceItem, err := models.FetchServiceItem(appCtx.DB(), currentSIT.ID)
+		// Need to retrieve the current service item so we can populate the Authorized End Date for the current SIT
+		currentServiceItem, err := models.FetchServiceItem(appCtx.DB(), currentSIT.ID)
 		if err != nil {
 			switch err {
 			case models.ErrFetchNotFound:
@@ -139,7 +140,7 @@ func (f shipmentSITStatus) CalculateShipmentSITStatus(appCtx appcontext.AppConte
 			}
 		}
 
-		sitAuthorizedEndDate := oldServiceItem.SITAuthorizedEndDate
+		sitAuthorizedEndDate := currentServiceItem.SITAuthorizedEndDate
 
 		shipmentSITStatus.CurrentSIT = &services.CurrentSIT{
 			ServiceItemID:        currentSIT.ID,
