e953983ffefa2ed0a7c9d43e055907b3aa9b991f
KonstanceH <146969726+KonstanceH@users.noreply.github.com>
2024-01-22 07:13:10 +0000

    stopping point

--- pkg/services/ppm_closeout/ppm_closeout.go
+++ pkg/services/ppm_closeout/ppm_closeout.go
@@ -10,6 +10,7 @@ import (
 	"github.com/transcom/mymove/pkg/db/utilities"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
+	"github.com/transcom/mymove/pkg/unit"
 )
 
 type ppmCloseoutFetcher struct{}
@@ -34,6 +35,7 @@ func (p *ppmCloseoutFetcher) GetPPMCloseout(appCtx appcontext.AppContext, ppmShi
 			"ProGearWeight",
 			"SpouseProGearWeight",
 			"FinalIncentive",
+			"AdvanceAmountReceived",
 		).
 		Find(&ppmShipment, ppmShipmentID)
 
@@ -66,6 +68,44 @@ func (p *ppmCloseoutFetcher) GetPPMCloseout(appCtx appcontext.AppContext, ppmShi
 		}
 	}
 
+	// get all mtoServiceItems IDs that share a mtoShipmentID
+	var mtoServiceItems models.MTOServiceItems
+	errTest4 := appCtx.DB().Eager("ID").Where("mto_service_items.mto_shipment_id = ?", &mtoShipmentID).All(&mtoServiceItems)
+
+	if errTest4 != nil {
+		return nil, errTest4
+	}
+
+	var paymentServiceItem models.PaymentServiceItem
+	var remainingIncentive unit.Cents
+	var test = &mtoServiceItems
+
+	for _, element := range *test {
+		errTest5 := appCtx.DB().Eager("PriceCents", "staus", "paid_at").Where("payment_service_items.mto_service_item_id = ?", element.ID).All(&paymentServiceItem)
+
+		if paymentServiceItem.Status == models.PaymentServiceItemStatusApproved && paymentServiceItem.PaidAt == nil {
+			remainingIncentive = remainingIncentive.AddCents(*paymentServiceItem.PriceCents)
+		}
+
+		if errTest5 != nil {
+			return nil, errTest5
+		}
+	}
+
+	// paymentServiceItemID := uuid.FromStringOrNil("4730fee7-663d-4b09-9d09-0dab2c22f5d8")
+	// errTest := appCtx.DB().Find(&paymentServiceItem, paymentServiceItemID)
+
+	// if errTest != nil {
+	// 	switch errTest {
+	// 	case sql.ErrNoRows:
+	// 		return nil, apperror.NewNotFoundError(paymentServiceItemID, "while looking for paymentServiceItem")
+	// 	default:
+	// 		return nil, apperror.NewQueryError("paymentServiceItem", errTest, "unable to find paymentServiceItem")
+	// 	}
+	// }
+
+	//services.DomesticLinehaulPricer
+
 	ppmCloseoutObj.ID = &ppmShipmentID
 	ppmCloseoutObj.PlannedMoveDate = mtoShipment.ScheduledPickupDate
 	ppmCloseoutObj.ActualMoveDate = mtoShipment.ActualPickupDate
@@ -76,8 +116,8 @@ func (p *ppmCloseoutFetcher) GetPPMCloseout(appCtx appcontext.AppContext, ppmShi
 	ppmCloseoutObj.ProGearWeightSpouse = ppmShipment.SpouseProGearWeight
 	ppmCloseoutObj.GrossIncentive = ppmShipment.FinalIncentive
 	ppmCloseoutObj.GCC = nil
-	ppmCloseoutObj.AOA = nil
-	ppmCloseoutObj.RemainingReimbursementOwed = nil
+	ppmCloseoutObj.AOA = ppmShipment.AdvanceAmountReceived
+	ppmCloseoutObj.RemainingReimbursementOwed = &remainingIncentive //paymentServiceItem.PriceCents
 	ppmCloseoutObj.HaulPrice = nil
 	ppmCloseoutObj.HaulFSC = nil
 	ppmCloseoutObj.DOP = nil
