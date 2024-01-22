96fee5672a3e87695e29d604899cf37d48d2e5b1
Daniel Jordan <daniel.jordan@caci.com>
2024-01-22 17:21:50 +0000

    tests tests tests and moreeeee tests

--- src/pages/MyMove/Multi-Moves/MultiMovesMoveContainer/MultiMovesMoveContainer.test.jsx
+++ src/pages/MyMove/Multi-Moves/MultiMovesMoveContainer/MultiMovesMoveContainer.test.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; // For expect assertions
 
-import { movesPCS } from '../MultiMovesTestData';
+import { mockMovesPCS } from '../MultiMovesTestData';
 
 import MultiMovesMoveContainer from './MultiMovesMoveContainer';
 
 describe('MultiMovesMoveContainer', () => {
-  const mockMove = movesPCS.previousMoves;
+  const mockCurrentMoves = mockMovesPCS.currentMove;
+  const mockPreviousMoves = mockMovesPCS.previousMoves;
 
-  it('renders move list correctly', () => {
-    render(<MultiMovesMoveContainer move={mockMove} />);
+  it('renders current move list correctly', () => {
+    render(<MultiMovesMoveContainer moves={mockCurrentMoves} />);
 
-    // Check if move codes and status are rendered
+    expect(screen.getByText('#MOVECO')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Go to Move' })).toBeInTheDocument();
+  });
+
+  it('renders previous move list correctly', () => {
+    render(<MultiMovesMoveContainer moves={mockPreviousMoves} />);
+
     expect(screen.getByText('#SAMPLE')).toBeInTheDocument();
     expect(screen.getByText('#EXAMPL')).toBeInTheDocument();
+    // should render two buttons
+    expect(screen.getAllByRole('button', { name: 'Download' })).toHaveLength(2);
   });
 });
