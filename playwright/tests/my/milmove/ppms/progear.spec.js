7e31bb681a976b5e09c47884892c57d097f81936
TevinAdams <tevin.adams@caci.com>
2024-01-24 18:20:05 +0000

    Undo test comments

--- playwright/tests/my/milmove/ppms/progear.spec.js
+++ playwright/tests/my/milmove/ppms/progear.spec.js
@@ -5,27 +5,27 @@
  */
 
 // @ts-check
-// import { expect, test, forEachViewport } from './customerPpmTestFixture';
+import { expect, test, forEachViewport } from './customerPpmTestFixture';
 
-// test.describe('Progear', () => {
-//   forEachViewport(async () => {
-//     test.beforeEach(async ({ customerPpmPage }) => {
-//       const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMProgearWeightTicket();
-//       await customerPpmPage.signInForPPMWithMove(move);
-//       await customerPpmPage.navigateToProgearPage();
-//     });
+test.describe('Progear', () => {
+  forEachViewport(async () => {
+    test.beforeEach(async ({ customerPpmPage }) => {
+      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMProgearWeightTicket();
+      await customerPpmPage.signInForPPMWithMove(move);
+      await customerPpmPage.navigateToProgearPage();
+    });
 
-//     test(`progear page loads`, async ({ customerPpmPage, page }) => {
-//       await customerPpmPage.submitProgearPage({ belongsToSelf: true });
+    test(`progear page loads`, async ({ customerPpmPage, page }) => {
+      await customerPpmPage.submitProgearPage({ belongsToSelf: true });
 
-//       const set2Heading = page.getByRole('heading', { name: 'Set 2' });
-//       await expect(set2Heading).toBeVisible();
-//       const progearSection = set2Heading.locator('../..').last();
-//       await expect(progearSection).toContainText('Pro-gear');
-//       await expect(progearSection).toContainText('Radio equipment');
-//       // contains won't work when text content is divided between multiple semantic html elements
-//       await expect(progearSection).toContainText('Weight:');
-//       await expect(progearSection).toContainText('2,000 lbs');
-//     });
-//   });
-// });
+      const set2Heading = page.getByRole('heading', { name: 'Set 2' });
+      await expect(set2Heading).toBeVisible();
+      const progearSection = set2Heading.locator('../..').last();
+      await expect(progearSection).toContainText('Pro-gear');
+      await expect(progearSection).toContainText('Radio equipment');
+      // contains won't work when text content is divided between multiple semantic html elements
+      await expect(progearSection).toContainText('Weight:');
+      await expect(progearSection).toContainText('2,000 lbs');
+    });
+  });
+});
