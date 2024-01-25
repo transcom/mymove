f8e98f81edd2a66d0a0ac2f498d7ff05043ee1b9
TevinAdams <tevin.adams@caci.com>
2024-01-25 19:56:59 +0000

    Used skip for tests

--- playwright/tests/my/milmove/ppms/estimatedIncentive.spec.js
+++ playwright/tests/my/milmove/ppms/estimatedIncentive.spec.js
@@ -5,21 +5,21 @@
  */
 
 // @ts-check
-// import { expect, test, forEachViewport } from './customerPpmTestFixture';
+import { expect, test, forEachViewport } from './customerPpmTestFixture';
 
-// test.describe('PPM Onboarding - Estimated Incentive', () => {
-//   forEachViewport(async ({ isMobile }) => {
-//     test.beforeEach(async ({ customerPpmPage }) => {
-//       const move = await customerPpmPage.testHarness.buildUnSubmittedMoveWithPPMShipmentThroughEstimatedWeights();
-//       await customerPpmPage.signInForPPMWithMove(move);
-//       await customerPpmPage.navigateFromHomePageToExistingPPMDateAndLocationPage();
-//       await customerPpmPage.navigateFromDateAndLocationPageToEstimatedWeightsPage();
-//       await customerPpmPage.navigateFromEstimatedWeightsPageToEstimatedIncentivePage();
-//       await expect(customerPpmPage.page.locator('.container h2')).toContainText('$10,000');
-//     });
+test.describe.skip('PPM Onboarding - Estimated Incentive', () => {
+  forEachViewport(async ({ isMobile }) => {
+    test.beforeEach(async ({ customerPpmPage }) => {
+      const move = await customerPpmPage.testHarness.buildUnSubmittedMoveWithPPMShipmentThroughEstimatedWeights();
+      await customerPpmPage.signInForPPMWithMove(move);
+      await customerPpmPage.navigateFromHomePageToExistingPPMDateAndLocationPage();
+      await customerPpmPage.navigateFromDateAndLocationPageToEstimatedWeightsPage();
+      await customerPpmPage.navigateFromEstimatedWeightsPageToEstimatedIncentivePage();
+      await expect(customerPpmPage.page.locator('.container h2')).toContainText('$10,000');
+    });
 
-//     test('go to estimated incentives page', async ({ customerPpmPage }) => {
-//       await customerPpmPage.generalVerifyEstimatedIncentivePage({ isMobile });
-//     });
-//   });
-// });
+    test('go to estimated incentives page', async ({ customerPpmPage }) => {
+      await customerPpmPage.generalVerifyEstimatedIncentivePage({ isMobile });
+    });
+  });
+});
--- playwright/tests/my/milmove/ppms/navigateToUploadDocs.spec.js
+++ playwright/tests/my/milmove/ppms/navigateToUploadDocs.spec.js
@@ -5,21 +5,21 @@
  */
 
 // @ts-check
-// import { expect, test, forEachViewport } from './customerPpmTestFixture';
+import { expect, test, forEachViewport } from './customerPpmTestFixture';
 
-// test.describe('PPM Request Payment - Begin providing documents flow', () => {
-//   forEachViewport(async () => {
-//     test.beforeEach(async ({ customerPpmPage }) => {
-//       const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
-//       await customerPpmPage.signInForPPMWithMove(move);
-//     });
+test.describe.skip('PPM Request Payment - Begin providing documents flow', () => {
+  forEachViewport(async () => {
+    test.beforeEach(async ({ customerPpmPage }) => {
+      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
+      await customerPpmPage.signInForPPMWithMove(move);
+    });
 
-//     test('has upload documents button enabled', async ({ page }) => {
-//       await expect(page.getByRole('heading', { name: 'Your move is in progress.' })).toBeVisible();
-//       const stepContainer5 = page.getByTestId('stepContainer5');
-//       await expect(stepContainer5.locator('p').getByText('15 Apr 2022')).toBeVisible();
-//       await stepContainer5.getByRole('button', { name: 'Upload PPM Documents' }).click();
-//       await expect(page).toHaveURL(/\/moves\/[^/]+\/shipments\/[^/]+\/about/);
-//     });
-//   });
-// });
+    test('has upload documents button enabled', async ({ page }) => {
+      await expect(page.getByRole('heading', { name: 'Your move is in progress.' })).toBeVisible();
+      const stepContainer5 = page.getByTestId('stepContainer5');
+      await expect(stepContainer5.locator('p').getByText('15 Apr 2022')).toBeVisible();
+      await stepContainer5.getByRole('button', { name: 'Upload PPM Documents' }).click();
+      await expect(page).toHaveURL(/\/moves\/[^/]+\/shipments\/[^/]+\/about/);
+    });
+  });
+});
