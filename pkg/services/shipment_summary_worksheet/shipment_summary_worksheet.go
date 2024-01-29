6ce863f0a00849f81334001787fec5a1264936b2
antgmann <anthony.mann@caci.com>
2024-01-29 16:45:33 +0000

    renamed generator function for reusability

--- pkg/services/shipment_summary_worksheet/shipment_summary_worksheet.go
+++ pkg/services/shipment_summary_worksheet/shipment_summary_worksheet.go
@@ -854,7 +854,7 @@ func (SSWPPMGenerator *SSWPPMGenerator) FillSSWPDFForm(Page1Values services.Page
 		fmt.Println("Error marshaling JSON:", err)
 		return
 	}
-	SSWWorksheet, err := SSWPPMGenerator.generator.FillPDFFormForSSW(jsonData, SSWPPMGenerator.templateReader)
+	SSWWorksheet, err := SSWPPMGenerator.generator.FillPDFForm(jsonData, SSWPPMGenerator.templateReader)
 	if err != nil {
 		return nil, nil, err
 	}
