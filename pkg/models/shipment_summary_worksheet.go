e382492465c163c14ab3de7823fb32435112be7a
joeydoyecaci <joseph.doye@caci.com>
2024-02-07 14:11:22 +0000

    Added Unit test fixes to use PPMShipment and it's linked properties.

--- pkg/models/shipment_summary_worksheet.go
+++ pkg/models/shipment_summary_worksheet.go
@@ -199,13 +199,21 @@ func (obligation Obligation) MaxAdvance() float64 {
 
 // FetchDataShipmentSummaryWorksheetFormData fetches the pages for the Shipment Summary Worksheet for a given Move ID
 func FetchDataShipmentSummaryWorksheetFormData(db *pop.Connection, session *auth.Session, moveID uuid.UUID) (ShipmentSummaryFormData, error) {
+	if moveID == uuid.Nil {
+		return ShipmentSummaryFormData{}, ErrInvalidMoveID
+	}
 	move := Move{}
 	dbQErr := db.Q().Eager(
 		"Orders",
 		"Orders.NewDutyLocation.Address",
 		"Orders.ServiceMember",
-		"Orders.OriginDutyLocation.Address",
-		"PersonallyProcuredMoves",
+		"Orders.ServiceMember.DutyLocation.Address",
+		"MTOShipments",
+		"MTOShipments.MoveTaskOrder",
+		"MTOShipments.PPMShipment",
+		"MTOShipments.PPMShipment.Status",
+		"MTOShipments.PPMShipment.SignedCertification",
+		"MTOShipments.PPMShipment.MovingExpenses",
 	).Find(&move, moveID)
 
 	if dbQErr != nil {
@@ -243,27 +251,27 @@ func FetchDataShipmentSummaryWorksheetFormData(db *pop.Connection, session *auth
 	if err != nil {
 		return ShipmentSummaryFormData{}, err
 	}
+	var signedCertification SignedCertification
+	var allPPMShipments PPMShipments
+	for _, shipment := range move.MTOShipments {
+		// get the first signed certification found
+		if shipment.PPMShipment.SignedCertification != nil {
+			signedCertification = *shipment.PPMShipment.SignedCertification
+		}
+		if shipment.PPMShipment != nil {
+			allPPMShipments = append(allPPMShipments, *shipment.PPMShipment)
+		}
 
-	signedCertification, err := FetchSignedCertificationsPPMPayment(db, session, moveID)
-	if err != nil {
-		return ShipmentSummaryFormData{}, err
 	}
-	if signedCertification == nil {
-		return ShipmentSummaryFormData{},
-			errors.New("shipment summary worksheet: signed certification is nil")
-	}
-	if move.Orders.OriginDutyLocation == nil {
-		return ShipmentSummaryFormData{},
-			errors.New("shipment summary worksheet: orders origin duty location is nil")
-	}
+
 	ssd := ShipmentSummaryFormData{
 		ServiceMember:           serviceMember,
 		Order:                   move.Orders,
 		CurrentDutyLocation:     *move.Orders.OriginDutyLocation,
 		NewDutyLocation:         move.Orders.NewDutyLocation,
 		WeightAllotment:         weightAllotment,
-		PersonallyProcuredMoves: move.PersonallyProcuredMoves,
-		SignedCertification:     *signedCertification,
+		SignedCertification:     signedCertification,
+		SSPPMShipments:          allPPMShipments,
 		PPMRemainingEntitlement: ppmRemainingEntitlement,
 	}
 	return ssd, nil
@@ -545,8 +553,8 @@ func getExpenseType(expense MovingExpense) string {
 }
 
 // FormatCurrentPPMStatus formats FormatCurrentPPMStatus for the Shipment Summary Worksheet
-func FormatCurrentPPMStatus(ppm PersonallyProcuredMove) string {
-	if ppm.Status == "PAYMENT_REQUESTED" {
+func FormatCurrentPPMStatus(ppm PPMShipment) string {
+	if ppm.Status == "NEEDS_PAYMENT_APPROVAL" {
 		return "At destination"
 	}
 	return FormatEnum(string(ppm.Status), " ")
@@ -558,12 +566,18 @@ func FormatPPMNumberAndType(i int) string {
 }
 
 // FormatPPMWeight formats a ppms NetWeight for the Shipment Summary Worksheet
-func FormatPPMWeight(ppm PersonallyProcuredMove) string {
-	if ppm.NetWeight != nil {
-		wtg := FormatWeights(unit.Pound(*ppm.NetWeight))
-		return fmt.Sprintf("%s lbs - FINAL", wtg)
+func FormatPPMWeight(ppm PPMShipment) string {
+	totalWeight := getNetWeightForWeightTicketsInPPMShipment(ppm)
+	wtg := FormatWeights(unit.Pound(totalWeight))
+	return fmt.Sprintf("%s lbs - FINAL", wtg)
+
+}
+func getNetWeightForWeightTicketsInPPMShipment(ppm PPMShipment) unit.Pound {
+	var totalNetWeight unit.Pound
+	for _, weightTicket := range ppm.WeightTickets {
+		totalNetWeight += *weightTicket.AdjustedNetWeight
 	}
-	return ""
+	return totalNetWeight
 }
 
 // FormatPPMPickupDate formats a shipments ActualPickupDate for the Shipment Summary Worksheet
