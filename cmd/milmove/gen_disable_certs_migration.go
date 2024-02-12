d65ecfcb771a8ae199b652cdc23d819f6aa7e516
Edward Harris <edward.harris@caci.com>
2024-02-12 16:34:43 +0000

    B-18528-API-CERT-DISABLE-2

--- cmd/milmove/gen_disable_certs_migration.go
+++ cmd/milmove/gen_disable_certs_migration.go
@@ -34,6 +34,7 @@ DELETE FROM users_roles
 where
 user_id = (select id from users where okta_email = '{{.Fingerprint}}@api.move.mil');
 
+UPDTE users
 SET active=false
 WHERE
 okta_email='{{.Fingerprint}}@api.move.mil';
