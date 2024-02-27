2412fef16990b723199321b95018c61e61ec52bb
TevinAdams <tevin.adams@caci.com>
2024-02-27 20:40:06 +0000

    Annotation added

--- playwright/tests/globalSetup.js
+++ playwright/tests/globalSetup.js
@@ -1,4 +1,5 @@
 // globalSetup.js
+// Summary: @typescript-eslint/no-var-requires disallows the use of require statements except in import statements. This is for the most part only a syntactical rule. If you don't care about TypeScript module syntax, then you will not need this rule.
 /* eslint-disable @typescript-eslint/no-var-requires */
 const dotenv = require('dotenv');
 
