9b81712003b230582b19819d350bea3b318b5aff
TevinAdams <tevin.adams@caci.com>
2024-02-27 19:09:00 +0000

    Remove unneeded changes

--- playwright/tests/utils/my/customerTest.js
+++ playwright/tests/utils/my/customerTest.js
@@ -48,7 +48,7 @@ export class CustomerPage extends BaseTestPage {
   async signInAsExistingCustomer(userId) {
     await this.signInAsUserWithId(userId);
     // ensure the home page has loaded
-    await this.page.getByText('Welcome to MilMove!');
+    await this.page.getByLabel('Home').waitFor();
   }
 
   /**
