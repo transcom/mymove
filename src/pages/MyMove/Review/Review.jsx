9aae9b75cb5de08fe5b886c4d09ce705594b3cef
Daniel Jordan <daniel.jordan@caci.com>
2024-02-27 20:02:39 +0000

    updated creating shipment flow to use params and updated review screen to look at right move

--- src/pages/MyMove/Review/Review.jsx
+++ src/pages/MyMove/Review/Review.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { arrayOf } from 'prop-types';
 import { connect } from 'react-redux';
 import { GridContainer, Grid } from '@trussworks/react-uswds';
 import { generatePath, useNavigate, useParams } from 'react-router-dom';
@@ -10,23 +9,38 @@ import ConnectedFlashMessage from 'containers/FlashMessage/FlashMessage';
 import ConnectedSummary from 'components/Customer/Review/Summary/Summary';
 import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
 import MOVE_STATUSES from 'constants/moves';
-import { customerRoutes, generalRoutes } from 'constants/routes';
+import { customerRoutes } from 'constants/routes';
 import 'scenes/Review/Review.css';
-import { selectCurrentMove, selectMTOShipmentsForCurrentMove } from 'store/entities/selectors';
+import { selectAllMoves } from 'store/entities/selectors';
 import formStyles from 'styles/form.module.scss';
 import { SHIPMENT_OPTIONS } from 'shared/constants';
-import { MoveShape } from 'types/customerShapes';
-import { ShipmentShape } from 'types/shipment';
 import { isPPMShipmentComplete } from 'utils/shipments';
 import { useTitle } from 'hooks/custom';
+import LoadingPlaceholder from 'shared/LoadingPlaceholder';
 
-const Review = ({ currentMove, mtoShipments }) => {
+const Review = ({ serviceMemberMoves }) => {
   useTitle('Move review');
   const navigate = useNavigate();
   const { moveId } = useParams();
   const handleCancel = () => {
-    navigate(generalRoutes.HOME_PATH);
+    navigate(`/move/${moveId}`);
   };
+
+  // loading placeholder while data loads - this handles any async issues
+  if (!serviceMemberMoves || !serviceMemberMoves.currentMove || !serviceMemberMoves.previousMoves) {
+    return (
+      <div className={styles.homeContainer}>
+        <div className={`usa-prose grid-container ${styles['grid-container']}`}>
+          <LoadingPlaceholder />
+        </div>
+      </div>
+    );
+  }
+
+  const currentMove = serviceMemberMoves.currentMove.find((m) => m.id === moveId);
+  const previousMove = serviceMemberMoves.previousMoves.find((m) => m.id === moveId);
+  const move = currentMove || previousMove;
+  const { mtoShipments } = move;
 
   const handleNext = () => {
     const nextPath = generatePath(customerRoutes.MOVE_AGREEMENT_PATH, {
@@ -35,7 +49,7 @@ const Review = ({ currentMove, mtoShipments }) => {
     navigate(nextPath);
   };
 
-  const inDraftStatus = currentMove.status === MOVE_STATUSES.DRAFT;
+  const inDraftStatus = move.status === MOVE_STATUSES.DRAFT;
 
   // PPM shipments can be left in an incomplete state, disable proceeding to the signature move
   // submission page to force them to complete or delete the shipment.
@@ -78,16 +92,11 @@ const Review = ({ currentMove, mtoShipments }) => {
   );
 };
 
-Review.propTypes = {
-  currentMove: MoveShape.isRequired,
-  mtoShipments: arrayOf(ShipmentShape).isRequired,
-};
-
 const mapStateToProps = (state, ownProps) => {
+  const serviceMemberMoves = selectAllMoves(state);
   return {
     ...ownProps,
-    currentMove: selectCurrentMove(state) || {},
-    mtoShipments: selectMTOShipmentsForCurrentMove(state) || [],
+    serviceMemberMoves,
   };
 };
 
