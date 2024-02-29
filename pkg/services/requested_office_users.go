b07fdbf18eb6a5b7a832af697a0fd262a0f43c04
Daniel Jordan <daniel.jordan@caci.com>
2024-02-29 22:18:53 +0000

    initial commit, happy path, need tests

--- pkg/services/requested_office_users.go
+++ pkg/services/requested_office_users.go
@@ -5,10 +5,17 @@ import (
 	"github.com/transcom/mymove/pkg/models"
 )
 
-// RequestedOfficeUserListFetcher is the exported interface for fetching multiple admin users
-//
+// RequestedOfficeUserListFetcher is the exported interface for fetching multiple requested office users
+
 //go:generate mockery --name RequestedOfficeUserListFetcher
 type RequestedOfficeUserListFetcher interface {
 	FetchRequestedOfficeUsersList(appCtx appcontext.AppContext, filters []QueryFilter, associations QueryAssociations, pagination Pagination, ordering QueryOrder) (models.OfficeUsers, error)
 	FetchRequestedOfficeUsersCount(appCtx appcontext.AppContext, filters []QueryFilter) (int, error)
+}
+
+// RequestedOfficeUserFetcher is the exported interface for fetching a single requested office user
+//
+//go:generate mockery --name RequestedOfficeUserFetcher
+type RequestedOfficeUserFetcher interface {
+	FetchRequestedOfficeUser(appCtx appcontext.AppContext, filters []QueryFilter) (models.OfficeUser, error)
 }
