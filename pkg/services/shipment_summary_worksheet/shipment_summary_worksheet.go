9000a0b8a618765bfaa3201b607588335e1a4a22
antgmann <anthony.mann@caci.com>
2024-03-04 20:41:38 +0000

    Font size fix for data fields

--- pkg/services/shipment_summary_worksheet/shipment_summary_worksheet.go
+++ pkg/services/shipment_summary_worksheet/shipment_summary_worksheet.go
@@ -929,7 +929,7 @@ func (SSWPPMGenerator *SSWPPMGenerator) FillSSWPDFForm(Page1Values services.Page
 	var sswHeader = header{
 		Source:   "SSWPDFTemplate.pdf",
 		Version:  "pdfcpu v0.6.0 dev",
-		Creation: "2024-03-04 19:20:05 UTC",
+		Creation: "2024-03-04 20:30:49 UTC",
 		Producer: "macOS Version 13.5 (Build 22G74) Quartz PDFContext, AppendMode 1.1",
 	}
 
