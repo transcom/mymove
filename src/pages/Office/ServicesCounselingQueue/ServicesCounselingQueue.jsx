4ec5b2e5691b312fc10770bd3d7f8b2b16465dd0
deandreJones <deandre.jones@caci.com>
2024-03-06 08:14:17 -0600

    initial

--- src/pages/Office/ServicesCounselingQueue/ServicesCounselingQueue.jsx
+++ src/pages/Office/ServicesCounselingQueue/ServicesCounselingQueue.jsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useState } from 'react';
 import { generatePath, useNavigate, Navigate, useParams, NavLink } from 'react-router-dom';
+import { Button } from '@trussworks/react-uswds';
 
 import styles from './ServicesCounselingQueue.module.scss';
 
+import { isBooleanFlagEnabled } from 'utils/featureFlags';
 import { createHeader } from 'components/Table/utils';
 import MultiSelectCheckBoxFilter from 'components/Table/Filters/MultiSelectCheckBoxFilter';
 import SelectFilter from 'components/Table/Filters/SelectFilter';
@@ -196,8 +198,13 @@ const ServicesCounselingQueue = () => {
     navigate(generatePath(servicesCounselingRoutes.BASE_MOVE_VIEW_PATH, { moveCode: values.locator }));
   };
 
+  const handleAddCustomerClick = () => {
+    navigate(generatePath(servicesCounselingRoutes.NAME_PATH));
+  };
+
   const [search, setSearch] = useState({ moveCode: null, dodID: null, customerName: null });
   const [searchHappened, setSearchHappened] = useState(false);
+  const counselorMoveCreateFeatureFlag = isBooleanFlagEnabled('counselor_move_create');
 
   const onSubmit = useCallback((values) => {
     const payload = {
@@ -291,6 +298,18 @@ const ServicesCounselingQueue = () => {
             roleType={roleTypes.SERVICES_COUNSELOR}
           />
         )}
+        {searchHappened &&
+          counselorMoveCreateFeatureFlag(
+            <Button
+              secondary={false}
+              type="Add Customer"
+              style={{ maxWidth: '225px' }}
+              hidden={false}
+              onClick={handleAddCustomerClick}
+            >
+              Add Customer
+            </Button>,
+          )}
       </div>
     );
   }
--- src/pages/Office/index.jsx
+++ src/pages/Office/index.jsx
@@ -41,6 +41,7 @@ import PrimeBanner from 'pages/PrimeUI/PrimeBanner/PrimeBanner';
 import PermissionProvider from 'components/Restricted/PermissionProvider';
 import withRouter from 'utils/routing';
 import { OktaLoggedOutBanner, OktaNeedsLoggedOutBanner } from 'components/OktaLogoutBanner';
+import SMName from 'pages/MyMove/Profile/Name';
 
 // Lazy load these dependencies (they correspond to unique routes & only need to be loaded when that URL is accessed)
 const SignIn = lazy(() => import('pages/SignIn/SignIn'));
@@ -273,6 +274,14 @@ export class OfficeApp extends Component {
                         }
                       />
                     )}
+                    <Route
+                      path={servicesCounselingRoutes.NAME_PATH}
+                      element={
+                        <PrivateRoute requiredRoles={[roleTypes.SERVICES_COUNSELOR]}>
+                          <SMName />,
+                        </PrivateRoute>
+                      }
+                    />
 
                     <Route
                       key="servicesCounselingMoveInfoRoute"
