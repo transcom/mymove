680f1625b3be395a42d3bb24c70616186faf97bf
cameroncaci <cameron.jewell@caci.com>
2024-03-08 18:09:47 +0000

    added min domestic weight conversion to DLH

--- pkg/services/ghcrateengine/domestic_linehaul_pricer.go
+++ pkg/services/ghcrateengine/domestic_linehaul_pricer.go
@@ -38,6 +38,7 @@ func (p domesticLinehaulPricer) Price(appCtx appcontext.AppContext, contractCode
 	if weight < 0 {
 		return 0, nil, fmt.Errorf("weight %d is not a valid number", weight)
 	}
+	convertUnderMinWeightToMinWeight(disableMinimimWeight, &weight)
 	if len(serviceArea) == 0 {
 		return 0, nil, errors.New("ServiceArea is required")
 	}
