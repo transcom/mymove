70af16715a324f6c798adba7300840f837764a94
Pam Becker <pamela.becker@caci.com>
2024-03-11 15:39:47 -0500

    cleanup

--- src/pages/Admin/OfficeUsers/OfficeUserEdit.jsx
+++ src/pages/Admin/OfficeUsers/OfficeUserEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   Edit,
   SimpleForm,
@@ -21,61 +21,44 @@ const OfficeUserEditToolbar = (props) => (
   </Toolbar>
 );
 
-const OfficeUserEdit = () => {
-  const [isDisabledPrivileges, setIsDisabledPrivileges] = useState(false);
-
-  const validatePrivileges = (input) => {
-    for (let i = 0; i < input?.length; i += 1) {
-      if (input[i] === 'customer' || input[i] === 'contracting_officer') {
-        setIsDisabledPrivileges(true);
-        return;
-      }
-    }
-    setIsDisabledPrivileges(false);
-  };
-
-  useEffect(() => {
-    validatePrivileges();
-  }, []);
-  return (
-    <Edit>
-      <SimpleForm
-        toolbar={<OfficeUserEditToolbar />}
-        sx={{ '& .MuiInputBase-input': { width: 232 } }}
-        mode="onBlur"
-        reValidateMode="onBlur"
+const OfficeUserEdit = () => (
+  <Edit>
+    <SimpleForm
+      toolbar={<OfficeUserEditToolbar />}
+      sx={{ '& .MuiInputBase-input': { width: 232 } }}
+      mode="onBlur"
+      reValidateMode="onBlur"
+    >
+      <TextInput source="id" disabled />
+      <TextInput source="userId" label="User Id" disabled />
+      <TextInput source="email" disabled />
+      <TextInput source="firstName" validate={required()} />
+      <TextInput source="middleInitials" />
+      <TextInput source="lastName" validate={required()} />
+      <TextInput source="telephone" validate={phoneValidators} />
+      <SelectInput
+        source="active"
+        choices={[
+          { id: true, name: 'Yes' },
+          { id: false, name: 'No' },
+        ]}
+        sx={{ width: 256 }}
+      />
+      <RolesCheckboxInput source="roles" validate={required()} />
+      <PrivilegesCheckboxInput source="privileges" />
+      <ReferenceInput
+        label="Transportation Office"
+        reference="offices"
+        source="transportationOfficeId"
+        perPage={500}
+        validate={required()}
       >
-        <TextInput source="id" disabled />
-        <TextInput source="userId" label="User Id" disabled />
-        <TextInput source="email" disabled />
-        <TextInput source="firstName" validate={required()} />
-        <TextInput source="middleInitials" />
-        <TextInput source="lastName" validate={required()} />
-        <TextInput source="telephone" validate={phoneValidators} />
-        <SelectInput
-          source="active"
-          choices={[
-            { id: true, name: 'Yes' },
-            { id: false, name: 'No' },
-          ]}
-          sx={{ width: 256 }}
-        />
-        <RolesCheckboxInput source="roles" validate={required()} onChange={validatePrivileges} />
-        <PrivilegesCheckboxInput source="privileges" disabled={isDisabledPrivileges} />
-        <ReferenceInput
-          label="Transportation Office"
-          reference="offices"
-          source="transportationOfficeId"
-          perPage={500}
-          validate={required()}
-        >
-          <AutocompleteInput optionText="name" sx={{ width: 256 }} />
-        </ReferenceInput>
-        <TextInput source="createdAt" disabled />
-        <TextInput source="updatedAt" disabled />
-      </SimpleForm>
-    </Edit>
-  );
-};
+        <AutocompleteInput optionText="name" sx={{ width: 256 }} />
+      </ReferenceInput>
+      <TextInput source="createdAt" disabled />
+      <TextInput source="updatedAt" disabled />
+    </SimpleForm>
+  </Edit>
+);
 
 export default OfficeUserEdit;
