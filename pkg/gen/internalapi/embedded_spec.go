6dc71702f52cba8d3f2dda390c26441bc3144a9d
TevinAdams <tevin.adams@caci.com>
2024-03-12 20:00:47 +0000

    Branch update

--- pkg/gen/internalapi/embedded_spec.go
+++ pkg/gen/internalapi/embedded_spec.go
@@ -5332,6 +5332,10 @@ func init() {
           "x-nullable": true,
           "$ref": "#/definitions/DutyLocationPayload"
         },
+        "provides_services_counseling": {
+          "type": "boolean",
+          "x-omitempty": false
+        },
         "report_by_date": {
           "description": "Report By Date",
           "type": "string",
@@ -5438,13 +5442,15 @@ func init() {
       "x-nullable": true
     },
     "PPMAdvanceStatus": {
-      "description": "Indicates whether an advance status has been accepted, rejected, or edited.",
+      "description": "Indicates whether an advance status has been accepted, rejected, or edited, or a prime counseled PPM has been received or not received",
       "type": "string",
       "title": "PPM Advance Status",
       "enum": [
         "APPROVED",
         "REJECTED",
-        "EDITED"
+        "EDITED",
+        "RECEIVED",
+        "NOT_RECEIVED"
       ],
       "x-nullable": true
     },
@@ -13144,6 +13150,10 @@ func init() {
           "x-nullable": true,
           "$ref": "#/definitions/DutyLocationPayload"
         },
+        "provides_services_counseling": {
+          "type": "boolean",
+          "x-omitempty": false
+        },
         "report_by_date": {
           "description": "Report By Date",
           "type": "string",
@@ -13250,13 +13260,15 @@ func init() {
       "x-nullable": true
     },
     "PPMAdvanceStatus": {
-      "description": "Indicates whether an advance status has been accepted, rejected, or edited.",
+      "description": "Indicates whether an advance status has been accepted, rejected, or edited, or a prime counseled PPM has been received or not received",
       "type": "string",
       "title": "PPM Advance Status",
       "enum": [
         "APPROVED",
         "REJECTED",
-        "EDITED"
+        "EDITED",
+        "RECEIVED",
+        "NOT_RECEIVED"
       ],
       "x-nullable": true
     },
