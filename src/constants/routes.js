e6c3e4fabe5e6a05736296664bcb85ce8dad6157
deandreJones <deandre.jones@caci.com>
2024-03-12 21:00:37 +0000

    B-18913 retryy

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -80,6 +80,7 @@ export const servicesCounselingRoutes = {
   SHIPMENT_REVIEW_PATH: 'shipments/:shipmentId/document-review',
   BASE_REVIEW_SHIPMENT_WEIGHTS_PATH: `${BASE_COUNSELING_MOVE_PATH}/review-shipment-weights`,
   REVIEW_SHIPMENT_WEIGHTS_PATH: 'review-shipment-weights',
+  CUSTOMER_NAME_PATH: '/Onboarding/customerName',
 };
 
 const BASE_MOVES_PATH = '/moves/:moveCode';
