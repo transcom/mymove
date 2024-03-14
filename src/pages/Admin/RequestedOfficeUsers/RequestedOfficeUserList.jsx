04c53a088d8a602f66c333204c4ec3437b08822a
Daniel Jordan <daniel.jordan@caci.com>
2024-03-14 13:43:31 +0000

    merge of c73ab71 commit

---
+++ src/pages/Admin/RequestedOfficeUsers/RequestedOfficeUserList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Datagrid, DateField, Filter, List, ReferenceField, TextField, TextInput, TopToolbar } from 'react-admin';
+
+import AdminPagination from 'scenes/SystemAdmin/shared/AdminPagination';
+
+// Overriding the default toolbar to add import button
+const ListActions = () => {
+  return <TopToolbar />;
+};
+
+const RequestedOfficeUserListFilter = () => (
+  <Filter>
+    <TextInput source="search" alwaysOn />
+  </Filter>
+);
+
+const defaultSort = { field: 'createdAt', order: 'DESC' };
+
+const RequestedOfficeUserList = () => (
+  <List
+    pagination={<AdminPagination />}
+    perPage={25}
+    sort={defaultSort}
+    filters={<RequestedOfficeUserListFilter />}
+    actions={<ListActions />}
+  >
+    <Datagrid bulkActionButtons={false} rowClick="show">
+      <TextField source="id" />
+      <TextField source="email" />
+      <TextField source="firstName" />
+      <TextField source="lastName" />
+      <ReferenceField label="Transportation Office" source="transportationOfficeId" reference="offices" link={false}>
+        <TextField source="name" />
+      </ReferenceField>
+      <TextField source="userId" label="User Id" />
+      <TextField source="status" />
+      <DateField showTime source="createdAt" label="Requested on" />
+    </Datagrid>
+  </List>
+);
+
+export default RequestedOfficeUserList;
