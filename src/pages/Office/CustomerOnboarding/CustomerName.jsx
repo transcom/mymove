ba1b80636bc47dc3392a0d9f8df606f4c163f19b
Daniel Jordan <daniel.jordan@caci.com>
2024-03-15 19:12:06 +0000

    initial commit, created form and validation, need to add tests

--- src/pages/Office/CustomerOnboarding/CustomerName.jsx
+++ src/pages/Office/CustomerOnboarding/CustomerName.jsx
@@ -1,54 +1,156 @@
-import PropTypes from 'prop-types';
 import React, { useState } from 'react';
-import { GridContainer, Grid, Alert } from '@trussworks/react-uswds';
+import { GridContainer, Grid, Alert, Label, Radio, Fieldset } from '@trussworks/react-uswds';
 import { useNavigate } from 'react-router-dom';
+import { Field, Formik } from 'formik';
+import * as Yup from 'yup';
 
 import styles from './CustomerName.module.scss';
 
+import { Form } from 'components/form/Form';
+import TextField from 'components/form/fields/TextField/TextField';
 import NotificationScrollToTop from 'components/NotificationScrollToTop';
-import NameForm from 'components/Customer/NameForm/NameForm';
-import { patchServiceMember, getResponseError } from 'services/internalApi';
-import { ServiceMemberShape } from 'types/customerShapes';
 import { servicesCounselingRoutes } from 'constants/routes';
+import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
+import SectionWrapper from 'components/Customer/SectionWrapper';
+import formStyles from 'styles/form.module.scss';
+import { CheckboxField, DropdownInput } from 'components/form/fields';
+import { dropdownInputOptions } from 'utils/formatters';
+import { SERVICE_MEMBER_AGENCY_LABELS } from 'content/serviceMemberAgencies';
+import MaskedTextField from 'components/form/fields/MaskedTextField/MaskedTextField';
+import { backupContactInfoSchema, requiredAddressSchema } from 'utils/validation';
 
-export const CustomerName = ({ serviceMember, updateServiceMember }) => {
-  const [serverError, setServerError] = useState(null);
+export const CustomerName = () => {
+  const [serverError] = useState(null);
   const navigate = useNavigate();
-  const initialValues = {
-    first_name: serviceMember?.first_name || '',
-    middle_name: serviceMember?.middle_name || '',
-    last_name: serviceMember?.last_name || '',
-    suffix: serviceMember?.suffix || '',
-  };
 
-  const handleNext = () => {
-    // add next route
+  const branchOptions = dropdownInputOptions(SERVICE_MEMBER_AGENCY_LABELS);
+  const statesList = [
+    { value: 'AL', key: 'AL' },
+    { value: 'AK', key: 'AK' },
+    { value: 'AR', key: 'AR' },
+    { value: 'AZ', key: 'AZ' },
+    { value: 'CA', key: 'CA' },
+    { value: 'CO', key: 'CO' },
+    { value: 'CT', key: 'CT' },
+    { value: 'DC', key: 'DC' },
+    { value: 'DE', key: 'DE' },
+    { value: 'FL', key: 'FL' },
+    { value: 'GA', key: 'GA' },
+    { value: 'HI', key: 'HI' },
+    { value: 'IA', key: 'IA' },
+    { value: 'ID', key: 'ID' },
+    { value: 'IL', key: 'IL' },
+    { value: 'IN', key: 'IN' },
+    { value: 'KS', key: 'KS' },
+    { value: 'KY', key: 'KY' },
+    { value: 'LA', key: 'LA' },
+    { value: 'MA', key: 'MA' },
+    { value: 'MD', key: 'MD' },
+    { value: 'ME', key: 'ME' },
+    { value: 'MI', key: 'MI' },
+    { value: 'MN', key: 'MN' },
+    { value: 'MO', key: 'MO' },
+    { value: 'MS', key: 'MS' },
+    { value: 'MT', key: 'MT' },
+    { value: 'NC', key: 'NC' },
+    { value: 'ND', key: 'ND' },
+    { value: 'NE', key: 'NE' },
+    { value: 'NH', key: 'NH' },
+    { value: 'NJ', key: 'NJ' },
+    { value: 'NM', key: 'NM' },
+    { value: 'NV', key: 'NV' },
+    { value: 'NY', key: 'NY' },
+    { value: 'OH', key: 'OH' },
+    { value: 'OK', key: 'OK' },
+    { value: 'OR', key: 'OR' },
+    { value: 'PA', key: 'PA' },
+    { value: 'RI', key: 'RI' },
+    { value: 'SC', key: 'SC' },
+    { value: 'SD', key: 'SD' },
+    { value: 'TN', key: 'TN' },
+    { value: 'TX', key: 'TX' },
+    { value: 'UT', key: 'UT' },
+    { value: 'VA', key: 'VA' },
+    { value: 'VT', key: 'VT' },
+    { value: 'WA', key: 'WA' },
+    { value: 'WI', key: 'WI' },
+    { value: 'WV', key: 'WV' },
+    { value: 'WY', key: 'WY' },
+  ];
+
+  const residentialAddressName = 'residential_address';
+  const backupAddressName = 'backup_mailing_address';
+  const backupContactName = 'backup_contact';
+
+  const initialValues = {
+    affiliation: '',
+    edipi: '',
+    first_name: '',
+    middle_name: '',
+    last_name: '',
+    suffix: '',
+    telephone: '',
+    secondary_telephone: '',
+    personal_email: '',
+    phone_is_preferred: '',
+    email_is_preferred: '',
+    [residentialAddressName]: {
+      streetAddress1: '',
+      streetAddress2: '',
+      streetAddress3: '',
+      city: '',
+      state: '',
+      postalCode: '',
+    },
+    [backupAddressName]: {
+      streetAddress1: '',
+      streetAddress2: '',
+      streetAddress3: '',
+      city: '',
+      state: '',
+      postalCode: '',
+    },
+    [backupContactName]: {
+      name: '',
+      telephone: '',
+      email: '',
+    },
+    create_okta_account: '',
   };
 
   const handleBack = () => {
     navigate(servicesCounselingRoutes.BASE_QUEUE_SEARCH_PATH);
   };
 
-  const handleSubmit = (values) => {
-    const payload = {
-      id: serviceMember.id,
-      first_name: values.first_name,
-      middle_name: values.middle_name,
-      last_name: values.last_name,
-      suffix: values.suffix,
-    };
-
-    return patchServiceMember(payload)
-      .then(updateServiceMember)
-      .then(handleNext)
-      .catch((e) => {
-        // TODO - error handling - below is rudimentary error handling to approximate existing UX
-        // Error shape: https://github.com/swagger-api/swagger-js/blob/master/docs/usage/http-client.md#errors
-        const { response } = e;
-        const errorMessage = getResponseError(response, 'failed to update service member due to server error');
-        setServerError(errorMessage);
-      });
+  const onSubmit = () => {
+    // pass in values and call the API
+    navigate(servicesCounselingRoutes.BASE_QUEUE_SEARCH_PATH);
   };
+
+  const validationSchema = Yup.object().shape({
+    affiliation: Yup.mixed().oneOf(Object.keys(SERVICE_MEMBER_AGENCY_LABELS)).required('Required'),
+    edipi: Yup.string().matches(/[0-9]{10}/, 'Enter a 10-digit DOD ID number'),
+    first_name: Yup.string().required('Required'),
+    middle_name: Yup.string(),
+    last_name: Yup.string().required('Required'),
+    suffix: Yup.string(),
+    telephone: Yup.string()
+      .min(12, 'Please enter a valid phone number. Phone numbers must be entered as ###-###-####.')
+      .required('Required'),
+    secondary_telephone: Yup.string().min(
+      12,
+      'Please enter a valid phone number. Phone numbers must be entered as ###-###-####.',
+    ),
+    personal_email: Yup.string()
+      .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/, 'Must be a valid email address')
+      .required('Required'),
+    phoneIsPreferred: Yup.boolean(),
+    emailIsPreferred: Yup.boolean(),
+    [residentialAddressName]: requiredAddressSchema.required(),
+    [backupAddressName]: requiredAddressSchema.required(),
+    [backupContactName]: backupContactInfoSchema.required(),
+    create_okta_account: Yup.boolean().required('Required'),
+  });
 
   return (
     <GridContainer>
@@ -66,16 +168,165 @@ export const CustomerName = ({ serviceMember, updateServiceMember }) => {
 
       <Grid className={styles.nameFormContainer}>
         <Grid col desktop={{ col: 8 }} className={styles.nameForm}>
-          <NameForm onSubmit={handleSubmit} onBack={handleBack} initialValues={initialValues} />
+          <Formik initialValues={initialValues} validateOnMount validationSchema={validationSchema} onSubmit={onSubmit}>
+            {({ isValid, handleSubmit }) => {
+              return (
+                <Form className={formStyles.form}>
+                  <h1 className={styles.header}>Create Customer Profile</h1>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Customer Affiliation</h3>
+                    <DropdownInput
+                      label="Branch of service"
+                      name="affiliation"
+                      id="affiliation"
+                      required
+                      options={branchOptions}
+                    />
+                    <TextField label="DoD ID number" name="edipi" id="edipi" labelHint="Optional" maxLength="10" />
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Customer Name</h3>
+                    <TextField label="First name" name="first_name" id="firstName" required />
+                    <TextField label="Middle name" name="middle_name" id="middleName" labelHint="Optional" />
+                    <TextField label="Last name" name="last_name" id="lastName" required />
+                    <TextField label="Suffix" name="suffix" id="suffix" labelHint="Optional" />
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Contact Info</h3>
+                    <MaskedTextField
+                      label="Best contact phone"
+                      id="telephone"
+                      name="telephone"
+                      type="tel"
+                      minimum="12"
+                      mask="000{-}000{-}0000"
+                      required
+                    />
+                    <MaskedTextField
+                      label="Alt. phone"
+                      labelHint="Optional"
+                      id="altTelephone"
+                      name="secondary_telephone"
+                      type="tel"
+                      minimum="12"
+                      mask="000{-}000{-}0000"
+                    />
+                    <TextField label="Personal email" id="personalEmail" name="personal_email" required />
+                    <Label>Preferred contact method (optional)</Label>
+                    <div className={formStyles.radioGroup}>
+                      <CheckboxField id="phoneIsPreferred" label="Phone" name="phone_is_preferred" />
+                      <CheckboxField id="emailIsPreferred" label="Email" name="email_is_preferred" />
+                    </div>
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Current Address</h3>
+                    <TextField label="Address 1" id="mailingAddress1" name="residential_address.streetAddress1" />
+                    <TextField
+                      label="Address 2"
+                      labelHint="Optional"
+                      id="mailingAddress2"
+                      name="residential_address.streetAddress2"
+                    />
+                    <TextField
+                      label="Address 3"
+                      labelHint="Optional"
+                      id="mailingAddress3"
+                      name="residential_address.streetAddress3"
+                    />
+                    <TextField label="City" id="city" name="residential_address.city" />
+
+                    <div className="grid-row grid-gap">
+                      <div className="mobile-lg:grid-col-6">
+                        <DropdownInput name="residential_address.state" id="state" label="State" options={statesList} />
+                      </div>
+                      <div className="mobile-lg:grid-col-6">
+                        <TextField label="ZIP" id="zip" name="residential_address.postalCode" maxLength={10} />
+                      </div>
+                    </div>
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Backup Address</h3>
+                    <TextField label="Address 1" id="bMailingAddress1" name="backup_mailing_address.streetAddress1" />
+                    <TextField
+                      label="Address 2"
+                      labelHint="Optional"
+                      id="mailingAddress2"
+                      name="backup_mailing_address.streetAddress2"
+                    />
+                    <TextField
+                      label="Address 3"
+                      labelHint="Optional"
+                      id="mailingAddress3"
+                      name="backup_mailing_address.streetAddress3"
+                    />
+                    <TextField label="City" id="city" name="backup_mailing_address.city" />
+
+                    <div className="grid-row grid-gap">
+                      <div className="mobile-lg:grid-col-6">
+                        <DropdownInput
+                          name="backup_mailing_address.state"
+                          id="state"
+                          label="State"
+                          options={statesList}
+                        />
+                      </div>
+                      <div className="mobile-lg:grid-col-6">
+                        <TextField label="ZIP" id="zip" name="backup_mailing_address.postalCode" maxLength={10} />
+                      </div>
+                    </div>
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Backup contact</h3>
+                    <TextField label="Name" id="backupContactName" name="backup_contact.name" required />
+                    <TextField label="Email" id="backupContaactEmail" name="backup_contact.email" required />
+                    <MaskedTextField
+                      label="Phone"
+                      id="backupContactTelephone"
+                      name="backup_contact.telephone"
+                      type="tel"
+                      minimum="12"
+                      mask="000{-}000{-}0000"
+                      required
+                    />
+                  </SectionWrapper>
+                  <SectionWrapper className={formStyles.formSection}>
+                    <h3>Okta Account</h3>
+                    <Fieldset className={styles.trailerOwnershipFieldset}>
+                      <legend className="usa-label">Do you want to create an Okta account for this customer?</legend>
+                      <div className="grid-row grid-gap">
+                        <Field
+                          as={Radio}
+                          id="yesCreateOktaAccount"
+                          label="Yes"
+                          name="create_okta_account"
+                          value="true"
+                        />
+                        <Field
+                          as={Radio}
+                          id="noCreateOktaAccount"
+                          label="No"
+                          name="create_okta_account"
+                          value="false"
+                        />
+                      </div>
+                    </Fieldset>
+                  </SectionWrapper>
+                  <div className={formStyles.formActions}>
+                    <WizardNavigation
+                      editMode
+                      onCancelClick={handleBack}
+                      disableNext={!isValid}
+                      onNextClick={handleSubmit}
+                    />
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
         </Grid>
       </Grid>
     </GridContainer>
   );
-};
-
-CustomerName.propTypes = {
-  updateServiceMember: PropTypes.func.isRequired,
-  serviceMember: ServiceMemberShape.isRequired,
 };
 
 export default CustomerName;
