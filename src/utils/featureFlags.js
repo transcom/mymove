e5ac80b7e92e391dfbfd364b0535453365bb3571
joeydoyecaci <joseph.doye@caci.com>
2024-03-15 14:45:41 +0000

    Removed duplicate declaration

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -168,21 +168,3 @@ export function isCounselorMoveCreateEnabled() {
       return false;
     });
 }
-
-export function isCounselorMoveCreateEnabled() {
-  const flagKey = 'counselor_move_create';
-  return getBooleanFeatureFlagForUser(flagKey, {})
-    .then((result) => {
-      if (result && typeof result.match !== 'undefined') {
-        // Found feature flag, "match" is its boolean value
-        return result.match;
-      }
-      throw new Error('counselor move creation feature flag is undefined');
-    })
-    .catch((error) => {
-      // On error, log it and then just return false setting it to be disabled.
-      // No need to return it for extra handling.
-      milmoveLogger.error(error);
-      return false;
-    });
-}
