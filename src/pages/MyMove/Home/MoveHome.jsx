0b5a93c2ff39e3059b4400ddd261b36c6e21429d
Logan Cunningham <logan.cunningham@caci.com>
2024-03-19 19:59:21 +0000

    added label to customer new move creation

--- src/pages/MyMove/Home/MoveHome.jsx
+++ src/pages/MyMove/Home/MoveHome.jsx
@@ -54,7 +54,6 @@ import { formatCustomerDate, formatWeight } from 'utils/formatters';
 import { isPPMAboutInfoComplete, isPPMShipmentComplete, isWeightTicketComplete } from 'utils/shipments';
 import withRouter from 'utils/routing';
 import { ADVANCE_STATUSES } from 'constants/ppms';
-import { CHECK_SPECIAL_ORDERS_TYPES, SPECIAL_ORDERS_TYPES } from 'constants/orders';
 
 const Description = ({ className, children, dataTestId }) => (
   <p className={`${styles.description} ${className}`} data-testid={dataTestId}>
@@ -405,8 +404,6 @@ const MoveHome = ({ serviceMemberMoves, isProfileComplete, serviceMember, signed
   // eslint-disable-next-line camelcase
   const currentLocation = current_location;
   const shipmentNumbersByType = {};
-
-  const isSpecialMove = CHECK_SPECIAL_ORDERS_TYPES(orders?.orders_type);
   return (
     <>
       <ConnectedDestructiveShipmentConfirmationModal
@@ -422,11 +419,6 @@ const MoveHome = ({ serviceMemberMoves, isProfileComplete, serviceMember, signed
       <DownloadAOAErrorModal isOpen={showDownloadPPMAOAPaperworkErrorAlert} closeModal={toggleDownloadAOAErrorModal} />
       <div className={styles.homeContainer}>
         <header data-testid="customer-header" className={styles['customer-header']}>
-          {isSpecialMove ? (
-            <div data-testid="specialMovesLabel" className={styles.specialMovesLabel}>
-              <p>{SPECIAL_ORDERS_TYPES[`${orders.orders_type}`]}</p>
-            </div>
-          ) : null}
           <div className={`usa-prose grid-container ${styles['grid-container']}`}>
             <h2>
               {serviceMember.first_name} {serviceMember.last_name}
