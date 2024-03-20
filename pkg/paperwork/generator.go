ef8d5698f86c9780df921cd33a14fca4610190b2
Ricky Mettler <ricky.mettler@caci.com>
2024-03-20 00:06:19 +0000

    write csv to string remove trailing ,

--- pkg/paperwork/generator.go
+++ pkg/paperwork/generator.go
@@ -373,9 +373,7 @@ func (g *Generator) ConvertUploadToPDF(appCtx appcontext.AppContext, upload mode
 		const constructedWeightSectionString = "Constructed Weight for this section"
 		const itemString = "Item"
 		const weightTemplateFields = "lr cu ft 1,lr pieces 1,lr total 1,lr cu ft 2,lr pieces 2,lr total 2,lr cu ft 3,lr pieces 3,lr total 3,lr cu ft 4,lr pieces 4,lr total 4,lr cu ft 5,lr pieces 5,lr total 5,lr cu ft 6,lr pieces 6,lr total 6,lr cu ft 7,lr pieces 7,lr total 7,lr cu ft 8,lr pieces 8,lr total 8,lr cu ft 9,lr pieces 9,lr total 9,lr cu ft 10,lr pieces 10,lr total 10,lr cu ft 11,lr pieces 11,lr total 11,lr cu ft 12,lr pieces 12,lr total 12,lr cu ft 13,lr pieces 13,lr total 13,lr cu ft 14,lr pieces 14,lr total 14,lr cu ft 15,lr pieces 15,lr total 15,lr cu ft 16,lr pieces 16,lr total 16,lr cu ft 17,lr pieces 17,lr total 17,lr cu ft 18,lr pieces 18,lr total 18,lr cu ft 19,lr pieces 19,lr total 19,lr cu ft 20,lr pieces 20,lr total 20,lr cu ft 21,lr pieces 21,lr total 21,lr cu ft 22,lr pieces 22,lr total 22,lr cu ft 23,lr pieces 23,lr total 23,lr cu ft 24,lr pieces 24,lr total 24,lr cu ft 25,lr pieces 25,lr total 25,lr cu ft 26,lr pieces 26,lr total 26,lr cu ft 27,lr pieces 27,lr total 27,lr cu ft 28,lr pieces 28,lr total 28,lr cu ft 29,lr pieces 29,lr total 29,lr cu ft 30,lr pieces 30,lr total 30,lr cu ft 31,lr pieces 31,lr total 31,lr cu ft 32,lr pieces 32,lr total 33,lr cu ft 34,lr pieces 34,lr total 34,lr cu ft 35,lr pieces 35,lr total 35,lr cu ft 36,lr pieces 36,lr total 36,lr cu ft 37,lr pieces 37,lr total 37,lr cu ft 38,lr pieces 38,lr total 38,lr cu ft 39,lr pieces 39,lr total 39,lr cu ft 40,lr pieces 40,lr total 40,lr cu ft 41,lr pieces 41,lr total 41,lr pieces total 1,lr cu ft total 1,lr cu ft 43,lr pieces 43,lr total 43,lr pieces total 2,lr cu ft total 2,lr total items,lr total cube,lr weight"
-
-		csvFile.WriteString(weightTemplateFields + "\n")
-		fmt.Print(weightTemplateFields + "\n")
+		var csvStringBuilder strings.Builder
 
 		for _, row := range rows {
 			//secondDataSection := false
@@ -407,7 +405,8 @@ func (g *Generator) ConvertUploadToPDF(appCtx appcontext.AppContext, upload mode
 					currentCell = 1
 					writeData = false
 
-					// If the first cell contains Item in the string its a section of just headers and no data
+					// If the first cell contains Item in the string or all 4 cells are empty, then its a section of just headers and no data
+					// so we do cleanup and skip to the next set of data
 					if cellColumnData[0] == itemString || (len(cellColumnData) == 4 && cellColumnData[0] == "" && cellColumnData[1] == "" &&
 						cellColumnData[2] == "" && cellColumnData[3] == "") {
 						cellColumnData = cellColumnData[:0]
@@ -415,23 +414,16 @@ func (g *Generator) ConvertUploadToPDF(appCtx appcontext.AppContext, upload mode
 						continue
 					} else if strings.Contains(cellColumnData[0], totalItemSectionString) ||
 						strings.Contains(cellColumnData[0], constructedWeightSectionString) {
-						csvFile.WriteString(cellColumnData[3] + ",")
-						fmt.Print(cellColumnData[3] + ",")
+						csvStringBuilder.WriteString(cellColumnData[3] + ",")
 					} else if strings.Contains(cellColumnData[0], totalCubeSectionString) {
-						csvFile.WriteString(cellColumnData[2] + ",")
-						fmt.Print(cellColumnData[2] + ",")
+						csvStringBuilder.WriteString(cellColumnData[2] + ",")
 					} else if cellColumnData[0] == "" {
-						csvFile.WriteString(cellColumnData[2] + ",")
-						csvFile.WriteString(cellColumnData[3] + ",")
-						fmt.Print(cellColumnData[2] + ",")
-						fmt.Print(cellColumnData[3] + ",")
+						csvStringBuilder.WriteString(cellColumnData[2] + ",")
+						csvStringBuilder.WriteString(cellColumnData[3] + ",")
 					} else {
-						csvFile.WriteString(cellColumnData[1] + ",")
-						csvFile.WriteString(cellColumnData[2] + ",")
-						csvFile.WriteString(cellColumnData[3] + ",")
-						fmt.Print(cellColumnData[1] + ",")
-						fmt.Print(cellColumnData[2] + ",")
-						fmt.Print(cellColumnData[3] + ",")
+						csvStringBuilder.WriteString(cellColumnData[1] + ",")
+						csvStringBuilder.WriteString(cellColumnData[2] + ",")
+						csvStringBuilder.WriteString(cellColumnData[3] + ",")
 					}
 
 					cellColumnData = cellColumnData[:0]
@@ -443,6 +435,20 @@ func (g *Generator) ConvertUploadToPDF(appCtx appcontext.AppContext, upload mode
 			cellColumnData = nil
 			currentCell = 1
 		}
+
+		//TODO: REMOVE THE FMT PRINTS
+		csvFile.WriteString(weightTemplateFields + "\n")
+		fmt.Print(weightTemplateFields + "\n")
+
+		csvString := csvStringBuilder.String()
+
+		// we remove the trailing , from the list because pdfcpu doesn't like it and will throw an error when we attempt to fill the pdf
+		if len(csvString) > 0 {
+			csvString = csvString[:len(csvString)-1]
+		}
+
+		csvFile.WriteString(csvString)
+		fmt.Print(csvString)
 	}
 
 	images := make([]inputFile, 0)
