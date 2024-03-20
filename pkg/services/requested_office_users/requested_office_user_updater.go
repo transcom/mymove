0905f9f7c9b82bac464d752bc3eccc90b0d9d093
Daniel Jordan <daniel.jordan@caci.com>
2024-03-20 17:03:02 +0000

    white space issues

--- pkg/services/requested_office_users/requested_office_user_updater.go
+++ pkg/services/requested_office_users/requested_office_user_updater.go
@@ -3,6 +3,7 @@ package adminuser
 import (
 	"github.com/gobuffalo/validate/v3"
 	"github.com/gofrs/uuid"
+
 	"github.com/transcom/mymove/pkg/appcontext"
 	"github.com/transcom/mymove/pkg/gen/adminmessages"
 	"github.com/transcom/mymove/pkg/models"
