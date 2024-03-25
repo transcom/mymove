119d00da5f503dc4b34564d66b134e3e4ae53d26
deandreJones <deandre.jones@caci.com>
2024-03-25 09:23:44 -0500

    add timeout

--- migrations/app/schema/20240322144246_updateAddressesCounty.up.sql
+++ migrations/app/schema/20240322144246_updateAddressesCounty.up.sql
@@ -1,3 +1,9 @@
+-- Set temp timeout due to possibly large record modification
+-- Time is 5 minutes in milliseconds
+SET statement_timeout = 300000;
+SET lock_timeout = 300000;
+SET idle_in_transaction_session_timeout = 300000;
+
 update
 	addresses adr
 set
