dbf8ee0f6b0a6909fc66466881b438c1ea722728
Daniel Jordan <daniel.jordan@caci.com>
2024-04-09 16:31:35 +0000

    initial commit, checking move status on frontend when getting move

--- pkg/handlers/ghcapi/api.go
+++ pkg/handlers/ghcapi/api.go
@@ -96,6 +96,7 @@ func NewGhcAPIHandler(handlerConfig handlers.HandlerConfig) *ghcops.MymoveAPI {
 	ghcAPI.MoveGetMoveHandler = GetMoveHandler{
 		HandlerConfig: handlerConfig,
 		MoveFetcher:   move.NewMoveFetcher(),
+		MoveRouter:    moveRouter,
 	}
 
 	ghcAPI.MoveGetMoveHistoryHandler = GetMoveHistoryHandler{
