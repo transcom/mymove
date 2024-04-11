9e09f7f2146d50456a8af213cc547601b5aed837
Daniel Jordan <daniel.jordan@caci.com>
2024-04-11 21:43:38 +0000

    have a happy path, need to work on tests and playwright

---
+++ pkg/models/application_parameters.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/pkg/errors"
+)
+
+// ApplicationParameters is a model representing validation codes stored in the database
+type ApplicationParameters struct {
+	ValidationCode string    `json:"validation_code" db:"validation_code"`
+	CreatedAt      time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func (a ApplicationParameters) TableName() string {
+	return "application_parameters"
+}
+
+// FetchValidationCode returns a specific validation code from the db
+func FetchValidationCode(db *pop.Connection, code string) (ApplicationParameters, error) {
+	var validationCode ApplicationParameters
+	err := db.Q().Where(`validation_code=$1`, code).First(&validationCode)
+	// if it isn't found, we'll return an empty object
+	if err != nil {
+		if errors.Cause(err).Error() == RecordNotFoundErrorString {
+			return ApplicationParameters{}, ErrFetchNotFound
+		}
+		return ApplicationParameters{}, err
+	}
+
+	return validationCode, nil
+}
--- src/pages/MyMove/Profile/ValidationCode.jsx
+++ src/pages/MyMove/Profile/ValidationCode.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { GridContainer, Grid, Alert } from '@trussworks/react-uswds';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -7,60 +6,47 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 import NotificationScrollToTop from 'components/NotificationScrollToTop';
-import { patchServiceMember, getResponseError } from 'services/internalApi';
+import { getResponseError, validateCode } from 'services/internalApi';
 import { updateServiceMember as updateServiceMemberAction } from 'store/entities/actions';
 import { selectServiceMemberFromLoggedInUser } from 'store/entities/selectors';
-import requireCustomerState from 'containers/requireCustomerState/requireCustomerState';
-import { profileStates } from 'constants/customerStates';
 import { customerRoutes } from 'constants/routes';
-import { ServiceMemberShape } from 'types/customerShapes';
-import ValidationCodeForm from 'components/Customer/ValidationCodeForm/ValidationCodeForm';
 import { Form } from 'components/form/Form';
 import formStyles from 'styles/form.module.scss';
 import TextField from 'components/form/fields/TextField/TextField';
 import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
 
-export const ValidationCode = ({ updateServiceMember, serviceMember }) => {
+export const ValidationCode = () => {
   const navigate = useNavigate();
   const [serverError, setServerError] = useState(null);
+  const [validationError, setValidationError] = useState(null);
 
   const initialValues = {
     code: '',
   };
 
-  const handleBack = () => {
-    navigate(customerRoutes.CONUS_OCONUS_PATH);
-  };
-
-  const handleNext = () => {
-    navigate(customerRoutes.NAME_PATH);
-  };
-
-  const onSubmit = (values) => {
-    const payload = {
-      id: serviceMember.id,
-      code: values.code,
+  const onSubmit = async (values) => {
+    const body = {
+      validationCode: values.code,
     };
 
-    console.log('payload', payload);
-
-    navigate(customerRoutes.CONUS_OCONUS_PATH);
-
-    // return patchServiceMember(payload)
-    //   .then(updateServiceMember)
-    //   .then(handleNext)
-    //   .catch((e) => {
-    //     // Error shape: https://github.com/swagger-api/swagger-js/blob/master/docs/usage/http-client.md#errors
-    //     const { response } = e;
-    //     const errorMessage = getResponseError(response, 'failed to update service member due to server error');
-    //     setServerError(errorMessage);
-    //   });
+    return validateCode(body)
+      .then((response) => {
+        const { validationCode } = response.body;
+        if (validationCode === body.validationCode) {
+          navigate(customerRoutes.CONUS_OCONUS_PATH);
+        } else {
+          setValidationError('Please try again.');
+        }
+      })
+      .catch((e) => {
+        const { response } = e;
+        const errorMessage = getResponseError(response, 'There was an internal server error submitting your request.');
+        setServerError(errorMessage);
+      });
   };
 
   const validationSchema = Yup.object().shape({
-    code: Yup.string()
-      .matches(/[0-9]{20}/, 'Enter a 20-digit number')
-      .required('Required'),
+    code: Yup.string().required('Required').max(20, 'Enter up to 20 digits'),
   });
 
   return (
@@ -76,6 +62,15 @@ export const ValidationCode = ({ updateServiceMember, serviceMember }) => {
           </Grid>
         </Grid>
       )}
+      {validationError && (
+        <Grid row>
+          <Grid col desktop={{ col: 8, offset: 2 }}>
+            <Alert type="error" headingLevel="h4" heading="Incorrect validation code">
+              {validationError}
+            </Alert>
+          </Grid>
+        </Grid>
+      )}
 
       <Grid row>
         <Grid col desktop={{ col: 8, offset: 2 }}>
@@ -83,7 +78,7 @@ export const ValidationCode = ({ updateServiceMember, serviceMember }) => {
             {({ isValid, handleSubmit }) => {
               return (
                 <Form className={formStyles.form}>
-                  <h1>Please enter validation code to begin creating a move</h1>
+                  <h1>Please enter a validation code to begin creating a move</h1>
                   <TextField
                     label="Validation code"
                     name="code"
@@ -105,11 +100,6 @@ export const ValidationCode = ({ updateServiceMember, serviceMember }) => {
       </Grid>
     </GridContainer>
   );
-};
-
-ValidationCode.propTypes = {
-  updateServiceMember: PropTypes.func.isRequired,
-  serviceMember: ServiceMemberShape.isRequired,
 };
 
 const mapDispatchToProps = {
