c0031cee30f648d97cf750abc52d14a7a025f095
Daniel Jordan <daniel.jordan@caci.com>
2024-04-12 18:11:24 +0000

    updated playwright test to check for validation input first

--- playwright/tests/my/mymove/onboarding.spec.js
+++ playwright/tests/my/mymove/onboarding.spec.js
@@ -73,8 +73,11 @@ test.describe('(MultiMove) Onboarding', () => {
     // Create new customer user
     await customerPage.signInAsNewCustomer();
 
-    // Input validation code
-    await customerPage.submitValidationCode();
+    // if the user needs to input a validation code
+    const validationCodeInput = page.locator('[name="code"]');
+    if (await validationCodeInput.isVisible()) {
+      await customerPage.submitValidationCode();
+    }
 
     // CONUS/OCONUS section
     await customerPage.waitForPage.onboardingConus();
