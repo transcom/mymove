c4333b85768c4a19d0ee25e8e88ed3454a29a772
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-04-12 17:38:49 +0000

    Undo change to mto_shipment_factory, added test DB query to unit test instead.

--- pkg/factory/mto_shipment_factory.go
+++ pkg/factory/mto_shipment_factory.go
@@ -184,10 +184,6 @@ func buildMTOShipmentWithBuildType(db *pop.Connection, customs []Customization, 
 		mustCreate(db, &newMTOShipment)
 	}
 
-	shipmentLocator := string(newMTOShipment.ID[:6])
-	shipmentLocator += "-01"
-	newMTOShipment.ShipmentLocator = &shipmentLocator
-
 	return newMTOShipment
 }
 
