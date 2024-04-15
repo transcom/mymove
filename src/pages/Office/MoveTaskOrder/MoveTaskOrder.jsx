fbdae4009badc2d518b9c86104155efe6d5c25a9
KonstanceH <146969726+KonstanceH@users.noreply.github.com>
2024-04-15 06:18:28 +0000

    added shipmenlocator to mto and shipments. Need design approval

--- src/pages/Office/MoveTaskOrder/MoveTaskOrder.jsx
+++ src/pages/Office/MoveTaskOrder/MoveTaskOrder.jsx
@@ -1106,6 +1106,7 @@ export const MoveTaskOrder = (props) => {
                     shipmentStatus: mtoShipment.status,
                     ifMatchEtag: mtoShipment.eTag,
                     moveTaskOrderID: mtoShipment.moveTaskOrderID,
+                    shipmentLocator: mtoShipment.shipmentLocator,
                   }}
                   handleShowCancellationModal={handleShowCancellationModal}
                 />
