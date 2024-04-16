329c86b956fb66b51e1233ebfff3504b788fcae3
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-04-16 21:12:49 +0000

    Reverted addition of gunSafe to shipment portion, moved up to "Move" details section instead.

--- src/pages/PrimeUI/MoveTaskOrder/MoveDetails.jsx
+++ src/pages/PrimeUI/MoveTaskOrder/MoveDetails.jsx
@@ -201,6 +201,10 @@ const MoveDetails = ({ setFlashMessage }) => {
                     <dd>{moveTaskOrder.id}</dd>
                   </div>
                   <div className={descriptionListStyles.row}>
+                    <dt>Gun Safe:</dt>
+                    <dd>{moveTaskOrder.order.entitlement.gunSafe ? 'yes' : 'no'}</dd>
+                  </div>
+                  <div className={descriptionListStyles.row}>
                     <Button onClick={handleDownloadOrders}>Download Move Orders</Button>
                     <select
                       onChange={handleDocumentTypeChange}
@@ -235,7 +239,6 @@ const MoveDetails = ({ setFlashMessage }) => {
                         <Shipment
                           shipment={mtoShipment}
                           moveId={moveTaskOrder.id}
-                          entitlements={moveTaskOrder.order.entitlement}
                           onDelete={handleDeleteShipment}
                           mtoServiceItems={mtoServiceItems}
                         />
