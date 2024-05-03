282e66e577cb5427fa8cf843e3e30d7e42d84548
antgmann <anthony.mann@caci.com>
2024-05-03 13:57:13 +0000

    fix copy paste error

--- migrations/app/schema/20240503123556_add_diversion_reason_to_mto_shipments.up.sql
+++ migrations/app/schema/20240503123556_add_diversion_reason_to_mto_shipments.up.sql
@@ -3,4 +3,4 @@ ALTER TABLE mto_shipments
 ADD COLUMN IF NOT EXISTS diversion_reason TEXT NULL;
 
 -- Comments on new column
-COMMENT ON COLUMN mto_shipments.actual_pro_gear_weight IS 'Stores the reason for a requested diversion.';
+COMMENT ON COLUMN mto_shipments.diversion_reason IS 'Stores the reason for a requested diversion.';
