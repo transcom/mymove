1ee338cae1cbdda7a7bea30033e11817be3c351b
Cory Kleinjan <cory.kleinjan@caci.com>
2024-05-03 16:12:29 +0000

    Initial commit to update application_parameters table

---
+++ migrations/app/schema/20240502183613_add_support_for_standalone_payment_cap.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE application_parameters
+ADD COLUMN IF NOT EXISTS parameter_NAME TEXT NOT NULL;
+
+ALTER TABLE application_parameters
+RENAME COLUMN validation_code TO parameter_value;
+
+COMMENT ON COLUMN application_parameters.parameter_name IS 'The name of the parameter';
+COMMENT ON COLUMN application_parameters.parameter_value IS 'The value of the parameter';
