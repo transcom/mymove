d8803b89d33858904327997b4b2c6930bfdfbd22
cameroncaci <cameron.jewell@caci.com>
2024-05-20 17:15:43 +0000

    update mock test

--- pkg/handlers/ghcapi/office_users_test.go
+++ pkg/handlers/ghcapi/office_users_test.go
@@ -93,7 +93,7 @@ func (suite *HandlerSuite) TestRequestOfficeUserHandler() {
 			mock.AnythingOfType("*appcontext.appContext"),
 			mock.Anything,
 			mock.Anything,
-		).Return(nil, nil).Once()
+		).Return(nil, nil, nil).Once()
 		// Mock successful role return
 		mockRoleFetcher.On(
 			"FetchRolesForUser",
