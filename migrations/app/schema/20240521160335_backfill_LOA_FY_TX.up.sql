2a15fb273f25acfe5059cf4dcccbb85f46315893
deandreJones <deandre.Jones@caci.com>
2024-05-21 20:52:41 +0000

    correct

--- migrations/app/schema/20240521160335_backfill_LOA_FY_TX.up.sql
+++ migrations/app/schema/20240521160335_backfill_LOA_FY_TX.up.sql
@@ -2,15 +2,15 @@ DO $$
 BEGIN
     UPDATE lines_of_accounting
     SET loa_bg_fy_tx = CASE
-                           WHEN extract(month FROM loa_bgn_dt) > 10 THEN extract(year FROM loa_bgn_dt)
-                           ELSE extract(year FROM loa_bgn_dt) - 1
+                           WHEN extract(month FROM loa_bgn_dt) > 10 THEN extract(year FROM loa_bgn_dt) +1
+                           ELSE extract(year FROM loa_bgn_dt)
                        END
     WHERE loa_bg_fy_tx IS NULL;
 
     UPDATE lines_of_accounting
     SET loa_end_fy_tx = CASE
-                            WHEN extract(month FROM loa_end_dt) > 10 THEN extract(year FROM loa_end_dt)
-                            ELSE extract(year FROM loa_end_dt) - 1
+                            WHEN extract(month FROM loa_end_dt) > 10 THEN extract(year FROM loa_end_dt) +1
+                            ELSE extract(year FROM loa_end_dt)
                         END
     WHERE loa_end_fy_tx IS NULL;
 END $$;
