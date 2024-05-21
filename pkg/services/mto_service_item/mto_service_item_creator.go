2da4555cd43dde1834c5ee4150bbdbbdb1533743
Maria Traskowsky <maria.traskowsky@caci.com>
2024-05-21 02:16:31 +0000

    price DUPK

--- pkg/services/mto_service_item/mto_service_item_creator.go
+++ pkg/services/mto_service_item/mto_service_item_creator.go
@@ -80,6 +80,21 @@ func FetchContractCode(appCtx appcontext.AppContext, date time.Time) (string, er
 	return contractCode, nil
 }
 
+func fetchDomesticServiceArea(appCtx appcontext.AppContext, contractCode string, zip3 string) (models.ReDomesticServiceArea, error) {
+	var domesticServiceArea models.ReDomesticServiceArea
+	err := appCtx.DB().Q().
+		Join("re_zip3s", "re_zip3s.domestic_service_area_id = re_domestic_service_areas.id").
+		Join("re_contracts", "re_contracts.id = re_domestic_service_areas.contract_id").
+		Where("re_zip3s.zip3 = ?", zip3).
+		Where("re_contracts.code = ?", contractCode).
+		First(&domesticServiceArea)
+	if err != nil {
+		return domesticServiceArea, fmt.Errorf("unable to find domestic service area for %s under contract code %s", zip3, contractCode)
+	}
+
+	return domesticServiceArea, nil
+}
+
 // CreateMTOServiceItem creates a MTO Service Item
 func (o *mtoServiceItemCreator) CreateMTOServiceItem(appCtx appcontext.AppContext, serviceItem *models.MTOServiceItem) (*models.MTOServiceItems, *validate.Errors, error) {
 	var verrs *validate.Errors
@@ -308,6 +323,43 @@ func (o *mtoServiceItemCreator) CreateMTOServiceItem(appCtx appcontext.AppContex
 		}
 
 		requestedServiceItems = append(requestedServiceItems, *extraServiceItems...)
+	}
+
+	// if estimated weight for shipment provided by the prime, calculate the estimate prices for
+	// Linehaul            = DLH   = linehaul
+	// Packing             = DPK   = domestic packing
+	// Origin Price        = DOP   = domestic origin price
+	// Destination Price   = DDP   = domestic destination price
+	// Unpacking           = DUPK  = domestic unpacking
+	// Fuel Surcharge      = FSC   = fuel surcharge
+
+	if mtoShipment.PrimeEstimatedWeight != nil {
+		if serviceItem.ReService.Code == models.ReServiceCodeDUPK {
+			currTime := time.Now()
+			contractCode, err := FetchContractCode(appCtx, currTime)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			// find the service area by querying for the service area associated with the zip3
+			destZip := mtoShipment.DestinationAddress.PostalCode
+			zip := destZip
+			zip3 := zip[0:3]
+
+			domesticServiceArea, err := fetchDomesticServiceArea(appCtx, contractCode, zip3)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			serviceSchedule := domesticServiceArea.ServicesSchedule
+
+			price, _, err := o.unpackPricer.Price(appCtx, contractCode, currTime, *mtoShipment.PrimeEstimatedWeight, serviceSchedule, false)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			serviceItem.PricingEstimate = &price
+		}
 	}
 	requestedServiceItems = append(requestedServiceItems, *serviceItem)
 
