4583444f2a80a7366205151f54ad95b17a554b6c
deandreJones <deandre.Jones@caci.com>
2024-05-22 13:12:10 +0000

    september is the end, not october

--- migrations/app/schema/20240521160335_backfill_LOA_FY_TX.up.sql
+++ migrations/app/schema/20240521160335_backfill_LOA_FY_TX.up.sql
@@ -2,14 +2,14 @@ DO $$
 BEGIN
     UPDATE lines_of_accounting
     SET loa_bg_fy_tx = CASE
-                           WHEN extract(month FROM loa_bgn_dt) > 10 THEN extract(year FROM loa_bgn_dt) +1
+                           WHEN extract(month FROM loa_bgn_dt) > 9 THEN extract(year FROM loa_bgn_dt) +1
                            ELSE extract(year FROM loa_bgn_dt)
                        END
     WHERE loa_bg_fy_tx IS NULL;
 
     UPDATE lines_of_accounting
     SET loa_end_fy_tx = CASE
-                            WHEN extract(month FROM loa_end_dt) > 10 THEN extract(year FROM loa_end_dt) +1
+                            WHEN extract(month FROM loa_end_dt) > 9 THEN extract(year FROM loa_end_dt) +1
                             ELSE extract(year FROM loa_end_dt)
                         END
     WHERE loa_end_fy_tx IS NULL;
