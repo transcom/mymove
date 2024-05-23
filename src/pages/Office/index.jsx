37727a37d5d8426849097b46fcbdbd2063b3f74a
cameroncaci <cameron.jewell@caci.com>
2024-05-23 12:55:16 +0000

    updated office index

--- src/pages/Office/index.jsx
+++ src/pages/Office/index.jsx
@@ -488,7 +488,7 @@ export class OfficeApp extends Component {
                       key="qaeCSRMoveSearchPath"
                       path={qaeCSRRoutes.MOVE_SEARCH_PATH}
                       element={
-                        <PrivateRoute requiredRoles={[roleTypes.QAE_CSR]}>
+                        <PrivateRoute requiredRoles={[roleTypes.QAE_CSR, roleTypes.CUSTOMER_SERVICE_REPRESENTATIVE]}>
                           <QAECSRMoveSearch />
                         </PrivateRoute>
                       }
@@ -498,7 +498,14 @@ export class OfficeApp extends Component {
                       key="txoMoveInfoRoute"
                       path="/moves/:moveCode/*"
                       element={
-                        <PrivateRoute requiredRoles={[roleTypes.TOO, roleTypes.TIO, roleTypes.QAE_CSR]}>
+                        <PrivateRoute
+                          requiredRoles={[
+                            roleTypes.TOO,
+                            roleTypes.TIO,
+                            roleTypes.QAE_CSR,
+                            roleTypes.CUSTOMER_SERVICE_REPRESENTATIVE,
+                          ]}
+                        >
                           <TXOMoveInfo />
                         </PrivateRoute>
                       }
