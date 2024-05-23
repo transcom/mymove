5b568c222ad9e32cde725493df79cbd3873dd1a2
pambecker <150711493+pambecker@users.noreply.github.com>
2024-05-23 13:53:39 -0500

    B 18544 int ff sc create safety move (#12744)
    * create Safety Moves FF
    
    * merge conflicts resolved from Main
    
    * more feature flag
    
    * add FF check, render conditions
    
    * fix DB load issue, add privilege to payload
    
    * passing userPrivileges
    
    * updates
    
    * fixing tests, fixing required logic
    
    * more udpates
    
    * passing safetymove checked param
    
    * new calls to empty out dodid and okta
    
    * hide cac question for SM
    
    * taking CAC back out for the moment
    
    * allow qae/csr to view payment request page
    
    * added test to access view documents page for qae
    
    * hiding CAC question, not show label on UI for Safety Move
    
    * create SM locator
    
    * ---
    updated-dependencies:
    - dependency-name: react-imask
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * changes
    
    * failing test
    
    * revert changes, didn't fix test
    
    * change to just Safety, one word
    
    * fix merge conflicts
    
    * fixing merge conflicts with tests
    
    * removing unused code
    
    * resolve issue with looping if user has no privileges
    
    * dont allow user to pick Safety from DD if they selected no on previous screen
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Tae Jung <tae.jung@caci.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Dre Jones <136509766+deandreJones@users.noreply.github.com>
    Co-authored-by: taeJungCaci <146856854+taeJungCaci@users.noreply.github.com>
    Co-authored-by: Tevin Adams <tevin.adams@caci.com>
    Co-authored-by: TevinAdams <136514590+TevinAdams@users.noreply.github.com>
    Co-authored-by: Ricky Mettler <149000222+r-mettler@users.noreply.github.com>

--- pkg/gen/ghcapi/embedded_spec.go
+++ pkg/gen/ghcapi/embedded_spec.go
@@ -8551,13 +8551,15 @@ func init() {
         "RETIREMENT",
         "SEPARATION",
         "WOUNDED_WARRIOR",
-        "BLUEBARK"
+        "BLUEBARK",
+        "SAFETY"
       ],
       "x-display-value": {
         "BLUEBARK": "BLUEBARK",
         "LOCAL_MOVE": "Local Move",
         "PERMANENT_CHANGE_OF_STATION": "Permanent Change Of Station",
         "RETIREMENT": "Retirement",
+        "SAFETY": "Safety",
         "SEPARATION": "Separation",
         "WOUNDED_WARRIOR": "Wounded Warrior"
       }
@@ -21586,13 +21588,15 @@ func init() {
         "RETIREMENT",
         "SEPARATION",
         "WOUNDED_WARRIOR",
-        "BLUEBARK"
+        "BLUEBARK",
+        "SAFETY"
       ],
       "x-display-value": {
         "BLUEBARK": "BLUEBARK",
         "LOCAL_MOVE": "Local Move",
         "PERMANENT_CHANGE_OF_STATION": "Permanent Change Of Station",
         "RETIREMENT": "Retirement",
+        "SAFETY": "Safety",
         "SEPARATION": "Separation",
         "WOUNDED_WARRIOR": "Wounded Warrior"
       }
--- pkg/gen/primev2api/embedded_spec.go
+++ pkg/gen/primev2api/embedded_spec.go
@@ -1954,13 +1954,15 @@ func init() {
         "RETIREMENT",
         "SEPARATION",
         "WOUNDED_WARRIOR",
-        "BLUEBARK"
+        "BLUEBARK",
+        "SAFETY"
       ],
       "x-display-value": {
         "BLUEBARK": "BLUEBARK",
         "LOCAL_MOVE": "Local Move",
         "PERMANENT_CHANGE_OF_STATION": "Permanent Change Of Station",
         "RETIREMENT": "Retirement",
+        "SAFETY": "Safety",
         "SEPARATION": "Separation",
         "WOUNDED_WARRIOR": "Wounded Warrior"
       }
@@ -5508,13 +5510,15 @@ func init() {
         "RETIREMENT",
         "SEPARATION",
         "WOUNDED_WARRIOR",
-        "BLUEBARK"
+        "BLUEBARK",
+        "SAFETY"
       ],
       "x-display-value": {
         "BLUEBARK": "BLUEBARK",
         "LOCAL_MOVE": "Local Move",
         "PERMANENT_CHANGE_OF_STATION": "Permanent Change Of Station",
         "RETIREMENT": "Retirement",
+        "SAFETY": "Safety",
         "SEPARATION": "Separation",
         "WOUNDED_WARRIOR": "Wounded Warrior"
       }
