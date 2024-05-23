0dd295d2efc3aaa9a7b45119e214d3f350c28e9f
Cory Kleinjan <cory.kleinjan@caci.com>
2024-05-23 09:28:49 -0500

    Updating migration file.

--- migrations/app/schema/20240521184834_add_standalone_field_to_service_items.up.sql
+++ migrations/app/schema/20240521184834_add_standalone_field_to_service_items.up.sql
@@ -1,1 +1,3 @@
-ALTER TABLE mto_service_items ADD COLUMN standalone_crate boolean;
+ALTER TABLE mto_service_items ADD COLUMN IF NOT EXISTS standalone_crate boolean;
+
+COMMENT ON COLUMN mto_service_items.standalone_crate IS 'This column stores a boolean to declare a crate as Standalone';
