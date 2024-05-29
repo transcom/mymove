7b705e9f8b4f7ff11479be5824aa288744a78c6c
Ricky Mettler <ricky.mettler@caci.com>
2024-05-29 19:39:10 +0000

    updating safety moves check

--- src/constants/userRoles.js
+++ src/constants/userRoles.js
@@ -10,7 +10,6 @@ export const roleTypes = {
   HQ: 'headquarters',
 };
 
-//TODO: ONLY ADD HQ ROLE IF FF IS TRUE
 export const adminOfficeRoles = [
   { roleType: 'customer', name: 'Customer' },
   { roleType: 'transportation_ordering_officer', name: 'Transportation Ordering Officer' },
