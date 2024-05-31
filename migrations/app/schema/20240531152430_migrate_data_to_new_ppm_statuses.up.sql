eb429dc5e694d13d97d36f433cf172cb78d2efdd
James Hawks <james.hawks@caci.com>
2024-05-31 11:03:17 -0500

    updated migrations sql

--- migrations/app/schema/20240531152430_migrate_data_to_new_ppm_statuses.up.sql
+++ migrations/app/schema/20240531152430_migrate_data_to_new_ppm_statuses.up.sql
@@ -15,14 +15,3 @@ DROP TYPE ppm_shipment_status_temp;
 UPDATE ppm_shipments set status = 'NEEDS_CLOSEOUT' WHERE status = 'NEEDS_PAYMENT_APPROVAL';
 UPDATE ppm_shipments set status = 'CLOSEOUT_COMPLETE' WHERE status = 'PAYMENT_APPROVED';
 
---- rename existing enum
-ALTER TYPE ppm_shipment_status RENAME TO ppm_shipment_status_temp;
-
--- create a new enum without the old statuses (NEEDS_PAYMENT_APPROVAL, PAYMENT_APPROVED)
-CREATE TYPE ppm_shipment_status AS ENUM('DRAFT', 'SUBMITTED', 'WAITING_ON_CUSTOMER', 'NEEDS_ADVANCE_APPROVAL', 'NEEDS_CLOSEOUT', 'CLOSEOUT_COMPLETE');
-
--- alter the ppm shipments status column to use the new enum
-ALTER TABLE ppm_shipments ALTER COLUMN status TYPE ppm_shipment_status USING status::text::ppm_shipment_status;
-
--- get rid of the temp type
-DROP TYPE ppm_shipment_status_temp;
