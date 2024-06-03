d9b93cf6b045d967b2605104d57e93289906590f
Alex Lusk <alex.lusk@caci.com>
2024-06-03 21:51:14 +0000

    HQ role can access all queues and searches via API

--- pkg/gen/ghcapi/embedded_spec.go
+++ pkg/gen/ghcapi/embedded_spec.go
@@ -3759,11 +3759,7 @@ func init() {
           "500": {
             "$ref": "#/responses/ServerError"
           }
-        },
-        "x-permissions": [
-          "update.move",
-          "create.serviceItem"
-        ]
+        }
       }
     },
     "/queues/payment-requests": {
@@ -16424,11 +16420,7 @@ func init() {
               "$ref": "#/definitions/Error"
             }
           }
-        },
-        "x-permissions": [
-          "update.move",
-          "create.serviceItem"
-        ]
+        }
       }
     },
     "/queues/payment-requests": {
