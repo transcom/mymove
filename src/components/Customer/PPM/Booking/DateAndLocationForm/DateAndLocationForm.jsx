53faa93a84db697d8b92d51f1694e4a04b562401
Alex Lusk <alex.lusk@caci.com>
2024-06-04 14:54:36 +0000

    Update PPM address language

--- src/components/Customer/PPM/Booking/DateAndLocationForm/DateAndLocationForm.jsx
+++ src/components/Customer/PPM/Booking/DateAndLocationForm/DateAndLocationForm.jsx
@@ -156,7 +156,7 @@ const DateAndLocationForm = ({ mtoShipment, destinationDutyLocation, serviceMemb
                   name="pickupAddress.address"
                   render={(fields) => (
                     <>
-                      <p>What address are the movers picking up from?</p>
+                      <p>What address are you moving from?</p>
                       <Checkbox
                         data-testid="useCurrentResidence"
                         label="Use my current origin address"
@@ -215,7 +215,7 @@ const DateAndLocationForm = ({ mtoShipment, destinationDutyLocation, serviceMemb
                   name="destinationAddress.address"
                   render={(fields) => (
                     <>
-                      <p>Please input Delivery Address</p>
+                      <p>Please input your destination address</p>
                       <Checkbox
                         data-testid="useCurrentDestinationAddress"
                         label="Use my current destination address"
@@ -254,7 +254,8 @@ const DateAndLocationForm = ({ mtoShipment, destinationDutyLocation, serviceMemb
                           <AddressFields name="secondaryDestinationAddress.address" />
                           <Hint className={ppmStyles.hint}>
                             <p>
-                              A second destination ZIP could mean that your final incentive is lower than your estimate.
+                              A second destination address could mean that your final incentive is lower than your
+                              estimate.
                             </p>
                             <p>
                               Get separate weight tickets for each leg of the trip to show how the weight changes. Talk
