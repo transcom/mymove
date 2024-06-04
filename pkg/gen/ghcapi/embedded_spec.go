c373bfaf0cdcba062515383599ffde7aa307cd8a
Tevin Adams <tevin.adams@caci.com>
2024-06-04 18:32:55 +0000

    generated code check

--- pkg/gen/ghcapi/embedded_spec.go
+++ pkg/gen/ghcapi/embedded_spec.go
@@ -8408,7 +8408,7 @@ func init() {
           "type": "string",
           "title": "name",
           "x-nullable": true,
-          "example": "Transportation Ordering Officer"
+          "example": "Task Ordering Officer"
         },
         "roleType": {
           "type": "string",
@@ -9916,7 +9916,7 @@ func init() {
         },
         "roleName": {
           "type": "string",
-          "example": "Transportation Ordering Officer"
+          "example": "Task Ordering Officer"
         },
         "roleType": {
           "type": "string",
@@ -21538,7 +21538,7 @@ func init() {
           "type": "string",
           "title": "name",
           "x-nullable": true,
-          "example": "Transportation Ordering Officer"
+          "example": "Task Ordering Officer"
         },
         "roleType": {
           "type": "string",
@@ -23048,7 +23048,7 @@ func init() {
         },
         "roleName": {
           "type": "string",
-          "example": "Transportation Ordering Officer"
+          "example": "Task Ordering Officer"
         },
         "roleType": {
           "type": "string",
