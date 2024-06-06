07b31f3f27dd10a35ea69f5b9c581593885aa32b
cameroncaci <cameron.jewell@caci.com>
2024-06-06 09:56:00 -0400

    created more loa service tests

--- pkg/services/line_of_accounting/line_of_accounting_fetcher_test.go
+++ pkg/services/line_of_accounting/line_of_accounting_fetcher_test.go
@@ -36,12 +36,16 @@ func (suite *LineOfAccountingServiceSuite) SetupTest() {
 }
 
 func (suite *LineOfAccountingServiceSuite) TestFetchOrderLineOfAccountings() {
-	suite.Run("successfully fetches LOAs", func() {
-		appCtx := suite.AppContextForTest()
+	setupTest := func() (time.Time, time.Time, time.Time, string) {
 		ordersIssueDate := time.Now()
 		startDate := ordersIssueDate.AddDate(-1, 0, 0)
 		endDate := ordersIssueDate.AddDate(1, 0, 0)
 		tacCode := "CACI"
+		return ordersIssueDate, startDate, endDate, tacCode
+	}
+	suite.Run("successfully fetches LOAs", func() {
+		appCtx := suite.AppContextForTest()
+		ordersIssueDate, startDate, endDate, tacCode := setupTest()
 		loa := factory.BuildLineOfAccounting(appCtx.DB(), []factory.Customization{
 			{
 				Model: models.LineOfAccounting{
@@ -52,7 +56,7 @@ func (suite *LineOfAccountingServiceSuite) TestFetchOrderLineOfAccountings() {
 				},
 			},
 		}, nil)
-		factory.BuildTransportationAccountingCode(appCtx.DB(), []factory.Customization{
+		factory.BuildTransportationAccountingCodeWithoutAttachedLoa(appCtx.DB(), []factory.Customization{
 			{
 				Model: models.TransportationAccountingCode{
 					TAC:               tacCode,
@@ -74,5 +78,124 @@ func (suite *LineOfAccountingServiceSuite) TestFetchOrderLineOfAccountings() {
 		loas, err := suite.loaFetcher.FetchLongLinesOfAccounting(models.AffiliationARMY, ordersIssueDate, tacCode, appCtx)
 		suite.NoError(err)
 		suite.Equal(loa.ID, loas[0].ID)
+	})
+	suite.Run("Successfully sorts according to FMBC", func() {
+		// Create 4 standalone TACs each with a different fbmc
+		fbmcs := []string{
+			"M",
+			"1",
+			"5",
+			"3",
+		}
+
+		appCtx := suite.AppContextForTest()
+		suite.NoError(appCtx.DB().TruncateAll())
+		ordersIssueDate, startDate, endDate, tacCode := setupTest()
+
+		// Setup TGET data for tests
+		for fbmcMemoryIteration := range fbmcs {
+			// Use a unique LoaSysID for each fbmc to avoid duplicates pulling from SQL
+			loaSysId := factory.MakeRandomString(20)
+			factory.BuildLineOfAccounting(appCtx.DB(), []factory.Customization{
+				{
+					Model: models.LineOfAccounting{
+						LoaBgnDt:   &startDate,
+						LoaEndDt:   &endDate,
+						LoaSysID:   &loaSysId,
+						LoaHsGdsCd: models.StringPointer(models.LineOfAccountingHouseholdGoodsCodeOfficer),
+						LoaDscTx:   &fbmcs[fbmcMemoryIteration],
+					},
+				},
+			}, nil)
+			factory.BuildTransportationAccountingCodeWithoutAttachedLoa(appCtx.DB(), []factory.Customization{
+				{
+					Model: models.TransportationAccountingCode{
+						TAC:               tacCode,
+						TrnsprtnAcntBgnDt: &startDate,
+						TrnsprtnAcntEndDt: &endDate,
+						TacFnBlModCd:      &fbmcs[fbmcMemoryIteration],
+						LoaSysID:          &loaSysId,
+					},
+				},
+			}, nil)
+		}
+
+		// Get the TACs that we will extract LOAs from
+		tacs, err := suite.tacFetcher.FetchOrderTransportationAccountingCodes(models.AffiliationARMY, ordersIssueDate, tacCode, appCtx)
+		suite.NoError(err)
+		suite.NotEmpty(tacs)
+		// Ensure we got 4 tacs
+		suite.Len(tacs, 4)
+		// Ensure LOA isn't nil
+		suite.NotNil(tacs[0].LineOfAccounting)
+		for _, tac := range tacs {
+			suite.NotNil(tac.LineOfAccounting)
+		}
+
+		// Now that we have our 4 tacs and loas, let's fetch and see if they're in order
+		// Extract LOAs
+		loas, err := suite.loaFetcher.FetchLongLinesOfAccounting(models.AffiliationARMY, ordersIssueDate, tacCode, appCtx)
+		suite.NoError(err)
+
+		// Check the order of LOAs
+		expectedOrder := []string{"1", "3", "5", "M"}
+		// Ensure we got the correct number of LOAs
+		suite.Len(loas, len(expectedOrder))
+		for i, loa := range loas {
+			suite.Equal(expectedOrder[i], *loa.LoaDscTx)
+		}
+	})
+	suite.Run("Filters out FMBC of value P", func() {
+		fbmcs := []string{
+			"1",
+			"P", // Make note of P. Our queries filter this out
+		}
+		ordersIssueDate, startDate, endDate, tacCode := setupTest()
+		appCtx := suite.AppContextForTest()
+
+		// Setup TGET data for tests
+		for fbmcMemoryIteration := range fbmcs {
+			// Use a unique LoaSysID for each fbmc to avoid duplicates
+			loaSysId := factory.MakeRandomString(20)
+			factory.BuildLineOfAccounting(appCtx.DB(), []factory.Customization{
+				{
+					Model: models.LineOfAccounting{
+						LoaBgnDt:   &startDate,
+						LoaEndDt:   &endDate,
+						LoaSysID:   &loaSysId,
+						LoaHsGdsCd: models.StringPointer(models.LineOfAccountingHouseholdGoodsCodeOfficer),
+						LoaDscTx:   &fbmcs[fbmcMemoryIteration],
+					},
+				},
+			}, nil)
+			factory.BuildTransportationAccountingCodeWithoutAttachedLoa(appCtx.DB(), []factory.Customization{
+				{
+					Model: models.TransportationAccountingCode{
+						TAC:               tacCode,
+						TrnsprtnAcntBgnDt: &startDate,
+						TrnsprtnAcntEndDt: &endDate,
+						TacFnBlModCd:      &fbmcs[fbmcMemoryIteration],
+						LoaSysID:          &loaSysId,
+					},
+				},
+			}, nil)
+		}
+
+		// Get the TACs that we will extract LOAs from
+		tacs, err := suite.tacFetcher.FetchOrderTransportationAccountingCodes(models.AffiliationARMY, ordersIssueDate, tacCode, appCtx)
+		suite.NoError(err)
+		suite.NotEmpty(tacs)
+		// Ensure we got 1 tac, FMBC P should've been filtered out
+		suite.Len(tacs, 1)
+		// Ensure LOA isn't nil
+		suite.NotNil(tacs[0].LineOfAccounting)
+		for _, tac := range tacs {
+			suite.NotNil(tac.LineOfAccounting)
+		}
+		// Extract LOAs
+		loas, err := suite.loaFetcher.FetchLongLinesOfAccounting(models.AffiliationARMY, ordersIssueDate, tacCode, appCtx)
+		suite.NoError(err)
+		// Ensure we got 1 loa, FBMC P should've been filtered out
+		suite.Len(loas, 1)
 	})
 }
