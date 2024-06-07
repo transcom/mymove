80c4dbbd9c90c283d3b6d5899b88883bb012b409
Maria Traskowsky <maria.traskowsky@caci.com>
2024-06-07 22:55:25 +0000

    remove unnecessary filtering of status causing SC completed moves to show up for New Move filter choice

--- pkg/handlers/ghcapi/queues.go
+++ pkg/handlers/ghcapi/queues.go
@@ -58,7 +58,10 @@ func (h GetMovesQueueHandler) Handle(params queues.GetMovesQueueParams) middlewa
 				OrderType:               params.OrderType,
 			}
 
-			ListOrderParams.Status = []string{string(models.MoveStatusServiceCounselingCompleted), string(models.MoveStatusAPPROVALSREQUESTED), string(models.MoveStatusSUBMITTED)}
+			// When no status filter applied, TOO should only see moves with status of New Move, Service Counseling Completed, or Approvals Requested
+			if params.Status == nil {
+				ListOrderParams.Status = []string{string(models.MoveStatusServiceCounselingCompleted), string(models.MoveStatusAPPROVALSREQUESTED), string(models.MoveStatusSUBMITTED)}
+			}
 
 			// Let's set default values for page and perPage if we don't get arguments for them. We'll use 1 for page and 20
 			// for perPage.
