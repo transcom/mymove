5011e290cb1363f7bd84830c645421e4d45aaa74
KonstanceH <Konstance.Haffaney@caci.com>
2024-06-07 15:32:36 +0000

    revert deleted line

--- src/constants/userRoles.js
+++ src/constants/userRoles.js
@@ -19,6 +19,7 @@ export const adminOfficeRoles = [
   { roleType: 'services_counselor', name: 'Services Counselor' },
   { roleType: 'prime_simulator', name: 'Prime Simulator' },
   { roleType: 'qae_csr', name: 'Quality Assurance and Customer Support' },
+  { roleType: 'headquarters', name: 'Headquarters' },
   { roleType: 'customer_service_representative', name: 'Customer Service Representative' },
 ];
 
