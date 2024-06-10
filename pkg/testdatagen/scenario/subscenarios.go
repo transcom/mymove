adcfd7e0047d7a788c92dcaa5a84135c850556ec
AaronW <aaron.weatherford@caci.com>
2024-06-10 21:14:37 +0000

    added nil check at the top of the devseed test to see if nil pointers are being passed into the test

--- pkg/testdatagen/scenario/subscenarios.go
+++ pkg/testdatagen/scenario/subscenarios.go
@@ -684,10 +684,13 @@ func subScenarioPaymentRequestCalculations(
 	shipmentFetcher services.MTOShipmentFetcher,
 ) func() {
 	return func() {
+		if appCtx == nil || userUploader == nil || primeUploader == nil || moveRouter == nil || shipmentFetcher == nil {
+			return
+		}
+
 		createTXO(appCtx)
 		createTXOUSMC(appCtx)
 
-		// For displaying the Domestic Line Haul calculations displayed on the Payment Requests and Service Item review page
 		createHHGMoveWithPaymentRequest(appCtx, userUploader, models.AffiliationAIRFORCE,
 			models.Move{
 				Locator: "SidDLH",
@@ -698,9 +701,7 @@ func subScenarioPaymentRequestCalculations(
 		)
 
 		createHHGWithPaymentServiceItems(appCtx, primeUploader, moveRouter, shipmentFetcher)
-		// Locator ORGSIT
 		createHHGWithOriginSITServiceItems(appCtx, primeUploader, moveRouter, shipmentFetcher)
-		// Locator DSTSIT
 		createHHGWithDestinationSITServiceItems(appCtx, primeUploader, moveRouter, shipmentFetcher)
 	}
 }
