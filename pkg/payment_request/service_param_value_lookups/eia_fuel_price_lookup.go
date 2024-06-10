7d4dc6574733f98fab86b379cfb2208dfa0b8d14
deandreJones <deandre.Jones@caci.com>
2024-06-10 15:20:42 +0000

    spelling and grammer

--- pkg/payment_request/service_param_value_lookups/eia_fuel_price_lookup.go
+++ pkg/payment_request/service_param_value_lookups/eia_fuel_price_lookup.go
@@ -27,12 +27,12 @@ func (r EIAFuelPriceLookup) lookup(appCtx appcontext.AppContext, _ *ServiceItemP
 
 	// Find the GHCDieselFuelPrice object effective before the shipment's ActualPickupDate and ends after the ActualPickupDate
 	var ghcDieselFuelPrice models.GHCDieselFuelPrice
-	err := db.Where("? BETWEEN effective_date and end_date", actualPickupDate).Order("publication_Date DESC").First(&ghcDieselFuelPrice) //only want the first published price per week
+	err := db.Where("? BETWEEN effective_date and end_date", actualPickupDate).Order("publication_date DESC").First(&ghcDieselFuelPrice) //only want the first published price per week
 	if err != nil {
 		switch err {
 		case sql.ErrNoRows:
 			// If no published price is found, look for the first published price after the actual pickup date
-			err = db.Where("publication_date <= ?", actualPickupDate).Order("publication_Date DESC").Last(&ghcDieselFuelPrice)
+			err = db.Where("publication_date <= ?", actualPickupDate).Order("publication_date DESC").Last(&ghcDieselFuelPrice)
 			if err != nil {
 				switch err {
 				case sql.ErrNoRows:
