2a8e916bce9424a8f8fa07cea04c3004a1d4f375
AaronW <aaron.weatherford@caci.com>
2024-06-12 16:35:33 -0500

    Revert "changed how distance zip param lookups are created in tests"
    This reverts commit b2b3935286ee3b472aae87683065f7eb095452fb.

--- pkg/payment_request/service_param_value_lookups/distance_zip_lookup_test.go
+++ pkg/payment_request/service_param_value_lookups/distance_zip_lookup_test.go
@@ -66,9 +66,10 @@ func (suite *ServiceParamValueLookupsSuite) TestDistanceLookup() {
 	suite.Run("Calculate zip distance lookup without a saved service item", func() {
 		ppmShipment := factory.BuildPPMShipment(suite.DB(), nil, nil)
 
-		paramKeyLookups := InitializeLookups(&ppmShipment.Shipment, nil)
-		distanceZipLookup, ok := paramKeyLookups[models.ServiceItemParamNameDistanceZip].(DistanceZipLookup)
-		suite.True(ok)
+		distanceZipLookup := DistanceZipLookup{
+			PickupAddress:      models.Address{PostalCode: ppmShipment.PickupPostalCode},
+			DestinationAddress: models.Address{PostalCode: ppmShipment.DestinationPostalCode},
+		}
 
 		appContext := suite.AppContextForTest()
 		distance, err := distanceZipLookup.lookup(appContext, &ServiceItemParamKeyData{
@@ -96,9 +97,10 @@ func (suite *ServiceParamValueLookupsSuite) TestDistanceLookup() {
 				},
 			},
 		}, nil)
-		paramKeyLookups := InitializeLookups(&ppmShipment.Shipment, nil)
-		distanceZipLookup, ok := paramKeyLookups[models.ServiceItemParamNameDistanceZip].(DistanceZipLookup)
-		suite.True(ok)
+		distanceZipLookup := DistanceZipLookup{
+			PickupAddress:      models.Address{PostalCode: ppmShipment.PickupPostalCode},
+			DestinationAddress: models.Address{PostalCode: ppmShipment.DestinationPostalCode},
+		}
 
 		appContext := suite.AppContextForTest()
 		distance, err := distanceZipLookup.lookup(appContext, &ServiceItemParamKeyData{
@@ -127,9 +129,11 @@ func (suite *ServiceParamValueLookupsSuite) TestDistanceLookup() {
 				},
 			},
 		}, nil)
-		paramKeyLookups := InitializeLookups(&shipment, nil)
-		distanceZipLookup, ok := paramKeyLookups[models.ServiceItemParamNameDistanceZip].(DistanceZipLookup)
-		suite.True(ok)
+
+		distanceZipLookup := DistanceZipLookup{
+			PickupAddress:      models.Address{PostalCode: shipment.PickupAddress.PostalCode},
+			DestinationAddress: models.Address{PostalCode: shipment.DestinationAddress.PostalCode},
+		}
 
 		appContext := suite.AppContextForTest()
 		distance, err := distanceZipLookup.lookup(appContext, &ServiceItemParamKeyData{
@@ -412,9 +416,10 @@ func (suite *ServiceParamValueLookupsSuite) TestDistanceLookup() {
 			},
 		}, nil)
 
-		paramKeyLookups := InitializeLookups(&MTOShipment, nil)
-		distanceZipLookup, ok := paramKeyLookups[models.ServiceItemParamNameDistanceZip].(DistanceZipLookup)
-		suite.True(ok)
+		distanceZipLookup := DistanceZipLookup{
+			PickupAddress:      models.Address{PostalCode: MTOShipment.PickupAddress.PostalCode},
+			DestinationAddress: models.Address{PostalCode: MTOShipment.DestinationAddress.PostalCode},
+		}
 
 		distance, err := distanceZipLookup.lookup(suite.AppContextForTest(), &ServiceItemParamKeyData{
 			planner:       suite.planner,
--- pkg/payment_request/service_param_value_lookups/service_param_value_lookups.go
+++ pkg/payment_request/service_param_value_lookups/service_param_value_lookups.go
@@ -210,7 +210,7 @@ func ServiceParamLookupInitialize(
 	// ReService code for current MTO Service Item
 	serviceItemCode := mtoServiceItem.ReService.Code
 
-	paramKeyLookups := InitializeLookups(appCtx, mtoShipment, mtoServiceItem)
+	paramKeyLookups := InitializeLookups(mtoShipment, mtoServiceItem)
 
 	for _, paramKeyName := range ServiceItemParamsWithLookups {
 		lookup, ok := paramKeyLookups[paramKeyName]
@@ -236,10 +236,8 @@ func (s *ServiceItemParamKeyData) setLookup(appCtx appcontext.AppContext, servic
 	return nil
 }
 
-func InitializeLookups(appCtx appcontext.AppContext, shipment models.MTOShipment, serviceItem models.MTOServiceItem) map[models.ServiceItemParamName]ServiceItemParamKeyLookup {
+func InitializeLookups(shipment models.MTOShipment, serviceItem models.MTOServiceItem) map[models.ServiceItemParamName]ServiceItemParamKeyLookup {
 	lookups := map[models.ServiceItemParamName]ServiceItemParamKeyLookup{}
-	shipment := mtoShipment
-	serviceItem := mtoServiceItem
 
 	if serviceItem.SITDestinationOriginalAddress == nil {
 		serviceItem.SITDestinationOriginalAddress = &models.Address{}
@@ -254,15 +252,15 @@ func InitializeLookups(appCtx appcontext.AppContext, shipment models.MTOShipment
 	}
 
 	lookups[models.ServiceItemParamNameActualPickupDate] = ActualPickupDateLookup{
-		MTOShipment: *shipment,
+		MTOShipment: shipment,
 	}
 
 	lookups[models.ServiceItemParamNameRequestedPickupDate] = RequestedPickupDateLookup{
-		MTOShipment: *shipment,
+		MTOShipment: shipment,
 	}
 
 	lookups[models.ServiceItemParamNameReferenceDate] = ReferenceDateLookup{
-		MTOShipment: *shipment,
+		MTOShipment: shipment,
 	}
 
 	serviceDestinationAddress, err := GetDestinationForDistanceLookup(appCtx, shipment, &serviceItem)
