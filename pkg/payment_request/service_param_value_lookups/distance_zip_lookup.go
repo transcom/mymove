7d08e6c8c9eafea197a875ca5e14b53613543554
AaronW <aaron.weatherford@caci.com>
2024-06-13 11:24:25 -0500

    Revert "changed distance zip lookup so that it will not run zip transit distance if its a ppm with a distance"
    This reverts commit cfb96a9655aa64cb695f5bd7367e45f1fdcf50f5.

--- pkg/payment_request/service_param_value_lookups/distance_zip_lookup.go
+++ pkg/payment_request/service_param_value_lookups/distance_zip_lookup.go
@@ -42,19 +42,7 @@ func (r DistanceZipLookup) lookup(appCtx appcontext.AppContext, keyData *Service
 
 	// Now calculate the distance between zips
 	pickupZip := r.PickupAddress.PostalCode
-	var destinationZip string
-	if mtoShipment.ShipmentType == models.MTOShipmentTypeHHG {
-		var destResult models.Address
-		var errDestinationZip error
-		destResult, errDestinationZip = GetDestinationForDistanceLookup(appCtx, mtoShipment, &keyData.MTOServiceItem)
-		if errDestinationZip != nil {
-			return "", err
-		}
-		destinationZip = destResult.PostalCode
-	} else {
-		destinationZip = r.DestinationAddress.PostalCode
-	}
-
+	destinationZip := r.DestinationAddress.PostalCode
 	errorMsgForPickupZip := fmt.Sprintf("Shipment must have valid pickup zipcode. Received: %s", pickupZip)
 	errorMsgForDestinationZip := fmt.Sprintf("Shipment must have valid destination zipcode. Received: %s", destinationZip)
 	if len(pickupZip) < 5 {
@@ -95,7 +83,7 @@ func (r DistanceZipLookup) lookup(appCtx appcontext.AppContext, keyData *Service
 	var distanceMiles int
 	if pickupZip == destinationZip {
 		distanceMiles = 1
-	} else if mtoShipment.ShipmentType == models.MTOShipmentTypePPM && mtoShipment.Distance != nil && *mtoShipment.Distance != 0 {
+	} else {
 		distanceMiles, err = planner.ZipTransitDistance(appCtx, pickupZip, destinationZip)
 	}
 
