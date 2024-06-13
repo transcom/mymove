8afab5a069b9bf953ecc5e49fc7bc11b4a38ee25
loganwc <logan.cunningham@caci.com>
2024-06-13 16:12:02 +0000

    added route and page for Supporting Documents

---
+++ src/pages/Office/SupportingDocuments/SupportingDocuments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SupportingDocuments = ({ moveCode }) => {
+  return (
+    <div>
+      {moveCode}
+      &nbsp;
+      <span>I did a thing.</span>
+    </div>
+  );
+};
+
+export default SupportingDocuments;
