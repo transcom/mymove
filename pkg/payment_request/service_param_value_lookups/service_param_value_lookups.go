0b9724b1d25d971d50b8f6b81c1725a068baafbe
AaronW <aaron.weatherford@caci.com>
2024-06-14 00:23:24 +0000

    test updates

--- pkg/payment_request/service_param_value_lookups/service_param_value_lookups.go
+++ pkg/payment_request/service_param_value_lookups/service_param_value_lookups.go
@@ -580,7 +580,7 @@ func getDestinationAddressForService(appCtx appcontext.AppContext, serviceCode m
 		}
 		if shipmentCopy.DeliveryAddressUpdate.Status == models.ShipmentAddressUpdateStatusApproved {
 			return shipmentCopy.DeliveryAddressUpdate.NewAddress, nil
-		} else if serviceCode == models.ReServiceCodeDUPK || serviceCode == models.ReServiceCodeDPK {
+		} else if serviceCode == models.ReServiceCodeDPK || serviceCode == models.ReServiceCodeDNPK {
 			return models.Address{}, nil
 		} else {
 			return *shipmentCopy.DestinationAddress, nil
