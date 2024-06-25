b07968a23e5b93a7bd380d64ef29f54a7101835f
joeydoyecaci <joseph.doye@caci.com>
2024-06-25 15:47:56 +0000

    removed postal code

--- migrations/app/schema/20240516230021_add_third_address_shipments.up.sql
+++ migrations/app/schema/20240516230021_add_third_address_shipments.up.sql
@@ -1,15 +1,11 @@
 -- Adds tertiary addresses to ppm_shipments.
 -- Only adds if the columns don't exist
 ALTER TABLE ppm_shipments
-ADD COLUMN IF NOT EXISTS tertiary_pickup_postal_code varchar NULL,
-ADD COLUMN IF NOT EXISTS tertiary_destination_postal_code varchar NULL,
 ADD COLUMN IF NOT EXISTS tertiary_pickup_postal_address_id UUID NULL,
 ADD COLUMN IF NOT EXISTS tertiary_destination_postal_address_id UUID NULL,
 ADD COLUMN IF NOT EXISTS has_tertiary_pickup_address bool NULL,
 ADD COLUMN IF NOT EXISTS has_tertiary_destination_address bool NULL;
 
-COMMENT ON COLUMN ppm_shipments.tertiary_pickup_postal_code IS 'Tertiary postal code where mto_shipment tertiary_pickup_postal_code is to be picked up.';
-COMMENT ON COLUMN ppm_shipments.tertiary_destination_postal_code IS 'Tertiary postal code where mto_shipment tertiary_delivery_postal_code is to be picked up.';
 COMMENT ON COLUMN ppm_shipments.tertiary_pickup_postal_address_id IS 'The tertiary destination address for this shipment.';
 COMMENT ON COLUMN ppm_shipments.tertiary_destination_postal_address_id IS 'The tertiary destination address for this shipment.';
 COMMENT ON COLUMN ppm_shipments.has_tertiary_pickup_address IS 'False if the ppm shipment does not have a tertiary pickup address. This column exists to make it possible to tell whether a shipment update should delete an address or not modify it.';
@@ -19,15 +15,11 @@ ALTER TABLE ppm_shipments ADD CONSTRAINT ppm_shipments_pickup_postal_address_id_
 ALTER TABLE ppm_shipments ADD CONSTRAINT ppm_shipments_destination_postal_address_id_fkey FOREIGN KEY (tertiary_destination_postal_address_id) REFERENCES addresses(id);
 
 ALTER TABLE mto_shipments
-ADD COLUMN IF NOT EXISTS tertiary_pickup_postal_code varchar NULL,
-ADD COLUMN IF NOT EXISTS tertiary_delivery_postal_code varchar NULL,
 ADD COLUMN IF NOT EXISTS tertiary_pickup_address_id UUID NULL,
 ADD COLUMN IF NOT EXISTS tertiary_delivery_address_id UUID NULL,
 ADD COLUMN IF NOT EXISTS has_tertiary_pickup_address bool NULL,
 ADD COLUMN IF NOT EXISTS has_tertiary_delivery_address bool NULL;
 
-COMMENT ON COLUMN mto_shipments.tertiary_pickup_postal_code IS 'Tertiary postal code where mto_shipment tertiary_pickup_postal_code is to be picked up.';
-COMMENT ON COLUMN mto_shipments.tertiary_delivery_postal_code IS 'Tertiary postal code where mto_shipment tertiary_delivery_postal_code is to be picked up.';
 COMMENT ON COLUMN mto_shipments.tertiary_pickup_address_id IS 'The secondary pickup address for this shipment.';
 COMMENT ON COLUMN mto_shipments.tertiary_delivery_address_id IS 'The secondary delivery address for this shipment.';
 COMMENT ON COLUMN mto_shipments.has_tertiary_pickup_address IS 'False if the ppm shipment does not have a tertiary pickup address. This column exists to make it possible to tell whether a shipment update should delete an address or not modify it.';
