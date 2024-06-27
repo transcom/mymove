fe5e737f6d8fb0b07f7933ed57d8d441541e68b5
Cory Kleinjan <cory.kleinjan@caci.com>
2024-06-27 14:10:58 -0500

    updating tio tests

--- playwright/tests/office/txo/tioUpdateMaxBillableWeight.spec.js
+++ playwright/tests/office/txo/tioUpdateMaxBillableWeight.spec.js
@@ -30,7 +30,7 @@ test.describe('TIO user', () => {
 
     const fieldset = page.locator('fieldset');
     await fieldset.locator('input#billableWeight').clear();
-    await fieldset.locator('input#billableWeight').type('7400');
+    await fieldset.locator('input#billableWeight').type('2200');
     await fieldset.locator('textarea#billableWeightJustification').type('Some basic remarks.');
 
     await page.getByRole('button', { name: 'Save changes' }).click();
