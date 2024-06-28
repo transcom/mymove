a6341ed4b1e5967922a0803ce6fe534abe6ab77b
Maria Traskowsky <maria.traskowsky@caci.com>
2024-06-28 16:25:27 +0000

    handle frontend combined affiliations

--- pkg/gen/internalapi/embedded_spec.go
+++ pkg/gen/internalapi/embedded_spec.go
@@ -3400,6 +3400,8 @@ func init() {
         "AIR_FORCE",
         "COAST_GUARD",
         "SPACE_FORCE",
+        "NAVY_AND_MARINES",
+        "AIR_AND_SPACE_FORCE",
         "OTHER"
       ],
       "x-display-value": {
@@ -11349,6 +11351,8 @@ func init() {
         "AIR_FORCE",
         "COAST_GUARD",
         "SPACE_FORCE",
+        "NAVY_AND_MARINES",
+        "AIR_AND_SPACE_FORCE",
         "OTHER"
       ],
       "x-display-value": {
