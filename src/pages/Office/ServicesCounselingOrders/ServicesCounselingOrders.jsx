548e3e621ef39b74314ea11f3ddd7ce91e23558f
Maria Traskowsky <maria.traskowsky@caci.com>
2024-06-28 21:18:41 +0000

    update comment

--- src/pages/Office/ServicesCounselingOrders/ServicesCounselingOrders.jsx
+++ src/pages/Office/ServicesCounselingOrders/ServicesCounselingOrders.jsx
@@ -106,7 +106,7 @@ const ServicesCounselingOrders = ({ hasDocuments }) => {
       return loa[key] || '';
     });
     let longLoa = dfasMap.join('*');
-    // remove spaces from instances of '* *' or '*    *' or any number of spaces between two asterisks
+    // remove any number of spaces following an asterisk in a LOA string
     longLoa = longLoa.replace(/\* +/g, '*');
     return longLoa;
   };
