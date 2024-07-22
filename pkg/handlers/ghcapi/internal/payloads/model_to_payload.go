40b5e3f708d7685fa278f5d45cbe1b9885a36b30
James Hawks <james.hawks@caci.com>
2024-07-22 15:12:37 -0500

    changed code and added test.

--- pkg/handlers/ghcapi/internal/payloads/model_to_payload.go
+++ pkg/handlers/ghcapi/internal/payloads/model_to_payload.go
@@ -2126,13 +2126,20 @@ func ShipmentsPaymentSITBalance(shipmentsSITBalance []services.ShipmentPaymentSI
 func SearchCustomers(customers models.ServiceMembers) *ghcmessages.SearchCustomers {
 	searchCustomers := make(ghcmessages.SearchCustomers, len(customers))
 	for i, customer := range customers {
+		var personalEmail string
+		if customer.PersonalEmail != nil {
+			personalEmail = *customer.PersonalEmail
+		} else {
+			personalEmail = ""
+		}
+
 		searchCustomers[i] = &ghcmessages.SearchCustomer{
 			FirstName:     customer.FirstName,
 			LastName:      customer.LastName,
 			DodID:         customer.Edipi,
 			Branch:        customer.Affiliation.String(),
 			ID:            *handlers.FmtUUID(customer.ID),
-			PersonalEmail: *customer.PersonalEmail,
+			PersonalEmail: personalEmail,
 			Telephone:     customer.Telephone,
 		}
 	}
