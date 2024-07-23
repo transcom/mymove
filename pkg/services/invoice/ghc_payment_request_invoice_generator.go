87b7cd1ae4f54e1a415d421c23f216a799d6d092
Daniel Jordan <daniel.jordan@caci.com>
2024-07-23 16:19:40 +0000

    initial commit, need to add test for changes

--- pkg/services/invoice/ghc_payment_request_invoice_generator.go
+++ pkg/services/invoice/ghc_payment_request_invoice_generator.go
@@ -44,6 +44,7 @@ const dateFormat = "20060102"
 const isaDateFormat = "060102"
 const timeFormat = "1504"
 const maxCityLength = 30
+const maxLocationlength = 60
 
 // Generate method takes a payment request and returns an Invoice858C
 func (g ghcPaymentRequestInvoiceGenerator) Generate(appCtx appcontext.AppContext, paymentRequest models.PaymentRequest, sendProductionInvoice bool) (ediinvoice.Invoice858C, error) {
@@ -428,10 +429,11 @@ func (g ghcPaymentRequestInvoiceGenerator) createBuyerAndSellerOrganizationNames
 
 	header.BuyerOrganizationName = edisegment.N1{
 		EntityIdentifierCode:        "BY",
-		Name:                        originDutyLocation.Name,
+		Name:                        truncateStr(originDutyLocation.Name, maxLocationlength),
 		IdentificationCodeQualifier: "92",
 		IdentificationCode:          modifyGblocIfMarines(*orders.ServiceMember.Affiliation, *orders.OriginDutyLocationGBLOC),
 	}
+
 	// seller organization name
 	header.SellerOrganizationName = edisegment.N1{
 		EntityIdentifierCode:        "SE",
@@ -463,7 +465,7 @@ func (g ghcPaymentRequestInvoiceGenerator) createOriginAndDestinationSegments(ap
 	// destination name
 	header.DestinationName = edisegment.N1{
 		EntityIdentifierCode:        "ST",
-		Name:                        destinationDutyLocation.Name,
+		Name:                        truncateStr(destinationDutyLocation.Name, maxLocationlength),
 		IdentificationCodeQualifier: "10",
 		IdentificationCode:          modifyGblocIfMarines(*orders.ServiceMember.Affiliation, destPostalCodeToGbloc.GBLOC),
 	}
@@ -524,7 +526,7 @@ func (g ghcPaymentRequestInvoiceGenerator) createOriginAndDestinationSegments(ap
 
 	header.OriginName = edisegment.N1{
 		EntityIdentifierCode:        "SF",
-		Name:                        originDutyLocation.Name,
+		Name:                        truncateStr(originDutyLocation.Name, maxLocationlength),
 		IdentificationCodeQualifier: "10",
 		IdentificationCode:          modifyGblocIfMarines(*orders.ServiceMember.Affiliation, *orders.OriginDutyLocationGBLOC),
 	}
