153e1ed78645696f2980f5adc5a580de62727fc0
Jon Spight <Jonathan.Spight@caci.com>
2024-08-08 20:21:05 +0000

    update migration file

--- migrations/app/schema/20240807212737_add_counseling_transportation_office_id_to_moves.up.sql
+++ migrations/app/schema/20240807212737_add_counseling_transportation_office_id_to_moves.up.sql
@@ -1,6 +1,6 @@
 ALTER TABLE moves
     ADD COLUMN IF NOT EXISTS counseling_transportation_office_id uuid
-    ADD CONSTRAINT moves_counseling_transportation_office_id_fkey
+    CONSTRAINT moves_counseling_transportation_office_id_fkey
             REFERENCES transportation_offices;
 
 COMMENT ON COLUMN moves.counseling_transportation_office_id IS 'A foreign key that points to the counseling transportation office on the transportation_offices table';
