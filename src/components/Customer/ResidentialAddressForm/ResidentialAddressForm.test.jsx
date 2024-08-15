be2e1eb903b5719661682839f0c88d5412a1be04
Ricky Mettler <ricky.mettler@caci.com>
2024-08-15 22:29:43 +0000

    test fixes

--- src/components/Customer/ResidentialAddressForm/ResidentialAddressForm.test.jsx
+++ src/components/Customer/ResidentialAddressForm/ResidentialAddressForm.test.jsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
-import selectEvent from 'react-select-event';
-import { renderWithRouter } from 'testUtils';
 
 import ResidentialAddressForm from './ResidentialAddressForm';
-import { searchLocationByZipCity } from 'services/internalApi';
-
-const mockNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockNavigate,
-}));
-
-jest.mock('services/internalApi', () => ({
-  ...jest.requireActual('services/internalApi'),
-  searchLocationByZipCity: jest.fn(),
-}));
 
 describe('ResidentialAddressForm component', () => {
   const formFieldsName = 'current_residence';
@@ -31,7 +16,6 @@ describe('ResidentialAddressForm component', () => {
         city: '',
         state: '',
         postalCode: '',
-        county: '',
       },
     },
     onSubmit: jest.fn().mockImplementation(() => Promise.resolve()),
@@ -44,24 +28,36 @@ describe('ResidentialAddressForm component', () => {
     city: 'El Paso',
     state: 'TX',
     postalCode: '79912',
-    county: 'El Paso',
   };
 
+  const dataProps = {
+    formFieldsName,
+    initialValues: {
+      [formFieldsName]: {
+        streetAddress1: '',
+        streetAddress2: '',
+        city: fakeAddress.city,
+        state: fakeAddress.state,
+        postalCode: fakeAddress.postalCode,
+        county: fakeAddress.county,
+      },
+    },
+    onSubmit: jest.fn().mockImplementation(() => Promise.resolve()),
+    onBack: jest.fn(),
+  };
+
+
   it('renders the form inputs and help text', async () => {
-    const { getByRole, getByLabelText, getByText } = render(<ResidentialAddressForm {...testProps} />);
+    const { getByLabelText, getByText } = render(<ResidentialAddressForm {...testProps} />);
 
     await waitFor(() => {
       expect(getByLabelText('Address 1')).toBeInstanceOf(HTMLInputElement);
 
       expect(getByLabelText(/Address 2/)).toBeInstanceOf(HTMLInputElement);
 
-      expect(getByRole('combobox', { id: 'zipCity-input' })).toBeInstanceOf(HTMLInputElement);
-
       expect(getByLabelText('City')).toBeInstanceOf(HTMLInputElement);
 
       expect(getByLabelText('State')).toBeInstanceOf(HTMLInputElement);
-
-      expect(getByLabelText('County')).toBeInstanceOf(HTMLInputElement);
 
       expect(getByLabelText('ZIP')).toBeInstanceOf(HTMLInputElement);
 
@@ -70,31 +66,10 @@ describe('ResidentialAddressForm component', () => {
   });
 
   it('shows an error message if trying to submit an invalid form', async () => {
-    const mockAddress = {
-      city: 'El Paso',
-      state: 'TX',
-      postalCode: '79912',
-      county: 'El Paso',
-    };
-    const mockSearchLocationByZipCity= () => Promise.resolve(mockAddress);
-    searchLocationByZipCity.mockImplementation(mockSearchLocationByZipCity);
-
     const { getByRole, findAllByRole, getByLabelText } = render(<ResidentialAddressForm {...testProps} />);
     await userEvent.click(getByLabelText('Address 1'));
     await userEvent.click(getByLabelText(/Address 2/));
-    const input = getByRole('combobox', { id: 'zipCity-input' });
-    fireEvent.change(input, {target: {value: mockAddress.postalCode} });
-    await act(() => selectEvent.select(input, 79912));
 
-    await waitFor(() => {
-      expect(screen.getByText(mockAddress.postalCode)).toBeInTheDocument();
-    });
-
-    // fireEvent.keyPress(input, { key: 'Enter', code: 13 });
-
-    await waitFor(() => {
-      expect(searchLocationByZipCity).toHaveBeenCalledTimes(1);
-    });
     const submitBtn = getByRole('button', { name: 'Next' });
     await userEvent.click(submitBtn);
 
@@ -110,29 +85,13 @@ describe('ResidentialAddressForm component', () => {
   });
 
   it('submits the form when its valid', async () => {
-    const mockAddress = {
-      city: 'El Paso',
-      state: 'TX',
-      postalCode: '79912',
-      county: 'El Paso',
-    };
-    const mockSearchLocationByZipCity= () => Promise.resolve(mockAddress);
-    searchLocationByZipCity.mockImplementation(mockSearchLocationByZipCity);
+    const { getByRole, getByLabelText } = render(<ResidentialAddressForm {...dataProps} />);
+    const submitBtn = getByRole('button', { name: 'Next' });
 
-    // const { getByRole, getByLabelText } = render(<ResidentialAddressForm {...testProps} />);
-    renderWithRouter(<ResidentialAddressForm {...testProps} />);
-    const submitBtn = screen.getByRole('button', { name: 'Next' });
+    await userEvent.type(getByLabelText('Address 1'), fakeAddress.streetAddress1);
+    await userEvent.type(getByLabelText(/Address 2/), fakeAddress.streetAddress2);
+    await userEvent.tab();
 
-    await userEvent.type(screen.getByLabelText('Address 1'), fakeAddress.streetAddress1);
-    await userEvent.type(screen.getByLabelText(/Address 2/), fakeAddress.streetAddress2);
-    const input = screen.getByLabelText('Zip/City Lookup');
-    await userEvent.type(input, '79912');
-    fireEvent.change(input, {target: {value: mockAddress.postalCode} });
-    // await waitFor(() => {
-    //   expect(screen.getByText(mockAddress.state)).toBeInTheDocument();
-    // });
-    await act(() => selectEvent.select(input, mockAddress.postalCode));
-    expect(screen.getByLabelText('State')).toHaveValue(mockAddress.state);
     await waitFor(() => {
       expect(submitBtn).toBeEnabled();
     });
@@ -143,7 +102,7 @@ describe('ResidentialAddressForm component', () => {
     };
 
     await waitFor(() => {
-      expect(testProps.onSubmit).toHaveBeenCalledWith(expectedParams, expect.anything());
+      expect(dataProps.onSubmit).toHaveBeenCalledWith(expectedParams, expect.anything());
     });
   });
 
