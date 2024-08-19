88da3b9de47b85be276a54e02db88122932a5ba6
Ricky Mettler <ricky.mettler@caci.com>
2024-08-19 17:20:00 +0000

    test fixes

--- src/components/form/fields/ZipCityInput.test.jsx
+++ src/components/form/fields/ZipCityInput.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { shallow } from 'enzyme';
 import AsyncSelect from 'react-select/async';
 
 import { ZipCityInput } from './ZipCityInput';
 
 import { LocationSearchBoxComponent, LocationSearchBoxContainer } from 'components/LocationSearchBox/LocationSearchBox';
+import { searchLocationByZipCity } from 'services/internalApi';
 
 const mockOnChange = jest.fn();
 const mockSetValue = jest.fn();
@@ -24,26 +24,23 @@ jest.mock('formik', () => {
   };
 });
 
-jest.mock('services/internalapi', () => {
-  return {
-    searchLocationByZipCity: () =>
-      new Promise((resolve) => {
-        resolve(["El Paso"]);
-      }),
-  };
-});
+jest.mock('services/internalApi', () => ({
+  ...jest.requireActual('services/internalApi'),
+  searchLocationByZipCity: jest.fn(),
+}));
 
 const handleZipCityChange = jest.fn();
 
 describe('ZipCityInput', () => {
   describe('with all required props', () => {
     const wrapper = shallow(
-        <ZipCityInput name="zipCity"
-            placeholder="Start typing a Zip Code or City..."
-            label="Zip/City Lookup"
-            displayAddress={false}
-            handleZipCityChange={handleZipCityChange} 
-        />
+      <ZipCityInput
+        name="zipCity"
+        placeholder="Start typing a Zip Code or City..."
+        label="Zip/City Lookup"
+        displayAddress={false}
+        handleZipCityChange={handleZipCityChange}
+      />,
     );
 
     it('renders a Zip City search input', () => {
@@ -52,11 +49,13 @@ describe('ZipCityInput', () => {
     });
 
     it('triggers onChange properly', async () => {
+      const cityName = 'El Paso';
+      searchLocationByZipCity.mockImplementation(() => Promise.resolve(cityName));
       const container = wrapper.find(LocationSearchBoxContainer).dive();
       const component = container.find(LocationSearchBoxComponent).dive();
       const select = component.find(AsyncSelect);
-      await select.simulate('change', { city: "El Paso" });
-      expect(mockSetValue).toHaveBeenCalledWith({ city: "El Paso"});
+      await select.simulate('change', { city: cityName });
+      expect(mockSetValue).toHaveBeenCalledWith({ city: cityName });
     });
   });
 
