e8622e813410b2ccfb9c21a62de86994931c5ff1
Tevin Adams <tevin.adams@caci.com>
2024-08-20 15:02:20 +0000

    Source PPM address information if PPM shipment

--- src/pages/Office/MoveTaskOrder/MoveTaskOrder.jsx
+++ src/pages/Office/MoveTaskOrder/MoveTaskOrder.jsx
@@ -1199,6 +1199,7 @@ export const MoveTaskOrder = (props) => {
             const dutyLocationPostal = { postalCode: order.destinationDutyLocation.address.postalCode };
             const { pickupAddress, destinationAddress } = mtoShipment;
             const formattedScheduledPickup = formatShipmentDate(mtoShipment.scheduledPickupDate);
+            const isPPM = mtoShipment.shipmentType === SHIPMENT_OPTIONS.PPM;
 
             return (
               <ShipmentContainer
@@ -1212,10 +1213,16 @@ export const MoveTaskOrder = (props) => {
                     shipmentID: mtoShipment.id,
                     shipmentType: mtoShipmentTypes[mtoShipment.shipmentType],
                     isDiversion: mtoShipment.diversion,
-                    originCity: pickupAddress?.city || '',
-                    originState: pickupAddress?.state || '',
-                    originPostalCode: pickupAddress?.postalCode || '',
-                    destinationAddress: destinationAddress || dutyLocationPostal,
+                    originCity: isPPM ? mtoShipment?.ppmShipment?.pickupAddress?.city || '' : pickupAddress?.city || '',
+                    originState: isPPM
+                      ? mtoShipment?.ppmShipment?.pickupAddress?.state || ''
+                      : pickupAddress?.state || '',
+                    originPostalCode: isPPM
+                      ? mtoShipment?.ppmShipment?.pickupAddress?.postalCode || ''
+                      : pickupAddress?.postalCode || '',
+                    destinationAddress: isPPM
+                      ? mtoShipment?.ppmShipment?.destinationAddress || ''
+                      : destinationAddress || dutyLocationPostal,
                     scheduledPickupDate: formattedScheduledPickup,
                     shipmentStatus: mtoShipment.status,
                     ifMatchEtag: mtoShipment.eTag,
