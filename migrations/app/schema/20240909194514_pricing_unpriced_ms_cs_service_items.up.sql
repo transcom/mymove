bbf942282aa73229f1341af78ba60f40a94a7743
Cory Kleinjan <cory.kleinjan@caci.com>
2024-09-10 02:09:50 +0000

    Updating new migration

--- migrations/app/schema/20240909194514_pricing_unpriced_ms_cs_service_items.up.sql
+++ migrations/app/schema/20240909194514_pricing_unpriced_ms_cs_service_items.up.sql
@@ -1,19 +1,23 @@
 UPDATE mto_service_items AS ms
 SET locked_price_cents =
         CASE
-            when price_cents > 0 AND (s.code = 'MS' OR s.code = 'CS') AND re_service_id = s.id  then price_cents
-            when price_cents = 0 AND (s.code = 'MS' OR s.code = 'CS') AND re_service_id = s.id  then 0
+            when price_cents > 0 AND (s.code = 'MS' OR s.code = 'CS') AND ms.re_service_id = s.id  then price_cents
+            when price_cents = 0 AND (s.code = 'MS' OR s.code = 'CS') AND ms.re_service_id = s.id  then 0
         END,
     pricing_estimate =
         CASE
-            when price_cents > 0 AND (s.code = 'MS' OR s.code = 'CS') AND re_service_id = s.id then price_cents
-            when price_cents = 0 AND (s.code = 'MS' OR s.code = 'CS') AND re_service_id = s.id  then 0
+            when price_cents > 0 AND (s.code = 'MS' OR s.code = 'CS') AND ms.re_service_id = s.id then price_cents
+            when price_cents = 0 AND (s.code = 'MS' OR s.code = 'CS') AND ms.re_service_id = s.id  then 0
         END
 FROM re_task_order_fees AS tf
 JOIN re_services AS s
 ON tf.service_id = s.id
 JOIN re_contract_years AS cy
 ON tf.contract_year_id = cy.id
-JOIN re_contracts AS c
-ON cy.contract_id = c.id
-WHERE (s.code = 'MS' OR s.code = 'CS') AND ms.re_service_id = s.id AND ms.locked_price_cents is null AND ms.pricing_estimate is null;
+JOIN re_contracts AS ct
+ON cy.contract_id = ct.id
+JOIN mto_service_items AS msi
+ON s.id = msi.re_service_id
+JOIN moves AS mo
+ON mo.id = msi.move_id
+WHERE (s.code = 'MS' OR s.code = 'CS') AND (mo.available_to_prime_at BETWEEN cy.start_date AND cy.end_date) AND ms.re_service_id = s.id AND ms.locked_price_cents is null AND ms.pricing_estimate is null;
