a0bdfae22907812bb7661ca77dadde7c5ed19bf9
Maria Traskowsky <maria.traskowsky@caci.com>
2024-09-13 14:45:29 +0000

    fix background color to be accessible

--- src/pages/Office/MovePaymentRequests/MovePaymentRequests.jsx
+++ src/pages/Office/MovePaymentRequests/MovePaymentRequests.jsx
@@ -221,7 +221,7 @@ const MovePaymentRequests = ({
       <div className={txoStyles.container} data-testid="MovePaymentRequests">
         <LeftNav sections={sections}>
           <LeftNavTag
-            background="#d63E04"
+            background="#e34b11"
             associatedSectionName="payment-requests"
             showTag={pendingCount > 0}
             testID="numOfPendingPaymentRequestsTag"
