c1a51e4a149beb6ed62b60cc4ab6f383c4b573c3
Maria Traskowsky <maria.traskowsky@caci.com>
2024-09-16 19:03:12 +0000

    ntsr logic tests passing

--- src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.test.jsx
+++ src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.test.jsx
@@ -542,7 +542,7 @@ describe('MoveDetails page', () => {
       },
     );
 
-    it('renders the number of shipment concerns for all shipments in a section', async () => {
+    it('renders warnings and errors on left nav bar for all shipments in a section', async () => {
       const moveDetailsQuery = {
         ...newMoveDetailsQuery,
         mtoShipments: [ntsrShipmentMissingRequiredInfo],
@@ -555,7 +555,8 @@ describe('MoveDetails page', () => {
 
       // In this case, we would expect 3 since this shipment is missing the storage facility
       // and tac type.
-      expect(await screen.findByTestId('requestedShipmentsTag')).toHaveTextContent('');
+      expect(await screen.findByTestId('requestedShipmentsTag')).toBeInTheDocument();
+      expect(await screen.findByTestId('shipment-missing-info-alert')).toBeInTheDocument();
     });
 
     it('shares the number of missing shipment information', () => {
@@ -683,7 +684,7 @@ describe('MoveDetails page', () => {
       expect(excessWeightAlert).toBeInTheDocument();
 
       // In this case, we would expect 1 shipment concern since the move has excess weight
-      expect(await screen.findByTestId('requestedShipmentsTag')).toHaveTextContent('1');
+      expect(await screen.findByTestId('requestedShipmentsTag')).toBeInTheDocument();
     });
 
     it('renders the allowances error message when allowances are less than moves values', async () => {
