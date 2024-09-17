aef165faac2a88e35d2e26fb552061c4f2a6be52
deandreJones <deandre.Jones@caci.com>
2024-09-17 14:06:18 -0500

    run always

--- playwright/tests/utils/waitForPage.js
+++ playwright/tests/utils/waitForPage.js
@@ -19,19 +19,17 @@ export class WaitForPage {
    */
 
   async runAccessibilityAudit() {
-    if (process.env.A11Y_AUDIT) {
-      await checkA11y(
-        this.page,
-        undefined,
-        {
-          detailedReport: true,
-          detailedReportOptions: { html: true },
-        },
-        // skip failures
-        false,
-        'html',
-      );
-    }
+    await checkA11y(
+      this.page,
+      undefined,
+      {
+        detailedReport: true,
+        detailedReportOptions: { html: true },
+      },
+      // skip failures
+      false,
+      'html',
+    );
   }
 
   /**
