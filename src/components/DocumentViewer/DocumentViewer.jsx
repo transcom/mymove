2f6fe415d9ebc4010ae4c47a4c81d46818b971f2
Tae Jung <tae.jung@caci.com>
2024-09-20 19:27:37 +0000

    undo

--- src/components/DocumentViewer/DocumentViewer.jsx
+++ src/components/DocumentViewer/DocumentViewer.jsx
@@ -21,7 +21,7 @@ import AsyncPacketDownloadLink from 'shared/AsyncPacketDownloadLink/AsyncPacketD
  * TODO
  * - implement next/previous pages instead of scroll through pages
  * - implement rotate left/right
- * - handle fetch doc errorsd
+ * - handle fetch doc errors
  */
 
 const DocumentViewer = ({ files, allowDownload, paymentRequestId }) => {
