b0af094af25689d3eacc5a456f32c6cdc1c87460
AaronW <aaron.weatherford@caci.com>
2024-09-30 18:42:32 +0000

    Wrapping original changes from PR 13642 back into INT
    The reason for this is because the test coverage was updated which cause test coverage errors in the PR going into MAIN

--- src/pages/Office/MoveDetails/MoveDetails.jsx
+++ src/pages/Office/MoveDetails/MoveDetails.jsx
@@ -412,6 +412,8 @@ const MoveDetails = ({
                 <option value={SHIPMENT_OPTIONS_URL.PPM}>PPM</option>
                 <option value={SHIPMENT_OPTIONS_URL.NTS}>NTS</option>
                 <option value={SHIPMENT_OPTIONS_URL.NTSrelease}>NTS-release</option>
+                <option value={SHIPMENT_OPTIONS_URL.BOAT}>Boat</option>
+                <option value={SHIPMENT_OPTIONS_URL.MOBILE_HOME}>Mobile Home</option>
               </ButtonDropdown>
             </Restricted>
           )}
