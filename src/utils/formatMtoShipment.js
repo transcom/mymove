c3cfaf1c9a3b97137683b1d85daeabe79fa1bfb6
AaronW <aaron.weatherford@caci.com>
2024-10-04 16:46:18 +0000

    wrapped in changes from previous merge

--- src/utils/formatMtoShipment.js
+++ src/utils/formatMtoShipment.js
@@ -518,6 +518,76 @@ export function convertInchesToFeetAndInches(totalInches) {
   return { feet, inches };
 }
 
+// Initial values for mobile home shipment
+export function formatMobileHomeShipmentForDisplay(mobileHomeShipment, initialValues) {
+  const { year, make, model, lengthInInches, widthInInches, heightInInches } = mobileHomeShipment || {};
+
+  const length = convertInchesToFeetAndInches(lengthInInches);
+  const width = convertInchesToFeetAndInches(widthInInches);
+  const height = convertInchesToFeetAndInches(heightInInches);
+
+  const displayValues = {
+    year: year?.toString() || null,
+    make: make || '',
+    model: model || '',
+    lengthFeet: length.feet,
+    lengthInches: length.inches,
+    widthFeet: width.feet,
+    widthInches: width.inches,
+    heightFeet: height.feet,
+    heightInches: height.inches,
+    ...initialValues,
+  };
+
+  return displayValues;
+}
+
+export function formatMobileHomeShipmentForAPI(values) {
+  const totalLengthInInches = toTotalInches(values.lengthFeet, values.lengthInches);
+  const totalWidthInInches = toTotalInches(values.widthFeet, values.widthInches);
+  const totalHeightInInches = toTotalInches(values.heightFeet, values.heightInches);
+
+  const mobileHomeShipment = {
+    year: Number(values.year),
+    make: values.make,
+    model: values.model,
+    lengthInInches: totalLengthInInches,
+    widthInInches: totalWidthInInches,
+    heightInInches: totalHeightInInches,
+  };
+
+  return {
+    mobileHomeShipment,
+  };
+}
+
+// Initial values for boat shipment
+export function formatBoatShipmentForDisplay(boatShipment, initialValues) {
+  const { year, make, model, lengthInInches, widthInInches, heightInInches, hasTrailer, isRoadworthy } =
+    boatShipment || {};
+
+  const length = convertInchesToFeetAndInches(lengthInInches);
+  const width = convertInchesToFeetAndInches(widthInInches);
+  const height = convertInchesToFeetAndInches(heightInInches);
+
+  const displayValues = {
+    year: year?.toString() || null,
+    make: make || '',
+    model: model || '',
+    lengthFeet: length.feet,
+    lengthInches: length.inches,
+    widthFeet: width.feet,
+    widthInches: width.inches,
+    heightFeet: height.feet,
+    heightInches: height.inches,
+    hasTrailer: hasTrailer ? 'true' : 'false',
+    isRoadworthy: isRoadworthy === null ? '' : isRoadworthy?.toString(),
+    ...initialValues,
+  };
+
+  return displayValues;
+}
+
 export default {
   formatMtoShipmentForAPI,
   formatMtoShipmentForDisplay,
@@ -527,4 +597,7 @@ export default {
   getMtoShipmentLabel,
   toTotalInches,
   convertInchesToFeetAndInches,
+  formatMobileHomeShipmentForDisplay,
+  formatMobileHomeShipmentForAPI,
+  formatBoatShipmentForDisplay,
 };
