3617a080314bb2e31bca02ef2583f1e83c0a78e8
KonstanceH <Konstance.Haffaney@caci.com>
2024-10-07 18:52:07 +0000

    accidentally removed mobile home option

--- src/pages/Office/MoveDetails/MoveDetails.jsx
+++ src/pages/Office/MoveDetails/MoveDetails.jsx
@@ -433,6 +433,7 @@ const MoveDetails = ({
                   <option value={SHIPMENT_OPTIONS_URL.NTS}>NTS</option>
                   <option value={SHIPMENT_OPTIONS_URL.NTSrelease}>NTS-release</option>
                   <option value={SHIPMENT_OPTIONS_URL.BOAT}>Boat</option>
+                  <option value={SHIPMENT_OPTIONS_URL.MOBILE_HOME}>Mobile Home</option>
                 </ButtonDropdown>
               </div>
             </Restricted>
