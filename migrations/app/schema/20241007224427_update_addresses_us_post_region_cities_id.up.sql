b3230788c5c92a1bd01cbb2e4bbc943a4873025c
Ricky Mettler <ricky.mettler@caci.com>
2024-10-08 17:41:53 +0000

    comment out update post region id for now

--- migrations/app/schema/20241007224427_update_addresses_us_post_region_cities_id.up.sql
+++ migrations/app/schema/20241007224427_update_addresses_us_post_region_cities_id.up.sql
@@ -1,7 +1,15 @@
-ALTER TABLE addresses add us_post_region_cities_id uuid;
+ALTER TABLE addresses ADD IF NOT EXISTS us_post_region_cities_id uuid;
 
-UPDATE addresses
-SET us_post_region_cities_id=uprc.id 
-FROM us_post_region_cities uprc
-WHERE upper(city)=upper(uprc.u_s_post_region_city_nm)
-  AND postal_code=uprc.uspr_zip_id;
+-- UPDATE addresses
+-- SET us_post_region_cities_id=uprc.id
+-- FROM us_post_region_cities uprc
+-- WHERE upper(city)=upper(uprc.u_s_post_region_city_nm)
+--   AND postal_code=uprc.uspr_zip_id;
+
+-- UPDATE addresses
+-- SET us_post_region_cities_id=uprc.id
+-- FROM us_post_region_cities uprc,
+--      re_us_post_regions upr,
+-- 	 re_cities c
+-- WHERE uprc.cities_id = c.id
+--   AND uprc.us_post_regions_id = upr.id;
