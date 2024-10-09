bd1c72c0db127569b23cbe1a2de88339302d212b
Beth Grohmann <beth.grohmann@caci.com>
2024-10-09 11:38:45 -0500

    B-21442 - add create re_oconus_rate_areas, re_intl_transit_times, update re_cities insert

---
+++ migrations/app/schema/20241007184200_create_re_oconus_rate_areas_re_intl_transit_times.up.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS re_oconus_rate_areas
+(id 			uuid		NOT NULL,
+rate_area_id	uuid		NOT NULL,
+country_id	    uuid	    NOT NULL
+	CONSTRAINT re_oconus_rate_areas_fkey01 REFERENCES re_countries (id),
+us_post_region_cities_id	uuid	NOT NULL
+	CONSTRAINT re_oconus_rate_areas_fkey02 REFERENCES us_post_region_cities (id),
+created_at		timestamp	NOT NULL DEFAULT NOW(),
+updated_at		timestamp	NOT NULL DEFAULT NOW(),
+inactive_flag	varchar(1)  DEFAULT 'N',
+CONSTRAINT re_oconus_rate_areas_pkey PRIMARY KEY (id),
+CONSTRAINT unique_re_oconus_rate_areas UNIQUE (rate_area_id, country_id, us_post_region_cities_id));
+
+COMMENT ON TABLE re_oconus_rate_areas IS 'Associates a country with a rate area.';
+COMMENT ON COLUMN re_oconus_rate_areas.rate_area_id IS 'The associated rate area id for this country.';
+COMMENT ON COLUMN re_oconus_rate_areas.country_id IS 'The associated country id for this country.';
+COMMENT ON COLUMN re_oconus_rate_areas.us_post_region_cities_id IS 'The associated id for this zip5, state and city association. Used to associate AK and HI rate areas.';
+COMMENT ON COLUMN re_oconus_rate_areas.inactive_flag IS 'Set to Y if record is inactive';
+
+CREATE TABLE IF NOT EXISTS re_intl_transit_times
+(id 						uuid		NOT NULL,
+origin_rate_area_id			uuid		NOT NULL
+	CONSTRAINT re_intl_transit_times_fkey01 REFERENCES re_rate_areas (id),
+destination_rate_area_id	uuid		NOT NULL
+	CONSTRAINT re_intl_transit_times_fkey02 REFERENCES re_rate_areas (id),
+hhg_transit_time			int,
+ub_transit_time				int,
+created_at		timestamp	NOT NULL DEFAULT NOW(),
+updated_at		timestamp	NOT NULL DEFAULT NOW(),
+inactive_flag	varchar(1),
+CONSTRAINT re_intl_transit_times_pkey PRIMARY KEY (id),
+CONSTRAINT unique_re_intl_transit_times UNIQUE (origin_rate_area_id, destination_rate_area_id));
+
+COMMENT ON TABLE re_intl_transit_times IS 'Stores transit time between 2 rate areas.';
+COMMENT ON COLUMN re_intl_transit_times.origin_rate_area_id IS 'The rate area id for the origin.';
+COMMENT ON COLUMN re_intl_transit_times.destination_rate_area_id IS 'The rate area id for the destination.';
+COMMENT ON COLUMN re_intl_transit_times.hhg_transit_time IS 'The HHG transit time between the origin and destination rate area.';
+COMMENT ON COLUMN re_intl_transit_times.ub_transit_time IS 'The UB transit time between the origin and destination rate area.';
+COMMENT ON COLUMN re_intl_transit_times.inactive_flag IS 'Set to Y if record is inactive';
+
