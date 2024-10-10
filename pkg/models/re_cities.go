9da2ebe00c01a8e0f5324691d1093bad413b6d5c
Ricky Mettler <ricky.mettler@caci.com>
2024-10-10 14:44:10 +0000

    linter fixes

--- pkg/models/re_cities.go
+++ pkg/models/re_cities.go
@@ -7,13 +7,13 @@ import (
 )
 
 type ReCities struct {
-	ID         uuid.UUID `json:"id" db:"id"`
+	ID        uuid.UUID `json:"id" db:"id"`
 	CityName  string    `json:"city_name" db:"city_name"`
-	StateID    uuid.UUID `json:"state_id" db:"state_id"`
-	CountryID  uuid.UUID `json:"country_id" db:"country_id"`
-	IsConus    bool      `json:"is_conus" db:"is_conus"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
+	StateID   uuid.UUID `json:"state_id" db:"state_id"`
+	CountryID uuid.UUID `json:"country_id" db:"country_id"`
+	IsConus   bool      `json:"is_conus" db:"is_conus"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
 // TableName overrides the table name used by Pop.
--- pkg/models/re_us_post_regions.go
+++ pkg/models/re_us_post_regions.go
@@ -7,12 +7,12 @@ import (
 )
 
 type ReUsPostRegions struct {
-	ID         uuid.UUID `json:"id" db:"id"`
-	UsprZipID  string    `json:"uspr_zip_id" db:"uspr_zip_id"`
-	StateId    uuid.UUID    `json:"state_id" db:"state_id"`
-	Zip3       string `json:"zip3" db:"zip3"`
-	CreatedAt  time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	UsprZipID string    `json:"uspr_zip_id" db:"uspr_zip_id"`
+	StateId   uuid.UUID `json:"state_id" db:"state_id"`
+	Zip3      string    `json:"zip3" db:"zip3"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
 // TableName overrides the table name used by Pop.
