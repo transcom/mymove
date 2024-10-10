f4359e56bad5e002e6720a987188368155369f36
Daniel Jordan <daniel.jordan@caci.com>
2024-10-10 20:40:03 +0000

    updating migration names, Go models, and insert names

--- migrations/app/schema/20241007184200_create_re_oconus_rate_areas_re_intl_transit_times.up.sql
+++ migrations/app/schema/20241007184200_create_re_oconus_rate_areas_re_intl_transit_times.up.sql
@@ -15,7 +15,7 @@ COMMENT ON TABLE re_oconus_rate_areas IS 'Associates a country with a rate area.
 COMMENT ON COLUMN re_oconus_rate_areas.rate_area_id IS 'The associated rate area id for this country.';
 COMMENT ON COLUMN re_oconus_rate_areas.country_id IS 'The associated country id for this country.';
 COMMENT ON COLUMN re_oconus_rate_areas.us_post_region_cities_id IS 'The associated id for this zip5, state and city association. Used to associate AK and HI rate areas.';
-COMMENT ON COLUMN re_oconus_rate_areas.inactive_flag IS 'Set to Y if record is inactive';
+COMMENT ON COLUMN re_oconus_rate_areas.active IS 'Set to true if the record is active.';
 
 CREATE TABLE IF NOT EXISTS re_intl_transit_times
 (id 						uuid		NOT NULL,
@@ -36,5 +36,5 @@ COMMENT ON COLUMN re_intl_transit_times.origin_rate_area_id IS 'The rate area id
 COMMENT ON COLUMN re_intl_transit_times.destination_rate_area_id IS 'The rate area id for the destination.';
 COMMENT ON COLUMN re_intl_transit_times.hhg_transit_time IS 'The HHG transit time between the origin and destination rate area.';
 COMMENT ON COLUMN re_intl_transit_times.ub_transit_time IS 'The UB transit time between the origin and destination rate area.';
-COMMENT ON COLUMN re_intl_transit_times.inactive_flag IS 'Set to Y if record is inactive';
+COMMENT ON COLUMN re_intl_transit_times.active IS 'Set to true if the record is active.';
 
