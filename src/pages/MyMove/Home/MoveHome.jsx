8adc26ffe10ab18260f39f0b195753293fbd071a
loganwc <logan.cunningham@caci.com>
2024-10-14 15:14:53 +0000

    removed accidental paste

--- src/pages/MyMove/Home/MoveHome.jsx
+++ src/pages/MyMove/Home/MoveHome.jsx
@@ -550,7 +550,7 @@ const MoveHome = ({ serviceMemberMoves, isProfileComplete, serviceMember, signed
                 {!hasSubmittedMove() && (
                   <Step
                     complete={hasOrdersAndUpload()}
-                    completedHeaderText="window.location.reloadOrders uploaded"
+                    completedHeaderText="Orders uploaded"
                     editBtnLabel={hasOrdersAndUpload() ? 'Edit' : ''}
                     onEditBtnClick={() => handleNewPathClick(ordersEditPath)}
                     headerText="Upload orders"
