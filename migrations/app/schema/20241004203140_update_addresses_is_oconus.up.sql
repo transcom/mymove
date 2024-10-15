b52f91c295de66cf18bff599b8443b6ca63633c5
Tevin Adams <tevin.adams@caci.com>
2024-10-15 14:35:57 +0000

    comment update

--- migrations/app/schema/20241004203140_update_addresses_is_oconus.up.sql
+++ migrations/app/schema/20241004203140_update_addresses_is_oconus.up.sql
@@ -1,4 +1,4 @@
--- Update is_oconus value on the addresses table based on the addresses country
+-- Update is_oconus value on the addresses table based on the addresses country and the state
 UPDATE addresses
 SET is_oconus = CASE
                     WHEN country_id is null OR (Select country from re_countries where id = country_id ) = 'US' AND state NOT IN ('AK', 'HI') THEN false
