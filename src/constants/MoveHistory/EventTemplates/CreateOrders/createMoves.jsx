93c252086f2745e08327b7144182f244c1dfccfe
pambecker <pamela.becker@caci.com>
2024-10-15 15:17:54 +0000

    udpate H&AL for counseling office

--- src/constants/MoveHistory/EventTemplates/CreateOrders/createMoves.jsx
+++ src/constants/MoveHistory/EventTemplates/CreateOrders/createMoves.jsx
@@ -4,12 +4,27 @@ import t from 'constants/MoveHistory/Database/Tables';
 import a from 'constants/MoveHistory/Database/Actions';
 import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
 
+const formatChangedValues = (historyRecord) => {
+  let newChangedValues = {
+    ...historyRecord.changedValues,
+  };
+
+  if (historyRecord.context) {
+    newChangedValues = {
+      ...newChangedValues,
+      ...historyRecord.context[0],
+    };
+  }
+
+  return { ...historyRecord, changedValues: newChangedValues };
+};
+
 export default {
   action: a.INSERT,
   eventName: '*', // Needs wild card to handle both createOrders and createOrder
   tableName: t.moves,
   getEventNameDisplay: () => 'Created move',
   getDetails: (historyRecord) => {
-    return <LabeledDetails historyRecord={historyRecord} />;
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
   },
 };
--- src/constants/MoveHistory/EventTemplates/UpdateOrders/updateOrderUpdateMove.jsx
+++ src/constants/MoveHistory/EventTemplates/UpdateOrders/updateOrderUpdateMove.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
-import o from 'constants/MoveHistory/UIDisplay/Operations';
 import a from 'constants/MoveHistory/Database/Actions';
 import t from 'constants/MoveHistory/Database/Tables';
 import LabeledDetails from 'pages/Office/MoveHistory/LabeledDetails';
 
+const formatChangedValues = (historyRecord) => {
+  let newChangedValues = {
+    ...historyRecord.changedValues,
+  };
+
+  if (newChangedValues.counseling_transportation_office_id === null) {
+    newChangedValues.counseling_office_name = ' - ';
+  }
+  if (historyRecord.context) {
+    newChangedValues = {
+      ...newChangedValues,
+      ...historyRecord.context[0],
+    };
+  }
+
+  return { ...historyRecord, changedValues: newChangedValues };
+};
+
 export default {
   action: a.UPDATE,
-  eventName: o.updateOrder,
+  eventName: '*', // Needs wild card to handle both createOrders and createOrder
   tableName: t.moves,
   getEventNameDisplay: () => 'Updated move',
-  getDetails: (historyRecord) => <LabeledDetails historyRecord={historyRecord} />,
+  getDetails: (historyRecord) => {
+    return <LabeledDetails historyRecord={formatChangedValues(historyRecord)} />;
+  },
 };
