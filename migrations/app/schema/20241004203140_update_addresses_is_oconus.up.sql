077890e71d83de0db5377ac8150bd2d2aa324e9a
Tevin Adams <tevin.adams@caci.com>
2024-10-15 17:05:30 +0000

    increasing statement timeout

--- migrations/app/schema/20241004203140_update_addresses_is_oconus.up.sql
+++ migrations/app/schema/20241004203140_update_addresses_is_oconus.up.sql
@@ -1,3 +1,9 @@
+-- Set temp timeout due to potentially large modification
+-- Time is 5 minutes in milliseconds
+SET statement_timeout = 300000;
+SET lock_timeout = 300000;
+SET idle_in_transaction_session_timeout = 300000;
+
 -- Update is_oconus value on the addresses table based on the addresses country and the state
 UPDATE addresses
 SET is_oconus = CASE
