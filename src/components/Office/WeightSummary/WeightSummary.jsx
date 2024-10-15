aa4155c084a4254774b5db8e2cc4bfe21f86dffc
antgmann <anthony.mann@caci.com>
2024-10-15 15:00:52 +0000

    Experimental changes

--- src/components/Office/WeightSummary/WeightSummary.jsx
+++ src/components/Office/WeightSummary/WeightSummary.jsx
@@ -26,6 +26,7 @@ const WeightSummary = ({ maxBillableWeight, weightRequested, weightAllowance, to
     if (shipment.shipmentType === SHIPMENT_OPTIONS.HHG && countHHG <= 1) return 'HHG';
     if (shipment.shipmentType === SHIPMENT_OPTIONS.NTS) return 'NTS';
     if (shipment.shipmentType === SHIPMENT_OPTIONS.NTSR) return 'NTSR';
+    if (shipment.shipmentType === SHIPMENT_OPTIONS.PPM) return 'PPM';
     return '';
   };
 
