49c7edf43373318f465b3d5aa13d14fb0e6ed560
Maria Traskowsky <maria.traskowsky@caci.com>
2024-10-16 21:47:38 +0000

    add MoveInfoModal.test.jsx

--- src/components/Customer/modals/MoveInfoModal/MoveInfoModal.jsx
+++ src/components/Customer/modals/MoveInfoModal/MoveInfoModal.jsx
@@ -5,24 +5,24 @@ import { Button } from '@trussworks/react-uswds';
 import Modal, { ModalTitle, ModalClose, ModalActions, connectModal } from 'components/Modal/Modal';
 
 export const MoveInfoModal = ({ closeModal, enablePPM, enableUB, hasOconusDutyLocation }) => (
-  <Modal>
+  <Modal data-testid="moveInfoModal">
     <ModalClose handleClick={closeModal} />
     <ModalTitle>
-      <h3>More info about shipments</h3>
+      <h3 data-testid="moveInfoModalHeading">More info about shipments</h3>
     </ModalTitle>
 
     <h4>
       <strong>HHG: Professional movers pack and ship your things, the government pays</strong>
     </h4>
-    <p>The moving company works out details with you, but handles everything.</p>
+    <p data-testid="hhgSubHeading">The moving company works out details with you, but handles everything.</p>
     <h5>Pros</h5>
-    <ul>
+    <ul data-testid="hhgProsList">
       <li>Everything is packed and moved for you</li>
       <li>Expert movers care for your things</li>
       <li>Anything damaged in professional shipments will be replaced</li>
     </ul>
     <h5>Cons</h5>
-    <ul>
+    <ul data-testid="hhgConsList">
       <li>Can only move on weekdays</li>
       <li>May have to work around availability of movers</li>
     </ul>
@@ -31,16 +31,16 @@ export const MoveInfoModal = ({ closeModal, enablePPM, enableUB, hasOconusDutyLo
         <h4>
           <strong>PPM: You get your things packed and moved, the government pays you</strong>
         </h4>
-        <p>You pack and move your own things, or arrange for someone else do it for you.</p>
+        <p data-testid="ppmSubHeading">You pack and move your own things, or arrange for someone else do it for you.</p>
         <h5>Pros</h5>
-        <ul>
+        <ul data-testid="ppmProsList">
           <li>Keep your things with you at all times</li>
           <li>Get paid for the weight you move</li>
           <li>Flexible dates, routes, timing</li>
           <li>You can hire movers, equipment, or portable storage</li>
         </ul>
         <h5>Cons</h5>
-        <ul>
+        <ul data-testid="ppmConsList">
           <li>You pack and move everything</li>
           <li>You’re responsible if your things get damaged — no compensation</li>
           <li>The more you own, the more you have to do</li>
@@ -54,9 +54,9 @@ export const MoveInfoModal = ({ closeModal, enablePPM, enableUB, hasOconusDutyLo
             UB: Professional movers pack and ship your more essential personal property, the government pays
           </strong>
         </h4>
-        <p>The moving company works out details with you, but handles everything.</p>
+        <p data-testid="ubSubHeading">The moving company works out details with you, but handles everything.</p>
         <h5>Pros</h5>
-        <ul>
+        <ul data-testid="ubProsList">
           <li>Everything is packed and moved for you</li>
           <li>Expert movers care for your things</li>
           <li>Anything damaged in professional shipments will be replaced</li>
@@ -64,7 +64,7 @@ export const MoveInfoModal = ({ closeModal, enablePPM, enableUB, hasOconusDutyLo
           <li>Shorter allowable transit time than a standard HHG shipment; should arrive sooner at your destination</li>
         </ul>
         <h5>Cons</h5>
-        <ul>
+        <ul data-testid="ubConsList">
           <li>Can only move on weekdays</li>
           <li>May have to work around availability of movers</li>
           <li>Your UB shipment has its own weight limitation</li>
@@ -83,11 +83,15 @@ export const MoveInfoModal = ({ closeModal, enablePPM, enableUB, hasOconusDutyLo
 MoveInfoModal.propTypes = {
   closeModal: PropTypes.func,
   enablePPM: PropTypes.bool,
+  enableUB: PropTypes.bool,
+  hasOconusDutyLocation: PropTypes.bool,
 };
 
 MoveInfoModal.defaultProps = {
   closeModal: () => {},
   enablePPM: true,
+  enableUB: true,
+  hasOconusDutyLocation: true,
 };
 
 MoveInfoModal.displayName = 'MoveInfoModal';
