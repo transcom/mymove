ffc72d8d1b4754691f247a527261c6e04849f647
deandreJones <deandre.Jones@caci.com>
2024-10-17 14:30:03 -0500

    add a test

---
+++ pkg/models/payment_request_edi_file_test.go
@@ -0,0 +1,27 @@
+package models_test
+
+import (
+	"github.com/gofrs/uuid"
+
+	"github.com/transcom/mymove/pkg/models"
+)
+
+func (suite *ModelSuite) TestCreatePaymentRequestEdiFile() {
+	suite.Run("test create PaymentRequest Edi file db save", func() {
+		fileRecord := models.PaymentRequestEdiFile{
+			ID:                   uuid.Must(uuid.NewV4()),
+			Filename:             "858.file.txt",
+			EdiString:            "Sample EDI content",
+			PaymentRequestNumber: "1111-2222-1",
+		}
+		suite.NoError(models.CreatePaymentRequestEdiFile(suite.DB(), fileRecord.Filename, fileRecord.EdiString, fileRecord.PaymentRequestNumber))
+	})
+	suite.Run("test create PaymentRequest Edi file db save", func() {
+		fileRecord := models.PaymentRequestEdiFile{
+			ID:                   uuid.Must(uuid.NewV4()),
+			EdiString:            "Sample EDI content",
+			PaymentRequestNumber: "1111-2222-1",
+		}
+		suite.Error(models.CreatePaymentRequestEdiFile(suite.DB(), fileRecord.Filename, fileRecord.EdiString, fileRecord.PaymentRequestNumber))
+	})
+}
