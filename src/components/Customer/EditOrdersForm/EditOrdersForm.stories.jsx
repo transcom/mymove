224224c15c0051c40cd83cc30b6b58fc1ebc05a8
Jon Spight <Jonathan.Spight@caci.com>
2024-10-17 19:26:42 +0000

    happo fixes

--- src/components/Customer/EditOrdersForm/EditOrdersForm.stories.jsx
+++ src/components/Customer/EditOrdersForm/EditOrdersForm.stories.jsx
@@ -75,24 +75,7 @@ const testProps = {
     report_by_date: '',
     has_dependents: '',
     new_duty_location: {},
-    origin_duty_location: {
-      address: {
-        city: 'Des Moines',
-        country: 'US',
-        id: 'a4b30b99-4e82-48a6-b736-01662b499d6a',
-        postalCode: '50309',
-        state: 'IA',
-        streetAddress1: '987 Other Avenue',
-        streetAddress2: 'P.O. Box 1234',
-        streetAddress3: 'c/o Another Person',
-      },
-      address_id: 'a4b30b99-4e82-48a6-b736-01662b499d6a',
-      affiliation: 'AIR_FORCE',
-      created_at: '2020-10-19T17:01:16.114Z',
-      id: 'f9299768-16d2-4a13-ae39-7087a58b1f62',
-      name: 'Yuma AFB',
-      updated_at: '2020-10-19T17:01:16.114Z',
-    },
+    origin_duty_location: {},
     grade: '',
     uploaded_orders: [
       {
