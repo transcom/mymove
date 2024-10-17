753828878de59f02fd6da9b3cd25de2b51bb45ed
JamesHawks224 <146897935+JamesHawks224@users.noreply.github.com>
2024-10-17 14:28:21 +0000

    added db changes

---
+++ migrations/app/schema/20241011201326_changes_for_actual_expense_reimbursement_field.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE ppm_shipments
+ADD COLUMN actual_expense_reimbursement BOOLEAN DEFAULT FALSE;
+COMMENT on COLUMN mto_service_items.customer_expense IS 'Whether or not the ppm is an actual expense reimbursement';
