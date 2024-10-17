1ad6b1b1aaaff4e8c97fd7e9435a01d74e522147
KonstanceH <Konstance.Haffaney@caci.com>
2024-10-17 15:15:00 +0000

    fix overwrite and changes

--- src/pages/MyMove/Home/MoveHome.jsx
+++ src/pages/MyMove/Home/MoveHome.jsx
@@ -503,7 +503,9 @@ const MoveHome = ({ serviceMemberMoves, isProfileComplete, serviceMember, signed
                   headerText="Profile complete"
                   step="1"
                   onEditBtnClick={() => handleNewPathClick(profileEditPath)}
-                  actionBtnLabel={isAdditionalDocumentsButtonAvailable() ? 'Upload Additional Documents' : null}
+                  actionBtnLabel={
+                    isAdditionalDocumentsButtonAvailable() ? 'Upload/Manage Non-Orders Documentation' : null
+                  }
                   onActionBtnClick={() => additionalDocumentsClick()}
                 >
                   <Description>Make sure to keep your personal information up to date during your move.</Description>
@@ -530,7 +532,7 @@ const MoveHome = ({ serviceMemberMoves, isProfileComplete, serviceMember, signed
                   <Step
                     complete={hasOrdersAndUpload() && hasSubmittedMove()}
                     completedHeaderText="Orders"
-                    editBtnLabel="Upload documents"
+                    editBtnLabel="Upload/Manage Orders Documentation"
                     onEditBtnClick={() => handleNewPathClick(ordersAmendPath)}
                     headerText="Orders"
                     step="2"
