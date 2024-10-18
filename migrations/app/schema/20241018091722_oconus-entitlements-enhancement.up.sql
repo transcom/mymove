07c5e4d5834a2deb5c23c945d2362ddb657691eb
cameroncaci <cameron.jewell@caci.com>
2024-10-18 14:45:21 +0000

    add oconus fields to entitlements model and table

---
+++ migrations/app/schema/20241018091722_oconus-entitlements-enhancement.up.sql
@@ -0,0 +1,11 @@
+
+ALTER TABLE entitlements
+ADD COLUMN accompanied_tour BOOLEAN NULL,
+    ADD COLUMN dependents_under_twelve INTEGER NULL,
+    ADD COLUMN dependents_twelve_and_over INTEGER NULL,
+    ADD COLUMN ub_allowance INTEGER NULL;
+
+COMMENT ON COLUMN entitlements.accompanied_tour IS 'Indicates if the move entitlement allows dependents to travel to the new Permanent Duty Station (PDS). This is only present on OCONUS moves.';
+COMMENT ON COLUMN entitlements.dependents_under_twelve IS 'Indicates the number of dependents under the age of twelve for a move. This is only present on OCONUS moves.';
+COMMENT ON COLUMN entitlements.dependents_twelve_and_over IS 'Indicates the number of dependents of the age twelve or older for a move. This is only present on OCONUS moves.';
+COMMENT ON COLUMN entitlements.ub_allowance IS 'The amount of weight in pounds that the move is entitled for shipment types of Unaccompanied Baggage.';
