31ee20f75193dcbb0aeb909d580f01444b0d8d07
deandreJones <deandre.Jones@caci.com>
2024-10-18 09:50:15 -0500

    test test test

--- pkg/models/payment_request_edi_file_test.go
+++ pkg/models/payment_request_edi_file_test.go
@@ -1,6 +1,8 @@
 package models_test
 
 import (
+	"testing"
+
 	"github.com/gofrs/uuid"
 
 	"github.com/transcom/mymove/pkg/models"
@@ -24,4 +26,37 @@ func (suite *ModelSuite) TestCreatePaymentRequestEdiFile() {
 		}
 		suite.Nil(models.CreatePaymentRequestEdiFile(suite.DB(), fileRecord.Filename, fileRecord.EdiString, fileRecord.PaymentRequestNumber))
 	})
+	suite.Run("TestFetchAllPaymentRequestEdiFiles", func() {
+		suite.T().Run("successfully fetches all PaymentRequestEdiFiles", func(t *testing.T) {
+			fileRecord := models.PaymentRequestEdiFile{
+				ID:                   uuid.Must(uuid.NewV4()),
+				Filename:             "858.file.txt",
+				EdiString:            "Sample EDI content",
+				PaymentRequestNumber: "1111-2222-1",
+			}
+			err := models.CreatePaymentRequestEdiFile(suite.DB(), fileRecord.Filename, fileRecord.EdiString, fileRecord.PaymentRequestNumber)
+			suite.NoError(err)
+
+			files, err := models.FetchAllPaymentRequestEdiFiles(suite.DB())
+			suite.NoError(err)
+			suite.NotEmpty(files)
+		})
+	})
+	suite.Run("TestFetchPaymentRequestEdiByPaymentRequestNumber", func() {
+		fileRecord := models.PaymentRequestEdiFile{
+			ID:                   uuid.Must(uuid.NewV4()),
+			Filename:             "858.file.txt",
+			EdiString:            "Sample EDI content",
+			PaymentRequestNumber: "1111-2222-1",
+		}
+		err := models.CreatePaymentRequestEdiFile(suite.DB(), fileRecord.Filename, fileRecord.EdiString, fileRecord.PaymentRequestNumber)
+		suite.NoError(err)
+
+		got, err := models.FetchPaymentRequestEdiByPaymentRequestNumber(suite.DB(), fileRecord.PaymentRequestNumber)
+		suite.NoError(err)
+		suite.Equal(fileRecord.Filename, got.Filename)
+		suite.Equal(fileRecord.EdiString, got.EdiString)
+		suite.Equal(fileRecord.PaymentRequestNumber, got.PaymentRequestNumber)
+	})
+
 }
