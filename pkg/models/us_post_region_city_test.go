f8c34037da1f7a3f57c9095a594666c609689cdd
Ricky Mettler <ricky.mettler@caci.com>
2024-10-18 22:18:35 +0000

    updating us post region city test

---
+++ pkg/models/us_post_region_city_test.go
@@ -0,0 +1,19 @@
+package models_test
+
+import (
+	"github.com/transcom/mymove/pkg/models"
+)
+
+// Our us_post_region_cities table has static data that won't be truncated
+// This test verifies we can get data from that table and find the county
+func (suite *ModelSuite) TestFindCountyByZipCode() {
+
+	// Attempt to gather 90210's County from the 90210 zip code
+	county, err := models.FindCountyByZipCode(suite.DB(), "90210")
+	suite.NoError(err)
+	suite.Equal("LOS ANGELES", county)
+
+	// Attempt to gather a non-existant county
+	_, err = models.FindCountyByZipCode(suite.DB(), "99999")
+	suite.Error(err)
+}
