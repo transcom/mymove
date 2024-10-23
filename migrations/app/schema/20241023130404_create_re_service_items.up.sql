b633e07fbcfb31046a76e70508f8936f1801faac
Beth Grohmann <beth.grohmann@caci.com>
2024-10-23 08:22:00 -0500

    Merge branch 'B-21442' of https://github.com/transcom/mymove into B-21442

---
+++ migrations/app/schema/20241023130404_create_re_service_items.up.sql
@@ -0,0 +1,64 @@
+CREATE TABLE IF NOT EXISTS re_service_items
+(id 			uuid		NOT NULL,
+service_id      uuid        NOT NULL
+    CONSTRAINT fk_re_service_items_service_id REFERENCES re_services (id),
+shipment_type   mto_shipment_type	NOT NULL,
+market_code     market_code_enum  	NOT NULL,
+is_optional     bool 		NOT NULL,
+created_at		timestamp	NOT NULL DEFAULT NOW(),
+updated_at		timestamp	NOT NULL DEFAULT NOW(),
+CONSTRAINT re_service_items_pkey PRIMARY KEY (id),
+CONSTRAINT unique_re_service_items UNIQUE (service_id, shipment_type, market_code)
+);
+
+COMMENT ON TABLE re_service_items IS 'Associates service items to market_code and shipment_type.';
+COMMENT ON COLUMN re_service_items.service_id IS 'The associated id for the service item';
+COMMENT ON COLUMN re_service_items.shipment_type IS 'The type of shipment from mto_shipments table.';
+COMMENT ON COLUMN re_service_items.market_code IS 'Market code indicator. i for international and d for domestic.';
+COMMENT ON COLUMN re_service_items.is_optional IS 'Set to true if the service item is optional for the shipment type and market.';
+
+INSERT INTO public.re_services
+(id, code, name, created_at, updated_at, priority)
+VALUES('fafad6c2-6037-4a95-af2f-d9861ba7db8e', 'UBP', 'International UB', now(), now(), 99);
+
+INSERT INTO public.re_services
+(id, code, name, created_at, updated_at, priority)
+VALUES('9f3d551a-0725-430e-897e-80ee9add3ae9', 'ISLH', 'International Shipping & Linehaul', now(), now(), 99);
+
+INSERT INTO re_service_items (id,service_id,shipment_type,market_code,is_optional,created_at,updated_at) VALUES
+	 ('2933a8a0-5d89-4fcb-99f0-60893edf8c8a'::uuid,'4780b30c-e846-437a-b39a-c499a6b09872'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('df4f7cbe-10d7-4042-a380-657835d408e1'::uuid,'fafad6c2-6037-4a95-af2f-d9861ba7db8e'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('78bde6ed-9fac-442b-aa74-dfba1a537336'::uuid,'ae84d292-f885-4138-86e2-b451855ffbf2'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('c4089abd-3225-4986-a5d9-37b46ae5b645'::uuid,'f2739142-97d1-40f3-a8f4-6a9daf390806'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('567f3d6d-c1a4-46fe-9424-4b60f1dba939'::uuid,'9f3d551a-0725-430e-897e-80ee9add3ae9'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('2008074d-e447-4619-b2bd-fe2a38a2759e'::uuid,'67ba1eaf-6ffd-49de-9a69-497be7789877'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('daf7b541-f9d1-4b1a-998b-d530ca48da3e'::uuid,'56e91c2d-015d-4243-9657-3ed34867abaa'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",false,now(),now()),
+	 ('07e8dca2-163c-459d-aa88-a6799b9158ab'::uuid,'387b9654-5685-4ac9-b213-81962be9c145'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('1b4826ca-1354-407f-8660-3ef7dff6cfff'::uuid,'20998cfd-bfc7-410b-a3c5-d709ead4f94e'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('32be29e1-5269-4fd0-85b4-03270cbe6a65'::uuid,'86203d72-7f7c-49ff-82f0-5b95e4958f60'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now());
+INSERT INTO re_service_items (id,service_id,shipment_type,market_code,is_optional,created_at,updated_at) VALUES
+	 ('7e54ce24-bd47-490b-9618-04943df57bca'::uuid,'021791b8-26ca-4494-a3d1-6945e4dde387'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('0ee7f930-6421-44b0-8820-48bffc2b2605'::uuid,'806c6d59-57ff-4a3f-9518-ebf29ba9cb10'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('e555e8a5-23bf-40ea-9ee8-01479c971716'::uuid,'28389ee1-56cf-400c-aa52-1501ecdd7c69'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('2705b054-3a05-492d-9262-3a6d4125c268'::uuid,'bd6064ca-e780-4ab4-a37b-0ae98eebb244'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('cbed2e49-799e-42c0-bd86-4204c3ca049d'::uuid,'22fc07ed-be15-4f50-b941-cbd38153b378'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('60be16ad-8d92-4e51-b565-7ef7904f4fe9'::uuid,'bd424e45-397b-4766-9712-de4ae3a2da36'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('c1000d89-ae2b-47bc-b3b1-63c2c24fc076'::uuid,'b488bf85-ea5e-49c8-ba5c-e2fa278ac806'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('b09f964d-fee7-4bf9-84ea-c06571b7ef51'::uuid,'6f4f6e31-0675-4051-b659-89832259f390'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('ab8981ca-b3f0-4421-8753-95860ca40cfb'::uuid,'624a97c5-dfbf-4da9-a6e9-526b4f95af8d'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('4d09972f-5fb3-44a2-bd0e-4b21f716e222'::uuid,'4132416b-b1aa-42e7-98f2-0ac0a03e8a31'::uuid,'HHG'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now());
+INSERT INTO re_service_items (id,service_id,shipment_type,market_code,is_optional,created_at,updated_at) VALUES
+	 ('b6542135-21b9-460a-b1c8-c3338fc02d3f'::uuid,'387b9654-5685-4ac9-b213-81962be9c145'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('2d706682-fdc1-4996-8d2a-1a1e7420c438'::uuid,'20998cfd-bfc7-410b-a3c5-d709ead4f94e'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('28487c9e-a91b-4870-b2e3-fbefb6a324e6'::uuid,'86203d72-7f7c-49ff-82f0-5b95e4958f60'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('c414420c-e4ef-4b86-a9fd-062f85fd50d2'::uuid,'021791b8-26ca-4494-a3d1-6945e4dde387'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('6bfeb792-7f08-4b33-904d-f837dc69c097'::uuid,'806c6d59-57ff-4a3f-9518-ebf29ba9cb10'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('f896c537-14e8-4612-833c-794d0ada17b8'::uuid,'28389ee1-56cf-400c-aa52-1501ecdd7c69'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('3d5cc86e-bd3a-49e0-a041-63e0625e690f'::uuid,'bd6064ca-e780-4ab4-a37b-0ae98eebb244'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('6d4cc684-55ba-4308-864f-76846ec66cf1'::uuid,'22fc07ed-be15-4f50-b941-cbd38153b378'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('3c3e99f3-bf63-48b9-9101-a9f8b91afea3'::uuid,'bd424e45-397b-4766-9712-de4ae3a2da36'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('047fcb9f-4b90-454b-aca4-3561b46e4de9'::uuid,'b488bf85-ea5e-49c8-ba5c-e2fa278ac806'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now());
+INSERT INTO re_service_items (id,service_id,shipment_type,market_code,is_optional,created_at,updated_at) VALUES
+	 ('d6c8ce58-bc00-499f-9cad-9047210b8746'::uuid,'6f4f6e31-0675-4051-b659-89832259f390'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('6799f4b5-c4ba-48e4-a4bd-c6adb71b7041'::uuid,'624a97c5-dfbf-4da9-a6e9-526b4f95af8d'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now()),
+	 ('faa6dc9d-e120-4d26-a54b-b84a2b2f6d7c'::uuid,'4132416b-b1aa-42e7-98f2-0ac0a03e8a31'::uuid,'UNACCOMPANIED_BAGGAGE'::public."mto_shipment_type",'i'::public."market_code_enum",true,now(),now());
---
+++ pkg/models/re_service_items.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type ReServiceItems struct {
+	ID           uuid.UUID       `db:"id" rw:"r"`
+	ServiceId    uuid.UUID       `db:"service_id" rw:"r"`
+	Code         ReServiceCode   `belongs_to:"re_services" fk_id:"service_id"`
+	ShipmentType MTOShipmentType `db:"shipment_type"`
+	MarketCode   MarketCode      `db:"market_code"`
+	IsOptional   bool            `db:"is_optional" rw:"r"`
+	CreatedAt    time.Time       `db:"created_at" rw:"r"`
+	UpdatedAt    time.Time       `db:"updated_at" rw:"r"`
+}
+
+func (g ReServiceItems) TableName() string {
+	return "re_service_items"
+}
