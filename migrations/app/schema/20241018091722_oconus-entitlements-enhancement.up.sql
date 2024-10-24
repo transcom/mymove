9922197f776a74b046c4031b777f8567beeb124d
cameroncaci <cameron.jewell@caci.com>
2024-10-24 19:16:00 +0000

    add if not exists

--- migrations/app/schema/20241018091722_oconus-entitlements-enhancement.up.sql
+++ migrations/app/schema/20241018091722_oconus-entitlements-enhancement.up.sql
@@ -1,9 +1,9 @@
 
 ALTER TABLE entitlements
 ADD COLUMN accompanied_tour BOOLEAN NULL,
-    ADD COLUMN dependents_under_twelve INTEGER NULL,
-    ADD COLUMN dependents_twelve_and_over INTEGER NULL,
-    ADD COLUMN ub_allowance INTEGER NULL;
+    ADD COLUMN IF NOT EXISTS dependents_under_twelve INTEGER NULL,
+    ADD COLUMN IF NOT EXISTS dependents_twelve_and_over INTEGER NULL,
+    ADD COLUMN IF NOT EXISTS ub_allowance INTEGER NULL;
 
 COMMENT ON COLUMN entitlements.accompanied_tour IS 'Indicates if the move entitlement allows dependents to travel to the new Permanent Duty Station (PDS). This is only present on OCONUS moves.';
 COMMENT ON COLUMN entitlements.dependents_under_twelve IS 'Indicates the number of dependents under the age of twelve for a move. This is only present on OCONUS moves.';
