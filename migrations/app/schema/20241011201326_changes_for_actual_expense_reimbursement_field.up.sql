4fb843ccfa652713ac4cf920bec113cd13be3336
JamesHawks224 <146897935+JamesHawks224@users.noreply.github.com>
2024-10-24 18:56:20 +0000

    added "if not exists" to add col statement.

--- migrations/app/schema/20241011201326_changes_for_actual_expense_reimbursement_field.up.sql
+++ migrations/app/schema/20241011201326_changes_for_actual_expense_reimbursement_field.up.sql
@@ -1,4 +1,4 @@
 -- adding boolean for actual expense reimbursement for ppm shipments
 ALTER TABLE ppm_shipments
-ADD COLUMN is_actual_expense_reimbursement BOOLEAN DEFAULT FALSE;
+ADD COLUMN IF NOT EXISTS is_actual_expense_reimbursement BOOLEAN DEFAULT FALSE;
 COMMENT on COLUMN ppm_shipments.is_actual_expense_reimbursement IS 'Whether or not the ppm is an actual expense reimbursement';
