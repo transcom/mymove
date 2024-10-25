e1fd194e68d02017f214dd86ba750429d89f113c
Daniel Jordan <daniel.jordan@caci.com>
2024-10-25 15:08:15 +0000

    removing duplicate test from merge conflict resolution

--- pkg/services/address/address_creator_test.go
+++ pkg/services/address/address_creator_test.go
@@ -56,27 +56,6 @@ func (suite *AddressSuite) TestAddressCreator() {
 		suite.NotNil(address.Country)
 	})
 
-	suite.Run("Successfully creates an OCONUS address with AK state", func() {
-		addressCreator := NewAddressCreator()
-		address, err := addressCreator.CreateAddress(suite.AppContextForTest(), &models.Address{
-			StreetAddress1: streetAddress1,
-			City:           city,
-			State:          oConusState,
-			PostalCode:     postalCode,
-		})
-
-		suite.Nil(err)
-		suite.NotNil(address)
-		suite.NotNil(address.ID)
-		suite.Equal(streetAddress1, address.StreetAddress1)
-		suite.Equal(city, address.City)
-		suite.Equal(oConusState, address.State)
-		suite.Equal(postalCode, address.PostalCode)
-		suite.True(*address.IsOconus)
-		suite.Nil(address.StreetAddress2)
-		suite.NotNil(address.Country)
-	})
-
 	suite.Run("Receives an error when trying to create an international address", func() {
 		addressCreator := NewAddressCreator()
 		address, err := addressCreator.CreateAddress(suite.AppContextForTest(), &models.Address{
