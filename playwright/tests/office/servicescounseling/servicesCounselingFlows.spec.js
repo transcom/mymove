b03eec5ecf9b37b06bc4cd5e6f783cfcf2e52baf
Tae Jung <tae.jung@caci.com>
2024-10-29 16:27:15 +0000

    merge conflict fix

--- playwright/tests/office/servicescounseling/servicesCounselingFlows.spec.js
+++ playwright/tests/office/servicescounseling/servicesCounselingFlows.spec.js
@@ -609,7 +609,6 @@ test.describe('Services counselor user', () => {
 
         await page.getByText('Review documents').click();
         await expect(page.getByRole('heading', { name: 'View documents' })).toBeVisible();
-        await page.getByTestId('shipmentInfo-showRequestDetailsButton').click();
 
         expect(await page.locator('[data-testid="tag"]').count()).toBe(0);
         await expect(page.locator('label').getByText('Actual Expense Reimbursement')).toBeVisible();
@@ -660,7 +659,6 @@ test.describe('Services counselor user', () => {
         await expect(page.getByRole('heading', { name: 'View documents' })).toBeVisible();
         await expect(page.getByTestId('tag')).toContainText('actual expense reimbursement');
 
-        await page.getByTestId('shipmentInfo-showRequestDetailsButton').click();
         await expect(page.locator('label').getByText('Actual Expense Reimbursement')).toBeVisible();
         expect(await page.getByTestId('isActualExpenseReimbursement').getByTestId('editTextButton').isDisabled()).toBe(
           true,
