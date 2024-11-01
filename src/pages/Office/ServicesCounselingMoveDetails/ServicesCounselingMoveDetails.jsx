e22234929211cfe24bc622bdd12bb110cd550233
Cory Kleinjan <cory.kleinjan@caci.com>
2024-11-01 15:35:15 -0500

    Inserting removed line

--- src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
+++ src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
@@ -498,6 +498,7 @@ const ServicesCounselingMoveDetails = ({
         mutateMoveStatus({ moveTaskOrderID: move.id, ifMatchETag: move.eTag });
       })
       .finally(() => {
+        mutateMoveStatus({ moveTaskOrderID: move.id, ifMatchETag: move.eTag });
         setIsSubmitModalVisible(false);
       });
   };
