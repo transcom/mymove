f0302e5025ec92914b01c99ce7721a72e0660603
Brian Manley <brian.manley@caci.com>
2024-11-05 16:02:00 +0000

    B-21566 reference service instead of code

--- pkg/models/re_service_item.go
+++ pkg/models/re_service_item.go
@@ -9,7 +9,7 @@ import (
 type ReServiceItem struct {
 	ID             uuid.UUID       `db:"id" rw:"r"`
 	ServiceId      uuid.UUID       `db:"service_id" rw:"r"`
-	Code           ReServiceCode   `belongs_to:"re_services" fk_id:"service_id" rw:"r"`
+	ReService      ReService       `belongs_to:"re_services" fk_id:"service_id" rw:"r"`
 	ShipmentType   MTOShipmentType `db:"shipment_type" rw:"r"`
 	MarketCode     MarketCode      `db:"market_code" rw:"r"`
 	IsAutoApproved bool            `db:"is_auto_approved" rw:"r"`
