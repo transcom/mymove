e26f2b5b2f80b9f1a06792210667932b3d0b5de1
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-11-07 19:23:22 +0000

    Fixed a code block that shouldn't have been deleted in testdatagen

--- pkg/testdatagen/scenario/shared.go
+++ pkg/testdatagen/scenario/shared.go
@@ -10429,6 +10429,21 @@ func CreateNeedsServicesCounseling(appCtx appcontext.AppContext, ordersType inte
 	requestedPickupDate = submittedAt.Add(30 * 24 * time.Hour)
 	requestedDeliveryDate = requestedPickupDate.Add(7 * 24 * time.Hour)
 
+	factory.BuildMTOShipment(db, []factory.Customization{
+		{
+			Model:    move,
+			LinkOnly: true,
+		},
+		{
+			Model: models.MTOShipment{
+				ShipmentType:          shipmentType,
+				Status:                models.MTOShipmentStatusSubmitted,
+				RequestedPickupDate:   &requestedPickupDate,
+				RequestedDeliveryDate: &requestedDeliveryDate,
+			},
+		},
+	}, nil)
+
 	officeUser := factory.BuildOfficeUserWithRoles(db, nil, []roles.RoleType{roles.RoleTypeTOO})
 	factory.BuildCustomerSupportRemark(db, []factory.Customization{
 		{
