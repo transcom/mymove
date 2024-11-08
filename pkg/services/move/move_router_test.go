819a7287cffa63089ccf224563a4ccc3da6523e1
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-11-08 15:22:31 +0000

    more test fixes from merge

--- pkg/services/move/move_router_test.go
+++ pkg/services/move/move_router_test.go
@@ -830,7 +830,7 @@ func (suite *MoveServiceSuite) TestMoveSubmission() {
 
 		sitDaysAllowance := -1 // Invalid value that should cause a validation error on MTOShipment
 
-		expError := "failure saving parent MTO shipment object for mobile home shipment when routing move submission"
+		expError := "failure saving parent MTO shipment object for boat/mobile home shipment when routing move submission"
 
 		move := factory.BuildMove(suite.DB(), []factory.Customization{
 			{
@@ -882,7 +882,7 @@ func (suite *MoveServiceSuite) TestMoveSubmission() {
 
 		sitDaysAllowance := -1 // Invalid value that should cause a validation error on MTOShipment
 
-		expError := "failure saving parent MTO shipment object for boat shipment when routing move submission"
+		expError := "failure saving parent MTO shipment object for boat/mobile shipment when routing move submission"
 
 		move := factory.BuildMove(suite.DB(), []factory.Customization{
 			{
