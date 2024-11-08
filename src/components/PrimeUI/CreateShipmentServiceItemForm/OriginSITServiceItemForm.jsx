57e740f2940912bdce399b877bc49da8bb0a1827
Alex Lusk <alex.lusk@caci.com>
2024-11-08 00:35:52 +0000

    Add zip city lookup to origin SIT Service Item Form

--- src/components/PrimeUI/CreateShipmentServiceItemForm/OriginSITServiceItemForm.jsx
+++ src/components/PrimeUI/CreateShipmentServiceItemForm/OriginSITServiceItemForm.jsx
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { Button } from '@trussworks/react-uswds';
 import React from 'react';
+import { useNavigate, useParams, generatePath } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { requiredAddressSchema, ZIP_CODE_REGEX } from 'utils/validation';
@@ -13,6 +14,7 @@ import MaskedTextField from 'components/form/fields/MaskedTextField/MaskedTextFi
 import { DatePickerInput } from 'components/form/fields';
 import { AddressFields } from 'components/form/AddressFields/AddressFields';
 import { ShipmentShape } from 'types/shipment';
+import { primeSimulatorRoutes } from 'constants/routes';
 
 const originSITValidationSchema = Yup.object().shape({
   reason: Yup.string().required('Required'),
@@ -37,9 +39,11 @@ const OriginSITServiceItemForm = ({ shipment, submission }) => {
     sitHHGActualOrigin: {
       streetAddress1: '',
       streetAddress2: '',
+      streetAddress3: '',
       city: '',
       state: '',
       postalCode: '',
+      county: '',
     },
   };
 
@@ -54,26 +58,62 @@ const OriginSITServiceItemForm = ({ shipment, submission }) => {
     submission({ body });
   };
 
+  const { moveCodeOrID } = useParams();
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    navigate(generatePath(primeSimulatorRoutes.VIEW_MOVE_PATH, { moveCodeOrID }));
+  };
+
   return (
     <Formik initialValues={initialValues} validationSchema={originSITValidationSchema} onSubmit={onSubmit}>
-      <Form data-testid="originSITServiceItemForm">
-        <input type="hidden" name="moveTaskOrderID" />
-        <input type="hidden" name="mtoShipmentID" />
-        <input type="hidden" name="modelType" />
-        <input type="hidden" name="reServiceCode" />
-        <TextField name="reason" id="reason" label="Reason" />
-        <MaskedTextField
-          id="sitPostalCode"
-          name="sitPostalCode"
-          label="SIT postal code"
-          mask="00000[{-}0000]"
-          placeholder="62225"
-        />
-        <DatePickerInput label="SIT entry Date" name="sitEntryDate" />
-        <DatePickerInput label="SIT departure Date" name="sitDepartureDate" />
-        <AddressFields legend="SIT HHG actual origin" name="sitHHGActualOrigin" />
-        <Button type="submit">Create service item</Button>
-      </Form>
+      {({ isValid, isSubmitting, handleSubmit, setValues }) => {
+        const handleLocationChange = (newValue) => {
+          setValues((prevValues) => {
+            return {
+              ...prevValues,
+              sitHHGActualOrigin: {
+                ...prevValues.address,
+                city: newValue.city,
+                state: newValue.state ? newValue.state : '',
+                county: newValue.county,
+                postalCode: newValue.postalCode,
+                usprcId: newValue.usPostRegionCitiesId ? newValue.usPostRegionCitiesId : '',
+              },
+            };
+          });
+        };
+
+        return (
+          <Form data-testid="originSITServiceItemForm">
+            <input type="hidden" name="moveTaskOrderID" />
+            <input type="hidden" name="mtoShipmentID" />
+            <input type="hidden" name="modelType" />
+            <input type="hidden" name="reServiceCode" />
+            <TextField name="reason" id="reason" label="Reason" />
+            <MaskedTextField
+              id="sitPostalCode"
+              name="sitPostalCode"
+              label="SIT postal code"
+              mask="00000[{-}0000]"
+              placeholder="62225"
+            />
+            <DatePickerInput label="SIT entry Date" name="sitEntryDate" />
+            <DatePickerInput label="SIT departure Date" name="sitDepartureDate" />
+            <AddressFields
+              legend="SIT HHG actual origin"
+              name="sitHHGActualOrigin"
+              zipCityEnabled
+              handleLocationChange={handleLocationChange}
+            />
+            <Button onClick={handleSubmit} disabled={isSubmitting || !isValid} type="submit">
+              Create service item
+            </Button>
+            <Button type="button" unstyled onClick={handleCancel}>
+              Cancel
+            </Button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
