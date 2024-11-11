bd969ec736e7322cea282de7773c9bffe730f216
Alex Lusk <alex.lusk@caci.com>
2024-11-11 21:38:15 +0000

    client validation to prevent transportation office assignments that are duplicate or in the same GBLOC

--- pkg/services/transportation_office/transportation_office_fetcher_test.go
+++ pkg/services/transportation_office/transportation_office_fetcher_test.go
@@ -8,11 +8,13 @@ import (
 
 	"github.com/transcom/mymove/pkg/factory"
 	"github.com/transcom/mymove/pkg/models"
+	"github.com/transcom/mymove/pkg/services"
 	"github.com/transcom/mymove/pkg/testingsuite"
 )
 
 type TransportationOfficeServiceSuite struct {
 	*testingsuite.PopTestSuite
+	toFetcher services.TransportationOfficesFetcher
 }
 
 func TestTransportationOfficeServiceSuite(t *testing.T) {
@@ -181,4 +183,41 @@ func (suite *TransportationOfficeServiceSuite) Test_FindCounselingOffices() {
 	suite.Len(offices, 2)
 	suite.Equal(offices[0].Name, "PPPO Hill AFB - USAF")
 	suite.Equal(offices[1].Name, "PPPO Travis AFB - USAF")
+}
+
+func (suite *TransportationOfficeServiceSuite) Test_GetTransportationOffice() {
+	suite.toFetcher = NewTransportationOfficesFetcher()
+	transportationOffice1 := factory.BuildTransportationOffice(suite.DB(), []factory.Customization{
+		{
+			Model: models.TransportationOffice{
+				Name:             "OFFICE ONE",
+				ProvidesCloseout: true,
+			},
+		},
+	}, nil)
+
+	transportationOffice2 := factory.BuildTransportationOffice(suite.DB(), []factory.Customization{
+		{
+			Model: models.TransportationOffice{
+				Name:             "OFFICE TWO",
+				ProvidesCloseout: false,
+			},
+		},
+	}, nil)
+
+	office1t, err1t := suite.toFetcher.GetTransportationOffice(suite.AppContextForTest(), transportationOffice1.ID, true)
+	office1f, err1f := suite.toFetcher.GetTransportationOffice(suite.AppContextForTest(), transportationOffice1.ID, false)
+
+	_, err2t := suite.toFetcher.GetTransportationOffice(suite.AppContextForTest(), transportationOffice2.ID, true)
+	office2f, err2f := suite.toFetcher.GetTransportationOffice(suite.AppContextForTest(), transportationOffice2.ID, false)
+
+	suite.NoError(err1t)
+	suite.NoError(err1f)
+	// Should return an error since no office matches the ID and provides closeout
+	suite.Error(err2t)
+	suite.NoError(err2f)
+
+	suite.Equal(office1t, "OFFICE ONE")
+	suite.Equal(office1f, "OFFICE ONE")
+	suite.Equal(office2f, "OFFICE TWO")
 }
