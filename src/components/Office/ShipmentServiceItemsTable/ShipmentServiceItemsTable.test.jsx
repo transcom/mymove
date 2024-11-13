6570207a90b4ebb3291364abf2b2ee79a6bf52a8
Brian Manley <brian.manley@caci.com>
2024-11-13 15:52:48 +0000

    B-21568 fix existing tests

--- src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
+++ src/components/Office/ShipmentServiceItemsTable/ShipmentServiceItemsTable.test.jsx
@@ -3,11 +3,50 @@ import { render, screen } from '@testing-library/react';
 
 import ShipmentServiceItemsTable from './ShipmentServiceItemsTable';
 
-import { SHIPMENT_OPTIONS } from 'shared/constants';
+import { SHIPMENT_OPTIONS, MARKET_CODES } from 'shared/constants';
 
-const destZip3 = '112';
-const sameDestZip3 = '902';
-const pickupZip3 = '902';
+const destinationAddress = {
+  postalCode: '11234',
+  isOconus: false,
+};
+
+const destinationAddressSameZip3 = {
+  postalCode: '90299',
+  isOconus: false,
+};
+
+const pickupAddress = {
+  postalCode: '90210',
+  isOconus: false,
+};
+
+const domesticHhgShipment = {
+  shipmentType: SHIPMENT_OPTIONS.HHG,
+  marketCode: MARKET_CODES.DOMESTIC,
+  pickupAddress,
+  destinationAddress,
+};
+
+const domesticNtsShipment = {
+  shipmentType: SHIPMENT_OPTIONS.NTS,
+  marketCode: MARKET_CODES.DOMESTIC,
+  pickupAddress,
+  destinationAddress,
+};
+
+const domesticNtsrShipment = {
+  shipmentType: SHIPMENT_OPTIONS.NTSR,
+  marketCode: MARKET_CODES.DOMESTIC,
+  pickupAddress,
+  destinationAddress,
+};
+
+const domesticHhgShipmentSameZip3 = {
+  shipmentType: SHIPMENT_OPTIONS.HHG,
+  marketCode: MARKET_CODES.DOMESTIC,
+  pickupAddress,
+  destinationAddress: destinationAddressSameZip3,
+};
 
 describe('Shipment Service Items Table', () => {
   describe('renders the hhg longhaul shipment type with service items', () => {
@@ -19,13 +58,7 @@ describe('Shipment Service Items Table', () => {
       ['Domestic packing'],
       ['Domestic unpacking'],
     ])('expects %s to be in the document', async (serviceItem) => {
-      render(
-        <ShipmentServiceItemsTable
-          destinationZip3={destZip3}
-          pickupZip3={pickupZip3}
-          shipmentType={SHIPMENT_OPTIONS.HHG}
-        />,
-      );
+      render(<ShipmentServiceItemsTable shipment={domesticHhgShipment} />);
       expect(
         await screen.findByRole('heading', { name: 'Service items for this shipment 6 items', level: 4 }),
       ).toBeInTheDocument();
@@ -42,13 +75,7 @@ describe('Shipment Service Items Table', () => {
       ['Domestic packing'],
       ['Domestic unpacking'],
     ])('expects %s to be in the document', async (serviceItem) => {
-      render(
-        <ShipmentServiceItemsTable
-          destinationZip3={sameDestZip3}
-          pickupZip3={pickupZip3}
-          shipmentType={SHIPMENT_OPTIONS.HHG}
-        />,
-      );
+      render(<ShipmentServiceItemsTable shipment={domesticHhgShipmentSameZip3} />);
       expect(
         await screen.findByRole('heading', { name: 'Service items for this shipment 6 items', level: 4 }),
       ).toBeInTheDocument();
@@ -64,13 +91,7 @@ describe('Shipment Service Items Table', () => {
       ['Domestic destination price'],
       ['Domestic NTS packing'],
     ])('expects %s to be in the document', async (serviceItem) => {
-      render(
-        <ShipmentServiceItemsTable
-          destinationZip3={destZip3}
-          pickupZip3={pickupZip3}
-          shipmentType={SHIPMENT_OPTIONS.NTS}
-        />,
-      );
+      render(<ShipmentServiceItemsTable shipment={domesticNtsShipment} />);
       expect(
         await screen.findByRole('heading', { name: 'Service items for this shipment 5 items', level: 4 }),
       ).toBeInTheDocument();
@@ -86,13 +107,7 @@ describe('Shipment Service Items Table', () => {
       ['Domestic destination price'],
       ['Domestic unpacking'],
     ])('expects %s to be in the document', async (serviceItem) => {
-      render(
-        <ShipmentServiceItemsTable
-          destinationZip3={destZip3}
-          pickupZip3={pickupZip3}
-          shipmentType={SHIPMENT_OPTIONS.NTSR}
-        />,
-      );
+      render(<ShipmentServiceItemsTable shipment={domesticNtsrShipment} />);
       expect(
         await screen.findByRole('heading', { name: 'Service items for this shipment 5 items', level: 4 }),
       ).toBeInTheDocument();
