da629fc4b833003c19ba788aeb9520b33dcedaf1
cameroncaci <cameron.jewell@caci.com>
2024-11-13 21:13:57 +0000

    enhanced webpack and pdf support

--- src/components/DocumentViewer/Content/Content.jsx
+++ src/components/DocumentViewer/Content/Content.jsx
@@ -33,7 +33,7 @@ const DocViewerContent = ({ fileType, filePath, saveRotation, setRotationValue, 
               <FontAwesomeIcon icon="search-plus" title="Zoom in" aria-label="Zoom in" />
               Zoom in
             </Button>
-            {['jpg', 'jpeg', 'gif', 'png'].includes(fileType) && (
+            {['jpg', 'jpeg', 'gif', 'png', 'pdf'].includes(fileType) && (
               <>
                 <Button type="button" unstyled onClick={handleRotateLeft}>
                   <FontAwesomeIcon icon="rotate-left" title="Rotate left" aria-label="Rotate left" />
