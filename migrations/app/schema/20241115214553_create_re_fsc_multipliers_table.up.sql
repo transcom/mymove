ea4941f119363fabdec664c9c41ae32cd73f75d3
msaki-caci <michael.saki@caci.com>
2024-11-21 21:08:07 +0000

    B-21671 Fix typo

--- migrations/app/schema/20241115214553_create_re_fsc_multipliers_table.up.sql
+++ migrations/app/schema/20241115214553_create_re_fsc_multipliers_table.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS re_fsc_multipliers (
     updated_at  timestamp        NOT NULL DEFAULT NOW()
 );
 
-COMMENT ON TABLE re_fsc_multipliers IS 'Stores data needed to needed to calculate FSC';
+COMMENT ON TABLE re_fsc_multipliers IS 'Stores data needed to calculate FSC';
 COMMENT ON COLUMN re_fsc_multipliers.low_weight IS 'The lowest weight permitted for a shipment';
 COMMENT ON COLUMN re_fsc_multipliers.high_weight IS 'The highest weight permitted for a shipment';
 COMMENT ON COLUMN re_fsc_multipliers.multiplier IS 'The decimal multiplier used to calculate the FSC';
