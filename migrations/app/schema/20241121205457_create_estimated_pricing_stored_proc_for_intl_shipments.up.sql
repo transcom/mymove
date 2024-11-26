72f892c0e04225f3d0e4f03c57214220c9ad0af7
Daniel Jordan <daniel.jordan@caci.com>
2024-11-26 22:50:28 +0000

    improving modularity with additional functions, adding in POE/PODFSC logic

--- migrations/app/schema/20241121205457_create_estimated_pricing_stored_proc_for_intl_shipments.up.sql
+++ migrations/app/schema/20241121205457_create_estimated_pricing_stored_proc_for_intl_shipments.up.sql
@@ -2,8 +2,6 @@ ALTER TABLE addresses
 	ADD CONSTRAINT us_post_region_cities_id_fkey
 	FOREIGN KEY (us_post_region_cities_id) REFERENCES us_post_region_cities (id);
 
-DROP FUNCTION IF EXISTS calculate_escalated_price(uuid, uuid, uuid, uuid);
-
 -- function to get the rate area id
 CREATE OR REPLACE FUNCTION get_rate_area_id(
     address_id UUID,
@@ -55,24 +53,37 @@ CREATE OR REPLACE FUNCTION calculate_escalated_price(
     o_rate_area_id UUID,
     d_rate_area_id UUID,
     re_service_id UUID,
-    c_id UUID
+    c_id UUID,
+    service_code TEXT
 ) RETURNS NUMERIC AS $$
 DECLARE
     per_unit_cents NUMERIC;
     escalation_factor NUMERIC;
     escalated_price NUMERIC;
 BEGIN
-    SELECT intl_prices.per_unit_cents
-    INTO per_unit_cents
-    FROM re_intl_prices intl_prices
-    WHERE intl_prices.origin_rate_area_id = o_rate_area_id
-      AND intl_prices.destination_rate_area_id = d_rate_area_id
-      AND intl_prices.service_id = re_service_id
-      AND intl_prices.contract_id = c_id;
+    -- we need to query the appropriate table based on the service code
+    IF service_code IN ('ISLH', 'UBP') THEN
+        SELECT rip.per_unit_cents
+        INTO per_unit_cents
+        FROM re_intl_prices rip
+        WHERE (rip.origin_rate_area_id = o_rate_area_id OR o_rate_area_id IS NULL)
+          AND (rip.destination_rate_area_id = d_rate_area_id OR d_rate_area_id IS NULL)
+          AND rip.service_id = re_service_id
+          AND rip.contract_id = c_id;
+    ELSE
+        SELECT riop.per_unit_cents
+        INTO per_unit_cents
+        FROM re_intl_other_prices riop
+        WHERE (riop.rate_area_id = o_rate_area_id OR riop.rate_area_id = d_rate_area_id OR
+            (o_rate_area_id IS NULL AND d_rate_area_id IS NULL))
+        AND riop.service_id = re_service_id
+        AND riop.contract_id = c_id;
 
-    -- IF per_unit_cents IS NULL THEN
-    --     RAISE EXCEPTION 'No matching price found for the given parameters';
-    -- END IF;
+    END IF;
+
+    IF per_unit_cents IS NULL THEN
+        RAISE EXCEPTION 'No per unit cents found for service item id: %, origin rate area: %, dest rate area: %, and contract_id: %', re_service_id, o_rate_area_id, d_rate_area_id, c_id;
+    END IF;
 
     SELECT rcy.escalation
     INTO escalation_factor
@@ -80,13 +91,118 @@ BEGIN
     WHERE rcy.contract_id = c_id;
 
     IF escalation_factor IS NULL THEN
-        RAISE EXCEPTION 'Escalation factor not found for contract_id %', contract_id;
+        RAISE EXCEPTION 'Escalation factor not found for contract_id %', c_id;
     END IF;
 
-    -- Calculate the escalated price
+    -- calculate the escalated price
     escalated_price := ROUND(per_unit_cents * escalation_factor::NUMERIC / 100, 2);
 
     RETURN escalated_price;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- get ZIP code by passing in a shipment ID and the address type
+-- used for PODFSC & POEFSC service item types
+CREATE OR REPLACE FUNCTION get_zip_code(shipment_id uuid, address_type VARCHAR)
+RETURNS INT AS $$
+    DECLARE zip_code VARCHAR;
+    BEGIN
+
+    IF address_type = 'pickup' THEN
+        SELECT vl.uspr_zip_id
+        INTO zip_code
+        FROM mto_shipments ms
+        JOIN addresses a ON a.id = ms.pickup_address_id
+        JOIN v_locations vl ON vl.uprc_id = a.us_post_region_cities_id
+        WHERE ms.id = shipment_id;
+    ELSIF address_type = 'destination' THEN
+        SELECT vl.uspr_zip_id
+        INTO zip_code
+        FROM mto_shipments ms
+        JOIN addresses a ON a.id = ms.destination_address_id
+        JOIN v_locations vl ON vl.uprc_id = a.us_post_region_cities_id
+        WHERE ms.id = shipment_id;
+    ELSIF address_type = 'poe' THEN
+        SELECT vl.uspr_zip_id
+        INTO zip_code
+        FROM mto_service_items si
+        JOIN port_locations pl ON pl.id = si.poe_location_id
+        JOIN v_locations vl ON vl.uprc_id = pl.us_post_region_cities_id
+        WHERE si.mto_shipment_id = shipment_id;
+    ELSIF address_type = 'pod' THEN
+        SELECT vl.uspr_zip_id
+        INTO zip_code
+        FROM mto_service_items si
+        JOIN port_locations pl ON pl.id = si.pod_location_id
+        JOIN v_locations vl ON vl.uprc_id = pl.us_post_region_cities_id
+        WHERE si.mto_shipment_id = shipment_id;
+    END IF;
+
+    RETURN zip_code;
+END;
+$$ LANGUAGE plpgsql;
+
+-- getting the distance between two ZIPs
+CREATE OR REPLACE FUNCTION get_distance(o_zip_code VARCHAR, d_zip_code VARCHAR)
+RETURNS INT AS $$
+    DECLARE dist INT;
+    BEGIN
+
+    SELECT zd.distance_miles
+    INTO dist
+    FROM zip3_distances zd
+    WHERE (zd.from_zip3 = o_zip_code AND zd.to_zip3 = d_zip_code)
+       OR (zd.from_zip3 = d_zip_code AND zd.to_zip3 = o_zip_code);
+
+    -- if no distance found, return 0 (or you could handle calling DTOD here)
+    IF dist IS NULL THEN
+        RETURN 0;
+    END IF;
+
+    RETURN dist;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- querying the re_fsc_multiplier table and getting the multiplier value
+CREATE OR REPLACE FUNCTION get_fsc_multiplier(weight INT, rate_area_id uuid)
+RETURNS DECIMAL AS $$
+    DECLARE m NUMERIC;
+    BEGIN
+
+    SELECT multiplier
+    INTO m
+    FROM re_fsc_multipliers
+    WHERE weight >= low_weight AND weight <= high_weight
+      AND rate_area_id = rate_area_id;
+
+    RETURN m;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- getting the fuel price from the ghc_diesel_fuel_prices table
+CREATE OR REPLACE FUNCTION get_fuel_price(requested_pickup_date DATE)
+RETURNS DECIMAL AS $$
+    DECLARE fuel_price DECIMAL;
+    BEGIN
+
+    SELECT fuel_price_in_millicents / 100000
+    INTO fuel_price
+    FROM ghc_diesel_fuel_prices
+    WHERE requested_pickup_date BETWEEN effective_date AND end_date;
+
+    RETURN fuel_price;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- calculating difference from fuel price from base price
+CREATE OR REPLACE FUNCTION calculate_price_difference(fuel_price DECIMAL)
+RETURNS DECIMAL AS $$
+BEGIN
+    RETURN (fuel_price - 2.50);
 END;
 $$ LANGUAGE plpgsql;
 
@@ -117,10 +233,18 @@ BEGIN
       AND rip.service_id = re_service_id
       AND rip.contract_id = c_id;
 
+    IF per_unit_cents IS NULL THEN
+        RAISE EXCEPTION 'No per unit cents found for service item id: %, origin rate area: %, dest rate area: %, and contract_id: %', re_service_id, o_rate_area_id, d_rate_area_id, c_id;
+    END IF;
+
     SELECT rcy.escalation
     INTO escalation_factor
     FROM re_contract_years rcy
     WHERE rcy.contract_id = c_id;
+
+    IF escalation_factor IS NULL THEN
+        RAISE EXCEPTION 'Escalation factor not found for contract_id %', contract_id;
+    END IF;
 
     escalated_price := ROUND(per_unit_cents * escalation_factor / 100, 2);
     estimated_price := ROUND(escalated_price * (estimated_weight / 100), 2);
@@ -153,10 +277,18 @@ BEGIN
       AND rip.service_id = re_service_id
       AND rip.contract_id = c_id;
 
+    IF per_unit_cents IS NULL THEN
+        RAISE EXCEPTION 'No per unit cents found for service item id: %, origin rate area: %, and contract_id: %', re_service_id, o_rate_area_id, c_id;
+    END IF;
+
     SELECT rcy.escalation
     INTO escalation_factor
     FROM re_contract_years rcy
     WHERE rcy.contract_id = c_id;
+
+    IF escalation_factor IS NULL THEN
+        RAISE EXCEPTION 'Escalation factor not found for contract_id %', contract_id;
+    END IF;
 
     escalated_price := ROUND(per_unit_cents * escalation_factor / 100, 2);
     estimated_price := ROUND(escalated_price * (estimated_weight / 100), 2);
@@ -177,6 +309,12 @@ DECLARE
     d_rate_area_id UUID;
     contract_id UUID;
     service_code TEXT;
+    o_zip_code TEXT;
+    d_zip_code TEXT;
+    distance NUMERIC;
+    estimated_fsc_multiplier NUMERIC;
+    fuel_price NUMERIC;
+    price_difference NUMERIC;
 BEGIN
     SELECT ms.id, ms.pickup_address_id, ms.destination_address_id, ms.requested_pickup_date, ms.prime_estimated_weight
     INTO shipment
@@ -185,6 +323,11 @@ BEGIN
 
     IF shipment IS NULL THEN
         RAISE EXCEPTION ''Shipment with ID % not found'', shipment_id;
+    END IF;
+
+    -- exit the proc if prime_estimated_weight is NULL
+    IF shipment.prime_estimated_weight IS NULL THEN
+        RETURN;
     END IF;
 
     -- loop through service items in the shipment
@@ -204,7 +347,7 @@ BEGIN
                 o_rate_area_id := get_rate_area_id(shipment.pickup_address_id, service_item.re_service_id);
                 d_rate_area_id := get_rate_area_id(shipment.destination_address_id, service_item.re_service_id);
                 contract_id := get_contract_id(shipment.requested_pickup_date);
-                escalated_price := calculate_escalated_price(o_rate_area_id, d_rate_area_id, service_item.re_service_id, contract_id);
+                escalated_price := calculate_escalated_price(o_rate_area_id, d_rate_area_id, service_item.re_service_id, contract_id, service_code);
 
                 IF shipment.prime_estimated_weight IS NOT NULL THEN
                     estimated_price := ROUND(escalated_price * (shipment.prime_estimated_weight / 100)::NUMERIC, 2);
@@ -214,7 +357,7 @@ BEGIN
                 -- perform IHPK/IUBPK-specific logic (no destination rate area)
                 o_rate_area_id := get_rate_area_id(shipment.pickup_address_id, service_item.re_service_id);
                 contract_id := get_contract_id(shipment.requested_pickup_date);
-                escalated_price := calculate_escalated_price(o_rate_area_id, NULL, service_item.re_service_id, contract_id);
+                escalated_price := calculate_escalated_price(o_rate_area_id, NULL, service_item.re_service_id, contract_id, service_code);
 
                 IF shipment.prime_estimated_weight IS NOT NULL THEN
                     estimated_price := ROUND(escalated_price * (shipment.prime_estimated_weight / 100)::NUMERIC, 2);
@@ -224,17 +367,40 @@ BEGIN
                 -- perform IHUPK/IUBUPK-specific logic (no origin rate area)
                 d_rate_area_id := get_rate_area_id(shipment.destination_address_id, service_item.re_service_id);
                 contract_id := get_contract_id(shipment.requested_pickup_date);
-                escalated_price := calculate_escalated_price(NULL, d_rate_area_id, service_item.re_service_id, contract_id);
+                escalated_price := calculate_escalated_price(NULL, d_rate_area_id, service_item.re_service_id, contract_id, service_code);
 
                 IF shipment.prime_estimated_weight IS NOT NULL THEN
                     estimated_price := ROUND(escalated_price * (shipment.prime_estimated_weight / 100)::NUMERIC, 2);
                 END IF;
 
             WHEN service_code IN (''POEFSC'', ''PODFSC'') THEN
-                estimated_price := 0; -- placeholder
+                IF service_code = ''POEFSC'' THEN
+                    o_zip_code := get_zip_code(shipment.id, ''pickup'');
+                    d_zip_code := get_zip_code(shipment.id, ''poe'');
+                END IF;
+
+                IF service_code = ''PODFSC'' THEN
+                    o_zip_code := get_zip_code(shipment.id, ''destination'');
+                    d_zip_code := get_zip_code(shipment.id, ''pod'');
+                END IF;
+
+                -- getting distance between the two ZIPs
+                distance := get_distance(o_zip_code, d_zip_code);
+
+                -- getting FSC multiplier from re_fsc_multipliers
+                estimated_fsc_multiplier := get_fsc_multiplier(shipment.prime_estimated_weight, o_rate_area_id);
+
+                fuel_price := get_fuel_price(shipment.requested_pickup_date);
+
+                price_difference := calculate_price_difference(fuel_price);
+
+                -- calculate estimated price
+                IF estimated_fsc_multiplier IS NOT NULL AND distance IS NOT NULL THEN
+                    estimated_price := ROUND(distance * estimated_fsc_multiplier * price_difference, 2);
+                END IF;
         END CASE;
 
-        -- Update the pricing_estimate in mto_service_items
+        -- update the pricing_estimate value in mto_service_items
         UPDATE mto_service_items
         SET pricing_estimate = estimated_price
         WHERE id = service_item.id;
