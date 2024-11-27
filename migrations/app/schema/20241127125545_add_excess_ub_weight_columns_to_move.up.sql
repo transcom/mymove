b2e2af19871706dcd41a808fb56db422d15c07bf
cameroncaci <cameron.jewell@caci.com>
2024-11-27 19:47:01 +0000

    fix migration

--- migrations/app/schema/20241127125545_add_excess_ub_weight_columns_to_move.up.sql
+++ migrations/app/schema/20241127125545_add_excess_ub_weight_columns_to_move.up.sql
@@ -4,5 +4,5 @@
 ALTER TABLE moves ADD COLUMN IF NOT EXISTS excess_unaccompanied_baggage_weight_qualified_at timestamp with time zone;
 ALTER TABLE moves ADD COLUMN IF NOT EXISTS excess_unaccompanied_baggage_weight_acknowledged_at timestamp with time zone;
 
-COMMENT ON COLUMN mto_shipments.excess_unaccompanied_baggage_weight_qualified_at IS "The date and time the sum of all the move's unaccompanied baggage shipment weights met or exceeded the excess unaccompanied baggage weight qualification threshold.";
-COMMENT ON COLUMN mto_shipments.excess_unaccompanied_baggage_weight_acknowledged_at IS 'The date and time the TOO dismissed the risk of excess unaccompanied baggage weight alert.';
+COMMENT ON COLUMN moves.excess_unaccompanied_baggage_weight_qualified_at IS 'The date and time the sum of all the move''s unaccompanied baggage shipment weights met or exceeded the excess unaccompanied baggage weight qualification threshold.';
+COMMENT ON COLUMN moves.excess_unaccompanied_baggage_weight_acknowledged_at IS 'The date and time the TOO dismissed the risk of excess unaccompanied baggage weight alert.';
