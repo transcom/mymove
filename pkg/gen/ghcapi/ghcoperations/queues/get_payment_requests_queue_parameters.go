92ffffb975ffc4644881c6441adb96c857072d9a
Jon Spight <Jonathan.Spight@caci.com>
2024-12-02 16:56:59 +0000

    fixed merge conflicts

--- pkg/gen/ghcapi/ghcoperations/queues/get_payment_requests_queue_parameters.go
+++ pkg/gen/ghcapi/ghcoperations/queues/get_payment_requests_queue_parameters.go
@@ -491,7 +491,7 @@ func (o *GetPaymentRequestsQueueParams) bindSort(rawData []string, hasKey bool, 
 // validateSort carries on validations for parameter Sort
 func (o *GetPaymentRequestsQueueParams) validateSort(formats strfmt.Registry) error {
 
-	if err := validate.EnumCase("sort", "query", *o.Sort, []interface{}{"customerName", "locator", "submittedAt", "branch", "status", "dodID", "emplid", "age", "originDutyLocation", "assignedTo", "counselingOffice"}, true); err != nil {
+	if err := validate.EnumCase("sort", "query", *o.Sort, []interface{}{"customerName", "locator", "submittedAt", "branch", "status", "edipi", "emplid", "age", "originDutyLocation", "assignedTo", "counselingOffice"}, true); err != nil {
 		return err
 	}
 
