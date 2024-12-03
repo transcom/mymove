6a4da09a720f3d2d6b2fbff31acd41d1f4a3d8f4
Tae Jung <tae.jung@caci.com>
2024-12-03 15:18:53 +0000

    allow payment request for international crate

--- src/components/PrimeUI/ServiceItem/ServiceItem.jsx
+++ src/components/PrimeUI/ServiceItem/ServiceItem.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import descriptionListStyles from '../../../styles/descriptionList.module.scss';
 import { MTOServiceItemShape } from '../../../types';
 
-import { SERVICE_ITEMS_ALLOWED_WEIGHT_BILLED_PARAM } from 'constants/serviceItems';
+import { SERVICE_ITEMS_ALLOWED_WEIGHT_BILLED_PARAM, SERVICE_ITEM_CODES } from 'constants/serviceItems';
+import { convertFromThousandthInchToInch } from 'utils/formatters';
 
 const ServiceItem = ({ serviceItem, mtoShipment }) => {
   return (
     <dl className={descriptionListStyles.descriptionList}>
-      <h3>{serviceItem.reServiceName}</h3>
+      <h3>
+        {serviceItem.reServiceName} {serviceItem.standaloneCrate && '- Standalone'}
+      </h3>
       <div className={descriptionListStyles.row}>
         <dt>Status:</dt>
         <dd>{serviceItem.status}</dd>
@@ -34,6 +37,42 @@ const ServiceItem = ({ serviceItem, mtoShipment }) => {
           <dt>Shipment Weight (pounds):</dt>
           <dd>{mtoShipment.primeActualWeight || 'Not provided'}</dd>
         </div>
+      )}
+      {(serviceItem.reServiceCode === SERVICE_ITEM_CODES.ICRT ||
+        serviceItem.reServiceCode === SERVICE_ITEM_CODES.IUCRT ||
+        serviceItem.reServiceCode === SERVICE_ITEM_CODES.DCRT ||
+        serviceItem.reServiceCode === SERVICE_ITEM_CODES.DUCRT) && (
+        <>
+          <div className={descriptionListStyles.row}>
+            <dt>Item Size:</dt>
+            <dd>
+              {convertFromThousandthInchToInch(serviceItem.item?.length)}&quot; x&nbsp;
+              {convertFromThousandthInchToInch(serviceItem.item?.width)}&quot; x&nbsp;
+              {convertFromThousandthInchToInch(serviceItem.item?.height)}&quot;
+            </dd>
+          </div>
+          <div className={descriptionListStyles.row}>
+            <dt>Crate Size:</dt>
+            <dd>
+              {convertFromThousandthInchToInch(serviceItem.crate?.length)}&quot; x&nbsp;
+              {convertFromThousandthInchToInch(serviceItem.crate?.width)}&quot; x&nbsp;
+              {convertFromThousandthInchToInch(serviceItem.crate?.height)}&quot;
+            </dd>
+          </div>
+        </>
+      )}
+      {(serviceItem.reServiceCode === SERVICE_ITEM_CODES.ICRT ||
+        serviceItem.reServiceCode === SERVICE_ITEM_CODES.IUCRT) && (
+        <>
+          <div className={descriptionListStyles.row}>
+            <dt>External Crate:</dt>
+            <dd>{serviceItem.externalCrate ? 'Yes' : 'No'}</dd>
+          </div>
+          <div className={descriptionListStyles.row}>
+            <dt>Market:</dt>
+            <dd>{serviceItem.market || 'Not provided'}</dd>
+          </div>
+        </>
       )}
     </dl>
   );
