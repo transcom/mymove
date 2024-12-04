575f4c4bb4c09e656740d7c85e4c281ca3d180ba
Ricky Mettler <ricky.mettler@caci.com>
2024-12-04 20:19:23 +0000

    devlocal test revert

--- pkg/cli/devlocal_test.go
+++ pkg/cli/devlocal_test.go
@@ -1,8 +1,6 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigDevlocal() {
-	var viper = suite.viper
-	viper.Set("devlocal-auth", true)
 	suite.Setup(InitDevlocalFlags, []string{})
-	suite.NoError(CheckDevlocal(viper))
+	suite.NoError(CheckDevlocal(suite.viper))
 }
