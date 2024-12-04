5ee3379d0724bbea722a8a0d7c3a538b66d8de90
deandreJones <deandre.Jones@caci.com>
2024-12-04 09:45:31 -0600

    remove exception for oconus-oconus

--- migrations/app/schema/20241119151019_stored_procs_for_ordering_service_items.up.sql
+++ migrations/app/schema/20241119151019_stored_procs_for_ordering_service_items.up.sql
@@ -159,10 +159,7 @@ BEGIN
                     RAISE EXCEPTION ''Error creating POEFSC service item for shipment %: %'', shipment_id, SQLERRM;
             END;
         END LOOP;
-    ELSE
-        RAISE EXCEPTION ''Invalid shipment direction for shipment %: Pickup is %CONUS, Destination is %CONUS.'',
-                         shipment_id, is_pickup_oconus, is_destination_oconus;
-    END IF;
+   END IF;
 
     -- create all other auto-approved service items, filtering out the POEFSC or PODFSC service items
     FOR service_item IN
