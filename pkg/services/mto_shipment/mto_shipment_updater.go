aeddb2c0cf4e4f533605ac884e22820fea38cd9d
AaronW <aaron.weatherford@caci.com>
2024-12-04 20:14:29 +0000

    corrected logic in another test

--- pkg/services/mto_shipment/mto_shipment_updater.go
+++ pkg/services/mto_shipment/mto_shipment_updater.go
@@ -748,16 +748,6 @@ func (f *mtoShipmentUpdater) updateShipmentRecord(appCtx appcontext.AppContext, 
 				}
 			}
 
-			if newShipment.PrimeActualWeight != nil {
-				if dbShipment.PrimeActualWeight == nil || *newShipment.PrimeActualWeight != *dbShipment.PrimeActualWeight {
-					var err error
-					autoReweighShipments, err = f.moveWeights.CheckAutoReweigh(txnAppCtx, dbShipment.MoveTaskOrderID, newShipment)
-					if err != nil {
-						return err
-					}
-				}
-			}
-
 			if dbShipment.PrimeEstimatedWeight == nil || *newShipment.PrimeEstimatedWeight != *dbShipment.PrimeEstimatedWeight {
 				existingMoveStatus := move.Status
 				// if the move is in excess weight risk and the TOO has not acknowledge that, need to change move status to "Approvals Requested"
@@ -786,6 +776,16 @@ func (f *mtoShipmentUpdater) updateShipmentRecord(appCtx appcontext.AppContext, 
 					if err != nil {
 						return err
 					}
+				}
+			}
+		}
+
+		if newShipment.PrimeActualWeight != nil {
+			if dbShipment.PrimeActualWeight == nil || *newShipment.PrimeActualWeight != *dbShipment.PrimeActualWeight {
+				var err error
+				autoReweighShipments, err = f.moveWeights.CheckAutoReweigh(txnAppCtx, dbShipment.MoveTaskOrderID, newShipment)
+				if err != nil {
+					return err
 				}
 			}
 		}
