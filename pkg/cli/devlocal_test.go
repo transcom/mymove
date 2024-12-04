b3a6189ae96a96da778ecd285267ff8ccdc5462a
Ricky Mettler <ricky.mettler@caci.com>
2024-12-04 19:43:22 +0000

    test coverage

--- pkg/cli/devlocal_test.go
+++ pkg/cli/devlocal_test.go
@@ -1,6 +1,8 @@
 package cli
 
 func (suite *cliTestSuite) TestConfigDevlocal() {
+	var viper = suite.viper
+	viper.Set("devlocal-auth", true)
 	suite.Setup(InitDevlocalFlags, []string{})
-	suite.NoError(CheckDevlocal(suite.viper))
+	suite.NoError(CheckDevlocal(viper))
 }
