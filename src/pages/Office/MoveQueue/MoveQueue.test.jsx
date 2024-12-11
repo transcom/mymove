75b6437980c8cb0ef7ad41f043c3d87e33889727
Daniel Jordan <daniel.jordan@caci.com>
2024-12-11 22:32:46 +0000

    UI tests

--- src/pages/Office/MoveQueue/MoveQueue.test.jsx
+++ src/pages/Office/MoveQueue/MoveQueue.test.jsx
@@ -324,7 +324,7 @@ describe('MoveQueue', () => {
     wrapper.update();
     expect(wrapper.find('[data-testid="multi-value-container"]').text()).toEqual('New move');
   });
-  it('renders Search and Move Queue tabs', () => {
+  it('renders Search, Destination Requests and Move Queue tabs', () => {
     reactRouterDom.useParams.mockReturnValue({ queueType: generalRoutes.QUEUE_SEARCH_PATH });
     render(
       <reactRouterDom.BrowserRouter>
@@ -334,6 +334,7 @@ describe('MoveQueue', () => {
     expect(screen.getByTestId('closeout-tab-link')).toBeInTheDocument();
     expect(screen.getByTestId('search-tab-link')).toBeInTheDocument();
     expect(screen.getByText('Task Order Queue', { selector: 'span' })).toBeInTheDocument();
+    expect(screen.getByText('Destination Requests Queue', { selector: 'span' })).toBeInTheDocument();
     expect(screen.getByText('Search', { selector: 'span' })).toBeInTheDocument();
   });
   it('renders TableQueue when Search tab is selected', () => {
