db639b15c82cb68cae0d18e86fa073695a0bb550
Alex Lusk <alex.lusk@caci.com>
2024-12-11 23:09:43 +0000

    Add test for transportation office assignment fetcher

---
+++ pkg/services/transportation_office_assignments/transportation_office_assignments_fetcher_test.go
@@ -0,0 +1,36 @@
+package transportationofficeassignments
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/transcom/mymove/pkg/factory"
+	"github.com/transcom/mymove/pkg/services"
+	"github.com/transcom/mymove/pkg/testingsuite"
+)
+
+type TransportationOfficeAssignmentsServiceSuite struct {
+	*testingsuite.PopTestSuite
+	toaFetcher services.TransportaionOfficeAssignmentFetcher
+}
+
+func TestTransportationOfficeAssignmentsServiceSuite(t *testing.T) {
+	ts := &TransportationOfficeAssignmentsServiceSuite{
+		PopTestSuite: testingsuite.NewPopTestSuite(
+			testingsuite.CurrentPackage(),
+			testingsuite.WithPerTestTransaction(),
+		),
+	}
+	suite.Run(t, ts)
+	ts.PopTestSuite.TearDown()
+}
+
+func (suite *TransportationOfficeAssignmentsServiceSuite) Test_FetchTransportaionOfficeAssignmentsByOfficeUserID() {
+	officeUser := factory.BuildOfficeUser(suite.DB(), nil, nil)
+	assignments, err := suite.toaFetcher.FetchTransportaionOfficeAssignmentsByOfficeUserID(suite.AppContextForTest(), officeUser.ID)
+
+	suite.NoError(err)
+	suite.Equal(1, len(assignments))
+	suite.Equal(officeUser.ID, assignments[0].ID)
+}
