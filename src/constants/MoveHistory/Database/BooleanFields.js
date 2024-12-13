7c7c7bd308e7043a6aa5043abd055eb07ee7769b
Cory Kleinjan <cory.kleinjan@caci.com>
2024-12-13 01:33:39 +0000

    Changing labels to be accurate

--- src/constants/MoveHistory/Database/BooleanFields.js
+++ src/constants/MoveHistory/Database/BooleanFields.js
@@ -16,8 +16,8 @@ export default {
   phone_is_preferred: 'phone_is_preferred',
   uses_external_vendor: 'uses_external_vendor',
   diversion: 'diversion',
-  has_secondary_pickup_address: 'Secondary Pickup Address',
-  has_secondary_delivery_address: 'Secondary Delivery Address',
-  has_tertiary_pickup_address: 'Third Pickup Address',
-  has_tertiary_delivery_address: 'Third Delivery Address',
+  has_secondary_pickup_address: 'has_secondary_pickup_address',
+  has_secondary_delivery_address: 'has_secondary_delivery_address',
+  has_tertiary_pickup_address: 'has_tertiary_pickup_address',
+  has_tertiary_delivery_address: 'has_tertiary_delivery_address',
 };
