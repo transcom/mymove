b915d6105f132ef7f442cbfa28f04e2ac2561445
Alex Lusk <alex.lusk@caci.com>
2024-12-19 18:57:41 +0000

    remove proxy wild cards causing 404 on logout

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,9 +11,9 @@ module.exports = (app) => {
   app.use('/testharness', createProxyMiddleware({ target: 'http://milmovelocal:8080/testharness' }));
   app.use('/storage', createProxyMiddleware({ target: 'http://milmovelocal:8080/storage' }));
   app.use('/devlocal-auth', createProxyMiddleware({ target: 'http://milmovelocal:8080/devlocal-auth' }));
-  app.use('/auth/**', createProxyMiddleware({ target: 'http://milmovelocal:8080/auth/**' }));
+  app.use('/auth', createProxyMiddleware({ target: 'http://milmovelocal:8080/auth' }));
   app.use('/logout', createProxyMiddleware({ target: 'http://milmovelocal:8080/logout' }));
   app.use('/downloads', createProxyMiddleware({ target: 'http://milmovelocal:8080/downloads' }));
-  app.use('/debug/**', createProxyMiddleware({ target: 'http://milmovelocal:8080/debug/**' }));
-  app.use('/client/**', createProxyMiddleware({ target: 'http://milmovelocal:8080/client/**' }));
+  app.use('/debug', createProxyMiddleware({ target: 'http://milmovelocal:8080/debug' }));
+  app.use('/client', createProxyMiddleware({ target: 'http://milmovelocal:8080/client' }));
 };
