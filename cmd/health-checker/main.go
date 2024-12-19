7fd61cca0a8b4eb31bc92dc04983b08564156f7c
deandreJones <deandre.Jones@caci.com>
2024-12-19 14:30:04 -0600

    test deadlock in demo

--- cmd/health-checker/main.go
+++ cmd/health-checker/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
+	"fmt"
 	"log"
 	"net/http"
 	neturl "net/url"
@@ -11,6 +12,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -341,6 +343,25 @@ func createLogger(env string, level string) (*zap.Logger, func(), error) {
 }
 
 func main() {
+	var wg sync.WaitGroup
+	var mu1, mu2 sync.Mutex
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		mu1.Lock()
+		fmt.Println("goroutine 1 acquired lock 1")
+		mu2.Lock()
+		fmt.Println("goroutine 1 acquired lock 2")
+	}()
+	go func() {
+		defer wg.Done()
+		mu2.Lock()
+		fmt.Println("goroutine 2 acquired lock 2")
+		mu1.Lock()
+		fmt.Println("goroutine 2 acquired lock 1")
+	}()
+	wg.Wait()
 
 	flag := pflag.CommandLine
 
