fc2779661faa103d9772e0ea0ebeaec17bf96bb8
Brooklyn Welsh <brooklyn.welsh@caci.com>
2024-12-31 21:09:43 +0000

    Added migration for mobile home specific pack/unpack and origin/destination price.

---
+++ migrations/app/schema/20241231210638_create_mobile_home_service_items.up.sql
@@ -0,0 +1,4 @@
+UPDATE re_services SET code = 'DMHDDP', updated_at = now() WHERE id = '392c734e-f655-4937-8784-c7824ff40d0a'
+UPDATE re_services SET code = 'DMHDOP', updated_at = now() WHERE id = '8e645ff9-cb4f-4d69-89fd-2625c4840952'
+UPDATE re_services SET code = 'DMHPK', updated_at = now() WHERE id = 'f0ddf50d-083e-4a14-9336-f093552c4ce3'
+UPDATE re_services SET code = 'DMHUPK', updated_at = now() WHERE id = 'cb1809cb-b55d-4836-ba72-63b921f312e9'
