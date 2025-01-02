776f7307919af7f5d3efeb963f0b812a75402cfb
Daniel Jordan <daniel.jordan@caci.com>
2025-01-02 20:34:42 +0000

    added tests for service param value lookups folder

--- pkg/payment_request/service_param_value_lookups/per_unit_cents_lookup.go
+++ pkg/payment_request/service_param_value_lookups/per_unit_cents_lookup.go
@@ -1,7 +1,6 @@
 package serviceparamvaluelookups
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/transcom/mymove/pkg/appcontext"
@@ -16,16 +15,17 @@ type PerUnitCentsLookup struct {
 }
 
 func (p PerUnitCentsLookup) lookup(appCtx appcontext.AppContext, s *ServiceItemParamKeyData) (string, error) {
-	var isPeakPeriod bool
 	serviceID := p.ServiceItem.ReServiceID
 	contractID := s.ContractID
-	if p.ServiceItem.ReService.Code == models.ReServiceCodeIHPK {
-		// IHPK we need the rate area id for the pickup address
-		rateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, p.ServiceItem.ReServiceID, contractID)
+
+	switch p.ServiceItem.ReService.Code {
+	case models.ReServiceCodeIHPK:
+		// IHPK: Need rate area ID for the pickup address
+		rateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, serviceID, contractID)
 		if err != nil {
-			return "", fmt.Errorf("error fetching rate area id for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, p.ServiceItem.ReServiceID, err)
+			return "", fmt.Errorf("error fetching rate area id for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, serviceID, err)
 		}
-		isPeakPeriod = ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
+		isPeakPeriod := ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
 		var reIntlOtherPrice models.ReIntlOtherPrice
 		err = appCtx.DB().Q().
 			Where("contract_id = ?", contractID).
@@ -37,14 +37,14 @@ func (p PerUnitCentsLookup) lookup(appCtx appcontext.AppContext, s *ServiceItemP
 			return "", fmt.Errorf("error fetching IHPK per unit cents for contractID: %s, serviceID %s, isPeakPeriod: %t, and rateAreaID: %s: %s", contractID, serviceID, isPeakPeriod, rateAreaID, err)
 		}
 		return reIntlOtherPrice.PerUnitCents.ToMillicents().ToCents().String(), nil
-	}
-	if p.ServiceItem.ReService.Code == models.ReServiceCodeIHUPK {
-		// IHUPK we need the rate area id for the destination address
-		rateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, p.ServiceItem.ReServiceID, contractID)
-		if err != nil && err != sql.ErrNoRows {
-			return "", fmt.Errorf("error fetching rate area id for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, p.ServiceItem.ReServiceID, err)
+
+	case models.ReServiceCodeIHUPK:
+		// IHUPK: Need rate area ID for the destination address
+		rateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, serviceID, contractID)
+		if err != nil {
+			return "", fmt.Errorf("error fetching rate area id for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, serviceID, err)
 		}
-		isPeakPeriod = ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
+		isPeakPeriod := ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
 		var reIntlOtherPrice models.ReIntlOtherPrice
 		err = appCtx.DB().Q().
 			Where("contract_id = ?", contractID).
@@ -56,17 +56,18 @@ func (p PerUnitCentsLookup) lookup(appCtx appcontext.AppContext, s *ServiceItemP
 			return "", fmt.Errorf("error fetching IHUPK per unit cents for contractID: %s, serviceID %s, isPeakPeriod: %t, and rateAreaID: %s: %s", contractID, serviceID, isPeakPeriod, rateAreaID, err)
 		}
 		return reIntlOtherPrice.PerUnitCents.ToMillicents().ToCents().String(), nil
-	} else if p.ServiceItem.ReService.Code == models.ReServiceCodeISLH {
-		// IHUPK we need the rate area id for the destination address
-		originRateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, p.ServiceItem.ReServiceID, contractID)
-		if err != nil && err != sql.ErrNoRows {
-			return "", fmt.Errorf("error fetching rate area id for origina address for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, p.ServiceItem.ReServiceID, err)
+
+	case models.ReServiceCodeISLH:
+		// ISLH: Need rate area IDs for origin and destination
+		originRateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.PickupAddressID, serviceID, contractID)
+		if err != nil {
+			return "", fmt.Errorf("error fetching rate area id for origin address for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, serviceID, err)
 		}
-		destRateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.DestinationAddressID, p.ServiceItem.ReServiceID, contractID)
-		if err != nil && err != sql.ErrNoRows {
-			return "", fmt.Errorf("error fetching rate area id for destination address for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, p.ServiceItem.ReServiceID, err)
+		destRateAreaID, err := models.FetchRateAreaID(appCtx.DB(), *p.MTOShipment.DestinationAddressID, serviceID, contractID)
+		if err != nil {
+			return "", fmt.Errorf("error fetching rate area id for destination address for shipment ID: %s and service ID %s: %s", p.MTOShipment.ID, serviceID, err)
 		}
-		isPeakPeriod = ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
+		isPeakPeriod := ghcrateengine.IsPeakPeriod(*p.MTOShipment.RequestedPickupDate)
 		var reIntlPrice models.ReIntlPrice
 		err = appCtx.DB().Q().
 			Where("contract_id = ?", contractID).
@@ -76,10 +77,11 @@ func (p PerUnitCentsLookup) lookup(appCtx appcontext.AppContext, s *ServiceItemP
 			Where("destination_rate_area_id = ?", destRateAreaID).
 			First(&reIntlPrice)
 		if err != nil {
-			return "", fmt.Errorf("error fetching ISLH per unit cents for contractID: %s, serviceID %s, isPeakPeriod: %t, originRateAreaID: %s, and destRateAreaid: %s: %s", contractID, serviceID, isPeakPeriod, originRateAreaID, destRateAreaID, err)
+			return "", fmt.Errorf("error fetching ISLH per unit cents for contractID: %s, serviceID %s, isPeakPeriod: %t, originRateAreaID: %s, and destRateAreaID: %s: %s", contractID, serviceID, isPeakPeriod, originRateAreaID, destRateAreaID, err)
 		}
 		return reIntlPrice.PerUnitCents.ToMillicents().ToCents().String(), nil
-	} else {
+
+	default:
 		return "", fmt.Errorf("unsupported service code to retrieve service item param PerUnitCents")
 	}
 }
