bea2ee0cc121c5266076af0f1068a1140bc483c6
Maria Traskowsky <maria.traskowsky@caci.com>
2025-01-03 20:58:13 +0000

    include DOPSIT

--- pkg/services/order/order_fetcher.go
+++ pkg/services/order/order_fetcher.go
@@ -797,7 +797,7 @@ func tooDestinationOnlyRequestsFilter(role roles.RoleType, locator *string) Quer
 			AND
 			(
 				(
-					(mto_service_items.status IS NULL OR (mto_service_items.status = 'SUBMITTED' AND re_services.code IN ('DOFSIT', 'DOASIT', 'DODSIT', 'DOSHUT', 'DOSFSC', 'IOFSIT', 'IOASIT', 'IODSIT', 'IOSHUT', 'IOPSIT', 'ICRT', 'IOSFSC')))
+					(mto_service_items.status IS NULL OR (mto_service_items.status = 'SUBMITTED' AND re_services.code IN ('DOFSIT', 'DOASIT', 'DODSIT', 'DOPSIT', 'DOSHUT', 'DOSFSC', 'IOFSIT', 'IOASIT', 'IODSIT', 'IOSHUT', 'IOPSIT', 'ICRT', 'IOSFSC')))
 				)
 				OR
 				(
@@ -808,7 +808,7 @@ func tooDestinationOnlyRequestsFilter(role roles.RoleType, locator *string) Quer
 			} else {
 				query.Where(`
 			(
-				(mto_service_items.status IS NULL OR (mto_service_items.status = 'SUBMITTED' AND re_services.code IN ('DOFSIT', 'DOASIT', 'DODSIT', 'DOSHUT', 'DOSFSC', 'IOFSIT', 'IOASIT', 'IODSIT', 'IOSHUT', 'IOPSIT', 'ICRT', 'IOSFSC')))
+				(mto_service_items.status IS NULL OR (mto_service_items.status = 'SUBMITTED' AND re_services.code IN ('DOFSIT', 'DOASIT', 'DODSIT', 'DOPSIT', 'DOSHUT', 'DOSFSC', 'IOFSIT', 'IOASIT', 'IODSIT', 'IOSHUT', 'IOPSIT', 'ICRT', 'IOSFSC')))
 			)
 			OR
 			(
