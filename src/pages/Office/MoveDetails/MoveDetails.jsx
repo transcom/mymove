6a767151b3bb4b09802b7b3c19fc4286319e537e
Alex Lusk <alex.lusk@caci.com>
2025-01-07 23:12:21 +0000

    Add counseling office to orders list component

--- src/pages/Office/MoveDetails/MoveDetails.jsx
+++ src/pages/Office/MoveDetails/MoveDetails.jsx
@@ -638,7 +638,7 @@ const MoveDetails = ({
               }
               shipmentsInfoNonPpm={shipmentsInfoNonPPM}
             >
-              <OrdersList ordersInfo={ordersInfo} />
+              <OrdersList ordersInfo={ordersInfo} moveInfo={move} />
             </DetailsPanel>
           </div>
           <div className={styles.section} id="allowances">
