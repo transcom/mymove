4ee0dbe4476dfad907d86104c317d241c0b7c04b
KonstanceH <Konstance.Haffaney@caci.com>
2025-01-09 22:33:56 +0000

    remove breaking type change

--- pkg/services/mto_shipment/mto_shipment_updater.go
+++ pkg/services/mto_shipment/mto_shipment_updater.go
@@ -854,8 +854,6 @@ func (f *mtoShipmentUpdater) updateShipmentRecord(appCtx appcontext.AppContext, 
 		// we only need to do this if the estimated weight, primary addresses, and pickup date are being updated since those all impact pricing
 		// we will compare data here to see if we even need to update the pricing
 		if newShipment.MarketCode == models.MarketCodeInternational &&
-			newShipment.ShipmentType != models.MTOShipmentTypeHHGIntoNTS ||
-			newShipment.ShipmentType != models.MTOShipmentTypeHHGOutOfNTSDom ||
 			newShipment.PrimeEstimatedWeight != nil ||
 			newShipment.PickupAddress != nil && dbShipment.PickupAddress != nil && newShipment.PickupAddress.PostalCode != dbShipment.PickupAddress.PostalCode ||
 			newShipment.DestinationAddress != nil && dbShipment.DestinationAddress != nil && newShipment.DestinationAddress.PostalCode != dbShipment.DestinationAddress.PostalCode ||
