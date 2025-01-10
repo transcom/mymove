341c8fa2ad84e912efb2456e56456d3f03edd978
deandreJones <deandre.Jones@caci.com>
2025-01-10 20:56:40 +0000

    remove comment

--- migrations/app/schema/20250106153750_admin_weight_restriction.up.sql
+++ migrations/app/schema/20250106153750_admin_weight_restriction.up.sql
@@ -1,6 +1,2 @@
 ALTER TABLE entitlements
 ADD COLUMN IF NOT EXISTS admin_restricted_weight_location BOOLEAN;
-
--- ALTER COLUMN admin_restricted_weight_location TYPE boolean USING (COALESCE(admin_restricted_weight_location, false)),
--- ALTER COLUMN admin_restricted_weight_location SET DEFAULT false,
--- ALTER COLUMN admin_restricted_weight_location SET NOT NULL;
