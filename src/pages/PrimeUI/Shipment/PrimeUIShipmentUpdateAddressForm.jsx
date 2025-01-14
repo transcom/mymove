78f694d602bf1f5b338798e94bca08744f24192b
KonstanceH <Konstance.Haffaney@caci.com>
2025-01-14 16:42:44 +0000

    change address form back to original

--- src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateAddressForm.jsx
+++ src/pages/PrimeUI/Shipment/PrimeUIShipmentUpdateAddressForm.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Formik } from 'formik';
 import { useNavigate, useParams, generatePath } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { FormGroup } from '@material-ui/core';
 import classnames from 'classnames';
 
 import SectionWrapper from 'components/Customer/SectionWrapper';
+import { ResidentialAddressShape } from 'types/address';
 import { AddressFields } from 'components/form/AddressFields/AddressFields';
 import { primeSimulatorRoutes } from 'constants/routes';
 import { Form } from 'components/form/Form';
 import formStyles from 'styles/form.module.scss';
 import WizardNavigation from 'components/Customer/WizardNavigation/WizardNavigation';
-import TextField from 'components/form/fields/TextField/TextField';
 
-const PrimeUIShipmentUpdateDestinationAddressForm = ({
+const PrimeUIShipmentUpdateAddressForm = ({
   initialValues,
+  addressLocation,
   onSubmit,
-  updateDestinationAddressSchema,
+  updateShipmentAddressSchema,
   name,
 }) => {
   const { moveCodeOrID } = useParams();
@@ -26,44 +28,19 @@ const PrimeUIShipmentUpdateDestinationAddressForm = ({
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={updateDestinationAddressSchema}>
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={updateShipmentAddressSchema}>
       {({ isValid, isSubmitting, handleSubmit, errors, ...formikProps }) => {
         return (
           <Form className={classnames(formStyles.form)}>
             <FormGroup error={errors != null && Object.keys(errors).length > 0 ? 1 : 0}>
               <SectionWrapper className={formStyles.formSection}>
-                <h2>Update Shipment Delivery Address</h2>
-                <SectionWrapper className={formStyles.formSection}>
-                  <div data-testid="destination-form-details">
-                    This is used to <strong>update</strong> the delivery address on an <strong>already approved</strong>{' '}
-                    shipment. <br />
-                    This also updates the final delivery address for destination SIT service items in the shipment.
-                    <br />
-                    <br />
-                    This endpoint should be used for changing the delivery address of HHG & NTSR shipments.
-                    <br />
-                    <br />
-                    The address update will be automatically approved unless it changes any of the following:
-                    <br />
-                    <strong>
-                      - the service area <br />
-                      - mileage bracket for direct delivery <br />
-                      - domestic short haul to domestic line haul or vice versa <br />- SIT delivery out over 50 miles{' '}
-                      <em>or </em>
-                      back under 50 miles
-                    </strong>
-                    <br />
-                    <br />
-                    If any of those change, the address change will require TOO approval.
-                  </div>
-                </SectionWrapper>
+                <h2>{addressLocation}</h2>
                 <AddressFields name={name} locationLookup formikProps={formikProps} />
-                <TextField label="Contractor Remarks" id="contractorRemarks" name="contractorRemarks" />
               </SectionWrapper>
               <WizardNavigation
                 editMode
                 className={formStyles.formActions}
-                aria-label="Update Shipment Delivery Address"
+                aria-label="Update Shipment Address"
                 type="submit"
                 disableNext={isSubmitting || !isValid}
                 onCancelClick={handleClose}
@@ -77,4 +54,20 @@ const PrimeUIShipmentUpdateDestinationAddressForm = ({
   );
 };
 
-export default PrimeUIShipmentUpdateDestinationAddressForm;
+PrimeUIShipmentUpdateAddressForm.propTypes = {
+  initialValues: PropTypes.shape({
+    address: ResidentialAddressShape,
+    addressID: PropTypes.string,
+    eTag: PropTypes.string,
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  updateShipmentAddressSchema: PropTypes.shape({
+    address: ResidentialAddressShape,
+    addressID: PropTypes.string,
+    eTag: PropTypes.string,
+  }).isRequired,
+  addressLocation: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default PrimeUIShipmentUpdateAddressForm;
