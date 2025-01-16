1b59d9dac6e0a89d1bd3aa1970a404f864c7eb3b
Steven Gleason <180579696+stevengleason-caci@users.noreply.github.com>
2025-01-16 23:02:31 +0000

    B-21569 nullSafeStringCompare function in utils.js.

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -171,3 +171,24 @@ export function isEmpty(obj) {
 export function isNullUndefinedOrWhitespace(value) {
   return value == null || value === undefined || value.trim() === '';
 }
+
+/**
+ * Compare strings. Null, undefined, and blanks are after other values.
+ * @returns -1, 0, 1
+ */
+export function nullSafeStringCompare(a, b) {
+  const A_BEFORE = -1;
+  const A_AFTER = 1;
+  const SAME = 0;
+
+  if (isNullUndefinedOrWhitespace(a) && isNullUndefinedOrWhitespace(b)) {
+    return SAME;
+  }
+  if (isNullUndefinedOrWhitespace(a)) {
+    return A_AFTER;
+  }
+  if (isNullUndefinedOrWhitespace(b)) {
+    return A_BEFORE;
+  }
+  return a.localeCompare(b);
+}
