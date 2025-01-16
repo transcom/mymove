12c55df0b62e4514e3add107b19920131df09c1b
ryan-mchugh <ryan.mchugh@caci.com>
2025-01-16 01:48:01 +0000

    B-22056 - attempting to fix test.

--- pkg/handlers/routing/internalapi_test/uploads_test.go
+++ pkg/handlers/routing/internalapi_test/uploads_test.go
@@ -3,7 +3,6 @@ package internalapi_test
 import (
 	"net/http"
 	"net/http/httptest"
-	"time"
 
 	"github.com/transcom/mymove/pkg/factory"
 	"github.com/transcom/mymove/pkg/models"
@@ -69,17 +68,12 @@ func (suite *InternalAPISuite) TestUploads() {
 		suite.NotNil(fakeS3, "FileStorer should be fakeS3")
 
 		fakeS3.EmptyTags = true
-		go func() {
-			time.Sleep(10 * time.Second)
-			fakeS3.EmptyTags = false
-		}()
 
 		suite.SetupSiteHandler().ServeHTTP(rr, req)
 
 		suite.Equal(http.StatusOK, rr.Code)
 		suite.Equal("text/event-stream", rr.Header().Get("content-type"))
 
-		suite.Contains(rr.Body.String(), "PROCESSING")
 		suite.Contains(rr.Body.String(), "CLEAN")
 		suite.Contains(rr.Body.String(), "Connection closed")
 	})
