80cf55244c75f49243df9b45298f338e62cdfeaa
ryan-mchugh <ryan.mchugh@caci.com>
2025-01-17 19:36:23 +0000

    B-22056 - tests for fakeS3 and local storage.

--- pkg/storage/filesystem_test.go
+++ pkg/storage/filesystem_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -19,5 +20,22 @@ func TestFilesystemPresignedURL(t *testing.T) {
 	expected := "https://example.text/files/key/to/file/12345?contentType=image%2Fjpeg&filename=testimage.jpeg"
 	if url != expected {
 		t.Errorf("wrong presigned url: expected %s, got %s", expected, url)
+	}
+}
+
+func TestFilesystemTags(t *testing.T) {
+	fsParams := FilesystemParams{
+		root:    "/home/username",
+		webRoot: "https://example.text/files",
+	}
+	fs := NewFilesystem(fsParams)
+
+	tags, err := fs.Tags("anyKey")
+	if err != nil {
+		t.Fatalf("could not get tags: %s", err)
+	}
+
+	if tag, exists := tags["av-status"]; exists && strings.Compare(tag, "CLEAN") != 0 {
+		t.Fatal("tag 'av-status' should return CLEAN")
 	}
 }
