2944d591b5fb7b9b21ae0f9505f4aa3977395651
KonstanceH <Konstance.Haffaney@caci.com>
2025-01-23 15:34:16 +0000

    adding orders fix

--- src/components/Office/AddOrdersForm/AddOrdersForm.test.jsx
+++ src/components/Office/AddOrdersForm/AddOrdersForm.test.jsx
@@ -493,7 +493,7 @@ describe('AddOrdersForm - Edge Cases and Additional Scenarios', () => {
       </Provider>,
     );
 
-    expect(screen.getByLabelText('Orders type')).toBeDisabled();
+    expect(screen.getByLabelText(/Orders type/)).toBeDisabled();
   });
 
   it('disables orders type when bluebark move is selected', async () => {
@@ -502,6 +502,6 @@ describe('AddOrdersForm - Edge Cases and Additional Scenarios', () => {
         <AddOrdersForm {...testProps} isBluebarkMoveSelected />
       </Provider>,
     );
-    expect(screen.getByLabelText('Orders type')).toBeDisabled();
+    expect(screen.getByLabelText(/Orders type/)).toBeDisabled();
   });
 });
