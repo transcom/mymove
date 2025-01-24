b964effa304ef856cb36f57f538d3af066a5803a
Maria Traskowsky <maria.traskowsky@caci.com>
2025-01-24 17:43:11 +0000

    Revert "add a bunch of logging to put target and task def"
    This reverts commit 03f00bdc25c4d8841e1f747258be57d4cd64aef9.

--- cmd/ecs-deploy/task_def.go
+++ cmd/ecs-deploy/task_def.go
@@ -243,31 +243,19 @@ func initTaskDefFlags(flag *pflag.FlagSet) {
 	flag.SortFlags = true
 }
 
-func checkTaskDefConfig(logger *log.Logger, v *viper.Viper) error {
+func checkTaskDefConfig(v *viper.Viper) error {
 
-	logger.Println("reached checkTaskDefConfig")
-
-	logger.Println("awsAccountIDFlag")
-	logger.Println(awsAccountIDFlag)
 	awsAccountID := v.GetString(awsAccountIDFlag)
-	logger.Println("awsAccountID")
-	logger.Println(awsAccountID)
 	if len(awsAccountID) == 0 {
 		return fmt.Errorf("%q is invalid: %w", awsAccountIDFlag, &errInvalidAccountID{AwsAccountID: awsAccountID})
 	}
-
-	logger.Println("cli.AWSRegionFlag")
-	logger.Println(cli.AWSRegionFlag)
 
 	_, err := cli.CheckAWSRegion(v)
 	if err != nil {
 		return fmt.Errorf("%q is invalid: %w", cli.AWSRegionFlag, err)
 	}
-	logger.Println("serviceFlag")
-	logger.Println(serviceFlag)
+
 	serviceName := v.GetString(serviceFlag)
-	logger.Println("serviceName")
-	logger.Println(serviceName)
 	if len(serviceName) == 0 {
 		return fmt.Errorf("%q is invalid: %w", serviceFlag, &errInvalidService{Service: serviceName})
 	}
@@ -282,11 +270,7 @@ func checkTaskDefConfig(logger *log.Logger, v *viper.Viper) error {
 		return fmt.Errorf("%q is invalid: %w", serviceFlag, &errInvalidService{Service: serviceName})
 	}
 
-	logger.Println("environmentFlag")
-	logger.Println(environmentFlag)
 	environmentName := v.GetString(environmentFlag)
-	logger.Println("environmentName")
-	logger.Println(environmentName)
 	if len(environmentName) == 0 {
 		return fmt.Errorf("%q is invalid: %w", environmentFlag, &errInvalidEnvironment{Environment: environmentName})
 	}
@@ -300,40 +284,27 @@ func checkTaskDefConfig(logger *log.Logger, v *viper.Viper) error {
 	if !validEnvironment {
 		return fmt.Errorf("%q is invalid: %w", environmentFlag, &errInvalidEnvironment{Environment: environmentName})
 	}
-	logger.Println("imageURIFlag")
-	logger.Println(imageURIFlag)
+
 	image := v.GetString(imageURIFlag)
-	logger.Println("image")
-	logger.Println(image)
 	if len(image) == 0 {
 		return fmt.Errorf("%q is invalid: %w", imageURIFlag, &errInvalidImage{Image: image})
 	}
 
 	if variablesFile := v.GetString(variablesFileFlag); len(variablesFile) > 0 {
-		logger.Println("variablesFile")
-		logger.Println(variablesFile)
 		if _, err := os.Stat(variablesFile); err != nil {
 			return fmt.Errorf("%q is invalid: %w", variablesFileFlag, &errInvalidFile{File: variablesFile})
 		}
 	}
 
-	logger.Println("entryPointFlag")
-	logger.Println(entryPointFlag)
 	entryPoint := v.GetString(entryPointFlag)
-	logger.Println("entryPoint")
-	logger.Println(entryPoint)
 	if len(entryPointFlag) == 0 {
 		return fmt.Errorf("%q is invalid: %w", entryPointFlag, &errInvalidEntryPoint{EntryPoint: entryPoint})
 	}
 	validEntryPoint := false
 	entryPoints := servicesToEntryPoints[serviceName]
-	logger.Println("mapped service to entry point")
 	for _, str := range entryPoints {
-		logger.Println("entryPoint")
-		logger.Println(entryPoint)
 		if entryPoint == str {
 			validEntryPoint = true
-			logger.Println("validEntryPoint is true")
 			break
 		}
 	}
@@ -476,12 +447,11 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 	}
 
 	// Ensure the configuration works against the variables
-	err = checkTaskDefConfig(logger, v)
+	err = checkTaskDefConfig(v)
 	if err != nil {
 		quit(logger, flag, err)
 	}
-	logger.Println("cli.AWSRegionFlag")
-	logger.Println(cli.AWSRegionFlag)
+
 	cfg, errCfg := config.LoadDefaultConfig(context.Background(),
 		config.WithRegion(v.GetString(cli.AWSRegionFlag)),
 	)
@@ -489,132 +459,63 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 		quit(logger, flag, err)
 	}
 
-	logger.Println("cfg")
-	logger.Println(cfg)
 	serviceCloudWatchEvents := cloudwatchevents.NewFromConfig(cfg)
 	serviceECS := ecs.NewFromConfig(cfg)
-	logger.Println("serviceECS")
-	logger.Println(serviceECS)
 	serviceECR := ecr.NewFromConfig(cfg)
-	logger.Println("serviceECR")
-	logger.Println(serviceECR)
 	serviceRDS := rds.NewFromConfig(cfg)
-	logger.Println("serviceRDS")
-	logger.Println(serviceRDS)
 
 	// ===== Limit the variables required =====
 	awsAccountID := v.GetString(awsAccountIDFlag)
-	logger.Println("awsAccountID")
-	logger.Println(awsAccountID)
 	awsRegion := v.GetString(cli.AWSRegionFlag)
-	logger.Println("awsRegion")
-	logger.Println(awsRegion)
 	environmentName := v.GetString(environmentFlag)
-	logger.Println("environmentName")
-	logger.Println(environmentName)
 	serviceName := v.GetString(serviceFlag)
-	logger.Println("serviceName")
-	logger.Println(serviceName)
 	imageURI := v.GetString(imageURIFlag)
-	logger.Println("imageURI")
-	logger.Println(imageURI)
 	variablesFile := v.GetString(variablesFileFlag)
-	logger.Println("variablesFile")
-	logger.Println(variablesFile)
 
 	// Short service name needed for RDS, CloudWatch Logs, and SSM
 	serviceNameParts := strings.Split(serviceName, "-")
-	logger.Println("serviceNameParts")
-	logger.Println(serviceNameParts)
 	serviceNameShort := serviceNameParts[0]
-	logger.Println("serviceNameShort")
-	logger.Println(serviceNameShort)
 
 	// Confirm the image exists
 	ecrImage, errECRImage := NewECRImage(imageURI)
-	logger.Println("ecrImage")
-	logger.Println(ecrImage)
-	logger.Println("errECRImage")
-	logger.Println(errECRImage)
 	if errECRImage != nil {
 		quit(logger, nil, fmt.Errorf("unable to recognize image URI %q: %w", imageURI, errECRImage))
 	}
 
 	errValidateImage := ecrImage.Validate(serviceECR)
-	logger.Println("errValidateImage")
-	logger.Println(errValidateImage)
 	if errValidateImage != nil {
 		quit(logger, nil, fmt.Errorf("unable to validate image %v: %w", ecrImage, errValidateImage))
 	}
 
 	// Entrypoint
 	entryPoint := v.GetString(entryPointFlag)
-	logger.Println("entryPoint")
-	logger.Println(entryPoint)
 	entryPointList := strings.Split(entryPoint, " ")
 	commandName := entryPointList[0]
-	logger.Println("commandName")
-	logger.Println(commandName)
 	subCommandName := entryPointList[1]
-	logger.Println("subCommandName")
-	logger.Println(subCommandName)
 
 	// Register the new task definition
-	logger.Println("trying to register new task def")
-
 	executionRoleArn := fmt.Sprintf("ecs-task-execution-role-%s-%s", serviceName, environmentName)
-	logger.Println("executionRoleArn")
-	logger.Println(executionRoleArn)
-
 	taskRoleArn := fmt.Sprintf("ecs-task-role-%s-%s", serviceName, environmentName)
-	logger.Println("taskRoleArn")
-	logger.Println(taskRoleArn)
 	family := fmt.Sprintf("%s-%s", serviceName, environmentName)
-	logger.Println("family")
-	logger.Println(family)
 
 	// handle entrypoint specific logic
 	var awsLogsStreamPrefix string
-	logger.Println("awsLogsStreamPrefix")
-	logger.Println(awsLogsStreamPrefix)
 	var awsLogsGroup string
-	logger.Println("awsLogsGroup")
-	logger.Println(awsLogsGroup)
 	var portMappings []ecstypes.PortMapping
-	logger.Println("portMappings")
-	logger.Println(portMappings)
 	var containerDefName string
-	logger.Println("containerDefName")
-	logger.Println(containerDefName)
 
 	ctx := context.Background()
 
 	if commandName == binMilMoveTasks {
-		logger.Println("commandName == binMilMoveTasks")
-
 		executionRoleArn = fmt.Sprintf("ecs-task-exec-role-%s-%s-%s", serviceNameShort, environmentName, subCommandName)
-		logger.Println("executionRoleArn")
-		logger.Println(executionRoleArn)
 		taskRoleArn = fmt.Sprintf("ecs-task-role-%s-%s-%s", serviceNameShort, environmentName, subCommandName)
-		logger.Println("taskRoleArn")
-		logger.Println(taskRoleArn)
 		family = fmt.Sprintf("%s-%s-%s", serviceNameShort, environmentName, subCommandName)
-		logger.Println("family")
-		logger.Println(family)
 
 		awsLogsStreamPrefix = serviceName
-		logger.Println("awsLogsStreamPrefix")
-		logger.Println(awsLogsStreamPrefix)
 		awsLogsGroup = fmt.Sprintf("ecs-tasks-%s-%s", serviceNameShort, environmentName)
-		logger.Println("awsLogsGroup")
-		logger.Println(awsLogsGroup)
 		containerDefName = fmt.Sprintf("%s-%s-%s", serviceName, subCommandName, environmentName)
-		logger.Println("containerDefName")
-		logger.Println(containerDefName)
 
 		ruleName := fmt.Sprintf("%s-%s", subCommandName, environmentName)
-		logger.Println("ruleName")
-		logger.Println(ruleName)
 		_, listTargetsByRuleErr := serviceCloudWatchEvents.ListTargetsByRule(
 			ctx,
 			&cloudwatchevents.ListTargetsByRuleInput{
@@ -624,7 +525,6 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 			quit(logger, nil, fmt.Errorf("error retrieving targets for rule %q: %w", ruleName, listTargetsByRuleErr))
 		}
 	} else if subCommandName == "migrate" {
-		logger.Println("subCommandName == migrate")
 		awsLogsStreamPrefix = serviceName
 		awsLogsGroup = fmt.Sprintf("ecs-tasks-%s-%s", serviceNameShort, environmentName)
 		containerDefName = fmt.Sprintf("%s-%s", serviceName, environmentName)
@@ -637,7 +537,6 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 		// This needs to be fixed in terraform and then rolled out
 		taskRoleArn = fmt.Sprintf("ecs-task-role-%s-migration-%s", serviceNameShort, environmentName)
 	} else if commandName == binWebhookClient {
-		logger.Println("commandName == binWebhookClient")
 		awsLogsStreamPrefix = serviceName
 		awsLogsGroup = fmt.Sprintf("ecs-tasks-%s-%s", serviceName, environmentName)
 		containerDefName = fmt.Sprintf("%s-%s", serviceName, environmentName)
@@ -659,59 +558,33 @@ func taskDefFunction(cmd *cobra.Command, args []string) error {
 
 	// Get the database host using the instance identifier
 	dbInstanceIdentifier := fmt.Sprintf("%s-%s", serviceNameShort, environmentName)
-	logger.Println("dbInstanceIdentifier")
-	logger.Println(dbInstanceIdentifier)
-
 	dbInstancesOutput, err := serviceRDS.DescribeDBInstances(
 		ctx,
 		&rds.DescribeDBInstancesInput{
 			DBInstanceIdentifier: aws.String(dbInstanceIdentifier),
 		})
-	logger.Println("dbInstancesOutput")
-	logger.Println(dbInstancesOutput)
 	if err != nil {
-		logger.Println("error retrieving database definition for")
 		quit(logger, nil, fmt.Errorf("error retrieving database definition for %q: %w", dbInstanceIdentifier, err))
 	}
 	dbHost := *dbInstancesOutput.DBInstances[0].Endpoint.Address
-	logger.Println("dbHost")
-	logger.Println(dbHost)
+
 	// CPU / MEM
 	cpu := strconv.Itoa(v.GetInt(cpuFlag))
 	mem := strconv.Itoa(v.GetInt(memFlag))
 
 	// Create the set of secrets and environment variables that will be injected into the
 	// container.
-	logger.Println("creating the set of secrets and environment variables that will be injected into the container")
 	secrets, err := buildSecrets(cfg, awsAccountID, serviceNameShort, environmentName)
-	logger.Println("secrets")
-	logger.Println(secrets)
-
 	if err != nil {
 		quit(logger, nil, err)
 	}
 	containerEnvironment := buildContainerEnvironment(environmentName, dbHost, variablesFile)
-	logger.Println("containerEnvironment")
-	logger.Println(containerEnvironment)
+
 	// AWS does not permit supplying both a secret and an environment variable that share the same
 	// name into an ECS task. In order to gracefully transition between setting values as secrets
 	// into setting them as environment variables, this function serves to remove any duplicates
 	// that have been transitioned into being set as environment variables.
-	secrets = removeSecretsWithMatchingEnvironmentVariables(logger, secrets, containerEnvironment)
-
-	logger.Println("aws.String(containerDefName)")
-	logger.Println(aws.String(containerDefName))
-	logger.Println("aws.String(ecrImage.ImageURI)")
-	logger.Println(aws.String(ecrImage.ImageURI))
-	logger.Println("containerEnvironment)")
-	logger.Println(containerEnvironment)
-
-	logger.Println("awsLogsGroup)")
-	logger.Println(awsLogsGroup)
-	logger.Println("awsRegion)")
-	logger.Println(awsRegion)
-	logger.Println("awsLogsStreamPrefix)")
-	logger.Println(awsLogsStreamPrefix)
+	secrets = removeSecretsWithMatchingEnvironmentVariables(secrets, containerEnvironment)
 
 	containerDefinitions := []ecstypes.ContainerDefinition{
 		{
@@ -1062,7 +935,7 @@ service:
 	return nil
 }
 
-func removeSecretsWithMatchingEnvironmentVariables(logger *log.Logger, secrets []ecstypes.Secret, containerEnvironment []ecstypes.KeyValuePair) []ecstypes.Secret {
+func removeSecretsWithMatchingEnvironmentVariables(secrets []ecstypes.Secret, containerEnvironment []ecstypes.KeyValuePair) []ecstypes.Secret {
 	// Remove any secrets that share a name with an environment variable. Do this by creating a new
 	// slice of secrets that does not any secrets that share a name with an environment variable.
 	newSecrets := []ecstypes.Secret{}
@@ -1076,9 +949,6 @@ func removeSecretsWithMatchingEnvironmentVariables(logger *log.Logger, secrets [
 
 		if conflictFound {
 			// Report any conflicts that are found.
-			logger.Println("found duplicate secret of ")
-			logger.Println(secret)
-
 			fmt.Fprintln(os.Stderr, "Found a secret with the same name as an environment variable. Discarding secret in favor of the environment variable:", *secret.Name)
 		} else {
 			// If no conflict is found, keep the secret.
--- cmd/ecs-deploy/task_def_test.go
+++ cmd/ecs-deploy/task_def_test.go
@@ -1,146 +1,150 @@
 package main
 
 import (
+	"reflect"
 	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
 )
 
 func TestRemoveSecretsWithMatchingEnvironmentVariables(t *testing.T) {
-	// cases := map[string]struct {
-	// 	inSecrets  []ecstypes.Secret
-	// 	inEnvVars  []ecstypes.KeyValuePair
-	// 	expSecrets []ecstypes.Secret
-	// }{
-	// 	"no secrets, no env vars": {
-	// 		inSecrets:  []ecstypes.Secret{},
-	// 		inEnvVars:  []ecstypes.KeyValuePair{},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"one secret, no env vars": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 	},
-	// 	"no secrets, one env var": {
-	// 		inSecrets: []ecstypes.Secret{},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"one secret, one env var, not matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 	},
-	// 	"one secret, one env var, matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"two secrets, one env var, none matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 	},
-	// 	"two secrets, one env var, one matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 	},
-	// 	"one secret, two env vars, none matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 2")},
-	// 			{Name: aws.String("my setting 3")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 	},
-	// 	"one secret, two env vars, one matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"two secrets, two env vars, both matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"two secrets, three env vars, two matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 			{Name: aws.String("my setting 3")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{},
-	// 	},
-	// 	"three secrets, two env vars, two matching": {
-	// 		inSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 			{Name: aws.String("my setting 3")},
-	// 		},
-	// 		inEnvVars: []ecstypes.KeyValuePair{
-	// 			{Name: aws.String("my setting 1")},
-	// 			{Name: aws.String("my setting 2")},
-	// 		},
-	// 		expSecrets: []ecstypes.Secret{
-	// 			{Name: aws.String("my setting 3")},
-	// 		},
-	// 	},
-	// }
+	cases := map[string]struct {
+		inSecrets  []ecstypes.Secret
+		inEnvVars  []ecstypes.KeyValuePair
+		expSecrets []ecstypes.Secret
+	}{
+		"no secrets, no env vars": {
+			inSecrets:  []ecstypes.Secret{},
+			inEnvVars:  []ecstypes.KeyValuePair{},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"one secret, no env vars": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+		},
+		"no secrets, one env var": {
+			inSecrets: []ecstypes.Secret{},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+			},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"one secret, one env var, not matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 2")},
+			},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+		},
+		"one secret, one env var, matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+			},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"two secrets, one env var, none matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting")},
+			},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+		},
+		"two secrets, one env var, one matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+			},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 2")},
+			},
+		},
+		"one secret, two env vars, none matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 2")},
+				{Name: aws.String("my setting 3")},
+			},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+		},
+		"one secret, two env vars, one matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"two secrets, two env vars, both matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"two secrets, three env vars, two matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+				{Name: aws.String("my setting 3")},
+			},
+			expSecrets: []ecstypes.Secret{},
+		},
+		"three secrets, two env vars, two matching": {
+			inSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+				{Name: aws.String("my setting 3")},
+			},
+			inEnvVars: []ecstypes.KeyValuePair{
+				{Name: aws.String("my setting 1")},
+				{Name: aws.String("my setting 2")},
+			},
+			expSecrets: []ecstypes.Secret{
+				{Name: aws.String("my setting 3")},
+			},
+		},
+	}
 
-	// for name, tc := range cases {
-	// 	actual := removeSecretsWithMatchingEnvironmentVariables(tc.inSecrets, tc.inEnvVars)
-	// 	if !reflect.DeepEqual(actual, tc.expSecrets) {
-	// 		t.Errorf("%v: expected %v, but got %v", name, tc.expSecrets, actual)
-	// 	}
-	// }
+	for name, tc := range cases {
+		actual := removeSecretsWithMatchingEnvironmentVariables(tc.inSecrets, tc.inEnvVars)
+		if !reflect.DeepEqual(actual, tc.expSecrets) {
+			t.Errorf("%v: expected %v, but got %v", name, tc.expSecrets, actual)
+		}
+	}
 }
