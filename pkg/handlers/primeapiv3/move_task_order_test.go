e700d91582b7fd5aabde0fdc83981502a3b4109a
Alex Lusk <alex.lusk@caci.com>
2025-01-24 19:43:12 +0000

    update prime rate area finder mocks

--- pkg/handlers/primeapiv3/move_task_order_test.go
+++ pkg/handlers/primeapiv3/move_task_order_test.go
@@ -44,7 +44,7 @@ func (suite *HandlerSuite) TestGetMoveTaskOrder() {
 
 	setupDefaultTestHandler := func() GetMoveTaskOrderHandler {
 		mockShipmentRateAreaFinder := &mocks.ShipmentRateAreaFinder{}
-		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentOconusRateArea",
+		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentRateAreas",
 			mock.AnythingOfType("*appcontext.appContext"),
 			mock.AnythingOfType("models.Move"),
 		).Return(nil, nil)
@@ -1446,7 +1446,7 @@ func (suite *HandlerSuite) TestGetMoveTaskOrder() {
 			},
 		}
 
-		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentOconusRateArea",
+		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentRateAreas",
 			mock.AnythingOfType("*appcontext.appContext"),
 			mock.AnythingOfType("models.Move"),
 		).Return(&shipmentPostalCodeRateArea, nil)
@@ -1525,7 +1525,7 @@ func (suite *HandlerSuite) TestGetMoveTaskOrder() {
 
 		defaultAddress := factory.BuildAddress(suite.DB(), nil, nil)
 
-		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentOconusRateArea",
+		mockShipmentRateAreaFinder.On("GetPrimeMoveShipmentRateAreas",
 			mock.AnythingOfType("*appcontext.appContext"),
 			mock.AnythingOfType("models.Move"),
 		).Return(nil, apperror.InternalServerError{})
