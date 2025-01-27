75850822706b8e3c1fc5c3ea101705308d067aba
deandreJones <deandre.Jones@caci.com>
2025-01-27 16:53:02 +0000

    test fix

--- src/components/Office/DefinitionLists/AllowancesList.test.jsx
+++ src/components/Office/DefinitionLists/AllowancesList.test.jsx
@@ -179,6 +179,6 @@ describe('AllowancesList', () => {
   it('renders weight restriction', () => {
     const adminRestrictedWtLoc = { ...info, adminRestrictedWeightLocation: true };
     render(<AllowancesList info={adminRestrictedWtLoc} />);
-    expect(screen.getByTestId('weightRestriction').textContent).toEqual('1,000 lbs');
+    expect(screen.getByTestId('weightRestriction').textContent).toEqual('1,500 lbs');
   });
 });
