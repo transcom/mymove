8b6b2c2dee38239b369def41ae6a8448311cf7ce
Michael Inthavongsay <m.inthavongsay@caci.com>
2025-01-28 20:00:39 +0000

    add more tests

--- pkg/services/sit_entry_date_update/sit_entry_date_updater_test.go
+++ pkg/services/sit_entry_date_update/sit_entry_date_updater_test.go
@@ -48,6 +48,44 @@ func (suite *UpdateSitEntryDateServiceSuite) TestUpdateSitEntryDate() {
 		return ddfServiceItem, ddaServiceItem
 	}
 
+	setupInternationalModels := func() (models.MTOServiceItem, models.MTOServiceItem) {
+		move := factory.BuildMove(suite.DB(), nil, nil)
+		shipment := factory.BuildMTOShipment(suite.DB(), []factory.Customization{
+			{
+				Model:    move,
+				LinkOnly: true,
+			},
+			{
+				Model: models.MTOShipment{
+					MarketCode: models.MarketCodeInternational,
+				},
+			},
+		}, nil)
+		idfServiceItem := factory.BuildMTOServiceItem(suite.DB(), []factory.Customization{
+			{
+				Model:    shipment,
+				LinkOnly: true,
+			},
+			{
+				Model: models.ReService{
+					Code: models.ReServiceCodeIDFSIT,
+				},
+			},
+		}, nil)
+		idaServiceItem := factory.BuildMTOServiceItem(suite.DB(), []factory.Customization{
+			{
+				Model:    shipment,
+				LinkOnly: true,
+			},
+			{
+				Model: models.ReService{
+					Code: models.ReServiceCodeIDASIT,
+				},
+			},
+		}, nil)
+		return idfServiceItem, idaServiceItem
+	}
+
 	// Test not found error
 	suite.Run("Not Found Error", func() {
 		ddfServiceItem, _ := setupModels()
@@ -66,9 +104,47 @@ func (suite *UpdateSitEntryDateServiceSuite) TestUpdateSitEntryDate() {
 		suite.IsType(apperror.NotFoundError{}, err)
 	})
 
+	suite.Run("Not Found Error - international", func() {
+		ddfServiceItem, _ := setupInternationalModels()
+		notFoundServiceItem := models.SITEntryDateUpdate{
+			ID:           ddfServiceItem.ID,
+			SITEntryDate: ddfServiceItem.SITEntryDate,
+		}
+		notFoundUUID, err := uuid.NewV4()
+		suite.NoError(err)
+		notFoundServiceItem.ID = notFoundUUID
+
+		updatedServiceItem, err := updater.UpdateSitEntryDate(suite.AppContextForTest(), &notFoundServiceItem)
+
+		suite.Nil(updatedServiceItem)
+		suite.Error(err)
+		suite.IsType(apperror.NotFoundError{}, err)
+	})
+
 	// Test successful update of both service items
 	suite.Run("Successful update of service items", func() {
 		ddfServiceItem, ddaServiceItem := setupModels()
+		updatedServiceItem := models.SITEntryDateUpdate{
+			ID:           ddfServiceItem.ID,
+			SITEntryDate: ddfServiceItem.SITEntryDate,
+		}
+		newSitEntryDate := time.Date(2020, time.December, 02, 0, 0, 0, 0, time.UTC)
+		newSitEntryDateNextDay := newSitEntryDate.Add(24 * time.Hour)
+
+		updatedServiceItem.SITEntryDate = &newSitEntryDate
+		ddaServiceItem.SITEntryDate = &newSitEntryDateNextDay
+
+		changedServiceItem, err := updater.UpdateSitEntryDate(suite.AppContextForTest(), &updatedServiceItem)
+
+		suite.NoError(err)
+		suite.NotNil(updatedServiceItem)
+		suite.Equal(ddfServiceItem.ID, updatedServiceItem.ID)
+		suite.Equal(updatedServiceItem.SITEntryDate.Local(), changedServiceItem.SITEntryDate.Local())
+		suite.Equal(ddaServiceItem.SITEntryDate.Local(), newSitEntryDateNextDay.Local())
+	})
+
+	suite.Run("Successful update of service items - international", func() {
+		ddfServiceItem, ddaServiceItem := setupInternationalModels()
 		updatedServiceItem := models.SITEntryDateUpdate{
 			ID:           ddfServiceItem.ID,
 			SITEntryDate: ddfServiceItem.SITEntryDate,
