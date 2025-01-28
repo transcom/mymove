3d188de8e25d8c43b5203b76a6233545a8f07337
KonstanceH <Konstance.Haffaney@caci.com>
2025-01-28 21:08:03 +0000

    pause on frontend

---
+++ src/pages/Admin/RejectedOfficeUsers/RejectedOfficeUserList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Datagrid, DateField, Filter, List, ReferenceField, TextField, TextInput, TopToolbar } from 'react-admin';
+
+import AdminPagination from 'scenes/SystemAdmin/shared/AdminPagination';
+
+// Overriding the default toolbar
+const ListActions = () => {
+  return <TopToolbar />;
+};
+
+const RejectedOfficeUserListFilter = () => (
+  <Filter>
+    <TextInput source="search" alwaysOn />
+  </Filter>
+);
+
+const defaultSort = { field: 'createdAt', order: 'DESC' };
+
+const RejectedOfficeUserList = () => (
+  <List
+    pagination={<AdminPagination />}
+    perPage={25}
+    sort={defaultSort}
+    filters={<RejectedOfficeUserListFilter />}
+    actions={<ListActions />}
+  >
+    <Datagrid bulkActionButtons={false} rowClick="show" data-testid="rejected-office-user-fields">
+      <TextField source="id" />
+      <TextField source="email" />
+      <TextField source="firstName" />
+      <TextField source="lastName" />
+      <ReferenceField label="Transportation Office" source="transportationOfficeId" reference="offices" link={false}>
+        <TextField source="name" />
+      </ReferenceField>
+      <TextField source="status" />
+      <DateField showTime source="rejectedOn" label="Rejected date" />
+      <TextField source="rejectionReason" label="Reason for rejection" />
+      <TextField source="roles[0].roleName" label="Roles requested" />
+    </Datagrid>
+  </List>
+);
+
+export default RejectedOfficeUserList;
