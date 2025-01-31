488ea84284b5620f534659839106caddd222d5da
joeydoyecaci <joseph.doye@caci.com>
2025-01-31 17:16:23 +0000

    Removed Comment

--- pkg/route/hhg_planner.go
+++ pkg/route/hhg_planner.go
@@ -58,7 +58,6 @@ func (p *hhgPlanner) ZipTransitDistance(appCtx appcontext.AppContext, source str
 	sourceZip3 := sourceZip5[0:3]
 	destZip3 := destZip5[0:3]
 
-	// we want to use DTOD for international shipments & same ZIPs for accuracy
 	if sourceZip3 == destZip3 || isInternationalShipment {
 		if sourceZip5 == destZip5 {
 			return 1, nil
