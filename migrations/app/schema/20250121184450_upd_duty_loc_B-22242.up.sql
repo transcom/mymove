f1ca3ef99aff44bab33a7d2dff70872e26e4cfdf
Beth Grohmann <beth.grohmann@caci.com>
2025-01-31 09:00:00 -0600

    Update 20250121184450_upd_duty_loc_B-22242.up.sql

--- migrations/app/schema/20250121184450_upd_duty_loc_B-22242.up.sql
+++ migrations/app/schema/20250121184450_upd_duty_loc_B-22242.up.sql
@@ -32,12 +32,45 @@ END $$;
 DO $$
 BEGIN
 
-	--associate duty loc Yuma, AZ 85365 to transportation office PPPO DMO MCAS Yuma - USMC
-	update duty_locations set transportation_office_id = '6ac7e595-1e0c-44cb-a9a4-cd7205868ed4' where id = '9e94208a-881d-47bc-82c0-4f375471751e';
-	
 	--remove duty loc Albuquerque, NM 87103
-	update orders set new_duty_location_id = '54acfb0e-222b-49eb-b94b-ccb00c6f529c' where new_duty_location_id = '2cc57072-19fa-438b-a44b-e349dff11763';
+	IF EXISTS (SELECT 1 FROM duty_locations WHERE id = '2cc57072-19fa-438b-a44b-e349dff11763') THEN
 	
-	delete from duty_locations where id = '2cc57072-19fa-438b-a44b-e349dff11763';
+		update orders set new_duty_location_id = '54acfb0e-222b-49eb-b94b-ccb00c6f529c' where new_duty_location_id = '2cc57072-19fa-438b-a44b-e349dff11763';
+	
+		delete from duty_locations where id = '2cc57072-19fa-438b-a44b-e349dff11763';
 
+	END IF;
+
+END $$;
+
+DO $$
+BEGIN
+	
+	--remove duty loc August, GA 30917
+	IF EXISTS (SELECT 1 FROM duty_locations WHERE id = '109ac405-47fb-4e1e-9efb-58290453ac09') THEN
+	
+		update orders set origin_duty_location_id = '595363c2-14ee-48e0-b318-e76ab0016453' where origin_duty_location_id = '109ac405-47fb-4e1e-9efb-58290453ac09';
+		update orders set new_duty_location_id = '595363c2-14ee-48e0-b318-e76ab0016453' where new_duty_location_id = '109ac405-47fb-4e1e-9efb-58290453ac09';
+		
+		delete from duty_locations where id = '109ac405-47fb-4e1e-9efb-58290453ac09';
+	
+	END IF;
+
+END $$;
+
+--associate duty loc Yuma, AZ 85365 to transportation office PPPO DMO MCAS Yuma - USMC
+update duty_locations set transportation_office_id = '6ac7e595-1e0c-44cb-a9a4-cd7205868ed4' where id = '9e94208a-881d-47bc-82c0-4f375471751e';
+
+--add Joint Base Lews McChord, WA 98438 duty location
+DO $$
+BEGIN
+	
+	IF NOT EXISTS (SELECT 1 FROM duty_locations WHERE id = '109ac405-47fb-4e1e-9efb-58290453ac09') THEN
+	
+		INSERT INTO public.duty_locations
+		(id, "name", affiliation, address_id, created_at, updated_at, transportation_office_id, provides_services_counseling)
+		VALUES('38fc6718-b80f-4761-a077-cfa62e414e27', 'Joint Base Lewis McChord', 'AIR_FORCE', '23d3140b-1ba2-400f-9d57-317034673c06'::uuid, now(), now(), '95abaeaa-452f-4fe0-9264-960cd2a15ccd', true);
+		
+	END IF;
+	
 END $$;
