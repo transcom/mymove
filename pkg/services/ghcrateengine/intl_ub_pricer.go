fd6c4b6503c6149d5886ab0e61fb73f9bbace852
Elizabeth Perkins <elizabeth.perkins@caci.com>
2025-02-20 20:38:56 +0000

    Adds check for weight

--- pkg/services/ghcrateengine/intl_ub_pricer.go
+++ pkg/services/ghcrateengine/intl_ub_pricer.go
@@ -13,8 +13,6 @@ import (
 	"github.com/transcom/mymove/pkg/unit"
 )
 
-const isUBPricerMinimumWeight = unit.Pound(300)
-
 type intlUBPricer struct {
 }
 
@@ -29,8 +27,8 @@ func (p intlUBPricer) Price(appCtx appcontext.AppContext, contractCode string, r
 	if referenceDate.IsZero() {
 		return 0, nil, errors.New("referenceDate is required")
 	}
-	if weight < isUBPricerMinimumWeight {
-		return 0, nil, fmt.Errorf("Weight must be at least %d pounds", isUBPricerMinimumWeight)
+	if weight < minIntlWeightUB {
+		return 0, nil, fmt.Errorf("weight must be at least %d pounds", minIntlWeightUB)
 	}
 	if perUnitCents == 0 {
 		return 0, nil, errors.New("PerUnitCents is required")
