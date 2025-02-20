c2ffc8c10f8cabff3b517501153d76fdbe7ff6f9
Daniel Jordan <daniel.jordan@caci.com>
2025-02-20 22:21:43 +0000

    fixing envrc, 508 improvements, storybook things

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -5,9 +5,16 @@ import { Oval } from 'react-loader-spinner';
 import styles from './LoadingSpinner.module.scss';
 
 const LoadingSpinner = ({ message }) => (
-  <div className={styles.container} data-testid="loading-spinner">
+  <div className={styles.container} data-testid="loading-spinner" role="status" aria-live="polite">
     <div className={styles.spinnerWrapper}>
-      <Oval visible height="150" width="150" color="#ffbe2e" secondaryColor="#565c65" ariaLabel="oval-loading" />
+      <Oval
+        visible
+        height="150"
+        width="150"
+        color="#ffbe2e"
+        secondaryColor="#565c65"
+        ariaLabel="Loading content, please wait"
+      />
       <p className={styles.message}>{message || 'Loading, please wait...'}</p>
     </div>
   </div>
