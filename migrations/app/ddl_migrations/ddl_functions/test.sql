288f207963e307cedeac86d8ceca71260dee611d
deandreJones <deandre.Jones@caci.com>
2025-02-21 10:13:37 -0600

    nope

--- migrations/app/ddl_migrations/ddl_functions/test.sql
+++
@@ -1,8 +0,0 @@
-UPDATE moves m
-SET m.cancel_reason = 'Terminated for Cause (TCN-0001)',
-    mto.status  = 'CANCELED'
-FROM mto_shipments mto
-WHERE m.id = mto.move_id
-and moves.locator in (
-
-);
