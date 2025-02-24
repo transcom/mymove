b6322f5e7f7146a8e4579a77949b8adfc6322c76
Daniel Jordan <daniel.jordan@caci.com>
2025-02-24 18:26:44 +0000

    adding functions and tests back in due to code on INT but not MAIN using it

--- pkg/services/transportation_office/transportation_office_fetcher_test.go
+++ pkg/services/transportation_office/transportation_office_fetcher_test.go
@@ -1,14 +1,17 @@
 package transportationoffice
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/transcom/mymove/pkg/auth"
 	"github.com/transcom/mymove/pkg/factory"
 	"github.com/transcom/mymove/pkg/models"
 	"github.com/transcom/mymove/pkg/services"
+	"github.com/transcom/mymove/pkg/testdatagen"
 	"github.com/transcom/mymove/pkg/testingsuite"
 )
 
@@ -189,6 +192,210 @@ func (suite *TransportationOfficeServiceSuite) Test_FindCounselingOffices() {
 	offices, err := suite.toFetcher.GetCounselingOffices(suite.AppContextForTest(), origDutyLocation.ID, serviceMember.ID)
 	suite.NoError(err)
 	suite.Len(*offices, 2)
+}
+
+func (suite *TransportationOfficeServiceSuite) Test_Oconus_AK_FindCounselingOffices() {
+	setupServiceMember := func(serviceMemberAffiliation models.ServiceMemberAffiliation) models.ServiceMember {
+		customServiceMember := models.ServiceMember{
+			FirstName:          models.StringPointer("Gregory"),
+			LastName:           models.StringPointer("Van der Heide"),
+			Telephone:          models.StringPointer("999-999-9999"),
+			SecondaryTelephone: models.StringPointer("123-555-9999"),
+			PersonalEmail:      models.StringPointer("peyton@example.com"),
+			Edipi:              models.StringPointer("1000011111"),
+			Affiliation:        &serviceMemberAffiliation,
+			Suffix:             models.StringPointer("Random suffix string"),
+			PhoneIsPreferred:   models.BoolPointer(false),
+			EmailIsPreferred:   models.BoolPointer(false),
+		}
+
+		customAddress := models.Address{
+			StreetAddress1: "987 Another Street",
+		}
+
+		customUser := models.User{
+			OktaEmail: "test_email@email.com",
+		}
+
+		serviceMember := factory.BuildServiceMember(suite.DB(), []factory.Customization{
+			{Model: customServiceMember},
+			{Model: customAddress},
+			{Model: customUser},
+		}, nil)
+
+		return serviceMember
+	}
+
+	setupDataForOconusSearchCounselingOffice := func(postalCode string, gbloc string) (models.ReRateArea, models.OconusRateArea, models.UsPostRegionCity, models.DutyLocation) {
+		contract := testdatagen.FetchOrMakeReContract(suite.DB(), testdatagen.Assertions{})
+
+		rateAreaCode := uuid.Must(uuid.NewV4()).String()[0:5]
+		rateArea := testdatagen.FetchOrMakeReRateArea(suite.DB(), testdatagen.Assertions{
+			ReRateArea: models.ReRateArea{
+				ContractID: contract.ID,
+				IsOconus:   true,
+				Name:       fmt.Sprintf("Alaska-%s", rateAreaCode),
+				Contract:   contract,
+			},
+		})
+		suite.NotNil(rateArea)
+
+		us_country, err := models.FetchCountryByCode(suite.DB(), "US")
+		suite.NotNil(us_country)
+		suite.Nil(err)
+
+		usprc, err := models.FindByZipCode(suite.AppContextForTest().DB(), postalCode)
+		suite.NotNil(usprc)
+		suite.FatalNoError(err)
+
+		oconusRateArea, err := models.FetchOconusRateAreaByCityId(suite.DB(), usprc.ID.String())
+		suite.NotNil(oconusRateArea)
+		suite.Nil(err)
+
+		address := factory.BuildAddress(suite.DB(), []factory.Customization{
+			{
+				Model: models.Address{
+					IsOconus:           models.BoolPointer(true),
+					UsPostRegionCityID: &usprc.ID,
+				},
+			},
+		}, nil)
+
+		origDutyLocation := factory.BuildDutyLocation(suite.DB(), []factory.Customization{
+			{
+				Model: models.DutyLocation{
+					AddressID:                  address.ID,
+					ProvidesServicesCounseling: true,
+				},
+			},
+			{
+				Model: models.TransportationOffice{
+					Name:             "TEST - PPO",
+					Gbloc:            gbloc,
+					ProvidesCloseout: true,
+				},
+			},
+		}, nil)
+		suite.MustSave(&origDutyLocation)
+
+		found_duty_location, _ := models.FetchDutyLocation(suite.DB(), origDutyLocation.ID)
+
+		return rateArea, *oconusRateArea, *usprc, found_duty_location
+	}
+
+	suite.Run("success - findOconusGblocDepartmentIndicator - returns default GBLOC for departmentAffiliation if no specific departmentAffilation mapping is defined", func() {
+		const fairbanksAlaskaPostalCode = "99790"
+		_, oconusRateArea, _, dutylocation := setupDataForOconusSearchCounselingOffice(fairbanksAlaskaPostalCode, "JEAT")
+
+		// setup department affiliation to GBLOC mappings
+		jppsoRegion, err := models.FetchJppsoRegionByCode(suite.DB(), "JEAT")
+		suite.NotNil(jppsoRegion)
+		suite.Nil(err)
+
+		gblocAors, err := models.FetchGblocAorsByJppsoCodeRateAreaDept(suite.DB(), jppsoRegion.ID, oconusRateArea.ID, models.DepartmentIndicatorARMY.String())
+		suite.NotNil(gblocAors)
+		suite.Nil(err)
+
+		serviceMember := setupServiceMember(models.AffiliationARMY)
+		appCtx := suite.AppContextWithSessionForTest(&auth.Session{
+			ServiceMemberID: serviceMember.ID,
+		})
+		departmentIndictor, err := findOconusGblocDepartmentIndicator(appCtx, dutylocation, serviceMember.ID)
+		suite.NotNil(departmentIndictor)
+		suite.Nil(err)
+		suite.Nil(departmentIndictor.DepartmentIndicator)
+		suite.Equal("JEAT", departmentIndictor.Gbloc)
+	})
+
+	suite.Run("success - findOconusGblocDepartmentIndicator - returns specific GBLOC for departmentAffiliation when a specific departmentAffilation mapping is defined -- simulate Zone 2 scenerio", func() {
+		const fairbanksAlaskaPostalCode = "99790"
+		_, oconusRateArea, _, dutylocation := setupDataForOconusSearchCounselingOffice(fairbanksAlaskaPostalCode, "MBFL")
+
+		// setup department affiliation to GBLOC mappings
+		jppsoRegion, err := models.FetchJppsoRegionByCode(suite.DB(), "MBFL")
+		suite.NotNil(jppsoRegion)
+		suite.Nil(err)
+
+		gblocAors, err := models.FetchGblocAorsByJppsoCodeRateAreaDept(suite.DB(), jppsoRegion.ID, oconusRateArea.ID, models.DepartmentIndicatorAIRANDSPACEFORCE.String())
+		suite.NotNil(gblocAors)
+		suite.Nil(err)
+
+		// loop through and make sure all branches are using it's own dedicated GBLOC and not default
+		serviceMember := setupServiceMember(models.AffiliationAIRFORCE)
+		appCtx := suite.AppContextWithSessionForTest(&auth.Session{
+			ServiceMemberID: serviceMember.ID,
+		})
+		suite.Nil(err)
+		departmentIndictor, err := findOconusGblocDepartmentIndicator(appCtx, dutylocation, appCtx.Session().ServiceMemberID)
+		suite.NotNil(departmentIndictor)
+		suite.Nil(err)
+		suite.NotNil(departmentIndictor.DepartmentIndicator)
+		suite.Equal(models.DepartmentIndicatorAIRANDSPACEFORCE.String(), *departmentIndictor.DepartmentIndicator)
+		suite.Equal("MBFL", departmentIndictor.Gbloc)
+	})
+
+	suite.Run("failure - findOconusGblocDepartmentIndicator - returns error when find service member ID fails", func() {
+		_, _, _, dutylocation := setupDataForOconusSearchCounselingOffice("99714", "JEAT")
+
+		appCtx := suite.AppContextWithSessionForTest(&auth.Session{
+			// create fake service member ID to raise NOT found error
+			ServiceMemberID: uuid.Must(uuid.NewV4()),
+		})
+
+		departmentIndictor, err := findOconusGblocDepartmentIndicator(appCtx, dutylocation, appCtx.Session().ServiceMemberID)
+		suite.Nil(departmentIndictor)
+		suite.NotNil(err)
+	})
+
+	suite.Run("failure - not found duty location returns error", func() {
+		appCtx := suite.AppContextWithSessionForTest(&auth.Session{
+			ServiceMemberID: uuid.Must(uuid.NewV4()),
+		})
+		unknown_duty_location_id := uuid.Must(uuid.NewV4())
+		offices, err := findCounselingOffice(appCtx, unknown_duty_location_id, appCtx.Session().ServiceMemberID)
+		suite.Nil(offices)
+		suite.NotNil(err)
+	})
+
+	suite.Run("success - offices using default departmentIndicator mapping", func() {
+		_, oconusRateArea, _, dutylocation := setupDataForOconusSearchCounselingOffice("99619", "MAPS")
+
+		// setup department affiliation to GBLOC mappings
+		jppsoRegion, err := models.FetchJppsoRegionByCode(suite.DB(), "MAPS")
+		suite.NotNil(jppsoRegion)
+		suite.Nil(err)
+
+		gblocAors, err := models.FetchGblocAorsByJppsoCodeRateAreaDept(suite.DB(), jppsoRegion.ID, oconusRateArea.ID, models.DepartmentIndicatorARMY.String())
+		suite.NotNil(gblocAors)
+		suite.Nil(err)
+
+		serviceMember := setupServiceMember(models.AffiliationAIRFORCE)
+		appCtx := suite.AppContextWithSessionForTest(&auth.Session{
+			ServiceMemberID: serviceMember.ID,
+		})
+
+		suite.Nil(err)
+		offices, err := findCounselingOffice(appCtx, dutylocation.ID, appCtx.Session().ServiceMemberID)
+		suite.NotNil(offices)
+		suite.Nil(err)
+		suite.Equal(1, len(offices))
+		suite.Equal("TEST - PPO", offices[0].Name)
+
+		// add another transportation office
+		factory.BuildTransportationOffice(suite.DB(), []factory.Customization{
+			{
+				Model: models.TransportationOffice{
+					Name:             "TEST - PPO2",
+					ProvidesCloseout: true,
+					Gbloc:            "MAPS",
+				},
+			},
+		}, nil)
+		offices, err = findCounselingOffice(appCtx, dutylocation.ID, appCtx.Session().ServiceMemberID)
+		suite.NotNil(offices)
+		suite.Nil(err)
+		suite.Equal(2, len(offices))
+	})
 }
 
 func (suite *TransportationOfficeServiceSuite) Test_GetTransportationOffice() {
