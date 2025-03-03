532047fe6f70f7d046c03769453840b6fd54ec25
Paul Stonebraker <paul.stonebraker@caci.com>
2025-03-03 05:15:03 +0000

    UPDATE move_fetcher SET order to be better WHERE i wrote the code

--- pkg/services/move/move_fetcher.go
+++ pkg/services/move/move_fetcher.go
@@ -66,16 +66,10 @@ func (f moveFetcher) FetchMove(appCtx appcontext.AppContext, locator string, sea
 func (f moveFetcher) FetchMovesByIdArray(appCtx appcontext.AppContext, moveIds []ghcmessages.BulkAssignmentMoveData) (models.Moves, error) {
 	moves := models.Moves{}
 
-	caseExpr := "CASE "
-	for i, moveId := range moveIds {
-		caseExpr += "WHEN id = '" + fmt.Sprintf("%v", moveId) + "' THEN " + fmt.Sprintf("%d", i) + " "
-	}
-	caseExpr += "ELSE " + fmt.Sprintf("%d", len(moveIds)) + " END"
-
 	query := appCtx.DB().Q().EagerPreload("Orders").
 		Where("show = TRUE").
 		Where("id in (?)", moveIds).
-		Order(caseExpr)
+		Order("ARRAY_POSITION(ARRAY[?]::uuid[], id)", moveIds)
 	err := query.All(&moves)
 
 	if err != nil {
