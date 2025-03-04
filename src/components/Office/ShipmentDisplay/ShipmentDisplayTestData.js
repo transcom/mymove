a85f92d2d35e7a142252d3d50e449c5221bdafc0
Daniel Jordan <daniel.jordan@caci.com>
2025-03-04 21:15:26 +0000

    updating storybooks, updating SSW, added test and logic when SC updates orders to update ppm type of submitted PPMs

--- src/components/Office/ShipmentDisplay/ShipmentDisplayTestData.js
+++ src/components/Office/ShipmentDisplay/ShipmentDisplayTestData.js
@@ -171,6 +171,7 @@ export const ppmInfo = {
 export const ppmInfoApprovedOrExcluded = {
   heading: 'PPM',
   ppmShipment: {
+    ppmType: PPM_TYPES.INCENTIVE_BASED,
     actualMoveDate: null,
     advanceAmountRequested: 598700,
     hasRequestedAdvance: true,
@@ -230,6 +231,7 @@ export const ppmInfoApprovedOrExcluded = {
 export const ppmInfoRejected = {
   heading: 'PPM',
   ppmShipment: {
+    ppmType: PPM_TYPES.INCENTIVE_BASED,
     actualMoveDate: null,
     advanceAmountRequested: 598700,
     hasRequestedAdvance: true,
@@ -255,6 +257,7 @@ export const ppmInfoRejected = {
 export const ppmInfoMultiple = {
   heading: 'PPM 1',
   ppmShipment: {
+    ppmType: PPM_TYPES.INCENTIVE_BASED,
     actualMoveDate: null,
     advanceAmountRequested: 598700,
     hasRequestedAdvance: true,
@@ -280,6 +283,7 @@ export const ppmInfoMultiple = {
 export const ppmInfoMultiple2 = {
   heading: 'PPM 2',
   ppmShipment: {
+    ppmType: PPM_TYPES.INCENTIVE_BASED,
     actualMoveDate: null,
     advanceAmountRequested: 4300,
     hasRequestedAdvance: true,
