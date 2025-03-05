ec6f37cce4f5b2da4e1a763a2b49b5e522e057d9
ryan-mchugh <ryan.mchugh@caci.com>
2025-03-05 22:54:33 +0000

    B-22483 - added sql files, need fix on dml

---
+++ migrations/app/schema/20250305204911_B-22483_init_mappings_on_roles_privileges.up.sql
@@ -0,0 +1,14 @@
+-- B-22483 - add initial mappings to roles_privileges table
+
+--insert applicable roles for supervisor privilege
+INSERT INTO roles_privileges (id, role_id, privilege_id, created_at, updated_at) VALUES
+    ('c261ca4c-185b-4a69-adfb-a8cce625d93e'::uuid,'92f779a7-8e77-42b0-8002-514304d63b9c'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()); -- Customer
+    -- ('e20c1e39-fea8-43ef-a56c-7e276bb33c87'::uuid,'1538be88-1351-4968-9ec1-7b4dc79c0d96'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Task Ordering Officer
+    -- ('2d32e971-1350-46e9-931c-1a53b2e3af3b'::uuid,'2032e756-7860-40d2-a8df-c0937136ae55'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Task Invoicing Officer
+    -- ('66eaee36-311c-4184-aeb9-178b1d407c0a'::uuid,'64af860c-261c-458b-b79f-5e7d37328e4e'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Contracting Officer
+    -- ('afe69e74-0abd-4ccf-9a15-e727fe18c746'::uuid,'010bdae1-8ebe-44c9-b8ee-8c4477fae2a6'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Services Counselor
+    -- ('14665c1a-888f-4969-9dd8-21077b41f401'::uuid,'63c07db0-5a7d-499c-ab64-90c08f74f654'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Prime Simulator
+    -- ('f33326bf-e56d-4bb8-999b-5961662be70b'::uuid,'a2af3cc0-d0cd-4a29-8092-70ad45723090'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Quality Assurance Evaluator
+    -- ('e62984fc-03d8-496e-8ba1-e4c979120f4a'::uuid,'72432922-bf2e-45de-8837-1a458f5d1011'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Customer Service Representative
+    -- ('e0d49f7a-96a8-4bde-874c-83bb322558f1'::uuid,'20d7deea-4010-424e-9f64-714a46e18c3c'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()), -- Government Surveillance Representative
+    -- ('1ed7973d-c022-48fa-9534-12278cb9d98c'::uuid,'0da36914-fcc1-4965-b49c-b4a0d447514c'::uuid,'463c2034-d197-4d9a-897e-8bbe64893a31'::uuid, now(), now()); -- Headquarters
