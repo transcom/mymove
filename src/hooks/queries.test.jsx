ffe865c1dc19bb1518d8474b9ecdebd5a0036933
Tae Jung <tae.jung@caci.com>
2025-03-05 15:49:39 +0000

    init changes

--- src/hooks/queries.test.jsx
+++ src/hooks/queries.test.jsx
@@ -18,6 +18,7 @@ import {
   useServicesCounselingQueuePPMQueries,
   useReviewShipmentWeightsQuery,
   useBulkAssignmentQueries,
+  usePPMShipmentAndDocsOnlyQueries,
 } from './queries';
 
 import { serviceItemCodes } from 'content/serviceItems';
@@ -1028,6 +1029,34 @@ describe('useServicesCounselingQueuePPMQueries', () => {
       isLoading: false,
       isError: false,
       isSuccess: true,
+    });
+  });
+});
+
+describe('usePPMShipmentAndDocsOnlyQueries', () => {
+  it('loads data', async () => {
+    const { result, waitFor } = renderHook(() => usePPMShipmentAndDocsOnlyQueries('1234'), { wrapper });
+
+    await waitFor(() => result.current.isSuccess);
+
+    expect(result.current).toEqual({
+      documents: {
+        MovingExpenses: [],
+        ProGearWeightTickets: [],
+        WeightTickets: [],
+      },
+      mtoShipment: {
+        customerRemarks: 'mock remarks',
+        id: '12345',
+        moveTaskOrderId: '67890',
+        requestedDeliveryDate: '2020-03-30',
+        requestedPickupDate: '2020-03-01',
+      },
+      isLoading: false,
+      isError: false,
+      isFetching: false,
+      isSuccess: true,
+      refetchMTOShipment: result.current.refetchMTOShipment,
     });
   });
 });
--- src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
+++ src/pages/Office/ServicesCounselingMoveDetails/ServicesCounselingMoveDetails.jsx
@@ -36,6 +36,7 @@ import {
   FEATURE_FLAG_KEYS,
   technicalHelpDeskURL,
 } from 'shared/constants';
+import { isPPMAboutInfoComplete } from 'utils/shipments';
 import { ppmShipmentStatuses, shipmentStatuses } from 'constants/shipments';
 import shipmentCardsStyles from 'styles/shipmentCards.module.scss';
 import LeftNav from 'components/LeftNav/LeftNav';
@@ -204,7 +205,7 @@ const ServicesCounselingMoveDetails = ({
 
   if (mtoShipments) {
     const submittedShipments = mtoShipments?.filter((shipment) => !shipment.deletedAt);
-    const submittedShipmentsNonPPM = submittedShipments.filter(
+    const submittedShipmentsNonPPMNeedsCloseout = submittedShipments.filter(
       (shipment) => shipment.ppmShipment?.status !== ppmShipmentStatuses.NEEDS_CLOSEOUT,
     );
     const ppmNeedsApprovalShipments = submittedShipments.filter(
@@ -289,21 +290,44 @@ const ServicesCounselingMoveDetails = ({
     counselorCanEditNonPPM =
       move.status === MOVE_STATUSES.NEEDS_SERVICE_COUNSELING && shipmentsInfo.shipmentType !== 'PPM';
 
-    shipmentsInfo = submittedShipmentsNonPPM.map((shipment) => {
+    shipmentsInfo = submittedShipmentsNonPPMNeedsCloseout.map((shipment) => {
       const editURL =
         counselorCanEdit || counselorCanEditNonPPM
           ? `../${generatePath(servicesCounselingRoutes.SHIPMENT_EDIT_PATH, {
               shipmentId: shipment.id,
             })}`
           : '';
-      const viewURL = // Read only view of approved documents
-        shipment?.ppmShipment?.status === ppmShipmentStatuses.CLOSEOUT_COMPLETE ||
-        (shipment?.ppmShipment?.weightTickets && shipment?.ppmShipment?.weightTickets[0]?.status)
-          ? `../${generatePath(servicesCounselingRoutes.SHIPMENT_VIEW_DOCUMENT_PATH, {
+      let viewURL = '';
+      let completePpmForCustomerURL = '';
+
+      if (shipment?.shipmentType === 'PPM') {
+        // Read only view of approved documents
+        if (
+          shipment?.ppmShipment?.status === ppmShipmentStatuses.CLOSEOUT_COMPLETE ||
+          (shipment?.ppmShipment?.weightTickets && shipment?.ppmShipment?.weightTickets[0]?.status)
+        ) {
+          viewURL = `../${generatePath(servicesCounselingRoutes.SHIPMENT_VIEW_DOCUMENT_PATH, {
+            moveCode,
+            shipmentId: shipment.id,
+          })}`;
+        }
+        // Complete PPM for Customer URL
+        if (shipment?.ppmShipment?.status === ppmShipmentStatuses.WAITING_ON_CUSTOMER) {
+          const aboutInfoComplete = isPPMAboutInfoComplete(shipment.ppmShipment);
+          if (!aboutInfoComplete) {
+            completePpmForCustomerURL = `../${generatePath(servicesCounselingRoutes.SHIPMENT_PPM_ABOUT_PATH, {
               moveCode,
               shipmentId: shipment.id,
-            })}`
-          : '';
+            })}`;
+          } else {
+            completePpmForCustomerURL = `../${generatePath(servicesCounselingRoutes.SHIPMENT_PPM_REVIEW_PATH, {
+              moveCode,
+              shipmentId: shipment.id,
+            })}`;
+          }
+        }
+      }
+
       const displayInfo = {
         heading: getShipmentTypeLabel(shipment.shipmentType),
         destinationAddress: shipment.destinationAddress || {
@@ -358,6 +382,7 @@ const ServicesCounselingMoveDetails = ({
         displayInfo,
         editURL,
         viewURL,
+        completePpmForCustomerURL,
         shipmentType: shipment.shipmentType,
       };
     });
@@ -826,6 +851,7 @@ const ServicesCounselingMoveDetails = ({
                     displayInfo={shipment.displayInfo}
                     editURL={shipment.editURL}
                     viewURL={shipment.viewURL}
+                    completePpmForCustomerURL={shipment.completePpmForCustomerURL}
                     isSubmitted={false}
                     key={shipment.id}
                     shipmentId={shipment.id}
