a178dfccfff970958db58cfc06c4f3f24dfbbc6a
ryan-mchugh <ryan.mchugh@caci.com>
2025-03-06 21:45:08 +0000

    B-22483 - modify old schema to match PROD uuid for roles.

--- migrations/app/schema/20191204200540_create_and_populate_roles_table.up.sql
+++ migrations/app/schema/20191204200540_create_and_populate_roles_table.up.sql
@@ -1,21 +1,26 @@
 CREATE TABLE roles (
-	id integer PRIMARY KEY,
+	id uuid PRIMARY KEY,
 	role_type text,
 	created_at timestamp without time zone NOT NULL,
-	updated_at timestamp without time zone NOT NULL
+	updated_at timestamp without time zone NOT NULL,
+	role_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE user_roles (
-	users_id uuid NOT NULL,
-	roles_id integer NOT NULL,
-	FOREIGN KEY (users_id) REFERENCES users (id),
-	FOREIGN KEY (roles_id) REFERENCES roles (id)
+CREATE TABLE users_roles (
+	user_id uuid PRIMARY KEY NOT NULL,
+	role_id uuid NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (role_id) REFERENCES roles (id),
+	created_at TIMESTAMP without time zone NOT NULL,
+	updated_at TIMESTAMP without time zone NOT NULL
 );
+
+ALTER TABLE roles
+    ADD CONSTRAINT unique_roles UNIQUE (role_type);
 
 INSERT INTO roles (id, role_type, created_at, updated_at)
 VALUES
-  ('1','customer', now(), now()),
-  ('2','transportation_ordering_officer', now(), now()),
-  ('3','transportation_invoicing_officer', now(), now()),
-  ('4','contracting_officer', now(), now()),
-  ('5','ppm_office_users', now(), now());
+  ('c728caf3-5f9d-4db6-a9d1-7cd8ff013b2e','customer', now(), now(), 'Customer'),
+  ('2b21e867-78c3-4980-95a1-c8242b78baba','task_ordering_officer', now(), now(), 'Task Ordering Officer'),
+  ('c19a5d5f-d320-4972-b294-1d760ee4b899','task_invoicing_officer', now(), now(), 'Task Invoicing Officer'),
+  ('5496a188-69dc-4ae4-9dab-ce6c063d648f','contracting_officer', now(), now(), 'Contracting Officer');
