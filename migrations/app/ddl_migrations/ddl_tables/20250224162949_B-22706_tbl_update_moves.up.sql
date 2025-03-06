9f7f5cf5cda00757c6114ead786d84cb157419af
loganwc <logan.cunningham@caci.com>
2025-03-06 00:15:44 +0000

    ing

--- migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
@@ -3,4 +3,4 @@ ADD COLUMN IF NOT EXISTS too_destination_assigned_id uuid
 CONSTRAINT moves_too_destination_assigned_id_fkey
 REFERENCES office_users;
 
-COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID of the Task Order Officer on the office_users table';
+COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID of the Task Ordering Officer on the office_users table';
