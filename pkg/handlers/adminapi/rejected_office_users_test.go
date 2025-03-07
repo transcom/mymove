4610b25e7ff136a15b384e8a895bdec9ca0af160
Konstance Haffaney <konstance.haffaney@caci.com>
2025-03-07 14:48:51 +0000

    most of hook changes... need 5183 in main

--- pkg/handlers/adminapi/rejected_office_users_test.go
+++ pkg/handlers/adminapi/rejected_office_users_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-openapi/strfmt"
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/mock"
+
 	"github.com/transcom/mymove/pkg/factory"
 	rejectedofficeuserop "github.com/transcom/mymove/pkg/gen/adminapi/adminoperations/rejected_office_users"
 	"github.com/transcom/mymove/pkg/handlers"
