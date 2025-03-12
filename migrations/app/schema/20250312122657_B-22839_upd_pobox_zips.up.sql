0149e9a93716263ee53900a7da4ed7255f18ba52
Beth Grohmann <beth.grohmann@caci.com>
2025-03-12 08:01:55 -0500

    Update PO box only zips

---
+++ migrations/app/schema/20250312122657_B-22839_upd_pobox_zips.up.sql
@@ -0,0 +1,25 @@
+--set PO box only zips
+--Tacoma, WA 98464
+--Columbia, SC 29230
+
+DO $$
+DECLARE
+	i			record;
+
+BEGIN
+
+	FOR i in (select duty_locations.id duty_loc_id,
+					 v_locations.us_post_regions_id
+			 	from duty_locations,
+					 addresses,
+					 v_locations
+			   where duty_locations.address_id = addresses.id
+				 and addresses.us_post_region_cities_id = v_locations.uprc_id
+				 and v_locations.uspr_zip_id in ('98464','29230')
+			)
+	LOOP
+
+		update re_us_post_regions set is_po_box = true, updated_at = now() where id = i.us_post_regions_id and is_po_box = false;
+
+	END LOOP;
+END $$;
