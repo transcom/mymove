6c1a97ea2f4a50b32194dcfa9735eacf502cdf9a
Beth Grohmann <beth.grohmann@caci.com>
2025-03-14 12:37:15 -0500

    Update 20250306215742_B-22770_upd_base_names.up.sql

--- migrations/app/schema/20250306215742_B-22770_upd_base_names.up.sql
+++ migrations/app/schema/20250306215742_B-22770_upd_base_names.up.sql
@@ -5,14 +5,14 @@ INSERT INTO public.addresses
 select 'c13715ec-68d9-4c77-ae9a-5a652ddd3787'::uuid, 'n/a', 'Fort Bragg', 'NC', '28307', now(), now(), 'CUMBERLAND', false, '791899e6-cd77-46f2-981b-176ecb8d7098'::uuid, 'c8898dc9-9ebb-4651-b9d4-6757a6a172fc'::uuid
 where not exists (select id from addresses where id = 'c13715ec-68d9-4c77-ae9a-5a652ddd3787');
 
-update duty_locations set name = 'Fort Bragg, NC 28307', address_id = 'c13715ec-68d9-4c77-ae9a-5a652ddd3787', updated_at = now() where id = 'fc916367-afd7-4035-b3d0-74b6be850cab';
+update duty_locations set name = 'Fort Bragg, NC 28307', address_id = 'c13715ec-68d9-4c77-ae9a-5a652ddd3787', updated_at = now() where name = 'Fort Liberty, NC 28307';
 
 INSERT INTO public.addresses
 (id, street_address_1, city, state, postal_code, created_at, updated_at, county, is_oconus, country_id, us_post_region_cities_id)
 select 'd8769fb0-e130-46b2-9191-509663bab4b4'::uuid, 'n/a', 'Fort Bragg', 'NC', '28310', now(), now(), 'CUMBERLAND', false, '791899e6-cd77-46f2-981b-176ecb8d7098'::uuid, '5888ab84-a8c6-4e8d-b3e6-7975e981241d'::uuid
 where not exists (select id from addresses where id = 'd8769fb0-e130-46b2-9191-509663bab4b4');
 
-update duty_locations set name = 'Fort Bragg, NC 28310', address_id = 'd8769fb0-e130-46b2-9191-509663bab4b4', updated_at = now() where id = 'a5a3eb41-d3e0-4c9a-a87b-695caf601486';
+update duty_locations set name = 'Fort Bragg, NC 28310', address_id = 'd8769fb0-e130-46b2-9191-509663bab4b4', updated_at = now() where name = 'Fort Liberty, NC 28310';
 
 update addresses set city = 'Fort Bragg', us_post_region_cities_id = '5888ab84-a8c6-4e8d-b3e6-7975e981241d' where id = '50ebc5c0-97f3-46e7-b2c7-a3f08b3b47b5';
 
