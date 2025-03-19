aea9685713f32257032f6fba356227ef73efd920
Konstance Haffaney <konstance.haffaney@caci.com>
2025-03-19 03:52:00 +0000

    fix missing servicememberID and add tests

--- pkg/handlers/ghcapi/progear_weight_ticket_test.go
+++ pkg/handlers/ghcapi/progear_weight_ticket_test.go
@@ -20,6 +20,102 @@ import (
 	"github.com/transcom/mymove/pkg/testdatagen"
 )
 
+// CREATE TEST
+func (suite *HandlerSuite) TestCreateProGearWeightTicketHandler() {
+	// Reusable objects
+	progearCreator := progear.NewOfficeProgearWeightTicketCreator()
+
+	type progearCreateSubtestData struct {
+		ppmShipment models.PPMShipment
+		params      progearops.CreateProGearWeightTicketParams
+		handler     CreateProGearWeightTicketHandler
+	}
+	makeCreateSubtestData := func(authenticateRequest bool) (subtestData progearCreateSubtestData) {
+		subtestData.ppmShipment = factory.BuildPPMShipment(suite.DB(), nil, nil)
+		endpoint := fmt.Sprintf("/ppm-shipments/%s/pro-gear-weight-tickets", subtestData.ppmShipment.ID.String())
+		req := httptest.NewRequest("POST", endpoint, nil)
+		serviceMember := subtestData.ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMember
+		if authenticateRequest {
+			req = suite.AuthenticateRequest(req, serviceMember)
+		}
+		subtestData.params = progearops.CreateProGearWeightTicketParams{
+			HTTPRequest:   req,
+			PpmShipmentID: *handlers.FmtUUID(subtestData.ppmShipment.ID),
+		}
+
+		subtestData.handler = CreateProGearWeightTicketHandler{
+			suite.HandlerConfig(),
+			progearCreator,
+		}
+
+		return subtestData
+	}
+
+	suite.Run("Successfully Create Weight Ticket - Integration Test", func() {
+		subtestData := makeCreateSubtestData(true)
+
+		response := subtestData.handler.Handle(subtestData.params)
+
+		suite.IsType(&progearops.CreateProGearWeightTicketCreated{}, response)
+
+		createdProgear := response.(*progearops.CreateProGearWeightTicketCreated).Payload
+
+		suite.NotEmpty(createdProgear.ID.String())
+		suite.NotNil(createdProgear.DocumentID.String())
+	})
+
+	suite.Run("POST failure - 400- bad request", func() {
+		subtestData := makeCreateSubtestData(true)
+		// Missing PPM Shipment ID
+		params := subtestData.params
+
+		params.PpmShipmentID = ""
+
+		response := subtestData.handler.Handle(params)
+
+		suite.IsType(&progearops.CreateProGearWeightTicketBadRequest{}, response)
+	})
+
+	suite.Run("POST failure - 404 - not found - wrong service member", func() {
+		subtestData := makeCreateSubtestData(false)
+
+		unauthorizedUser := factory.BuildServiceMember(suite.DB(), nil, nil)
+		req := subtestData.params.HTTPRequest
+		unauthorizedReq := suite.AuthenticateRequest(req, unauthorizedUser)
+		unauthorizedParams := subtestData.params
+		unauthorizedParams.HTTPRequest = unauthorizedReq
+
+		response := subtestData.handler.Handle(unauthorizedParams)
+
+		suite.IsType(&progearops.CreateProGearWeightTicketNotFound{}, response)
+	})
+
+	suite.Run("Post failure - 500 - Server Error", func() {
+		mockCreator := mocks.ProgearWeightTicketCreator{}
+
+		subtestData := makeCreateSubtestData(true)
+		params := subtestData.params
+		serverErr := errors.New("ServerError")
+
+		mockCreator.On("CreateProgearWeightTicket",
+			mock.AnythingOfType("*appcontext.appContext"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(nil, serverErr)
+
+		handler := CreateProGearWeightTicketHandler{
+			suite.HandlerConfig(),
+			&mockCreator,
+		}
+
+		response := handler.Handle(params)
+
+		suite.IsType(&progearops.CreateProGearWeightTicketInternalServerError{}, response)
+	})
+}
+
+//
+// UPDATE Customer test
+//
 func (suite *HandlerSuite) TestUpdateProGearWeightTicketHandler() {
 	// Reusable objects
 	progearUpdater := progear.NewCustomerProgearWeightTicketUpdater()
@@ -152,5 +248,147 @@ func (suite *HandlerSuite) TestUpdateProGearWeightTicketHandler() {
 		response := handler.Handle(params)
 
 		suite.IsType(&progearops.UpdateProGearWeightTicketInternalServerError{}, response)
+	})
+}
+
+// DELETE test
+func (suite *HandlerSuite) TestDeleteProgearWeightTicketHandler() {
+	// Create Reusable objects
+	progearWeightTicketDeleter := progear.NewProgearWeightTicketDeleter()
+
+	type progearWeightTicketDeleteSubtestData struct {
+		ppmShipment         models.PPMShipment
+		progearWeightTicket models.ProgearWeightTicket
+		params              progearops.DeleteProGearWeightTicketParams
+		handler             DeleteProGearWeightTicketHandler
+	}
+	makeDeleteSubtestData := func(authenticateRequest bool) (subtestData progearWeightTicketDeleteSubtestData) {
+		// Fake data:
+		subtestData.progearWeightTicket = factory.BuildProgearWeightTicket(suite.DB(), nil, nil)
+		subtestData.ppmShipment = subtestData.progearWeightTicket.PPMShipment
+		serviceMember := subtestData.ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMember
+
+		endpoint := fmt.Sprintf("/ppm-shipments/%s/pro-gear-weight-tickets/%s", subtestData.ppmShipment.ID.String(), subtestData.progearWeightTicket.ID.String())
+		req := httptest.NewRequest("DELETE", endpoint, nil)
+		if authenticateRequest {
+			req = suite.AuthenticateRequest(req, serviceMember)
+		}
+		subtestData.params = progearops.
+			DeleteProGearWeightTicketParams{
+			HTTPRequest:           req,
+			PpmShipmentID:         *handlers.FmtUUID(subtestData.ppmShipment.ID),
+			ProGearWeightTicketID: *handlers.FmtUUID(subtestData.progearWeightTicket.ID),
+		}
+
+		// Use createS3HandlerConfig for the HandlerConfig because we are required to upload a doc
+		subtestData.handler = DeleteProGearWeightTicketHandler{
+			suite.createS3HandlerConfig(),
+			progearWeightTicketDeleter,
+		}
+
+		return subtestData
+	}
+
+	suite.Run("Successfully Delete Pro-gear Weight Ticket - Integration Test", func() {
+		subtestData := makeDeleteSubtestData(true)
+
+		params := subtestData.params
+		response := subtestData.handler.Handle(params)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketNoContent{}, response)
+	})
+
+	suite.Run("DELETE failure - 401 - permission denied - not authenticated", func() {
+		subtestData := makeDeleteSubtestData(false)
+		response := subtestData.handler.Handle(subtestData.params)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketUnauthorized{}, response)
+	})
+
+	suite.Run("DELETE failure - 403 - permission denied - wrong application / user", func() {
+		subtestData := makeDeleteSubtestData(false)
+
+		officeUser := factory.BuildOfficeUser(suite.DB(), nil, nil)
+
+		req := subtestData.params.HTTPRequest
+		unauthorizedReq := suite.AuthenticateOfficeRequest(req, officeUser)
+		unauthorizedParams := subtestData.params
+		unauthorizedParams.HTTPRequest = unauthorizedReq
+
+		response := subtestData.handler.Handle(unauthorizedParams)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketForbidden{}, response)
+	})
+
+	suite.Run("DELETE failure - 403 - permission denied - wrong service member user", func() {
+		subtestData := makeDeleteSubtestData(false)
+
+		otherServiceMember := factory.BuildServiceMember(suite.DB(), nil, nil)
+
+		req := subtestData.params.HTTPRequest
+		unauthorizedReq := suite.AuthenticateRequest(req, otherServiceMember)
+		unauthorizedParams := subtestData.params
+		unauthorizedParams.HTTPRequest = unauthorizedReq
+
+		response := subtestData.handler.Handle(unauthorizedParams)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketForbidden{}, response)
+	})
+
+	suite.Run("DELETE failure - 404- not found", func() {
+		subtestData := makeDeleteSubtestData(true)
+		params := subtestData.params
+		// Wrong ID provided
+		uuidString := handlers.FmtUUID(testdatagen.ConvertUUIDStringToUUID("e392b01d-3b23-45a9-8f98-e4d5b03c8a93"))
+		params.ProGearWeightTicketID = *uuidString
+
+		response := subtestData.handler.Handle(params)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketNotFound{}, response)
+	})
+
+	suite.Run("DELETE failure - 404 - not found - ppm shipment ID and moving expense ID don't match", func() {
+		subtestData := makeDeleteSubtestData(false)
+		serviceMember := subtestData.ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMember
+
+		otherPPMShipment := factory.BuildPPMShipment(suite.DB(), []factory.Customization{
+			{
+				Model:    subtestData.ppmShipment.Shipment.MoveTaskOrder.Orders,
+				LinkOnly: true,
+			},
+		}, nil)
+
+		subtestData.params.PpmShipmentID = *handlers.FmtUUID(otherPPMShipment.ID)
+		req := subtestData.params.HTTPRequest
+		unauthorizedReq := suite.AuthenticateRequest(req, serviceMember)
+		unauthorizedParams := subtestData.params
+		unauthorizedParams.HTTPRequest = unauthorizedReq
+
+		response := subtestData.handler.Handle(unauthorizedParams)
+		suite.IsType(&progearops.DeleteProGearWeightTicketNotFound{}, response)
+	})
+	suite.Run("DELETE failure - 500 - server error", func() {
+		mockDeleter := mocks.ProgearWeightTicketDeleter{}
+
+		subtestData := makeDeleteSubtestData(true)
+		params := subtestData.params
+
+		err := errors.New("ServerError")
+
+		mockDeleter.On("DeleteProgearWeightTicket",
+			mock.AnythingOfType("*appcontext.appContext"),
+			mock.AnythingOfType("uuid.UUID"),
+			mock.AnythingOfType("uuid.UUID"),
+		).Return(err)
+
+		// Use createS3HandlerConfig for the HandlerConfig because we are required to upload a doc
+		handler := DeleteProGearWeightTicketHandler{
+			suite.createS3HandlerConfig(),
+			&mockDeleter,
+		}
+
+		response := handler.Handle(params)
+
+		suite.IsType(&progearops.DeleteProGearWeightTicketInternalServerError{}, response)
 	})
 }
--- pkg/services/progear_weight_ticket/progear_weight_ticket_creator_test.go
+++ pkg/services/progear_weight_ticket/progear_weight_ticket_creator_test.go
@@ -9,12 +9,63 @@ import (
 )
 
 func (suite *ProgearWeightTicketSuite) TestProgearWeightTicketCreator() {
-	suite.Run("Successfully creates a ProgearWeightTicket", func() {
+	suite.Run("Successfully creates a ProgearWeightTicket - Customer", func() {
 		ppmShipment := factory.BuildMinimalPPMShipment(suite.DB(), nil, nil)
 		serviceMemberID := ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMemberID
-
 		session := &auth.Session{
 			ApplicationName: auth.MilApp,
+			ServiceMemberID: serviceMemberID,
+		}
+
+		progearWeightTicketCreator := NewCustomerProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), ppmShipment.ID)
+
+		suite.Nil(err)
+		suite.NotNil(progearWeightTicket)
+		suite.Equal(ppmShipment.ID, progearWeightTicket.PPMShipmentID)
+		suite.NotNil(progearWeightTicket.DocumentID)
+		suite.Equal(serviceMemberID, progearWeightTicket.Document.ServiceMemberID)
+	})
+
+	suite.Run("Fails when an invalid ppmShipmentID is used", func() {
+		serviceMember := factory.BuildServiceMember(suite.DB(), nil, nil)
+		session := &auth.Session{
+			ApplicationName: auth.MilApp,
+			ServiceMemberID: serviceMember.ID,
+		}
+
+		progearWeightTicketCreator := NewCustomerProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), uuid.Nil)
+
+		suite.Nil(progearWeightTicket)
+
+		expectedErr := apperror.NewNotFoundError(uuid.Nil, "while looking for PPMShipment")
+
+		suite.ErrorIs(err, expectedErr)
+	})
+
+	suite.Run("Fails when session has invalid serviceMemberID", func() {
+		session := &auth.Session{
+			ApplicationName: auth.MilApp,
+			ServiceMemberID: uuid.Must(uuid.NewV4()),
+		}
+		ppmShipment := factory.BuildMinimalPPMShipment(suite.DB(), nil, nil)
+
+		progearWeightTicketCreator := NewCustomerProgearWeightTicketCreator()
+		progearWeightTicket, err := progearWeightTicketCreator.CreateProgearWeightTicket(suite.AppContextWithSessionForTest(session), ppmShipment.ID)
+
+		suite.Nil(progearWeightTicket)
+
+		expectedErr := apperror.NewNotFoundError(ppmShipment.ID, "while looking for PPMShipment")
+
+		suite.ErrorIs(err, expectedErr)
+	})
+
+	suite.Run("Successfully creates a ProgearWeightTicket - Office", func() {
+		ppmShipment := factory.BuildMinimalPPMShipment(suite.DB(), nil, nil)
+		serviceMemberID := ppmShipment.Shipment.MoveTaskOrder.Orders.ServiceMemberID
+		session := &auth.Session{
+			ApplicationName: auth.OfficeApp,
 			ServiceMemberID: serviceMemberID,
 		}
 
