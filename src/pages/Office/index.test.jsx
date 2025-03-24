462aea2ed5cdd89e480ecb18013152e7945d7f7f
msaki-caci <michael.saki@caci.com>
2025-03-24 20:13:23 +0000

    B-22702 Refactor OfficeApp test pt 2
    - changed render function
    - added case for loadspinner

--- src/pages/Office/index.test.jsx
+++ src/pages/Office/index.test.jsx
@@ -1,13 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 
 import OfficeApp from './index';
 
-// import { roleTypes } from 'constants/userRoles';
 import { configureStore } from 'shared/store';
-// import { isBooleanFlagEnabled } from 'utils/featureFlags';
+import { mockPage } from 'testUtils';
+
+afterEach(() => {
+  cleanup();
+});
+
+jest.mock('utils/featureFlags', () => ({
+  ...jest.requireActual('utils/featureFlags'),
+  isBooleanFlagEnabled: jest.fn().mockImplementation(() => Promise.resolve(false)),
+}));
 
 // Mock Redux actions to prevent actual API calls
 jest.mock('store/auth/actions', () => ({
@@ -27,8 +36,40 @@ jest.mock('utils/featureFlags', () => ({
   isBooleanFlagEnabled: jest.fn().mockImplementation(() => Promise.resolve(false)),
 }));
 
+// Mock the components that are routed to from the index, ordered the same as the routes in the index file
+mockPage('pages/SignIn/SignIn');
+mockPage('pages/InvalidPermissions/InvalidPermissions');
+mockPage('pages/Office/MoveQueue/MoveQueue');
+mockPage('pages/Office/HeadquartersQueues/HeadquartersQueues', 'Headquarters Queues');
+mockPage('pages/Office/PaymentRequestQueue/PaymentRequestQueue');
+mockPage('pages/Office/ServicesCounselingAddShipment/ServicesCounselingAddShipment');
+mockPage('pages/Office/ServicesCounselingQueue/ServicesCounselingQueue');
+mockPage('pages/Office/ServicesCounselingMoveInfo/ServicesCounselingMoveInfo');
+mockPage('pages/Office/AddShipment/AddShipment');
+mockPage('pages/Office/EditShipmentDetails/EditShipmentDetails');
+mockPage('pages/PrimeUI/MoveTaskOrder/MoveDetails', 'Prime Simulator Move Details');
+mockPage('pages/PrimeUI/Shipment/PrimeUIShipmentCreate', 'Prime Simulator Shipment Create');
+mockPage('pages/PrimeUI/Shipment/PrimeUIShipmentUpdateAddress', 'Prime Simulator Shipment Update Address');
+mockPage('pages/PrimeUI/Shipment/PrimeUIShipmentUpdate', 'Prime Simulator Shipment Update');
+mockPage('pages/PrimeUI/CreatePaymentRequest/CreatePaymentRequest', 'Prime Simulator Create Payment Request');
+mockPage(
+  'pages/PrimeUI/UploadPaymentRequestDocuments/UploadPaymentRequestDocuments',
+  'Prime Simulator Upload Payment Request Documents',
+);
+mockPage(
+  'pages/PrimeUI/UploadServiceRequestDocuments/UploadServiceRequestDocuments',
+  'Prime Simulator Upload Service Request Documents',
+);
+mockPage('pages/PrimeUI/CreateServiceItem/CreateServiceItem', 'Prime Simulator Create Service Item');
+mockPage('pages/PrimeUI/Shipment/PrimeUIShipmentUpdateReweigh', 'Prime Simulator Shipment Update Reweigh');
+mockPage('pages/Office/QAECSRMoveSearch/QAECSRMoveSearch', 'QAE CSR Move Search');
+mockPage('pages/Office/TXOMoveInfo/TXOMoveInfo', 'TXO Move Info');
+mockPage('pages/PrimeUI/AvailableMoves/AvailableMovesQueue', 'Prime Simulator Available Moves Queue');
+mockPage('components/NotFound/NotFound');
+
 const defaultState = {
   auth: {
+    activeRole: null,
     hasErrored: false,
     hasSucceeded: true,
     isLoading: false,
@@ -40,186 +81,105 @@ const defaultState = {
     hasSucceeded: true,
     isLoading: false,
   },
+  interceptor: {
+    hasRecentError: false,
+    traceId: 'mock-trace-id',
+  },
   generalState: {
-    moveId: '',
     showLoadingSpinner: false,
     loadingSpinnerMessage: null,
   },
-};
-
-const loggedOutState = {
-  ...defaultState,
-  auth: {
-    ...defaultState.auth,
-    activeRole: null,
-    isLoggedIn: false,
+  entities: {
+    user: {
+      userId123: {
+        permissions: [],
+        privileges: [],
+        roles: [],
+      },
+    },
   },
 };
 
-// const loggedInState = {
-//   ...defaultState,
-//   auth: {
-//     ...defaultState.auth,
-//     isLoggedIn: true,
-//     activeRole: roleTypes.TOO,
-//   },
-// };
+const loggedInState = {
+  ...defaultState,
+  auth: {
+    ...defaultState.auth,
+    isLoggedIn: true,
+    activeRole: 'TOO',
+  },
+  entities: {
+    user: {
+      testUser: {
+        id: 'testUser',
+        roles: [{ roleType: 'TOO' }],
+      },
+    },
+  },
+};
 
-const renderWithState = (state, path) => {
-  const mockStore = configureStore({ ...state });
+// Render the OfficeApp component with routing and Redux setup for the provided route and role
+const renderWithProviders = (
+  ui,
+  { route = '/', initialState = {}, store = configureStore(initialState).store } = {},
+) => {
+  // isBooleanFlagEnabled.mockImplementation(() => Promise.resolve(true));
+  // const mockStore = createMockStore(role);
+  // const userRoles = role ? [{ roleType: role }] : [];
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <Provider store={store}>{ui}</Provider>
+    </MemoryRouter>,
+  );
+};
 
+describe('Office App', () => {
   const minProps = {
     loadPublicSchema: jest.fn(),
     loadInternalSchema: jest.fn(),
     loadUser: jest.fn(),
   };
 
-  return render(
-    <MemoryRouter initialEntries={[path]}>
-      <Provider store={mockStore.store}>
-        <OfficeApp {...minProps} />
-      </Provider>
-    </MemoryRouter>,
-  );
-};
-
-// const createMockStore = (role) => {
-//   // Otherwise, use logged in state with the provided role
-//   const state = {
-//     auth: {
-//       activeRole: role,
-//       isLoading: false,
-//       isLoggedIn: true,
-//     },
-//     swaggerInternal: {
-//       hasErrored: false,
-//       hasSucceeded: true,
-//       isLoading: false,
-//     },
-//     generalState: {
-//       moveId: '',
-//       showLoadingSpinner: true,
-//       loadingSpinnerMessage: 'test message',
-//     },
-//     entities: {
-//       user: {
-//         userId123: {
-//           id: 'userId123',
-//           roles: [{ roleType: role }],
-//         },
-//       },
-//     },
-//   };
-
-//   return configureStore(state);
-// };
-
-// Render the OfficeApp component with routing and Redux setup for the provided route and role
-// const renderOfficeAppAtRoute = (route, role) => {
-//   isBooleanFlagEnabled.mockImplementation(() => Promise.resolve(true));
-//   const mockStore = createMockStore(role);
-//   const userRoles = role ? [{ roleType: role }] : [];
-//   render(
-//     <MemoryRouter initialEntries={[route]}>
-//       <Provider store={mockStore.store}>
-//         <OfficeApp
-//           router={{ location: { pathname: route } }}
-//           loadInternalSchema={jest.fn()}
-//           loadPublicSchema={jest.fn()}
-//           loadUser={jest.fn()}
-//           hasRecentError={false}
-//           activeRole={role || null}
-//           userRoles={userRoles}
-//           traceId=""
-//           loginIsLoading={!!role}
-//           userIsLoggedIn={!!role}
-//           hqRoleFlag
-//           gsrRoleFlag
-//         />
-//       </Provider>
-//     </MemoryRouter>,
-//   );
-// };
-
-describe('OfficeApp', () => {
-  it('renders Sign In page when logged out', async () => {
-    renderWithState(loggedOutState, '/sign-in');
+  it('renders Sign In page when user is logged out', async () => {
+    renderWithProviders(<OfficeApp {...minProps} />, {
+      route: '/sign-in',
+      initialState: defaultState,
+    });
     await waitFor(() => expect(screen.getByText(/sign in/i)).toBeInTheDocument());
   });
-  it('displays maintenance page when under maintenance', async () => {
+  it('displays Maintenance page when under maintenance is true', async () => {
     const updatedState = {
-      ...loggedOutState,
+      ...defaultState,
       auth: {
-        ...loggedOutState.auth,
+        ...defaultState.auth,
         underMaintenance: true,
       },
     };
+    renderWithProviders(<OfficeApp {...minProps} />, {
+      route: '/',
+      initialState: updatedState,
+    });
 
-    renderWithState(updatedState, '/');
     await waitFor(() =>
       expect(screen.getByText(/This system is currently undergoing maintenance/i)).toBeInTheDocument(),
     );
   });
-  // it('shows the loading spinner when props are set to show it', async () => {
-  //   const updatedState = {
-  //     ...loggedInState,
-  //     generalState: {
-  //       ...loggedInState.generalState,
-  //       showLoadingSpinner: true,
-  //       loadingSpinnerMessage: 'test message',
-  //     },
-  //   };
+  it('shows loading spinner when showLoadingSpinner is true', async () => {
+    const updatedState = {
+      ...loggedInState,
+      generalState: {
+        ...loggedInState.generalState,
+        showLoadingSpinner: true,
+        loadingSpinnerMessage: 'Loading...',
+      },
+    };
 
-  //   renderOfficeAppAtRoute('/', roleTypes.TOO);
-  //   await waitFor(
-  //     () => expect(screen.getByTestId('loading-spinner')).toBeInTheDocument(),
-  //     expect(screen.getByText(/test message/i)).toBeInTheDocument(),
-  //   );
-
-  // });
-
-  //   it('shows the loading spinner when props are set to show it', async () => {
-  //     const updatedState = {
-  //       ...loggedInState,
-  //       generalState: {
-  //         ...loggedInState.generalState,
-  //         showLoadingSpinner: true,
-  //         loadingSpinnerMessage: 'test message',
-  //       },
-  //     };
-
-  //     renderWithState(updatedState, '/');
-  //     await waitFor(
-  //       () => expect(screen.getByTestId('loading-spinner')).toBeInTheDocument(),
-  //       expect(screen.getByText(/test message/i)).toBeInTheDocument(),
-  //     );
-  //   });
-
-  //   it('handles the Invalid Permissions URL for logged in user', async () => {
-  //     renderWithState(loggedInState, '/invalid-permissions');
-
-  //     expect(screen.getByText('Skip to content')).toBeInTheDocument();
-  //     expect(screen.getByText('Controlled Unclassified Information')).toBeInTheDocument();
-
-  //     await waitFor(() =>
-  //       expect(screen.getByText(/You do not have permission to access this site/i)).toBeInTheDocument(),
-  //     );
-  //   });
-
-  //   it('shows the server error for logged in user', async () => {
-  //     renderWithState(loggedInState, '/server_error');
-
-  //     await waitFor(() => expect(screen.getByText('We are experiencing an internal server error')));
-  //   });
-
-  //   it('handles the forbidden URL for logged in user', async () => {
-  //     renderWithState(loggedInState, '/forbidden');
-
-  //     await waitFor(() => expect(screen.getByText('You are forbidden to use this endpoint')));
-  //   });
-
-  //   it('renders Footer component', async () => {
-  //     renderWithState(loggedInState, '/');
-  //     await waitFor(() => expect(screen.getByText(/Military OneSource/i)).toBeInTheDocument());
-  //   });
+    renderWithProviders(<OfficeApp {...minProps} />, {
+      route: '/',
+      initialState: updatedState,
+    });
+    await waitFor(() => {
+      expect(screen.getByTestId('loading-spinner')).toBeInTheDocument();
+      expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
+    });
+  });
 });
