9cc22c48442b88367570a9f09de5e37d43151e77
cameroncaci <cameron.jewell@caci.com>
2025-03-26 13:59:00 +0000

    add comment

--- migrations/app/ddl_migrations/ddl_functions/20250325161602_fn_if_modified_func.up.sql
+++ migrations/app/ddl_migrations/ddl_functions/20250325161602_fn_if_modified_func.up.sql
@@ -7,6 +7,8 @@
 CREATE OR REPLACE FUNCTION if_modified_func()
 RETURNS TRIGGER
 LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = pg_catalog, public
 AS $body$
 DECLARE
 	audit_row audit_history%ROWTYPE;
@@ -134,4 +136,35 @@ BEGIN
         );
 	RETURN NULL;
 END;
+$body$;
+
+COMMENT ON FUNCTION if_modified_func() IS $body$
+Track changes to a table at the statement and/or row level.
+
+Optional parameters to trigger in CREATE TRIGGER call:
+
+param 0: boolean, whether to log the query text. Default 't'.
+
+param 1: text[], columns to ignore in updates. Default [].
+
+         Updates to ignored cols are omitted from changed_data.
+
+         Updates with only ignored cols changed or have no changes are not inserted
+         into the audit log.
+
+         Almost all the processing work is still done for updates
+         that ignored. If you need to save the load, you need to use
+         WHEN clause on the trigger instead.
+
+         No warning or error is issued if ignored_cols contains columns
+         that do not exist in the target table. This lets you specify
+         a standard set of ignored columns.
+
+There is no parameter to disable logging of values. Add this trigger as
+a 'FOR EACH STATEMENT' rather than 'FOR EACH ROW' trigger if you do not
+want to log row values.
+
+Note that the user name logged is the login role for the session. The audit trigger
+cannot obtain the active role because it is reset by the SECURITY DEFINER invocation
+of the audit trigger its self.
 $body$;
