69a48f03d79111e9dc2121d84c0972bd74b3b760
Cory Kleinjan <cory.kleinjan@caci.com>
2025-03-27 14:19:19 +0000

    Updating lint versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
   plugins: ['prettier', 'security', 'no-only-tests', 'you-dont-need-lodash-underscore', 'ato'],
   extends: [
+    'react-app',
     'airbnb',
     'prettier',
     'prettier/prettier',
     'plugin:security/recommended',
     'plugin:you-dont-need-lodash-underscore/compatible',
+    'plugin:jsx-a11y/recommended',
   ],
   root: true,
   parserOptions: {
