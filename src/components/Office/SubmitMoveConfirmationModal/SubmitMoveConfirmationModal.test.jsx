d572f04232fc51915892a134640166cabc998f8f
antgmann <anthony.mann@caci.com>
2025-04-02 16:35:38 +0000

    Forgot a line

--- src/components/Office/SubmitMoveConfirmationModal/SubmitMoveConfirmationModal.test.jsx
+++ src/components/Office/SubmitMoveConfirmationModal/SubmitMoveConfirmationModal.test.jsx
@@ -19,6 +19,7 @@ describe('SubmitMoveConfirmationModal', () => {
     expect(wrapper.find('ModalClose').exists()).toBe(true);
     expect(wrapper.find('button[data-testid="modalCancelButton"]').exists()).toBe(true);
     expect(wrapper.find('button[type="submit"]').exists()).toBe(true);
+    expect(wrapper.containsMatchingElement(<p>You can’t make changes after you submit the move.</p>)).toEqual(true);
   });
 
   it('renders the component with shipment text', () => {
