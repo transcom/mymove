24aac17c5da7ea53deb0e23608c77cce0117a28a
Maria Traskowsky <maria.traskowsky@caci.com>
2025-04-03 19:31:14 +0000

    update cron schedule to 5:00 utc

--- migrations/app/schema/20250402213810_B-22761_add_cron_schedule.up.sql
+++ migrations/app/schema/20250402213810_B-22761_add_cron_schedule.up.sql
@@ -1,1 +1,1 @@
-SELECT cron.schedule('flag_sent_to_gex_for_review', '0 3 * * *', $$SELECT flag_sent_to_gex_for_review();$$); -- runs every day at 3 UTC
+SELECT cron.schedule('flag_sent_to_gex_for_review', '0 5 * * *', $$SELECT flag_sent_to_gex_for_review();$$); -- runs every day at 5:00 UTC
