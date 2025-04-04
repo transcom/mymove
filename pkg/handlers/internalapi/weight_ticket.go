0e4a03ad2c745d57a93b727b6c2502b13845f1ad
Paul Stonebraker <paul.stonebraker@caci.com>
2025-04-04 15:48:58 +0000

    add comment for error message in internalapi weight ticket handler

--- pkg/handlers/internalapi/weight_ticket.go
+++ pkg/handlers/internalapi/weight_ticket.go
@@ -99,6 +99,7 @@ func (h UpdateWeightTicketHandler) Handle(params weightticketops.UpdateWeightTic
 			updateWeightTicket, err := h.weightTicketUpdater.UpdateWeightTicket(appCtx, *weightTicket, params.IfMatch)
 
 			// temp error until E-06515 is implemented
+			// once that is implemented, remove this tempDTOD error and comment in the error below in the EventError case
 			tempDTODError := "We are unable to calculate your distance. It may be that you have entered an invalid ZIP Code. Please press 'Cancel' and check your ZIP Code you entered under 'About your PPM' to ensure it was entered correctly and is not a PO Box. If you do not have a different ZIP Code, then please contact the Technical Help Desk."
 			if err != nil {
 				appCtx.Logger().Error("internalapi.UpdateWeightTicketHandler", zap.Error(err))
