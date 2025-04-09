9a8066344075db8d6ce5f13952757f4c423aa7f6
Daniel Jordan <daniel.jordan@caci.com>
2025-04-09 15:12:15 +0000

    adding tests

--- src/constants/MoveHistory/EventTemplates/DenySITExtension/denySITExtension.test.jsx
+++ src/constants/MoveHistory/EventTemplates/DenySITExtension/denySITExtension.test.jsx
@@ -6,9 +6,6 @@ import denySITExtension from 'constants/MoveHistory/EventTemplates/DenySITExtens
 describe('when given a Deny SIT Extension item history record', () => {
   const historyRecord = {
     action: 'UPDATE',
-    actionTstampClk: '2025-04-09T13:43:45.092Z',
-    actionTstampStm: '2025-04-09T13:43:45.092Z',
-    actionTstampTx: '2025-04-09T13:43:45.081Z',
     changedValues: {
       customer_expense: false,
       decision_date: '2025-04-09T13:43:45.090591',
@@ -23,29 +20,7 @@ describe('when given a Deny SIT Extension item history record', () => {
       },
     ],
     eventName: 'denySITExtension',
-    id: 'a5f69178-dd8d-4392-b087-b1325a985531',
-    objectId: 'f77846bc-b5b4-47e8-9883-870806d34d4c',
-    oldValues: {
-      approved_days: null,
-      contractor_remarks: 'remarks',
-      customer_expense: null,
-      decision_date: null,
-      id: 'f77846bc-b5b4-47e8-9883-870806d34d4c',
-      mto_shipment_id: '3d95a553-803a-4441-b8c1-48b32e9e539e',
-      office_remarks: null,
-      request_reason: 'SERIOUS_ILLNESS_MEMBER',
-      requested_days: 10,
-      status: 'PENDING',
-    },
-    relId: 19955,
-    schemaName: 'public',
-    sessionUserEmail: 'multi-role-20250409133916-e8d4c95f3042@example.com',
-    sessionUserFirstName: 'Alice',
-    sessionUserId: '73a87ab6-bee0-4c6f-b008-7bd1682c6e9f',
-    sessionUserLastName: 'Bob',
-    sessionUserTelephone: '333-333-3333',
     tableName: 'sit_extensions',
-    transactionId: 2031,
   };
 
   it('correctly matches to the Deny SIT extension template', () => {
