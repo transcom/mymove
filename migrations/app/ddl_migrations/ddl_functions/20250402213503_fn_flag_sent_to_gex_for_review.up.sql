975e9a9d6ecbec5fdb1ab00b05ab983ff0f50233
Maria Traskowsky <maria.traskowsky@caci.com>
2025-04-11 13:41:32 +0000

    fix typo in interval

--- migrations/app/ddl_migrations/ddl_functions/20250402213503_fn_flag_sent_to_gex_for_review.up.sql
+++ migrations/app/ddl_migrations/ddl_functions/20250402213503_fn_flag_sent_to_gex_for_review.up.sql
@@ -1,8 +1,8 @@
 --B-22761 Maria Traskowsky added flag_sent_to_gex_for_review
 CREATE OR REPLACE FUNCTION flag_sent_to_gex_for_review() RETURNS void AS $$
 DECLARE -- time interval and timestamp for considering a payment request stuck in SENT_TO_GEX status
-  stale_internal INTERVAL := INTERVAL '12 hours';
-stale_sent_to_gex TIMESTAMP := now() - stale_internal;
+  stale_interval INTERVAL := INTERVAL '12 hours';
+stale_sent_to_gex TIMESTAMP := now() - stale_interval;
 BEGIN
 UPDATE payment_requests
 SET status = 'REVIEWED'::payment_request_status,
