75b1ef20f32c70cf764e2301317fcb11f9017be4
Maria Traskowsky <maria.traskowsky@caci.com>
2025-04-14 16:07:07 +0000

    add reflagged_payment_requests to track reflagged prs

--- migrations/app/ddl_migrations/ddl_functions/20250402213503_fn_flag_sent_to_gex_for_review.up.sql
+++ migrations/app/ddl_migrations/ddl_functions/20250402213503_fn_flag_sent_to_gex_for_review.up.sql
@@ -1,14 +1,33 @@
 --B-22761 Maria Traskowsky added flag_sent_to_gex_for_review
+CREATE TABLE IF NOT EXISTS reflagged_payment_requests (
+  payment_request_number TEXT NOT NULL PRIMARY KEY,
+  reflagged_count INTEGER NOT NULL DEFAULT 0,
+  updated_at TIMESTAMP DEFAULT now()
+);
 CREATE OR REPLACE FUNCTION flag_sent_to_gex_for_review() RETURNS void AS $$
 DECLARE -- time interval and timestamp for considering a payment request stuck in SENT_TO_GEX status
   stale_interval INTERVAL := INTERVAL '12 hours';
-stale_sent_to_gex TIMESTAMP := now() - stale_interval;
-BEGIN
-UPDATE payment_requests
-SET status = 'REVIEWED'::payment_request_status,
-  sent_to_gex_at = NULL
-WHERE status = 'SENT_TO_GEX'::payment_request_status
-  AND sent_to_gex_at IS NOT NULL -- checks for older sent_to_gex_at than stale_sent_to_gex
-  AND sent_to_gex_at < stale_sent_to_gex;
+  stale_sent_to_gex TIMESTAMP := now() - stale_interval;
+BEGIN WITH updated_payment_requests AS (
+  UPDATE payment_requests
+  SET status = 'REVIEWED'::payment_request_status,
+    sent_to_gex_at = NULL
+  WHERE status = 'SENT_TO_GEX'::payment_request_status
+    AND sent_to_gex_at IS NOT NULL -- checks for older sent_to_gex_at than stale_sent_to_gex
+    AND sent_to_gex_at < stale_sent_to_gex
+  RETURNING payment_request_number
+)
+INSERT INTO reflagged_payment_requests (
+    payment_request_number,
+    reflagged_count,
+    updated_at
+  )
+SELECT payment_request_number,
+  1,
+  now()
+FROM updated_payment_requests ON CONFLICT (payment_request_number) DO
+UPDATE
+SET reflagged_count = reflagged_payment_requests.reflagged_count + 1,
+  updated_at = now();
 END;
 $$ LANGUAGE plpgsql;
