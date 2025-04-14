c1b3bd6b3c3c2ccab41ae57d1fa7a2b18e39eac1
loganwc <logan.cunningham@caci.com>
2025-04-14 16:11:09 +0000

    estimated prices should show whether it's approved or not

--- src/components/Office/ServiceItemDetails/ServiceItemDetails.jsx
+++ src/components/Office/ServiceItemDetails/ServiceItemDetails.jsx
@@ -13,7 +13,7 @@ import { SitStatusShape } from 'types/sitStatusShape';
 import { formatDateWithUTC } from 'shared/dates';
 import { formatCityStateAndPostalCode } from 'utils/shipmentDisplay';
 import { formatWeight, convertFromThousandthInchToInch, formatCents, toDollarString } from 'utils/formatters';
-import SERVICE_ITEM_STATUSES, { SERVICE_ITEM_CODES } from 'constants/serviceItems';
+import { SERVICE_ITEM_CODES } from 'constants/serviceItems';
 
 function generateDetailText(details, id, className) {
   const detailList = Object.keys(details).map((detail) => (
@@ -246,11 +246,10 @@ const generateDestinationSITDetailSection = (id, serviceRequestDocUploads, detai
             ) : null}
           </>
         )}
-        {details?.status === SERVICE_ITEM_STATUSES.APPROVED &&
-          generateDetailText({
-            'Estimated Price':
-              details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
-          })}
+        {generateDetailText({
+          'Estimated Price':
+            details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
+        })}
       </dl>
     </div>
   );
@@ -276,11 +275,10 @@ const ServiceItemDetails = ({ id, code, details, serviceRequestDocs, shipment, s
               },
               id,
             )}
-            {details?.status === SERVICE_ITEM_STATUSES.APPROVED &&
-              generateDetailText({
-                'Estimated Price':
-                  details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
-              })}
+            {generateDetailText({
+              'Estimated Price':
+                details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
+            })}
             {details.rejectionReason &&
               generateDetailText({ 'Rejection reason': details.rejectionReason }, id, 'margin-top-2')}
             {!isEmpty(serviceRequestDocUploads) ? (
@@ -333,11 +331,10 @@ const ServiceItemDetails = ({ id, code, details, serviceRequestDocs, shipment, s
               },
               id,
             )}
-            {details?.status === SERVICE_ITEM_STATUSES.APPROVED &&
-              generateDetailText({
-                'Estimated Price':
-                  details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
-              })}
+            {generateDetailText({
+              'Estimated Price':
+                details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
+            })}
             {details.rejectionReason &&
               generateDetailText({ 'Rejection reason': details.rejectionReason }, id, 'margin-top-2')}
             {!isEmpty(serviceRequestDocUploads) ? (
@@ -376,11 +373,10 @@ const ServiceItemDetails = ({ id, code, details, serviceRequestDocs, shipment, s
               },
               id,
             )}
-            {details?.status === SERVICE_ITEM_STATUSES.APPROVED &&
-              generateDetailText({
-                'Estimated Price':
-                  details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
-              })}
+            {generateDetailText({
+              'Estimated Price':
+                details.estimatedPrice !== null ? toDollarString(formatCents(details.estimatedPrice)) : '-',
+            })}
             {details.rejectionReason &&
               generateDetailText({ 'Rejection reason': details.rejectionReason }, id, 'margin-top-2')}
             {!isEmpty(serviceRequestDocUploads) ? (
