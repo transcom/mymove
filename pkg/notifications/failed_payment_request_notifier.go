0cbd3b17fb7cb932e4a17d2f52589aba2f7ffab7
deandreJones <deandre.Jones@caci.com>
2025-04-17 12:37:18 -0500

    B-22762- initial

---
+++ pkg/notifications/failed_payment_request_notifier.go
@@ -0,0 +1,116 @@
+package notifications
+
+import (
+	"bytes"
+	"fmt"
+	html "html/template"
+	text "text/template"
+
+	"go.uber.org/zap"
+
+	"github.com/transcom/mymove/pkg/appcontext"
+	"github.com/transcom/mymove/pkg/assets"
+	"github.com/transcom/mymove/pkg/models"
+)
+
+var (
+	paymentRequestFailedRawTextTemplate = string(assets.MustAsset("notifications/templates/failed_payment_request_template.txt"))
+	paymentRequestFailedTextTemplate    = text.Must(text.New("text_template").Parse(paymentRequestFailedRawTextTemplate))
+	paymentRequestFailedRawHTMLTemplate = string(assets.MustAsset("notifications/templates/failed_payment_request_template.html"))
+	paymentRequestFailedHTMLTemplate    = html.Must(html.New("text_template").Parse(paymentRequestFailedRawHTMLTemplate))
+)
+
+type PaymentRequestFailed struct {
+	paymentRequest models.PaymentRequest
+	htmlTemplate   *html.Template
+	textTemplate   *text.Template
+}
+
+func NewPaymentRequestFailed(paymentRequest models.PaymentRequest) *PaymentRequestFailed {
+	return &PaymentRequestFailed{
+		paymentRequest: paymentRequest,
+		htmlTemplate:   paymentRequestFailedHTMLTemplate,
+		textTemplate:   paymentRequestFailedTextTemplate,
+	}
+}
+
+func (p PaymentRequestFailed) emails(appCtx appcontext.AppContext) ([]emailContent, error) {
+	var emails []emailContent
+	/* srcEmail, err := models.FetchParameterValueByName(appCtx.DB(), "src_email")
+	if err != nil {
+		return emails, nil
+	}
+	dstEmail, err := models.FetchParameterValueByName(appCtx.DB(), "transcom_dstro_email")
+	if err != nil {
+		return emails, nil
+	}
+	opsEmail, _ := models.FetchParameterValueByName(appCtx.DB(), "milmove_ops_email")
+	if err != nil {
+		return emails, nil
+	}
+
+	recipients := []string{*srcEmail.ParameterValue, *dstEmail.ParameterValue, *opsEmail.ParameterValue}
+	*/
+	ediError, err := models.FetchEdiErrorByPaymentRequestID(appCtx.DB(), p.paymentRequest.ID)
+	if err != nil {
+		return emails, nil
+	}
+
+	recipients := []string{"deandre.jones@caci.com", "deandrejones2002@gmail.com", "deandrejones2002@yahoo.com"}
+	htmlBody, textBody, err := p.renderTemplates(appCtx, emailData{
+		PaymentRequestNumber: p.paymentRequest.PaymentRequestNumber,
+		ErrorCode:            *ediError.Code,
+	})
+	if err != nil {
+		return emails, err
+	}
+
+	for _, recipient := range recipients {
+		email := emailContent{
+			recipientEmail: recipient,
+			subject:        "Payment Request Failed",
+			htmlBody:       htmlBody,
+			textBody:       textBody,
+		}
+		emails = append(emails, email)
+	}
+
+	return emails, nil
+
+}
+
+func (p PaymentRequestFailed) renderTemplates(appCtx appcontext.AppContext, data emailData) (string, string, error) {
+	htmlBody, err := p.RenderHTML(appCtx, data)
+	if err != nil {
+		return "", "", fmt.Errorf("error rendering html template using %#v", data)
+	}
+	textBody, err := p.RenderText(appCtx, data)
+	if err != nil {
+		return "", "", fmt.Errorf("error rendering text template using %#v", data)
+	}
+	return htmlBody, textBody, nil
+}
+
+type emailData struct {
+	PaymentRequestNumber string
+	ErrorCode            string
+}
+
+// RenderHTML renders the html for the email
+func (p PaymentRequestFailed) RenderHTML(appCtx appcontext.AppContext, data emailData) (string, error) {
+	var htmlBuffer bytes.Buffer
+	if err := p.htmlTemplate.Execute(&htmlBuffer, data); err != nil {
+		appCtx.Logger().Error("cant render html template ", zap.Error(err))
+	}
+	return htmlBuffer.String(), nil
+}
+
+// RenderText renders the text for the email
+func (p PaymentRequestFailed) RenderText(appCtx appcontext.AppContext, data emailData) (string, error) {
+	var textBuffer bytes.Buffer
+	if err := p.textTemplate.Execute(&textBuffer, data); err != nil {
+		appCtx.Logger().Error("cant render text template ", zap.Error(err))
+		return "", err
+	}
+	return textBuffer.String(), nil
+}
