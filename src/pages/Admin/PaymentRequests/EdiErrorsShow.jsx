f4f80b07307f6ab3f282acca493b923145187d15
Maria Traskowsky <maria.traskowsky@caci.com>
2025-04-17 16:52:37 +0000

    add nicer titles for individual edi error page

--- src/pages/Admin/PaymentRequests/EdiErrorsShow.jsx
+++ src/pages/Admin/PaymentRequests/EdiErrorsShow.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { SimpleShowLayout, Show, TextField } from 'react-admin';
+import { SimpleShowLayout, Show, TextField, DateField } from 'react-admin';
 
 const EdiErrorsShow = (props) => (
   <Show {...props}>
     <SimpleShowLayout>
-      <TextField source="id" />
-      <TextField source="paymentRequestID" />
-      <TextField source="paymentRequestNumber" />
-      <TextField source="ediType" />
-      <TextField source="code" />
-      <TextField source="description" />
-      <TextField source="createdAt" />
+      <TextField source="id" label="EDI Error ID" />
+      <TextField source="paymentRequestID" label="Payment Request ID" />
+      <TextField source="paymentRequestNumber" label="Payment Request Number" />
+      <TextField source="ediType" label="Error Type" />
+      <TextField source="code" label="Error Code" />
+      <TextField source="description" label="Error Description" />
+      <DateField source="createdAt" showTime label="Error Created At" sortable={false} />
     </SimpleShowLayout>
   </Show>
 );
