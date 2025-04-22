ea853f4f863873190997d666892471704e57a953
joeydoyecaci <joseph.doye@caci.com>
2025-04-22 14:22:39 +0000

    Added fix for playwright test

--- src/components/CertificationText/CertificationText.jsx
+++ src/components/CertificationText/CertificationText.jsx
@@ -31,7 +31,7 @@ export const CertificationText = ({ certificationText, onScrollToBottom }) => {
   };
 
   return (
-    <div className="certification_text_box" onScroll={handleScroll}>
+    <div data-testid="certificationTextScrollBox" className="certification_text_box" onScroll={handleScroll}>
       <Box data-testid="certificationTextBox" ref={scrollContainerRef} onScroll={handleScroll}>
         {getTextMarkdown(certificationText)}
       </Box>
