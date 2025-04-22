d5fd44a0834463fae878a991cab05af07bbe0626
Steven Gleason <180579696+stevengleason-caci@users.noreply.github.com>
2025-04-22 14:16:08 +0000

    B-22042 Fix server tests.
    Query for Roles and Privileges was causing test DB connection issues. No need to pull those, just do the delete call. Same for the other tables.
    Don't allow admin users to be deleted.
    Use the txnAppCtx within the transaction.
    Remove privileges from the tests because privilege setup did not work.
    Change 'disable' to 'inctivate'.
    Temporarily replace the server error with fixed text.

--- pkg/services/user/user_deleter_test.go
+++ pkg/services/user/user_deleter_test.go
@@ -13,10 +13,7 @@ import (
 )
 
 func (suite *UserServiceSuite) TestDeleteUser() {
-	queryBuilder := query.NewQueryBuilder()
-	deleter := NewUserDeleter(queryBuilder)
-
-	setupBasicUser := func(userRoleType roles.RoleType) models.User {
+	setupBasicUserWithRole := func(userRoleType roles.RoleType) models.User {
 		userRole := roles.Role{
 			RoleType: userRoleType,
 		}
@@ -38,9 +35,8 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		initialOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		initialAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		initialUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		initialUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 
-		testUser := setupBasicUser(roles.RoleTypeTOO)
+		testUser := setupBasicUserWithRole(roles.RoleTypeTOO)
 
 		// Verify the test data exists
 		var user models.User
@@ -53,26 +49,20 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.NotEmpty(userRoles, "Expected roles for the user after setup")
 
-		var userPrivileges []models.UsersPrivileges
-		// TODO: change or remove for service members
-		//err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		//suite.NoError(err)
-		//suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
-
 		setupUserCount, _ := suite.DB().Count(&models.User{})
 		setupServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		setupOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		setupAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		setupUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		setupUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount+1, setupUserCount)
 		suite.Equal(initialServiceMemberCount, setupServiceMemberCount)
 		suite.Equal(initialOfficeUserCount, setupOfficeUserCount)
 		suite.Equal(initialAdminUserCount, setupAdminUserCount)
 		suite.Equal(initialUserRolesCount+1, setupUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, setupUserPrivilegesCount)
 
 		// Delete the user
+		queryBuilder := query.NewQueryBuilder()
+		deleter := NewUserDeleter(queryBuilder)
 		err = deleter.DeleteUser(suite.AppContextForTest(), testUser.ID)
 		suite.NoError(err)
 
@@ -85,22 +75,16 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.Empty(userRoles, "Expected no roles to remain for the user")
 
-		err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		suite.NoError(err)
-		suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
-
 		finalUserCount, _ := suite.DB().Count(&models.User{})
 		finalServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		finalOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		finalAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		finalUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		finalUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount, finalUserCount)
 		suite.Equal(initialServiceMemberCount, finalServiceMemberCount)
 		suite.Equal(initialOfficeUserCount, finalOfficeUserCount)
 		suite.Equal(initialAdminUserCount, finalAdminUserCount)
 		suite.Equal(initialUserRolesCount, finalUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, finalUserPrivilegesCount)
 	})
 
 	suite.Run("Success - delete an Office User", func() {
@@ -109,24 +93,25 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		initialOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		initialAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		initialUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		initialUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 
-		testUser := factory.BuildDefaultUser(suite.DB())
-		status := models.OfficeUserStatusREQUESTED
-		_ = factory.BuildOfficeUserWithRoles(suite.DB(), []factory.Customization{
+		status := models.OfficeUserStatusAPPROVED
+		userRole := roles.Role{
+			RoleType: roles.RoleTypeTOO,
+		}
+		testOfficeUser := factory.BuildOfficeUser(suite.DB(), []factory.Customization{
 			{
 				Model: models.OfficeUser{
 					Active: true,
-					UserID: &testUser.ID,
-					Email:  testUser.OktaEmail,
 					Status: &status,
 				},
 			},
 			{
-				Model:    testUser,
-				LinkOnly: true,
+				Model: models.User{
+					Roles: []roles.Role{userRole},
+				},
 			},
-		}, []roles.RoleType{roles.RoleTypeTOO})
+		}, nil)
+		testUser := testOfficeUser.User
 
 		// Verify the test data exists
 		var user models.User
@@ -144,26 +129,20 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.NotEmpty(userRoles, "Expected roles for the user after setup")
 
-		// TODO: create privilages in the test data
-		var userPrivileges []models.UsersPrivileges
-		//err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		//suite.NoError(err)
-		//suite.NotEmpty(userPrivileges, "Expected privileges for the user after setup")
-
 		setupUserCount, _ := suite.DB().Count(&models.User{})
 		setupServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		setupOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		setupAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		setupUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		setupUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount+1, setupUserCount)
 		suite.Equal(initialServiceMemberCount, setupServiceMemberCount)
 		suite.Equal(initialOfficeUserCount+1, setupOfficeUserCount)
 		suite.Equal(initialAdminUserCount, setupAdminUserCount)
 		suite.Equal(initialUserRolesCount+1, setupUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, setupUserPrivilegesCount)
 
 		// delete the user
+		queryBuilder := query.NewQueryBuilder()
+		deleter := NewUserDeleter(queryBuilder)
 		err = deleter.DeleteUser(suite.AppContextForTest(), testUser.ID)
 		suite.NoError(err)
 
@@ -180,22 +159,16 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.Empty(userRoles, "Expected no roles to remain for the user")
 
-		err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		suite.NoError(err)
-		suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
-
 		finalUserCount, _ := suite.DB().Count(&models.User{})
 		finalServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		finalOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		finalAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		finalUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		finalUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount, finalUserCount)
 		suite.Equal(initialServiceMemberCount, finalServiceMemberCount)
 		suite.Equal(initialOfficeUserCount, finalOfficeUserCount)
 		suite.Equal(initialAdminUserCount, finalAdminUserCount)
 		suite.Equal(initialUserRolesCount, finalUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, finalUserPrivilegesCount)
 	})
 
 	suite.Run("Success - delete an Admin User", func() {
@@ -204,9 +177,8 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		initialOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		initialAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		initialUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		initialUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 
-		testUser := setupBasicUser(roles.RoleTypeHQ)
+		testUser := setupBasicUserWithRole(roles.RoleTypeHQ)
 		_ = factory.BuildAdminUser(suite.DB(), []factory.Customization{
 			{
 				Model: models.AdminUser{
@@ -237,58 +209,48 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.NotEmpty(userRoles, "Expected roles for the user after setup")
 
-		// TODO: create privilages in the test data
-		var userPrivileges []models.UsersPrivileges
-		//err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		//suite.NoError(err)
-		//suite.NotEmpty(userPrivileges, "Expected privileges for the user after setup")
-
 		setupUserCount, _ := suite.DB().Count(&models.User{})
 		setupServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		setupOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		setupAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		setupUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		setupUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount+1, setupUserCount)
 		suite.Equal(initialServiceMemberCount, setupServiceMemberCount)
 		suite.Equal(initialOfficeUserCount, setupOfficeUserCount)
 		suite.Equal(initialAdminUserCount+1, setupAdminUserCount)
-		suite.Equal(initialUserRolesCount+1, setupUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, setupUserPrivilegesCount)
+		suite.Equal(initialUserRolesCount+2, setupUserRolesCount)
 
 		// delete the user
+		queryBuilder := query.NewQueryBuilder()
+		deleter := NewUserDeleter(queryBuilder)
 		err = deleter.DeleteUser(suite.AppContextForTest(), testUser.ID)
-		suite.NoError(err)
-
-		// Test that the user was deleted
-		err = suite.DB().Where("id = ?", testUser.ID).First(&user)
 		suite.Error(err)
-		suite.Equal(sql.ErrNoRows, err, "sql: no rows in result set")
+		suite.ErrorContains(err, "User is forbidden") // TODO: consider a different code
+		suite.ErrorContains(err, "This is an Admin User and cannot be deleted")
+
+		// Test that the user was not deleted
+		err = suite.DB().Where("id = ?", testUser.ID).First(&user)
+		suite.NoError(err)
+		suite.NotEmpty(user, "Expected the user remains after failed delete")
 
 		err = suite.DB().Where("user_id = ?", testUser.ID).First(&adminUser)
-		suite.Error(err)
-		suite.Equal(sql.ErrNoRows, err, "sql: no rows in result set")
+		suite.NoError(err)
+		suite.NotEmpty(adminUser, "Expected the admin user to remain after failed delete")
 
 		err = suite.DB().Where("user_id = ?", testUser.ID).All(&userRoles)
 		suite.NoError(err)
-		suite.Empty(userRoles, "Expected no roles to remain for the user")
-
-		err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		suite.NoError(err)
-		suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
+		suite.NotEmpty(userRoles, "Expected roles to remain after failed delete")
 
 		finalUserCount, _ := suite.DB().Count(&models.User{})
 		finalServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		finalOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		finalAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		finalUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		finalUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
-		suite.Equal(initialUserCount, finalUserCount)
-		suite.Equal(initialServiceMemberCount, finalServiceMemberCount)
-		suite.Equal(initialOfficeUserCount, finalOfficeUserCount)
-		suite.Equal(initialAdminUserCount, finalAdminUserCount)
-		suite.Equal(initialUserRolesCount, finalUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, finalUserPrivilegesCount)
+		suite.Equal(setupUserCount, finalUserCount)
+		suite.Equal(setupServiceMemberCount, finalServiceMemberCount)
+		suite.Equal(setupOfficeUserCount, finalOfficeUserCount)
+		suite.Equal(setupAdminUserCount, finalAdminUserCount)
+		suite.Equal(setupUserRolesCount, finalUserRolesCount)
 	})
 
 	suite.Run("error - a customer user has a move and cannot be deleted", func() {
@@ -297,9 +259,8 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		initialOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		initialAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		initialUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		initialUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 
-		testUser := setupBasicUser(roles.RoleTypeCustomer)
+		testUser := setupBasicUserWithRole(roles.RoleTypeCustomer)
 		_ = factory.BuildMove(suite.DB(), []factory.Customization{
 			{
 				Model:    testUser,
@@ -323,26 +284,20 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.NotEmpty(userRoles, "Expected roles for the user after setup")
 
-		//var userPrivileges []models.UsersPrivileges
-		// TODO: change or remove for service members
-		//err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		//suite.NoError(err)
-		//suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
-
 		setupUserCount, _ := suite.DB().Count(&models.User{})
 		setupServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		setupOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		setupAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		setupUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		setupUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(initialUserCount+1, setupUserCount)
 		suite.Equal(initialServiceMemberCount+1, setupServiceMemberCount)
 		suite.Equal(initialOfficeUserCount, setupOfficeUserCount)
 		suite.Equal(initialAdminUserCount, setupAdminUserCount)
 		suite.Equal(initialUserRolesCount+1, setupUserRolesCount)
-		suite.Equal(initialUserPrivilegesCount, setupUserPrivilegesCount)
 
 		// Delete the user
+		queryBuilder := query.NewQueryBuilder()
+		deleter := NewUserDeleter(queryBuilder)
 		err = deleter.DeleteUser(suite.AppContextForTest(), testUser.ID)
 		suite.Error(err)
 
@@ -359,28 +314,24 @@ func (suite *UserServiceSuite) TestDeleteUser() {
 		suite.NoError(err)
 		suite.NotEmpty(userRoles, "Expected roles for the user remain after failed delete")
 
-		//err = suite.DB().Where("user_id = ?", testUser.ID).All(&userPrivileges)
-		//suite.NoError(err)
-		//suite.Empty(userPrivileges, "Expected no privileges to remain for the user")
-
 		finalUserCount, _ := suite.DB().Count(&models.User{})
 		finalServiceMemberCount, _ := suite.DB().Count(&models.ServiceMember{})
 		finalOfficeUserCount, _ := suite.DB().Count(&models.OfficeUser{})
 		finalAdminUserCount, _ := suite.DB().Count(&models.AdminUser{})
 		finalUserRolesCount, _ := suite.DB().Count(&models.UsersRoles{})
-		finalUserPrivilegesCount, _ := suite.DB().Count(&models.UsersPrivileges{})
 		suite.Equal(setupUserCount, finalUserCount)
 		suite.Equal(setupServiceMemberCount, finalServiceMemberCount)
 		suite.Equal(setupOfficeUserCount, finalOfficeUserCount)
 		suite.Equal(setupAdminUserCount, finalAdminUserCount)
 		suite.Equal(setupUserRolesCount, finalUserRolesCount)
-		suite.Equal(setupUserPrivilegesCount, finalUserPrivilegesCount)
 	})
 
 	suite.Run("error - a user is not found", func() {
 		userID := uuid.Must(uuid.NewV4())
 		expectedError := apperror.NewNotFoundError(userID, "while looking for User")
 
+		queryBuilder := query.NewQueryBuilder()
+		deleter := NewUserDeleter(queryBuilder)
 		err := deleter.DeleteUser(suite.AppContextForTest(), userID)
 		suite.Error(err)
 		suite.Equal(err, expectedError)
