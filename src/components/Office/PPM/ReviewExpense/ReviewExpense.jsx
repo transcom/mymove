083166239cfaa0c7f6fc0bbe76d314bfe40b3f13
Tae Jung <tae.jung@caci.com>
2025-04-23 14:54:51 +0000

    deletes

--- src/components/Office/PPM/ReviewExpense/ReviewExpense.jsx
+++ src/components/Office/PPM/ReviewExpense/ReviewExpense.jsx
@@ -35,7 +35,7 @@ import TextField from 'components/form/fields/TextField/TextField';
 import { LOCATION_TYPES } from 'types/sitStatusShape';
 import SitCost from 'components/Office/PPM/SitCost/SitCost';
 import { useGetPPMSITEstimatedCostQuery } from 'hooks/queries';
-import SmallPackageForm from 'components/Customer/PPM/Closeout/SmallPackageForm/SmallPackageForm';
+import SmallPackageForm from 'components/Shared/PPM/Closeout/SmallPackageForm/SmallPackageForm';
 
 const sitLocationOptions = dropdownInputOptions(LOCATION_TYPES);
 
