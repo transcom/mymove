690c7121234ed4cb3a1ee2697b4b65ab3c7e7762
Paul Stonebraker <paul.stonebraker@caci.com>
2025-05-07 17:13:09 +0000

    update table migration to use existing alter ppm shipments file

--- migrations/app/ddl_migrations/ddl_tables/20250224200700_tbl_ppm_shipments.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224200700_tbl_ppm_shipments.up.sql
@@ -1,3 +1,8 @@
 -- B-22653 Daniel Jordan add ppm_type column to ppm_shipments
+-- B-22945 Paul Stonebraker remove actual postal code columns from ppm_shipments table
 ALTER TABLE ppm_shipments
-ADD COLUMN IF NOT EXISTS ppm_type ppm_shipment_type NOT NULL DEFAULT 'INCENTIVE_BASED';
+    ADD COLUMN IF NOT EXISTS ppm_type ppm_shipment_type NOT NULL DEFAULT 'INCENTIVE_BASED';
+
+ALTER TABLE ppm_shipments
+    DROP COLUMN IF EXISTS actual_pickup_postal_code,
+    DROP COLUMN IF EXISTS actual_destination_postal_code;
