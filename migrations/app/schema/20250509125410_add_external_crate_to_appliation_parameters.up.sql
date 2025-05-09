d410f46893a7df10c531d638d11a00624445d556
Cory Kleinjan <cory.kleinjan@caci.com>
2025-05-09 14:37:20 +0000

    fixing uuid issue

--- migrations/app/schema/20250509125410_add_external_crate_to_appliation_parameters.up.sql
+++ migrations/app/schema/20250509125410_add_external_crate_to_appliation_parameters.up.sql
@@ -1,4 +1,4 @@
 INSERT into application_parameters
     (id, parameter_name, parameter_value)
 VALUES
-    ('61a6c498-42d3-4b49-b368-b6165cd1ce18', 'externalCrateMinimum', '4.0');
+    ('a966dc40-21ac-4856-9aea-2febdf60a105', 'externalCrateMinimum', '4.0');
