dfdb42255dc72f043b782608a5e2a316f34592fc
Tae Jung <tae.jung@caci.com>
2025-05-15 18:11:43 +0000

    migration fix

--- migrations/app/ddl_migrations/ddl_tables/20250221195354_tbl_alter_entitlements_B-22651.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250221195354_tbl_alter_entitlements_B-22651.up.sql
@@ -1,7 +1,7 @@
 --B-22651   Maria Traskowsky    Add ub_weight_restriction column to entitlements table
 --B-23342   Tae Jung    Add gun_safe_weight column to entitlements table
 ALTER TABLE entitlements
-ADD COLUMN IF NOT EXISTS ub_weight_restriction int;
+ADD COLUMN IF NOT EXISTS ub_weight_restriction int,
 ADD COLUMN IF NOT EXISTS gun_safe_weight integer DEFAULT 0 NOT NULL CHECK (gun_safe_weight >= 0);
 
 COMMENT ON COLUMN entitlements.weight_restriction IS 'The weight restriction of the entitlement.';
--- migrations/app/ddl_migrations/ddl_tables/20250224200700_tbl_ppm_shipments.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224200700_tbl_ppm_shipments.up.sql
@@ -1,7 +1,7 @@
 -- B-22653 Daniel Jordan add ppm_type column to ppm_shipments
 -- B-23342 Tae Jung add has_gun_safe and gun_safe_weight columns to ppm_shipments
 ALTER TABLE ppm_shipments
-ADD COLUMN IF NOT EXISTS ppm_type ppm_shipment_type NOT NULL DEFAULT 'INCENTIVE_BASED';
+ADD COLUMN IF NOT EXISTS ppm_type ppm_shipment_type NOT NULL DEFAULT 'INCENTIVE_BASED',
 ADD COLUMN IF NOT EXISTS has_gun_safe bool,
 ADD COLUMN IF NOT EXISTS gun_safe_weight int4 CHECK (gun_safe_weight >= 0);
 
