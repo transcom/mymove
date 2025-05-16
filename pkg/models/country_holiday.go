c17d880596e4202b45bbd6f72cc3915859b3a652
Brian Manley <brian.manley@caci.com>
2025-05-16 18:07:36 +0000

    B-23575 peer review improvements

--- pkg/models/country_holiday.go
+++ pkg/models/country_holiday.go
@@ -9,7 +9,7 @@ import (
 type CountryHoliday struct {
 	ID              uuid.UUID `json:"id" db:"id" rw:"r"`
 	CountryId       uuid.UUID `json:"country_id" db:"country_id" rw:"r"`
-	Country         Country   `belongs_to:"re_countries" fk_id:"country_id" rw:"r"`
+	Country         *Country  `belongs_to:"re_countries" fk_id:"country_id" rw:"r"`
 	HolidayName     string    `json:"holiday_name" db:"holiday_name" rw:"r"`
 	ObservationDate time.Time `json:"observation_date" db:"observation_date" rw:"r"`
 	CreatedAt       time.Time `json:"created_at" db:"created_at" rw:"r"`
@@ -20,5 +20,5 @@ func (c CountryHoliday) TableName() string {
 	return "country_holidays"
 }
 
-// CountryHolidays is a slice containing CountryHolidays
+// CountryHolidays is a slice containing CountryHoliday
 type CountryHolidays []CountryHoliday
