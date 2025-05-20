d67b4afd38f3949e6a9638bd134c4f27801d8ce3
Daniel Reither <daniel.reither@caci.com>
2025-05-20 19:02:07 +0000

    change made to avoid merge conflict

--- playwright/tests/my/milmove/ppms/customerPpmTestFixture.js
+++ playwright/tests/my/milmove/ppms/customerPpmTestFixture.js
@@ -159,7 +159,13 @@ export class CustomerPpmPage extends CustomerPage {
   async fillOutAboutPage(options = { selectAdvance: false }) {
     // editing this field with the keyboard instead of the date picker runs async validators for pre-filled postal codes
     // this helps debounce the API calls that would be triggered in quick succession
-    await this.page.locator('input[name="actualMoveDate"]').fill('01 Feb 2025');
+
+    // Set the expected departure date to today's date formatted as "DD MMM YYYY"
+    const today = new Date();
+    const formattedDate = `${today.getDate().toString().padStart(2, '0')} ${today.toLocaleString('en-US', {
+      month: 'short',
+    })} ${today.getFullYear()}`;
+    await this.page.locator('input[name="actualMoveDate"]').fill(formattedDate);
 
     const pickupLocation = 'YUMA, AZ 85364 (YUMA)';
     const destinationLocation = 'YUMA, AZ 85366 (YUMA)';
