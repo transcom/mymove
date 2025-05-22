6b9c009e8c7e7a7217c5ffe7b518a3a9d7cb9732
Michael Inthavongsay <m.inthavongsay@caci.com>
2025-05-22 21:40:15 +0000

    Add mileage to pricing calculation when over 50 miles. Calculation was missing mileage multiplier after PO demo verificationfor pickup/delivery SIT

--- migrations/app/ddl_migrations/ddl_functions/20250221213413_fn_update_service_item_pricing.up.sql
+++ migrations/app/ddl_migrations/ddl_functions/20250221213413_fn_update_service_item_pricing.up.sql
@@ -206,20 +206,28 @@ BEGIN
                     escalated_price := calculate_escalated_price(NULL, d_rate_area_id, service_item.re_service_id, contract_id, service_code, shipment.requested_pickup_date, distance);
                 END IF;
 
-                IF escalated_price IS NOT NULL AND shipment.prime_estimated_weight IS NOT NULL THEN
+                IF distance IS NOT NULL AND escalated_price IS NOT NULL AND shipment.prime_estimated_weight IS NOT NULL THEN
                     RAISE NOTICE ''escalated_price = $% cents'', escalated_price;
 
-                    -- multiply by 110% of estimated weight
-                    estimated_price := ROUND((escalated_price * (shipment.prime_estimated_weight * 1.1) / 100), 2) * 100;
+                    IF distance > 50 THEN
+                        -- multiply by 110% of estimated weight
+                        -- multiply by mileage
+                        estimated_price := ROUND((escalated_price * (shipment.prime_estimated_weight * 1.1) / 100) * distance, 2) * 100;
 
-                    RAISE NOTICE ''%: Received estimated price of % (% * (% * 1.1) / 100)) cents'', service_code, estimated_price, escalated_price, shipment.prime_estimated_weight;
+                        RAISE NOTICE ''%: Received estimated price of % (% * (% * 1.1) / 100) * %) cents'', service_code, estimated_price, escalated_price, shipment.prime_estimated_weight, distance;
+                    ELSE
+                        -- multiply by 110% of estimated weight
+                        estimated_price := ROUND((escalated_price * (shipment.prime_estimated_weight * 1.1) / 100), 2) * 100;
+
+                        RAISE NOTICE ''%: Received estimated price of % (% * (% * 1.1) / 100)) cents'', service_code, estimated_price, escalated_price, shipment.prime_estimated_weight;
+                    END IF;
 
                     -- update the pricing_estimate value in mto_service_items
                     UPDATE mto_service_items
                     SET pricing_estimate = estimated_price
                     WHERE id = service_item.id;
                 ELSE
-                    RAISE NOTICE ''service_code: % - Failed to compute pricing[escalated_price: %, prime_estimated_weight: %]'', service_code, escalated_price, shipment.prime_estimated_weight;
+                    RAISE NOTICE ''service_code: % - Failed to compute pricing[escalated_price: %, prime_estimated_weight: %, distance: %]'', service_code, escalated_price, shipment.prime_estimated_weight, distance;
                 END IF;
 
             ELSE
