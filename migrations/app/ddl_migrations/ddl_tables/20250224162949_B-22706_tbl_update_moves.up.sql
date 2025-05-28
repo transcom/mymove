2feb25d498acca45a8e166de81289f95e47647f9
Paul Stonebraker <paul.stonebraker@caci.com>
2025-05-28 05:01:22 +0000

    add sc_assigned_id back to move model

--- migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
@@ -9,13 +9,10 @@ ALTER TABLE moves
         CONSTRAINT moves_sc_closeout_assigned_id_fkey
             REFERENCES office_users;
 
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.columns
-               WHERE table_name = 'moves' AND column_name = 'sc_assigned_id') THEN
-        ALTER TABLE moves RENAME COLUMN sc_assigned_id TO sc_counseling_assigned_id;
-    END IF;
-END $$;
+ALTER TABLE moves
+    ADD COLUMN IF NOT EXISTS sc_counseling_assigned_id uuid
+        CONSTRAINT moves_sc_counseling_assigned_id_fkey
+            REFERENCES office_users;
 
 COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID of the Task Ordering Officer on the office_users table';
 COMMENT ON COLUMN moves.sc_counseling_assigned_id IS 'A foreign key that points to the ID on the office_users table of the counseling queue assigned office user';
