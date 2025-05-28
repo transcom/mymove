b4a45f6ade2f2b71c17b20572500425611b2798d
Daniel Jordan <daniel.jordan@caci.com>
2025-05-28 19:14:03 +0000

    do it for SC too

--- src/pages/Office/ServicesCounselingOrders/ServicesCounselingOrders.jsx
+++ src/pages/Office/ServicesCounselingOrders/ServicesCounselingOrders.jsx
@@ -332,7 +332,12 @@ const ServicesCounselingOrders = ({ files, amendedDocumentId, updateAmendedDocum
 
   return (
     <div className={styles.sidebar}>
-      <Formik initialValues={initialValues} validationSchema={ordersFormValidationSchema} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={ordersFormValidationSchema}
+        onSubmit={onSubmit}
+        validateOnChange
+      >
         {(formik) => {
           const hhgTacWarning = tacValidationState[LOA_TYPE.HHG].isValid ? '' : tacWarningMsg;
           const ntsTacWarning = tacValidationState[LOA_TYPE.NTS].isValid ? '' : tacWarningMsg;
