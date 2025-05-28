59f5a775c73942de219032b0196296e7e162486e
Paul Stonebraker <paul.stonebraker@caci.com>
2025-05-28 04:21:14 +0000

    test

--- migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
@@ -1,12 +1,7 @@
--- B-23698 Paul Stonebraker add sc_closeout_assigned_id columm, sc_counseling_assigned_id column, too_task_order_assigned_id column, tio_payment_requests_assigned_id
+-- B-23581 Paul Stonebraker add sc_closeout_assigned_id columm, rename sc_assigned_id to sc_counseling_assigned_id
 ALTER TABLE moves
     ADD COLUMN IF NOT EXISTS too_destination_assigned_id uuid
         CONSTRAINT moves_too_destination_assigned_id_fkey
-            REFERENCES office_users;
-
-ALTER TABLE moves
-    ADD COLUMN IF NOT EXISTS too_task_order_assigned_id uuid
-        CONSTRAINT moves_too_task_order_assigned_id_fkey
             REFERENCES office_users;
 
 ALTER TABLE moves
@@ -14,18 +9,14 @@ ALTER TABLE moves
         CONSTRAINT moves_sc_closeout_assigned_id_fkey
             REFERENCES office_users;
 
-ALTER TABLE moves
-    ADD COLUMN IF NOT EXISTS sc_counseling_assigned_id uuid
-        CONSTRAINT moves_sc_counseling_assigned_id_fkey
-            REFERENCES office_users;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns
+               WHERE table_name = 'moves' AND column_name = 'sc_assigned_id') THEN
+        ALTER TABLE moves RENAME COLUMN sc_assigned_id TO sc_counseling_assigned_id;
+    END IF;
+END $$;
 
-ALTER TABLE moves
-    ADD COLUMN IF NOT EXISTS tio_payment_requests_assigned_id uuid
-        CONSTRAINT moves_tio_payment_requests_assigned_id_fkey
-            REFERENCES office_users;
-
-COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID on the office_users table of the destination requests queue assigned office user';
-COMMENT ON COLUMN moves.too_task_order_assigned_id IS 'A foreign key that points to the ID on the office_users table of the task order queue assigned office user';
+COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID of the Task Ordering Officer on the office_users table';
 COMMENT ON COLUMN moves.sc_counseling_assigned_id IS 'A foreign key that points to the ID on the office_users table of the counseling queue assigned office user';
 COMMENT ON COLUMN moves.sc_closeout_assigned_id IS 'A foreign key that points to the ID on the office_users table of the closeout queue assigned office user';
-COMMENT ON COLUMN moves.tio_payment_requests_assigned_id IS 'A foreign key that points to the ID on the office_users table of the payment requests queue assigned office user';
