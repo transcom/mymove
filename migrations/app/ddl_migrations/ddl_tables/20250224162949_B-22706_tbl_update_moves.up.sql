7d2962a3b0c9806aee5a20ec11892b680985f8e4
Paul Stonebraker <paul.stonebraker@caci.com>
2025-05-28 12:57:46 +0000

    add cols to moves table

--- migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
+++ migrations/app/ddl_migrations/ddl_tables/20250224162949_B-22706_tbl_update_moves.up.sql
@@ -1,7 +1,12 @@
--- B-23581 Paul Stonebraker add sc_closeout_assigned_id columm, rename sc_assigned_id to sc_counseling_assigned_id
+-- B-23698 Paul Stonebraker add sc_closeout_assigned_id columm, sc_counseling_assigned_id column, too_task_order_assigned_id column, tio_payment_requests_assigned_id
 ALTER TABLE moves
     ADD COLUMN IF NOT EXISTS too_destination_assigned_id uuid
         CONSTRAINT moves_too_destination_assigned_id_fkey
+            REFERENCES office_users;
+
+ALTER TABLE moves
+    ADD COLUMN IF NOT EXISTS too_task_order_assigned_id uuid
+        CONSTRAINT moves_too_task_order_assigned_id_fkey
             REFERENCES office_users;
 
 ALTER TABLE moves
@@ -15,10 +20,17 @@ ALTER TABLE moves
             REFERENCES office_users;
 
 ALTER TABLE moves
+    ADD COLUMN IF NOT EXISTS tio_payment_requests_assigned_id uuid
+        CONSTRAINT moves_tio_payment_requests_assigned_id_fkey
+            REFERENCES office_users;
+
+ALTER TABLE moves
     ADD COLUMN IF NOT EXISTS sc_assigned_id uuid
         CONSTRAINT moves_sc_assigned_id_fkey
             REFERENCES office_users;
 
-COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID of the Task Ordering Officer on the office_users table';
+COMMENT ON COLUMN moves.too_destination_assigned_id IS 'A foreign key that points to the ID on the office_users table of the destination requests queue assigned office user';
+COMMENT ON COLUMN moves.too_task_order_assigned_id IS 'A foreign key that points to the ID on the office_users table of the task order queue assigned office user';
 COMMENT ON COLUMN moves.sc_counseling_assigned_id IS 'A foreign key that points to the ID on the office_users table of the counseling queue assigned office user';
 COMMENT ON COLUMN moves.sc_closeout_assigned_id IS 'A foreign key that points to the ID on the office_users table of the closeout queue assigned office user';
+COMMENT ON COLUMN moves.tio_payment_requests_assigned_id IS 'A foreign key that points to the ID on the office_users table of the payment requests queue assigned office user';
