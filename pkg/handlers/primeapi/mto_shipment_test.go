b704de562b4a202ba3e3a2ddfd17b12cb9e2305c
Brooklyn Welsh <brooklyn.welsh@caci.com>
2025-05-29 21:40:26 +0000

    Revert "missed config?"
    This reverts commit 5b445532923abd19c6e35bb8b8326a37cfe8cc82.

--- pkg/handlers/primeapi/mto_shipment_test.go
+++ pkg/handlers/primeapi/mto_shipment_test.go
@@ -213,7 +213,7 @@ func (suite *HandlerSuite) TestUpdateShipmentDestinationAddressHandler() {
 		subtestData := makeSubtestData()
 		mockCreator := mocks.ShipmentAddressUpdateRequester{}
 		handler := UpdateShipmentDestinationAddressHandler{
-			suite.NewHandlerConfig(),
+			suite.HandlerConfig(),
 			&mockCreator,
 			vLocationServices,
 		}
