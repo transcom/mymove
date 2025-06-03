7e15cba999626e051500bc38e38efe58d74ba5b7
antgmann <anthony.mann@caci.com>
2025-06-03 09:55:02 -0400

    INT-only fix linter

--- playwright/tests/my/mymove/additionalDocs.spec.js
+++ playwright/tests/my/mymove/additionalDocs.spec.js
@@ -1,7 +1,6 @@
 import { test, expect } from '../../utils/my/customerTest';
 
 test.describe('Additional Documents', () => {
-  
   test('Users can download documents uploaded to Additional Documents', async ({ page, customerPage }) => {
     // Generate a move that has the status of SUBMITTED
     const move = await customerPage.testHarness.buildSubmittedMoveWithPPMShipmentForSC();
