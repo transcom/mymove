9ab15b0179a4cb6a0fa48bc689b934f802915fb3
Jon Spight <Jonathan.Spight@caci.com>
2025-06-05 15:21:38 +0000

    fixed spacing

--- migrations/app/schema/20250422164417_B-23147_add_ranks.up.sql
+++ migrations/app/schema/20250422164417_B-23147_add_ranks.up.sql
@@ -207,6 +207,7 @@ begin
 			where pg.grade = o.grade
 			  and o.service_member_id = sm.id)
 	loop
+
 		select count(*) into v_count
 		  from ranks
 		 where pay_grade_id = i.pay_grade_id
@@ -219,6 +220,9 @@ begin
 			   and p.pay_grade_id = i.pay_grade_id
 			   and p.affiliation = i.affiliation
 			   and o.rank_id is null;
+
 		end if;
+
 	end loop;
+
 end ';
