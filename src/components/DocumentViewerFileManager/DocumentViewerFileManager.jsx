fd61239ea1d415ba01fe97d91d82b3ed7ab42bc9
Daniel Jordan <daniel.jordan@caci.com>
2025-06-12 13:41:58 +0000

    fixing buttons in documeentViewFileManager per PR feedback

--- src/components/DocumentViewerFileManager/DocumentViewerFileManager.jsx
+++ src/components/DocumentViewerFileManager/DocumentViewerFileManager.jsx
@@ -272,7 +272,11 @@ const DocumentViewerFileManager = ({
         />
       )}
       {!isExpandedView && !useChevron && (
-        <Button disabled={isFileProcessing || fileUploadRequired} onClick={toggleUploadVisibility}>
+        <Button
+          disabled={isFileProcessing || fileUploadRequired}
+          onClick={toggleUploadVisibility}
+          className={styles.fullWidth}
+        >
           {buttonHeaderText}
         </Button>
       )}
@@ -314,9 +318,11 @@ const DocumentViewerFileManager = ({
               />
               <Hint>PDF, JPG, or PNG only. Maximum file size 25MB. Each page must be clear and legible</Hint>
               {!isExpandedView && (
-                <Button disabled={isFileProcessing || fileUploadRequired} onClick={toggleUploadVisibility}>
-                  Done
-                </Button>
+                <div className={styles.flexRight}>
+                  <Button disabled={isFileProcessing || fileUploadRequired} onClick={toggleUploadVisibility}>
+                    Done
+                  </Button>
+                </div>
               )}
             </div>
           </>
