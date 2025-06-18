f21b37dbfce4f2e98b28996b92fae35b7b150963
Daniel Reither <daniel.reither@caci.com>
2025-06-18 11:40:20 -0500

    matching this branch to INT for these files with multimove flag removed.

--- playwright/tests/my/milmove/ppms/progear.spec.js
+++ playwright/tests/my/milmove/ppms/progear.spec.js
@@ -12,47 +12,6 @@ test.describe('Progear', () => {
     test.beforeEach(async ({ customerPpmPage }) => {
       const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMProgearWeightTicket();
       await customerPpmPage.signInForPPMWithMove(move);
-      await customerPpmPage.navigateToProgearPage();
-    });
-
-    test(`progear page loads`, async ({ customerPpmPage, page }) => {
-      await customerPpmPage.submitProgearPage({ belongsToSelf: true });
-
-      const set2Heading = page.getByRole('heading', { name: 'Set 2' });
-      await expect(set2Heading).toBeVisible();
-      const progearSection = set2Heading.locator('../..').last();
-      await expect(progearSection).toContainText('Pro-gear');
-      await expect(progearSection).toContainText('Radio equipment');
-      // contains won't work when text content is divided between multiple semantic html elements
-      await expect(progearSection).toContainText('Weight:');
-      await expect(progearSection).toContainText('2,000 lbs');
-    });
-  });
-});
-
-test.describe('Progear', () => {
-  forEachViewport(async () => {
-    test.beforeEach(async ({ customerPpmPage }) => {
-      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
-      await customerPpmPage.signInForPPMWithMove(move);
-      await customerPpmPage.clickOnGoToMoveButton();
-      await customerPpmPage.navigateToAboutPage();
-      await customerPpmPage.navigateFromWeightTicketPageBadUpload();
-      await customerPpmPage.navigateFromCloseoutReviewPageToAddProGearPage();
-    });
-
-    test('Test upload of wrong xlsx file to progear page', async ({ customerPpmPage }) => {
-      await customerPpmPage.selectMeProGear();
-      await customerPpmPage.submitIncorrectXlsxFileForProGear();
-    });
-  });
-});
-
-test.describe('(MultiMove) Progear', () => {
-  forEachViewport(async () => {
-    test.beforeEach(async ({ customerPpmPage }) => {
-      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMProgearWeightTicket();
-      await customerPpmPage.signInForPPMWithMove(move);
       await customerPpmPage.clickOnGoToMoveButton();
       await customerPpmPage.navigateToPPMReviewPageWithCompletePPM();
       await customerPpmPage.navigateFromCloseoutReviewPageToProGearPage();
--- playwright/tests/my/milmove/ppms/weightTickets.spec.js
+++ playwright/tests/my/milmove/ppms/weightTickets.spec.js
@@ -12,47 +12,6 @@ test.describe('About Your PPM', () => {
     test.beforeEach(async ({ customerPpmPage }) => {
       const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMWithAboutFormComplete();
       await customerPpmPage.signInForPPMWithMove(move);
-      await customerPpmPage.navigateToWeightTicketPage();
-    });
-
-    test('proceed with weight ticket documents', async ({ customerPpmPage }) => {
-      await customerPpmPage.submitWeightTicketPage();
-    });
-
-    test('proceed with claiming trailer', async ({ customerPpmPage }) => {
-      await customerPpmPage.submitWeightTicketPage({ hasTrailer: true, ownTrailer: true });
-    });
-
-    test('proceed without claiming trailer', async ({ customerPpmPage }) => {
-      await customerPpmPage.submitWeightTicketPage({ hasTrailer: true, ownTrailer: false });
-    });
-
-    test('proceed with constructed weight ticket documents', async ({ customerPpmPage }) => {
-      await customerPpmPage.submitWeightTicketPage({ useConstructedWeight: true });
-    });
-  });
-});
-
-test.describe('About You PPM with incorrect Xlsx Upload', () => {
-  forEachViewport(async () => {
-    test.beforeEach(async ({ customerPpmPage }) => {
-      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPM();
-      await customerPpmPage.signInForPPMWithMove(move);
-      await customerPpmPage.clickOnGoToMoveButton();
-      await customerPpmPage.navigateToAboutPage({ selectAdvance: false });
-    });
-
-    test('Fill out weight ticket with incorrect Xlsx document', async ({ customerPpmPage }) => {
-      await customerPpmPage.submitIncorrectXlsxFileForWeightTicketPage();
-    });
-  });
-});
-
-test.describe('(MultiMove) About Your PPM', () => {
-  forEachViewport(async () => {
-    test.beforeEach(async ({ customerPpmPage }) => {
-      const move = await customerPpmPage.testHarness.buildApprovedMoveWithPPMWithAboutFormComplete();
-      await customerPpmPage.signInForPPMWithMove(move);
       await customerPpmPage.clickOnGoToMoveButton();
       await customerPpmPage.navigateToWeightTicketPage();
     });
