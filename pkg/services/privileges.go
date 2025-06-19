0b74a7edeb418d7f98ddf4c9b3c07a0fb8a58786
msaki-caci <michael.saki@caci.com>
2025-06-19 19:39:38 +0000

    MAIN-B-23183 Remove outdated comment

--- pkg/services/privileges.go
+++ pkg/services/privileges.go
@@ -5,8 +5,6 @@ import (
 	"github.com/transcom/mymove/pkg/models/roles"
 )
 
-// PrivilegeFetcher is the service object interface for fetching privileges for a user id
-//
 //go:generate mockery --name PrivilegeFetcher
 type PrivilegeFetcher interface {
 	FetchPrivilegeTypes(appCtx appcontext.AppContext) ([]roles.PrivilegeType, error)
