f8fbc90ef40803d94c2f8089ff07997f7af4a0cd
Beth Grohmann <beth.grohmann@caci.com>
2025-06-25 14:01:06 -0500

    Update syntax

--- migrations/app/ddl_migrations/ddl_functions/20250624215416_fn_drop_calculate_ihpk_iubpk_price.up.sql
+++ migrations/app/ddl_migrations/ddl_functions/20250624215416_fn_drop_calculate_ihpk_iubpk_price.up.sql
@@ -1,2 +1,2 @@
 -- B-23853 Beth Grohmann  Drop unused function - this is handled in calculate_escalated_price
-DROP FUNCTION calculate_ihpk_iubpk_price;
+DROP FUNCTION IF EXISTS calculate_ihpk_iubpk_price;
