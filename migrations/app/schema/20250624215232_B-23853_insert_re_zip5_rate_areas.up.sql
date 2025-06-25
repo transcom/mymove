357cf16d848569160435a58377477d339c6ee6b1
Beth Grohmann <beth.grohmann@caci.com>
2025-06-25 14:05:07 -0500

    Update 20250624215232_B-23853_insert_re_zip5_rate_areas.up.sql

--- migrations/app/schema/20250624215232_B-23853_insert_re_zip5_rate_areas.up.sql
+++ migrations/app/schema/20250624215232_B-23853_insert_re_zip5_rate_areas.up.sql
@@ -1,2 +1,2 @@
 --insert missing zip5 rate area association for Titusville, FL
-insert into re_zip5_rate_areas values (‘1f272c56-7a97-4bae-9a9f-2a6f46047bea’, 'ddd74fb8-c0f1-41a9-9d4f-234bd295ae1a','32780',now(),now(),'070f7c82-fad0-4ae8-9a83-5de87a56472e');
+insert into re_zip5_rate_areas values ('1f272c56-7a97-4bae-9a9f-2a6f46047bea', 'ddd74fb8-c0f1-41a9-9d4f-234bd295ae1a','32780',now(),now(),'070f7c82-fad0-4ae8-9a83-5de87a56472e');
